aa45cc1319dcc11110cbe9173d2bd119fd4a1757
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:29:44 +0200

    sulong: interop/construct002 behavior differs on darwin with newer clang versions

--- dataset/GitHub_Java/oracle.graal/sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/LLVMInteropTest.java
+++ dataset/GitHub_Java/oracle.graal/sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/LLVMInteropTest.java
@@ -31,7 +31,6 @@ package com.oracle.truffle.llvm.tests.interop;
 
 import com.oracle.truffle.api.TruffleOptions;
 import com.oracle.truffle.api.dsl.Cached;
-import com.oracle.truffle.api.dsl.CachedContext;
 import com.oracle.truffle.api.interop.ArityException;
 import com.oracle.truffle.api.interop.InteropException;
 import com.oracle.truffle.api.interop.InteropLibrary;
@@ -41,7 +40,6 @@ import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
-import com.oracle.truffle.llvm.runtime.LLVMContext;
 import com.oracle.truffle.llvm.runtime.LLVMLanguage;
 import com.oracle.truffle.llvm.runtime.NFIContextExtension;
 import com.oracle.truffle.llvm.tests.SulongSuite;
@@ -1109,14 +1107,13 @@ public class LLVMInteropTest {
             return new ArrayObject("foo");
         }
 
-        static Object getTestToNative(LLVMContext context) {
-            return context.getEnv().importSymbol("test_to_native");
+        static TruffleObject getTestToNative() {
+            return (TruffleObject) LLVMLanguage.getLLVMContextReference().get().getEnv().importSymbol("test_to_native");
         }
 
-        @ExportMessage(limit = "3")
-        @SuppressWarnings("unused")
-        void toNative(@CachedContext(LLVMLanguage.class) LLVMContext context,
-                        @Cached(value = "getTestToNative(context)", allowUncached = true) Object testToNative,
+        @ExportMessage
+        void toNative(
+                        @Cached(value = "getTestToNative()", allowUncached = true) TruffleObject testToNative,
                         @CachedLibrary("testToNative") InteropLibrary interop) {
             try {
                 interop.execute(testToNative, this);
