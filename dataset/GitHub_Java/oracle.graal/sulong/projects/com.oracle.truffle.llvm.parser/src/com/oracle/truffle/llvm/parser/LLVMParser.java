4353c2965260eeadebe4e4f7e6c2dd06d955eb82
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:07:44 +0200

    Sulong: look for target of alias only in the file scope.

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/LLVMParser.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/LLVMParser.java
@@ -79,33 +79,36 @@ public final class LLVMParser {
         List<GlobalVariable> definedGlobals = new ArrayList<>();
         List<FunctionSymbol> externalFunctions = new ArrayList<>();
         List<FunctionSymbol> definedFunctions = new ArrayList<>();
+        List<String> importedSymbols = new ArrayList<>();
 
-        defineGlobals(module.getGlobalVariables(), definedGlobals, externalGlobals);
-        defineFunctions(module, definedFunctions, externalFunctions, targetDataLayout);
-        defineAliases(module.getAliases());
+        defineGlobals(module.getGlobalVariables(), definedGlobals, externalGlobals, importedSymbols);
+        defineFunctions(module, definedFunctions, externalFunctions, importedSymbols, targetDataLayout);
+        defineAliases(module.getAliases(), importedSymbols);
 
         LLVMSymbolReadResolver symbolResolver = new LLVMSymbolReadResolver(runtime, StackManager.createRootFrame(), GetStackSpaceFactory.createAllocaFactory(), targetDataLayout, false);
         createDebugInfo(module, symbolResolver);
-        return new LLVMParserResult(runtime, definedFunctions, externalFunctions, definedGlobals, externalGlobals, targetDataLayout);
+        return new LLVMParserResult(runtime, definedFunctions, externalFunctions, definedGlobals, externalGlobals, importedSymbols, targetDataLayout);
     }
 
-    private void defineGlobals(List<GlobalVariable> globals, List<GlobalVariable> definedGlobals, List<GlobalVariable> externalGlobals) {
+    private void defineGlobals(List<GlobalVariable> globals, List<GlobalVariable> definedGlobals, List<GlobalVariable> externalGlobals, List<String> importedSymbols) {
         for (GlobalVariable global : globals) {
             if (global.isExternal()) {
                 externalGlobals.add(global);
+                importedSymbols.add(global.getName());
             } else {
-                defineGlobal(global);
+                defineGlobal(global, importedSymbols);
                 definedGlobals.add(global);
             }
         }
     }
 
-    private void defineFunctions(ModelModule model, List<FunctionSymbol> definedFunctions, List<FunctionSymbol> externalFunctions, DataLayout dataLayout) {
+    private void defineFunctions(ModelModule model, List<FunctionSymbol> definedFunctions, List<FunctionSymbol> externalFunctions, List<String> importedSymbols, DataLayout dataLayout) {
         for (FunctionDefinition function : model.getDefinedFunctions()) {
             if (function.isExternal()) {
                 externalFunctions.add(function);
+                importedSymbols.add(function.getName());
             } else {
-                defineFunction(function, model, dataLayout);
+                defineFunction(function, model, importedSymbols, dataLayout);
                 definedFunctions.add(function);
             }
         }
@@ -113,16 +116,17 @@ public final class LLVMParser {
         for (FunctionDeclaration function : model.getDeclaredFunctions()) {
             assert function.isExternal();
             externalFunctions.add(function);
+            importedSymbols.add(function.getName());
         }
     }
 
-    private void defineAliases(List<GlobalAlias> aliases) {
+    private void defineAliases(List<GlobalAlias> aliases, List<String> importedSymbols) {
         for (GlobalAlias alias : aliases) {
-            defineAlias(alias);
+            defineAlias(alias, importedSymbols);
         }
     }
 
-    private void defineGlobal(GlobalVariable global) {
+    private void defineGlobal(GlobalVariable global, List<String> importedSymbols) {
         assert !global.isExternal();
         // handle the file scope
         LLVMGlobal globalSymbol = LLVMGlobal.create(global.getName(), global.getType(), global.getSourceSymbol(), global.isReadOnly(), global.getIndex(), runtime.getBitcodeID());
@@ -140,7 +144,9 @@ public final class LLVMParser {
             LLVMSymbol exportedSymbolFromGlobal = runtime.getGlobalScope().get(global.getName());
             if (exportedSymbolFromGlobal == null) {
                 runtime.getGlobalScope().register(globalSymbol);
-            } else if (!exportedSymbolFromGlobal.isGlobalVariable()) {
+            } else if (exportedSymbolFromGlobal.isGlobalVariable()) {
+                importedSymbols.add(global.getName());
+            } else {
                 assert exportedSymbolFromGlobal.isFunction();
                 // TODO (je) Symbol resolution is currently not correct [GR-21400] - doing
                 // nothing instead of throwing an exception does not make it more wrong but
@@ -152,7 +158,7 @@ public final class LLVMParser {
         }
     }
 
-    private void defineFunction(FunctionSymbol functionSymbol, ModelModule model, DataLayout dataLayout) {
+    private void defineFunction(FunctionSymbol functionSymbol, ModelModule model, List<String> importedSymbols, DataLayout dataLayout) {
         assert !functionSymbol.isExternal();
         // handle the file scope
         FunctionDefinition functionDefinition = (FunctionDefinition) functionSymbol;
@@ -173,7 +179,9 @@ public final class LLVMParser {
             LLVMSymbol exportedSymbolFromGlobal = runtime.getGlobalScope().get(functionSymbol.getName());
             if (exportedSymbolFromGlobal == null) {
                 runtime.getGlobalScope().register(llvmFunction);
-            } else if (!exportedSymbolFromGlobal.isFunction()) {
+            } else if (exportedSymbolFromGlobal.isFunction()) {
+                importedSymbols.add(functionSymbol.getName());
+            } else {
                 assert exportedSymbolFromGlobal.isGlobalVariable();
                 // TODO (je) Symbol resolution is currently not correct [GR-21400] - doing
                 // nothing instead of throwing an exception does not make it more wrong but
@@ -185,7 +193,7 @@ public final class LLVMParser {
         }
     }
 
-    private void defineAlias(GlobalAlias alias) {
+    private void defineAlias(GlobalAlias alias, List<String> importedSymbols) {
         LLVMSymbol alreadyRegisteredSymbol = runtime.getFileScope().get(alias.getName());
         if (alreadyRegisteredSymbol != null) {
             // this alias was already registered by a recursive call
@@ -193,34 +201,38 @@ public final class LLVMParser {
             return;
         }
 
-        defineAlias(alias.getName(), alias.isExported(), alias.getValue());
+        defineAlias(alias.getName(), alias.isExported(), alias.getValue(), importedSymbols);
     }
 
-    private void defineAlias(String aliasName, boolean isAliasExported, SymbolImpl value) {
+    private void defineAlias(String aliasName, boolean isAliasExported, SymbolImpl value, List<String> importedSymbols) {
         if (value instanceof FunctionSymbol) {
             FunctionSymbol function = (FunctionSymbol) value;
-            defineAlias(function.getName(), function.isExported(), aliasName, isAliasExported);
+            defineAlias(function.getName(), function.isExported(), aliasName, isAliasExported, importedSymbols);
         } else if (value instanceof GlobalVariable) {
             GlobalVariable global = (GlobalVariable) value;
-            defineAlias(global.getName(), global.isExported(), aliasName, isAliasExported);
+            defineAlias(global.getName(), global.isExported(), aliasName, isAliasExported, importedSymbols);
         } else if (value instanceof GlobalAlias) {
             GlobalAlias target = (GlobalAlias) value;
-            defineAlias(target);
-            defineAlias(target.getName(), target.isExported(), aliasName, isAliasExported);
+            defineAlias(target, importedSymbols);
+            defineAlias(target.getName(), target.isExported(), aliasName, isAliasExported, importedSymbols);
         } else if (value instanceof CastConstant) {
             // TODO (chaeubl): this is not perfectly accurate as we are loosing the type cast
             CastConstant cast = (CastConstant) value;
-            defineAlias(aliasName, isAliasExported, cast.getValue());
+            defineAlias(aliasName, isAliasExported, cast.getValue(), importedSymbols);
         } else {
             throw new LLVMLinkerException("Unknown alias type: " + value.getClass());
         }
     }
 
-    private void defineAlias(String existingName, boolean existingExported, String newName, boolean newExported) {
+    private void defineAlias(String existingName, boolean existingExported, String newName, boolean newExported, List<String> importedSymbols) {
         // handle the file scope
         LLVMSymbol aliasTarget = runtime.lookupSymbol(existingName);
         LLVMAlias aliasSymbol = new LLVMAlias(library, newName, aliasTarget);
         runtime.getFileScope().register(aliasSymbol);
+
+        if (existingExported && aliasTarget.getLibrary() != library) {
+            importedSymbols.add(aliasTarget.getName());
+        }
 
         // handle the global scope
         if (newExported) {
@@ -233,13 +245,10 @@ public final class LLVMParser {
             LLVMSymbol exportedSymbolFromGlobal = runtime.getGlobalScope().get(newName);
             if (exportedSymbolFromGlobal == null) {
                 runtime.getGlobalScope().register(aliasSymbol);
-            } else if (!(aliasSymbol.isFunction() && exportedSymbolFromGlobal.isFunction()) || !(aliasSymbol.isGlobalVariable() && exportedSymbolFromGlobal.isGlobalVariable())) {
-                // TODO (je) Symbol resolution is currently not correct [GR-21400] - doing
-                // nothing instead of throwing an exception does not make it more wrong but
-                // allows certain use cases to work correctly
-                // This was:
-                // throw new LLVMLinkerException("The alias " + newName + " conflicts with another
-                // symbol that has a different type but the same name.");
+            } else if (aliasSymbol.isFunction() && exportedSymbolFromGlobal.isFunction() || aliasSymbol.isGlobalVariable() && exportedSymbolFromGlobal.isGlobalVariable()) {
+                importedSymbols.add(newName);
+            } else {
+                throw new LLVMLinkerException("The alias " + newName + " conflicts with another symbol that has a different type but the same name.");
             }
         }
     }
