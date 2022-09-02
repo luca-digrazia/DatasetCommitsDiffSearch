19ebf58c26ad049524ddd31c17a15fd971c4a799
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:54:14 +0200

    2287 - ImageClassLoader doesn't need to hold on to Methods and Fields which do not have annotations

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/ImageClassLoader.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/ImageClassLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -76,12 +76,6 @@ public final class ImageClassLoader {
 
     private static final String CLASS_EXTENSION = ".class";
     private static final int CLASS_EXTENSION_LENGTH = CLASS_EXTENSION.length();
-
-    /*
-     * This cannot be a HostedOption because the option parsing already relies on the list of loaded
-     * classes.
-     */
-    private static final int CLASS_LOADING_MAX_SCALING = 8;
     private static final int CLASS_LOADING_TIMEOUT_IN_MINUTES = 10;
 
     static {
@@ -142,7 +136,7 @@ public final class ImageClassLoader {
     }
 
     private void initAllClasses() {
-        final ForkJoinPool executor = new ForkJoinPool(Math.min(Runtime.getRuntime().availableProcessors(), CLASS_LOADING_MAX_SCALING));
+        final ForkJoinPool executor = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
 
         if (JavaVersionUtil.JAVA_SPEC > 8) {
             Set<String> modules = new HashSet<>();
@@ -173,11 +167,7 @@ public final class ImageClassLoader {
                                         .collect(Collectors.toList()));
         uniquePaths.parallelStream().forEach(path -> loadClassesFromPath(executor, path));
 
-        boolean completed = executor.awaitQuiescence(CLASS_LOADING_TIMEOUT_IN_MINUTES, TimeUnit.MINUTES);
-        if (!completed) {
-            throw shouldNotReachHere("timed out while initializing classes");
-        }
-        executor.shutdownNow();
+        executor.awaitQuiescence(CLASS_LOADING_TIMEOUT_IN_MINUTES, TimeUnit.MINUTES);
     }
 
     static Stream<Path> toClassPathEntries(String classPathEntry) {
@@ -267,10 +257,31 @@ public final class ImageClassLoader {
         }
     }
 
-    private static boolean annotationsAvailable(AnnotatedElement element) {
+    /**
+     * @param element The element to check
+     * @return Returns true if the annotations on the {@code element} can be loaded without any
+     *         errors.
+     */
+    private static boolean canLoadAnnotations(AnnotatedElement element) {
         try {
             element.getAnnotations();
             return true;
+        } catch (Throwable t) {
+            handleClassLoadingError(t);
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param element The element to check
+     * @return Returns true if and only if the the {@code element} has any annotations present and
+     *         the {@link AnnotatedElement#getAnnotations()} did not throw any error.
+     */
+    private static boolean annotationsAvailable(AnnotatedElement element) {
+        try {
+            final Annotation[] annotations = element.getAnnotations();
+            return annotations.length != 0;
         } catch (Throwable t) {
             handleClassLoadingError(t);
             return false;
@@ -372,7 +383,7 @@ public final class ImageClassLoader {
             cur = clazz;
         }
         do {
-            if (!annotationsAvailable(cur)) {
+            if (!canLoadAnnotations(cur)) {
                 return;
             }
             Platforms platformsAnnotation = cur.getAnnotation(Platforms.class);
