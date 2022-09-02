2ef1da87907a2dff66e6ea207677662a3e819899
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:50:11 +0200

    don't let resolveTypeKlass become part of compilation from Field.setObject

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/Field.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/Field.java
@@ -25,7 +25,6 @@ package com.oracle.truffle.espresso.impl;
 import com.oracle.truffle.api.Assumption;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.espresso.classfile.Constants;
 import com.oracle.truffle.espresso.classfile.RuntimeConstantPool;
@@ -53,12 +52,8 @@ public final class Field extends Member<Type> implements FieldRef {
     private final LinkedField linkedField;
     private final ObjectKlass holder;
 
-    @CompilationFinal
-    private Klass typeKlassCache;
-    @CompilationFinal
-    private FieldVersion fieldVersion;
-    @CompilationFinal
-    private boolean changedByRedefinition = false;
+    @CompilationFinal private FieldVersion fieldVersion;
+    @CompilationFinal private boolean changedByRedefinition = false;
 
     public Field(ObjectKlass holder, LinkedField linkedField, RuntimeConstantPool pool) {
         this.linkedField = linkedField;
@@ -139,29 +134,7 @@ public final class Field extends Member<Type> implements FieldRef {
     }
 
     public Klass resolveTypeKlass() {
-        Klass tk = typeKlassCache;
-        if (tk == null) {
-            if (CompilerDirectives.isPartialEvaluationConstant(this)) {
-                // This can be used from contexts where this is not a constant (e.g., Unsafe)
-                // as well as context where this is constant (e.g., field access)
-                CompilerDirectives.transferToInterpreterAndInvalidate();
-            }
-            doResolveType();
-        }
-        return typeKlassCache;
-    }
-
-    @TruffleBoundary
-    private void doResolveType() {
-        synchronized (this) {
-            Klass tk = typeKlassCache;
-            if (tk == null) {
-                tk = holder.getMeta().resolveSymbolOrFail(getType(),
-                        holder.getDefiningClassLoader(),
-                        holder.protectionDomain());
-                typeKlassCache = tk;
-            }
-        }
+        return getFieldVersion().resolveTypeKlass();
     }
 
     public Attribute getAttribute(Symbol<Name> attrName) {
@@ -194,26 +167,16 @@ public final class Field extends Member<Type> implements FieldRef {
     public Object get(StaticObject obj, boolean forceVolatile) {
         // @formatter:off
         switch (getKind()) {
-            case Boolean:
-                return getBoolean(obj, forceVolatile);
-            case Byte:
-                return getByte(obj, forceVolatile);
-            case Short:
-                return getShort(obj, forceVolatile);
-            case Char:
-                return getChar(obj, forceVolatile);
-            case Int:
-                return getInt(obj, forceVolatile);
-            case Float:
-                return getFloat(obj, forceVolatile);
-            case Long:
-                return getLong(obj, forceVolatile);
-            case Double:
-                return getDouble(obj, forceVolatile);
-            case Object:
-                return getObject(obj, forceVolatile);
-            default:
-                throw EspressoError.shouldNotReachHere();
+            case Boolean : return getBoolean(obj, forceVolatile);
+            case Byte    : return getByte(obj, forceVolatile);
+            case Short   : return getShort(obj, forceVolatile);
+            case Char    : return getChar(obj, forceVolatile);
+            case Int     : return getInt(obj, forceVolatile);
+            case Float   : return getFloat(obj, forceVolatile);
+            case Long    : return getLong(obj, forceVolatile);
+            case Double  : return getDouble(obj, forceVolatile);
+            case Object  : return getObject(obj, forceVolatile);
+            default      : throw EspressoError.shouldNotReachHere();
         }
         // @formatter:on
     }
@@ -225,35 +188,16 @@ public final class Field extends Member<Type> implements FieldRef {
     public void set(StaticObject obj, Object value, boolean forceVolatile) {
         // @formatter:off
         switch (getKind()) {
-            case Boolean:
-                setBoolean(obj, (boolean) value, forceVolatile);
-                break;
-            case Byte:
-                setByte(obj, (byte) value, forceVolatile);
-                break;
-            case Short:
-                setShort(obj, (short) value, forceVolatile);
-                break;
-            case Char:
-                setChar(obj, (char) value, forceVolatile);
-                break;
-            case Int:
-                setInt(obj, (int) value, forceVolatile);
-                break;
-            case Float:
-                setFloat(obj, (float) value, forceVolatile);
-                break;
-            case Long:
-                setLong(obj, (long) value, forceVolatile);
-                break;
-            case Double:
-                setDouble(obj, (double) value, forceVolatile);
-                break;
-            case Object:
-                setObject(obj, value, forceVolatile);
-                break;
-            default:
-                throw EspressoError.shouldNotReachHere();
+            case Boolean : setBoolean(obj, (boolean) value, forceVolatile);    break;
+            case Byte    : setByte(obj, (byte) value, forceVolatile);          break;
+            case Short   : setShort(obj, (short) value, forceVolatile);        break;
+            case Char    : setChar(obj, (char) value, forceVolatile);          break;
+            case Int     : setInt(obj, (int) value, forceVolatile);            break;
+            case Float   : setFloat(obj, (float) value, forceVolatile);        break;
+            case Long    : setLong(obj, (long) value, forceVolatile);          break;
+            case Double  : setDouble(obj, (double) value, forceVolatile);      break;
+            case Object  : setObject(obj, value, forceVolatile);               break;
+            default      : throw EspressoError.shouldNotReachHere();
         }
         // @formatter:on
     }
@@ -787,10 +731,8 @@ public final class Field extends Member<Type> implements FieldRef {
      */
     static final class StableBoolean {
 
-        @CompilationFinal
-        private volatile Assumption unchanged;
-        @CompilationFinal
-        private volatile boolean value;
+        @CompilationFinal private volatile Assumption unchanged;
+        @CompilationFinal private volatile boolean value;
 
         StableBoolean(boolean initialValue) {
             this.value = initialValue;
@@ -825,10 +767,8 @@ public final class Field extends Member<Type> implements FieldRef {
         private final Assumption assumption;
         private final Symbol<Symbol.Type> type;
         private final RuntimeConstantPool pool;
-        @CompilationFinal
-        private volatile Klass typeKlassCache;
-        @CompilationFinal
-        private Symbol<ModifiedUTF8> genericSignature;
+        @CompilationFinal private volatile Klass typeKlassCache;
+        @CompilationFinal private Symbol<ModifiedUTF8> genericSignature;
 
         FieldVersion(Symbol<Symbol.Type> type, RuntimeConstantPool pool) {
             this.assumption = Truffle.getRuntime().createAssumption();
@@ -880,8 +820,8 @@ public final class Field extends Member<Type> implements FieldRef {
                     tk = typeKlassCache;
                     if (tk == null) {
                         tk = holder.getMeta().resolveSymbolOrFail(type,
-                                holder.getDefiningClassLoader(),
-                                holder.protectionDomain());
+                                        holder.getDefiningClassLoader(),
+                                        holder.protectionDomain());
                         typeKlassCache = tk;
                     }
                 }
