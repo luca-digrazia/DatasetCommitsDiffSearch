a059e7636d6906810d62f867ecdb303b26da06a0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:14:31 +0200

    More precise return type for getLazyLock().

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/RootNode.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/RootNode.java
@@ -43,6 +43,7 @@ package com.oracle.truffle.api.nodes;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import com.oracle.truffle.api.CallTarget;
