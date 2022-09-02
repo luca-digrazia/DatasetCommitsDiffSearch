490a99c4d3c60b1e921c30fedc025e2c8c2a4839
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:12:48 +0200

    Fix NoClassDefFoundError thrown by enclosing type resolution.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/meta/AnalysisType.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/meta/AnalysisType.java
@@ -703,7 +703,7 @@ public class AnalysisType implements WrappedJavaType, OriginalClassProvider, Com
     @Override
     public void initialize() {
         if (!wrapped.isInitialized()) {
-            throw GraalError.shouldNotReachHere("Classes can only be initialized using methods in ClassInitializationFeature: " + toClassName());
+            throw GraalError.shouldNotReachHere("Classes can only be initialized using methods in ClassInitializationFeature");
         }
     }
 
