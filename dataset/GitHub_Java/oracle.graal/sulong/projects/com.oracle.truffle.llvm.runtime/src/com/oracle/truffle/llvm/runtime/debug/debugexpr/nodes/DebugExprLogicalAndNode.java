1b597b51e16d9930ce8fdc5968219455cc503e3a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:43:25 +0200

    short circuit evaluation: renamed method, refactored comment

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/debug/debugexpr/nodes/DebugExprLogicalAndNode.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/debug/debugexpr/nodes/DebugExprLogicalAndNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2019, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -30,10 +30,14 @@
 package com.oracle.truffle.llvm.runtime.debug.debugexpr.nodes;
 
 import com.oracle.truffle.api.nodes.NodeInfo;
-import com.oracle.truffle.llvm.runtime.debug.debugexpr.nodes.DebugExprShortCircuitEvaluationNode.ShortCircuitOpNode;
+import com.oracle.truffle.llvm.runtime.nodes.api.LLVMExpressionNode;
 
 @NodeInfo(shortName = "&&")
-public class DebugExprLogicalAndNode extends ShortCircuitOpNode {
+public class DebugExprLogicalAndNode extends DebugExprShortCircuitEvaluationNode {
+
+    public DebugExprLogicalAndNode(LLVMExpressionNode leftNode, LLVMExpressionNode rightNode) {
+        super(leftNode, rightNode);
+    }
 
     @Override
     protected boolean shouldEvaluateRight(boolean leftValue) {
