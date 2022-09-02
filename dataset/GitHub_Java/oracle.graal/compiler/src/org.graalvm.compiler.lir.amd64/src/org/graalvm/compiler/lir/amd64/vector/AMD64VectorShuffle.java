136e5b60efeb20d8a09b17b9818ddf73242fc3fb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:08:30 +0200

    Fix Vector Shuffle width on x86
        The vector shuffle needs to take its operating width on the source - it
        may shuffle to produce a smaller value, but we must use the input so we
        can see and access all elements.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/vector/AMD64VectorShuffle.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/vector/AMD64VectorShuffle.java
@@ -158,8 +158,7 @@ public class AMD64VectorShuffle {
 
         public ConstShuffleBytesOp(AllocatableValue result, AllocatableValue source, byte... selector) {
             super(TYPE);
-            assert AVXKind.getRegisterSize(((AMD64Kind) source.getPlatformKind())).getBytes() == selector.length : " Register size=" +
-                            AVXKind.getRegisterSize(((AMD64Kind) source.getPlatformKind())).getBytes() + " select length=" + selector.length;
+            assert AVXKind.getRegisterSize(((AMD64Kind) source.getPlatformKind())).getBytes() == selector.length;
             this.result = result;
             this.source = source;
             this.selector = selector;
