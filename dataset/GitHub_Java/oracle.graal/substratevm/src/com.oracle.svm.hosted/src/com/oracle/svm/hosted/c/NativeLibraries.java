d971184d96605a04fc5a8c2d0dc732471160884e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:46:05 +0200

    Implement sunec library automatic static linking

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/NativeLibraries.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/NativeLibraries.java
@@ -159,8 +159,8 @@ public final class NativeLibraries {
                         return false;
                     }
                     String lib = libName.substring(libPrefix.length(), libName.length() - libSuffix.length());
-                    return PlatformNativeLibrarySupport.defaultBuiltInLibraries.contains(lib);
-                }).count() == PlatformNativeLibrarySupport.defaultBuiltInLibraries.size()) {
+                    return PlatformNativeLibrarySupport.builtInLibraries.contains(lib);
+                }).count() == PlatformNativeLibrarySupport.builtInLibraries.size()) {
                     staticLibsDir = jdkLibDir;
                 }
             }
