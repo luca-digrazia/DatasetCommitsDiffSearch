4d2b47351d98214d8fd74e74a1e9937e7ac85450
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:26:59 +0200

    Report UserError for all platforms that require static JDK libraries.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/NativeLibraries.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/NativeLibraries.java
@@ -102,16 +102,13 @@ public final class NativeLibraries {
 
     private final CAnnotationProcessorCache cache;
 
-    public final Path tempDirectory;
-
     public NativeLibraries(ConstantReflectionProvider constantReflection, MetaAccessProvider metaAccess, SnippetReflectionProvider snippetReflection, TargetDescription target,
-                    ClassInitializationSupport classInitializationSupport, Path tempDirectory) {
+                    ClassInitializationSupport classInitializationSupport) {
         this.metaAccess = metaAccess;
         this.constantReflection = constantReflection;
         this.snippetReflection = snippetReflection;
         this.target = target;
         this.classInitializationSupport = classInitializationSupport;
-        this.tempDirectory = tempDirectory;
 
         elementToInfo = new HashMap<>();
         errors = new ArrayList<>();
@@ -348,7 +345,7 @@ public final class NativeLibraries {
         }
     }
 
-    public void finish() {
+    public void finish(Path tempDirectory) {
         libraryPaths.addAll(OptionUtils.flatten(",", SubstrateOptions.CLibraryPath.getValue()));
         for (NativeCodeContext context : compilationUnitToContext.values()) {
             if (context.isInConfiguration()) {
