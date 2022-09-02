a030549b4ea2342fe48f7cd8d0a9786518691e38
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:30:56 +0200

    Native Image does not yet support the field-based storage.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.staticobject.test/src/com/oracle/truffle/api/staticobject/test/ShapeAndFactoryTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.staticobject.test/src/com/oracle/truffle/api/staticobject/test/ShapeAndFactoryTest.java
@@ -2,43 +2,82 @@
  * Copyright (c) 2021, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * (a) the Software, and
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.oracle.truffle.api.staticobject.test;
 
 import com.oracle.truffle.api.staticobject.StaticShape;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 import java.util.regex.Pattern;
 
-public class ShapeAndFactoryTest extends StaticObjectTest {
+@RunWith(Theories.class)
+public class ShapeAndFactoryTest extends StaticObjectModelTest {
+    @DataPoints //
+    public static TestEnvironment[] environments;
 
-    @Test
-    public void correct() {
-        StaticShape.Builder b = StaticShape.newBuilder(this);
+    @BeforeClass
+    public static void setup() {
+        environments = getTestEnvironments();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        for (TestEnvironment env : environments) {
+            env.close();
+        }
+    }
+
+    @Theory
+    public void correct(TestEnvironment te) {
+        StaticShape.Builder b = StaticShape.newBuilder(te.testLanguage);
         b.build(CustomStaticObject.class, CustomFactoryInterface.class);
     }
 
-    @Test
-    public void wrongFinalClone() {
-        StaticShape.Builder b = StaticShape.newBuilder(this);
+    @Theory
+    public void wrongFinalClone(TestEnvironment te) {
+        StaticShape.Builder b = StaticShape.newBuilder(te.testLanguage);
         try {
             b.build(WrongCloneCustomStaticObject.class, WrongCloneFactoryInterface.class);
             Assert.fail();
@@ -47,9 +86,9 @@ public class ShapeAndFactoryTest extends StaticObjectTest {
         }
     }
 
-    @Test
-    public void wrongFactoryArgs() {
-        StaticShape.Builder b = StaticShape.newBuilder(this);
+    @Theory
+    public void wrongFactoryArgs(TestEnvironment te) {
+        StaticShape.Builder b = StaticShape.newBuilder(te.testLanguage);
         try {
             b.build(CustomStaticObject.class, WrongArgsFactoryInterface.class);
             Assert.fail();
@@ -58,9 +97,9 @@ public class ShapeAndFactoryTest extends StaticObjectTest {
         }
     }
 
-    @Test
-    public void wrongFactoryReturnType() {
-        StaticShape.Builder b = StaticShape.newBuilder(this);
+    @Theory
+    public void wrongFactoryReturnType(TestEnvironment te) {
+        StaticShape.Builder b = StaticShape.newBuilder(te.testLanguage);
         try {
             b.build(CustomStaticObject.class, WrongReturnTypeFactoryInterface.class);
             Assert.fail();
@@ -108,5 +147,10 @@ public class ShapeAndFactoryTest extends StaticObjectTest {
 
     public interface WrongReturnTypeFactoryInterface {
         String create(int i, Object o);
+    }
+
+    @Test
+    public void dummy() {
+        // to make sure this file is recognized as a test
     }
 }
