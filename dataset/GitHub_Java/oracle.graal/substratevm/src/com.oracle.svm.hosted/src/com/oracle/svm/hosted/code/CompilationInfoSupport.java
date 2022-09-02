081f2ad2b19ee70f1e450b17161b7f4cfbffca36
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:12:13 +0200

    Enhancing -H:VerifyDeoptimizationEntryPoints to check more information.
        Previously -H:VerifyDeoptimizationEntryPoints checked that there was a
        target for every potential deoptimization point. Now this check also
        confirms every deoptimization point's frame can be used to recreate its
        destination target frame.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CompilationInfoSupport.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CompilationInfoSupport.java
@@ -31,7 +31,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.graalvm.compiler.nodeinfo.Verbosity;
 import org.graalvm.compiler.nodes.FrameState;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.nativeimage.ImageSingletons;
@@ -96,12 +95,10 @@ public class CompilationInfoSupport {
                             expectedKinds.size() == otherKinds.size();
             if (!matchingSizes) {
                 StringBuilder errorMessage = new StringBuilder();
-                errorMessage.append("Unexpected number of values in state to merge. Please report this problem.\n");
-                errorMessage.append(String.format("****Merge FrameState****\n%s************************\n", state.toString(Verbosity.Debugger)));
-                errorMessage.append(String.format("bci: %d, duringCall: %b, rethrowException: %b\n", state.bci, state.duringCall(), state.rethrowException()));
+                errorMessage.append("Unexpected number of values in state to merge.\n");
                 errorMessage.append(String.format("DeoptSourceFrameInfo: locals-%d, stack-%d, locks-%d.\n", numLocals, numStack, numLocks));
                 errorMessage.append(String.format("Merge FrameState: locals-%d, stack-%d, locks-%d.\n", state.localsSize(), state.stackSize(), state.locksSize()));
-                throw VMError.shouldNotReachHere(errorMessage.toString());
+                VMError.shouldNotReachHere(errorMessage.toString());
             }
 
             for (int i = 0; i < expectedKinds.size(); i++) {
