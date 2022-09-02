c7a4ae9b73bdaa17fa42c17db42111a2cefdc44e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:44:56 +0200

    Strictly enforce assignability checks

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.common/src/org/graalvm/compiler/core/common/Fields.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.common/src/org/graalvm/compiler/core/common/Fields.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,7 +27,9 @@ package org.graalvm.compiler.core.common;
 import static org.graalvm.compiler.serviceprovider.GraalUnsafeAccess.getUnsafe;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.graalvm.compiler.debug.GraalError;
 
@@ -39,6 +41,7 @@ import sun.misc.Unsafe;
 public class Fields {
 
     private static final Unsafe UNSAFE = getUnsafe();
+    private static final Fields EMPTY_FIELDS = new Fields(Collections.emptyList());
 
     /**
      * Offsets used with {@link Unsafe} to access the fields.
@@ -60,10 +63,10 @@ public class Fields {
     public static Fields forClass(Class<?> clazz, Class<?> endClazz, boolean includeTransient, FieldsScanner.CalcOffset calcOffset) {
         FieldsScanner scanner = new FieldsScanner(calcOffset == null ? new FieldsScanner.DefaultCalcOffset() : calcOffset);
         scanner.scan(clazz, endClazz, includeTransient);
-        return new Fields(scanner.data);
+        return create(scanner.data);
     }
 
-    public Fields(ArrayList<? extends FieldsScanner.FieldInfo> fields) {
+    protected Fields(List<? extends FieldsScanner.FieldInfo> fields) {
         Collections.sort(fields);
         this.offsets = new long[fields.size()];
         this.names = new String[offsets.length];
@@ -77,6 +80,13 @@ public class Fields {
             declaringClasses[index] = f.declaringClass;
             index++;
         }
+    }
+
+    public static Fields create(ArrayList<? extends FieldsScanner.FieldInfo> fields) {
+        if (fields.size() == 0) {
+            return EMPTY_FIELDS;
+        }
+        return new Fields(fields);
     }
 
     /**
@@ -146,9 +156,40 @@ public class Fields {
                 }
             } else {
                 Object obj = UNSAFE.getObject(from, offset);
+                if (obj != null && type.isArray()) {
+                    if (type.getComponentType().isPrimitive()) {
+                        obj = copyObjectAsArray(obj);
+                    } else {
+                        obj = ((Object[]) obj).clone();
+                    }
+                }
                 UNSAFE.putObject(to, offset, trans == null ? obj : trans.apply(index, obj));
             }
         }
+    }
+
+    private static Object copyObjectAsArray(Object obj) {
+        Object objCopy;
+        if (obj instanceof int[]) {
+            objCopy = Arrays.copyOf((int[]) obj, ((int[]) obj).length);
+        } else if (obj instanceof short[]) {
+            objCopy = Arrays.copyOf((short[]) obj, ((short[]) obj).length);
+        } else if (obj instanceof long[]) {
+            objCopy = Arrays.copyOf((long[]) obj, ((long[]) obj).length);
+        } else if (obj instanceof float[]) {
+            objCopy = Arrays.copyOf((float[]) obj, ((float[]) obj).length);
+        } else if (obj instanceof double[]) {
+            objCopy = Arrays.copyOf((double[]) obj, ((double[]) obj).length);
+        } else if (obj instanceof boolean[]) {
+            objCopy = Arrays.copyOf((boolean[]) obj, ((boolean[]) obj).length);
+        } else if (obj instanceof byte[]) {
+            objCopy = Arrays.copyOf((byte[]) obj, ((byte[]) obj).length);
+        } else if (obj instanceof char[]) {
+            objCopy = Arrays.copyOf((char[]) obj, ((char[]) obj).length);
+        } else {
+            throw GraalError.shouldNotReachHere();
+        }
+        return objCopy;
     }
 
     /**
@@ -261,38 +302,11 @@ public class Fields {
      * @param value a value that will be assigned to the field
      */
     private boolean checkAssignableFrom(Object object, int index, Object value) {
-        assert value == null || getType(index).isAssignableFrom(value.getClass()) : String.format("Field %s.%s of type %s is not assignable from %s", object.getClass().getSimpleName(),
-                        getName(index), getType(index).getSimpleName(), value.getClass().getSimpleName());
-        return true;
-    }
-
-    public void set(Object object, int index, Object value) {
-        long offset = offsets[index];
-        Class<?> type = types[index];
-        if (type.isPrimitive()) {
-            if (type == Integer.TYPE) {
-                UNSAFE.putInt(object, offset, (Integer) value);
-            } else if (type == Long.TYPE) {
-                UNSAFE.putLong(object, offset, (Long) value);
-            } else if (type == Boolean.TYPE) {
-                UNSAFE.putBoolean(object, offset, (Boolean) value);
-            } else if (type == Float.TYPE) {
-                UNSAFE.putFloat(object, offset, (Float) value);
-            } else if (type == Double.TYPE) {
-                UNSAFE.putDouble(object, offset, (Double) value);
-            } else if (type == Short.TYPE) {
-                UNSAFE.putShort(object, offset, (Short) value);
-            } else if (type == Character.TYPE) {
-                UNSAFE.putChar(object, offset, (Character) value);
-            } else if (type == Byte.TYPE) {
-                UNSAFE.putByte(object, offset, (Byte) value);
-            } else {
-                assert false : "unhandled property type: " + type;
-            }
-        } else {
-            assert checkAssignableFrom(object, index, value);
-            UNSAFE.putObject(object, offset, value);
+        if (value != null && !getType(index).isAssignableFrom(value.getClass())) {
+            throw new GraalError(String.format("Field %s.%s of type %s in %s is not assignable from %s", object.getClass().getSimpleName(),
+                            getName(index), object, getType(index).getSimpleName(), value.getClass().getSimpleName()));
         }
+        return true;
     }
 
     public void setRawPrimitive(Object object, int index, long value) {
@@ -379,6 +393,11 @@ public class Fields {
 
     public void putObject(Object object, int i, Object value) {
         assert checkAssignableFrom(object, i, value);
+        UNSAFE.putObject(object, offsets[i], value);
+    }
+
+    public void putObjectChecked(Object object, int i, Object value) {
+        checkAssignableFrom(object, i, value);
         UNSAFE.putObject(object, offsets[i], value);
     }
 }
