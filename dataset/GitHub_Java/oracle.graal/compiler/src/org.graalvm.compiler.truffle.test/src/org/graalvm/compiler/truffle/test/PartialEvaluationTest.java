96a6a7fb2c1a179ea14555fafa22744e99f4ddb6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:40:05 +0200

    Share a method that creates a RootNode from a FrameFunction.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/PartialEvaluationTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/PartialEvaluationTest.java
@@ -101,6 +101,20 @@ public abstract class PartialEvaluationTest extends TruffleCompilerImplTest {
         return compilable;
     }
 
+    @FunctionalInterface
+    protected interface FrameFunciton {
+        Object execute(VirtualFrame frame);
+    }
+
+    protected RootNode toRootNode(FrameFunciton f) {
+        return new RootNode(null) {
+            @Override
+            public Object execute(VirtualFrame frame) {
+                return f.execute(frame);
+            }
+        };
+    }
+
     protected OptimizedCallTarget assertPartialEvalEquals(RootNode expected, RootNode actual, Object[] arguments) {
         return assertPartialEvalEquals(expected, actual, arguments, true);
     }
@@ -156,11 +170,6 @@ public abstract class PartialEvaluationTest extends TruffleCompilerImplTest {
             @Override
             public TruffleInliningData inliningData() {
                 return inlining;
-            }
-
-            @Override
-            public boolean hasNextTier() {
-                return false;
             }
         };
     }
