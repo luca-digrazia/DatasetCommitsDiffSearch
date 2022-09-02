57465ccfab43b317c35bfed2d87d1200936c9719
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:11:57 +0200

    Refactoring: make final.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/CompilationTask.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/CompilationTask.java
@@ -26,7 +26,6 @@ package org.graalvm.compiler.truffle.runtime;
 
 import java.lang.ref.WeakReference;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
@@ -35,13 +34,13 @@ import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 
 import org.graalvm.compiler.truffle.common.TruffleCompilationTask;
-import org.graalvm.compiler.truffle.common.TruffleInliningData;
 
 import com.oracle.truffle.api.Truffle;
+import org.graalvm.compiler.truffle.common.TruffleInliningData;
 
 public final class CompilationTask implements TruffleCompilationTask, Callable<Void>, Comparable<CompilationTask> {
 
-    private static final Consumer<CompilationTask> COMPILATION_ACTION = new Consumer<CompilationTask>() {
+    private static final Consumer<CompilationTask> compilationAction = new Consumer<CompilationTask>() {
         @Override
         public void accept(CompilationTask task) {
             OptimizedCallTarget callTarget = task.targetRef.get();
@@ -88,30 +87,21 @@ public final class CompilationTask implements TruffleCompilationTask, Callable<V
     }
 
     static CompilationTask createCompilationTask(BackgroundCompileQueue.Priority priority, WeakReference<OptimizedCallTarget> targetRef, long id) {
-        return new CompilationTask(priority, targetRef, COMPILATION_ACTION, id);
+        return new CompilationTask(priority, targetRef, compilationAction, id);
     }
 
     public void awaitCompletion(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        try {
-            future.get(timeout, unit);
-        } catch (CancellationException e) {
-            // Ignored
-        }
+        future.get(timeout, unit);
     }
 
     public void awaitCompletion() throws ExecutionException, InterruptedException {
-        try {
-            future.get();
-        } catch (CancellationException e) {
-            // Ignored
-        }
+        future.get();
     }
 
     public synchronized boolean cancel() {
         if (!cancelled) {
             cancelled = true;
             if (!started) {
-                future.cancel(false);
                 finished();
             }
             return true;
@@ -152,19 +142,6 @@ public final class CompilationTask implements TruffleCompilationTask, Callable<V
     @Override
     public TruffleInliningData inliningData() {
         return inliningData;
-    }
-
-    @Override
-    public boolean hasNextTier() {
-        if (isLastTier()) {
-            return false;
-        }
-        OptimizedCallTarget callTarget = targetRef.get();
-        if (callTarget == null) {
-            // Does not matter what we return if the target is not available
-            return false;
-        }
-        return !callTarget.engine.firstTierOnly;
     }
 
     public Future<?> getFuture() {
@@ -228,10 +205,6 @@ public final class CompilationTask implements TruffleCompilationTask, Callable<V
      * corrupt a queue data structure.
      */
     boolean isHigherPriorityThan(CompilationTask other) {
-        if (action != COMPILATION_ACTION) {
-            // Any non-compilation action (e.g. compiler init) is higher priority.
-            return true;
-        }
         int tier = tier();
         if (engineData.traversingFirstTierPriority && tier != other.tier()) {
             return tier < other.tier();
@@ -248,17 +221,14 @@ public final class CompilationTask implements TruffleCompilationTask, Callable<V
         return false;
     }
 
-    /**
-     * @return false if the target reference is null (i.e. if the target was garbage-collected).
-     */
-    boolean updateWeight(long currentTime) {
+    double updateWeight(long currentTime) {
         OptimizedCallTarget target = targetRef.get();
         if (target == null) {
-            return false;
+            return -1.0;
         }
         long elapsed = currentTime - lastTime;
         if (elapsed < 1_000_000) {
-            return true;
+            return lastWeight;
         }
         int count = target.getCallAndLoopCount();
         double weight = rate(count, elapsed) * count;
@@ -266,7 +236,7 @@ public final class CompilationTask implements TruffleCompilationTask, Callable<V
         lastCount = count;
         lastWeight = weight;
         assert weight >= 0.0 : "weight must be positive";
-        return true;
+        return weight;
     }
 
     private double rate(int count, long elapsed) {
