a00822e21337a8c389f309c0567249ebe8d82395
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:37:38 +0200

    compiler: introduce MacroInvokable interface

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/meta/HotSpotInvocationPlugins.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/meta/HotSpotInvocationPlugins.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -22,17 +24,19 @@
  */
 package org.graalvm.compiler.hotspot.meta;
 
-import static org.graalvm.compiler.core.common.util.Util.Java8OrEarlier;
+import static jdk.vm.ci.hotspot.HotSpotJVMCIRuntime.runtime;
 
 import java.lang.reflect.Type;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 import org.graalvm.compiler.core.common.GraalOptions;
-import org.graalvm.compiler.core.common.util.ModuleAPI;
 import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.graph.Node;
 import org.graalvm.compiler.graph.iterators.NodeIterable;
 import org.graalvm.compiler.hotspot.GraalHotSpotVMConfig;
+import org.graalvm.compiler.hotspot.HotSpotGraalRuntimeProvider;
 import org.graalvm.compiler.hotspot.phases.AheadOfTimeVerificationPhase;
 import org.graalvm.compiler.nodes.ConstantNode;
 import org.graalvm.compiler.nodes.FrameState;
@@ -41,10 +45,8 @@ import org.graalvm.compiler.nodes.graphbuilderconf.InvocationPlugin;
 import org.graalvm.compiler.nodes.graphbuilderconf.InvocationPlugins;
 import org.graalvm.compiler.nodes.type.StampTool;
 import org.graalvm.compiler.phases.tiers.CompilerConfiguration;
-import org.graalvm.compiler.replacements.nodes.MacroNode;
-import org.graalvm.util.EconomicSet;
+import org.graalvm.compiler.replacements.nodes.MacroInvokable;
 
-import jdk.vm.ci.hotspot.HotSpotResolvedJavaType;
 import jdk.vm.ci.meta.JavaKind;
 import jdk.vm.ci.meta.ResolvedJavaType;
 
@@ -52,40 +54,39 @@ import jdk.vm.ci.meta.ResolvedJavaType;
  * Extension of {@link InvocationPlugins} that disables plugins based on runtime configuration.
  */
 final class HotSpotInvocationPlugins extends InvocationPlugins {
+    private final HotSpotGraalRuntimeProvider graalRuntime;
     private final GraalHotSpotVMConfig config;
-    private final EconomicSet<Object> trustedModules;
-    private final ClassLoader extLoader;
 
-    HotSpotInvocationPlugins(GraalHotSpotVMConfig config, CompilerConfiguration compilerConfiguration) {
+    /**
+     * Predicates that determine which types may be intrinsified.
+     */
+    private final List<Predicate<ResolvedJavaType>> intrinsificationPredicates = new ArrayList<>();
+
+    HotSpotInvocationPlugins(HotSpotGraalRuntimeProvider graalRuntime, GraalHotSpotVMConfig config, CompilerConfiguration compilerConfiguration) {
+        this.graalRuntime = graalRuntime;
         this.config = config;
-        if (Java8OrEarlier) {
-            extLoader = getExtLoader();
-            trustedModules = null;
-        } else {
-            extLoader = null;
-            trustedModules = initTrustedModules(compilerConfiguration);
-        }
+        registerIntrinsificationPredicate(runtime().getIntrinsificationTrustPredicate(compilerConfiguration.getClass()));
     }
 
     @Override
-    public void register(InvocationPlugin plugin, Type declaringClass, String name, Type... argumentTypes) {
+    protected void register(InvocationPlugin plugin, boolean isOptional, boolean allowOverwrite, Type declaringClass, String name, Type... argumentTypes) {
         if (!config.usePopCountInstruction) {
             if (name.equals("bitCount")) {
                 assert declaringClass.equals(Integer.class) || declaringClass.equals(Long.class);
                 return;
             }
         }
-        super.register(plugin, declaringClass, name, argumentTypes);
+        super.register(plugin, isOptional, allowOverwrite, declaringClass, name, argumentTypes);
     }
 
     @Override
     public void checkNewNodes(GraphBuilderContext b, InvocationPlugin plugin, NodeIterable<Node> newNodes) {
         for (Node node : newNodes) {
-            if (node instanceof MacroNode) {
+            if (node instanceof MacroInvokable) {
                 // MacroNode based plugins can only be used for inlining since they
                 // require a valid bci should they need to replace themselves with
                 // an InvokeNode during lowering.
-                assert plugin.inlineOnly() : String.format("plugin that creates a %s (%s) must return true for inlineOnly(): %s", MacroNode.class.getSimpleName(), node, plugin);
+                assert plugin.inlineOnly() : String.format("plugin that creates a %s (%s) must return true for inlineOnly(): %s", MacroInvokable.class.getSimpleName(), node, plugin);
             }
         }
         if (GraalOptions.ImmutableCode.getValue(b.getOptions())) {
@@ -113,70 +114,23 @@ final class HotSpotInvocationPlugins extends InvocationPlugins {
         return type != null && "Ljava/lang/Class;".equals(type.getName());
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * On JDK 8, only classes loaded by the boot, JVMCI or extension class loaders are trusted.
-     *
-     * On JDK 9 and later, only classes in the {@link CompilerConfiguration} defining module or any
-     * of its module dependencies are trusted.
-     */
     @Override
-    protected boolean canBeIntrinsified(ResolvedJavaType declaringClass) {
-        if (declaringClass instanceof HotSpotResolvedJavaType) {
-            Class<?> javaClass = ((HotSpotResolvedJavaType) declaringClass).mirror();
-            if (Java8OrEarlier) {
-                ClassLoader cl = javaClass.getClassLoader();
-                return cl == null || cl == getClass().getClassLoader() || cl == extLoader;
-            } else {
-                Object module = ModuleAPI.getModule.invoke(javaClass);
-                return trustedModules.contains(module);
-            }
-        }
-        return false;
+    public void registerIntrinsificationPredicate(Predicate<ResolvedJavaType> predicate) {
+        intrinsificationPredicates.add(predicate);
     }
 
-    private static ClassLoader getExtLoader() {
-        try {
-            Object launcher = Class.forName("sun.misc.Launcher").getMethod("getLauncher").invoke(null);
-            ClassLoader appLoader = (ClassLoader) launcher.getClass().getMethod("getClassLoader").invoke(launcher);
-            ClassLoader extLoader = appLoader.getParent();
-            assert extLoader.getClass().getName().equals("sun.misc.Launcher$ExtClassLoader") : extLoader;
-            return extLoader;
-        } catch (Exception e) {
-            throw new GraalError(e);
+    @Override
+    public boolean canBeIntrinsified(ResolvedJavaType declaringClass) {
+        boolean ok = false;
+        for (Predicate<ResolvedJavaType> p : intrinsificationPredicates) {
+            ok |= p.test(declaringClass);
         }
-    }
-
-    private static EconomicSet<Object> initTrustedModules(CompilerConfiguration compilerConfiguration) throws GraalError {
-        try {
-            EconomicSet<Object> res = EconomicSet.create();
-            Object compilerConfigurationModule = ModuleAPI.getModule.invoke(compilerConfiguration.getClass());
-            res.add(compilerConfigurationModule);
-            Class<?> moduleClass = compilerConfigurationModule.getClass();
-            Object layer = new ModuleAPI(moduleClass, "getLayer").invoke(compilerConfigurationModule);
-            Class<? extends Object> layerClass = layer.getClass();
-            ModuleAPI getName = new ModuleAPI(moduleClass, "getName");
-            Set<Object> modules = new ModuleAPI(layerClass, "modules").invoke(layer);
-            Object descriptor = new ModuleAPI(moduleClass, "getDescriptor").invoke(compilerConfigurationModule);
-            Class<?> moduleDescriptorClass = descriptor.getClass();
-            Set<Object> requires = new ModuleAPI(moduleDescriptorClass, "requires").invoke(descriptor);
-            ModuleAPI requireNameGetter = null;
-            for (Object require : requires) {
-                if (requireNameGetter == null) {
-                    requireNameGetter = new ModuleAPI(require.getClass(), "name");
-                }
-                String name = requireNameGetter.invoke(require);
-                for (Object module : modules) {
-                    String moduleName = getName.invoke(module);
-                    if (moduleName.equals(name)) {
-                        res.add(module);
-                    }
-                }
+        if (!ok) {
+            if (graalRuntime.isBootstrapping()) {
+                throw GraalError.shouldNotReachHere("Class declaring a method for which a Graal intrinsic is available should be trusted for intrinsification: " + declaringClass.toJavaName());
             }
-            return res;
-        } catch (Exception e) {
-            throw new GraalError(e);
+            return false;
         }
+        return true;
     }
 }
