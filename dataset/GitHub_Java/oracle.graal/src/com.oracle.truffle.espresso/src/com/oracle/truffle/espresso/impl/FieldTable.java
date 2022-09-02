8025f3f11be0f2965650775ce4e434e945dc25e6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:38:18 +0200

    Fix comment.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/FieldTable.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/FieldTable.java
@@ -241,7 +241,7 @@ class FieldTable {
     /**
      * Greedily tries to fill the space between a parent's fields and its child.
      */
-    static final class FillingSchedule {
+    static class FillingSchedule {
         static final int[][] EMPTY_INT_ARRAY_ARRAY = new int[0][];
 
         List<ScheduleEntry> schedule;
