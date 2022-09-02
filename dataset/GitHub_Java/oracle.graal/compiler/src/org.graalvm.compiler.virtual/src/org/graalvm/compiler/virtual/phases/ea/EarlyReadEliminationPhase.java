36a384b58d8f2d15f5df0c49325bb3e3373bf3c4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:04:15 +0200

    read elimination: add java doc

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.virtual/src/org/graalvm/compiler/virtual/phases/ea/EarlyReadEliminationPhase.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.virtual/src/org/graalvm/compiler/virtual/phases/ea/EarlyReadEliminationPhase.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2011, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -27,26 +29,61 @@ import static org.graalvm.compiler.core.common.GraalOptions.EscapeAnalyzeOnly;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.StructuredGraph.ScheduleResult;
 import org.graalvm.compiler.nodes.cfg.ControlFlowGraph;
+import org.graalvm.compiler.nodes.spi.CoreProviders;
 import org.graalvm.compiler.phases.common.CanonicalizerPhase;
-import org.graalvm.compiler.phases.tiers.PhaseContext;
+import org.graalvm.compiler.phases.graph.ReentrantBlockIterator;
+import org.graalvm.word.LocationIdentity;
 
-public class EarlyReadEliminationPhase extends EffectsPhase<PhaseContext> {
+/**
+ * This phase performs read and (simple) write elimination on a graph. It operates on multiple
+ * granularities, i.e., before and after high-tier lowering. The phase iterates the graph in a
+ * reverse-post-order fashion {@linkplain ReentrantBlockIterator} and tracks the currently active
+ * value for a specific {@linkplain LocationIdentity}, which allows the removal of subsequent reads
+ * if no writes happen in between, etc. if the value read from memory is in a virtual register
+ * (node).
+ *
+ * A trivial example for read elimination can be seen below:
+ *
+ * <pre>
+ * int i = object.fieldValue;
+ * // code not changing object.fieldValue but using i
+ * consume(object.fieldValue);
+ * </pre>
+ *
+ * Read elimination will transform this piece of code to the code below and remove the second,
+ * unnecessary, memory read of the field:
+ *
+ * <pre>
+ * int i = object.fieldValue;
+ * // code not changing object.fieldValue but using i
+ * consume(i);
+ * </pre>
+ */
+public class EarlyReadEliminationPhase extends EffectsPhase<CoreProviders> {
+
+    protected final boolean considerGuards;
 
     public EarlyReadEliminationPhase(CanonicalizerPhase canonicalizer) {
         super(1, canonicalizer, true);
+        this.considerGuards = true;
+    }
+
+    public EarlyReadEliminationPhase(CanonicalizerPhase canonicalizer, boolean considerGuards) {
+        super(1, canonicalizer, true);
+        this.considerGuards = considerGuards;
     }
 
     @Override
-    protected void run(StructuredGraph graph, PhaseContext context) {
+    protected void run(StructuredGraph graph, CoreProviders context) {
         if (VirtualUtil.matches(graph, EscapeAnalyzeOnly.getValue(graph.getOptions()))) {
             runAnalysis(graph, context);
         }
     }
 
     @Override
-    protected Closure<?> createEffectsClosure(PhaseContext context, ScheduleResult schedule, ControlFlowGraph cfg) {
+    protected Closure<?> createEffectsClosure(CoreProviders context, ScheduleResult schedule, ControlFlowGraph cfg) {
         assert schedule == null;
-        return new ReadEliminationClosure(cfg);
+        return new ReadEliminationClosure(cfg, considerGuards);
     }
 
     @Override
