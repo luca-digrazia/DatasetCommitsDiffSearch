369540db157f704a65064eb08870cad1dfb524c7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:47:09 +0200

    Add test for Class.getPackage()

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.test/src/com/oracle/svm/test/GetPackageTest.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.test/src/com/oracle/svm/test/GetPackageTest.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class GetPackageTest {
+public final class GetPackageTest {
     @Test
     public void testGetPackage() {
         try {
