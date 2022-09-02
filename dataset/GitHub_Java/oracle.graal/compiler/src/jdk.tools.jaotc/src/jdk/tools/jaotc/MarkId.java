618e5689ac82a90f86ca18c26e239be8fdc5745a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 23:22:27 +0200

    AOT: Optimize reverse lookup of MarkId

--- dataset/GitHub_Java/oracle.graal/compiler/src/jdk.tools.jaotc/src/jdk/tools/jaotc/MarkId.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/jdk.tools.jaotc/src/jdk/tools/jaotc/MarkId.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -64,7 +64,6 @@ enum MarkId {
             lookup.put(e.value, e);
         }
     }
-
     MarkId(String name) {
         this.value = (int) (long) HotSpotJVMCIRuntime.runtime().getConfigStore().getConstants().get(name);
     }
