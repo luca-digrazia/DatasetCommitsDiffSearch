ce4d13e4b1e95e766eb39881f55a86d73c562db2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:19:28 +0200

    clear field breakpoint event requests

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/RequestedJDWPEvents.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/RequestedJDWPEvents.java
@@ -176,7 +176,7 @@ public class RequestedJDWPEvents {
             case 5: // class positive pattern
                 String classPattern = input.readString();
                 try {
-                    Pattern pattern = Pattern.compile(classPattern.replace('$', '_'));
+                    Pattern pattern = Pattern.compile(classPattern);
                     filter.addPositivePattern(pattern);
                 } catch (PatternSyntaxException ex) {
                     // wrong input pattern, silently ignore this breakpoint request then
@@ -185,7 +185,7 @@ public class RequestedJDWPEvents {
             case 6:
                 classPattern = input.readString();
                 try {
-                    Pattern pattern = Pattern.compile(classPattern.replace('$', '_'));
+                    Pattern pattern = Pattern.compile(classPattern);
                     filter.addExcludePattern(pattern);
                 } catch (PatternSyntaxException ex) {
                     // wrong input pattern, silently ignore this breakpoint request then
