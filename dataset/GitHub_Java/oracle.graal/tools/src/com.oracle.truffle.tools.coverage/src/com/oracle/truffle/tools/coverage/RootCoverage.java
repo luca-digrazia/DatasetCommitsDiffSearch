cc1e333bc36d4f784c205beb162b6200dab6c749
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:59:42 +0200

    Javadoc.

--- dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.coverage/src/com/oracle/truffle/tools/coverage/RootCoverage.java
+++ dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.coverage/src/com/oracle/truffle/tools/coverage/RootCoverage.java
@@ -26,39 +26,74 @@ package com.oracle.truffle.tools.coverage;
 
 import com.oracle.truffle.api.source.SourceSection;
 
-public class RootCoverage {
+/**
+ * Coverage data of a particular {@link com.oracle.truffle.api.instrumentation.StandardTags.RootTag
+ * root}. Coverage data is organized per {@link SourceSection} contained in this root.
+ *
+ * @since 19.3.0
+ */
+public final class RootCoverage {
 
-    private final SourceSection[] loadedStatements;
-    private final SourceSection[] coveredStatements;
+    private final SectionCoverage[] sectionCoverage;
     private final boolean covered;
+    private final long count;
     private final SourceSection sourceSection;
     private final String name;
 
-    RootCoverage(SourceSection[] loadedStatements, SourceSection[] coveredStatements, boolean covered, SourceSection sourceSection, String name) {
-        this.loadedStatements = loadedStatements;
-        this.coveredStatements = coveredStatements;
+    RootCoverage(SectionCoverage[] sectionCoverage, boolean covered, long count, SourceSection sourceSection, String name) {
+        this.sectionCoverage = sectionCoverage;
         this.covered = covered;
+        this.count = count;
         this.sourceSection = sourceSection;
         this.name = name;
     }
 
-    public SourceSection[] getLoadedStatements() {
-        return loadedStatements;
+    /**
+     * @return Coverage data for each {@link SourceSection} in this root
+     *
+     * @since 19.3.0
+     */
+    public SectionCoverage[] getSectionCoverage() {
+        return sectionCoverage;
     }
 
-    public SourceSection[] getCoveredStatements() {
-        return coveredStatements;
-    }
-
+    /**
+     * @return Was this root covered (i.e. executed) during the tracking execution.
+     *
+     * @since 19.3.0
+     */
     public boolean isCovered() {
         return covered;
     }
 
+    /**
+     * @return The source section this coverage relates to.
+     *
+     * @since 19.3.0
+     */
     public SourceSection getSourceSection() {
         return sourceSection;
     }
 
+    /**
+     * @return The name of the root (e.g. function/method name).
+     *
+     * @since 19.3.0
+     */
     public String getName() {
         return name;
+    }
+
+    /**
+     * This value is only available if the
+     * {@link com.oracle.truffle.tools.coverage.CoverageTracker.Config tracker config} specified to
+     * count executions.
+     * 
+     * @return How many times was the corresponding root executed. -1 if counting was disabled.
+     *
+     * @since 19.3.0
+     */
+    public long getCount() {
+        return count;
     }
 }
