d0ffa08b8c25fa92d16bc8a454061676734eaff3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:02:23 +0200

    AArch64: jaotc generates incorrect code for compressed OOPs with non-zero heap base
        On AArch64 if the VM jaotc is running in uses compressed oops with a
        zero base it will not emit instructions to add/subtract the heap base
        when compressing/decompressing oops. This causes a crash if the AOT'd
        shared library is loaded into a VM with non-zero heap base.
    
        Fix for JDK-8244164.
    
        Change-Id: I9983f6ac81b5ef4a91ce595f61455b90ba1546c3

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot.aarch64/src/org/graalvm/compiler/hotspot/aarch64/AArch64HotSpotMove.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot.aarch64/src/org/graalvm/compiler/hotspot/aarch64/AArch64HotSpotMove.java
@@ -38,7 +38,7 @@ import org.graalvm.compiler.asm.aarch64.AArch64Address;
 import org.graalvm.compiler.asm.aarch64.AArch64Assembler;
 import org.graalvm.compiler.asm.aarch64.AArch64MacroAssembler;
 import org.graalvm.compiler.core.common.CompressEncoding;
-import org.graalvm.compiler.hotspot.HotSpotMarkId;
+import org.graalvm.compiler.hotspot.GraalHotSpotVMConfig;
 import org.graalvm.compiler.lir.LIRInstruction;
 import org.graalvm.compiler.lir.LIRInstructionClass;
 import org.graalvm.compiler.lir.StandardOp.LoadConstantOp;
@@ -90,10 +90,12 @@ public class AArch64HotSpotMove {
         public static final LIRInstructionClass<BaseMove> TYPE = LIRInstructionClass.create(BaseMove.class);
 
         @LIRInstruction.Def({REG, HINT}) protected AllocatableValue result;
+        private final GraalHotSpotVMConfig config;
 
-        public BaseMove(AllocatableValue result) {
+        public BaseMove(AllocatableValue result, GraalHotSpotVMConfig config) {
             super(TYPE);
             this.result = result;
+            this.config = config;
         }
 
         @Override
@@ -104,7 +106,7 @@ public class AArch64HotSpotMove {
                 masm.add(64, scratch, scratch, 1);
                 masm.ldr(64, asRegister(result), AArch64Address.createBaseRegisterOnlyAddress(scratch));
                 masm.nop();
-                crb.recordMark(HotSpotMarkId.NARROW_KLASS_BASE_ADDRESS);
+                crb.recordMark(config.MARKID_NARROW_KLASS_BASE_ADDRESS);
             }
         }
 
@@ -234,16 +236,16 @@ public class AArch64HotSpotMove {
     // masm.cmov(64, result, result, ARMv8.zr, ARMv8Assembler.ConditionFlag.NE);
     // }
 
-    public static void decodeKlassPointer(CompilationResultBuilder crb, AArch64MacroAssembler masm, Register result, Register ptr, CompressEncoding encoding) {
+    public static void decodeKlassPointer(CompilationResultBuilder crb, AArch64MacroAssembler masm, Register result, Register ptr, CompressEncoding encoding, GraalHotSpotVMConfig config) {
         try (AArch64MacroAssembler.ScratchRegister sc = masm.getScratchRegister()) {
             Register scratch = sc.getRegister();
             boolean pic = GeneratePIC.getValue(crb.getOptions());
             if (pic || encoding.hasBase() || encoding.getShift() != 0) {
                 if (pic) {
-                    masm.adrpAdd(scratch);
+                    masm.addressOf(scratch);
                     masm.ldr(64, scratch, AArch64Address.createBaseRegisterOnlyAddress(scratch));
                     masm.add(64, result, scratch, ptr, AArch64Assembler.ExtendType.UXTX, encoding.getShift());
-                    crb.recordMark(HotSpotMarkId.NARROW_KLASS_BASE_ADDRESS);
+                    crb.recordMark(config.MARKID_NARROW_KLASS_BASE_ADDRESS);
                 } else {
                     masm.mov(scratch, encoding.getBase());
                     masm.add(64, result, scratch, ptr, AArch64Assembler.ExtendType.UXTX, encoding.getShift());
