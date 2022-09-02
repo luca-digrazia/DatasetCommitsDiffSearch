49419107c73447c54faec2763ae221a807210648
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:04:39 +0200

    Add missing transferToInterpreterAndInvalidate.

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/interop/LLVMReadStringNode.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/interop/LLVMReadStringNode.java
@@ -64,11 +64,6 @@ public abstract class LLVMReadStringNode extends LLVMNode {
         return address;
     }
 
-    @Specialization(guards = "isString(foreign)")
-    String readString(LLVMManagedPointer foreign) {
-        return (String) foreign.getObject();
-    }
-
     @Specialization(guards = "isForeign(foreign)")
     String readForeign(LLVMManagedPointer foreign,
                     @Cached("create()") ForeignReadStringNode read) {
@@ -82,10 +77,6 @@ public abstract class LLVMReadStringNode extends LLVMNode {
             readOther = insert(PointerReadStringNode.create());
         }
         return readOther.execute(address);
-    }
-
-    protected static boolean isString(LLVMManagedPointer pointer) {
-        return pointer.getOffset() == 0 && pointer.getObject() instanceof String;
     }
 
     protected static boolean isForeign(LLVMManagedPointer pointer) {
