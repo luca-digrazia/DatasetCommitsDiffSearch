e336d09ac93aee31ba7d6f4177bb4fb1b649f9a9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:39:41 +0200

    Remove unnecessary variable and add missing header

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.micro.benchmarks/src/micro/benchmarks/ConcurrentHashBenchmark.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.micro.benchmarks/src/micro/benchmarks/ConcurrentHashBenchmark.java
@@ -28,12 +28,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
 
 @State(Scope.Benchmark)
 public class ConcurrentHashBenchmark extends BenchmarkBase {
