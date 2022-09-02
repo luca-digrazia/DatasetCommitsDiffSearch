8e1b6d1e251bb40a4ae3ae8e4b99181ae7b18eea
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:27:55 +0200

    Migrate Truffle*MinInvokeThreshold options to --engine options

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.common.processor/src/org/graalvm/compiler/truffle/common/processor/Option.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.common.processor/src/org/graalvm/compiler/truffle/common/processor/Option.java
@@ -333,7 +333,7 @@ public class Option {
 
         option("TruffleMultiTier")
             .type("Boolean")
-            .category("EXPERT")
+            .category("USER")
             .def("false")
             .help("Whether to use multiple Truffle compilation tiers by default."),
     };
