847a37b4a60e53f7c06efd63dfaa4846d04febe3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:06:05 +0200

    Add missing Truffle boundary.

--- dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/predefined/wasi/WasiArgsGetNode.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/predefined/wasi/WasiArgsGetNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -43,7 +43,8 @@ package org.graalvm.wasm.predefined.wasi;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import org.graalvm.wasm.WasmContext;
 import org.graalvm.wasm.WasmLanguage;
-import org.graalvm.wasm.WasmInstance;
+import org.graalvm.wasm.WasmModule;
+import org.graalvm.wasm.WasmVoidResult;
 import org.graalvm.wasm.exception.WasmExecutionException;
 import org.graalvm.wasm.memory.WasmMemory;
 import org.graalvm.wasm.predefined.WasmBuiltinRootNode;
@@ -53,7 +54,7 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 import java.nio.charset.StandardCharsets;
 
 public class WasiArgsGetNode extends WasmBuiltinRootNode {
-    public WasiArgsGetNode(WasmLanguage language, WasmInstance module) {
+    WasiArgsGetNode(WasmLanguage language, WasmModule module) {
         super(language, module);
     }
 
@@ -80,7 +81,7 @@ public class WasiArgsGetNode extends WasmBuiltinRootNode {
             argvBuffPointer++;
         }
 
-        return 0;
+        return WasmVoidResult.getInstance();
     }
 
     @TruffleBoundary
