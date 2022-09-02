d020e326eee29a011aef40efd0693fdeae832eac
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:55:47 +0200

    make sure the correct copyright header is on all files

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/JDWPContextImpl.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/JDWPContextImpl.java
@@ -59,6 +59,7 @@ public final class JDWPContextImpl implements JDWPContext {
     private final EspressoContext context;
     private final Ids<Object> ids;
     private JDWPSetup setup;
+    private Object previous;
 
     public JDWPContextImpl(EspressoContext context) {
         this.context = context;
@@ -75,6 +76,20 @@ public final class JDWPContextImpl implements JDWPContext {
             return control.getEventListener();
         }
         return new EmptyListener();
+    }
+
+    public void enterTruffleContext() {
+        if (previous == null) {
+            if (context.canEnterOtherThread()) {
+                previous = context.getEnv().getContext().enter();
+            }
+        }
+    }
+
+    public void leaveTruffleContext() {
+        if (previous != null) {
+            context.getEnv().getContext().leave(previous);
+        }
     }
 
     public void finalizeContext() {
