cb1127a0996be79e8be6f34b2d14a0e5fe2b7c44
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:32:33 +0200

    Use CallSignatureNode directly instead of CallTarget in monomorphic NFI calls.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/NFILanguage.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/NFILanguage.java
@@ -1,142 +1,115 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * (a) the Software, and
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.oracle.truffle.nfi;
 
+import com.oracle.truffle.api.Assumption;
 import com.oracle.truffle.api.CallTarget;
-import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
+import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.TruffleLanguage;
-import com.oracle.truffle.api.TruffleLanguage.Env;
-import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.TruffleLanguage.ContextPolicy;
 import com.oracle.truffle.api.nodes.RootNode;
-import com.oracle.truffle.nfi.types.NativeLibraryDescriptor;
-import com.oracle.truffle.nfi.types.Parser;
-import java.util.Arrays;
+import com.oracle.truffle.nfi.NativeSource.Content;
+import com.oracle.truffle.nfi.NativeSource.ParsedLibrary;
+import com.oracle.truffle.nfi.NativeSource.ParsedSignature;
 
-@TruffleLanguage.Registration(name = "TruffleNFI", version = "0.1", mimeType = NFILanguage.MIME_TYPE, internal = true)
-public class NFILanguage extends TruffleLanguage<Env> {
+@TruffleLanguage.Registration(id = "nfi", name = "TruffleNFI", version = "0.1", characterMimeTypes = NFILanguage.MIME_TYPE, internal = true, contextPolicy = ContextPolicy.SHARED)
+public class NFILanguage extends TruffleLanguage<NFIContext> {
 
     public static final String MIME_TYPE = "application/x-native";
 
+    private final Assumption singleContextAssumption = Truffle.getRuntime().createAssumption("NFI single context");
+
     @Override
-    protected Env createContext(Env env) {
-        return env;
+    protected NFIContext createContext(Env env) {
+        return new NFIContext(env);
     }
 
-    private static class LoadLibraryNode extends RootNode {
-
-        private final String name;
-        private final int flags;
-
-        @CompilationFinal private LibFFILibrary cached;
-
-        LoadLibraryNode(String name, int flags) {
-            super(null);
-            this.name = name;
-            this.flags = flags;
-        }
-
-        @Override
-        public Object execute(VirtualFrame frame) {
-            if (cached == null) {
-                CompilerDirectives.transferToInterpreterAndInvalidate();
-                cached = LibFFILibrary.create(NativeAccess.loadLibrary(name, flags));
-            }
-            return cached;
-        }
+    @Override
+    protected boolean patchContext(NFIContext context, Env newEnv) {
+        context.patch(newEnv);
+        return true;
     }
 
-    private static class GetDefaultLibraryNode extends RootNode {
+    @Override
+    protected void initializeMultipleContexts() {
+        super.initializeMultipleContexts();
+        singleContextAssumption.invalidate();
+    }
 
-        GetDefaultLibraryNode() {
-            super(null);
-        }
+    static NFILanguage getCurrentLanguage() {
+        CompilerAsserts.neverPartOfCompilation("getCurrentLanguage");
+        return getCurrentLanguage(NFILanguage.class);
+    }
 
-        @Override
-        public Object execute(VirtualFrame frame) {
-            return LibFFILibrary.DEFAULT;
-        }
+    static Assumption getSingleContextAssumption() {
+        return getCurrentLanguage().singleContextAssumption;
     }
 
     @Override
     protected CallTarget parse(ParsingRequest request) throws Exception {
-        String library = request.getSource().getCode();
-        String[] lines = library.split("\n");
-        RootNode root;
-        if (lines.length > 1) {
-            library = lines[0];
-        }
-        NativeLibraryDescriptor descriptor = Parser.parseLibraryDescriptor(library);
+        CharSequence nfiSource = request.getSource().getCharacters();
+        NativeSource source = Parser.parseNFISource(nfiSource);
 
-        if (descriptor.isDefaultLibrary()) {
-            root = new GetDefaultLibraryNode();
+        String backendId;
+        if (source.isDefaultBackend()) {
+            backendId = "native";
         } else {
-            int flags = 0;
-            boolean lazyOrNow = false;
-            if (descriptor.getFlags() != null) {
-                for (String flag : descriptor.getFlags()) {
-                    switch (flag) {
-                        case "RTLD_GLOBAL":
-                            flags |= NativeAccess.RTLD_GLOBAL;
-                            break;
-                        case "RTLD_LOCAL":
-                            flags |= NativeAccess.RTLD_LOCAL;
-                            break;
-                        case "RTLD_LAZY":
-                            flags |= NativeAccess.RTLD_LAZY;
-                            lazyOrNow = true;
-                            break;
-                        case "RTLD_NOW":
-                            flags |= NativeAccess.RTLD_NOW;
-                            lazyOrNow = true;
-                            break;
-                    }
-                }
-            }
-            if (!lazyOrNow) {
-                // default to 'RTLD_NOW' if neither 'RTLD_LAZY' nor 'RTLD_NOW' was specified
-                flags |= NativeAccess.RTLD_NOW;
-            }
-            root = new LoadLibraryNode(descriptor.getFilename(), flags);
+            backendId = source.getNFIBackendId();
         }
 
-        if (lines.length > 1) {
-            root = new LookupAndBind(root, Arrays.asList(lines).subList(1, lines.length));
+        Content c = source.getContent();
+        assert c != null;
+        if (c instanceof ParsedLibrary) {
+            ParsedLibrary lib = (ParsedLibrary) c;
+            return Truffle.getRuntime().createCallTarget(new NFIRootNode(this, lib, backendId));
+        } else {
+            ParsedSignature sig = (ParsedSignature) c;
+            RootNode buildSignature = new SignatureRootNode(this, backendId, sig.getBuildSignatureNode());
+            return Truffle.getRuntime().createCallTarget(buildSignature);
         }
-
-        return Truffle.getRuntime().createCallTarget(root);
     }
 
     @Override
-    protected Object getLanguageGlobal(Env context) {
-        return null;
-    }
-
-    @Override
-    protected boolean isObjectOfLanguage(Object object) {
-        return object instanceof LibFFIFunction || object instanceof LibFFILibrary || object instanceof NativePointer || object instanceof NativeString;
+    protected boolean isThreadAccessAllowed(Thread thread, boolean singleThreaded) {
+        return true;
     }
 }
