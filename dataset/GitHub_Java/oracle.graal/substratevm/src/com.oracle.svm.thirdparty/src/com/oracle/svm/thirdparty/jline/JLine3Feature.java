e5509c9f71a677bb442d0df057d182ec3e9bdb0d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 15:10:30 +0200

    Cleanup.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.thirdparty/src/com/oracle/svm/thirdparty/jline/JLine3Feature.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.thirdparty/src/com/oracle/svm/thirdparty/jline/JLine3Feature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,36 +22,56 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+/*
+ * Copyright (c) 2002-2020, the original author or authors.
+ *
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
 package com.oracle.svm.thirdparty.jline;
 
-import java.util.function.BooleanSupplier;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
-import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.Feature;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
-import com.oracle.svm.core.annotate.Substitute;
-import com.oracle.svm.core.annotate.TargetClass;
+import com.oracle.svm.core.jdk.Resources;
 
 @AutomaticFeature
 final class JLine3Feature implements Feature {
+
+    private static final List<String> RESOURCES = Arrays.asList(
+                    "capabilities.txt",
+                    "colors.txt",
+                    "ansi.caps",
+                    "dumb.caps",
+                    "dumb-color.caps",
+                    "screen.caps",
+                    "screen-256color.caps",
+                    "windows.caps",
+                    "windows-256color.caps",
+                    "windows-conemu.caps",
+                    "windows-vtp.caps",
+                    "xterm.caps",
+                    "xterm-256color.caps");
+    private static final String RESOURCE_PATH = "org/graalvm/shadowed/org/jline/utils/";
+    private static final String JNA_SUPPORT_IMPL = "org.graalvm.shadowed.org.jline.terminal.impl.jna.JnaSupportImpl";
+
     @Override
     public boolean isInConfiguration(IsInConfigurationAccess access) {
-        return access.findClassByName("org.jline.terminal.impl.jna.JnaSupportImpl") != null;
+        return access.findClassByName(JNA_SUPPORT_IMPL) != null;
     }
 
-    static final class IsEnabled implements BooleanSupplier {
-        @Override
-        public boolean getAsBoolean() {
-            return ImageSingletons.contains(JLine3Feature.class);
+    @Override
+    public void beforeAnalysis(BeforeAnalysisAccess access) {
+        for (String resource : RESOURCES) {
+            String resourcePath = RESOURCE_PATH + resource;
+            final InputStream resourceAsStream = ClassLoader.getSystemResourceAsStream(resourcePath);
+            Resources.registerResource(resourcePath, resourceAsStream);
         }
-    }
-}
-
-@TargetClass(className = "org.jline.terminal.impl.jna.JnaSupportImpl", onlyWith = JLine3Feature.IsEnabled.class)
-final class Target_org_jline_terminal_impl_jna_JnaSupportImpl_open {
-    @Substitute
-    protected static Object open() {
-        throw new RuntimeException();
     }
 }
