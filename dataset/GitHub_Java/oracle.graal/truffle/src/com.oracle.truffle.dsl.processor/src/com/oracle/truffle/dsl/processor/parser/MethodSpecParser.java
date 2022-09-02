902245d91fe5307025703d932d4f63e8198634ca
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 00:53:41 +0200

    Fixed os dependent line feed.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/parser/MethodSpecParser.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/parser/MethodSpecParser.java
@@ -245,12 +245,6 @@ public final class MethodSpecParser {
             if (resolvedParameter == null) {
                 return null;
             }
-            for (ParameterSpec annotationSpec : spec.getAnnotations()) {
-                if (annotationSpec.matches(actualType)) {
-                    return null;
-                }
-            }
-
             parsedParams.add(resolvedParameter);
             typeIndex++;
             specificationIndex++;
