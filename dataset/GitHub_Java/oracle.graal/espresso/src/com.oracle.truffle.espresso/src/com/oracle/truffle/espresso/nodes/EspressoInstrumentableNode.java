8f7a160b03235dbc5d3a3b404b414b45a43bfc70
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:34:00 +0200

    use variable instead of method call

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoInstrumentableNode.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoInstrumentableNode.java
@@ -22,7 +22,6 @@
  */
 package com.oracle.truffle.espresso.nodes;
 
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.frame.Frame;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.instrumentation.GenerateWrapper;
@@ -67,7 +66,6 @@ public abstract class EspressoInstrumentableNode extends Node implements Instrum
     }
 
     @ExportMessage
-    @TruffleBoundary
     @SuppressWarnings("static-method")
     public final Object getScope(Frame frame, @SuppressWarnings("unused") boolean nodeEnter) {
         // construct the current scope with valid local variables information
