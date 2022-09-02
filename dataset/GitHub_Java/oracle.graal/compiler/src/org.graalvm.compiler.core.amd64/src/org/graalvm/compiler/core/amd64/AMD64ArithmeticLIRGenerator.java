2c5c2b4a99a3c46e00fd4ab87c2a84091a5b1ddd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:34:16 +0200

    Support any enum for optionType in SubstrateOptionsParser

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.amd64/src/org/graalvm/compiler/core/amd64/AMD64ArithmeticLIRGenerator.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.amd64/src/org/graalvm/compiler/core/amd64/AMD64ArithmeticLIRGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -120,7 +120,6 @@ import org.graalvm.compiler.lir.amd64.AMD64MathExpOp;
 import org.graalvm.compiler.lir.amd64.AMD64MathLog10Op;
 import org.graalvm.compiler.lir.amd64.AMD64MathLogOp;
 import org.graalvm.compiler.lir.amd64.AMD64MathPowOp;
-import org.graalvm.compiler.lir.amd64.AMD64MathSignumOp;
 import org.graalvm.compiler.lir.amd64.AMD64MathSinOp;
 import org.graalvm.compiler.lir.amd64.AMD64MathTanOp;
 import org.graalvm.compiler.lir.amd64.AMD64Move;
@@ -1153,13 +1152,6 @@ public class AMD64ArithmeticLIRGenerator extends ArithmeticLIRGenerator implemen
     @Override
     public Value emitMathPow(Value x, Value y) {
         return new AMD64MathPowOp().emitLIRWrapper(getLIRGen(), x, y);
-    }
-
-    @Override
-    public Value emitMathSignum(Value input) {
-        Variable result = getLIRGen().newVariable(input.getValueKind());
-        getLIRGen().append(new AMD64MathSignumOp(getLIRGen(), result, asAllocatable(input)));
-        return result;
     }
 
     protected AMD64LIRGenerator getAMD64LIRGen() {
