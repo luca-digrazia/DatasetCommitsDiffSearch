34f89c8f5a9e2b460d03acb8a2affb0cf11f404a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 00:21:17 +0200

    Re-read jvm specs -> if multiple maximally specific methods, fail *at invocation*.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/InterfaceTables.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/InterfaceTables.java
@@ -147,14 +147,27 @@ class InterfaceTables {
     }
 
     private static Method resolveMaximallySpecific(Method m1, Method m2) {
+        switch (checkMaximallySpecific(m1, m2)) {
+            case 1:
+                return m1;
+            case 2:
+                return m2;
+            case 0:
+                Method m = new Method(m2);
+                m.setPoisonPill();
+                return m;
+            default:
+                return m1;
+        }
+    }
+
+    private static int checkMaximallySpecific(Method m1, Method m2) {
         if (m1.getDeclaringKlass().isAssignableFrom(m2.getDeclaringKlass())) {
-            return m2;
+            return 2;
         } else if (m2.getDeclaringKlass().isAssignableFrom(m1.getDeclaringKlass())) {
-            return m1;
+            return 1;
         } else {
-            Method m = new Method(m2);
-            m.setPoisonPill();
-            return m;
+            return 0;
         }
     }
 
