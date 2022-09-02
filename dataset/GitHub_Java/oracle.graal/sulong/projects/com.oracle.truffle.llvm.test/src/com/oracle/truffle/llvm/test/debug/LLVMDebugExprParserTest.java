fef2aa1adc0d2937d83eaf4b648be75f87e963ec
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:52:30 +0200

    adjusted tests for invalid expressions, disabled console output for     working tests

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/debug/LLVMDebugExprParserTest.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/debug/LLVMDebugExprParserTest.java
@@ -1,32 +1,3 @@
-/*
- * Copyright (c) 2019, Oracle and/or its affiliates.
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are
- * permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
- * conditions and the following disclaimer in the documentation and/or other materials provided
- * with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written
- * permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.oracle.truffle.llvm.test.debug;
 
 import static org.junit.Assert.assertEquals;
@@ -67,7 +38,7 @@ import com.oracle.truffle.tck.DebuggerTester;
 public final class LLVMDebugExprParserTest {
 
     private static final Path BC_DIR_PATH = Paths.get(TestOptions.TEST_SUITE_PATH, "debugexpr");
-    private static final Path SRC_DIR_PATH = Paths.get(TestOptions.PROJECT_ROOT, "..", "tests", "com.oracle.truffle.llvm.tests.debugexpr", "debugexpr");
+    private static final Path SRC_DIR_PATH = Paths.get(TestOptions.PROJECT_ROOT, "..", "tests", "com.oracle.truffle.llvm.tests.debugexpr", "debug");
     private static final Path TRACE_DIR_PATH = Paths.get(TestOptions.PROJECT_ROOT, "..", "tests", "com.oracle.truffle.llvm.tests.debugexpr", "testExpr");
 
     private static final String OPTION_ENABLE_LVI = "llvm.enableLVI";
@@ -80,7 +51,7 @@ public final class LLVMDebugExprParserTest {
     }
 
     static void setContextOptions(Builder contextBuilder) {
-        // contextBuilder.option(EXPERIMENTAL_OPTIONS, String.valueOf(true));
+// contextBuilder.option(EXPERIMENTAL_OPTIONS, String.valueOf(true));
         contextBuilder.option(OPTION_ENABLE_LVI, String.valueOf(true));
     }
 
