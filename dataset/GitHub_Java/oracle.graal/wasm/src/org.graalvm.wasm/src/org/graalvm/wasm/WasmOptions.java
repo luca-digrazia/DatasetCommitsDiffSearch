6c4d5e6bf9c9442d9791c56e21edc9db7bfe7707
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:00:15 +0200

    Add newline

--- dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/WasmOptions.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/WasmOptions.java
@@ -66,8 +66,5 @@ public class WasmOptions {
     public static OptionType<StoreConstantsPolicyEnum> StoreConstantsPolicyOptionType = new OptionType<>("StoreConstantsPolicy", StoreConstantsPolicyEnum::valueOf);
 
     @Option(help = "Whenever to store the constants in a pool or not.", category = OptionCategory.EXPERT, stability = OptionStability.EXPERIMENTAL)//
-    public static final OptionKey<StoreConstantsPolicyEnum> StoreConstantsPolicy = new OptionKey<>(StoreConstantsPolicyEnum.NONE, StoreConstantsPolicyOptionType);
-
-    @Option(help = "Use sun.misc.Unsafe-based memory.", category = OptionCategory.EXPERT, stability = OptionStability.EXPERIMENTAL)//
-    public static final OptionKey<Boolean> UseUnsafeMemory = new OptionKey<>(false);
+    public static final OptionKey<StoreConstantsPolicyEnum> StoreConstantsInPool = new OptionKey<>(StoreConstantsPolicyEnum.NONE, StoreConstantsPolicyOptionType);
 }
