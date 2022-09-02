9c934668a4f4a7943cc9a4d55e33debbfacdb809
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 13:38:17 +0200

    Clear ResourceBundle cacheList field in the final image

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/ResourceBundleSubstitutions.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/ResourceBundleSubstitutions.java
@@ -4,7 +4,9 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -27,13 +29,17 @@ package com.oracle.svm.core.jdk;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.ResourceBundle.Control;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import org.graalvm.nativeimage.ImageSingletons;
 
 import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.RecomputeFieldValue;
+import com.oracle.svm.core.annotate.RecomputeFieldValue.Kind;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
+import com.oracle.svm.core.annotate.TargetElement;
 import com.oracle.svm.core.util.VMError;
 
 import sun.util.resources.OpenListResourceBundle;
@@ -41,6 +47,9 @@ import sun.util.resources.OpenListResourceBundle;
 @TargetClass(java.util.ResourceBundle.class)
 @SuppressWarnings({"unused"})
 final class Target_java_util_ResourceBundle {
+
+    @Alias @RecomputeFieldValue(kind = Kind.FromAlias)//
+    private static ConcurrentMap<?, ?> cacheList = new ConcurrentHashMap<>();
 
     @Substitute
     private static ResourceBundle getBundle(String baseName) {
@@ -61,6 +70,33 @@ final class Target_java_util_ResourceBundle {
     private static ResourceBundle getBundle(String baseName, Locale targetLocale, Control control) {
         return ImageSingletons.lookup(LocalizationSupport.class).getCached(baseName, targetLocale);
     }
+
+    @Substitute
+    private static ResourceBundle getBundle(String baseName, Locale locale, ClassLoader loader) {
+        return ImageSingletons.lookup(LocalizationSupport.class).getCached(baseName, locale);
+    }
+
+    @Substitute
+    private static ResourceBundle getBundle(String baseName, Locale targetLocale, ClassLoader loader, Control control) {
+        return ImageSingletons.lookup(LocalizationSupport.class).getCached(baseName, targetLocale);
+    }
+
+    /*
+     * Currently there is no support for the module system at run time. Module arguments are
+     * therefore ignored.
+     */
+
+    @TargetElement(onlyWith = JDK11OrLater.class)
+    @Substitute
+    private static ResourceBundle getBundle(String baseName, Target_java_lang_Module module) {
+        return ImageSingletons.lookup(LocalizationSupport.class).getCached(baseName, Locale.getDefault());
+    }
+
+    @TargetElement(onlyWith = JDK11OrLater.class)
+    @Substitute
+    private static ResourceBundle getBundle(String baseName, Locale targetLocale, Target_java_lang_Module module) {
+        return ImageSingletons.lookup(LocalizationSupport.class).getCached(baseName, targetLocale);
+    }
 }
 
 @TargetClass(java.util.ListResourceBundle.class)
@@ -69,7 +105,7 @@ final class Target_java_util_ListResourceBundle {
 
     @Substitute
     private void loadLookup() {
-        throw VMError.unsupportedFeature("Resource bundle lookup must be loaded during native image generation");
+        throw VMError.unsupportedFeature("Resource bundle lookup must be loaded during native image generation: " + getClass().getTypeName());
     }
 }
 
@@ -79,7 +115,7 @@ final class Target_sun_util_resources_OpenListResourceBundle {
 
     @Substitute
     private void loadLookup() {
-        throw VMError.unsupportedFeature("Resource bundle lookup must be loaded during native image generation");
+        throw VMError.unsupportedFeature("Resource bundle lookup must be loaded during native image generation: " + getClass().getTypeName());
     }
 }
 
@@ -91,7 +127,7 @@ final class Target_sun_util_resources_ParallelListResourceBundle {
 
     @Substitute
     private void setParallelContents(OpenListResourceBundle rb) {
-        throw VMError.unsupportedFeature("Resource bundle lookup must be loaded during native image generation");
+        throw VMError.unsupportedFeature("Resource bundle lookup must be loaded during native image generation: " + getClass().getTypeName());
     }
 
     @Substitute
@@ -102,9 +138,16 @@ final class Target_sun_util_resources_ParallelListResourceBundle {
     @Substitute
     private void loadLookupTablesIfNecessary() {
         if (lookup == null) {
-            throw VMError.unsupportedFeature("Resource bundle lookup must be loaded during native image generation");
+            throw VMError.unsupportedFeature("Resource bundle lookup must be loaded during native image generation: " + getClass().getTypeName());
         }
     }
+}
+
+@TargetClass(java.text.DateFormatSymbols.class)
+final class Target_java_text_DateFormatSymbols {
+
+    @Alias @RecomputeFieldValue(kind = Kind.FromAlias) //
+    private static ConcurrentMap<?, ?> cachedInstances = new ConcurrentHashMap<>(3);
 }
 
 /** Dummy class to have a class with the file's name. */
