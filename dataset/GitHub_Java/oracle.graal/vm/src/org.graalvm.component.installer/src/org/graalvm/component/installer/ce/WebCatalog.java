b8e10956d0dcf36090ea09a0699b4c94ed1a531a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:43:07 +0200

    [GR-29030] Accept any compatible catalog. Merge catalog sources by default.

--- dataset/GitHub_Java/oracle.graal/vm/src/org.graalvm.component.installer/src/org/graalvm/component/installer/ce/WebCatalog.java
+++ dataset/GitHub_Java/oracle.graal/vm/src/org.graalvm.component.installer/src/org/graalvm/component/installer/ce/WebCatalog.java
@@ -24,113 +24,236 @@
  */
 package org.graalvm.component.installer.ce;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.net.NoRouteToHostException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.Enumeration;
 import java.util.Map;
 import java.util.Properties;
-import java.util.jar.JarFile;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import org.graalvm.component.installer.BundleConstants;
 import org.graalvm.component.installer.CommandInput;
 import org.graalvm.component.installer.CommonConstants;
 import org.graalvm.component.installer.Feedback;
-import org.graalvm.component.installer.jar.JarMetaLoader;
+import org.graalvm.component.installer.IncompatibleException;
 import org.graalvm.component.installer.model.ComponentRegistry;
 import org.graalvm.component.installer.model.ComponentStorage;
 import org.graalvm.component.installer.remote.FileDownloader;
-import org.graalvm.component.installer.persist.MetadataLoader;
 import org.graalvm.component.installer.remote.RemotePropertiesStorage;
 import org.graalvm.component.installer.SoftwareChannel;
+import org.graalvm.component.installer.SoftwareChannelSource;
+import org.graalvm.component.installer.SystemUtils;
+import org.graalvm.component.installer.Version;
+import org.graalvm.component.installer.model.ComponentInfo;
+import org.graalvm.component.installer.model.RemoteInfoProcessor;
 
-/**
- *
- * @author sdedic
- */
 public class WebCatalog implements SoftwareChannel {
-    private CommandInput input;
-    private Feedback feedback;
-    private String urlString;
-    private URL catalogURL;
-    private ComponentRegistry local;
+    private final String urlString;
+    private final SoftwareChannelSource source;
 
-    @Override
-    public boolean setupLocation(String url) {
-        int schColon = url.indexOf(':');
-        if (schColon == -1) {
-            return false;
-        }
-        String scheme = url.toLowerCase().substring(0, schColon);
-        if (acceptURLScheme(scheme)) {
-            urlString = url;
-            return true;
-        } else {
-            return false;
-        }
+    private URL catalogURL;
+    private Feedback feedback;
+    private ComponentRegistry local;
+    private ComponentStorage storage;
+    private RuntimeException savedException;
+    private RemoteInfoProcessor remoteProcessor = RemoteInfoProcessor.NONE;
+
+    /**
+     * The accepted version - only exact match is supported at the moment.
+     */
+    private Version.Match matchVersion;
+
+    /**
+     * If true, enables loading different versions from V1 catalogs.
+     */
+    private boolean enableV1Versions;
+
+    public WebCatalog(String u, SoftwareChannelSource source) {
+        this.urlString = u;
+        this.source = source;
     }
 
-    protected boolean acceptURLScheme(String scheme) {
+    public RemoteInfoProcessor getRemoteProcessor() {
+        return remoteProcessor;
+    }
+
+    public void setRemoteProcessor(RemoteInfoProcessor remoteProcessor) {
+        this.remoteProcessor = remoteProcessor;
+    }
+
+    public Version.Match getMatchVersion() {
+        return matchVersion;
+    }
+
+    public boolean isEnableV1Versions() {
+        return enableV1Versions;
+    }
+
+    /**
+     * Enables version processing in V1 catalogs. Note that not complete version string semantics is
+     * possible with v1 format, as version may contain underscores (_), which are delimiters in v1
+     * catalogs.
+     * <p/>
+     * The default setting is false.
+     * 
+     * @param enableV1Versions true, if should be enabled.
+     */
+    public void setEnableV1Versions(boolean enableV1Versions) {
+        this.enableV1Versions = enableV1Versions;
+    }
+
+    /**
+     * Overrides versions loaded by this catalog. By default own version only is accepted. If
+     * matchVersion is set, the catalog will load matching version(s). At this moment <b>only exact
+     * match is supported</b>.
+     * 
+     * @param aMatchVersion
+     */
+    public void setMatchVersion(Version.Match aMatchVersion) {
+        this.matchVersion = aMatchVersion;
+    }
+
+    protected static boolean acceptURLScheme(String scheme, String urlSpec) {
         switch (scheme) {
-            case "http":
-            case "https":
-            case "ftp":
-            case "ftps":
+            case "http":    // NOI18N
+            case "https":   // NOI18N
+            case "ftp":     // NOI18N
+            case "ftps":    // NOI18N
                 return true;
+            case "file":    // NOI18N
+                // accept only regular files
+                try {
+                    Path p = new File(new URI(urlSpec)).toPath();
+                    return Files.isRegularFile(p) && Files.isReadable(p);
+                } catch (URISyntaxException ex) {
+                    // cannot be converted to file, bail out
+                    break;
+                }
         }
         return false;
     }
 
-    @Override
     public void init(CommandInput in, Feedback out) {
-        assert this.input == null;
-
-        this.input = in;
-        this.feedback = out.withBundle(WebCatalog.class);
-        this.local = in.getLocalRegistry();
+        init(in.getLocalRegistry(), out);
     }
 
-    @SuppressWarnings("unchecked")
+    public void init(ComponentRegistry aLocal, Feedback out) {
+        this.feedback = out.withBundle(WebCatalog.class);
+        this.local = aLocal;
+    }
+
     @Override
-    public ComponentRegistry getRegistry() {
-        FileDownloader dn;
-        try {
-            catalogURL = new URL(urlString);
-            dn = new FileDownloader(feedback.l10n("REMOTE_CatalogLabel"), catalogURL, feedback);
-            dn.download();
-        } catch (MalformedURLException ex) {
-            throw feedback.failure("REMOTE_InvalidURL", ex, catalogURL, ex.getLocalizedMessage());
-        } catch (NoRouteToHostException | ConnectException ex) {
-            throw feedback.failure("REMOTE_ErrorDownloadCatalogProxy", ex, catalogURL, ex.getLocalizedMessage());
-        } catch (FileNotFoundException ex) {
-            throw feedback.failure("REMOTE_ErrorDownloadCatalogNotFound", ex, catalogURL);
-        } catch (IOException ex) {
-            throw feedback.failure("REMOTE_ErrorDownloadCatalog", ex, catalogURL, ex.getLocalizedMessage());
+    public ComponentStorage getStorage() {
+        if (this.storage != null) {
+            return this.storage;
         }
 
-        StringBuilder sb = new StringBuilder();
         Map<String, String> graalCaps = local.getGraalCapabilities();
-        sb.append(graalCaps.get(CommonConstants.CAP_GRAALVM_VERSION).toLowerCase());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(SystemUtils.patternOsName(graalCaps.get(CommonConstants.CAP_OS_NAME)).toLowerCase());
         sb.append("_");
-        sb.append(graalCaps.get(CommonConstants.CAP_OS_NAME).toLowerCase());
-        sb.append("_");
-        sb.append(graalCaps.get(CommonConstants.CAP_OS_ARCH).toLowerCase());
+        sb.append(SystemUtils.patternOsArch(graalCaps.get(CommonConstants.CAP_OS_ARCH).toLowerCase()));
+
+        try {
+            catalogURL = new URL(urlString);
+        } catch (MalformedURLException ex) {
+            throw feedback.failure("REMOTE_InvalidURL", ex, catalogURL, ex.getLocalizedMessage());
+        }
 
         Properties props = new Properties();
+        // create the storage. If the init fails, but process will not terminate, the storage will
+        // serve no components on the next call.
+        RemotePropertiesStorage newStorage = createPropertiesStorage(feedback, local, props, sb.toString(), catalogURL);
+        if (remoteProcessor != null) {
+            newStorage.setRemoteProcessor(remoteProcessor);
+        }
+        Properties loadProps = new Properties();
+        FileDownloader dn;
+        try {
+            // avoid duplicate (failed) downloads
+            if (savedException != null) {
+                throw savedException;
+            }
+            catalogURL = new URL(urlString);
+            String l = source.getLabel();
+            dn = new FileDownloader(feedback.l10n(l == null || l.isEmpty() ? "REMOTE_CatalogLabel2" : "REMOTE_CatalogLabel", l), catalogURL, feedback);
+            dn.download();
+        } catch (NoRouteToHostException | ConnectException ex) {
+            throw savedException = feedback.failure("REMOTE_ErrorDownloadCatalogProxy", ex, catalogURL, ex.getLocalizedMessage());
+        } catch (FileNotFoundException ex) {
+            // treat missing resources as non-fatal errors, print warning
+            feedback.error("REMOTE_WarningErrorDownloadCatalogNotFoundSkip", ex, catalogURL);
+            this.storage = newStorage;
+            return storage;
+        } catch (IOException ex) {
+            throw savedException = feedback.failure("REMOTE_ErrorDownloadCatalog", ex, catalogURL, ex.getLocalizedMessage());
+        }
+        // download is successful; if the processing fails after download, next call will report an
+        // empty catalog.
+        this.storage = newStorage;
+
+        StringBuilder oldGraalPref = new StringBuilder("^" + BundleConstants.GRAAL_COMPONENT_ID);
+        oldGraalPref.append('.');
+
+        String graalVersionString;
+        Version normalizedVersion;
+
+        if (matchVersion != null) {
+            graalVersionString = matchVersion.getVersion().displayString();
+            normalizedVersion = matchVersion.getVersion().installVersion();
+        } else {
+            // read from the release file
+            graalVersionString = graalCaps.get(CommonConstants.CAP_GRAALVM_VERSION).toLowerCase();
+            normalizedVersion = local.getGraalVersion().installVersion();
+        }
+
+        StringBuilder graalPref = new StringBuilder(oldGraalPref);
+
+        oldGraalPref.append(Pattern.quote(graalVersionString));
+
+        oldGraalPref.append('_').append(sb);
+        graalPref.append(sb).append('/');
+        graalPref.append("(?<ver>[^/]+)$"); // NOI18N
+
         try (FileInputStream fis = new FileInputStream(dn.getLocalFile())) {
-            props.load(fis);
+            loadProps.load(fis);
         } catch (IllegalArgumentException | IOException ex) {
             throw feedback.failure("REMOTE_CorruptedCatalogFile", ex, catalogURL);
         }
-        if (props.getProperty(BundleConstants.GRAAL_COMPONENT_ID + "." + sb.toString()) == null) {
+
+        Pattern oldPrefixPattern = Pattern.compile(oldGraalPref.toString(), Pattern.CASE_INSENSITIVE);
+        Pattern newPrefixPattern = Pattern.compile(graalPref.toString(), Pattern.CASE_INSENSITIVE);
+        Stream<String> propNames = loadProps.stringPropertyNames().stream();
+        boolean foundPrefix = propNames.anyMatch(p -> {
+            if (oldPrefixPattern.matcher(p).matches()) {
+                return true;
+            }
+            Matcher m = newPrefixPattern.matcher(p);
+            if (!m.find() || m.start() > 0) {
+                return false;
+            }
+            try {
+                Version v = Version.fromString(m.group("ver")); // NOI18N
+                return normalizedVersion.match(Version.Match.Type.INSTALLABLE).test(v);
+            } catch (IllegalArgumentException ex) {
+                return false;
+            }
+        });
+        if (!foundPrefix) {
             boolean graalPrefixFound = false;
             boolean componentFound = false;
-            for (String s : Collections.list((Enumeration<String>) props.propertyNames())) {
+            for (String s : loadProps.stringPropertyNames()) {
                 if (s.startsWith(BundleConstants.GRAAL_COMPONENT_ID)) {
                     graalPrefixFound = true;
                 }
@@ -138,27 +261,60 @@ public class WebCatalog implements SoftwareChannel {
                     componentFound = true;
                 }
             }
-            if (!(graalPrefixFound && componentFound)) {
+            if (!componentFound) {
+                // no graal prefix, no components
+                feedback.verboseOutput("REMOTE_CatalogDoesNotContainComponents", catalogURL);
+                return newStorage;
+            } else if (!graalPrefixFound) {
+                // strange thing, no graal declaration, but components are there ?
                 throw feedback.failure("REMOTE_CorruptedCatalogFile", null, catalogURL);
             } else {
-                throw feedback.failure("REMOTE_UnsupportedGraalVersion", null,
-                                graalCaps.get(CommonConstants.CAP_GRAALVM_VERSION),
-                                graalCaps.get(CommonConstants.CAP_OS_NAME),
-                                graalCaps.get(CommonConstants.CAP_OS_ARCH));
+                throw new IncompatibleException(
+                                feedback.l10n("REMOTE_UnsupportedGraalVersion",
+                                                graalCaps.get(CommonConstants.CAP_GRAALVM_VERSION),
+                                                graalCaps.get(CommonConstants.CAP_OS_NAME),
+                                                graalCaps.get(CommonConstants.CAP_OS_ARCH)),
+                                null);
             }
         }
-        ComponentStorage storage = new RemotePropertiesStorage(feedback, local, props, sb.toString(), catalogURL);
-        return new ComponentRegistry(feedback, storage);
+        props.putAll(loadProps);
+        return newStorage;
+    }
+
+    protected RemotePropertiesStorage createPropertiesStorage(Feedback aFeedback,
+                    ComponentRegistry aLocal, Properties props, String selector, URL baseURL) {
+        return new RemotePropertiesStorage(
+                        aFeedback, aLocal, props, selector, null, baseURL);
     }
 
     @Override
-    public MetadataLoader createLocalFileLoader(Path localFile) throws IOException {
-        return new JarMetaLoader(new JarFile(localFile.toFile(), false), feedback);
-    }
-
-    @Override
-    public FileDownloader configureDownloader(FileDownloader dn) {
+    public FileDownloader configureDownloader(ComponentInfo cInfo, FileDownloader dn) {
         return dn;
     }
 
+    public static class WebCatalogFactory implements SoftwareChannel.Factory {
+        private CommandInput input;
+
+        @Override
+        public SoftwareChannel createChannel(SoftwareChannelSource src, CommandInput in, Feedback fb) {
+            String urlSpec = src.getLocationURL();
+            int schColon = urlSpec.indexOf(':'); // NOI18N
+            if (schColon == -1) {
+                return null;
+            }
+            String scheme = urlSpec.toLowerCase().substring(0, schColon);
+            if (acceptURLScheme(scheme, urlSpec)) {
+                WebCatalog c = new WebCatalog(urlSpec, src);
+                c.init(in, fb);
+                return c;
+            }
+            return null;
+        }
+
+        @Override
+        public void init(CommandInput in, Feedback out) {
+            assert this.input == null;
+            this.input = in;
+        }
+    }
 }
--- dataset/GitHub_Java/oracle.graal/vm/src/org.graalvm.component.installer/src/org/graalvm/component/installer/remote/MergeStorage.java
+++ dataset/GitHub_Java/oracle.graal/vm/src/org.graalvm.component.installer/src/org/graalvm/component/installer/remote/MergeStorage.java
@@ -31,46 +31,109 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.graalvm.component.installer.ComponentCatalog;
+import org.graalvm.component.installer.FailedOperationException;
 import org.graalvm.component.installer.Feedback;
 import org.graalvm.component.installer.IncompatibleException;
+import org.graalvm.component.installer.InstallerStopException;
 import org.graalvm.component.installer.SoftwareChannel;
+import org.graalvm.component.installer.SoftwareChannelSource;
 import org.graalvm.component.installer.model.ComponentInfo;
 import org.graalvm.component.installer.model.ComponentRegistry;
 import org.graalvm.component.installer.persist.AbstractCatalogStorage;
+import org.graalvm.component.installer.persist.MetadataLoader;
 
 /**
  * The Storage merges storages of individual providers.
  * 
  * @author sdedic
  */
-class MergeStorage extends AbstractCatalogStorage {
+public class MergeStorage extends AbstractCatalogStorage implements ComponentCatalog.DownloadInterceptor {
     private final Map<ComponentInfo, SoftwareChannel> channelMap = new HashMap<>();
     private final List<SoftwareChannel> channels = new ArrayList<>();
+    private final Map<SoftwareChannel, SoftwareChannelSource> channelInfos = new HashMap<>();
 
-    MergeStorage(ComponentRegistry localRegistry, Feedback feedback) {
+    private boolean ignoreCatalogErrors;
+    private boolean acceptAllSources = true;
+
+    public MergeStorage(ComponentRegistry localRegistry, Feedback feedback) {
         super(localRegistry, feedback, null);
     }
 
-    public void addChannel(SoftwareChannel delegate) {
-        channels.add(delegate);
+    public boolean isAcceptAllSources() {
+        return acceptAllSources;
     }
+
+    public void setAcceptAllSources(boolean acceptAllSources) {
+        this.acceptAllSources = acceptAllSources;
+    }
+
+    public void addChannel(SoftwareChannelSource info, SoftwareChannel delegate) {
+        channels.add(delegate);
+        channelInfos.put(delegate, info);
+    }
+
+    public boolean isIgnoreCatalogErrors() {
+        return ignoreCatalogErrors;
+    }
+
+    public void setIgnoreCatalogErrors(boolean ignoreCatalogErrors) {
+        this.ignoreCatalogErrors = ignoreCatalogErrors;
+    }
+
+    private void reportError(Exception exc, SoftwareChannel errChannel) {
+        if (exc == null) {
+            return;
+        }
+        // the previous error is overwritten, so at least report it before it is
+        // forgot:
+        SoftwareChannelSource info = channelInfos.get(errChannel);
+        String l = info.getLabel();
+        if (l == null) {
+            l = info.getLocationURL();
+        }
+        feedback.error("REMOTE_CannotLoadChannel", exc, l, exc.getLocalizedMessage());
+    }
+
+    private boolean idsLoaded;
 
     @Override
     public Set<String> listComponentIDs() throws IOException {
         Set<String> ids = new HashSet<>();
-        IncompatibleException incEx = null;
+        List<Exception> savedEx = new ArrayList<>();
+        List<SoftwareChannel> errChannels = new ArrayList<>();
         boolean oneSucceeded = false;
-        for (SoftwareChannel del : channels) {
+        Exception toThrow = null;
+        for (SoftwareChannel del : new ArrayList<>(channels)) {
             try {
                 ids.addAll(del.getStorage().listComponentIDs());
                 oneSucceeded = true;
             } catch (IncompatibleException ex) {
-                incEx = ex;
+                savedEx.add(ex);
+                errChannels.add(del);
+                channels.remove(del);
+            } catch (IOException | FailedOperationException ex) {
+                if (!isIgnoreCatalogErrors()) {
+                    throw ex;
+                }
+                if (!idsLoaded) {
+                    reportError(ex, del);
+                }
+                toThrow = ex;
+                channels.remove(del);
             }
         }
-        if (!oneSucceeded && incEx != null) {
-            throw incEx;
+        if (!oneSucceeded || ids.isEmpty()) {
+            for (int i = 0; i < savedEx.size(); i++) {
+                reportError(toThrow = savedEx.get(i), errChannels.get(i));
+            }
+            if (toThrow instanceof IOException) {
+                throw (IOException) toThrow;
+            } else if (toThrow != null) {
+                throw (InstallerStopException) toThrow;
+            }
         }
+        idsLoaded = true;
         return ids;
     }
 
@@ -78,19 +141,35 @@ class MergeStorage extends AbstractCatalogStorage {
         return channels;
     }
 
+    private int getChannelPriority(SoftwareChannel ch) {
+        SoftwareChannelSource src = this.channelInfos.get(ch);
+        if (src != null) {
+            return src.getPriority();
+        } else {
+            int index = channels.indexOf(ch);
+            return index == -1 ? 0 : index;
+        }
+    }
+
     @Override
     public Set<ComponentInfo> loadComponentMetadata(String id) throws IOException {
         Set<ComponentInfo> cis = new HashSet<>();
         for (SoftwareChannel swch : channels) {
             Set<ComponentInfo> newInfos = swch.getStorage().loadComponentMetadata(id);
-            if (newInfos == null) {
+            if (newInfos == null || newInfos.isEmpty()) {
                 continue;
             }
-            newInfos.removeAll(cis);
+            if (!acceptAllSources) {
+                newInfos.removeAll(cis);
+            }
             for (ComponentInfo ci : newInfos) {
+                ci.setPriority(getChannelPriority(swch));
                 channelMap.put(ci, swch);
             }
             cis.addAll(newInfos);
+            if (!acceptAllSources) {
+                break;
+            }
         }
         return cis;
     }
@@ -98,4 +177,21 @@ class MergeStorage extends AbstractCatalogStorage {
     public SoftwareChannel getOrigin(ComponentInfo ci) {
         return channelMap.get(ci);
     }
+
+    @Override
+    public FileDownloader processDownloader(ComponentInfo info, FileDownloader dn) {
+        SoftwareChannel orig = getOrigin(info);
+        return orig != null ? orig.configureDownloader(info, dn) : dn;
+    }
+
+    @Override
+    public MetadataLoader interceptMetadataLoader(ComponentInfo info, MetadataLoader delegate) {
+        SoftwareChannel orig = getOrigin(info);
+        if (orig instanceof ComponentCatalog.DownloadInterceptor) {
+            return ((ComponentCatalog.DownloadInterceptor) orig).interceptMetadataLoader(info, delegate);
+        } else {
+            return delegate;
+        }
+    }
+
 }
