daf969b272270f8caa95d58ec1b5584671235c3d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 15:50:26 +0200

    Use remembered sets on the image heap.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
@@ -726,7 +726,7 @@ public final class GCImpl implements GC {
 
     @SuppressWarnings("try")
     private void blackenDirtyImageHeapRoots() {
-        if (!HeapImpl.usesImageHeapCardMarking()) {
+        if (!HeapImpl.usesImageHeapRememberedSets()) {
             blackenImageHeapRoots();
             return;
         }
