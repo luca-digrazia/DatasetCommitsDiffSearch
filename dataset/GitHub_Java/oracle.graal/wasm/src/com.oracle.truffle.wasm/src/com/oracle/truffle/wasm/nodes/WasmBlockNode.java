eb696a4bbc501ceffce077419eaeda16a0c180b6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:23:05 +0200

    Add missing default cases.

--- dataset/GitHub_Java/oracle.graal/wasm/src/com.oracle.truffle.wasm/src/com/oracle/truffle/wasm/nodes/WasmBlockNode.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/com.oracle.truffle.wasm/src/com/oracle/truffle/wasm/nodes/WasmBlockNode.java
@@ -291,7 +291,7 @@ public final class WasmBlockNode extends WasmNode implements RepeatingNode {
     }
 
     @SuppressWarnings("hiding")
-    public void initialize(WasmNode[] nestedControlTable, Node[] callNodeTable, int byteLength, int byteConstantLength,
+    public final void initialize(WasmNode[] nestedControlTable, Node[] callNodeTable, int byteLength, int byteConstantLength,
                     int intConstantLength, int longConstantLength, int branchTableLength) {
         initialize(byteLength);
         this.nestedControlTable = nestedControlTable;
