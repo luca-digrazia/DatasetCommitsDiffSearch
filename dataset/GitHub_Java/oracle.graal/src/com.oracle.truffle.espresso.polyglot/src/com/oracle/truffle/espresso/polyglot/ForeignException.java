d3486de6cff16e8ec33fb907b5c46ebf8aa86091
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:37:51 +0200

    Add javadoc for ForeignException.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.polyglot/src/com/oracle/truffle/espresso/polyglot/ForeignException.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.polyglot/src/com/oracle/truffle/espresso/polyglot/ForeignException.java
@@ -53,7 +53,7 @@ package com.oracle.truffle.espresso.polyglot;
  * try {
  *     throw Interop.throwException(foreignException);
  * } catch (ForeignException e) {
- *     log(e.getClass() + ": " + e.getMessage());
+ *     System.err.println(e.getMessage());
  *     ...
  *     throw e;
  * }
