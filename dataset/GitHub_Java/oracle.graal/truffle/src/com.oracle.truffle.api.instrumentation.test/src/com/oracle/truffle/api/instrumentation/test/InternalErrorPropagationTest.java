5fbd228aad3e0a2b49025b45634f878b9e2fc3b7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:40:55 +0200

    Fix internal error propagation test.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation.test/src/com/oracle/truffle/api/instrumentation/test/InternalErrorPropagationTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation.test/src/com/oracle/truffle/api/instrumentation/test/InternalErrorPropagationTest.java
@@ -1,6 +1,44 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.oracle.truffle.api.instrumentation.test;
-
-import java.util.function.Consumer;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.PolyglotException;
@@ -22,21 +60,35 @@ public class InternalErrorPropagationTest extends AbstractPolyglotTest {
 
     static final String OTHER_LANGUAGE = "InstrumentationExceptionTest_OtherLanguage";
 
+    public static class TestProxyInstrument extends ProxyInstrument {
+
+        private final TestContextListener listener;
+
+        public TestProxyInstrument(TestContextListener listener) {
+            this.listener = listener;
+        }
+
+        public TestProxyInstrument() {
+            this.listener = null;
+        }
+
+        @Override
+        protected void onCreate(Env env) {
+            super.onCreate(env);
+            env.getInstrumenter().attachContextsListener(listener, true);
+        }
+    }
+
     @Test
     public void testInstrumentCreateException() {
-        TestListener listener = new TestListener();
-        ProxyInstrument instrument = new ProxyInstrument() {
-            @Override
-            protected void onCreate(Env env) {
-                super.onCreate(env);
-                env.getInstrumenter().attachContextsListener(listener, true);
-            }
-        };
+        TestContextListener listener = new TestContextListener();
+        ProxyInstrument instrument = new TestProxyInstrument(listener);
+        TestEventListener triggerFailure = new TestEventListener();
 
         setupEnv(Context.create(ProxyLanguage.ID), instrument);
 
         try {
-            listener.onContextCreated = (c) -> triggerParseFailure(languageEnv, c);
+            listener.onContextCreated = triggerFailure;
             setupEnv(Context.create(ProxyLanguage.ID), instrument);
             Assert.fail();
         } catch (PolyglotException e) {
@@ -45,7 +97,7 @@ public class InternalErrorPropagationTest extends AbstractPolyglotTest {
 
         try {
             listener.onContextCreated = null;
-            listener.onLanguageContextCreated = (c) -> triggerParseFailure(languageEnv, c);
+            listener.onLanguageContextCreated = triggerFailure;
             setupEnv(Context.create(ProxyLanguage.ID), instrument);
             Assert.fail();
         } catch (PolyglotException e) {
@@ -54,7 +106,7 @@ public class InternalErrorPropagationTest extends AbstractPolyglotTest {
 
         try {
             listener.onLanguageContextCreated = null;
-            listener.onLanguageContextInitialized = (c) -> triggerParseFailure(languageEnv, c);
+            listener.onLanguageContextInitialized = triggerFailure;
             setupEnv(Context.create(ProxyLanguage.ID), instrument);
             Assert.fail();
         } catch (PolyglotException e) {
@@ -64,7 +116,7 @@ public class InternalErrorPropagationTest extends AbstractPolyglotTest {
         listener.onLanguageContextInitialized = null;
         setupEnv(Context.create(ProxyLanguage.ID), instrument);
         try {
-            listener.onLanguageContextFinalized = (c) -> triggerParseFailure(languageEnv, c);
+            listener.onLanguageContextFinalized = triggerFailure;
             context.close();
             Assert.fail();
         } catch (PolyglotException e) {
@@ -76,7 +128,7 @@ public class InternalErrorPropagationTest extends AbstractPolyglotTest {
         listener.onLanguageContextFinalized = null;
         setupEnv(Context.create(ProxyLanguage.ID), instrument);
         try {
-            listener.onLanguageContextDisposed = (c) -> triggerParseFailure(languageEnv, c);
+            listener.onLanguageContextDisposed = triggerFailure;
             context.close();
             Assert.fail();
         } catch (PolyglotException e) {
@@ -88,7 +140,7 @@ public class InternalErrorPropagationTest extends AbstractPolyglotTest {
         listener.onLanguageContextDisposed = null;
         setupEnv(Context.create(ProxyLanguage.ID), instrument);
         try {
-            listener.onContextClosed = (c) -> triggerParseFailure(languageEnv, c);
+            listener.onContextClosed = triggerFailure;
             context.close();
             Assert.fail();
         } catch (PolyglotException e) {
@@ -117,14 +169,22 @@ public class InternalErrorPropagationTest extends AbstractPolyglotTest {
         }
     }
 
-    static class TestListener implements ContextsListener {
+    final class TestEventListener {
 
-        Consumer<TruffleContext> onLanguageContextInitialized;
-        Consumer<TruffleContext> onLanguageContextFinalized;
-        Consumer<TruffleContext> onLanguageContextDisposed;
-        Consumer<TruffleContext> onLanguageContextCreated;
-        Consumer<TruffleContext> onContextCreated;
-        Consumer<TruffleContext> onContextClosed;
+        void accept(TruffleContext c) {
+            triggerParseFailure(languageEnv, c);
+        }
+
+    }
+
+    static class TestContextListener implements ContextsListener {
+
+        TestEventListener onLanguageContextInitialized;
+        TestEventListener onLanguageContextFinalized;
+        TestEventListener onLanguageContextDisposed;
+        TestEventListener onLanguageContextCreated;
+        TestEventListener onContextCreated;
+        TestEventListener onContextClosed;
 
         public void onLanguageContextInitialized(TruffleContext c, LanguageInfo l) {
             if (onLanguageContextInitialized != null) {
