2ad9a712ad98f52128a90773871ec3a52beb567e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:03:15 +0200

    Fix longConstantOffset

--- dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/nodes/WasmBlockNode.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/nodes/WasmBlockNode.java
@@ -879,6 +879,7 @@ public final class WasmBlockNode extends WasmNode implements RepeatingNode {
                     intConstantOffset += intConstantDelta(offset);
                     byteConstantOffset += byteConstantDelta(offset);
                     offset += offsetDelta(offset, byteConstantOffset);
+                    boolean inPool = isLeb128InPool(offset);
                     // endregion
 
                     byte type = module().symbolTable().globalValueType(index);
@@ -1199,7 +1200,7 @@ public final class WasmBlockNode extends WasmNode implements RepeatingNode {
                 }
                 case I64_CONST: {
                     // region Load LEB128 Signed64 -> value
-                    long value = signedLongConstant(offset, longConstantOffset);
+                    long value = signedLongConstant(offset, intConstantOffset);
                     longConstantOffset += longConstantDelta(offset);
                     byteConstantOffset += byteConstantDelta(offset);
                     offset += offsetDelta(offset, byteConstantOffset);
