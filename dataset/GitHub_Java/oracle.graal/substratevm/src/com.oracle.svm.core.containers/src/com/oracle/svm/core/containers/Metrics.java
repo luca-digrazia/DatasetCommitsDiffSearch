e2b98f0e7e2bd6eafd4049dbcfe9b2d5d7273ad7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:26:11 +0200

    Fix JavaDoc in com.oracle.svm.core.containers.Metrics

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.containers/src/com/oracle/svm/core/containers/Metrics.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.containers/src/com/oracle/svm/core/containers/Metrics.java
@@ -30,8 +30,7 @@ import java.lang.reflect.Method;
 /**
  * Operating System Metrics class
  *
- * Implementation note:
- * Some of the APIs within this class return metrics for an
+ * implNote: Some of the APIs within this class return metrics for an
  * "Isolation Group" or "Container".  When the term "Isolation Group"
  * is used in the API description, this refers to either:
  *
@@ -72,7 +71,7 @@ public interface Metrics {
      * Returns the interface responsible for providing the
      * platform metrics.
      *
-     * Implementation note:
+     * implNote:
      * Metrics are currently only supported Linux.
      * The provider for Linux is cgroups (version 1 or 2).
      *
@@ -164,7 +163,7 @@ public interface Metrics {
      * Group used for prioritizing the scheduling of processes across
      * all Isolation Groups running on a host.
      *
-     * Implementation note:
+     * implNote:
      * Popular container orchestration systems have standardized shares
      * to be multiples of 1024, where 1024 is interpreted as 1 CPU share
      * of execution.  Users can distribute CPU resources to multiple
