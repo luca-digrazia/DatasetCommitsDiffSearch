5f1bb3bc596c71edc5368a607fe38890ad5df99b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:52:41 +0200

    [GR-25085] Profile arguments of IndirectCallNode as well
        * To avoid deoptimizing other compilations of the callee needlessly.
        * The callee will in most cases still see similar arguments and still
          benefit from profiled arguments.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedIndirectCallNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedIndirectCallNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,58 +27,38 @@ package org.graalvm.compiler.truffle.runtime;
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.nodes.IndirectCallNode;
-import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
-import com.oracle.truffle.api.nodes.NodeUtil;
 import com.oracle.truffle.api.profiles.ValueProfile;
 
 /**
- * A call node with a constant {@link CallTarget} that can be optimized by Graal.
+ * A call node with a dynamic {@link CallTarget} that can be optimized by Graal.
  */
 @NodeInfo
 public final class OptimizedIndirectCallNode extends IndirectCallNode {
 
     @CompilationFinal private ValueProfile exceptionProfile;
 
+    /*
+     * Should be instantiated with the runtime.
+     */
+    OptimizedIndirectCallNode() {
+    }
+
     @Override
     public Object call(CallTarget target, Object... arguments) {
         try {
-            return ((OptimizedCallTarget) target).callIndirect(this, arguments);
+            OptimizedCallTarget optimizedTarget = ((OptimizedCallTarget) target);
+            return optimizedTarget.callIndirect(this, arguments);
         } catch (Throwable t) {
             if (exceptionProfile == null) {
                 CompilerDirectives.transferToInterpreterAndInvalidate();
                 exceptionProfile = ValueProfile.createClassProfile();
             }
             Throwable profiledT = exceptionProfile.profile(t);
-            OptimizedCallTarget.runtime().getTvmci().onThrowable(this, null, profiledT, null);
+            GraalRuntimeAccessor.LANGUAGE.onThrowable(this, null, profiledT, null);
             throw OptimizedCallTarget.rethrow(profiledT);
         }
-    }
-
-    static IndirectCallNode createUncached() {
-        return new IndirectCallNode() {
-            @Override
-            public boolean isAdoptable() {
-                return false;
-            }
-
-            @Override
-            @TruffleBoundary
-            public Object call(CallTarget target, Object... arguments) {
-                /*
-                 * Clear encapsulating node for uncached indirect call boundary. The encapsulating
-                 * node is not longer needed if a call boundary is crossed.
-                 */
-                Node prev = NodeUtil.pushEncapsulatingNode(null);
-                try {
-                    return ((OptimizedCallTarget) target).callIndirect(prev, arguments);
-                } finally {
-                    NodeUtil.popEncapsulatingNode(prev);
-                }
-            }
-        };
     }
 
 }
