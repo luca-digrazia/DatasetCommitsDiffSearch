db19f7a1afe85ad872a643bf1b1bb1832aefa852
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:02:48 +0200

    Do not include class name in generated assertion message.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/library/ExportsGenerator.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/library/ExportsGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -883,7 +883,7 @@ public class ExportsGenerator extends CodeTypeElementFactory<ExportsData> {
                 acceptsBuilder.string(name);
             }
         } else {
-            if (libraryExports.isFinalReceiver() || (!cached && libraryExports.getLibrary().isDynamicDispatch())) {
+            if (libraryExports.isFinalReceiver()) {
                 acceptsBuilder.string(receiverName, " instanceof ").type(exportReceiverType);
             } else {
                 TypeMirror receiverType = libraryExports.getReceiverType();
