b05282c049e1fe8727a438c825edbc7eaf791faa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:34:02 +0200

    Fix mixed i32/i64 arithmetic in array range write barriers

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/gc/WriteBarrierSnippets.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/gc/WriteBarrierSnippets.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,6 +31,7 @@ import org.graalvm.compiler.nodes.memory.address.AddressNode.Address;
 import org.graalvm.compiler.replacements.nodes.AssertionNode;
 import org.graalvm.compiler.word.Word;
 import org.graalvm.word.LocationIdentity;
+import org.graalvm.word.WordFactory;
 
 public abstract class WriteBarrierSnippets {
     public static final LocationIdentity GC_CARD_LOCATION = NamedLocationIdentity.mutable("GC-Card");
@@ -38,20 +39,20 @@ public abstract class WriteBarrierSnippets {
     protected static void verifyNotArray(Object object) {
         if (object != null) {
             // Manually build the null check and cast because we're in snippet that's lowered late.
-            AssertionNode.assertion(false, !PiNode.piCastNonNull(object, SnippetAnchorNode.anchor()).getClass().isArray(), "imprecise card mark used with array");
+            AssertionNode.dynamicAssert(!PiNode.piCastNonNull(object, SnippetAnchorNode.anchor()).getClass().isArray(), "imprecise card mark used with array");
         }
     }
 
-    protected static long getPointerToFirstArrayElement(Address address, int length, int elementStride) {
+    protected static Word getPointerToFirstArrayElement(Address address, long length, int elementStride) {
         long result = Word.fromAddress(address).rawValue();
         if (elementStride < 0) {
             // the address points to the place after the last array element
             result = result + elementStride * length;
         }
-        return result;
+        return WordFactory.unsigned(result);
     }
 
-    protected static long getPointerToLastArrayElement(Address address, int length, int elementStride) {
+    protected static Word getPointerToLastArrayElement(Address address, long length, int elementStride) {
         long result = Word.fromAddress(address).rawValue();
         if (elementStride < 0) {
             // the address points to the place after the last array element
@@ -59,6 +60,6 @@ public abstract class WriteBarrierSnippets {
         } else {
             result = result + (length - 1) * elementStride;
         }
-        return result;
+        return WordFactory.unsigned(result);
     }
 }
