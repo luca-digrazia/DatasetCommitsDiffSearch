0488e3e2be439de6e1e29f96e4f7e70f4af7b9e4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 00:17:50 +0200

    Add documentation, style fixes

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/threads/SynchronizedParserInlineTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/threads/SynchronizedParserInlineTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,19 +33,63 @@ public final class SynchronizedParserInlineTest extends JTTTest {
 
     private static SynchronizedParserInlineTest object = new SynchronizedParserInlineTest();
 
-    public Integer test(boolean b) {
+    public static Integer test(boolean b) {
         foo(object);
         return b ? 42 : 1337;
     }
 
     @BytecodeParserForceInline
-    public synchronized void foo(SynchronizedParserInlineTest o) {
+    public static synchronized void foo(SynchronizedParserInlineTest o) {
         o.notifyAll();
     }
 
     @Test
     public void run0() {
         runTest("test", false);
+    }
+
+    public static Integer test1(int b) {
+        return foo1(b);
+    }
+
+    @BytecodeParserForceInline
+    public static synchronized int foo1(int b) {
+        if (b < 0) {
+            return 7777;
+        } else if (b > 100) {
+            throw new RuntimeException();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void run1() {
+        runTest("test1", -1);
+        runTest("test1", 1);
+        runTest("test1", 101);
+    }
+
+    public static Integer test2(int b) {
+        return foo2(b);
+    }
+
+    @BytecodeParserForceInline
+    public static int foo2(int b) {
+        if (b < 0) {
+            return 7777;
+        } else if (b > 100) {
+            throw new RuntimeException();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void run2() {
+        runTest("test2", -1);
+        runTest("test2", 1);
+        runTest("test2", 101);
     }
 
 }
