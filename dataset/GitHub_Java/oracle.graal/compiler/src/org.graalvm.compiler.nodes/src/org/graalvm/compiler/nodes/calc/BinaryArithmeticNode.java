7cd9269cf6b1bb4d7e8bdabd632d07ae74128777
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 14:46:03 +0200

    reassociation: exclude math exact nodes

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/BinaryArithmeticNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/BinaryArithmeticNode.java
@@ -350,7 +350,7 @@ public abstract class BinaryArithmeticNode<OP> extends BinaryNode implements Ari
             } else {
                 return BinaryArithmeticNode.add(associated, matchValue, view);
             }
-        } else if (isNonExactMul(node)) {
+        } else if (node instanceof MulNode) {
             // Re-association from "x * (y * C)" to "(x * y) * C"
             return BinaryArithmeticNode.mul(matchValue, BinaryArithmeticNode.mul(otherValue1, otherValue2, view), view);
         } else if (node instanceof AndNode) {
@@ -399,7 +399,7 @@ public abstract class BinaryArithmeticNode<OP> extends BinaryNode implements Ari
         if (match1 == null) {
             return node;
         }
-        if (isExactMathOperation(node)) {
+        if (isExactAddOrSub(node)) {
             return node;
         }
         ValueNode otherValue = match1.getOtherValue(node);
@@ -419,7 +419,7 @@ public abstract class BinaryArithmeticNode<OP> extends BinaryNode implements Ari
         if (match2 == null) {
             return node;
         }
-        if (isExactMathOperation(other)) {
+        if (isExactAddOrSub(other)) {
             return node;
         }
         boolean invertA = false;
@@ -458,7 +458,7 @@ public abstract class BinaryArithmeticNode<OP> extends BinaryNode implements Ari
                 return BinaryArithmeticNode.sub(a, associated, view);
             }
             return BinaryArithmeticNode.add(a, associated, view);
-        } else if (isNonExactMul(node)) {
+        } else if (node instanceof MulNode) {
             return BinaryArithmeticNode.mul(a, AddNode.mul(m1, m2, view), view);
         } else if (node instanceof AndNode) {
             return AndNode.create(a, AndNode.create(m1, m2, view), view);
@@ -469,13 +469,6 @@ public abstract class BinaryArithmeticNode<OP> extends BinaryNode implements Ari
         } else {
             throw GraalError.shouldNotReachHere();
         }
-    }
-
-    private static boolean isNonExactMul(Node n) {
-        if (n instanceof MulNode) {
-            return !((MulNode) n).isExact();
-        }
-        return false;
     }
 
     private static boolean isNonExactAdd(Node n) {
@@ -496,15 +489,12 @@ public abstract class BinaryArithmeticNode<OP> extends BinaryNode implements Ari
         return isNonExactAdd(n) || isNonExactSub(n);
     }
 
-    private static boolean isExactMathOperation(Node n) {
+    private static boolean isExactAddOrSub(Node n) {
         if (n instanceof AddNode) {
             return ((AddNode) n).isExact();
         }
         if (n instanceof SubNode) {
             return ((SubNode) n).isExact();
-        }
-        if (n instanceof MulNode) {
-            return ((MulNode) n).isExact();
         }
         return false;
     }
