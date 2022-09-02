74608c38fed94a56406767d96d4939255dd5c066
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:24:48 +0200

    [GR-14050] Optimizations on division by constants should update the stamps accordingly.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/SwitchCanonicalizerTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/SwitchCanonicalizerTest.java
@@ -27,7 +27,6 @@ package org.graalvm.compiler.core.test;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.extended.IntegerSwitchNode;
 import org.graalvm.compiler.phases.common.CanonicalizerPhase;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class SwitchCanonicalizerTest extends GraalCompilerTest {
@@ -70,7 +69,6 @@ public class SwitchCanonicalizerTest extends GraalCompilerTest {
     }
 
     @Test
-    @Ignore("rem by power of 2 does not preserve the stamp information.")
     public void testRemByPowerOf2() {
         shouldFoldSwitch("remByPowerOf2");
     }
