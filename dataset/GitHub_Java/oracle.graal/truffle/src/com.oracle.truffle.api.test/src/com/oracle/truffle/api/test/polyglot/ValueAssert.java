d6f428a472b496ab5e7963e67a9566e14bcadcf0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:56:08 +0200

    Improve check in ValueAssert for static classes

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/ValueAssert.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/ValueAssert.java
@@ -180,7 +180,6 @@ public class ValueAssert {
                                 assertClassMembers(value, (Class<?>) hostObject, true);
                             } else {
                                 assertClassMembers(value, Class.class, false);
-                                assertTrue(value.hasMember("static"));
                             }
                         } else {
                             assertClassMembers(value, hostObject.getClass(), false);
