73d55edc46d395601638a1e4c42b1955b3cdaee0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:44:51 +0200

    Introduce hash table switch strategy

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/hashing/Hasher.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/hashing/Hasher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -94,9 +94,10 @@ public final class Hasher {
         Set<Integer> seen = new HashSet<>(keys.length);
         for (JavaConstant key : keys) {
             int hash = function.apply(key.asLong(), min) & (cardinality - 1);
-            if (!seen.add(hash)) {
+            if (seen.contains(hash)) {
                 return false;
             }
+            seen.add(hash);
         }
         return true;
     }
