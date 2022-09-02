de48ede601469b30877bfcdd3199dbda2ff8ca4e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 00:37:38 +0200

    Replace System.getProperty

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.printer/src/org/graalvm/compiler/printer/CFGPrinterObserver.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.printer/src/org/graalvm/compiler/printer/CFGPrinterObserver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -209,19 +209,14 @@ public class CFGPrinterObserver implements DebugDumpHandler {
             } else if (object instanceof ScheduleResult) {
                 cfgPrinter.printSchedule(message, (ScheduleResult) object);
             } else if (object instanceof StructuredGraph) {
-                StructuredGraph graph = (StructuredGraph) object;
                 if (cfgPrinter.cfg == null) {
-                    ScheduleResult scheduleResult = GraalDebugHandlersFactory.tryGetSchedule(debug, graph);
-                    if (scheduleResult != null) {
-                        cfgPrinter.cfg = scheduleResult.getCFG();
-                    }
-                }
-                if (cfgPrinter.cfg != null) {
-                    if (graph.nodeIdCount() > cfgPrinter.cfg.getNodeToBlock().capacity()) {
-                        cfgPrinter.cfg = ControlFlowGraph.compute(graph, true, true, true, false);
-                    }
+                    StructuredGraph graph = (StructuredGraph) object;
+                    cfgPrinter.cfg = ControlFlowGraph.compute(graph, true, true, true, false);
+                    cfgPrinter.printCFG(message, cfgPrinter.cfg.getBlocks(), true);
+                } else {
                     cfgPrinter.printCFG(message, cfgPrinter.cfg.getBlocks(), true);
                 }
+
             } else if (object instanceof CompilationResult) {
                 final CompilationResult compResult = (CompilationResult) object;
                 cfgPrinter.printMachineCode(disassemble(codeCache, compResult, null), message);
