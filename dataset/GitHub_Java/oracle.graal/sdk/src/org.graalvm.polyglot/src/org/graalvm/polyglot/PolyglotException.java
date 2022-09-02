ac4c83202802f0810b989b4a143e1d711ec54278
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:34:40 +0200

    Fix exception javadoc.

--- dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/PolyglotException.java
+++ dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/PolyglotException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -310,17 +310,6 @@ public final class PolyglotException extends RuntimeException {
      */
     public boolean isCancelled() {
         return impl.isCancelled();
-    }
-
-    /**
-     * Returns <code>true</code> if the current application thread was interrupted by an
-     * {@link InterruptedException}. {@link InterruptedException} from the host return
-     * <code>true</code> for interrupted.
-     *
-     * @since 20.3
-     */
-    public boolean isInterrupted() {
-        return impl.isInterrupted();
     }
 
     /**
