5b7e878a5b3914caae1cc42506b00b1f628794a9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:24:55 +0200

    Fix missing boundaries.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/HostObject.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/HostObject.java
@@ -1200,7 +1200,7 @@ final class HostObject implements TruffleObject {
     PolyglotEngineImpl getEngine() {
         PolyglotContextImpl context = languageContext != null ? languageContext.context : null;
         if (context == null) {
-            context = PolyglotContextImpl.currentNotEntered();
+            context = PolyglotContextImpl.requireContext();
         }
         return context.engine;
     }
