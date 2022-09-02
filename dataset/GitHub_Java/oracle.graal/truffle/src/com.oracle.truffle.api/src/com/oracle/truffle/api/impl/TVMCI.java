ff30ec28e2498b258dd7fa9a5a785263746aa6a4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:26:40 +0200

    getOrCreateRuntimeData() can be static

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/TVMCI.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/TVMCI.java
@@ -283,17 +283,12 @@ public abstract class TVMCI {
      */
     @SuppressWarnings("unchecked")
     protected static <T> T getOrCreateRuntimeData(RootNode rootNode, Function<OptionValues, T> constructor) {
+        Objects.requireNonNull(rootNode);
         Objects.requireNonNull(constructor);
         final Accessor.NodeSupport nodesAccess = TVMCIAccessor.nodesAccess();
         final EngineSupport engineAccess = TVMCIAccessor.engineAccess();
 
-        final Object sourceVM;
-        if (rootNode == null) {
-            sourceVM = engineAccess.getCurrentVM();
-        } else {
-            sourceVM = nodesAccess.getSourceVM(rootNode);
-        }
-
+        final Object sourceVM = nodesAccess.getSourceVM(rootNode);
         if (sourceVM != null) {
             return engineAccess.getOrCreateRuntimeData(sourceVM, constructor);
         } else {
