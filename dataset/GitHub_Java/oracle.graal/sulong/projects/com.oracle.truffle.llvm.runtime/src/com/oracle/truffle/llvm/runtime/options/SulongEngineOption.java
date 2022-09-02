78464a6f706644ad14ddb9f8624d087ef462e270
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:53:29 +0200

    sulong: make loadC++Libraries off by default

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/options/SulongEngineOption.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/options/SulongEngineOption.java
@@ -126,14 +126,8 @@ public final class SulongEngineOption {
 
     @Option(name = "llvm.enableLVI",
             category = OptionCategory.EXPERT,
-            help = "This option is deprecated, local variable inspection is always enabled.",
-            deprecated = true)
+            help = "Enable source-level inspection of local variables.")
     public static final OptionKey<Boolean> ENABLE_LVI = new OptionKey<>(false);
-
-    @Option(name = "llvm.OSR",
-            category = OptionCategory.EXPERT,
-            help = "Enable on-stack-replacement of loops.")
-    public static final OptionKey<Boolean> ENABLE_OSR = new OptionKey<>(true);
 
     public static final String LAZY_PARSING_NAME = "llvm.lazyParsing";
     @Option(name = LAZY_PARSING_NAME,
