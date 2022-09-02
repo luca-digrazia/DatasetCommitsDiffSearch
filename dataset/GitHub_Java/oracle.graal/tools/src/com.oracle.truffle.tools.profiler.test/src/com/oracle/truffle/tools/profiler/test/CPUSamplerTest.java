5d07700df2e4d89c2c6299d55553e33e11204b31
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:33:36 +0200

    Initial fix of ManualSamplingTest and removal of statement tests from CPUSamplerTest.

--- dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.profiler.test/src/com/oracle/truffle/tools/profiler/test/CPUSamplerTest.java
+++ dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.profiler.test/src/com/oracle/truffle/tools/profiler/test/CPUSamplerTest.java
@@ -65,6 +65,7 @@ public class CPUSamplerTest extends AbstractProfilerTest {
         Assert.assertNotNull(sampler);
         synchronized (sampler) {
             sampler.setGatherSelfHitTimes(true);
+            sampler.setDelaySamplingUntilNonInternalLangInit(false);
         }
     }
 
