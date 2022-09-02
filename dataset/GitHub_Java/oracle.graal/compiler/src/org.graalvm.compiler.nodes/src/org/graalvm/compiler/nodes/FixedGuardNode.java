01e7ac5610dc69407a7204ab9075f47fff193929
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:54:45 +0200

    FixedGuardNode.isDefaultSuccessor() now bypasses the side-effect in getDefault()

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/FixedGuardNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/FixedGuardNode.java
@@ -141,7 +141,7 @@ public final class FixedGuardNode extends AbstractFixedGuardNode implements Lowe
 
     @Override
     public boolean isDefaultSuccessor(AbstractBeginNode beginNode) {
-        return beginNode.next() == next();
+        return beginNode == next();
     }
 
     @Override
