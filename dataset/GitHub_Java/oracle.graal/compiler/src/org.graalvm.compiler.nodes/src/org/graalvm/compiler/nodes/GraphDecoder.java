0b91291e571d9850fedcdd2ff484dcf2cb63fe0b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:50:12 +0200

    graph decoder: merge outer loops if we have nested loops and we use UNROLL_UNTIL_RETURN

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/GraphDecoder.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/GraphDecoder.java
@@ -450,7 +450,7 @@ public class GraphDecoder {
         return loopScope;
     }
 
-    public static final boolean PRINT_DEBUG_STDOUT = false;
+    public static final boolean PRINT_DEBUG_STDOUT = true;
 
     protected final void decode(LoopScope initialLoopScope) {
         LoopScope loopScope = initialLoopScope;
@@ -474,6 +474,9 @@ public class GraphDecoder {
 
                 /* Finished with a loop. */
                 if (loopScope.hasIterationsToProccess()) {
+                    /* Loop explosion: process the loop iteration. */
+                    // assert loopScope.getNextIterationToProcess(false).loopIteration ==
+                    // loopScope.loopIteration + 1;
                     loopScope = loopScope.getNextIterationToProcess(true);
                 } else {
                     propagateCreatedNodes(loopScope);
@@ -513,7 +516,7 @@ public class GraphDecoder {
         }
     }
 
-    public static final boolean DUMP_DURING_FIXED_NODE_PROCESSING = false;
+    public static final boolean DUMP_DURING_FIXED_NODE_PROCESSING = true;
 
     protected LoopScope processNextNode(MethodScope methodScope, LoopScope loopScope) {
         int nodeOrderId = loopScope.nodesToProcess.nextSetBit(0);
@@ -659,7 +662,7 @@ public class GraphDecoder {
             } else if (methodScope.loopExplosion.useExplosion() && node instanceof LoopEndNode) {
                 int loopEndDuplicationsBefore = loopScope.numberOfNextIterationsFromLoopEndDuplication();
                 node = handleLoopExplosionEnd(methodScope, loopScope, (LoopEndNode) node);
-                Deque<LoopScope> phiScope = loopScope.numberOfNextIterationsFromLoopEndDuplication() > loopEndDuplicationsBefore ? //
+                Deque<LoopScope> phiScope = loopScope.numberOfNextIterationsFromLoopEndDuplication() > loopEndDuplicationsBefore ?//
                                 loopScope.nextIterationFromLoopEndDuplication
                                 : loopScope.nextIterationsFromUnrolling;
                 phiNodeScope = phiScope.getLast();
