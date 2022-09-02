21aaa237f6d8671c5a34f2c6e5321e6b5c8e06f4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:54:35 +0200

    Perform runtime serialization checksumming when building with -esa

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.util/src/com/oracle/svm/util/SerializationChecksumCalculator.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.util/src/com/oracle/svm/util/SerializationChecksumCalculator.java
@@ -111,10 +111,10 @@ public class SerializationChecksumCalculator {
             // Checkstyle: resume
             warningPrinted = true;
         }
+    }
 
-        public static boolean printUsageWarning() {
-            return warningPrinted;
-        }
+    public static boolean printUsageWarning() {
+        return Holder.warningPrinted;
     }
 
     /**
@@ -127,7 +127,6 @@ public class SerializationChecksumCalculator {
             if (isClassAbstract(serializationClass)) {
                 return "0";
             }
-            Holder.printUsageWarning();
             MessageDigest md = createMessageDigest();
             if (targetConstructorClassName != null && targetConstructorClassName.length() > 0) {
                 String currentClassName = serializationClassName;
