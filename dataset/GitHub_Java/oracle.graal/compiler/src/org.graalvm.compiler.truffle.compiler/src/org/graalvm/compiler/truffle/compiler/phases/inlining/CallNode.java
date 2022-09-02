49665427736b87a6e94e3e3afb6a98630d498c59
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 14:41:53 +0200

    Formatting.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.compiler/src/org/graalvm/compiler/truffle/compiler/phases/inlining/CallNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.compiler/src/org/graalvm/compiler/truffle/compiler/phases/inlining/CallNode.java
@@ -402,15 +402,6 @@ public final class CallNode extends Node implements Comparable<CallNode> {
         }
     }
 
-    public void collectInlinedTargets(TruffleMetaAccessProvider inliningPlan) {
-        if (state == State.Inlined) {
-            inliningPlan.addInlinedTarget(truffleAST);
-            for (CallNode child : children) {
-                child.collectInlinedTargets(inliningPlan);
-            }
-        }
-    }
-
     public enum State {
         Cutoff,
         Expanded,
