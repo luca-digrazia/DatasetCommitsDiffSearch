f0edb34dd2593d8e8542e54779ee0b987fd2c49c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 14:40:01 +0200

    Avoid StackOverflowError during reporting of unhandled exception

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/graal/snippets/CEntryPointSnippets.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/graal/snippets/CEntryPointSnippets.java
@@ -66,9 +66,7 @@ import org.graalvm.word.PointerBase;
 import org.graalvm.word.WordFactory;
 
 import com.oracle.svm.core.Isolates;
-import com.oracle.svm.core.JavaMainWrapper.JavaMainSupport;
-import com.oracle.svm.core.RuntimeAssertionsSupport;
-import com.oracle.svm.core.SubstrateUtil;
+import com.oracle.svm.core.SubstrateOptions;
 import com.oracle.svm.core.annotate.RestrictHeapAccess;
 import com.oracle.svm.core.annotate.Uninterruptible;
 import com.oracle.svm.core.c.CGlobalData;
@@ -86,7 +84,6 @@ import com.oracle.svm.core.graal.nodes.CEntryPointUtilityNode;
 import com.oracle.svm.core.jdk.PlatformNativeLibrarySupport;
 import com.oracle.svm.core.jdk.RuntimeSupport;
 import com.oracle.svm.core.log.Log;
-import com.oracle.svm.core.option.RuntimeOptionParser;
 import com.oracle.svm.core.snippets.SnippetRuntime;
 import com.oracle.svm.core.snippets.SnippetRuntime.SubstrateForeignCallDescriptor;
 import com.oracle.svm.core.snippets.SubstrateForeignCallTarget;
@@ -97,7 +94,7 @@ import com.oracle.svm.core.thread.VMOperationControl;
 import com.oracle.svm.core.thread.VMThreads;
 import com.oracle.svm.core.util.VMError;
 
-// Checkstyle: stop
+//Checkstyle: stop
 import sun.misc.Unsafe;
 // Checkstyle: resume
 
@@ -145,7 +142,7 @@ public final class CEntryPointSnippets extends SubstrateTemplates implements Sni
     public static native int runtimeCall(@ConstantNodeParameter ForeignCallDescriptor descriptor, Throwable exception);
 
     @NodeIntrinsic(value = ForeignCallNode.class)
-    public static native int runtimeCallInitializeIsolate(@ConstantNodeParameter ForeignCallDescriptor descriptor, CEntryPointCreateIsolateParameters parameters);
+    public static native int runtimeCallInitializeIsolate(@ConstantNodeParameter ForeignCallDescriptor descriptor);
 
     @NodeIntrinsic(value = ForeignCallNode.class)
     public static native int runtimeCallTearDownIsolate(@ConstantNodeParameter ForeignCallDescriptor descriptor);
@@ -184,7 +181,7 @@ public final class CEntryPointSnippets extends SubstrateTemplates implements Sni
             Safepoint.transitionNativeToJava();
         }
 
-        result = runtimeCallInitializeIsolate(INITIALIZE_ISOLATE, parameters);
+        result = runtimeCallInitializeIsolate(INITIALIZE_ISOLATE);
         return result;
     }
 
@@ -242,7 +239,7 @@ public final class CEntryPointSnippets extends SubstrateTemplates implements Sni
     }
 
     @SubstrateForeignCallTarget(stubCallingConvention = false)
-    private static int initializeIsolate(CEntryPointCreateIsolateParameters parameters) {
+    private static int initializeIsolate() {
         boolean firstIsolate = false;
 
         final long initStateAddr = FIRST_ISOLATE_INIT_STATE.get().rawValue();
@@ -262,18 +259,6 @@ public final class CEntryPointSnippets extends SubstrateTemplates implements Sni
             }
         }
 
-        if (UseDedicatedVMOperationThread.getValue()) {
-            VMOperationControl.startVMOperationThread();
-        }
-
-        if (parameters.isNonNull() && parameters.version() >= 3 && parameters.getArgv().isNonNull()) {
-            String[] args = SubstrateUtil.getArgs(parameters.getArgc(), parameters.getArgv());
-            args = RuntimeOptionParser.parseAndConsumeAllOptions(args);
-            if (ImageSingletons.contains(JavaMainSupport.class)) {
-                ImageSingletons.lookup(JavaMainSupport.class).mainArgs = args;
-            }
-        }
-
         boolean success = PlatformNativeLibrarySupport.singleton().initializeBuiltinLibraries();
         if (firstIsolate) { // let other isolates (if any) initialize now
             state = success ? FirstIsolateInitStates.SUCCESSFUL : FirstIsolateInitStates.FAILED;
@@ -286,6 +271,9 @@ public final class CEntryPointSnippets extends SubstrateTemplates implements Sni
         assert !isolateInitialized;
         isolateInitialized = true;
 
+        if (UseDedicatedVMOperationThread.getValue()) {
+            VMOperationControl.startVMOperationThread();
+        }
         RuntimeSupport.executeInitializationHooks();
         return CEntryPointErrors.NO_ERROR;
     }
@@ -484,7 +472,7 @@ public final class CEntryPointSnippets extends SubstrateTemplates implements Sni
 
     @Fold
     static boolean runtimeAssertionsEnabled() {
-        return RuntimeAssertionsSupport.singleton().desiredAssertionStatus(CEntryPointSnippets.class);
+        return SubstrateOptions.getRuntimeAssertionsForClass(CEntryPointSnippets.class.getName());
     }
 
     /**
