d60b719011e1fdd969115513e35ce7ee6a69214a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:54:52 +0200

    Allow empty symbols.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/descriptors/ByteSequence.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/descriptors/ByteSequence.java
@@ -137,8 +137,4 @@ public abstract class ByteSequence {
             throw EspressoError.shouldNotReachHere(e);
         }
     }
-
-    public boolean isValid() {
-        return Utf8.isValid(value, offset(), length());
-    }
 }
