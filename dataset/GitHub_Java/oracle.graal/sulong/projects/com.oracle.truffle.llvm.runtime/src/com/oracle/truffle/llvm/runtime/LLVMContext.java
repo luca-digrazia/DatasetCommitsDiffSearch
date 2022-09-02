c782e162a695895a84b6458a10e6ebcaebc0b062
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:06:39 +0200

    Sulong: Removed creating the global and local scope in the parser, and also removed misc functions for ensuring the new scopes are correct. Created the __sulong_init_context and __sulong_dispose_context symbols for the context.

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/LLVMContext.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/LLVMContext.java
@@ -206,6 +206,9 @@ public final class LLVMContext {
         pThreadContext = new LLVMPThreadContext(this);
 
         symbolStorage = new AssumedValue[10][];
+
+        sulongInitContext = null;
+        sulongDisposeContext = null;
     }
 
     boolean patchContext(Env newEnv) {
