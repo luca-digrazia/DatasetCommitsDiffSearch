4d3455721b062fd4c4dc343d0cbd5414d58a86ca
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 13:00:17 +0200

    Reduce number of benchmarks for Truffle.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.benchmark/src/com/oracle/truffle/api/benchmark/DSLInterpreterBenchmark.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.benchmark/src/com/oracle/truffle/api/benchmark/DSLInterpreterBenchmark.java
@@ -1,48 +1,65 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * (a) the Software, and
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.oracle.truffle.api.benchmark;
+
+import java.util.function.Supplier;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.TearDown;
 
-import com.oracle.truffle.api.benchmark.DSLInterpreterBenchmarkFactory.DSLNodeGen;
+import com.oracle.truffle.api.benchmark.DSLInterpreterBenchmarkFactory.CachedDSLNodeGen;
+import com.oracle.truffle.api.benchmark.DSLInterpreterBenchmarkFactory.SimpleDSLNodeGen;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.dsl.UnsupportedSpecializationException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 
-@Warmup(iterations = 5)
-@Measurement(iterations = 5)
 @State(Scope.Thread)
 @Fork(value = 1)
 public class DSLInterpreterBenchmark extends TruffleBenchmark {
@@ -50,8 +67,8 @@ public class DSLInterpreterBenchmark extends TruffleBenchmark {
     private static final int NODES = 10000;
     private static TestRootNode root = new TestRootNode();
 
-    private static DSLNode createNode() {
-        DSLNode node = DSLNodeGen.create();
+    private static <T extends AbstractNode> T createNode(Supplier<T> nodeFactory) {
+        T node = nodeFactory.get();
 
         // adopt in a root node to initialize the parent pointer
         root.child = node;
@@ -60,15 +77,39 @@ public class DSLInterpreterBenchmark extends TruffleBenchmark {
     }
 
     @State(Scope.Thread)
-    public static class SpecializeState {
+    public static class SimpleFirstIterationState {
 
-        final DSLNode[] nodes = new DSLNode[NODES];
+        final SimpleDSLNode[] nodes = new SimpleDSLNode[NODES];
 
         @Setup(Level.Invocation)
         public void setup() {
             for (int i = 0; i < NODES; i++) {
-                nodes[i] = createNode();
+                nodes[i] = createNode(SimpleDSLNodeGen::create);
             }
+        }
+
+        @TearDown(Level.Invocation)
+        public void tearDown() {
+            System.gc();
+        }
+
+    }
+
+    @State(Scope.Thread)
+    public static class CachedFirstIterationState {
+
+        final CachedDSLNode[] nodes = new CachedDSLNode[NODES];
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            for (int i = 0; i < NODES; i++) {
+                nodes[i] = createNode(CachedDSLNodeGen::create);
+            }
+        }
+
+        @TearDown(Level.Invocation)
+        public void tearDown() {
+            System.gc();
         }
 
     }
@@ -78,9 +119,9 @@ public class DSLInterpreterBenchmark extends TruffleBenchmark {
      * warmed up interpreter all specialization paths are taken.
      */
     @Setup
-    public static void setupInterpreterProfile() {
+    public void setupInterpreterProfile() {
         for (int i = 0; i < 100; i++) {
-            DSLNode node = createNode();
+            AbstractNode node = createNode(SimpleDSLNodeGen::create);
             node.execute(42L);
             node.execute(42);
             try {
@@ -88,7 +129,15 @@ public class DSLInterpreterBenchmark extends TruffleBenchmark {
             } catch (UnsupportedSpecializationException e) {
             }
 
-            node = createNode();
+            node = createNode(SimpleDSLNodeGen::create);
+            node.execute(42);
+            node.execute(42L);
+            try {
+                node.execute("");
+            } catch (UnsupportedSpecializationException e) {
+            }
+
+            node = createNode(CachedDSLNodeGen::create);
             node.execute(42);
             node.execute(42L);
             try {
@@ -99,14 +148,29 @@ public class DSLInterpreterBenchmark extends TruffleBenchmark {
     }
 
     @State(Scope.Thread)
-    public static class SecondIterationState {
+    public static class SimpleSecondIterationState {
 
-        final DSLNode[] nodes = new DSLNode[NODES];
+        final SimpleDSLNode[] nodes = new SimpleDSLNode[NODES];
 
         @Setup(Level.Invocation)
         public void setup() {
             for (int i = 0; i < NODES; i++) {
-                nodes[i] = createNode();
+                nodes[i] = createNode(SimpleDSLNodeGen::create);
+                nodes[i].execute(42);
+            }
+        }
+
+    }
+
+    @State(Scope.Thread)
+    public static class CachedSecondIterationState {
+
+        final CachedDSLNode[] nodes = new CachedDSLNode[NODES];
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            for (int i = 0; i < NODES; i++) {
+                nodes[i] = createNode(CachedDSLNodeGen::create);
                 nodes[i].execute(42);
             }
         }
@@ -126,9 +190,12 @@ public class DSLInterpreterBenchmark extends TruffleBenchmark {
         }
     }
 
-    abstract static class DSLNode extends Node {
+    abstract static class AbstractNode extends Node {
 
         abstract int execute(Object v);
+    }
+
+    abstract static class SimpleDSLNode extends AbstractNode {
 
         @Specialization
         int doInt(int v) {
@@ -142,9 +209,25 @@ public class DSLInterpreterBenchmark extends TruffleBenchmark {
 
     }
 
+    abstract static class CachedDSLNode extends AbstractNode {
+
+        @Specialization
+        int doCached(@SuppressWarnings("unused") int v, @Cached("CACHED") int cached) {
+            return cached;
+        }
+
+        @Specialization
+        int doLong(long v) {
+            return (int) v;
+        }
+
+        static final int CACHED = 42;
+
+    }
+
     @Benchmark
     @OperationsPerInvocation(NODES)
-    public int firstIteration(SpecializeState state) {
+    public int simpleFirstIteration(SimpleFirstIterationState state) {
         Integer v = Integer.valueOf(42);
         int sum = 0;
         for (int i = 0; i < NODES; i++) {
@@ -155,7 +238,29 @@ public class DSLInterpreterBenchmark extends TruffleBenchmark {
 
     @Benchmark
     @OperationsPerInvocation(NODES)
-    public int secondIteration(SecondIterationState state) {
+    public int simpleSecondIteration(SimpleSecondIterationState state) {
+        Integer v = Integer.valueOf(42);
+        int sum = 0;
+        for (int i = 0; i < NODES; i++) {
+            sum += state.nodes[i].execute(v);
+        }
+        return sum;
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(NODES)
+    public int cachedFirstIteration(CachedFirstIterationState state) {
+        Integer v = Integer.valueOf(42);
+        int sum = 0;
+        for (int i = 0; i < NODES; i++) {
+            sum += state.nodes[i].execute(v);
+        }
+        return sum;
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(NODES)
+    public int cachedSecondIteration(CachedSecondIterationState state) {
         Integer v = Integer.valueOf(42);
         int sum = 0;
         for (int i = 0; i < NODES; i++) {
--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.benchmark/src/com/oracle/truffle/api/benchmark/InterpreterCallBenchmark.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.benchmark/src/com/oracle/truffle/api/benchmark/InterpreterCallBenchmark.java
@@ -1,108 +1,351 @@
 /*
- * Copyright (c) 2018, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * (a) the Software, and
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.oracle.truffle.api.benchmark;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
+import org.graalvm.polyglot.Context;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.CompilerControl;
+import org.openjdk.jmh.annotations.CompilerControl.Mode;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.TearDown;
 
 import com.oracle.truffle.api.CallTarget;
-import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.Truffle;
-import com.oracle.truffle.api.TruffleOptions;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.impl.DefaultTruffleRuntime;
 import com.oracle.truffle.api.nodes.DirectCallNode;
+import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.nodes.RootNode;
 
-@Warmup(iterations = 5)
-@Measurement(iterations = 5)
-@Fork(value = 1, jvmArgsAppend = "-Dgraal.TruffleCompilationThreshold=2147483647")
 public class InterpreterCallBenchmark extends TruffleBenchmark {
 
-    @State(org.openjdk.jmh.annotations.Scope.Thread)
-    public static class CallTargetCallState {
-        final Integer argument = 42;
-        final CallTarget callee;
-        final CallTarget caller;
-
+    @State(Scope.Thread)
+    public static class BenchmarkState {
+        final AbstractRootNode[] rootNodes;
+        final CallTarget[] callTargets;
+        final DirectCallNode[] directCallNodes;
+        final IndirectCallNode indirectCall;
+        final Object[] singleArg = new Object[]{42};
+        final VirtualFrame frame;
+        final Context context;
         {
-            callee = Truffle.getRuntime().createCallTarget(new RootNode(null) {
-                @Override
-                public Object execute(VirtualFrame frame) {
-                    CompilerAsserts.neverPartOfCompilation("do not compile");
-                    return frame.getArguments()[0];
-                }
+            if (Truffle.getRuntime() instanceof DefaultTruffleRuntime) {
+                context = Context.newBuilder().build();
+            } else {
+                context = Context.newBuilder().allowExperimentalOptions(true).option("engine.Compilation", "false").build();
+            }
+            context.enter();
+            rootNodes = new AbstractRootNode[ROOT_CLASSES.length];
+            callTargets = new CallTarget[ROOT_CLASSES.length];
+            directCallNodes = new DirectCallNode[ROOT_CLASSES.length];
 
-                @Override
-                public String toString() {
-                    return "callee";
+            for (int i = 0; i < ROOT_CLASSES.length; i++) {
+                Class<?> rootClass = ROOT_CLASSES[i];
+                try {
+                    rootNodes[i] = (AbstractRootNode) rootClass.getConstructor().newInstance();
+                } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                    throw new AssertionError(e);
                 }
-            });
-            caller = Truffle.getRuntime().createCallTarget(new RootNode(null) {
-                @Child private DirectCallNode callNode = Truffle.getRuntime().createDirectCallNode(callee);
+                callTargets[i] = Truffle.getRuntime().createCallTarget(rootNodes[i]);
+                directCallNodes[i] = Truffle.getRuntime().createDirectCallNode(callTargets[i]);
+            }
 
-                @Override
-                public Object execute(VirtualFrame frame) {
-                    CompilerAsserts.neverPartOfCompilation("do not compile");
-                    return callNode.call(new Object[]{frame.getArguments()[0]});
-                }
-
-                @Override
-                public String toString() {
-                    return "caller";
-                }
-            });
+            this.frame = Truffle.getRuntime().createVirtualFrame(singleArg, rootNodes[0].getFrameDescriptor());
+            indirectCall = Truffle.getRuntime().createIndirectCallNode();
         }
 
-        @Setup
-        public void setup() {
-            // Ensure call boundary method is compiled.
-            ensureTruffleCompilerInitialized();
+        @TearDown
+        public void tearDown() {
+            context.leave();
+            context.close();
         }
     }
 
     @Benchmark
-    public Object directCall(CallTargetCallState state) {
-        return state.caller.call(state.argument);
+    @OperationsPerInvocation(ROOT_CLASSES_LENGTH)
+    public int upperBound(BenchmarkState state) {
+        AbstractRootNode[] roots = state.rootNodes;
+        Object[] args = state.singleArg;
+        int sum = 0;
+        for (int i = 0; i < roots.length; i++) {
+            RootNode root = roots[i];
+            sum += (int) callBoundary(args, root);
+        }
+        return sum;
     }
 
-    static void ensureTruffleCompilerInitialized() {
-        if (TruffleOptions.AOT) {
-            return;
-        }
-        try {
-            Method getTruffleCompiler = Truffle.getRuntime().getClass().getMethod("getTruffleCompiler");
-            getTruffleCompiler.invoke(Truffle.getRuntime());
-        } catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException e) {
-            System.err.println("Could not invoke getTruffleCompiler(): " + e);
-        }
+    @CompilerControl(Mode.DONT_INLINE)
+    private static Object callBoundary(Object[] args, RootNode root) {
+        return root.execute(Truffle.getRuntime().createVirtualFrame(args, root.getFrameDescriptor()));
     }
+
+    private static final int TARGETS = 10000;
+
+    @State(Scope.Thread)
+    public static class FirstCallState extends BenchmarkState {
+
+        final CallTarget[] callTargets = new CallTarget[TARGETS];
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            for (int i = 0; i < TARGETS; i++) {
+                callTargets[i] = Truffle.getRuntime().createCallTarget(rootNodes[i % rootNodes.length]);
+            }
+        }
+
+        @TearDown(Level.Invocation)
+        public void tearDownIteration() {
+            System.gc();
+        }
+
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(TARGETS)
+    public int firstCall(FirstCallState state) {
+        int sum = 0;
+        IndirectCallNode callNode = state.indirectCall;
+        for (int i = 0; i < TARGETS; i++) {
+            sum += (int) callNode.call(state.callTargets[i], state.singleArg);
+        }
+        return sum;
+    }
+
+    @State(Scope.Thread)
+    public static class SecondCallState extends FirstCallState {
+
+        @Override
+        @Setup(Level.Invocation)
+        public void setup() {
+            super.setup();
+            for (int i = 0; i < TARGETS; i++) {
+                indirectCall.call(callTargets[i], singleArg);
+            }
+        }
+
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(TARGETS)
+    public int secondCall(SecondCallState state) {
+        int sum = 0;
+        IndirectCallNode callNode = state.indirectCall;
+        for (int i = 0; i < TARGETS; i++) {
+            sum += (int) callNode.call(state.callTargets[i], state.singleArg);
+        }
+        return sum;
+    }
+
+    @State(Scope.Thread)
+    public static class CallTargetCreateState extends BenchmarkState {
+
+        final CallTarget[] callTargets = new CallTarget[TARGETS];
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            for (int i = 0; i < TARGETS; i++) {
+                callTargets[i] = Truffle.getRuntime().createCallTarget(rootNodes[i % rootNodes.length]);
+            }
+        }
+
+    }
+
+    @Benchmark
+    public Object callTargetCreate() {
+        return Truffle.getRuntime().createCallTarget(new RootNode1());
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(ROOT_CLASSES_LENGTH)
+    public Object directCall(BenchmarkState state) {
+        DirectCallNode[] callNodes = state.directCallNodes;
+        Object[] args = state.singleArg;
+        int sum = 0;
+        for (int i = 0; i < callNodes.length; i++) {
+            DirectCallNode callNode = callNodes[i];
+            sum += (int) callNode.call(args);
+        }
+        return sum;
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(ROOT_CLASSES_LENGTH)
+    public Object indirectCall(BenchmarkState state) {
+        CallTarget[] targets = state.callTargets;
+        Object[] args = state.singleArg;
+        IndirectCallNode callNode = state.indirectCall;
+        int sum = 0;
+        for (int i = 0; i < targets.length; i++) {
+            sum += (int) callNode.call(targets[i], args);
+        }
+        return sum;
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(ROOT_CLASSES_LENGTH)
+    public Object slowPathCall(BenchmarkState state) {
+        CallTarget[] targets = state.callTargets;
+        Object[] args = state.singleArg;
+        int sum = 0;
+        for (int i = 0; i < targets.length; i++) {
+            sum += (int) targets[i].call(args);
+        }
+        return sum;
+    }
+
+    /*
+     * In order to be representative of a real interpreter we simulate many different root nodes
+     * such that the root node call becomes megamorphic, like in a typical interpreter.
+     */
+    static final Class<?>[] ROOT_CLASSES = new Class<?>[]{
+                    RootNode1.class, RootNode2.class,
+                    RootNode3.class, RootNode4.class,
+                    RootNode5.class, RootNode6.class,
+                    RootNode7.class, RootNode8.class,
+                    RootNode9.class, RootNode10.class,
+    };
+    static final int ROOT_CLASSES_LENGTH = 10;
+
+    public abstract static class AbstractRootNode extends RootNode {
+
+        protected AbstractRootNode() {
+            super(null);
+        }
+
+    }
+
+    public static class RootNode1 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode2 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode3 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode4 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode5 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode6 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode7 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode8 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode9 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
+    public static class RootNode10 extends AbstractRootNode {
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return frame.getArguments()[0];
+        }
+
+    }
+
 }
