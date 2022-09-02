abb6608947c8209c8abf20543f64f43df43bee5a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:50:28 +0200

    code quality: remove redundant last location access fields

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/memory/AbstractWriteNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/memory/AbstractWriteNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -66,11 +66,6 @@ public abstract class AbstractWriteNode extends FixedAccessNode implements State
 
     public ValueNode value() {
         return value;
-    }
-
-    public void setValue(ValueNode value) {
-        updateUsages(this.value, value);
-        this.value = value;
     }
 
     protected AbstractWriteNode(NodeClass<? extends AbstractWriteNode> c, AddressNode address, LocationIdentity location, ValueNode value, BarrierType barrierType) {
