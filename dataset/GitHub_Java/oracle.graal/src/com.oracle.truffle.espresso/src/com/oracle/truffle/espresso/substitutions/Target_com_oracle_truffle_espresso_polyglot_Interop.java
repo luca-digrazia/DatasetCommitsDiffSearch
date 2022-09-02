8c7f6aba88f3522b65a1fcaa298ee1dd3f973bf5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:42:42 +0200

    Formatter pass.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_com_oracle_truffle_espresso_polyglot_Interop.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_com_oracle_truffle_espresso_polyglot_Interop.java
@@ -75,7 +75,6 @@ public final class Target_com_oracle_truffle_espresso_polyglot_Interop {
      *
      * @see InteropLibrary#isString(Object)
      */
-    @Substitution
     public static boolean isString(@Host(Object.class) StaticObject receiver) {
         return UNCACHED.isString(unwrap(receiver));
     }
