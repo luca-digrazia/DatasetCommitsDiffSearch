efdd839d7414d3074bc7e635df32396fb88f0c56
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:53:34 +0200

    sulong: use getSimpleLibraryName for resolveRenamedSymbols

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/Runner.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/Runner.java
@@ -1004,7 +1004,7 @@ final class Runner {
             library.makeBitcodeLibrary();
             context.ensureExternalLibraryAdded(library);
             context.addLibraryPaths(binaryParserResult.getLibraryPaths());
-            ArrayList<ExternalLibrary> dependencies = processDependencies(library, binaryParserResult, parseContext);
+            ArrayList<ExternalLibrary> dependencies = processDependencies(binaryParserResult, parseContext, library);
             LLVMParserResult parserResult = parseBinary(binaryParserResult, library);
             parserResult.setDependencies(dependencies);
             parseContext.parserResultsAdd(parserResult);
@@ -1022,14 +1022,14 @@ final class Runner {
      * {@link BinaryParserResult} into {@link ExternalLibrary}s and add them to the
      * {@link ParseContext#dependencyQueueAddLast dependency queue} if not already in there.
      */
-    private ArrayList<ExternalLibrary> processDependencies(ExternalLibrary library, BinaryParserResult binaryParserResult, ParseContext parseContext) {
+    private ArrayList<ExternalLibrary> processDependencies(BinaryParserResult binaryParserResult, ParseContext parseContext, Object reason) {
         ArrayList<ExternalLibrary> dependencies = new ArrayList<>();
-        for (String lib : context.injectDependencies(library, binaryParserResult.getLibraries())) {
-            ExternalLibrary dependency = context.findExternalLibrary(lib, library, binaryParserResult.getLocator());
+        for (String lib : binaryParserResult.getLibraries()) {
+            ExternalLibrary dependency = context.findExternalLibrary(lib, reason, binaryParserResult.getLocator());
             if (dependency != null) {
                 dependencies.add(dependency);
             } else {
-                dependency = context.addExternalLibrary(lib, library, binaryParserResult.getLocator());
+                dependency = context.addExternalLibrary(lib, reason, binaryParserResult.getLocator());
                 if (dependency != null) {
                     parseContext.dependencyQueueAddLast(dependency);
                     dependencies.add(dependency);
