f3fd24ec1137abce06a3de27d14abf963c847039
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:39:09 +0200

    Allow methods defined in VarHandle itself

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/phases/IntrinsifyMethodHandlesInvocationPlugin.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/phases/IntrinsifyMethodHandlesInvocationPlugin.java
@@ -92,6 +92,7 @@ import org.graalvm.compiler.word.WordOperationPlugin;
 import org.graalvm.nativeimage.ImageSingletons;
 
 import com.oracle.graal.pointsto.constraints.UnsupportedFeatureException;
+import com.oracle.graal.pointsto.meta.AnalysisType;
 import com.oracle.graal.pointsto.meta.AnalysisUniverse;
 import com.oracle.svm.core.FrameAccess;
 import com.oracle.svm.core.graal.nodes.DeadEndNode;
@@ -369,15 +370,15 @@ public class IntrinsifyMethodHandlesInvocationPlugin implements NodePlugin {
 
         @Override
         public boolean canConstantFoldDynamicAllocation(ResolvedJavaType type) {
-            if (hUniverse == null) {
+            ResolvedJavaType convertedType = lookup(type);
+            if (convertedType instanceof AnalysisType) {
                 /*
                  * During static analysis, every type can be constant folded and the static analysis
                  * will see the real allocation.
                  */
                 return true;
             } else {
-                ResolvedJavaType convertedType = optionalLookup(type);
-                return convertedType != null && ((HostedType) convertedType).isInstantiated();
+                return ((HostedType) convertedType).isInstantiated();
             }
         }
     }
@@ -721,14 +722,6 @@ public class IntrinsifyMethodHandlesInvocationPlugin implements NodePlugin {
         ResolvedJavaType result = aUniverse.lookup(type);
         if (hUniverse != null) {
             result = hUniverse.lookup(result);
-        }
-        return result;
-    }
-
-    private ResolvedJavaType optionalLookup(ResolvedJavaType type) {
-        ResolvedJavaType result = aUniverse.optionalLookup(type);
-        if (result != null && hUniverse != null) {
-            result = hUniverse.optionalLookup(result);
         }
         return result;
     }
