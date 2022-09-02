a2cde8a1dd43cb90ecbfff10686d807844c3764a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 15:16:00 +0200

    merge explode: add failing test case and comment inabilities of merge explode algorithm to produce correct proxies for non live nodes in loops proxied to outer, merge exploded, loops

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/MergeExplodeProxyTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/MergeExplodeProxyTest.java
@@ -733,6 +733,7 @@ public class MergeExplodeProxyTest extends PartialEvaluationTest {
             return res;
         }
 
+        @Ignore("Merge explode partial evaluation cannot proxy nodes that are not alive in the framestate of inner loop begins")
         @Override
         @ExplodeLoop(kind = LoopExplosionKind.MERGE_EXPLODE)
         public Object execute(VirtualFrame frame) {
@@ -853,7 +854,6 @@ public class MergeExplodeProxyTest extends PartialEvaluationTest {
 
     }
 
-    @Ignore("Merge explode partial evaluation cannot proxy nodes that are not alive in the framestate of inner loop begins")
     @Test
     public void testNoneLiveLoopExitProxy() {
         byte[] bytecodes = new byte[]{
