2d8e25f5a7bd3308b3b0299242b4da20eb686ef4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:40:18 +0200

    Make ImageSingletons initialization more obvious

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/NativeImageGenerator.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/NativeImageGenerator.java
@@ -465,7 +465,8 @@ public class NativeImageGenerator {
             setSystemPropertiesForImageLate(k);
 
             ImageSingletonsSupportImpl.HostedManagement.installInThread(new ImageSingletonsSupportImpl.HostedManagement());
-            this.buildExecutor = createForkJoinPool(compilationExecutor.getParallelism());
+            int maxConcurrentThreads = NativeImageOptions.getMaximumNumberOfConcurrentThreads(new OptionValues(optionProvider.getHostedValues()));
+            this.buildExecutor = createForkJoinPool(maxConcurrentThreads);
             buildExecutor.submit(() -> {
                 ImageSingletons.add(ClassLoaderQuery.class, new ClassLoaderQueryImpl(loader.getClassLoader()));
                 ImageSingletons.add(HostedOptionValues.class, new HostedOptionValues(optionProvider.getHostedValues()));
