b73f24e53cb2517eefc8e2584f20af0c0e161b3c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:13:38 +0200

    Fix find/replace goof.

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_com_oracle_truffle_espresso_polyglot_Polyglot.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_com_oracle_truffle_espresso_polyglot_Polyglot.java
@@ -204,7 +204,7 @@ public final class Target_com_oracle_truffle_espresso_polyglot_Polyglot {
         @JavaType(Object.class)
         StaticObject doString(
                         EspressoContext context,
-                        @SuppressWarnings("unused") Klass targetKlass,
+                        Klass targetKlass,
                         @JavaType(Object.class) StaticObject value,
                         @CachedLibrary(limit = "LIMIT") InteropLibrary interop,
                         @Cached BranchProfile exceptionProfile) {
@@ -224,7 +224,7 @@ public final class Target_com_oracle_truffle_espresso_polyglot_Polyglot {
         @JavaType(Object.class)
         StaticObject doForeignException(
                         EspressoContext context,
-                        @SuppressWarnings("unused") Klass targetKlass,
+                        Klass targetKlass,
                         @JavaType(Object.class) StaticObject value,
                         @CachedLibrary(limit = "LIMIT") InteropLibrary interop,
                         @Cached BranchProfile exceptionProfile) {
