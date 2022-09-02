2c632b5bbd4d197563751ccbcde3d107fd10a608
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:22:50 +0200

    Comments and minor improvements.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/CompilationTask.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/CompilationTask.java
@@ -231,7 +231,6 @@ public final class CompilationTask implements TruffleCompilationTask, Callable<V
         lastTime = currentTime;
         lastCount = count;
         lastWeight = weight;
-        assert weight >= 0.0 : "weight must be positive";
         return weight;
     }
 
