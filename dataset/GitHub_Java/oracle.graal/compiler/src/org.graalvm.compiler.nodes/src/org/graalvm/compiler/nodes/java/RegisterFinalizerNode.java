6905aba26b0db6c6c5f252700c9229223107ddea
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:55:40 +0200

    Fix AbstractObjectStamp.equals

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/java/RegisterFinalizerNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/java/RegisterFinalizerNode.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -22,7 +24,6 @@
  */
 package org.graalvm.compiler.nodes.java;
 
-import static org.graalvm.compiler.nodeinfo.InputType.State;
 import static org.graalvm.compiler.nodeinfo.NodeCycles.CYCLES_UNKNOWN;
 import static org.graalvm.compiler.nodeinfo.NodeSize.SIZE_8;
 import static org.graalvm.compiler.nodes.java.ForeignCallDescriptors.REGISTER_FINALIZER;
@@ -31,26 +32,33 @@ import org.graalvm.compiler.core.common.spi.ForeignCallLinkage;
 import org.graalvm.compiler.core.common.type.ObjectStamp;
 import org.graalvm.compiler.core.common.type.StampFactory;
 import org.graalvm.compiler.graph.NodeClass;
-import org.graalvm.compiler.graph.spi.Canonicalizable;
-import org.graalvm.compiler.graph.spi.CanonicalizerTool;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
 import org.graalvm.compiler.nodes.AbstractStateSplit;
 import org.graalvm.compiler.nodes.DeoptimizingNode;
-import org.graalvm.compiler.nodes.FrameState;
 import org.graalvm.compiler.nodes.NodeView;
 import org.graalvm.compiler.nodes.ValueNode;
+import org.graalvm.compiler.nodes.spi.Canonicalizable;
+import org.graalvm.compiler.nodes.spi.CanonicalizerTool;
 import org.graalvm.compiler.nodes.spi.LIRLowerable;
+import org.graalvm.compiler.nodes.spi.Lowerable;
 import org.graalvm.compiler.nodes.spi.NodeLIRBuilderTool;
 import org.graalvm.compiler.nodes.spi.Virtualizable;
 import org.graalvm.compiler.nodes.spi.VirtualizerTool;
+import org.graalvm.compiler.nodes.type.StampTool;
 import org.graalvm.compiler.nodes.virtual.VirtualObjectNode;
 
 import jdk.vm.ci.meta.Assumptions;
 import jdk.vm.ci.meta.Assumptions.AssumptionResult;
+import jdk.vm.ci.meta.MetaAccessProvider;
+import jdk.vm.ci.meta.ResolvedJavaType;
 
 /**
  * This node is used to perform the finalizer registration at the end of the java.lang.Object
  * constructor.
+ *
+ * Can be optimized away if the class may not have a finalizer (if we know the exact type or can
+ * record an assumption that it does not have a finalizable subclass). On HotSpot, it is lowered to
+ * a conditional runtime call (see {@code RegisterFinalizerSnippets.registerFinalizerSnippet}).
  */
 // @formatter:off
 @NodeInfo(cycles = CYCLES_UNKNOWN,
@@ -58,14 +66,17 @@ import jdk.vm.ci.meta.Assumptions.AssumptionResult;
           size = SIZE_8,
           sizeRationale = "Rough estimation for register handling & calling")
 // @formatter:on
-public final class RegisterFinalizerNode extends AbstractStateSplit implements Canonicalizable.Unary<ValueNode>, LIRLowerable, Virtualizable, DeoptimizingNode.DeoptAfter {
+public class RegisterFinalizerNode extends AbstractStateSplit implements Canonicalizable.Unary<ValueNode>, LIRLowerable, Lowerable, Virtualizable, DeoptimizingNode.DeoptAfter {
 
     public static final NodeClass<RegisterFinalizerNode> TYPE = NodeClass.create(RegisterFinalizerNode.class);
-    @OptionalInput(State) FrameState deoptState;
     @Input ValueNode value;
 
     public RegisterFinalizerNode(ValueNode value) {
-        super(TYPE, StampFactory.forVoid());
+        this(TYPE, value);
+    }
+
+    protected RegisterFinalizerNode(NodeClass<? extends RegisterFinalizerNode> c, ValueNode value) {
+        super(c, StampFactory.forVoid());
         this.value = value;
     }
 
@@ -87,26 +98,27 @@ public final class RegisterFinalizerNode extends AbstractStateSplit implements C
      * Determines if the compiler should emit code to test whether a given object has a finalizer
      * that must be registered with the runtime upon object initialization.
      */
-    public static boolean mayHaveFinalizer(ValueNode object, Assumptions assumptions) {
+    public static boolean mayHaveFinalizer(ValueNode object, MetaAccessProvider metaAccess, Assumptions assumptions) {
         ObjectStamp objectStamp = (ObjectStamp) object.stamp(NodeView.DEFAULT);
+        ResolvedJavaType objectType = StampTool.typeOrNull(objectStamp, metaAccess);
         if (objectStamp.isExactType()) {
-            return objectStamp.type().hasFinalizer();
-        } else if (objectStamp.type() != null) {
-            AssumptionResult<Boolean> result = objectStamp.type().hasFinalizableSubclass();
-            if (result.canRecordTo(assumptions)) {
-                result.recordTo(assumptions);
-                return result.getResult();
-            }
+            return objectType.hasFinalizer();
+        }
+        AssumptionResult<Boolean> result = objectType.hasFinalizableSubclass();
+        if (result.canRecordTo(assumptions)) {
+            result.recordTo(assumptions);
+            return result.getResult();
         }
         return true;
     }
 
     @Override
     public ValueNode canonical(CanonicalizerTool tool, ValueNode forValue) {
-        if (!(forValue.stamp(NodeView.DEFAULT) instanceof ObjectStamp)) {
+        NodeView view = NodeView.from(tool);
+        if (!(forValue.stamp(view) instanceof ObjectStamp)) {
             return this;
         }
-        if (!mayHaveFinalizer(forValue, graph().getAssumptions())) {
+        if (!mayHaveFinalizer(forValue, tool.getMetaAccess(), graph().getAssumptions())) {
             return null;
         }
 
@@ -125,7 +137,4 @@ public final class RegisterFinalizerNode extends AbstractStateSplit implements C
     public boolean canDeoptimize() {
         return true;
     }
-
-    @NodeIntrinsic
-    public static native void register(Object thisObj);
 }
