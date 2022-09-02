a061662e406b0fe82c5c855646b9f09ce0cc88af
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:07:43 +0200

    Cleanups.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/thread/VMOperationControl.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/thread/VMOperationControl.java
@@ -43,9 +43,7 @@ import org.graalvm.word.WordFactory;
 import com.oracle.svm.core.SubstrateOptions;
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import com.oracle.svm.core.annotate.NeverInline;
-import com.oracle.svm.core.annotate.RestrictHeapAccess;
 import com.oracle.svm.core.annotate.Uninterruptible;
-import com.oracle.svm.core.annotate.RestrictHeapAccess.Access;
 import com.oracle.svm.core.locks.VMCondition;
 import com.oracle.svm.core.locks.VMMutex;
 import com.oracle.svm.core.log.Log;
@@ -119,31 +117,29 @@ public final class VMOperationControl {
         dedicatedVMOperationThread.waitUntilStarted();
     }
 
-    public static void shutdownAndDetachVMOperationThread() {
+    public static void stopVMOperationThread() {
         assert UseDedicatedVMOperationThread.getValue();
         JavaVMOperation.enqueueBlockingNoSafepoint("Stop VMOperationThread", () -> {
             dedicatedVMOperationThread.shutdown();
         });
 
-        waitUntilVMOperationThreadDetached();
         assert get().mainQueues.isEmpty();
     }
 
-    @RestrictHeapAccess(access = Access.NO_ALLOCATION, reason = "Called during teardown")
     @NeverInline("Must not be inlined in a caller that has an exception handler: We only support InvokeNode and not InvokeWithExceptionNode between a CFunctionPrologueNode and CFunctionEpilogueNode.")
-    private static void waitUntilVMOperationThreadDetached() {
+    public static void waitUntilVMOperationThreadExited() {
         CFunctionPrologueNode.cFunctionPrologue();
-        waitUntilVMOperationThreadDetachedInNative();
+        waitUntilVMOperationThreadExitedInNative();
         CFunctionEpilogueNode.cFunctionEpilogue();
     }
 
     /**
-     * Wait until the VM operation thread reached a point where it detached from SVM and is
-     * therefore no longer executing Java code.
+     * Wait until the VM operation thread reached a point where it does not access the heap anymore.
+     * Otherwise, we might tear down the heap although it is still being accessed.
      */
     @Uninterruptible(reason = "Must not stop while in native.")
     @NeverInline("Provide a return address for the Java frame anchor.")
-    private static void waitUntilVMOperationThreadDetachedInNative() {
+    private static void waitUntilVMOperationThreadExitedInNative() {
         // this method may only access data in the image heap
         VMThreads.THREAD_MUTEX.lockNoTransition();
         try {
