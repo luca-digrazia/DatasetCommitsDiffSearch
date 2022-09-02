a149bb651ae48c4904f1f05c164bf1e869a44106
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 14:55:26 +0200

    Sulong: formatting

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/initialization/InitializeExternalNode.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/initialization/InitializeExternalNode.java
@@ -38,7 +38,6 @@ import com.oracle.truffle.llvm.parser.model.GlobalSymbol;
 import com.oracle.truffle.llvm.parser.model.functions.FunctionSymbol;
 import com.oracle.truffle.llvm.runtime.LLVMContext;
 import com.oracle.truffle.llvm.runtime.LLVMFunction;
-import com.oracle.truffle.llvm.runtime.LLVMFunctionCode;
 import com.oracle.truffle.llvm.runtime.LLVMIntrinsicProvider;
 import com.oracle.truffle.llvm.runtime.LLVMLanguage;
 import com.oracle.truffle.llvm.runtime.LLVMLocalScope;
@@ -96,11 +95,10 @@ public final class InitializeExternalNode extends LLVMNode {
         for (FunctionSymbol symbol : result.getExternalFunctions()) {
             String name = symbol.getName();
             LLVMFunction function = fileScope.getFunction(name);
-            LLVMFunctionCode functionCode = new LLVMFunctionCode(function);
             if (name.startsWith("llvm.") || name.startsWith("__builtin_") || name.equals("polyglot_get_arg") || name.equals("polyglot_get_arg_count")) {
                 continue;
             }
-            allocExternaSymbolsList.add(AllocExternalFunctionNodeGen.create(function, functionCode, nodeFactory));
+            allocExternaSymbolsList.add(AllocExternalFunctionNodeGen.create(function, nodeFactory));
             writeSymbolsList.add(LLVMWriteSymbolNodeGen.create(function));
         }
 
