3027c9a2f26f37bf79cc30551ac26af73b2a1c6f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:57:38 +0200

    Refactor: Move method. Comments.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.compiler/src/org/graalvm/compiler/truffle/compiler/phases/inlining/GraphManager.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.compiler/src/org/graalvm/compiler/truffle/compiler/phases/inlining/GraphManager.java
@@ -24,6 +24,11 @@
  */
 package org.graalvm.compiler.truffle.compiler.phases.inlining;
 
+import jdk.vm.ci.meta.JavaConstant;
+import jdk.vm.ci.meta.MetaAccessProvider;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+import jdk.vm.ci.meta.ResolvedJavaType;
+import jdk.vm.ci.meta.SpeculationLog;
 import org.graalvm.collections.EconomicMap;
 import org.graalvm.collections.UnmodifiableEconomicMap;
 import org.graalvm.compiler.core.common.CompilationIdentifier;
@@ -44,12 +49,6 @@ import org.graalvm.compiler.truffle.common.CompilableTruffleAST;
 import org.graalvm.compiler.truffle.common.TruffleCallNode;
 import org.graalvm.compiler.truffle.common.TruffleCompilerRuntime;
 import org.graalvm.compiler.truffle.compiler.PartialEvaluator;
-
-import jdk.vm.ci.meta.JavaConstant;
-import jdk.vm.ci.meta.MetaAccessProvider;
-import jdk.vm.ci.meta.ResolvedJavaMethod;
-import jdk.vm.ci.meta.ResolvedJavaType;
-import jdk.vm.ci.meta.SpeculationLog;
 
 public class GraphManager {
 
@@ -103,10 +102,6 @@ public class GraphManager {
                         "cost-benefit analysis", "AgnosticInliningPhase");
     }
 
-    public CoreProviders getCoreProviders() {
-        return partialEvaluator.getProviders();
-    }
-
     static class Entry {
         final StructuredGraph graph;
         final EconomicMap<TruffleCallNode, Invoke> truffleCallNodeToInvoke;
@@ -115,6 +110,10 @@ public class GraphManager {
             this.graph = graph;
             this.truffleCallNodeToInvoke = truffleCallNodeToInvoke;
         }
+    }
+
+    public CoreProviders getCoreProviders() {
+        return partialEvaluator.getProviders();
     }
 
     private static class PEAgnosticInlineInvokePlugin extends PartialEvaluator.PEInlineInvokePlugin {
