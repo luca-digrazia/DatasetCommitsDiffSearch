fc9e0ec463513a2829c13596f094692c095ad835
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:03:29 +0200

    remove empty space

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoRootNode.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoRootNode.java
@@ -79,11 +79,6 @@ public abstract class EspressoRootNode extends RootNode implements ContextAccess
         return getMethodNode().getSourceSection();
     }
 
-    @Override
-    public SourceSection getEncapsulatingSourceSection() {
-        return getMethodNode().getEncapsulatingSourceSection();
-    }
-
     public final boolean isBytecodeNode() {
         return getMethodNode() instanceof BytecodeNode;
     }
