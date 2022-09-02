7599798fa38b13a1a0b6d1e47e1bde705f9b5779
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:07:29 +0200

    support for invoking static interface methods through jdwp

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/JDWP.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/JDWP.java
@@ -30,7 +30,9 @@ import com.oracle.truffle.espresso.jdwp.api.LocalRef;
 import com.oracle.truffle.espresso.jdwp.api.MethodRef;
 import com.oracle.truffle.espresso.jdwp.api.KlassRef;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import static com.oracle.truffle.espresso.jdwp.impl.TagConstants.BOOLEAN;
