5a826c64af963873e825dc1e87be2f3dc4e3d067
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:59:46 +0200

    Fix transient consurrent access to quickened bytecode array.

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/BytecodeNode.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/BytecodeNode.java
@@ -1072,9 +1072,7 @@ public final class BytecodeNode extends EspressoMethodNode {
 
                     case INVOKEDYNAMIC: top += quickenInvokeDynamic(frame, primitives, refs, top, curBCI, curOpcode); break;
                     case QUICK: {
-                        // Force a volatile read of the opcode.
-                        CompilerAsserts.partialEvaluationConstant(bs.currentVolatileBC(curBCI));
-                        QuickNode quickNode = nodes[bs.readCPI(curBCI)];
+                        QuickNode quickNode = nodes[bs.volatileReadCPI(curBCI)];
                         if (quickNode.removedByRedefintion()) {
                             CompilerDirectives.transferToInterpreterAndInvalidate();
                             synchronized (this) {
