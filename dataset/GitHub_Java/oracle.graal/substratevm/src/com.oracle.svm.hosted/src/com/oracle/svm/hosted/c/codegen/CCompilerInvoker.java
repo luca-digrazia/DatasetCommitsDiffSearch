a8c7dda073ac885e8fa0ad2b90cea8b995994dde
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:39:42 +0200

    Provide compilerPath in CompilerInfo

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/codegen/CCompilerInvoker.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/codegen/CCompilerInvoker.java
@@ -397,8 +397,7 @@ public abstract class CCompilerInvoker {
         for (String s : cmd) {
             if (s.indexOf(' ') != -1) {
                 // Quote command line arguments that contain a space
-                String escaped = s.replace("'", "\\'");
-                sb.append('\'').append(escaped).append('\'');
+                sb.append('\'').append(s).append('\'');
             } else {
                 sb.append(s);
             }
