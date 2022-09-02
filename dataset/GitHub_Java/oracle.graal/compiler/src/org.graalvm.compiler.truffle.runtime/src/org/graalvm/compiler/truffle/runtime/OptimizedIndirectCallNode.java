d22bcf7c77e16d40add37b215a8e5a356d175d7d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 15:28:35 +0200

    Simply call OptimizedCallTarget#call() in the uncached OptimizedIndirectCallNode

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedIndirectCallNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedIndirectCallNode.java
@@ -27,6 +27,7 @@ package org.graalvm.compiler.truffle.runtime;
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.profiles.ValueProfile;
@@ -58,6 +59,21 @@ public final class OptimizedIndirectCallNode extends IndirectCallNode {
             GraalRuntimeAccessor.LANGUAGE.onThrowable(this, null, profiledT, null);
             throw OptimizedCallTarget.rethrow(profiledT);
         }
+    }
+
+    static IndirectCallNode createUncached() {
+        return new IndirectCallNode() {
+            @Override
+            public boolean isAdoptable() {
+                return false;
+            }
+
+            @Override
+            @TruffleBoundary
+            public Object call(CallTarget target, Object... arguments) {
+                return target.call(arguments);
+            }
+        };
     }
 
 }
