631658b83f2fcc810e24d12760027fce93ca3d2a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:19:43 +0200

    Fix bugs within AArch64Move.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.asm.aarch64/src/org/graalvm/compiler/asm/aarch64/AArch64MacroAssembler.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.asm.aarch64/src/org/graalvm/compiler/asm/aarch64/AArch64MacroAssembler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -780,7 +780,7 @@ public class AArch64MacroAssembler extends AArch64Assembler {
     }
 
     /**
-     * Conditional select. dst = src1 if condition else src2.
+     * Conditional move. dst = src1 if condition else src2.
      *
      * @param size register size. Has to be 32 or 64.
      * @param result general purpose register. May not be null or the stackpointer.
@@ -788,8 +788,7 @@ public class AArch64MacroAssembler extends AArch64Assembler {
      * @param falseValue general purpose register. May not be null or the stackpointer.
      * @param cond any condition flag. May not be null.
      */
-    @Override
-    public void csel(int size, Register result, Register trueValue, Register falseValue, ConditionFlag cond) {
+    public void cmov(int size, Register result, Register trueValue, Register falseValue, ConditionFlag cond) {
         super.csel(size, result, trueValue, falseValue, cond);
     }
 
@@ -1590,18 +1589,6 @@ public class AArch64MacroAssembler extends AArch64Assembler {
     }
 
     /**
-     * dst = src1 & ~(src2) and sets condition flags.
-     *
-     * @param size register size. Has to be 32 or 64.
-     * @param dst general purpose register. May not be null or stackpointer.
-     * @param src1 general purpose register. May not be null or stackpointer.
-     * @param src2 general purpose register. May not be null or stackpointer.
-     */
-    public void bics(int size, Register dst, Register src1, Register src2) {
-        super.bics(size, dst, src1, src2, ShiftType.LSL, 0);
-    }
-
-    /**
      * dst = src1 ^ ~(shiftType(src2, imm)).
      *
      * @param size register size. Has to be 32 or 64.
@@ -1732,7 +1719,7 @@ public class AArch64MacroAssembler extends AArch64Assembler {
     }
 
     /**
-     * Conditional select. dst = src1 if condition else src2.
+     * Conditional move. dst = src1 if condition else src2.
      *
      * @param size register size.
      * @param result floating point register. May not be null.
@@ -1740,8 +1727,7 @@ public class AArch64MacroAssembler extends AArch64Assembler {
      * @param falseValue floating point register. May not be null.
      * @param condition every condition allowed. May not be null.
      */
-    @Override
-    public void fcsel(int size, Register result, Register trueValue, Register falseValue, ConditionFlag condition) {
+    public void fcmov(int size, Register result, Register trueValue, Register falseValue, ConditionFlag condition) {
         super.fcsel(size, result, trueValue, falseValue, condition);
     }
 
@@ -1823,7 +1809,7 @@ public class AArch64MacroAssembler extends AArch64Assembler {
                     mov(64, dst, temp1);
                     mov(temp1, 0x80000000);
                     // Develop 0 (EQ), or 0x80000000 (NE)
-                    csel(32, temp1, temp1, zr, ConditionFlag.NE);
+                    cmov(32, temp1, temp1, zr, ConditionFlag.NE);
                     cmp(32, temp1, 1);
                     // 0x80000000 - 1 => VS
                     break;
@@ -1837,7 +1823,7 @@ public class AArch64MacroAssembler extends AArch64Assembler {
                     // NE => overflow
                     mov(temp1, 0x80000000);
                     // Develop 0 (EQ), or 0x80000000 (NE)
-                    csel(32, temp1, temp1, zr, ConditionFlag.NE);
+                    cmov(32, temp1, temp1, zr, ConditionFlag.NE);
                     cmp(32, temp1, 1);
                     // 0x80000000 - 1 => VS
                     break;
