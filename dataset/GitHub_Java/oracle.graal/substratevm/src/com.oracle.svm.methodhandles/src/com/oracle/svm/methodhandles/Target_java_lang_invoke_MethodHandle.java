cb8a97b01735c0e7277aaac78872b0ae3f16064a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:38:49 +0200

    Fix boxed argument casting for method handle invoke

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.methodhandles/src/com/oracle/svm/methodhandles/Target_java_lang_invoke_MethodHandle.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.methodhandles/src/com/oracle/svm/methodhandles/Target_java_lang_invoke_MethodHandle.java
@@ -29,7 +29,6 @@ import java.lang.invoke.MethodType;
 // Checkstyle: stop
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 // Checkstyle: resume
@@ -126,9 +125,6 @@ final class Target_java_lang_invoke_MethodHandle {
                             }
                         }
                     }
-                } catch (InvocationTargetException e) {
-                    /* Exceptions are thrown unchanged from method handles */
-                    throw e.getCause();
                 } finally {
                     executable.override = oldOverride;
                 }
