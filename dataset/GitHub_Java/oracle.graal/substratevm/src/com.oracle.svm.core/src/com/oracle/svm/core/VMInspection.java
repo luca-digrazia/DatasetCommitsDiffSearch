8fd6876374105a112817f485bc20e8502c69e18f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:43:25 +0200

    Make -H:+AllowVMInspection work on Windows

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/VMInspection.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/VMInspection.java
@@ -129,11 +129,12 @@ class DumpHeapReport implements SignalHandler {
         Signal.handle(new Signal("USR1"), new DumpHeapReport());
     }
 
-    private static void performHeapDump(FileOutputStream fileOutputStream) {
-        try {
-            RuntimeSupport.getRuntimeSupport().dumpHeap(fileOutputStream, true);
-        } catch (IOException e) {
-            Log.log().string("HeapDump failed: ").string(e.getMessage()).newline();
+    @SuppressWarnings("deprecation")
+    @NeverInline("Ensure ClassCastException gets caught")
+    private static void performHeapDump(FileOutputStream fileOutputStream) throws Exception {
+        Object[] args = new Object[]{"HeapDump.dumpHeap(FileOutputStream, Boolean)Boolean", fileOutputStream, Boolean.TRUE};
+        if (!((Boolean) Compiler.command(args))) {
+            throw new RuntimeException();
         }
     }
 
