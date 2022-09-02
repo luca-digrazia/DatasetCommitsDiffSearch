f59879fa42b8f42d708a1938a9a774e0c458e93e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:59:38 +0200

    Remove redundant implication.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/IntegerLessThanNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/IntegerLessThanNode.java
@@ -226,6 +226,7 @@ public final class IntegerLessThanNode extends IntegerLowerThanNode {
                             }
                         }
                     }
+
                 }
             }
 
