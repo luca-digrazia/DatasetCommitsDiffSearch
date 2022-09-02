11030a815310085b68e120f96c01dfe7bc165080
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:28:36 +0200

    Rename caller reference constants for better readability.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCallTarget.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCallTarget.java
@@ -275,7 +275,7 @@ public abstract class OptimizedCallTarget implements CompilableTruffleAST, RootC
         this.callAndLoopThreshold = engine.firstTierCallAndLoopThreshold;
     }
 
-    protected List<OptimizedAssumption> getProfiledTypesAssumptions() {
+    protected final List<OptimizedAssumption> getProfiledTypesAssumptions() {
         List<OptimizedAssumption> result = new ArrayList<>();
         if (getProfiledArgumentTypes() != null) {
             result.add(profiledArgumentTypesAssumption);
