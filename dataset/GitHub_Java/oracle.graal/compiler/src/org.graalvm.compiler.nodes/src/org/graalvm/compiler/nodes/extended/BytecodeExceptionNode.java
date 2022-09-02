e2537366d56c1b51916f80fb4fcbdad409cc080c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:10:07 +0200

    Update comments

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/extended/BytecodeExceptionNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/extended/BytecodeExceptionNode.java
@@ -41,7 +41,7 @@ import org.graalvm.compiler.nodeinfo.Verbosity;
 import org.graalvm.compiler.nodes.FrameState;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.memory.AbstractMemoryCheckpoint;
-import org.graalvm.compiler.nodes.memory.SingleMemoryKill;
+import org.graalvm.compiler.nodes.memory.MemoryCheckpoint;
 import org.graalvm.compiler.nodes.spi.Lowerable;
 import org.graalvm.compiler.nodes.spi.LoweringTool;
 import org.graalvm.word.LocationIdentity;
@@ -58,7 +58,7 @@ import jdk.vm.ci.meta.MetaAccessProvider;
           cyclesRationale = "Node will be lowered to a foreign call.",
           size = SIZE_8)
 // @formatter:on
-public final class BytecodeExceptionNode extends AbstractMemoryCheckpoint implements Lowerable, SingleMemoryKill, Canonicalizable {
+public final class BytecodeExceptionNode extends AbstractMemoryCheckpoint implements Lowerable, MemoryCheckpoint.Single, Canonicalizable {
 
     public enum BytecodeExceptionKind {
         NULL_POINTER(0, NullPointerException.class),
@@ -127,9 +127,7 @@ public final class BytecodeExceptionNode extends AbstractMemoryCheckpoint implem
      * Create a new stateDuring for use by a foreign call.
      */
     public FrameState createStateDuring() {
-        boolean rethrowException = false;
-        boolean duringCall = true;
-        return stateAfter.duplicateModified(graph(), stateAfter.bci, rethrowException, duringCall,
+        return stateAfter.duplicateModified(graph(), stateAfter.bci, /* rethrowException */ false, /* duringCall */ true,
                         JavaKind.Object, null, null);
     }
 
