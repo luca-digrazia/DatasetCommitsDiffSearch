8e35b81fe16a0210091d06dd6792cb660228ce38
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:16:55 +0200

    Remove unnecessary toIntExact.

--- dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/memory/WasmMemory.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/memory/WasmMemory.java
@@ -466,7 +466,7 @@ public abstract class WasmMemory implements TruffleObject {
                     @CachedLibrary(limit = "3") InteropLibrary valueLib,
                     @Shared("errorBranch") @Cached BranchProfile errorBranch)
                     throws InvalidArrayIndexException, UnsupportedMessageException, UnsupportedTypeException {
-        if (!isArrayElementModifiable(address)) {
+        if (!isArrayElementReadable(address)) {
             errorBranch.enter();
             throw InvalidArrayIndexException.create(address);
         }
