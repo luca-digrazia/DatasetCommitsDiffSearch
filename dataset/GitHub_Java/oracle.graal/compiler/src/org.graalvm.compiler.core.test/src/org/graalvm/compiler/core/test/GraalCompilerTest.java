8f0db11607103e761fb2a6c9ec022c550fdafe33
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 23:04:05 +0200

    Add scope at GraalCompilerTest.applyFrontEnd.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/GraalCompilerTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/GraalCompilerTest.java
@@ -345,13 +345,8 @@ public abstract class GraalCompilerTest extends GraalTest {
     }
 
     public GraalCompilerTest() {
-        this(new ConcurrentHashMap<>());
-    }
-
-    public GraalCompilerTest(Map<ResolvedJavaMethod, InstalledCode> cache) {
         this.backend = Graal.getRequiredCapability(RuntimeProvider.class).getHostBackend();
         this.providers = getBackend().getProviders();
-        this.cache = cache;
     }
 
     /**
@@ -928,7 +923,7 @@ public abstract class GraalCompilerTest extends GraalTest {
         }
     }
 
-    private Map<ResolvedJavaMethod, InstalledCode> cache;
+    private Map<ResolvedJavaMethod, InstalledCode> cache = new ConcurrentHashMap<>();
 
     /**
      * Gets installed code for a given method, compiling it first if necessary. The graph is parsed
