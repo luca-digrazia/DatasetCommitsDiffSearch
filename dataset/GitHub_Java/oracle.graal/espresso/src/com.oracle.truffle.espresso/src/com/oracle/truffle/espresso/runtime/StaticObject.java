db2dcb93cf4a954aba9698c3d6145ed832294f09
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:19:58 +0200

    Cannot wrap boolean[].
        They are internally represented as byte[].

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/StaticObject.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/StaticObject.java
@@ -1890,7 +1890,7 @@ public final class StaticObject implements TruffleObject {
         assert array != null;
         assert array.getClass().isArray() && array.getClass().getComponentType().isPrimitive();
         if (array instanceof boolean[]) {
-            throw EspressoError.shouldNotReachHere("Cannot wrap a boolean[]. Create a byte[] and call `StaticObject.createArray(meta._boolean_array, byteArray)`.");
+            throw EspressoError.shouldNotReachHere("Cannot wrap a boolean[]. Create a byte[] wrap that.");
         }
         if (array instanceof byte[]) {
             return wrap((byte[]) array, meta);
