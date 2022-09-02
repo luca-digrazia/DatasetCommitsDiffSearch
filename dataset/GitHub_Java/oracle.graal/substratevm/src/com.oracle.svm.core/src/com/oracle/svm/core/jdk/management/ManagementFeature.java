ff698ff0d040bec42536c7c24aa31badbc07779a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 15:58:29 +0200

    No need to rerun in javax.management

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/management/ManagementFeature.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/management/ManagementFeature.java
@@ -47,7 +47,6 @@ import javax.management.openmbean.OpenType;
 
 import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
@@ -82,8 +81,6 @@ public final class ManagementFeature extends JNIRegistrationUtil implements Feat
             }
         }
         access.registerObjectReplacer(this::replaceHostedPlatformManagedObject);
-
-        RuntimeClassInitialization.initializeAtBuildTime("com.sun.jmx.mbeanserver.DefaultMXBeanMappingFactory", "Avoids unnecessary reflection in the image");
     }
 
     /**
