f5b1d00b2b7605d01ed1696944231e01a8520131
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:50:45 +0200

    Remove some useless exception handling. Uses castExact when getting a field from StaticObject.getUnsafeField() to obtain precise location identity.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoRootNode.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoRootNode.java
@@ -87,14 +87,6 @@ public abstract class EspressoRootNode extends RootNode implements ContextAccess
         return getMethodNode() instanceof BytecodeNode;
     }
 
-    public final BytecodeNode getBytecodeNode() {
-        if (isBytecodeNode()) {
-            return (BytecodeNode) getMethodNode();
-        } else {
-            return null;
-        }
-    }
-
     private EspressoMethodNode getMethodNode() {
         Node child = methodNode;
         if (child instanceof WrapperNode) {
@@ -133,11 +125,7 @@ public abstract class EspressoRootNode extends RootNode implements ContextAccess
             // monitor accesses until Espresso has its own monitor handling.
             //
             // synchronized (monitor) {
-            if (isBytecodeNode()) {
-                ((BytecodeNode) getMethodNode()).synchronizedMethodMonitorEnter(frame, monitor);
-            } else {
-                InterpreterToVM.monitorEnter(monitor);
-            }
+            InterpreterToVM.monitorEnter(monitor);
             Object result;
             try {
                 result = methodNode.execute(frame);
@@ -145,15 +133,6 @@ public abstract class EspressoRootNode extends RootNode implements ContextAccess
                 InterpreterToVM.monitorExit(monitor);
             }
             return result;
-        }
-
-        private EspressoMethodNode getMethodNode() {
-            Node child = methodNode;
-            if (child instanceof WrapperNode) {
-                child = ((WrapperNode) child).getDelegateNode();
-            }
-            assert !(child instanceof WrapperNode);
-            return (EspressoMethodNode) child;
         }
     }
 
