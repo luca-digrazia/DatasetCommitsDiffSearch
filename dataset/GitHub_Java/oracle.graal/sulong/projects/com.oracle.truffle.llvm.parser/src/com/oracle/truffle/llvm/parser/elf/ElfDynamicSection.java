3f126485647ab26f8cc1d94d563bb3c06149b938
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:34:07 +0200

    Split multiple paths when parsing RPATH/RUNPATH in ELF loader.

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/elf/ElfDynamicSection.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/elf/ElfDynamicSection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018, Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2019, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -33,6 +33,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.graalvm.polyglot.io.ByteSequence;
 
 public final class ElfDynamicSection {
@@ -114,17 +116,21 @@ public final class ElfDynamicSection {
         return getEntry(DT_NEEDED);
     }
 
-    public List<String> getDTRunPath() {
-        return getEntry(DT_RUNPATH);
+    private static Stream<String> splitPaths(String path) {
+        return Arrays.asList(path.split(":")).stream();
+    }
+
+    public Stream<String> getDTRunPathStream() {
+        return getEntryStream(DT_RUNPATH).flatMap(ElfDynamicSection::splitPaths);
     }
 
     public List<String> getDTRPath() {
-        return getEntry(DT_RPATH);
+        return getEntryStream(DT_RPATH).flatMap(ElfDynamicSection::splitPaths).collect(Collectors.toList());
     }
 
     private static ElfSectionHeaderTable.Entry getDynamiSHEntry(ElfSectionHeaderTable sht) {
         for (ElfSectionHeaderTable.Entry e : sht.getEntries()) {
-            if (e.getName(sht).equals(".dynamic")) {
+            if (".dynamic".equals(e.getName(sht))) {
                 return e;
             }
         }
@@ -132,7 +138,11 @@ public final class ElfDynamicSection {
     }
 
     private List<String> getEntry(int tag) {
-        return Arrays.stream(entries).filter(e -> e.getTag() == tag).map(e -> getString(e.getValue())).collect(Collectors.toList());
+        return getEntryStream(tag).collect(Collectors.toList());
+    }
+
+    private Stream<String> getEntryStream(int tag) {
+        return Arrays.stream(entries).filter(e -> e.getTag() == tag).map(e -> getString(e.getValue()));
     }
 
     private String getString(long offset) {
