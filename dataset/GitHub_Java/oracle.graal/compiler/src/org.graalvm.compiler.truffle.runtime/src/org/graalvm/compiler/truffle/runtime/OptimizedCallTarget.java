9ca02dc61f49d8afa94bb660281be3ea8f2f808a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:26:53 +0200

    Add comment about why transferToInterpreter() is used

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCallTarget.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCallTarget.java
@@ -884,8 +884,7 @@ public abstract class OptimizedCallTarget implements CompilableTruffleAST, RootC
     }
 
     private boolean isValidArgumentProfile(Object[] args) {
-        assert callProfiled;
-        return profiledArgumentTypesAssumption.isValid() && checkProfiledArgumentTypes(args, profiledArgumentTypes);
+        return profiledArgumentTypesAssumption != null && profiledArgumentTypesAssumption.isValid() && checkProfiledArgumentTypes(args, profiledArgumentTypes);
     }
 
     private static boolean checkProfiledArgumentTypes(Object[] args, Class<?>[] types) {
@@ -911,7 +910,6 @@ public abstract class OptimizedCallTarget implements CompilableTruffleAST, RootC
 
     // This should be private but can't be. GR-19397
     public final void stopProfilingArguments() {
-        assert !callProfiled;
         Assumption argumentTypesAssumption = profiledArgumentTypesAssumption;
         if (argumentTypesAssumption != null && argumentTypesAssumption.isValid()) {
             // Argument profiling is not possible for targets of indirect calls.
@@ -925,7 +923,6 @@ public abstract class OptimizedCallTarget implements CompilableTruffleAST, RootC
     // This should be private but can't be. GR-19397
     @ExplodeLoop
     public final void profileArguments(Object[] args) {
-        assert !callProfiled;
         Assumption typesAssumption = profiledArgumentTypesAssumption;
         if (typesAssumption == null) {
             if (CompilerDirectives.inInterpreter()) {
@@ -955,7 +952,6 @@ public abstract class OptimizedCallTarget implements CompilableTruffleAST, RootC
 
     private void initializeProfiledArgumentTypes(Object[] args) {
         CompilerAsserts.neverPartOfCompilation();
-        assert !callProfiled;
         if (args.length <= MAX_PROFILED_ARGUMENTS && engine.argumentTypeSpeculation) {
             Class<?>[] result = new Class<?>[args.length];
             for (int i = 0; i < args.length; i++) {
@@ -970,7 +966,6 @@ public abstract class OptimizedCallTarget implements CompilableTruffleAST, RootC
 
     private void updateProfiledArgumentTypes(Object[] args, Class<?>[] types) {
         CompilerAsserts.neverPartOfCompilation();
-        assert !callProfiled;
         profiledArgumentTypesAssumption.invalidate();
         for (int j = 0; j < types.length; j++) {
             types[j] = joinTypes(types[j], classOf(args[j]));
