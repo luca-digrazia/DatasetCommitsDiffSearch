57c0b1e23b13ae5720b16bdc743e3658acf5dde4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:32:27 +0200

    formatting pass

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/JDWP.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/JDWP.java
@@ -2022,7 +2022,7 @@ final class JDWP {
                     return new CommandResult(reply);
                 }
 
-                Object[] children = context.getChildrenThreads(threadGroup);
+                Object[] children = context.getChildrenThreds(threadGroup);
                 reply.writeInt(children.length);
                 for (Object child : children) {
                     reply.writeLong(context.getIds().getIdAsLong(child));
