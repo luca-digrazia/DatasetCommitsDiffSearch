10c65137386de7247d45f1f5c6f76217777d0c3f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:15:29 +0200

    Wide line formatting a'la Graal.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/BigIntegerIntrinsicsTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/BigIntegerIntrinsicsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,21 +22,24 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 package org.graalvm.compiler.hotspot.test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.math.BigInteger;
 import java.util.Random;
 
 import org.graalvm.compiler.api.test.Graal;
+import org.graalvm.compiler.core.test.GraalCompilerTest;
 import org.graalvm.compiler.hotspot.GraalHotSpotVMConfig;
 import org.graalvm.compiler.hotspot.HotSpotGraalRuntimeProvider;
-import org.graalvm.compiler.replacements.test.MethodSubstitutionTest;
 import org.graalvm.compiler.runtime.RuntimeProvider;
 
 import org.junit.Test;
 
 import jdk.vm.ci.amd64.AMD64;
 import jdk.vm.ci.code.InstalledCode;
+import jdk.vm.ci.code.InvalidInstalledCodeException;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
 
 /*
@@ -51,8 +54,8 @@ import jdk.vm.ci.meta.ResolvedJavaMethod;
  * via BigInteger.multiply() and .modPow(). Note that the actual substitution
  * is not tested per se (only execution based on admissible intrinsics).
  *
- */// @formatter:off
-public final class BigIntegerIntrinsicsTest extends MethodSubstitutionTest {
+ */
+public final class BigIntegerIntrinsicsTest extends GraalCompilerTest {
 
     static final int N = 100;
 
@@ -67,7 +70,7 @@ public final class BigIntegerIntrinsicsTest extends MethodSubstitutionTest {
         Class<?> javaclass = Class.forName("java.math.BigInteger");
 
         TestIntrinsic tin = new TestIntrinsic("testMultiplyAux", javaclass,
-                                              "multiply", BigInteger.class);
+                        "multiply", BigInteger.class);
 
         for (int i = 0; i < N; i++) {
 
@@ -94,14 +97,14 @@ public final class BigIntegerIntrinsicsTest extends MethodSubstitutionTest {
 
         // Intrinsic must be available.
         org.junit.Assume.assumeTrue(config.useMulAddIntrinsic() ||
-                                    config.useSquareToLenIntrinsic());
+                        config.useSquareToLenIntrinsic());
         // Test case is (currently) AMD64 only.
         org.junit.Assume.assumeTrue(getTarget().arch instanceof AMD64);
 
         Class<?> javaclass = Class.forName("java.math.BigInteger");
 
         TestIntrinsic tin = new TestIntrinsic("testMultiplyAux", javaclass,
-                                              "multiply", BigInteger.class);
+                        "multiply", BigInteger.class);
 
         for (int i = 0; i < N; i++) {
 
@@ -127,14 +130,14 @@ public final class BigIntegerIntrinsicsTest extends MethodSubstitutionTest {
 
         // Intrinsic must be available.
         org.junit.Assume.assumeTrue(config.useMontgomeryMultiplyIntrinsic() ||
-                                    config.useMontgomerySquareIntrinsic());
+                        config.useMontgomerySquareIntrinsic());
         // Test case is (currently) AMD64 only.
         org.junit.Assume.assumeTrue(getTarget().arch instanceof AMD64);
 
         Class<?> javaclass = Class.forName("java.math.BigInteger");
 
         TestIntrinsic tin = new TestIntrinsic("testMontgomeryAux", javaclass,
-                                              "modPow", BigInteger.class, BigInteger.class);
+                        "modPow", BigInteger.class, BigInteger.class);
 
         for (int i = 0; i < N; i++) {
 
@@ -149,7 +152,8 @@ public final class BigIntegerIntrinsicsTest extends MethodSubstitutionTest {
 
             assertDeepEquals(res1, res2);
 
-            // Invoke BigInteger testMontgomeryAux(BigInteger, BigExp, BigInteger) through code handle.
+            // Invoke BigInteger testMontgomeryAux(BigInteger, BigExp, BigInteger)
+            // through code handle.
             BigInteger res3 = (BigInteger) tin.invokeCode(big1, bigTwo, big2);
 
             assertDeepEquals(res1, res3);
@@ -167,7 +171,6 @@ public final class BigIntegerIntrinsicsTest extends MethodSubstitutionTest {
     private class TestIntrinsic {
 
         TestIntrinsic(String testmname, Class<?> javaclass, String javamname, Class<?>... params) {
-
             javamethod = getResolvedJavaMethod(javaclass, javamname, params);
             testmethod = getResolvedJavaMethod(testmname);
 
@@ -178,31 +181,46 @@ public final class BigIntegerIntrinsicsTest extends MethodSubstitutionTest {
             testcode = getCode(testmethod);
 
             assert testcode != null;
+            assert testcode.isValid();
         }
 
         Object invokeJava(BigInteger big, Object... args) {
-
             return invokeSafe(javamethod, big, args);
         }
 
         Object invokeTest(Object... args) {
-
             return invokeSafe(testmethod, null, args);
         }
 
         Object invokeCode(Object... args) {
+            try {
+                return testcode.executeVarargs(args);
+            } catch (InvalidInstalledCodeException e) {
+                // Ensure the installed code is valid, possibly recompiled.
+                testcode = getCode(testmethod);
 
-            return executeVarargsSafe(testcode, args);
+                assert testcode != null;
+                assert testcode.isValid();
+
+                return invokeCode(args);
+            }
+        }
+
+        private Object invokeSafe(ResolvedJavaMethod method, Object receiver, Object... args) {
+            try {
+                return invoke(method, receiver, args);
+            } catch (IllegalAccessException | InvocationTargetException | IllegalArgumentException | InstantiationException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         // Private data section:
         private ResolvedJavaMethod javamethod;
         private ResolvedJavaMethod testmethod;
-        private InstalledCode      testcode;
+        private InstalledCode testcode;
     }
 
-    private static GraalHotSpotVMConfig config =
-        ((HotSpotGraalRuntimeProvider) Graal.getRequiredCapability(RuntimeProvider.class)).getVMConfig();
+    private static GraalHotSpotVMConfig config = ((HotSpotGraalRuntimeProvider) Graal.getRequiredCapability(RuntimeProvider.class)).getVMConfig();
 
     private static BigInteger bigTwo = BigInteger.valueOf(2);
     private static Random rnd = new Random(17);
