f92ec5eb51844afd37ff3de9afe97b3592f4d43a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:11:38 +0200

    Update comments about JDK 16 change in `MethodHandleNatives.resolve`

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.methodhandles/src/com/oracle/svm/methodhandles/Target_java_lang_invoke_MethodHandleNatives.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.methodhandles/src/com/oracle/svm/methodhandles/Target_java_lang_invoke_MethodHandleNatives.java
@@ -259,7 +259,6 @@ final class Util_java_lang_invoke_MethodHandleNatives {
         }
     }
 
-    @SuppressWarnings("unused")
     public static Target_java_lang_invoke_MemberName resolve(Target_java_lang_invoke_MemberName self, Class<?> caller, boolean speculativeResolve)
                     throws LinkageError, ClassNotFoundException {
         if (self.reflectAccess != null) {
