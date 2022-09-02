ee886a2c7630cf1315e0862c48e2362b58f0f638
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 13:26:05 +0200

    Add MethodAnnotationProvider interface, and implement it in Truffle.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.compiler.hotspot/src/org/graalvm/compiler/truffle/compiler/hotspot/TruffleMethodAnnotationProvider.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.compiler.hotspot/src/org/graalvm/compiler/truffle/compiler/hotspot/TruffleMethodAnnotationProvider.java
@@ -5,6 +5,7 @@ import org.graalvm.compiler.nodes.spi.MethodAnnotationProvider;
 import org.graalvm.compiler.serviceprovider.ServiceProvider;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 
 @ServiceProvider(MethodAnnotationProvider.class)
 public class TruffleMethodAnnotationProvider implements MethodAnnotationProvider {
@@ -16,6 +17,21 @@ public class TruffleMethodAnnotationProvider implements MethodAnnotationProvider
             return method.isAnnotationPresent((Class<? extends Annotation>) cls);
         } catch (ClassNotFoundException e) {
             return false;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T getAnnotationElement(ResolvedJavaMethod method, String annotationClassName, String elementName, Class<?> elementType) {
+        try {
+            final Class<?> cls = Class.forName(annotationClassName);
+            final Annotation annotation = method.getAnnotation((Class<? extends Annotation>) cls);
+            if (annotation == null) {
+                return null;
+            }
+            return (T) cls.getDeclaredMethod(elementName).invoke(annotation);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            return null;
         }
     }
 }
