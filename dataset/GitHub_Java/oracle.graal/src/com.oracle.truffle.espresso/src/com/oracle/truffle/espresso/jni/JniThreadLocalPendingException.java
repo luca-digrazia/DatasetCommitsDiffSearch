4f93cc3702ec0a4a4f2a978baabaa9fa5f779dad
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:33:26 +0200

    Minor cosmetic fixes.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/JniThreadLocalPendingException.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/JniThreadLocalPendingException.java
@@ -35,11 +35,9 @@ public final class JniThreadLocalPendingException {
     }
 
     public void set(StaticObject t) {
-        // TODO(peterssen): Warn about overwritten pending exceptions.
-        // if (pendingException.get() != null) {
-        // System.err.println("Overwriting pending exception: " + pendingException.get().getKlass() + " with
-        // " + (t == null ? "<null>" : t.getKlass()));
-        // }
+        if (pendingException.get() != null) {
+            System.err.println("Overwriting pending exception: " + pendingException.get().getKlass() + " with " + (t == null ? "<null>" : t.getKlass()));
+        }
         pendingException.set(t);
     }
 
