5a68689ae6417f85a73e15f692249e4df67bc9ae
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:35:00 +0200

    [GR-20821] Transient issue in PolyglotCachingTest.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/PolyglotCachingTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/PolyglotCachingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -123,7 +123,7 @@ public class PolyglotCachingTest {
         c.eval(source);
 
         assertNotNull(innerSource.get());
-        Field f = Source.class.getDeclaredField("receiver");
+        Field f = Source.class.getDeclaredField("impl");
         f.setAccessible(true);
 
         assertEquals(f.get(source), innerSource.get());
