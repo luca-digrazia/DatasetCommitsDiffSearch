d227e236a88463108732081a271cd994002fdefb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 00:09:47 +0200

    Remove tabs.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.graphio/src/org/graalvm/graphio/GraphProtocol.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.graphio/src/org/graalvm/graphio/GraphProtocol.java
@@ -28,7 +28,6 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
 import java.nio.charset.Charset;
@@ -86,13 +85,6 @@ abstract class GraphProtocol<Graph, Node, NodeClass, Edges, Block, ResolvedJavaM
     final int versionMajor;
     final int versionMinor;
     private boolean printing;
-
-    /**
-     * See {@code org.graalvm.compiler.serviceprovider.BufferUtil}.
-     */
-    private static Buffer asBaseBuffer(Buffer obj) {
-        return obj;
-    }
 
     GraphProtocol(WritableByteChannel channel, int major, int minor, boolean embedded) throws IOException {
         if (major > MAJOR_VERSION || (major == MAJOR_VERSION && minor > MINOR_VERSION)) {
@@ -336,7 +328,7 @@ abstract class GraphProtocol<Graph, Node, NodeClass, Edges, Block, ResolvedJavaM
     }
 
     private void flush() throws IOException {
-        asBaseBuffer(buffer).flip();
+        buffer.flip();
         /*
          * Try not to let interrupted threads abort the write. There's still a race here but an
          * interrupt that's been pending for a long time shouldn't stop this writing.
@@ -419,12 +411,12 @@ abstract class GraphProtocol<Graph, Node, NodeClass, Edges, Block, ResolvedJavaM
         while (b.position() < limit) {
             int toWrite = Math.min(limit - b.position(), buffer.capacity());
             ensureAvailable(toWrite);
-            asBaseBuffer(b).limit(b.position() + toWrite);
+            b.limit(b.position() + toWrite);
             try {
                 buffer.put(b);
                 written += toWrite;
             } finally {
-                asBaseBuffer(b).limit(limit);
+                b.limit(limit);
             }
         }
         return written;
@@ -438,7 +430,7 @@ abstract class GraphProtocol<Graph, Node, NodeClass, Edges, Block, ResolvedJavaM
             int sizeInBytes = b.length * 4;
             ensureAvailable(sizeInBytes);
             buffer.asIntBuffer().put(b);
-            asBaseBuffer(buffer).position(buffer.position() + sizeInBytes);
+            buffer.position(buffer.position() + sizeInBytes);
         }
     }
 
@@ -450,7 +442,7 @@ abstract class GraphProtocol<Graph, Node, NodeClass, Edges, Block, ResolvedJavaM
             int sizeInBytes = b.length * 8;
             ensureAvailable(sizeInBytes);
             buffer.asDoubleBuffer().put(b);
-            asBaseBuffer(buffer).position(buffer.position() + sizeInBytes);
+            buffer.position(buffer.position() + sizeInBytes);
         }
     }
 
