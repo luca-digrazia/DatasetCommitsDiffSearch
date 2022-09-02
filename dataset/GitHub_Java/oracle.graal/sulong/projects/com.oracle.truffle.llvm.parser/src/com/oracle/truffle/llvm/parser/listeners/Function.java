a067def1f0303a2da763093de05bdac2d6ebe7b1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:35:15 +0200

    sulong: replace StructureType#create*ByCopy with fixed argument number factories

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/listeners/Function.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/listeners/Function.java
@@ -204,7 +204,6 @@ public final class Function implements ParserListener {
         scope.exitLocalScope();
     }
 
-    @SuppressWarnings("fallthrough")
     @Override
     public void record(RecordBuffer buffer) {
         int opCode = buffer.getId();
