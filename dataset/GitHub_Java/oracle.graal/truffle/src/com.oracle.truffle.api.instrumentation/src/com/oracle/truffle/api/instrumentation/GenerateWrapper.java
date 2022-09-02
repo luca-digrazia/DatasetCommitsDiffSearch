d6c5e9bea59b723e8674a0077bb4235ce16c937d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 15:50:59 +0200

    Formatting.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/GenerateWrapper.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/GenerateWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -93,7 +93,7 @@ import com.oracle.truffle.api.frame.VirtualFrame;
  * </pre>
  *
  * <p>
- * <b>Example that converts incoming byte values to int:</b>
+ * <b>Example that converts incoming byte values to int.</b>
  *
  * <pre>
  * &#64;GenerateWrapper
@@ -112,27 +112,6 @@ import com.oracle.truffle.api.frame.VirtualFrame;
  *             return (int) ((byte) incomingValue);
  *         }
  *         return incomingValue;
- *     }
- * }
- * </pre>
- *
- * <p>
- * <b>Example that prevents instrumentation from being added to a method:</b>
- *
- * <pre>
- * &#64;GenerateWrapper
- * abstract class ExpressionNode extends Node implements InstrumentableNode {
- *
- *     abstract Object execute(VirtualFrame frame);
- *
- *     &#64;Override
- *     public WrapperNode createWrapper(ProbeNode probeNode) {
- *         return new ExpressionNodeWrapper(this, probeNode);
- *     }
- *
- *     &#64;GenerateWrapper.Ignore
- *     public Object executeWithoutInstrumentation(VirtualFrame frame) {
- *         return null;
  *     }
  * }
  * </pre>
@@ -187,17 +166,6 @@ public @interface GenerateWrapper {
     @Retention(RetentionPolicy.CLASS)
     @Target({ElementType.METHOD})
     public @interface OutgoingConverter {
-    }
-
-    /**
-     * Annotates a method which should not be instrumented in the generated wrapper subclass.
-     *
-     * @see GenerateWrapper for usage examples
-     * @since 21.2
-     */
-    @Retention(RetentionPolicy.CLASS)
-    @Target({ElementType.METHOD})
-    public @interface Ignore {
     }
 
 }
