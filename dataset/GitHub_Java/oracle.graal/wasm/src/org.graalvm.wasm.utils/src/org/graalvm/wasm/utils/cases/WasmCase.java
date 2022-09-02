8cde5413394cf3c30a4432e824c83ec7c00e0b1f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:07:55 +0200

    Remove @NotNull

--- dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm.utils/src/org/graalvm/wasm/utils/cases/WasmCase.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm.utils/src/org/graalvm/wasm/utils/cases/WasmCase.java
@@ -1,31 +1,42 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * All rights reserved.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * Redistribution and use in source and binary forms, with or without modification, are
- * permitted provided that the following conditions are met:
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * 1. Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
+ * (a) the Software, and
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
- * conditions and the following disclaimer in the documentation and/or other materials provided
- * with the distribution.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
  *
- * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written
- * permission.
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package org.graalvm.wasm.utils.cases;
 
@@ -36,9 +47,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.function.BiConsumer;
 
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.io.ByteSequence;
 import org.graalvm.wasm.utils.Assert;
 import org.graalvm.wasm.utils.SystemProperties;
 import org.graalvm.wasm.utils.WasmInitialization;
@@ -77,7 +92,17 @@ public abstract class WasmCase {
         return options;
     }
 
-    public abstract byte[] createBinary() throws IOException, InterruptedException;
+    public ArrayList<Source> getSources() throws IOException, InterruptedException {
+        ArrayList<Source> sources = new ArrayList<>();
+        for (Map.Entry<String, byte[]> entry : createBinaries().entrySet()) {
+            Source.Builder sourceBuilder = Source.newBuilder("wasm", ByteSequence.create(entry.getValue()), entry.getKey());
+            Source source = sourceBuilder.build();
+            sources.add(source);
+        }
+        return sources;
+    }
+
+    public abstract Map<String, byte[]> createBinaries() throws IOException, InterruptedException;
 
     public static WasmStringCase create(String name, WasmCaseData data, String program) {
         return new WasmStringCase(name, data, program, null, new Properties());
@@ -107,8 +132,8 @@ public abstract class WasmCase {
         return new WasmCaseData((Value result, String output) -> Assert.assertDoubleEquals("Failure: result:", expectedValue, result.as(Double.class), delta));
     }
 
-    public static WasmCaseData expectedThrows(String expectedErrorMessage) {
-        return new WasmCaseData(expectedErrorMessage);
+    public static WasmCaseData expectedThrows(String expectedErrorMessage, WasmCaseData.ErrorType phase) {
+        return new WasmCaseData(expectedErrorMessage.trim(), phase);
     }
 
     public static Collection<WasmCase> collectFileCases(String type, String resource) throws IOException {
@@ -124,20 +149,35 @@ public abstract class WasmCase {
 
         // Iterate through the available test of the bundle.
         while (indexReader.ready()) {
-            String caseName = indexReader.readLine().trim();
+            String caseSpec = indexReader.readLine().trim();
 
-            if (caseName.equals("") || caseName.startsWith("#")) {
+            if (caseSpec.equals("") || caseSpec.startsWith("#")) {
                 // Skip empty lines or lines starting with a hash (treat as a comment).
                 continue;
+            } else {
+                collectedCases.add(collectFileCase(type, resource, caseSpec));
             }
-            collectedCases.add(collectFileCase(type, resource, caseName));
         }
 
         return collectedCases;
     }
 
-    public static WasmCase collectFileCase(String type, String resource, String caseName) throws IOException {
-        Object mainContent = WasmResource.getResourceAsTest(String.format("/%s/%s/%s", type, resource, caseName), true);
+    public static WasmCase collectFileCase(String type, String resource, String caseSpec) throws IOException {
+        Map<String, Object> mainContents = new LinkedHashMap<>();
+        String caseName;
+        if (caseSpec.contains("/")) {
+            // Collect multi-module test case.
+            final String[] dirFiles = caseSpec.split("/");
+            final String dir = dirFiles[0];
+            final String[] moduleFiles = dirFiles[1].split(";");
+            for (String file : moduleFiles) {
+                mainContents.put(file, WasmResource.getResourceAsTest(String.format("/%s/%s/%s/%s", type, resource, dir, file), true));
+            }
+            caseName = dir;
+        } else {
+            mainContents.put(caseSpec, WasmResource.getResourceAsTest(String.format("/%s/%s/%s", type, resource, caseSpec), true));
+            caseName = caseSpec;
+        }
         String resultContent = WasmResource.getResourceAsString(String.format("/%s/%s/%s.result", type, resource, caseName), true);
         String initContent = WasmResource.getResourceAsString(String.format("/%s/%s/%s.init", type, resource, caseName), false);
         String optsContent = WasmResource.getResourceAsString(String.format("/%s/%s/%s.opts", type, resource, caseName), false);
@@ -165,22 +205,42 @@ public abstract class WasmCase {
             case "double":
                 caseData = WasmCase.expected(Double.parseDouble(resultValue.trim()));
                 break;
+            case "validation":
+                caseData = WasmCase.expectedThrows(resultValue, WasmCaseData.ErrorType.Validation);
+                break;
             case "exception":
-                caseData = WasmCase.expectedThrows(resultValue);
+                caseData = WasmCase.expectedThrows(resultValue, WasmCaseData.ErrorType.Runtime);
                 break;
             default:
                 Assert.fail(String.format("Unknown type in result specification: %s", resultType));
         }
 
-        if (mainContent instanceof String) {
-            return WasmCase.create(caseName, caseData, (String) mainContent, initializer, options);
-        } else if (mainContent instanceof byte[]) {
-            return WasmCase.create(caseName, caseData, (byte[]) mainContent, initializer, options);
-        } else {
-            Assert.fail("Unknown content type: " + mainContent.getClass());
+        if (mainContents.size() == 1) {
+            Object content = mainContents.values().iterator().next();
+            if (content instanceof String) {
+                return WasmCase.create(caseName, caseData, (String) content, initializer, options);
+            } else if (content instanceof byte[]) {
+                return WasmCase.create(caseName, caseData, (byte[]) content, initializer, options);
+            } else {
+                Assert.fail("Unknown content type: " + content.getClass());
+            }
+        } else if (mainContents.size() > 1) {
+            return new WasmMultiCase(caseName, caseData, mainContents, initializer, options);
         }
 
         return null;
+    }
+
+    public static WasmCase loadBenchmarkCase(String resource) throws IOException {
+        final String name = SystemProperties.BENCHMARK_NAME;
+
+        Assert.assertNotNull("Please select a benchmark by setting -D" + SystemProperties.BENCHMARK_NAME_PROPERTY_NAME, name);
+        Assert.assertTrue("Benchmark name must not be empty", !name.trim().isEmpty());
+
+        final WasmCase result = WasmCase.collectFileCase("bench", resource, name);
+        Assert.assertNotNull(String.format("Benchmark %s.%s not found", name, name), result);
+
+        return result;
     }
 
     public static void validateResult(BiConsumer<Value, String> validator, Value result, OutputStream capturedStdout) {
