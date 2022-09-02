f669259c4eaa7e36ff1e9d3925008c34de169c53
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:24:22 +0200

    remove unused import

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/headers/ZLib.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/headers/ZLib.java
@@ -25,13 +25,11 @@
 package com.oracle.svm.core.posix.headers;
 
 import org.graalvm.nativeimage.PinnedObject;
-import org.graalvm.nativeimage.Platform;
-import org.graalvm.nativeimage.Platforms;
 import org.graalvm.nativeimage.c.CContext;
 import org.graalvm.nativeimage.c.constant.CConstant;
 import org.graalvm.nativeimage.c.function.CFunction;
-import org.graalvm.nativeimage.c.function.CFunctionPointer;
 import org.graalvm.nativeimage.c.function.CLibrary;
+import org.graalvm.nativeimage.c.function.CFunctionPointer;
 import org.graalvm.nativeimage.c.function.InvokeCFunctionPointer;
 import org.graalvm.nativeimage.c.struct.CField;
 import org.graalvm.nativeimage.c.struct.CStruct;
@@ -39,6 +37,9 @@ import org.graalvm.nativeimage.c.struct.SizeOf;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CLongPointer;
 import org.graalvm.nativeimage.c.type.WordPointer;
+import org.graalvm.nativeimage.Platforms;
+import org.graalvm.nativeimage.Platform.DARWIN_AND_JNI;
+import org.graalvm.nativeimage.Platform.LINUX_AND_JNI;
 import org.graalvm.word.PointerBase;
 import org.graalvm.word.UnsignedWord;
 
@@ -49,7 +50,7 @@ import org.graalvm.word.UnsignedWord;
  *
  * We only include this class in the JNI implementation in order to add -lz to the link line.
  */
-@Platforms({Platform.DARWIN.class, Platform.LINUX.class})
+@Platforms({DARWIN_AND_JNI.class, LINUX_AND_JNI.class})
 @CContext(PosixDirectives.class)
 @CLibrary("z")
 public class ZLib {
