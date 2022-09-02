99501920831e999fefb5f8e810919b56015b82ce
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:20:36 +0200

    Disable inlining if engine mode is "latency".

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.compiler/src/org/graalvm/compiler/truffle/compiler/phases/inlining/AgnosticInliningPhase.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.compiler/src/org/graalvm/compiler/truffle/compiler/phases/inlining/AgnosticInliningPhase.java
@@ -81,7 +81,6 @@ public final class AgnosticInliningPhase extends BasePhase<CoreProviders> {
             policy.run(tree);
             tree.dumpBasic("After Inline");
             tree.collectTargetsToDequeue(request.inliningPlan);
-            tree.updateTracingInfo(request.inliningPlan);
         }
         tree.finalizeGraph();
         tree.trace();
