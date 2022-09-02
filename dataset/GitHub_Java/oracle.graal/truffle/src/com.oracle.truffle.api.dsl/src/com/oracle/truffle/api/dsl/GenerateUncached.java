d749f166c1f5a899e4fb70e4b3ed2957c0306ad7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 23:26:07 +0200

    GenerateUncached cannot inhert by default for compatibility.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.dsl/src/com/oracle/truffle/api/dsl/GenerateUncached.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.dsl/src/com/oracle/truffle/api/dsl/GenerateUncached.java
@@ -116,7 +116,7 @@ public @interface GenerateUncached {
     /**
      * Inherits the semantics of the annotation to subclasses.
      *
-     * @since 19.1.0
+     * @since 20.0.0 beta 1
      */
     boolean inherit() default false;
 
