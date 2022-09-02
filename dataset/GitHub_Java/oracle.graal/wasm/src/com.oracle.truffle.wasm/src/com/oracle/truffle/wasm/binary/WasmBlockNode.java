b29903dd8fad5679ce6c32b89f4d35c10165a955
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 23:44:21 +0200

    Add I32_SUB support.

--- dataset/GitHub_Java/oracle.graal/wasm/src/com.oracle.truffle.wasm/src/com/oracle/truffle/wasm/binary/WasmBlockNode.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/com.oracle.truffle.wasm/src/com/oracle/truffle/wasm/binary/WasmBlockNode.java
@@ -74,7 +74,7 @@ public class WasmBlockNode extends WasmNode {
                     break;
                 }
                 case I32_CONST: {
-                    int value = BinaryStreamReader.peekSignedInt32(codeEntry().data(), offset, null);
+                    int value = BinaryStreamReader.peek1(codeEntry().data(), offset);
                     byte constantLength = constantLengthTable[constantOffset];
                     constantOffset++;
                     offset += constantLength;
