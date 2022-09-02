9501660415da6638657c1caef99a8f0da42d3b8b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:58:57 +0200

    Checkstyle.

--- dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.coverage.test/src/com/oracle/truffle/tools/coverage/test/CoverageTest.java
+++ dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.coverage.test/src/com/oracle/truffle/tools/coverage/test/CoverageTest.java
@@ -25,16 +25,12 @@
 package com.oracle.truffle.tools.coverage.test;
 
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
-import org.graalvm.polyglot.Value;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -82,11 +78,7 @@ public final class CoverageTest {
     }
 
     private static SourceCoverage[] execInServiceAndGetCoverage(Context context, ExecutorService executorService, CoverageTracker tracker) throws InterruptedException, ExecutionException {
-        List<Future<Value>> tasks = new ArrayList<>(2);
-        tasks.add(executorService.submit(() -> context.eval(defaultSource)));
-        for (Future<Value> task : tasks) {
-            task.get();
-        }
+        executorService.submit(() -> context.eval(defaultSource)).get();
         return tracker.getCoverage();
     }
 
@@ -126,7 +118,7 @@ public final class CoverageTest {
     @Test
     public void testMultiThreaded() throws InterruptedException, ExecutionException {
         try (Context context = Context.newBuilder().in(System.in).out(out).err(err).build()) {
-            ExecutorService executorService = Executors.newFixedThreadPool(2);
+            ExecutorService executorService = Executors.newFixedThreadPool(1);
             final CoverageTracker tracker = CoverageInstrument.getTracker(context.getEngine());
             // Is the coverage empty when not tracking?
             Assert.assertEquals(0, execInServiceAndGetCoverage(context, executorService, tracker).length);
