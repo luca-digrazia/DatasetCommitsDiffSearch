c36cfd683ec649dc6137eea21225a5e8103049e4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 00:18:11 +0200

    Fix file separators in various reports

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/reports/ObjectTreePrinter.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/reports/ObjectTreePrinter.java
@@ -287,7 +287,7 @@ public final class ObjectTreePrinter extends ObjectScanner {
     private final SimpleMatcher defaultSuppressRootMatcher;
 
     private ObjectTreePrinter(BigBang bigbang) {
-        super(bigbang, new ReusableSet());
+        super(bigbang);
 
         /* Use linked hash map for predictable iteration order. */
         this.constantToNode = new LinkedHashMap<>();
