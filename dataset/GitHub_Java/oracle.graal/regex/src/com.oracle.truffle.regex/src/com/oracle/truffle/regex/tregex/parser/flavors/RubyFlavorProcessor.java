0e6630cabf024a123c226bc5b5851a09a6ba40d5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 13:21:53 +0200

    Ruby flavor: Refactoring, handling lots more corner cases
        * Backreferences are now disabled in lookbehind.
        * We do a first pass to identify capture groups.
          * This lets us parse forward references.
          * Also, it sort out issues with named capture groups disabling unnamed
            capture groups.
        * \P negative Unicode properties are now supported

--- dataset/GitHub_Java/oracle.graal/regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/parser/flavors/RubyFlavorProcessor.java
+++ dataset/GitHub_Java/oracle.graal/regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/parser/flavors/RubyFlavorProcessor.java
@@ -1125,7 +1125,7 @@ public final class RubyFlavorProcessor implements RegexFlavorProcessor {
      */
     private boolean stringEscape() {
         if (match("u{")) {
-            getMany(c ->  ASCII_POSIX_CHAR_CLASSES.get("space").contains(c));
+            getMany(c -> WHITESPACE.get(c));
             while (!match("}")) {
                 String code = getMany(RubyFlavorProcessor::isHexDigit);
                 try {
@@ -1230,12 +1230,8 @@ public final class RubyFlavorProcessor implements RegexFlavorProcessor {
             default:
                 if (isOctDigit(ch)) {
                     retreat();
-                    String code = getUpTo(3, RubyFlavorProcessor::isOctDigit);
-                    int codePoint = Integer.parseInt(code, 8);
-                    if (codePoint > 0xFF) {
-                        syntaxError("too big number");
-                    }
-                    return codePoint;
+                    String code = getUpTo(curChar() == '0' ? 2 : 3, RubyFlavorProcessor::isOctDigit);
+                    return Integer.parseInt(code, 8);
                 } else {
                     return ch;
                 }
