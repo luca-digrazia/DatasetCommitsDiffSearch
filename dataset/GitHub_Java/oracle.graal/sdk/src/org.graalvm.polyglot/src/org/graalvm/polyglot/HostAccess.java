f4a14cc555db9cb9831d00a7ce56df31663a8112
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 00:45:16 +0200

    Documenting the new API elements

--- dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/HostAccess.java
+++ dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/HostAccess.java
@@ -42,7 +42,6 @@ package org.graalvm.polyglot;
 
 import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
