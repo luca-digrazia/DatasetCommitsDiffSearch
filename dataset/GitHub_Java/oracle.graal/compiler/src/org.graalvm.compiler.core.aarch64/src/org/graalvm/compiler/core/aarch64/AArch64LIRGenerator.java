2a8b072c90e904133e2c259134947fe5b765b3cc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:44:04 +0200

    Touching up AArch64ArrayIndexOfOp.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.aarch64/src/org/graalvm/compiler/core/aarch64/AArch64LIRGenerator.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.aarch64/src/org/graalvm/compiler/core/aarch64/AArch64LIRGenerator.java
@@ -55,8 +55,6 @@ import org.graalvm.compiler.lir.aarch64.AArch64AtomicMove.AtomicReadAndAddOp;
 import org.graalvm.compiler.lir.aarch64.AArch64AtomicMove.AtomicReadAndWriteOp;
 import org.graalvm.compiler.lir.aarch64.AArch64AtomicMove.CompareAndSwapOp;
 import org.graalvm.compiler.lir.aarch64.AArch64ByteSwapOp;
-import org.graalvm.compiler.lir.aarch64.AArch64CacheWritebackOp;
-import org.graalvm.compiler.lir.aarch64.AArch64CacheWritebackPostSyncOp;
 import org.graalvm.compiler.lir.aarch64.AArch64Compare;
 import org.graalvm.compiler.lir.aarch64.AArch64ControlFlow;
 import org.graalvm.compiler.lir.aarch64.AArch64ControlFlow.BranchOp;
@@ -642,19 +640,6 @@ public abstract class AArch64LIRGenerator extends LIRGenerator {
     @Override
     public void emitPause() {
         append(new AArch64PauseOp());
-    }
-
-    @Override
-    public void emitCacheWriteback(Value address) {
-        append(new AArch64CacheWritebackOp(asAddressValue(address)));
-    }
-
-    @Override
-    public void emitCacheWritebackSync(boolean isPreSync) {
-        // only need a barrier post sync is required on AArch64
-        if (!isPreSync) {
-            append(new AArch64CacheWritebackPostSyncOp());
-        }
     }
 
     public abstract void emitCCall(long address, CallingConvention nativeCallingConvention, Value[] args);
