969d7189ae3a2ee6211f259ee03bee78af828680
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 15:12:28 +0200

    Simplify LLVMTypedForeignObject.

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/LLVMTypedForeignObject.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/LLVMTypedForeignObject.java
@@ -39,7 +39,7 @@ import com.oracle.truffle.llvm.spi.NativeTypeLibrary;
 
 @ValueType
 @ExportLibrary(value = InteropLibrary.class, delegateTo = "foreign")
-@ExportLibrary(value = NativeTypeLibrary.class, useForAOT = false)
+@ExportLibrary(NativeTypeLibrary.class)
 @ExportLibrary(LLVMAsForeignLibrary.class)
 public final class LLVMTypedForeignObject extends LLVMInternalTruffleObject {
 
