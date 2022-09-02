986944fce5d08c7250f8977439633791de20309f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:36:24 +0200

    formatting

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoOptions.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoOptions.java
@@ -251,7 +251,7 @@ public final class EspressoOptions {
 
                         private boolean yesOrNo(String key, String value) {
                             if (!"y".equals(value) && !"n".equals(value)) {
-                                throw new IllegalArgumentException("Invalid option value: -Xrunjwdp:" + key + " can be only 'y' or 'n'.");
+                                throw new IllegalArgumentException("Invalid JDWP option value: " + key + " can be only 'y' or 'n'.");
                             }
                             return "y".equals(value);
                         }
@@ -260,7 +260,8 @@ public final class EspressoOptions {
                         public JDWPOptions apply(String s) {
                             final String[] options = s.split(",");
                             String transport = null;
-                            String address = null;
+                            String host = null;
+                            String port = null;
                             String logLevel = null;
                             boolean server = false;
                             boolean suspend = true;
@@ -268,22 +269,34 @@ public final class EspressoOptions {
                             for (String keyValue : options) {
                                 String[] parts = keyValue.split("=");
                                 if (parts.length != 2) {
-                                    throw new IllegalArgumentException("-Xrunjdwp: options must be a comma separated list of key=value pairs.");
+                                    throw new IllegalArgumentException("JDWP options must be a comma separated list of key=value pairs.");
                                 }
                                 String key = parts[0];
                                 String value = parts[1];
                                 switch (key) {
                                     case "address":
+                                        parts = value.split(":");
+                                        String inputHost = null;
+                                        String inputPort;
+                                        if (parts.length == 1) {
+                                            inputPort = parts[0];
+                                        } else if (parts.length == 2) {
+                                            inputHost = parts[0];
+                                            inputPort = parts[1];
+                                        } else {
+                                            throw new IllegalArgumentException("Invalid JDWP option, address: " + value + ". Not a 'host:port' pair.");
+                                        }
                                         long realValue;
                                         try {
-                                            realValue = Long.valueOf(value);
+                                            realValue = Long.valueOf(inputPort);
                                             if (realValue < 0 || realValue > 65535) {
-                                                throw new IllegalArgumentException("Invalid option for -Xrunjdwp, address: " + value + ". Must be in the 0 - 65535 range.");
+                                                throw new IllegalArgumentException("Invalid JDWP option, address: " + value + ". Must be in the 0 - 65535 range.");
                                             }
                                         } catch (NumberFormatException ex) {
-                                            throw new IllegalArgumentException("Invalid option for -Xrunjdwp, address is not a number. Must be a number in the 0 - 65535 range.");
+                                            throw new IllegalArgumentException("Invalid JDWP option, address is not a number. Must be a number in the 0 - 65535 range.");
                                         }
-                                        address = value;
+                                        host = inputHost;
+                                        port = inputPort;
                                         break;
                                     case "transport":
                                         transport = value;
@@ -298,79 +311,16 @@ public final class EspressoOptions {
                                         logLevel = value;
                                         break;
                                     default:
-                                        throw new IllegalArgumentException("Invalid option -Xrunjdwp:" + key + ". Supported options: 'transport', 'address', 'server' and 'suspend'.");
+                                        throw new IllegalArgumentException("Invalid JDWP option: " + key + ". Supported options: 'transport', 'address', 'server' and 'suspend'.");
                                 }
                             }
-                            return new JDWPOptions(transport, address, server, suspend, logLevel);
+                            return new JDWPOptions(transport, host, port, server, suspend, logLevel);
                         }
                     });
 
-    private static final OptionType<com.oracle.truffle.espresso.jdwp.api.JDWPOptions> JDWP_OPTIONS_OPTION_AGENT_TYPE = new OptionType<>("JDWPOptions",
-            new Function<String, JDWPOptions>() {
-
-                private boolean yesOrNo(String key, String value) {
-                    if (!"y".equals(value) && !"n".equals(value)) {
-                        throw new IllegalArgumentException("Invalid option value: -agentlib:jdwp " + key + " can be only 'y' or 'n'.");
-                    }
-                    return "y".equals(value);
-                }
-
-                @Override
-                public JDWPOptions apply(String s) {
-                    final String[] options = s.split(",");
-                    String transport = null;
-                    String address = null;
-                    String logLevel = null;
-                    boolean server = false;
-                    boolean suspend = true;
-
-                    for (String keyValue : options) {
-                        String[] parts = keyValue.split("=");
-                        if (parts.length != 2) {
-                            throw new IllegalArgumentException("-agentlib:jdwp options must be a comma separated list of key=value pairs.");
-                        }
-                        String key = parts[0];
-                        String value = parts[1];
-                        switch (key) {
-                            case "address":
-                                long realValue;
-                                try {
-                                    realValue = Long.valueOf(value);
-                                    if (realValue < 0 || realValue > 65535) {
-                                        throw new IllegalArgumentException("Invalid option for -agentlib:jwdp, address: " + value + ". Must be in the 0 - 65535 range.");
-                                    }
-                                } catch (NumberFormatException ex) {
-                                    throw new IllegalArgumentException("Invalid option for -agentlib:jwdp, address is not a number. Must be a number in the 0 - 65535 range.");
-                                }
-                                address = value;
-                                break;
-                            case "transport":
-                                transport = value;
-                                break;
-                            case "server":
-                                server = yesOrNo(key, value);
-                                break;
-                            case "suspend":
-                                suspend = yesOrNo(key, value);
-                                break;
-                            case "logLevel":
-                                logLevel = value;
-                                break;
-                            default:
-                                throw new IllegalArgumentException("Invalid option -agentlib:jwdp " + key + ". Supported options: 'transport', 'address', 'server' and 'suspend'.");
-                        }
-                    }
-                    return new JDWPOptions(transport, address, server, suspend, logLevel);
-                }
-            });
-
-    @Option(help = "JDWP Options. e.g. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=y", //
-                    category = OptionCategory.EXPERT, stability = OptionStability.STABLE) //
-    public static final OptionKey<JDWPOptions> JDWPRunOptions = new OptionKey<>(null, JDWP_OPTIONS_OPTION_TYPE);
-
     @Option(help = "JDWP agent Options. e.g. -agentlib:jdwp=transport=dt_socket,server=y,address=localhost:8000,suspend=y", //
-            category = OptionCategory.EXPERT, stability = OptionStability.STABLE) //
-    public static final OptionKey<JDWPOptions> JDWPAgentOptions = new OptionKey<>(null, JDWP_OPTIONS_OPTION_AGENT_TYPE);
+                    category = OptionCategory.EXPERT, stability = OptionStability.STABLE) //
+    public static final OptionKey<JDWPOptions> JDWPOptions = new OptionKey<>(null, JDWP_OPTIONS_OPTION_TYPE);
 
     @Option(help = "Enable experimental java.lang.management APIs. Incur a bookkeeping overhead.", //
                     category = OptionCategory.EXPERT, stability = OptionStability.EXPERIMENTAL) //
@@ -390,10 +340,6 @@ public final class EspressoOptions {
     @Option(help = "Enables espresso runtime timers.", //
                     category = OptionCategory.INTERNAL, stability = OptionStability.STABLE) //
     public static final OptionKey<Boolean> EnableTimers = new OptionKey<>(false);
-
-    @Option(help = "Enable polyglot support in Espresso.", //
-                    category = OptionCategory.EXPERT, stability = OptionStability.EXPERIMENTAL) //
-    public static final OptionKey<Boolean> Polyglot = new OptionKey<>(true);
 
     public static final String INCEPTION_NAME = System.getProperty("espresso.inception.name", "#");
 }
