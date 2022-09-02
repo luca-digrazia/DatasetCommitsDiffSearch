13443265e2290051a956efa6c88050ca11c4e29a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:21:28 +0200

    enter the Truffle context when needed from jdwp thread and remove the hack in EspressoLanguage

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoLanguage.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoLanguage.java
@@ -196,9 +196,6 @@ public final class EspressoLanguage extends TruffleLanguage<EspressoContext> {
         } else {
             System.out.println("Time spent in Espresso: " + (totalTime) + "ms");
         }
-
-        context.prepareDispose();
-
         // Shutdown.shutdown creates a Cleaner thread. At this point, Polyglot doesn't allow new
         // threads. We must perform shutdown before then, after main has finished.
         context.interruptActiveThreads();
