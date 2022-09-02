8e9c8dd0ec1bd82373e948271a96eb91079cfc5a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:47:25 +0200

    Add metaobject to host language tck provider.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.tck.tests/src/com/oracle/truffle/tck/tests/JavaHostLanguageProvider.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.tck.tests/src/com/oracle/truffle/tck/tests/JavaHostLanguageProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -40,6 +40,25 @@
  */
 package com.oracle.truffle.tck.tests;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
@@ -53,28 +72,6 @@ import org.graalvm.polyglot.proxy.ProxyTimeZone;
 import org.graalvm.polyglot.tck.LanguageProvider;
 import org.graalvm.polyglot.tck.Snippet;
 import org.graalvm.polyglot.tck.TypeDescriptor;
-
-import java.nio.ByteBuffer;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public final class JavaHostLanguageProvider implements LanguageProvider {
     private static final String ID = "java-host";
@@ -137,7 +134,6 @@ public final class JavaHostLanguageProvider implements LanguageProvider {
         for (Primitive primitive : primitives.values()) {
             result.add(createPrimitive(context, primitive));
         }
-
         // Arrays
         result.add(Snippet.newBuilder("Array<int>", export(context, new ValueSupplier<>(new int[]{1, 2})),
                         TypeDescriptor.array(TypeDescriptor.NUMBER)).build());
@@ -148,50 +144,6 @@ public final class JavaHostLanguageProvider implements LanguageProvider {
         for (Primitive primitive : primitives.values()) {
             result.add(createProxyArray(context, primitive));
         }
-// TODO: Enable when guest language TCK tests are updated to support Iterators and Iterables
-// // Iterables
-// result.add(Snippet.newBuilder("Iterable<int>", export(context, new ValueSupplier<>(new
-// IterableImpl(1, 2))),
-// TypeDescriptor.iterable(TypeDescriptor.NUMBER)).build());
-//
-// result.add(Snippet.newBuilder("Iterable<java.lang.Object>", export(context, new
-// ValueSupplier<>(new IterableImpl(1, "TEST"))),
-// TypeDescriptor.iterable(TypeDescriptor.union(TypeDescriptor.NUMBER,
-// TypeDescriptor.STRING))).build());
-//
-// // Iterable Proxies
-// result.add(Snippet.newBuilder("Proxy<Iterable<int>>", export(context, new
-// ValueSupplier<>(ProxyArrayIterable.from(new IterableImpl(1, 2)))),
-// TypeDescriptor.iterable(TypeDescriptor.NUMBER)).build());
-//
-// result.add(Snippet.newBuilder("Proxy<Iterable<java.lang.Object>>", export(context, new
-// ValueSupplier<>(ProxyArrayIterable.from(new IterableImpl(1, "TEST")))),
-// TypeDescriptor.iterable(TypeDescriptor.union(TypeDescriptor.NUMBER,
-// TypeDescriptor.STRING))).build());
-//
-// // Iterators
-// result.add(Snippet.newBuilder("Iterator<int>", export(context, new ValueSupplier<>(new
-// IteratorImpl(1, 2))),
-// TypeDescriptor.iterator(TypeDescriptor.NUMBER)).build());
-//
-// result.add(Snippet.newBuilder("Iterator<java.lang.Object>", export(context, new
-// ValueSupplier<>(new IteratorImpl(1, "TEST"))),
-// TypeDescriptor.iterator(TypeDescriptor.union(TypeDescriptor.NUMBER,
-// TypeDescriptor.STRING))).build());
-//
-// // Iterator Proxies
-// result.add(Snippet.newBuilder("Proxy<Iterator<int>>", export(context, new
-// ValueSupplier<>(ProxyIterator.from(new IteratorImpl(1, 2)))),
-// TypeDescriptor.iterator(TypeDescriptor.NUMBER)).build());
-//
-// result.add(Snippet.newBuilder("Proxy<Iterator<java.lang.Object>>", export(context, new
-// ValueSupplier<>(ProxyIterator.from(new IteratorImpl(1, "TEST")))),
-// TypeDescriptor.iterator(TypeDescriptor.union(TypeDescriptor.NUMBER,
-// TypeDescriptor.STRING))).build());
-
-        // Buffers
-        result.add(Snippet.newBuilder("HeapByteBuffer", export(context, new ValueSupplier<>(ByteBuffer.wrap(new byte[]{1, 2, 3}))), TypeDescriptor.OBJECT).build());
-        result.add(Snippet.newBuilder("HeapByteBufferR", export(context, new ValueSupplier<>(ByteBuffer.wrap(new byte[]{1, 2, 3}).asReadOnlyBuffer())), TypeDescriptor.OBJECT).build());
 
         // Object Proxies
         result.add(Snippet.newBuilder("Proxy<java.lang.Object{}>", export(context, new ValueSupplier<>(ProxyObject.fromMap(Collections.emptyMap()))), TypeDescriptor.OBJECT).build());
@@ -441,43 +393,6 @@ public final class JavaHostLanguageProvider implements LanguageProvider {
 
         public double abs() {
             return Math.sqrt(real * real + imag * imag);
-        }
-    }
-
-    public static final class IteratorImpl implements Iterator<Object> {
-
-        private final Object[] values;
-        private int index;
-
-        IteratorImpl(Object... values) {
-            this.values = values;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return index < values.length;
-        }
-
-        @Override
-        public Object next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return values[index++];
-        }
-    }
-
-    public static final class IterableImpl implements Iterable<Object> {
-
-        private final Object[] values;
-
-        IterableImpl(Object... values) {
-            this.values = values;
-        }
-
-        @Override
-        public Iterator<Object> iterator() {
-            return new IteratorImpl(values);
         }
     }
 }
