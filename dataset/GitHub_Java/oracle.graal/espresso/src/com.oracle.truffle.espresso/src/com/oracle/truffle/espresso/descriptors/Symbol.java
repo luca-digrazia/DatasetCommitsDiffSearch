c8d89402bd314215dbf055c6c5ef737641fe221e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:42:12 +0200

    Relocate Thread hidden fields.

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/descriptors/Symbol.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/descriptors/Symbol.java
@@ -208,10 +208,8 @@ public final class Symbol<T> extends ByteSequence {
         public static final Symbol<Name> getTypeName = StaticSymbols.putName("getTypeName");
         public static final Symbol<Name> forName = StaticSymbols.putName("forName");
         public static final Symbol<Name> module = StaticSymbols.putName("module");
-        public static final Symbol<Name> classData = StaticSymbols.putName("classData");
         public static final Symbol<Name> classLoader = StaticSymbols.putName("classLoader");
         public static final Symbol<Name> classRedefinedCount = StaticSymbols.putName("classRedefinedCount");
-        public static final Symbol<Name> componentType = StaticSymbols.putName("componentType");
 
         // j.l.ClassLoader
         public static final Symbol<Name> addClass = StaticSymbols.putName("addClass");
@@ -225,11 +223,6 @@ public final class Symbol<T> extends ByteSequence {
 
         // j.l.Module
         public static final Symbol<Name> loader = StaticSymbols.putName("loader");
-
-        // j.l.RecordComponent
-        public static final Symbol<Name> accessor = StaticSymbols.putName("accessor");
-        public static final Symbol<Name> annotations = StaticSymbols.putName("annotations");
-        public static final Symbol<Name> typeAnnotations = StaticSymbols.putName("typeAnnotations");
 
         // j.l.String
         public static final Symbol<Name> hash = StaticSymbols.putName("hash");
@@ -387,7 +380,6 @@ public final class Symbol<T> extends ByteSequence {
         public static final Symbol<Name> isPrivileged = StaticSymbols.putName("isPrivileged");
         public static final Symbol<Name> privilegedContext = StaticSymbols.putName("privilegedContext");
         public static final Symbol<Name> doPrivileged = StaticSymbols.putName("doPrivileged");
-        public static final Symbol<Name> executePrivileged = StaticSymbols.putName("executePrivileged");
 
         // jdk.internal.misc.UnsafeConstants
         public static final Symbol<Name> ADDRESS_SIZE0 = StaticSymbols.putName("ADDRESS_SIZE0");
@@ -769,10 +761,6 @@ public final class Symbol<T> extends ByteSequence {
         // Modules
         public static final Symbol<Type> java_lang_Module = StaticSymbols.putType("Ljava/lang/Module;");
 
-        // Record
-        public static final Symbol<Type> java_lang_Record = StaticSymbols.putType("Ljava/lang/Record;");
-        public static final Symbol<Type> java_lang_reflect_RecordComponent = StaticSymbols.putType("Ljava/lang/reflect/RecordComponent;");
-
         // Unsafe Constants (required for 13+)
         public static final Symbol<Type> jdk_internal_misc_UnsafeConstants = StaticSymbols.putType("Ljdk/internal/misc/UnsafeConstants;");
 
@@ -920,19 +908,6 @@ public final class Symbol<T> extends ByteSequence {
 
         public static final Symbol<Signature> java_util_Iterator = StaticSymbols.putSignature(Type.java_util_Iterator);
         public static final Symbol<Signature> java_util_Set = StaticSymbols.putSignature(Type.java_util_Set);
-
-        public static final Symbol<Signature> java_lang_reflect_Method_init_signature = StaticSymbols.putSignature(Type._void,
-                        /* declaringClass */ Type.java_lang_Class,
-                        /* name */ Type.java_lang_String,
-                        /* parameterTypes */ Type.java_lang_Class_array,
-                        /* returnType */ Type.java_lang_Class,
-                        /* checkedExceptions */ Type.java_lang_Class_array,
-                        /* modifiers */ Type._int,
-                        /* slot */ Type._int,
-                        /* signature */ Type.java_lang_String,
-                        /* annotations */ Type._byte_array,
-                        /* parameterAnnotations */ Type._byte_array,
-                        /* annotationDefault */ Type._byte_array);
 
         public static final Symbol<Signature> MethodType_Class_Class = StaticSymbols.putSignature(Type.java_lang_invoke_MethodType, Type.java_lang_Class, Type.java_lang_Class_array);
         public static final Symbol<Signature> MethodType_String_ClassLoader = StaticSymbols.putSignature(Type.java_lang_invoke_MethodType, Type.java_lang_String, Type.java_lang_ClassLoader);
