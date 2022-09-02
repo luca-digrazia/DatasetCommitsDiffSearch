171600abe7a9626ead5f0111a3d83e417e70c79b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 13:23:22 +0200

    Ruby flavor: Respect a, d and u flags when translating character types, POSIX classes and word boundaries

--- dataset/GitHub_Java/oracle.graal/regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/parser/flavors/RubyFlavorProcessor.java
+++ dataset/GitHub_Java/oracle.graal/regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/parser/flavors/RubyFlavorProcessor.java
@@ -732,11 +732,11 @@ public final class RubyFlavorProcessor implements RegexFlavorProcessor {
                 parens();
                 break;
             case '^':
-                emitSnippet("(?:^|(?<=[\\n])(?=.))");
+                emitSnippet("(?:^|(?<=[\\r\\n])(?=.))");
                 lastTerm = TermCategory.Assertion;
                 break;
             case '$':
-                emitSnippet("(?:$|(?=[\\n]))");
+                emitSnippet("(?:$|(?=[\\r\\n]))");
                 lastTerm = TermCategory.Assertion;
                 break;
             default:
