e9fcda7a591ae48d89760fd56d9295575c61daaa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:59:04 +0200

    [GR-31947] PolyglotUncaughtExceptionHandler should not log CancelExecution.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/PolyglotThread.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/PolyglotThread.java
@@ -1,41 +1,74 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * (a) the Software, and
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.oracle.truffle.polyglot;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.RootNode;
+import com.oracle.truffle.polyglot.PolyglotEngineImpl.CancelExecution;
+
 final class PolyglotThread extends Thread {
 
     private final PolyglotLanguageContext languageContext;
 
-    Object context;
+    PolyglotContextImpl context;
+
+    final CallTarget callTarget;
+
+    PolyglotThread(PolyglotLanguageContext languageContext, Runnable runnable, ThreadGroup group, long stackSize) {
+        super(group, runnable, createDefaultName(languageContext), stackSize);
+        this.languageContext = languageContext;
+        setUncaughtExceptionHandler(languageContext.getPolyglotExceptionHandler());
+        this.callTarget = ThreadSpawnRootNode.lookup(languageContext.getLanguageInstance());
+    }
+
+    PolyglotThread(PolyglotLanguageContext languageContext, Runnable runnable, ThreadGroup group) {
+        this(languageContext, runnable, group, 0);
+    }
 
     PolyglotThread(PolyglotLanguageContext languageContext, Runnable runnable) {
-        super(runnable, createDefaultName(languageContext));
-        this.languageContext = languageContext;
-        setUncaughtExceptionHandler(languageContext.uncaughtExceptionHandler);
+        this(languageContext, runnable, null, 0);
     }
 
     private static String createDefaultName(PolyglotLanguageContext creator) {
@@ -48,15 +81,75 @@ final class PolyglotThread extends Thread {
 
     @Override
     public void run() {
-        Object prev = languageContext.enterThread(this);
-        assert prev == null;
-        try {
-            super.run();
-        } finally {
-            languageContext.leaveThread(prev, this);
-        }
+        // always call through a HostToGuestRootNode so that stack/frame
+        // walking can determine in which context the frame was executed
+        callTarget.call(languageContext, this, new PolyglotThreadRunnable() {
+            @Override
+            @TruffleBoundary
+            public void execute() {
+                PolyglotThread.super.run();
+            }
+        });
     }
 
     private static final AtomicInteger THREAD_INIT_NUMBER = new AtomicInteger(0);
 
+    // replacing Runnable with dedicated interface to avoid having Runnable.run() on the fast path,
+    // since SVM will otherwise pull in all Runnable implementations as runtime compiled methods
+    private interface PolyglotThreadRunnable {
+        void execute();
+    }
+
+    static final class ThreadSpawnRootNode extends RootNode {
+
+        ThreadSpawnRootNode(PolyglotLanguageInstance languageInstance) {
+            super(languageInstance.spi);
+        }
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            Object[] args = frame.getArguments();
+            return executeImpl((PolyglotLanguageContext) args[0], (PolyglotThread) args[1], (PolyglotThreadRunnable) args[2]);
+        }
+
+        @TruffleBoundary
+        private static Object executeImpl(PolyglotLanguageContext languageContext, PolyglotThread thread, PolyglotThreadRunnable run) {
+            PolyglotContextImpl prev;
+            try {
+                prev = languageContext.enterThread(thread);
+            } catch (PolyglotEngineException polyglotException) {
+                if (polyglotException.closingContext) {
+                    return null;
+                } else {
+                    throw polyglotException;
+                }
+            }
+            assert prev == null; // is this assertion correct?
+            try {
+                run.execute();
+            } catch (CancelExecution cancel) {
+                if (PolyglotEngineOptions.TriggerUncaughtExceptionHandlerForCancel.getValue(languageContext.context.engine.getEngineOptionValues())) {
+                    throw cancel;
+                } else {
+                    return null;
+                }
+            } finally {
+                languageContext.leaveAndDisposePolyglotThread(prev, thread);
+            }
+            return null;
+        }
+
+        @Override
+        public boolean isInternal() {
+            return true;
+        }
+
+        public static CallTarget lookup(PolyglotLanguageInstance languageInstance) {
+            CallTarget target = languageInstance.lookupCallTarget(ThreadSpawnRootNode.class);
+            if (target == null) {
+                target = languageInstance.installCallTarget(new ThreadSpawnRootNode(languageInstance));
+            }
+            return target;
+        }
+    }
 }
