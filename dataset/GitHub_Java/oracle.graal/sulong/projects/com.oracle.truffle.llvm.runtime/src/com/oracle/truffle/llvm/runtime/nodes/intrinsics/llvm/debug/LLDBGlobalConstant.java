1a2cacecf132bd05ee2e5f8e0178ce2ee5750f93
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:31:06 +0200

    Remove the static method to regular method, and use class field instead of passing it.

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/llvm/debug/LLDBGlobalConstant.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/llvm/debug/LLDBGlobalConstant.java
@@ -165,13 +165,13 @@ final class LLDBGlobalConstant implements LLVMDebugValue {
 
     private LLVMDebugValue getCurrentValue() {
         if (isInNative()) {
-            return new LLDBMemoryValue(LLVMNativePointer.cast(context.getGlobalStorage().get(global)));
+            return new LLDBMemoryValue(LLVMNativePointer.cast(context.getGlobalStorage(global)));
         } else {
-            return LLVMLanguage.getLLDBSupport().createDebugValueBuilder().build(context.getGlobalStorage().get(global));
+            return LLVMLanguage.getLLDBSupport().createDebugValueBuilder().build(context.getGlobalStorage(global));
         }
     }
 
     private boolean isInNative() {
-        return LLVMNativePointer.isInstance(context.getGlobalStorage().get(global));
+        return LLVMNativePointer.isInstance(context.getGlobalStorage(global));
     }
 }
