aeb9acb0796e36723a41db6dca2ccb3e0466db12
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:16:28 +0200

    Remove SourceSection field from OSRRootNode, use LoopNode's instead.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedOSRLoopNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedOSRLoopNode.java
@@ -231,11 +231,14 @@ public abstract class OptimizedOSRLoopNode extends LoopNode implements ReplaceOb
 
     private OptimizedCallTarget compileImpl(VirtualFrame frame) {
         RootNode root = getRootNode();
+        Node parent = getParent();
         if (speculationLog == null) {
             speculationLog = GraalTruffleRuntime.getRuntime().createSpeculationLog();
         }
-        OptimizedCallTarget osrTarget = GraalTruffleRuntime.getRuntime().createOSRCallTarget(createRootNodeImpl(root, frame.getClass()));
+        OptimizedCallTarget osrTarget = (OptimizedCallTarget) GraalTruffleRuntime.getRuntime().createCallTarget(createRootNodeImpl(root, frame.getClass()));
         osrTarget.setSpeculationLog(speculationLog);
+        // let the old parent re-adopt the children
+        parent.adoptChildren();
         osrTarget.compile(true);
         return osrTarget;
     }
@@ -375,7 +378,6 @@ public abstract class OptimizedOSRLoopNode extends LoopNode implements ReplaceOb
 
         protected final Class<? extends VirtualFrame> clazz;
 
-        /** Not adopted by the OSRRootNode; belongs to another RootNode. */
         @Child protected OptimizedOSRLoopNode loopNode;
 
         OSRRootNode(OptimizedOSRLoopNode loop, FrameDescriptor frameDescriptor, Class<? extends VirtualFrame> clazz) {
