f8a93091e82e3a2d634f43ad47866c8eb7398400
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:28:11 +0200

    Fix cancellation of TimeLimitChecker task.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/PolyglotLimits.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/PolyglotLimits.java
@@ -123,7 +123,7 @@ final class PolyglotLimits {
         final EventContext eventContext;
         final PolyglotEngineImpl engine;
         final FrameSlot readContext;
-        final ConditionProfile needsLookup = ConditionProfile.create();
+        final ConditionProfile needsLookup = ConditionProfile.createBinaryProfile();
         final FrameDescriptor descriptor;
         @CompilationFinal private boolean seenInnerContext;
 
