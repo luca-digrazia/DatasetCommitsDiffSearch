f1ea9a2e32cc5d718823fc0e121a03f36e233b67
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:17:27 +0200

    sulong: merge CommonTestUtils.ExcludingRunner and ExcludingTruffleRunner

--- dataset/GitHub_Java/oracle.graal/sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/CxxMethodsTest.java
+++ dataset/GitHub_Java/oracle.graal/sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/CxxMethodsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -29,37 +29,36 @@
  */
 package com.oracle.truffle.llvm.tests.interop;
 
-import java.util.Set;
 import org.graalvm.polyglot.Value;
-import org.junit.BeforeClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.oracle.truffle.llvm.tests.CommonTestUtils;
+
+@RunWith(CommonTestUtils.ExcludingTruffleRunner.class)
 public class CxxMethodsTest extends InteropTestBase {
 
     private static Value allocPoint;
     private static Value freePoint;
-
-    private static Value getX;
-    private static Value getY;
-    private static Value setX;
-    private static Value setY;
+    private static Value allocXtendPoint;
+    private static Value freeXtendPoint;
 
     private static Value constructor;
-
     private static Value squaredEuclideanDistance;
     private static Value swap;
 
+    private static Value testLibrary;
+
     @BeforeClass
     public static void loadTestBitcode() {
-        Value testLibrary = loadTestBitcodeValue("methodsTest.cpp");
+        testLibrary = loadTestBitcodeValue("methodsTest.cpp");
 
         allocPoint = testLibrary.getMember("allocNativePoint");
         freePoint = testLibrary.getMember("freeNativePoint");
-        getX = testLibrary.getMember("getX");
-        getY = testLibrary.getMember("getY");
-        setX = testLibrary.getMember("setX");
-        setY = testLibrary.getMember("setY");
+        allocXtendPoint = testLibrary.getMember("allocNativeXtendPoint");
+        freeXtendPoint = testLibrary.getMember("freeNativeXtendPoint");
         squaredEuclideanDistance = testLibrary.getMember("squaredEuclideanDistance");
         constructor = testLibrary.getMember("Point");
         swap = testLibrary.getMember("swap");
@@ -68,9 +67,6 @@ public class CxxMethodsTest extends InteropTestBase {
     private static void checkPoint(Value point, int x, int y) {
         Assert.assertTrue("hasMembers", point.hasMembers());
         Assert.assertFalse("hasArrayElements", point.hasArrayElements());
-
-        Set<String> members = point.getMemberKeys();
-        Assert.assertArrayEquals("getMemberKeys", new Object[]{"x", "y"}, members.toArray());
 
         Assert.assertTrue("hasMember(x)", point.hasMember("x"));
         Assert.assertTrue("hasMember(y)", point.hasMember("y"));
@@ -96,10 +92,10 @@ public class CxxMethodsTest extends InteropTestBase {
     public void testGettersAndSetters() {
         Value point = allocPoint.execute();
         try {
-            setX.execute(point, 30000);
-            setY.execute(point, 4);
+            point.invokeMember("setX", 30000);
+            point.invokeMember("setY", 4);
             checkPoint(point, 30000, 4);
-            checkPoint(point, getX.execute(point).asInt(), getY.execute(point).asInt());
+            checkPoint(point, point.invokeMember("getX").asInt(), point.invokeMember("getY").asInt());
 
         } finally {
             freePoint.execute(point);
@@ -108,13 +104,14 @@ public class CxxMethodsTest extends InteropTestBase {
 
     @Test
     public void testMemberFunction() {
-        Value point = allocPoint.execute();
+        Value point = testLibrary.invokeMember("allocNativePoint");
         Value point2 = allocPoint.execute();
         try {
-            setX.execute(point, 3);
-            setY.execute(point, -4);
-            setX.execute(point2, -6);
-            setY.execute(point2, 8);
+            point.invokeMember("setX", 3);
+            Assert.assertEquals("getX()==3 after setX(3)", 3, point.invokeMember("getX").asInt());
+            point.invokeMember("setY", -4);
+            point2.invokeMember("setX", -6);
+            testLibrary.invokeMember("setY", point2, 8);
             checkPoint(point, 3, -4);
             checkPoint(point2, -6, 8);
             // swap point <-> point2
@@ -123,25 +120,68 @@ public class CxxMethodsTest extends InteropTestBase {
             checkPoint(point, -6, 8);
             // calculate distance
             Value distanceResult1 = squaredEuclideanDistance.execute(point, point2);
-            Value distanceResult2 = squaredEuclideanDistance.execute(point, point2);
+            Value distanceResult2 = squaredEuclideanDistance.execute(point2, point);
+            Value distanceResult3 = point2.invokeMember("squaredEuclideanDistance", point);
+            Value distanceResult4 = point.invokeMember("squaredEuclideanDistance", point2);
+
             Assert.assertEquals("distance(p, p2)", 0, Double.compare(225, distanceResult1.asDouble()));
             Assert.assertEquals("distance(p2, p)", 0, Double.compare(225, distanceResult2.asDouble()));
+            Assert.assertEquals("p.distance(p2)", 0, Double.compare(225, distanceResult3.asDouble()));
+            Assert.assertEquals("p2.distance(p)", 0, Double.compare(225, distanceResult4.asDouble()));
 
         } finally {
-            freePoint.execute(point);
+            testLibrary.invokeMember("freeNativePoint", point);
             freePoint.execute(point2);
         }
     }
 
-    @Test
-    public void testConstructor() {
+    @Test(expected = IllegalArgumentException.class)
+    public void testNonExistingMethod() {
         Value point = allocPoint.execute();
         try {
-            constructor.execute(point);
-            checkPoint(point, 0, 0);
+            point.invokeMember("methodWhichDoesNotExist");
         } finally {
-            freePoint.execute(point);
+            testLibrary.invokeMember("freeNativePoint", point);
         }
+    }
+
+    @Test
+    public void testOverloadedMethods() {
+        Value xPoint = allocXtendPoint.execute();
+        try {
+            xPoint.invokeMember("setZ", 1);
+            Assert.assertEquals("getZ()", 1, xPoint.invokeMember("getZ").asInt());
+            Assert.assertEquals("getZ(2)", 3, xPoint.invokeMember("getZ", 2).asInt());
+        } finally {
+            freeXtendPoint.execute(xPoint);
+        }
+    }
+
+    @Test
+    public void testInheritedMethodsFromSuperclass() {
+        Value xPoint = allocXtendPoint.execute();
+        try {
+            xPoint.invokeMember("setX", 6);
+            xPoint.invokeMember("setY", 7);
+            Assert.assertEquals("direct call: getX()", 12, xPoint.invokeMember("getX").asInt());
+            Assert.assertEquals("superclass::getY()", 7, xPoint.invokeMember("getY").asInt());
+        } finally {
+            freeXtendPoint.execute(xPoint);
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWrongArity() {
+        Value point = constructor.newInstance();
+        point.invokeMember("setX");
+    }
+
+    @Test
+    public void testConstructor() {
+        Value point = constructor.newInstance();
+        checkPoint(point, 0, 0);
+        point.invokeMember("setX", 6);
+        checkPoint(point, 6, 0);
     }
 
 }
