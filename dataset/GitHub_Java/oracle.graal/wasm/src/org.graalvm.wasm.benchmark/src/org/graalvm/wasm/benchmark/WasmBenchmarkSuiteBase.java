cc58b02ad0dd9e8ce26e3249de1142abd3d37763
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:34:50 +0200

    Cleanup wasm benchmark.

--- dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm.benchmark/src/org/graalvm/wasm/benchmark/WasmBenchmarkSuiteBase.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm.benchmark/src/org/graalvm/wasm/benchmark/WasmBenchmarkSuiteBase.java
@@ -1,46 +1,53 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * All rights reserved.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * Redistribution and use in source and binary forms, with or without modification, are
- * permitted provided that the following conditions are met:
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * 1. Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
+ * (a) the Software, and
  *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
- * conditions and the following disclaimer in the documentation and/or other materials provided
- * with the distribution.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
  *
- * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written
- * permission.
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package org.graalvm.wasm.benchmark;
 
-import static org.graalvm.wasm.benchmark.WasmBenchmarkSuiteBase.Defaults.FORKS;
-import static org.graalvm.wasm.benchmark.WasmBenchmarkSuiteBase.Defaults.MEASUREMENT_ITERATIONS;
-import static org.graalvm.wasm.benchmark.WasmBenchmarkSuiteBase.Defaults.WARMUP_ITERATIONS;
+import static org.graalvm.wasm.utils.SystemProperties.DISABLE_COMPILATION_FLAG;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
-import org.graalvm.polyglot.io.ByteSequence;
-import org.graalvm.wasm.predefined.testutil.TestutilModule;
 import org.graalvm.wasm.utils.Assert;
 import org.graalvm.wasm.utils.cases.WasmCase;
 import org.openjdk.jmh.annotations.Fork;
@@ -50,71 +57,57 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
 
-import org.graalvm.wasm.utils.WasmInitialization;
-
-@Warmup(iterations = WARMUP_ITERATIONS)
-@Measurement(iterations = MEASUREMENT_ITERATIONS)
-@Fork(FORKS)
+@Warmup(iterations = 6)
+@Measurement(iterations = 8)
+@Fork(1)
 public abstract class WasmBenchmarkSuiteBase {
-    public static class Defaults {
-        public static final int MEASUREMENT_ITERATIONS = 10;
-        public static final int WARMUP_ITERATIONS = 10;
-        public static final int FORKS = 1;
-    }
-
     public abstract static class WasmBenchmarkState {
         private WasmCase benchmarkCase;
-
-        private Value benchmarkSetupOnce;
+        private Context context;
+        private Value benchmarkSetupEach;
+        private Value benchmarkTeardownEach;
         private Value benchmarkRun;
-        private Value resetContext;
-        private Value customInitializer;
-        private WasmInitialization initialization;
         private Value result;
+
         /**
          * Benchmarks must not be validated via their standard out, unlike tests.
          */
         private ByteArrayOutputStream dummyStdout = new ByteArrayOutputStream();
 
+        abstract protected String benchmarkResource();
+
         @Setup(Level.Trial)
         public void setup() throws IOException, InterruptedException {
-            final String wantedBenchmarkName = WasmBenchmarkOptions.BENCHMARK_NAME;
-
-            if (wantedBenchmarkName == null || wantedBenchmarkName.trim().equals("")) {
-                Assert.fail("Please select a benchmark by setting -Dwasmbench.benchmarkName");
+            final Context.Builder contextBuilder = Context.newBuilder("wasm");
+            contextBuilder.option("wasm.Builtins", "testutil,env:emscripten,wasi_snapshot_preview1");
+            if (!Objects.isNull(DISABLE_COMPILATION_FLAG)) {
+                contextBuilder.allowExperimentalOptions(true);
+                contextBuilder.option("engine.Compilation", "false");
             }
+            context = contextBuilder.build();
+            benchmarkCase = WasmCase.loadBenchmarkCase(benchmarkResource());
+            System.out.println("...::: Benchmark " + benchmarkCase.name() + " :::...");
+            benchmarkCase.getSources().forEach(context::eval);
 
-            benchmarkCase = WasmCase.collectFileCase("bench", benchmarkResource(), WasmBenchmarkOptions.BENCHMARK_NAME);
-
-            Assert.assertNotNull(String.format("Benchmark %s.%s not found", benchmarkResource(), wantedBenchmarkName), benchmarkCase);
-
-            Context.Builder contextBuilder = Context.newBuilder("wasm");
-            contextBuilder.option("wasm.PredefinedModules", "testutil:testutil,env:emscripten");
-
-            byte[] binary = benchmarkCase.createBinary();
-            Context context = contextBuilder.build();
-            Source source = Source.newBuilder("wasm", ByteSequence.create(binary), "test").build();
-
-            context.eval(source);
-
-            Value wasmBindings = context.getBindings("wasm");
-            benchmarkSetupOnce = wasmBindings.getMember("_benchmarkSetupOnce");
-            benchmarkRun = wasmBindings.getMember("_benchmarkRun");
-            resetContext = wasmBindings.getMember(TestutilModule.Names.RESET_CONTEXT);
-            customInitializer = wasmBindings.getMember(TestutilModule.Names.RUN_CUSTOM_INITIALIZATION);
-            initialization = benchmarkCase.initialization();
-
-            // Initialization is done only once, and before the module starts.
-            // It is the benchmark's job to ensure that it executes meaningful workloads
-            // that run correctly despite the fact that the VM state changed.
-            // I.e. benchmark workloads must assume that they are run multiple times.
-            if (initialization != null) {
-                customInitializer.execute(initialization);
-            }
+            // TODO: This should call benchmarkCase.name(), and not main (GR-26734),
+            // but we currently have a hack because the WASI module imports
+            // a memory from a module called main.
+            // We should fix that in the future.
+            Value benchmarkModule = context.getBindings("wasm").getMember("main");
+            Value benchmarkSetupOnce = benchmarkModule.getMember("benchmarkSetupOnce");
+            benchmarkSetupEach = benchmarkModule.getMember("benchmarkSetupEach");
+            benchmarkTeardownEach = benchmarkModule.getMember("benchmarkTeardownEach");
+            benchmarkRun = benchmarkModule.getMember("benchmarkRun");
+            Assert.assertNotNull(String.format("No benchmarkRun method in %s.", benchmarkCase.name()), benchmarkRun);
 
             if (benchmarkSetupOnce != null) {
                 benchmarkSetupOnce.execute();
             }
+        }
+
+        @TearDown(Level.Trial)
+        public void teardown() {
+            context.close();
         }
 
         @Setup(Level.Iteration)
@@ -127,16 +120,25 @@ public abstract class WasmBenchmarkSuiteBase {
         public void teardownIteration() {
             // Validate result.
             WasmCase.validateResult(benchmarkCase.data().resultValidator(), result, dummyStdout);
+        }
 
+        @Setup(Level.Invocation)
+        public void setupInvocation() {
             // Note: we deliberately not reset the context here.
             // It would be slow, and the invariant we expect from the benchmarks
             // is that they can handle VM-state side-effects.
             // We may support benchmark-specific teardown actions in the future (at the invocation
             // level).
+            benchmarkSetupEach.execute();
         }
 
-        @Setup(Level.Invocation)
-        public void setupInvocation() {
+        @TearDown(Level.Invocation)
+        public void teardownInvocation() {
+            benchmarkTeardownEach.execute(0);
+        }
+
+        public void run() {
+            this.result = benchmarkRun.execute();
         }
 
         public Value benchmarkRun() {
@@ -146,7 +148,5 @@ public abstract class WasmBenchmarkSuiteBase {
         public void setResult(Value result) {
             this.result = result;
         }
-
-        protected abstract String benchmarkResource();
     }
 }
