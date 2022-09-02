7a5f091640d8414f36f262d43263cba0b68e4525
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:11:02 +0200

    Use outer class inside of the @link tag

--- dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.agentscript.test/src/com/oracle/truffle/tools/agentscript/test/AgentScriptAPI.java
+++ dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.agentscript.test/src/com/oracle/truffle/tools/agentscript/test/AgentScriptAPI.java
@@ -43,10 +43,6 @@ public interface AgentScriptAPI {
              * @return name of the loaded source
              */
             String name();
-            /** Character content of the {@link #sourceLoaded}.
-             * @return content of the loaded source
-             */
-            String characters();
         }
         void sourceLoaded(Info info);
     }
