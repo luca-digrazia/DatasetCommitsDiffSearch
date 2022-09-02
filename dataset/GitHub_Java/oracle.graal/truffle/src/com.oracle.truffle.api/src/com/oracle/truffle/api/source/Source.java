f9e7811cab88ad33840aa6051acd7c3badf4523e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:45:35 +0200

    Reverse condition to clarify when it can apply

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/source/Source.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/source/Source.java
@@ -1837,7 +1837,7 @@ public abstract class Source {
 
         /**
          * @since 0.15
-         * @deprecated see {@link SourceBuilder#uri(URI)}
+         * @deprecated see {@link SourceBuilder#content(CharSequence)}
          */
         @Deprecated
         public Builder<E1, E2, E3> uri(URI ownUri) {
