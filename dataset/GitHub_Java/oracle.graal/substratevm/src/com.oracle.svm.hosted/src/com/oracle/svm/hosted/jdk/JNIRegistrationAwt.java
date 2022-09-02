8c847fb525a3b99a8f758493615473008d9af0af
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:31:20 +0200

    All built-in packages must be registered before information is used the first time

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/jdk/JNIRegistrationAwt.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/jdk/JNIRegistrationAwt.java
@@ -24,7 +24,6 @@
  */
 package com.oracle.svm.hosted.jdk;
 
-import com.oracle.svm.core.SubstrateOptions;
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import com.oracle.svm.core.configure.ResourcesRegistry;
 import com.oracle.svm.core.jdk.JNIRegistrationUtil;
@@ -60,9 +59,13 @@ public class JNIRegistrationAwt extends JNIRegistrationUtil implements Feature {
                             clazz(access, "sun.awt.X11GraphicsEnvironment"),
                             clazz(access, "sun.font.FontManagerNativeLibrary"),
                             clazz(access, "sun.java2d.Disposer"));
+            PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("java_awt");
+            PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("sun_awt");
+            PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("sun_java2d");
 
             access.registerReachabilityHandler(JNIRegistrationAwt::registerFreeType,
                             clazz(access, "sun.font.FontManagerNativeLibrary"));
+            PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("sun_font");
 
             access.registerReachabilityHandler(JNIRegistrationAwt::registerLCMS,
                             clazz(access, "sun.java2d.cmm.lcms.LCMS"));
@@ -74,6 +77,7 @@ public class JNIRegistrationAwt extends JNIRegistrationUtil implements Feature {
                             clazz(access, "sun.awt.image.JPEGImageDecoder"),
                             clazz(access, "com.sun.imageio.plugins.jpeg.JPEGImageReader"),
                             clazz(access, "com.sun.imageio.plugins.jpeg.JPEGImageWriter"));
+            PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("com_sun_imageio_plugins_jpeg");
 
             access.registerReachabilityHandler(JNIRegistrationAwt::registerColorProfiles,
                             clazz(access, "java.awt.color.ICC_Profile"));
@@ -115,10 +119,6 @@ public class JNIRegistrationAwt extends JNIRegistrationUtil implements Feature {
             nativeLibraries.addDynamicNonJniLibrary("Xi");
         }
 
-        PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("java_awt");
-        PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("sun_awt");
-        PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("sun_java2d");
-
         nativeLibraries.addDynamicNonJniLibrary("stdc++");
         nativeLibraries.addDynamicNonJniLibrary("m");
 
@@ -127,9 +127,6 @@ public class JNIRegistrationAwt extends JNIRegistrationUtil implements Feature {
     }
 
     private static void registerJPEG(DuringAnalysisAccess access) {
-
-        PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("com_sun_imageio_plugins_jpeg");
-
         NativeLibraries nativeLibraries = getNativeLibraries(access);
 
         NativeLibrarySupport.singleton().preregisterUninitializedBuiltinLibrary("javajpeg");
@@ -151,20 +148,11 @@ public class JNIRegistrationAwt extends JNIRegistrationUtil implements Feature {
     }
 
     private static void registerFreeType(DuringAnalysisAccess access) {
-        if (SubstrateOptions.StaticExecutable.getValue()) {
-            /*
-             * Freetype uses fontconfig through dlsym. This may not work in a statically linked
-             * executable
-             */
-            return;
-        }
         NativeLibraries nativeLibraries = getNativeLibraries(access);
 
         NativeLibrarySupport.singleton().preregisterUninitializedBuiltinLibrary("fontmanager");
         nativeLibraries.addStaticJniLibrary("fontmanager", isHeadless() ? "awt_headless" : "awt_xawt");
         nativeLibraries.addStaticJniLibrary("harfbuzz");
-
-        PlatformNativeLibrarySupport.singleton().addBuiltinPkgNativePrefix("sun_font");
 
         nativeLibraries.addDynamicNonJniLibrary("freetype");
 
