93027f5b7cf28ebee1746ac8fc27ec11b7682c1b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:11:35 +0200

    remove wrong invalidation of new class verison

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ObjectKlass.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ObjectKlass.java
@@ -50,7 +50,6 @@ import com.oracle.truffle.espresso.descriptors.Symbol;
 import com.oracle.truffle.espresso.descriptors.Symbol.Name;
 import com.oracle.truffle.espresso.descriptors.Symbol.Signature;
 import com.oracle.truffle.espresso.descriptors.Symbol.Type;
-import com.oracle.truffle.espresso.jdwp.api.MethodRef;
 import com.oracle.truffle.espresso.meta.EspressoError;
 import com.oracle.truffle.espresso.meta.Meta;
 import com.oracle.truffle.espresso.runtime.Attribute;
@@ -431,15 +430,6 @@ public final class ObjectKlass extends Klass {
     @Override
     public Method[] getDeclaredMethods() {
         return declaredMethods;
-    }
-
-    @Override
-    public MethodRef[] getDeclaredMethodRefs() {
-        MethodRef[] result = new MethodRef[declaredMethods.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = declaredMethods[i].getMethodVersion();
-        }
-        return result;
     }
 
     @Override
