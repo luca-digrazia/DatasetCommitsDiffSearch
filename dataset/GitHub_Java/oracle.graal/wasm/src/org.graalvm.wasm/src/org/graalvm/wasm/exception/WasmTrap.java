a8dffb22a93af36cf77bcedcdfb094c63a5d2289
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 14:08:59 +0200

    Export WasmTrap as an error.

--- dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/exception/WasmTrap.java
+++ dataset/GitHub_Java/oracle.graal/wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/exception/WasmTrap.java
@@ -87,7 +87,7 @@ public final class WasmTrap extends AbstractTruffleException {
 
     @ExportMessage
     @SuppressWarnings({"unused", "static-method"})
-    Object getMembers(boolean includeInternal) throws UnsupportedMessageException {
+    final Object getMembers(boolean includeInternal) throws UnsupportedMessageException {
         throw UnsupportedMessageException.create();
     }
 
