da4f1ca2202eea92adb453aaf2046e10984aceba
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:47:18 +0200

    show detailed info when manyIfsProgram times out

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/BytecodeInterpreterPartialEvaluationTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/BytecodeInterpreterPartialEvaluationTest.java
@@ -51,8 +51,6 @@ import com.oracle.truffle.api.nodes.ControlFlowException;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.nodes.ExplodeLoop.LoopExplosionKind;
 import com.oracle.truffle.api.nodes.RootNode;
-import org.graalvm.compiler.truffle.compiler.PolyglotCompilerOptionsScope;
-import org.graalvm.compiler.truffle.runtime.TruffleRuntimeOptions;
 
 public class BytecodeInterpreterPartialEvaluationTest extends PartialEvaluationTest {
 
