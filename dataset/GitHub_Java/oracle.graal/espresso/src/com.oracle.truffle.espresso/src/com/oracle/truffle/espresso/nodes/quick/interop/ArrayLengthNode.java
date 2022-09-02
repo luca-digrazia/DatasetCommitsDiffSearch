c3b7dd8d8dcbfea03bae39e2c3d94c25fa060c4d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 09:13:24 +0200

    Change behavior when foreign array .length overflow.

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/quick/interop/ArrayLengthNode.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/quick/interop/ArrayLengthNode.java
@@ -55,8 +55,8 @@ import com.oracle.truffle.espresso.vm.InterpreterToVM;
  * {@code byte[]}, ARRAYLENGTH is mapped to {@link InteropLibrary#getBufferSize(Object)}.
  * </ul>
  *
- * <p>
- * If the size of the foreign array does NOT fit in an int, {@link Integer#MAX_VALUE} is returned.
+ * <h3>Exceptions</h3>
+ * <li>Throws guest {@link ClassCastException} if the length does not fit in an int.
  */
 @ImportStatic(Utils.class)
 public abstract class ArrayLengthNode extends QuickNode {
