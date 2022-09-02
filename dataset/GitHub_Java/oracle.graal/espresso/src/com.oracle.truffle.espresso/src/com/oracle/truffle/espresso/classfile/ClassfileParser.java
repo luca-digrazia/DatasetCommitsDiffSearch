5e17818de6b31dfcd3f210edcbdfeea84ee44668
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:54:11 +0200

    Fix nullcheck warning.

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/classfile/ClassfileParser.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/classfile/ClassfileParser.java
@@ -1145,11 +1145,6 @@ public final class ClassfileParser {
                 throw ConstantPool.classFormatError("Class is both outer and inner class");
             }
 
-            Symbol<Name> innerClassName = null;
-            if (innerClassIndex != 0) {
-                innerClassName = pool.classAt(innerClassIndex).getName(pool);
-            }
-
             for (int j = 0; j < i; ++j) {
                 // Inner class info is often small: better to use array lookup for startup.
                 final InnerClassesAttribute.Entry otherInnerClassInfo = innerClassInfos[j];
@@ -1162,7 +1157,7 @@ public final class ClassfileParser {
                                     // compare by name instead.
                                     (innerClassIndex != 0 &&
                                                     otherInnerClassInfo.innerClassIndex != 0 &&
-                                                    innerClassName.equals(pool.classAt(otherInnerClassInfo.innerClassIndex).getName(pool)))) {
+                                                    pool.classAt(innerClassIndex).getName(pool) == pool.classAt(otherInnerClassInfo.innerClassIndex).getName(pool))) {
                         duplicateInnerClass = true;
                     }
                 }
@@ -1181,7 +1176,7 @@ public final class ClassfileParser {
         return new InnerClassesAttribute(name, innerClassInfos);
     }
 
-    int findInnerClassIndexEntry(InnerClassesAttribute.Entry[] entries, Symbol<Name> innerClassName) {
+    int findInnerClassIndexEntry(InnerClassesAttribute.Entry[] entries, int innerClass) {
         for (int i = 0; i < entries.length; i++) {
             InnerClassesAttribute.Entry entry = entries[i];
             if (entry.innerClassIndex == 0) {
@@ -1189,7 +1184,7 @@ public final class ClassfileParser {
             }
             // The same class can be referenced by two different CP indices, compare by name
             // instead.
-            if (innerClassName == pool.classAt(entry.innerClassIndex).getName(pool)) {
+            if (pool.classAt(innerClass).getName(pool) == pool.classAt(entry.innerClassIndex).getName(pool)) {
                 return i;
             }
         }
@@ -1209,7 +1204,7 @@ public final class ClassfileParser {
                 if (v == 0) {
                     break;
                 }
-                int index = findInnerClassIndexEntry(entries, pool.classAt(v).getName(pool));
+                int index = findInnerClassIndexEntry(entries, v);
                 if (index < 0) { // no edge found
                     break;
                 }
