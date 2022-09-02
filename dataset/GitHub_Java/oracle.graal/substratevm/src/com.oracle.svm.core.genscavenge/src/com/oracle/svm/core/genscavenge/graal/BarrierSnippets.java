f2191f0754e4475dd36eebe9aacd767dbf68f80c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:36:32 +0200

    Moved the UseCardRememberedSetHeap option.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/graal/BarrierSnippets.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/graal/BarrierSnippets.java
@@ -101,8 +101,8 @@ public class BarrierSnippets extends SubstrateTemplates implements Snippets {
             return;
         }
         // The object needs a write-barrier. Is it aligned or unaligned?
-        final boolean aligned = ObjectHeaderImpl.isAlignedHeaderUnsafe(objectHeader);
-        if (BranchProbabilityNode.probability(BranchProbabilityNode.LIKELY_PROBABILITY, aligned)) {
+        final boolean unaligned = ObjectHeaderImpl.isHeapObjectUnaligned(objectHeader);
+        if (BranchProbabilityNode.probability(BranchProbabilityNode.LIKELY_PROBABILITY, !unaligned)) {
             // Next most likely (?): aligned objects.
             counters().postWriteBarrierAligned.inc();
             AlignedHeapChunk.dirtyCardForObjectOfAlignedHeapChunk(fixedObject, verifyOnly);
