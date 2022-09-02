0858520e129529307afafaa784b4595aad6db64e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 15:46:26 +0200

    Fix a comment formatting

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/StaticObject.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/StaticObject.java
@@ -1078,7 +1078,6 @@ public final class StaticObject implements TruffleObject {
     private StaticObject(Klass klass, Object foreignObject, @SuppressWarnings("unused") Void unused) {
         this.klass = klass;
         assert foreignObject != null;
-        assert !(foreignObject instanceof StaticObject) : "Espresso objects cannot be wrapped";
         this.primitiveFields = FOREIGN_OBJECT_MARKER;
         this.fields = foreignObject;
     }
@@ -1104,18 +1103,12 @@ public final class StaticObject implements TruffleObject {
         return new StaticObject(klass, array);
     }
 
-    public static StaticObject createForeign(Klass klass, Object foreignObject, InteropLibrary interopLibrary) {
+    public static StaticObject createForeign(Klass klass, Object foreignObject) {
         assert foreignObject != null;
-        if (interopLibrary.isNull(foreignObject)) {
-            return createForeignNull(foreignObject);
+        if (InteropLibrary.getUncached().isNull(foreignObject)) {
+            return new StaticObject(null, foreignObject, null);
         }
         return new StaticObject(klass, foreignObject, null);
-    }
-
-    public static StaticObject createForeignNull(Object foreignObject) {
-        assert foreignObject != null;
-        assert InteropLibrary.getUncached().isNull(foreignObject);
-        return new StaticObject(null, foreignObject, null);
     }
 
     public Klass getKlass() {
