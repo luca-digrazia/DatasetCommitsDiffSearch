e5e35708ddbb02fbcbb5e2701d79cb7baf7aa071
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 23:04:01 +0200

    Correct register kills for compress/inflate intrinsics

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64StringLatin1InflateOp.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64StringLatin1InflateOp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -267,8 +267,4 @@ public final class AMD64StringLatin1InflateOp extends AMD64LIRInstruction {
         masm.bind(labelDone);
     }
 
-    @Override
-    public boolean needsClearUpperVectorRegisters() {
-        return true;
-    }
 }
--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64StringUTF16CompressOp.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64StringUTF16CompressOp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -342,8 +342,4 @@ public final class AMD64StringUTF16CompressOp extends AMD64LIRInstruction {
         masm.bind(labelDone);
     }
 
-    @Override
-    public boolean needsClearUpperVectorRegisters() {
-        return true;
-    }
 }
