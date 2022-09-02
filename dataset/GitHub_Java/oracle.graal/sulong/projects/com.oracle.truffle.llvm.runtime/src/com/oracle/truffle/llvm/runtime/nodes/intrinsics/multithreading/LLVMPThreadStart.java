955b93e68303b02624d84f01e2100c685ed72162
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:52:43 +0200

    Apply Code Formatter

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/multithreading/LLVMPThreadStart.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/multithreading/LLVMPThreadStart.java
@@ -97,7 +97,6 @@ public final class LLVMPThreadStart {
                             }
                         }
                     }
-                    pThreadContext.clearThreadId();
                 }
             }
         }
