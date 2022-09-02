a6bcd5f3013e9510391a2748c078470aa4fae928
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:44:21 +0200

    Reset LibraryFactory default exports.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.library/src/com/oracle/truffle/api/library/LibraryFactory.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.library/src/com/oracle/truffle/api/library/LibraryFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -40,8 +40,6 @@
  */
 package com.oracle.truffle.api.library;
 
-import static com.oracle.truffle.api.CompilerDirectives.shouldNotReachHere;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -60,9 +58,9 @@ import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.TruffleOptions;
 import com.oracle.truffle.api.dsl.GeneratedBy;
 import com.oracle.truffle.api.library.LibraryExport.DelegateExport;
-import com.oracle.truffle.api.nodes.EncapsulatingNodeReference;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeCost;
+import com.oracle.truffle.api.nodes.NodeUtil;
 import com.oracle.truffle.api.utilities.FinalBitSet;
 
 import sun.misc.Unsafe;
@@ -258,9 +256,9 @@ public abstract class LibraryFactory<T extends Library> {
      * <p>
      * Whenever the limit is reached for a node and the uncached version is in use, the current
      * enclosing node will be available to the uncached library export of the library using
-     * {@link EncapsulatingNodeReference}.
+     * {@link NodeUtil#getCurrentEncapsulatingNode()}.
      *
-     * @see EncapsulatingNodeReference
+     * @see NodeUtil#getCurrentEncapsulatingNode()
      * @see CachedLibrary
      * @since 19.0
      */
@@ -539,7 +537,7 @@ public abstract class LibraryFactory<T extends Library> {
      * @since 20.0
      */
     protected FinalBitSet createMessageBitSet(@SuppressWarnings({"unused", "hiding"}) Message... enabledMessages) {
-        throw shouldNotReachHere("should be generated");
+        throw new AssertionError("should be generated");
     }
 
     /**
@@ -603,7 +601,7 @@ public abstract class LibraryFactory<T extends Library> {
 
     private void validateExport(Object receiver, Class<?> dispatchedClass, LibraryExport<T> exports) throws AssertionError {
         if (!exports.getReceiverClass().isInstance(receiver)) {
-            throw shouldNotReachHere(
+            throw new AssertionError(
                             String.format("Receiver class %s was dynamically dispatched to incompatible exports %s. Expected receiver class %s.",
                                             receiver.getClass().getName(), dispatchedClass.getName(), exports.getReceiverClass().getName()));
         }
@@ -697,7 +695,7 @@ public abstract class LibraryFactory<T extends Library> {
     protected static <T extends Library> void register(Class<T> libraryClass, LibraryFactory<T> library) {
         LibraryFactory<?> lib = LIBRARIES.putIfAbsent(libraryClass, library);
         if (lib != null) {
-            throw shouldNotReachHere("Reflection cannot be installed for a library twice.");
+            throw new AssertionError("Reflection cannot be installed for a library twice.");
         }
     }
 
@@ -761,7 +759,6 @@ public abstract class LibraryFactory<T extends Library> {
             return (LibraryExport<T>) lib;
         }
 
-        /** NOTE: this method is called reflectively by downstream projects. */
         @TruffleBoundary
         static ResolvedDispatch lookup(Class<?> receiverClass) {
             ResolvedDispatch type = CACHE.get(receiverClass);
@@ -813,7 +810,7 @@ public abstract class LibraryFactory<T extends Library> {
                 loadGeneratedClass(dispatchClass);
                 libs = REGISTRY.get(dispatchClass);
                 if (libs == null) {
-                    throw shouldNotReachHere(String.format("Libraries for class '%s' could not be resolved. Not registered?", dispatchClass.getName()));
+                    throw new AssertionError(String.format("Libraries for class '%s' could not be resolved. Not registered?", dispatchClass.getName()));
                 }
             }
 
@@ -836,7 +833,7 @@ public abstract class LibraryFactory<T extends Library> {
             try {
                 Class.forName(generatedClassName, true, currentReceiverClass.getClassLoader());
             } catch (ClassNotFoundException e) {
-                throw shouldNotReachHere(String.format("Generated class '%s' for class '%s' not found. " +
+                throw new AssertionError(String.format("Generated class '%s' for class '%s' not found. " +
                                 "Did the Truffle annotation processor run?", generatedClassName, currentReceiverClass.getName()), e);
             }
         }
