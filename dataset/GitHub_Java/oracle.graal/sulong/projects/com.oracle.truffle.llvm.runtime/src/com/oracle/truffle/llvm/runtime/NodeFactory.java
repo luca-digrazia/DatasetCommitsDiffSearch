e61c206b488f1b64d0fa4795127a6e839b42ee44
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:13:47 +0200

    Moved toGenericDebuggerValue into the CommonNodeFactory.

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/NodeFactory.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/NodeFactory.java
@@ -229,8 +229,6 @@ public interface NodeFactory extends InteropNodeFactory {
 
     LLVMMemoryOpNode createProtectGlobalsBlock();
 
-    LLVMMemoryOpNode createFreeGlobalsBlock(boolean readOnly);
-
     LLVMExpressionNode createStackSave();
 
     LLVMExpressionNode createStackRestore(LLVMExpressionNode stackPointer);
