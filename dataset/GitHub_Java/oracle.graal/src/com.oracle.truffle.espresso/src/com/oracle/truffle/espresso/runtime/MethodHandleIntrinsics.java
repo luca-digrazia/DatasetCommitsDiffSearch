6e53bdbff004f8129ac328981eaacb26f8d694f2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:32:19 +0200

    Stack walk should ignore MH.invoke method frames.     HotSpot does not include MH.invoke, so it doesn't have to ignore it     explicitly. Espresso does push the MH.invoke frame, so the verbatim     implementation from HotSpot is not correct in Espresso.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/MethodHandleIntrinsics.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/MethodHandleIntrinsics.java
@@ -22,10 +22,8 @@
  */
 package com.oracle.truffle.espresso.runtime;
 
-import static com.oracle.truffle.espresso.classfile.Constants.ACC_NATIVE;
-import static com.oracle.truffle.espresso.classfile.Constants.ACC_VARARGS;
-
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.espresso.descriptors.Symbol;
@@ -33,35 +31,15 @@ import com.oracle.truffle.espresso.descriptors.Symbol.Name;
 import com.oracle.truffle.espresso.descriptors.Symbol.Signature;
 import com.oracle.truffle.espresso.descriptors.Symbol.Type;
 import com.oracle.truffle.espresso.impl.ContextAccess;
-import com.oracle.truffle.espresso.impl.Klass;
 import com.oracle.truffle.espresso.impl.Method;
 import com.oracle.truffle.espresso.meta.EspressoError;
 import com.oracle.truffle.espresso.meta.Meta;
-import com.oracle.truffle.espresso.nodes.HandleIntrinsicNode;
-import com.oracle.truffle.espresso.nodes.MHInvokeBasicNode;
-import com.oracle.truffle.espresso.nodes.MHInvokeGenericNode;
-import com.oracle.truffle.espresso.nodes.MHLinkToNode;
+import com.oracle.truffle.espresso.nodes.EspressoBaseNode;
+
+import static com.oracle.truffle.espresso.classfile.Constants.ACC_NATIVE;
+import static com.oracle.truffle.espresso.classfile.Constants.ACC_VARARGS;
 
 public final class MethodHandleIntrinsics implements ContextAccess {
-
-    public HandleIntrinsicNode createIntrinsicNode(Method method, Klass accessingKlass, Symbol<Name> methodName, Symbol<Signature> signature) {
-        PolySigIntrinsics id = getId(method);
-        switch (id) {
-            case InvokeBasic:
-                return new MHInvokeBasicNode(method);
-            case InvokeGeneric:
-                return MHInvokeGenericNode.create(accessingKlass, method, methodName, signature, getMeta());
-            case LinkToVirtual:
-            case LinkToStatic:
-            case LinkToSpecial:
-            case LinkToInterface:
-                return MHLinkToNode.create(method, id);
-            default:
-                throw EspressoError.shouldNotReachHere("unrecognized intrinsic polymorphic method: " + method);
-
-        }
-    }
-
     public enum PolySigIntrinsics {
         None(0),
         InvokeGeneric(1),
@@ -113,7 +91,6 @@ public final class MethodHandleIntrinsics implements ContextAccess {
     }
 
     public static PolySigIntrinsics getId(Method m) {
-        assert m.getDeclaringKlass() == m.getMeta().MethodHandle;
         Symbol<Name> name = m.getName();
         if (name == Name.linkToStatic) {
             return PolySigIntrinsics.LinkToStatic;
@@ -182,14 +159,14 @@ public final class MethodHandleIntrinsics implements ContextAccess {
         return context;
     }
 
-    public Method findIntrinsic(Method thisMethod, Symbol<Signature> signature, PolySigIntrinsics id) {
+    public Method findIntrinsic(Method thisMethod, Symbol<Signature> signature, Function<Method, EspressoBaseNode> baseNodeFactory, PolySigIntrinsics id) {
         ConcurrentHashMap<Symbol<Signature>, Method> intrinsics = getIntrinsicMap(id, thisMethod);
         Method method = intrinsics.get(signature);
         if (method != null) {
             return method;
         }
         CompilerAsserts.neverPartOfCompilation();
-        method = thisMethod.createIntrinsic(signature);
+        method = thisMethod.createIntrinsic(signature, baseNodeFactory);
         Method previous = intrinsics.putIfAbsent(signature, method);
         if (previous != null) {
             return previous;
