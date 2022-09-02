733b8724b4ee2b71d109d8d5e25be798bd831e72
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 21:19:35 +0200

    [GR-16891] sulong: do not load libpolyglot-mock via nfi

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/NFIContextExtension.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/NFIContextExtension.java
@@ -33,7 +33,6 @@ import java.nio.file.Path;
 import java.util.List;
 
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.TruffleFile;
 import org.graalvm.collections.EconomicMap;
 import org.graalvm.collections.MapCursor;
 
@@ -50,7 +49,6 @@ import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.llvm.runtime.LLVMContext.ExternalLibrary;
 import com.oracle.truffle.llvm.runtime.except.LLVMLinkerException;
 import com.oracle.truffle.llvm.runtime.interop.nfi.LLVMNativeWrapper;
-import com.oracle.truffle.llvm.runtime.nodes.asm.syscall.LLVMInfo;
 import com.oracle.truffle.llvm.runtime.options.SulongEngineOption;
 import com.oracle.truffle.llvm.runtime.types.FunctionType;
 import com.oracle.truffle.llvm.runtime.types.PointerType;
@@ -183,13 +181,25 @@ public final class NFIContextExtension implements ContextExtension {
              * available. The libc++ dependency is optional. The bitcode interpreter will still work
              * if it is not found, but C++ programs might not work because of unresolved symbols.
              */
-            String libName = LLVMInfo.SYSNAME.toLowerCase().contains("mac") ? "libc++.dylib" : "libc++.so.1";
-            TruffleFile tf = DefaultLibraryLocator.locateGlobal(context, libName);
-            if (tf != null) {
-                libName = tf.getPath();
+            TruffleObject cxxlib;
+            if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                cxxlib = loadLibrary("libc++.dylib", true, null, context);
+            } else {
+                cxxlib = loadLibrary("libc++.so.1", true, null, context);
+                if (cxxlib == null) {
+                    /*
+                     * On Ubuntu, libc++ can not be dynamically loaded because of a missing
+                     * dependeny on libgcc_s. Work around this by loading it manually first.
+                     */
+                    TruffleObject libgcc = loadLibrary("libgcc_s.so.1", true, "RTLD_GLOBAL", context);
+                    if (libgcc != null) {
+                        cxxlib = loadLibrary("libc++.so.1", true, null, context);
+                    }
+                }
             }
-            TruffleObject cxxlib = loadLibrary(libName, false, null, context);
-            libraryHandles.put(lib, cxxlib);
+            if (cxxlib != null) {
+                libraryHandles.put(lib, cxxlib);
+            }
             return true;
         } else {
             return false;
--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/Runner.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/Runner.java
@@ -44,6 +44,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 
+import com.oracle.truffle.llvm.runtime.LibraryLocator;
 import org.graalvm.collections.EconomicMap;
 import org.graalvm.collections.EconomicSet;
 import org.graalvm.collections.Equivalence;
@@ -51,7 +52,6 @@ import org.graalvm.polyglot.io.ByteSequence;
 
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.RootCallTarget;
 import com.oracle.truffle.api.Truffle;
@@ -174,7 +174,7 @@ final class Runner {
 
         final SulongLibrary sulongLibrary;
         final FrameSlot stackPointerSlot;
-        @CompilationFinal ContextReference<LLVMContext> ctxRef;
+        final ContextReference<LLVMContext> ctxRef;
 
         final int initContextBefore;
         @Child LLVMStatementNode initContext;
@@ -182,13 +182,11 @@ final class Runner {
         @Children final InitializeSymbolsNode[] initSymbols;
         @Children final InitializeModuleNode[] initModules;
 
-        private final Runner runner;
-
         LoadModulesNode(Runner runner, FrameDescriptor rootFrame, InitializationOrder order, SulongLibrary sulongLibrary) {
             super(runner.language, rootFrame);
-            this.runner = runner;
             this.sulongLibrary = sulongLibrary;
             this.stackPointerSlot = rootFrame.findFrameSlot(LLVMStack.FRAME_ID);
+            this.ctxRef = runner.language.getContextReference();
 
             this.initContextBefore = order.sulongLibraries.size();
             this.initContext = runner.context.createInitializeContextNode(rootFrame);
@@ -212,11 +210,6 @@ final class Runner {
 
         @Override
         public Object execute(VirtualFrame frame) {
-            if (ctxRef == null) {
-                CompilerDirectives.transferToInterpreterAndInvalidate();
-                this.ctxRef = lookupContextReference(runner.language.getClass());
-            }
-
             LLVMContext ctx = ctxRef.get();
             try (StackPointer stackPointer = ctxRef.get().getThreadingStack().getStack().newFrame()) {
                 frame.setObject(stackPointerSlot, stackPointer);
@@ -691,6 +684,7 @@ final class Runner {
         } else if (!library.isNative()) {
             throw new LLVMParserException("The file '" + source.getName() + "' is not a bitcode file nor an ELF or Mach-O object file with an embedded bitcode section.");
         } else {
+            LibraryLocator.traceDelegateNative(context, library);
             return null;
         }
     }
