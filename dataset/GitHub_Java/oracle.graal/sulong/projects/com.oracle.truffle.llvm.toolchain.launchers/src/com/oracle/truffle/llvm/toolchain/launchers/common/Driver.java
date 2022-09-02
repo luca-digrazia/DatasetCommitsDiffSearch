9cb445e9d4226779818046191284787108f0c32d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:24:41 +0200

    sulong: make Driver.OS.getCurrent lazy

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.toolchain.launchers/src/com/oracle/truffle/llvm/toolchain/launchers/common/Driver.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.toolchain.launchers/src/com/oracle/truffle/llvm/toolchain/launchers/common/Driver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -61,7 +61,6 @@ public class Driver {
 
     public enum OS {
 
-        WINDOWS,
         DARWIN,
         LINUX;
 
@@ -72,9 +71,6 @@ public class Driver {
             }
             if (name.equals("Mac OS X") || name.equals("Darwin")) {
                 return DARWIN;
-            }
-            if (name.startsWith("Windows")) {
-                return WINDOWS;
             }
             throw new IllegalArgumentException("unknown OS: " + name);
         }
