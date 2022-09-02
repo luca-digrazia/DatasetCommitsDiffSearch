67f43752738f3e56f5e9db8cea0ad02196d8d65d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:40:34 +0200

    Fix newlines in CSourceCodeWriter.writeFile

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/codegen/CSourceCodeWriter.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/codegen/CSourceCodeWriter.java
@@ -26,21 +26,24 @@ package com.oracle.svm.hosted.c.codegen;
 
 import static com.oracle.svm.core.util.VMError.shouldNotReachHere;
 import static com.oracle.svm.hosted.NativeImageOptions.CStandards.C11;
+import static com.oracle.svm.hosted.NativeImageOptions.CStandards.C99;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.channels.ClosedByInterruptException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
+import org.graalvm.nativeimage.Platform;
 import org.graalvm.nativeimage.c.function.CFunctionPointer;
 import org.graalvm.nativeimage.c.function.InvokeCFunctionPointer;
-import org.graalvm.nativeimage.Platform;
 import org.graalvm.word.SignedWord;
 import org.graalvm.word.UnsignedWord;
 
@@ -63,10 +66,8 @@ import jdk.vm.ci.meta.ResolvedJavaType;
 
 public class CSourceCodeWriter {
 
-    private static final String CHARSET = "US-ASCII";
     private static final String INDENT4 = "    ";
     public static final String C_SOURCE_FILE_EXTENSION = ".c";
-    public static final String CXX_SOURCE_FILE_EXTENSION = ".cpp";
 
     private final List<String> lines;
     private final StringBuilder currentLine;
@@ -78,6 +79,21 @@ public class CSourceCodeWriter {
         this.tempDirectory = tempDirectory;
         this.lines = new ArrayList<>();
         this.currentLine = new StringBuilder(100);
+    }
+
+    public void writeCStandardHeaders() {
+        if (NativeImageOptions.getCStandard().compatibleWith(C99)) {
+            if (!Platform.includedIn(Platform.WINDOWS.class)) {
+                /*
+                 * CStandard says we are C99 compatible yet we cannot include stdbool.h because old
+                 * Windows native compilers do not support it. This should be fixed.
+                 */
+                includeFiles(Collections.singletonList("<stdbool.h>"));
+            }
+        }
+        if (NativeImageOptions.getCStandard().compatibleWith(C11)) {
+            includeFiles(Collections.singletonList("<stdint.h>"));
+        }
     }
 
     public int currentLineNumber() {
@@ -94,7 +110,7 @@ public class CSourceCodeWriter {
 
     public void includeFiles(List<String> headerFiles) {
         for (String headerFile : headerFiles) {
-            String headerFileName = null;
+            String headerFileName;
             if (headerFile.startsWith("<") && headerFile.endsWith(">")) {
                 headerFileName = headerFile.substring(1, headerFile.length() - 1);
                 Path headerFilePath = Paths.get(headerFileName);
@@ -104,7 +120,7 @@ public class CSourceCodeWriter {
                 Path headerFilePath = Paths.get(headerFileName);
                 appendln("#include " + "\"" + headerFilePath.toString() + "\"");
             } else {
-                throw UserError.abort("header file name must be surrounded by <...> or \"...\": " + headerFile);
+                throw UserError.abort("Header file name must be surrounded by <...> or \"...\": %s", headerFile);
             }
         }
     }
@@ -157,25 +173,13 @@ public class CSourceCodeWriter {
     }
 
     public Path writeFile(String fileName) {
-        return writeFile(fileName, true);
-    }
-
-    public Path writeFile(String fileName, boolean ensureCorrectExtension) {
         assert currentLine.length() == 0 : "last line not finished";
 
-        String fixedFileName = fileName;
-        String srcFileExtension = Platform.includedIn(Platform.WINDOWS.class) ? CXX_SOURCE_FILE_EXTENSION : C_SOURCE_FILE_EXTENSION;
-        if (!fileName.endsWith(srcFileExtension) && ensureCorrectExtension) {
-            fixedFileName = fileName.concat(srcFileExtension);
-        }
-
-        Path outputFile = tempDirectory.resolve(fixedFileName);
-        Charset charset = Charset.forName(CHARSET);
-
-        try (BufferedWriter writer = Files.newBufferedWriter(outputFile, charset)) {
+        Path outputFile = tempDirectory.resolve(fileName);
+        try (BufferedWriter writer = Files.newBufferedWriter(outputFile, StandardCharsets.UTF_8)) {
             for (String line : lines) {
                 writer.write(line);
-                writer.write("\n");
+                writer.newLine();
             }
         } catch (ClosedByInterruptException ex) {
             throw new InterruptImageBuilding();
@@ -186,51 +190,57 @@ public class CSourceCodeWriter {
         return outputFile;
     }
 
-    public static String toCTypeName(ResolvedJavaMethod method, ResolvedJavaType type, boolean isConst, boolean isUnsigned, MetaAccessProvider metaAccess, NativeLibraries nativeLibs) {
-        if (type.getJavaKind().isNumericInteger()) {
+    public static String toCTypeName(ResolvedJavaMethod method, ResolvedJavaType type, Optional<String> useSiteTypedef, boolean isConst, boolean isUnsigned, MetaAccessProvider metaAccess,
+                    NativeLibraries nativeLibs) {
+        boolean isNumericInteger = type.getJavaKind().isNumericInteger();
+        UserError.guarantee(isNumericInteger || !isUnsigned,
+                        "Only integer types can be unsigned. %s is not an integer type in %s", type, method);
+
+        boolean isUnsignedWord = metaAccess.lookupJavaType(UnsignedWord.class).isAssignableFrom(type);
+        boolean isSignedWord = metaAccess.lookupJavaType(SignedWord.class).isAssignableFrom(type);
+        boolean isWord = isUnsignedWord || isSignedWord;
+        boolean isObject = type.getJavaKind() == JavaKind.Object && !isWord;
+        UserError.guarantee(isObject || !isConst,
+                        "Only pointer types can be const. %s in method %s is not a pointer type.", type, method);
+
+        if (useSiteTypedef.isPresent()) {
+            return (isConst ? "const " : "") + useSiteTypedef.get();
+        } else if (isNumericInteger) {
             return toCIntegerType(type, isUnsigned);
-        }
-        UserError.guarantee(!isUnsigned,
-                        "Only integer types can be unsigned. " + type.getJavaKind().getJavaName() + " is not an integer type in " + method.format("%H.%n(%p)"));
-
-        if (type.getJavaKind() == JavaKind.Object) {
-            return cTypeForObject(type, isConst, metaAccess, nativeLibs);
-        }
-
-        UserError.guarantee(!isConst,
-                        "Only pointer types can be const. " + type.getJavaKind().getJavaName() + " in method " + method.format("%H.%n(%p)") + " is not an pointer type.");
-
-        switch (type.getJavaKind()) {
-            case Double:
-                return "double";
-            case Float:
-                return "float";
-            case Void:
-                return "void";
-            default:
-                throw shouldNotReachHere();
+        } else if (isUnsignedWord) {
+            return "size_t";
+        } else if (isSignedWord) {
+            return "ssize_t";
+        } else if (isObject) {
+            return (isConst ? "const " : "") + cTypeForObject(type, metaAccess, nativeLibs);
+        } else {
+            switch (type.getJavaKind()) {
+                case Double:
+                    return "double";
+                case Float:
+                    return "float";
+                case Void:
+                    return "void";
+                default:
+                    throw shouldNotReachHere();
+            }
         }
     }
 
-    private static String cTypeForObject(ResolvedJavaType type, boolean isConst, MetaAccessProvider metaAccess, NativeLibraries nativeLibs) {
-        String prefix = isConst ? "const " : "";
+    private static String cTypeForObject(ResolvedJavaType type, MetaAccessProvider metaAccess, NativeLibraries nativeLibs) {
         ElementInfo elementInfo = nativeLibs.findElementInfo(type);
         if (elementInfo instanceof PointerToInfo) {
             PointerToInfo pointerToInfo = (PointerToInfo) elementInfo;
-            return (pointerToInfo.getTypedefName() != null ? pointerToInfo.getTypedefName() : prefix + pointerToInfo.getName() + "*");
+            return (pointerToInfo.getTypedefName() != null ? pointerToInfo.getTypedefName() : pointerToInfo.getName() + "*");
         } else if (elementInfo instanceof StructInfo) {
             StructInfo structInfo = (StructInfo) elementInfo;
-            return structInfo.getTypedefName() != null ? structInfo.getTypedefName() : prefix + structInfo.getName() + "*";
+            return structInfo.getTypedefName() != null ? structInfo.getTypedefName() : structInfo.getName() + "*";
         } else if (elementInfo instanceof EnumInfo) {
             return elementInfo.getName();
-        } else if (metaAccess.lookupJavaType(UnsignedWord.class).isAssignableFrom(type)) {
-            return "size_t";
-        } else if (metaAccess.lookupJavaType(SignedWord.class).isAssignableFrom(type)) {
-            return "ssize_t";
         } else if (isFunctionPointer(metaAccess, type)) {
-            return InfoTreeBuilder.getTypedefName(type) != null ? InfoTreeBuilder.getTypedefName(type) : prefix + "void *";
+            return InfoTreeBuilder.getTypedefName(type) != null ? InfoTreeBuilder.getTypedefName(type) : "void *";
         }
-        return prefix + "void *";
+        return "void *";
     }
 
     private static String toCIntegerType(ResolvedJavaType type, boolean isUnsigned) {
@@ -249,7 +259,6 @@ public class CSourceCodeWriter {
             case Byte:
                 return prefix + (c11Compatible ? "int8_t" : "char");
             case Char:
-                return prefix + (c11Compatible ? "int16_t" : "short");
             case Short:
                 return prefix + (c11Compatible ? "int16_t" : "short");
             case Int:
@@ -270,6 +279,8 @@ public class CSourceCodeWriter {
      * Appends definition of "flags" like macro.
      */
     public void appendMacroDefinition(String preDefine) {
+        appendln("#ifndef " + preDefine);
         appendln("#define " + preDefine);
+        appendln("#endif");
     }
 }
