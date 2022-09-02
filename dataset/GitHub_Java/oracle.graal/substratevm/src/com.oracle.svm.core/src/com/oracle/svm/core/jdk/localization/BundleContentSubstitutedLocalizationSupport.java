6782453e248b3459574fd92ce3d14882be82c504
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:06:53 +0200

    Localization: compress the bundles in parallel

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/localization/BundleContentSubstitutedLocalizationSupport.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/localization/BundleContentSubstitutedLocalizationSupport.java
@@ -24,11 +24,11 @@
  */
 package com.oracle.svm.core.jdk.localization;
 
+import java.util.List;
 import java.util.ListResourceBundle;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ForkJoinPool;
 
@@ -38,8 +38,6 @@ import com.oracle.svm.core.jdk.localization.compression.GzipBundleCompression;
 import org.graalvm.compiler.debug.GraalError;
 
 // Checkstyle: stop
-import org.graalvm.nativeimage.Platform;
-import org.graalvm.nativeimage.Platforms;
 import sun.util.resources.OpenListResourceBundle;
 import sun.util.resources.ParallelListResourceBundle;
 // Checkstyle: resume
@@ -50,15 +48,16 @@ public class BundleContentSubstitutedLocalizationSupport extends LocalizationSup
 
     private final Map<Class<?>, StoredBundle> storedBundles = new ConcurrentHashMap<>();
 
+    private final GzipBundleCompression compressionAlgorithm = new GzipBundleCompression();
+
     private final ForkJoinPool pool;
 
-    public BundleContentSubstitutedLocalizationSupport(Locale defaultLocale, Set<Locale> locales, ForkJoinPool pool) {
+    public BundleContentSubstitutedLocalizationSupport(Locale defaultLocale, List<Locale> locales, ForkJoinPool pool) {
         super(defaultLocale, locales);
         this.pool = pool;
     }
 
     @Override
-    @Platforms(Platform.HOSTED_ONLY.class)
     protected void onBundlePrepared(ResourceBundle bundle) {
         if (isBundleSupported(bundle)) {
             if (pool != null) {
@@ -69,17 +68,15 @@ public class BundleContentSubstitutedLocalizationSupport extends LocalizationSup
         }
     }
 
-    @Platforms(Platform.HOSTED_ONLY.class)
     private void storeBundleContentOf(ResourceBundle bundle) {
         GraalError.guarantee(isBundleSupported(bundle), "Unsupported bundle %s of type %s", bundle, bundle.getClass());
         storedBundles.put(bundle.getClass(), processBundle(bundle));
     }
 
-    @Platforms(Platform.HOSTED_ONLY.class)
     private StoredBundle processBundle(ResourceBundle bundle) {
         boolean isInDefaultLocale = bundle.getLocale().equals(defaultLocale);
-        if (!isInDefaultLocale && GzipBundleCompression.canCompress(bundle)) {
-            return GzipBundleCompression.compress(bundle);
+        if (!isInDefaultLocale && compressionAlgorithm.canCompress(bundle)) {
+            return compressionAlgorithm.compress(bundle);
         }
         Map<String, Object> content = extractContent(bundle);
         return new ExtractedBundle(content);
@@ -98,7 +95,6 @@ public class BundleContentSubstitutedLocalizationSupport extends LocalizationSup
         return super.getBundleContentOf(bundleClass);
     }
 
-    @Platforms(Platform.HOSTED_ONLY.class)
     public boolean isBundleSupported(ResourceBundle bundle) {
         return bundle instanceof ListResourceBundle || bundle instanceof OpenListResourceBundle || bundle instanceof ParallelListResourceBundle;
     }
