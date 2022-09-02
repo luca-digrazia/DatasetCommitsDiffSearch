b3fcfcdbbdc7a40da205b8d4dd16db099165003f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:17:18 +0200

    Every float can be exactly represented as a double

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.interop/src/com/oracle/truffle/api/interop/DefaultFloatExports.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.interop/src/com/oracle/truffle/api/interop/DefaultFloatExports.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -40,12 +40,14 @@
  */
 package com.oracle.truffle.api.interop;
 
-import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
+import com.oracle.truffle.api.source.SourceSection;
 
 @SuppressWarnings("unused")
-@ExportLibrary(value = InteropLibrary.class, receiverClass = Float.class)
+@ExportLibrary(value = InteropLibrary.class, receiverType = Float.class)
 final class DefaultFloatExports {
 
     // slow method
@@ -95,23 +97,12 @@ final class DefaultFloatExports {
     }
 
     @ExportMessage
-    static boolean fitsInDouble(Float receiver) {
-        float f = receiver;
-        double d = f;
-        if (!Float.isFinite(f) || d == f) {
-            return true;
-        }
-        return false;
-    }
-
-    @ExportMessage
     static byte asByte(Float receiver) throws UnsupportedMessageException {
         float f = receiver;
         byte b = (byte) f;
         if (b == f && !NumberUtils.isNegativeZero(f)) {
             return b;
         }
-        CompilerDirectives.transferToInterpreter();
         throw UnsupportedMessageException.create();
     }
 
@@ -122,7 +113,6 @@ final class DefaultFloatExports {
         if (s == f && !NumberUtils.isNegativeZero(f)) {
             return s;
         }
-        CompilerDirectives.transferToInterpreter();
         throw UnsupportedMessageException.create();
     }
 
@@ -136,7 +126,6 @@ final class DefaultFloatExports {
                 return i;
             }
         }
-        CompilerDirectives.transferToInterpreter();
         throw UnsupportedMessageException.create();
     }
 
@@ -149,18 +138,6 @@ final class DefaultFloatExports {
                 return l;
             }
         }
-        CompilerDirectives.transferToInterpreter();
-        throw UnsupportedMessageException.create();
-    }
-
-    @ExportMessage
-    static double asDouble(Float receiver) throws UnsupportedMessageException {
-        float f = receiver;
-        double d = f;
-        if (!Float.isFinite(f) || d == f) {
-            return d;
-        }
-        CompilerDirectives.transferToInterpreter();
         throw UnsupportedMessageException.create();
     }
 
@@ -178,6 +155,56 @@ final class DefaultFloatExports {
     @ExportMessage
     static float asFloat(Float receiver) {
         return receiver;
+    }
+
+    @ExportMessage
+    static boolean fitsInDouble(Float receiver) {
+        return true;
+    }
+
+    @ExportMessage
+    static double asDouble(Float receiver) {
+        return receiver;
+    }
+
+    /*
+     * We export these messages explicitly because the legacy default is very costly. Remove with
+     * the complicated legacy implementation in InteropLibrary.
+     */
+    @ExportMessage
+    static boolean hasLanguage(Float receiver) {
+        return false;
+    }
+
+    @ExportMessage
+    static Class<? extends TruffleLanguage<?>> getLanguage(Float receiver) throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    static boolean hasSourceLocation(Float receiver) {
+        return false;
+    }
+
+    @ExportMessage
+    static SourceSection getSourceLocation(Float receiver) throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    static boolean hasMetaObject(Float receiver) {
+        return false;
+    }
+
+    @ExportMessage
+    static Object getMetaObject(Float receiver) throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    @TruffleBoundary
+    static Object toDisplayString(Float receiver, boolean allowSideEffects) {
+        return receiver.toString();
     }
 
 }
