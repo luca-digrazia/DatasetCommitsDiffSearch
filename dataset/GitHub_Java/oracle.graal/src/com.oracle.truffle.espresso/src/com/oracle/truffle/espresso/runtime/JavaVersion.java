700c231c8c2049a256990c1975676b18c0d0f961
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 14:40:43 +0200

    Set correct guest Java version properties depending on the guest Java version.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/JavaVersion.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/JavaVersion.java
@@ -60,10 +60,6 @@ public final class JavaVersion {
         return java9OrLater();
     }
 
-    public int classFileVersion() {
-        return version + 44;
-    }
-
     @Override
     public String toString() {
         return Integer.toString(version);
