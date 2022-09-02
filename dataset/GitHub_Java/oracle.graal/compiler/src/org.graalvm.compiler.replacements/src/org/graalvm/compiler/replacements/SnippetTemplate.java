94b34bf66e949059f45fda428154fdcc1d93a201
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:46:02 +0200

    Fix FindBugs warning about possible NPE

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/SnippetTemplate.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/SnippetTemplate.java
@@ -80,15 +80,11 @@ import org.graalvm.compiler.graph.NodeClass;
 import org.graalvm.compiler.graph.NodeMap;
 import org.graalvm.compiler.graph.NodeSourcePosition;
 import org.graalvm.compiler.graph.Position;
-import org.graalvm.compiler.graph.spi.Simplifiable;
-import org.graalvm.compiler.graph.spi.SimplifierTool;
 import org.graalvm.compiler.loop.LoopEx;
 import org.graalvm.compiler.loop.LoopsData;
 import org.graalvm.compiler.loop.phases.LoopTransformations;
 import org.graalvm.compiler.nodeinfo.InputType;
-import org.graalvm.compiler.nodeinfo.NodeCycles;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
-import org.graalvm.compiler.nodeinfo.NodeSize;
 import org.graalvm.compiler.nodes.AbstractBeginNode;
 import org.graalvm.compiler.nodes.AbstractMergeNode;
 import org.graalvm.compiler.nodes.BeginNode;
@@ -706,6 +702,18 @@ public class SnippetTemplate {
     // These values must be compared with equals() not '==' to support replay compilation.
     private static final Object UNUSED_PARAMETER = "UNUSED_PARAMETER";
     private static final Object CONSTANT_PARAMETER = "CONSTANT_PARAMETER";
+
+    /**
+     * Determines if any parameter of a given method is annotated with {@link ConstantParameter}.
+     */
+    public static boolean hasConstantParameter(ResolvedJavaMethod method) {
+        for (ConstantParameter p : method.getParameterAnnotations(ConstantParameter.class)) {
+            if (p != null) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     private final SnippetReflectionProvider snippetReflection;
 
@@ -1596,7 +1604,6 @@ public class SnippetTemplate {
 
             // Replace all usages of the replacee with the value returned by the snippet
             ValueNode returnValue = null;
-            AbstractBeginNode originalWithExceptionNextNode = null;
             if (returnNode != null && !(replacee instanceof ControlSinkNode)) {
                 ReturnNode returnDuplicate = (ReturnNode) duplicates.get(returnNode);
                 returnValue = returnDuplicate.result();
@@ -1614,7 +1621,7 @@ public class SnippetTemplate {
                         fwn.setNext(null);
                     } else if (replacee instanceof WithExceptionNode) {
                         WithExceptionNode withExceptionNode = (WithExceptionNode) replacee;
-                        next = originalWithExceptionNextNode = withExceptionNode.next();
+                        next = withExceptionNode.next();
                         withExceptionNode.setNext(null);
                     }
                     returnDuplicate.replaceAndDelete(next);
@@ -1632,31 +1639,7 @@ public class SnippetTemplate {
                 GraphUtil.killCFG(unwindPathDuplicate);
 
             } else {
-                /*
-                 * Since the snippet unwindPath is null, a placeholder WithExceptionNode needs to be
-                 * added for any WithExceptionNode replacee. This placeholder WithExceptionNode
-                 * temporarily preserves the replacee's original exception edge and is needed
-                 * because lowering should not remove edges from the original CFG.
-                 */
-                if (replacee instanceof WithExceptionNode) {
-                    GraalError.guarantee(!replacee.graph().isAfterFloatingReadPhase(), "Using a snippet with an UnwindNode after floating reads would require support for the memory graph");
-                    GraalError.guarantee(originalWithExceptionNextNode != null, "Need to have next node to link placeholder to.");
-
-                    WithExceptionNode newExceptionNode = replacee.graph().add(new PlaceholderWithExceptionNode());
-
-                    /*
-                     * First attaching placeholder as predecessor of original WithExceptionNode next
-                     * edge.
-                     */
-                    ((FixedWithNextNode) originalWithExceptionNextNode.predecessor()).setNext(newExceptionNode);
-                    newExceptionNode.setNext(originalWithExceptionNextNode);
-
-                    /* Now connecting exception edge. */
-                    WithExceptionNode oldExceptionNode = (WithExceptionNode) replacee;
-                    AbstractBeginNode exceptionEdge = oldExceptionNode.exceptionEdge();
-                    oldExceptionNode.setExceptionEdge(null);
-                    newExceptionNode.setExceptionEdge(exceptionEdge);
-                }
+                GraalError.guarantee(!(replacee instanceof WithExceptionNode), "Snippet does not have an UnwindNode, but replacee is a node with an exception handler");
             }
 
             if (killReplacee) {
@@ -2088,27 +2071,5 @@ public class SnippetTemplate {
 
     public void setMayRemoveLocation(boolean mayRemoveLocation) {
         this.mayRemoveLocation = mayRemoveLocation;
-    }
-}
-
-/**
- * This class represent a temporary WithExceptionNode which will be removed during the following
- * simplification phase. This class is needed during lowering to temporarily preserve the original
- * CFG edges for select snippet lowerings.
- */
-@NodeInfo(size = NodeSize.SIZE_0, cycles = NodeCycles.CYCLES_0, cyclesRationale = "This node is immediately removed on next simplification pass")
-final class PlaceholderWithExceptionNode extends WithExceptionNode implements Simplifiable {
-    static final NodeClass<PlaceholderWithExceptionNode> TYPE = NodeClass.create(PlaceholderWithExceptionNode.class);
-
-    protected PlaceholderWithExceptionNode() {
-        super(TYPE, StampFactory.forVoid());
-    }
-
-    @Override
-    public void simplify(SimplifierTool tool) {
-        if (exceptionEdge != null) {
-            killExceptionEdge();
-        }
-        graph().removeSplit(this, next());
     }
 }
