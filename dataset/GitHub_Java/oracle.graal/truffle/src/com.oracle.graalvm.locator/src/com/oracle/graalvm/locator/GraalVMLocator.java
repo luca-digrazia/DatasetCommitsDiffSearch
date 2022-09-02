920ea24891ec2b7035145ddd96f25d79a30e428a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:35:56 +0200

    Remove deprecated graalvm.version property, use the HomeFinder instead
        * The property org.graalvm.version remains available but might not be in the future.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.graalvm.locator/src/com/oracle/graalvm/locator/GraalVMLocator.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.graalvm.locator/src/com/oracle/graalvm/locator/GraalVMLocator.java
@@ -1,30 +1,47 @@
 /*
- * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * (a) the Software, and
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.oracle.graalvm.locator;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -45,7 +62,7 @@ import com.oracle.truffle.api.impl.TruffleLocator;
 public final class GraalVMLocator extends TruffleLocator
                 implements Callable<ClassLoader> {
 
-    private static final boolean LOCATOR_TRACE = Boolean.valueOf(System.getProperty("truffle.class.path.trace", "false"));
+    private static final boolean LOCATOR_TRACE = Boolean.parseBoolean(System.getProperty("truffle.class.path.trace", "false"));
 
     private static URLClassLoader loader;
 
@@ -56,17 +73,12 @@ public final class GraalVMLocator extends TruffleLocator
         Path homePath = homeFinder.getHomeFolder();
         if (homePath != null) {
             String home = homePath.toString();
-            if (System.getProperty("graalvm.home") == null) {
-                // automatically set graalvm.home
-                System.setProperty("graalvm.home", home);
-            }
             if (System.getProperty("org.graalvm.home") == null) {
-                // automatically set graalvm.home
+                // automatically set org.graalvm.home
                 System.setProperty("org.graalvm.home", home);
             }
         }
         String version = homeFinder.getVersion();
-        System.setProperty("graalvm.version", version);
         System.setProperty("org.graalvm.version", version);
         for (Map.Entry<String, Path> languageHome : homeFinder.getLanguageHomes().entrySet()) {
             setLanguageHomeProperty(languageHome.getKey(), languageHome.getValue());
@@ -98,9 +110,10 @@ public final class GraalVMLocator extends TruffleLocator
             }
         }
         if (LOCATOR_TRACE) {
-            System.out.println("Setting up Truffle GuestLanguageTools classpath:");
+            PrintStream out = System.out;
+            out.println("Setting up Truffle GuestLanguageTools classpath:");
             for (URL url : classPath) {
-                System.out.println(url);
+                out.println(url);
             }
         }
         return classPath;
