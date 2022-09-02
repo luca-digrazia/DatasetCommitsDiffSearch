57b8f0182b74bbc6a78191986d47ff8338c20797
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 23:39:57 +0200

    fixed jdk11 related issues

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.libgraal.jdk11/src/org/graalvm/libgraal/LibGraal.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.libgraal.jdk11/src/org/graalvm/libgraal/LibGraal.java
@@ -24,154 +24,63 @@
  */
 package org.graalvm.libgraal;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-
 import jdk.vm.ci.hotspot.HotSpotJVMCIRuntime;
-import jdk.vm.ci.services.Services;
 
 /**
- * JDK11 version of {@code LibGraal}.
+ * JDK11+ version of {@code LibGraal}.
  */
 public class LibGraal {
 
-    static final MethodHandle runtimeUnhandMethod;
-    static final MethodHandle runtimeTranslateMethod;
-    static final MethodHandle runtimeRegisterNativeMethods;
-    static final MethodHandle runtimeIsCurrentThreadAttached;
-    static final MethodHandle runtimeAttachCurrentThread;
-    static final MethodHandle runtimeDetachCurrentThread;
+    static final long isolate = 0L;
 
-    static {
-        MethodHandle unhand = null;
-        MethodHandle translate = null;
-        MethodHandle registerNativeMethods = null;
-        MethodHandle isCurrentThreadAttached = null;
-        MethodHandle attachCurrentThread = null;
-        MethodHandle detachCurrentThread = null;
-        boolean firstFound = false;
-        try {
-            Class<?> runtimeClass = HotSpotJVMCIRuntime.class;
-            unhand = MethodHandles.lookup().unreflect(runtimeClass.getDeclaredMethod("unhand", Class.class, Long.TYPE));
-            firstFound = true;
-            translate = MethodHandles.lookup().unreflect(runtimeClass.getDeclaredMethod("translate", Object.class));
-            registerNativeMethods = MethodHandles.lookup().unreflect(runtimeClass.getDeclaredMethod("registerNativeMethods", Class.class));
-            isCurrentThreadAttached = MethodHandles.lookup().unreflect(runtimeClass.getDeclaredMethod("isCurrentThreadAttached"));
-            attachCurrentThread = MethodHandles.lookup().unreflect(runtimeClass.getDeclaredMethod("attachCurrentThread", Boolean.TYPE));
-            detachCurrentThread = MethodHandles.lookup().unreflect(runtimeClass.getDeclaredMethod("detachCurrentThread"));
-        } catch (Exception e) {
-            // If the very first method is unavailable assume nothing is available. Otherwise only
-            // some are missing so complain about it.
-            if (firstFound) {
-                throw new InternalError("some methods are unavailable", e);
-            }
-        }
-
-        runtimeUnhandMethod = unhand;
-        runtimeTranslateMethod = translate;
-        runtimeRegisterNativeMethods = registerNativeMethods;
-        runtimeIsCurrentThreadAttached = isCurrentThreadAttached;
-        runtimeAttachCurrentThread = attachCurrentThread;
-        runtimeDetachCurrentThread = detachCurrentThread;
+    private static IllegalStateException unavailable() {
+        throw new IllegalStateException("libgraal is not available");
     }
 
     public static boolean isAvailable() {
-        return inLibGraal() || isolate != 0L;
+        return false;
     }
 
     public static boolean inLibGraal() {
-        return Services.IS_IN_NATIVE_IMAGE;
+        throw unavailable();
     }
 
+    public static long getIsolate() {
+        throw unavailable();
+    }
+
+    @SuppressWarnings("unused")
     public static void registerNativeMethods(HotSpotJVMCIRuntime runtime, Class<?> clazz) {
-        if (clazz.isPrimitive()) {
-            throw new IllegalArgumentException();
-        }
-        if (inLibGraal() || !isAvailable()) {
-            throw new IllegalStateException();
-        }
-        try {
-            runtimeRegisterNativeMethods.invoke(runtime, clazz);
-        } catch (Error e) {
-            throw e;
-        } catch (Throwable throwable) {
-            throw new InternalError(throwable);
-        }
+        throw unavailable();
     }
 
+    @SuppressWarnings("unused")
     public static long translate(HotSpotJVMCIRuntime runtime, Object obj) {
-        if (!isAvailable()) {
-            throw new IllegalStateException();
-        }
-        if (!inLibGraal() && LibGraalScope.currentScope.get() == null) {
-            throw new IllegalStateException("Not within a " + LibGraalScope.class.getName());
-        }
-        try {
-            return (long) runtimeTranslateMethod.invoke(runtime, obj);
-        } catch (Throwable throwable) {
-            throw new InternalError(throwable);
-        }
+        throw unavailable();
     }
 
+    @SuppressWarnings("unused")
     public static <T> T unhand(HotSpotJVMCIRuntime runtime, Class<T> type, long handle) {
-        if (!isAvailable()) {
-            throw new IllegalStateException();
-        }
-        if (!inLibGraal() && LibGraalScope.currentScope.get() == null) {
-            throw new IllegalStateException("Not within a " + LibGraalScope.class.getName());
-        }
-        try {
-            return (T) runtimeUnhandMethod.invoke(runtime, type, handle);
-        } catch (Throwable throwable) {
-            throw new InternalError(throwable);
-        }
+        throw unavailable();
     }
 
-    private static long initializeLibgraal() {
-        if (runtimeRegisterNativeMethods == null) {
-            return 0L;
-        }
-        try {
-            // Initialize JVMCI to ensure JVMCI opens its packages to
-            // Graal otherwise the call to HotSpotJVMCIRuntime.runtime()
-            // below might fail
-            Services.initializeJVMCI();
-
-            HotSpotJVMCIRuntime runtime = HotSpotJVMCIRuntime.runtime();
-            long[] nativeInterface = (long[]) runtimeRegisterNativeMethods.invoke(runtime, LibGraal.class);
-            return nativeInterface[1];
-        } catch (UnsupportedOperationException e) {
-            return 0L;
-        } catch (Throwable throwable) {
-            throw new InternalError(throwable);
-        }
-    }
-
-    static final long isolate = Services.IS_BUILDING_NATIVE_IMAGE ? 0L : initializeLibgraal();
-
+    @SuppressWarnings("unused")
     static boolean isCurrentThreadAttached(HotSpotJVMCIRuntime runtime) {
-        try {
-            return (boolean) runtimeIsCurrentThreadAttached.invoke(runtime);
-        } catch (Throwable throwable) {
-            throw new InternalError(throwable);
-        }
+        throw unavailable();
     }
 
+    @SuppressWarnings("unused")
     static boolean attachCurrentThread(HotSpotJVMCIRuntime runtime) {
-        try {
-            return (boolean) runtimeAttachCurrentThread.invoke(runtime, false);
-        } catch (Throwable throwable) {
-            throw new InternalError(throwable);
-        }
+        throw unavailable();
     }
 
+    @SuppressWarnings("unused")
     static void detachCurrentThread(HotSpotJVMCIRuntime runtime) {
-        try {
-            runtimeDetachCurrentThread.invoke(runtime);
-        } catch (Throwable throwable) {
-            throw new InternalError(throwable);
-        }
+        throw unavailable();
     }
 
-    static native long getCurrentIsolateThread(long iso);
+    @SuppressWarnings("unused")
+    static long getCurrentIsolateThread(long iso) {
+        throw unavailable();
+    }
 }
