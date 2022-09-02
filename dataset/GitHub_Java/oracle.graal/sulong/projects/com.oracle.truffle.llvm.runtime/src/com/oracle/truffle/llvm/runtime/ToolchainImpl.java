553c51ecc216c8d8b07dd08499773c6a4d8f1164
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 10:36:10 +0200

    sulong: ToolchainAPI needs to add .exe or .cmd on Windows [GR-28961]

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/ToolchainImpl.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/ToolchainImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2021, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -32,9 +32,15 @@ package com.oracle.truffle.llvm.runtime;
 import com.oracle.truffle.api.TruffleFile;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.llvm.api.Toolchain;
+import com.oracle.truffle.llvm.runtime.nodes.asm.syscall.LLVMInfo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public final class ToolchainImpl implements Toolchain {
 
+    private static final String[] WINDOWS_EXECUTABLE_EXTENSIONS = {".exe", ".cmd"};
     private final ToolchainConfig toolchainConfig;
     private final LLVMLanguage language;
 
@@ -45,22 +51,102 @@ public final class ToolchainImpl implements Toolchain {
 
     @Override
     public TruffleFile getToolPath(String tool) {
+        TruffleFile res = getToolPathImpl(tool);
+        if (LLVMInfo.SYSNAME.toLowerCase().contains("windows")) {
+            /*
+             * On Windows the tools have either a .exe or a .cmd suffix. ATM there is no other way
+             * than to see if the file exists.
+             */
+            TruffleFile parent = res.getParent();
+            String name = res.getName();
+            for (String ext : WINDOWS_EXECUTABLE_EXTENSIONS) {
+                TruffleFile fileWithExt = parent.resolve(name + ext);
+                try {
+                    if (fileWithExt.exists()) {
+                        return fileWithExt;
+                    }
+                } catch (Throwable e) {
+                    // ignore if call to exists() fails
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Please keep this list in sync with Toolchain.java (method documentation) and mx_sulong.py's
+     * ToolchainConfig::_tool_map.
+     */
+    private TruffleFile getToolPathImpl(String tool) {
+
         if (toolchainConfig == null) {
             return null;
         }
-        TruffleLanguage.Env env = language.getContextReference().get().getEnv();
-        String toolchainRoot = toolchainConfig.getToolchainRootOverride();
-        TruffleFile root = toolchainRoot != null //
-                        ? env.getTruffleFile(toolchainRoot)
-                        : env.getTruffleFile(language.getLLVMLanguageHome()).resolve(toolchainConfig.getToolchainSubdir());
+
+        final TruffleFile binPrefix = getWrappersRoot().resolve("bin");
         switch (tool) {
+            case "PATH":
+                return binPrefix;
             case "CC":
-                return root.resolve("bin").resolve("graalvm-" + toolchainConfig.getToolchainSubdir() + "-clang");
+                return binPrefix.resolve("graalvm-" + toolchainConfig.getToolchainSubdir() + "-clang");
             case "CXX":
-                return root.resolve("bin").resolve("graalvm-" + toolchainConfig.getToolchainSubdir() + "-clang++");
+                if (!toolchainConfig.enableCXX()) {
+                    return null;
+                }
+                return binPrefix.resolve("graalvm-" + toolchainConfig.getToolchainSubdir() + "-clang++");
+            case "LD":
+                return binPrefix.resolve("graalvm-" + toolchainConfig.getToolchainSubdir() + "-ld");
+            case "AR":
+            case "NM":
+            case "OBJCOPY":
+            case "OBJDUMP":
+            case "RANLIB":
+            case "READELF":
+            case "READOBJ":
+            case "STRIP":
+                return binPrefix.resolve(tool.toLowerCase());
             default:
                 return null;
         }
+    }
+
+    @Override
+    public List<TruffleFile> getPaths(String pathName) {
+        if (toolchainConfig == null) {
+            return null;
+        }
+
+        switch (pathName) {
+            case "PATH":
+                return Collections.unmodifiableList(Arrays.asList(getWrappersRoot().resolve("bin")));
+            case "LD_LIBRARY_PATH":
+                return Collections.unmodifiableList(Arrays.asList(getSysroot().resolve("lib")));
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Returns the directory where the toolchain wrappers live.
+     *
+     * Usually this is the same as {@link #getSysroot}, but for the bootstrap toolchain the wrappers
+     * are in a different location.
+     */
+    private TruffleFile getWrappersRoot() {
+        TruffleLanguage.Env env = LLVMLanguage.getContext().getEnv();
+        String toolchainRoot = toolchainConfig.getToolchainRootOverride();
+        return toolchainRoot != null //
+                        ? env.getInternalTruffleFile(toolchainRoot)
+                        : getSysroot();
+    }
+
+    /**
+     * Returns the "sysroot" of the toolchain. That is the directory where the toolchain specific
+     * header files an libraries are located.
+     */
+    private TruffleFile getSysroot() {
+        TruffleLanguage.Env env = LLVMLanguage.getContext().getEnv();
+        return env.getInternalTruffleFile(language.getLLVMLanguageHome()).resolve(toolchainConfig.getToolchainSubdir());
     }
 
     @Override
@@ -69,5 +155,10 @@ public final class ToolchainImpl implements Toolchain {
             return null;
         }
         return toolchainConfig.getToolchainSubdir();
+    }
+
+    @Override
+    public String toString() {
+        return toolchainConfig.getClass().getSimpleName();
     }
 }
