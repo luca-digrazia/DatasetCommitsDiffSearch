e98de77e4f457f04d96fc56e5f80a50aa91ac9ca
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:35:50 +0200

    Address comments on PR.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/HotSpotGraalRuntimeProvider.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/HotSpotGraalRuntimeProvider.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -22,7 +24,15 @@
  */
 package org.graalvm.compiler.hotspot;
 
+import java.io.PrintStream;
+import java.util.Map;
+
 import org.graalvm.compiler.api.runtime.GraalRuntime;
+import org.graalvm.compiler.core.CompilationWrapper.ExceptionAction;
+import org.graalvm.compiler.core.common.CompilationIdentifier;
+import org.graalvm.compiler.debug.DebugContext;
+import org.graalvm.compiler.debug.DebugHandlersFactory;
+import org.graalvm.compiler.debug.DiagnosticsOutputDirectory;
 import org.graalvm.compiler.hotspot.meta.HotSpotProviders;
 import org.graalvm.compiler.options.OptionValues;
 import org.graalvm.compiler.replacements.SnippetCounter.Group;
@@ -48,10 +58,23 @@ public interface HotSpotGraalRuntimeProvider extends GraalRuntime, RuntimeProvid
         return getClass().getSimpleName();
     }
 
+    HotSpotGraalRuntime.HotSpotGC getGarbageCollector();
+
     @Override
     HotSpotBackend getHostBackend();
 
     GraalHotSpotVMConfig getVMConfig();
+
+    /**
+     * Opens a debug context for compiling {@code compilable}. The {@link DebugContext#close()}
+     * method should be called on the returned object once the compilation is finished.
+     *
+     * @param compilationOptions the options used to configure the compilation debug context
+     * @param compilationId a system wide unique compilation id
+     * @param compilable the input to the compilation
+     * @param logStream the log stream to use in this context
+     */
+    DebugContext openDebugContext(OptionValues compilationOptions, CompilationIdentifier compilationId, Object compilable, Iterable<DebugHandlersFactory> factories, PrintStream logStream);
 
     /**
      * Gets the option values associated with this runtime.
@@ -69,12 +92,24 @@ public interface HotSpotGraalRuntimeProvider extends GraalRuntime, RuntimeProvid
     boolean isShutdown();
 
     /**
-     * Gets a directory into which diagnostics such crash reports and dumps should be written. This
-     * method will create the directory if it doesn't exist so it should only be called if
-     * diagnostics are about to be generated.
-     *
-     * @return the directory into which diagnostics can be written or {@code null} if the directory
-     *         does not exist and could not be created
+     * Gets a directory into which diagnostics such crash reports and dumps should be written.
      */
-    String getOutputDirectory();
+    DiagnosticsOutputDirectory getOutputDirectory();
+
+    /**
+     * Gets the map used to count compilation problems at each {@link ExceptionAction} level. All
+     * updates and queries to the map should be synchronized.
+     */
+    Map<ExceptionAction, Integer> getCompilationProblemsPerAction();
+
+    /**
+     * Returns the unique compiler configuration name that is in use. Useful for users to find out
+     * which configuration is in use.
+     */
+    String getCompilerConfigurationName();
+
+    /**
+     * Returns the instance holding the instrumentation data structures.
+     */
+    Instrumentation getInstrumentation();
 }
