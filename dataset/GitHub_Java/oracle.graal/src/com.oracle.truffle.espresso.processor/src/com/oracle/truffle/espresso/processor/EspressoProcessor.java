5f59ae8e2c2a2d8c8e8e2fedf22bc52431efdc69
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 22:33:28 +0200

    Corrects the name of class that generated collectors.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.processor/src/com/oracle/truffle/espresso/processor/EspressoProcessor.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso.processor/src/com/oracle/truffle/espresso/processor/EspressoProcessor.java
@@ -10,12 +10,10 @@ import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -56,7 +54,7 @@ public abstract class EspressoProcessor extends AbstractProcessor {
      * @see EspressoProcessor#IMPORT_STATIC_OBJECT
      * @see EspressoProcessor#IMPORT_TRUFFLE_OBJECT
      */
-    abstract String generateImports(String className, String targetMethodName, List<String> parameterTypeName, List<String> guestCalls, SubstitutionHelper helper);
+    abstract String generateImports(String className, String targetMethodName, List<String> parameterTypeName, SubstitutionHelper helper);
 
     /**
      * Generates the string corresponding to the Constructor for the current substitutor. In
@@ -64,7 +62,7 @@ public abstract class EspressoProcessor extends AbstractProcessor {
      * 
      * @see EspressoProcessor#SUBSTITUTOR
      */
-    abstract String generateFactoryConstructorBody(String className, String targetMethodName, List<String> parameterTypeName, List<String> guestCalls, SubstitutionHelper helper);
+    abstract String generateConstructor(String className, String targetMethodName, List<String> parameterTypeName, SubstitutionHelper helper);
 
     /**
      * Generates th string that corresponds to the code of the invoke method for the current
@@ -78,7 +76,7 @@ public abstract class EspressoProcessor extends AbstractProcessor {
      * @see EspressoProcessor#FACTORY_IS_NULL
      * @see EspressoProcessor#STATIC_OBJECT_NULL
      */
-    abstract String generateInvoke(String className, String targetMethodName, List<String> parameterTypeName, List<String> guestCalls, SubstitutionHelper helper);
+    abstract String generateInvoke(String className, String targetMethodName, List<String> parameterTypeName, SubstitutionHelper helper);
 
     EspressoProcessor(String SUBSTITUTION_PACKAGE, String SUBSTITUTOR, String COLLECTOR, String COLLECTOR_INSTANCE_NAME) {
         this.SUBSTITUTION_PACKAGE = SUBSTITUTION_PACKAGE;
@@ -107,15 +105,9 @@ public abstract class EspressoProcessor extends AbstractProcessor {
     protected HashSet<String> classes = new HashSet<>();
     protected StringBuilder collector = null;
 
-    // Special annotations
-    TypeElement guestCall;
-    private static final String GUEST_CALL = "com.oracle.truffle.espresso.substitutions.GuestCall";
-
     // Global constants
-    private static final String FACTORY = "Factory";
-    private static final String FACTORY_INSTANCE = "factory";
-    private static final String FACTORY_GETTER = "getFactory";
-    private static final String COLLECTOR_GETTER = "getCollector";
+    private static final String INSTANCE_NAME = "theInstance";
+    private static final String GETTER = "getInstance";
 
     private static final String COPYRIGHT = "/* Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.\n" +
                     " * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.\n" +
@@ -143,28 +135,15 @@ public abstract class EspressoProcessor extends AbstractProcessor {
     private static final String AT_LINK = "@link ";
     private static final String FACTORY_IS_NULL = "InteropLibrary.getFactory().getUncached().isNull";
     private static final String PRIVATE_STATIC_FINAL = "private static final";
-    private static final String PUBLIC_FINAL = "public final";
-    private static final String PRIVATE_FINAL = "private final";
     private static final String PUBLIC_STATIC_FINAL = "public static final";
-    private static final String PUBLIC_STATIC_FINAL_CLASS = "public static final class ";
     private static final String PUBLIC_FINAL_CLASS = "public final class ";
     private static final String OVERRIDE = "@Override";
-    private static final String SUPPRESS_UNUSED = "@SuppressWarnings(\"unused\")";
 
     static final String STATIC_OBJECT_NULL = "StaticObject.NULL";
 
     static final String IMPORT_INTEROP_LIBRARY = "import com.oracle.truffle.api.interop.InteropLibrary;\n";
     static final String IMPORT_STATIC_OBJECT = "import com.oracle.truffle.espresso.runtime.StaticObject;\n";
     static final String IMPORT_TRUFFLE_OBJECT = "import com.oracle.truffle.api.interop.TruffleObject;\n";
-    static final String IMPORT_META = "import com.oracle.truffle.espresso.meta.Meta;\n";
-    static final String IMPORT_DIRECT_CALL_NODE = "import com.oracle.truffle.api.nodes.DirectCallNode;\n";
-
-    static final String META_CLASS = "Meta ";
-    static final String META_VAR = "meta";
-    static final String META_ARG = META_CLASS + META_VAR;
-
-    static final String DIRECT_CALL_NODE = "DirectCallNode";
-    static final String CREATE = "create";
 
     static final String PUBLIC_FINAL_OBJECT = "public final Object ";
     static final String ARGS_NAME = "args";
@@ -172,7 +151,6 @@ public abstract class EspressoProcessor extends AbstractProcessor {
     static final String TAB_1 = "    ";
     static final String TAB_2 = TAB_1 + TAB_1;
     static final String TAB_3 = TAB_2 + TAB_1;
-    static final String TAB_4 = TAB_3 + TAB_1;
 
     private static final Map<String, NativeSimpleType> classToNative = buildClassToNative();
 
@@ -212,7 +190,6 @@ public abstract class EspressoProcessor extends AbstractProcessor {
         if (done) {
             return false;
         }
-        guestCall = processingEnv.getElementUtils().getTypeElement(GUEST_CALL);
         processImpl(roundEnv);
         // We are done, push the collector.
         commitFiles();
@@ -244,9 +221,9 @@ public abstract class EspressoProcessor extends AbstractProcessor {
         collector.append(IMPORTS_COLLECTOR).append("\n");
         collector.append("// ").append(GENERATED_BY).append(SUBSTITUTOR).append("\n");
         collector.append(PUBLIC_FINAL_CLASS).append(COLLECTOR).append(" {\n");
-        collector.append(generateInstance("ArrayList<>", COLLECTOR_INSTANCE_NAME, "List<" + SUBSTITUTOR + "." + FACTORY + ">"));
+        collector.append(generateInstance("ArrayList<>", COLLECTOR_INSTANCE_NAME, "List<" + SUBSTITUTOR + ">"));
         collector.append(TAB_1).append("private ").append(COLLECTOR).append("() {\n").append(TAB_1).append("}\n");
-        collector.append(generateGetter(COLLECTOR_INSTANCE_NAME, "List<" + SUBSTITUTOR + "." + FACTORY + ">", COLLECTOR_GETTER)).append("\n");
+        collector.append(generateGetter(COLLECTOR_INSTANCE_NAME, "List<" + SUBSTITUTOR + ">", GETTER)).append("\n");
         collector.append(TAB_1).append("static {\n");
     }
 
@@ -267,7 +244,7 @@ public abstract class EspressoProcessor extends AbstractProcessor {
     }
 
     void addSubstitutor(StringBuilder str, String substitutorName) {
-        str.append(TAB_2).append(COLLECTOR_INSTANCE_NAME).append(".add(").append(substitutorName).append(".").append(FACTORY_GETTER).append("()").append(");\n");
+        str.append(TAB_2).append(COLLECTOR_INSTANCE_NAME).append(".add(").append(substitutorName).append(".").append(GETTER).append("()").append(");\n");
     }
 
     static String castTo(String obj, String clazz) {
@@ -328,18 +305,17 @@ public abstract class EspressoProcessor extends AbstractProcessor {
         }
     }
 
-    @SuppressWarnings("unused")
-    static String generateGeneratedBy(String className, String targetMethodName, List<String> parameterTypes, List<String> guestCalls) {
+    static String generateGeneratedBy(String className, String targetMethodName, List<String> parameterTypes) {
         StringBuilder str = new StringBuilder();
         str.append("/**\n * ").append(GENERATED_BY).append("{").append(AT_LINK).append(className).append("#").append(targetMethodName).append("(");
         boolean first = true;
         for (String param : parameterTypes) {
-            first = checkFirst(str, first);
+            if (first) {
+                first = false;
+            } else {
+                str.append(", ");
+            }
             str.append(param);
-        }
-        for (String call : guestCalls) {
-            first = checkFirst(str, first);
-            str.append(DIRECT_CALL_NODE);
         }
         str.append(")}\n */");
         return str.toString();
@@ -364,22 +340,6 @@ public abstract class EspressoProcessor extends AbstractProcessor {
         StringBuilder str = new StringBuilder();
         str.append(TAB_1).append(PRIVATE_STATIC_FINAL).append(" ").append(instanceClass).append(" ").append(instanceName);
         str.append(" = new ").append(substitutorName).append("();\n");
-        return str.toString();
-    }
-
-    String generateFactory(String className, String targetMethodName, List<String> parameterTypeName, List<String> guestCalls, SubstitutionHelper helper) {
-        StringBuilder str = new StringBuilder();
-        str.append(TAB_1).append(PUBLIC_STATIC_FINAL_CLASS).append(FACTORY).append(" extends ").append(SUBSTITUTOR).append(".").append(FACTORY).append(" {\n");
-        str.append(TAB_2).append("private ").append(FACTORY).append("() {\n");
-        str.append(generateFactoryConstructorBody(className, targetMethodName, parameterTypeName, guestCalls, helper)).append("\n");
-        str.append(TAB_2).append(OVERRIDE).append("\n");
-        str.append(TAB_2).append(PUBLIC_FINAL).append(" ").append(SUBSTITUTOR).append(" ").append(CREATE).append("(");
-        str.append(META_CLASS).append(META_VAR).append(") {\n");
-        str.append(TAB_3).append("return new ").append(className).append("(").append(META_VAR).append(");\n");
-        str.append(TAB_2).append("}\n");
-        str.append(TAB_1).append("}\n");
-        str.append(TAB_1).append(PRIVATE_STATIC_FINAL).append(" ").append(FACTORY).append(" ").append(FACTORY_INSTANCE);
-        str.append(" = new ").append(FACTORY).append("();");
         return str.toString();
     }
 
@@ -410,103 +370,35 @@ public abstract class EspressoProcessor extends AbstractProcessor {
      * @param className The name of the class where the substituted method is found.
      * @param targetMethodName The name of the substituted method.
      * @param parameterTypeName The list of *Host* parameter types of the substituted method.
-     * @param guestCalls
      * @param helper A helper structure.
      * @return The string forming the substitutor.
      */
-    String spawnSubstitutor(String className, String targetMethodName, List<String> parameterTypeName, List<String> guestCalls, SubstitutionHelper helper) {
+    String spawnSubstitutor(String className, String targetMethodName, List<String> parameterTypeName, SubstitutionHelper helper) {
         String substitutorName = getSubstitutorClassName(className, targetMethodName, parameterTypeName);
         StringBuilder classFile = new StringBuilder();
         // Header
         classFile.append(COPYRIGHT);
         classFile.append(PACKAGE);
-        classFile.append(IMPORT_META);
-        if (!guestCalls.isEmpty()) {
-            classFile.append(IMPORT_DIRECT_CALL_NODE);
-        }
-        classFile.append(generateImports(substitutorName, targetMethodName, parameterTypeName, guestCalls, helper));
+        classFile.append(generateImports(substitutorName, targetMethodName, parameterTypeName, helper));
 
         // Class
-        classFile.append(generateGeneratedBy(className, targetMethodName, parameterTypeName, guestCalls)).append("\n");
+        classFile.append(generateGeneratedBy(className, targetMethodName, parameterTypeName)).append("\n");
         classFile.append(PUBLIC_FINAL_CLASS).append(substitutorName).append(EXTENSION);
 
-        // Instance Factory
-        classFile.append(generateFactory(substitutorName, targetMethodName, parameterTypeName, guestCalls, helper)).append("\n");
-
-        // Instance variables
-        classFile.append(generateInstanceFields(guestCalls)).append("\n");
+        // Instance
+        classFile.append(generateInstance(substitutorName, INSTANCE_NAME, SUBSTITUTOR)).append("\n");
 
         // Constructor
-        classFile.append(TAB_1).append(SUPPRESS_UNUSED).append("\n");
-        classFile.append(generateConstructor(substitutorName, guestCalls));
+        classFile.append(generateConstructor(substitutorName, targetMethodName, parameterTypeName, helper)).append("\n");
 
         // Getter
-        classFile.append(generateGetter(FACTORY_INSTANCE, FACTORY, FACTORY_GETTER)).append("\n");
+        classFile.append(generateGetter(INSTANCE_NAME, SUBSTITUTOR, GETTER)).append("\n");
 
         // Invoke method
         classFile.append(TAB_1).append(OVERRIDE).append("\n");
-        classFile.append(generateInvoke(className, targetMethodName, parameterTypeName, guestCalls, helper));
+        classFile.append(generateInvoke(className, targetMethodName, parameterTypeName, helper));
 
         // End
         return classFile.toString();
-    }
-
-    static private String generateInstanceFields(List<String> guestCalls) {
-        if (guestCalls.isEmpty()) {
-            return "";
-        }
-        StringBuilder str = new StringBuilder();
-        for (String call : guestCalls) {
-            str.append(TAB_1).append(PRIVATE_FINAL).append(" ").append(DIRECT_CALL_NODE).append(" ").append(call).append(";\n");
-        }
-        return str.toString();
-    }
-
-    static String generateGuestCalls(List<String> guestCalls) {
-        if (guestCalls.isEmpty()) {
-            return "";
-        }
-        StringBuilder str = new StringBuilder();
-        for (String call : guestCalls) {
-            str.append(TAB_2).append(call).append(" = ").append(DIRECT_CALL_NODE).append(".").append(CREATE).append("(");
-            str.append(META_VAR).append(".").append(call).append(".").append("getCallTarget").append("());\n");
-        }
-        return str.toString();
-    }
-
-    List<String> getGuestCalls(ExecutableElement method) {
-        ArrayList<String> guestCalls = new ArrayList<>();
-        for (VariableElement param : method.getParameters()) {
-            if (getAnnotation(param.asType(), guestCall) != null) {
-                guestCalls.add(param.getSimpleName().toString());
-            }
-        }
-        return guestCalls;
-    }
-
-    static boolean checkFirst(StringBuilder str, boolean first) {
-        if (!first) {
-            str.append(", ");
-        }
-        return false;
-    }
-
-    static String getGuestCallsForInvoke(List<String> guestCalls, boolean wasFirst) {
-        StringBuilder str = new StringBuilder();
-        boolean first = wasFirst;
-        for (String call : guestCalls) {
-            first = checkFirst(str, first);
-            str.append("\n");
-            str.append(TAB_3).append(call);
-        }
-        return str.toString();
-    }
-
-    private String generateConstructor(String substitutorName, List<String> guestCalls) {
-        StringBuilder str = new StringBuilder();
-        str.append(TAB_1).append("private ").append(substitutorName).append("(").append(META_ARG).append(") {\n");
-        str.append(generateGuestCalls(guestCalls)).append("\n");
-        str.append(TAB_1).append("}\n");
-        return str.toString();
     }
 }
