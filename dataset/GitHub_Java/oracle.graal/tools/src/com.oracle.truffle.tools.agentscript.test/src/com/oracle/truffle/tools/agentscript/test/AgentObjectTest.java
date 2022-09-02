d65581ecf93bb668161aec0a28c9bdbaddff7b80
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:30:05 +0200

    Addressing ecj lints

--- dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.agentscript.test/src/com/oracle/truffle/tools/agentscript/test/AgentObjectTest.java
+++ dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.agentscript.test/src/com/oracle/truffle/tools/agentscript/test/AgentObjectTest.java
@@ -340,12 +340,8 @@ public class AgentObjectTest {
             Assert.assertNotNull("Agent API obtained", agentAPI);
 
             int[] expressionCounter = {0};
-            int[] expressionReturnCounter = {0};
             agentAPI.on("enter", (ev, frame) -> {
                 expressionCounter[0]++;
-            }, AgentObjectFactory.createConfig(true, false, false, null));
-            agentAPI.on("return", (ev, frame) -> {
-                expressionReturnCounter[0]++;
             }, AgentObjectFactory.createConfig(true, false, false, null));
 
             // @formatter:off
@@ -362,48 +358,6 @@ public class AgentObjectTest {
             c.eval(sampleScript);
 
             assertEquals("10x2 expressions", 20, expressionCounter[0]);
-            assertEquals("Same amount of expressions", expressionCounter[0], expressionReturnCounter[0]);
-        }
-    }
-
-    @Test
-    public void internalScriptsAreIgnored() throws Exception {
-        try (Context c = AgentObjectFactory.newContext()) {
-            Value agent = AgentObjectFactory.createAgentObject(c);
-            AgentScriptAPI agentAPI = agent.as(AgentScriptAPI.class);
-            Assert.assertNotNull("Agent API obtained", agentAPI);
-
-            // @formatter:off
-            Source sampleScript = Source.newBuilder(InstrumentationTestLanguage.ID,
-                "ROOT(\n" +
-                "  DEFINE(foo,\n" +
-                "    LOOP(10, STATEMENT(EXPRESSION,EXPRESSION))\n" +
-                "  ),\n" +
-                "  CALL(foo)\n" +
-                ")",
-                "sample.px"
-            ).internal(true).build();
-
-            final AgentScriptAPI.OnSourceLoadedHandler listener = (ev) -> {
-                if (ev.name().equals(sampleScript.getName())) {
-                    Assert.fail("Don't load internal scripts: " + ev.uri());
-                }
-            };
-            agentAPI.on("source", listener);
-
-
-            int[] expressionCounter = {0};
-            agentAPI.on("enter", (ev, frame) -> {
-                expressionCounter[0]++;
-            }, AgentObjectFactory.createConfig(true, false, false, null));
-            agentAPI.on("return", (ev, frame) -> {
-                expressionCounter[0]++;
-            }, AgentObjectFactory.createConfig(true, false, false, null));
-
-            // @formatter:on
-            c.eval(sampleScript);
-
-            assertEquals("No expressions entered & exited", 0, expressionCounter[0]);
         }
     }
 
