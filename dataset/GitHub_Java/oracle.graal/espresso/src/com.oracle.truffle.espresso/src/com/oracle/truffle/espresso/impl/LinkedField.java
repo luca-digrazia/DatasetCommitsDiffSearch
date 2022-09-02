6a31969d6b383a30c7bc76ddd4af180af420ab6b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 11:51:56 +0200

    Move unsafe accesses from 'Field' to 'StaticProperty'.

--- dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/LinkedField.java
+++ dataset/GitHub_Java/oracle.graal/espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/LinkedField.java
@@ -33,7 +33,7 @@ final class LinkedField extends StaticProperty {
     private final int slot;
 
     LinkedField(ParserField parserField, int slot, int offset) {
-        super(parserField.getKind(), offset);
+        super(offset);
         this.parserField = parserField;
         this.slot = slot;
     }
