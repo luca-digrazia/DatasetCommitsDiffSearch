ebf01579d5ee4708594798eba23f292dd28200ff
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 00:45:01 +0200

    Testing invocation of a functional interface via lamda function

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/ExposeToGuestTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/ExposeToGuestTest.java
@@ -50,6 +50,7 @@ import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Value;
 import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 
@@ -66,7 +67,7 @@ public class ExposeToGuestTest {
         assertPropertyUndefined(msg, "value", readValue, value);
     }
 
-    static void assertPropertyUndefined(String msg, String propName, Value readValue, Object value) {
+    private static void assertPropertyUndefined(String msg, String propName, Value readValue, Object value) {
         try {
             readValue.execute(value);
             fail(msg);
