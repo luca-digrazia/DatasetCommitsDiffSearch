46fdf4bfeefd693cd077059d1aaec7a2f798de75
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 19:14:09 +0200

    UseBranchesWithin32ByteBoundary default to false.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.asm.amd64/src/org/graalvm/compiler/asm/amd64/AMD64Assembler.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.asm.amd64/src/org/graalvm/compiler/asm/amd64/AMD64Assembler.java
@@ -107,9 +107,7 @@ public class AMD64Assembler extends AMD64BaseAssembler {
     public static class Options {
         // @formatter:off
         @Option(help = "Force branch instructions to align with 32-bytes boundary, to mitigate the jcc erratum. " +
-                "See https://www.intel.com/content/dam/support/us/en/documents/processors/mitigations-jump-conditional-code-erratum.pdf for more details. " +
-                "If not set explicitly, the default value will be overwritten according to the CPU model. " + 
-                "See VM_Version::compute_has_intel_jcc_erratum for more details.", type = OptionType.User)
+                "See https://www.intel.com/content/dam/support/us/en/documents/processors/mitigations-jump-conditional-code-erratum.pdf for more details.", type = OptionType.User)
         public static final OptionKey<Boolean> UseBranchesWithin32ByteBoundary = new OptionKey<>(false);
         // @formatter:on
     }
@@ -122,26 +120,17 @@ public class AMD64Assembler extends AMD64BaseAssembler {
 
     protected CodePatchShifter codePatchShifter = null;
 
-    /**
-     * Constructs an assembler for the AMD64 architecture.
-     */
     public AMD64Assembler(TargetDescription target) {
         super(target);
         useBranchesWithin32ByteBoundary = false;
     }
 
+    /**
+     * Constructs an assembler for the AMD64 architecture.
+     */
     public AMD64Assembler(TargetDescription target, OptionValues optionValues) {
         super(target);
         useBranchesWithin32ByteBoundary = Options.UseBranchesWithin32ByteBoundary.getValue(optionValues);
-    }
-
-    public AMD64Assembler(TargetDescription target, OptionValues optionValues, boolean hasIntelJccErratum) {
-        super(target);
-        if (Options.UseBranchesWithin32ByteBoundary.hasBeenSet(optionValues)) {
-            useBranchesWithin32ByteBoundary = Options.UseBranchesWithin32ByteBoundary.getValue(optionValues);
-        } else {
-            useBranchesWithin32ByteBoundary = hasIntelJccErratum;
-        }
     }
 
     public void setCodePatchShifter(CodePatchShifter codePatchShifter) {
@@ -4146,14 +4135,6 @@ public class AMD64Assembler extends AMD64BaseAssembler {
         evexPrefix(dst, Register.None, Register.None, src, AVXSize.ZMM, P_F3, M_0F, W1, Z0, B0);
         emitByte(0x6F);
         emitOperandHelper(dst, src, 0, EVEXTuple.FVM.getDisp8ScalingFactor(AVXSize.ZMM));
-    }
-
-    public final void evmovdqu64(AMD64Address dst, Register src) {
-        assert supports(CPUFeature.AVX512F);
-        assert inRC(XMM, src);
-        evexPrefix(src, Register.None, Register.None, dst, AVXSize.ZMM, P_F3, M_0F, W1, Z0, B0);
-        emitByte(0x7F);
-        emitOperandHelper(src, dst, 0, EVEXTuple.FVM.getDisp8ScalingFactor(AVXSize.ZMM));
     }
 
     // Insn: VPMOVZXBW zmm1, m256
