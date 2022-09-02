48debbb8840c6ede5047e86c52b027c1b2a39b96
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 18:30:56 +0200

    sulong: rename and document all Runner#parse functions

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/Runner.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/Runner.java
@@ -908,15 +908,11 @@ final class Runner {
             List<String> libraries = binaryParserResult.getLibraries();
             ArrayList<ExternalLibrary> dependencies = new ArrayList<>();
             for (String lib : libraries) {
-                boolean added = false;
-                ExternalLibrary dependency = context.findExternalLibrary(lib, true, library, binaryParserResult.getLocator());
-                if (dependency == null) {
-                    dependency = context.addExternalLibrary(lib, true, library, binaryParserResult.getLocator());
-                    added = true;
-                }
-                if (dependency != null) {
+                LLVMContext.LibraryAddResult result = context.addOrGetExternalLibrary(lib, true, library, binaryParserResult.getLocator());
+                if (result != null) {
+                    ExternalLibrary dependency = result.library;
                     dependencies.add(dependency);
-                    if (added) {
+                    if (result.added) {
                         parseContext.dependencyQueueAddLast(dependency);
                     }
                 }
