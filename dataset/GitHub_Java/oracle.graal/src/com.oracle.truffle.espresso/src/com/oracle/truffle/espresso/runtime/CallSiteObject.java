544c845044c6eff2354888598a19d3304f227f6b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 01:19:00 +0200

    Lambdas should now be fully supported

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/CallSiteObject.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/CallSiteObject.java
@@ -1,21 +1,13 @@
 package com.oracle.truffle.espresso.runtime;
 
-import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.ForeignAccess;
 import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.espresso.classfile.MethodHandleConstant;
 import com.oracle.truffle.espresso.classfile.MethodHandleConstant.RefKind;
-import com.oracle.truffle.espresso.descriptors.Signatures;
 import com.oracle.truffle.espresso.descriptors.Symbol;
-import com.oracle.truffle.espresso.descriptors.Symbol.Name;
-import com.oracle.truffle.espresso.descriptors.Symbol.Signature;
 import com.oracle.truffle.espresso.descriptors.Symbol.Type;
 import com.oracle.truffle.espresso.impl.Klass;
 import com.oracle.truffle.espresso.impl.Method;
-import com.oracle.truffle.espresso.meta.JavaKind;
-import com.oracle.truffle.espresso.meta.Meta;
-import com.oracle.truffle.espresso.nodes.BytecodeNode;
-import com.oracle.truffle.espresso.vm.InterpreterToVM;
 
 public final class CallSiteObject extends StaticObject {
 
@@ -24,19 +16,17 @@ public final class CallSiteObject extends StaticObject {
     private final Symbol<Type>[] signature;
     private final DirectCallNode callNode;
     private final RefKind kind;
-    private final StaticObject emulatedThis;
     private final Boolean hasReceiver;
     private final int stackEffect;
     private final int endModif;
 
-    public CallSiteObject(Klass klass, Method target, Symbol<Type>[] signature, Object[] args, MethodHandleConstant mh, StaticObject emulatedThis) {
+    public CallSiteObject(Klass klass, Method target, Symbol<Type>[] signature, Object[] args, MethodHandleConstant mh) {
         super(klass);
         this.target = target;
         this.signature = signature;
         this.args = args;
         this.callNode = DirectCallNode.create(target.getCallTarget());
         this.kind = mh.getRefKind();
-        this.emulatedThis = emulatedThis;
         this.hasReceiver = !(kind == RefKind.INVOKESTATIC);
         int kindEffect;
         int endEffect;
@@ -64,16 +54,6 @@ public final class CallSiteObject extends StaticObject {
     @Override
     public boolean isCallSite() {return true;}
 
-    @Override
-    public final Method lookupMethod(Symbol<Name> methodName, Symbol<Signature> methodSignature) {
-        Method result = this.getKlass().lookupMethod(methodName, methodSignature);
-        if (result.hasBytecodes()) {
-            return result;
-        } else {
-            return target;
-        }
-    }
-
     public RefKind getKind() {
         return kind;
     }
@@ -98,26 +78,11 @@ public final class CallSiteObject extends StaticObject {
         return this.hasReceiver;
     }
 
-    public int invoke(final VirtualFrame frame, int top, BytecodeNode root) {
-        if (kind == MethodHandleConstant.RefKind.NEWINVOKESPECIAL) {
-            Klass klass = target.getDeclaringKlass();
-            klass.safeInitialize();
-            root.putKind(frame, top - 1, InterpreterToVM.newObject(klass), JavaKind.Object);
-        }
-        Object[] targetArgs = root.peekArgumentsWithCSO(frame, top, hasReceiver, target.getParsedSignature(), this);
-        Object result = call(targetArgs);
-        JavaKind toPush = Signatures.returnKind(target.getParsedSignature());
-        if (toPush != JavaKind.Void && toPush.isPrimitive()) {
-            result = Meta.box(root.getMeta(), result);
-            toPush = JavaKind.Object;
-        }
-        int resultAt = top - Signatures.slotsForParameters(target.getParsedSignature()) + stackEffect; // pop CSO
-        int ret = (resultAt - top) + root.putKind(frame, resultAt, result, toPush);
-        //return (resultAt - top) + root.putKind(frame, resultAt, result, method.getReturnKind()); //
-        return ret + endModif;
+    public int getStackEffect() {
+        return this.stackEffect;
     }
 
-    public StaticObject getEmulatedThis() {
-        return emulatedThis;
+    public int getEndModif() {
+        return this.endModif;
     }
 }
