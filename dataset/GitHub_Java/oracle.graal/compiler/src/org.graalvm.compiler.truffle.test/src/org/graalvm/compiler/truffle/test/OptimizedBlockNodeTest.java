cf38a11945a270eb116ca43702b7038ac12d9837
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 14:05:05 +0200

    Force invalidate to update call node count to be more precise.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/OptimizedBlockNodeTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/OptimizedBlockNodeTest.java
@@ -582,7 +582,6 @@ public class OptimizedBlockNodeTest {
         clearContext();
         context = Context.newBuilder().allowAllAccess(true)//
                         .option("engine.BackgroundCompilation", "false") //
-                        .option("engine.MultiTier", "false") //
                         .option("engine.PartialBlockCompilationSize", String.valueOf(blockCompilationSize))//
                         .option("engine.MaximumGraalNodeCount", String.valueOf(maxGraalNodeCount))//
                         .option("engine.CompilationThreshold", String.valueOf(TEST_COMPILATION_THRESHOLD)).build();
