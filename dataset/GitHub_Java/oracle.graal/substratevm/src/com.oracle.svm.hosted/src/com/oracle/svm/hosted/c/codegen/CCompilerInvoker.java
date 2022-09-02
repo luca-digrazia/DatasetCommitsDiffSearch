61be48b657f9e96f31fbd9355f295c24a6c95bf6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 14:55:41 +0200

    Add support for detecting icc

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/codegen/CCompilerInvoker.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/c/codegen/CCompilerInvoker.java
@@ -417,8 +417,10 @@ public abstract class CCompilerInvoker {
                         .command(createCompilerCommand(options, target.normalize(), source.normalize()))
                         .directory(tempDirectory.toFile());
         Process compilingProcess = null;
-        try (DebugContext.Scope s = debug.scope("InvokeCC")) {
-            debug.log("Using CompilerCommand: %s", SubstrateUtil.getShellCommandString(pb.command(), false));
+        try {
+            try (DebugContext.Scope s = debug.scope("InvokeCC")) {
+                debug.log("Using CompilerCommand: %s", SubstrateUtil.getShellCommandString(pb.command(), false));
+            }
             compilingProcess = pb.start();
 
             List<String> lines;
@@ -427,16 +429,11 @@ public abstract class CCompilerInvoker {
             }
             boolean errorReported = false;
             for (String line : lines) {
-                if (debug.isLogEnabled(DebugContext.VERBOSE_LEVEL)) {
-                    debug.log(DebugContext.VERBOSE_LEVEL, "%s", line);
-                    errorReported = true;
-                } else {
-                    if (detectError(line)) {
-                        if (handler != null) {
-                            handler.handle(pb, source, line);
-                        }
-                        errorReported = true;
+                if (detectError(line)) {
+                    if (handler != null) {
+                        handler.handle(pb, source, line);
                     }
+                    errorReported = true;
                 }
             }
 
