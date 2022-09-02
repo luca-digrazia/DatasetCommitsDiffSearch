c3124058223f7dd2eb29b5201ed07910cc5829b6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 12:59:14 +0200

    Addressing ecj lints

--- dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.agentscript/src/com/oracle/truffle/tools/agentscript/impl/EventContextObject.java
+++ dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.agentscript/src/com/oracle/truffle/tools/agentscript/impl/EventContextObject.java
@@ -108,16 +108,10 @@ final class EventContextObject extends AbstractContextObject {
         if (args.length == 0) {
             return ArityException.create(1, 0);
         }
-        Object retValue = Truffle.getRuntime().iterateFrames((frameInstance) -> {
+        Truffle.getRuntime().iterateFrames((frameInstance) -> {
             final Node n = frameInstance.getCallNode();
             if (n == null) {
                 // skip top most record about the instrument
-                return null;
-            }
-            LocationObject location = new LocationObject(n);
-            final SourceSection ss = location.getInstrumentedSourceSection();
-            if (ss == null || ss.getSource().isInternal()) {
-                // skip internal frames
                 return null;
             }
             final Frame frame = frameInstance.getFrame(FrameInstance.FrameAccess.READ_ONLY);
@@ -126,15 +120,15 @@ final class EventContextObject extends AbstractContextObject {
             if (lib.hasScope(n, frame)) {
                 try {
                     Object frameVars = lib.getScope(n, frame, false);
-                    Object ret = iop.execute(args[0], location, frameVars);
-                    return iop.isNull(ret) ? null : ret;
+                    LocationObject location = new LocationObject(n);
+                    iop.execute(args[0], location, frameVars);
                 } catch (UnsupportedMessageException | UnsupportedTypeException | ArityException ex) {
                     throw InsightException.raise(ex);
                 }
             }
             return null;
         });
-        return NullObject.nullCheck(retValue);
+        return NullObject.nullCheck(null);
     }
 
     @ExportMessage
