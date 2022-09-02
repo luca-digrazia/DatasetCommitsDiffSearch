863379a1a0302bc15eeedeee76c8ee07ef6b2791
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 17:25:14 +0200

    [GR-22307] Enhancing annotations encoding to object containing a single array  to properly represent different annotations groups

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/hub/AnnotationsEncoding.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/hub/AnnotationsEncoding.java
@@ -67,7 +67,7 @@ public final class AnnotationsEncoding {
     }
 
     public Annotation[] getAnnotations() {
-        return allAnnotations == null ? EMPTY_ANNOTATION_ARRAY : allAnnotations.clone();
+        return allAnnotations == null ? EMPTY_ANNOTATION_ARRAY : allAnnotations;
     }
 
     public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
