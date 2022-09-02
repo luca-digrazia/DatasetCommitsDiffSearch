58390b1b5309148208c3cb65096e40fda9b3e397
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 16:48:06 +0200

    Enable SulongEngineOption for interop tests

--- dataset/GitHub_Java/oracle.graal/sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/InteropTestBase.java
+++ dataset/GitHub_Java/oracle.graal/sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/InteropTestBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -34,23 +34,23 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import com.oracle.truffle.llvm.runtime.NFIContextExtension;
+import com.oracle.truffle.llvm.runtime.options.SulongEngineOption;
+import com.oracle.truffle.llvm.tests.BaseSuiteHarness;
 import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Value;
 import org.junit.ClassRule;
 
 import com.oracle.truffle.api.CallTarget;
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.TruffleFile;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.InteropException;
 import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.source.Source;
-import com.oracle.truffle.llvm.runtime.NativeContextExtension;
-import com.oracle.truffle.llvm.runtime.options.SulongEngineOption;
-import com.oracle.truffle.llvm.tests.BaseTestHarness;
 import com.oracle.truffle.llvm.tests.options.TestOptions;
 import com.oracle.truffle.tck.TruffleRunner;
+import org.graalvm.polyglot.Value;
 
 public class InteropTestBase {
 
@@ -61,27 +61,24 @@ public class InteropTestBase {
         return Context.newBuilder().allowAllAccess(true).allowExperimentalOptions(true).option(SulongEngineOption.LIBRARY_PATH_NAME, lib).option(SulongEngineOption.CXX_INTEROP_NAME, "true");
     }
 
-    protected static final Path testBase = Paths.get(TestOptions.TEST_SUITE_PATH, "interop");
-    public static final String TEST_FILE_NAME = "O1." + NativeContextExtension.getNativeLibrarySuffix();
+    private static final Path testBase = Paths.get(TestOptions.TEST_SUITE_PATH, "interop");
+    public static final String TEST_FILE_NAME = "O1." + NFIContextExtension.getNativeLibrarySuffix();
 
-    protected static Object loadTestBitcodeInternal(String name) {
-        File file = Paths.get(testBase.toString(), name + BaseTestHarness.TEST_DIR_EXT, TEST_FILE_NAME).toFile();
-        CallTarget target = getTestBitcodeCallTarget(file);
-        return target.call();
-    }
-
-    protected static CallTarget getTestBitcodeCallTarget(File file) {
+    protected static TruffleObject loadTestBitcodeInternal(String name) {
+        File file = Paths.get(testBase.toString(), name + BaseSuiteHarness.TEST_DIR_EXT, TEST_FILE_NAME).toFile();
         TruffleFile tf = runWithPolyglot.getTruffleTestEnv().getPublicTruffleFile(file.toURI());
+        Source source;
         try {
-            Source source = Source.newBuilder("llvm", tf).build();
-            return runWithPolyglot.getTruffleTestEnv().parsePublic(source);
+            source = Source.newBuilder("llvm", tf).build();
         } catch (IOException ex) {
             throw new AssertionError(ex);
         }
+        CallTarget target = runWithPolyglot.getTruffleTestEnv().parsePublic(source);
+        return (TruffleObject) target.call();
     }
 
     protected static Value loadTestBitcodeValue(String name) {
-        File file = Paths.get(testBase.toString(), name + BaseTestHarness.TEST_DIR_EXT, TEST_FILE_NAME).toFile();
+        File file = Paths.get(testBase.toString(), name + BaseSuiteHarness.TEST_DIR_EXT, TEST_FILE_NAME).toFile();
         org.graalvm.polyglot.Source source;
         try {
             source = org.graalvm.polyglot.Source.newBuilder("llvm", file).build();
@@ -96,7 +93,7 @@ public class InteropTestBase {
         private final Object function;
         @Child InteropLibrary interop;
 
-        protected SulongTestNode(Object testLibrary, String fnName) {
+        protected SulongTestNode(TruffleObject testLibrary, String fnName) {
             super(null);
             try {
                 function = InteropLibrary.getFactory().getUncached().readMember(testLibrary, fnName);
@@ -111,7 +108,7 @@ public class InteropTestBase {
             try {
                 return interop.execute(function, frame.getArguments());
             } catch (InteropException ex) {
-                throw CompilerDirectives.shouldNotReachHere(ex);
+                throw new AssertionError(ex);
             }
         }
     }
