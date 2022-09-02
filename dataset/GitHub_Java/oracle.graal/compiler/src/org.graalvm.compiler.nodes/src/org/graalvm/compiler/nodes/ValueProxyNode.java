d154d09581adb873d81ffdf48b14433b238d6050
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-02 20:47:55 +0200

    Make value proxy canonicalizations slightly more general.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/ValueProxyNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/ValueProxyNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -96,10 +96,5 @@ public final class ValueProxyNode extends ProxyNode implements Canonicalizable, 
     @Override
     public GuardingNode getGuard() {
         return this.proxyPoint();
-    }
-
-    @Override
-    public ProxyNode patchProxy(LoopExitNode newProxyPoint, ValueNode newOriginalNode) {
-        return graph().addWithoutUnique(new ValueProxyNode(newOriginalNode, newProxyPoint));
     }
 }
