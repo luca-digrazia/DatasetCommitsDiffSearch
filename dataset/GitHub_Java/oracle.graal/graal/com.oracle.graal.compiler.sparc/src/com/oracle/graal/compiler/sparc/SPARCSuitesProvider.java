302d4f2654aed39211431a8ccde30a7eec7e71ed
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:54:39 +0200

    Move SPARCSubIntCompareCanonicalizer to lowTier

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.sparc/src/com/oracle/graal/compiler/sparc/SPARCSuitesProvider.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.sparc/src/com/oracle/graal/compiler/sparc/SPARCSuitesProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,14 +22,9 @@
  */
 package com.oracle.graal.compiler.sparc;
 
-import java.util.ListIterator;
-
+import com.oracle.graal.graphbuilderconf.GraphBuilderConfiguration.Plugins;
 import com.oracle.graal.java.DefaultSuitesProvider;
-import com.oracle.graal.nodes.graphbuilderconf.GraphBuilderConfiguration.Plugins;
-import com.oracle.graal.phases.BasePhase;
-import com.oracle.graal.phases.schedule.SchedulePhase;
 import com.oracle.graal.phases.tiers.CompilerConfiguration;
-import com.oracle.graal.phases.tiers.LowTierContext;
 import com.oracle.graal.phases.tiers.Suites;
 
 public class SPARCSuitesProvider extends DefaultSuitesProvider {
@@ -40,9 +35,7 @@ public class SPARCSuitesProvider extends DefaultSuitesProvider {
     @Override
     public Suites createSuites() {
         Suites s = super.createSuites();
-        ListIterator<BasePhase<? super LowTierContext>> l = s.getLowTier().findPhase(SchedulePhase.class);
-        l.previous();
-        l.add(new SPARCSubIntCompareCanonicalizationPhase());
+        s.getLowTier().appendPhase(new SPARCSubIntCompareCanonicalizationPhase());
         return s;
     }
 }
