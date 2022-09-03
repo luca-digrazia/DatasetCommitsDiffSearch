61f92c8e077635f078f0dd17cb669334123dc9d5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:08:37 +0200

    Remove trailing whitespaces

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.graphio/src/org/graalvm/graphio/GraphTypes.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.graphio/src/org/graalvm/graphio/GraphTypes.java
@@ -23,21 +23,22 @@
 package org.graalvm.graphio;
 
 /**
- * Special support for dealing with enums.Normally one can represent various {@link GraphOutput
+ * Special support for dealing with enums. Normally one can represent various {@link GraphOutput
  * graph} enum values with real {@link Enum} instances. In case this is not possible, the
- * {@link GraphOutput.Builder} allows one to register implementation of this interface to treat them
- * especially.
+ * {@link GraphOutput.Builder} allows one to
+ * {@link GraphOutput.Builder#types(org.graalvm.graphio.GraphTypes) register} an implementation of
+ * this interface to treat them specially.
  */
 public interface GraphTypes {
     /**
      * Recognizes an
-     * <q>enum</q> object. If this object represents an enum, then an object that represents its
-     * class shall be returned.
+     * <q>enum</q> object. If the <code>enumValue</code> object represents an enum, then an object
+     * that represents its class shall be returned.
      *
      * @param enumValue the value to test
      * @return <code>null</code> if the value isn't enum, otherwise its class
      */
-    Object findEnumClass(Object enumValue);
+    Object enumClass(Object enumValue);
 
     /**
      * Ordinal of an enum. If the <code>obj</code> represents an enum, then return its ordinal
@@ -46,21 +47,21 @@ public interface GraphTypes {
      * @param obj the value to test
      * @return <code>-1</code> if the obj isn't enum, otherwise its ordinal number
      */
-    int findEnumOrdinal(Object obj);
+    int enumOrdinal(Object obj);
 
     /**
-     * All possible values of an enum. If the provided clazz object represents an enum, then compute
-     * enum value names in ordinal order and return them as a string array. Otherwise return
-     * <code>null</code>
-     * 
+     * All possible values of an enum. If the provided <code>maybeEnumClass</code> object represents
+     * an enum, then compute enum value names in ordinal order and return them as a string array.
+     * Otherwise return <code>null</code>
+     *
      * @param maybeEnumClass the class to test
      * @return <code>null</code> if the clazz isn't an enum, otherwise names of its values
      */
-    String[] findEnumTypeValues(Object maybeEnumClass);
+    String[] enumTypeValues(Object maybeEnumClass);
 
     /**
      * Finds Java type name for a given class.
-     * 
+     *
      * @param maybeClass object representing the class
      * @return the type name of the class or <code>null</code> if the parameter doesn't represent a
      *         class
