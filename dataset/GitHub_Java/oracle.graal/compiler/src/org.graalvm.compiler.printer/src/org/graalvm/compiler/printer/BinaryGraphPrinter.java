a58f31d99018d547b92ceeaba13dc80b817fc227
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:59:02 +0200

    Special support for encoding NodeSourcePosition objects

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.printer/src/org/graalvm/compiler/printer/BinaryGraphPrinter.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.printer/src/org/graalvm/compiler/printer/BinaryGraphPrinter.java
@@ -70,6 +70,7 @@ import jdk.vm.ci.meta.JavaType;
 import jdk.vm.ci.meta.ResolvedJavaField;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
 import jdk.vm.ci.meta.Signature;
+import org.graalvm.compiler.graph.NodeSourcePosition;
 
 public class BinaryGraphPrinter implements GraphPrinter {
 
@@ -98,11 +99,12 @@ public class BinaryGraphPrinter implements GraphPrinter {
     private static final int PROPERTY_FALSE = 0x06;
     private static final int PROPERTY_ARRAY = 0x07;
     private static final int PROPERTY_SUBGRAPH = 0x08;
+    private static final int PROPERTY_NODE_SOURCE_POSITION = 0x09;
 
     private static final int KLASS = 0x00;
     private static final int ENUM_KLASS = 0x01;
 
-    static final int CURRENT_MAJOR_VERSION = 3;
+    static final int CURRENT_MAJOR_VERSION = 2;
     static final int CURRENT_MINOR_VERSION = 0;
 
     static final byte[] MAGIC_BYTES = {'B', 'I', 'G', 'V'};
@@ -178,7 +180,7 @@ public class BinaryGraphPrinter implements GraphPrinter {
         writeString(format);
         writeInt(args.length);
         for (Object a : args) {
-            writePropertyObject(a);
+            writePoolObject(a);
         }
         writeGraph(graph, properties);
         flush();
@@ -480,6 +482,16 @@ public class BinaryGraphPrinter implements GraphPrinter {
                     writePoolObject(o);
                 }
             }
+        } else if (obj instanceof NodeSourcePosition) {
+            writeByte(PROPERTY_NODE_SOURCE_POSITION);
+            NodeSourcePosition pos = (NodeSourcePosition) obj;
+            while (pos != null) {
+                ResolvedJavaMethod method = pos.getMethod();
+                writePoolObject(method);
+                writeInt(pos.getBCI());
+                pos = pos.getCaller();
+            }
+            writePoolObject(null);
         } else {
             writeByte(PROPERTY_POOL);
             writePoolObject(obj);
