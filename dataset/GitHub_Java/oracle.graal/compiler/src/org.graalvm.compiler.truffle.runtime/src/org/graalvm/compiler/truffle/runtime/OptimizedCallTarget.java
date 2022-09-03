4e7ac8617e99f72ccf4f2521aaa0c537ead8d517
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:36:53 +0200

    More thread-safe assignment for needs split.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCallTarget.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCallTarget.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -742,7 +742,7 @@ public abstract class OptimizedCallTarget implements CompilableTruffleAST, RootC
         }
         if (depth > TruffleRuntimeOptions.getValue(SharedTruffleRuntimeOptions.TruffleExperimentalSplittingMaxPropagationDepth) || needsSplit || numberOfKnownCallNodes == 0 ||
                         compilationProfile.getCallCount() == 1) {
-            return needsSplit;
+            return false;
         }
         if (numberOfKnownCallNodes == 1) {
             if (onlyCaller != null) {
