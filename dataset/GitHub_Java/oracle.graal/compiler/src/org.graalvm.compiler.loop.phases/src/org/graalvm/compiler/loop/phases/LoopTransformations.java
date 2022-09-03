e941eb40a5850e816cff9870eb7496359e66b18a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:57:35 +0200

    Simplify updatePreLoopLimit

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.loop.phases/src/org/graalvm/compiler/loop/phases/LoopTransformations.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.loop.phases/src/org/graalvm/compiler/loop/phases/LoopTransformations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -231,6 +231,8 @@ public abstract class LoopTransformations {
         graph.getDebug().log("LoopTransformations.insertPrePostLoops %s", loop);
         LoopFragmentWhole preLoop = loop.whole();
         CountedLoopInfo preCounted = loop.counted();
+        IfNode preLimit = preCounted.getLimitTest();
+        assert preLimit != null;
         LoopBeginNode preLoopBegin = loop.loopBegin();
         LoopExitNode preLoopExitNode = preLoopBegin.getSingleLoopExit();
         FixedNode continuationNode = preLoopExitNode.next();
