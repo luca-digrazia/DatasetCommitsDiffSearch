955a79651cd17ec11d0aabe3158919e285f63948
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 13:56:03 +0200

    OM-DSL: tidy up LayoutParser.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.object.dsl.processor/src/com/oracle/truffle/object/dsl/processor/LayoutProcessor.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.object.dsl.processor/src/com/oracle/truffle/object/dsl/processor/LayoutProcessor.java
@@ -4,7 +4,9 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -23,7 +25,9 @@
 package com.oracle.truffle.object.dsl.processor;
 
 import com.oracle.truffle.api.object.dsl.Layout;
-import com.oracle.truffle.object.dsl.processor.model.LayoutModel;
+import com.oracle.truffle.object.dsl.processor.layout.LayoutGenerator;
+import com.oracle.truffle.object.dsl.processor.layout.LayoutParser;
+import com.oracle.truffle.object.dsl.processor.layout.model.LayoutModel;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
