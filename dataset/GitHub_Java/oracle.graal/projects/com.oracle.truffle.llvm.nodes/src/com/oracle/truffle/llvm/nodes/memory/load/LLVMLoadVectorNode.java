4cb022e36af0c751fd71638a07447253ec190945
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 07:33:00 +0200

    Add support for foreign objects to LLVMLoad/StoreVectorNode.

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/load/LLVMLoadVectorNode.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/load/LLVMLoadVectorNode.java
@@ -30,6 +30,7 @@
 package com.oracle.truffle.llvm.nodes.memory.load;
 
 import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeField;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
@@ -39,6 +40,7 @@ import com.oracle.truffle.llvm.runtime.global.LLVMGlobalVariable;
 import com.oracle.truffle.llvm.runtime.global.LLVMGlobalVariableAccess;
 import com.oracle.truffle.llvm.runtime.interop.convert.ForeignToLLVM.ForeignToLLVMType;
 import com.oracle.truffle.llvm.runtime.memory.LLVMMemory;
+import com.oracle.truffle.llvm.runtime.nodes.api.LLVMExpressionNode;
 import com.oracle.truffle.llvm.runtime.vector.LLVMAddressVector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMDoubleVector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMFloatVector;
@@ -50,8 +52,9 @@ import com.oracle.truffle.llvm.runtime.vector.LLVMI8Vector;
 
 public class LLVMLoadVectorNode {
 
+    @NodeChild(type = LLVMExpressionNode.class)
     @NodeField(name = "size", type = int.class)
-    public abstract static class LLVMLoadI1VectorNode extends LLVMLoadNode {
+    public abstract static class LLVMLoadI1VectorNode extends LLVMExpressionNode {
 
         public abstract int getSize();
 
@@ -75,8 +78,9 @@ public class LLVMLoadVectorNode {
         }
     }
 
+    @NodeChild(type = LLVMExpressionNode.class)
     @NodeField(name = "size", type = int.class)
-    public abstract static class LLVMLoadI8VectorNode extends LLVMLoadNode {
+    public abstract static class LLVMLoadI8VectorNode extends LLVMExpressionNode {
 
         public abstract int getSize();
 
@@ -100,8 +104,9 @@ public class LLVMLoadVectorNode {
         }
     }
 
+    @NodeChild(type = LLVMExpressionNode.class)
     @NodeField(name = "size", type = int.class)
-    public abstract static class LLVMLoadI16VectorNode extends LLVMLoadNode {
+    public abstract static class LLVMLoadI16VectorNode extends LLVMExpressionNode {
 
         public abstract int getSize();
 
@@ -125,8 +130,9 @@ public class LLVMLoadVectorNode {
         }
     }
 
+    @NodeChild(type = LLVMExpressionNode.class)
     @NodeField(name = "size", type = int.class)
-    public abstract static class LLVMLoadI32VectorNode extends LLVMLoadNode {
+    public abstract static class LLVMLoadI32VectorNode extends LLVMExpressionNode {
 
         public abstract int getSize();
 
@@ -150,8 +156,9 @@ public class LLVMLoadVectorNode {
         }
     }
 
+    @NodeChild(type = LLVMExpressionNode.class)
     @NodeField(name = "size", type = int.class)
-    public abstract static class LLVMLoadI64VectorNode extends LLVMLoadNode {
+    public abstract static class LLVMLoadI64VectorNode extends LLVMExpressionNode {
 
         public abstract int getSize();
 
@@ -175,8 +182,9 @@ public class LLVMLoadVectorNode {
         }
     }
 
+    @NodeChild(type = LLVMExpressionNode.class)
     @NodeField(name = "size", type = int.class)
-    public abstract static class LLVMLoadFloatVectorNode extends LLVMLoadNode {
+    public abstract static class LLVMLoadFloatVectorNode extends LLVMExpressionNode {
 
         public abstract int getSize();
 
@@ -201,8 +209,9 @@ public class LLVMLoadVectorNode {
 
     }
 
+    @NodeChild(type = LLVMExpressionNode.class)
     @NodeField(name = "size", type = int.class)
-    public abstract static class LLVMLoadDoubleVectorNode extends LLVMLoadNode {
+    public abstract static class LLVMLoadDoubleVectorNode extends LLVMExpressionNode {
 
         public abstract int getSize();
 
@@ -226,8 +235,9 @@ public class LLVMLoadVectorNode {
         }
     }
 
+    @NodeChild(type = LLVMExpressionNode.class)
     @NodeField(name = "size", type = int.class)
-    public abstract static class LLVMLoadAddressVectorNode extends LLVMLoadNode {
+    public abstract static class LLVMLoadAddressVectorNode extends LLVMExpressionNode {
 
         public abstract int getSize();
 
