5b8cb684401a53cec7c73acea787503a3d4956b9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 19:35:27 +0200

    onAdopt callback for ASTs

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/Node.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/Node.java
@@ -340,6 +340,7 @@ public abstract class Node implements Cloneable {
     }
 
     private void traceRewrite(Node newNode, CharSequence reason) {
+
         if (TruffleOptions.TraceRewritesFilterFromCost != null) {
             if (filterByKind(this, TruffleOptions.TraceRewritesFilterFromCost)) {
                 return;
@@ -359,11 +360,8 @@ public abstract class Node implements Cloneable {
             return;
         }
 
-        final SourceSection reportedSourceSection = getEncapsulatingSourceSection();
-
         PrintStream out = System.out;
-        out.printf("[truffle]   rewrite %-50s |From %-40s |To %-40s |Reason %s%s%n", this.toString(), formatNodeInfo(this), formatNodeInfo(newNode), reason != null && reason.length() > 0 ? reason
-                        : "unknown", reportedSourceSection != null ? " at " + reportedSourceSection.getShortDescription() : "");
+        out.printf("[truffle]   rewrite %-50s |From %-40s |To %-40s |Reason %s.%n", this.toString(), formatNodeInfo(this), formatNodeInfo(newNode), reason);
     }
 
     /**
