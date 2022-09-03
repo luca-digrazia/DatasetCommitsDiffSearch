df2e29b4aaabf592c26f55ba238cfe6a1038f4b8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:05:12 +0200

    defend against missing LocalVariableTable method attribute and missing slots in the table

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/SnippetTemplate.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/SnippetTemplate.java
@@ -62,8 +62,6 @@ import com.oracle.graal.api.replacements.SnippetReflectionProvider;
 import com.oracle.graal.compiler.common.GraalOptions;
 import com.oracle.graal.compiler.common.type.Stamp;
 import com.oracle.graal.compiler.common.type.StampFactory;
-import com.oracle.graal.compiler.common.type.StampPair;
-import com.oracle.graal.compiler.common.type.TypeReference;
 import com.oracle.graal.debug.Debug;
 import com.oracle.graal.debug.Debug.Scope;
 import com.oracle.graal.debug.DebugCloseable;
@@ -478,7 +476,7 @@ public class SnippetTemplate {
         protected final Varargs varargs;
 
         protected VarargsPlaceholderNode(Varargs varargs, MetaAccessProvider metaAccess) {
-            super(TYPE, StampFactory.objectNonNull(TypeReference.createExactTrusted(metaAccess.lookupJavaType(varargs.componentType).getArrayClass())));
+            super(TYPE, StampFactory.exactNonNull(metaAccess.lookupJavaType(varargs.componentType).getArrayClass()));
             this.varargs = varargs;
         }
 
@@ -729,7 +727,7 @@ public class SnippetTemplate {
                         assert parameterCount < 10000;
                         int idx = (i + 1) * 10000 + j;
                         assert idx >= parameterCount : "collision in parameter numbering";
-                        ParameterNode local = snippetCopy.unique(new ParameterNode(idx, StampPair.createSingle(stamp)));
+                        ParameterNode local = snippetCopy.unique(new ParameterNode(idx, stamp));
                         params[j] = local;
                     }
                     parameters[i] = params;
