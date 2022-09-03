a58f8df09917fb66131df2e0b54b8c97812f9290
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:38:31 +0200

    Fix license.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.benchmark/src/com/oracle/truffle/api/benchmark/InterpreterCallBenchmark.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.benchmark/src/com/oracle/truffle/api/benchmark/InterpreterCallBenchmark.java
@@ -3,7 +3,7 @@
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
- *
+ * 
  * Subject to the condition set forth below, permission is hereby granted to any
  * person obtaining a copy of this software, associated documentation and/or
  * data (collectively the "Software"), free of charge and under any and all
@@ -11,25 +11,25 @@
  * freely licensable by each licensor hereunder covering either (i) the
  * unmodified Software as contributed to or provided by such licensor, or (ii)
  * the Larger Works (as defined below), to deal in both
- *
+ * 
  * (a) the Software, and
- *
+ * 
  * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
  * one is included with the Software each a "Larger Work" to which the Software
  * is contributed by such licensors),
- *
+ * 
  * without restriction, including without limitation the rights to copy, create
  * derivative works of, display, perform, and distribute the Software and make,
  * use, sell, offer for sale, import, export, have made, and have sold the
  * Software and the Larger Work(s), and to sublicense the foregoing rights on
  * either these or other terms.
- *
+ * 
  * This license is subject to the following condition:
- *
+ * 
  * The above copyright notice and either this complete permission notice or at a
  * minimum a reference to the UPL must be included in all copies or substantial
  * portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -56,20 +56,18 @@ import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.TruffleOptions;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.DirectCallNode;
-import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.nodes.RootNode;
 
-@Warmup(iterations = 5, time = 5)
-@Measurement(iterations = 5, time = 5)
-@Fork(value = 1, jvmArgsAppend = "-Dgraal.TruffleCompilation=false")
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@Fork(value = 1, jvmArgsAppend = "-Dgraal.TruffleCompilationThreshold=2147483647")
 public class InterpreterCallBenchmark extends TruffleBenchmark {
 
     @State(org.openjdk.jmh.annotations.Scope.Thread)
     public static class CallTargetCallState {
         final Integer argument = 42;
         final CallTarget callee;
-        final DirectCallNode directCall;
-        final IndirectCallNode indirectCall;
+        final CallTarget caller;
 
         {
             callee = Truffle.getRuntime().createCallTarget(new RootNode(null) {
@@ -84,8 +82,20 @@ public class InterpreterCallBenchmark extends TruffleBenchmark {
                     return "callee";
                 }
             });
-            directCall = Truffle.getRuntime().createDirectCallNode(callee);
-            indirectCall = Truffle.getRuntime().createIndirectCallNode();
+            caller = Truffle.getRuntime().createCallTarget(new RootNode(null) {
+                @Child private DirectCallNode callNode = Truffle.getRuntime().createDirectCallNode(callee);
+
+                @Override
+                public Object execute(VirtualFrame frame) {
+                    CompilerAsserts.neverPartOfCompilation("do not compile");
+                    return callNode.call(new Object[]{frame.getArguments()[0]});
+                }
+
+                @Override
+                public String toString() {
+                    return "caller";
+                }
+            });
         }
 
         @Setup
@@ -97,17 +107,7 @@ public class InterpreterCallBenchmark extends TruffleBenchmark {
 
     @Benchmark
     public Object directCall(CallTargetCallState state) {
-        return state.directCall.call(new Object[]{state.argument});
-    }
-
-    @Benchmark
-    public Object indirectCall(CallTargetCallState state) {
-        return state.indirectCall.call(state.callee, new Object[]{state.argument});
-    }
-
-    @Benchmark
-    public Object call(CallTargetCallState state) {
-        return state.callee.call(state.argument);
+        return state.caller.call(state.argument);
     }
 
     static void ensureTruffleCompilerInitialized() {
