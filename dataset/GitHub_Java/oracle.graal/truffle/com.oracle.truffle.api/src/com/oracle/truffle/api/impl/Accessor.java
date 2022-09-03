02bacb325c9aea757f86bc34edb3452fd9811e80
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:02:38 +0200

    Making sure invokeForeign as well as executionStart are never included in the compiled Truffle code

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/Accessor.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/Accessor.java
@@ -51,7 +51,6 @@ import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.source.Source;
 
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Communication between PolyglotEngine, TruffleLanguage API/SPI, and other services.
@@ -112,7 +111,7 @@ public abstract class Accessor {
             }
         };
         lng.hashCode();
-        new Node() {
+        new Node(null) {
         }.getRootNode();
 
         try {
@@ -278,8 +277,8 @@ public abstract class Accessor {
     @TruffleBoundary
     @SuppressWarnings("unused")
     protected Closeable executionStart(Object vm, int currentDepth, Debugger debugger, Source s) {
+        vm.getClass();
         CompilerAsserts.neverPartOfCompilation();
-        Objects.requireNonNull(vm);
         final Object prev = CURRENT_VM.get();
         final Closeable debugClose = DEBUG.executionStart(vm, prev == null ? 0 : -1, debugger, s);
         if (!(vm == previousVM.get())) {
