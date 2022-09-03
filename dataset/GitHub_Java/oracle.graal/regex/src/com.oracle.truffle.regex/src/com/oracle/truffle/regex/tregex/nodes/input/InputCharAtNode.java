b8e374bdf698d2506b8d6b3c5b5aeca56a8af4f1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:06:05 +0200

    Handle long-valued code points in TRegex TruffleObject inputs

--- dataset/GitHub_Java/oracle.graal/regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/nodes/input/InputCharAtNode.java
+++ dataset/GitHub_Java/oracle.graal/regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/nodes/input/InputCharAtNode.java
@@ -26,17 +26,17 @@ package com.oracle.truffle.regex.tregex.nodes.input;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Cached;
-import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.InvalidArrayIndexException;
+import com.oracle.truffle.api.interop.ForeignAccess;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnknownIdentifierException;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.interop.UnsupportedTypeException;
-import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.nodes.Node;
-import com.oracle.truffle.regex.runtime.nodes.ToCharNode;
+import com.oracle.truffle.regex.tregex.util.ForeignAccessUtil;
 
-@GenerateUncached
+@ImportStatic(ForeignAccessUtil.class)
 public abstract class InputCharAtNode extends Node {
 
     public static InputCharAtNode create() {
@@ -46,19 +46,24 @@ public abstract class InputCharAtNode extends Node {
     public abstract char execute(Object input, int index);
 
     @Specialization
-    static char doString(String input, int index) {
+    public char doCharAt(String input, int index) {
         return input.charAt(index);
     }
 
-    @Specialization(guards = "inputs.hasArrayElements(input)", limit = "2")
-    static char doBoxedCharArray(Object input, int index,
-                    @CachedLibrary("input") InteropLibrary inputs,
-                    @Cached ToCharNode toCharNode) {
+    @Specialization
+    public char doCharAt(TruffleObject input, int index, @Cached("createReadMessageNode()") Node readNode) {
         try {
-            return toCharNode.execute(inputs.readArrayElement(input, index));
-        } catch (UnsupportedMessageException | InvalidArrayIndexException | UnsupportedTypeException e) {
+            Object c = ForeignAccess.sendRead(readNode, input, index);
+            if (c instanceof Character) {
+                return (char) c;
+            } else if (c instanceof Number) {
+                assert ((Number) c).intValue() < Character.MAX_VALUE;
+                return (char) ((Number) c).intValue();
+            }
             CompilerDirectives.transferToInterpreter();
-            // should never be reached
+            throw UnsupportedTypeException.raise(new Object[]{c});
+        } catch (UnknownIdentifierException | UnsupportedMessageException e) {
+            CompilerDirectives.transferToInterpreter();
             throw new RuntimeException(e);
         }
     }
