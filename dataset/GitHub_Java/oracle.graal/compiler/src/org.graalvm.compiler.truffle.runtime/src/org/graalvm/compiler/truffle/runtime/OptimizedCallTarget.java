324a06022046ad1efa16e86022c2c4c0bcfae9a7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:54:31 +0200

    Better name for compiler flag.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCallTarget.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCallTarget.java
@@ -694,14 +694,11 @@ public abstract class OptimizedCallTarget implements CompilableTruffleAST, RootC
         }
         if (knownCallNodes.size() == 1) {
             final OptimizedDirectCallNode callNode = knownCallNodes.iterator().next();
-            final RootNode rootNode = callNode.getRootNode();
-            if (rootNode != null && rootNode.getCallTarget() != null) {
-                final OptimizedCallTarget callTarget = (OptimizedCallTarget) rootNode.getCallTarget();
-                if (TruffleCompilerOptions.getValue(TruffleDumpPolymorphicSpecialize)) {
-                    pullOutParentChain(callNode, toDump);
-                }
-                needsSplit = callTarget.maybeSetNeedsSplit(depth + 1, toDump);
+            final OptimizedCallTarget callTarget = (OptimizedCallTarget) callNode.getRootNode().getCallTarget();
+            if (TruffleCompilerOptions.getValue(TruffleDumpPolymorphicSpecialize)) {
+                pullOutParentChain(callNode, toDump);
             }
+            needsSplit = callTarget.maybeSetNeedsSplit(depth + 1, toDump);
         } else {
             if (TruffleCompilerOptions.getValue(TruffleDumpPolymorphicSpecialize)) {
                 PolymorphicSpecializeDump.dumpPolymorphicSpecialize(toDump, knownCallNodes);
