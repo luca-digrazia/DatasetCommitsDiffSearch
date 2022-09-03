9812f22e813489055768a9faa77233a596ca62a9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:10:45 +0200

    Make AssumedValue thread-safe

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/utilities/AssumedValue.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/utilities/AssumedValue.java
@@ -24,9 +24,13 @@
  */
 package com.oracle.truffle.api.utilities;
 
-import com.oracle.truffle.api.*;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+import com.oracle.truffle.api.Assumption;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
-import com.oracle.truffle.api.nodes.*;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.nodes.InvalidAssumptionException;
 
 /**
  * A value that the compiler can assume is constant, but can be changed by invalidation.
@@ -35,18 +39,26 @@ import com.oracle.truffle.api.nodes.*;
  * take care to only change values infrequently, or to monitor the number of times the value has
  * changed and at some point to replace the value with something more generic so that it does not
  * have to be changed and code does not have to keep being recompiled.
+ *
+ * @since 0.8 or earlier
  */
 public class AssumedValue<T> {
 
     private final String name;
 
+    // value behaves as volatile by piggybacking on Assumption semantics
     @CompilationFinal private T value;
-    @CompilationFinal private Assumption assumption;
+    @CompilationFinal private volatile Assumption assumption;
 
+    @SuppressWarnings("rawtypes") private static final AtomicReferenceFieldUpdater<AssumedValue, Assumption> ASSUMPTION_UPDATER = //
+                    AtomicReferenceFieldUpdater.newUpdater(AssumedValue.class, Assumption.class, "assumption");
+
+    /** @since 0.8 or earlier */
     public AssumedValue(T initialValue) {
         this(null, initialValue);
     }
 
+    /** @since 0.8 or earlier */
     public AssumedValue(String name, T initialValue) {
         this.name = name;
         value = initialValue;
@@ -56,6 +68,8 @@ public class AssumedValue<T> {
     /**
      * Get the current value, updating it if it has been {@link #set}. The compiler may be able to
      * make this method return a constant value, but still accommodate mutation.
+     *
+     * @since 0.8 or earlier
      */
     public T get() {
         try {
@@ -69,13 +83,15 @@ public class AssumedValue<T> {
 
     /**
      * Set a new value, which will be picked up the next time {@link #get} is called.
+     *
+     * @since 0.8 or earlier
      */
     public void set(T newValue) {
         CompilerDirectives.transferToInterpreter();
-
         value = newValue;
-        final Assumption oldAssumption = assumption;
-        assumption = Truffle.getRuntime().createAssumption(name);
+
+        Assumption newAssumption = Truffle.getRuntime().createAssumption(name);
+        Assumption oldAssumption = ASSUMPTION_UPDATER.getAndSet(this, newAssumption);
         oldAssumption.invalidate();
     }
 
