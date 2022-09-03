b1d23373b2e622af8eab2a5d4abf2e2a602108d4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:42:19 +0200

    Concatenate bytecode positions when formatting inline decisions as lists.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/InliningLog.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/InliningLog.java
@@ -24,6 +24,8 @@ package org.graalvm.compiler.nodes;
 
 import jdk.vm.ci.code.BytecodePosition;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
+import org.graalvm.compiler.debug.GraalError;
+import org.graalvm.compiler.debug.TTY;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,15 +33,15 @@ import java.util.List;
 import java.util.Map;
 
 public class InliningLog {
-    public static final class BytecodePositionWithId extends BytecodePosition implements Comparable<BytecodePositionWithId> {
-        private int id;
+    public static final class BytecodePositionWithId extends BytecodePosition {
+        private long id;
 
-        public BytecodePositionWithId(BytecodePositionWithId caller, ResolvedJavaMethod method, int bci, int id) {
+        public BytecodePositionWithId(BytecodePositionWithId caller, ResolvedJavaMethod method, int bci, long id) {
             super(caller, method, bci);
             this.id = id;
         }
 
-        public BytecodePositionWithId(BytecodePosition position, int id) {
+        public BytecodePositionWithId(BytecodePosition position, long id) {
             super(toPositionWithId(position.getCaller()), position.getMethod(), position.getBCI());
             this.id = id;
         }
@@ -64,34 +66,8 @@ public class InliningLog {
             return (BytecodePositionWithId) super.getCaller();
         }
 
-        public BytecodePositionWithId withoutCaller() {
-            return new BytecodePositionWithId(null, getMethod(), getBCI(), id);
-        }
-
         public long getId() {
             return id;
-        }
-
-        @Override
-        public boolean equals(Object that) {
-            if (!(that instanceof BytecodePositionWithId)) {
-                return false;
-            }
-            return super.equals(that) && this.id == ((BytecodePositionWithId) that).id;
-        }
-
-        @Override
-        public int hashCode() {
-            return super.hashCode() ^ (id << 16);
-        }
-
-        public int compareTo(BytecodePositionWithId that) {
-            int diff = this.getBCI() - that.getBCI();
-            if (diff != 0) {
-                return diff;
-            }
-            diff = (int) (this.getId() - that.getId());
-            return diff;
         }
     }
 
@@ -133,39 +109,10 @@ public class InliningLog {
     }
 
     private static class Callsite {
-        public String decision;
-        public final Map<BytecodePositionWithId, Callsite> children;
-        public final BytecodePositionWithId position;
+        public Map<BytecodePositionWithId, Callsite> children;
 
-        Callsite(BytecodePositionWithId position) {
+        Callsite() {
             this.children = new HashMap<>();
-            this.position = position;
-        }
-
-        public Callsite getOrCreateChild(BytecodePositionWithId position) {
-            Callsite child = children.get(position.withoutCaller());
-            if (child == null) {
-                child = new Callsite(position);
-                children.put(position.withoutCaller(), child);
-            }
-            return child;
-        }
-
-        public Callsite createCallsite(BytecodePositionWithId position, String decision) {
-            Callsite parent = getOrCreateCallsite(position.getCaller());
-            Callsite callsite = parent.getOrCreateChild(position);
-            callsite.decision = decision;
-            return null;
-        }
-
-        private Callsite getOrCreateCallsite(BytecodePositionWithId position) {
-            if (position == null) {
-                return this;
-            } else {
-                Callsite parent = getOrCreateCallsite(position.getCaller());
-                Callsite callsite = parent.getOrCreateChild(position);
-                return callsite;
-            }
         }
     }
 
@@ -197,7 +144,8 @@ public class InliningLog {
             BytecodePositionWithId absolutePosition = decision.getPosition().addCallerWithId(caller);
             String position = "  " + decision.getPosition().toString().replaceAll("\n", "\n  ");
             String line = String.format("<%s> %s: %s\n%s", phaseStack, positive, decision.getReason(), position);
-            builder.append(line).append(System.lineSeparator());
+            builder.append(line);
+            builder.append(System.lineSeparator());
             if (decision.getChildLog() != null) {
                 formatAsList(phaseStack, absolutePosition, decision.getChildLog().getDecisions(), builder);
             }
@@ -205,33 +153,8 @@ public class InliningLog {
     }
 
     public String formatAsTree() {
-        Callsite root = new Callsite(null);
-        createTree("", null, root, decisions);
-        StringBuilder builder = new StringBuilder();
-        formatAsTree(root, "", builder);
-        return builder.toString();
-    }
+        Callsite root = new Callsite();
 
-    private void createTree(String phasePrefix, BytecodePositionWithId caller, Callsite root, List<Decision> decisions) {
-        for (Decision decision : decisions) {
-            String phaseStack = phasePrefix.equals("") ? decision.getPhase() : phasePrefix + "-" + decision.getPhase();
-            String positive = decision.isPositive() ? "inline" : "do not inline";
-            BytecodePositionWithId absolutePosition = decision.getPosition().addCallerWithId(caller);
-            String line = String.format("<%s> %s: %s", phaseStack, positive, decision.getReason());
-            root.createCallsite(absolutePosition, line);
-            if (decision.getChildLog() != null) {
-                createTree(phaseStack, absolutePosition, root, decision.getChildLog().getDecisions());
-            }
-        }
-    }
-
-    private void formatAsTree(Callsite site, String indent, StringBuilder builder) {
-        String position = site.position != null ? site.position.withoutCaller().toString() : "<root>";
-        String decision = site.decision != null ? site.decision : "";
-        String line = String.format("%s%s; %s", indent, position, decision);
-        builder.append(line).append(System.lineSeparator());
-        String childIndent = indent + "  ";
-        site.children.entrySet().stream().sorted((x, y) -> x.getKey().compareTo(y.getKey()))
-                        .forEach(e -> formatAsTree(e.getValue(), childIndent, builder));
+        throw GraalError.unimplemented();
     }
 }
