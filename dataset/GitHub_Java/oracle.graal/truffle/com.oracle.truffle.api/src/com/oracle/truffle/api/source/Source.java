fe697a5d04baf53f00b81272814ae75f673658e5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:28:11 +0200

    The withMimeType method is obsolete. Use the Builder's mimeType one.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/source/Source.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/source/Source.java
@@ -1095,7 +1095,7 @@ public abstract class Source {
          */
         @SuppressWarnings("unchecked")
         public Builder<Source, E> mimeType(String newMimeType) {
-            Objects.requireNonNull(newMimeType);
+            Objects.nonNull(newMimeType);
             this.mime = newMimeType;
             return (Builder<Source, E>) this;
         }
