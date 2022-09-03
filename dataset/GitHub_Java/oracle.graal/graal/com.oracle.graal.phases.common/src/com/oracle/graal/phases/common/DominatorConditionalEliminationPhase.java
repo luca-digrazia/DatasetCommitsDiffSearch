a01bc1d3f11b83e89e02ad71e2123bc5b03a76f3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 16:13:30 +0200

    Change recursive LoweringPhase.Round.processBlock to state machine with emulated stack. Also use the same traversal in DominatorConditionalEliminationPhase.Instance.processBlock. Required, as the recursive implementation exceeds the stack on SPARC.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/DominatorConditionalEliminationPhase.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/DominatorConditionalEliminationPhase.java
@@ -144,6 +144,7 @@ public class DominatorConditionalEliminationPhase extends Phase {
 
         public void processBlock(Block startBlock) {
             LoweringPhase.processBlock(new InstanceFrame(startBlock, null));
+
         }
 
         public class InstanceFrame extends LoweringPhase.Frame<InstanceFrame> {
