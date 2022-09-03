19cd964780d10b360c7fa2523eabc78adb8c9dc2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 20:23:13 +0200

    Sort options alphabetically so that diffs do not show false positives

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.options/src/com/oracle/graal/options/OptionProcessor.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.options/src/com/oracle/graal/options/OptionProcessor.java
@@ -264,7 +264,7 @@ public class OptionProcessor extends AbstractProcessor {
 
     private static Element topDeclaringType(Element element) {
         Element enclosing = element.getEnclosingElement();
-        if (enclosing == null || enclosing.getKind() == ElementKind.PACKAGE) {
+        if (element == null || enclosing.getKind() == ElementKind.PACKAGE) {
             assert element.getKind() == ElementKind.CLASS || element.getKind() == ElementKind.INTERFACE;
             return element;
         }
