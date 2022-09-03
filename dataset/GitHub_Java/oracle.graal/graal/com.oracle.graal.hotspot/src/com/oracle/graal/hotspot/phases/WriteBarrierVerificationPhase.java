eaa95263dd408f56c5718438eb6d774e7b8249cd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 23:02:42 +0200

    Small refactoring

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/phases/WriteBarrierVerificationPhase.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/phases/WriteBarrierVerificationPhase.java
@@ -25,6 +25,7 @@ package com.oracle.graal.hotspot.phases;
 
 import java.util.*;
 
+import com.oracle.graal.api.code.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.nodes.*;
 import com.oracle.graal.nodes.extended.*;
