abc9b6cbaf87b2e2168ac06956fa41d3d01348c0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 18:24:53 +0200

    InvokeNode does not need to implement IterableNodeType

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/InvokeNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/InvokeNode.java
@@ -42,9 +42,9 @@ public class InvokeNode extends AbstractMemoryCheckpoint implements Invoke, LIRL
     @Input(InputType.Extension) CallTargetNode callTarget;
     @OptionalInput(InputType.State) FrameState stateDuring;
     @OptionalInput(InputType.Guard) GuardingNode guard;
-    protected int bci;
-    protected boolean polymorphic;
-    protected boolean useForInlining;
+    private final int bci;
+    private boolean polymorphic;
+    private boolean useForInlining;
 
     /**
      * Constructs a new Invoke instruction.
