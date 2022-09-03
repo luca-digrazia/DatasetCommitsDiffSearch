9a662f079135b1aa5439637235e2a5d83dd0a3a6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 16:18:17 +0200

    fix canonicalization of ObjectEqualsNode to TypeCheckNode

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/ObjectEqualsNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/ObjectEqualsNode.java
@@ -22,7 +22,6 @@
  */
 package com.oracle.graal.nodes.calc;
 
-import com.oracle.graal.api.meta.Assumptions.AssumptionResult;
 import com.oracle.graal.api.meta.*;
 import com.oracle.graal.compiler.common.*;
 import com.oracle.graal.compiler.common.calc.*;
@@ -63,20 +62,11 @@ public final class ObjectEqualsNode extends PointerEqualsNode implements Virtual
     protected ValueNode canonicalizeSymmetricConstant(CanonicalizerTool tool, Constant constant, ValueNode nonConstant, boolean mirrored) {
         ResolvedJavaType type = tool.getConstantReflection().asJavaType(constant);
         if (type != null && nonConstant instanceof GetClassNode) {
-            if (type.isPrimitive()) {
+            if (type.getKind() == Kind.Void) {
                 return LogicConstantNode.forBoolean(false);
             }
-            ResolvedJavaType exactType = type.asExactType();
-            if (exactType == null) {
-                AssumptionResult<ResolvedJavaType> leafConcreteSubtype = type.findLeafConcreteSubtype();
-                if (leafConcreteSubtype != null) {
-                    graph().getAssumptions().record(leafConcreteSubtype);
-                    exactType = leafConcreteSubtype.getResult();
-                }
-            }
-
-            if (exactType == type) {
-                return TypeCheckNode.create(exactType, ((GetClassNode) nonConstant).getObject());
+            if (type.isConcrete() || type.isArray()) {
+                return TypeCheckNode.create(type, ((GetClassNode) nonConstant).getObject());
             }
         }
         return super.canonicalizeSymmetricConstant(tool, constant, nonConstant, mirrored);
@@ -119,7 +109,7 @@ public final class ObjectEqualsNode extends PointerEqualsNode implements Virtual
                 /*
                  * One of the two objects has identity, the other doesn't. In code, this looks like
                  * "Integer.valueOf(a) == new Integer(b)", which is always false.
-                 * 
+                 *
                  * In other words: an object created via valueOf can never be equal to one created
                  * by new in the same compilation unit.
                  */
