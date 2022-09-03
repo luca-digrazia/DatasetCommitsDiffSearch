8947204823ace72ec87191e0420badd05a92d0b1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:05:55 +0200

    Fix eclipse format

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases/src/com/oracle/graal/phases/PhaseSuite.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases/src/com/oracle/graal/phases/PhaseSuite.java
@@ -22,35 +22,125 @@
  */
 package com.oracle.graal.phases;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
 
-import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.StructuredGraph;
 
+/**
+ * A compiler phase that can apply an ordered collection of phases to a graph.
+ */
 public class PhaseSuite<C> extends BasePhase<C> {
 
-    private final List<BasePhase<? super C>> phases;
+    private List<BasePhase<? super C>> phases;
+    private boolean immutable;
 
     public PhaseSuite() {
         this.phases = new ArrayList<>();
     }
 
-    public final void addPhase(BasePhase<? super C> phase) {
+    public boolean isImmutable() {
+        return immutable;
+    }
+
+    public synchronized void setImmutable() {
+        if (!immutable) {
+            phases = Collections.unmodifiableList(phases);
+            immutable = true;
+        }
+    }
+
+    /**
+     * Add a new phase at the beginning of this suite.
+     */
+    public final void prependPhase(BasePhase<? super C> phase) {
+        phases.add(0, phase);
+    }
+
+    /**
+     * Add a new phase at the end of this suite.
+     */
+    public final void appendPhase(BasePhase<? super C> phase) {
         phases.add(phase);
     }
 
+    /**
+     * Returns a {@link ListIterator} at the position of the first phase which is an instance of
+     * {@code phaseClass} or null if no such phase can be found.
+     *
+     * Calling {@link ListIterator#previous()} would return the phase that was found.
+     *
+     * @param phaseClass the type of phase to look for.
+     */
     public final ListIterator<BasePhase<? super C>> findPhase(Class<? extends BasePhase<? super C>> phaseClass) {
-        ListIterator<BasePhase<? super C>> it = phases.listIterator();
-        findNextPhase(it, phaseClass);
-        return it;
+        return findPhase(phaseClass, false);
     }
 
-    public static <C> void findNextPhase(ListIterator<BasePhase<? super C>> it, Class<? extends BasePhase<? super C>> phaseClass) {
+    /**
+     * Returns a {@link ListIterator} at the position of the first phase which is an instance of
+     * {@code phaseClass} or, if {@code recursive} is true, is a {@link PhaseSuite} containing a
+     * phase which is an instance of {@code phaseClass}. This method returns null if no such phase
+     * can be found.
+     *
+     * Calling {@link ListIterator#previous()} would return the phase or phase suite that was found.
+     *
+     * @param phaseClass the type of phase to look for
+     * @param recursive whether to recursively look into phase suites.
+     */
+    public final ListIterator<BasePhase<? super C>> findPhase(Class<? extends BasePhase<? super C>> phaseClass, boolean recursive) {
+        ListIterator<BasePhase<? super C>> it = phases.listIterator();
+        if (findNextPhase(it, phaseClass, recursive)) {
+            return it;
+        } else {
+            return null;
+        }
+    }
+
+    public static <C> boolean findNextPhase(ListIterator<BasePhase<? super C>> it, Class<? extends BasePhase<? super C>> phaseClass) {
+        return findNextPhase(it, phaseClass, false);
+    }
+
+    public static <C> boolean findNextPhase(ListIterator<BasePhase<? super C>> it, Class<? extends BasePhase<? super C>> phaseClass, boolean recursive) {
         while (it.hasNext()) {
             BasePhase<? super C> phase = it.next();
             if (phaseClass.isInstance(phase)) {
-                break;
+                return true;
+            } else if (recursive && phase instanceof PhaseSuite) {
+                @SuppressWarnings("unchecked")
+                PhaseSuite<C> suite = (PhaseSuite<C>) phase;
+                if (suite.findPhase(phaseClass, true) != null) {
+                    return true;
+                }
             }
         }
+        return false;
+    }
+
+    /**
+     * Removes the first instance of the given phase class, looking recursively into inner phase
+     * suites.
+     */
+    public boolean removePhase(Class<? extends BasePhase<? super C>> phaseClass) {
+        ListIterator<BasePhase<? super C>> it = phases.listIterator();
+        while (it.hasNext()) {
+            BasePhase<? super C> phase = it.next();
+            if (phaseClass.isInstance(phase)) {
+                it.remove();
+                return true;
+            } else if (phase instanceof PhaseSuite) {
+                @SuppressWarnings("unchecked")
+                PhaseSuite<C> innerSuite = (PhaseSuite<C>) phase;
+                if (innerSuite.removePhase(phaseClass)) {
+                    if (innerSuite.phases.isEmpty()) {
+                        it.remove();
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
@@ -58,5 +148,11 @@ public class PhaseSuite<C> extends BasePhase<C> {
         for (BasePhase<? super C> phase : phases) {
             phase.apply(graph, context);
         }
+    }
+
+    public PhaseSuite<C> copy() {
+        PhaseSuite<C> suite = new PhaseSuite<>();
+        suite.phases.addAll(phases);
+        return suite;
     }
 }
