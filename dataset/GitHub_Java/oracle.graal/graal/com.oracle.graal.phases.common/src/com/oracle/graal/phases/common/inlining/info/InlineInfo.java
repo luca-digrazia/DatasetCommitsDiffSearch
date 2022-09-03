665c3cd419a294d1ba875cddc8fa4a09093a3336
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 19:04:48 +0200

    [inliner] added a factory method in InlineInfo to make code uniform elsewhere

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/inlining/info/InlineInfo.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/inlining/info/InlineInfo.java
@@ -22,12 +22,18 @@
  */
 package com.oracle.graal.phases.common.inlining.info;
 
+import java.util.*;
+
 import com.oracle.graal.api.code.Assumptions;
 import com.oracle.graal.api.meta.MetaAccessProvider;
 import com.oracle.graal.api.meta.ResolvedJavaMethod;
+import com.oracle.graal.graph.*;
 import com.oracle.graal.nodes.Invoke;
 import com.oracle.graal.nodes.StructuredGraph;
+import com.oracle.graal.phases.common.CanonicalizerPhase;
 import com.oracle.graal.phases.common.inlining.info.elem.Inlineable;
+import com.oracle.graal.phases.common.inlining.walker.CallsiteHolder;
+import com.oracle.graal.phases.tiers.HighTierContext;
 import com.oracle.graal.phases.util.Providers;
 
 /**
@@ -68,8 +74,10 @@ public interface InlineInfo {
      * Performs the inlining described by this object and returns the node that represents the
      * return value of the inlined method (or null for void methods and methods that have no
      * non-exceptional exit).
+     * 
+     * @return a collection of nodes that need to be canonicalized after the inlining
      */
-    void inline(Providers providers, Assumptions assumptions);
+    Collection<Node> inline(Providers providers, Assumptions assumptions);
 
     /**
      * Try to make the call static bindable to avoid interface and virtual method calls.
@@ -77,4 +85,8 @@ public interface InlineInfo {
     void tryToDevirtualizeInvoke(MetaAccessProvider metaAccess, Assumptions assumptions);
 
     boolean shouldInline();
+
+    void populateInlinableElements(HighTierContext context, Assumptions calleeAssumptions, CanonicalizerPhase canonicalizer);
+
+    CallsiteHolder buildCallsiteHolderForElement(int index, double invokeProbability, double invokeRelevance);
 }
