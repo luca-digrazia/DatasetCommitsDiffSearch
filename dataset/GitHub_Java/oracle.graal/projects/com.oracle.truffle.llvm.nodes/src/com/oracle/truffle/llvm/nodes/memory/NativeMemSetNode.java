b82953c0a07e8af2fabcef71accadb3d32b7d200
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:52:05 +0200

    A missing spec for LLVMGlobal added to NativeMemSetNode

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/NativeMemSetNode.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/NativeMemSetNode.java
@@ -33,6 +33,7 @@ import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.llvm.nodes.intrinsics.interop.LLVMTruffleManagedMalloc.ManagedMallocObject;
 import com.oracle.truffle.llvm.runtime.LLVMAddress;
 import com.oracle.truffle.llvm.runtime.LLVMTruffleObject;
@@ -86,9 +87,9 @@ public abstract class NativeMemSetNode extends LLVMMemSetNode {
     }
 
     @Specialization(guards = {"!isManagedMallocObject(object)"})
-    protected Object memset(LLVMTruffleObject object, byte value, long length, @Cached("createToNativeWithTarget()") LLVMToNativeNode globalAccess,
+    protected Object memset(VirtualFrame frame, LLVMTruffleObject object, byte value, long length, @Cached("createToNativeWithTarget()") LLVMToNativeNode globalAccess,
                     @Cached("getLLVMMemory()") LLVMMemory memory) {
-        return memset(globalAccess.executeWithTarget(object), value, length, memory);
+        return memset(globalAccess.executeWithTarget(frame, object), value, length, memory);
     }
 
     @SuppressWarnings("unused")
@@ -109,10 +110,10 @@ public abstract class NativeMemSetNode extends LLVMMemSetNode {
     }
 
     @Specialization
-    protected Object memset(LLVMGlobal global, byte value, long length,
+    protected Object memset(VirtualFrame frame, LLVMGlobal global, byte value, long length,
                     @Cached("createToNativeWithTarget()") LLVMToNativeNode globalAccess,
                     @Cached("getLLVMMemory()") LLVMMemory memory) {
-        return memset(globalAccess.executeWithTarget(global), value, length, memory);
+        return memset(globalAccess.executeWithTarget(frame, global), value, length, memory);
     }
 
 }
