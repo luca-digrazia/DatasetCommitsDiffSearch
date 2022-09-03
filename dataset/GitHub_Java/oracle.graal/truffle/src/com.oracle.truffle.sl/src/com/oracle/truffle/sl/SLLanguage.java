6895f30b4eefe1fdf2275de88428b55cc0ffc8c1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:42:10 +0200

    SimpleLanguage needs an id property set.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/SLLanguage.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/SLLanguage.java
@@ -49,7 +49,6 @@ import com.oracle.truffle.api.debug.DebuggerTags;
 import com.oracle.truffle.api.frame.Frame;
 import com.oracle.truffle.api.instrumentation.ProvidedTags;
 import com.oracle.truffle.api.instrumentation.StandardTags;
-import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.metadata.ScopeProvider;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.source.Source;
@@ -150,11 +149,7 @@ public final class SLLanguage extends TruffleLanguage<SLContext> implements Scop
 
     @Override
     protected boolean isObjectOfLanguage(Object object) {
-        if (!(object instanceof TruffleObject)) {
-            return false;
-        }
-        TruffleObject truffleObject = (TruffleObject) object;
-        return truffleObject instanceof SLFunction || truffleObject instanceof SLBigNumber || SLContext.isSLObject(truffleObject);
+        return object instanceof SLFunction;
     }
 
     @Override
