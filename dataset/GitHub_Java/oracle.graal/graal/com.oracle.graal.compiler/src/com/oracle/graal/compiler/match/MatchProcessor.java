f4ccb0a7096fb3beac16406a193833ab5bfeaa84
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 18:09:39 +0200

    Allow unused imports in MatchStatementSets (as imports just taken from the originating class)

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/match/MatchProcessor.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/match/MatchProcessor.java
@@ -498,12 +498,7 @@ public class MatchProcessor extends AbstractProcessor {
                 out.println("import " + p + ".*;");
             }
             out.println("");
-
-            // FIXME: Ugly hack, don't know how to work this problem around (import
-            // com.oracle.graal.nodes.*; is unused, but cannot be avoided
-            if (matchStatementClassName.contains("SPARCNodeLIRBuilder")) {
-                out.println("@SuppressWarnings(\"unused\")");
-            }
+            out.println("@SuppressWarnings(\"unused\")");
             out.println("public class " + matchStatementClassName + " implements " + MatchStatementSet.class.getSimpleName() + " {");
 
             out.println();
