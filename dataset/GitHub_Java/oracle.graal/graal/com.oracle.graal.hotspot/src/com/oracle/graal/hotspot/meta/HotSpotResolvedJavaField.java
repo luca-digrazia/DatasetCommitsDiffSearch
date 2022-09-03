f2348e8495215f39f9616d474878396e963366a1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 23:56:23 +0200

    fixed bootstrap performance issue that occurred because non-static final fields are no longer assumed constant

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotResolvedJavaField.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotResolvedJavaField.java
@@ -98,6 +98,8 @@ public class HotSpotResolvedJavaField extends CompilerObject implements Resolved
                 Constant value = readValue(receiver);
                 if (assumeNonStaticFinalFieldsAsFinal(receiver.asObject().getClass()) || !value.isDefaultForKind()) {
                     return value;
+                } else {
+                    TTY.println("Non-static final field %s.%s=%s", holder, name, value.toString());
                 }
             }
         }
