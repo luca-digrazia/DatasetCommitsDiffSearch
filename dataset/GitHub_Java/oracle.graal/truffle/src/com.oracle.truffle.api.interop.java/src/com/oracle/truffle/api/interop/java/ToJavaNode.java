b62766ce7a90a53e464bfd23328f68753561b1cc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:02:27 +0200

    Make sure int methods take precedence over char ones

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/ToJavaNode.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/ToJavaNode.java
@@ -79,16 +79,13 @@ abstract class ToJavaNode extends Node {
     private Object convertImpl(Object value, Class<?> targetType, Type genericType, Object languageContext) {
         Object convertedValue;
         if (isAssignableFromTrufflePrimitiveType(targetType)) {
-            convertedValue = primitive.toPrimitive(value, targetType);
+            convertedValue = primitive.toPrimitive(value, targetType, 0);
             if (convertedValue != null) {
                 return convertedValue;
             } else if (targetType == char.class || targetType == Character.class) {
-                Integer safeChar = primitive.toInteger(value);
-                if (safeChar != null) {
-                    int v = safeChar;
-                    if (v >= 0 && v < 65536) {
-                        return (char) v;
-                    }
+                convertedValue = primitive.toPrimitive(value, targetType, 1);
+                if (convertedValue != null) {
+                    return convertedValue;
                 }
             }
         }
