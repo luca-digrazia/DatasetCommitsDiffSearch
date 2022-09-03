0b9b96d8f8de077e1276b16a916229d86c9e6147
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 07:02:41 +0200

    Implement rep prefix for stosb

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.asm.amd64/src/com/oracle/truffle/llvm/asm/amd64/AsmFactory.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.asm.amd64/src/com/oracle/truffle/llvm/asm/amd64/AsmFactory.java
@@ -58,7 +58,7 @@ import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64BsrNodeFactory.LLVMAMD64BsrqNo
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64BsrNodeFactory.LLVMAMD64BsrwNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64BswapNodeFactory.LLVMAMD64BswaplNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64BswapNodeFactory.LLVMAMD64BswapqNodeGen;
-import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64SetFlagNode;
+import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64ClearFlagNode;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64CmpNodeFactory.LLVMAMD64CmpbNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64CmpNodeFactory.LLVMAMD64CmplNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64CmpNodeFactory.LLVMAMD64CmpqNodeGen;
@@ -163,9 +163,6 @@ import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64ShrNodeFactory.LLVMAMD64ShrwNo
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64StoreFlagsFactory.LLVMAMD64SahfNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64StoreFlagsFactory.LLVMAMD64WriteFlagswNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64StosNodeFactory.LLVMAMD64StosbNodeGen;
-import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64StosNodeFactory.LLVMAMD64StosdNodeGen;
-import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64StosNodeFactory.LLVMAMD64StosqNodeGen;
-import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64StosNodeFactory.LLVMAMD64StoswNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64SubNodeFactory.LLVMAMD64SubbNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64SubNodeFactory.LLVMAMD64SublNodeGen;
 import com.oracle.truffle.llvm.nodes.asm.LLVMAMD64SubNodeFactory.LLVMAMD64SubqNodeGen;
@@ -408,21 +405,10 @@ class AsmFactory {
         }
     }
 
-    private void createRep(LLVMExpressionNode body) {
-        if ("rep".equals(currentPrefix)) {
-            LLVMExpressionNode rcx = getOperandLoad(PrimitiveType.I64, new AsmRegisterOperand("rcx"));
-            LLVMAMD64WriteValueNode writeRCX = getStore(PrimitiveType.I64, new AsmRegisterOperand("rcx"));
-            LLVMExpressionNode rep = new LLVMAMD64RepNode(writeRCX, rcx, body);
-            statements.add(rep);
-        } else {
-            statements.add(body);
-        }
-    }
-
     void createOperation(String operation) {
         switch (operation) {
             case "cld":
-                statements.add(new LLVMAMD64SetFlagNode(getFlagWrite(LLVMAMD64Flags.DF), false));
+                statements.add(new LLVMAMD64ClearFlagNode(getFlagWrite(LLVMAMD64Flags.DF)));
                 break;
             case "clc":
             case "cli":
@@ -459,10 +445,8 @@ class AsmFactory {
                 statements.add(write);
                 break;
             }
-            case "std":
-                statements.add(new LLVMAMD64SetFlagNode(getFlagWrite(LLVMAMD64Flags.DF), true));
-                break;
             case "stc":
+            case "std":
             case "sti":
                 statements.add(new LLVMUnsupportedInlineAssemblerNode(sourceLocation, "Unsupported operation: " + operation));
                 break;
@@ -516,34 +500,14 @@ class AsmFactory {
                 LLVMExpressionNode df = getFlag(LLVMAMD64Flags.DF);
                 LLVMAMD64WriteValueNode writeRDI = getStore(PrimitiveType.I64, new AsmRegisterOperand("rdi"));
                 LLVMExpressionNode stosb = LLVMAMD64StosbNodeGen.create(writeRDI, al, rdi, df);
-                createRep(stosb);
-                break;
-            }
-            case "stosw": {
-                LLVMExpressionNode ax = getOperandLoad(PrimitiveType.I16, new AsmRegisterOperand("ax"));
-                LLVMExpressionNode rdi = getOperandLoad(PrimitiveType.I64, new AsmRegisterOperand("rdi"));
-                LLVMExpressionNode df = getFlag(LLVMAMD64Flags.DF);
-                LLVMAMD64WriteValueNode writeRDI = getStore(PrimitiveType.I64, new AsmRegisterOperand("rdi"));
-                LLVMExpressionNode stosw = LLVMAMD64StoswNodeGen.create(writeRDI, ax, rdi, df);
-                createRep(stosw);
-                break;
-            }
-            case "stosd": {
-                LLVMExpressionNode eax = getOperandLoad(PrimitiveType.I32, new AsmRegisterOperand("eax"));
-                LLVMExpressionNode rdi = getOperandLoad(PrimitiveType.I64, new AsmRegisterOperand("rdi"));
-                LLVMExpressionNode df = getFlag(LLVMAMD64Flags.DF);
-                LLVMAMD64WriteValueNode writeRDI = getStore(PrimitiveType.I64, new AsmRegisterOperand("rdi"));
-                LLVMExpressionNode stosd = LLVMAMD64StosdNodeGen.create(writeRDI, eax, rdi, df);
-                createRep(stosd);
-                break;
-            }
-            case "stosq": {
-                LLVMExpressionNode rax = getOperandLoad(PrimitiveType.I64, new AsmRegisterOperand("rax"));
-                LLVMExpressionNode rdi = getOperandLoad(PrimitiveType.I64, new AsmRegisterOperand("rdi"));
-                LLVMExpressionNode df = getFlag(LLVMAMD64Flags.DF);
-                LLVMAMD64WriteValueNode writeRDI = getStore(PrimitiveType.I64, new AsmRegisterOperand("rdi"));
-                LLVMExpressionNode stosq = LLVMAMD64StosqNodeGen.create(writeRDI, rax, rdi, df);
-                createRep(stosq);
+                if ("rep".equals(currentPrefix)) {
+                    LLVMExpressionNode rcx = getOperandLoad(PrimitiveType.I64, new AsmRegisterOperand("rcx"));
+                    LLVMAMD64WriteValueNode writeRCX = getStore(PrimitiveType.I64, new AsmRegisterOperand("rcx"));
+                    LLVMExpressionNode rep = new LLVMAMD64RepNode(writeRCX, rcx, stosb);
+                    statements.add(rep);
+                } else {
+                    statements.add(stosb);
+                }
                 break;
             }
             default:
