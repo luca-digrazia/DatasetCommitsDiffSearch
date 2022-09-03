698aac8267014529599248752c08dbe9efcbc118
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 17:20:41 +0200

    Truffle: LoopNode: child field type can be RepeatingNode now

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/RepeatingNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/RepeatingNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,22 +29,8 @@ import com.oracle.truffle.api.frame.*;
 /**
  * Experimental API: may change significantly without notice.
  */
-public abstract class LoopNode extends Node {
+public interface RepeatingNode extends NodeInterface {
 
-    @Child protected RepeatingNode repeatingNode;
-
-    public LoopNode(RepeatingNode repeatingNode) {
-        this.repeatingNode = repeatingNode;
-    }
-
-    public abstract void executeLoop(VirtualFrame frame);
-
-    protected final boolean executeRepeatingNode(VirtualFrame frame) {
-        return getRepeatingNode().executeRepeating(frame);
-    }
-
-    public final RepeatingNode getRepeatingNode() {
-        return repeatingNode;
-    }
+    boolean executeRepeating(VirtualFrame frame);
 
 }
