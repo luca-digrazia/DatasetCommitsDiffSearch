6db96b0c86f02fa7671f48d84f1015fa67c96714
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:35:48 +0200

    collection failed speculations before partial evaluation

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/deopt/SubstrateSpeculationLog.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/deopt/SubstrateSpeculationLog.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -24,98 +22,42 @@
  */
 package com.oracle.svm.core.deopt;
 
-import static com.oracle.svm.core.snippets.KnownIntrinsics.convertUnknownValue;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import com.oracle.svm.core.meta.SubstrateObjectConstant;
 
 import jdk.vm.ci.meta.JavaConstant;
 import jdk.vm.ci.meta.SpeculationLog;
 
-// Checkstyle: allow synchronization
-
 public class SubstrateSpeculationLog implements SpeculationLog {
 
-    public static final class SubstrateSpeculation extends Speculation {
-        public SubstrateSpeculation(SpeculationReason reason) {
-            super(reason);
-        }
-    }
-
-    private static final class LogEntry {
-        private final SpeculationReason reason;
-        private final LogEntry next;
-
-        private LogEntry(SpeculationReason reason, LogEntry next) {
-            this.reason = reason;
-            this.next = next;
-        }
-    }
-
-    /** The collected set of speculations, for quick access during compilation. */
-    private Map<SpeculationReason, Boolean> failedSpeculations;
-
-    /**
-     * Newly added speculation failures. Atomic linked list to allow lock free append during
-     * deoptimization.
-     */
-    private volatile LogEntry addedFailedSpeculationsHead;
-
-    private static final AtomicReferenceFieldUpdater<SubstrateSpeculationLog, LogEntry> HEAD_UPDATER = AtomicReferenceFieldUpdater.newUpdater(SubstrateSpeculationLog.class,
-                    LogEntry.class, "addedFailedSpeculationsHead");
+    private final ConcurrentMap<SpeculationReason, Boolean> failedSpeculations = new ConcurrentHashMap<>();
 
     public void addFailedSpeculation(SpeculationReason speculation) {
-        /*
-         * This method is called from inside the VMOperation that performs deoptimization, and
-         * thefore must not be synchronization free. Note that this even precludes using a
-         * ConcurrentHashMap, because it also has some code paths that require synchronization.
-         *
-         * Therefore we use our own very simple atomic linked list.
-         */
-        while (true) {
-            LogEntry oldHead = addedFailedSpeculationsHead;
-            LogEntry newHead = new LogEntry(speculation, oldHead);
-            if (HEAD_UPDATER.compareAndSet(this, oldHead, newHead)) {
-                break;
-            }
-        }
+        failedSpeculations.put(speculation, Boolean.TRUE);
     }
 
     @Override
-    public synchronized void collectFailedSpeculations() {
-        LogEntry cur = HEAD_UPDATER.getAndSet(this, null);
-        while (cur != null) {
-            if (failedSpeculations == null) {
-                failedSpeculations = new HashMap<>();
-            }
-            failedSpeculations.put(cur.reason, Boolean.TRUE);
-            cur = cur.next;
-        }
+    public void collectFailedSpeculations() {
+        // Nothing to do.
     }
 
     @Override
-    public synchronized boolean maySpeculate(SpeculationReason reason) {
-        return failedSpeculations != null && !failedSpeculations.containsKey(reason);
+    public boolean maySpeculate(SpeculationReason reason) {
+        return !failedSpeculations.containsKey(reason);
     }
 
     @Override
-    public Speculation speculate(SpeculationReason reason) {
+    public JavaConstant speculate(SpeculationReason reason) {
         if (!maySpeculate(reason)) {
             throw new IllegalArgumentException("Cannot make speculation with reason " + reason + " as it is known to fail");
         }
-        return new SubstrateSpeculation(reason);
+        return SubstrateObjectConstant.forObject(reason);
     }
 
     @Override
     public boolean hasSpeculations() {
         return true;
-    }
-
-    @Override
-    public Speculation lookupSpeculation(JavaConstant constant) {
-        return new SubstrateSpeculation((SpeculationReason) convertUnknownValue(SubstrateObjectConstant.asObject(constant), Object.class));
     }
 }
