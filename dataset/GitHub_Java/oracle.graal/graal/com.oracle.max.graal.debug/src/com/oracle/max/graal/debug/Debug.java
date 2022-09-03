ccff666d4ff3ca516f4142707b05314338c03627
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 01:03:10 +0200

    Allow Debug context with multiple objects

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.max.graal.debug/src/com/oracle/max/graal/debug/Debug.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.max.graal.debug/src/com/oracle/max/graal/debug/Debug.java
@@ -22,60 +22,204 @@
  */
 package com.oracle.max.graal.debug;
 
-import java.io.*;
-
+import com.oracle.max.graal.debug.internal.*;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Debug {
-    public static boolean ENABLED = false;
+
+    private static boolean ENABLED = false;
+
+    public static void enable() {
+        ENABLED = true;
+        DebugScope.initialize();
+    }
+
+    public static boolean isEnabled() {
+        return ENABLED;
+    }
+
+    public static boolean isDumpEnabled() {
+        return ENABLED && DebugScope.getInstance().isDumpEnabled();
+    }
+
+    public static boolean isMeterEnabled() {
+        return ENABLED && DebugScope.getInstance().isMeterEnabled();
+    }
+
+    public static boolean isTimeEnabled() {
+        return ENABLED && DebugScope.getInstance().isTimeEnabled();
+    }
+
+    public static boolean isLogEnabled() {
+        return ENABLED && DebugScope.getInstance().isLogEnabled();
+    }
+
+    public static void sandbox(String name, Runnable runnable) {
+        if (ENABLED) {
+            DebugScope.getInstance().scope(name, runnable, null, true, new Object[0]);
+        } else {
+            runnable.run();
+        }
+    }
+
+    public static void scope(String name, Runnable runnable) {
+        scope(name, new Object[0], runnable);
+    }
+
+    public static <T> T scope(String name, Callable<T> callable) {
+        return scope(name, new Object[0], callable);
+    }
+
+    public static void scope(String name, Object context, Runnable runnable) {
+        scope(name, new Object[] {context}, runnable);
+    }
+
+    public static void scope(String name, Object[] context, Runnable runnable) {
+        if (ENABLED) {
+            DebugScope.getInstance().scope(name, runnable, null, false, context);
+        } else {
+            runnable.run();
+        }
+    }
+
+    public static String currentScope() {
+        if (ENABLED) {
+            return DebugScope.getInstance().getQualifiedName();
+        } else {
+            return "";
+        }
+    }
+
+    public static <T> T scope(String name, Object context, Callable<T> callable) {
+        return scope(name, new Object[] {context}, callable);
+    }
+
+    public static <T> T scope(String name, Object[] context, Callable<T> callable) {
+        if (ENABLED) {
+            return DebugScope.getInstance().scope(name, null, callable, false, context);
+        } else {
+            return DebugScope.call(callable);
+        }
+    }
 
     public static void log(String msg, Object... args) {
-        if (ENABLED) {
-            DebugContext.getInstance().log(msg, args);
+        if (ENABLED && DebugScope.getInstance().isLogEnabled()) {
+            DebugScope.getInstance().log(msg, args);
         }
     }
 
-    public static Scope openScope(String name, Object... context) {
+    public static void dump(Object object, String msg, Object... args) {
+        if (ENABLED && DebugScope.getInstance().isDumpEnabled()) {
+            DebugScope.getInstance().dump(object, msg, args);
+        }
+    }
+
+    public static Iterable<Object> context() {
         if (ENABLED) {
-            return new Scope(name, context);
+            return DebugScope.getInstance().getCurrentContext();
         } else {
-            return VOID_SCOPE;
+            return Collections.emptyList();
         }
     }
 
-    private static final Scope VOID_SCOPE = new Scope(null);
-
-    public static final class Scope implements AutoCloseable {
-        private String name;
-        private Object[] context;
-
-        private Scope(String name, Object... context) {
-            this.name = name;
-            this.context = context;
-        }
-
-        @Override
-        public void close() {
-        }
-    }
-
-    private static class ScopeChild {
-        private String name;
-
-        protected ScopeChild(String name) {
-            this.name = name;
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> contextSnapshot(Class<T> clazz) {
+        if (ENABLED) {
+            List<T> result = new ArrayList<>();
+            for (Object o : context()) {
+                if (clazz.isInstance(o)) {
+                    result.add((T) o);
+                }
+            }
+            return result;
+        } else {
+            return Collections.emptyList();
         }
     }
 
-    public static final class Metric extends ScopeChild {
-        private Metric(String name) {
-            super(name);
+    @SuppressWarnings("unchecked")
+    public static <T> T contextLookup(Class<T> clazz) {
+        if (ENABLED) {
+            for (Object o : context()) {
+                if (clazz.isInstance(o)) {
+                    return ((T) o);
+                }
+            }
         }
-
+        return null;
     }
 
-    public static final class Timer extends ScopeChild {
-        private Timer(String name) {
-            super(name);
+    public static DebugMetric metric(String name) {
+        if (ENABLED) {
+            return new MetricImpl(name);
+        } else {
+            return VOID_METRIC;
         }
     }
+
+    public static void setConfig(DebugConfig config) {
+        if (ENABLED) {
+            DebugScope.getInstance().setConfig(config);
+        }
+    }
+
+    public static DebugConfig fixedConfig(final boolean isLogEnabled, final boolean isDumpEnabled, final boolean isMeterEnabled, final boolean isTimerEnabled, final Collection<? extends DebugDumpHandler> dumpHandlers) {
+        return new DebugConfig() {
+
+            @Override
+            public boolean isLogEnabled() {
+                return isLogEnabled;
+            }
+
+            @Override
+            public boolean isMeterEnabled() {
+                return isMeterEnabled;
+            }
+
+            @Override
+            public boolean isDumpEnabled() {
+                return isDumpEnabled;
+            }
+
+            @Override
+            public boolean isTimeEnabled() {
+                return isTimerEnabled;
+            }
+
+            @Override
+            public RuntimeException interceptException(Throwable e) {
+                return null;
+            }
+
+            @Override
+            public Collection< ? extends DebugDumpHandler> dumpHandlers() {
+                return dumpHandlers;
+            }
+        };
+    }
+
+    private static final DebugMetric VOID_METRIC = new DebugMetric() {
+
+        public void increment() {
+        }
+
+        public void add(int value) {
+        }
+    };
+
+    public static DebugTimer timer(String name) {
+        if (ENABLED) {
+            return new TimerImpl(name);
+        } else {
+            return VOID_TIMER;
+        }
+    }
+
+    private static final DebugTimer VOID_TIMER = new DebugTimer() {
+
+        public TimerCloseable start() {
+            return TimerImpl.VOID_CLOSEABLE;
+        }
+    };
 }
