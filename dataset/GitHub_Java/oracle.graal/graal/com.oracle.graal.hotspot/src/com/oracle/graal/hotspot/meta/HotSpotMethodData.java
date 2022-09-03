832a83a95a9dac53d37e52e6582e054a7875ecb2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 17:33:22 +0200

    removed usages of HotSPotGraalRuntime.unsafeReadWord()

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotMethodData.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotMethodData.java
@@ -28,8 +28,6 @@ import static java.lang.String.*;
 
 import java.util.*;
 
-import sun.misc.*;
-
 import com.oracle.graal.api.meta.*;
 import com.oracle.graal.api.meta.JavaMethodProfile.ProfiledMethod;
 import com.oracle.graal.api.meta.JavaTypeProfile.ProfiledType;
@@ -173,7 +171,8 @@ public final class HotSpotMethodData extends CompilerObject {
 
     /**
      * Since the values are stored in cells (platform words) this method uses
-     * {@link Unsafe#getAddress} to read the right value on both little and big endian machines.
+     * {@link HotSpotGraalRuntime#unsafeReadWord} to read the right value on both little and big
+     * endian machines.
      */
     private long readUnsignedInt(int position, int offsetInBytes) {
         long fullOffsetInBytes = computeFullOffset(position, offsetInBytes);
@@ -187,7 +186,8 @@ public final class HotSpotMethodData extends CompilerObject {
 
     /**
      * Since the values are stored in cells (platform words) this method uses
-     * {@link Unsafe#getAddress} to read the right value on both little and big endian machines.
+     * {@link HotSpotGraalRuntime#unsafeReadWord} to read the right value on both little and big
+     * endian machines.
      */
     private int readInt(int position, int offsetInBytes) {
         long fullOffsetInBytes = computeFullOffset(position, offsetInBytes);
