ea6961d0453532549f40d37efae9667aae2c5c74
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 17:15:31 +0200

    made Graph.cachedLeafNodes stable across VM executions to support replay compilation

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.graph/src/com/oracle/graal/graph/Graph.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.graph/src/com/oracle/graal/graph/Graph.java
@@ -123,11 +123,6 @@ public class Graph {
             }
             return false;
         }
-
-        @Override
-        public String toString() {
-            return node.toString();
-        }
     }
 
     /**
@@ -749,9 +744,6 @@ public class Graph {
         node.id = id;
         if (nodeEventListener != null) {
             nodeEventListener.nodeAdded(node);
-        }
-        if (Fingerprint.ENABLED) {
-            Fingerprint.submit("%s: %s", NodeEvent.NODE_ADDED, node);
         }
     }
 
