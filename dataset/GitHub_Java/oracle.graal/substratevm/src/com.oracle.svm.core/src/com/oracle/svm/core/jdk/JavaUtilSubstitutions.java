1dd12629e215c42adb70aa5870433e43d2420775
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:05:52 +0200

    Enforce that @Alias fields have the default value unless FromAlias recomputation is used

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaUtilSubstitutions.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaUtilSubstitutions.java
@@ -32,8 +32,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.InjectAccessors;
@@ -95,6 +93,7 @@ final class Target_java_util_IdentityHashMap {
     Set<?> entrySet;
 }
 
+/* TODO: Why is sun.misc.SoftCache used with JDK-8, but sun.awt.SoftCache is not used with JDK-9? */
 @TargetClass(className = "sun.misc.SoftCache", onlyWith = JDK8OrEarlier.class)
 final class Target_sun_misc_SoftCache {
 
@@ -208,9 +207,9 @@ final class Target_java_util_concurrent_ConcurrentSkipListMap {
     ConcurrentNavigableMap<?, ?> descendingMapJDK8OrEarlier;
 
     @Alias //
-    @TargetElement(name = "descendingMap", onlyWith = JDK11OrLater.class) //
+    @TargetElement(name = "descendingMap", onlyWith = JDK9OrLater.class) //
     @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
-    Target_java_util_concurrent_ConcurrentSkipListMap_SubMap descendingMapJDK11OrLater;
+    Target_java_util_concurrent_ConcurrentSkipListMap_SubMap descendingMapJDK9OrLater;
 }
 
 @TargetClass(value = java.util.concurrent.ConcurrentSkipListMap.class, innerClass = "KeySet")
@@ -221,7 +220,7 @@ final class Target_java_util_concurrent_ConcurrentSkipListMap_KeySet {
 final class Target_java_util_concurrent_ConcurrentSkipListMap_EntrySet {
 }
 
-@TargetClass(value = java.util.concurrent.ConcurrentSkipListMap.class, innerClass = "SubMap", onlyWith = JDK11OrLater.class)
+@TargetClass(value = java.util.concurrent.ConcurrentSkipListMap.class, innerClass = "SubMap", onlyWith = JDK9OrLater.class)
 final class Target_java_util_concurrent_ConcurrentSkipListMap_SubMap {
 }
 
@@ -255,8 +254,6 @@ class SplittableRandomAccessors {
 
     private static volatile AtomicLong defaultGen;
 
-    private static final Lock lock = new ReentrantLock();
-
     /** The get-accessor for SplittableRandom.defaultGen. */
     static AtomicLong getDefaultGen() {
         AtomicLong result = defaultGen;
@@ -266,37 +263,33 @@ class SplittableRandomAccessors {
         return result;
     }
 
-    private static AtomicLong initialize() {
-        lock.lock();
-        try {
-
-            AtomicLong result = defaultGen;
-            if (result != null) {
-                return result;
-            }
-
-            /*
-             * The code below to compute the seed is taken from the original
-             * SplittableRandom.initialSeed() implementation.
-             */
-            long seed;
-            if (SECURE_SEED) {
-                byte[] seedBytes = java.security.SecureRandom.getSeed(8);
-                seed = seedBytes[0] & 0xffL;
-                for (int i = 1; i < 8; ++i) {
-                    seed = (seed << 8) | (seedBytes[i] & 0xffL);
-                }
-            } else {
-                seed = Target_java_util_SplittableRandom.mix64(System.currentTimeMillis()) ^ Target_java_util_SplittableRandom.mix64(System.nanoTime());
-            }
-
-            result = new AtomicLong(seed);
-            defaultGen = result;
+    // Checkstyle: allow synchronization
+    private static synchronized AtomicLong initialize() {
+        AtomicLong result = defaultGen;
+        if (result != null) {
             return result;
-        } finally {
-            lock.unlock();
         }
+
+        /*
+         * The code below to compute the seed is taken from the original
+         * SplittableRandom.initialSeed() implementation.
+         */
+        long seed;
+        if (SECURE_SEED) {
+            byte[] seedBytes = java.security.SecureRandom.getSeed(8);
+            seed = seedBytes[0] & 0xffL;
+            for (int i = 1; i < 8; ++i) {
+                seed = (seed << 8) | (seedBytes[i] & 0xffL);
+            }
+        } else {
+            seed = Target_java_util_SplittableRandom.mix64(System.currentTimeMillis()) ^ Target_java_util_SplittableRandom.mix64(System.nanoTime());
+        }
+
+        result = new AtomicLong(seed);
+        defaultGen = result;
+        return result;
     }
+    // Checkstyle: disallow synchronization
 }
 
 @TargetClass(java.util.Currency.class)
