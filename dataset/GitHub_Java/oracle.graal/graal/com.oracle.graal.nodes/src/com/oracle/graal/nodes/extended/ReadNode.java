8525420effe07e15f3d350c32b4ed948e31281bf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 21:42:33 +0200

    ReadNode: minor refactoring in push()

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/ReadNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/ReadNode.java
@@ -72,7 +72,7 @@ public final class ReadNode extends FloatableAccessNode implements Node.Iterable
 
     public static ValueNode canonicalizeRead(ValueNode read, LocationNode location, ValueNode object, CanonicalizerTool tool, boolean compressible) {
         MetaAccessProvider runtime = tool.runtime();
-        if (read.usages().isEmpty()) {
+        if (read.usages().count() == 0) {
             // Read without usages can be savely removed.
             return null;
         }
