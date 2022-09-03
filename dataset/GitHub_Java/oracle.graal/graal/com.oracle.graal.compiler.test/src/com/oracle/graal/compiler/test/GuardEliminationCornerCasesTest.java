a2f0b233eee83ff5c36a2179f3a4aa403365ced0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 16:09:50 +0200

    Skip deoptimizations that do not invalidate the compiled code in ConvertDeoptimizationToGuards

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.test/src/com/oracle/graal/compiler/test/GuardEliminationCornerCasesTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.test/src/com/oracle/graal/compiler/test/GuardEliminationCornerCasesTest.java
@@ -22,27 +22,18 @@
  */
 package com.oracle.graal.compiler.test;
 
-import org.junit.Test;
+import org.junit.*;
 
-import com.oracle.graal.api.directives.GraalDirectives;
-import com.oracle.graal.debug.Debug;
-import com.oracle.graal.graph.Node;
-import com.oracle.graal.nodes.AbstractBeginNode;
-import com.oracle.graal.nodes.BeginNode;
-import com.oracle.graal.nodes.FixedNode;
-import com.oracle.graal.nodes.GuardNode;
-import com.oracle.graal.nodes.LogicNode;
-import com.oracle.graal.nodes.StructuredGraph;
+import com.oracle.graal.api.directives.*;
+import com.oracle.graal.debug.*;
+import com.oracle.graal.graph.*;
+import com.oracle.graal.nodes.*;
 import com.oracle.graal.nodes.StructuredGraph.AllowAssumptions;
-import com.oracle.graal.nodes.java.InstanceOfNode;
-import com.oracle.graal.nodes.spi.LoweringTool;
-import com.oracle.graal.nodes.spi.ValueProxy;
-import com.oracle.graal.phases.common.CanonicalizerPhase;
-import com.oracle.graal.phases.common.ConditionalEliminationPhase;
-import com.oracle.graal.phases.common.ConvertDeoptimizeToGuardPhase;
-import com.oracle.graal.phases.common.LoweringPhase;
-import com.oracle.graal.phases.schedule.SchedulePhase;
-import com.oracle.graal.phases.tiers.HighTierContext;
+import com.oracle.graal.nodes.java.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.phases.common.*;
+import com.oracle.graal.phases.schedule.*;
+import com.oracle.graal.phases.tiers.*;
 
 public class GuardEliminationCornerCasesTest extends GraalCompilerTest {
 
@@ -94,7 +85,7 @@ public class GuardEliminationCornerCasesTest extends GraalCompilerTest {
         for (Node n : graph.getNodes()) {
             if (n instanceof GuardNode) {
                 GuardNode guardNode = (GuardNode) n;
-                LogicNode condition = guardNode.getCondition();
+                LogicNode condition = guardNode.condition();
                 if (condition instanceof InstanceOfNode) {
                     InstanceOfNode instanceOfNode = (InstanceOfNode) condition;
                     if (instanceOfNode.getValue() instanceof ValueProxy) {
