02e5cbfaaa7228a2501e33f6e350b69b977ff617
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:27:48 +0200

    made NewObjectSnippets optionally load the initial mark word when initializing an object - this is disabled until the bug triggered in lusearch by this change is fixed

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/snippets/HotSpotSnippetUtils.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/snippets/HotSpotSnippetUtils.java
@@ -82,8 +82,8 @@ public class HotSpotSnippetUtils {
     }
 
     @Fold
-    static int prototypeMarkWordOffset() {
-        return HotSpotGraalRuntime.getInstance().getConfig().prototypeMarkWordOffset;
+    static int initialMarkWordOffset() {
+        return HotSpotGraalRuntime.getInstance().getConfig().initialMarkWordOffset;
     }
 
     @Fold
