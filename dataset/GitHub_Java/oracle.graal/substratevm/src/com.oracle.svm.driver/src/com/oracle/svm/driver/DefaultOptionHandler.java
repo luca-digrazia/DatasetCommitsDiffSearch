d2ca86cebe400c7f1bc0b19646ba2c248028adee
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:02:49 +0200

    Make sure jar-based -H:Class & -H:Name are only fallbacks

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/DefaultOptionHandler.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/DefaultOptionHandler.java
@@ -168,7 +168,7 @@ class DefaultOptionHandler extends NativeImage.OptionHandler<NativeImage> {
             if (headArg.equals(optimizeOption)) {
                 NativeImage.showError("The " + optimizeOption + " option should not be followed by a space");
             } else {
-                nativeImage.addPlainImageBuilderArg(nativeImage.oHOptimize + headArg.substring(2));
+                nativeImage.addPlainImageBuilderArg(NativeImage.oHOptimize + headArg.substring(2));
             }
             return true;
         }
@@ -183,7 +183,7 @@ class DefaultOptionHandler extends NativeImage.OptionHandler<NativeImage> {
             if (mainClass == null) {
                 NativeImage.showError("No main manifest attribute, in " + filePath);
             }
-            nativeImage.addPlainImageBuilderArg(nativeImage.oHClass + mainClass);
+            nativeImage.addPlainImageBuilderArg(NativeImage.oHClass + mainClass);
             String jarFileName = filePath.getFileName().toString();
             String jarSuffix = ".jar";
             String jarFileNameBase;
@@ -193,7 +193,7 @@ class DefaultOptionHandler extends NativeImage.OptionHandler<NativeImage> {
                 jarFileNameBase = jarFileName;
             }
             if (!jarFileNameBase.isEmpty()) {
-                nativeImage.addPlainImageBuilderArg(nativeImage.oHName + jarFileNameBase);
+                nativeImage.addPlainImageBuilderArg(NativeImage.oHName + jarFileNameBase);
             }
             String classPath = mainAttributes.getValue("Class-Path");
             /* Missing Class-Path Attribute is tolerable */
