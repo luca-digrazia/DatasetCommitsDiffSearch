e8171789b7854fbdaf12dec74b97dbcdce5b4e71
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:53:42 +0200

    Binary ops shouldn't allow stack unless they really support it

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir.amd64/src/com/oracle/graal/lir/amd64/AMD64Binary.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir.amd64/src/com/oracle/graal/lir/amd64/AMD64Binary.java
@@ -105,9 +105,9 @@ public class AMD64Binary {
         @Opcode private final AMD64RRMOp opcode;
         private final OperandSize size;
 
-        @Def({REG}) protected AllocatableValue result;
+        @Def({REG, HINT}) protected AllocatableValue result;
         @Use({REG}) protected AllocatableValue x;
-        @Use({REG, STACK}) protected AllocatableValue y;
+        @Alive({REG, STACK}) protected AllocatableValue y;
 
         public ThreeOp(AMD64RRMOp opcode, OperandSize size, AllocatableValue result, AllocatableValue x, AllocatableValue y) {
             super(TYPE);
@@ -181,7 +181,7 @@ public class AMD64Binary {
         @Opcode private final AMD64RRMOp opcode;
         private final OperandSize size;
 
-        @Def({REG}) protected AllocatableValue result;
+        @Def({REG, HINT}) protected AllocatableValue result;
         @Use({REG}) protected AllocatableValue x;
         @Use({REG, STACK}) protected AllocatableValue y;
 
@@ -289,7 +289,7 @@ public class AMD64Binary {
         @Opcode private final AMD64RRMOp opcode;
         private final OperandSize size;
 
-        @Def({REG}) protected AllocatableValue result;
+        @Def({REG, HINT}) protected AllocatableValue result;
         @Use({REG}) protected AllocatableValue x;
         private final JavaConstant y;
 
@@ -380,9 +380,9 @@ public class AMD64Binary {
         @Opcode private final AMD64RRMOp opcode;
         private final OperandSize size;
 
-        @Def({REG}) protected AllocatableValue result;
+        @Def({REG, HINT}) protected AllocatableValue result;
         @Use({REG}) protected AllocatableValue x;
-        @Use({COMPOSITE}) protected AMD64AddressValue y;
+        @Alive({COMPOSITE}) protected AMD64AddressValue y;
 
         @State protected LIRFrameState state;
 
