61feaaae73e48a7c22acf2d286df8dfe037c5dd9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:55:23 +0200

    Rename packages because of the jdk.internal.misc split in JDK12

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.test/src/org/graalvm/compiler/test/GraalTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.test/src/org/graalvm/compiler/test/GraalTest.java
@@ -240,10 +240,8 @@ public class GraalTest {
         assertDeepEquals(message, expected, actual, equalFloatsOrDoublesDelta());
     }
 
-    /**
-     * @see "https://bugs.openjdk.java.net/browse/JDK-8076557"
-     */
-    public static void assumeManagementLibraryIsLoadable() {
+    /** @see <a href="https://bugs.openjdk.java.net/browse/JDK-8076557">JDK-8076557</a> */
+    protected static void assumeManagementLibraryIsLoadable() {
         try {
             /* Trigger loading of the management library using the bootstrap class loader. */
             GraalServices.getCurrentThreadAllocatedBytes();
