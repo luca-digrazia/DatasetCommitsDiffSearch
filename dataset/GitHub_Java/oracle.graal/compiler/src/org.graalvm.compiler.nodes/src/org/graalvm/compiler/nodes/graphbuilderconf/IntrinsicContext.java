46a2f78ad79c56eecdd8c8566e7383916ca4e141
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:55:01 +0200

    Don't move guard through LoopExitNodes missing FrameStates

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/graphbuilderconf/IntrinsicContext.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/graphbuilderconf/IntrinsicContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,7 +28,6 @@ import static jdk.vm.ci.code.BytecodeFrame.AFTER_BCI;
 import static jdk.vm.ci.code.BytecodeFrame.AFTER_EXCEPTION_BCI;
 import static jdk.vm.ci.code.BytecodeFrame.BEFORE_BCI;
 import static jdk.vm.ci.code.BytecodeFrame.INVALID_FRAMESTATE_BCI;
-import static org.graalvm.compiler.nodes.graphbuilderconf.IntrinsicContext.CompilationContext.ENCODING;
 import static org.graalvm.compiler.nodes.graphbuilderconf.IntrinsicContext.CompilationContext.INLINE_AFTER_PARSING;
 import static org.graalvm.compiler.nodes.graphbuilderconf.IntrinsicContext.CompilationContext.ROOT_COMPILATION;
 
@@ -137,11 +136,7 @@ public class IntrinsicContext {
     }
 
     public boolean isCompilationRoot() {
-        return compilationContext.equals(ROOT_COMPILATION) || compilationContext.equals(ENCODING);
-    }
-
-    public boolean isIntrinsicEncoding() {
-        return compilationContext.equals(ENCODING);
+        return compilationContext.equals(ROOT_COMPILATION);
     }
 
     public NodeSourcePosition getNodeSourcePosition() {
@@ -171,12 +166,7 @@ public class IntrinsicContext {
         /**
          * An intrinsic is the root of compilation.
          */
-        ROOT_COMPILATION,
-
-        /**
-         * An intrinsic is the root of a compilation done for graph encoding.
-         */
-        ENCODING
+        ROOT_COMPILATION
     }
 
     /**
@@ -201,11 +191,6 @@ public class IntrinsicContext {
         void addSideEffect(StateSplit sideEffect);
     }
 
-    @SuppressWarnings("unused")
-    public boolean isDeferredInvoke(StateSplit stateSplit) {
-        return false;
-    }
-
     public FrameState createFrameState(StructuredGraph graph, SideEffectsState sideEffects, StateSplit forStateSplit, NodeSourcePosition sourcePosition) {
         assert forStateSplit != graph.start();
         if (forStateSplit.hasSideEffect()) {
@@ -220,16 +205,12 @@ public class IntrinsicContext {
                     lastSideEffect.setStateAfter(invalid);
                 }
             }
+            sideEffects.addSideEffect(forStateSplit);
             FrameState frameState;
-            if (isDeferredInvoke(forStateSplit)) {
-                frameState = graph.add(new FrameState(INVALID_FRAMESTATE_BCI));
+            if (forStateSplit instanceof ExceptionObjectNode) {
+                frameState = graph.add(new FrameState(AFTER_EXCEPTION_BCI, (ExceptionObjectNode) forStateSplit));
             } else {
-                sideEffects.addSideEffect(forStateSplit);
-                if (forStateSplit instanceof ExceptionObjectNode) {
-                    frameState = graph.add(new FrameState(AFTER_EXCEPTION_BCI, (ExceptionObjectNode) forStateSplit));
-                } else {
-                    frameState = graph.add(new FrameState(AFTER_BCI));
-                }
+                frameState = graph.add(new FrameState(AFTER_BCI));
             }
             if (graph.trackNodeSourcePosition()) {
                 frameState.setNodeSourcePosition(sourcePosition);
@@ -262,7 +243,6 @@ public class IntrinsicContext {
 
     @Override
     public String toString() {
-        return "Intrinsic{original: " + originalMethod.format("%H.%n(%p)") + ", intrinsic: " + (intrinsicMethod != null ? intrinsicMethod.format("%H.%n(%p)") : "null") + ", context: " +
-                        compilationContext + "}";
+        return "Intrinsic{original: " + originalMethod.format("%H.%n(%p)") + ", intrinsic: " + intrinsicMethod.format("%H.%n(%p)") + ", context: " + compilationContext + "}";
     }
 }
