a8252738f4b681d33e7484b1a08a938fd6cd06f1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 18:43:01 +0200

    added forwarding methods to MetaUtil and marked them with @Deprecated to simplify adapting new API

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.api.meta/src/com/oracle/graal/api/meta/JavaType.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.api.meta/src/com/oracle/graal/api/meta/JavaType.java
@@ -28,7 +28,7 @@ import static com.oracle.graal.api.meta.MetaUtil.*;
  * Represents a resolved or unresolved type. Types include primitives, objects, {@code void}, and
  * arrays thereof.
  */
-public interface JavaType extends Remote {
+public interface JavaType {
 
     /**
      * Returns the name of this type in internal form. The following are examples of strings
@@ -41,25 +41,6 @@ public interface JavaType extends Remote {
      * </pre>
      */
     String getName();
-
-    /**
-     * Returns an unqualified name of this type.
-     *
-     * <pre>
-     *     "Object"
-     *     "Integer"
-     * </pre>
-     */
-    default String getUnqualifiedName() {
-        String name = getName();
-        if (name.indexOf('/') != -1) {
-            name = name.substring(name.lastIndexOf('/') + 1);
-        }
-        if (name.endsWith(";")) {
-            name = name.substring(0, name.length() - 1);
-        }
-        return name;
-    }
 
     /**
      * For array types, gets the type of the components, or {@code null} if this is not an array
