5d525687fda110f3eeb108404c8ec07b32c56854
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:07:07 +0200

    LazyInitializationTest: whitelist OptionValue types

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.truffle.test/src/com/oracle/graal/truffle/test/LazyInitializationTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.truffle.test/src/com/oracle/graal/truffle/test/LazyInitializationTest.java
@@ -25,14 +25,18 @@ package com.oracle.graal.truffle.test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-
-import jdk.vm.ci.runtime.JVMCICompilerFactory;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 
 import com.oracle.graal.compiler.CompilerThreadFactory;
@@ -41,8 +45,9 @@ import com.oracle.graal.options.OptionDescriptor;
 import com.oracle.graal.options.OptionDescriptors;
 import com.oracle.graal.options.OptionValue;
 import com.oracle.graal.options.OptionsParser;
-import com.oracle.graal.options.OptionsParser.OptionDescriptorsProvider;
 import com.oracle.graal.test.SubprocessUtil;
+
+import jdk.vm.ci.runtime.services.JVMCICompilerFactory;
 
 /**
  * Test lazy initialization of Graal in the context of Truffle. When simply executing Truffle code,
@@ -52,10 +57,14 @@ public class LazyInitializationTest {
 
     private final Class<?> hotSpotVMEventListener;
     private final Class<?> hotSpotGraalCompilerFactoryOptions;
+    private final Class<?> jvmciVersionCheck;
+
+    private static boolean Java8OrEarlier = System.getProperty("java.specification.version").compareTo("1.9") < 0;
 
     public LazyInitializationTest() {
-        hotSpotVMEventListener = forNameOrNull("jdk.vm.ci.hotspot.HotSpotVMEventListener");
+        hotSpotVMEventListener = forNameOrNull("jdk.vm.ci.hotspot.services.HotSpotVMEventListener");
         hotSpotGraalCompilerFactoryOptions = forNameOrNull("com.oracle.graal.hotspot.HotSpotGraalCompilerFactory$Options");
+        jvmciVersionCheck = forNameOrNull("com.oracle.graal.hotspot.JVMCIVersionCheck");
     }
 
     private static Class<?> forNameOrNull(String name) {
@@ -68,11 +77,34 @@ public class LazyInitializationTest {
 
     @Test
     public void testSLTck() throws IOException, InterruptedException {
-        spawnUnitTests("com.oracle.truffle.sl.test.SLTckTest");
+        spawnUnitTests("com.oracle.truffle.sl.test.SLFactorialTest");
     }
 
     private static final String VERBOSE_PROPERTY = "LazyInitializationTest.verbose";
     private static final boolean VERBOSE = Boolean.getBoolean(VERBOSE_PROPERTY);
+
+    private static final Pattern CLASS_INIT_LOG_PATTERN = Pattern.compile("\\[info\\]\\[class,init\\] \\d+ Initializing '([^']+)'");
+
+    /**
+     * Extracts the class name from a line of log output.
+     */
+    private static String extractClass(String line) {
+        if (Java8OrEarlier) {
+            String traceClassLoadingPrefix = "[Loaded ";
+            int index = line.indexOf(traceClassLoadingPrefix);
+            if (index != -1) {
+                int start = index + traceClassLoadingPrefix.length();
+                int end = line.indexOf(' ', start);
+                return line.substring(start, end);
+            }
+        } else {
+            Matcher matcher = CLASS_INIT_LOG_PATTERN.matcher(line);
+            if (matcher.find()) {
+                return matcher.group(1).replace('/', '.');
+            }
+        }
+        return null;
+    }
 
     /**
      * Spawn a new VM, execute unit tests, and check which classes are loaded.
@@ -80,23 +112,34 @@ public class LazyInitializationTest {
     private void spawnUnitTests(String... tests) throws IOException, InterruptedException {
         ArrayList<String> args = new ArrayList<>(SubprocessUtil.getVMCommandLine());
 
-        int jvmciArg = args.indexOf("-jvmci");
-        if (jvmciArg >= 0) {
-            args.set(jvmciArg, "-server");
+        // Remove debugger arguments
+        for (Iterator<String> i = args.iterator(); i.hasNext();) {
+            String arg = i.next();
+            if (arg.equals("-Xdebug") || arg.startsWith("-Xrunjdwp:")) {
+                i.remove();
+            }
         }
 
-        args.add("-XX:+TraceClassLoading");
+        boolean usesJvmciCompiler = args.contains("-jvmci") || args.contains("-XX:+UseJVMCICompiler");
+        Assume.assumeFalse("This test can only run if JVMCI is not one of the default compilers", usesJvmciCompiler);
+
+        args.add(Java8OrEarlier ? "-XX:+TraceClassLoading" : "-Xlog:class+init=info");
         args.add("com.oracle.mxtool.junit.MxJUnitWrapper");
         args.addAll(Arrays.asList(tests));
 
-        ArrayList<Class<?>> loadedGraalClasses = new ArrayList<>();
+        ArrayList<String> loadedGraalClassNames = new ArrayList<>();
 
-        Process process = new ProcessBuilder(args).start();
+        ProcessBuilder processBuilder = new ProcessBuilder(args);
+        if (VERBOSE) {
+            processBuilder.redirectError(Redirect.INHERIT);
+        }
 
         if (VERBOSE) {
-            System.out.println("-----------------------------------------------------------------------------");
+            System.out.println("\n=============================================================================");
             System.out.println(Util.join(args, " "));
+            System.out.println("-----------------------------------------------------------------------------");
         }
+        Process process = processBuilder.start();
         int testCount = 0;
         BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
@@ -104,16 +147,10 @@ public class LazyInitializationTest {
             if (VERBOSE) {
                 System.out.println(line);
             }
-            if (line.startsWith("[Loaded ")) {
-                int start = "[Loaded ".length();
-                int end = line.indexOf(' ', start);
-                String loadedClass = line.substring(start, end);
+            String loadedClass = extractClass(line);
+            if (loadedClass != null) {
                 if (isGraalClass(loadedClass)) {
-                    try {
-                        loadedGraalClasses.add(Class.forName(loadedClass));
-                    } catch (ClassNotFoundException e) {
-                        Assert.fail("loaded class " + loadedClass + " not found");
-                    }
+                    loadedGraalClassNames.add(loadedClass);
                 }
             } else if (line.startsWith("OK (")) {
                 Assert.assertTrue(testCount == 0);
@@ -123,28 +160,38 @@ public class LazyInitializationTest {
             }
         }
         if (VERBOSE) {
-            System.out.println("-----------------------------------------------------------------------------");
+            System.out.println("=============================================================================");
         }
 
         String suffix = VERBOSE ? "" : " (use -D" + VERBOSE_PROPERTY + "=true to debug)";
-        Assert.assertNotEquals("test count" + suffix, 0, testCount);
         Assert.assertEquals("exit code" + suffix, 0, process.waitFor());
+        Assert.assertNotEquals("test count" + suffix, 0, testCount);
 
-        checkAllowedGraalClasses(loadedGraalClasses, suffix);
+        checkAllowedGraalClasses(loadedGraalClassNames, suffix);
     }
 
     private static boolean isGraalClass(String className) {
-        if (className.startsWith("com.oracle.graal.truffle.")) {
+        if (className.startsWith("com.oracle.graal.truffle.") || className.startsWith("com.oracle.graal.serviceprovider.")) {
             // Ignore classes in the com.oracle.graal.truffle package, they are all allowed.
+            // Also ignore classes in the graal serviceprovider package, as they might not be lazily
+            // loaded.
             return false;
         } else {
             return className.startsWith("com.oracle.graal.");
         }
     }
 
-    private void checkAllowedGraalClasses(List<Class<?>> loadedGraalClasses, String errorMessageSuffix) {
+    private void checkAllowedGraalClasses(List<String> loadedGraalClassNames, String errorMessageSuffix) {
         HashSet<Class<?>> whitelist = new HashSet<>();
+        List<Class<?>> loadedGraalClasses = new ArrayList<>();
 
+        for (String name : loadedGraalClassNames) {
+            try {
+                loadedGraalClasses.add(Class.forName(name));
+            } catch (ClassNotFoundException e) {
+                Assert.fail("loaded class " + name + " not found");
+            }
+        }
         /*
          * Look for all loaded OptionDescriptors classes, and whitelist the classes that declare the
          * options. They may be loaded by the option parsing code.
@@ -155,20 +202,25 @@ public class LazyInitializationTest {
                     OptionDescriptors optionDescriptors = cls.asSubclass(OptionDescriptors.class).newInstance();
                     for (OptionDescriptor option : optionDescriptors) {
                         whitelist.add(option.getDeclaringClass());
+                        whitelist.add(option.getType());
                     }
                 } catch (ReflectiveOperationException e) {
                 }
             }
         }
 
+        List<String> forbiddenClasses = new ArrayList<>();
         for (Class<?> cls : loadedGraalClasses) {
             if (whitelist.contains(cls)) {
                 continue;
             }
 
             if (!isGraalClassAllowed(cls)) {
-                Assert.fail("loaded class: " + cls.getName() + errorMessageSuffix);
+                forbiddenClasses.add(cls.getName());
             }
+        }
+        if (!forbiddenClasses.isEmpty()) {
+            Assert.fail("loaded forbidden classes:\n    " + forbiddenClasses.stream().collect(Collectors.joining("\n    ")) + "\n" + errorMessageSuffix);
         }
     }
 
@@ -179,7 +231,17 @@ public class LazyInitializationTest {
         }
 
         if (cls.equals(hotSpotGraalCompilerFactoryOptions)) {
-            // The JVMCI initialization code needs to accesses an option defined in this class.
+            // Graal initialization needs to access this class.
+            return true;
+        }
+
+        if (cls.equals(Util.class)) {
+            // Provider of the Java runtime check utility used during Graal initialization.
+            return true;
+        }
+
+        if (cls.equals(jvmciVersionCheck)) {
+            // The Graal initialization needs to check the JVMCI version.
             return true;
         }
 
@@ -193,13 +255,18 @@ public class LazyInitializationTest {
             return true;
         }
 
-        if (OptionDescriptorsProvider.class.isAssignableFrom(cls) || cls == OptionsParser.class) {
+        if (cls == OptionsParser.class) {
             // Classes implementing Graal option loading
             return true;
         }
 
         if (OptionValue.class.isAssignableFrom(cls)) {
             // If options are specified, that may implicitly load a custom OptionValue subclass.
+            return true;
+        }
+
+        if (OptionValue.OverrideScope.class.isAssignableFrom(cls)) {
+            // Reading options can check override scopes
             return true;
         }
 
