79cf447ea7e8826b0f8aabee4a1c8c0995e1e9ef
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 01:42:26 +0200

    Fix for degenerated phis. Now jtt and DaCapo/fop passes with ArithmeticOp and Constant being flexible in the graph.

--- dataset/GitHub_Java/oracle.graal/graal/GraalCompiler/src/com/oracle/max/graal/schedule/Schedule.java
+++ dataset/GitHub_Java/oracle.graal/graal/GraalCompiler/src/com/oracle/max/graal/schedule/Schedule.java
@@ -243,13 +243,6 @@ public class Schedule {
                         block = getCommonDominator(block, mergeBlock.getPredecessors().get(i));
                     }
                 }
-            } else if (usage instanceof FrameState && ((FrameState) usage).block() != null) {
-                Merge merge = ((FrameState) usage).block();
-                for (Node pred : merge.predecessors()) {
-                    if (isCFG(pred)) {
-                        block = getCommonDominator(block, nodeToBlock.get(pred));
-                    }
-                }
             } else {
                 block = getCommonDominator(block, assignLatestPossibleBlockToNode(usage));
             }
