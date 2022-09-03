a1f14cb1ee0b176015a98ad1a17f2f1a17c98b4d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:33:34 +0200

    Invalidate compiled code when changing the FrameSlotKind of a FrameSlot
        The field kind is defined as @CompilationFinal, so according to Truffle rules compiled code must be invalidated before changing it. That most likely happened anyway because of the version assumption that got invalidated, but it is better to have it explicit.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/frame/FrameSlot.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/frame/FrameSlot.java
@@ -24,11 +24,14 @@
  */
 package com.oracle.truffle.api.frame;
 
-import com.oracle.truffle.api.*;
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 
 /**
- * A slot in a frame that can store a value of a given type.
+ * A slot in a {@link Frame} and {@link FrameDescriptor} that can store a value of a given type.
+ * 
+ * @since 0.8 or earlier
  */
 public final class FrameSlot implements Cloneable {
 
@@ -38,7 +41,19 @@ public final class FrameSlot implements Cloneable {
     private final int index;
     @CompilationFinal private FrameSlotKind kind;
 
+    /**
+     * @deprecated use
+     *             {@link FrameDescriptor#addFrameSlot(java.lang.Object, java.lang.Object, com.oracle.truffle.api.frame.FrameSlotKind)}
+     *             to create new instance of the slot. This method will be made package private in
+     *             the future.
+     * @since 0.8 or earlier
+     */
+    @Deprecated
     public FrameSlot(FrameDescriptor descriptor, Object identifier, Object info, int index, FrameSlotKind kind) {
+        this(descriptor, identifier, info, kind, index);
+    }
+
+    FrameSlot(FrameDescriptor descriptor, Object identifier, Object info, FrameSlotKind kind, int index) {
         this.descriptor = descriptor;
         this.identifier = identifier;
         this.info = info;
@@ -46,35 +61,83 @@ public final class FrameSlot implements Cloneable {
         this.kind = kind;
     }
 
+    /**
+     * Identifier of the slot.
+     *
+     * @return value as specified in {@link FrameDescriptor#addFrameSlot(java.lang.Object)}
+     *         parameter
+     * @since 0.8 or earlier
+     */
     public Object getIdentifier() {
         return identifier;
     }
 
+    /**
+     * Information about the slot.
+     *
+     * @return value as specified as second parameter of
+     *         {@link FrameDescriptor#addFrameSlot(java.lang.Object, java.lang.Object, com.oracle.truffle.api.frame.FrameSlotKind)}
+     * @since 0.8 or earlier
+     */
     public Object getInfo() {
         return info;
     }
 
+    /**
+     * Index of the slot in the {@link FrameDescriptor}.
+     *
+     * @return position of the slot computed after
+     *         {@link FrameDescriptor#addFrameSlot(java.lang.Object, java.lang.Object, com.oracle.truffle.api.frame.FrameSlotKind)
+     *         adding} it.
+     * @since 0.8 or earlier
+     */
     public int getIndex() {
         return index;
     }
 
+    /**
+     * Kind of the slot. Specified either at
+     * {@link FrameDescriptor#addFrameSlot(java.lang.Object, com.oracle.truffle.api.frame.FrameSlotKind)
+     * creation time} or updated via {@link #setKind(com.oracle.truffle.api.frame.FrameSlotKind)}
+     * method.
+     *
+     * @return current kind of this slot
+     * @since 0.8 or earlier
+     */
     public FrameSlotKind getKind() {
         return kind;
     }
 
+    /**
+     * Changes the kind of this slot. Change of the slot kind is done on <em>slow path</em> and
+     * invalidates assumptions about {@link FrameDescriptor#createVersion() version} of
+     * {@link #getFrameDescriptor() associated descriptor}.
+     *
+     * @param kind new kind of the slot
+     * @since 0.8 or earlier
+     */
     public void setKind(final FrameSlotKind kind) {
         if (this.kind != kind) {
-            CompilerDirectives.transferToInterpreter();
+            CompilerDirectives.transferToInterpreterAndInvalidate();
             this.kind = kind;
             this.descriptor.updateVersion();
         }
     }
 
+    /** @since 0.8 or earlier */
     @Override
     public String toString() {
+        CompilerAsserts.neverPartOfCompilation("do not call FrameSlot.toString from compiled code");
         return "[" + index + "," + identifier + "," + kind + "]";
     }
 
+    /**
+     * Frame descriptor this slot is associated with.
+     *
+     * @return instance of descriptor that {@link FrameDescriptor#addFrameSlot(java.lang.Object)
+     *         created} the slot
+     * @since 0.8 or earlier
+     */
     public FrameDescriptor getFrameDescriptor() {
         return this.descriptor;
     }
