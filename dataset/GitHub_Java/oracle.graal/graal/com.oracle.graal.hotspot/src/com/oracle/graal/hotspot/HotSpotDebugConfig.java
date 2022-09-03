35978ab9159060e1c1a83e0c8a4a690916438656
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:40:33 +0200

    added support for extending the filtering context of a DebugConfig

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/HotSpotDebugConfig.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/HotSpotDebugConfig.java
@@ -64,22 +64,13 @@ public class HotSpotDebugConfig implements DebugConfig {
         if (logFilter != null || meterFilter != null || timerFilter != null || dumpFilter != null || methodFilter != null) {
             TTY.println(Thread.currentThread().getName() + ": " + toString());
         }
-        if (GraalOptions.PrintBinaryGraphs) {
-            if (GraalOptions.PrintIdealGraphFile) {
-                dumpHandlers.add(new BinaryGraphPrinterDumpHandler());
-            } else {
-                dumpHandlers.add(new BinaryGraphPrinterDumpHandler(GraalOptions.PrintIdealGraphAddress, GraalOptions.PrintBinaryGraphPort));
-            }
+
+        if (GraalOptions.PrintIdealGraphFile) {
+            dumpHandlers.add(new IdealGraphPrinterDumpHandler());
         } else {
-            if (GraalOptions.PrintIdealGraphFile) {
-                dumpHandlers.add(new IdealGraphPrinterDumpHandler());
-            } else {
-                dumpHandlers.add(new IdealGraphPrinterDumpHandler(GraalOptions.PrintIdealGraphAddress, GraalOptions.PrintIdealGraphPort));
-            }
+            dumpHandlers.add(new IdealGraphPrinterDumpHandler(GraalOptions.PrintIdealGraphAddress, GraalOptions.PrintIdealGraphPort));
         }
-        if (GraalOptions.PrintCFG) {
-            dumpHandlers.add(new CFGPrinterObserver());
-        }
+        dumpHandlers.add(new CFGPrinterObserver());
         this.output = output;
     }
 
