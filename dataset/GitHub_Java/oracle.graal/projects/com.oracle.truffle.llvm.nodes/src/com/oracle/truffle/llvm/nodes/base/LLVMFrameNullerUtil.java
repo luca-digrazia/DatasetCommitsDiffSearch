78af88f76e5d0dad998396cc9eae688f5884a580
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:49:02 +0200

    Support for disabling the frame nulling for debugging purposes.

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/base/LLVMFrameNullerUtil.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/base/LLVMFrameNullerUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates.
+ * Copyright (c) 2016, 2018, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -32,11 +32,14 @@ package com.oracle.truffle.llvm.nodes.base;
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.api.frame.FrameSlotKind;
 import com.oracle.truffle.api.frame.VirtualFrame;
-import com.oracle.truffle.llvm.runtime.LLVMAddress;
-import com.oracle.truffle.llvm.runtime.LLVMFunctionHandle;
 import com.oracle.truffle.llvm.runtime.LLVMIVarBit;
 import com.oracle.truffle.llvm.runtime.floating.LLVM80BitFloat;
+import com.oracle.truffle.llvm.runtime.pointer.LLVMNativePointer;
+import com.oracle.truffle.llvm.runtime.pointer.LLVMPointer;
+import com.oracle.truffle.llvm.runtime.types.FunctionType;
+import com.oracle.truffle.llvm.runtime.types.PointerType;
 import com.oracle.truffle.llvm.runtime.types.PrimitiveType;
 import com.oracle.truffle.llvm.runtime.types.PrimitiveType.PrimitiveKind;
 import com.oracle.truffle.llvm.runtime.types.Type;
@@ -49,134 +52,118 @@ import com.oracle.truffle.llvm.runtime.vector.LLVMI1Vector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI32Vector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI64Vector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI8Vector;
+import com.oracle.truffle.llvm.runtime.vector.LLVMPointerVector;
+import com.oracle.truffle.llvm.runtime.vector.LLVMVector;
 
 public final class LLVMFrameNullerUtil {
+    private static final boolean USE_FRAME_NULLING = true;
+
     private LLVMFrameNullerUtil() {
     }
 
-    public static void nullI1(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setBoolean(frameSlot, false);
-    }
+    public static void nullFrameSlot(VirtualFrame frame, FrameSlot frameSlot, boolean forceNulling) {
+        if (!USE_FRAME_NULLING) {
+            return;
+        }
 
-    public static void nullI8(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setByte(frameSlot, (byte) 0);
-    }
-
-    public static void nullI16(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setInt(frameSlot, 0);
-    }
-
-    public static void nullI32(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setInt(frameSlot, 0);
-    }
-
-    public static void nullI64(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setLong(frameSlot, 0L);
-    }
-
-    public static void nullFloat(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setFloat(frameSlot, 0f);
-    }
-
-    public static void nullDouble(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setDouble(frameSlot, 0d);
-    }
-
-    public static void nullAddress(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setObject(frameSlot, LLVMAddress.nullPointer());
-    }
-
-    public static void nullIVarBit(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setObject(frameSlot, LLVMIVarBit.createNull());
-    }
-
-    public static void null80BitFloat(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setObject(frameSlot, new LLVM80BitFloat(false, 0, 0));
-    }
-
-    public static void nullFunction(VirtualFrame frame, FrameSlot frameSlot) {
-        frame.setObject(frameSlot, LLVMFunctionHandle.createHandle(0));
-    }
-
-    public static void nullFrameSlot(VirtualFrame frame, FrameSlot frameSlot) {
-        CompilerAsserts.partialEvaluationConstant(frameSlot.getKind());
-        switch (frameSlot.getKind()) {
-            case Boolean:
-                nullI1(frame, frameSlot);
-                break;
-            case Byte:
-                nullI8(frame, frameSlot);
-                break;
-            case Int:
-                nullI32(frame, frameSlot);
-                break;
-            case Long:
-                nullI64(frame, frameSlot);
-                break;
-            case Float:
-                nullFloat(frame, frameSlot);
-                break;
-            case Double:
-                nullDouble(frame, frameSlot);
-                break;
-            case Object:
-                CompilerAsserts.partialEvaluationConstant(frameSlot.getInfo());
-                if (frameSlot.getInfo() == null) {
-                    nullAddress(frame, frameSlot);
-                    break;
-                } else {
-                    Type type = (Type) frameSlot.getInfo();
-                    if (Type.isFunctionOrFunctionPointer(type)) {
-                        nullFunction(frame, frameSlot);
-                    } else if (type instanceof VectorType) {
-                        nullVector(frame, frameSlot, ((VectorType) type).getElementType().getPrimitiveKind());
-                    } else if (type instanceof VariableBitWidthType) {
-                        nullIVarBit(frame, frameSlot);
-                    } else if (type instanceof PrimitiveType && ((PrimitiveType) type).getPrimitiveKind() == PrimitiveKind.X86_FP80) {
-                        null80BitFloat(frame, frameSlot);
-                    } else {
-                        /*
-                         * This is a best effort approach. It could still be that LLVMAddress and
-                         * LLVMGlobalVariable clash.
-                         */
-                        nullAddress(frame, frameSlot);
-                    }
-                }
-                break;
-            case Illegal:
-            default:
+        FrameSlotKind kind = frame.getFrameDescriptor().getFrameSlotKind(frameSlot);
+        CompilerAsserts.partialEvaluationConstant(kind);
+        if (kind == FrameSlotKind.Object) {
+            // object frame slots always need to be nulled (otherwise we would impact GC)
+            nullObject(frame, frameSlot);
+        } else if (CompilerDirectives.inCompiledCode() || forceNulling) {
+            // Nulling primitive frame slots is only necessary in compiled code (otherwise, we would
+            // compute values that are only used in framestates). This code must NOT be moved to a
+            // separate method as it would cause endless deopts (the method or classes that are used
+            // within the method might be unresolved because they were never executed). For the same
+            // reason, we also must NOT use a switch statement.
+            if (kind == FrameSlotKind.Boolean) {
+                frame.setBoolean(frameSlot, false);
+            } else if (kind == FrameSlotKind.Byte) {
+                frame.setByte(frameSlot, (byte) 0);
+            } else if (kind == FrameSlotKind.Int) {
+                frame.setInt(frameSlot, 0);
+            } else if (kind == FrameSlotKind.Long) {
+                frame.setLong(frameSlot, 0L);
+            } else if (kind == FrameSlotKind.Float) {
+                frame.setFloat(frameSlot, 0f);
+            } else if (kind == FrameSlotKind.Double) {
+                frame.setDouble(frameSlot, 0d);
+            } else {
                 throw new UnsupportedOperationException("unexpected frameslot kind");
+            }
         }
     }
 
-    public static void nullVector(VirtualFrame frame, FrameSlot frameSlot, PrimitiveKind elementType) {
-        CompilerAsserts.partialEvaluationConstant(elementType);
-        switch (elementType) {
-            case DOUBLE:
-                frame.setObject(frameSlot, LLVMDoubleVector.create(null));
-                break;
-            case FLOAT:
-                frame.setObject(frameSlot, LLVMFloatVector.create(null));
-                break;
-            case I1:
-                frame.setObject(frameSlot, LLVMI1Vector.create(null));
-                break;
-            case I16:
-                frame.setObject(frameSlot, LLVMI16Vector.create(null));
-                break;
-            case I32:
-                frame.setObject(frameSlot, LLVMI32Vector.create(null));
-                break;
-            case I64:
-                frame.setObject(frameSlot, LLVMI64Vector.create(null));
-                break;
-            case I8:
-                frame.setObject(frameSlot, LLVMI8Vector.create(null));
-                break;
-            default:
-                CompilerDirectives.transferToInterpreter();
-                throw new AssertionError();
+    private static void nullAddress(VirtualFrame frame, FrameSlot frameSlot) {
+        frame.setObject(frameSlot, LLVMNativePointer.createNull());
+    }
 
+    private static void nullIVarBit(VirtualFrame frame, FrameSlot frameSlot) {
+        frame.setObject(frameSlot, LLVMIVarBit.createNull());
+    }
+
+    private static void null80BitFloat(VirtualFrame frame, FrameSlot frameSlot) {
+        frame.setObject(frameSlot, new LLVM80BitFloat(false, 0, 0));
+    }
+
+    private static void nullObject(VirtualFrame frame, FrameSlot frameSlot) {
+        CompilerAsserts.partialEvaluationConstant(frameSlot.getInfo());
+        CompilerAsserts.partialEvaluationConstant(frameSlot.getInfo() == null);
+        if (frameSlot.getInfo() != null) {
+            Type type = (Type) frameSlot.getInfo();
+            CompilerAsserts.partialEvaluationConstant(type instanceof VectorType);
+            CompilerAsserts.partialEvaluationConstant(type instanceof VariableBitWidthType);
+            CompilerAsserts.partialEvaluationConstant(type instanceof PrimitiveType && ((PrimitiveType) type).getPrimitiveKind() == PrimitiveKind.X86_FP80);
+            if (type instanceof VectorType) {
+                nullVector(frame, frameSlot, (VectorType) type);
+                return;
+            } else if (type instanceof VariableBitWidthType) {
+                nullIVarBit(frame, frameSlot);
+                return;
+            } else if (type instanceof PrimitiveType && ((PrimitiveType) type).getPrimitiveKind() == PrimitiveKind.X86_FP80) {
+                null80BitFloat(frame, frameSlot);
+                return;
+            }
+        }
+
+        // This is a best effort approach. It could still be that LLVMAddress clashes with some
+        // other class.
+        nullAddress(frame, frameSlot);
+    }
+
+    private static void nullVector(VirtualFrame frame, FrameSlot frameSlot, VectorType vectorType) {
+        frame.setObject(frameSlot, getNullVector(vectorType));
+    }
+
+    private static LLVMVector getNullVector(VectorType vectorType) {
+        CompilerAsserts.partialEvaluationConstant(vectorType);
+        Type elementType = vectorType.getElementType();
+        if (elementType instanceof PrimitiveType) {
+            switch (((PrimitiveType) elementType).getPrimitiveKind()) {
+                case I1:
+                    return LLVMI1Vector.create(new boolean[0]);
+                case I8:
+                    return LLVMI8Vector.create(new byte[0]);
+                case I16:
+                    return LLVMI16Vector.create(new short[0]);
+                case I32:
+                    return LLVMI32Vector.create(new int[0]);
+                case I64:
+                    return LLVMI64Vector.create(new long[0]);
+                case DOUBLE:
+                    return LLVMDoubleVector.create(new double[0]);
+                case FLOAT:
+                    return LLVMFloatVector.create(new float[0]);
+                default:
+                    CompilerDirectives.transferToInterpreter();
+                    throw new AssertionError();
+            }
+        } else if (elementType instanceof PointerType || elementType instanceof FunctionType) {
+            return LLVMPointerVector.create(new LLVMPointer[0]);
+        } else {
+            CompilerDirectives.transferToInterpreter();
+            throw new IllegalStateException("Unknown vector element type: " + elementType);
         }
     }
 }
