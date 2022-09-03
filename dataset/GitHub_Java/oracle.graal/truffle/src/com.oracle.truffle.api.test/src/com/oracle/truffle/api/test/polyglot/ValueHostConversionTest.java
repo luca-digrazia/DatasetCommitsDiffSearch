58c8fc8c47837bc6dee2797114565f5cef6a640f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:14:00 +0200

    Remove test workaround for already fixed findMetaObject

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/ValueHostConversionTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/ValueHostConversionTest.java
@@ -2,41 +2,25 @@
  * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * The Universal Permissive License (UPL), Version 1.0
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * (a) the Software, and
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package com.oracle.truffle.api.test.polyglot;
 
@@ -220,7 +204,7 @@ public class ValueHostConversionTest extends AbstractPolyglotTest {
         assertTrue(newInstance.getMetaObject().newInstance().asHostObject() instanceof JavaRecord);
         assertSame(JavaRecord.class, newInstance.getMetaObject().asHostObject());
 
-        assertValue(recordClass, Trait.INSTANTIABLE, Trait.MEMBERS, Trait.HOST_OBJECT);
+        assertValue(context, recordClass, Trait.INSTANTIABLE, Trait.MEMBERS, Trait.HOST_OBJECT);
     }
 
     @Test
@@ -245,7 +229,7 @@ public class ValueHostConversionTest extends AbstractPolyglotTest {
         assertTrue(newInstance.getMetaObject().newInstance().asHostObject() instanceof JavaRecord);
         assertSame(JavaRecord.class, newInstance.getMetaObject().asHostObject());
 
-        assertValue(recordClass, Trait.INSTANTIABLE, Trait.MEMBERS, Trait.HOST_OBJECT);
+        assertValue(context, recordClass, Trait.INSTANTIABLE, Trait.MEMBERS, Trait.HOST_OBJECT);
 
         Value bigIntegerStatic = getStaticClass(BigInteger.class);
         assertTrue(bigIntegerStatic.hasMember("ZERO"));
@@ -293,8 +277,8 @@ public class ValueHostConversionTest extends AbstractPolyglotTest {
         assertTrue(record.hasMember("wait"));
         assertTrue(record.hasMember("notifyAll"));
 
-        assertValue(record, Trait.MEMBERS, Trait.HOST_OBJECT);
-        assertValue(record.getMetaObject(), Trait.INSTANTIABLE, Trait.MEMBERS, Trait.HOST_OBJECT);
+        assertValue(context, record, Trait.MEMBERS, Trait.HOST_OBJECT);
+        assertValue(context, record.getMetaObject(), Trait.INSTANTIABLE, Trait.MEMBERS, Trait.HOST_OBJECT);
     }
 
     @Test
@@ -897,76 +881,6 @@ public class ValueHostConversionTest extends AbstractPolyglotTest {
 
         assertEquals("object", hierarchy.execute(42).asString());
         assertEquals("object", hierarchy.execute(false).asString());
-    }
-
-    @SuppressWarnings("unused")
-    public static class PrimitiveHierarchy {
-        public String hierarchy(byte arg) {
-            return "byte";
-        }
-
-        public String hierarchy(short arg) {
-            return "short";
-        }
-
-        public String hierarchy(char arg) {
-            return "char";
-        }
-
-        public String hierarchy(int arg) {
-            return "int";
-        }
-
-        public String hierarchy(long arg) {
-            return "long";
-        }
-
-        public String hierarchy(double arg) {
-            return "double";
-        }
-
-        public String hierarchy(float arg) {
-            return "float";
-        }
-
-        public String hierarchy(boolean arg) {
-            return "boolean";
-        }
-    }
-
-    @Test
-    public void testStringToPrimitive() {
-        Value hierarchy = context.asValue(new PrimitiveHierarchy()).getMember("hierarchy");
-
-        assertEquals("int", hierarchy.execute(String.valueOf(Integer.MIN_VALUE)).asString());
-        assertEquals("int", hierarchy.execute(String.valueOf(Integer.MAX_VALUE)).asString());
-        assertEquals("int", hierarchy.execute(String.valueOf((long) Integer.MIN_VALUE)).asString());
-        assertEquals("int", hierarchy.execute(String.valueOf((long) Integer.MAX_VALUE)).asString());
-        assertEquals("byte", hierarchy.execute(String.valueOf(Byte.MIN_VALUE)).asString());
-        assertEquals("byte", hierarchy.execute(String.valueOf(Byte.MAX_VALUE)).asString());
-        assertEquals("short", hierarchy.execute(String.valueOf(Short.MIN_VALUE)).asString());
-        assertEquals("short", hierarchy.execute(String.valueOf(Short.MAX_VALUE)).asString());
-        assertEquals("long", hierarchy.execute(String.valueOf(Integer.MIN_VALUE - 1L)).asString());
-        assertEquals("long", hierarchy.execute(String.valueOf(Integer.MAX_VALUE + 1L)).asString());
-        assertEquals("long", hierarchy.execute(String.valueOf(Long.MIN_VALUE)).asString());
-        assertEquals("long", hierarchy.execute(String.valueOf(Long.MAX_VALUE)).asString());
-
-        assertEquals("float", hierarchy.execute(String.valueOf((float) -(Math.pow(2, 24) - 1))).asString());
-        assertEquals("float", hierarchy.execute(String.valueOf((float) +(Math.pow(2, 24) - 1))).asString());
-        assertEquals("float", hierarchy.execute(String.valueOf(Float.MIN_VALUE)).asString());
-        assertEquals("float", hierarchy.execute(String.valueOf(Float.MAX_VALUE)).asString());
-
-        // lossy
-        assertEquals("float", hierarchy.execute(String.valueOf((float) Integer.MIN_VALUE)).asString());
-        assertEquals("float", hierarchy.execute(String.valueOf((float) Integer.MAX_VALUE)).asString());
-        assertEquals("float", hierarchy.execute(String.valueOf((double) Integer.MIN_VALUE)).asString());
-        assertEquals("float", hierarchy.execute(String.valueOf((double) Integer.MAX_VALUE)).asString());
-
-        assertEquals("double", hierarchy.execute(String.valueOf(Double.MIN_VALUE)).asString());
-        assertEquals("double", hierarchy.execute(String.valueOf(Double.MAX_VALUE)).asString());
-
-        assertEquals("boolean", hierarchy.execute(String.valueOf(false)).asString());
-        assertEquals("boolean", hierarchy.execute(String.valueOf(true)).asString());
     }
 
     @Test
