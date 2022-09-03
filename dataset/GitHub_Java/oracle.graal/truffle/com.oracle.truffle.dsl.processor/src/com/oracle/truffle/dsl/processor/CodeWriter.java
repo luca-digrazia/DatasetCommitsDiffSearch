849415f78e2cbee86463c833965be222c4840604
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:58:46 +0200

    Don't add header comment in generated sources

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/CodeWriter.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/CodeWriter.java
@@ -22,15 +22,16 @@
  */
 package com.oracle.truffle.dsl.processor;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
 
-import javax.annotation.processing.*;
-import javax.lang.model.element.*;
-import javax.tools.*;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+import javax.tools.JavaFileObject;
 
-import com.oracle.truffle.dsl.processor.java.compiler.*;
-import com.oracle.truffle.dsl.processor.java.model.*;
-import com.oracle.truffle.dsl.processor.java.transform.*;
+import com.oracle.truffle.dsl.processor.java.model.CodeTypeElement;
+import com.oracle.truffle.dsl.processor.java.transform.AbstractCodeWriter;
 
 public final class CodeWriter extends AbstractCodeWriter {
 
@@ -54,10 +55,6 @@ public final class CodeWriter extends AbstractCodeWriter {
             return;
         }
         writeLn("// CheckStyle: start generated");
-        String comment = CompilerFactory.getCompiler(originalElement).getHeaderComment(env, originalElement);
-        if (comment != null) {
-            writeLn(comment);
-        }
     }
 
 }
