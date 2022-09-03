9aafb5b150a55f3dc6f36fbb6ab0297b7c49cdc7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 16:24:23 +0200

    support disassembling a subrange of a bytecode method

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.java/src/com/oracle/graal/java/BytecodeDisassembler.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.java/src/com/oracle/graal/java/BytecodeDisassembler.java
@@ -22,16 +22,10 @@
  */
 package com.oracle.graal.java;
 
-import com.oracle.jvmci.meta.JavaField;
-import com.oracle.jvmci.meta.ResolvedJavaMethod;
-import com.oracle.jvmci.meta.BytecodeDisassemblerProvider;
-import com.oracle.jvmci.meta.JavaConstant;
-import com.oracle.jvmci.meta.JavaType;
-import com.oracle.jvmci.meta.JavaMethod;
-import com.oracle.jvmci.meta.ConstantPool;
-import static com.oracle.jvmci.bytecode.Bytecodes.*;
+import static com.oracle.graal.bytecode.Bytecodes.*;
 
-import com.oracle.jvmci.bytecode.*;
+import com.oracle.graal.api.meta.*;
+import com.oracle.graal.bytecode.*;
 
 /**
  * Utility for producing a {@code javap}-like disassembly of bytecode.
