c3e63cda6ece89a335b4db16fede6a88e98fb5d0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:01:57 +0200

    Improve TimeBoxing test speed.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.debug.test/src/com/oracle/truffle/api/debug/test/TimeBoxingTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.debug.test/src/com/oracle/truffle/api/debug/test/TimeBoxingTest.java
@@ -44,12 +44,11 @@ public class TimeBoxingTest {
     public void testTimeBoxing() throws Exception {
         final Context context = Context.create();
         Source source = Source.newBuilder(InstrumentationTestLanguage.ID, "ROOT(LOOP(infinity,STATEMENT))", "NotEnoughTime").buildLiteral();
-        Debugger debugger = context.getEngine().getInstruments().get("debugger").lookup(Debugger.class);
 
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
+                Debugger debugger = context.getEngine().getInstruments().get("debugger").lookup(Debugger.class);
                 debugger.startSession(new SuspendedCallback() {
                     public void onSuspend(SuspendedEvent event) {
                         event.prepareKill();
@@ -61,11 +60,9 @@ public class TimeBoxingTest {
         try {
             context.eval(source); // throws KillException, wrapped by PolyglotException
             Assert.fail();
-        } catch (PolyglotException error) {
-            Assert.assertTrue(error.isCancelled());
-            Assert.assertTrue(error.getMessage(), error.getMessage().contains("Execution cancelled by a debugging session."));
+        } catch (PolyglotException pex) {
+            Assert.assertEquals("com.oracle.truffle.api.debug.KillException", pex.getMessage());
         }
-        timer.cancel();
     }
 
 }
--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.sl.test/src/com/oracle/truffle/sl/test/SLDebugTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.sl.test/src/com/oracle/truffle/sl/test/SLDebugTest.java
@@ -320,7 +320,7 @@ public class SLDebugTest {
         }
     }
 
-    @Test
+    @Test(expected = PolyglotException.class)
     public void testTimeboxing() throws Throwable {
         final Source endlessLoop = slCode("function main() {\n" +
                         "  i = 1; \n" +
@@ -331,12 +331,12 @@ public class SLDebugTest {
                         "}\n");
 
         final Context context = Context.create("sl");
-        Debugger debugger = context.getEngine().getInstruments().get("debugger").lookup(Debugger.class);
+
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                debugger.startSession(new SuspendedCallback() {
+                context.getEngine().getInstruments().get("debugger").lookup(Debugger.class).startSession(new SuspendedCallback() {
                     public void onSuspend(SuspendedEvent event) {
                         event.prepareKill();
                     }
@@ -344,13 +344,7 @@ public class SLDebugTest {
             }
         }, 0, 10);
 
-        try {
-            context.eval(endlessLoop); // throws KillException, wrapped by PolyglotException
-            Assert.fail();
-        } catch (PolyglotException pex) {
-            Assert.assertTrue(pex.isCancelled());
-        }
-        timer.cancel();
+        context.eval(endlessLoop);
     }
 
     @Test
