fe6d231bc709074689dc29810fe8c9eb19458204
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 15:17:12 +0200

    fix generic types in FindContextNode

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/FindContextNode.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/FindContextNode.java
@@ -24,18 +24,27 @@
  */
 package com.oracle.truffle.api.impl;
 
+import com.oracle.truffle.api.Assumption;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.nodes.Node;
 
 public final class FindContextNode<C> extends Node {
     private final Class<? extends TruffleLanguage<C>> languageClass;
+    @CompilerDirectives.CompilationFinal private C context;
+    @CompilerDirectives.CompilationFinal private Assumption oneVM;
 
     public FindContextNode(Class<? extends TruffleLanguage<C>> type) {
         this.languageClass = type;
     }
 
     public C executeFindContext() {
-        return ExecutionImpl.findContext(languageClass);
+        if (context != null && oneVM.isValid()) {
+            return context;
+        }
+        CompilerDirectives.transferToInterpreterAndInvalidate();
+        oneVM = Accessor.oneVMAssumption();
+        return context = Accessor.findContext(languageClass);
     }
 
     public Class<? extends TruffleLanguage<C>> getLanguageClass() {
