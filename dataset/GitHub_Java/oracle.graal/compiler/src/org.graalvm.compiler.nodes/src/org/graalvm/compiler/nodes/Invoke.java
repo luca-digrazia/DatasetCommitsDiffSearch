b4690080237ddfe0e2127592badb008143f82dfb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:28:49 +0200

    Fix style issues.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/Invoke.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/Invoke.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -50,6 +50,8 @@ public interface Invoke extends StateSplit, Lowerable, MemoryCheckpoint.Single, 
     ValueNode classInit();
 
     void setClassInit(ValueNode node);
+
+    void intrinsify(Node node);
 
     boolean useForInlining();
 
