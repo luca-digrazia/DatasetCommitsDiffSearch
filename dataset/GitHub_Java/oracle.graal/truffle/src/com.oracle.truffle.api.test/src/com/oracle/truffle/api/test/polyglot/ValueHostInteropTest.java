4c613d6d762501c73731b5beb164e20a9633a7eb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:46:56 +0200

    Make JDK 10 happy by not using core matchers.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/ValueHostInteropTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/ValueHostInteropTest.java
@@ -2,41 +2,23 @@
  * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * The Universal Permissive License (UPL), Version 1.0
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
  *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * (a) the Software, and
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package com.oracle.truffle.api.test.polyglot;
 
@@ -70,17 +52,18 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.TypeLiteral;
 import org.graalvm.polyglot.Value;
 import org.graalvm.polyglot.proxy.ProxyObject;
 import org.hamcrest.CoreMatchers;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.interop.ForeignAccess;
 import com.oracle.truffle.api.interop.KeyInfo;
 import com.oracle.truffle.api.interop.MessageResolution;
@@ -90,7 +73,7 @@ import com.oracle.truffle.api.interop.UnknownIdentifierException;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.nodes.Node;
 
-public class ValueHostInteropTest extends AbstractPolyglotTest {
+public class ValueHostInteropTest {
 
     public static class Data {
         public int x;
@@ -111,9 +94,16 @@ public class ValueHostInteropTest extends AbstractPolyglotTest {
         }
     }
 
+    private Context context;
+
     @Before
     public void initObjects() {
-        setupEnv();
+        context = Context.create();
+    }
+
+    @After
+    public void cleanup() {
+        context.close();
     }
 
     @Test
@@ -125,8 +115,8 @@ public class ValueHostInteropTest extends AbstractPolyglotTest {
         Value calendarValue = testListValue.getArrayElement(0);
         assertTrue(calendarValue.isHostObject());
 
-        assertValue(testListValue);
-        assertValue(calendarValue);
+        assertValue(context, testListValue);
+        assertValue(context, calendarValue);
     }
 
     @Test
@@ -193,7 +183,7 @@ public class ValueHostInteropTest extends AbstractPolyglotTest {
         assertFalse(memberKeys.contains("one"));
         assertFalse(memberKeys.contains("null"));
         assertFalse(memberKeys.contains("three"));
-        assertValue(context.asValue(map));
+        assertValue(context, context.asValue(map));
     }
 
     @Test
@@ -444,8 +434,6 @@ public class ValueHostInteropTest extends AbstractPolyglotTest {
         assertEquals(50, f.call((Object) 13, (Object) 37));
         f.hashCode();
         f.equals(null);
-        assertTrue(f.equals(f));
-        assertEquals(f, context.asValue(executable).as(FunctionalWithDefaults.class));
         f.toString();
     }
 
@@ -456,8 +444,6 @@ public class ValueHostInteropTest extends AbstractPolyglotTest {
         assertEquals(50, f.call(13, 37));
         f.hashCode();
         f.equals(null);
-        assertTrue(f.equals(f));
-        assertEquals(f, context.asValue(executable).as(FunctionalWithObjectMethodOverrides.class));
         f.toString();
     }
 
@@ -972,8 +958,6 @@ public class ValueHostInteropTest extends AbstractPolyglotTest {
         @Resolve(message = "EXECUTE")
         @SuppressWarnings("unused")
         abstract static class Execute extends Node {
-
-            @TruffleBoundary
             protected Object access(FunctionObject obj, Object[] args) {
                 return Arrays.stream(args).mapToInt(o -> (int) o).sum();
             }
