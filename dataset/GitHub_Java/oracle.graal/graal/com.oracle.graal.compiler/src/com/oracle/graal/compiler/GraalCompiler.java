9698b225e9d627d5bb4f14732f076547cc04917e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:04:10 +0200

    Experiment with new block order for LSRA.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/GraalCompiler.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/GraalCompiler.java
@@ -149,9 +149,6 @@ public class GraalCompiler {
         if (GraalOptions.PartialEscapeAnalysis && !plan.isPhaseDisabled(PartialEscapeAnalysisPhase.class)) {
             new PartialEscapeAnalysisPhase(target, runtime, assumptions, true).apply(graph);
         }
-
-        new LockEliminationPhase().apply(graph);
-
         if (GraalOptions.OptLoopTransform) {
             new LoopTransformHighPhase().apply(graph);
             new LoopTransformLowPhase().apply(graph);
