4a88afac1e7fe5f9641a897f338aa2159fd03127
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 01:55:14 +0200

    Add testcase for index function.

--- dataset/GitHub_Java/guolindev.LitePal/sample/src/androidTest/java/com/litepaltest/model/Cellphone.java
+++ dataset/GitHub_Java/guolindev.LitePal/sample/src/androidTest/java/com/litepaltest/model/Cellphone.java
@@ -1,16 +1,32 @@
 package com.litepaltest.model;
 
-import org.litepal.crud.DataSupport;
+import org.litepal.annotation.Column;
+import org.litepal.crud.LitePalSupport;
 
-public class Cellphone extends DataSupport {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cellphone extends LitePalSupport {
 
 	private Long id;
 
-	private String brand;
+	@Column(index = true)
+	public String brand;
 
 	private Character inStock;
 
-	private Double price;
+	protected Double price;
+
+    @Column(unique = true, nullable = false)
+    String serial;
+
+    @Column(nullable = true, defaultValue = "0.0.0.0")
+    private String mac;
+
+    @Column(ignore = true)
+    private String uuid;
+
+    private List<WeiboMessage> messages = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -44,4 +60,35 @@ public class Cellphone extends DataSupport {
 		this.price = price;
 	}
 
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public List<WeiboMessage> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<WeiboMessage> messages) {
+        this.messages = messages;
+    }
 }
