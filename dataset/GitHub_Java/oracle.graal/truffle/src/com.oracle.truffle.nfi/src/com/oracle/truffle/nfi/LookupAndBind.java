7f6191c5895b0a4f2e0b43cb7981d509958c3ae1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:49:08 +0200

    Simplify access to a native library through a Java interface

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/LookupAndBind.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/LookupAndBind.java
@@ -30,6 +30,7 @@ import com.oracle.truffle.api.interop.InteropException;
 import com.oracle.truffle.api.interop.Message;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.interop.UnknownIdentifierException;
+import com.oracle.truffle.api.interop.java.JavaInterop;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 import java.util.HashMap;
@@ -66,7 +67,7 @@ final class LookupAndBind extends RootNode {
                 throw UnknownIdentifierException.raise(nameAndSignature);
             }
         }
-        return library.register(libraryWrapper);
+        return JavaInterop.asTruffleObject(libraryWrapper);
     }
 
 }
