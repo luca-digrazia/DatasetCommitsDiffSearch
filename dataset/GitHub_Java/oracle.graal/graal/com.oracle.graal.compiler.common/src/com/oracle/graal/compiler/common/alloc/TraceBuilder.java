eaaf7aca389f3026439f032d433aeaec80b8687b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:15:07 +0200

    TraceRA: work around warning with newer ecj versions.
        Fixing the warning would cause an error with older ecj versions.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.common/src/com/oracle/graal/compiler/common/alloc/TraceBuilder.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.common/src/com/oracle/graal/compiler/common/alloc/TraceBuilder.java
@@ -55,28 +55,13 @@ public final class TraceBuilder<T extends AbstractBlockBase<T>> {
         }
 
         public boolean incomingEdges(int traceNr) {
-            List<T> trace = getTraces().get(traceNr);
-            return incomingEdges(traceNr, trace);
+            /* TODO (je): not efficient. find better solution. */
+            return getTraces().get(traceNr).stream().flatMap(b -> b.getPredecessors().stream()).anyMatch(s -> getTraceForBlock(s) != traceNr);
         }
 
         public boolean incomingSideEdges(int traceNr) {
-            List<T> trace = getTraces().get(traceNr);
-            if (trace.size() <= 1) {
-                return false;
-            }
-            return incomingEdges(traceNr, trace.subList(1, trace.size()));
-        }
-
-        private boolean incomingEdges(int traceNr, List<T> trace) {
             /* TODO (je): not efficient. find better solution. */
-            for (T block : trace) {
-                for (T pred : block.getPredecessors()) {
-                    if (getTraceForBlock(pred) != traceNr) {
-                        return true;
-                    }
-                }
-            }
-            return false;
+            return getTraces().get(traceNr).stream().skip(1).flatMap(b -> b.getPredecessors().stream()).anyMatch(s -> getTraceForBlock(s) != traceNr);
         }
 
     }
