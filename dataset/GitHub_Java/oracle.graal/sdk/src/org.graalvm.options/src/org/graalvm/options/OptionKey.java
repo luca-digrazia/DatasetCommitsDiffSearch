7e1d6c86abac9b9ffd2e67d340962eef795b6e88
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:26:48 +0200

    Javadoc:  first steps in Context, more to come.

--- dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.options/src/org/graalvm/options/OptionKey.java
+++ dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.options/src/org/graalvm/options/OptionKey.java
@@ -1,42 +1,26 @@
 /*
- * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * The Universal Permissive License (UPL), Version 1.0
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * (a) the Software, and
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package org.graalvm.options;
 
@@ -45,7 +29,7 @@ import java.util.Objects;
 /**
  * Represents the option key for an option specification.
  *
- * @since 19.0
+ * @since 1.0
  */
 public final class OptionKey<T> {
 
@@ -57,7 +41,7 @@ public final class OptionKey<T> {
      * no default {@link OptionType} could be {@link OptionType#defaultType(Object) resolved} for
      * the given type. The default value must not be <code>null</code>.
      *
-     * @since 19.0
+     * @since 1.0
      */
     public OptionKey(T defaultValue) {
         Objects.requireNonNull(defaultValue);
@@ -69,11 +53,13 @@ public final class OptionKey<T> {
     }
 
     /**
-     * Constructs a new option key given a default value and option key.
+     * Constructs a new option key given a default value and option key. The default value and the
+     * type must not be <code>null</code>.
      *
-     * @since 19.0
+     * @since 1.0
      */
     public OptionKey(T defaultValue, OptionType<T> type) {
+        Objects.requireNonNull(defaultValue);
         Objects.requireNonNull(type);
         this.defaultValue = defaultValue;
         this.type = type;
@@ -82,7 +68,7 @@ public final class OptionKey<T> {
     /**
      * Returns the option type of this key.
      *
-     * @since 19.0
+     * @since 1.0
      */
     public OptionType<T> getType() {
         return type;
@@ -91,7 +77,7 @@ public final class OptionKey<T> {
     /**
      * Returns the default value for this option.
      *
-     * @since 19.0
+     * @since 1.0
      */
     public T getDefaultValue() {
         return defaultValue;
@@ -100,7 +86,7 @@ public final class OptionKey<T> {
     /**
      * Returns the value of this key given the {@link OptionValues values}.
      *
-     * @since 19.0
+     * @since 1.0
      */
     public T getValue(OptionValues values) {
         return values.get(this);
@@ -110,7 +96,7 @@ public final class OptionKey<T> {
      * Returns <code>true</code> if a value for this key has been set for the given option values or
      * <code>false</code> if no value has been set.
      *
-     * @since 19.0
+     * @since 1.0
      */
     public boolean hasBeenSet(OptionValues values) {
         return values.hasBeenSet(this);
