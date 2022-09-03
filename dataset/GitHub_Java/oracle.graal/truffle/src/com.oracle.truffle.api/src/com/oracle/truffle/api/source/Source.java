ff0f735dd2f680f882da09896cd3b3b31db9a9b0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:54:40 +0200

    Fix missing since tag.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/source/Source.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/source/Source.java
@@ -1,42 +1,26 @@
 /*
- * Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * The Universal Permissive License (UPL), Version 1.0
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * (a) the Software, and
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package com.oracle.truffle.api.source;
 
@@ -721,7 +705,7 @@ public abstract class Source {
      * @since 1.0
      */
     public static SourceBuilder newBuilder(String language, TruffleFile file) {
-        return EMPTY.new LiteralBuilder(language, file);
+        return newBuilder(language, SourceAccessor.asFile(file));
     }
 
     /*
@@ -858,7 +842,7 @@ public abstract class Source {
      * @since 1.0
      */
     public static String findMimeType(TruffleFile file) throws IOException {
-        return findMimeType(SourceAccessor.getPath(file), null);
+        return findMimeType(SourceAccessor.asFile(file).toPath(), null);
     }
 
     /**
@@ -918,21 +902,7 @@ public abstract class Source {
         String useMimeType = mimeType;
         String usePath = null;
         URL useUrl = null;
-        if (origin instanceof TruffleFile) {
-            TruffleFile file = (TruffleFile) origin;
-            TruffleFile absoluteFile = file.exists() ? file.getCanonicalFile() : file;
-            useName = useName == null ? file.getName() : useName;
-            usePath = usePath == null ? absoluteFile.getPath() : usePath;
-            useUri = useUri == null ? absoluteFile.toUri() : useUri;
-            useMimeType = useMimeType == null ? findMimeType(SourceAccessor.getPath(absoluteFile), getValidMimeTypes(language)) : useMimeType;
-            if (useContent == null) {
-                if (isCharacterBased(language, useMimeType)) {
-                    useContent = new String(file.readAllBytes(), StandardCharsets.UTF_8);
-                } else {
-                    useContent = ByteSequence.create(file.readAllBytes());
-                }
-            }
-        } else if (origin instanceof File) {
+        if (origin instanceof File) {
             final File file = (File) origin;
             File absoluteFile = file.exists() ? file.getCanonicalFile() : file;
             useName = useName == null ? file.getName() : useName;
@@ -998,7 +968,7 @@ public abstract class Source {
     }
 
     static byte[] readBytes(File file) throws IOException {
-        return Files.readAllBytes(file.toPath());
+        return SourceAccessor.isTruffleFile(file) ? SourceAccessor.readTruffleFile(file) : Files.readAllBytes(file.toPath());
     }
 
     static byte[] readBytes(URLConnection connection) throws IOException {
