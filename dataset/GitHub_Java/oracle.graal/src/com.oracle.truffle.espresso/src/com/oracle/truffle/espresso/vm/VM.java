d011cb4f886acee1ed6afbfac4e3d23f9873a658
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:18:42 +0200

    Fix GetEnv + silence stderr logging.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/vm/VM.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/vm/VM.java
@@ -611,7 +611,7 @@ public class VM extends NativeEnv {
             Field f = lib.getClass().getDeclaredField("handle");
             f.setAccessible(true);
             long handle = (long) f.get(lib);
-            if (!handle2Lib.containsKey((Long) handle)) {
+            if (!handle2Lib.contains(handle)) {
                 handle2Lib.put(handle, lib);
             }
             return handle;
@@ -635,7 +635,9 @@ public class VM extends NativeEnv {
         try {
             TruffleObject function = NativeLibrary.lookup(handle2Lib.get(libHandle), name);
             long handle = (long) ForeignAccess.sendUnbox(Message.UNBOX.createNode(), function);
-            handle2Sym.put(handle, function);
+            if (!handle2Sym.contains(handle)) {
+                handle2Sym.put(handle, function);
+            }
             return handle;
         } catch (UnsupportedMessageException e) {
             throw EspressoError.shouldNotReachHere(e);
