36b037aa0d1236ef72b1a4ab3a64efc4c5e2ce7b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:53:08 +0200

    Remove unnecessary method

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.interop/src/com/oracle/truffle/api/interop/ForeignAccess.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.interop/src/com/oracle/truffle/api/interop/ForeignAccess.java
@@ -267,37 +267,6 @@ public final class ForeignAccess {
     }
 
     /**
-     * Sends a {@link Message#REMOVE REMOVE message} to the foreign receiver object by executing the
-     * <code> removeNode </code>.
-     *
-     * @param removeNode the node created by {@link Message#createNode()}
-     * @param receiver foreign object to receive the message passed to {@link Message#createNode()}
-     *            method
-     * @param identifier name of the property to be removed
-     * @return <code>true</code> if the property was successfully removed, <code>false</code>
-     *         otherwise
-     * @throws ClassCastException if the createNode has not been created by
-     *             {@link Message#createNode()} method.
-     * @throws UnsupportedMessageException if the <code>receiver</code> does not support the
-     *             {@link Message#createNode() message represented} by <code>readNode</code>
-     * @throws UnknownIdentifierException if the <code>receiver</code> does not allow removing a
-     *             property for the given <code>identifier</code>
-     * @since 0.32
-     */
-    public static boolean sendRemove(Node removeNode, TruffleObject receiver, Object identifier)
-                    throws UnknownIdentifierException, UnsupportedMessageException {
-        try {
-            return (boolean) ((InteropAccessNode) removeNode).execute(receiver, identifier);
-        } catch (UnsupportedMessageException | UnknownIdentifierException e) {
-            CompilerDirectives.transferToInterpreter();
-            throw e;
-        } catch (InteropException e) {
-            CompilerDirectives.transferToInterpreter();
-            throw new AssertionError("Unexpected exception caught.", e);
-        }
-    }
-
-    /**
      * Sends an {@link Message#UNBOX UNBOX message} to the foreign receiver object by executing the
      * <code> unboxNode </code>.
      *
@@ -961,17 +930,6 @@ public final class ForeignAccess {
         }
 
         /**
-         * Handles {@link Message#REMOVE} message.
-         *
-         * @return call target to handle the message or <code>null</code> if this message is not
-         *         supported
-         * @since 0.32
-         */
-        default CallTarget accessRemove() {
-            return null;
-        }
-
-        /**
          * Handles {@link Message#createExecute(int)} messages.
          *
          * @param argumentsLength number of parameters the messages has been created for
@@ -1293,12 +1251,12 @@ public final class ForeignAccess {
         private static CallTarget accessMessage(StandardFactory factory, Message msg) {
             if (msg instanceof KnownMessage) {
                 switch (msg.hashCode()) {
-                    case Execute.HASH:
-                        return factory.accessExecute(0);
-                    case Invoke.HASH:
-                        return factory.accessInvoke(0);
-                    case New.HASH:
-                        return factory.accessNew(0);
+                    case Execute.EXECUTE:
+                        return factory.accessExecute(((Execute) msg).getArity());
+                    case Execute.INVOKE:
+                        return factory.accessInvoke(((Execute) msg).getArity());
+                    case Execute.NEW:
+                        return factory.accessNew(((Execute) msg).getArity());
                     case GetSize.HASH:
                         return factory.accessGetSize();
                     case HasKeys.HASH:
@@ -1319,8 +1277,6 @@ public final class ForeignAccess {
                         return factory.accessUnbox();
                     case Write.HASH:
                         return factory.accessWrite();
-                    case Remove.HASH:
-                        return factory.accessRemove();
                     case Keys.HASH:
                         return factory.accessKeys();
                     case KeyInfoMsg.HASH:
@@ -1362,12 +1318,12 @@ public final class ForeignAccess {
         private static CallTarget accessMessage(Factory26 factory, Message msg) {
             if (msg instanceof KnownMessage) {
                 switch (msg.hashCode()) {
-                    case Execute.HASH:
-                        return factory.accessExecute(0);
-                    case Invoke.HASH:
-                        return factory.accessInvoke(0);
-                    case New.HASH:
-                        return factory.accessNew(0);
+                    case Execute.EXECUTE:
+                        return factory.accessExecute(((Execute) msg).getArity());
+                    case Execute.INVOKE:
+                        return factory.accessInvoke(((Execute) msg).getArity());
+                    case Execute.NEW:
+                        return factory.accessNew(((Execute) msg).getArity());
                     case GetSize.HASH:
                         return factory.accessGetSize();
                     case HasSize.HASH:
