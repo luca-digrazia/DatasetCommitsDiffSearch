c2e26be43f7ec8f2989497138ca61dd0184c9b0a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 23:09:39 +0200

    Fix new line at end of file.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.api/src/com/oracle/truffle/api/CompilerDirectives.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.api/src/com/oracle/truffle/api/CompilerDirectives.java
@@ -22,7 +22,6 @@
  */
 package com.oracle.truffle.api;
 
-import java.lang.annotation.*;
 import java.util.concurrent.*;
 
 /**
@@ -85,14 +84,5 @@ public class CompilerDirectives {
      * @param reason the reason for the bailout
      */
     public static void bailout(String reason) {
-    }
-
-    /**
-     * Marks fields that should be considered final for a Truffle compilation although they are not
-     * final while executing in the interpreter.
-     */
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target({ElementType.FIELD})
-    public @interface CompilationFinal {
     }
 }
