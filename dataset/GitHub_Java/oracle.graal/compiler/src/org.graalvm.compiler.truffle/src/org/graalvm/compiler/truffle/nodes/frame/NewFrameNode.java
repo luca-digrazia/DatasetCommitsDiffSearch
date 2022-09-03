4646a978c7ee8068b9b59a46b41714afc2619078
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:20:15 +0200

    Invalid guarantee removed

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle/src/org/graalvm/compiler/truffle/nodes/frame/NewFrameNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle/src/org/graalvm/compiler/truffle/nodes/frame/NewFrameNode.java
@@ -31,6 +31,7 @@ import java.util.List;
 
 import org.graalvm.compiler.core.common.type.StampFactory;
 import org.graalvm.compiler.core.common.type.TypeReference;
+import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.graph.IterableNodeType;
 import org.graalvm.compiler.graph.Node;
 import org.graalvm.compiler.graph.NodeClass;
