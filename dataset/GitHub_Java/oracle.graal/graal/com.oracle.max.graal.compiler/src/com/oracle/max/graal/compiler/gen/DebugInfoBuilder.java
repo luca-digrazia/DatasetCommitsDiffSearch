e0cd87c47c45975d305a104b95f05759c6115a29
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 01:05:48 +0200

    Fixed a wrong frame state for the exception edge after a polymorphic inlining. Removed a NPE allocation in a runtime call branch introduced for Graal.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/gen/DebugInfoBuilder.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/gen/DebugInfoBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,60 +25,31 @@ package com.oracle.max.graal.compiler.gen;
 import java.util.*;
 import java.util.Map.Entry;
 
-import com.oracle.max.graal.compiler.*;
-import com.oracle.max.graal.compiler.lir.*;
-import com.oracle.max.graal.compiler.lir.FrameMap.*;
+import com.oracle.max.cri.ci.*;
+import com.oracle.max.graal.compiler.gen.LIRGenerator.LockScope;
 import com.oracle.max.graal.graph.*;
+import com.oracle.max.graal.lir.*;
 import com.oracle.max.graal.nodes.*;
 import com.oracle.max.graal.nodes.virtual.*;
-import com.sun.cri.ci.*;
 
 public class DebugInfoBuilder {
-    public final GraalCompilation compilation;
+    private final NodeMap<CiValue> nodeOperands;
 
-    private final NodeMap<StackBlock> lockDataMap;
-
-    public DebugInfoBuilder(GraalCompilation compilation) {
-        this.compilation = compilation;
-        if (needLockData()) {
-            lockDataMap = new NodeMap<StackBlock>(compilation.graph);
-        } else {
-            lockDataMap = null;
-        }
-    }
-
-    public boolean needLockData() {
-        return compilation.compiler.runtime.sizeOfLockData() > 0;
-    }
-
-    public StackBlock lockDataFor(MonitorObject object) {
-        if (!needLockData()) {
-            return null;
-        }
-
-        StackBlock result = lockDataMap.get(object);
-        if (result == null) {
-            result = compilation.frameMap().reserveStackBlock(compilation.compiler.runtime.sizeOfLockData(), false);
-            lockDataMap.set(object, result);
-        }
-        return result;
+    public DebugInfoBuilder(NodeMap<CiValue> nodeOperands) {
+        this.nodeOperands = nodeOperands;
     }
 
 
-    private HashMap<VirtualObjectNode, CiVirtualObject> virtualObjects = new HashMap<VirtualObjectNode, CiVirtualObject>();
+    private HashMap<VirtualObjectNode, CiVirtualObject> virtualObjects = new HashMap<>();
 
-    public LIRDebugInfo build(FrameState topState, List<CiStackSlot> pointerSlots, LabelRef exceptionEdge) {
-        if (compilation.placeholderState != null) {
-            return null;
-        }
-
+    public LIRDebugInfo build(FrameState topState, LockScope locks, List<CiStackSlot> pointerSlots, LabelRef exceptionEdge) {
         assert virtualObjects.size() == 0;
-        CiFrame frame = computeFrameForState(topState);
+        CiFrame frame = computeFrameForState(topState, locks);
 
         CiVirtualObject[] virtualObjectsArray = null;
         if (virtualObjects.size() != 0) {
             // collect all VirtualObjectField instances:
-            IdentityHashMap<VirtualObjectNode, VirtualObjectFieldNode> objectStates = new IdentityHashMap<VirtualObjectNode, VirtualObjectFieldNode>();
+            IdentityHashMap<VirtualObjectNode, VirtualObjectFieldNode> objectStates = new IdentityHashMap<>();
             FrameState current = topState;
             do {
                 for (Node n : current.virtualObjectMappings()) {
@@ -95,7 +66,7 @@ public class DebugInfoBuilder {
             boolean changed;
             do {
                 changed = false;
-                IdentityHashMap<VirtualObjectNode, CiVirtualObject> virtualObjectsCopy = new IdentityHashMap<VirtualObjectNode, CiVirtualObject>(virtualObjects);
+                IdentityHashMap<VirtualObjectNode, CiVirtualObject> virtualObjectsCopy = new IdentityHashMap<>(virtualObjects);
                 for (Entry<VirtualObjectNode, CiVirtualObject> entry : virtualObjectsCopy.entrySet()) {
                     if (entry.getValue().values() == null) {
                         VirtualObjectNode vobj = entry.getKey();
@@ -134,28 +105,38 @@ public class DebugInfoBuilder {
         return new LIRDebugInfo(frame, virtualObjectsArray, pointerSlots, exceptionEdge);
     }
 
-    private CiFrame computeFrameForState(FrameState state) {
-        CiValue[] values = new CiValue[state.valuesSize() + state.locksSize()];
+    private CiFrame computeFrameForState(FrameState state, LockScope locks) {
+        int numLocks = (locks != null && locks.callerState == state.outerFrameState()) ? locks.stateDepth + 1 : 0;
+
+        CiValue[] values = new CiValue[state.valuesSize() + numLocks];
         int valueIndex = 0;
 
         for (int i = 0; i < state.valuesSize(); i++) {
             values[valueIndex++] = toCiValue(state.valueAt(i));
         }
 
-        for (int i = 0; i < state.locksSize(); i++) {
-            MonitorObject monitorObject = state.lockAt(i);
-            CiValue owner = toCiValue(monitorObject.owner());
-            CiValue lockData = lockDataFor(monitorObject);
-            boolean eliminated = owner instanceof CiVirtualObject;
+        LockScope nextLock = locks;
+        for (int i = numLocks - 1; i >= 0; i--) {
+            assert locks != null && nextLock.callerState == state.outerFrameState() && nextLock.stateDepth == i;
 
-            values[valueIndex++] = new CiMonitorValue(owner, lockData, eliminated);
+            CiValue owner = toCiValue(nextLock.monitor.object());
+            CiValue lockData = nextLock.lockData;
+            boolean eliminated = nextLock.monitor.eliminated();
+            values[state.valuesSize() + nextLock.stateDepth] = new CiMonitorValue(owner, lockData, eliminated);
+
+            nextLock = nextLock.outer;
         }
 
         CiFrame caller = null;
         if (state.outerFrameState() != null) {
-            caller = computeFrameForState(state.outerFrameState());
+            caller = computeFrameForState(state.outerFrameState(), nextLock);
+        } else {
+            if (nextLock != null) {
+                throw new CiBailout("unbalanced monitors: found monitor for unknown frame");
+            }
         }
-        CiFrame frame = new CiFrame(caller, state.method(), state.bci, state.rethrowException(), values, state.localsSize(), state.stackSize(), state.locksSize());
+        assert state.bci >= 0 || state.bci == FrameState.BEFORE_BCI;
+        CiFrame frame = new CiFrame(caller, state.method(), state.bci, state.rethrowException(), state.duringCall(), values, state.localsSize(), state.stackSize(), numLocks);
         return frame;
     }
 
@@ -173,8 +154,8 @@ public class DebugInfoBuilder {
             return ((ConstantNode) value).value;
 
         } else if (value != null) {
-            CiValue operand = compilation.operand(value);
-            assert operand != null && operand instanceof CiVariable || operand instanceof CiConstant;
+            CiValue operand = nodeOperands.get(value);
+            assert operand != null && (operand instanceof Variable || operand instanceof CiConstant);
             return operand;
 
         } else {
