82e16b0fbe6ab33b4ec6dc13a1e1461efd131bf9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 01:26:46 +0200

    Disabled assert about null state at block entry.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/gen/LIRGenerator.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/gen/LIRGenerator.java
@@ -1505,9 +1505,6 @@ public abstract class LIRGenerator extends ValueVisitor {
     public void visitLoopEnd(LoopEnd x) {
         setNoResult(x);
         moveToPhi(x.loopBegin(), x);
-        if (GraalOptions.GenSafepoints) {
-            xir.genSafepoint(site(x));
-        }
         lir.jump(getLIRBlock(x.loopBegin()));
     }
 
