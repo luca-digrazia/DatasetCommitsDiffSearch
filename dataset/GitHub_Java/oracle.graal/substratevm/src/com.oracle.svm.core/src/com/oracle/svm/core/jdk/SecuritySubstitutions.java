71e1b1d7cd3a7967df2739598f6de78d2cb1f7fe
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:11:35 +0200

    [feature] support the JarVerifier in native-image

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/SecuritySubstitutions.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/SecuritySubstitutions.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -77,11 +75,6 @@ final class Target_java_security_AccessController {
 
     @Substitute
     private static <T> T doPrivileged(PrivilegedAction<T> action, AccessControlContext context) {
-        return action.run();
-    }
-
-    @Substitute
-    private static <T> T doPrivileged(PrivilegedAction<T> action, AccessControlContext context, Permission... perms) {
         return action.run();
     }
 
