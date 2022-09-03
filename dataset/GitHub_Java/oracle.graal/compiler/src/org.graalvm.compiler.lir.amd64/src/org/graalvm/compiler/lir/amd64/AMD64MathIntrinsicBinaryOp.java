5ffe2c80b3daa7e4ba2c11ce1b8219e71b83f7ed
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:30:06 +0200

    Update javadoc.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64MathIntrinsicBinaryOp.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64MathIntrinsicBinaryOp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,7 +26,6 @@ package org.graalvm.compiler.lir.amd64;
 
 import static jdk.vm.ci.amd64.AMD64.xmm0;
 import static jdk.vm.ci.amd64.AMD64.xmm1;
-import static org.graalvm.compiler.lir.amd64.AMD64HotSpotHelper.registersToValues;
 
 import org.graalvm.compiler.core.common.LIRKind;
 import org.graalvm.compiler.lir.LIRInstructionClass;
@@ -39,12 +38,12 @@ import jdk.vm.ci.code.RegisterValue;
 import jdk.vm.ci.meta.Value;
 
 /**
- * AMD64MathIntrinsicBinaryOp assumes that the input values are stored in the xmm0 and xmm1
+ * AMD64MathIntrinsicUnaryOp assumes that the input values are stored in the xmm0 and xmm1
  * registers, and it will emit the output value into the xmm0 register.
- * {@link #emitLIRWrapper(LIRGenerator, Value, Value)} is provided for emitting necessary mov LIRs
+ * {{@link #emitLIRWrapper(LIRGenerator, Value, Value)}} is provided for emitting necessary mov LIRs
  * before and after this LIR instruction.
  */
-public abstract class AMD64MathIntrinsicBinaryOp extends AMD64LIRInstruction {
+public abstract class AMD64MathIntrinsicBinaryOp extends AMD64MathIntrinsicOp {
 
     @Def protected Value output;
     @Use protected Value input0;
@@ -61,12 +60,12 @@ public abstract class AMD64MathIntrinsicBinaryOp extends AMD64LIRInstruction {
         temps = registersToValues(registers);
     }
 
-    public final Variable emitLIRWrapper(LIRGenerator gen, Value x, Value y) {
-        LIRKind kind = LIRKind.combine(x, y);
+    public final Variable emitLIRWrapper(LIRGenerator gen, Value input0, Value input1) {
+        LIRKind kind = LIRKind.combine(input0, input1);
         RegisterValue xmm0Value = xmm0.asValue(kind);
-        gen.emitMove(xmm0Value, x);
+        gen.emitMove(xmm0Value, input0);
         RegisterValue xmm1Value = xmm1.asValue(kind);
-        gen.emitMove(xmm1Value, y);
+        gen.emitMove(xmm1Value, input1);
         gen.append(this);
         Variable result = gen.newVariable(kind);
         gen.emitMove(result, xmm0Value);
--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64MathIntrinsicUnaryOp.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64MathIntrinsicUnaryOp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,25 +25,24 @@
 package org.graalvm.compiler.lir.amd64;
 
 import static jdk.vm.ci.amd64.AMD64.xmm0;
-import static org.graalvm.compiler.lir.amd64.AMD64HotSpotHelper.registersToValues;
 
+import jdk.vm.ci.code.RegisterValue;
 import org.graalvm.compiler.core.common.LIRKind;
 import org.graalvm.compiler.lir.LIRInstructionClass;
-import org.graalvm.compiler.lir.Variable;
-import org.graalvm.compiler.lir.gen.LIRGeneratorTool;
 
 import jdk.vm.ci.amd64.AMD64Kind;
 import jdk.vm.ci.code.Register;
-import jdk.vm.ci.code.RegisterValue;
 import jdk.vm.ci.meta.Value;
+import org.graalvm.compiler.lir.Variable;
+import org.graalvm.compiler.lir.gen.LIRGenerator;
 
 /**
  * AMD64MathIntrinsicUnaryOp assumes that the input value is stored at the xmm0 register, and will
  * emit the output value into the xmm0 register as well.
- * {@link #emitLIRWrapper(LIRGeneratorTool, Value)} is provided for emitting necessary mov LIRs
- * before and after this LIR instruction.
+ * {{@link #emitLIRWrapper(LIRGenerator, Value)}} is provided for emitting necessary mov LIRs before
+ * and after this LIR instruction.
  */
-public abstract class AMD64MathIntrinsicUnaryOp extends AMD64LIRInstruction {
+public abstract class AMD64MathIntrinsicUnaryOp extends AMD64MathIntrinsicOp {
 
     @Def protected Value output;
     @Use protected Value input;
@@ -58,10 +57,10 @@ public abstract class AMD64MathIntrinsicUnaryOp extends AMD64LIRInstruction {
         temps = registersToValues(registers);
     }
 
-    public final Variable emitLIRWrapper(LIRGeneratorTool gen, Value value) {
-        LIRKind kind = LIRKind.combine(value);
+    public final Variable emitLIRWrapper(LIRGenerator gen, Value input) {
+        LIRKind kind = LIRKind.combine(input);
         RegisterValue xmm0Value = xmm0.asValue(kind);
-        gen.emitMove(xmm0Value, value);
+        gen.emitMove(xmm0Value, input);
         gen.append(this);
         Variable result = gen.newVariable(kind);
         gen.emitMove(result, xmm0Value);
