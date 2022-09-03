5233fdab5a9bb1b5bbff522b1bb9202b941a3cea
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:10:49 +0200

    TraceRA: make TraceLinearScanAllocationPhase a TraceAllocationPhase

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/TraceTrivialAllocator.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/TraceTrivialAllocator.java
@@ -52,11 +52,11 @@ import jdk.vm.ci.meta.Value;
 final class TraceTrivialAllocator extends TraceAllocationPhase<TraceAllocationPhase.TraceAllocationContext> {
 
     @Override
-    protected <B extends AbstractBlockBase<B>> void run(TargetDescription target, LIRGenerationResult lirGenRes, List<B> codeEmittingOrder, Trace trace, TraceAllocationContext context) {
+    protected <B extends AbstractBlockBase<B>> void run(TargetDescription target, LIRGenerationResult lirGenRes, List<B> codeEmittingOrder, Trace<B> trace, TraceAllocationContext context) {
         LIR lir = lirGenRes.getLIR();
-        TraceBuilderResult resultTraces = context.resultTraces;
+        TraceBuilderResult<?> resultTraces = context.resultTraces;
         assert isTrivialTrace(lir, trace) : "Not a trivial trace! " + trace;
-        AbstractBlockBase<?> block = trace.getBlocks().get(0);
+        B block = trace.getBlocks().iterator().next();
 
         AbstractBlockBase<?> pred = TraceUtil.getBestTraceInterPredecessor(resultTraces, block);
 
