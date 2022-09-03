ecc55c24f2e283de5ee3fe9bcf4a8490452048af
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:29:53 +0200

    Add test case for inline tracing.
        (cherry picked from commit e696e4aa0129f9d67a0352c19a1c144af629708c)

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/inlining/InliningTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/inlining/InliningTest.java
@@ -204,15 +204,15 @@ public class InliningTest extends GraalCompilerTest {
         assertFewMethodInfopoints(assertNotInlined(getGraph("invokeOverriddenInterfaceMethodSnippet", true)));
     }
 
-    public static void traceInliningTest() {
+    public void traceInliningTest() {
         callTrivial();
     }
 
-    private static void callTrivial() {
+    private void callTrivial() {
         callNonTrivial();
     }
 
-    private static double callNonTrivial() {
+    private double callNonTrivial() {
         double x = 0.0;
         for (int i = 0; i < 10; i++) {
             x += i * 1.21;
@@ -231,10 +231,8 @@ public class InliningTest extends GraalCompilerTest {
         String inliningTree = graph.getInliningLog().formatAsTree(false);
         String expectedRegex =
                         "compilation of org.graalvm.compiler.core.test.inlining.InliningTest.traceInliningTest.*: \\R" +
-                        "  at .*org.graalvm.compiler.core.test.inlining.InliningTest.traceInliningTest.*: <GraphBuilderPhase> org.graalvm.compiler.core.test.inlining.InliningTest.callTrivial.*: yes, inline method\\R" +
-                        "    at .*org.graalvm.compiler.core.test.inlining.InliningTest.callTrivial.*: .*\\R" +
-                        "       ├──<GraphBuilderPhase> org.graalvm.compiler.core.test.inlining.InliningTest.callNonTrivial.*: .*(.*\\R)*" +
-                        "       └──<InliningPhase> org.graalvm.compiler.core.test.inlining.InliningTest.callNonTrivial.*: .*(.*\\R)*";
+                        "  at org.graalvm.compiler.core.test.inlining.InliningTest.traceInliningTest.*: <GraphBuilderPhase> org.graalvm.compiler.core.test.inlining.InliningTest.callTrivial.*: inline method\\R" +
+                        "    at org.graalvm.compiler.core.test.inlining.InliningTest.callTrivial.*: <InliningPhase> org.graalvm.compiler.core.test.inlining.InliningTest.callNonTrivial.*: .*\\R";
         Pattern expectedPattern = Pattern.compile(expectedRegex, Pattern.MULTILINE);
         Assert.assertTrue("Got: " + inliningTree, expectedPattern.matcher(inliningTree).matches());
     }
