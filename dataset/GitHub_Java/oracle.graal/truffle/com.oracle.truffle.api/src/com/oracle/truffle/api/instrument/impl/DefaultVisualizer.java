5583c5b3dc747683f7ed20ec7ca68c84c781e29d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:32:35 +0200

    Truffle/Instrumentation:  make the language object visualizer more robust when given null.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/instrument/impl/DefaultVisualizer.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/instrument/impl/DefaultVisualizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,7 +32,6 @@ import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.source.SourceSection;
 
-@SuppressWarnings("deprecation")
 public class DefaultVisualizer implements Visualizer {
 
     private final ASTPrinter astPrinter;
