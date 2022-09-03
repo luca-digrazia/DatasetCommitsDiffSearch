93f444ea6b593877149062c5f7d812cdf7f83ec2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:21:15 +0200

    TruffleLanguage.isVisible allows a language to control whether a result of an evaluation should be visible or not. interactive attribute is used to control whether a language should be visible in a REPL or not.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/vm/InteractiveEvalTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/vm/InteractiveEvalTest.java
@@ -110,21 +110,21 @@ public class InteractiveEvalTest {
     }
 
     @Test
-    public void isInteractive1() {
+    public void isInteractive1() throws UnsupportedEncodingException {
         PolyglotEngine engine = PolyglotEngine.newBuilder().build();
         PolyglotEngine.Language language = engine.getLanguages().get("application/x-test-specinteract");
         assertFalse("SpecialInteractive language isn't interactive", language.isInteractive());
     }
 
     @Test
-    public void isInteractive2() {
+    public void isInteractive2() throws UnsupportedEncodingException {
         PolyglotEngine engine = PolyglotEngine.newBuilder().build();
         PolyglotEngine.Language language = engine.getLanguages().get("application/x-test-definteract");
         assertTrue("DefaultInteractive language is interactive", language.isInteractive());
     }
 
     @Test
-    public void isInteractive3() {
+    public void isInteractive3() throws UnsupportedEncodingException {
         PolyglotEngine engine = PolyglotEngine.newBuilder().build();
         PolyglotEngine.Language language = engine.getLanguages().get("application/x-test-async");
         assertTrue("By default a language is interactive", language.isInteractive());
