38aab4e4dd118c6bda86f0fbde953a7f68a51a52
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:34:05 +0200

    Allow setting callsite information later.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/nodes/MacroNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/nodes/MacroNode.java
@@ -89,20 +89,17 @@ public abstract class MacroNode extends FixedWithNextNode implements Lowerable, 
     protected int bci;
     protected ResolvedJavaMethod targetMethod;
     protected InvokeKind invokeKind;
-    protected StampPair returnStamp;
+    protected final StampPair returnStamp;
 
     protected MacroNode(NodeClass<? extends MacroNode> c, InvokeKind invokeKind, ResolvedJavaMethod targetMethod, int bci, StampPair returnStamp, ValueNode... arguments) {
-        super(c, returnStamp != null ? returnStamp.getTrustedStamp() : null);
-        assertArgumentCount(targetMethod, arguments);
+        super(c, returnStamp.getTrustedStamp());
+        assert targetMethod.getSignature().getParameterCount(!targetMethod.isStatic()) == arguments.length;
         this.arguments = new NodeInputList<>(this, arguments);
         this.bci = bci;
         this.targetMethod = targetMethod;
         this.returnStamp = returnStamp;
         this.invokeKind = invokeKind;
-    }
-
-    protected void assertArgumentCount(ResolvedJavaMethod targetMethod, ValueNode... arguments) {
-        assert targetMethod.getSignature().getParameterCount(!targetMethod.isStatic()) == arguments.length;
+        assert !isPlaceholderBci(bci);
     }
 
     public ValueNode getArgument(int i) {
