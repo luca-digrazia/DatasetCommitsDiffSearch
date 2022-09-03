cf149beafb5325026eb9d04897710ec059651102
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 09:03:28 +0200

    Add test for late intrinsic inlining.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements.test/src/org/graalvm/compiler/replacements/test/ReplacementsParseTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements.test/src/org/graalvm/compiler/replacements/test/ReplacementsParseTest.java
@@ -434,10 +434,6 @@ public class ReplacementsParseTest extends ReplacementsTest {
         return res;
     }
 
-    public static int callCopyFirstWrapper(byte[] in, byte[] out, boolean left2right) {
-        return callCopyFirst(in, out, left2right);
-    }
-
     public static int callCopyFirstL2R(byte[] in, byte[] out) {
         int res = TestObject.copyFirstL2R(in, out);
         if (res == 17) {
@@ -481,8 +477,8 @@ public class ReplacementsParseTest extends ReplacementsTest {
         try {
             byte[] in = {0, 1, 2, 3, 4};
             byte[] out = new byte[in.length];
-            test(options, "callCopyFirstWrapper", in, out, true);
-            test(options, "callCopyFirstWrapper", in, out, false);
+            test(options, "callCopyFirst", in, out, true);
+            test(options, "callCopyFirst", in, out, false);
         } finally {
             inlineInvokeDecision = null;
         }
