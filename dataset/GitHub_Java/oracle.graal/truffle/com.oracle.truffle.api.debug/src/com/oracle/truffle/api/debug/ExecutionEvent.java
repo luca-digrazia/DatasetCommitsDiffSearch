55e353b965445e9c30f98f5c20a89a9840bb5c92
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:46:07 +0200

    Properly synchronize debugger access, start and finish.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/ExecutionEvent.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/ExecutionEvent.java
@@ -72,8 +72,13 @@ public final class ExecutionEvent {
         if (debugger[0] instanceof Debugger) {
             return (Debugger) debugger[0];
         }
-        Debugger dbg = Debugger.find(engine, true);
-        dbg.executionStarted(currentDepth, source);
+        Debugger dbg;
+        try {
+            dbg = Debugger.find(engine, true);
+            dbg.executionStarted(currentDepth, source);
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
         debugger[0] = dbg;
         return dbg;
     }
