5c9382f302420b2aa85eb25bad99647ace98e1bd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:22:34 +0200

    Fix some issues when running "mx su-tests llvm-as" with enabled debugging flags

--- dataset/GitHub_Java/oracle.graal/projects/uk.ac.man.cs.llvm/src/uk/ac/man/cs/llvm/ir/module/MetadataV32.java
+++ dataset/GitHub_Java/oracle.graal/projects/uk.ac.man.cs.llvm/src/uk/ac/man/cs/llvm/ir/module/MetadataV32.java
@@ -31,7 +31,6 @@ package uk.ac.man.cs.llvm.ir.module;
 
 import java.util.List;
 
-import com.oracle.truffle.llvm.runtime.LLVMLogger;
 import com.oracle.truffle.llvm.runtime.options.LLVMBaseOptionFacade;
 import uk.ac.man.cs.llvm.ir.module.records.MetadataRecord;
 import uk.ac.man.cs.llvm.ir.SymbolGenerator;
@@ -193,13 +192,13 @@ public class MetadataV32 extends Metadata {
 
                 default:
                     metadata.add(null);
-                    LLVMLogger.info("! - TODO: #" + record + " - " + ident);
+                    System.out.println("! - TODO: #" + record + " - " + ident);
                     break;
             }
         } else {
             parsedArgs.rewind();
             metadata.add(null);
-            LLVMLogger.info("! - TODO: #" + MetadataRecord.OLD_NODE + ": " + parsedArgs);
+            System.out.println("! - TODO: #" + MetadataRecord.OLD_NODE + ": " + parsedArgs);
         }
     }
 
