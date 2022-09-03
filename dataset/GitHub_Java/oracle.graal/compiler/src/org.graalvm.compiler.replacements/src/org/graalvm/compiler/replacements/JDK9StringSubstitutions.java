1d71e9a2de2bb46b4064ef73e65cc8b40b97ca5c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:21:09 +0200

    Format code.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/JDK9StringSubstitutions.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/JDK9StringSubstitutions.java
@@ -55,12 +55,8 @@ public class JDK9StringSubstitutions {
         if (thisString.length() != thatString.length()) {
             return false;
         }
-
         if (thisString.length() == 0) {
             return true;
-        }
-        if (getCoder(thisString) != getCoder(thatString)) {
-            return false;
         }
 
         final byte[] array1 = getValue(thisString);
@@ -73,6 +69,4 @@ public class JDK9StringSubstitutions {
      * Will be intrinsified with an {@link InvocationPlugin} to a {@link LoadFieldNode}.
      */
     public static native byte[] getValue(String s);
-
-    public static native int getCoder(String s);
 }
