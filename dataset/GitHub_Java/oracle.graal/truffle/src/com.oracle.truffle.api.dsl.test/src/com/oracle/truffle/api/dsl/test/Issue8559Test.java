e3d86ae08a74ef0cbc007c0a8e304ae013d77521
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:11:02 +0200

    Fix style error.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.dsl.test/src/com/oracle/truffle/api/dsl/test/Issue8559Test.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.dsl.test/src/com/oracle/truffle/api/dsl/test/Issue8559Test.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -30,7 +28,7 @@ import org.junit.Test;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeChildren;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.dsl.test.Issue8559TestFactory.Issue8559LookupAndCallBinaryNodeGen;
+import com.oracle.truffle.api.dsl.test.Issue8559TestFactory.LookupAndCallBinaryNodeGen;
 import com.oracle.truffle.api.dsl.test.Issue8559TestFactory.SomeNodeGen;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
@@ -41,7 +39,7 @@ public class Issue8559Test {
 
     @Test
     public void testCall() throws UnexpectedResultException {
-        Issue8559LookupAndCallBinaryNode bin = Issue8559LookupAndCallBinaryNodeGen.create(new LiteralNode(1), new LiteralNode(2));
+        LookupAndCallBinaryNode bin = LookupAndCallBinaryNodeGen.create(new LiteralNode(1), new LiteralNode(2));
         Object result = null;
 
         // does not raise UnexpectedResultException
@@ -133,7 +131,7 @@ public class Issue8559Test {
     }
 
     @NodeChildren({@NodeChild("arg"), @NodeChild("arg2")})
-    public abstract static class Issue8559LookupAndCallBinaryNode extends BaseNode {
+    public abstract static class LookupAndCallBinaryNode extends BaseNode {
 
         public abstract int executeInt(int arg, int arg2) throws UnexpectedResultException;
 
