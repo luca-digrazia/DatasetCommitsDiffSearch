81995da662e6682e54c14d4b2d125bbcf44dcb84
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:55:01 +0200

    Updated compiler options.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedDirectCallNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedDirectCallNode.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -64,7 +62,6 @@ public final class OptimizedDirectCallNode extends DirectCallNode {
     @Override
     public Object call(Object[] arguments) {
         if (CompilerDirectives.inInterpreter()) {
-            // TODO: See why enabling this for low-tier leads to terrible performance in high-tier compilations.
             onInterpreterCall();
         }
         try {
--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/TruffleSplittingStrategy.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/TruffleSplittingStrategy.java
@@ -56,7 +56,7 @@ final class TruffleSplittingStrategy {
             }
         }
         if (TruffleCompilerOptions.getValue(TruffleExperimentalSplitting)) {
-            if (polymorphicSpecializationBasedShouldSplit(call, engineData)) {
+            if (pollutionBasedShouldSplit(call, engineData)) {
                 engineData.splitCount += call.getCallTarget().getUninitializedNodeCount();
                 doSplit(call);
             }
@@ -82,7 +82,7 @@ final class TruffleSplittingStrategy {
         }
     }
 
-    private static boolean polymorphicSpecializationBasedShouldSplit(OptimizedDirectCallNode call, GraalTVMCI.EngineData engineData) {
+    private static boolean pollutionBasedShouldSplit(OptimizedDirectCallNode call, GraalTVMCI.EngineData engineData) {
         OptimizedCallTarget callTarget = call.getCurrentCallTarget();
         if (!callTarget.isNeedsSplit()) {
             return false;
