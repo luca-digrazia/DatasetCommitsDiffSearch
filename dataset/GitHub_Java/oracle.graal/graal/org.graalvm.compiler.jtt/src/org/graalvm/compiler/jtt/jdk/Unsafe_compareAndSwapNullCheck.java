3968e7d61698660a5c8a3202e6d3135b4cae4b1f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:18:24 +0200

    Refactor tests to avoid redundant unsafe initialization.

--- dataset/GitHub_Java/oracle.graal/graal/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/jdk/Unsafe_compareAndSwapNullCheck.java
+++ dataset/GitHub_Java/oracle.graal/graal/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/jdk/Unsafe_compareAndSwapNullCheck.java
@@ -22,10 +22,7 @@
  */
 package org.graalvm.compiler.jtt.jdk;
 
-import static org.graalvm.compiler.options.OptionValues.GLOBAL;
-
 import org.graalvm.compiler.jtt.JTTTest;
-import org.junit.Assume;
 import org.junit.Test;
 
 public class Unsafe_compareAndSwapNullCheck extends JTTTest {
@@ -50,8 +47,6 @@ public class Unsafe_compareAndSwapNullCheck extends JTTTest {
 
     @Test
     public void run0() throws Throwable {
-        // GR-2921: Unsafe_compareAndSwapNullCheck test crashes on jdk9
-        Assume.assumeTrue(Java8OrEarlier);
-        runTest(GLOBAL, EMPTY, false, true, "test", null, 1L, 2L);
+        runTest(EMPTY, false, true, "test", null, 1L, 2L);
     }
 }
