c900fed6d9b24f6ba74676c568f6412b31d38538
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 13:25:10 +0200

    add memcpy assert for zero length

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.types/src/com/oracle/truffle/llvm/types/memory/LLVMHeap.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.types/src/com/oracle/truffle/llvm/types/memory/LLVMHeap.java
@@ -30,6 +30,7 @@
 package com.oracle.truffle.llvm.types.memory;
 
 import com.oracle.truffle.llvm.types.LLVMAddress;
+import com.oracle.truffle.llvm.types.LLVMFunction;
 
 public class LLVMHeap extends LLVMMemory {
 
@@ -101,13 +102,14 @@ public class LLVMHeap extends LLVMMemory {
     // current hack: we cannot directly store the LLVMFunction in the native memory due to GC
     public static final int FUNCTION_PTR_SIZE_BYTE = 8;
 
-    public static void putFunctionIndex(LLVMAddress address, long functionIndex) {
-        UNSAFE.putLong(LLVMMemory.extractAddr(address), functionIndex);
+    public static void putFunction(LLVMAddress address, LLVMFunction function) {
+        LLVMAddress functionIndex = function.getFunctionAddress();
+        UNSAFE.putLong(LLVMMemory.extractAddr(address), functionIndex.getVal());
     }
 
-    public static long getFunctionIndex(LLVMAddress addr) {
+    public static LLVMFunction getFunction(LLVMAddress addr) {
         long functionIndex = UNSAFE.getLong(LLVMMemory.extractAddr(addr));
-        return functionIndex;
+        return LLVMFunction.createFromAddress(LLVMAddress.fromLong(functionIndex));
     }
 
 }
