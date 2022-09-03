040b6c59a8fc6b660dced0395587c3e9e00a8582
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:07:24 +0200

    Bugfix: only array types have a component type

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/java/LoadIndexedNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/java/LoadIndexedNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,12 +22,14 @@
  */
 package com.oracle.graal.nodes.java;
 
-import static com.oracle.graal.nodeinfo.NodeCycles.CYCLES_8;
-import static com.oracle.graal.nodeinfo.NodeSize.SIZE_8;
+import jdk.vm.ci.meta.ConstantReflectionProvider;
+import jdk.vm.ci.meta.JavaConstant;
+import jdk.vm.ci.meta.JavaKind;
+import jdk.vm.ci.meta.MetaAccessProvider;
+import jdk.vm.ci.meta.ResolvedJavaType;
 
 import com.oracle.graal.compiler.common.type.Stamp;
 import com.oracle.graal.compiler.common.type.StampFactory;
-import com.oracle.graal.compiler.common.type.TypeReference;
 import com.oracle.graal.graph.Node;
 import com.oracle.graal.graph.NodeClass;
 import com.oracle.graal.graph.spi.Canonicalizable;
@@ -41,17 +43,10 @@ import com.oracle.graal.nodes.type.StampTool;
 import com.oracle.graal.nodes.virtual.VirtualArrayNode;
 import com.oracle.graal.nodes.virtual.VirtualObjectNode;
 
-import jdk.vm.ci.meta.Assumptions;
-import jdk.vm.ci.meta.ConstantReflectionProvider;
-import jdk.vm.ci.meta.JavaConstant;
-import jdk.vm.ci.meta.JavaKind;
-import jdk.vm.ci.meta.MetaAccessProvider;
-import jdk.vm.ci.meta.ResolvedJavaType;
-
 /**
  * The {@code LoadIndexedNode} represents a read from an element of an array.
  */
-@NodeInfo(cycles = CYCLES_8, size = SIZE_8)
+@NodeInfo
 public class LoadIndexedNode extends AccessIndexedNode implements Virtualizable, Canonicalizable {
 
     public static final NodeClass<LoadIndexedNode> TYPE = NodeClass.create(LoadIndexedNode.class);
@@ -63,26 +58,26 @@ public class LoadIndexedNode extends AccessIndexedNode implements Virtualizable,
      * @param index the instruction producing the index
      * @param elementKind the element type
      */
-    public LoadIndexedNode(Assumptions assumptions, ValueNode array, ValueNode index, JavaKind elementKind) {
-        this(TYPE, createStamp(assumptions, array, elementKind), array, index, elementKind);
+    public LoadIndexedNode(ValueNode array, ValueNode index, JavaKind elementKind) {
+        this(TYPE, createStamp(array, elementKind), array, index, elementKind);
     }
 
-    public static ValueNode create(Assumptions assumptions, ValueNode array, ValueNode index, JavaKind elementKind, MetaAccessProvider metaAccess, ConstantReflectionProvider constantReflection) {
+    public static ValueNode create(ValueNode array, ValueNode index, JavaKind elementKind, MetaAccessProvider metaAccess, ConstantReflectionProvider constantReflection) {
         ValueNode constant = tryConstantFold(array, index, metaAccess, constantReflection);
         if (constant != null) {
             return constant;
         }
-        return new LoadIndexedNode(assumptions, array, index, elementKind);
+        return new LoadIndexedNode(array, index, elementKind);
     }
 
     protected LoadIndexedNode(NodeClass<? extends LoadIndexedNode> c, Stamp stamp, ValueNode array, ValueNode index, JavaKind elementKind) {
         super(c, stamp, array, index, elementKind);
     }
 
-    private static Stamp createStamp(Assumptions assumptions, ValueNode array, JavaKind kind) {
+    private static Stamp createStamp(ValueNode array, JavaKind kind) {
         ResolvedJavaType type = StampTool.typeOrNull(array);
         if (kind == JavaKind.Object && type != null && type.isArray()) {
-            return StampFactory.object(TypeReference.createTrusted(assumptions, type.getComponentType()));
+            return StampFactory.declaredTrusted(type.getComponentType());
         } else {
             return StampFactory.forKind(kind);
         }
@@ -90,7 +85,7 @@ public class LoadIndexedNode extends AccessIndexedNode implements Virtualizable,
 
     @Override
     public boolean inferStamp() {
-        return updateStamp(createStamp(graph().getAssumptions(), array(), elementKind()));
+        return updateStamp(createStamp(array(), elementKind()));
     }
 
     @Override
@@ -106,7 +101,6 @@ public class LoadIndexedNode extends AccessIndexedNode implements Virtualizable,
         }
     }
 
-    @Override
     public Node canonical(CanonicalizerTool tool) {
         ValueNode constant = tryConstantFold(array(), index(), tool.getMetaAccess(), tool.getConstantReflection());
         if (constant != null) {
@@ -119,13 +113,9 @@ public class LoadIndexedNode extends AccessIndexedNode implements Virtualizable,
         if (array.isConstant() && !array.isNullConstant() && index.isConstant()) {
             JavaConstant arrayConstant = array.asJavaConstant();
             if (arrayConstant != null) {
-                int stableDimension = ((ConstantNode) array).getStableDimension();
-                if (stableDimension > 0) {
-                    JavaConstant constant = constantReflection.readArrayElement(arrayConstant, index.asJavaConstant().asInt());
-                    boolean isDefaultStable = ((ConstantNode) array).isDefaultStable();
-                    if (constant != null && (isDefaultStable || !constant.isDefaultForKind())) {
-                        return ConstantNode.forConstant(constant, stableDimension - 1, isDefaultStable, metaAccess);
-                    }
+                JavaConstant constant = constantReflection.readConstantArrayElement(arrayConstant, index.asJavaConstant().asInt());
+                if (constant != null) {
+                    return ConstantNode.forConstant(constant, metaAccess);
                 }
             }
         }
