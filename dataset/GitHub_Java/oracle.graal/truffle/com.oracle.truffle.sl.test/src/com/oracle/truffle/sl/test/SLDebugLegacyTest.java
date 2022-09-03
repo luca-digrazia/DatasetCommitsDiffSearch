f8ca319596bf50a7105c8c25440e1a74c6550984
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:30:47 +0200

    Debug test invoked via JavaInterop

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.sl.test/src/com/oracle/truffle/sl/test/SLDebugLegacyTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.sl.test/src/com/oracle/truffle/sl/test/SLDebugLegacyTest.java
@@ -65,16 +65,17 @@ import com.oracle.truffle.api.frame.MaterializedFrame;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.ForeignAccess;
 import com.oracle.truffle.api.interop.ForeignAccess.Factory;
-import com.oracle.truffle.api.interop.ForeignAccess.Factory10;
+import com.oracle.truffle.api.interop.ForeignAccess.Factory18;
 import com.oracle.truffle.api.interop.Message;
 import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.java.JavaInterop;
 import com.oracle.truffle.api.nodes.RootNode;
-import com.oracle.truffle.api.source.LineLocation;
 import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.api.source.SourceSection;
 import com.oracle.truffle.api.vm.PolyglotEngine;
 import com.oracle.truffle.api.vm.PolyglotEngine.Value;
 import com.oracle.truffle.sl.SLLanguage;
+import java.util.concurrent.Callable;
 
 @SuppressWarnings("deprecation")
 public class SLDebugLegacyTest {
@@ -191,7 +192,7 @@ public class SLDebugLegacyTest {
                 try {
                     assertNull(suspendedEvent);
                     assertNotNull(executionEvent);
-                    LineLocation nMinusOne = factorial.createLineLocation(8);
+                    com.oracle.truffle.api.source.LineLocation nMinusOne = factorial.createLineLocation(8);
                     debugger.setLineBreakpoint(0, nMinusOne, false);
                     executionEvent.prepareContinue();
                 } catch (IOException e) {
@@ -259,6 +260,15 @@ public class SLDebugLegacyTest {
 
     @Test
     public void stepInStepOver() throws Throwable {
+        doStepInStepOver(true);
+    }
+
+    @Test
+    public void stepInStepOverWithJavaInterop() throws Throwable {
+        doStepInStepOver(false);
+    }
+
+    private void doStepInStepOver(boolean direct) throws Throwable {
         final Source factorial = createFactorial();
         engine.eval(factorial);
 
@@ -308,12 +318,20 @@ public class SLDebugLegacyTest {
         assertLocation("test", 3, true, "println(res)", "res", "2");
         stepOut();
 
-        Value value = engine.findGlobalSymbol("test").execute();
+        Value value = engine.findGlobalSymbol("test");
+        Number result;
+        if (direct) {
+            value = value.execute();
+            result = value.as(Number.class);
+        } else {
+            final TruffleObject fn = (TruffleObject) value.get();
+            Callable<?> test = JavaInterop.asJavaFunction(Callable.class, fn);
+            result = (Number) test.call();
+        }
         assertExecutedOK();
 
-        Number n = value.as(Number.class);
-        assertNotNull(n);
-        assertEquals("Factorial computed OK", 2, n.intValue());
+        assertNotNull(result);
+        assertEquals("Factorial computed OK", 2, result.intValue());
     }
 
     @Test
@@ -378,13 +396,12 @@ public class SLDebugLegacyTest {
     }
 
     private static Source createNull() {
-        return Source.fromText("function nullTest() {\n" +
+        return Source.newBuilder("function nullTest() {\n" +
                         "  res = doNull();\n" +
                         "  return res;\n" +
                         "}\n" +
                         "function doNull() {\n" +
-                        "}\n",
-                        "nullTest.sl").withMimeType(SLLanguage.MIME_TYPE);
+                        "}\n").name("nullTest.sl").mimeType(SLLanguage.MIME_TYPE).build();
     }
 
     @Test
@@ -545,7 +562,7 @@ public class SLDebugLegacyTest {
 
     }
 
-    private static class ExecNotifyHandlerForeign implements Factory10, Factory {
+    private static class ExecNotifyHandlerForeign implements Factory18, Factory {
 
         private final ExecNotifyHandler nh;
 
@@ -616,6 +633,11 @@ public class SLDebugLegacyTest {
         @Override
         public boolean canHandle(TruffleObject to) {
             return (to instanceof ExecNotifyHandler);
+        }
+
+        @Override
+        public CallTarget accessKeys() {
+            return null;
         }
 
     }
