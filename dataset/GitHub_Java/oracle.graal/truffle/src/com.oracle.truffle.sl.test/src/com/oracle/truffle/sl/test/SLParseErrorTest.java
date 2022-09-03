2ef317984d9902b38c285c9afaeee67bac89fe4c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 07:33:10 +0200

    [fix] unused import int SLParseErrorTest

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.sl.test/src/com/oracle/truffle/sl/test/SLParseErrorTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.sl.test/src/com/oracle/truffle/sl/test/SLParseErrorTest.java
@@ -41,7 +41,6 @@
 package com.oracle.truffle.sl.test;
 
 import java.io.IOException;
-
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Source;
