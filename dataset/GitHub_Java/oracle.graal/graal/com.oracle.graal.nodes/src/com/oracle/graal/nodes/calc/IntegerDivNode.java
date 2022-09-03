9299d4fd11ca42077370163ee9731ec4b758b394
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:03:45 +0200

    Canonicalize ((a - a % b) / b) into (a / b).

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/IntegerDivNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/IntegerDivNode.java
@@ -109,11 +109,6 @@ public class IntegerDivNode extends FixedBinaryNode implements Canonicalizable, 
 
     @Override
     public void generate(LIRGeneratorTool gen) {
-        gen.setResult(this, gen.emitDiv(gen.operand(x()), gen.operand(y()), this));
-    }
-
-    @Override
-    public boolean canDeoptimize() {
-        return y().integerStamp().contains(0);
+        gen.setResult(this, gen.emitDiv(gen.operand(x()), gen.operand(y())));
     }
 }
