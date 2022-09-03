53a6e74345217af2c3b1ffcc67ded5315b23964d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 15:42:33 +0200

    BytecodeParser#lookupField: use eagerResolving instead of unresolvedIsError to guard explicit initialization.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.java/src/com/oracle/graal/java/BytecodeParser.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.java/src/com/oracle/graal/java/BytecodeParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,6 +27,7 @@ import static com.oracle.graal.compiler.common.GraalOptions.*;
 import static com.oracle.graal.compiler.common.type.StampFactory.*;
 import static com.oracle.graal.graphbuilderconf.IntrinsicContext.CompilationContext.*;
 import static com.oracle.graal.java.BytecodeParser.Options.*;
+import static com.oracle.graal.nodes.StructuredGraph.*;
 import static com.oracle.graal.nodes.type.StampTool.*;
 import static com.oracle.jvmci.code.TypeCheckHints.*;
 import static com.oracle.jvmci.common.JVMCIError.*;
@@ -61,7 +62,6 @@ import com.oracle.graal.nodes.util.*;
 import com.oracle.graal.phases.*;
 import com.oracle.jvmci.code.*;
 import com.oracle.jvmci.common.*;
-import com.oracle.jvmci.compiler.Compiler;
 import com.oracle.jvmci.debug.*;
 import com.oracle.jvmci.debug.Debug.Scope;
 import com.oracle.jvmci.meta.*;
@@ -1524,7 +1524,7 @@ public class BytecodeParser implements GraphBuilderContext {
     private void parseAndInlineCallee(ResolvedJavaMethod targetMethod, ValueNode[] args, IntrinsicContext calleeIntrinsicContext) {
         try (IntrinsicScope s = calleeIntrinsicContext != null && !parsingIntrinsic() ? new IntrinsicScope(this, targetMethod.getSignature().toParameterKinds(!targetMethod.isStatic()), args) : null) {
 
-            BytecodeParser parser = graphBuilderInstance.createBytecodeParser(graph, this, targetMethod, Compiler.INVOCATION_ENTRY_BCI, calleeIntrinsicContext);
+            BytecodeParser parser = graphBuilderInstance.createBytecodeParser(graph, this, targetMethod, INVOCATION_ENTRY_BCI, calleeIntrinsicContext);
             FrameStateBuilder startFrameState = new FrameStateBuilder(parser, targetMethod, graph);
             if (!targetMethod.isStatic()) {
                 args[0] = nullCheckedValue(args[0]);
@@ -2294,7 +2294,7 @@ public class BytecodeParser implements GraphBuilderContext {
                     throw new BailoutException("OSR into a JSR scope is not supported");
                 }
                 EntryMarkerNode x = append(new EntryMarkerNode());
-                frameState.insertProxies(value -> graph.unique(new EntryProxyNode(value, x)));
+                frameState.insertProxies(value -> ProxyNode.forValue(value, x, graph));
                 x.setStateAfter(createFrameState(bci, x));
             }
 
@@ -2974,13 +2974,6 @@ public class BytecodeParser implements GraphBuilderContext {
     }
 
     private ValueNode appendNullCheck(ValueNode object) {
-        if (object.stamp() instanceof AbstractPointerStamp) {
-            AbstractPointerStamp stamp = (AbstractPointerStamp) object.stamp();
-            if (stamp.nonNull()) {
-                return object;
-            }
-        }
-
         IsNullNode isNull = append(new IsNullNode(object));
         FixedGuardNode fixedGuard = append(new FixedGuardNode(isNull, DeoptimizationReason.NullCheckException, DeoptimizationAction.InvalidateReprofile, true));
         return append(new PiNode(object, object.stamp().join(StampFactory.objectNonNull()), fixedGuard));
