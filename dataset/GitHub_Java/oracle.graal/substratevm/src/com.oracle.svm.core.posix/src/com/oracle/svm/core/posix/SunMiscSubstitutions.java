afaccc5d2c334f86bcda46350a6f4d9df655ed78
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:40:18 +0200

    [GR-7818] Unknown signal POLL.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/SunMiscSubstitutions.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/SunMiscSubstitutions.java
@@ -26,8 +26,6 @@ import java.io.Console;
 import java.nio.charset.Charset;
 import java.util.concurrent.locks.ReentrantLock;
 
-import org.graalvm.nativeimage.Platform;
-import org.graalvm.nativeimage.Platforms;
 import org.graalvm.word.WordFactory;
 
 import com.oracle.svm.core.annotate.Alias;
@@ -47,7 +45,6 @@ import com.oracle.svm.core.snippets.KnownIntrinsics;
 import com.oracle.svm.core.util.VMError;
 
 @TargetClass(sun.misc.SharedSecrets.class)
-@Platforms({Platform.LINUX.class, Platform.DARWIN.class})
 final class Target_sun_misc_SharedSecrets {
 
     @Alias @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
@@ -61,7 +58,7 @@ final class Target_sun_misc_SharedSecrets {
                 public Console console() {
                     if (Target_java_io_Console.istty()) {
                         if (Target_java_lang_System.cons == null) {
-                            Target_java_lang_System.cons = Util_java_io_Console.toConsole(new Target_java_io_Console());
+                            Target_java_lang_System.cons = KnownIntrinsics.unsafeCast(new Target_java_io_Console(), Console.class);
                         }
                         return Target_java_lang_System.cons;
                     }
@@ -78,17 +75,9 @@ final class Target_sun_misc_SharedSecrets {
         }
         return javaIOAccess;
     }
-
-    @Alias private static sun.misc.JavaLangAccess javaLangAccess;
-
-    @Substitute
-    public static sun.misc.JavaLangAccess getJavaLangAccess() {
-        return javaLangAccess;
-    }
 }
 
 @TargetClass(sun.misc.Signal.class)
-@Platforms({Platform.LINUX.class, Platform.DARWIN.class})
 final class Target_sun_misc_Signal {
 
     @Substitute
@@ -374,7 +363,6 @@ final class Util_sun_misc_Signal {
 
 /** Translated from: jdk/src/share/native/sun/misc/NativeSignalHandler.c?v=Java_1.8.0_40_b10. */
 @TargetClass(className = "sun.misc.NativeSignalHandler")
-@Platforms({Platform.LINUX.class, Platform.DARWIN.class})
 final class Target_sun_misc_NativeSignalHandler {
 
     /**
--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/headers/Signal.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/headers/Signal.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -143,7 +141,7 @@ public class Signal {
                 __sigset_t uc_sigmask;
                 struct _libc_fpstate __fpregs_mem;
               } ucontext_t;
-        
+
             // Context to describe whole processor state.
             typedef struct
               {
@@ -286,24 +284,7 @@ public class Signal {
     @CContext(PosixDirectives.class)
     public enum LinuxSignalEnum {
         /* Pollable event (Sys V). Synonym for SIGIO */
-        SIGPOLL,
-        /* Power failure restart (System V). */
-        SIGPWR;
-
-        @CEnumValue
-        public native int getCValue();
-    }
-
-    /** An alphabetical list of Darwin-specific signals. */
-    /* Workaround for GR-7858: @Platform @CEnum members. */
-    @Platforms(Platform.DARWIN.class)
-    @CEnum
-    @CContext(PosixDirectives.class)
-    public enum DarwinSignalEnum {
-        /* status request from keyboard */
-        SIGINFO,
-        /* EMT instruction */
-        SIGEMT;
+        SIGPOLL;
 
         @CEnumValue
         public native int getCValue();
