073f06a5095961302baf43448079337f287b2731
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 01:59:02 +0200

    Fix bug of missing foreign key column occasionally on lollipop.

--- dataset/GitHub_Java/guolindev.LitePal/litepal/src/main/java/org/litepal/LitePalBase.java
+++ dataset/GitHub_Java/guolindev.LitePal/litepal/src/main/java/org/litepal/LitePalBase.java
@@ -16,6 +16,17 @@
 
 package org.litepal;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.litepal.annotation.Column;
 import org.litepal.crud.model.AssociationsInfo;
 import org.litepal.exceptions.DatabaseGenerateException;
@@ -32,16 +43,6 @@ import org.litepal.tablemanager.typechange.TextOrm;
 import org.litepal.util.BaseUtility;
 import org.litepal.util.Const;
 import org.litepal.util.DBUtility;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Base class of all the LitePal components. If each component need to
