b0314470a2ecd705c1eb415ff341ea2961044a08
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:43:24 +0200

    Rename package categorization. Cleanup package description.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.metadata/src/com/oracle/truffle/api/metadata/package-info.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.metadata/src/com/oracle/truffle/api/metadata/package-info.java
@@ -23,6 +23,12 @@
  * questions.
  */
 
+/*
+ @ApiInfo(
+ group="Truffle"
+ )
+ */
+
 /**
  * The metadata package provides scope APIs related to declared guest language variables. Tools are
  * the main client of these APIs.
@@ -30,8 +36,9 @@
  * Services declared in this package are registered by directly implementing the
  * {@link com.oracle.truffle.api.TruffleLanguage} with the appropriate service interface.
  * <p>
- * This package was deprecated since 0.30 in favor of {@link com.oracle.truffle.api.Scope} class and
- * {@link com.oracle.truffle.api.TruffleLanguage#findScopes}.
+ * This package was deprecated since 0.30 in favor of {@link com.oracle.truffle.api.Scope} class,
+ * {@link com.oracle.truffle.api.TruffleLanguage#findLocalScopes} and
+ * {@link com.oracle.truffle.api.instrumentation.TruffleInstrument.Env#findLocalScopes}.
  *
  * @since 0.26
  * @deprecated
