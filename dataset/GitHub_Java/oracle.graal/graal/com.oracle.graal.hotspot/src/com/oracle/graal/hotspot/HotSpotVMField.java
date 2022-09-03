d6215c54b9b529631f42f0ba843ce7aff38bc1b3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 20:48:21 +0200

    added type to HotSpotVMField annotation in order to verify the expected type

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/HotSpotVMField.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/HotSpotVMField.java
@@ -38,11 +38,5 @@ public @interface HotSpotVMField {
 
     Type get();
 
-    /**
-     * List of architectures where this constant is required. Names are derived from
-     * {@link HotSpotVMConfig#getHostArchitectureName()}.
-     */
-    String[] archs() default {};
-
     boolean optional() default false;
 }
