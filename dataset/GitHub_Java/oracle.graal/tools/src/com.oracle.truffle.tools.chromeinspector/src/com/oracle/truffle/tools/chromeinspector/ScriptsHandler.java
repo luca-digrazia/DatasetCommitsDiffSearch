1f2333d2912289192d75c50d83357a462e2ba468
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:42:01 +0200

    [GR-10409] Workaround for a Chrome URL bug.

--- dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/ScriptsHandler.java
+++ dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/ScriptsHandler.java
@@ -43,10 +43,8 @@ public final class ScriptsHandler implements LoadSourceListener {
     private final Map<Source, Integer> sourceIDs = new HashMap<>(100);
     private final List<Script> scripts = new ArrayList<>(100);
     private final List<LoadScriptListener> listeners = new ArrayList<>();
-    private final boolean reportInternal;
 
-    public ScriptsHandler(boolean reportInternal) {
-        this.reportInternal = reportInternal;
+    public ScriptsHandler() {
     }
 
     public int getScriptId(Source source) {
@@ -294,13 +292,17 @@ public final class ScriptsHandler implements LoadSourceListener {
     @Override
     public void onLoad(LoadSourceEvent event) {
         Source source = event.getSource();
-        if (reportInternal || !source.isInternal()) {
+        if (!source.isInternal()) {
             assureLoaded(source);
         }
     }
 
     static boolean compareURLs(String url1, String url2) {
-        return stripScheme(url1).equals(stripScheme(url2));
+        String surl1 = stripScheme(url1);
+        String surl2 = stripScheme(url2);
+        // Either equals,
+        // or equals while ignoring the initial slash (workaround for Chromium bug #851853)
+        return surl1.equals(surl2) || surl1.startsWith("/") && surl1.substring(1).equals(surl2);
     }
 
     private static String stripScheme(String url) {
@@ -316,4 +318,8 @@ public final class ScriptsHandler implements LoadSourceListener {
         void loadedScript(Script script);
     }
 
+    public interface Provider {
+
+        ScriptsHandler getScriptsHandler();
+    }
 }
