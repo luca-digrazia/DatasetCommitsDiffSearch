010a9b1563d1bf8d4229adc18bf14254eb612c29
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:50:40 +0200

    Automatically converting plain Java objects to TruffleObject wrappers when doing execute. Similar functionality is already available for PolyglotEngine.Builder.globalSymbol.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/SymbolInvokerImpl.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/SymbolInvokerImpl.java
@@ -66,15 +66,6 @@ abstract class SymbolInvokerImpl {
         return new TemporaryRoot(lang, foreignAccess, function);
     }
 
-    static void unwrapArgs(final Object[] args) {
-        for (int i = 0; i < args.length; i++) {
-            if (args[i] instanceof EngineTruffleObject) {
-                args[i] = ((EngineTruffleObject) args[i]).getDelegate();
-            }
-            args[i] = JavaInterop.asTruffleValue(args[i]);
-        }
-    }
-
     static class TemporaryRoot extends RootNode {
         @Child private Node foreignAccess;
         @Child private ConvertNode convert;
@@ -118,7 +109,11 @@ abstract class SymbolInvokerImpl {
         @Override
         protected Object executeImpl(VirtualFrame frame) {
             final Object[] args = frame.getArguments();
-            unwrapArgs(args);
+            if (PolyglotEngine.JAVA_INTEROP_ENABLED) {
+                for (int i = 0; i < args.length; i++) {
+                    args[i] = JavaInterop.asTruffleValue(args[i]);
+                }
+            }
             try {
                 if (foreignAccess == null) {
                     CompilerDirectives.transferToInterpreterAndInvalidate();
