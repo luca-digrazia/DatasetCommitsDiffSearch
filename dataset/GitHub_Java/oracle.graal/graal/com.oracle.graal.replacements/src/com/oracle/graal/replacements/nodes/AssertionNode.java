2ae2fc2d5214dc611cc097040d576d68f20e72d2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 16:20:22 +0200

    allow satisfied compile time assertions to survive until LIR lowering

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/nodes/AssertionNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/nodes/AssertionNode.java
@@ -22,28 +22,20 @@
  */
 package com.oracle.graal.replacements.nodes;
 
-import com.oracle.graal.compiler.common.type.StampFactory;
-import com.oracle.graal.debug.GraalError;
-import com.oracle.graal.graph.Node;
-import com.oracle.graal.graph.NodeClass;
-import com.oracle.graal.graph.spi.Canonicalizable;
-import com.oracle.graal.graph.spi.CanonicalizerTool;
-import com.oracle.graal.nodeinfo.NodeCycles;
-import com.oracle.graal.nodeinfo.NodeInfo;
-import com.oracle.graal.nodeinfo.NodeSize;
-import com.oracle.graal.nodes.FixedWithNextNode;
-import com.oracle.graal.nodes.ValueNode;
-import com.oracle.graal.nodes.spi.LIRLowerable;
-import com.oracle.graal.nodes.spi.Lowerable;
-import com.oracle.graal.nodes.spi.LoweringTool;
-import com.oracle.graal.nodes.spi.NodeLIRBuilderTool;
+import com.oracle.graal.compiler.common.*;
+import com.oracle.graal.compiler.common.type.*;
+import com.oracle.graal.graph.*;
+import com.oracle.graal.graph.spi.*;
+import com.oracle.graal.nodeinfo.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.spi.*;
 
 /**
  * Assertion nodes will go away as soon as the value evaluates to true. Compile-time assertions will
  * fail if this has not happened by the time the node is lowered to LIR, while runtime assertions
  * may need to insert a check.
  */
-@NodeInfo(cycles = NodeCycles.CYCLES_UNKNOWN, size = NodeSize.SIZE_UNKNOWN)
+@NodeInfo
 public final class AssertionNode extends FixedWithNextNode implements Lowerable, Canonicalizable, LIRLowerable {
 
     public static final NodeClass<AssertionNode> TYPE = NodeClass.create(AssertionNode.class);
@@ -79,22 +71,20 @@ public final class AssertionNode extends FixedWithNextNode implements Lowerable,
         return this;
     }
 
-    @Override
     public void lower(LoweringTool tool) {
         if (!compileTimeAssertion) {
             tool.getLowerer().lower(this, tool);
         }
     }
 
-    @Override
     public void generate(NodeLIRBuilderTool generator) {
         assert compileTimeAssertion;
         if (value.isConstant()) {
             if (value.asJavaConstant().asInt() == 0) {
-                throw new GraalError("%s: failed compile-time assertion: %s", this, message);
+                throw new GraalInternalError("%s: failed compile-time assertion: %s", this, message);
             }
         } else {
-            throw new GraalError("%s: failed compile-time assertion (value %s): %s", this, value, message);
+            throw new GraalInternalError("%s: failed compile-time assertion (value %s): %s", this, value, message);
         }
     }
 
