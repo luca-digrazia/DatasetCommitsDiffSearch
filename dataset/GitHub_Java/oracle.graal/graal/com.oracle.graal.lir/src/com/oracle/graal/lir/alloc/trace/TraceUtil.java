f5195197d2f71bfede22d09dc11e97da043455e8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:01:32 +0200

    TraceRA: merge blocks are in general not trivial

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/TraceUtil.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/TraceUtil.java
@@ -22,18 +22,25 @@
  */
 package com.oracle.graal.lir.alloc.trace;
 
-import jdk.vm.ci.meta.Value;
+import java.util.List;
 
+import com.oracle.graal.compiler.common.alloc.Trace;
 import com.oracle.graal.compiler.common.alloc.TraceBuilderResult;
 import com.oracle.graal.compiler.common.cfg.AbstractBlockBase;
+import com.oracle.graal.lir.LIR;
+import com.oracle.graal.lir.LIRInstruction;
+import com.oracle.graal.lir.StandardOp.JumpOp;
+import com.oracle.graal.lir.StandardOp.LabelOp;
+
+import jdk.vm.ci.meta.Value;
 
 public class TraceUtil {
 
-    public static AbstractBlockBase<?> getBestTraceInterPredecessor(TraceBuilderResult<?> traceResult, AbstractBlockBase<?> block) {
+    public static AbstractBlockBase<?> getBestTraceInterPredecessor(TraceBuilderResult traceResult, AbstractBlockBase<?> block) {
         AbstractBlockBase<?> bestPred = null;
-        int bestTraceId = traceResult.getTraceForBlock(block);
+        int bestTraceId = traceResult.getTraceForBlock(block).getId();
         for (AbstractBlockBase<?> pred : block.getPredecessors()) {
-            int predTraceId = traceResult.getTraceForBlock(pred);
+            int predTraceId = traceResult.getTraceForBlock(pred).getId();
             if (predTraceId < bestTraceId) {
                 bestPred = pred;
                 bestTraceId = predTraceId;
@@ -50,5 +57,33 @@ public class TraceUtil {
     public static ShadowedRegisterValue asShadowedRegisterValue(Value value) {
         assert isShadowedRegisterValue(value);
         return (ShadowedRegisterValue) value;
+    }
+
+    public static boolean isTrivialTrace(LIR lir, Trace trace) {
+        if (trace.size() != 1) {
+            return false;
+        }
+        List<LIRInstruction> instructions = lir.getLIRforBlock(trace.getBlocks()[0]);
+        if (instructions.size() != 2) {
+            return false;
+        }
+        assert instructions.get(0) instanceof LabelOp : "First instruction not a LabelOp: " + instructions.get(0);
+        if (((LabelOp) instructions.get(0)).isPhiIn()) {
+            /*
+             * Merge blocks are in general not trivial block because variables defined by a PHI
+             * always need a location. If the outgoing value of the predecessor is a constant we
+             * need to find an appropriate location (register or stack).
+             *
+             * Note that this case should not happen in practice since the trace containing the
+             * merge block should also contain one of the predecessors. For non-standard trace
+             * builders (e.g. the single block trace builder) this is not the true, though.
+             */
+            return false;
+        }
+        /*
+         * Now we need to check if the BlockEndOp has no special operand requirements (i.e.
+         * stack-slot, register). For now we just check for JumpOp because we know that it doesn't.
+         */
+        return instructions.get(1) instanceof JumpOp;
     }
 }
