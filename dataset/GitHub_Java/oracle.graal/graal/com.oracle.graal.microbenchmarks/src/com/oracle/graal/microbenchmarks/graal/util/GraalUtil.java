0e9c9c9bd068b9de291a99ce416ee315f129cfaa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 13:01:12 +0200

    microbenchmarks/GraphUtil: allow turning off profiling.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.microbenchmarks/src/com/oracle/graal/microbenchmarks/graal/util/GraalUtil.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.microbenchmarks/src/com/oracle/graal/microbenchmarks/graal/util/GraalUtil.java
@@ -22,8 +22,6 @@
  */
 package com.oracle.graal.microbenchmarks.graal.util;
 
-import static com.oracle.graal.compiler.common.CompilationIdentifier.INVALID_COMPILATION_ID;
-
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -133,7 +131,7 @@ public class GraalUtil {
     }
 
     public static StructuredGraph getGraph(GraalState graal, ResolvedJavaMethod javaMethod, boolean useProfilingInfo) {
-        StructuredGraph graph = new StructuredGraph(javaMethod, StructuredGraph.AllowAssumptions.YES, useProfilingInfo, INVALID_COMPILATION_ID);
+        StructuredGraph graph = new StructuredGraph(javaMethod, StructuredGraph.AllowAssumptions.YES, useProfilingInfo);
         PhaseSuite<HighTierContext> graphBuilderSuite = new PhaseSuite<>();
         MetaAccessProvider metaAccess = graal.providers.getMetaAccess();
         graphBuilderSuite.appendPhase(new GraphBuilderPhase(GraphBuilderConfiguration.getDefault(new Plugins(new InvocationPlugins(metaAccess)))));
--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.microbenchmarks/src/com/oracle/graal/microbenchmarks/lir/GraalCompilerState.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.microbenchmarks/src/com/oracle/graal/microbenchmarks/lir/GraalCompilerState.java
@@ -455,10 +455,6 @@ public abstract class GraalCompilerState {
         return graph;
     }
 
-    protected LIR getLIR() {
-        return lirGenRes.getLIR();
-    }
-
     public abstract static class Compile extends GraalCompilerState {
 
         @Setup(Level.Trial)
