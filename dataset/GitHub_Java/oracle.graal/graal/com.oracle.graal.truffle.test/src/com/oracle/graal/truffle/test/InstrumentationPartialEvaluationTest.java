60790a38d019df963d45687d0512514bb86c4c86
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 15:56:13 +0200

    Truffle/Instrumentation:  change the signature for creation of Advanced instruments, refactor how the callback listener is managed.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.truffle.test/src/com/oracle/graal/truffle/test/InstrumentationPartialEvaluationTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.truffle.test/src/com/oracle/graal/truffle/test/InstrumentationPartialEvaluationTest.java
@@ -217,7 +217,7 @@ public class InstrumentationPartialEvaluationTest extends PartialEvaluationTest 
             public AdvancedInstrumentRoot createInstrumentRoot(Probe probe, Node node) {
                 return null;
             }
-        }, null, "test AdvancedInstrument");
+        }, "test AdvancedInstrument");
         testProbe.attach(instrument);
 
         // It all gets compiled away
@@ -248,7 +248,7 @@ public class InstrumentationPartialEvaluationTest extends PartialEvaluationTest 
                     }
                 };
             }
-        }, null, "test AdvancedInstrument");
+        }, "test AdvancedInstrument");
         testProbe.attach(instrument);
 
         // It all gets compiled away.
