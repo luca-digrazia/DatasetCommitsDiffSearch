7d2aaa5c4da7ecc334e98fbad42b6f8104c2f816
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 21:18:09 +0200

    TruffleRuntime.createVirtualFrame.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/OptimizedCallTarget.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/OptimizedCallTarget.java
@@ -183,12 +183,6 @@ public final class OptimizedCallTarget extends DefaultCallTarget implements Fram
     @Override
     public void nodeReplaced() {
         replaceCount++;
-        if (compiledMethod != null) {
-            if (compiledMethod.isValid()) {
-                compiledMethod.invalidate();
-            }
-            compiledMethod = null;
-        }
         compilationPolicy.nodeReplaced();
     }
 
