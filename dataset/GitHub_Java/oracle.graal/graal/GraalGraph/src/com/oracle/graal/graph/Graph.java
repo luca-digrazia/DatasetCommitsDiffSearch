efa78af0e00fbdd250652e978d6f115862404be2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 01:53:33 +0200

    Added Oracle header. Moved author declaration into package-info.java file.

--- dataset/GitHub_Java/oracle.graal/graal/GraalGraph/src/com/oracle/graal/graph/Graph.java
+++ dataset/GitHub_Java/oracle.graal/graal/GraalGraph/src/com/oracle/graal/graph/Graph.java
@@ -27,28 +27,26 @@ import java.util.Collection;
 import java.util.Collections;
 
 public class Graph {
+	private final ArrayList<Node> nodes;
+	private int nextId;
+	
+	public Graph() {
+		nodes = new ArrayList<Node>();
+	}
 
-    private final ArrayList<Node> nodes;
-    private int nextId;
-
-    public Graph() {
-        nodes = new ArrayList<Node>();
-    }
-
-    public synchronized int nextId(Node node) {
-        int id = nextId++;
-        nodes.add(id, node);
-        return id;
-    }
-
-    public Collection<Node> getNodes() {
-        return Collections.unmodifiableCollection(nodes);
-    }
-
-    public Node local(Node node) {
-        if (node.graph() == this) {
-            return node;
-        }
-        return node.cloneNode(this);
-    }
+	public synchronized int nextId(Node node) {
+		int id = nextId++;
+		nodes.add(id, node);
+		return id;
+	}
+	
+	public Collection<Node> getNodes(){
+		return Collections.unmodifiableCollection(nodes);
+	}
+	
+	public Node local(Node node) {
+		if(node.getGraph() == this)
+			return node;
+		return node.cloneNode(this);
+	}
 }
