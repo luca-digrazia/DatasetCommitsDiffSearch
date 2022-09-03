c71f9f68639f92301af93a2578338484bd2933fa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:02:32 +0200

    Let toShort method return proper type

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/ToPrimitiveNode.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/ToPrimitiveNode.java
@@ -56,10 +56,6 @@ final class ToPrimitiveNode extends Node {
     }
 
     Object toPrimitive(Object value, Class<?> requestedType) {
-        return toPrimitive(value, requestedType, 0);
-    }
-
-    Object toPrimitive(Object value, Class<?> requestedType, int conversionLevel) {
         Object attr;
         if (value instanceof JavaObject) {
             attr = ((JavaObject) value).obj;
@@ -101,14 +97,9 @@ final class ToPrimitiveNode extends Node {
                     return str.charAt(0);
                 }
             } else {
-                if (conversionLevel > 0) {
-                    Integer safeChar = toInt(value);
-                    if (safeChar != null) {
-                        int v = safeChar;
-                        if (v >= 0 && v < 65536) {
-                            return (char) v;
-                        }
-                    }
+                Short safeChar = toShort(value);
+                if (safeChar != null) {
+                    return (char) (short) safeChar;
                 }
             }
         } else if (requestedType == String.class || requestedType == CharSequence.class) {
@@ -195,7 +186,7 @@ final class ToPrimitiveNode extends Node {
         return null;
     }
 
-    private static Integer toInt(Object value) {
+    private static Object toInt(Object value) {
         if (value instanceof Integer) {
             Integer i = (Integer) value;
             return i;
