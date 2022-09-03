2710604b1e817c182428a56c80b6f6d0ce02c9e0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 15:33:14 +0200

    Empty object stamps should always be nonNull, assert it.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.common/src/com/oracle/graal/compiler/common/type/AbstractPointerStamp.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.common/src/com/oracle/graal/compiler/common/type/AbstractPointerStamp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,56 +22,94 @@
  */
 package com.oracle.graal.compiler.common.type;
 
-import com.oracle.graal.api.meta.*;
-import com.oracle.graal.compiler.common.*;
-import com.oracle.graal.compiler.common.spi.*;
+import jdk.internal.jvmci.meta.*;
 
 /**
  * Abstract base class of all pointer types.
  */
 public abstract class AbstractPointerStamp extends Stamp {
 
-    private final PointerType type;
+    private final boolean nonNull;
+    private final boolean alwaysNull;
 
-    protected AbstractPointerStamp(PointerType type) {
-        this.type = type;
+    protected AbstractPointerStamp(boolean nonNull, boolean alwaysNull) {
+        this.nonNull = nonNull;
+        this.alwaysNull = alwaysNull;
     }
 
-    public PointerType getType() {
-        return type;
+    public boolean nonNull() {
+        assert !this.isEmpty() || nonNull;
+        return nonNull;
+    }
+
+    public boolean alwaysNull() {
+        return alwaysNull;
+    }
+
+    protected abstract AbstractPointerStamp copyWith(boolean newNonNull, boolean newAlwaysNull);
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (alwaysNull ? 1231 : 1237);
+        result = prime * result + (nonNull ? 1231 : 1237);
+        return result;
     }
 
     @Override
-    public boolean isCompatible(Stamp otherStamp) {
-        if (otherStamp instanceof AbstractPointerStamp) {
-            AbstractPointerStamp other = (AbstractPointerStamp) otherStamp;
-            return this.type == other.type;
+    public Stamp join(Stamp stamp) {
+        AbstractPointerStamp other = (AbstractPointerStamp) stamp;
+        boolean joinNonNull = this.nonNull || other.nonNull;
+        boolean joinAlwaysNull = this.alwaysNull || other.alwaysNull;
+        if (joinNonNull && joinAlwaysNull) {
+            return empty();
+        } else {
+            return copyWith(joinNonNull, joinAlwaysNull);
         }
-        return false;
     }
 
     @Override
-    public LIRKind getLIRKind(LIRKindTool tool) {
-        return tool.getPointerKind(getType());
+    public Stamp improveWith(Stamp other) {
+        return join(other);
+    }
+
+    @Override
+    public Stamp meet(Stamp stamp) {
+        AbstractPointerStamp other = (AbstractPointerStamp) stamp;
+        boolean meetNonNull = this.nonNull && other.nonNull;
+        boolean meetAlwaysNull = this.alwaysNull && other.alwaysNull;
+        return copyWith(meetNonNull, meetAlwaysNull);
+    }
+
+    @Override
+    public Stamp unrestricted() {
+        return copyWith(false, false);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        AbstractPointerStamp other = (AbstractPointerStamp) obj;
+        return this.alwaysNull == other.alwaysNull && this.nonNull == other.nonNull;
+    }
+
+    @Override
+    public Constant asConstant() {
+        if (alwaysNull) {
+            return JavaConstant.NULL_POINTER;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Kind getStackKind() {
         return Kind.Illegal;
-    }
-
-    @Override
-    public Constant readConstant(ConstantReflectionProvider provider, Constant base, long displacement) {
-        return provider.readPointerConstant(type, base, displacement);
-    }
-
-    @Override
-    public ResolvedJavaType javaType(MetaAccessProvider metaAccess) {
-        throw GraalInternalError.shouldNotReachHere(type + " pointer has no Java type");
-    }
-
-    @Override
-    public String toString() {
-        return type + "*";
     }
 }
