7f83c706da5d14e2079427e6199b6c8a1016160b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:57:44 +0200

    TraceRA: minor clean ups in the UniDirectionalTraceBuilder

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.common/src/org/graalvm/compiler/core/common/alloc/UniDirectionalTraceBuilder.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.core.common/src/org/graalvm/compiler/core/common/alloc/UniDirectionalTraceBuilder.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -65,7 +63,7 @@ public final class UniDirectionalTraceBuilder {
     }
 
     private static int compare(AbstractBlockBase<?> a, AbstractBlockBase<?> b) {
-        return Double.compare(b.getRelativeFrequency(), a.getRelativeFrequency());
+        return Double.compare(b.probability(), a.probability());
     }
 
     private boolean processed(AbstractBlockBase<?> b) {
@@ -110,7 +108,7 @@ public final class UniDirectionalTraceBuilder {
         int blockNumber = 0;
         try (Indent i = debug.logAndIndent("StartTrace: %s", traceStart)) {
             for (AbstractBlockBase<?> block = traceStart; block != null; block = selectNext(block)) {
-                debug.log("add %s (freq: %f)", block, block.getRelativeFrequency());
+                debug.log("add %s (prob: %f)", block, block.probability());
                 processed.set(block.getId());
                 trace.add(block);
                 unblock(block);
@@ -149,7 +147,7 @@ public final class UniDirectionalTraceBuilder {
     private AbstractBlockBase<?> selectNext(AbstractBlockBase<?> block) {
         AbstractBlockBase<?> next = null;
         for (AbstractBlockBase<?> successor : block.getSuccessors()) {
-            if (!processed(successor) && (next == null || successor.getRelativeFrequency() > next.getRelativeFrequency())) {
+            if (!processed(successor) && (next == null || successor.probability() > next.probability())) {
                 next = successor;
             }
         }
