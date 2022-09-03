49b7a97fe1ed9a0ef44c690315cb6ed2301c4b4a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 15:39:02 +0200

    Limit inlining depth during partial evaluation to avoid StackOverflowError, provide useful error message instead

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/PEGraphDecoder.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/PEGraphDecoder.java
@@ -23,14 +23,9 @@
 package com.oracle.graal.replacements;
 
 import static com.oracle.graal.java.BytecodeParser.Options.*;
-import static jdk.internal.jvmci.common.JVMCIError.*;
+import static com.oracle.jvmci.common.JVMCIError.*;
 
 import java.util.*;
-
-import jdk.internal.jvmci.code.*;
-import jdk.internal.jvmci.debug.*;
-import jdk.internal.jvmci.meta.*;
-import jdk.internal.jvmci.options.*;
 
 import com.oracle.graal.compiler.common.type.*;
 import com.oracle.graal.graph.*;
@@ -46,6 +41,10 @@ import com.oracle.graal.nodes.extended.*;
 import com.oracle.graal.nodes.java.*;
 import com.oracle.graal.nodes.spi.*;
 import com.oracle.graal.phases.common.inlining.*;
+import com.oracle.jvmci.code.*;
+import com.oracle.jvmci.debug.*;
+import com.oracle.jvmci.meta.*;
+import com.oracle.jvmci.options.*;
 
 /**
  * A graph decoder that performs partial evaluation, i.e., that performs method inlining and
@@ -176,7 +175,7 @@ public abstract class PEGraphDecoder extends SimplifyingGraphDecoder {
         }
 
         @Override
-        public void handleReplacedInvoke(InvokeKind invokeKind, ResolvedJavaMethod targetMethod, ValueNode[] args, boolean inlineEverything) {
+        public void handleReplacedInvoke(InvokeKind invokeKind, ResolvedJavaMethod targetMethod, ValueNode[] args) {
             throw unimplemented();
         }
 
