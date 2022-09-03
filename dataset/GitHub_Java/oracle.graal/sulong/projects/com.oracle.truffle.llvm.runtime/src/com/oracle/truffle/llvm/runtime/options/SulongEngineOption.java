b545b243b2ec2e3d593784647a9719365605ad4e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:36:04 +0200

    Remove Unused Trace Option

--- dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/options/SulongEngineOption.java
+++ dataset/GitHub_Java/oracle.graal/sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/options/SulongEngineOption.java
@@ -45,7 +45,7 @@ public final class SulongEngineOption {
 
     public static final String OPTION_ARRAY_SEPARATOR = ":";
 
-    public static final OptionKey<String> CONFIGURATION = new OptionKey<>("native");
+    public static final OptionKey<String> CONFIGURATION = new OptionKey<>("basic");
     public static final String CONFIGURATION_NAME = "llvm.configuration";
     public static final String CONFIGURATION_INFO = "Sulongs configuration (default=basic).";
 
@@ -65,10 +65,6 @@ public final class SulongEngineOption {
     public static final OptionKey<Boolean> ENABLE_NFI = new OptionKey<>(true);
     public static final String ENABLE_NFI_NAME = "llvm.enableExternalNativeAccess";
     public static final String ENABLE_NFI_INFO = "Enable Sulongs native interface.";
-
-    public static final OptionKey<String> DEBUG = new OptionKey<>(String.valueOf(false));
-    public static final String DEBUG_NAME = "llvm.debug";
-    public static final String DEBUG_INFO = "Turns debugging on/off. Can be \'true\', \'false\', \'stdout\', \'stderr\' or a filepath.";
 
     public static final OptionKey<String> DEBUG_SYSCALLS = new OptionKey<>(String.valueOf(false));
     public static final String DEBUG_SYSCALLS_NAME = "llvm.debugSysCalls";
