adfb23909b3ad1381b743378a2f0ff6c1e726111
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:20:53 +0200

    Remove usage of hosted elements in Truffle images

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.truffle/src/com/oracle/svm/truffle/api/SubstrateTruffleCompiler.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.truffle/src/com/oracle/svm/truffle/api/SubstrateTruffleCompiler.java
@@ -24,7 +24,6 @@
  */
 package com.oracle.svm.truffle.api;
 
-import java.io.PrintStream;
 import java.util.Map;
 
 import org.graalvm.compiler.api.replacements.SnippetReflectionProvider;
@@ -88,13 +87,13 @@ public class SubstrateTruffleCompiler extends TruffleCompilerImpl {
     }
 
     @Override
-    public CompilationIdentifier createCompilationIdentifier(CompilableTruffleAST optimizedCallTarget) {
+    public CompilationIdentifier getCompilationIdentifier(CompilableTruffleAST optimizedCallTarget) {
         return new SubstrateTruffleCompilationIdentifier((OptimizedCallTarget) optimizedCallTarget);
     }
 
     @Override
-    public DebugContext createDebugContext(OptionValues options, CompilationIdentifier compilationId, CompilableTruffleAST callTarget, PrintStream logStream) {
-        return GraalSupport.get().openDebugContext(options, compilationId, callTarget, logStream);
+    public DebugContext openDebugContext(OptionValues options, CompilationIdentifier compilationId, CompilableTruffleAST callTarget) {
+        return GraalSupport.get().openDebugContext(options, compilationId, callTarget);
     }
 
     @Override
