950a75379fe5f77567fbce5d71bf3b6359134271
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:47:42 +0200

    look at the probability of IfNodes during ComputeLinearScanOrder

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/alloc/ComputeLinearScanOrder.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/alloc/ComputeLinearScanOrder.java
@@ -27,7 +27,6 @@ import java.util.*;
 
 import com.oracle.max.criutils.*;
 import com.oracle.graal.compiler.*;
-import com.oracle.graal.compiler.util.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.lir.cfg.*;
 import com.oracle.graal.nodes.*;
@@ -39,17 +38,17 @@ public final class ComputeLinearScanOrder {
     List<Block> linearScanOrder; // the resulting list of blocks in correct order
     List<Block> codeEmittingOrder;
 
-    final BitSet visitedBlocks; // used for recursive processing of blocks
-    final BitSet activeBlocks; // used for recursive processing of blocks
-    final BitSet dominatorBlocks; // temporary BitMap used for computation of dominator
+    final BitMap visitedBlocks; // used for recursive processing of blocks
+    final BitMap activeBlocks; // used for recursive processing of blocks
+    final BitMap dominatorBlocks; // temporary BitMap used for computation of dominator
     final int[] forwardBranches; // number of incoming forward branches for each block
     final List<Block> workList; // temporary list (used in markLoops and computeOrder)
     final Block[] loopHeaders;
 
     // accessors for visitedBlocks and activeBlocks
     void initVisited() {
-        activeBlocks.clear();
-        visitedBlocks.clear();
+        activeBlocks.clearAll();
+        visitedBlocks.clearAll();
     }
 
     boolean isVisited(Block b) {
@@ -92,9 +91,9 @@ public final class ComputeLinearScanOrder {
     public ComputeLinearScanOrder(int maxBlockId, int loopCount, Block startBlock) {
         loopHeaders = new Block[loopCount];
 
-        visitedBlocks = new BitSet(maxBlockId);
-        activeBlocks = new BitSet(maxBlockId);
-        dominatorBlocks = new BitSet(maxBlockId);
+        visitedBlocks = new BitMap(maxBlockId);
+        activeBlocks = new BitMap(maxBlockId);
+        dominatorBlocks = new BitMap(maxBlockId);
         forwardBranches = new int[maxBlockId];
         workList = new ArrayList<>(8);
 
@@ -305,21 +304,15 @@ public final class ComputeLinearScanOrder {
             Block cur = workList.remove(workList.size() - 1);
             appendBlock(cur);
 
+            // make the most successor with the highest probability the immediate successor
             Node endNode = cur.getEndNode();
-            if (endNode instanceof ControlSplitNode) {
-                // Sort the successors according to their probabilities:
-                final ControlSplitNode split = (ControlSplitNode) endNode;
-                Integer[] indexes = Util.createSortedPermutation(split.blockSuccessorCount(), new Comparator<Integer>() {
-                    @Override
-                    public int compare(Integer o1, Integer o2) {
-                        return split.probability(o1) < split.probability(o2) ? 1 : split.probability(o1) > split.probability(o2) ? -1 : 0;
-                    }
-                });
-                for (int index : indexes) {
-                    Block sux = cur.getSuccessors().get(indexes[index]);
-                    if (readyForProcessing(sux)) {
-                        sortIntoWorkList(sux);
-                    }
+            if (endNode instanceof IfNode && ((IfNode) endNode).probability() < 0.5) {
+                assert cur.numberOfSux() == 2;
+                if (readyForProcessing(cur.suxAt(1))) {
+                    sortIntoWorkList(cur.suxAt(1));
+                }
+                if (readyForProcessing(cur.suxAt(0))) {
+                    sortIntoWorkList(cur.suxAt(0));
                 }
             } else {
                 for (Block sux : cur.getSuccessors()) {
