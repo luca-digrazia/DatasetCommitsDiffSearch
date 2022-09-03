86ea05878a681f13418c9bf0b1c96511d28e7250
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:44:23 +0200

    Rename jdk.internal.jvmci to jdk.vm.ci

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/Truffle.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/Truffle.java
@@ -61,14 +61,8 @@ public class Truffle {
                 TruffleRuntimeAccess access = null;
                 Class<?> servicesClass = null;
                 try {
-                    servicesClass = Class.forName("jdk.vm.ci.services.Services");
+                    servicesClass = Class.forName("jdk.vm.ci.service.Services");
                 } catch (ClassNotFoundException e) {
-                }
-                if (servicesClass == null) {
-                    try {
-                        servicesClass = Class.forName("jdk.vm.ci.service.Services");
-                    } catch (ClassNotFoundException e) {
-                    }
                 }
                 if (servicesClass == null) {
                     try {
