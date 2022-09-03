0afc420c53f1487c0b6acc91574ee3a21ef108c4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:47:50 +0200

    Fix error for unused parameter

--- dataset/GitHub_Java/oracle.graal/projects/uk.ac.man.cs.llvm/src/uk/ac/man/cs/llvm/bc/Parser.java
+++ dataset/GitHub_Java/oracle.graal/projects/uk.ac.man.cs.llvm/src/uk/ac/man/cs/llvm/bc/Parser.java
@@ -27,27 +27,6 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/*
- * Copyright (c) 2016 University of Manchester
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 package uk.ac.man.cs.llvm.bc;
 
 import java.util.ArrayList;
@@ -99,9 +78,9 @@ public class Parser {
         result = result.getParser().read(Primitive.SUBBLOCK_ID_SIZE);
         long idsize = result.getValue();
 
-        parser = result.getParser().align(Integer.SIZE);
+        Parser p = result.getParser().align(Integer.SIZE);
 
-        result = parser.read(Integer.SIZE);
+        result = p.read(Integer.SIZE);
         long size = result.getValue();
 
         return result.getParser().enter(id, size, idsize);
@@ -121,9 +100,7 @@ public class Parser {
             operands[(int) i] = result.getValue();
         }
 
-        parser = result.getParser().handleRecord(id, operands);
-
-        return parser;
+        return result.getParser().handleRecord(id, operands);
     };
 
     protected static final Operation[] DEFAULT_OPERATIONS = new Operation[]{
@@ -167,8 +144,9 @@ public class Parser {
         this.offset = offset;
     }
 
-    protected Parser instantiate(Parser parser, Bitstream stream, Block block, ParserListener listener, Parser parent, Operation[][] operations, long idsize, long offset) {
-        return new Parser(stream, block, listener, parent, operations, idsize, offset);
+    protected Parser instantiate(@SuppressWarnings("unused") Parser argParser, Bitstream argStream, Block argBlock, ParserListener argListener, Parser argParent, Operation[][] argOperations,
+                    long argIdSize, long argOffset) {
+        return new Parser(argStream, argBlock, argListener, argParent, argOperations, argIdSize, argOffset);
     }
 
     public Parser align(long bits) {
@@ -179,13 +157,13 @@ public class Parser {
         return this;
     }
 
-    public Parser enter(long id, long size, long idsize) {
+    public Parser enter(long id, long size, long argIdSize) {
         Block subblock = Block.lookup(id);
         if (subblock == null) {
             // Cannot find block so just skip it
             return offset(getOffset() + size * Integer.SIZE);
         } else {
-            return subblock.getParser(instantiate(this, stream, subblock, listener.enter(subblock), this, operations, idsize, getOffset()));
+            return subblock.getParser(instantiate(this, stream, subblock, listener.enter(subblock), this, operations, argIdSize, getOffset()));
         }
     }
 
@@ -198,8 +176,8 @@ public class Parser {
         return offset;
     }
 
-    public Parser offset(long offset) {
-        return instantiate(this, stream, block, listener, parent, operations, idsize, offset);
+    public Parser offset(long argOffset) {
+        return instantiate(this, stream, block, listener, parent, operations, idsize, argOffset);
     }
 
     public Operation getOperation(long id) {
@@ -261,7 +239,7 @@ public class Parser {
 
     public ParserResult readChar() {
         char value = CHAR6.charAt((int) stream.read(offset, Primitive.CHAR6.getBits()));
-        return new ParserResult(offset(offset + Primitive.CHAR6.getBits()), (long) value);
+        return new ParserResult(offset(offset + Primitive.CHAR6.getBits()), value);
     }
 
     public ParserResult readId() {
