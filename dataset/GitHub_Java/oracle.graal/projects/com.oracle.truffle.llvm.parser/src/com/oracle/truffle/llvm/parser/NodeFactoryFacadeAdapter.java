6596dc7ddfaeb3be4ba2714a1bb034cacb042d31
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 13:13:52 +0200

    Remove obsolete createPhiNode method in the facade adapter

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/NodeFactoryFacadeAdapter.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/NodeFactoryFacadeAdapter.java
@@ -231,7 +231,12 @@ public abstract class NodeFactoryFacadeAdapter implements NodeFactoryFacade {
     }
 
     @Override
-    public LLVMExpressionNode createAlloc(ResolvedType type, int byteSize, int alignment, LLVMBaseType llvmType, LLVMExpressionNode numElements) {
+    public LLVMExpressionNode createAlloc(LLVMBaseType llvmType, LLVMExpressionNode numElements, int byteSize, int alignment) {
+        return null;
+    }
+
+    @Override
+    public LLVMExpressionNode createAlloc(int size, int alignment) {
         return null;
     }
 
@@ -266,7 +271,7 @@ public abstract class NodeFactoryFacadeAdapter implements NodeFactoryFacade {
     }
 
     @Override
-    public LLVMExpressionNode createStructureConstantNode(ResolvedType structType, boolean packed, ResolvedType[] types, LLVMExpressionNode[] constants) {
+    public LLVMExpressionNode createStructureConstantNode(boolean packed, int structSize, ResolvedType[] types, LLVMExpressionNode[] constants) {
         return null;
     }
 
