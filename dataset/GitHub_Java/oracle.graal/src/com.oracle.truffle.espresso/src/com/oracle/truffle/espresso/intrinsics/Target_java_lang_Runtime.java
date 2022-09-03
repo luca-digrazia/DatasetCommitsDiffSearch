94b7068b123f235c1a745f235a4fdc0fc0a7d110
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:04:48 +0200

    Add missing runtime natives for tests.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/intrinsics/Target_java_lang_Runtime.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/intrinsics/Target_java_lang_Runtime.java
@@ -34,6 +34,11 @@ import com.oracle.truffle.espresso.runtime.StaticObjectArray;
 
 @EspressoIntrinsics
 public class Target_java_lang_Runtime {
+    @Intrinsic(hasReceiver = true)
+    public static int availableProcessors(Object self) {
+        return Runtime.getRuntime().availableProcessors();
+    }
+
     // TODO(peterssen): This a hack to be able to spawn processes without going down to UNIXProcess.
     @Intrinsic(hasReceiver = true)
     public static @Type(Process.class) Object exec(StaticObject self, @Type(String[].class) StaticObject cmdarray) {
@@ -48,5 +53,15 @@ public class Target_java_lang_Runtime {
             throw new RuntimeException(e);
         }
         return StaticObject.NULL;
+    }
+
+    @Intrinsic(hasReceiver = true)
+    public static long totalMemory(Object self) {
+        return Runtime.getRuntime().totalMemory();
+    }
+
+    @Intrinsic(hasReceiver = true)
+    public static void gc(Object self) {
+        Runtime.getRuntime().gc();
     }
 }
