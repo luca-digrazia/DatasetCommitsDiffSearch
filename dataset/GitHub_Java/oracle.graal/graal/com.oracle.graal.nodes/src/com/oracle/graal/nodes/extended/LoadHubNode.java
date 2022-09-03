43bd8de2ac84af0be31288e08e032befb2affef0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 20:55:36 +0200

    Lower LoadHubNode in all late stages

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/LoadHubNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/LoadHubNode.java
@@ -61,7 +61,9 @@ public final class LoadHubNode extends FloatingGuardedNode implements Lowerable,
 
     @Override
     public void lower(LoweringTool tool) {
-        tool.getLowerer().lower(this, tool);
+        if (graph().getGuardsStage() == StructuredGraph.GuardsStage.FIXED_DEOPTS || graph().getGuardsStage() == StructuredGraph.GuardsStage.AFTER_FSA) {
+            tool.getLowerer().lower(this, tool);
+        }
     }
 
     @Override
