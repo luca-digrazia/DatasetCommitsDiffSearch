c8484eea7b9fe891879c28ec7d3146338de9d0a8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:45:21 +0200

    Move log output out of uninterruptible code block

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/thread/ThreadingSupportFeature.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/thread/ThreadingSupportFeature.java
@@ -28,14 +28,17 @@ import org.graalvm.compiler.api.replacements.Fold;
 import org.graalvm.compiler.options.Option;
 import org.graalvm.nativeimage.Feature;
 import org.graalvm.nativeimage.ImageSingletons;
+import org.graalvm.nativeimage.Threading.RecurringCallback;
+import org.graalvm.nativeimage.Threading.RecurringCallbackAccess;
 import org.graalvm.nativeimage.impl.ThreadingSupport;
 
 import com.oracle.svm.core.SubstrateOptions;
 import com.oracle.svm.core.annotate.AutomaticFeature;
-import com.oracle.svm.core.annotate.MustNotAllocate;
+import com.oracle.svm.core.annotate.RestrictHeapAccess;
 import com.oracle.svm.core.annotate.Uninterruptible;
 import com.oracle.svm.core.log.Log;
 import com.oracle.svm.core.option.HostedOptionKey;
+import com.oracle.svm.core.thread.Safepoint.SafepointException;
 import com.oracle.svm.core.thread.Safepoint.SafepointRequestValues;
 import com.oracle.svm.core.threadlocal.FastThreadLocalFactory;
 import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
@@ -65,6 +68,13 @@ class ThreadingSupportImpl implements ThreadingSupport {
      * adapt to a changing frequency of safepoint checks in the code that the thread executes.
      */
     static class RecurringCallbackTimer {
+        private static final RecurringCallbackAccess CALLBACK_ACCESS = new RecurringCallbackAccess() {
+            @Override
+            public void throwException(Throwable t) {
+                throw new SafepointException(t);
+            }
+        };
+
         /**
          * Weight of the newest sample in {@link #ewmaChecksPerNano}. Older samples have a total
          * weight of 1 - {@link #EWMA_LAMBDA}.
@@ -75,7 +85,7 @@ class ThreadingSupportImpl implements ThreadingSupport {
         private static final long MINIMUM_INTERVAL_NANOS = 1_000;
 
         private final long targetIntervalNanos;
-        private final Runnable callback;
+        private final RecurringCallback callback;
 
         private long requestedChecks;
         private double ewmaChecksPerNano;
@@ -84,7 +94,7 @@ class ThreadingSupportImpl implements ThreadingSupport {
 
         private volatile boolean isExecuting = false;
 
-        RecurringCallbackTimer(long targetIntervalNanos, Runnable callback) {
+        RecurringCallbackTimer(long targetIntervalNanos, RecurringCallback callback) {
             this.targetIntervalNanos = Math.max(MINIMUM_INTERVAL_NANOS, targetIntervalNanos);
             this.callback = callback;
 
@@ -100,9 +110,10 @@ class ThreadingSupportImpl implements ThreadingSupport {
                 return;
             }
             assert VMThreads.StatusSupport.isStatusJava();
+            final long unsignedIntMax = 0xffffffffL;
             isExecuting = true;
+            long now = 0;
             try {
-                final long unsignedIntMax = 0xffffffffL;
                 long elapsedNanos = timestamp - lastCapture;
                 long skippedChecks = value & unsignedIntMax; // interpret as unsigned
                 if (elapsedNanos > 0 && skippedChecks < requestedChecks) { // basic sanity
@@ -113,47 +124,58 @@ class ThreadingSupportImpl implements ThreadingSupport {
                         ewmaChecksPerNano = EWMA_LAMBDA * checksPerNano + (1 - EWMA_LAMBDA) * ewmaChecksPerNano;
                     }
                 }
-                long now = 0;
                 boolean expired = (timestamp >= nextDeadline); // avoid one nanoTime() call if true
                 if (!expired) {
                     now = System.nanoTime();
                     expired = (now >= nextDeadline);
                 }
                 if (expired) {
-                    invokeCallback();
-                    now = System.nanoTime();
-                    nextDeadline = now + targetIntervalNanos;
+                    try {
+                        invokeCallback();
+                        /*
+                         * Note that the callback is allowed to throw an exception (e.g., to stop or
+                         * interrupt long-running code). All code that must run to reinitialize the
+                         * recurring callback state must therefore be in a finally-block.
+                         */
+                    } finally {
+                        now = System.nanoTime();
+                        nextDeadline = now + targetIntervalNanos;
+                    }
                 }
+            } finally {
                 long remainingNanos = nextDeadline - now;
                 remainingNanos = (remainingNanos < MINIMUM_INTERVAL_NANOS) ? MINIMUM_INTERVAL_NANOS : remainingNanos;
                 double checks = ewmaChecksPerNano * remainingNanos;
                 requestedChecks = (checks > unsignedIntMax) ? unsignedIntMax : ((checks < 1L) ? 1L : (long) checks);
                 lastCapture = now;
                 Safepoint.setSafepointRequested((int) requestedChecks);
-            } catch (Throwable t) {
-                Log.log().string("Exception caught in recurring callback (ignored): ").object(t).newline();
-            } finally {
                 isExecuting = false;
             }
         }
 
         /**
-         * Separate method to invoke {@link #callback} so
+         * Separate method to invoke {@link #callback} so that
          * {@link #onSafepointCheckSlowpath(long, int)} can be strictly {@link Uninterruptible} and
          * allocation-free.
          */
         @Uninterruptible(reason = "Required by caller, but does not apply to callee.", calleeMustBe = false)
-        @MustNotAllocate(reason = "Callee may allocate", list = MustNotAllocate.WHITELIST)
+        @RestrictHeapAccess(reason = "Callee may allocate", access = RestrictHeapAccess.Access.UNRESTRICTED, overridesCallers = true)
         private void invokeCallback() {
-            Safepoint.setSafepointRequested(SafepointRequestValues.RESET);
-            callback.run();
+            try {
+                Safepoint.setSafepointRequested(SafepointRequestValues.RESET);
+                callback.run(CALLBACK_ACCESS);
+            } catch (SafepointException se) {
+                throw se;
+            } catch (Throwable t) {
+                Log.log().string("Exception caught in recurring callback (ignored): ").object(t).newline();
+            }
         }
     }
 
     private static final FastThreadLocalObject<RecurringCallbackTimer> activeTimer = FastThreadLocalFactory.createObject(RecurringCallbackTimer.class);
 
     @Override
-    public void registerRecurringCallback(long interval, TimeUnit unit, Runnable callback) {
+    public void registerRecurringCallback(long interval, TimeUnit unit, RecurringCallback callback) {
         if (callback != null) {
             UserError.guarantee(SubstrateOptions.MultiThreaded.getValue(), "Recurring callbacks are only supported in multi-threaded mode.");
             long intervalNanos = unit.toNanos(interval);
@@ -170,7 +192,7 @@ class ThreadingSupportImpl implements ThreadingSupport {
 
     /**
      * Callback from the safepoint check slow path.
-     * 
+     *
      * @param timestamp Time when the slow path was entered (before the execution of safepoint
      *            operations, if any), as reported by {@link System#nanoTime}.
      * @param value The value of {@code Safepoint.safepointRequested} when the slow path was entered
