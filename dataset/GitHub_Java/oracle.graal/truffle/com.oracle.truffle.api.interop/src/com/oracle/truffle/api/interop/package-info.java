d08cf071283009c621e6623f6388cafad3920b2b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 15:18:28 +0200

    interop API is stable

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.interop/src/com/oracle/truffle/api/interop/package-info.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.interop/src/com/oracle/truffle/api/interop/package-info.java
@@ -33,10 +33,8 @@
  * This package provides inter-operability between different
  * {@link com.oracle.truffle.api.TruffleLanguage Truffle languages}.
  * <p>
- * Languages can exchange primitive Java type wrapper objects (e.g., {@link java.lang.Byte},
- * {@link java.lang.Short}, {@link java.lang.Integer}, {@link java.lang.Long},
- * {@link java.lang.Float}, {@link java.lang.Double}, {@link java.lang.Character},
- * {@link java.lang.Boolean}, and {@link java.lang.String}) as well as any type implementing
+ * Languages can exchange primitive Java type wrapper objects (e.g., {@link java.lang.Integer},
+ * {@link java.lang.Double}, {@link java.lang.String}, etc) as well as any type implementing
  * {@link com.oracle.truffle.api.interop.TruffleObject}. Foreign objects are precisely those
  * implementing {@link com.oracle.truffle.api.interop.TruffleObject}.
  * <p>
@@ -47,3 +45,4 @@
  * {@link com.oracle.truffle.api.interop.ForeignAccess#execute}.
  */
 package com.oracle.truffle.api.interop;
+
