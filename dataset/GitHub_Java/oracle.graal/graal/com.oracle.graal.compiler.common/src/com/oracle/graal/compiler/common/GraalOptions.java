d3bd23ca12037d1576d840b9d138339562607bd5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 19:32:40 +0200

    disabling flow-sensitive reductions until their issues are addressed

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.common/src/com/oracle/graal/compiler/common/GraalOptions.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.common/src/com/oracle/graal/compiler/common/GraalOptions.java
@@ -150,8 +150,6 @@ public final class GraalOptions {
     public static final OptionValue<Integer> PrintIdealGraphPort = new OptionValue<>(4444);
     @Option(help = "")
     public static final OptionValue<Integer> PrintBinaryGraphPort = new OptionValue<>(4445);
-    @Option(help = "")
-    public static final OptionValue<Boolean> PrintIdealGraphSchedule = new OptionValue<>(false);
 
     // Other printing settings
     @Option(help = "")
@@ -176,7 +174,11 @@ public final class GraalOptions {
     public static final OptionValue<String> DecompileAfterPhase = new OptionValue<>(null);
 
     // HotSpot command line options
-    @Option(help = "Print inlining optimizations")
+    @Option(help = "")
+    public static final OptionValue<Boolean> HotSpotPrintCompilation = new OptionValue<>(false);
+    @Option(help = "")
+    public static final OptionValue<Boolean> HotSpotCIPrintCompilerName = new OptionValue<>(false);
+    @Option(help = "")
     public static final OptionValue<Boolean> HotSpotPrintInlining = new OptionValue<>(false);
 
     // Register allocator debugging
