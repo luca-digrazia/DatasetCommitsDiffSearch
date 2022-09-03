96ecd71242c0a607ffae00744b1849f697f1f066
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:24:45 +0200

    added Log.printf() for more flexibility in debugging snippets

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.api.code/src/com/oracle/graal/api/code/RuntimeCall.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.api.code/src/com/oracle/graal/api/code/RuntimeCall.java
@@ -27,32 +27,62 @@ import static com.oracle.graal.api.meta.Kind.*;
 import com.oracle.graal.api.meta.*;
 
 /**
- * Enumerates the calls that must be provided by the runtime system. The compiler
- * may generate code that calls the runtime services for unresolved and slow cases of some
- * bytecodes.
+ * Enumerates the calls that must be provided by the runtime system. The compiler may generate code that calls the
+ * runtime services for unresolved and slow cases of some bytecodes.
  */
-public enum RuntimeCall {
-    UnwindException(Void, Object),
-    Deoptimize(Void),
-    RegisterFinalizer(Void, Object),
-    SetDeoptInfo(Void, Object),
-    CreateNullPointerException(Object),
-    CreateOutOfBoundsException(Object, Int),
-    JavaTimeMillis(Long),
-    JavaTimeNanos(Long),
-    Debug(Void),
-    ArithmeticFrem(Float, Float, Float),
-    ArithmeticDrem(Double, Double, Double),
-    ArithmeticCos(Double, Double),
-    ArithmeticTan(Double, Double),
-    ArithmeticSin(Double, Double),
-    GenericCallback(Object, Object, Object);
+public class RuntimeCall {
 
-    public final Kind resultKind;
-    public final Kind[] arguments;
+    // TODO Move the singletons to projects where they are actually used. A couple of them
+    // are HotSpot-specific.
+    public static final RuntimeCall UnwindException = new RuntimeCall("UnwindException", Void, true, Object);
+    public static final RuntimeCall Deoptimize = new RuntimeCall("Deoptimize", Void, true);
+    public static final RuntimeCall RegisterFinalizer = new RuntimeCall("RegisterFinalizer", Void, true, Object);
+    public static final RuntimeCall SetDeoptInfo = new RuntimeCall("SetDeoptInfo", Void, true, Object);
+    public static final RuntimeCall CreateNullPointerException = new RuntimeCall("CreateNullPointerException", Object, true);
+    public static final RuntimeCall CreateOutOfBoundsException = new RuntimeCall("CreateOutOfBoundsException", Object, true, Int);
+    public static final RuntimeCall JavaTimeMillis = new RuntimeCall("JavaTimeMillis", Long, false);
+    public static final RuntimeCall JavaTimeNanos = new RuntimeCall("JavaTimeNanos", Long, false);
+    public static final RuntimeCall Debug = new RuntimeCall("Debug", Void, true);
+    public static final RuntimeCall ArithmeticFrem = new RuntimeCall("ArithmeticFrem", Float, false, Float, Float);
+    public static final RuntimeCall ArithmeticDrem = new RuntimeCall("ArithmeticDrem", Double, false, Double, Double);
+    public static final RuntimeCall ArithmeticCos = new RuntimeCall("ArithmeticCos", Double, false, Double);
+    public static final RuntimeCall ArithmeticTan = new RuntimeCall("ArithmeticTan", Double, false, Double);
+    public static final RuntimeCall ArithmeticSin = new RuntimeCall("ArithmeticSin", Double, false, Double);
+    public static final RuntimeCall GenericCallback = new RuntimeCall("GenericCallback", Object, true, Object, Object);
+    public static final RuntimeCall LogPrintf = new RuntimeCall("LogPrintf", Void, false, Object, Long);
+    public static final RuntimeCall LogPrimitive = new RuntimeCall("LogPrimitive", Void, false, Int, Long, Boolean);
+    public static final RuntimeCall LogObject = new RuntimeCall("LogObject", Void, false, Object, Int);
 
-    private RuntimeCall(Kind resultKind, Kind... args) {
+    private final String name;
+    private final Kind resultKind;
+    private final Kind[] argumentKinds;
+    private final boolean hasSideEffect;
+
+    public RuntimeCall(String name, Kind resultKind, boolean hasSideEffect, Kind... args) {
+        this.name = name;
         this.resultKind = resultKind;
-        this.arguments = args;
+        this.argumentKinds = args;
+        this.hasSideEffect = hasSideEffect;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Kind getResultKind() {
+        return resultKind;
+    }
+
+    public Kind[] getArgumentKinds() {
+        return argumentKinds;
+    }
+
+    public boolean hasSideEffect() {
+        return hasSideEffect;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.snippets/src/com/oracle/graal/snippets/Log.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.snippets/src/com/oracle/graal/snippets/Log.java
@@ -22,9 +22,7 @@
  */
 package com.oracle.graal.snippets;
 
-import java.io.*;
-
-import com.oracle.graal.api.code.RuntimeCallTarget.Descriptor;
+import com.oracle.graal.api.code.*;
 import com.oracle.graal.api.meta.*;
 import com.oracle.graal.graph.Node.ConstantNodeParameter;
 import com.oracle.graal.graph.Node.NodeIntrinsic;
@@ -33,67 +31,62 @@ import com.oracle.graal.nodes.extended.*;
 //JaCoCo Exclude
 
 /**
- * Provides {@link PrintStream}-like logging facility. This should only be used in {@linkplain Snippet snippets}.
+ * Provides printf-like debug facility. This should only be used in {@linkplain Snippet snippets}.
  */
 public final class Log {
-
-    public static final Descriptor LOG_PRIMITIVE = new Descriptor("logPrimitive", false, Kind.Void, Kind.Int, Kind.Long, Kind.Boolean);
-    public static final Descriptor LOG_OBJECT = new Descriptor("logObject", false, Kind.Void, Kind.Object, Kind.Int);
-    public static final Descriptor LOG_PRINTF = new Descriptor("logPrintf", false, Kind.Void, Kind.Object, Kind.Long, Kind.Long, Kind.Long);
 
     // Note: Must be kept in sync with constants in c1_Runtime1.hpp
     private static final int LOG_OBJECT_NEWLINE = 0x01;
     private static final int LOG_OBJECT_STRING  = 0x02;
     private static final int LOG_OBJECT_ADDRESS = 0x04;
 
+    @SuppressWarnings("unused")
     @NodeIntrinsic(RuntimeCallNode.class)
-    private static native void log(@ConstantNodeParameter Descriptor logObject, Object object, int flags);
+    private static void log(@ConstantNodeParameter RuntimeCall logObject, Object object, int flags) {
+        throw new UnsupportedOperationException("This method may only be compiled with the Graal compiler");
+    }
 
+    @SuppressWarnings("unused")
     @NodeIntrinsic(RuntimeCallNode.class)
-    private static native void log(@ConstantNodeParameter Descriptor logPrimitive, int typeChar, long value, boolean newline);
+    private static void log(@ConstantNodeParameter RuntimeCall logPrimitive, int typeChar, long value, boolean newline) {
+        throw new UnsupportedOperationException("This method may only be compiled with the Graal compiler");
+    }
 
+    @SuppressWarnings("unused")
     @NodeIntrinsic(RuntimeCallNode.class)
-    private static native void printf(@ConstantNodeParameter Descriptor logPrintf, String format, long v1, long v2, long v3);
+    private static void printf(@ConstantNodeParameter RuntimeCall logPrintf, String format, long value) {
+        throw new UnsupportedOperationException("This method may only be compiled with the Graal compiler");
+    }
 
     public static void print(boolean value) {
-        log(LOG_PRIMITIVE, Kind.Boolean.getTypeChar(), value ? 1L : 0L, false);
+        log(RuntimeCall.LogPrimitive, Kind.Boolean.typeChar, value ? 1L : 0L, false);
     }
 
     public static void print(byte value) {
-        log(LOG_PRIMITIVE, Kind.Byte.getTypeChar(), value, false);
+        log(RuntimeCall.LogPrimitive, Kind.Byte.typeChar, value, false);
     }
 
     public static void print(char value) {
-        log(LOG_PRIMITIVE, Kind.Char.getTypeChar(), value, false);
+        log(RuntimeCall.LogPrimitive, Kind.Char.typeChar, value, false);
     }
 
     public static void print(short value) {
-        log(LOG_PRIMITIVE, Kind.Short.getTypeChar(), value, false);
+        log(RuntimeCall.LogPrimitive, Kind.Short.typeChar, value, false);
     }
 
     public static void print(int value) {
-        log(LOG_PRIMITIVE, Kind.Int.getTypeChar(), value, false);
+        log(RuntimeCall.LogPrimitive, Kind.Int.typeChar, value, false);
     }
 
     public static void print(long value) {
-        log(LOG_PRIMITIVE, Kind.Long.getTypeChar(), value, false);
+        log(RuntimeCall.LogPrimitive, Kind.Long.typeChar, value, false);
     }
 
     /**
-     * Prints a formatted string to the log stream.
-     *
-     * @param format a C style printf format value that can contain at most one conversion specifier (i.e., a sequence
-     *            of characters starting with '%').
-     * @param value the value associated with the conversion specifier
+     * @param format a C style printf format value - must be between 0 and 1024 characters
      */
     public static void printf(String format, long value) {
-        printf(LOG_PRINTF, format, value, 0L, 0L);
-    }
-    public static void printf(String format, long v1, long v2) {
-        printf(LOG_PRINTF, format, v1, v2, 0L);
-    }
-    public static void printf(String format, long v1, long v2, long v3) {
-        printf(LOG_PRINTF, format, v1, v2, v3);
+        printf(RuntimeCall.LogPrintf, format, value);
     }
 
     public static void print(float value) {
@@ -104,7 +97,7 @@ public final class Log {
         } else if (value == Float.NEGATIVE_INFINITY) {
             print("-Infinity");
         } else {
-            log(LOG_PRIMITIVE, Kind.Float.getTypeChar(), Float.floatToRawIntBits(value), false);
+            log(RuntimeCall.LogPrimitive, Kind.Float.typeChar, Float.floatToRawIntBits(value), false);
         }
     }
 
@@ -116,44 +109,44 @@ public final class Log {
         } else if (value == Double.NEGATIVE_INFINITY) {
             print("-Infinity");
         } else {
-            log(LOG_PRIMITIVE, Kind.Double.getTypeChar(), Double.doubleToRawLongBits(value), false);
+            log(RuntimeCall.LogPrimitive, Kind.Double.typeChar, Double.doubleToRawLongBits(value), false);
         }
     }
 
     public static void print(String value) {
-        log(LOG_OBJECT, value, LOG_OBJECT_STRING);
+        log(RuntimeCall.LogObject, value, LOG_OBJECT_STRING);
     }
 
     public static void printAddress(Object o) {
-        log(LOG_OBJECT, o, LOG_OBJECT_ADDRESS);
+        log(RuntimeCall.LogObject, o, LOG_OBJECT_ADDRESS);
     }
 
     public static void printObject(Object o) {
-        log(LOG_OBJECT, o, 0);
+        log(RuntimeCall.LogObject, o, 0);
     }
 
     public static void println(boolean value) {
-        log(LOG_PRIMITIVE, Kind.Boolean.getTypeChar(), value ? 1L : 0L, true);
+        log(RuntimeCall.LogPrimitive, Kind.Boolean.typeChar, value ? 1L : 0L, true);
     }
 
     public static void println(byte value) {
-        log(LOG_PRIMITIVE, Kind.Byte.getTypeChar(), value, true);
+        log(RuntimeCall.LogPrimitive, Kind.Byte.typeChar, value, true);
     }
 
     public static void println(char value) {
-        log(LOG_PRIMITIVE, Kind.Char.getTypeChar(), value, true);
+        log(RuntimeCall.LogPrimitive, Kind.Char.typeChar, value, true);
     }
 
     public static void println(short value) {
-        log(LOG_PRIMITIVE, Kind.Short.getTypeChar(), value, true);
+        log(RuntimeCall.LogPrimitive, Kind.Short.typeChar, value, true);
     }
 
     public static void println(int value) {
-        log(LOG_PRIMITIVE, Kind.Int.getTypeChar(), value, true);
+        log(RuntimeCall.LogPrimitive, Kind.Int.typeChar, value, true);
     }
 
     public static void println(long value) {
-        log(LOG_PRIMITIVE, Kind.Long.getTypeChar(), value, true);
+        log(RuntimeCall.LogPrimitive, Kind.Long.typeChar, value, true);
     }
 
     public static void println(float value) {
@@ -164,7 +157,7 @@ public final class Log {
         } else if (value == Float.NEGATIVE_INFINITY) {
             println("-Infinity");
         } else {
-            log(LOG_PRIMITIVE, Kind.Float.getTypeChar(), Float.floatToRawIntBits(value), true);
+            log(RuntimeCall.LogPrimitive, Kind.Float.typeChar, Float.floatToRawIntBits(value), true);
         }
     }
 
@@ -176,20 +169,20 @@ public final class Log {
         } else if (value == Double.NEGATIVE_INFINITY) {
             println("-Infinity");
         } else {
-            log(LOG_PRIMITIVE, Kind.Double.getTypeChar(), Double.doubleToRawLongBits(value), true);
+            log(RuntimeCall.LogPrimitive, Kind.Double.typeChar, Double.doubleToRawLongBits(value), true);
         }
     }
 
     public static void println(String value) {
-        log(LOG_OBJECT, value, LOG_OBJECT_NEWLINE | LOG_OBJECT_STRING);
+        log(RuntimeCall.LogObject, value, LOG_OBJECT_NEWLINE | LOG_OBJECT_STRING);
     }
 
     public static void printlnAddress(Object o) {
-        log(LOG_OBJECT, o, LOG_OBJECT_NEWLINE | LOG_OBJECT_ADDRESS);
+        log(RuntimeCall.LogObject, o, LOG_OBJECT_NEWLINE | LOG_OBJECT_ADDRESS);
     }
 
     public static void printlnObject(Object o) {
-        log(LOG_OBJECT, o, LOG_OBJECT_NEWLINE);
+        log(RuntimeCall.LogObject, o, LOG_OBJECT_NEWLINE);
     }
 
     public static void println() {
