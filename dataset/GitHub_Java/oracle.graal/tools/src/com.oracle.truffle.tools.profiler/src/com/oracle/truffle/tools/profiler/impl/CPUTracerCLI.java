8b3f03ccbcc90503c2b73d260b9fa0ebe100ad83
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:47:36 +0200

    Do not use spaces in JSON keys.

--- dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.profiler/src/com/oracle/truffle/tools/profiler/impl/CPUTracerCLI.java
+++ dataset/GitHub_Java/oracle.graal/tools/src/com.oracle.truffle.tools.profiler/src/com/oracle/truffle/tools/profiler/impl/CPUTracerCLI.java
@@ -96,12 +96,6 @@ class CPUTracerCLI extends ProfilerCLI {
 
     private static void printTracerJson(PrintStream out, CPUTracer tracer) {
         JSONPrinter printer = new JSONPrinter(out);
-        printer.startObject();
-        printer.printKeyValue("tool", CPUTracerInstrument.ID);
-        printer.comma();
-        printer.printKeyValue("version", CPUTracerInstrument.VERSION);
-        printer.comma();
-        printer.printKey("profile");
         List<CPUTracer.Payload> payloads = new ArrayList<>(tracer.getPayloads());
         printer.startArray();
         int i = 0;
@@ -122,7 +116,6 @@ class CPUTracerCLI extends ProfilerCLI {
             }
         }
         printer.endArray();
-        printer.endObject();
     }
 
     static void printTracerHistogram(PrintStream out, CPUTracer tracer) {
