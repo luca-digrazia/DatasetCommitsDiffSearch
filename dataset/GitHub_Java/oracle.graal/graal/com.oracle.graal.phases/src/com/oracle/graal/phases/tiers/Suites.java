308b324ddb972f251403a81cd1f4af4e855fe1cb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 16:34:15 +0200

    Make sure new "economy" compiler configuration is not picked up as the default.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases/src/com/oracle/graal/phases/tiers/Suites.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases/src/com/oracle/graal/phases/tiers/Suites.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,15 +22,32 @@
  */
 package com.oracle.graal.phases.tiers;
 
-import com.oracle.graal.lir.phases.LIRSuites;
-import com.oracle.graal.phases.PhaseSuite;
+import static com.oracle.graal.phases.tiers.Suites.Options.*;
+
+import java.util.*;
+
+import com.oracle.graal.api.runtime.*;
+import com.oracle.graal.compiler.common.*;
+import com.oracle.graal.lir.phases.*;
+import com.oracle.graal.options.*;
+import com.oracle.graal.phases.*;
 
 public final class Suites {
+
+    static class Options {
+
+        // @formatter:off
+        @Option(help = "The compiler configuration to use", type = OptionType.User)
+        static final OptionValue<String> CompilerConfiguration = new OptionValue<>("");
+        // @formatter:on
+    }
 
     private final PhaseSuite<HighTierContext> highTier;
     private final PhaseSuite<MidTierContext> midTier;
     private final PhaseSuite<LowTierContext> lowTier;
-    private boolean immutable;
+
+    private static final CompilerConfiguration defaultConfiguration;
+    private static final Map<String, CompilerConfiguration> configurations;
 
     public PhaseSuite<HighTierContext> getHighTier() {
         return highTier;
@@ -44,34 +61,93 @@ public final class Suites {
         return lowTier;
     }
 
+    static {
+        configurations = new HashMap<>();
+        CompilerConfiguration basic = null;
+        CompilerConfiguration nonBasic = null;
+        int nonBasicCount = 0;
+
+        for (CompilerConfiguration config : Services.load(CompilerConfiguration.class)) {
+            String name = config.getClass().getSimpleName();
+            if (name.endsWith("CompilerConfiguration")) {
+                name = name.substring(0, name.length() - "CompilerConfiguration".length());
+            }
+            name = name.toLowerCase();
+
+            configurations.put(name, config);
+            if (name.equals("economy")) {
+                // ignore economy configuration if not explicitely specified
+            } else if (name.equals("basic")) {
+                assert basic == null;
+                basic = config;
+            } else {
+                nonBasic = config;
+                nonBasicCount++;
+            }
+        }
+
+        if (nonBasicCount == 1) {
+            /*
+             * There is exactly one non-basic configuration. We use this one as default.
+             */
+            defaultConfiguration = nonBasic;
+        } else {
+            /*
+             * There is either no extended configuration available, or more than one. In that case,
+             * default to "basic".
+             */
+            defaultConfiguration = basic;
+            if (defaultConfiguration == null) {
+                throw new GraalInternalError("unable to find basic compiler configuration");
+            }
+        }
+    }
+
     public Suites(PhaseSuite<HighTierContext> highTier, PhaseSuite<MidTierContext> midTier, PhaseSuite<LowTierContext> lowTier) {
         this.highTier = highTier;
         this.midTier = midTier;
         this.lowTier = lowTier;
     }
 
-    public static Suites createSuites(CompilerConfiguration config) {
-        return new Suites(config.createHighTier(), config.createMidTier(), config.createLowTier());
+    private Suites(CompilerConfiguration config) {
+        highTier = config.createHighTier();
+        midTier = config.createMidTier();
+        lowTier = config.createLowTier();
     }
 
-    public static LIRSuites createLIRSuites(CompilerConfiguration config) {
-        return new LIRSuites(config.createPreAllocationOptimizationStage(), config.createAllocationStage(), config.createPostAllocationOptimizationStage());
-    }
-
-    public boolean isImmutable() {
-        return immutable;
-    }
-
-    public synchronized void setImmutable() {
-        if (!immutable) {
-            highTier.setImmutable();
-            midTier.setImmutable();
-            lowTier.setImmutable();
-            immutable = true;
+    public static Suites createDefaultSuites() {
+        String selected = CompilerConfiguration.getValue();
+        if (selected.equals("")) {
+            return new Suites(defaultConfiguration);
+        } else {
+            return createSuites(selected);
         }
     }
 
-    public Suites copy() {
-        return new Suites(highTier.copy(), midTier.copy(), lowTier.copy());
+    public static Suites createSuites(String name) {
+        CompilerConfiguration config = configurations.get(name);
+        if (config == null) {
+            throw new GraalInternalError("unknown compiler configuration: " + name);
+        }
+        return new Suites(config);
     }
+
+    public static LIRSuites createDefaultLIRSuites() {
+        String selected = CompilerConfiguration.getValue();
+        if (selected.equals("")) {
+            return new LIRSuites(defaultConfiguration.createPreAllocationOptimizationStage(), defaultConfiguration.createAllocationStage(),
+                            defaultConfiguration.createPostAllocationOptimizationStage());
+        } else {
+            return createLIRSuites(selected);
+        }
+    }
+
+    public static LIRSuites createLIRSuites(String name) {
+        CompilerConfiguration config = configurations.get(name);
+        if (config == null) {
+            throw new GraalInternalError("unknown compiler configuration: " + name);
+        }
+        return new LIRSuites(config.createPreAllocationOptimizationStage(), config.createAllocationStage(), config.createPostAllocationOptimizationStage());
+    }
+
 }
