94dd7e3d1b4d93ec8b0ff8d275da8ef4219124b8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 20:21:03 +0200

    Fix corner case in stamp computation of zero extension.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.jtt/src/com/oracle/graal/jtt/optimize/ConvertCompare.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.jtt/src/com/oracle/graal/jtt/optimize/ConvertCompare.java
@@ -22,9 +22,8 @@
  */
 package com.oracle.graal.jtt.optimize;
 
-import org.junit.*;
-
 import com.oracle.graal.jtt.*;
+import org.junit.*;
 
 public class ConvertCompare extends JTTTest {
     public static boolean test(int a, float d) {
