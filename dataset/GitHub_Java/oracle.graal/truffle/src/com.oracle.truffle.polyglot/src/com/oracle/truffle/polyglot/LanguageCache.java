ab95a51bb60c02388178a916d13bb410ac91d424
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:08:46 +0200

    Document the need for suppressing JarFile caching

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/LanguageCache.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/LanguageCache.java
@@ -224,15 +224,6 @@ final class LanguageCache implements Comparable<LanguageCache> {
         if (loader == null) {
             return;
         }
-        try {
-            Class<?> truffleLanguageClassAsSeenByLoader = Class.forName(TruffleLanguage.class.getName(), true, loader);
-            if (truffleLanguageClassAsSeenByLoader != TruffleLanguage.class) {
-                return;
-            }
-        } catch (ClassNotFoundException ex) {
-            return;
-        }
-
         Enumeration<URL> en;
         try {
             en = loader.getResources("META-INF/truffle/language");
@@ -417,10 +408,8 @@ final class LanguageCache implements Comparable<LanguageCache> {
                         } else {
                             policy = loadedClass.getAnnotation(Registration.class).contextPolicy();
                         }
-                        @SuppressWarnings("rawtypes")
-                        Class<? extends TruffleLanguage> loadedLanguageClass = loadedClass.asSubclass(TruffleLanguage.class);
-                        languageClass = (Class<? extends TruffleLanguage<?>>) loadedLanguageClass;
-                    } catch (ClassNotFoundException e) {
+                        languageClass = (Class<? extends TruffleLanguage<?>>) loadedClass;
+                    } catch (Exception e) {
                         throw new IllegalStateException("Cannot load language " + name + ". Language implementation class " + className + " failed to load.", e);
                     }
                 }
