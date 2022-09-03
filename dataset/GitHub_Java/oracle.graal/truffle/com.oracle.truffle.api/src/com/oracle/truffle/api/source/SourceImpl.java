99ed47888d893ed9b542d658ae9b26a604787521
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:32:07 +0200

    Add missing TruffleBoundary to SourceImpl.equals

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/source/SourceImpl.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/source/SourceImpl.java
@@ -33,12 +33,7 @@ final class SourceImpl extends Source implements Cloneable {
     private static Ref SOURCES = null;
 
     SourceImpl(Content content) {
-        super(content, null);
-        registerSource(this);
-    }
-
-    SourceImpl(Content content, String mimeType) {
-        super(content, mimeType);
+        super(content);
         registerSource(this);
     }
 
