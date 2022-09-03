283cfdeb32a09826127f0f4b432bccacd1e09899
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 07:42:31 +0200

    Support INVOKE message on LibFFILibrary object

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.nfi.test/src/com/oracle/truffle/nfi/test/StringAsInterfaceNFITest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.nfi.test/src/com/oracle/truffle/nfi/test/StringAsInterfaceNFITest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,32 +24,24 @@
  */
 package com.oracle.truffle.nfi.test;
 
-import com.oracle.truffle.api.CallTarget;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import org.junit.Assume;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Test;
-
 import com.oracle.truffle.api.TruffleOptions;
 import com.oracle.truffle.api.interop.ForeignAccess;
 import com.oracle.truffle.api.interop.Message;
 import com.oracle.truffle.api.interop.TruffleObject;
-import com.oracle.truffle.api.interop.java.*;
+import com.oracle.truffle.api.interop.java.JavaInterop;
 import com.oracle.truffle.api.source.Source;
-import com.oracle.truffle.tck.TruffleRunner;
-import org.graalvm.polyglot.Context;
+import com.oracle.truffle.api.vm.PolyglotEngine;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-/**
- * This test is to be removed with the PolyglotEngine. Truffle NFI is currently no longer accessible
- * from the embedder API.
- */
-@SuppressWarnings("deprecation")
 public class StringAsInterfaceNFITest {
     private static StdLib stdlib;
-
-    @ClassRule public static TruffleRunner.RunWithPolyglotRule runWithPolyglot = new TruffleRunner.RunWithPolyglotRule(Context.newBuilder().allowNativeAccess(true));
+    private static PolyglotEngine engine;
 
     @BeforeClass
     public static void loadLibraries() {
@@ -58,13 +50,20 @@ public class StringAsInterfaceNFITest {
             return;
         }
 
-        CallTarget load = runWithPolyglot.getTruffleTestEnv().parse(Source.newBuilder("default {\n" + //
+        engine = PolyglotEngine.newBuilder().build();
+        stdlib = engine.eval(Source.newBuilder("default {\n" + //
                         "  strdup(string):string;\n" + //
                         "  malloc(UINT32):pointer;\n" + //
                         "  free(pointer):void;\n" + //
                         "}" //
-        ).name("(load default)").language("nfi").build());
-        stdlib = JavaInterop.asJavaObject(StdLib.class, (TruffleObject) load.call());
+        ).name("(load default)").mimeType("application/x-native").build()).as(StdLib.class);
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        if (engine != null) {
+            engine.dispose();
+        }
     }
 
     interface StdLib {
@@ -99,17 +98,18 @@ public class StringAsInterfaceNFITest {
         TruffleObject rawStdLib = JavaInterop.asTruffleObject(stdlib);
         Object mem = ForeignAccess.sendInvoke(Message.createInvoke(1).createNode(), rawStdLib, "malloc", 512);
         assertNotNull("some memory allocated", mem);
-        ForeignAccess.sendInvoke(Message.createInvoke(1).createNode(), rawStdLib, "free", mem);
+        Object res = ForeignAccess.sendInvoke(Message.createInvoke(1).createNode(), rawStdLib, "free", mem);
+        assertTrue("It is number", res instanceof Number);
+        assertEquals("Zero return code", 0, ((Number) res).intValue());
     }
 
     @Test
     public void canViewDefaultLibraryAsAnotherInterface() {
         Assume.assumeFalse("disable test on AOT", TruffleOptions.AOT);
-        CallTarget load = runWithPolyglot.getTruffleTestEnv().parse(Source.newBuilder("default {\n" + //
+        Strndup second = engine.eval(Source.newBuilder("default {\n" + //
                         "  strndup(string, UINT32):string;\n" + //
                         "}" //
-        ).name("(load default)").language("nfi").build());
-        Strndup second = JavaInterop.asJavaObject(Strndup.class, (TruffleObject) load.call());
+        ).name("(load default 2nd time)").mimeType("application/x-native").build()).as(Strndup.class);
 
         String copy = stdlib.strdup("Hello World!");
         String hello = second.strndup(copy, 5);
