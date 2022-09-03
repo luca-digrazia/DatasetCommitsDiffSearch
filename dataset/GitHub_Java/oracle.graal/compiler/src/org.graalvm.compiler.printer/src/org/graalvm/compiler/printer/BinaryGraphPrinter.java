de2d00ca802040152b87760d31e02ea61bed601d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:56:40 +0200

    Also revert the nameTemplate changes to switch back to IGV 1.0

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.printer/src/org/graalvm/compiler/printer/BinaryGraphPrinter.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.printer/src/org/graalvm/compiler/printer/BinaryGraphPrinter.java
@@ -175,7 +175,7 @@ public class BinaryGraphPrinter implements GraphPrinter {
     @Override
     public void print(Graph graph, Map<Object, Object> properties, int id, String format, Object... args) throws IOException {
         writeByte(BEGIN_GRAPH);
-        if (CURRENT_MAJOR_VERSION >= 3) {
+        if (CURRENT_MAJOR_VERSION == 3) {
             writeInt(id);
             writeString(format);
             writeInt(args.length);
@@ -183,7 +183,7 @@ public class BinaryGraphPrinter implements GraphPrinter {
                 writePropertyObject(a);
             }
         } else {
-            writePoolObject(formatTitle(id, format, args));
+            writePoolObject(String.format(format, args));
         }
         writeGraph(graph, properties);
         flush();
