ca8e051838df33fc94630136c0575b059a1a3859
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 16:10:00 +0200

    Add DebugMetric.isEnabled().

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.debug/src/com/oracle/graal/debug/internal/MetricImpl.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.debug/src/com/oracle/graal/debug/internal/MetricImpl.java
@@ -28,10 +28,8 @@ public final class MetricImpl extends DebugValue implements DebugMetric {
 
     public MetricImpl(String name, boolean conditional) {
         super(name, conditional);
-        if (isEnabled()) {
-            // Allows for zero-count metrics to be shown
-            getCurrentValue();
-        }
+        // Allows for zero-count metrics to be shown
+        getCurrentValue();
     }
 
     public void increment() {
