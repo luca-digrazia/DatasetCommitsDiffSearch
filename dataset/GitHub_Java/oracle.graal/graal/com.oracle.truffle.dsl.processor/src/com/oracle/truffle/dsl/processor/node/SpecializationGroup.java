732820803179ac7291937a4d38c35fc7339f2ae3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 21:39:00 +0200

    Truffle-DSL: fixed a bug in else connections of generic generated methods.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/node/SpecializationGroup.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/node/SpecializationGroup.java
@@ -119,11 +119,6 @@ public final class SpecializationGroup {
         if (previous == null || previous.getGuards().size() != elseConnectedGuards.size() + 1) {
             return null;
         }
-
-        if (elseConnectedGuards.contains(guard)) {
-            return guard;
-        }
-
         GuardData previousGuard = previous.getGuards().get(elseConnectedGuards.size());
         if (guard.getMethod().equals(previousGuard.getMethod()) && guard.isNegated() != previousGuard.isNegated()) {
             return guard;
@@ -217,7 +212,7 @@ public final class SpecializationGroup {
             int signatureIndex = 0;
             for (ActualParameter parameter : guardMatch.getParameters()) {
                 signatureIndex++;
-                if (!parameter.getSpecification().isSignature()) {
+                if (parameter.getSpecification().isSignature()) {
                     continue;
                 }
 
