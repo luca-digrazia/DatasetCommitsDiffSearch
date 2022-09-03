515fc757f199ff59c2c02e1c22c8a71feb21c749
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:28:47 +0200

    Fix the assignment of source and result aggregate in the insertvalue instruction

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.parser.factories/src/com/oracle/truffle/llvm/parser/factories/LLVMAggregateFactory.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.parser.factories/src/com/oracle/truffle/llvm/parser/factories/LLVMAggregateFactory.java
@@ -93,14 +93,6 @@ public final class LLVMAggregateFactory {
                 return LLVMExtractDoubleValueNodeGen.create(targetAddress);
             case X86_FP80:
                 return LLVMExtract80BitFloatValueNodeGen.create(targetAddress);
-            case I1_POINTER:
-            case I8_POINTER:
-            case I16_POINTER:
-            case I32_POINTER:
-            case I64_POINTER:
-            case HALF_POINTER:
-            case FLOAT_POINTER:
-            case DOUBLE_POINTER:
             case ADDRESS:
             case STRUCT:
                 return LLVMExtractAddressValueNodeGen.create(targetAddress);
@@ -168,14 +160,6 @@ public final class LLVMAggregateFactory {
                 } else {
                     return new LLVMCompoundStructWriteNode((LLVMAddressNode) parsedConstant, byteSize);
                 }
-            case I1_POINTER:
-            case I8_POINTER:
-            case I16_POINTER:
-            case I32_POINTER:
-            case I64_POINTER:
-            case HALF_POINTER:
-            case FLOAT_POINTER:
-            case DOUBLE_POINTER:
             case ADDRESS:
                 return new LLVMAddressStructWriteNode((LLVMAddressNode) parsedConstant);
             case FUNCTION_ADDRESS:
