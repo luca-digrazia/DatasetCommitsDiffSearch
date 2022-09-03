bef4f1f1141779b5b7945678fab6075fae8f74de
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:22:23 +0200

    Use lazily initialized ArrayList for Source-Scope Members

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/debug/scope/LLVMSourceLocation.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/debug/scope/LLVMSourceLocation.java
@@ -34,6 +34,7 @@ import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.api.source.SourceSection;
+import com.oracle.truffle.llvm.runtime.debug.LLVMSourceSymbol;
 
 import java.util.ArrayList;
 import java.util.Collections;
