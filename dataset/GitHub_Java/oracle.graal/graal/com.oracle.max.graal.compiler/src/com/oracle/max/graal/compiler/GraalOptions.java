34ea99be4dc8bfb03c217840d825e352c6cf5447
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 01:15:30 +0200

    Remove TraceLIRVisit

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/GraalOptions.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/GraalOptions.java
@@ -110,6 +110,7 @@ public final class GraalOptions {
     public static int     TraceLinearScanLevel               = 0;
     public static boolean TraceRegisterAllocation            = false;
     public static int     TraceLIRGeneratorLevel             = 0;
+    public static boolean TraceRelocation                    = ____;
     public static boolean TraceEscapeAnalysis                = ____;
     public static int     TraceBytecodeParserLevel           = 0;
     public static boolean ExitVMOnBailout                    = ____;
