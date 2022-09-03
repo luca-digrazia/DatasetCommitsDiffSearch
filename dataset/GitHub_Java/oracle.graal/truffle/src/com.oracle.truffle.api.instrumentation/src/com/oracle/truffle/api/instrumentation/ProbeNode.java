8326906c8488591df4aafb368f3f4859da5324c9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:15:55 +0200

    Fix invalid error message when index is verified.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/ProbeNode.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/ProbeNode.java
@@ -140,8 +140,7 @@ public final class ProbeNode extends Node {
     /**
      * Should get invoked after the node is invoked successfully.
      *
-     * @param result the result value of the operation, must be an interop type (i.e. either
-     *            implementing TruffleObject or be a primitive value), or <code>null</code>.
+     * @param result the result value of the operation
      * @param frame the current frame of the execution.
      * @since 0.12
      */
