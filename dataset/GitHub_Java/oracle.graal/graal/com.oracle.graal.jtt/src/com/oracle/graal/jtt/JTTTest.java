666d53e01f739250b714ee8eff138cf91422bb3d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:12:13 +0200

    Simplify parameter binding in JTT tests

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.jtt/src/com/oracle/graal/jtt/JTTTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.jtt/src/com/oracle/graal/jtt/JTTTest.java
@@ -35,7 +35,6 @@ import jdk.vm.ci.meta.ResolvedJavaMethod;
 
 import org.junit.Assert;
 
-import com.oracle.graal.compiler.common.CompilationIdentifier;
 import com.oracle.graal.compiler.test.GraalCompilerTest;
 import com.oracle.graal.nodes.ConstantNode;
 import com.oracle.graal.nodes.ParameterNode;
@@ -65,8 +64,8 @@ public class JTTTest extends GraalCompilerTest {
     }
 
     @Override
-    protected StructuredGraph parseEager(ResolvedJavaMethod m, AllowAssumptions allowAssumptions, CompilationIdentifier compilationId) {
-        StructuredGraph graph = super.parseEager(m, allowAssumptions, compilationId);
+    protected StructuredGraph parseEager(ResolvedJavaMethod m, AllowAssumptions allowAssumptions) {
+        StructuredGraph graph = super.parseEager(m, allowAssumptions);
         if (argsToBind != null) {
             Object receiver = isStatic(m.getModifiers()) ? null : this;
             Object[] args = argsWithReceiver(receiver, argsToBind);
