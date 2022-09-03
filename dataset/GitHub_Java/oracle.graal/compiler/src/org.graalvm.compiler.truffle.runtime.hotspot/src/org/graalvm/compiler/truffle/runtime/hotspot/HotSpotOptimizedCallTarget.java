877e6758ec7590bcbb92e763336d3d5f6189a364
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:23:43 +0200

    Change the compilation suffix, and improve documentation.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime.hotspot/src/org/graalvm/compiler/truffle/runtime/hotspot/HotSpotOptimizedCallTarget.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime.hotspot/src/org/graalvm/compiler/truffle/runtime/hotspot/HotSpotOptimizedCallTarget.java
@@ -26,6 +26,7 @@ package org.graalvm.compiler.truffle.runtime.hotspot;
 
 import org.graalvm.compiler.truffle.common.CompilableTruffleAST;
 import org.graalvm.compiler.truffle.common.OptimizedAssumptionDependency;
+import org.graalvm.compiler.truffle.common.TruffleCompilationTask;
 import org.graalvm.compiler.truffle.common.TruffleCompiler;
 import org.graalvm.compiler.truffle.runtime.OptimizedCallTarget;
 import org.graalvm.compiler.truffle.runtime.TruffleCallBoundary;
@@ -73,8 +74,7 @@ public class HotSpotOptimizedCallTarget extends OptimizedCallTarget implements O
         }
         invalidateCode();
         // A default nmethod can be called from entry points in the VM (e.g., Method::_code)
-        // and so allowing it to be installed here would invalidate the truth of
-        // `soleExecutionEntryPoint`
+        // and so allowing it to be installed here would invalidate the truth of `soleExecutionEntryPoint`
         if (code instanceof HotSpotNmethod) {
             HotSpotNmethod nmethod = (HotSpotNmethod) code;
             if (nmethod.isDefault()) {
