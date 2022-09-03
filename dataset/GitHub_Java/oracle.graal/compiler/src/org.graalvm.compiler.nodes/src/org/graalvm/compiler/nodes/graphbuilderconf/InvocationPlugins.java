00032cdc1e56fbcade75e8777726234079ea9dbd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:16:07 +0200

    account for parent and deferred registrations in InvocationPlugins.isEmpty

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/graphbuilderconf/InvocationPlugins.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/graphbuilderconf/InvocationPlugins.java
@@ -442,7 +442,6 @@ public class InvocationPlugins {
          *            {@code declaringClass}
          */
         public void register(InvocationPlugin plugin, String name, Type... argumentTypes) {
-            assert plugins != null : String.format("Late registrations of invocation plugins for %s is already closed", declaringType);
             boolean isStatic = argumentTypes.length == 0 || argumentTypes[0] != InvocationPlugin.Receiver.class;
             if (!isStatic) {
                 argumentTypes[0] = declaringType;
@@ -568,7 +567,7 @@ public class InvocationPlugins {
         /**
          * Maps method names to binding lists.
          */
-        final EconomicMap<String, Binding> bindings = EconomicMap.create(Equivalence.DEFAULT);
+        private final EconomicMap<String, Binding> bindings = EconomicMap.create(Equivalence.DEFAULT);
 
         /**
          * Gets the invocation plugin for a given method.
@@ -884,33 +883,19 @@ public class InvocationPlugins {
         flushDeferrables();
     }
 
-    /**
-     * Determines if this object currently contains any plugins (in any state of registration). If
-     * this object has any {@link #defer(Runnable) deferred registrations}, it is assumed that
-     * executing them will result in at least one plugin being registered.
-     */
     public boolean isEmpty() {
         if (parent != null && !parent.isEmpty()) {
             return false;
         }
         UnmodifiableEconomicMap<ResolvedJavaMethod, InvocationPlugin> resolvedRegs = resolvedRegistrations;
         if (resolvedRegs != null) {
-            if (!resolvedRegs.isEmpty()) {
-                return false;
-            }
+            return resolvedRegistrations.isEmpty();
         }
         List<Runnable> deferred = deferredRegistrations;
         if (deferred != null) {
-            if (!deferred.isEmpty()) {
-                return false;
-            }
+            return deferred.isEmpty();
         }
-        for (LateClassPlugins late = lateRegistrations; late != null; late = late.next) {
-            if (!late.bindings.isEmpty()) {
-                return false;
-            }
-        }
-        return registrations.size() == 0;
+        return registrations.size() == 0 && lateRegistrations == null;
     }
 
     /**
