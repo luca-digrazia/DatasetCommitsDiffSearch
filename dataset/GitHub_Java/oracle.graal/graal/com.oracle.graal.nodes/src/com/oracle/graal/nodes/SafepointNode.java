0e0b12b00aa15ed22cebebb087a39692db1807eb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 23:40:15 +0200

    made SafepointNode extensible (temp. solution until SafepointNodes are lowered via snippets)

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/SafepointNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/SafepointNode.java
@@ -22,7 +22,6 @@
  */
 package com.oracle.graal.nodes;
 
-import com.oracle.graal.api.meta.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.nodes.spi.*;
 import com.oracle.graal.nodes.type.*;
@@ -30,7 +29,7 @@ import com.oracle.graal.nodes.type.*;
 /**
  * Marks a position in the graph where a safepoint should be emitted.
  */
-public class SafepointNode extends DeoptimizingFixedWithNextNode implements LIRLowerable, Node.IterableNodeType {
+public class SafepointNode extends FixedWithNextNode implements LIRLowerable, Node.IterableNodeType {
 
     public SafepointNode() {
         this(StampFactory.forVoid());
@@ -43,15 +42,5 @@ public class SafepointNode extends DeoptimizingFixedWithNextNode implements LIRL
     @Override
     public void generate(LIRGeneratorTool gen) {
         gen.visitSafepointNode(this);
-    }
-
-    @Override
-    public DeoptimizationReason getDeoptimizationReason() {
-        return null;
-    }
-
-    @Override
-    public boolean canDeoptimize() {
-        return true;
     }
 }
