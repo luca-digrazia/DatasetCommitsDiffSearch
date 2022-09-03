03aac227f99b01c5251e7a7f6b0b8faeef1771ed
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:16:25 +0200

    Improve method substitution test

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements.test/src/com/oracle/graal/replacements/test/MethodSubstitutionTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements.test/src/com/oracle/graal/replacements/test/MethodSubstitutionTest.java
@@ -22,19 +22,27 @@
  */
 package com.oracle.graal.replacements.test;
 
-import static org.junit.Assert.*;
+import java.lang.reflect.InvocationTargetException;
 
-import java.util.concurrent.*;
+import com.oracle.graal.api.replacements.MethodSubstitution;
+import com.oracle.graal.compiler.test.GraalCompilerTest;
+import com.oracle.graal.debug.Debug;
+import com.oracle.graal.debug.Debug.Scope;
+import com.oracle.graal.graph.Node;
+import com.oracle.graal.nodes.Invoke;
+import com.oracle.graal.nodes.StructuredGraph;
+import com.oracle.graal.nodes.StructuredGraph.AllowAssumptions;
+import com.oracle.graal.nodes.spi.LoweringTool;
+import com.oracle.graal.phases.common.CanonicalizerPhase;
+import com.oracle.graal.phases.common.DeadCodeEliminationPhase;
+import com.oracle.graal.phases.common.LoweringPhase;
+import com.oracle.graal.phases.common.inlining.InliningPhase;
+import com.oracle.graal.phases.tiers.HighTierContext;
+import com.oracle.graal.replacements.nodes.MacroNode;
 
-import com.oracle.graal.api.code.*;
-import com.oracle.graal.api.replacements.*;
-import com.oracle.graal.compiler.test.*;
-import com.oracle.graal.debug.*;
-import com.oracle.graal.graph.*;
-import com.oracle.graal.nodes.*;
-import com.oracle.graal.phases.*;
-import com.oracle.graal.phases.common.*;
-import com.oracle.graal.phases.tiers.*;
+import jdk.vm.ci.code.InstalledCode;
+import jdk.vm.ci.code.InvalidInstalledCodeException;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
 
 /**
  * Tests if {@link MethodSubstitution}s are inlined correctly. Most test cases only assert that
@@ -43,25 +51,29 @@ import com.oracle.graal.phases.tiers.*;
  */
 public abstract class MethodSubstitutionTest extends GraalCompilerTest {
 
-    protected StructuredGraph test(final String snippet) {
-        return Debug.scope("MethodSubstitutionTest", runtime.lookupJavaMethod(getMethod(snippet)), new Callable<StructuredGraph>() {
-
-            @Override
-            public StructuredGraph call() {
-                StructuredGraph graph = parse(snippet);
-                PhasePlan phasePlan = getDefaultPhasePlan();
-                Assumptions assumptions = new Assumptions(true);
-                HighTierContext context = new HighTierContext(runtime(), assumptions, replacements, null, phasePlan, OptimisticOptimizations.ALL);
-                Debug.dump(graph, "Graph");
-                new InliningPhase().apply(graph, context);
-                Debug.dump(graph, "Graph");
-                new CanonicalizerPhase(true).apply(graph, context);
-                new DeadCodeEliminationPhase().apply(graph);
-
-                assertNotInGraph(graph, Invoke.class);
-                return graph;
+    @SuppressWarnings("try")
+    protected StructuredGraph testGraph(final String snippet) {
+        try (Scope s = Debug.scope("MethodSubstitutionTest", getResolvedJavaMethod(snippet))) {
+            StructuredGraph graph = parseEager(snippet, AllowAssumptions.YES);
+            HighTierContext context = getDefaultHighTierContext();
+            Debug.dump(Debug.BASIC_LOG_LEVEL, graph, "Graph");
+            new InliningPhase(new CanonicalizerPhase()).apply(graph, context);
+            Debug.dump(Debug.BASIC_LOG_LEVEL, graph, "Graph");
+            new CanonicalizerPhase().apply(graph, context);
+            new DeadCodeEliminationPhase().apply(graph);
+            // Try to ensure any macro nodes are lowered to expose any resulting invokes
+            if (graph.getNodes().filter(MacroNode.class).isNotEmpty()) {
+                new LoweringPhase(new CanonicalizerPhase(), LoweringTool.StandardLoweringStage.HIGH_TIER).apply(graph, context);
             }
-        });
+            if (graph.getNodes().filter(MacroNode.class).isNotEmpty()) {
+                new LoweringPhase(new CanonicalizerPhase(), LoweringTool.StandardLoweringStage.MID_TIER).apply(graph, context);
+            }
+            assertNotInGraph(graph, MacroNode.class);
+            assertNotInGraph(graph, Invoke.class);
+            return graph;
+        } catch (Throwable e) {
+            throw Debug.handle(e);
+        }
     }
 
     protected static StructuredGraph assertNotInGraph(StructuredGraph graph, Class<?> clazz) {
@@ -73,6 +85,32 @@ public abstract class MethodSubstitutionTest extends GraalCompilerTest {
         return graph;
     }
 
+    protected void testSubstitution(String testMethodName, Class<?> intrinsicClass, Class<?> holder, String methodName, Class<?>[] parameterTypes, boolean optional, Object[] args1, Object[] args2) {
+        ResolvedJavaMethod realMethod = getResolvedJavaMethod(holder, methodName, parameterTypes);
+        ResolvedJavaMethod testMethod = getResolvedJavaMethod(testMethodName);
+        StructuredGraph graph = testGraph(testMethodName);
+
+        // Check to see if the resulting graph contains the expected node
+        StructuredGraph replacement = getReplacements().getSubstitution(realMethod, -1);
+        if (replacement == null && !optional) {
+            assertInGraph(graph, intrinsicClass);
+        }
+
+        // Force compilation
+        InstalledCode code = getCode(testMethod);
+        assert optional || code != null;
+
+        for (int i = 0; i < args1.length; i++) {
+            Object arg1 = args1[i];
+            Object arg2 = args2[i];
+            Object expected = invokeSafe(realMethod, null, arg1, arg2);
+            // Verify that the original method and the substitution produce the same value
+            assertDeepEquals(expected, invokeSafe(testMethod, null, arg1, arg2));
+            // Verify that the generated code and the original produce the same value
+            assertDeepEquals(expected, executeVarargsSafe(code, arg1, arg2));
+        }
+    }
+
     protected static StructuredGraph assertInGraph(StructuredGraph graph, Class<?> clazz) {
         for (Node node : graph.getNodes()) {
             if (clazz.isInstance(node)) {
@@ -82,4 +120,21 @@ public abstract class MethodSubstitutionTest extends GraalCompilerTest {
         fail("Graph does not contain a node of class " + clazz.getName());
         return graph;
     }
+
+    protected static Object executeVarargsSafe(InstalledCode code, Object... args) {
+        try {
+            return code.executeVarargs(args);
+        } catch (InvalidInstalledCodeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected Object invokeSafe(ResolvedJavaMethod method, Object receiver, Object... args) {
+        try {
+            return invoke(method, receiver, args);
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
