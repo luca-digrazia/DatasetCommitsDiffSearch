778a01df7ede003f2aca21bd91610c6e57a6bc68
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 20:58:38 +0200

    HSAIL: used scoped option overriding for all GraalKernelTester based tests     Contributed-by: Tom Deneau <tom.deneau@amd.com>

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.hsail.test/src/com/oracle/graal/compiler/hsail/test/StringContainsAcceptTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.hsail.test/src/com/oracle/graal/compiler/hsail/test/StringContainsAcceptTest.java
@@ -23,9 +23,9 @@
 
 package com.oracle.graal.compiler.hsail.test;
 
-import static org.junit.Assume.*;
+import org.junit.Test;
 
-import org.junit.*;
+import static org.junit.Assume.*;
 
 /**
  * Tests codegen for String.contains() but with a wrapper method such as one would get in the
