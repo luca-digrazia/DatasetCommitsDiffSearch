ba45808143ed6a332d1f36499a4457e95c7e4cbe
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 17:22:58 +0200

    SL: make lookup of NodeInfo annotation more rebust.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.sl/src/com/oracle/truffle/sl/runtime/SLContext.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.sl/src/com/oracle/truffle/sl/runtime/SLContext.java
@@ -50,11 +50,11 @@ public final class SLContext extends ExecutionContext {
     private static final Layout LAYOUT = Layout.createLayout();
 
     private final BufferedReader input;
-    private final PrintWriter output;
+    private final PrintStream output;
     private final SLFunctionRegistry functionRegistry;
     private final Shape emptyShape;
 
-    public SLContext(BufferedReader input, PrintWriter output) {
+    public SLContext(BufferedReader input, PrintStream output) {
         this.input = input;
         this.output = output;
         this.functionRegistry = new SLFunctionRegistry();
@@ -80,7 +80,7 @@ public final class SLContext extends ExecutionContext {
      * The default default, i.e., the output for the {@link SLPrintlnBuiltin}. To allow unit
      * testing, we do not use {@link System#out} directly.
      */
-    public PrintWriter getOutput() {
+    public PrintStream getOutput() {
         return output;
     }
 
