9fed813deda65933c667f015ac82ae982973e26f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:35:34 +0200

    Send AS_POINTER directly on the result of TO_NATIVE instead of using recursion

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/impl/TypeConversion.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/impl/TypeConversion.java
@@ -1,49 +1,32 @@
 /*
- * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * The Universal Permissive License (UPL), Version 1.0
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * (a) the Software, and
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package com.oracle.truffle.nfi.impl;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
-import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.interop.ForeignAccess;
 import com.oracle.truffle.api.interop.Message;
@@ -56,11 +39,14 @@ import com.oracle.truffle.nfi.impl.TypeConversionFactory.AsStringNodeGen;
 
 abstract class TypeConversion extends Node {
 
+    protected static Node createIsNull() {
+        return Message.IS_NULL.createNode();
+    }
+
     protected static boolean checkNull(Node isNull, TruffleObject object) {
         return ForeignAccess.sendIsNull(isNull, object);
     }
 
-    @ImportStatic(Message.class)
     abstract static class AsPointerNode extends TypeConversion {
 
         abstract NativePointer execute(TruffleObject arg);
@@ -68,8 +54,8 @@ abstract class TypeConversion extends Node {
         @Specialization(guards = "checkIsPointer(isPointer, arg)")
         @SuppressWarnings("unused")
         protected NativePointer serializePointer(TruffleObject arg,
-                        @Cached("IS_POINTER.createNode()") Node isPointer,
-                        @Cached("AS_POINTER.createNode()") Node asPointer) {
+                        @Cached("createIsPointer()") Node isPointer,
+                        @Cached("createAsPointer()") Node asPointer) {
             try {
                 long pointer = ForeignAccess.sendAsPointer(asPointer, arg);
                 return new NativePointer(pointer);
@@ -81,16 +67,16 @@ abstract class TypeConversion extends Node {
 
         @Specialization(guards = "checkNull(isNull, arg)")
         @SuppressWarnings("unused")
-        NativePointer nullAsPointer(TruffleObject arg, @Cached("IS_NULL.createNode()") Node isNull) {
+        NativePointer nullAsPointer(TruffleObject arg, @Cached("createIsNull()") Node isNull) {
             return new NativePointer(0);
         }
 
         @Specialization(guards = "!checkNull(isNull, arg)", replaces = "serializePointer")
         @SuppressWarnings("unused")
         protected NativePointer transitionToNative(TruffleObject arg,
-                        @Cached("IS_NULL.createNode()") Node isNull,
-                        @Cached("TO_NATIVE.createNode()") Node toNative,
-                        @Cached("AS_POINTER.createNode()") Node asPointer) {
+                        @Cached("createIsNull()") Node isNull,
+                        @Cached("createToNative()") Node toNative,
+                        @Cached("createAsPointer()") Node asPointer) {
             try {
                 Object nativeObj = ForeignAccess.sendToNative(toNative, arg);
                 long pointer = ForeignAccess.sendAsPointer(asPointer, (TruffleObject) nativeObj);
@@ -105,12 +91,23 @@ abstract class TypeConversion extends Node {
             return ForeignAccess.sendIsPointer(isPointer, object);
         }
 
+        static Node createIsPointer() {
+            return Message.IS_POINTER.createNode();
+        }
+
+        static Node createAsPointer() {
+            return Message.AS_POINTER.createNode();
+        }
+
+        static Node createToNative() {
+            return Message.TO_NATIVE.createNode();
+        }
+
         static AsPointerNode createRecursive() {
             return AsPointerNodeGen.create();
         }
     }
 
-    @ImportStatic(Message.class)
     abstract static class AsStringNode extends TypeConversion {
 
         final boolean acceptAnything;
@@ -129,15 +126,15 @@ abstract class TypeConversion extends Node {
         @Specialization(guards = "checkNull(isNull, arg)")
         @SuppressWarnings("unused")
         String nullAsString(TruffleObject arg,
-                        @Cached("IS_NULL.createNode()") Node isNull) {
+                        @Cached("createIsNull()") Node isNull) {
             return null;
         }
 
         @Specialization(guards = "checkIsBoxed(isBoxed, arg)")
         @SuppressWarnings("unused")
         String boxedAsString(TruffleObject arg,
-                        @Cached("IS_BOXED.createNode()") Node isBoxed,
-                        @Cached("UNBOX.createNode()") Node unbox,
+                        @Cached("createIsBoxed()") Node isBoxed,
+                        @Cached("createUnbox()") Node unbox,
                         @Cached("createRecursive()") AsStringNode asString) {
             try {
                 Object unboxed = ForeignAccess.sendUnbox(unbox, arg);
@@ -152,6 +149,14 @@ abstract class TypeConversion extends Node {
         @TruffleBoundary
         String otherAsString(Object arg) {
             return arg.toString();
+        }
+
+        protected static Node createIsBoxed() {
+            return Message.IS_BOXED.createNode();
+        }
+
+        protected static Node createUnbox() {
+            return Message.UNBOX.createNode();
         }
 
         protected static boolean checkIsBoxed(Node isBoxed, TruffleObject arg) {
