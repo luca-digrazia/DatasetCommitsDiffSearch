0793ab494c5691e397c59e3765f555fabaff9d36
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 22:48:53 +0200

    Rename asAddress and make it protected.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.amd64/src/com/oracle/graal/compiler/amd64/AMD64LIRGenerator.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.amd64/src/com/oracle/graal/compiler/amd64/AMD64LIRGenerator.java
@@ -366,7 +366,9 @@ public abstract class AMD64LIRGenerator extends LIRGenerator {
     @Override
     public void emitNullCheck(ValueNode v, DeoptimizingNode deoping) {
         assert v.kind() == Kind.Object;
-        append(new AMD64Move.NullCheckOp(load(operand(v)), state(deoping)));
+        Variable obj = newVariable(Kind.Object);
+        emitMove(obj, operand(v));
+        append(new AMD64Move.NullCheckOp(obj, state(deoping)));
     }
 
     @Override
