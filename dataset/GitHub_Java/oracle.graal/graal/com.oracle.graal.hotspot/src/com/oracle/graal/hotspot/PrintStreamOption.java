494bb6c0bdb6874bce3e50ca90c1690a14abbbce
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:35:49 +0200

    use UniquePathUtilities.globalTimeStamp

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/PrintStreamOption.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/PrintStreamOption.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,10 +22,19 @@
  */
 package com.oracle.graal.hotspot;
 
-import java.io.*;
-import java.lang.management.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
-import com.oracle.graal.options.*;
+import com.oracle.graal.options.OptionValue;
+import com.oracle.graal.options.UniquePathUtilities;
+
+import jdk.vm.ci.hotspot.HotSpotJVMCIRuntime;
+import jdk.vm.ci.hotspot.HotSpotJVMCIRuntimeProvider;
 
 /**
  * An option that encapsulates and configures a print stream.
@@ -45,13 +54,17 @@ public class PrintStreamOption extends OptionValue<String> {
     private volatile PrintStream ps;
 
     /**
-     * Replace any instance of %p with a an identifying name. Try to get it from the RuntimeMXBean
-     * name.
-     * 
+     * Replaces any instance of %p with an identifying name such as a process ID extracted from
+     * {@link RuntimeMXBean#getName()} and any instance of %t with the value of
+     * {@link System#currentTimeMillis()}.
+     *
      * @return the name of the file to log to
      */
     private String getFilename() {
         String name = getValue();
+        if (name.contains("%t")) {
+            name = name.replaceAll("%t", String.valueOf(UniquePathUtilities.getGlobalTimeStamp()));
+        }
         if (name.contains("%p")) {
             String runtimeName = ManagementFactory.getRuntimeMXBean().getName();
             try {
@@ -69,7 +82,49 @@ public class PrintStreamOption extends OptionValue<String> {
     }
 
     /**
-     * Gets the print stream configured by this option.
+     * An output stream that redirects to {@link HotSpotJVMCIRuntimeProvider#getLogStream()}. The
+     * {@link HotSpotJVMCIRuntimeProvider#getLogStream()} value is only accessed the first time an
+     * IO operation is performed on the stream. This is required to break a deadlock in early JVMCI
+     * initialization.
+     */
+    static class DelayedOutputStream extends OutputStream {
+        private volatile OutputStream lazy;
+
+        private OutputStream lazy() {
+            if (lazy == null) {
+                synchronized (this) {
+                    if (lazy == null) {
+                        lazy = HotSpotJVMCIRuntime.runtime().getLogStream();
+                    }
+                }
+            }
+            return lazy;
+        }
+
+        @Override
+        public void write(byte[] b, int off, int len) throws IOException {
+            lazy().write(b, off, len);
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            lazy().write(b);
+        }
+
+        @Override
+        public void flush() throws IOException {
+            lazy().flush();
+        }
+
+        @Override
+        public void close() throws IOException {
+            lazy().close();
+        }
+    }
+
+    /**
+     * Gets the print stream configured by this option. If no file is configured, the print stream
+     * will output to HotSpot's {@link HotSpotJVMCIRuntimeProvider#getLogStream() log} stream.
      */
     public PrintStream getStream() {
         if (ps == null) {
@@ -79,13 +134,22 @@ public class PrintStreamOption extends OptionValue<String> {
                         try {
                             final boolean enableAutoflush = true;
                             ps = new PrintStream(new FileOutputStream(getFilename()), enableAutoflush);
+                            /*
+                             * Add the JVM and Java arguments to the log file to help identity it.
+                             */
+                            String inputArguments = String.join(" ", ManagementFactory.getRuntimeMXBean().getInputArguments());
+                            ps.println("VM Arguments: " + inputArguments);
+                            String cmd = System.getProperty("sun.java.command");
+                            if (cmd != null) {
+                                ps.println("sun.java.command=" + cmd);
+                            }
                         } catch (FileNotFoundException e) {
                             throw new RuntimeException("couldn't open file: " + getValue(), e);
                         }
                     }
                 }
             } else {
-                ps = System.out;
+                ps = new PrintStream(new DelayedOutputStream());
             }
         }
         return ps;
--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.options/src/com/oracle/graal/options/UniquePathUtilities.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.options/src/com/oracle/graal/options/UniquePathUtilities.java
@@ -25,11 +25,11 @@ package com.oracle.graal.options;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class UniquePathUtilities {
 
-    private static final long globalTimeStamp = System.currentTimeMillis();
-
+    private static final AtomicLong globalTimeStamp = new AtomicLong();
     /**
      * This generates a per thread persistent id to aid mapping related dump files with each other.
      */
@@ -52,6 +52,13 @@ public class UniquePathUtilities {
         return "." + ext;
     }
 
+    public static long getGlobalTimeStamp() {
+        if (globalTimeStamp.get() == 0) {
+            globalTimeStamp.compareAndSet(0, System.currentTimeMillis());
+        }
+        return globalTimeStamp.get();
+    }
+
     /**
      * Generate a {@link Path} using the format "%s-%d_%d%s" with the {@link OptionValue#getValue()
      * base filename}, a {@link #globalTimeStamp global timestamp}, {@link #getThreadDumpId a per
@@ -60,10 +67,27 @@ public class UniquePathUtilities {
      * @return the output file path or null if the flag is null
      */
     public static Path getPath(OptionValue<String> option, OptionValue<String> defaultDirectory, String extension) {
+        return getPath(option, defaultDirectory, extension, true);
+    }
+
+    /**
+     * Generate a {@link Path} using the format "%s-%d_%s" with the {@link OptionValue#getValue()
+     * base filename}, a {@link #globalTimeStamp global timestamp} and an optional {@code extension}
+     * .
+     *
+     * @return the output file path or null if the flag is null
+     */
+    public static Path getPathGlobal(OptionValue<String> option, OptionValue<String> defaultDirectory, String extension) {
+        return getPath(option, defaultDirectory, extension, false);
+    }
+
+    private static Path getPath(OptionValue<String> option, OptionValue<String> defaultDirectory, String extension, boolean includeThreadId) {
         if (option.getValue() == null) {
             return null;
         }
-        String name = String.format("%s-%d_%d%s", option.getValue(), globalTimeStamp, getThreadDumpId(), formatExtension(extension));
+        final String name = includeThreadId
+                        ? String.format("%s-%d_%d%s", option.getValue(), getGlobalTimeStamp(), getThreadDumpId(), formatExtension(extension))
+                        : String.format("%s-%d%s", option.getValue(), getGlobalTimeStamp(), formatExtension(extension));
         Path result = Paths.get(name);
         if (result.isAbsolute() || defaultDirectory == null) {
             return result;
