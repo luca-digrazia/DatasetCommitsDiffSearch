6b22e4cbb1165d888ef5514ef57ef19835a8ebbb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 16:41:56 +0200

    Add test timeout for long if cascade test.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.truffle.test/src/com/oracle/graal/truffle/test/BytecodeInterpreterPartialEvaluationTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.truffle.test/src/com/oracle/graal/truffle/test/BytecodeInterpreterPartialEvaluationTest.java
@@ -41,11 +41,15 @@ public class BytecodeInterpreterPartialEvaluationTest extends PartialEvaluationT
 
     public static class Program extends RootNode {
         @CompilationFinal final byte[] bytecodes;
+        private final int maxStack;
+        private final int maxLocals;
         @CompilationFinal private final FrameSlot[] locals;
         @CompilationFinal private final FrameSlot[] stack;
 
         public Program(byte[] bytecodes, int maxLocals, int maxStack) {
             this.bytecodes = bytecodes;
+            this.maxLocals = maxLocals;
+            this.maxStack = maxStack;
             locals = new FrameSlot[maxLocals];
             stack = new FrameSlot[maxStack];
             for (int i = 0; i < maxLocals; ++i) {
@@ -76,7 +80,7 @@ public class BytecodeInterpreterPartialEvaluationTest extends PartialEvaluationT
         }
 
         @Override
-        @ExplodeLoop(merge = true)
+        @ExplodeLoop
         public Object execute(VirtualFrame frame) {
             int topOfStack = -1;
             int bci = 0;
