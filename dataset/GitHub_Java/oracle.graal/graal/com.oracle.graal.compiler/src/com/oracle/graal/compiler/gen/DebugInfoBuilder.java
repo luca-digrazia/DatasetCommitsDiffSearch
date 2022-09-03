a133b0fdad6570f28fc7722c3cca66bf42b907f2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:56:41 +0200

    Allow VirtualPhis in FrameStates, now the Virtual state in a LoopBegin's FrameState is the VirtualLoopPhi

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/gen/DebugInfoBuilder.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/gen/DebugInfoBuilder.java
@@ -25,39 +25,46 @@ package com.oracle.graal.compiler.gen;
 import java.util.*;
 import java.util.Map.Entry;
 
-import com.oracle.graal.api.code.*;
-import com.oracle.graal.api.meta.*;
+import com.oracle.max.cri.ci.*;
 import com.oracle.graal.compiler.gen.LIRGenerator.LockScope;
 import com.oracle.graal.debug.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.lir.*;
 import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.PhiNode.PhiType;
 import com.oracle.graal.nodes.virtual.*;
 
 public class DebugInfoBuilder {
-    private final NodeMap<Value> nodeOperands;
+    private final NodeMap<CiValue> nodeOperands;
 
-    public DebugInfoBuilder(NodeMap<Value> nodeOperands) {
+    public DebugInfoBuilder(NodeMap<CiValue> nodeOperands) {
         this.nodeOperands = nodeOperands;
     }
 
 
-    private HashMap<VirtualObjectNode, VirtualObject> virtualObjects = new HashMap<>();
+    private HashMap<VirtualObjectNode, CiVirtualObject> virtualObjects = new HashMap<>();
 
-    public LIRDebugInfo build(FrameState topState, LockScope locks, List<StackSlot> pointerSlots, LabelRef exceptionEdge, long leafGraphId) {
+    public LIRDebugInfo build(FrameState topState, LockScope locks, List<CiStackSlot> pointerSlots, LabelRef exceptionEdge, long leafGraphId) {
         assert virtualObjects.size() == 0;
-        BytecodeFrame frame = computeFrameForState(topState, locks, leafGraphId);
+        CiFrame frame = computeFrameForState(topState, locks, leafGraphId);
 
-        VirtualObject[] virtualObjectsArray = null;
+        CiVirtualObject[] virtualObjectsArray = null;
         if (virtualObjects.size() != 0) {
             // collect all VirtualObjectField instances:
-            IdentityHashMap<VirtualObjectNode, VirtualObjectState> objectStates = new IdentityHashMap<>();
+            IdentityHashMap<VirtualObjectNode, VirtualObjectFieldNode> objectStates = new IdentityHashMap<>();
             FrameState current = topState;
             do {
-                for (VirtualObjectState state : current.virtualObjectMappings()) {
+                for (Node n : current.virtualObjectMappings()) {
+                    Node p = n;
+                    while (p instanceof PhiNode) {
+                        PhiNode phi = (PhiNode) p;
+                        assert phi.type() == PhiType.Virtual;
+                        p = phi.valueAt(0);
+                    }
+                    VirtualObjectFieldNode field = (VirtualObjectFieldNode) p;
                     // null states occur for objects with 0 fields
-                    if (!objectStates.containsKey(state.object())) {
-                        objectStates.put(state.object(), state);
+                    if (field != null && !objectStates.containsKey(field.object())) {
+                        objectStates.put(field.object(), field);
                     }
                 }
                 current = current.outerFrameState();
@@ -67,42 +74,51 @@ public class DebugInfoBuilder {
             boolean changed;
             do {
                 changed = false;
-                IdentityHashMap<VirtualObjectNode, VirtualObject> virtualObjectsCopy = new IdentityHashMap<>(virtualObjects);
-                for (Entry<VirtualObjectNode, VirtualObject> entry : virtualObjectsCopy.entrySet()) {
+                IdentityHashMap<VirtualObjectNode, CiVirtualObject> virtualObjectsCopy = new IdentityHashMap<>(virtualObjects);
+                for (Entry<VirtualObjectNode, CiVirtualObject> entry : virtualObjectsCopy.entrySet()) {
                     if (entry.getValue().values() == null) {
                         VirtualObjectNode vobj = entry.getKey();
                         if (vobj instanceof BoxedVirtualObjectNode) {
                             BoxedVirtualObjectNode boxedVirtualObjectNode = (BoxedVirtualObjectNode) vobj;
-                            entry.getValue().setValues(new Value[]{toCiValue(boxedVirtualObjectNode.getUnboxedValue())});
+                            entry.getValue().setValues(new CiValue[]{toCiValue(boxedVirtualObjectNode.getUnboxedValue())});
                         } else {
-                            Value[] values = new Value[vobj.fieldsCount()];
+                            CiValue[] values = new CiValue[vobj.fieldsCount()];
                             entry.getValue().setValues(values);
                             if (values.length > 0) {
                                 changed = true;
-                                VirtualObjectState currentField = objectStates.get(vobj);
+                                ValueNode currentField = objectStates.get(vobj);
                                 assert currentField != null;
-                                for (int i = 0; i < vobj.fieldsCount(); i++) {
-                                    values[i] = toCiValue(currentField.fields().get(i));
-                                }
+                                do {
+                                    if (currentField instanceof VirtualObjectFieldNode) {
+                                        int index = ((VirtualObjectFieldNode) currentField).index();
+                                        if (values[index] == null) {
+                                            values[index] = toCiValue(((VirtualObjectFieldNode) currentField).input());
+                                        }
+                                        currentField = ((VirtualObjectFieldNode) currentField).lastState();
+                                    } else {
+                                        assert currentField instanceof PhiNode : currentField;
+                                        currentField = ((PhiNode) currentField).valueAt(0);
+                                    }
+                                } while (currentField != null);
                             }
                         }
                     }
                 }
             } while (changed);
 
-            virtualObjectsArray = virtualObjects.values().toArray(new VirtualObject[virtualObjects.size()]);
+            virtualObjectsArray = virtualObjects.values().toArray(new CiVirtualObject[virtualObjects.size()]);
             virtualObjects.clear();
         }
 
         return new LIRDebugInfo(frame, virtualObjectsArray, pointerSlots, exceptionEdge);
     }
 
-    private BytecodeFrame computeFrameForState(FrameState state, LockScope locks, long leafGraphId) {
+    private CiFrame computeFrameForState(FrameState state, LockScope locks, long leafGraphId) {
         int numLocals = state.localsSize();
         int numStack = state.stackSize();
         int numLocks = (locks != null && locks.callerState == state.outerFrameState()) ? locks.stateDepth + 1 : 0;
 
-        Value[] values = new Value[numLocals + numStack + numLocks];
+        CiValue[] values = new CiValue[numLocals + numStack + numLocks];
         for (int i = 0; i < numLocals; i++) {
             values[i] = toCiValue(state.localAt(i));
         }
@@ -114,33 +130,33 @@ public class DebugInfoBuilder {
         for (int i = numLocks - 1; i >= 0; i--) {
             assert locks != null && nextLock.callerState == state.outerFrameState() && nextLock.stateDepth == i;
 
-            Value owner = toCiValue(nextLock.monitor.object());
-            Value lockData = nextLock.lockData;
+            CiValue owner = toCiValue(nextLock.monitor.object());
+            CiValue lockData = nextLock.lockData;
             boolean eliminated = nextLock.monitor.eliminated();
-            values[numLocals + numStack + nextLock.stateDepth] = new MonitorValue(owner, lockData, eliminated);
+            values[numLocals + numStack + nextLock.stateDepth] = new CiMonitorValue(owner, lockData, eliminated);
 
             nextLock = nextLock.outer;
         }
 
-        BytecodeFrame caller = null;
+        CiFrame caller = null;
         if (state.outerFrameState() != null) {
             caller = computeFrameForState(state.outerFrameState(), nextLock, -1);
         } else {
             if (nextLock != null) {
-                throw new BailoutException("unbalanced monitors: found monitor for unknown frame");
+                throw new CiBailout("unbalanced monitors: found monitor for unknown frame");
             }
         }
         assert state.bci >= 0 || state.bci == FrameState.BEFORE_BCI;
-        BytecodeFrame frame = new BytecodeFrame(caller, state.method(), state.bci, state.rethrowException(), state.duringCall(), values, state.localsSize(), state.stackSize(), numLocks, leafGraphId);
+        CiFrame frame = new CiFrame(caller, state.method(), state.bci, state.rethrowException(), state.duringCall(), values, state.localsSize(), state.stackSize(), numLocks, leafGraphId);
         return frame;
     }
 
-    private Value toCiValue(ValueNode value) {
+    private CiValue toCiValue(ValueNode value) {
         if (value instanceof VirtualObjectNode) {
             VirtualObjectNode obj = (VirtualObjectNode) value;
-            VirtualObject ciObj = virtualObjects.get(value);
+            CiVirtualObject ciObj = virtualObjects.get(value);
             if (ciObj == null) {
-                ciObj = VirtualObject.get(obj.type(), null, virtualObjects.size());
+                ciObj = CiVirtualObject.get(obj.type(), null, virtualObjects.size());
                 virtualObjects.put(obj, ciObj);
             }
             Debug.metric("StateVirtualObjects").increment();
@@ -152,14 +168,14 @@ public class DebugInfoBuilder {
 
         } else if (value != null) {
             Debug.metric("StateVariables").increment();
-            Value operand = nodeOperands.get(value);
-            assert operand != null && (operand instanceof Variable || operand instanceof Constant);
+            CiValue operand = nodeOperands.get(value);
+            assert operand != null && (operand instanceof Variable || operand instanceof CiConstant);
             return operand;
 
         } else {
             // return a dummy value because real value not needed
             Debug.metric("StateIllegals").increment();
-            return Value.IllegalValue;
+            return CiValue.IllegalValue;
         }
     }
 }
