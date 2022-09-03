7eb2955ab730ba1607cdd86e6aeecce8718b220c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:43:40 +0200

    Improve message printing.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotExceptionImpl.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotExceptionImpl.java
@@ -115,14 +115,8 @@ final class PolyglotExceptionImpl extends AbstractExceptionImpl implements VMObj
             this.guestObject = languageContext.context.getHostContext().nullValue;
         }
 
-        if (!isInternalError() && guestObject != languageContext.context.getHostContext().nullValue) {
-            String msg;
-            try {
-                msg = getGuestObject().toString();
-            } catch (PolyglotException e) {
-                msg = exception.getMessage();
-            }
-            this.message = msg;
+        if (!getGuestObject().isNull() && !isInternalError()) {
+            this.message = getGuestObject().toString();
         } else if (isHostException()) {
             this.message = asHostException().getMessage();
         } else {
