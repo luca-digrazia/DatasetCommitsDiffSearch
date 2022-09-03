25070851fd81ce0e1c4e61cb371ba8458afb669c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:06:36 +0200

    require compiler configuration owning module to be non-automatic

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/meta/HotSpotInvocationPlugins.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/meta/HotSpotInvocationPlugins.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -24,9 +22,15 @@
  */
 package org.graalvm.compiler.hotspot.meta;
 
-import java.lang.reflect.Type;
+import static org.graalvm.compiler.serviceprovider.JDK9Method.Java8OrEarlier;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.reflect.Type;
+import java.util.Set;
+
+import org.graalvm.collections.EconomicSet;
 import org.graalvm.compiler.core.common.GraalOptions;
+import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.graph.Node;
 import org.graalvm.compiler.graph.iterators.NodeIterable;
 import org.graalvm.compiler.hotspot.GraalHotSpotVMConfig;
@@ -39,6 +43,7 @@ import org.graalvm.compiler.nodes.graphbuilderconf.InvocationPlugins;
 import org.graalvm.compiler.nodes.type.StampTool;
 import org.graalvm.compiler.phases.tiers.CompilerConfiguration;
 import org.graalvm.compiler.replacements.nodes.MacroNode;
+import org.graalvm.compiler.serviceprovider.JDK9Method;
 
 import jdk.vm.ci.hotspot.HotSpotResolvedJavaType;
 import jdk.vm.ci.meta.JavaKind;
@@ -49,22 +54,29 @@ import jdk.vm.ci.meta.ResolvedJavaType;
  */
 final class HotSpotInvocationPlugins extends InvocationPlugins {
     private final GraalHotSpotVMConfig config;
-    private final IntrinsificationPredicate intrinsificationPredicate;
+    private final EconomicSet<Object> trustedModules;
+    private final ClassLoader extLoader;
 
     HotSpotInvocationPlugins(GraalHotSpotVMConfig config, CompilerConfiguration compilerConfiguration) {
         this.config = config;
-        intrinsificationPredicate = new IntrinsificationPredicate(compilerConfiguration);
+        if (Java8OrEarlier) {
+            extLoader = getExtLoader();
+            trustedModules = null;
+        } else {
+            extLoader = null;
+            trustedModules = initTrustedModules(compilerConfiguration);
+        }
     }
 
     @Override
-    protected void register(InvocationPlugin plugin, boolean isOptional, boolean allowOverwrite, Type declaringClass, String name, Type... argumentTypes) {
+    public void register(InvocationPlugin plugin, Type declaringClass, String name, Type... argumentTypes) {
         if (!config.usePopCountInstruction) {
             if (name.equals("bitCount")) {
                 assert declaringClass.equals(Integer.class) || declaringClass.equals(Long.class);
                 return;
             }
         }
-        super.register(plugin, isOptional, allowOverwrite, declaringClass, name, argumentTypes);
+        super.register(plugin, declaringClass, name, argumentTypes);
     }
 
     @Override
@@ -102,12 +114,79 @@ final class HotSpotInvocationPlugins extends InvocationPlugins {
         return type != null && "Ljava/lang/Class;".equals(type.getName());
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * On JDK 8, only classes loaded by the boot, JVMCI or extension class loaders are trusted.
+     *
+     * On JDK 9 and later, only classes in the {@link CompilerConfiguration} defining module or any
+     * of its module dependencies are trusted.
+     */
     @Override
     public boolean canBeIntrinsified(ResolvedJavaType declaringClass) {
         if (declaringClass instanceof HotSpotResolvedJavaType) {
-            HotSpotResolvedJavaType type = (HotSpotResolvedJavaType) declaringClass;
-            return intrinsificationPredicate.apply(type.mirror());
+            Class<?> javaClass = ((HotSpotResolvedJavaType) declaringClass).mirror();
+            if (Java8OrEarlier) {
+                ClassLoader cl = javaClass.getClassLoader();
+                return cl == null || cl == getClass().getClassLoader() || cl == extLoader;
+            } else {
+                Object module = JDK9Method.getModule(javaClass);
+                return trustedModules.contains(module);
+            }
         }
         return false;
+    }
+
+    private static ClassLoader getExtLoader() {
+        try {
+            Object launcher = Class.forName("sun.misc.Launcher").getMethod("getLauncher").invoke(null);
+            ClassLoader appLoader = (ClassLoader) launcher.getClass().getMethod("getClassLoader").invoke(launcher);
+            ClassLoader extLoader = appLoader.getParent();
+            assert extLoader.getClass().getName().equals("sun.misc.Launcher$ExtClassLoader") : extLoader;
+            return extLoader;
+        } catch (Exception e) {
+            throw new GraalError(e);
+        }
+    }
+
+    /**
+     * Gets the set of modules whose methods can be intrinsified. This set is the module owning the
+     * class of {@code compilerConfiguration} and all its dependencies.
+     */
+    private static EconomicSet<Object> initTrustedModules(CompilerConfiguration compilerConfiguration) throws GraalError {
+        try {
+            EconomicSet<Object> res = EconomicSet.create();
+            Object compilerConfigurationModule = JDK9Method.getModule(compilerConfiguration.getClass());
+            res.add(compilerConfigurationModule);
+            Class<?> moduleClass = compilerConfigurationModule.getClass();
+            Object layer = JDK9Method.lookupMethodHandle(moduleClass, "getLayer").invoke(compilerConfigurationModule);
+            Class<? extends Object> layerClass = layer.getClass();
+            MethodHandle getName = JDK9Method.lookupMethodHandle(moduleClass, "getName");
+            Set<Object> modules = (Set<Object>) JDK9Method.lookupMethodHandle(layerClass, "modules").invoke(layer);
+            Object descriptor = JDK9Method.lookupMethodHandle(moduleClass, "getDescriptor").invoke(compilerConfigurationModule);
+            Class<?> moduleDescriptorClass = descriptor.getClass();
+            Set<Object> requires = (Set<Object>) JDK9Method.lookupMethodHandle(moduleDescriptorClass, "requires").invoke(descriptor);
+            boolean isAutomatic = (Boolean) JDK9Method.lookupMethodHandle(moduleDescriptorClass, "isAutomatic").invoke(descriptor);
+            if (isAutomatic) {
+                throw new IllegalArgumentException(String.format("The module '%s' defining the Graal compiler configuration class '%s' must not be an automatic module",
+                                getName.invoke(compilerConfigurationModule), compilerConfiguration.getClass().getName()));
+            }
+            MethodHandle requireNameGetter = null;
+            for (Object require : requires) {
+                if (requireNameGetter == null) {
+                    requireNameGetter = JDK9Method.lookupMethodHandle(require.getClass(), "name");
+                }
+                String name = (String) requireNameGetter.invoke(require);
+                for (Object module : modules) {
+                    String moduleName = (String) getName.invoke(module);
+                    if (moduleName.equals(name)) {
+                        res.add(module);
+                    }
+                }
+            }
+            return res;
+        } catch (Throwable e) {
+            throw new GraalError(e);
+        }
     }
 }
