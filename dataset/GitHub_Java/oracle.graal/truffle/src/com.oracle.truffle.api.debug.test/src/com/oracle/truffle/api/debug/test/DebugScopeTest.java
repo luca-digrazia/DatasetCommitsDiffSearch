716f962a811ba0e339fd07ac6e3966eb786d33f2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:46:24 +0200

    [GR-7786] Provide arguments when they are defined as a list.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.debug.test/src/com/oracle/truffle/api/debug/test/DebugScopeTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.debug.test/src/com/oracle/truffle/api/debug/test/DebugScopeTest.java
@@ -24,16 +24,12 @@
  */
 package com.oracle.truffle.api.debug.test;
 
+import java.util.Iterator;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
-import java.util.Iterator;
-
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Source;
-import org.graalvm.polyglot.Value;
 import org.junit.Test;
 
 import com.oracle.truffle.api.debug.DebugScope;
@@ -41,7 +37,10 @@ import com.oracle.truffle.api.debug.DebugValue;
 import com.oracle.truffle.api.debug.Debugger;
 import com.oracle.truffle.api.debug.DebuggerSession;
 import com.oracle.truffle.api.debug.SuspendedEvent;
-import com.oracle.truffle.api.instrumentation.test.InstrumentationTestLanguage;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
 
 public class DebugScopeTest extends AbstractDebugTest {
 
@@ -86,7 +85,7 @@ public class DebugScopeTest extends AbstractDebugTest {
                         "))\n");
         Context context = Context.create();
         context.eval(source);
-        Value functionValue = context.getBindings(InstrumentationTestLanguage.ID).getMember("function");
+        Value functionValue = context.importSymbol("function");
         assertNotNull(functionValue);
         Debugger debugger = context.getEngine().getInstruments().get("debugger").lookup(Debugger.class);
 
