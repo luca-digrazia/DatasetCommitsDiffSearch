ba8470c4f6669fd4db7391a3f30ff39606502e4e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:15:35 +0200

    Fix javadoc in Tag.Identifier.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/Tag.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/Tag.java
@@ -37,7 +37,8 @@ import com.oracle.truffle.api.instrumentation.InstrumentationHandler.AccessorIns
 import com.oracle.truffle.api.nodes.LanguageInfo;
 
 /**
- * Base class for tags used in the Truffle instrumentation framework.
+ * Base class for tags. A tag subclass can be used to {@link InstrumentableNode#hasTag(Class)} mark
+ * AST nodes can be .
  *
  * @see StandardTags For the standard set of tags
  * @see #findProvidedTag(LanguageInfo, String) to find a provided tag of a language
