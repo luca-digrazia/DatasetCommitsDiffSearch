8c763f1cb13b3533857ae6c4ff1df4f87d9ea745
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:19:21 +0200

    Load libraries differently in SVM.

--- dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/NativeLibrary.java
+++ dataset/GitHub_Java/oracle.graal/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/NativeLibrary.java
@@ -41,7 +41,7 @@ public class NativeLibrary {
         // On SVM no need to use dlmopen backend.
         // Prepend "with dlmopen " in HotSpot.
         StringBuilder sb = new StringBuilder();
-        if (!ImageInfo.inImageCode()) {
+        if (!ImageInfo.inImageRuntimeCode()) {
             sb.append("with dlmopen ");
         }
         sb.append("load(RTLD_LAZY) '").append(lib).append('\'');
