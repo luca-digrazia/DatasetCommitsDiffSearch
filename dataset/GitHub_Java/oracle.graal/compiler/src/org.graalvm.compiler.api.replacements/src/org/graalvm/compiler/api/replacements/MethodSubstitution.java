66883d03cd7d3e5a15ea5226980799713299b09b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 07:02:01 +0200

    Add documentation to partial intrinsic.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.api.replacements/src/org/graalvm/compiler/api/replacements/MethodSubstitution.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.api.replacements/src/org/graalvm/compiler/api/replacements/MethodSubstitution.java
@@ -26,7 +26,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.reflect.Array;
 
 import jdk.vm.ci.meta.Signature;
 
