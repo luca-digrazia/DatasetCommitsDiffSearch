ad243871b6bfc201d53ba139fe94f46159802672
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:43:39 +0200

    Fix a few style errors.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir.aarch64/src/com/oracle/graal/lir/aarch64/AArch64Unary.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir.aarch64/src/com/oracle/graal/lir/aarch64/AArch64Unary.java
@@ -74,11 +74,10 @@ public class AArch64Unary {
             }
             AArch64Address address = input.toAddress();
             Register dst = asRegister(result);
-            if (isSigned) {
+            if (isSigned)
                 masm.ldrs(targetSize, srcSize, dst, address);
-            } else {
+            else
                 masm.ldr(srcSize, dst, address);
-            }
         }
 
         @Override
