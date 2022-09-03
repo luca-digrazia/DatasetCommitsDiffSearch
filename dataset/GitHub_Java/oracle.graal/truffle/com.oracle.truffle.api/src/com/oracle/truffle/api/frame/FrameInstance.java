ef0184eb74a9f495be7b2771d889f6d6a642e1ab
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:13:58 +0200

    Fix eclipseformat error.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/frame/FrameInstance.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/frame/FrameInstance.java
@@ -24,23 +24,112 @@
  */
 package com.oracle.truffle.api.frame;
 
-import com.oracle.truffle.api.*;
-import com.oracle.truffle.api.nodes.*;
+import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.TruffleRuntime;
+import com.oracle.truffle.api.nodes.Node;
 
+/**
+ * @since 0.8 or earlier
+ */
 public interface FrameInstance {
-
-    public static enum FrameAccess {
+    /**
+     * Access mode for {@link FrameInstance#getFrame(FrameAccess)}.
+     *
+     * @see FrameInstance#getFrame(FrameAccess)
+     * @since 0.8 or earlier
+     */
+    enum FrameAccess {
+        /**
+         * @since 0.8 or earlier
+         * @deprecated without replacement. This mode always returns <code>null</code>.
+         **/
+        @Deprecated //
         NONE,
+
+        /**
+         * This mode allows to read the frame and provides read only access to its local variables.
+         * The returned frame must not be stored/persisted. Writing local variables in this mode
+         * will result in an {@link AssertionError} only if assertions (-ea) are enabled.
+         *
+         * @since 0.8 or earlier
+         */
         READ_ONLY,
+
+        /**
+         * This mode allows to read the frame and provides read and write access to its local
+         * variables. The returned frame must not be stored/persisted.
+         *
+         * @since 0.8 or earlier
+         **/
         READ_WRITE,
+        /**
+         * This mode allows to read a materialized version of the frame and provides read and write
+         * access to its local variables. In addition to {@link #READ_WRITE} this mode allows to
+         * store/persist the returned frame.
+         *
+         * @since 0.8 or earlier
+         **/
         MATERIALIZE
     }
 
-    Frame getFrame(FrameAccess access, boolean slowPath);
+    /**
+     *
+     * @since 0.8 or earlier
+     * @deprecated use {@link #getFrame(FrameAccess)} instead. It is equivalent to
+     *             <code>FrameInstance.getFrame(access, true)</code>.
+     */
+    @Deprecated
+    default Frame getFrame(FrameAccess access, @SuppressWarnings("unused") boolean slowPath) {
+        return getFrame(access);
+    }
 
+    /**
+     * Accesses the underlying frame using a specified {@link FrameAccess access mode}.
+     *
+     * @see FrameAccess
+     * @since 0.23
+     */
+    default Frame getFrame(FrameAccess access) {
+        return getFrame(access, true);
+    }
+
+    /** @since 0.8 or earlier */
     boolean isVirtualFrame();
 
+    /**
+     * Returns a node representing the callsite of the next new target on the stack.
+     *
+     * This picture indicates how {@link FrameInstance} groups the stack.
+     *
+     * <pre>
+     *                      ===============
+     *  {@link TruffleRuntime#getCurrentFrame() Current}:         ,>|  CallTarget   | FrameInstance
+     *                   |  ===============
+     *  {@link TruffleRuntime#getCallerFrame() Caller}:          '-|  CallNode     | FrameInstance
+     *                   ,>|  CallTarget   |
+     *                   |  ===============
+     *                   '-|  CallNode     | FrameInstance
+     *                     |  CallTarget   |
+     *                      ===============
+     *                           ...
+     *                      ===============
+     *                     |  CallNode     | FrameInstance
+     * Initial call:       |  CallTarget   |
+     *                      ===============
+     *
+     * </pre>
+     *
+     * @return a node representing the callsite of the next new target on the stack. Null in case
+     *         there is no upper target or if the target was not invoked using a
+     *         {@link TruffleRuntime#createDirectCallNode(CallTarget) direct} or
+     *         {@link TruffleRuntime#createIndirectCallNode() indirect} call node.
+     *
+     * @since 0.8 or earlier
+     **/
     Node getCallNode();
 
+    /**
+     * @since 0.8 or earlier
+     **/
     CallTarget getCallTarget();
 }
