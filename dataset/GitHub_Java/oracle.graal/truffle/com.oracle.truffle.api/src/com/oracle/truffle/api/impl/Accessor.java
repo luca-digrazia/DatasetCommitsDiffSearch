fe3ea94bca52284c17573f2db9cc075935ee62e6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 15:32:19 +0200

    Allows debugging and executing any project's unittest from inside of NetBeans. Added --attach localhost:8000 option to allow the JVM to attach to IDE's (and any other) JPDA server.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/Accessor.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/Accessor.java
@@ -31,8 +31,6 @@ import java.util.*;
 import com.oracle.truffle.api.*;
 import com.oracle.truffle.api.debug.*;
 import com.oracle.truffle.api.instrument.*;
-import com.oracle.truffle.api.nodes.Node;
-import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.source.*;
 import com.oracle.truffle.api.vm.*;
 
@@ -42,13 +40,13 @@ import com.oracle.truffle.api.vm.*;
 public abstract class Accessor {
     private static Accessor API;
     private static Accessor SPI;
-    private static Accessor NODES;
-    private static Accessor INSTRUMENT;
-    private static Accessor DEBUG;
-    private static final ThreadLocal<TruffleVM> CURRENT_VM = new ThreadLocal<>();
-
     static {
         TruffleLanguage lng = new TruffleLanguage(null) {
+            @Override
+            protected Object eval(Source code) throws IOException {
+                return null;
+            }
+
             @Override
             protected Object findExportedSymbol(String globalName, boolean onlyExplicit) {
                 return null;
@@ -73,21 +71,8 @@ public abstract class Accessor {
             protected DebugSupportProvider getDebugSupport() {
                 return null;
             }
-
-            @Override
-            protected CallTarget parse(Source code, Node context, String... argumentNames) throws IOException {
-                throw new IOException();
-            }
         };
         lng.hashCode();
-        new Node(null) {
-        }.getRootNode();
-
-        try {
-            Class.forName(Debugger.class.getName(), true, Debugger.class.getClassLoader());
-        } catch (ClassNotFoundException ex) {
-            throw new IllegalStateException(ex);
-        }
     }
 
     protected Accessor() {
@@ -96,21 +81,6 @@ public abstract class Accessor {
                 throw new IllegalStateException();
             }
             API = this;
-        } else if (this.getClass().getSimpleName().endsWith("Nodes")) {
-            if (NODES != null) {
-                throw new IllegalStateException();
-            }
-            NODES = this;
-        } else if (this.getClass().getSimpleName().endsWith("Instrument")) {
-            if (INSTRUMENT != null) {
-                throw new IllegalStateException();
-            }
-            INSTRUMENT = this;
-        } else if (this.getClass().getSimpleName().endsWith("Debug")) {
-            if (DEBUG != null) {
-                throw new IllegalStateException();
-            }
-            DEBUG = this;
         } else {
             if (SPI != null) {
                 throw new IllegalStateException();
@@ -147,52 +117,16 @@ public abstract class Accessor {
         return API.getDebugSupport(l);
     }
 
-    protected Object invoke(TruffleLanguage lang, Object obj, Object[] args) throws IOException {
+    protected Object invoke(Object obj, Object[] args) throws IOException {
         for (SymbolInvoker si : ServiceLoader.load(SymbolInvoker.class)) {
-            return si.invoke(lang, obj, args);
+            return si.invoke(obj, args);
         }
         throw new IOException("No symbol invoker found!");
     }
 
-    protected Class<? extends TruffleLanguage> findLanguage(RootNode n) {
-        return NODES.findLanguage(n);
-    }
-
-    protected Class<? extends TruffleLanguage> findLanguage(Probe probe) {
-        return INSTRUMENT.findLanguage(probe);
-    }
-
-    protected TruffleLanguage findLanguage(TruffleVM vm, Class<? extends TruffleLanguage> languageClass) {
-        if (vm == null) {
-            vm = CURRENT_VM.get();
-            if (vm == null) {
-                throw new IllegalStateException();
-            }
-        }
-        return SPI.findLanguage(vm, languageClass);
-    }
-
-    protected Closeable executionStart(TruffleVM vm, Debugger[] fillIn, Source s) {
-        final Closeable debugClose = DEBUG.executionStart(vm, fillIn, s);
-        final TruffleVM prev = CURRENT_VM.get();
-        CURRENT_VM.set(vm);
-        class ContextCloseable implements Closeable {
-            @Override
-            public void close() throws IOException {
-                CURRENT_VM.set(prev);
-                debugClose.close();
-            }
-        }
-        return new ContextCloseable();
-    }
-
-    protected void dispatchEvent(TruffleVM vm, Object event) {
-        SPI.dispatchEvent(vm, event);
-    }
-
     /**
      * Don't call me. I am here only to let NetBeans debug any Truffle project.
-     *
+     * 
      * @param args
      */
     public static void main(String... args) {
