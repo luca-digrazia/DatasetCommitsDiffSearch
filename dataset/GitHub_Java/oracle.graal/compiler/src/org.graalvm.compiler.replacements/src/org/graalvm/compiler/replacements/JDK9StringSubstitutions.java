440de1f55af85e0cd256c43e70608602d785e12a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:27:04 +0200

    Add indexOf-related intrinsics for Strings on JDK9 and later.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/JDK9StringSubstitutions.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/JDK9StringSubstitutions.java
@@ -34,10 +34,7 @@ import org.graalvm.compiler.replacements.nodes.ArrayEqualsNode;
 // JaCoCo Exclude
 
 /**
- * Substitutions for {@link String} methods for JDK9 and later.
- *
- * {@link String} changed in JDK9 to contain a byte array instead of a char array. We therefore need
- * new substitutions for the related methods.
+ * Substitutions for {@link String} methods.
  */
 @ClassSubstitution(String.class)
 public class JDK9StringSubstitutions {
