2189019f1964185d4aa6739be12c177ddb468a09
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 23:43:04 +0200

    Use MoveOp marker interface in AllocatorTest.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.test/src/com/oracle/graal/compiler/test/backend/AllocatorTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.test/src/com/oracle/graal/compiler/test/backend/AllocatorTest.java
@@ -41,7 +41,36 @@ import com.oracle.graal.phases.*;
 
 public class AllocatorTest extends GraalCompilerTest {
 
-    protected void test(String snippet, final int expectedRegisters, final int expectedRegRegMoves, final int expectedSpillMoves) {
+    @Test
+    public void test1() {
+        test("test1snippet", 2, 1, 0);
+    }
+
+    public static long test1snippet(long x) {
+        return x + 5;
+    }
+
+    @Ignore
+    @Test
+    public void test2() {
+        test("test2snippet", 2, 0, 0);
+    }
+
+    public static long test2snippet(long x) {
+        return x * 5;
+    }
+
+    @Ignore
+    @Test
+    public void test3() {
+        test("test3snippet", 4, 1, 0);
+    }
+
+    public static long test3snippet(long x) {
+        return x / 3 + x % 3;
+    }
+
+    private void test(String snippet, final int expectedRegisters, final int expectedRegRegMoves, final int expectedSpillMoves) {
         final StructuredGraph graph = parse(snippet);
         Debug.scope("AllocatorTest", new Object[]{graph, graph.method(), backend.target}, new Runnable() {
 
@@ -62,7 +91,7 @@ public class AllocatorTest extends GraalCompilerTest {
         });
     }
 
-    private class RegisterStats {
+    class RegisterStats {
 
         public final LIR lir;
         public HashSet<Register> registers = new HashSet<>();
