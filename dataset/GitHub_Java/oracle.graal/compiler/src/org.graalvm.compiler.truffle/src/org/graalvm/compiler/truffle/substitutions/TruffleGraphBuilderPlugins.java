5745aaa24e45140abde22491823608936391e371
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:19:55 +0200

    Registered reflectively accessed fields

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle/src/org/graalvm/compiler/truffle/substitutions/TruffleGraphBuilderPlugins.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle/src/org/graalvm/compiler/truffle/substitutions/TruffleGraphBuilderPlugins.java
@@ -414,7 +414,7 @@ public class TruffleGraphBuilderPlugins {
         });
     }
 
-    public static void registerOptimizedCallTargetPlugins(InvocationPlugins plugins, boolean canDelayIntrinsification, KnownTruffleFields knownFields) {
+    public static void registerOptimizedCallTargetPlugins(InvocationPlugins plugins, boolean canDelayIntrinsification, KnownTruffleFields knownTruffleFields) {
         Registration r = new Registration(plugins, OptimizedCallTarget.class);
         r.register2("createFrame", FrameDescriptor.class, Object[].class, new InvocationPlugin() {
             @Override
@@ -426,8 +426,10 @@ public class TruffleGraphBuilderPlugins {
                     throw b.bailout("Parameter 'descriptor' is not a compile-time constant");
                 }
 
-                ValueNode nonNullArguments = b.add(PiNode.create(args, StampFactory.objectNonNull(StampTool.typeReferenceOrNull(args))));
-                b.addPush(JavaKind.Object, new NewFrameNode(b, descriptor, nonNullArguments, knownFields));
+                final ValueNode nonNullArguments = b.add(PiNode.create(args, StampFactory.objectNonNull(StampTool.typeReferenceOrNull(args))));
+                final NewFrameNode node = new NewFrameNode(knownTruffleFields, b.getMetaAccess(), b.getConstantReflection(), b.getGraph(),
+                                knownTruffleFields.classFrameClass, descriptor, nonNullArguments);
+                b.addPush(JavaKind.Object, node);
                 return true;
             }
         });
