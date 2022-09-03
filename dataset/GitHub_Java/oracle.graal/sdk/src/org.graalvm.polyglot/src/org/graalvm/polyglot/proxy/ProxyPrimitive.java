a798f8d174022bd25fab2860120e69d429daf4bd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:51:56 +0200

    Update javadoc.

--- dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/proxy/ProxyPrimitive.java
+++ dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/proxy/ProxyPrimitive.java
@@ -24,11 +24,30 @@
  */
 package org.graalvm.polyglot.proxy;
 
+import org.graalvm.polyglot.PolyglotException;
+
+/**
+ * Interface to be implemented to mimic lazy primitive values. The primitive value is
+ * {@link #asPrimitive() computed} lazily when accessed by the guest language. Valid primitive
+ * values are {@link String}, {@link Byte}, {@link Character}, {@link Short}, {@link Integer},
+ * {@link Long}, {@link Float} and {@link Double}.
+ *
+ * @see #asPrimitive()
+ * @see Proxy
+ * @since 1.0
+ */
+@FunctionalInterface
 public interface ProxyPrimitive extends Proxy {
 
     /**
-     * Unboxes the proxy to a primitive value. A primitive value can either be String, byte, char,
-     * short, int, long, float and double.
+     * Unboxes the proxy to a primitive value. The primitive value is {@link #asPrimitive()
+     * computed} lazily when accessed by the guest language. Valid primitive values are
+     * {@link String}, {@link Byte}, {@link Character}, {@link Short}, {@link Integer}, {@link Long}
+     * , {@link Float} and {@link Double}. If the returned type is invalid then a
+     * {@link IllegalStateException} {@link PolyglotException#isHostException() host exception} is
+     * thrown to the guest language.
+     *
+     * @since 1.0
      */
     Object asPrimitive();
 
