06ceec835e7bd4568d36655e800e401972d282a2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 22:37:31 +0200

    CR-806: Changes to build Graal for SPARC

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot.sparc/src/com/oracle/graal/hotspot/sparc/SPARCHotSpotGraalRuntime.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot.sparc/src/com/oracle/graal/hotspot/sparc/SPARCHotSpotGraalRuntime.java
@@ -53,8 +53,7 @@ final class SPARCHotSpotGraalRuntime extends HotSpotGraalRuntime {
     protected TargetDescription createTarget() {
         final int stackFrameAlignment = 16;
         final int implicitNullCheckLimit = 4096;
-        final boolean inlineObjects = false;  // TODO We might want to change this later.
-        return new TargetDescription(createArchitecture(), true, stackFrameAlignment, implicitNullCheckLimit, inlineObjects);
+        return new TargetDescription(createArchitecture(), true, stackFrameAlignment, implicitNullCheckLimit, true);
     }
 
     @Override
