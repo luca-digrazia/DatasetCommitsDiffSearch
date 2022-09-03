4f269e602c422cbb33d7cd2b116c00d8e052b60c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 22:15:53 +0200

    Add scopes to capture the graphs in ReplacementsImpl

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/ReplacementsImpl.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/ReplacementsImpl.java
@@ -123,15 +123,12 @@ public class ReplacementsImpl implements Replacements {
 
             int modifiers = substituteMethod.getModifiers();
             if (!Modifier.isStatic(modifiers)) {
-                throw new GraalInternalError("Substitution methods must be static: " + substituteMethod);
+                throw new RuntimeException("Substitution methods must be static: " + substituteMethod);
             }
 
             if (methodSubstitution != null) {
-                if (macroSubstitution != null && macroSubstitution.isStatic() != methodSubstitution.isStatic()) {
-                    throw new GraalInternalError("Macro and method substitution must agree on isStatic attribute: " + substituteMethod);
-                }
                 if (Modifier.isAbstract(modifiers) || Modifier.isNative(modifiers)) {
-                    throw new GraalInternalError("Substitution method must not be abstract or native: " + substituteMethod);
+                    throw new RuntimeException("Substitution method must not be abstract or native: " + substituteMethod);
                 }
                 String originalName = originalName(substituteMethod, methodSubstitution.value());
                 Class[] originalParameters = originalParameters(substituteMethod, methodSubstitution.signature(), methodSubstitution.isStatic());
