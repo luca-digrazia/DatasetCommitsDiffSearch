48ceef1e8adbeb7d123ac55997b37cea5e12bac9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:56:06 +0200

    Fix an Implicit Cast Error

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.parser.bc.impl/src/com/oracle/truffle/llvm/parser/bc/impl/LLVMBitcodeInstructionVisitor.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.parser.bc.impl/src/com/oracle/truffle/llvm/parser/bc/impl/LLVMBitcodeInstructionVisitor.java
@@ -363,7 +363,13 @@ public final class LLVMBitcodeInstructionVisitor implements InstructionVisitor {
         final LLVMExpressionNode element = symbols.resolve(insert.getValue());
         final Type type = insert.getType();
         final LLVMBaseType resultType = type.getLLVMBaseType();
-        final LLVMExpressionNode result = factoryFacade.createInsertElement(resultType, vector, type, element, index);
+
+        final int size = type.getSize(method.getTargetDataLayout());
+        final int alignment = type.getAlignment(method.getTargetDataLayout());
+        final LLVMAddressNode target = (LLVMAddressNode) factoryFacade.createAlloc(type, size, alignment, null, null);
+
+        final LLVMExpressionNode result = factoryFacade.createInsertElement(resultType, vector, target, element, index);
+
         createFrameWrite(result, insert);
     }
 
