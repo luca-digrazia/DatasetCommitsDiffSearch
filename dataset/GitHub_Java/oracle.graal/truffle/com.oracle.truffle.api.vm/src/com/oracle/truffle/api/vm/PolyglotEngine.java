8ccafef2ed39f85dc2d4c8fddb3e0b5f3b5641e3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 13:12:50 +0200

    Using static final fields to hold various Accessor interface to help the VM realize that they can be inlined

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotEngine.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotEngine.java
@@ -824,37 +824,27 @@ public class PolyglotEngine {
          */
         public Value execute(final Object... args) throws IOException {
             assertNoTruffle();
-            if (executor == null) {
-                Object ret = executeDirect(args);
-                return new Value(language, ret);
-            }
-
             get();
             ComputeInExecutor<Object> invokeCompute = new ComputeInExecutor<Object>(executor) {
                 @SuppressWarnings("try")
                 @Override
                 protected Object compute() throws IOException {
-                    return executeDirect(args);
+                    try (final Closeable c = Access.EXEC.executionStart(context, -1, debugger, null)) {
+                        for (;;) {
+                            try {
+                                if (target == null) {
+                                    target = SymbolInvokerImpl.createCallTarget(language[0], compute.get(), args);
+                                }
+                                return target.call(args);
+                            } catch (ArgumentsMishmashException ex) {
+                                target = null;
+                            }
+                        }
+                    }
                 }
             };
             invokeCompute.perform();
             return new Value(language, invokeCompute);
-        }
-
-        @SuppressWarnings("try")
-        private Object executeDirect(Object[] args) throws IOException {
-            try (final Closeable c = Access.EXEC.executionStart(context, -1, debugger, null)) {
-                for (;;) {
-                    try {
-                        if (target == null) {
-                            target = SymbolInvokerImpl.createCallTarget(language[0], compute.get(), args);
-                        }
-                        return target.call(args);
-                    } catch (ArgumentsMishmashException ex) {
-                        target = null;
-                    }
-                }
-            }
         }
 
         private Object waitForSymbol() throws IOException {
