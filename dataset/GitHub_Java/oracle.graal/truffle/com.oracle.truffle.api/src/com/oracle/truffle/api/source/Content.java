36a524effc6283e06660fd6f3ccdfa8f5dc14955
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:37:48 +0200

    Index is char based, that is for sure

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/source/Content.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/source/Content.java
@@ -38,6 +38,10 @@ abstract class Content {
 
     abstract String getCode();
 
+    String getCode(int offset, int length) {
+        return getCode().substring(offset, offset + length);
+    }
+
     abstract String getName();
 
     abstract String getShortName();
@@ -47,6 +51,10 @@ abstract class Content {
     abstract String getPath();
 
     abstract URL getURL();
+
+    int getCodeLength() {
+        return getCode().length();
+    }
 
     @SuppressWarnings("unused")
     void appendCode(CharSequence chars) {
