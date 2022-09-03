2986bbd44aa8861da288a385988998826e000685
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:18:38 +0200

    SpecializationNode cleanup

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.dsl/src/com/oracle/truffle/api/dsl/internal/SpecializationNode.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.dsl/src/com/oracle/truffle/api/dsl/internal/SpecializationNode.java
@@ -269,18 +269,7 @@ public abstract class SpecializationNode extends Node {
     }
 
     protected final SpecializationNode removeSame(final CharSequence reason) {
-        SpecializationNode start = SpecializationNode.this.findStart();
-        SpecializationNode current = start;
-        while (current != null) {
-            if (current.isSame(SpecializationNode.this)) {
-                NodeUtil.nonAtomicReplace(current, current.next, reason);
-                if (current == start) {
-                    start = start.next;
-                }
-            }
-            current = current.next;
-        }
-        return SpecializationNode.this.findEnd().findStart();
+        return removeSameImpl(SpecializationNode.this, reason);
     }
 
     /** Find the topmost of the specialization chain. */
@@ -300,6 +289,21 @@ public abstract class SpecializationNode extends Node {
 
     private Node findRoot() {
         return findStart().getParent();
+    }
+
+    private static SpecializationNode removeSameImpl(SpecializationNode toRemove, CharSequence reason) {
+        SpecializationNode start = toRemove.findStart();
+        SpecializationNode current = start;
+        while (current != null) {
+            if (current.isSame(toRemove)) {
+                NodeUtil.nonAtomicReplace(current, current.next, reason);
+                if (current == start) {
+                    start = start.next;
+                }
+            }
+            current = current.next;
+        }
+        return toRemove.findEnd().findStart();
     }
 
     @SuppressWarnings("unused")
