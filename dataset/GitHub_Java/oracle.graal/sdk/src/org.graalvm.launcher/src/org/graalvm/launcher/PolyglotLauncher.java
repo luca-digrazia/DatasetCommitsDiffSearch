de793b647b87eb69d3abc3731f5314002f87d590
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 07:58:10 +0200

    Allow creation of threads in language and polyglot launchers.

--- dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.launcher/src/org/graalvm/launcher/PolyglotLauncher.java
+++ dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.launcher/src/org/graalvm/launcher/PolyglotLauncher.java
@@ -28,7 +28,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -96,7 +95,7 @@ public final class PolyglotLauncher extends Launcher {
     private void launch(String[] args) {
         List<String> arguments = new ArrayList<>(Arrays.asList(args));
         if (isAOT()) {
-            nativeAccess.maybeExec(arguments, true, Collections.emptyMap(), VMType.Native);
+            nativeAccess.maybeExec(arguments, true, null, VMType.Native);
             nativeAccess.setGraalVMProperties();
         }
 
