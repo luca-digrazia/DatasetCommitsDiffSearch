fc07152e07e7b57c7c44d89c119ad2a476d192ae
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 21:26:17 +0200

    Group Deoptimization which have the same FrameState

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/phases/MidTier.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/phases/MidTier.java
@@ -81,7 +81,7 @@ public class MidTier extends PhaseSuite<MidTierContext> {
 
         appendPhase(new LoopSafepointEliminationPhase());
 
-        appendPhase(new LoopSafepointInsertionPhase());
+        appendPhase(new SafepointInsertionPhase());
 
         appendPhase(new GuardLoweringPhase());
 
