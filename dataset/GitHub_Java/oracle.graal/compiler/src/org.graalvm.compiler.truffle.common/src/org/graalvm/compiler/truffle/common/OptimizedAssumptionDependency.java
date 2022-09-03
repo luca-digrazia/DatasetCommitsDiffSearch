355a3729f66572cdd002ca5265cd7eb58afabe22
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:23:41 +0200

    added comments to new Access interfaces

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.common/src/org/graalvm/compiler/truffle/common/OptimizedAssumptionDependency.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.common/src/org/graalvm/compiler/truffle/common/OptimizedAssumptionDependency.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -50,16 +48,14 @@ public interface OptimizedAssumptionDependency {
     }
 
     /**
-     * Determines if a reference to this object is the only way the machine code can be executed. If
-     * {@code true}, it means the assumption will use a weak reference to this object. Once the weak
-     * reference is cleared, assumption invalidation can ignore this object without posing the risk
-     * of invalid code remaining live.
+     * Determines if the reachability of this object corresponds with the validity of the referenced
+     * machine code.
      *
-     * @return {@code true} if the referenced machine code is guaranteed never to be executed when
-     *         this object dies, {@code false} the referenced machine code can be still be executed
-     *         even when this object is dead
+     * @return {@code true} if the referenced machine code is guaranteed to be invalid when this
+     *         object becomes unreachable, {@code false} if the reachability of this object says
+     *         nothing about the validity of the referenced machine code
      */
-    default boolean soleExecutionEntryPoint() {
+    default boolean reachabilityDeterminesValidity() {
         return true;
     }
 
