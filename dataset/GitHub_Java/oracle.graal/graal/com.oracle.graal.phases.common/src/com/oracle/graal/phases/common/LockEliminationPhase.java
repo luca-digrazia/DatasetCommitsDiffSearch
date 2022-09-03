49c806552ef1cfc7f5512d5498a685e536abb464
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:02:24 +0200

    Fix for lock elimination phase.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/LockEliminationPhase.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/LockEliminationPhase.java
@@ -27,6 +27,7 @@ import com.oracle.graal.nodes.java.*;
 import com.oracle.graal.nodes.util.*;
 import com.oracle.graal.phases.*;
 
+
 public class LockEliminationPhase extends Phase {
 
     @Override
