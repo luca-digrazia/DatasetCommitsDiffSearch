e6c97f82285bd1d353d8ca74b11a2e180bcdbde7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:35:39 +0200

    Changing pmovzxbw and shlxl into using predefined emit sequences.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.asm.amd64/src/org/graalvm/compiler/asm/amd64/AMD64Assembler.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.asm.amd64/src/org/graalvm/compiler/asm/amd64/AMD64Assembler.java
@@ -2045,21 +2045,18 @@ public class AMD64Assembler extends AMD64BaseAssembler {
         movq(dst, src, false);
     }
 
-    public final void movq(Register dst, AMD64Address src, boolean force4BytesDisplacement) {
+    public final void movq(Register dst, AMD64Address src, boolean wide) {
         if (inRC(XMM, dst)) {
             // Insn: MOVQ xmm, r/m64
-            // Code: F3 0F 7E /r
-            // An alternative instruction would be 66 REX.W 0F 6E /r. We prefer the REX.W free
-            // format, because it would allow us to emit 2-bytes-prefixed vex-encoding instruction
-            // when applicable.
-            simdPrefix(dst, Register.None, src, SS, P_0F, false);
-            emitByte(0x7E);
-            emitOperandHelper(dst, src, force4BytesDisplacement, 0);
+            // Code: 66 REX.W 0F 6E /r
+            simdPrefix(dst, Register.None, src, PD, P_0F, true);
+            emitByte(0x6E);
+            emitOperandHelper(dst, src, wide, 0);
         } else {
             // gpr version of movq
             prefixq(src, dst);
             emitByte(0x8B);
-            emitOperandHelper(dst, src, force4BytesDisplacement, 0);
+            emitOperandHelper(dst, src, wide, 0);
         }
     }
 
@@ -2073,12 +2070,9 @@ public class AMD64Assembler extends AMD64BaseAssembler {
     public final void movq(AMD64Address dst, Register src) {
         if (inRC(XMM, src)) {
             // Insn: MOVQ r/m64, xmm
-            // Code: 66 0F D6 /r
-            // An alternative instruction would be 66 REX.W 0F 7E /r. We prefer the REX.W free
-            // format, because it would allow us to emit 2-bytes-prefixed vex-encoding instruction
-            // when applicable.
-            simdPrefix(src, Register.None, dst, PD, P_0F, false);
-            emitByte(0xD6);
+            // Code: 66 REX.W 0F 7E /r
+            simdPrefix(src, Register.None, dst, PD, P_0F, true);
+            emitByte(0x7E);
             emitOperandHelper(src, dst, 0);
         } else {
             // gpr version of movq
@@ -2513,19 +2507,13 @@ public class AMD64Assembler extends AMD64BaseAssembler {
     // Insn: VPMOVZXBW xmm1, xmm2/m64
 
     public final void pmovzxbw(Register dst, AMD64Address src) {
-        assert supports(CPUFeature.SSE4_1);
         assert inRC(XMM, dst);
-        simdPrefix(dst, Register.None, src, PD, P_0F38, false);
-        emitByte(0x30);
-        emitOperandHelper(dst, src, 0);
+        VexRMOp.VPMOVZXBW.emit(this, AVXSize.XMM, dst, src);
     }
 
     public final void pmovzxbw(Register dst, Register src) {
-        assert supports(CPUFeature.SSE4_1);
         assert inRC(XMM, dst) && inRC(XMM, src);
-        simdPrefix(dst, Register.None, src, PD, P_0F38, false);
-        emitByte(0x30);
-        emitModRM(dst, src);
+        VexRMOp.VPMOVZXBW.emit(this, AVXSize.XMM, dst, src);
     }
 
     public final void push(Register src) {
