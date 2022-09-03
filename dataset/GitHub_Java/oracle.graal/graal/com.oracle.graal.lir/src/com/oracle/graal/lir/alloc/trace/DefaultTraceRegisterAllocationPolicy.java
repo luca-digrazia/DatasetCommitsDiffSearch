795562a30fee7a714168976882291f34aab5b819
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:07:48 +0200

    TraceRA: add BottomUpOnly TraceRAPolicy strategy

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/DefaultTraceRegisterAllocationPolicy.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/DefaultTraceRegisterAllocationPolicy.java
@@ -87,7 +87,6 @@ public final class DefaultTraceRegisterAllocationPolicy {
     public static class BottomUpStrategy extends AllocationStrategy {
 
         public BottomUpStrategy(TraceRegisterAllocationPolicy plan) {
-            // explicitly specify the enclosing instance for the superclass constructor call
             plan.super();
         }
 
@@ -119,7 +118,6 @@ public final class DefaultTraceRegisterAllocationPolicy {
     public static final class TraceLinearScanStrategy extends AllocationStrategy {
 
         public TraceLinearScanStrategy(TraceRegisterAllocationPolicy plan) {
-            // explicitly specify the enclosing instance for the superclass constructor call
             plan.super();
         }
 
