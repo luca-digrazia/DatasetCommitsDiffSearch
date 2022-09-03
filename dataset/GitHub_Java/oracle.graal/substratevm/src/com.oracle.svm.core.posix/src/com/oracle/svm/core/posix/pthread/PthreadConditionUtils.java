b87f6a6dd2ae6adfcb24be7ab73dc78ee0c9691e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:32:12 +0200

    [GR-12724] Limit `tv_nsec` to values strictly less than `TimeUtils.nanosPerSecond`.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/pthread/PthreadConditionUtils.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/pthread/PthreadConditionUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -82,7 +82,7 @@ public class PthreadConditionUtils {
              * microsecond-granularity without prior rounding to milliseconds.
              */
             Time.timeval tv = StackValue.get(Time.timeval.class);
-            Time.NoTransitions.gettimeofday(tv, WordFactory.nullPointer());
+            Time.gettimeofday(tv, WordFactory.nullPointer());
             result.set_tv_sec(tv.tv_sec());
             result.set_tv_nsec(TimeUtils.microsToNanos(tv.tv_usec()));
         }
