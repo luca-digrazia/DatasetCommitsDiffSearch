c9477674a84039c99f274a312e3f7091413ba9ec
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:46:10 +0200

    Expose ForeignCallsProvider to phases

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.phases/src/org/graalvm/compiler/phases/tiers/PhaseContext.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.phases/src/org/graalvm/compiler/phases/tiers/PhaseContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,12 +24,72 @@
  */
 package org.graalvm.compiler.phases.tiers;
 
-import org.graalvm.compiler.nodes.spi.CoreProviders;
-import org.graalvm.compiler.nodes.spi.CoreProvidersImpl;
+import org.graalvm.compiler.core.common.spi.ConstantFieldProvider;
+import org.graalvm.compiler.nodes.spi.LoweringProvider;
+import org.graalvm.compiler.nodes.spi.Replacements;
+import org.graalvm.compiler.nodes.spi.StampProvider;
+import org.graalvm.compiler.phases.util.Providers;
 
-public class PhaseContext extends CoreProvidersImpl {
+import jdk.vm.ci.meta.ConstantReflectionProvider;
+import jdk.vm.ci.meta.MetaAccessProvider;
+import org.graalvm.compiler.core.common.spi.ForeignCallsProvider;
 
-    public PhaseContext(CoreProviders providers) {
-        super(providers);
+public class PhaseContext {
+
+    private final MetaAccessProvider metaAccess;
+    private final ConstantReflectionProvider constantReflection;
+    private final ConstantFieldProvider constantFieldProvider;
+    private final LoweringProvider lowerer;
+    private final Replacements replacements;
+    private final StampProvider stampProvider;
+    private final ForeignCallsProvider foreignCalls;
+
+    public PhaseContext(MetaAccessProvider metaAccess, ConstantReflectionProvider constantReflection, ConstantFieldProvider constantFieldProvider, LoweringProvider lowerer, Replacements replacements,
+                    StampProvider stampProvider, ForeignCallsProvider foreignCalls) {
+        this.metaAccess = metaAccess;
+        this.constantReflection = constantReflection;
+        this.constantFieldProvider = constantFieldProvider;
+        this.lowerer = lowerer;
+        this.replacements = replacements;
+        this.stampProvider = stampProvider;
+        this.foreignCalls = foreignCalls;
+    }
+
+    public PhaseContext(MetaAccessProvider metaAccess, ConstantReflectionProvider constantReflection, ConstantFieldProvider constantFieldProvider, LoweringProvider lowerer, Replacements replacements,
+                    StampProvider stampProvider) {
+        this(metaAccess, constantReflection, constantFieldProvider, lowerer, replacements, stampProvider, null);
+    }
+
+    public PhaseContext(Providers providers) {
+        this(providers.getMetaAccess(), providers.getConstantReflection(), providers.getConstantFieldProvider(), providers.getLowerer(), providers.getReplacements(), providers.getStampProvider(),
+                        providers.getForeignCalls());
+    }
+
+    public MetaAccessProvider getMetaAccess() {
+        return metaAccess;
+    }
+
+    public ConstantReflectionProvider getConstantReflection() {
+        return constantReflection;
+    }
+
+    public ConstantFieldProvider getConstantFieldProvider() {
+        return constantFieldProvider;
+    }
+
+    public LoweringProvider getLowerer() {
+        return lowerer;
+    }
+
+    public Replacements getReplacements() {
+        return replacements;
+    }
+
+    public StampProvider getStampProvider() {
+        return stampProvider;
+    }
+
+    public ForeignCallsProvider getForeignCalls() {
+        return foreignCalls;
     }
 }
