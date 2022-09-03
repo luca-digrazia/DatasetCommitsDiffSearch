556020aeb047cabbbf3ba66e008dff2dd92bc47d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:22:32 +0200

    Fix snippet.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/StringUTF16IndexOfNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/StringUTF16IndexOfNode.java
@@ -24,20 +24,24 @@
  */
 package org.graalvm.compiler.replacements;
 
-import static org.graalvm.compiler.nodeinfo.NodeCycles.CYCLES_256;
-import static org.graalvm.compiler.nodeinfo.NodeSize.SIZE_64;
-
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+import org.graalvm.compiler.core.common.type.StampPair;
 import org.graalvm.compiler.graph.NodeClass;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
+import org.graalvm.compiler.nodes.CallTargetNode.InvokeKind;
+import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.spi.LoweringTool;
 import org.graalvm.compiler.replacements.nodes.MacroStateSplitNode;
+
+import static org.graalvm.compiler.nodeinfo.NodeCycles.CYCLES_256;
+import static org.graalvm.compiler.nodeinfo.NodeSize.SIZE_64;
 
 @NodeInfo(size = SIZE_64, cycles = CYCLES_256)
 public class StringUTF16IndexOfNode extends MacroStateSplitNode {
     public static final NodeClass<StringUTF16IndexOfNode> TYPE = NodeClass.create(StringUTF16IndexOfNode.class);
 
-    public StringUTF16IndexOfNode(MacroParams p) {
-        super(TYPE, p);
+    public StringUTF16IndexOfNode(InvokeKind invokeKind, ResolvedJavaMethod targetMethod, int bci, StampPair returnStamp, ValueNode... arguments) {
+        super(TYPE, invokeKind, targetMethod, bci, returnStamp, arguments);
     }
 
     @Override
