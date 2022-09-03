d3c25077153241e7a7c27f88786963c39c41699a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:00:05 +0200

    fixed typo

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.api.replacements/src/com/oracle/graal/api/replacements/MethodSubstitution.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.api.replacements/src/com/oracle/graal/api/replacements/MethodSubstitution.java
@@ -22,13 +22,19 @@
  */
 package com.oracle.graal.api.replacements;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-import com.oracle.graal.api.meta.*;
+import jdk.vm.ci.meta.Signature;
 
 /**
- * Denotes a substitute method. A substitute method can call the original/substituted method by
- * making a recursive call to itself.
+ * Denotes a method whose body is used by a compiler as the substitute (or intrinsification) of
+ * another method. The exact method used to do the substitution is compiler dependent but every
+ * compiler should require substitute methods to be annotated with {@link MethodSubstitution}. In
+ * addition, a compiler is recommended to implement {@link MethodSubstitutionRegistry} to advertise
+ * the mechanism by which it supports registration of method substitutes.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
@@ -48,10 +54,17 @@ public @interface MethodSubstitution {
     boolean isStatic() default true;
 
     /**
-     * Gets the {@linkplain MetaUtil#signatureToMethodDescriptor signature} of the original method.
+     * Gets the {@linkplain Signature#toMethodDescriptor signature} of the original method.
      * <p>
      * If the default value is specified for this element, then the signature of the original method
      * is the same as the substitute method.
      */
     String signature() default "";
+
+    /**
+     * Determines if the substitution is for a method that may not be part of the runtime. For
+     * example, a method introduced in a later JDK version. Substitutions for such methods are
+     * omitted if the original method cannot be found.
+     */
+    boolean optional() default false;
 }
