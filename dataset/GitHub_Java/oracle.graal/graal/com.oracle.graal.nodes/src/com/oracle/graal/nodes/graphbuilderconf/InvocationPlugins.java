d7bcc0b75bb119584407cdd0a7726619647f2f1b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:31:11 +0200

    make InvocationPlugin lookup robust under class redefinition

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/graphbuilderconf/InvocationPlugins.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/graphbuilderconf/InvocationPlugins.java
@@ -335,12 +335,10 @@ public class InvocationPlugins {
         public boolean equals(Object obj) {
             if (obj instanceof ResolvedJavaMethodKey) {
                 ResolvedJavaMethodKey that = (ResolvedJavaMethodKey) obj;
-                if (this.method.isStatic() == that.method.isStatic()) {
-                    if (this.method.getDeclaringClass().equals(that.method.getDeclaringClass())) {
-                        if (this.method.getName().equals(that.method.getName())) {
-                            if (this.method.getSignature().equals(that.method.getSignature())) {
-                                return true;
-                            }
+                if (this.method.getDeclaringClass().equals(that.method.getDeclaringClass())) {
+                    if (this.method.getName().equals(that.method.getName())) {
+                        if (this.method.getSignature().equals(that.method.getSignature())) {
+                            return true;
                         }
                     }
                 }
