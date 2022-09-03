f2375ab392be9724c92f8ad346f7cd7f7b64cc21
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:52:19 +0200

    Insert deoptimization proxy nodes also for deopt entry at method start

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.java/src/org/graalvm/compiler/java/BytecodeParser.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.java/src/org/graalvm/compiler/java/BytecodeParser.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -3635,7 +3633,7 @@ public class BytecodeParser implements GraphBuilderContext {
         frameState.storeLocal(index, kind, value);
     }
 
-    protected void genLoadConstant(int cpi, int opcode) {
+    private void genLoadConstant(int cpi, int opcode) {
         Object con = lookupConstant(cpi, opcode);
 
         if (con instanceof JavaType) {
