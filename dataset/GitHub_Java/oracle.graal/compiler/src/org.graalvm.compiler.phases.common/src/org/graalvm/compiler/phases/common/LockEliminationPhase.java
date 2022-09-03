6a6f331c9605d0315873876192bc9191025554d3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:36:19 +0200

    Lock elimination can only combine locks with same lock depth

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/LockEliminationPhase.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/LockEliminationPhase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,6 +32,7 @@ import org.graalvm.compiler.nodes.java.AccessMonitorNode;
 import org.graalvm.compiler.nodes.java.MonitorEnterNode;
 import org.graalvm.compiler.nodes.java.MonitorExitNode;
 import org.graalvm.compiler.nodes.java.MonitorIdNode;
+import org.graalvm.compiler.nodes.java.RawMonitorEnterNode;
 import org.graalvm.compiler.nodes.util.GraphUtil;
 import org.graalvm.compiler.phases.Phase;
 
@@ -41,7 +42,7 @@ public class LockEliminationPhase extends Phase {
     protected void run(StructuredGraph graph) {
         for (MonitorExitNode monitorExitNode : graph.getNodes(MonitorExitNode.TYPE)) {
             FixedNode next = monitorExitNode.next();
-            if ((next instanceof MonitorEnterNode)) {
+            if ((next instanceof MonitorEnterNode || next instanceof RawMonitorEnterNode)) {
                 // should never happen, osr monitor enters are always direct successors of the graph
                 // start
                 assert !(next instanceof OSRMonitorEnterNode);
@@ -74,7 +75,6 @@ public class LockEliminationPhase extends Phase {
          */
         ValueNode object1 = GraphUtil.unproxify(lock1.object());
         ValueNode object2 = GraphUtil.unproxify(lock2.object());
-        return object1 == object2 && lock1.getMonitorId().getLockDepth() == lock2.getMonitorId().getLockDepth() &&
-                        lock1.getMonitorId().isMultipleEntry() == lock2.getMonitorId().isMultipleEntry();
+        return object1 == object2 && lock1.getMonitorId().getLockDepth() == lock2.getMonitorId().getLockDepth();
     }
 }
