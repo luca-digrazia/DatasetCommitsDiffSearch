7b83782240524fb6d9644faac792d4d49c01522c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:10:29 +0200

    Merge-o.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/target/Target_java_lang_reflect_Executable.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/target/Target_java_lang_reflect_Executable.java
@@ -27,10 +27,8 @@ package com.oracle.svm.reflect.target;
 // Checkstyle: allow reflection
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Executable;
 import java.lang.reflect.Parameter;
-import java.lang.reflect.Type;
 import java.util.Map;
 
 import com.oracle.svm.core.annotate.Alias;
@@ -42,6 +40,7 @@ import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 import com.oracle.svm.core.annotate.TargetElement;
 import com.oracle.svm.core.jdk.JDK8OrEarlier;
+import com.oracle.svm.core.util.VMError;
 import com.oracle.svm.reflect.hosted.ReflectionFeature;
 
 import jdk.vm.ci.meta.MetaAccessProvider;
@@ -50,12 +49,14 @@ import jdk.vm.ci.meta.ResolvedJavaField;
 @TargetClass(value = Executable.class, onlyWith = ReflectionFeature.IsEnabled.class)
 public final class Target_java_lang_reflect_Executable {
 
-    /**
-     * The parameters field doesn't need a value recomputation. Its value is pre-loaded in the
-     * {@link com.oracle.svm.reflect.hosted.ReflectionMetadataFeature}.
-     */
-    @Alias //
-    Parameter[] parameters;
+    public static final class ParameterAnnotationsComputer implements CustomFieldValueComputer {
+
+        @Override
+        public Object compute(MetaAccessProvider metaAccess, ResolvedJavaField original, ResolvedJavaField annotated, Object receiver) {
+            Executable executable = (Executable) receiver;
+            return executable.getParameterAnnotations();
+        }
+    }
 
     /**
      * The declaredAnnotations field doesn't need a value recomputation. Its value is pre-loaded in
@@ -67,108 +68,54 @@ public final class Target_java_lang_reflect_Executable {
     @Inject @RecomputeFieldValue(kind = Kind.Custom, declClass = ParameterAnnotationsComputer.class) //
     Annotation[][] parameterAnnotations;
 
-    @Inject @RecomputeFieldValue(kind = Kind.Custom, declClass = AnnotatedReceiverTypeComputer.class) //
-    AnnotatedType annotatedReceiverType;
-
-    @Inject @RecomputeFieldValue(kind = Kind.Custom, declClass = AnnotatedParameterTypesComputer.class) //
-    AnnotatedType[] annotatedParameterTypes;
-
-    @Inject @RecomputeFieldValue(kind = Kind.Custom, declClass = AnnotatedReturnTypeComputer.class) //
-    AnnotatedType annotatedReturnType;
-
-    @Inject @RecomputeFieldValue(kind = Kind.Custom, declClass = AnnotatedExceptionTypesComputer.class) //
-    AnnotatedType[] annotatedExceptionTypes;
+    /**
+     * The parameters field doesn't need a value recomputation. Its value is pre-loaded in the
+     * {@link com.oracle.svm.reflect.hosted.ReflectionMetadataFeature}.
+     */
+    @Alias //
+    Parameter[] parameters;
 
     @Alias //
     @TargetElement(onlyWith = JDK8OrEarlier.class)
     native Target_java_lang_reflect_Executable getRoot();
 
     @Substitute
-    private Parameter[] privateGetParameters() {
-        Target_java_lang_reflect_Executable holder = ReflectionHelper.getHolder(this);
-        return ReflectionHelper.requireNonNull(holder.parameters, "Parameters must be computed during native image generation");
-    }
-
-    @Substitute
     Map<Class<? extends Annotation>, Annotation> declaredAnnotations() {
-        Target_java_lang_reflect_Executable holder = ReflectionHelper.getHolder(this);
-        return ReflectionHelper.requireNonNull(holder.declaredAnnotations, "Annotations must be computed during native image generation");
+        Target_java_lang_reflect_Executable holder = getRoot();
+        if (holder == null) {
+            holder = this;
+        }
+
+        if (holder.declaredAnnotations == null) {
+            throw VMError.shouldNotReachHere("Annotations must be computed during native image generation");
+        }
+        return holder.declaredAnnotations;
     }
 
     @Substitute
     @SuppressWarnings("unused")
     Annotation[][] sharedGetParameterAnnotations(Class<?>[] parameterTypes, byte[] annotations) {
-        Target_java_lang_reflect_Executable holder = ReflectionHelper.getHolder(this);
-        return ReflectionHelper.requireNonNull(holder.parameterAnnotations, "Parameter annotations must be computed during native image generation");
+        Target_java_lang_reflect_Executable holder = getRoot();
+        if (holder == null) {
+            holder = this;
+        }
+
+        if (holder.parameterAnnotations == null) {
+            throw VMError.shouldNotReachHere("Parameter annotations must be computed during native image generation");
+        }
+        return holder.parameterAnnotations;
     }
 
     @Substitute
-    public AnnotatedType getAnnotatedReceiverType() {
-        Target_java_lang_reflect_Executable holder = ReflectionHelper.getHolder(this);
-        /* The annotatedReceiverType can be null. */
-        return holder.annotatedReceiverType;
-    }
-
-    @Substitute
-    public AnnotatedType[] getAnnotatedParameterTypes() {
-        Target_java_lang_reflect_Executable holder = ReflectionHelper.getHolder(this);
-        return ReflectionHelper.requireNonNull(holder.annotatedParameterTypes, "Annotated parameter types must be computed during native image generation");
-    }
-
-    @Substitute
-    public AnnotatedType getAnnotatedReturnType0(@SuppressWarnings("unused") Type returnType) {
-        Target_java_lang_reflect_Executable holder = ReflectionHelper.getHolder(this);
-        return ReflectionHelper.requireNonNull(holder.annotatedReturnType, "Annotated return type must be computed during native image generation");
-    }
-
-    @Substitute
-    public AnnotatedType[] getAnnotatedExceptionTypes() {
-        Target_java_lang_reflect_Executable holder = ReflectionHelper.getHolder(this);
-        return ReflectionHelper.requireNonNull(holder.annotatedExceptionTypes, "Annotated exception types must be computed during native image generation");
-    }
-
-    public static final class ParameterAnnotationsComputer implements CustomFieldValueComputer {
-
-        @Override
-        public Object compute(MetaAccessProvider metaAccess, ResolvedJavaField original, ResolvedJavaField annotated, Object receiver) {
-            Executable executable = (Executable) receiver;
-            return executable.getParameterAnnotations();
+    private Parameter[] privateGetParameters() {
+        Target_java_lang_reflect_Executable holder = getRoot();
+        if (holder == null) {
+            holder = this;
         }
-    }
 
-    public static final class AnnotatedReceiverTypeComputer implements CustomFieldValueComputer {
-
-        @Override
-        public Object compute(MetaAccessProvider metaAccess, ResolvedJavaField original, ResolvedJavaField annotated, Object receiver) {
-            Executable executable = (Executable) receiver;
-            return executable.getAnnotatedReceiverType();
+        if (holder.parameters == null) {
+            throw VMError.shouldNotReachHere("Parameters must be computed during native image generation");
         }
-    }
-
-    public static final class AnnotatedParameterTypesComputer implements CustomFieldValueComputer {
-
-        @Override
-        public Object compute(MetaAccessProvider metaAccess, ResolvedJavaField original, ResolvedJavaField annotated, Object receiver) {
-            Executable executable = (Executable) receiver;
-            return executable.getAnnotatedParameterTypes();
-        }
-    }
-
-    public static final class AnnotatedReturnTypeComputer implements CustomFieldValueComputer {
-
-        @Override
-        public Object compute(MetaAccessProvider metaAccess, ResolvedJavaField original, ResolvedJavaField annotated, Object receiver) {
-            Executable executable = (Executable) receiver;
-            return executable.getAnnotatedReturnType();
-        }
-    }
-
-    public static final class AnnotatedExceptionTypesComputer implements CustomFieldValueComputer {
-
-        @Override
-        public Object compute(MetaAccessProvider metaAccess, ResolvedJavaField original, ResolvedJavaField annotated, Object receiver) {
-            Executable executable = (Executable) receiver;
-            return executable.getAnnotatedExceptionTypes();
-        }
+        return holder.parameters;
     }
 }
