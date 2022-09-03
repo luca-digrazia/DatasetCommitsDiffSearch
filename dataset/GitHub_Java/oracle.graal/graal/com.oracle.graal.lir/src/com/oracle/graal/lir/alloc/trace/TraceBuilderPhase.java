04f485477f1dc28cd4e9cba7330169893afba233
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:01:07 +0200

    TraceRA: replace TraceRAbiDirectionalTraceBuilder and TraceRAsingleBlock option with TraceBuilding

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/TraceBuilderPhase.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/TraceBuilderPhase.java
@@ -22,46 +22,96 @@
  */
 package com.oracle.graal.lir.alloc.trace;
 
+import static com.oracle.graal.lir.alloc.trace.TraceUtil.isTrivialTrace;
+
 import java.util.List;
 
-import com.oracle.graal.compiler.common.alloc.UniDirectionalTraceBuilder;
 import com.oracle.graal.compiler.common.alloc.BiDirectionalTraceBuilder;
+import com.oracle.graal.compiler.common.alloc.SingleBlockTraceBuilder;
+import com.oracle.graal.compiler.common.alloc.Trace;
 import com.oracle.graal.compiler.common.alloc.TraceBuilderResult;
+import com.oracle.graal.compiler.common.alloc.TraceBuilderResult.TrivialTracePredicate;
+import com.oracle.graal.compiler.common.alloc.TraceStatisticsPrinter;
+import com.oracle.graal.compiler.common.alloc.UniDirectionalTraceBuilder;
 import com.oracle.graal.compiler.common.cfg.AbstractBlockBase;
+import com.oracle.graal.debug.Debug;
+import com.oracle.graal.debug.GraalError;
 import com.oracle.graal.lir.LIR;
 import com.oracle.graal.lir.gen.LIRGenerationResult;
-import com.oracle.graal.lir.phases.LIRPhase;
+import com.oracle.graal.lir.phases.AllocationPhase;
+import com.oracle.graal.options.EnumOptionValue;
 import com.oracle.graal.options.Option;
 import com.oracle.graal.options.OptionType;
 import com.oracle.graal.options.OptionValue;
 
 import jdk.vm.ci.code.TargetDescription;
 
-public class TraceBuilderPhase extends LIRPhase<TraceBuilderPhase.TraceBuilderContext> {
+public class TraceBuilderPhase extends AllocationPhase {
+
+    public enum TraceBuilder {
+        UniDirectional,
+        BiDirectional,
+        SingleBlock
+    }
 
     public static class Options {
         // @formatter:off
-        @Option(help = "", type = OptionType.Debug)
-        public static final OptionValue<Boolean> TraceRAbiDirectionalTraceBuilder = new OptionValue<>(false);
+        @Option(help = "Trace building algorithm.", type = OptionType.Debug)
+        public static final EnumOptionValue<TraceBuilder> TraceBuilding = new EnumOptionValue<>(TraceBuilder.UniDirectional);
+        @Option(help = "Schedule trivial traces as early as possible.", type = OptionType.Debug)
+        public static final OptionValue<Boolean> TraceRAScheduleTrivialTracesEarly = new OptionValue<>(true);
         // @formatter:on
     }
 
-    public static final class TraceBuilderContext {
-        public TraceBuilderResult<?> traceBuilderResult;
-
-    }
+    private static final int TRACE_LOG_LEVEL = 1;
+    public static final int TRACE_DUMP_LEVEL = 3;
 
     @Override
-    protected <B extends AbstractBlockBase<B>> void run(TargetDescription target, LIRGenerationResult lirGenRes, List<B> codeEmittingOrder, List<B> linearScanOrder, TraceBuilderContext context) {
-        B startBlock = linearScanOrder.get(0);
+    protected void run(TargetDescription target, LIRGenerationResult lirGenRes, AllocationContext context) {
+        AbstractBlockBase<?>[] linearScanOrder = lirGenRes.getLIR().linearScanOrder();
+        AbstractBlockBase<?> startBlock = linearScanOrder[0];
         LIR lir = lirGenRes.getLIR();
         assert startBlock.equals(lir.getControlFlowGraph().getStartBlock());
-        if (Options.TraceRAbiDirectionalTraceBuilder.getValue()) {
-            context.traceBuilderResult = BiDirectionalTraceBuilder.computeTraces(startBlock, linearScanOrder);
-        } else {
-            context.traceBuilderResult = UniDirectionalTraceBuilder.computeTraces(startBlock, linearScanOrder);
-        }
 
+        final TraceBuilderResult traceBuilderResult = getTraceBuilderResult(lir, startBlock, linearScanOrder);
+
+        if (Debug.isLogEnabled(TRACE_LOG_LEVEL)) {
+            List<Trace> traces = traceBuilderResult.getTraces();
+            for (int i = 0; i < traces.size(); i++) {
+                Trace trace = traces.get(i);
+                Debug.log(TRACE_LOG_LEVEL, "Trace %5d: %s%s", i, trace, isTrivialTrace(lirGenRes.getLIR(), trace) ? " (trivial)" : "");
+            }
+        }
+        TraceStatisticsPrinter.printTraceStatistics(traceBuilderResult, lirGenRes.getCompilationUnitName());
+        Debug.dump(TRACE_DUMP_LEVEL, traceBuilderResult, "After TraceBuilding");
+        context.contextAdd(traceBuilderResult);
     }
 
+    private static TraceBuilderResult getTraceBuilderResult(LIR lir, AbstractBlockBase<?> startBlock, AbstractBlockBase<?>[] linearScanOrder) {
+        TraceBuilderResult.TrivialTracePredicate pred = getTrivialTracePredicate(lir);
+
+        TraceBuilder selectedTraceBuilder = Options.TraceBuilding.getValue();
+        Debug.log(TRACE_LOG_LEVEL, "Building Traces using %s", selectedTraceBuilder);
+        switch (Options.TraceBuilding.getValue()) {
+            case SingleBlock:
+                return SingleBlockTraceBuilder.computeTraces(startBlock, linearScanOrder, pred);
+            case BiDirectional:
+                return BiDirectionalTraceBuilder.computeTraces(startBlock, linearScanOrder, pred);
+            case UniDirectional:
+                return UniDirectionalTraceBuilder.computeTraces(startBlock, linearScanOrder, pred);
+        }
+        throw GraalError.shouldNotReachHere("Unknown trace building algorithm: " + Options.TraceBuilding.getValue());
+    }
+
+    public static TraceBuilderResult.TrivialTracePredicate getTrivialTracePredicate(LIR lir) {
+        if (!Options.TraceRAScheduleTrivialTracesEarly.getValue()) {
+            return null;
+        }
+        return new TrivialTracePredicate() {
+            @Override
+            public boolean isTrivialTrace(Trace trace) {
+                return TraceUtil.isTrivialTrace(lir, trace);
+            }
+        };
+    }
 }
