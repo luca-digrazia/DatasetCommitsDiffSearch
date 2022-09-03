8b1158dc5ae65430c44f556d342afe91eb8b93bf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:37:11 +0200

    Add missing specialization for LLVMFunctionDescriptor in ToLLVMNode.

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/ToLLVMNode.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/ToLLVMNode.java
@@ -167,7 +167,7 @@ public abstract class ToLLVMNode extends Node {
                 return (int) convertPrimitive(int.class, unboxed);
             } catch (UnsupportedMessageException e) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not unboxable!", e);
+                throw new IllegalStateException(e);
             }
         }
     }
@@ -231,7 +231,7 @@ public abstract class ToLLVMNode extends Node {
                 return (long) convertPrimitive(long.class, unboxed);
             } catch (UnsupportedMessageException e) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not unboxable!", e);
+                throw new IllegalStateException(e);
             }
         }
     }
@@ -296,7 +296,7 @@ public abstract class ToLLVMNode extends Node {
                 return (short) convertPrimitive(short.class, unboxed);
             } catch (UnsupportedMessageException e) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not unboxable!", e);
+                throw new IllegalStateException(e);
             }
         }
     }
@@ -361,7 +361,7 @@ public abstract class ToLLVMNode extends Node {
                 return (byte) convertPrimitive(byte.class, unboxed);
             } catch (UnsupportedMessageException e) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not unboxable!", e);
+                throw new IllegalStateException(e);
             }
         }
     }
@@ -426,7 +426,7 @@ public abstract class ToLLVMNode extends Node {
                 return (char) convertPrimitive(char.class, unboxed);
             } catch (UnsupportedMessageException e) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not unboxable!", e);
+                throw new IllegalStateException(e);
             }
         }
     }
@@ -490,7 +490,7 @@ public abstract class ToLLVMNode extends Node {
                 return (float) convertPrimitive(float.class, unboxed);
             } catch (UnsupportedMessageException e) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not unboxable!", e);
+                throw new IllegalStateException(e);
             }
         }
     }
@@ -554,7 +554,7 @@ public abstract class ToLLVMNode extends Node {
                 return (double) convertPrimitive(double.class, unboxed);
             } catch (UnsupportedMessageException e) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not unboxable!", e);
+                throw new IllegalStateException(e);
             }
         }
     }
@@ -619,7 +619,7 @@ public abstract class ToLLVMNode extends Node {
                 return (boolean) convertPrimitive(boolean.class, unboxed);
             } catch (UnsupportedMessageException e) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not unboxable!", e);
+                throw new IllegalStateException(e);
             }
         }
     }
@@ -693,7 +693,7 @@ public abstract class ToLLVMNode extends Node {
                 return LLVMAddress.fromLong(raw);
             } catch (UnsupportedMessageException ex) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not a pointer!", ex);
+                throw new RuntimeException(ex);
             }
         }
 
@@ -767,7 +767,7 @@ public abstract class ToLLVMNode extends Node {
                 return LLVMAddress.fromLong(raw);
             } catch (UnsupportedMessageException ex) {
                 CompilerDirectives.transferToInterpreter();
-                throw new IllegalStateException("Foreign value is not a pointer!", ex);
+                throw new RuntimeException(ex);
             }
         }
 
