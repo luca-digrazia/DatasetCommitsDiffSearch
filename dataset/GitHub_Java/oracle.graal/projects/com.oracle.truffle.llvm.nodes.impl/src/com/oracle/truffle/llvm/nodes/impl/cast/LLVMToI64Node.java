d961b424bf237449c5ee9c57f628e833ca795c10
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 10:38:41 +0200

    Add LLVMTruffleObject specialization to LLVMToI64Node

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/cast/LLVMToI64Node.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/cast/LLVMToI64Node.java
@@ -29,131 +29,123 @@
  */
 package com.oracle.truffle.llvm.nodes.impl.cast;
 
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.llvm.nodes.impl.base.LLVMAddressNode;
-import com.oracle.truffle.llvm.nodes.impl.base.LLVMFunctionNode;
-import com.oracle.truffle.llvm.nodes.impl.base.floating.LLVM80BitFloatNode;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.interop.ForeignAccess;
+import com.oracle.truffle.api.interop.Message;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.llvm.nodes.base.LLVMExpressionNode;
 import com.oracle.truffle.llvm.nodes.impl.base.floating.LLVMDoubleNode;
-import com.oracle.truffle.llvm.nodes.impl.base.floating.LLVMFloatNode;
 import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMI16Node;
-import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMI1Node;
 import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMI32Node;
 import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMI64Node;
 import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMI8Node;
-import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMIVarBitNode;
 import com.oracle.truffle.llvm.types.LLVMAddress;
-import com.oracle.truffle.llvm.types.LLVMFunction;
+import com.oracle.truffle.llvm.types.LLVMFunctionDescriptor;
 import com.oracle.truffle.llvm.types.LLVMIVarBit;
+import com.oracle.truffle.llvm.types.LLVMTruffleObject;
 import com.oracle.truffle.llvm.types.floating.LLVM80BitFloat;
+import com.oracle.truffle.llvm.types.vector.LLVMFloatVector;
 
 public abstract class LLVMToI64Node extends LLVMI64Node {
 
-    @NodeChild(value = "fromNode", type = LLVMI1Node.class)
-    public abstract static class LLVMI1ToI64Node extends LLVMToI64Node {
+    @NodeChild(value = "fromNode", type = LLVMExpressionNode.class)
+    public abstract static class LLVMAnyToI64Node extends LLVMToI64Node {
 
         @Specialization
         public long executeI64(boolean from) {
             return from ? -1 : 0;
         }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMI1Node.class)
-    public abstract static class LLVMI1ToI64ZeroExtNode extends LLVMToI64Node {
-
-        @Specialization
-        public long executeI64(boolean from) {
-            return from ? 1 : 0;
-        }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMI8Node.class)
-    public abstract static class LLVMI8ToI64Node extends LLVMToI64Node {
 
         @Specialization
         public long executeI64(byte from) {
             return from;
         }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMI8Node.class)
-    public abstract static class LLVMI8ToI64ZeroExtNode extends LLVMToI64Node {
-
-        @Specialization
-        public long executeI64(byte from) {
-            return from & LLVMI8Node.MASK;
-        }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMI16Node.class)
-    public abstract static class LLVMI16ToI64Node extends LLVMToI64Node {
 
         @Specialization
         public long executeI64(short from) {
             return from;
         }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMI16Node.class)
-    public abstract static class LLVMI16ToI64ZeroExtNode extends LLVMToI64Node {
-
-        @Specialization
-        public long executeI64(short from) {
-            return from & LLVMI16Node.MASK;
-        }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMI32Node.class)
-    public abstract static class LLVMI32ToI64Node extends LLVMToI64Node {
 
         @Specialization
         public long executeI64(int from) {
             return from;
         }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMI32Node.class)
-    public abstract static class LLVMI32ToI64ZeroExtNode extends LLVMToI64Node {
 
         @Specialization
-        public long executeI64(int from) {
-            return from & LLVMI32Node.MASK;
+        public long executeI64(long from) {
+            return from;
         }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMIVarBitNode.class)
-    public abstract static class LLVMIVarToI64Node extends LLVMToI64Node {
 
         @Specialization
         public long executeI64(LLVMIVarBit from) {
             return from.getLongValue();
         }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMIVarBitNode.class)
-    public abstract static class LLVMIVarToI64ZeroExtNode extends LLVMToI64Node {
-
-        @Specialization
-        public long executeI64(LLVMIVarBit from) {
-            return from.getZeroExtendedLongValue();
-        }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMFloatNode.class)
-    public abstract static class LLVMFloatToI64Node extends LLVMToI64Node {
 
         @Specialization
         public long executeI64(float from) {
             return (long) from;
         }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMDoubleNode.class)
-    public abstract static class LLVMDoubleToI64Node extends LLVMToI64Node {
 
         @Specialization
         public long executeI64(double from) {
             return (long) from;
+        }
+
+        @Specialization
+        public long executeI64(LLVM80BitFloat from) {
+            return from.getLongValue();
+        }
+
+        @Specialization
+        public long executeI64(LLVMAddress from) {
+            return from.getVal();
+        }
+
+        @Specialization(guards = "!isLLVMFunctionDescriptor(from)")
+        public long executeUnbox(VirtualFrame frame, TruffleObject from,
+                        @Cached("createUnboxNode()") Node unboxNode) {
+            try {
+                return (long) ForeignAccess.sendUnbox(unboxNode, frame, from);
+            } catch (UnsupportedMessageException e) {
+                throw new UnsupportedOperationException(e);
+            }
+        }
+
+        @Specialization
+        public long executeUnbox(VirtualFrame frame, LLVMTruffleObject from,
+                        @Cached("createUnboxNode()") Node unboxNode) {
+            try {
+                long head = (long) ForeignAccess.sendUnbox(unboxNode, frame, from.getObject());
+                return head + from.getOffset();
+            } catch (UnsupportedMessageException e) {
+                throw new UnsupportedOperationException(e);
+            }
+        }
+
+        protected Node createUnboxNode() {
+            return Message.UNBOX.createNode();
+        }
+
+        protected boolean isLLVMFunctionDescriptor(TruffleObject object) {
+            return object instanceof LLVMFunctionDescriptor;
+        }
+
+        @Specialization
+        public long executeI64(LLVMFloatVector from) {
+            float f1 = from.getValue(0);
+            float f2 = from.getValue(1);
+            long composedValue = (long) Float.floatToRawIntBits(f1) << Float.SIZE | Float.floatToRawIntBits(f2);
+            return composedValue;
+        }
+
+        @Specialization
+        public long executeI64(LLVMFunctionDescriptor from) {
+            return from.getFunctionIndex();
         }
     }
 
@@ -166,30 +158,33 @@ public abstract class LLVMToI64Node extends LLVMI64Node {
         }
     }
 
-    @NodeChild(value = "fromNode", type = LLVM80BitFloatNode.class)
-    public abstract static class LLVM80BitFloatToI64Node extends LLVMToI64Node {
+    @NodeChild(value = "fromNode", type = LLVMExpressionNode.class)
+    public abstract static class LLVMToI64ZeroExtNode extends LLVMToI64Node {
 
         @Specialization
-        public long executeI64(LLVM80BitFloat from) {
-            return from.getLongValue();
+        public long executeI64(boolean from) {
+            return from ? 1 : 0;
+        }
+
+        @Specialization
+        public long executeI64(byte from) {
+            return from & LLVMI8Node.MASK;
+        }
+
+        @Specialization
+        public long executeI64(short from) {
+            return from & LLVMI16Node.MASK;
+        }
+
+        @Specialization
+        public long executeI64(int from) {
+            return from & LLVMI32Node.MASK;
+        }
+
+        @Specialization
+        public long executeI64(LLVMIVarBit from) {
+            return from.getZeroExtendedLongValue();
         }
     }
 
-    @NodeChild(value = "fromNode", type = LLVMAddressNode.class)
-    public abstract static class LLVMAddressToI64Node extends LLVMToI64Node {
-
-        @Specialization
-        public long executeI64(LLVMAddress from) {
-            return from.getVal();
-        }
-    }
-
-    @NodeChild(value = "fromNode", type = LLVMFunctionNode.class)
-    public abstract static class LLVMFunctionToI64Node extends LLVMToI64Node {
-
-        @Specialization
-        public long executeI64(LLVMFunction from) {
-            return from.getFunctionAddress().getVal();
-        }
-    }
 }
