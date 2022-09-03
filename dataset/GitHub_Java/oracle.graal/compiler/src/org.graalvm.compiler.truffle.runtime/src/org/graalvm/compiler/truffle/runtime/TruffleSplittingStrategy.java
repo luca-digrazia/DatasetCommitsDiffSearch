6b62dc233d77f2e19245853b2c374b337c8b4b7d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:53:51 +0200

    Use correct output stream for tracing summary.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/TruffleSplittingStrategy.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/TruffleSplittingStrategy.java
@@ -109,7 +109,7 @@ final class TruffleSplittingStrategy {
 
     static void forceSplitting(OptimizedDirectCallNode call, GraalTVMCI tvmci) {
         if (!TruffleCompilerOptions.getValue(TruffleExperimentalSplitting) || TruffleCompilerOptions.getValue(TruffleExperimentalSplittingAllowForcedSplits)) {
-            if (!canSplit(call) || isRecursiveSplit(call)) {
+            if (!canSplit(call)) {
                 return;
             }
             final GraalTVMCI.EngineData engineData = getEngineData(call, tvmci);
@@ -168,16 +168,14 @@ final class TruffleSplittingStrategy {
         if (isMaxSingleCall(call)) {
             return true;
         }
+
         return countPolymorphic(call) >= 1;
     }
 
     private static boolean isRecursiveSplit(OptimizedDirectCallNode call) {
         final OptimizedCallTarget splitCandidateTarget = call.getCallTarget();
-        final RootNode rootNode = call.getRootNode();
-        if (rootNode == null) {
-            return false;
-        }
-        OptimizedCallTarget callRootTarget = (OptimizedCallTarget) rootNode.getCallTarget();
+
+        OptimizedCallTarget callRootTarget = (OptimizedCallTarget) call.getRootNode().getCallTarget();
         OptimizedCallTarget callSourceTarget = callRootTarget.getSourceCallTarget();
         int depth = 0;
         while (callSourceTarget != null) {
