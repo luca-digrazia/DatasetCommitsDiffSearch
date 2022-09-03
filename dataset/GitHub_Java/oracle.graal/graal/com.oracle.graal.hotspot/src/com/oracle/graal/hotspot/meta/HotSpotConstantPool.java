7f7500057177bcc12d5316889c717d59d2be9b34
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 18:03:24 +0200

    added table to speed up mapping JVM_CONSTANT tag value to JVM_CONSTANT enum object

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotConstantPool.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotConstantPool.java
@@ -198,9 +198,6 @@ public class HotSpotConstantPool extends CompilerObject implements ConstantPool 
         HotSpotVMConfig config = runtime().getConfig();
         final long metaspaceConstantPoolTags = unsafe.getAddress(metaspaceConstantPool + config.constantPoolTagsOffset);
         final int tag = unsafe.getByteVolatile(null, metaspaceConstantPoolTags + config.arrayU1DataOffset + index);
-        if (tag == 0) {
-            return null;
-        }
         return JVM_CONSTANT.getEnum(tag);
     }
 
@@ -538,10 +535,6 @@ public class HotSpotConstantPool extends CompilerObject implements ConstantPool 
         }
 
         JVM_CONSTANT tag = getTagAt(index);
-        if (tag == null) {
-            assert getTagAt(index - 1) == JVM_CONSTANT.Double || getTagAt(index - 1) == JVM_CONSTANT.Long;
-            return;
-        }
         switch (tag) {
             case Fieldref:
             case MethodRef:
@@ -561,19 +554,14 @@ public class HotSpotConstantPool extends CompilerObject implements ConstantPool 
                 }
                 break;
             case InvokeDynamic:
-                if (isInvokedynamicIndex(cpi)) {
-                    runtime().getCompilerToVM().resolveInvokeDynamic(metaspaceConstantPool, cpi);
+                if (!isInvokedynamicIndex(cpi)) {
+                    throw new IllegalArgumentException("InvokeDynamic entries must be accessed");
                 }
+                runtime().getCompilerToVM().resolveInvokeDynamic(metaspaceConstantPool, cpi);
                 break;
             default:
                 // nothing
                 break;
         }
-    }
-
-    @Override
-    public String toString() {
-        HotSpotResolvedObjectType holder = getHolder();
-        return "HotSpotConstantPool<" + holder.toJavaName() + ">";
     }
 }
