861a1dbe0ef28aa31f527e114998edef2dee9596
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 23:47:25 +0200

    Fixed error messages in parser.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.codegen.processor/src/com/oracle/truffle/codegen/processor/node/NodeParser.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.codegen.processor/src/com/oracle/truffle/codegen/processor/node/NodeParser.java
@@ -94,7 +94,6 @@ public class NodeParser extends TemplateParser<NodeData> {
         if (rootNode != null) {
             rootNode.setDeclaredChildren(children);
         }
-
         return rootNode;
     }
 
@@ -117,10 +116,6 @@ public class NodeParser extends TemplateParser<NodeData> {
         }
         if (!Utils.isAssignable(type.asType(), context.getTruffleTypes().getNode())) {
             return null; // not a node
-        }
-
-        if (type.getModifiers().contains(Modifier.PRIVATE)) {
-            return null; // not visible
         }
 
         List<Element> elements = new ArrayList<>(context.getEnvironment().getElementUtils().getAllMembers(type));
