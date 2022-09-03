3b108564770aab9060aa88390ac551ad9fb7fc0c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 15:40:46 +0200

    converted OptionsVerifier to be a unit test

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.test/src/com/oracle/graal/compiler/test/OptionsVerifierTest.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.compiler.test/src/com/oracle/graal/compiler/test/OptionsVerifierTest.java
@@ -22,37 +22,20 @@
  */
 package com.oracle.graal.compiler.test;
 
-import static java.lang.String.format;
+import static java.lang.String.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Executable;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.ServiceLoader;
-import java.util.Set;
+import java.io.*;
+import java.lang.reflect.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.zip.*;
 
-import jdk.internal.org.objectweb.asm.ClassReader;
-import jdk.internal.org.objectweb.asm.ClassVisitor;
-import jdk.internal.org.objectweb.asm.Label;
-import jdk.internal.org.objectweb.asm.MethodVisitor;
-import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.internal.org.objectweb.asm.*;
 import jdk.internal.org.objectweb.asm.Type;
-import jdk.vm.ci.options.OptionDescriptor;
-import jdk.vm.ci.options.OptionDescriptors;
-import jdk.vm.ci.options.OptionValue;
 
-import org.junit.Test;
+import org.junit.*;
+
+import com.oracle.jvmci.options.*;
 
 /**
  * Verifies a class declaring one or more {@linkplain OptionValue options} has a class initializer
@@ -65,7 +48,7 @@ public class OptionsVerifierTest {
     public void verifyOptions() throws IOException {
         try (Classpath cp = new Classpath()) {
             HashSet<Class<?>> checked = new HashSet<>();
-            for (OptionDescriptors opts : ServiceLoader.load(OptionDescriptors.class, getClass().getClassLoader())) {
+            for (Options opts : ServiceLoader.load(Options.class, getClass().getClassLoader())) {
                 for (OptionDescriptor desc : opts) {
                     OptionsVerifier.checkClass(desc.getDeclaringClass(), desc, checked, cp);
                 }
@@ -76,19 +59,15 @@ public class OptionsVerifierTest {
     static class Classpath implements AutoCloseable {
         private final Map<String, Object> entries = new LinkedHashMap<>();
 
-        public Classpath() throws IOException {
+        public Classpath() throws ZipException, IOException {
             String[] names = (System.getProperty("sun.boot.class.path") + File.pathSeparatorChar + System.getProperty("java.class.path")).split(File.pathSeparator);
             for (String n : names) {
                 File path = new File(n);
                 if (path.exists()) {
                     if (path.isDirectory()) {
                         entries.put(n, path);
-                    } else if (n.endsWith(".jimage")) {
-                        URL url = path.toURI().toURL();
-                        entries.put(n, new URLClassLoader(new URL[]{url}));
                     } else if (n.endsWith(".jar") || n.endsWith(".zip")) {
-                        URL url = new URL("jar", "", "file:" + n + "!/");
-                        entries.put(n, new URLClassLoader(new URL[]{url}));
+                        entries.put(n, new ZipFile(path));
                     }
                 }
             }
@@ -96,8 +75,8 @@ public class OptionsVerifierTest {
 
         public void close() throws IOException {
             for (Object e : entries.values()) {
-                if (e instanceof URLClassLoader) {
-                    ((URLClassLoader) e).close();
+                if (e instanceof ZipFile) {
+                    ((ZipFile) e).close();
                 }
             }
         }
@@ -109,17 +88,15 @@ public class OptionsVerifierTest {
                     if (path.exists()) {
                         return Files.readAllBytes(path.toPath());
                     }
-                } else {
-                    assert e instanceof URLClassLoader;
-                    URLClassLoader ucl = (URLClassLoader) e;
-                    try (InputStream in = ucl.getResourceAsStream(classFilePath)) {
-                        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                        int nRead;
-                        byte[] data = new byte[1024];
-                        while ((nRead = in.read(data, 0, data.length)) != -1) {
-                            buffer.write(data, 0, nRead);
-                        }
-                        return buffer.toByteArray();
+                } else if (e instanceof ZipFile) {
+                    ZipFile zf = (ZipFile) e;
+                    ZipEntry ze = zf.getEntry(classFilePath);
+                    if (ze != null) {
+                        byte[] res = new byte[(int) ze.getSize()];
+                        DataInputStream dis = new DataInputStream(zf.getInputStream(ze));
+                        dis.readFully(res);
+                        dis.close();
+                        return res;
                     }
                 }
             }
