ea70f4ff3afaeadf79e2fb5919ccfd4e6f211596
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:23:12 +0200

    removed test output on console

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/CompilationWrapperTest.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/CompilationWrapperTest.java
@@ -82,8 +82,6 @@ public class CompilationWrapperTest extends GraalCompilerTest {
                         "test");
     }
 
-    private static final boolean VERBOSE = Boolean.getBoolean(CompilationWrapperTest.class.getSimpleName() + ".verbose");
-
     private static void testHelper(List<String> extraVmArgs, String... mainClassAndArgs) throws IOException, InterruptedException {
         final File dumpPath = new File(CompilationWrapperTest.class.getSimpleName() + "_" + System.currentTimeMillis()).getAbsoluteFile();
         List<String> vmArgs = withoutDebuggerArguments(getVMCommandLine());
@@ -94,9 +92,7 @@ public class CompilationWrapperTest extends GraalCompilerTest {
         vmArgs.addAll(extraVmArgs);
 
         Subprocess proc = SubprocessUtil.java(vmArgs, mainClassAndArgs);
-        if (VERBOSE) {
-            System.out.println(proc);
-        }
+        System.out.println(proc);
 
         String forcedCrashString = "Forced crash after compiling";
         String diagnosticOutputFilePrefix = "Graal diagnostic output saved in ";
