fc9543da36ce0168fbdbde1c6a2deeb268f3c6c5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:54:03 +0200

    Merge fix.

--- dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation.test/src/com/oracle/truffle/api/instrumentation/test/AllocationReporterTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/src/com.oracle.truffle.api.instrumentation.test/src/com/oracle/truffle/api/instrumentation/test/AllocationReporterTest.java
@@ -1,42 +1,26 @@
 /*
- * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * The Universal Permissive License (UPL), Version 1.0
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * (a) the Software, and
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package com.oracle.truffle.api.instrumentation.test;
 
@@ -66,7 +50,6 @@ import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.TruffleLanguage;
-import com.oracle.truffle.api.frame.MaterializedFrame;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.instrumentation.AllocationEvent;
 import com.oracle.truffle.api.instrumentation.AllocationEventFilter;
@@ -576,6 +559,13 @@ public class AllocationReporterTest {
 
     @Test
     public void testReporterChangeListener() {
+        try {
+            Class.forName("java.beans.PropertyChangeListener");
+        } catch (ClassNotFoundException ex) {
+            // skip the test if running only with java.base JDK9 module
+            return;
+        }
+
         // Test of AllocationReporter property change listener notifications
         allocation.setEnabled(false);
         Source source = Source.create(AllocationReporterLanguage.ID, "NEW");
@@ -770,7 +760,7 @@ public class AllocationReporterTest {
                 AllocationReporter reporter = contextRef.get().getEnv().lookup(AllocationReporter.class);
                 if (newValue == null) { // No allocation
                     value = InstrumentationTestLanguage.Null.INSTANCE;
-                    execChildren(frame.materialize());
+                    execChildren(frame);
                 } else if (oldValue == null) {
                     // new allocation
                     if (reporter.isActive()) {
@@ -779,7 +769,7 @@ public class AllocationReporterTest {
                             reporter.onEnter(null, 0, getAllocationSizeEstimate(newValue));
                         }
                     }
-                    execChildren(frame.materialize());
+                    execChildren(frame);
                     value = allocateValue(newValue);
                     if (reporter.isActive()) {
                         reporter.onReturnValue(value, 0, computeValueSize(newValue, value));
@@ -794,7 +784,7 @@ public class AllocationReporterTest {
                         newSize = getAllocationSizeEstimate(newValue);
                         reporter.onEnter(value, oldSize, newSize);
                     }
-                    execChildren(frame.materialize());
+                    execChildren(frame);
                     // Re-allocate, oldValue -> newValue
                     if (reporter.isActive()) {
                         if (newSize == AllocationReporter.SIZE_UNKNOWN) {
@@ -810,8 +800,7 @@ public class AllocationReporterTest {
                 return value;
             }
 
-            @TruffleBoundary
-            private void execChildren(MaterializedFrame frame) {
+            private void execChildren(VirtualFrame frame) {
                 if (children != null) {
                     for (AllocNode ch : children) {
                         ch.execute(frame);
