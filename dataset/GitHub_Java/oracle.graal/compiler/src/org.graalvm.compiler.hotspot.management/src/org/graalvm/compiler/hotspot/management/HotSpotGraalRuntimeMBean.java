7309fb703a42b8b5aeeafa25cc84096d7c6729eb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:40:01 +0200

    Avoid referencing javax.management package by using generic type signature

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot.management/src/org/graalvm/compiler/hotspot/management/HotSpotGraalRuntimeMBean.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.hotspot.management/src/org/graalvm/compiler/hotspot/management/HotSpotGraalRuntimeMBean.java
@@ -80,16 +80,7 @@ final class HotSpotGraalRuntimeMBean implements DynamicMBean {
         return runtime;
     }
 
-    private static final boolean DEBUG = initDebug();
-
-    private static boolean initDebug() {
-        try {
-            return Boolean.getBoolean(HotSpotGraalRuntimeMBean.class.getSimpleName() + ".debug");
-        } catch (SecurityException e) {
-            // Swallow the exception
-            return false;
-        }
-    }
+    private static final boolean DEBUG = Boolean.getBoolean(HotSpotGraalRuntimeMBean.class.getSimpleName() + ".debug");
 
     @Override
     public Object getAttribute(String name) throws AttributeNotFoundException {
