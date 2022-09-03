6957b860447f20554f44978a88a54339d693fc12
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 07:18:37 +0200

    Support interop objects in LLVMAMD64WriteNode

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/asm/support/LLVMAMD64WriteNode.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/asm/support/LLVMAMD64WriteNode.java
@@ -71,57 +71,57 @@ public abstract class LLVMAMD64WriteNode extends Node {
     }
 
     @Specialization(guards = "!isFrameSlot(addr)")
-    protected void doI8(Object addr, byte value) {
-        storeI8.executeWithTarget(addr, value);
+    protected void executeI8(VirtualFrame frame, Object addr, byte value) {
+        storeI8.executeWithTarget(frame, addr, value);
     }
 
     @Specialization(guards = "!isFrameSlot(addr)")
-    protected void doI16(Object addr, short value) {
-        storeI16.executeWithTarget(addr, value);
+    protected void executeI16(VirtualFrame frame, Object addr, short value) {
+        storeI16.executeWithTarget(frame, addr, value);
     }
 
     @Specialization(guards = "!isFrameSlot(addr)")
-    protected void doI32(Object addr, int value) {
-        storeI32.executeWithTarget(addr, value);
+    protected void executeI32(VirtualFrame frame, Object addr, int value) {
+        storeI32.executeWithTarget(frame, addr, value);
     }
 
     @Specialization(guards = "!isFrameSlot(addr)")
-    protected void doI64(Object addr, long value) {
-        storeI64.executeWithTarget(addr, value);
+    protected void executeI64(VirtualFrame frame, Object addr, long value) {
+        storeI64.executeWithTarget(frame, addr, value);
     }
 
     @Specialization(guards = "!isFrameSlot(addr)")
-    protected void doObject(Object addr, Object value) {
-        storeAddress.executeWithTarget(addr, value);
+    protected void executeObject(VirtualFrame frame, Object addr, Object value) {
+        storeAddress.executeWithTarget(frame, addr, value);
     }
 
     @Specialization
-    protected void doI8(VirtualFrame frame, FrameSlot slot, byte value) {
+    protected void executeI8(VirtualFrame frame, FrameSlot slot, byte value) {
         long reg = readRegister.execute(frame, slot);
         long val = (reg & mask) | (Byte.toUnsignedLong(value) << shift);
         frame.setLong(slot, val);
     }
 
     @Specialization
-    protected void doI16(VirtualFrame frame, FrameSlot slot, short value) {
+    protected void executeI16(VirtualFrame frame, FrameSlot slot, short value) {
         long reg = readRegister.execute(frame, slot);
         long val = (reg & MASK_16) | Short.toUnsignedLong(value);
         frame.setLong(slot, val);
     }
 
     @Specialization
-    protected void doI32(VirtualFrame frame, FrameSlot slot, int value) {
+    protected void executeI32(VirtualFrame frame, FrameSlot slot, int value) {
         long val = Integer.toUnsignedLong(value);
         frame.setLong(slot, val);
     }
 
     @Specialization
-    protected void doI64(VirtualFrame frame, FrameSlot slot, long value) {
+    protected void executeI64(VirtualFrame frame, FrameSlot slot, long value) {
         frame.setLong(slot, value);
     }
 
     @Specialization
-    protected void doAddress(VirtualFrame frame, FrameSlot slot, LLVMAddress value) {
+    protected void executeAddress(VirtualFrame frame, FrameSlot slot, LLVMAddress value) {
         frame.setLong(slot, value.getVal());
     }
 
