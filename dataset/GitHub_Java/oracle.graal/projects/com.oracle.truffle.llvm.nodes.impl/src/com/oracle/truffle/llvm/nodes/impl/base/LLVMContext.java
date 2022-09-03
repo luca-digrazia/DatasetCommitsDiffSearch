70c0c9618beae5e369ca9f5a6a1b82bd8bf7aef2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:58:04 +0200

    Introduce an option to make Sulong only parse a file and not execute it

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/base/LLVMContext.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/base/LLVMContext.java
@@ -34,6 +34,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.oracle.nfi.api.NativeFunctionHandle;
+import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.ExecutionContext;
 import com.oracle.truffle.api.RootCallTarget;
@@ -65,6 +66,7 @@ public class LLVMContext extends ExecutionContext {
     public LLVMContext(NodeFactoryFacade facade, LLVMOptimizationConfiguration optimizationConfig) {
         nativeLookup = new NativeLookup(facade);
         this.registry = new LLVMFunctionRegistry(optimizationConfig, facade);
+        setLastContext(this);
     }
 
     public RootCallTarget getFunction(LLVMFunctionDescriptor function) {
@@ -122,6 +124,22 @@ public class LLVMContext extends ExecutionContext {
 
     public Source getSourceFile() {
         return sourceFile;
+    }
+
+    // TODO No static access to this class from LLVMFunction at the moment
+
+    private static LLVMContext lastContext;
+
+    public static CallTarget getCallTarget(LLVMFunctionDescriptor function) {
+        return lastContext.registry.lookup(function);
+    }
+
+    private static void setLastContext(LLVMContext context) {
+        lastContext = context;
+    }
+
+    public static LLVMStack getStaticStack() {
+        return lastContext.stack;
     }
 
     public void registerStaticDestructor(RootCallTarget staticDestructor) {
