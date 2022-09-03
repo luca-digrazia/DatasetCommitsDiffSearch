d6042f871e16799bd553b8f973e53eede5853697
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:00:13 +0200

    ArrayCopyNode: cleanup, unrolling for fixed length arraycopy

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/snippets/ArrayCopyNode.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/snippets/ArrayCopyNode.java
@@ -77,12 +77,8 @@ public class ArrayCopyNode extends MacroNode implements Virtualizable, IterableN
     }
 
     private static void unrollFixedLengthLoop(StructuredGraph snippetGraph, int length, LoweringTool tool) {
-        LocalNode lengthLocal = snippetGraph.getLocal(4);
-        if (lengthLocal != null) {
-            snippetGraph.replaceFloating(lengthLocal, ConstantNode.forInt(length, snippetGraph));
-        }
-        // the canonicalization before loop unrolling is needed to propagate the length into
-        // additions, etc.
+        snippetGraph.replaceFloating(snippetGraph.getLocal(4), ConstantNode.forInt(length, snippetGraph));
+        // the canonicalization before loop unrolling is needed to propagate the length into additions, etc.
         new CanonicalizerPhase(tool.getTarget(), tool.getRuntime(), tool.assumptions()).apply(snippetGraph);
         new LoopFullUnrollPhase(tool.getRuntime(), tool.assumptions()).apply(snippetGraph);
         new CanonicalizerPhase(tool.getTarget(), tool.getRuntime(), tool.assumptions()).apply(snippetGraph);
@@ -93,8 +89,8 @@ public class ArrayCopyNode extends MacroNode implements Virtualizable, IterableN
         ResolvedJavaMethod snippetMethod = selectSnippet(tool);
         if (snippetMethod == null) {
             snippetMethod = tool.getRuntime().lookupJavaMethod(ArrayCopySnippets.increaseGenericCallCounterMethod);
-            // we will call the generic method. the generic snippet will only increase the counter,
-            // not call the actual method. therefore we create a second invoke here.
+            // we will call the generic method. the generic snippet will only increase the counter, not call the actual
+            // method. therefore we create a second invoke here.
             ((StructuredGraph) graph()).addAfterFixed(this, createInvoke());
         }
         if (Debug.isLogEnabled()) {
