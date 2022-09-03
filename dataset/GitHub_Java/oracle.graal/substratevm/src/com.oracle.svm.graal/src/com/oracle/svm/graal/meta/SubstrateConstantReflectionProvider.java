089c1823753423b908ed23fb5953b207ee99bc85
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:22:32 +0200

    SubstrateConstantReflectionProvider#readFieldValue must not propagate IllegalArgumentException

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/meta/SubstrateConstantReflectionProvider.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/meta/SubstrateConstantReflectionProvider.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -98,7 +96,7 @@ public class SubstrateConstantReflectionProvider extends SharedConstantReflectio
 
         assert SubstrateObjectConstant.asObject(base) != null;
         try {
-            return SubstrateMemoryAccessProviderImpl.SINGLETON.readUnsafeConstant(field.type.getStorageKind(), base, field.location, field.isVolatile());
+            return SubstrateMemoryAccessProviderImpl.SINGLETON.readUnsafeConstant(field.type.getStorageKind(), base, field.location);
         } catch (IllegalArgumentException e) {
             return null;
         }
