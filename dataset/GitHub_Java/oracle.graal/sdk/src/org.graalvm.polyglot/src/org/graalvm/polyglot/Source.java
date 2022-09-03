d87b5c6b48b609c7f6afd83c53c39376c1c63aae
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 08:32:16 +0200

    Breaking: refactor source to include IOException again. Migrate javadoc from Truffle source API.

--- dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/Source.java
+++ dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/Source.java
@@ -381,9 +381,9 @@ public final class Source {
         return newBuilder(source).setName(name).build();
     }
 
-    public static final class LiteralBuilder extends Builder {
+    public static class LiteralBuilder extends Builder {
 
-        LiteralBuilder(CharSequence origin) {
+        private LiteralBuilder(CharSequence origin) {
             super(origin);
         }
 
@@ -462,7 +462,7 @@ public final class Source {
         private boolean internal;
         private String content;
 
-        Builder(Object origin) {
+        private Builder(Object origin) {
             this.origin = origin;
         }
 
