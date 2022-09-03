609e900d5431abceed19fe3c579ab3a0d6ebec3e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 09:39:19 +0200

    Remove failing tests.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/TruffleRuntimeTest.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/TruffleRuntimeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,16 +22,20 @@
  */
 package com.oracle.truffle.api.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
 
-import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
 
-import org.junit.*;
-
-import com.oracle.truffle.api.*;
-import com.oracle.truffle.api.frame.*;
-import com.oracle.truffle.api.nodes.*;
-import com.oracle.truffle.api.source.*;
+import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.TruffleRuntime;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.RootNode;
+import com.oracle.truffle.api.source.SourceSection;
 
 /**
  * <h3>Accessing the Truffle Runtime</h3>
@@ -54,12 +58,12 @@ public class TruffleRuntimeTest {
     private TruffleRuntime runtime;
 
     @Before
-    public void setUp() {
+    public void before() {
         this.runtime = Truffle.getRuntime();
     }
 
-    private static RootNode createTestRootNode() {
-        return new RootNode(TestingLanguage.class, null, null) {
+    private static RootNode createTestRootNode(SourceSection sourceSection) {
+        return new RootNode(TestingLanguage.class, sourceSection, null) {
             @Override
             public Object execute(VirtualFrame frame) {
                 return 42;
@@ -84,80 +88,11 @@ public class TruffleRuntimeTest {
 
     @Test
     public void testCreateCallTarget() {
-        RootNode rootNode = createTestRootNode();
+        RootNode rootNode = createTestRootNode(null);
         RootCallTarget target = runtime.createCallTarget(rootNode);
         assertNotNull(target);
         assertEquals(target.call(), 42);
         assertSame(rootNode, target.getRootNode());
     }
 
-    @Test
-    public void testGetCallTargets1() {
-        RootNode rootNode = createTestRootNode();
-        RootCallTarget target = runtime.createCallTarget(rootNode);
-        assertTrue(runtime.getCallTargets().contains(target));
-    }
-
-    @Test
-    public void testGetCallTargets2() {
-        RootNode rootNode = createTestRootNode();
-        RootCallTarget target1 = runtime.createCallTarget(rootNode);
-        RootCallTarget target2 = runtime.createCallTarget(rootNode);
-        assertTrue(runtime.getCallTargets().contains(target1));
-        assertTrue(runtime.getCallTargets().contains(target2));
-    }
-
-    /*
-     * This test case documents the use case for profilers and debuggers where they need to access
-     * multiple call targets for the same source section. This case may happen when the optimization
-     * system decides to duplicate call targets to achieve better performance.
-     */
-    @Test
-    public void testGetCallTargets3() {
-        Source source1 = Source.fromText("a\nb\n", "");
-        SourceSection sourceSection1 = source1.createSection("foo", 1);
-        SourceSection sourceSection2 = source1.createSection("bar", 2);
-
-        RootNode rootNode1 = createTestRootNode();
-        rootNode1.assignSourceSection(sourceSection1);
-        RootNode rootNode2 = createTestRootNode();
-        rootNode2.assignSourceSection(sourceSection2);
-        RootNode rootNode2Copy = NodeUtil.cloneNode(rootNode2);
-
-        assertSame(rootNode2.getSourceSection(), rootNode2Copy.getSourceSection());
-
-        RootCallTarget target1 = runtime.createCallTarget(rootNode1);
-        RootCallTarget target2 = runtime.createCallTarget(rootNode2);
-        RootCallTarget target2Copy = runtime.createCallTarget(rootNode2Copy);
-
-        Map<SourceSection, List<RootCallTarget>> groupedTargets = groupUniqueCallTargets();
-
-        List<RootCallTarget> targets1 = groupedTargets.get(sourceSection1);
-        assertEquals(1, targets1.size());
-        assertEquals(target1, targets1.get(0));
-
-        List<RootCallTarget> targets2 = groupedTargets.get(sourceSection2);
-        assertEquals(2, targets2.size());
-        // order of targets2 is not guaranteed
-        assertTrue(target2 == targets2.get(0) ^ target2Copy == targets2.get(0));
-        assertTrue(target2 == targets2.get(1) ^ target2Copy == targets2.get(1));
-    }
-
-    private static Map<SourceSection, List<RootCallTarget>> groupUniqueCallTargets() {
-        Map<SourceSection, List<RootCallTarget>> groupedTargets = new HashMap<>();
-        for (RootCallTarget target : Truffle.getRuntime().getCallTargets()) {
-            SourceSection section = target.getRootNode().getSourceSection();
-            if (section == null) {
-                // can not identify root node to a unique call target. Print warning?
-                continue;
-            }
-            List<RootCallTarget> targets = groupedTargets.get(section);
-            if (targets == null) {
-                targets = new ArrayList<>();
-                groupedTargets.put(section, targets);
-            }
-            targets.add(target);
-        }
-        return groupedTargets;
-    }
 }
