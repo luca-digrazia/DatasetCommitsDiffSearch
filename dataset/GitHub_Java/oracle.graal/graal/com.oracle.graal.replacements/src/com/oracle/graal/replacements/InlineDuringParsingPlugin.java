8160e7d83eacd0f0ac4e7fe04dfe5937d4d2a97b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:48:48 +0200

    apply forced inlining during bytecode parsing

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/InlineDuringParsingPlugin.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/InlineDuringParsingPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,20 +22,46 @@
  */
 package com.oracle.graal.replacements;
 
-import static com.oracle.graal.compiler.common.GraalOptions.*;
-import static com.oracle.graal.java.BytecodeParser.Options.*;
+import static com.oracle.graal.compiler.common.GraalOptions.TrivialInliningSize;
+import static com.oracle.graal.java.BytecodeParserOptions.InlineDuringParsingMaxDepth;
 
-import com.oracle.graal.graphbuilderconf.*;
-import com.oracle.graal.nodes.*;
-import com.oracle.jvmci.meta.*;
+import com.oracle.graal.nodes.ValueNode;
+import com.oracle.graal.nodes.graphbuilderconf.GraphBuilderContext;
+import com.oracle.graal.nodes.graphbuilderconf.InlineInvokePlugin;
+
+import jdk.vm.ci.meta.ResolvedJavaMethod;
 
 public final class InlineDuringParsingPlugin implements InlineInvokePlugin {
 
     @Override
-    public InlineInfo shouldInlineInvoke(GraphBuilderContext b, ResolvedJavaMethod method, ValueNode[] args, JavaType returnType) {
-        if (method.hasBytecodes() && !method.isSynchronized() && method.getCode().length <= TrivialInliningSize.getValue() && b.getDepth() < InlineDuringParsingMaxDepth.getValue()) {
-            return new InlineInfo(method, false);
+    public InlineInfo shouldInlineInvoke(GraphBuilderContext b, ResolvedJavaMethod method, ValueNode[] args) {
+        // @formatter:off
+        if (method.hasBytecodes() &&
+            method.getDeclaringClass().isLinked() &&
+            method.canBeInlined()) {
+
+            // Test force inlining first
+            if (method.shouldBeInlined()) {
+                return new InlineInfo(method, false);
+            }
+
+            if (!method.isSynchronized() &&
+                checkSize(method, args) &&
+                b.getDepth() < InlineDuringParsingMaxDepth.getValue()) {
+                return new InlineInfo(method, false);
+            }
         }
+        // @formatter:on
         return null;
+    }
+
+    private static boolean checkSize(ResolvedJavaMethod method, ValueNode[] args) {
+        int bonus = 1;
+        for (ValueNode v : args) {
+            if (v.isConstant()) {
+                bonus++;
+            }
+        }
+        return method.getCode().length <= TrivialInliningSize.getValue() * bonus;
     }
 }
