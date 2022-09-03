13d50db4be4f98c3d77ba51cd672d8817c4d967a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 20:31:10 +0200

    Truffle-DSL: added default buffering to all java source code that is written. JavaFileObject#openWriter may return a non-buffered writer.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/AnnotationProcessor.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/AnnotationProcessor.java
@@ -22,17 +22,18 @@
  */
 package com.oracle.truffle.dsl.processor;
 
+import java.io.*;
 import java.util.*;
 
+import javax.annotation.processing.*;
 import javax.lang.model.element.*;
 import javax.lang.model.type.*;
+import javax.tools.*;
 
-import com.oracle.truffle.dsl.processor.generator.*;
-import com.oracle.truffle.dsl.processor.java.*;
-import com.oracle.truffle.dsl.processor.java.model.*;
-import com.oracle.truffle.dsl.processor.java.transform.*;
-import com.oracle.truffle.dsl.processor.model.*;
-import com.oracle.truffle.dsl.processor.parser.*;
+import com.oracle.truffle.dsl.processor.ast.*;
+import com.oracle.truffle.dsl.processor.codewriter.*;
+import com.oracle.truffle.dsl.processor.compiler.*;
+import com.oracle.truffle.dsl.processor.template.*;
 
 /**
  * THIS IS NOT PUBLIC API.
@@ -40,11 +41,13 @@ import com.oracle.truffle.dsl.processor.parser.*;
 class AnnotationProcessor<M extends Template> {
 
     private final AbstractParser<M> parser;
-    private final AbstractCompilationUnitFactory<M> factory;
+    private final CompilationUnitFactory<M> factory;
+    private final ProcessorContext context;
 
     private final Set<String> processedElements = new HashSet<>();
 
-    public AnnotationProcessor(AbstractParser<M> parser, AbstractCompilationUnitFactory<M> factory) {
+    public AnnotationProcessor(ProcessorContext context, AbstractParser<M> parser, CompilationUnitFactory<M> factory) {
+        this.context = context;
         this.parser = parser;
         this.factory = factory;
     }
@@ -53,19 +56,23 @@ class AnnotationProcessor<M extends Template> {
         return parser;
     }
 
+    public ProcessorContext getContext() {
+        return context;
+    }
+
     @SuppressWarnings({"unchecked"})
-    public void process(Element element, boolean callback) {
+    public void process(RoundEnvironment env, Element element, boolean callback) {
+
         // since it is not guaranteed to be called only once by the compiler
         // we check for already processed elements to avoid errors when writing files.
         if (!callback && element instanceof TypeElement) {
-            String qualifiedName = ElementUtils.getQualifiedName((TypeElement) element);
+            String qualifiedName = Utils.getQualifiedName((TypeElement) element);
             if (processedElements.contains(qualifiedName)) {
                 return;
             }
             processedElements.add(qualifiedName);
         }
 
-        ProcessorContext context = ProcessorContext.getInstance();
         TypeElement type = (TypeElement) element;
 
         M model = (M) context.getTemplate(type.asType(), false);
@@ -73,7 +80,7 @@ class AnnotationProcessor<M extends Template> {
 
         if (firstRun || !callback) {
             context.registerTemplate(type, null);
-            model = parser.parse(element);
+            model = parser.parse(env, element);
             context.registerTemplate(type, model);
 
             if (model != null) {
@@ -84,13 +91,42 @@ class AnnotationProcessor<M extends Template> {
                 DeclaredType overrideType = (DeclaredType) context.getType(Override.class);
                 DeclaredType unusedType = (DeclaredType) context.getType(SuppressWarnings.class);
                 unit.accept(new GenerateOverrideVisitor(overrideType), null);
-                unit.accept(new FixWarningsVisitor(context.getEnvironment(), unusedType, overrideType), null);
+                unit.accept(new FixWarningsVisitor(context, unusedType, overrideType), null);
 
                 if (!callback) {
                     unit.accept(new CodeWriter(context.getEnvironment(), element), null);
                 }
             }
         }
+    }
+
+    private static class CodeWriter extends AbstractCodeWriter {
+
+        private final Element originalElement;
+        private final ProcessingEnvironment env;
+
+        public CodeWriter(ProcessingEnvironment env, Element originalElement) {
+            this.env = env;
+            this.originalElement = originalElement;
+        }
+
+        @Override
+        protected Writer createWriter(CodeTypeElement clazz) throws IOException {
+            JavaFileObject jfo = env.getFiler().createSourceFile(clazz.getQualifiedName(), originalElement);
+            return new BufferedWriter(jfo.openWriter());
+        }
+
+        @Override
+        protected void writeHeader() {
+            if (env == null) {
+                return;
+            }
+            String comment = CompilerFactory.getCompiler(originalElement).getHeaderComment(env, originalElement);
+            if (comment != null) {
+                writeLn(comment);
+            }
+        }
+
     }
 
 }
