5add1fdf4f516bd70b10fc10245ee826075ebd31
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 11:38:06 +0200

    Wrap POJOs as TruffleObjects

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/ToJavaNode.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/ToJavaNode.java
@@ -141,17 +141,8 @@ final class ToJavaNode extends Node {
         @Override
         public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
             Object ret;
-            if (method.isVarArgs()) {
-                if (arguments.length == 1) {
-                    ret = call((Object[]) arguments[0]);
-                } else {
-                    final int allButOne = arguments.length - 1;
-                    Object[] last = (Object[]) arguments[allButOne];
-                    Object[] merge = new Object[allButOne + last.length];
-                    System.arraycopy(arguments, 0, merge, 0, allButOne);
-                    System.arraycopy(last, 0, merge, allButOne, last.length);
-                    ret = call(merge);
-                }
+            if (method.isVarArgs() && arguments.length == 1) {
+                ret = call((Object[]) arguments[0]);
             } else {
                 ret = call(arguments);
             }
