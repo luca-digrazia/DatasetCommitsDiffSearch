196ffaa3acac83b75befc40acba46b956e591860
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:10:01 +0200

    Fixed javadoc text

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases/src/com/oracle/graal/phases/tiers/SuitesProvider.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases/src/com/oracle/graal/phases/tiers/SuitesProvider.java
@@ -22,24 +22,32 @@
  */
 package com.oracle.graal.phases.tiers;
 
-import com.oracle.graal.phases.*;
+import com.oracle.graal.lir.phases.LIRSuites;
+import com.oracle.graal.phases.PhaseSuite;
+
+/**
+ * Main interface providing access to suites used for compilation.
+ */
 
 public interface SuitesProvider {
 
     /**
-     * Get the default phase suites of this compiler.
+     * Get the default phase suites of this compiler. This will take in account any options enabled
+     * at the time of call, returning an appropriately constructed suite. The returned suite is
+     * immutable by default but {@link Suites#copy} can be used to create a customized version.
      */
     Suites getDefaultSuites();
-
-    /**
-     * Create a new set of phase suites. Initially, the suites are the same as the
-     * {@link #getDefaultSuites default} suites.
-     */
-    Suites createSuites();
 
     /**
      * Get the default phase suite for creating new graphs.
      */
     PhaseSuite<HighTierContext> getDefaultGraphBuilderSuite();
 
+    /**
+     * Get the default LIR phase suites of this compiler. This will take in account any options
+     * enabled at the time of call, returning an appropriately constructed suite. The returned suite
+     * is immutable by default but {@link LIRSuites#copy} can be used to create a customized
+     * version.
+     */
+    LIRSuites getDefaultLIRSuites();
 }
