3a988843cb628c2875340399ac75c0a9ba835b79
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 09:26:34 +0200

    [GR-3484] MoveGuardsUpwards should move guards above loops.

--- dataset/GitHub_Java/oracle.graal/graal/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/NewConditionalEliminationPhase.java
+++ dataset/GitHub_Java/oracle.graal/graal/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/NewConditionalEliminationPhase.java
@@ -948,10 +948,6 @@ public class NewConditionalEliminationPhase extends BasePhase<PhaseContext> {
 
         @Override
         public Node apply(Node node, Node curNode) {
-            if (!ok) {
-                // Abort the recursion
-                return curNode;
-            }
             if (!(curNode instanceof ValueNode)) {
                 ok = false;
                 return curNode;
