d7b50ebaae33d4da46d6317edd940c206bea0c0c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:51:03 +0200

    Removing useless null check

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/JavaInterop.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/JavaInterop.java
@@ -335,29 +335,21 @@ public final class JavaInterop {
             }
 
             if (message == null) {
-                Object ret;
-                try {
-                    List<Object> callArgs = new ArrayList<>(args.length);
-                    callArgs.add(name);
-                    callArgs.addAll(Arrays.asList(args));
-                    ret = message(Message.createInvoke(callArgs.size()), obj, callArgs.toArray());
-                } catch (IllegalArgumentException ex) {
-                    val = message(Message.READ, obj, name);
-                    if (isPrimitive(val)) {
-                        return val;
-                    }
-                    TruffleObject attr = (TruffleObject) val;
-                    if (Boolean.FALSE.equals(message(Message.IS_EXECUTABLE, attr))) {
-                        if (args.length == 0) {
-                            return toJava(attr, method);
-                        }
-                        throw new IllegalStateException(attr + " cannot be invoked with " + args.length + " parameters");
-                    }
-                    List<Object> callArgs = new ArrayList<>(args.length + 1);
-                    // callArgs.add(attr);
-                    callArgs.addAll(Arrays.asList(args));
-                    ret = message(Message.createExecute(callArgs.size()), attr, callArgs.toArray());
+                val = message(Message.READ, obj, name);
+                if (isPrimitive(val)) {
+                    return val;
                 }
+                TruffleObject attr = (TruffleObject) val;
+                if (Boolean.FALSE.equals(message(Message.IS_EXECUTABLE, attr))) {
+                    if (args.length == 0) {
+                        return toJava(attr, method);
+                    }
+                    throw new IllegalStateException(attr + " cannot be invoked with " + args.length + " parameters");
+                }
+                List<Object> callArgs = new ArrayList<>(args.length + 1);
+                // callArgs.add(attr);
+                callArgs.addAll(Arrays.asList(args));
+                Object ret = message(Message.createExecute(callArgs.size()), attr, callArgs.toArray());
                 return toJava(ret, method);
             }
             throw new IllegalStateException("Unknown message: " + message);
