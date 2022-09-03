40931d82815a11b1eb61adc9a8886dc724f46cc8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 21:49:55 +0200

    more javadoc for debug framework

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.debug/src/com/oracle/graal/debug/internal/DebugValue.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.debug/src/com/oracle/graal/debug/internal/DebugValue.java
@@ -30,12 +30,10 @@ public abstract class DebugValue implements Comparable<DebugValue> {
 
     private final String name;
     private int index;
-    private boolean conditional;
 
-    protected DebugValue(String name, boolean conditional) {
+    protected DebugValue(String name) {
         this.name = name;
         this.index = -1;
-        this.conditional = conditional;
     }
 
     protected long getCurrentValue() {
@@ -46,14 +44,6 @@ public abstract class DebugValue implements Comparable<DebugValue> {
     protected void setCurrentValue(long l) {
         ensureInitialized();
         DebugScope.getInstance().setCurrentValue(index, l);
-    }
-
-    public void setConditional(boolean flag) {
-        conditional = flag;
-    }
-
-    public boolean isConditional() {
-        return conditional;
     }
 
     private void ensureInitialized() {
--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.debug/src/com/oracle/graal/debug/internal/DebugValueMap.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.debug/src/com/oracle/graal/debug/internal/DebugValueMap.java
@@ -55,15 +55,6 @@ public class DebugValueMap {
         }
     }
 
-    public void reset() {
-        Arrays.fill(values, 0L);
-        if (children != null) {
-            for (DebugValueMap child : children) {
-                child.reset();
-            }
-        }
-    }
-
     private void ensureSize(int index) {
         if (values == null) {
             values = new long[index + 1];
