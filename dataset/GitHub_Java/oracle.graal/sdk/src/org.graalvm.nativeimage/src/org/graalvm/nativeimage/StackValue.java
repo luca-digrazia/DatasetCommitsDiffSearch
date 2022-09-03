cf1a95dac39c95986b2c5458bd88c7d0aec769d8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 03:47:25 +0200

    [GR-11384] Wrong implementation of StackValue.get.

--- dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.nativeimage/src/org/graalvm/nativeimage/StackValue.java
+++ dataset/GitHub_Java/oracle.graal/sdk/src/org.graalvm.nativeimage/src/org/graalvm/nativeimage/StackValue.java
@@ -1,42 +1,26 @@
 /*
- * Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * The Universal Permissive License (UPL), Version 1.0
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * (a) the Software, and
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package org.graalvm.nativeimage;
 
@@ -49,7 +33,7 @@ import org.graalvm.word.PointerBase;
 /**
  * Contains static methods for memory allocation in the stack frame.
  *
- * @since 19.0
+ * @since 1.0
  */
 public final class StackValue {
 
@@ -70,7 +54,7 @@ public final class StackValue {
      * @param structType the requested structure class - must be a compile time constant
      * @return pointer to on-stack allocated location for the requested structure
      *
-     * @since 19.0
+     * @since 1.0
      */
     public static <T extends PointerBase> T get(Class<T> structType) {
         throw new IllegalStateException("Cannot invoke method during native image generation");
@@ -92,7 +76,7 @@ public final class StackValue {
      * @param structType the requested structure class - must be a compile time constant
      * @return pointer to on-stack allocated location for the requested structure
      *
-     * @since 19.0
+     * @since 1.0
      */
     public static <T extends PointerBase> T get(int numberOfElements, Class<T> structType) {
         throw new IllegalStateException("Cannot invoke method during native image generation");
@@ -107,7 +91,7 @@ public final class StackValue {
      * frame when the method starts execution. The memory is not initialized. Two distinct calls of
      * this method return different pointers.
      *
-     * @since 19.0
+     * @since 1.0
      */
     @SuppressWarnings("unused")
     public static <T extends PointerBase> T get(int size) {
@@ -117,7 +101,7 @@ public final class StackValue {
     /**
      * Utility method that performs size arithmetic, otherwise equivalent to {@link #get(int)}.
      *
-     * @since 19.0
+     * @since 1.0
      */
     @SuppressWarnings("unused")
     public static <T extends PointerBase> T get(int numberOfElements, int elementSize) {
