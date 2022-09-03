cc67db1dbec796bcfbbc94fe399d7477a6576184
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:50:56 +0200

    Fix IfNodeCanonicalizationTest

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/ConvertDeoptimizeToGuardPhase.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/ConvertDeoptimizeToGuardPhase.java
@@ -162,7 +162,7 @@ public class ConvertDeoptimizeToGuardPhase extends BasePhase<PhaseContext> {
         while (current != null) {
             if (GraalOptions.GuardPriorities.getValue(from.getOptions()) && current instanceof FixedGuardNode) {
                 FixedGuardNode otherGuard = (FixedGuardNode) current;
-                if (otherGuard.computePriority().compareTo(deopt.computePriority()) < 0) {
+                if (otherGuard.computePriority().ordinal() < deopt.computePriority().ordinal()) {
                     moveAsDeoptAfter(otherGuard, deopt);
                     return;
                 }
