2acba29026f7b44cfc5c03ab306e6f312a620708
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 02:29:56 +0200

    Fix style comments.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/nodes/MacroNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/nodes/MacroNode.java
@@ -60,7 +60,6 @@ import org.graalvm.compiler.phases.tiers.PhaseContext;
 
 import jdk.vm.ci.meta.JavaKind;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
-import org.graalvm.word.LocationIdentity;
 
 /**
  * Macro nodes can be used to temporarily replace an invoke. They can, for example, be used to
@@ -228,13 +227,9 @@ public abstract class MacroNode extends FixedWithNextNode implements Lowerable, 
         }
     }
 
-    public LocationIdentity getLocationIdentity() {
-        return LocationIdentity.any();
-    }
-
     protected InvokeNode createInvoke() {
         MethodCallTargetNode callTarget = graph().add(new MethodCallTargetNode(invokeKind, targetMethod, arguments.toArray(new ValueNode[arguments.size()]), returnStamp, null));
-        InvokeNode invoke = graph().add(new InvokeNode(callTarget, bci, getLocationIdentity()));
+        InvokeNode invoke = graph().add(new InvokeNode(callTarget, bci));
         if (stateAfter() != null) {
             invoke.setStateAfter(stateAfter().duplicate());
             if (getStackKind() != JavaKind.Void) {
