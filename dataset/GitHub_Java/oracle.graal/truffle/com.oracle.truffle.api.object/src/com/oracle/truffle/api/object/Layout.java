2678cfd2ba4e6a99d615753f7c3e81dcc40c67a7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 12:18:35 +0200

    Attempt to load LayoutFactory via TruffleRuntime

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.object/src/com/oracle/truffle/api/object/Layout.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api.object/src/com/oracle/truffle/api/object/Layout.java
@@ -24,52 +24,105 @@
  */
 package com.oracle.truffle.api.object;
 
-import java.util.*;
+import java.util.EnumSet;
+import java.util.ServiceLoader;
 
-import com.oracle.truffle.api.nodes.NodeUtil.FieldOffsetProvider;
+import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.object.Shape.Allocator;
 
+/**
+ * Describes layout and behavior of a {@link DynamicObject} subclass and is used to create shapes.
+ *
+ * An object may change its shape but only to shapes of the same layout.
+ *
+ * @since 0.8 or earlier
+ */
 public abstract class Layout {
-    public static final EnumSet<ImplicitCast> NONE = EnumSet.noneOf(ImplicitCast.class);
-    public static final EnumSet<ImplicitCast> INT_TO_DOUBLE = EnumSet.of(ImplicitCast.IntToDouble);
-    public static final EnumSet<ImplicitCast> INT_TO_LONG = EnumSet.of(ImplicitCast.IntToLong);
-
+    /** @since 0.8 or earlier */
     public static final String OPTION_PREFIX = "truffle.object.";
 
     private static final LayoutFactory LAYOUT_FACTORY = loadLayoutFactory();
 
+    /**
+     * Constructor for subclasses.
+     *
+     * @since 0.8 or earlier
+     */
+    protected Layout() {
+    }
+
+    /**
+     * Specifies the allowed implicit casts between primitive types without losing type information.
+     *
+     * @since 0.8 or earlier
+     */
     public enum ImplicitCast {
+        /** @since 0.8 or earlier */
         IntToDouble,
-        IntToLong,
+        /** @since 0.8 or earlier */
+        IntToLong
     }
 
+    /**
+     * Creates a new {@link Builder}.
+     *
+     * @since 0.8 or earlier
+     */
+    public static Builder newLayout() {
+        return new Builder();
+    }
+
+    /**
+     * Equivalent to {@code Layout.newLayout().build()}.
+     *
+     * @since 0.8 or earlier
+     */
     public static Layout createLayout() {
-        return createLayout(NONE);
+        return newLayout().build();
     }
 
-    public static Layout createLayout(EnumSet<ImplicitCast> allowedImplicitCasts) {
-        return new LayoutBuilder().setAllowedImplicitCasts(allowedImplicitCasts).build();
-    }
-
-    public static Layout createLayout(EnumSet<ImplicitCast> allowedImplicitCasts, FieldOffsetProvider fieldOffsetProvider) {
-        return new LayoutBuilder().setAllowedImplicitCasts(allowedImplicitCasts).setFieldOffsetProvider(fieldOffsetProvider).build();
-    }
-
+    /** @since 0.8 or earlier */
     public abstract DynamicObject newInstance(Shape shape);
 
+    /** @since 0.8 or earlier */
     public abstract Class<? extends DynamicObject> getType();
 
-    public abstract Shape createShape(ObjectType operations);
+    /**
+     * Create a root shape.
+     *
+     * @param objectType that describes the object instance with this shape.
+     * @since 0.8 or earlier
+     */
+    public abstract Shape createShape(ObjectType objectType);
 
-    public abstract Shape createShape(ObjectType operations, Object sharedData);
+    /**
+     * Create a root shape.
+     *
+     * @param objectType that describes the object instance with this shape.
+     * @param sharedData for language-specific use
+     * @since 0.8 or earlier
+     */
+    public abstract Shape createShape(ObjectType objectType, Object sharedData);
 
-    public abstract Shape createShape(ObjectType operations, Object sharedData, int id);
+    /**
+     * Create a root shape.
+     *
+     * @param objectType that describes the object instance with this shape.
+     * @param sharedData for language-specific use
+     * @param id for language-specific use
+     * @return new instance of a shape
+     * @since 0.8 or earlier
+     */
+    public abstract Shape createShape(ObjectType objectType, Object sharedData, int id);
 
     /**
      * Create an allocator for static property creation. Reserves all array extension slots.
+     *
+     * @since 0.8 or earlier
      */
     public abstract Allocator createAllocator();
 
+    /** @since 0.8 or earlier */
     protected static LayoutFactory getFactory() {
         return LAYOUT_FACTORY;
     }
@@ -91,13 +144,16 @@ public abstract class Layout {
                 throw new AssertionError(e);
             }
         } else {
-            ServiceLoader<LayoutFactory> serviceLoader = ServiceLoader.load(LayoutFactory.class, Layout.class.getClassLoader());
-            for (LayoutFactory currentLayoutFactory : serviceLoader) {
-                if (bestLayoutFactory == null) {
-                    bestLayoutFactory = currentLayoutFactory;
-                } else if (currentLayoutFactory.getPriority() >= bestLayoutFactory.getPriority()) {
-                    assert currentLayoutFactory.getPriority() != bestLayoutFactory.getPriority();
-                    bestLayoutFactory = currentLayoutFactory;
+            bestLayoutFactory = Truffle.getRuntime().getCapability(LayoutFactory.class);
+            if (bestLayoutFactory == null) {
+                ServiceLoader<LayoutFactory> serviceLoader = ServiceLoader.load(LayoutFactory.class, Layout.class.getClassLoader());
+                for (LayoutFactory currentLayoutFactory : serviceLoader) {
+                    if (bestLayoutFactory == null) {
+                        bestLayoutFactory = currentLayoutFactory;
+                    } else if (currentLayoutFactory.getPriority() >= bestLayoutFactory.getPriority()) {
+                        assert currentLayoutFactory.getPriority() != bestLayoutFactory.getPriority();
+                        bestLayoutFactory = currentLayoutFactory;
+                    }
                 }
             }
         }
@@ -106,5 +162,74 @@ public abstract class Layout {
             throw new AssertionError("LayoutFactory not found");
         }
         return bestLayoutFactory;
+    }
+
+    /**
+     * Layout builder.
+     *
+     * @see Layout
+     * @since 0.8 or earlier
+     */
+    public static final class Builder {
+        private EnumSet<ImplicitCast> allowedImplicitCasts;
+        private boolean polymorphicUnboxing;
+
+        /**
+         * Create a new layout builder.
+         */
+        private Builder() {
+            this.allowedImplicitCasts = EnumSet.noneOf(ImplicitCast.class);
+        }
+
+        /**
+         * Build {@link Layout} from the configuration in this builder.
+         *
+         * @since 0.8 or earlier
+         */
+        public Layout build() {
+            return Layout.getFactory().createLayout(this);
+        }
+
+        /**
+         * Set the allowed implicit casts in this layout.
+         *
+         * @see Layout.ImplicitCast
+         * @since 0.8 or earlier
+         */
+        public Builder setAllowedImplicitCasts(EnumSet<ImplicitCast> allowedImplicitCasts) {
+            this.allowedImplicitCasts = allowedImplicitCasts;
+            return this;
+        }
+
+        /**
+         * Add an allowed implicit cast in this layout.
+         *
+         * @see Layout.ImplicitCast
+         * @since 0.8 or earlier
+         */
+        public Builder addAllowedImplicitCast(ImplicitCast allowedImplicitCast) {
+            this.allowedImplicitCasts.add(allowedImplicitCast);
+            return this;
+        }
+
+        /**
+         * If {@code true}, try to keep properties with polymorphic primitive types unboxed.
+         *
+         * @since 0.8 or earlier
+         */
+        public Builder setPolymorphicUnboxing(boolean polymorphicUnboxing) {
+            this.polymorphicUnboxing = polymorphicUnboxing;
+            return this;
+        }
+    }
+
+    /** @since 0.8 or earlier */
+    protected static EnumSet<ImplicitCast> getAllowedImplicitCasts(Builder builder) {
+        return builder.allowedImplicitCasts;
+    }
+
+    /** @since 0.8 or earlier */
+    protected static boolean getPolymorphicUnboxing(Builder builder) {
+        return builder.polymorphicUnboxing;
     }
 }
