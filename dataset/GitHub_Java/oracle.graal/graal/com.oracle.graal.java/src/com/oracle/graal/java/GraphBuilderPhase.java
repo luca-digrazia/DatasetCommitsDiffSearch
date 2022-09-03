25fe012d482c10a5611a8416adfe9662d9dca4d5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:40:03 +0200

    Add correct checkcast node to entry of catch block after exception dispatch.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.java/src/com/oracle/graal/java/GraphBuilderPhase.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.java/src/com/oracle/graal/java/GraphBuilderPhase.java
@@ -662,7 +662,7 @@ public final class GraphBuilderPhase extends Phase {
 
     void genNewInstance(int cpi) {
         JavaType type = lookupType(cpi, NEW);
-        if (type instanceof ResolvedJavaType && ((ResolvedJavaType) type).isInitialized()) {
+        if (type instanceof ResolvedJavaType) {
             NewInstanceNode n = currentGraph.add(new NewInstanceNode((ResolvedJavaType) type));
             frameState.apush(append(n));
         } else {
