ae8fbddd42a4762b18f5fd95dfa8bcb4c8c11b09
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:32:21 +0200

    Ensure that snippet-based replacements properly update and preserve the inlining log.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/SnippetTemplate.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/SnippetTemplate.java
@@ -757,8 +757,7 @@ public class SnippetTemplate {
                 }
             }
             if (GraalOptions.TraceInlining.getValue(snippetCopy.getOptions()).isTracing()) {
-                try (InliningLog.UpdateScope ignored = snippetCopy.getInliningLog().createUpdateScope((oldNode, newNode) -> {
-                })) {
+                try (InliningLog.UpdateScope ignored = snippetCopy.getInliningLog().createUpdateScope((oldNode, newNode) -> { })) {
                     UnmodifiableEconomicMap<Node, Node> duplicates = snippetCopy.addDuplicates(snippetGraph.getNodes(), snippetGraph, snippetGraph.getNodeCount(), nodeReplacements);
                     snippetCopy.getInliningLog().replaceLog(duplicates, snippetGraph.getInliningLog());
                 }
