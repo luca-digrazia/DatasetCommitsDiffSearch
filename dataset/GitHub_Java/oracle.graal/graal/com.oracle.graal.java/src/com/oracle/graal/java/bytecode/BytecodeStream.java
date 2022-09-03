15aef6d482165d09164c66526fb427691bd518a1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 00:58:54 +0200

    bugfix, parallel GC seems stable

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.java/src/com/oracle/graal/java/bytecode/BytecodeStream.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.java/src/com/oracle/graal/java/bytecode/BytecodeStream.java
@@ -22,6 +22,7 @@
  */
 package com.oracle.graal.java.bytecode;
 
+
 /**
  * A utility class that makes iterating over bytecodes and reading operands
  * simpler and less error prone. For example, it handles the {@link Bytecodes#WIDE} instruction
@@ -183,6 +184,7 @@ public final class BytecodeStream {
         curBCI = bci;
         if (curBCI < code.length) {
             opcode = Bytes.beU1(code, bci);
+            assert opcode < Bytecodes.BREAKPOINT : "illegal bytecode";
             nextBCI = bci + lengthOf();
         } else {
             opcode = Bytecodes.END;
