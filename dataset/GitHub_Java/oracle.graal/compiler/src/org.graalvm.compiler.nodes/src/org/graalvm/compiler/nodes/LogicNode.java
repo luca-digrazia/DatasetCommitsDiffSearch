0110edbb8e878a478d2ac933d10301c3ee3ae4a6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 05:02:50 +0200

    Add implies interface to LogicNode.

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/LogicNode.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/LogicNode.java
@@ -1,12 +1,10 @@
 /*
- * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -92,11 +90,5 @@ public abstract class LogicNode extends FloatingNode implements IndirectCanonica
      */
     public TriState implies(boolean thisNegated, LogicNode other) {
         return TriState.UNKNOWN;
-    }
-
-    protected static TriState flip(TriState triState) {
-        return triState.isUnknown()
-                ? triState
-                : TriState.get(!triState.toBoolean());
     }
 }
