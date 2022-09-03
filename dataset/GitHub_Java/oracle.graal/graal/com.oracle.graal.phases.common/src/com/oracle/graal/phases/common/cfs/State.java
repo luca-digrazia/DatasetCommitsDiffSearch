30a628e249daf5e5afa2eb3cbcc23ec92b1f41cc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 19:30:10 +0200

    [flow-sensitive] less ambitious addFactInstanceOf

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/cfs/State.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/cfs/State.java
@@ -275,11 +275,9 @@ public final class State extends MergeableState<State> implements Cloneable {
             return true;
         }
 
-        for (State other : withReachableStates) {
-            versionNr = Math.max(versionNr, other.versionNr) + 1;
-            if (!other.isUnreachable) {
-                isUnreachable = false;
-            }
+        for (State state : withReachableStates) {
+            versionNr = Math.max(versionNr, state.versionNr) + 1;
+            isUnreachable &= state.isUnreachable;
         }
 
         if (isUnreachable) {
