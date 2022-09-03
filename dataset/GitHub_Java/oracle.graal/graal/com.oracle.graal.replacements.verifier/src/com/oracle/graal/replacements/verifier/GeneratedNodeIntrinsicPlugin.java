1abc27e3e5b881dddeba440428a799ef44c1bd06
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 14:12:37 +0200

    Add targetMethod parameter to node intrisics with custom intrinsify method

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements.verifier/src/com/oracle/graal/replacements/verifier/GeneratedNodeIntrinsicPlugin.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements.verifier/src/com/oracle/graal/replacements/verifier/GeneratedNodeIntrinsicPlugin.java
@@ -30,9 +30,10 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.tools.Diagnostic.Kind;
+
+import jdk.vm.ci.meta.JavaKind;
 
 import com.oracle.graal.graph.Node.ConstantNodeParameter;
 import com.oracle.graal.graph.Node.InjectedNodeParameter;
@@ -103,7 +104,7 @@ public abstract class GeneratedNodeIntrinsicPlugin extends GeneratedPlugin {
 
         @Override
         public void extraImports(Set<String> imports) {
-            if (intrinsicMethod.getReturnType().getKind() != TypeKind.VOID) {
+            if (getReturnKind(intrinsicMethod) != JavaKind.Void) {
                 imports.add("jdk.vm.ci.meta.JavaKind");
             }
         }
@@ -128,10 +129,11 @@ public abstract class GeneratedNodeIntrinsicPlugin extends GeneratedPlugin {
                 out.printf("            node.setStamp(%s);\n", deps.use(WellKnownDependency.RETURN_STAMP));
             }
 
-            if (intrinsicMethod.getReturnType().getKind() == TypeKind.VOID) {
+            JavaKind returnKind = getReturnKind(intrinsicMethod);
+            if (returnKind == JavaKind.Void) {
                 out.printf("            b.add(node);\n");
             } else {
-                out.printf("            b.addPush(JavaKind.%s, node);\n", getReturnKind(intrinsicMethod));
+                out.printf("            b.addPush(JavaKind.%s, node);\n", returnKind.name());
             }
             out.printf("            return true;\n");
         }
--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements.verifier/src/com/oracle/graal/replacements/verifier/NodeIntrinsicVerifier.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.replacements.verifier/src/com/oracle/graal/replacements/verifier/NodeIntrinsicVerifier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,18 +22,32 @@
  */
 package com.oracle.graal.replacements.verifier;
 
-import java.lang.annotation.*;
-import java.util.*;
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import javax.annotation.processing.*;
-import javax.lang.model.element.*;
-import javax.lang.model.type.*;
-import javax.lang.model.util.*;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.TypeVariable;
+import javax.lang.model.util.ElementFilter;
 import javax.tools.Diagnostic.Kind;
 
 import com.oracle.graal.graph.Node.ConstantNodeParameter;
 import com.oracle.graal.graph.Node.InjectedNodeParameter;
 import com.oracle.graal.graph.Node.NodeIntrinsic;
+import com.oracle.graal.nodeinfo.InputType;
+import com.oracle.graal.nodeinfo.NodeInfo;
+import com.oracle.graal.nodeinfo.StructuralInput.MarkerType;
 
 public final class NodeIntrinsicVerifier extends AbstractVerifier {
 
@@ -52,7 +66,19 @@ public final class NodeIntrinsicVerifier extends AbstractVerifier {
     }
 
     private TypeMirror resolvedJavaTypeType() {
-        return env.getElementUtils().getTypeElement("com.oracle.graal.api.meta.ResolvedJavaType").asType();
+        return env.getElementUtils().getTypeElement("jdk.vm.ci.meta.ResolvedJavaType").asType();
+    }
+
+    private TypeMirror resolvedJavaMethodType() {
+        return env.getElementUtils().getTypeElement("jdk.vm.ci.meta.ResolvedJavaMethod").asType();
+    }
+
+    private TypeMirror structuralInputType() {
+        return env.getElementUtils().getTypeElement("com.oracle.graal.nodeinfo.StructuralInput").asType();
+    }
+
+    private TypeMirror graphBuilderContextType() {
+        return env.getElementUtils().getTypeElement("com.oracle.graal.nodes.graphbuilderconf.GraphBuilderContext").asType();
     }
 
     public NodeIntrinsicVerifier(ProcessingEnvironment env) {
@@ -65,7 +91,7 @@ public final class NodeIntrinsicVerifier extends AbstractVerifier {
     }
 
     @Override
-    public void verify(Element element, AnnotationMirror annotation) {
+    public void verify(Element element, AnnotationMirror annotation, PluginGenerator generator) {
         if (element.getKind() != ElementKind.METHOD) {
             assert false : "Element is guaranteed to be a method.";
             return;
@@ -74,6 +100,9 @@ public final class NodeIntrinsicVerifier extends AbstractVerifier {
         ExecutableElement intrinsicMethod = (ExecutableElement) element;
         if (!intrinsicMethod.getModifiers().contains(Modifier.STATIC)) {
             env.getMessager().printMessage(Kind.ERROR, String.format("A @%s method must be static.", NodeIntrinsic.class.getSimpleName()), element, annotation);
+        }
+        if (!intrinsicMethod.getModifiers().contains(Modifier.NATIVE)) {
+            env.getMessager().printMessage(Kind.ERROR, String.format("A @%s method must be native.", NodeIntrinsic.class.getSimpleName()), element, annotation);
         }
 
         TypeMirror nodeClassMirror = resolveAnnotationValue(TypeMirror.class, findAnnotationValue(annotation, NODE_CLASS_NAME));
@@ -89,12 +118,66 @@ public final class NodeIntrinsicVerifier extends AbstractVerifier {
             }
         }
 
-        if (isNodeType(nodeClass)) {
-            TypeMirror[] constructorSignature = constructorSignature(intrinsicMethod);
-            findConstructor(nodeClass, constructorSignature, intrinsicMethod, annotation);
-        } else {
-            env.getMessager().printMessage(Kind.ERROR, String.format("The class %s is not a Node subclass.", nodeClass.getSimpleName()), element, annotation);
+        if (intrinsicMethod.getReturnType() instanceof TypeVariable) {
+            env.getMessager().printMessage(Kind.ERROR, "@NodeIntrinsic cannot have a generic return type.", element, annotation);
         }
+
+        TypeMirror[] constructorSignature = constructorSignature(intrinsicMethod);
+        ExecutableElement custom = findCustomIntrinsifyMethod(nodeClass, constructorSignature);
+        if (custom != null) {
+            generator.addPlugin(new GeneratedNodeIntrinsicPlugin.CustomFactoryPlugin(intrinsicMethod, custom, constructorSignature));
+        } else {
+            if (isNodeType(nodeClass)) {
+                if (nodeClass.getModifiers().contains(Modifier.ABSTRACT)) {
+                    env.getMessager().printMessage(Kind.ERROR, String.format("Cannot make @NodeIntrinsic for abstract node class %s.", nodeClass.getSimpleName()), element, annotation);
+                } else {
+                    TypeMirror ret = intrinsicMethod.getReturnType();
+                    if (env.getTypeUtils().isAssignable(ret, structuralInputType())) {
+                        checkInputType(nodeClass, ret, element, annotation);
+                    }
+
+                    ExecutableElement constructor = findConstructor(nodeClass, constructorSignature, intrinsicMethod, annotation);
+                    if (constructor != null) {
+                        generator.addPlugin(new GeneratedNodeIntrinsicPlugin.ConstructorPlugin(intrinsicMethod, constructor, constructorSignature));
+                    }
+                }
+            } else {
+                env.getMessager().printMessage(Kind.ERROR, String.format("The class %s is not a Node subclass.", nodeClass.getSimpleName()), element, annotation);
+            }
+        }
+    }
+
+    private void checkInputType(TypeElement nodeClass, TypeMirror returnType, Element element, AnnotationMirror annotation) {
+        InputType inputType = getInputType(returnType, element, annotation);
+        if (inputType != InputType.Value) {
+            boolean allowed = false;
+            InputType[] allowedTypes = nodeClass.getAnnotation(NodeInfo.class).allowedUsageTypes();
+            for (InputType allowedType : allowedTypes) {
+                if (inputType == allowedType) {
+                    allowed = true;
+                    break;
+                }
+            }
+            if (!allowed) {
+                env.getMessager().printMessage(Kind.ERROR, String.format("@NodeIntrinsic returns input type %s, but only %s is allowed.", inputType, Arrays.toString(allowedTypes)), element,
+                                annotation);
+            }
+        }
+    }
+
+    private InputType getInputType(TypeMirror type, Element element, AnnotationMirror annotation) {
+        TypeElement current = (TypeElement) env.getTypeUtils().asElement(type);
+        while (current != null) {
+            MarkerType markerType = current.getAnnotation(MarkerType.class);
+            if (markerType != null) {
+                return markerType.value();
+            }
+
+            current = (TypeElement) env.getTypeUtils().asElement(current.getSuperclass());
+        }
+
+        env.getMessager().printMessage(Kind.ERROR, String.format("The class %s is a subclass of StructuralInput, but isn't annotated with @MarkerType.", type), element, annotation);
+        return InputType.Value;
     }
 
     private boolean isNodeType(TypeElement nodeClass) {
@@ -118,46 +201,97 @@ public final class NodeIntrinsicVerifier extends AbstractVerifier {
         return parameters;
     }
 
-    private void findConstructor(TypeElement nodeClass, TypeMirror[] signature, ExecutableElement intrinsicMethod, AnnotationMirror intrinsicAnnotation) {
+    private ExecutableElement findConstructor(TypeElement nodeClass, TypeMirror[] signature, ExecutableElement intrinsicMethod, AnnotationMirror intrinsicAnnotation) {
         List<ExecutableElement> constructors = ElementFilter.constructorsIn(nodeClass.getEnclosedElements());
+        List<String> failureReasons = new ArrayList<>();
 
-        nextConstructor: for (ExecutableElement constructor : constructors) {
-            int sIdx = 0;
-            int cIdx = 0;
-            while (cIdx < constructor.getParameters().size()) {
-                VariableElement parameter = constructor.getParameters().get(cIdx++);
-                if (parameter.getAnnotation(InjectedNodeParameter.class) != null) {
-                    // skip injected parameters
-                    continue;
-                }
-
-                TypeMirror paramType = parameter.asType();
-                if (cIdx == constructor.getParameters().size() && paramType.getKind() == TypeKind.ARRAY) {
-                    // last argument of constructor is varargs, match remaining intrinsic arguments
-                    TypeMirror varargsType = ((ArrayType) paramType).getComponentType();
-                    while (sIdx < signature.length) {
-                        if (!isTypeCompatible(varargsType, signature[sIdx++])) {
-                            continue nextConstructor;
-                        }
-                    }
-                } else if (sIdx >= signature.length) {
-                    // too many arguments in intrinsic method
-                    continue nextConstructor;
-                } else if (!isTypeCompatible(paramType, signature[sIdx++])) {
-                    continue nextConstructor;
-                }
-            }
-
-            if (sIdx == signature.length) {
+        for (ExecutableElement constructor : constructors) {
+            String failureReason = matchSignature(0, constructor, signature);
+            if (failureReason == null) {
                 // found
-                return;
+                return constructor;
             }
 
-            // too many arguments in constructor
+            failureReasons.add(failureReason);
         }
 
         // not found
-        env.getMessager().printMessage(Kind.ERROR, "Could not find matching constructor for node intrinsic.", intrinsicMethod, intrinsicAnnotation);
+        if (failureReasons.isEmpty()) {
+            env.getMessager().printMessage(Kind.ERROR, "Could not find matching constructor for node intrinsic.", intrinsicMethod, intrinsicAnnotation);
+        } else {
+            for (String reason : failureReasons) {
+                env.getMessager().printMessage(Kind.ERROR, reason, intrinsicMethod, intrinsicAnnotation);
+            }
+        }
+
+        return null;
+    }
+
+    private ExecutableElement findCustomIntrinsifyMethod(TypeElement nodeClass, TypeMirror[] signature) {
+        List<ExecutableElement> methods = ElementFilter.methodsIn(nodeClass.getEnclosedElements());
+        for (ExecutableElement method : methods) {
+            if (!method.getSimpleName().toString().equals("intrinsify")) {
+                continue;
+            }
+
+            if (method.getParameters().size() < 2) {
+                continue;
+            }
+
+            VariableElement firstArg = method.getParameters().get(0);
+            if (!isTypeCompatible(firstArg.asType(), graphBuilderContextType())) {
+                continue;
+            }
+
+            VariableElement secondArg = method.getParameters().get(1);
+            if (!isTypeCompatible(secondArg.asType(), resolvedJavaMethodType())) {
+                continue;
+            }
+
+            String failureReason = matchSignature(2, method, signature);
+            if (failureReason == null) {
+                // found
+                return method;
+            }
+        }
+
+        return null;
+    }
+
+    private String matchSignature(int numSkippedParameters, ExecutableElement method, TypeMirror[] signature) {
+        int sIdx = 0;
+        int cIdx = numSkippedParameters;
+        while (cIdx < method.getParameters().size()) {
+            VariableElement parameter = method.getParameters().get(cIdx++);
+            if (parameter.getAnnotation(InjectedNodeParameter.class) != null) {
+                // skip injected parameters
+                continue;
+            }
+
+            TypeMirror paramType = parameter.asType();
+            if (cIdx == method.getParameters().size() && paramType.getKind() == TypeKind.ARRAY) {
+                // last argument of constructor is varargs, match remaining intrinsic arguments
+                TypeMirror varargsType = ((ArrayType) paramType).getComponentType();
+                while (sIdx < signature.length) {
+                    if (!isTypeCompatible(varargsType, signature[sIdx++])) {
+                        return String.format("%s failed because the types of argument %d are incompatible: %s != %s", method, sIdx, varargsType, signature[sIdx - 1]);
+                    }
+                }
+            } else if (sIdx >= signature.length) {
+                // too many arguments in intrinsic method
+                return String.format("Too many arguments for %s", method);
+            } else if (!isTypeCompatible(paramType, signature[sIdx++])) {
+                return String.format("%s failed because the types of argument %d are incompatible: %s != %s", method, sIdx, paramType, signature[sIdx - 1]);
+            }
+        }
+
+        if (sIdx == signature.length) {
+            // found
+            return null;
+        }
+
+        // too many arguments in constructor
+        return String.format("Not enough arguments for %s", method);
     }
 
     private boolean isTypeCompatible(TypeMirror originalType, TypeMirror substitutionType) {
