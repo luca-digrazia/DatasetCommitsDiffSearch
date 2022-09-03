062116a8dc83b288921df9634da9697704b58faf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 21:53:17 +0200

    Move SQRT from AMD64MathIntrinsicOp to AMD64Arithmetic.

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.asm.amd64/src/com/oracle/graal/asm/amd64/AMD64Assembler.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.asm.amd64/src/com/oracle/graal/asm/amd64/AMD64Assembler.java
@@ -1189,12 +1189,6 @@ public class AMD64Assembler extends AbstractAssembler {
         emitByte(0xD8 | encode);
     }
 
-    public final void notl(Register dst) {
-        int encode = prefixAndEncode(dst.encoding);
-        emitByte(0xF7);
-        emitByte(0xD0 | encode);
-    }
-
     public final void ensureUniquePC() {
         nop();
     }
@@ -2216,12 +2210,6 @@ public class AMD64Assembler extends AbstractAssembler {
         int encode = prefixqAndEncode(dst.encoding);
         emitByte(0xF7);
         emitByte(0xD8 | encode);
-    }
-
-    public final void notq(Register dst) {
-        int encode = prefixqAndEncode(dst.encoding);
-        emitByte(0xF7);
-        emitByte(0xD0 | encode);
     }
 
     public final void orq(Register dst, int imm32) {
