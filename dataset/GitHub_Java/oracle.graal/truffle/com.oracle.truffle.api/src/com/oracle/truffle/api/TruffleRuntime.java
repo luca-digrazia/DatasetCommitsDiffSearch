cf7af95efd95c3f78b2e1b792de6bf4a4c198509
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 09:39:24 +0200

    Add default method for TruffleRuntime#getCalltargets.

--- dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/TruffleRuntime.java
+++ dataset/GitHub_Java/oracle.graal/truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/TruffleRuntime.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,14 +24,26 @@
  */
 package com.oracle.truffle.api;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
 
-import com.oracle.truffle.api.frame.*;
-import com.oracle.truffle.api.nodes.*;
+import com.oracle.truffle.api.frame.Frame;
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.FrameInstance;
+import com.oracle.truffle.api.frame.FrameInstanceVisitor;
+import com.oracle.truffle.api.frame.MaterializedFrame;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.DirectCallNode;
+import com.oracle.truffle.api.nodes.IndirectCallNode;
+import com.oracle.truffle.api.nodes.LoopNode;
+import com.oracle.truffle.api.nodes.RepeatingNode;
+import com.oracle.truffle.api.nodes.RootNode;
 
 /**
  * Interface representing a Truffle runtime object. The runtime is responsible for creating call
  * targets and performing optimizations for them.
+ *
+ * @since 0.8 or earlier
  */
 public interface TruffleRuntime {
 
@@ -39,6 +51,7 @@ public interface TruffleRuntime {
      * Name describing this runtime implementation for debugging purposes.
      *
      * @return the name as a String
+     * @since 0.8 or earlier
      */
     String getName();
 
@@ -49,6 +62,7 @@ public interface TruffleRuntime {
      *            {@link RootNode#execute(com.oracle.truffle.api.frame.VirtualFrame)} method
      *            represents the entry point
      * @return the new call target object
+     * @since 0.8 or earlier
      */
     RootCallTarget createCallTarget(RootNode rootNode);
 
@@ -57,11 +71,17 @@ public interface TruffleRuntime {
      *
      * @param target the direct {@link CallTarget} to call
      * @return the new call node
+     * @since 0.8 or earlier
      */
     DirectCallNode createDirectCallNode(CallTarget target);
 
     /**
-     * Experimental API. May change without notice.
+     * Creates a new loop node with an implementation provided by a Truffle runtime implementation.
+     * Using Truffle loop nodes allows the runtime to do additional optimizations such as on stack
+     * replacement for loops.
+     *
+     * @see LoopNode usage example
+     * @since 0.8 or earlier
      */
     LoopNode createLoopNode(RepeatingNode body);
 
@@ -69,6 +89,7 @@ public interface TruffleRuntime {
      * Creates a new runtime specific version of {@link IndirectCallNode}.
      *
      * @return the new call node
+     * @since 0.8 or earlier
      */
     IndirectCallNode createIndirectCallNode();
 
@@ -76,6 +97,7 @@ public interface TruffleRuntime {
      * Creates a new assumption object that can be checked and invalidated.
      *
      * @return the newly created assumption object
+     * @since 0.8 or earlier
      */
     Assumption createAssumption();
 
@@ -84,6 +106,7 @@ public interface TruffleRuntime {
      *
      * @param name the name for the new assumption
      * @return the newly created assumption object
+     * @since 0.8 or earlier
      */
     Assumption createAssumption(String name);
 
@@ -92,6 +115,7 @@ public interface TruffleRuntime {
      * optimizable by the runtime.
      *
      * @return the newly created virtual frame object
+     * @since 0.8 or earlier
      */
     VirtualFrame createVirtualFrame(Object[] arguments, FrameDescriptor frameDescriptor);
 
@@ -99,6 +123,7 @@ public interface TruffleRuntime {
      * Creates a new materialized frame object that can be used to store values.
      *
      * @return the newly created materialized frame object
+     * @since 0.8 or earlier
      */
     MaterializedFrame createMaterializedFrame(Object[] arguments);
 
@@ -108,6 +133,7 @@ public interface TruffleRuntime {
      *
      * @param frameDescriptor the frame descriptor describing this frame's values
      * @return the newly created materialized frame object
+     * @since 0.8 or earlier
      */
     MaterializedFrame createMaterializedFrame(Object[] arguments, FrameDescriptor frameDescriptor);
 
@@ -116,13 +142,13 @@ public interface TruffleRuntime {
      * runtime.
      *
      * @return the newly created compiler options object
+     * @since 0.8 or earlier
      */
     CompilerOptions createCompilerOptions();
 
     /**
      * Accesses the current stack, i.e., the contents of the {@link Frame}s and the associated
-     * {@link CallTarget}s. Iteration starts at the caller frame, i.e., it does not include the
-     * current frame.
+     * {@link CallTarget}s. Iteration starts at the current frame.
      *
      * Iteration continues as long as {@link FrameInstanceVisitor#visitFrame}, which is invoked for
      * every {@link FrameInstance}, returns null. Any non-null result of the visitor indicates that
@@ -131,18 +157,23 @@ public interface TruffleRuntime {
      * @param visitor the visitor that is called for every matching frame.
      * @return the last result returned by the visitor (which is non-null to indicate that iteration
      *         should stop), or null if the whole stack was iterated.
+     * @since 0.8 or earlier
      */
     <T> T iterateFrames(FrameInstanceVisitor<T> visitor);
 
     /**
      * Accesses the caller frame. This is a convenience method that returns the first frame that is
      * passed to the visitor of {@link #iterateFrames}.
+     *
+     * @since 0.8 or earlier
      */
     FrameInstance getCallerFrame();
 
     /**
      * Accesses the current frame, i.e., the frame of the closest {@link CallTarget}. It is
      * important to note that this {@link FrameInstance} supports only slow path access.
+     *
+     * @since 0.8 or earlier
      */
     FrameInstance getCurrentFrame();
 
@@ -151,18 +182,39 @@ public interface TruffleRuntime {
      *
      * @param capability the type of the interface representing the capability
      * @return an implementation of the capability or {@code null} if the runtime does not offer it
+     * @since 0.8 or earlier
      */
     <T> T getCapability(Class<T> capability);
 
     /**
      * Returns a list of all still referenced {@link RootCallTarget} instances that were created
      * using {@link #createCallTarget(RootNode)}.
+     *
+     * @since 0.8 or earlier
+     * @deprecated Loaded sources and source sections can be accessed using the Truffle
+     *             instrumentation framework. Deprecated in Truffle 0.15.
      */
-    Collection<RootCallTarget> getCallTargets();
+    @Deprecated
+    default Collection<RootCallTarget> getCallTargets() {
+        return Collections.emptyList();
+    }
 
     /**
      * Internal API method. Do not use.
+     *
+     * @since 0.8 or earlier
      */
     void notifyTransferToInterpreter();
+
+    /**
+     * Whether or not the {@link TruffleRuntime} implementation can or wants to use gathered
+     * profiling information Truffle compilation. If this method returns <code>false</code> then all
+     * profiles in the {@link com.oracle.truffle.api.utilities} package are returning void
+     * implementations. If it returns <code>true</code> then all implementations gather profilinig
+     * information.
+     *
+     * @since 0.8 or earlier
+     */
+    boolean isProfilingEnabled();
 
 }
