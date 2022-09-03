dedfd67f20c507b28cce38f27e26700d6360b787
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:05:42 +0200

    add missing specialization to LLVMPointerStoreNode for storing long addresses

--- dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/store/LLVMPointerStoreNode.java
+++ dataset/GitHub_Java/oracle.graal/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/store/LLVMPointerStoreNode.java
@@ -51,15 +51,10 @@ public abstract class LLVMPointerStoreNode extends LLVMStoreNodeCommon {
         super(sourceLocation);
     }
 
-    @Specialization(guards = "!isAutoDerefHandle(addr)")
+    @Specialization
     protected void doAddress(long addr, Object value,
                     @Cached("createToNativeWithTarget()") LLVMToNativeNode toNative) {
         getLLVMMemoryCached().putPointer(addr, toNative.executeWithTarget(value));
-    }
-
-    @Specialization(guards = "isAutoDerefHandle(addr)")
-    protected void doDerefAddress(long addr, Object value) {
-        doTruffleObject(getDerefHandleGetReceiverNode().execute(addr), value);
     }
 
     @Specialization(guards = "!isAutoDerefHandle(addr)")
