1ed8f4650a301680a99f7843e3d17def93687141
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 23:45:54 +0200

    new ReentrantNodeIterator

--- dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases/src/com/oracle/graal/phases/graph/ReentrantNodeIterator.java
+++ dataset/GitHub_Java/oracle.graal/graal/com.oracle.graal.phases/src/com/oracle/graal/phases/graph/ReentrantNodeIterator.java
@@ -24,6 +24,7 @@ package com.oracle.graal.phases.graph;
 
 import java.util.*;
 
+import com.oracle.graal.graph.*;
 import com.oracle.graal.graph.NodeClass.NodeClassIterator;
 import com.oracle.graal.nodes.*;
 
