419bb214cdd22c5e049c831af3329a2b2c5b69e7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 07:54:01 +0200

    In GraphKit, use the invoke node's kind for frame state, which can differ from the signature's return kind because of plugins

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/GraphKit.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/GraphKit.java
@@ -252,10 +252,6 @@ public class GraphKit implements GraphBuilderTool {
         return new MethodCallTargetNode(invokeKind, targetMethod, args, returnStamp, null);
     }
 
-    protected final JavaKind asKind(JavaType type) {
-        return wordTypes != null ? wordTypes.asKind(type) : type.getJavaKind();
-    }
-
     /**
      * Determines if a given set of arguments is compatible with the signature of a given method.
      *
@@ -271,12 +267,14 @@ public class GraphKit implements GraphBuilderTool {
         }
         int argIndex = 0;
         if (!isStatic) {
-            JavaKind expected = asKind(method.getDeclaringClass());
+            ResolvedJavaType expectedType = method.getDeclaringClass();
+            JavaKind expected = wordTypes == null ? expectedType.getJavaKind() : wordTypes.asKind(expectedType);
             JavaKind actual = args[argIndex++].stamp().getStackKind();
             assert expected == actual : graph + ": wrong kind of value for receiver argument of call to " + method + " [" + actual + " != " + expected + "]";
         }
         for (int i = 0; i != signature.getParameterCount(false); i++) {
-            JavaKind expected = asKind(signature.getParameterType(i, method.getDeclaringClass())).getStackKind();
+            JavaType expectedType = signature.getParameterType(i, method.getDeclaringClass());
+            JavaKind expected = wordTypes == null ? expectedType.getJavaKind().getStackKind() : wordTypes.asKind(expectedType).getStackKind();
             JavaKind actual = args[argIndex++].stamp().getStackKind();
             if (expected != actual) {
                 throw new AssertionError(graph + ": wrong kind of value for argument " + i + " of call to " + method + " [" + actual + " != " + expected + "]");
