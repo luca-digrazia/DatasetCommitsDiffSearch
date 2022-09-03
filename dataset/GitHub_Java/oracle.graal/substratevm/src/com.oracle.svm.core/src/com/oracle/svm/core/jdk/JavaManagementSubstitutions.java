8ffd52fd519b4cf64e1de74f3b605693871017ed
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 04:37:04 +0200

    [GR-10489] Implement RuntimeMXBean.{getClassPath,getStartTime,getUptime}.

--- dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaManagementSubstitutions.java
+++ dataset/GitHub_Java/oracle.graal/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaManagementSubstitutions.java
@@ -44,7 +44,6 @@ import javax.management.ObjectName;
 
 import org.graalvm.compiler.serviceprovider.GraalServices;
 import org.graalvm.nativeimage.Feature;
-import org.graalvm.nativeimage.ProcessProperties;
 import org.graalvm.nativeimage.ImageSingletons;
 
 import com.oracle.svm.core.JavaMainWrapper.JavaMainSupport;
@@ -171,13 +170,12 @@ final class SubstrateRuntimeMXBean implements RuntimeMXBean {
         throw VMError.unsupportedFeature(MSG);
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public String getName() {
         long id;
         String hostName;
         try {
-            id = ProcessProperties.getProcessID();
+            id = (Integer) Compiler.command(new Object[]{"com.oracle.svm.core.posix.PosixUtils.getpid()int"});
         } catch (Throwable t) {
             id = GraalServices.getGlobalTimeStamp();
         }
