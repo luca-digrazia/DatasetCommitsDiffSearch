ecb8c400ec8f839a4d1da61ee43a75ceb27fcdc7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-03 06:33:28 +0200

    TraceRA: refactor global move resolver

--- dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/alloc/trace/TraceGlobalMoveResolutionPhase.java
+++ dataset/GitHub_Java/oracle.graal/compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/alloc/trace/TraceGlobalMoveResolutionPhase.java
@@ -155,10 +155,6 @@ public final class TraceGlobalMoveResolutionPhase {
         // GLI
         Value[] locFrom = livenessInfo.getOutLocation(fromBlock);
         Value[] locTo = livenessInfo.getInLocation(toBlock);
-        if (locFrom == locTo) {
-            // a strategy might reuse the locations array if locations are the same
-            return;
-        }
         assert locFrom.length == locTo.length;
 
         for (int i = 0; i < locFrom.length; i++) {
