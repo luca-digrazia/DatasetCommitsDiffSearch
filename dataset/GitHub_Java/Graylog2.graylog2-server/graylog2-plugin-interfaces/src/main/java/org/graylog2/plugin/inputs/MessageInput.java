90e3ca27017d1051f9bbb682cd9b830920d53aad
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:45:45 +0200

    Adding creation date of input to serialisation.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/inputs/MessageInput.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/inputs/MessageInput.java
@@ -22,65 +22,30 @@
  */
 package org.graylog2.plugin.inputs;
 
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.MetricSet;
-import com.codahale.metrics.Timer;
 import com.google.common.collect.Maps;
-import org.graylog2.plugin.Message;
 import org.graylog2.plugin.Tools;
 import org.graylog2.plugin.buffers.Buffer;
-import org.graylog2.plugin.buffers.BufferOutOfCapacityException;
-import org.graylog2.plugin.buffers.ProcessingDisabledException;
 import org.graylog2.plugin.configuration.Configuration;
 import org.graylog2.plugin.configuration.ConfigurationException;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
-import org.graylog2.plugin.configuration.fields.ConfigurationField;
 import org.graylog2.plugin.configuration.fields.TextField;
-import org.graylog2.plugin.inputs.codecs.Codec;
-import org.graylog2.plugin.inputs.transports.Transport;
-import org.graylog2.plugin.journal.RawMessage;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+
+/**
+ * @author Lennart Koopmann <lennart@socketfeed.com>
+ */
 public abstract class MessageInput {
+
     private static final Logger LOG = LoggerFactory.getLogger(MessageInput.class);
 
-    public static final String CK_OVERRIDE_SOURCE = "override_source";
     public static final String CK_RECV_BUFFER_SIZE = "recv_buffer_size";
 
-    public static final String FIELD_TYPE = "type";
-    public static final String FIELD_INPUT_ID = "input_id";
-    public static final String FIELD_PERSIST_ID = "persist_id";
-    public static final String FIELD_NODE_ID = "node_id";
-    public static final String FIELD_RADIO_ID = "radio_id";
-    public static final String FIELD_NAME = "name";
-    public static final String FIELD_TITLE = "title";
-    public static final String FIELD_CONFIGURATION = "configuration";
-    public static final String FIELD_CREATOR_USER_ID = "creator_user_id";
-    public static final String FIELD_CREATED_AT = "created_at";
-    public static final String FIELD_STARTED_AT = "started_at";
-    public static final String FIELD_ATTRIBUTES = "attributes";
-    public static final String FIELD_STATIC_FIELDS = "static_fields";
-    public static final String FIELD_GLOBAL = "global";
-
     private static long defaultRecvBufferSize = 1024 * 1024;
-
-    private final MetricRegistry metricRegistry;
-    private final Transport transport;
-    private final MetricRegistry localRegistry;
-    private final Codec codec;
-    private final Meter failures;
-    private final Meter incompleteMessages;
-    private final Meter incomingMessages;
-    private final Meter processedMessages;
-    private final Timer parseTime;
-    private final Meter rawSize;
 
     protected String title;
     protected String creatorUserId;
@@ -89,84 +54,35 @@ public abstract class MessageInput {
     protected Boolean global = false;
 
     protected Configuration configuration;
-    protected Buffer processBuffer;
 
+    private Map<String, Extractor> extractors = Maps.newHashMap(); // access is synchronized.
     private Map<String, String> staticFields = Maps.newConcurrentMap();
 
-    public MessageInput(MetricRegistry metricRegistry,
-                        Transport transport,
-                        MetricRegistry localRegistry, Codec codec) {
-        this.metricRegistry = metricRegistry;
-        this.transport = transport;
-        this.localRegistry = localRegistry;
-        this.codec = codec;
-
-        parseTime = localRegistry.timer("parseTime");
-        processedMessages = localRegistry.meter("processedMessages");
-        failures = localRegistry.meter("failures");
-        incompleteMessages = localRegistry.meter("incompleteMessages");
-        rawSize = localRegistry.meter("rawSize");
-        incomingMessages = localRegistry.meter("incomingMessages");
+    public void initialize(Configuration configuration) {
     }
 
-    public void initialize() {
-        final MetricSet transportMetrics = transport.getMetricSet();
-
-        if (transportMetrics != null) {
-            metricRegistry.register(getUniqueReadableId(), transportMetrics);
-        }
-        metricRegistry.register(getUniqueReadableId(), localRegistry);
-    }
+    public abstract void checkConfiguration(Configuration configuration) throws ConfigurationException;
 
     public void checkConfiguration() throws ConfigurationException {
-        final ConfigurationRequest cr = getRequestedConfiguration();
-        cr.check(getConfiguration());
+        checkConfiguration(getConfiguration());
     }
 
-    public void launch(final Buffer buffer) throws MisfireException {
-        this.processBuffer = buffer;
-        try {
-            transport.setMessageAggregator(codec.getAggregator());
+    public abstract void launch(Buffer processBuffer) throws MisfireException;
+    public abstract void stop();
 
-            transport.launch(this);
-        } catch (Exception e) {
-            processBuffer = null;
-            throw new MisfireException(e);
-        }
-    }
-
-    public void stop() {
-        transport.stop();
-    }
-
-    public ConfigurationRequest getRequestedConfiguration() {
-        final ConfigurationRequest transportConfig = transport.getRequestedConfiguration();
-        final ConfigurationRequest codecConfig = codec.getRequestedConfiguration();
-        final ConfigurationRequest r = new ConfigurationRequest();
-        r.putAll(transportConfig.getFields());
-        r.putAll(codecConfig.getFields());
-
-        r.addField(new TextField(
-                CK_OVERRIDE_SOURCE,
-                "Override source",
-                null,
-                "The source is a hostname derived from the received packet by default. Set this if you want to override " +
-                        "it with a custom string.",
-                ConfigurationField.Optional.OPTIONAL
-        ));
-
-        // give the codec the opportunity to override default values for certain configuration fields,
-        // this is commonly being used to default to some well known port for protocols such as GELF or syslog
-        codec.overrideDefaultValues(r);
-
-        return r;
-    }
+    /**
+     * Description of the config settings this input needs.
+     *
+     * Must not be null.
+     *
+     * @return a possibly empty ConfigurationRequest object
+     */
+    public abstract ConfigurationRequest getRequestedConfiguration();
 
     public abstract boolean isExclusive();
-
     public abstract String getName();
-
     public abstract String linkToDocs();
+    public abstract Map<String, Object> getAttributes();
 
     public void setPersistId(String id) {
         this.persistId = id;
@@ -220,25 +136,24 @@ public abstract class MessageInput {
         this.global = global;
     }
 
-    @SuppressWarnings("unchecked")
     public Object getAttributesWithMaskedPasswords() {
+        Map<String, Object> result = Maps.newHashMap();
+
         final ConfigurationRequest config = getRequestedConfiguration();
         if (config == null) {
-            return Collections.emptyMap();
+            return result;
         }
 
-        final Map<String, Object> attributes = configuration.getSource();
-        final Map<String, Object> result = Maps.newHashMapWithExpectedSize(attributes.size());
-        for (Map.Entry<String, Object> attribute : attributes.entrySet()) {
+        for(Map.Entry<String, Object> attribute : getAttributes().entrySet()) {
             Object value = attribute.getValue();
 
             final Map<String, Map<String, Object>> configAsList = config.asList();
-            final Map<String, Object> attributesForConfigSetting = configAsList.get(attribute.getKey());
-
+            final String attributeKey = attribute.getKey();
+            final Map<String, Object> attributesForConfigSetting = configAsList.get(attributeKey);
             if (attributesForConfigSetting != null) {
                 // we know the config setting, check its attributes
-                final List<String> attrs = (List<String>) attributesForConfigSetting.get("attributes");
-                if (attrs.contains(TextField.Attribute.IS_PASSWORD.toString().toLowerCase())) {
+                final List<String> attributes = (List<String>) attributesForConfigSetting.get("attributes");
+                if (attributes.contains(TextField.Attribute.IS_PASSWORD.toString().toLowerCase())) {
                     value = "********";
                 }
             } else {
@@ -247,35 +162,40 @@ public abstract class MessageInput {
                                  " though this is likely a bug.", attribute, value);
             }
 
-            result.put(attribute.getKey(), value);
+            result.put(attributeKey, value);
         }
 
         return result;
     }
 
     public Map<String, Object> asMap() {
-        final Map<String, Object> inputMap = Maps.newHashMap();
+        Map<String, Object> inputMap = Maps.newHashMap();
 
-        inputMap.put(FIELD_TYPE, this.getClass().getCanonicalName());
-        inputMap.put(FIELD_INPUT_ID, this.getId());
-        inputMap.put(FIELD_PERSIST_ID, this.getPersistId());
-        inputMap.put(FIELD_NAME, this.getName());
-        inputMap.put(FIELD_TITLE, this.getTitle());
-        inputMap.put(FIELD_CREATOR_USER_ID, this.getCreatorUserId());
-        inputMap.put(FIELD_CREATED_AT, Tools.getISO8601String(this.getCreatedAt()));
-        inputMap.put(FIELD_ATTRIBUTES, this.getAttributesWithMaskedPasswords());
-        inputMap.put(FIELD_STATIC_FIELDS, this.getStaticFields());
-        inputMap.put(FIELD_GLOBAL, this.getGlobal());
+        inputMap.put("type", this.getClass().getCanonicalName());
+        inputMap.put("input_id", this.getId());
+        inputMap.put("persist_id", this.getPersistId());
+        inputMap.put("name", this.getName());
+        inputMap.put("title", this.getTitle());
+        inputMap.put("creator_user_id", this.getCreatorUserId());
+        inputMap.put("created_at", this.getCreatedAt());
+        inputMap.put("started_at", Tools.getISO8601String(this.getCreatedAt()));
+        inputMap.put("attributes", this.getAttributesWithMaskedPasswords());
+        inputMap.put("static_fields", this.getStaticFields());
+        inputMap.put("global", this.getGlobal());
 
         return inputMap;
     }
 
-    public void addStaticField(String key, String value) {
-        this.staticFields.put(key, value);
+    public synchronized void addExtractor(String id, Extractor extractor) {
+        this.extractors.put(id, extractor);
     }
 
-    public void addStaticFields(Map<String, String> staticFields) {
-        this.staticFields.putAll(staticFields);
+    public Map<String, Extractor> getExtractors() {
+        return this.extractors;
+    }
+
+    public void addStaticField(String key, String value) {
+        this.staticFields.put(key, value);
     }
 
     public Map<String, String> getStaticFields() {
@@ -283,7 +203,8 @@ public abstract class MessageInput {
     }
 
     public String getUniqueReadableId() {
-        return getClass().getName() + "." + getId();
+        String readableId = getClass().getName() + "." + getId();
+        return readableId;
     }
 
     @Override
@@ -292,9 +213,9 @@ public abstract class MessageInput {
     }
 
     @Override
-    public boolean equals(final Object obj) {
+    public boolean equals(Object obj) {
         if (obj instanceof MessageInput) {
-            final MessageInput input = (MessageInput) obj;
+            MessageInput input = (MessageInput) obj;
             return this.getPersistId().equals(input.getPersistId());
         } else {
             return false;
@@ -305,85 +226,10 @@ public abstract class MessageInput {
         defaultRecvBufferSize = size;
     }
 
-    public Codec getCodec() {
-        return codec;
-    }
-
-    public void processRawMessage(RawMessage rawMessage) {
-        incomingMessages.mark();
-
-        final Message message;
-
-        try (Timer.Context ignored = parseTime.time()){
-            message = codec.decode(rawMessage);
-        } catch (RuntimeException e) {
-            LOG.warn("Codec " + codec + " threw exception", e);
-            failures.mark();
-            return;
+    public long getRecvBufferSize() {
+        if (configuration.intIsSet(CK_RECV_BUFFER_SIZE)) {
+            return configuration.getInt(CK_RECV_BUFFER_SIZE);
         }
-
-        if (message == null) {
-            failures.mark();
-            LOG.warn("Could not decode message. Dropping message {}", rawMessage.getId());
-            return;
-        }
-        if (!message.isComplete()) {
-            incompleteMessages.mark();
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Dropping incomplete message. Parsed fields: [{}]", message.getFields());
-            }
-            return;
-        }
-
-        processedMessages.mark();
-        rawSize.mark(rawMessage.getPayload().length);
-        processBuffer.insertCached(message, this);
-    }
-
-    /**
-     * Basically a reordered version of {@link #processRawMessage(org.graylog2.plugin.journal.RawMessage)} that only records a message
-     * as processed when adding it to processbuffer has worked.
-     * @param rawMessage
-     * @return true if the message was malformed and discarded. do not try to re-insert the message in that case but discard it.
-     *          false if the message was successfully processed, exception is raised otherwise
-     */
-    public boolean processRawMessageFailFast(RawMessage rawMessage) throws BufferOutOfCapacityException, ProcessingDisabledException {
-        final Message message;
-
-        try (Timer.Context ignored = parseTime.time()){
-            message = codec.decode(rawMessage);
-        } catch (RuntimeException e) {
-            LOG.warn("Codec " + codec + " threw exception", e);
-            incomingMessages.mark();
-            failures.mark();
-            return false;
-        }
-
-        if (message == null) {
-            incomingMessages.mark();
-            failures.mark();
-            LOG.warn("Could not decode message. Dropping message {}", rawMessage.getId());
-            return false;
-        }
-        if (!message.isComplete()) {
-            incomingMessages.mark();
-            incompleteMessages.mark();
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Dropping incomplete message. Parsed fields: [{}]", message.getFields());
-            }
-            return false;
-        }
-
-        processBuffer.insertFailFast(message, this);
-        // the following statements are only executed if insertFailFail does not throw!
-        incomingMessages.mark();
-        processedMessages.mark();
-        rawSize.mark(rawMessage.getPayload().length);
-        return true;
-    }
-
-    public interface Factory<M> {
-        M create(Configuration configuration);
-        M create(Configuration configuration, Transport transport, Codec codec);
+        return defaultRecvBufferSize;
     }
 }
