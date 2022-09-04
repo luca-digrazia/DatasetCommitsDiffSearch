e9c2896f6709c18704733499fd9414fc9c3d3205
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:18:35 +0200

    New Reflection tests

--- dataset/GitHub_Java/quarkusio.quarkus/examples/strict/src/test/java/org/jboss/shamrock/example/test/CoreReflectionInGraalITCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/examples/strict/src/test/java/org/jboss/shamrock/example/test/CoreReflectionInGraalITCase.java
@@ -2,7 +2,11 @@ package org.jboss.shamrock.example.test;
 
 import static org.junit.Assert.assertEquals;
 
-import org.jboss.shamrock.example.testutils.URLTester;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.jboss.shamrock.junit.GraalTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +16,16 @@ public class CoreReflectionInGraalITCase {
 
     @Test
     public void testFieldAndGetterReflectionOnEntityFromServlet() throws Exception {
-        assertEquals("OK", URLTester.relative("core/reflection").invokeURL().asString());
+        URL uri = new URL("http://localhost:8080/core/reflection");
+        URLConnection connection = uri.openConnection();
+        InputStream in = connection.getInputStream();
+        byte[] buf = new byte[100];
+        int r;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        while ((r = in.read(buf)) > 0) {
+            out.write(buf, 0, r);
+        }
+        assertEquals("OK", new String(out.toByteArray()));
     }
 
 }
