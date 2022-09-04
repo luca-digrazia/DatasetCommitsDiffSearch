b6b5b06f5fbe1a41d9d3b2aff30564feef837eae
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:17:44 +0200

    Remove temporary logs

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/rest/GetPostProcessor.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/rest/GetPostProcessor.java
@@ -1,51 +1,219 @@
 package com.googlecode.androidannotations.processing.rest;
 
-import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.WildcardType;
 
 import com.googlecode.androidannotations.helper.CanonicalNameConstants;
 import com.googlecode.androidannotations.processing.EBeanHolder;
-import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JExpr;
 import com.sun.codemodel.JInvocation;
-import com.sun.codemodel.JVar;
+import com.sun.codemodel.JPackage;
 
 public abstract class GetPostProcessor extends MethodProcessor {
 
 	protected EBeanHolder holder;
+	protected JPackage restClientPackage;
 
-	public GetPostProcessor(ProcessingEnvironment processingEnv, RestImplementationsHolder restImplementationHolder) {
-		super(processingEnv, restImplementationHolder);
+	public GetPostProcessor(ProcessingEnvironment processingEnv, RestImplementationsHolder restImplementationsHolder) {
+		super(processingEnv, restImplementationsHolder);
 	}
-
-	@Override
-	public abstract Class<? extends Annotation> getTarget();
 
 	@Override
 	public void process(Element element, JCodeModel codeModel, EBeanHolder holder) {
 		this.holder = holder;
-		ExecutableElement executableElement = (ExecutableElement) element;
-		TypeMirror returnType = executableElement.getReturnType();
+		restClientPackage = restImplementationsHolder.getEnclosingHolder(element).restImplementationClass._package();
+
 		String urlSuffix = retrieveUrlSuffix(element);
+
+		ExecutableElement executableElement = (ExecutableElement) element;
 		MethodProcessorHolder processorHolder = new MethodProcessorHolder(holder, executableElement, urlSuffix, null, null, codeModel);
 
+		// Retrieve return type
+		TypeMirror returnType = executableElement.getReturnType();
 		if (returnType.getKind() != TypeKind.VOID) {
-			retrieveReturnClass(holder, returnType, processorHolder);
+			retrieveReturnAndExpectedClasses(returnType, processorHolder);
 		}
 
 		generateRestTemplateCallBlock(processorHolder);
 	}
 
-	public abstract void retrieveReturnClass(EBeanHolder holder, TypeMirror returnType, MethodProcessorHolder processorHolder);
-
 	public abstract String retrieveUrlSuffix(Element element);
+
+	/**
+	 * Retrieve the expected and method return classes to use in generated code.
+	 * <p>
+	 * If the annotated method return a <b>ResponseEntity&lt;T&gt;</b> then :
+	 * 
+	 * <pre>
+	 * expectedClass = T.class, methodReturnClass = ResponseEntity&lt;T&gt;
+	 * </pre>
+	 * 
+	 * 
+	 * @param returnType
+	 * @param processorHolder
+	 */
+	public void retrieveReturnAndExpectedClasses(TypeMirror returnType, MethodProcessorHolder processorHolder) {
+		String returnTypeString = returnType.toString();
+		JClass expectedClass = null;
+
+		if (returnTypeString.startsWith(CanonicalNameConstants.RESPONSE_ENTITY)) {
+			DeclaredType declaredReturnType = (DeclaredType) returnType;
+			if (declaredReturnType.getTypeArguments().size() > 0) {
+				expectedClass = resolveExpectedClass(declaredReturnType.getTypeArguments().get(0));
+			} else {
+				expectedClass = holder.parseClass(CanonicalNameConstants.RESPONSE_ENTITY);
+			}
+		} else {
+			expectedClass = resolveExpectedClass(returnType);
+		}
+
+		processorHolder.setExpectedClass(expectedClass);
+		processorHolder.setMethodReturnClass(holder.parseClass(returnTypeString));
+	}
+
+	/**
+	 * Resolve the expected class for the input type according to the following
+	 * rules :
+	 * <ul>
+	 * <li>The type is a primitive : Directly return the JClass as usual</li>
+	 * <li>The type is NOT a generics : Directly return the JClass as usual</li>
+	 * <li>The type is a generics and enclosing type is a class C&lt;T&gt; :
+	 * Generate a subclass of C&lt;T&gt; and return it</li>
+	 * <li>The type is a generics and enclosing type is an interface I&lt;T&gt;
+	 * : Looking the inheritance tree, then</li>
+	 * <ol>
+	 * <li>One of the parent is a {@link Map} : Generate a subclass of
+	 * {@link LinkedHashMap}&lt;T&gt; one and return it</li>
+	 * <li>One of the parent is a {@link Set} : Generate a subclass of
+	 * {@link TreeSet}&lt;T&gt; one and return it</li>
+	 * <li>One of the parent is a {@link Collection} : Generate a subclass of
+	 * {@link ArrayList}&lt;T&gt; one and return it</li>
+	 * <li>Return {@link Object} definition</li>
+	 * </ol>
+	 * </ul>
+	 * 
+	 * @param expectedType
+	 */
+	private JClass resolveExpectedClass(TypeMirror expectedType) {
+		// is a class or an interface
+		if (expectedType.getKind() == TypeKind.DECLARED) {
+			DeclaredType declaredType = (DeclaredType) expectedType;
+			List<? extends TypeMirror> typeArguments = declaredType.getTypeArguments();
+
+			// is NOT a generics
+			if (typeArguments.size() == 0) {
+				return holder.parseClass(expectedType.toString());
+			}
+
+			// is a generics
+			JClass baseClass = holder.parseClass(declaredType.toString()).erasure();
+			JClass decoratedExpectedClass = retrieveDecoratedExpectedClass(declaredType, baseClass);
+			return decoratedExpectedClass == null ? baseClass : decoratedExpectedClass;
+		} else if (expectedType.getKind() == TypeKind.ARRAY) {
+			ArrayType arrayType = (ArrayType) expectedType;
+			return resolveExpectedClass(arrayType.getComponentType()).array();
+		}
+
+		// is not a class nor an interface
+		return holder.parseClass(expectedType.toString());
+	}
+
+	/**
+	 * Recursive method used to find if one of the grand-parent of the
+	 * <code>enclosingJClass</code> is {@link Map}, {@link Set} or
+	 * {@link Collection}.
+	 * 
+	 * @param declaredType
+	 * @param currentClass
+	 * @return
+	 */
+	private JClass retrieveDecoratedExpectedClass(DeclaredType declaredType, JClass currentClass) {
+		// Looking for basic java.util interfaces to set a default
+		// implementation
+		String decoratedClassName = null;
+		if (currentClass.isInterface() || currentClass.isAbstract()) {
+			if (currentClass.fullName().equals(CanonicalNameConstants.MAP)) {
+				decoratedClassName = LinkedHashMap.class.getCanonicalName();
+			} else if (currentClass.fullName().equals(CanonicalNameConstants.SET)) {
+				decoratedClassName = TreeSet.class.getCanonicalName();
+			} else if (currentClass.fullName().equals(CanonicalNameConstants.LIST)) {
+				decoratedClassName = ArrayList.class.getCanonicalName();
+			} else if (currentClass.fullName().equals(CanonicalNameConstants.COLLECTION)) {
+				decoratedClassName = ArrayList.class.getCanonicalName();
+			}
+		} else {
+			decoratedClassName = currentClass.erasure().fullName();
+		}
+
+		if (decoratedClassName != null) {
+			// Configure the super class of the final decorated class
+			String decoratedClassNameSuffix = "";
+			JClass decoratedSuperClass = holder.parseClass(decoratedClassName);
+			for (TypeMirror typeArgument : declaredType.getTypeArguments()) {
+				String typeArgumentName = typeArgument.toString();
+				if (typeArgument instanceof WildcardType) {
+					WildcardType wildcardType = (WildcardType) typeArgument;
+					if (wildcardType.getExtendsBound() != null) {
+						typeArgumentName = wildcardType.getExtendsBound().toString();
+					} else if (wildcardType.getSuperBound() != null) {
+						typeArgumentName = wildcardType.getSuperBound().toString();
+					} else {
+						typeArgumentName = CanonicalNameConstants.OBJECT;
+					}
+				}
+				JClass narrowJClass = holder.parseClass(typeArgumentName);
+				decoratedSuperClass = decoratedSuperClass.narrow(narrowJClass);
+				decoratedClassNameSuffix += plainName(narrowJClass);
+			}
+
+			String decoratedFinalClassName = currentClass.name() + "_" + decoratedClassNameSuffix;
+			decoratedFinalClassName = decoratedFinalClassName.replaceAll("\\[\\]", "s");
+			decoratedFinalClassName = restClientPackage.name() + "." + decoratedFinalClassName;
+			JDefinedClass decoratedJClass = holder.definedClass(decoratedFinalClassName);
+			decoratedJClass._extends(decoratedSuperClass);
+
+			return decoratedJClass;
+		}
+
+		// Try to find the superclass and make a recursive call to the this
+		// method
+		JClass enclosingSuperJClass = currentClass._extends();
+		if (enclosingSuperJClass != null) {
+			return retrieveDecoratedExpectedClass(declaredType, enclosingSuperJClass);
+		}
+
+		// Falling back to the current enclosingJClass if Class can't be found
+		return null;
+	}
+
+	protected String plainName(JClass jClass) {
+		String plainName = jClass.erasure().name();
+		List<JClass> typeParameters = jClass.getTypeParameters();
+		if (typeParameters.size() > 0) {
+			plainName += "_";
+			for (JClass typeParameter : typeParameters) {
+				plainName += plainName(typeParameter);
+			}
+		}
+		return plainName;
+	}
 
 	@Override
 	protected JInvocation addHttpEntityVar(JInvocation restCall, MethodProcessorHolder methodHolder) {
@@ -65,7 +233,7 @@ public abstract class GetPostProcessor extends MethodProcessor {
 
 	@Override
 	protected JInvocation addResultCallMethod(JInvocation restCall, MethodProcessorHolder methodHolder) {
-		JClass generatedReturnType = methodHolder.getGeneratedReturnType();
+		JClass generatedReturnType = methodHolder.getMethodReturnClass();
 		if (generatedReturnType == null) {
 			return restCall;
 		}
@@ -75,11 +243,6 @@ public abstract class GetPostProcessor extends MethodProcessor {
 		}
 
 		return restCall;
-	}
-
-	@Override
-	protected JVar addHttpHeadersVar(JBlock body, ExecutableElement executableElement) {
-		return generateHttpHeadersVar(holder, body, executableElement);
 	}
 
 }
