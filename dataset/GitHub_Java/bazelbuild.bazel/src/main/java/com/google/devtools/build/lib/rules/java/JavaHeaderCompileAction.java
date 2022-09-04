067a7fe87af943ac440569b2860ff244be5282f3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 00:52:12 +0200

    Never use shell quoting in turbine params files
        PiperOrigin-RevId: 161415544

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaHeaderCompileAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaHeaderCompileAction.java
@@ -23,7 +23,6 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.actions.Action;
 import com.google.devtools.build.lib.actions.ActionExecutionContext;
 import com.google.devtools.build.lib.actions.ActionInput;
@@ -389,7 +388,10 @@ public class JavaHeaderCompileAction extends SpawnAction {
             .setJarExecutable(
                 JavaCommon.getHostJavaExecutable(ruleContext),
                 javaToolchain.getHeaderCompiler(),
-                javaToolchain.getJvmOptions())
+                ImmutableList.<String>builder()
+                    .add("-Xbootclasspath/p:" + javacJar.getExecPath())
+                    .addAll(javaToolchain.getJvmOptions())
+                    .build())
             .setMnemonic("Turbine")
             .setProgressMessage(getProgressMessage())
             .build(ruleContext);
@@ -486,6 +488,7 @@ public class JavaHeaderCompileAction extends SpawnAction {
           .addPath(JavaCommon.getHostJavaExecutable(ruleContext))
           .add("-Xverify:none")
           .add(javaToolchain.getJvmOptions())
+          .addPaths("-Xbootclasspath/p:%s", javacJar.getExecPath())
           .addExecPath("-jar", javaToolchain.getHeaderCompiler());
     }
 
@@ -503,24 +506,19 @@ public class JavaHeaderCompileAction extends SpawnAction {
 
       result.add("--temp_dir").addPath(tempDirectory);
 
-      if (!Iterables.isEmpty(bootclasspathEntries)) {
-        result.add("--bootclasspath").addExecPaths(bootclasspathEntries);
-      }
+      result.addExecPaths("--bootclasspath", bootclasspathEntries);
 
-      if (!sourceFiles.isEmpty()) {
-        result.add("--sources").addExecPaths(sourceFiles);
-      }
+      result.addExecPaths("--sources", sourceFiles);
 
       if (!sourceJars.isEmpty()) {
-        result.add("--source_jars").addExecPaths(sourceJars);
+        result.addExecPaths("--source_jars", sourceJars);
       }
 
-      if (!javacOpts.isEmpty()) {
-        result.add("--javacopts").add(javacOpts);
-      }
+      result.add("--javacopts", javacOpts);
 
       if (ruleKind != null) {
-        result.add("--rule_kind").add(ruleKind);
+        result.add("--rule_kind");
+        result.add(ruleKind);
       }
       if (targetLabel != null) {
         result.add("--target_label");
@@ -533,9 +531,7 @@ public class JavaHeaderCompileAction extends SpawnAction {
           result.add("@" + targetLabel);
         }
       }
-      if (!classpathEntries.isEmpty()) {
-        result.add("--classpath").addExecPaths(classpathEntries);
-      }
+      result.addExecPaths("--classpath", classpathEntries);
       return result;
     }
 
@@ -544,18 +540,18 @@ public class JavaHeaderCompileAction extends SpawnAction {
       CustomCommandLine.Builder result = CustomCommandLine.builder();
       baseCommandLine(result, classpathEntries);
       if (!processorNames.isEmpty()) {
-        result.add("--processors").add(processorNames);
+        result.add("--processors", processorNames);
       }
       if (!processorFlags.isEmpty()) {
-        result.add("--javacopts").add(processorFlags);
+        result.add("--javacopts", processorFlags);
       }
       if (!processorPath.isEmpty()) {
-        result.add("--processorpath").addExecPaths(processorPath);
+        result.addExecPaths("--processorpath", processorPath);
       }
       if (strictJavaDeps != BuildConfiguration.StrictDepsMode.OFF) {
         result.add(new JavaCompileAction.JarsToTargetsArgv(classpathEntries, directJars));
         if (!compileTimeDependencyArtifacts.isEmpty()) {
-          result.add("--deps_artifacts").addExecPaths(compileTimeDependencyArtifacts);
+          result.addExecPaths("--deps_artifacts", compileTimeDependencyArtifacts);
         }
       }
       return result.build();
