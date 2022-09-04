cc120eefe6062d375fffc4677198a7b5db5aa4eb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:23:13 +0200

    protectionDomain creation for byteContent class definition for RuntimeClassLoader

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/runner/RuntimeClassLoader.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/runner/RuntimeClassLoader.java
@@ -8,15 +8,12 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.CodeSource;
@@ -252,7 +249,6 @@ public class RuntimeClassLoader extends ClassLoader implements ClassOutput, Tran
                         debugPath.mkdir();
                     }
                     File classFile = new File(debugPath, dotName + ".class");
-                    classFile.getParentFile().mkdirs();
                     try (FileOutputStream classWriter = new FileOutputStream(classFile)) {
                         classWriter.write(data);
                     }
@@ -278,25 +274,6 @@ public class RuntimeClassLoader extends ClassLoader implements ClassOutput, Tran
                 throw new RuntimeException(e);
             }
         }
-    }
-
-    @Override
-    public Writer writeSource(final String className) {
-        if (DEBUG_CLASSES_DIR != null) {
-            try {
-                File debugPath = new File(DEBUG_CLASSES_DIR);
-                if (!debugPath.exists()) {
-                    debugPath.mkdir();
-                }
-                File classFile = new File(debugPath, className + ".zig");
-                classFile.getParentFile().mkdirs();
-                log.infof("Wrote %s", classFile.getAbsolutePath());
-                return new OutputStreamWriter(new FileOutputStream(classFile), StandardCharsets.UTF_8);
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-        }
-        return ClassOutput.super.writeSource(className);
     }
 
     @Override
@@ -462,16 +439,7 @@ public class RuntimeClassLoader extends ClassLoader implements ClassOutput, Tran
         Path resourcePath = null;
 
         for (Path i : applicationClassDirectories) {
-            // Resource names are always separated by the "/" character.
-            // Here we are trying to resolve those resources using a filesystem
-            // Path, so we replace the "/" character with the filesystem
-            // specific separator before resolving
-            String path = name;
-            final String pathSeparator = i.getFileSystem().getSeparator();
-            if (!pathSeparator.equals("/")) {
-                path = name.replace("/", pathSeparator);
-            }
-            resourcePath = i.resolve(path);
+            resourcePath = i.resolve(name);
             if (Files.exists(resourcePath)) {
                 break;
             }
