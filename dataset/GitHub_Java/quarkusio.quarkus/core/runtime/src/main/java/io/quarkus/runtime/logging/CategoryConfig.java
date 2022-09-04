090715dbe617dc773445a5d91e8cbef2f63a5ce2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:16:12 +0200

    Remove the unused property minLevel from CategoryConfig

--- dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/io/quarkus/runtime/logging/CategoryConfig.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/io/quarkus/runtime/logging/CategoryConfig.java
@@ -1,8 +1,5 @@
 package io.quarkus.runtime.logging;
 
-import java.util.List;
-import java.util.Optional;
-
 import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 
@@ -13,18 +10,5 @@ public class CategoryConfig {
      * The log level level for this category
      */
     @ConfigItem(defaultValue = "inherit")
-    InheritableLevel level;
-
-    /**
-     * The names of the handlers to link to this category.
-     */
-    @ConfigItem
-    Optional<List<String>> handlers;
-
-    /**
-     * Specify whether or not this logger should send its output to its parent Logger
-     */
-    @ConfigItem(defaultValue = "true")
-    boolean useParentHandlers;
-
+    String level;
 }
