7f464651f06b2eacef733e2304ce74996a848fad
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 17:50:24 +0200

    Remove a usage of AndroidAaptVersion.
        Previous passes only removed instances of AAPT1.  Version is always AAPT2, unless if it's null (which has happened before :().
    
        PiperOrigin-RevId: 311556325

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/RClassGeneratorActionBuilder.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/RClassGeneratorActionBuilder.java
@@ -13,54 +13,33 @@
 // limitations under the License.
 package com.google.devtools.build.lib.rules.android;
 
-import com.google.common.base.Function;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.actions.Artifact;
-import com.google.devtools.build.lib.actions.ParameterFile.ParameterFileType;
-import com.google.devtools.build.lib.analysis.RuleConfiguredTarget.Mode;
-import com.google.devtools.build.lib.analysis.RuleContext;
-import com.google.devtools.build.lib.analysis.actions.CustomCommandLine;
-import com.google.devtools.build.lib.analysis.actions.SpawnAction;
-import com.google.devtools.build.lib.collect.nestedset.NestedSet;
-import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
-import com.google.devtools.build.lib.collect.nestedset.Order;
-import com.google.devtools.build.lib.rules.android.AndroidResourcesProvider.ResourceContainer;
+import com.google.devtools.build.lib.rules.android.AndroidDataConverter.JoinerType;
+import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
+import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec.VisibleForSerialization;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Nullable;
-
-/**
- * Builds up the spawn action for $android_rclass_generator.
- */
+/** Builds up the spawn action for $android_rclass_generator. */
 public class RClassGeneratorActionBuilder {
 
-  private final RuleContext ruleContext;
-  private ResourceContainer primary;
+  @AutoCodec @VisibleForSerialization
+  static final AndroidDataConverter<ValidatedAndroidResources> AAPT2_CONVERTER =
+      AndroidDataConverter.<ValidatedAndroidResources>builder(JoinerType.COLON_COMMA)
+          .with(RClassGeneratorActionBuilder::depsToBusyboxArg)
+          .build();
+
   private ResourceDependencies dependencies;
 
   private Artifact classJarOut;
 
-  private static final ContainerToArg DEPS_TO_ARG = new ContainerToArg();
-  private static final ContainerToArtifacts DEPS_TO_ARTIFACTS = new ContainerToArtifacts();
-
-  /**
-   * @param ruleContext The RuleContext that is used to create a SpawnAction.Builder.
-   */
-  public RClassGeneratorActionBuilder(RuleContext ruleContext) {
-    this.ruleContext = ruleContext;
-  }
-
-  public RClassGeneratorActionBuilder withPrimary(ResourceContainer primary) {
-    this.primary = primary;
-    return this;
-  }
+  private boolean finalFields = true;
 
   public RClassGeneratorActionBuilder withDependencies(ResourceDependencies resourceDeps) {
     this.dependencies = resourceDeps;
+    return this;
+  }
+
+  public RClassGeneratorActionBuilder finalFields(boolean finalFields) {
+    this.finalFields = finalFields;
     return this;
   }
 
@@ -69,76 +48,40 @@ public class RClassGeneratorActionBuilder {
     return this;
   }
 
-  private static class ContainerToArg implements Function<ResourceContainer, String> {
+  public ResourceApk build(AndroidDataContext dataContext, ProcessedAndroidData data) {
+    build(dataContext, data.getRTxt(), data.getManifest());
 
-    @Override
-    public String apply(ResourceContainer container) {
-      return (container.getRTxt() != null ? container.getRTxt().getExecPath() : "")
-          + ":"
-          + (container.getManifest() != null ? container.getManifest().getExecPath() : "");
-    }
+    return data.withValidatedResources(classJarOut);
   }
 
-  private static class ContainerToArtifacts implements
-      Function<ResourceContainer, NestedSet<Artifact>> {
+  private void build(
+      AndroidDataContext dataContext, Artifact rTxt, ProcessedAndroidManifest manifest) {
+    BusyBoxActionBuilder builder =
+        BusyBoxActionBuilder.create(dataContext, "GENERATE_BINARY_R")
+            .addInput("--primaryRTxt", rTxt)
+            .addInput("--primaryManifest", manifest.getManifest())
+            .maybeAddFlag("--packageForR", manifest.getPackage())
+            .addFlag(finalFields ? "--finalFields" : "--nofinalFields");
 
-    @Override
-    public NestedSet<Artifact> apply(ResourceContainer container) {
-      NestedSetBuilder<Artifact> artifacts = NestedSetBuilder.naiveLinkOrder();
-      addIfNotNull(container.getRTxt(), artifacts);
-      addIfNotNull(container.getManifest(), artifacts);
-      return artifacts.build();
+    if (dependencies != null && !dependencies.getResourceContainers().isEmpty()) {
+      builder
+          .addTransitiveFlagForEach(
+              "--library", dependencies.getResourceContainers(), AAPT2_CONVERTER)
+          .addTransitiveInputValues(dependencies.getTransitiveAapt2RTxt())
+          .addTransitiveInputValues(dependencies.getTransitiveManifests())
+          .addTransitiveInputValues(dependencies.getTransitiveAapt2ValidationArtifacts());
     }
 
-    private void addIfNotNull(@Nullable Artifact artifact, NestedSetBuilder<Artifact> artifacts) {
-      if (artifact != null) {
-        artifacts.add(artifact);
-      }
-    }
-
+    builder
+        .addOutput("--classJarOutput", classJarOut)
+        .addLabelFlag("--targetLabel")
+        .buildAndRegister("Generating R Classes", "RClassGenerator");
   }
 
-  public void build() {
-    CustomCommandLine.Builder builder = new CustomCommandLine.Builder();
-    NestedSetBuilder<Artifact> inputs = NestedSetBuilder.naiveLinkOrder();
-    inputs.addAll(ruleContext.getExecutablePrerequisite("$android_rclass_generator", Mode.HOST)
-        .getRunfilesSupport()
-        .getRunfilesArtifactsWithoutMiddlemen());
-
-    List<Artifact> outs = new ArrayList<>();
-    if (primary.getRTxt() != null) {
-      builder.addExecPath("--primaryRTxt", primary.getRTxt());
-      inputs.add(primary.getRTxt());
-    }
-    if (primary.getManifest() != null) {
-      builder.addExecPath("--primaryManifest", primary.getManifest());
-      inputs.add(primary.getManifest());
-    }
-    if (dependencies != null) {
-      Iterable<ResourceContainer> depResources = dependencies.getResources();
-      if (depResources.iterator().hasNext()) {
-        builder.addJoinStrings("--libraries", ",", Iterables.transform(depResources, DEPS_TO_ARG));
-        inputs.addTransitive(NestedSetBuilder.wrap(
-            Order.NAIVE_LINK_ORDER,
-            FluentIterable.from(depResources)
-                .transformAndConcat(DEPS_TO_ARTIFACTS)));
-      }
-    }
-    builder.addExecPath("--classJarOutput", classJarOut);
-    outs.add(classJarOut);
-
-    // Create the spawn action.
-    SpawnAction.Builder spawnActionBuilder = new SpawnAction.Builder();
-    ruleContext.registerAction(
-        spawnActionBuilder
-            .addTransitiveInputs(inputs.build())
-            .addOutputs(ImmutableList.<Artifact>copyOf(outs))
-            .useParameterFile(ParameterFileType.SHELL_QUOTED)
-            .setCommandLine(builder.build())
-            .setExecutable(
-                ruleContext.getExecutablePrerequisite("$android_rclass_generator", Mode.HOST))
-            .setProgressMessage("Generating R Classes: " + ruleContext.getLabel())
-            .setMnemonic("RClassGenerator")
-            .build(ruleContext));
+  private static String depsToBusyboxArg(ValidatedAndroidResources container) {
+    Artifact rTxt = container.getAapt2RTxt();
+    return (rTxt != null ? rTxt.getExecPath() : "")
+        + ","
+        + (container.getManifest() != null ? container.getManifest().getExecPath() : "");
   }
 }
