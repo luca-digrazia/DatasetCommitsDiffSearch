09c96ec2b82935552802773bc86acd3ad0c5f01e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:35:57 +0200

    Add a flag to stop adding the testrunner to java_test.deps
        PiperOrigin-RevId: 210843901

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaOptions.java
@@ -127,17 +127,6 @@ public class JavaOptions extends FragmentOptions {
   public List<String> javacOpts;
 
   @Option(
-      name = "host_javacopt",
-      allowMultiple = true,
-      defaultValue = "",
-      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-      effectTags = {OptionEffectTag.UNKNOWN},
-      help =
-          "Additional options to pass to javac when building tools that are executed during a"
-              + " build.")
-  public List<String> hostJavacOpts;
-
-  @Option(
     name = "jvmopt",
     allowMultiple = true,
     defaultValue = "",
@@ -590,7 +579,7 @@ public class JavaOptions extends FragmentOptions {
     host.javaBase = hostJavaBase;
     host.jvmOpts = ImmutableList.of("-XX:ErrorFile=/dev/stderr");
 
-    host.javacOpts = hostJavacOpts;
+    host.javacOpts = javacOpts;
     host.javaToolchain = hostJavaToolchain;
 
     host.javaLauncher = hostJavaLauncher;
