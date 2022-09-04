3589f3d292d1af01704295fcf45f1c20df936c8e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:51:54 +0200

    Introduce a processor that checks for split package issues.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/SplitPackageProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/SplitPackageProcessor.java
@@ -6,9 +6,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import org.jboss.jandex.ClassInfo;
 import org.jboss.logging.Logger;
@@ -32,19 +29,6 @@ public class SplitPackageProcessor {
 
     private static final Logger LOGGER = Logger.getLogger(SplitPackageProcessor.class);
 
-    private static final Predicate<String> IGNORE_PACKAGE = new Predicate<>() {
-
-        @Override
-        public boolean test(String packageName) {
-            // Remove the elements from this list when the original issue is fixed
-            // so that we can detect further issues.
-            return packageName.startsWith("io.fabric8.kubernetes")
-                    || packageName.equals("io.quarkus.hibernate.orm.panache")
-                    || packageName.equals("io.quarkus.mongodb.panache.reactive")
-                    || packageName.equals("io.quarkus.mongodb.panache");
-        }
-    };
-
     @BuildStep
     void splitPackageDetection(ApplicationArchivesBuildItem archivesBuildItem,
             // Dummy producer to make sure the build step executes
@@ -64,17 +48,12 @@ public class SplitPackageProcessor {
         }
         // for each split package, create something like
         // - "com.me.app.sub" found in [archiveA, archiveB]
-        StringBuilder splitPackagesWarning = new StringBuilder();
+        StringBuilder splitPackages = new StringBuilder();
         for (String packageName : packageToArchiveMap.keySet()) {
-            if (IGNORE_PACKAGE.test(packageName)) {
-                continue;
-            }
-
             Set<ApplicationArchive> applicationArchives = packageToArchiveMap.get(packageName);
             if (applicationArchives.size() > 1) {
-                splitPackagesWarning.append("\n- \"" + packageName + "\" found in ");
+                splitPackages.append("\n- \"" + packageName + "\" found in " + "[");
                 Iterator<ApplicationArchive> iterator = applicationArchives.iterator();
-                Set<String> splitPackages = new TreeSet<>();
                 while (iterator.hasNext()) {
                     ApplicationArchive next = iterator.next();
                     AppArtifactKey a = next.getArtifactKey();
@@ -82,28 +61,33 @@ public class SplitPackageProcessor {
                     if (a == null) {
                         if (archivesBuildItem.getRootArchive().equals(next)) {
                             // the archive we found is a root archive, e.g. application classes
-                            splitPackages.add("application classes");
+                            splitPackages.append("application classes");
                         } else {
                             // as next best effort, we try to take first path from archive paths collection
                             Iterator<Path> pathIterator = next.getPaths().iterator();
                             if (pathIterator.hasNext()) {
-                                splitPackages.add(pathIterator.next().toString());
+                                splitPackages.append(pathIterator.next().toString());
                             } else {
                                 // if all else fails, we mark it as unknown archive
-                                splitPackages.add("unknown archive");
+                                splitPackages.append("unknown archive");
                             }
                         }
+
                     } else {
-                        splitPackages.add(a.getGroupId() + ":" + a.getArtifactId());
+                        splitPackages.append(a.getGroupId() + ":" + a.getArtifactId());
+                    }
+                    if (iterator.hasNext()) {
+                        splitPackages.append(", ");
+                    } else {
+                        splitPackages.append("]");
                     }
                 }
-                splitPackagesWarning.append(splitPackages.stream().collect(Collectors.joining(", ", "[", "]")));
             }
         }
         // perform logging if needed
-        if (splitPackagesWarning.length() > 0) {
+        if (splitPackages.length() > 0) {
             LOGGER.warnf("Detected a split package usage which is considered a bad practice and should be avoided. " +
-                    "Following packages were detected in multiple archives: %s", splitPackagesWarning.toString());
+                    "Following packages were detected in multiple archives: %s", splitPackages.toString());
         }
     }
 }
