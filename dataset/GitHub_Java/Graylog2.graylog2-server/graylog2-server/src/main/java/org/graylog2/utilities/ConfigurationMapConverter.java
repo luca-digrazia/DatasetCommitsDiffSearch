02cf8a5b1efccfabeb9fc7996d4d8f0cd56f0047
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:29:36 +0200

    Merge pull request #1543 from Graylog2/web-issue-1677
        Prevent conversion of `null` to "null" in ConfigurationMapConverter
        (cherry picked from commit 3aa106443362bd1683015d082eeafaf76e0997fe)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/utilities/ConfigurationMapConverter.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/utilities/ConfigurationMapConverter.java
@@ -1,30 +1,30 @@
 /**
- * This file is part of Graylog2.
+ * This file is part of Graylog.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.utilities;
 
 import com.google.common.collect.Maps;
-import org.graylog2.plugin.database.ValidationException;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
+import org.graylog2.plugin.database.ValidationException;
 
 import java.util.Map;
 
 public class ConfigurationMapConverter {
     /**
-     * Converts the values in the map to the requested types. This has been copied from the Graylog2 web interface
+     * Converts the values in the map to the requested types. This has been copied from the Graylog web interface
      * and should be removed once we have better configuration objects.
      */
     public static Map<String, Object> convertValues(final Map<String, Object> data, final ConfigurationRequest configurationRequest) throws ValidationException {
@@ -32,30 +32,36 @@ public class ConfigurationMapConverter {
         final Map<String, Map<String, Object>> configurationFields = configurationRequest.asList();
 
         for (final Map.Entry<String, Object> entry : data.entrySet()) {
-            final Object value;
+            final String field = entry.getKey();
+            final Map<String, Object> fieldDescription = configurationFields.get(field);
+            if (fieldDescription == null || fieldDescription.isEmpty()) {
+                throw new ValidationException(field, "Unknown configuration field description for field \"" + field + "\"");
+            }
+
+            final String type = (String) fieldDescription.get("type");
+
             // Decide what to cast to. (string, bool, number)
-            switch ((String) configurationFields.get(entry.getKey()).get("type")) {
+            final Object value;
+            switch (type) {
                 case "text":
-                    value = String.valueOf(entry.getValue());
+                case "dropdown":
+                    value = entry.getValue() == null ? "" : String.valueOf(entry.getValue());
                     break;
                 case "number":
                     try {
                         value = Integer.parseInt(String.valueOf(entry.getValue()));
                     } catch (NumberFormatException e) {
-                        throw new ValidationException(entry.getKey(), e.getMessage());
+                        throw new ValidationException(field, e.getMessage());
                     }
                     break;
                 case "boolean":
-                    value = "true".equals(String.valueOf(entry.getValue()));
-                    break;
-                case "dropdown":
-                    value = String.valueOf(entry.getValue());
+                    value = "true".equalsIgnoreCase(String.valueOf(entry.getValue()));
                     break;
                 default:
-                    value = entry.getValue();
+                    throw new ValidationException(field, "Unknown configuration field type \"" + type + "\"");
             }
 
-            configuration.put(entry.getKey(), value);
+            configuration.put(field, value);
         }
 
         return configuration;
