f5101a297036fa8f76ddfbd5598db1c5bd204682
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:26:37 +0200

    Remove the unnecessary unbounded thread pool
        Even if we wanted to do the stats in a thread pool or async, we'd have
        to do it differently. But it's probably not worth it - the stat results
        should already be in the kernel cache (since we just stat the files
        earlier - all action outputs are stat at the end of action execution).
    
        PiperOrigin-RevId: 214400653

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/buildeventstream/LocalFilesArtifactUploader.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/buildeventstream/LocalFilesArtifactUploader.java
@@ -16,18 +16,13 @@ package com.google.devtools.build.lib.buildeventstream;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.devtools.build.lib.buildeventstream.BuildEvent.LocalFile;
-import com.google.devtools.build.lib.buildeventstream.PathConverter.FileUriPathConverter;
 import com.google.devtools.build.lib.vfs.Path;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nullable;
 
 /** An uploader that simply turns paths into local file URIs. */
-public class LocalFilesArtifactUploader implements BuildEventArtifactUploader {
-  private static final FileUriPathConverter FILE_URI_PATH_CONVERTER = new FileUriPathConverter();
-  private final ConcurrentHashMap<Path, Boolean> fileIsDirectory = new ConcurrentHashMap<>();
-
+class LocalFilesArtifactUploader implements BuildEventArtifactUploader {
   @Override
   public ListenableFuture<PathConverter> upload(Map<Path, LocalFile> files) {
     return Futures.immediateFuture(new PathConverterImpl(files.keySet()));
@@ -38,7 +33,8 @@ public class LocalFilesArtifactUploader implements BuildEventArtifactUploader {
     // Intentionally left empty
   }
 
-  private class PathConverterImpl implements PathConverter {
+  private static class PathConverterImpl implements PathConverter {
+    private static final FileUriPathConverter FILE_URI_PATH_CONVERTER = new FileUriPathConverter();
     private final Set<Path> paths;
 
     private PathConverterImpl(Set<Path> paths) {
@@ -52,7 +48,7 @@ public class LocalFilesArtifactUploader implements BuildEventArtifactUploader {
         // We should throw here, the file wasn't declared in BuildEvent#referencedLocalFiles
         return null;
       }
-      if (fileIsDirectory.computeIfAbsent(path, Path::isDirectory)) {
+      if (path.isDirectory()) {
         return null;
       }
       return FILE_URI_PATH_CONVERTER.apply(path);
