f7cd151ba101924e75f5ab25f34dc9a3e8fe36ca
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:53:48 +0200

    Fix VisibleForTesting warnings for RuleConfiguredTarget
        RuleConfiguredTarget is using VisibleForTesting annotations even though the
        class is used in production. mproperly, resulting with warnings from
        error-prone. Drop the annotation for the class and methods declared in the
        interface it implements.
    
        RELNOTES: None.
        PiperOrigin-RevId: 246035331

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/RuleConfiguredTargetValue.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/RuleConfiguredTargetValue.java
@@ -16,8 +16,10 @@ package com.google.devtools.build.lib.skyframe;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.actions.ActionAnalysisMetadata;
 import com.google.devtools.build.lib.actions.ActionLookupValue;
+import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
 import com.google.devtools.build.lib.analysis.configuredtargets.RuleConfiguredTarget;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
@@ -25,6 +27,7 @@ import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
 import com.google.devtools.build.lib.packages.Package;
 import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
+import java.math.BigInteger;
 import javax.annotation.Nullable;
 
 /** A configured target in the context of a Skyframe graph. */
@@ -38,6 +41,7 @@ public final class RuleConfiguredTargetValue extends ActionLookupValue
   // clear(true) is called.
   @Nullable private RuleConfiguredTarget configuredTarget;
   private final ImmutableList<ActionAnalysisMetadata> actions;
+  private final ImmutableMap<Artifact, Integer> generatingActionIndex;
 
   // May be null either after clearing or because transitive packages are not tracked.
   @Nullable private NestedSet<Package> transitivePackagesForPackageRootResolution;
@@ -45,16 +49,22 @@ public final class RuleConfiguredTargetValue extends ActionLookupValue
   // Transitive packages are not serialized.
   @AutoCodec.Instantiator
   RuleConfiguredTargetValue(RuleConfiguredTarget configuredTarget) {
-    this(configuredTarget, /*transitivePackagesForPackageRootResolution=*/ null);
+    this(
+        configuredTarget,
+        /*transitivePackagesForPackageRootResolution=*/ null,
+        /*nonceVersion=*/ null);
   }
 
   RuleConfiguredTargetValue(
       RuleConfiguredTarget configuredTarget,
-      @Nullable NestedSet<Package> transitivePackagesForPackageRootResolution) {
+      @Nullable NestedSet<Package> transitivePackagesForPackageRootResolution,
+      @Nullable BigInteger nonceVersion) {
+    super(nonceVersion);
     this.configuredTarget = Preconditions.checkNotNull(configuredTarget);
     this.transitivePackagesForPackageRootResolution = transitivePackagesForPackageRootResolution;
     // These are specifically *not* copied to save memory.
     this.actions = configuredTarget.getActions();
+    this.generatingActionIndex = configuredTarget.getGeneratingActionIndex();
   }
 
   @Override
@@ -66,6 +76,11 @@ public final class RuleConfiguredTargetValue extends ActionLookupValue
   @Override
   public ImmutableList<ActionAnalysisMetadata> getActions() {
     return actions;
+  }
+
+  @Override
+  protected ImmutableMap<Artifact, Integer> getGeneratingActionIndex() {
+    return generatingActionIndex;
   }
 
   @Override
@@ -85,6 +100,7 @@ public final class RuleConfiguredTargetValue extends ActionLookupValue
   @Override
   public String toString() {
     return MoreObjects.toStringHelper(this)
+        .add("generatingActionIndex", generatingActionIndex)
         .add("actions", actions)
         .add("configuredTarget", configuredTarget)
         .toString();
