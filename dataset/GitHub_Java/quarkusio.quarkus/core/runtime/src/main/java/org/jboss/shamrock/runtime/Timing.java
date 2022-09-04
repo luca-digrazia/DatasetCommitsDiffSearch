dc772ed53cf813f46cbe77a1c5d021f7656523a9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:14:19 +0200

    Use logging (but not too early) to print nicer startup messages

--- dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/org/jboss/shamrock/runtime/Timing.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/org/jboss/shamrock/runtime/Timing.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.jboss.shamrock.runtime;
 
 import org.jboss.logging.Logger;
@@ -44,10 +28,9 @@ public class Timing {
         bootStartTime = System.nanoTime();
     }
 
-    public static void printStartupTime(String version, String features, String httpServer) {
+    public static void printStartupTime() {
         final long time = System.nanoTime() - bootStartTime + 500;
-        Logger.getLogger("org.jboss.shamrock").infof("Shamrock %s started in %d.%03dms. %s", version, Long.valueOf(time / 1_000_000), Long.valueOf(time % 1_000_000 / 1_000), httpServer);
-        Logger.getLogger("org.jboss.shamrock").infof("Installed features: [%s]", features);
+        Logger.getLogger("org.jboss.shamrock").infof("Shamrock started in %d.%03dms", Long.valueOf(time / 1_000_000), Long.valueOf(time % 1_000_000 / 1_000));
     }
 
 }
