ec39d118bcfb21e4b393856a0ffea032f9fce6b1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:46:34 +0200

    Apply test profiles on super-tests, for native image

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit5/src/main/java/io/quarkus/test/junit/NativeTestExtension.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit5/src/main/java/io/quarkus/test/junit/NativeTestExtension.java
@@ -1,31 +1,35 @@
 package io.quarkus.test.junit;
 
-import static io.quarkus.test.junit.IntegrationTestUtil.determineTestProfileAndProperties;
-import static io.quarkus.test.junit.IntegrationTestUtil.doProcessTestInstance;
-import static io.quarkus.test.junit.IntegrationTestUtil.ensureNoInjectAnnotationIsUsed;
-import static io.quarkus.test.junit.IntegrationTestUtil.getSysPropsToRestore;
-import static io.quarkus.test.junit.IntegrationTestUtil.handleDevDb;
-import static io.quarkus.test.junit.IntegrationTestUtil.startLauncher;
-
-import java.util.Collections;
+import java.io.Closeable;
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.enterprise.inject.Alternative;
+import javax.inject.Inject;
 
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+import org.junit.platform.commons.JUnitException;
 import org.opentest4j.TestAbortedException;
 
+import io.quarkus.runtime.configuration.ProfileManager;
 import io.quarkus.runtime.test.TestHttpEndpointProvider;
 import io.quarkus.test.common.NativeImageLauncher;
+import io.quarkus.test.common.PropertyTestUtil;
 import io.quarkus.test.common.RestAssuredURLManager;
 import io.quarkus.test.common.TestResourceManager;
 import io.quarkus.test.common.TestScopeManager;
+import io.quarkus.test.common.http.TestHTTPResourceManager;
 
 public class NativeTestExtension
         implements BeforeEachCallback, AfterEachCallback, BeforeAllCallback, TestInstancePostProcessor {
@@ -65,15 +69,31 @@ public class NativeTestExtension
         ensureStarted(extensionContext);
     }
 
-    private IntegrationTestExtensionState ensureStarted(ExtensionContext extensionContext) {
+    private void ensureNoInjectAnnotationIsUsed(Class<?> testClass) {
+        Class<?> current = testClass;
+        while (current.getSuperclass() != null) {
+            for (Field field : current.getDeclaredFields()) {
+                Inject injectAnnotation = field.getAnnotation(Inject.class);
+                if (injectAnnotation != null) {
+                    throw new JUnitException(
+                            "@Inject is not supported in NativeImageTest tests. Offending field is "
+                                    + field.getDeclaringClass().getTypeName() + "."
+                                    + field.getName());
+                }
+            }
+            current = current.getSuperclass();
+        }
+
+    }
+
+    private ExtensionState ensureStarted(ExtensionContext extensionContext) {
         Class<?> testClass = extensionContext.getRequiredTestClass();
         ensureNoInjectAnnotationIsUsed(testClass);
 
         ExtensionContext root = extensionContext.getRoot();
         ExtensionContext.Store store = root.getStore(ExtensionContext.Namespace.GLOBAL);
-        IntegrationTestExtensionState state = store.get(IntegrationTestExtensionState.class.getName(),
-                IntegrationTestExtensionState.class);
-        Class<? extends QuarkusTestProfile> selectedProfile = IntegrationTestUtil.findProfile(testClass);
+        ExtensionState state = store.get(ExtensionState.class.getName(), ExtensionState.class);
+        Class<? extends QuarkusTestProfile> selectedProfile = findProfile(testClass);
         boolean wrongProfile = !Objects.equals(selectedProfile, quarkusTestProfile);
         // we reload the test resources if we changed test class and if we had or will have per-test test resources
         boolean reloadTestResources = !Objects.equals(extensionContext.getRequiredTestClass(), currentJUnitTestClass)
@@ -88,9 +108,10 @@ public class NativeTestExtension
                     }
                 }
             }
+            PropertyTestUtil.setLogFileProperty();
             try {
                 state = doNativeStart(extensionContext, selectedProfile);
-                store.put(IntegrationTestExtensionState.class.getName(), state);
+                store.put(ExtensionState.class.getName(), state);
 
             } catch (Throwable e) {
                 failedBoot = true;
@@ -100,33 +121,80 @@ public class NativeTestExtension
         return state;
     }
 
-    private IntegrationTestExtensionState doNativeStart(ExtensionContext context, Class<? extends QuarkusTestProfile> profile)
+    private Class<? extends QuarkusTestProfile> findProfile(Class<?> testClass) {
+        while (testClass != Object.class) {
+            TestProfile annotation = testClass.getAnnotation(TestProfile.class);
+            if (annotation != null) {
+                return annotation.value();
+            }
+            testClass = testClass.getSuperclass();
+        }
+        return null;
+    }
+
+    private ExtensionState doNativeStart(ExtensionContext context, Class<? extends QuarkusTestProfile> profile)
             throws Throwable {
-        Map<String, String> devDbProps = handleDevDb(context);
         quarkusTestProfile = profile;
         currentJUnitTestClass = context.getRequiredTestClass();
         TestResourceManager testResourceManager = null;
         try {
             Class<?> requiredTestClass = context.getRequiredTestClass();
 
-            Map<String, String> sysPropRestore = getSysPropsToRestore();
-            TestProfileAndProperties testProfileAndProperties = determineTestProfileAndProperties(profile, sysPropRestore);
+            Map<String, String> sysPropRestore = new HashMap<>();
+            sysPropRestore.put(ProfileManager.QUARKUS_TEST_PROFILE_PROP,
+                    System.getProperty(ProfileManager.QUARKUS_TEST_PROFILE_PROP));
 
-            testResourceManager = new TestResourceManager(requiredTestClass, quarkusTestProfile,
-                    Collections.emptyList(), testProfileAndProperties.testProfile != null
-                            && testProfileAndProperties.testProfile.disableGlobalTestResources());
+            QuarkusTestProfile profileInstance = null;
+            final Map<String, String> additional = new HashMap<>();
+            if (profile != null) {
+                profileInstance = profile.newInstance();
+                additional.putAll(profileInstance.getConfigOverrides());
+                final Set<Class<?>> enabledAlternatives = profileInstance.getEnabledAlternatives();
+                if (!enabledAlternatives.isEmpty()) {
+                    additional.put("quarkus.arc.selected-alternatives", enabledAlternatives.stream()
+                            .peek((c) -> {
+                                if (!c.isAnnotationPresent(Alternative.class)) {
+                                    throw new RuntimeException(
+                                            "Enabled alternative " + c + " is not annotated with @Alternative");
+                                }
+                            })
+                            .map(Class::getName).collect(Collectors.joining(",")));
+                }
+                final String configProfile = profileInstance.getConfigProfile();
+                if (configProfile != null) {
+                    additional.put(ProfileManager.QUARKUS_PROFILE_PROP, configProfile);
+                }
+                additional.put("quarkus.configuration.build-time-mismatch-at-runtime", "fail");
+                for (Map.Entry<String, String> i : additional.entrySet()) {
+                    sysPropRestore.put(i.getKey(), System.getProperty(i.getKey()));
+                }
+                for (Map.Entry<String, String> i : additional.entrySet()) {
+                    System.setProperty(i.getKey(), i.getValue());
+                }
+            }
+
+            testResourceManager = new TestResourceManager(requiredTestClass);
             testResourceManager.init();
             hasPerTestResources = testResourceManager.hasPerTestResources();
 
-            Map<String, String> additionalProperties = new HashMap<>(testProfileAndProperties.properties);
-            additionalProperties.putAll(devDbProps);
-            additionalProperties.putAll(testResourceManager.start());
+            additional.putAll(testResourceManager.start());
 
             NativeImageLauncher launcher = new NativeImageLauncher(requiredTestClass);
-            startLauncher(launcher, additionalProperties, () -> ssl = true);
+            launcher.addSystemProperties(additional);
+            try {
+                launcher.start();
+            } catch (IOException e) {
+                try {
+                    launcher.close();
+                } catch (Throwable t) {
+                }
+                throw e;
+            }
+            if (launcher.isDefaultSsl()) {
+                ssl = true;
+            }
 
-            final IntegrationTestExtensionState state = new IntegrationTestExtensionState(testResourceManager, launcher,
-                    sysPropRestore);
+            final ExtensionState state = new ExtensionState(testResourceManager, launcher, sysPropRestore);
 
             testHttpEndpointProviders = TestHttpEndpointProvider.load();
 
@@ -145,9 +213,13 @@ public class NativeTestExtension
     }
 
     @Override
-    public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
+    public void postProcessTestInstance(Object testInstance, ExtensionContext context) throws Exception {
         if (!failedBoot) {
-            doProcessTestInstance(testInstance, context);
+            TestHTTPResourceManager.inject(testInstance);
+            ExtensionContext root = context.getRoot();
+            ExtensionContext.Store store = root.getStore(ExtensionContext.Namespace.GLOBAL);
+            ExtensionState state = store.get(ExtensionState.class.getName(), ExtensionState.class);
+            state.testResourceManager.inject(testInstance);
         }
     }
 
@@ -155,15 +227,49 @@ public class NativeTestExtension
         if (firstException != null) {
             Throwable throwable = firstException;
             firstException = null;
-
-            if (throwable instanceof Exception) {
-                throw (Exception) throwable;
-            }
-
             throw new RuntimeException(throwable);
         } else {
             throw new TestAbortedException("Boot failed");
         }
     }
 
+    public class ExtensionState implements ExtensionContext.Store.CloseableResource {
+
+        private final TestResourceManager testResourceManager;
+        private final Closeable resource;
+        private final Map<String, String> sysPropRestore;
+        private final Thread shutdownHook;
+
+        ExtensionState(TestResourceManager testResourceManager, Closeable resource, Map<String, String> sysPropRestore) {
+            this.testResourceManager = testResourceManager;
+            this.resource = resource;
+            this.sysPropRestore = sysPropRestore;
+            this.shutdownHook = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        ExtensionState.this.close();
+                    } catch (IOException ignored) {
+                    }
+                }
+            }, "Quarkus Test Cleanup Shutdown task");
+            Runtime.getRuntime().addShutdownHook(shutdownHook);
+
+        }
+
+        @Override
+        public void close() throws IOException {
+            testResourceManager.close();
+            resource.close();
+            for (Map.Entry<String, String> entry : sysPropRestore.entrySet()) {
+                String val = entry.getValue();
+                if (val == null) {
+                    System.clearProperty(entry.getKey());
+                } else {
+                    System.setProperty(entry.getKey(), val);
+                }
+            }
+            Runtime.getRuntime().removeShutdownHook(shutdownHook);
+        }
+    }
 }
