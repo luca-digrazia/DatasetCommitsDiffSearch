6fa8c82fcec96f026aa242848b5eed090168c43d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:05:23 +0200

    Panache: Support collections in Hibernate Filters
        This PR allows to filter by collections using Panache:
    
        ```java
        @FilterDef(name = "nameFilter", parameters = {@ParamDef(name = "names", type = "string")})
        @Filter(name = "nameFilter", condition = "name in (:names)")
        public class Person extends PanacheEntity {
        // ...
        }
        ```

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/hibernate-orm-panache/src/test/java/io/quarkus/it/panache/PanacheFunctionalityTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/hibernate-orm-panache/src/test/java/io/quarkus/it/panache/PanacheFunctionalityTest.java
@@ -1,28 +1,27 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.quarkus.it.panache;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.StringWriter;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.transaction.Transactional;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.it.panache.Person;
-import io.quarkus.test.junit.DisabledOnSubstrate;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.quarkus.hibernate.orm.runtime.PersistenceUnitUtil;
+import io.quarkus.test.junit.DisabledOnIntegrationTest;
+import io.quarkus.test.junit.DisabledOnNativeImage;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -33,6 +32,14 @@ import io.restassured.http.ContentType;
 @QuarkusTest
 public class PanacheFunctionalityTest {
 
+    /**
+     * Tests that direct use of the entity in the test class does not break transformation
+     *
+     * see https://github.com/quarkusio/quarkus/issues/1724
+     */
+    @SuppressWarnings("unused")
+    Person p = new Person();
+
     @Test
     public void testPanacheFunctionality() throws Exception {
         RestAssured.when().get("/test/model-dao").then().body(is("OK"));
@@ -41,6 +48,7 @@ public class PanacheFunctionalityTest {
 
         RestAssured.when().get("/test/model1").then().body(is("OK"));
         RestAssured.when().get("/test/model2").then().body(is("OK"));
+        RestAssured.when().get("/test/projection").then().body(is("OK"));
         RestAssured.when().get("/test/model3").then().body(is("OK"));
     }
 
@@ -56,9 +64,169 @@ public class PanacheFunctionalityTest {
                         "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><person><id>666</id><name>Eddie</name><serialisationTrick>1</serialisationTrick><status>DECEASED</status></person>"));
     }
 
-    @DisabledOnSubstrate
+    @DisabledOnNativeImage
     @Test
     public void testPanacheInTest() {
         Assertions.assertEquals(0, Person.count());
+    }
+
+    @Test
+    public void testBug5274() {
+        RestAssured.when().get("/test/5274").then().body(is("OK"));
+    }
+
+    @Test
+    public void testBug5885() {
+        RestAssured.when().get("/test/5885").then().body(is("OK"));
+    }
+
+    @Test
+    public void testFilterWithCollections() {
+        RestAssured.when().get("/test/testFilterWithCollections").then().body(is("OK"));
+    }
+
+    @Test
+    public void testJaxbAnnotationTransfer() {
+        RestAssured.when()
+                .get("/test/testJaxbAnnotationTransfer")
+                .then()
+                .body(is("OK"));
+    }
+
+    /**
+     * _PanacheEntityBase_ has the method _isPersistent_. This method is used by Jackson to serialize the attribute *peristent*
+     * in the JSON which is not intended. This test ensures that the attribute *persistent* is not generated when using Jackson.
+     *
+     * This test does not interact with the Quarkus application itself. It is just using the Jackson ObjectMapper with a
+     * PanacheEntity. Thus this test is disabled in native mode. The test code runs the JVM and not native.
+     */
+    @DisabledOnNativeImage
+    @Test
+    public void jacksonDeserializationIgnoresPersistentAttribute() throws JsonProcessingException {
+        // set Up
+        Person person = new Person();
+        person.name = "max";
+        // do
+        ObjectMapper objectMapper = new ObjectMapper();
+        // make sure the Jaxb module is loaded
+        objectMapper.findAndRegisterModules();
+        String personAsString = objectMapper.writeValueAsString(person);
+        // check
+        // hence no 'persistence'-attribute
+        assertEquals(
+                "{\"id\":null,\"name\":\"max\",\"uniqueName\":null,\"address\":null,\"status\":null,\"dogs\":[],\"serialisationTrick\":1}",
+                personAsString);
+    }
+
+    /**
+     * This test is disabled in native mode as there is no interaction with the quarkus integration test endpoint.
+     */
+    @DisabledOnNativeImage
+    @Test
+    public void jaxbDeserializationHasAllFields() throws JsonProcessingException, JAXBException {
+        // set Up
+        Person person = new Person();
+        person.name = "max";
+        // do
+        JAXBContext jaxbContext = JAXBContext.newInstance(Person.class);
+
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        StringWriter sw = new StringWriter();
+        marshaller.marshal(person, sw);
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
+                + "<person><name>max</name><serialisationTrick>1</serialisationTrick></person>",
+                sw.toString());
+    }
+
+    /**
+     * This test is disabled in native mode as there is no interaction with the quarkus integration test endpoint.
+     */
+    @DisabledOnIntegrationTest
+    @Test
+    public void jsonbDeserializationHasAllFields() throws JsonProcessingException {
+        // set Up
+        Person person = new Person();
+        person.name = "max";
+        // do
+
+        Jsonb jsonb = JsonbBuilder.create();
+        String json = jsonb.toJson(person);
+        assertEquals(
+                "{\"dogs\":[],\"name\":\"max\",\"serialisationTrick\":1}",
+                json);
+    }
+
+    @Test
+    public void testCompositeKey() {
+        RestAssured.when()
+                .get("/test/composite")
+                .then()
+                .body(is("OK"));
+    }
+
+    @Test
+    public void testBug7721() {
+        RestAssured.when().get("/test/7721").then().body(is("OK"));
+    }
+
+    @Test
+    public void testBug8254() {
+        RestAssured.when().get("/test/8254").then().body(is("OK"));
+    }
+
+    @Test
+    public void testBug9025() {
+        RestAssured.when().get("/test/9025").then().body(is("OK"));
+    }
+
+    @Test
+    public void testBug9036() {
+        RestAssured.when().get("/test/9036").then().body(is("OK"));
+    }
+
+    @Test
+    public void testMetrics() {
+        RestAssured.when()
+                .get("/q/metrics")
+                .then()
+                .body(containsString("vendor_hibernate_cache_update_timestamps_requests_total{entityManagerFactory=\""
+                        + PersistenceUnitUtil.DEFAULT_PERSISTENCE_UNIT_NAME + "\",result=\"miss\"}"));
+    }
+
+    @DisabledOnNativeImage
+    @Transactional
+    @Test
+    void testBug7102InOneTransaction() {
+        testBug7102();
+    }
+
+    @DisabledOnNativeImage
+    @Test
+    public void testBug7102() {
+        Person person = createBug7102();
+        Person person1 = getBug7102(person.id);
+        Assertions.assertEquals("pero", person1.name);
+        updateBug7102(person.id);
+        Person person2 = getBug7102(person.id);
+        Assertions.assertEquals("jozo", person2.name);
+    }
+
+    @Transactional
+    Person createBug7102() {
+        Person personPanache = new Person();
+        personPanache.name = "pero";
+        personPanache.persistAndFlush();
+        return personPanache;
+    }
+
+    @Transactional
+    void updateBug7102(Long id) {
+        final Person person = Person.findById(id);
+        person.name = "jozo";
+    }
+
+    @Transactional
+    Person getBug7102(Long id) {
+        return Person.findById(id);
     }
 }
