b9193402f2d0f15918cdf72a7aceb7c83036417a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:00:07 +0200

    Fix RestResourceBaseTest

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/rest/resources/RestResourceBaseTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/rest/resources/RestResourceBaseTest.java
@@ -16,18 +16,72 @@
  */
 package org.graylog2.rest.resources;
 
-import com.google.inject.Module;
-import org.assertj.core.util.Lists;
+import org.apache.shiro.subject.Subject;
 import org.graylog2.shared.bindings.GuiceInjectorHolder;
+import org.graylog2.shared.rest.resources.RestResource;
 import org.junit.Before;
+import org.junit.Test;
 
-import java.util.List;
+import java.util.Collections;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RestResourceBaseTest {
     @Before
     public void setUpInjector() throws Exception {
         // The list of modules is empty for now so only JIT injection will be used.
-        final List<Module> modules = Lists.emptyList();
-        GuiceInjectorHolder.createInjector(modules);
+        GuiceInjectorHolder.createInjector(Collections.emptyList());
+    }
+
+    @Test
+    public void testisAnyPermitted() {
+        final PermissionDeniedResource failingResource = new PermissionDeniedResource();
+        final AllPermissionsGrantedResource allGranted = new AllPermissionsGrantedResource();
+        final SomePermissionsGrantedResource someGranted = new SomePermissionsGrantedResource();
+
+        assertFalse("User doesn't have any permissions", failingResource.runCheck());
+        assertTrue("User has all permissions", allGranted.runCheck());
+        assertTrue("User has some permissions", someGranted.runCheck());
+    }
+
+    private static class PermissionDeniedResource extends RestResource {
+        @Override
+        protected Subject getSubject() {
+            final Subject mock = mock(Subject.class);
+            when(mock.isPermitted((String[]) any())).thenReturn(new boolean[]{false, false});
+            return mock;
+        }
+
+        public boolean runCheck() {
+            return isAnyPermitted(new String[]{"a:b", "a:c"}, "instance");
+        }
+    }
+    private static class AllPermissionsGrantedResource extends RestResource {
+        @Override
+        protected Subject getSubject() {
+            final Subject mock = mock(Subject.class);
+            when(mock.isPermitted((String[]) any())).thenReturn(new boolean[]{true, true});
+            return mock;
+        }
+
+        public boolean runCheck() {
+            return isAnyPermitted(new String[]{"a:b", "a:c"}, "instance");
+        }
+    }
+    private static class SomePermissionsGrantedResource extends RestResource {
+        @Override
+        protected Subject getSubject() {
+            final Subject mock = mock(Subject.class);
+            when(mock.isPermitted((String[]) any())).thenReturn(new boolean[]{false, true});
+            return mock;
+        }
+
+        public boolean runCheck() {
+            return isAnyPermitted(new String[]{"a:b", "a:c"}, "instance");
+        }
     }
 }
