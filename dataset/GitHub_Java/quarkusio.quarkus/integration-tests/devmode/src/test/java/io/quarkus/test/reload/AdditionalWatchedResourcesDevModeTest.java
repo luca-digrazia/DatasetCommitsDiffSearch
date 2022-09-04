0b84962f6376416c2b2b662390e6514b27ef9f92
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:50:42 +0200

    Register profile specific configuration files for hot-reload

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/devmode/src/test/java/io/quarkus/test/reload/AdditionalWatchedResourcesDevModeTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/devmode/src/test/java/io/quarkus/test/reload/AdditionalWatchedResourcesDevModeTest.java
@@ -6,22 +6,30 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusDevModeTest;
 import io.restassured.RestAssured;
 
+@DisabledOnOs(OS.WINDOWS)
 public class AdditionalWatchedResourcesDevModeTest {
 
     private static final String RES_WATCHED = "watched.txt";
     private static final String RES_WATCHED_SUBPATH = "sub/watched.txt";
     private static final String RES_NOT_WATCHED = "not-watched.txt";
+    private static final String RES_GLOB_WATCHED = "*.sample";
+    private static final String SAMPLE_FILE = "data.sample";
+    private static final String APPLICATION_DEV_PROPERTIES = "application-dev.properties";
 
     private static final String PROPERTY = "quarkus.live-reload.watched-resources="
-            + RES_WATCHED + "," + RES_WATCHED_SUBPATH;
+            + RES_WATCHED + "," + RES_WATCHED_SUBPATH + "," + RES_GLOB_WATCHED;
 
     private static final String MODIFIED = "modified";
     private static final String INITIAL = "initial";
+    private static final String INITIAL_PROPERTY = "greeting.message=initial";
+    private static final String MODIFIED_PROPERTY = "greeting.message=modified";
 
     @RegisterExtension
     static final QuarkusDevModeTest TEST = new QuarkusDevModeTest()
@@ -30,6 +38,8 @@ public class AdditionalWatchedResourcesDevModeTest {
                     .addAsResource(new StringAsset(INITIAL), RES_WATCHED)
                     .addAsResource(new StringAsset(INITIAL), RES_WATCHED_SUBPATH)
                     .addAsResource(new StringAsset(INITIAL), RES_NOT_WATCHED)
+                    .addAsResource(new StringAsset(INITIAL), SAMPLE_FILE)
+                    .addAsResource(new StringAsset(INITIAL_PROPERTY), "application-dev.properties")
                     .addAsResource(new StringAsset(PROPERTY), "application.properties"));
 
     @Test
@@ -58,4 +68,23 @@ public class AdditionalWatchedResourcesDevModeTest {
 
         RestAssured.get("/content/{name}", RES_NOT_WATCHED).then().body(is(INITIAL));
     }
+
+    @Test
+    public void globWatch() {
+        RestAssured.get("/content/{name}", SAMPLE_FILE).then().body(is(INITIAL));
+
+        TEST.modifyResourceFile(SAMPLE_FILE, oldSource -> MODIFIED);
+
+        RestAssured.get("/content/{name}", SAMPLE_FILE).then().body(is(MODIFIED));
+    }
+
+    @Test
+    public void watchedProfileSpecificConfigFile() {
+        RestAssured.get("/content/{name}", APPLICATION_DEV_PROPERTIES).then().body(is(INITIAL_PROPERTY));
+
+        TEST.modifyResourceFile(APPLICATION_DEV_PROPERTIES, oldSource -> MODIFIED_PROPERTY);
+
+        RestAssured.get("/content/{name}", APPLICATION_DEV_PROPERTIES).then().body(is(MODIFIED_PROPERTY));
+    }
+
 }
