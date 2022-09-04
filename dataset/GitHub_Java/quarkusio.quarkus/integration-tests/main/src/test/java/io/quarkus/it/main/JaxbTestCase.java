f8645ef2a2310681cebbd0f3f107ceaa077dfedc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:04:11 +0200

    Ensure @QuarkusTest works with @Nested and test lifecycle methods
        Fixes: #17975

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/main/src/test/java/io/quarkus/it/main/JaxbTestCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/main/src/test/java/io/quarkus/it/main/JaxbTestCase.java
@@ -2,6 +2,11 @@ package io.quarkus.it.main;
 
 import static org.hamcrest.Matchers.contains;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -10,9 +15,27 @@ import io.restassured.RestAssured;
 @QuarkusTest
 public class JaxbTestCase {
 
-    @Test
-    public void testNews() {
-        RestAssured.when().get("/test/jaxb/getnews").then()
-                .body("author", contains("Emmanuel Bernard"));
+    private static final AtomicInteger count = new AtomicInteger(0);
+
+    @BeforeEach
+    public void beforeInEnclosing() {
+        count.incrementAndGet();
     }
+
+    @Nested
+    class SomeClass {
+
+        @BeforeEach
+        public void beforeInTest() {
+            count.incrementAndGet();
+        }
+
+        @Test
+        public void testNews() {
+            RestAssured.when().get("/test/jaxb/getnews").then()
+                    .body("author", contains("Emmanuel Bernard"));
+            Assertions.assertEquals(2, count.get());
+        }
+    }
+
 }
