7da5a76ee74ac375147f85c242c6a740eea40c87
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:48:52 +0200

    Fix potential NPE in NodeImpl (#4632)
        Refs #4627

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/cluster/NodeImpl.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/cluster/NodeImpl.java
@@ -1,42 +1,36 @@
-/*
- * Copyright 2013-2014 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.cluster;
 
-import com.google.common.collect.Maps;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bson.types.ObjectId;
 import org.graylog2.database.CollectionName;
 import org.graylog2.database.PersistedImpl;
 import org.graylog2.plugin.database.validators.Validator;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
+import java.util.Locale;
 import java.util.Map;
 
-/**
- * @author Lennart Koopmann <lennart@socketfeed.com>
- */
 @CollectionName("nodes")
 public class NodeImpl extends PersistedImpl implements Node {
-
-    private static final Logger LOG = LoggerFactory.getLogger(NodeImpl.class);
 
     protected NodeImpl(Map<String, Object> fields) {
         super(fields);
@@ -52,6 +46,7 @@ public class NodeImpl extends PersistedImpl implements Node {
     }
 
     @Override
+    @JsonProperty("is_master")
     public boolean isMaster() {
         return (Boolean) fields.get("is_master");
     }
@@ -63,7 +58,7 @@ public class NodeImpl extends PersistedImpl implements Node {
 
     @Override
     public DateTime getLastSeen() {
-        return new DateTime(((Integer) fields.get("last_seen"))*1000L, DateTimeZone.UTC);
+        return new DateTime(((Integer) fields.getOrDefault("last_seen", 0)) * 1000L, DateTimeZone.UTC);
     }
 
     @Override
@@ -77,17 +72,24 @@ public class NodeImpl extends PersistedImpl implements Node {
             return Type.SERVER;
         }
 
-        return Type.valueOf(fields.get("type").toString().toUpperCase());
+        return Type.valueOf(fields.get("type").toString().toUpperCase(Locale.ENGLISH));
     }
 
     @Override
+    public String getHostname() {
+        return (String)fields.get("hostname");
+    }
+
+    @Override
+    @JsonIgnore
     public Map<String, Validator> getValidations() {
-        return Maps.newHashMap();
+        return Collections.emptyMap();
     }
 
     @Override
+    @JsonIgnore
     public Map<String, Validator> getEmbeddedValidations(String key) {
-        return Maps.newHashMap();
+        return Collections.emptyMap();
     }
 
 }
