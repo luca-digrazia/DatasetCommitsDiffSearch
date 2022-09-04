70bee3de02d8225d3d7ae5b0452b3b4256390ac0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:59:11 +0200

    Test for GraphQL client with NonNull types in native mode

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/smallrye-graphql-client/src/test/java/io/quarkus/it/smallrye/graphql/client/TypesafeClientTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/smallrye-graphql-client/src/test/java/io/quarkus/it/smallrye/graphql/client/TypesafeClientTest.java
@@ -1,9 +1,9 @@
 package io.quarkus.it.smallrye.graphql.client;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URL;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,11 +20,23 @@ public class TypesafeClientTest {
 
     @Test
     public void testTypesafeClient() throws Exception {
-
         LuckyNumbersClientApi client = TypesafeGraphQLClientBuilder.newBuilder()
                 .endpoint(url.toString() + "/graphql")
                 .build(LuckyNumbersClientApi.class);
         client.setLuckyNumber(21);
         assertEquals(21, client.luckyNumber());
+    }
+
+    /**
+     * Test a method that contains a `@NonNull` parameter.
+     */
+    @Test
+    public void testNonNull() throws Exception {
+        LuckyNumbersClientApi client = TypesafeGraphQLClientBuilder.newBuilder()
+                .endpoint(url.toString() + "/graphql")
+                .build(LuckyNumbersClientApi.class);
+        List<Integer> someNumbers = List.of(12, 33);
+        List<Integer> returned = client.echoList(someNumbers);
+        assertEquals(someNumbers, returned);
     }
 }
