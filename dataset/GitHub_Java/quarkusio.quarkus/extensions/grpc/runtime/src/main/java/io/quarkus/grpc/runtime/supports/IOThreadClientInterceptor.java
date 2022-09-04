54e08555f94227af86cc7ae93abcfe89f62e5fbe
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:09:44 +0200

    gRPC: IOThreadInterceptor check if context != null before delegating to event loop

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/grpc/runtime/src/main/java/io/quarkus/grpc/runtime/supports/IOThreadClientInterceptor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/grpc/runtime/src/main/java/io/quarkus/grpc/runtime/supports/IOThreadClientInterceptor.java
@@ -3,10 +3,21 @@ package io.quarkus.grpc.runtime.supports;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.spi.Prioritized;
 
-import io.grpc.*;
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ForwardingClientCall;
+import io.grpc.ForwardingClientCallListener;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.grpc.Status;
 import io.vertx.core.Context;
 import io.vertx.core.Vertx;
 
+/**
+ * gRPC Client emissions should be on the event loop if the subscription is executed on the event loop
+ */
 @ApplicationScoped
 public class IOThreadClientInterceptor implements ClientInterceptor, Prioritized {
 
@@ -14,6 +25,7 @@ public class IOThreadClientInterceptor implements ClientInterceptor, Prioritized
     public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method,
             CallOptions callOptions, Channel next) {
 
+        boolean isOnEventLoop = Context.isOnEventLoopThread();
         Context context = Vertx.currentContext();
 
         return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
@@ -21,12 +33,22 @@ public class IOThreadClientInterceptor implements ClientInterceptor, Prioritized
             @Override
             public void start(Listener<RespT> responseListener, Metadata headers) {
                 super.start(new ForwardingClientCallListener.SimpleForwardingClientCallListener<RespT>(responseListener) {
+
                     @Override
                     public void onMessage(RespT message) {
-                        if (context != null) {
+                        if (isOnEventLoop && context != null) {
                             context.runOnContext(unused -> super.onMessage(message));
                         } else {
                             super.onMessage(message);
+                        }
+                    }
+
+                    @Override
+                    public void onClose(Status status, Metadata trailers) {
+                        if (isOnEventLoop && context != null) {
+                            context.runOnContext(unused -> super.onClose(status, trailers));
+                        } else {
+                            super.onClose(status, trailers);
                         }
                     }
                 }, headers);
