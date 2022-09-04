12086cdae60397b1e3b1d3ff27cb1fd17905561e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:40:02 +0200

    Change tabs to spaces.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-hibernate/src/test/java/io/dropwizard/hibernate/ScanningHibernateBundleTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-hibernate/src/test/java/io/dropwizard/hibernate/ScanningHibernateBundleTest.java
@@ -14,23 +14,10 @@ public class ScanningHibernateBundleTest {
         //given
         String packageWithEntities = "io.dropwizard.hibernate.fake.entities.pckg";
         //when
-        ImmutableList<Class<?>> findEntityClassesFromDirectory = ScanningHibernateBundle.findEntityClassesFromDirectory(new String[] { packageWithEntities });
+        ImmutableList<Class<?>> findEntityClassesFromDirectory = ScanningHibernateBundle.findEntityClassesFromDirectory(packageWithEntities);
 
         //then
         assertFalse(findEntityClassesFromDirectory.isEmpty());
         assertEquals(4, findEntityClassesFromDirectory.size());
-    }
-
-    @Test
-    public void testFindEntityClassesFromMultipleDirectories() {
-        //given
-        String packageWithEntities = "io.dropwizard.hibernate.fake.entities.pckg";
-        String packageWithEntities2 = "io.dropwizard.hibernate.fake2.entities.pckg";
-        //when
-        ImmutableList<Class<?>> findEntityClassesFromDirectory = ScanningHibernateBundle.findEntityClassesFromDirectory(new String[] { packageWithEntities, packageWithEntities2 });
-
-        //then
-        assertFalse(findEntityClassesFromDirectory.isEmpty());
-        assertEquals(8, findEntityClassesFromDirectory.size());
     }
 }
