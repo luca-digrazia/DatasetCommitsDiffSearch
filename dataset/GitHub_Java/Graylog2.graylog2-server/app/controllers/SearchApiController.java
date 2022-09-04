f65fbfdb944699c647fdecd4015ffbf45d750971
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:48:05 +0200

    fix bug after merge

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/SearchApiController.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/SearchApiController.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+/*
+ * Copyright 2013 TORCH UG
  *
  * This file is part of Graylog2.
  *
@@ -15,17 +15,18 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 package controllers;
 
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
+import com.google.inject.Inject;
 import lib.APIException;
 import lib.SearchTools;
 import lib.timeranges.*;
 import models.UniversalSearch;
 import models.api.responses.FieldStatsResponse;
+import models.api.responses.FieldTermsResponse;
 import play.mvc.Result;
 
 import java.io.IOException;
@@ -35,6 +36,9 @@ import java.util.Map;
  * @author Lennart Koopmann <lennart@torch.sh>
  */
 public class SearchApiController extends AuthenticatedController {
+
+    @Inject
+    private UniversalSearch.Factory searchFactory;
 
     public Result fieldStats(String q, String field, String rangeType, int relative, String from, String to, String keyword, String interval) {
         if (q == null || q.isEmpty()) {
@@ -47,23 +51,9 @@ public class SearchApiController extends AuthenticatedController {
         }
 
         // Determine timerange type.
-        TimeRange.Type timerangeType;
         TimeRange timerange;
         try {
-            timerangeType = TimeRange.Type.valueOf(rangeType.toUpperCase());
-            switch (timerangeType) {
-                case RELATIVE:
-                    timerange = new RelativeRange(relative);
-                    break;
-                case ABSOLUTE:
-                    timerange = new AbsoluteRange(from, to);
-                    break;
-                case KEYWORD:
-                    timerange = new KeywordRange(keyword);
-                    break;
-                default:
-                    throw new InvalidRangeParametersException();
-            }
+            timerange = TimeRange.factory(rangeType, relative, from, to, keyword);
         } catch(InvalidRangeParametersException e2) {
             return status(400, views.html.errors.error.render("Invalid range parameters provided.", e2, request()));
         } catch(IllegalArgumentException e1) {
@@ -71,7 +61,7 @@ public class SearchApiController extends AuthenticatedController {
         }
 
         try {
-            UniversalSearch search = new UniversalSearch(timerange, q);
+            UniversalSearch search = searchFactory.queryWithRange(q, timerange);
             FieldStatsResponse stats = search.fieldStats(field);
 
             Map<String, Object> result = Maps.newHashMap();
@@ -83,6 +73,59 @@ public class SearchApiController extends AuthenticatedController {
             result.put("variance", stats.variance);
             result.put("sum_of_squares", stats.sumOfSquares);
             result.put("std_deviation", stats.stdDeviation);
+
+            return ok(new Gson().toJson(result)).as("application/json");
+        } catch (IOException e) {
+            return internalServerError("io exception");
+        } catch (APIException e) {
+            if (e.getHttpCode() == 400) {
+                // This usually means the field does not have a numeric type. Pass through!
+                return badRequest();
+            }
+
+            return internalServerError("api exception " + e);
+        }
+    }
+
+    public Result fieldTerms(String q, String field, String rangeType, int relative, String from, String to, String keyword, String interval) {
+        if (q == null || q.isEmpty()) {
+            q = "*";
+        }
+
+        // Histogram interval.
+        if (interval == null || interval.isEmpty() || !SearchTools.isAllowedDateHistogramInterval(interval)) {
+            interval = "hour";
+        }
+
+        // Determine timerange type.
+        TimeRange timerange;
+        try {
+            timerange = TimeRange.factory(rangeType, relative, from, to, keyword);
+        } catch(InvalidRangeParametersException e2) {
+            return status(400, views.html.errors.error.render("Invalid range parameters provided.", e2, request()));
+        } catch(IllegalArgumentException e1) {
+            return status(400, views.html.errors.error.render("Invalid range type provided.", e1, request()));
+        }
+
+
+        ////////////// REMOVE ME
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+
+        try {
+            UniversalSearch search = searchFactory.queryWithRange(q, timerange);;
+            FieldTermsResponse terms = search.fieldTerms(field);
+
+            Map<String, Object> result = Maps.newHashMap();
+            result.put("total", terms.total);
+            result.put("missing", terms.missing);
+            result.put("time", terms.time);
+            result.put("other", terms.other);
+            result.put("terms", terms.terms);
 
             return ok(new Gson().toJson(result)).as("application/json");
         } catch (IOException e) {
