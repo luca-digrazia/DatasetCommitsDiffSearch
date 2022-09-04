c14ed92a96dbc1731b723acc4e38ea1bf542fdf2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:45:38 +0200

    Fix snapshot testing library when snapshot doesn't exist     By default, it should fail to avoid succeeding in CI when the snapshot doesn't exist

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/panache/hibernate-orm-panache/deployment/src/main/java/io/quarkus/hibernate/orm/panache/deployment/PanacheHibernateResourceProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/panache/hibernate-orm-panache/deployment/src/main/java/io/quarkus/hibernate/orm/panache/deployment/PanacheHibernateResourceProcessor.java
@@ -1,59 +1,81 @@
 package io.quarkus.hibernate.orm.panache.deployment;
 
+import static io.quarkus.panache.common.deployment.PanacheConstants.META_INF_PANACHE_ARCHIVE_MARKER;
+
+import java.lang.reflect.Modifier;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
+import org.jboss.jandex.FieldInfo;
 
 import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
 import io.quarkus.arc.deployment.ValidationPhaseBuildItem;
 import io.quarkus.bootstrap.classloading.ClassPathElement;
 import io.quarkus.bootstrap.classloading.QuarkusClassLoader;
 import io.quarkus.builder.BuildException;
+import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.Consume;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.AdditionalApplicationArchiveMarkerBuildItem;
+import io.quarkus.deployment.builditem.ApplicationArchivesBuildItem;
 import io.quarkus.deployment.builditem.BytecodeTransformerBuildItem;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.util.JandexUtil;
+import io.quarkus.gizmo.DescriptorUtils;
 import io.quarkus.hibernate.orm.deployment.AdditionalJpaModelBuildItem;
 import io.quarkus.hibernate.orm.deployment.HibernateEnhancersRegisteredBuildItem;
+import io.quarkus.hibernate.orm.deployment.JpaModelPersistenceUnitMappingBuildItem;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.hibernate.orm.panache.runtime.PanacheHibernateOrmRecorder;
 import io.quarkus.panache.common.deployment.EntityField;
 import io.quarkus.panache.common.deployment.EntityModel;
 import io.quarkus.panache.common.deployment.MetamodelInfo;
 import io.quarkus.panache.common.deployment.PanacheEntityClassesBuildItem;
 import io.quarkus.panache.common.deployment.PanacheFieldAccessEnhancer;
+import io.quarkus.panache.common.deployment.PanacheJpaEntityAccessorsEnhancer;
+import io.quarkus.panache.common.deployment.PanacheJpaEntityOperationsEnhancer;
 import io.quarkus.panache.common.deployment.PanacheMethodCustomizer;
 import io.quarkus.panache.common.deployment.PanacheMethodCustomizerBuildItem;
-import io.quarkus.panache.common.deployment.PanacheRepositoryEnhancer;
 
 public final class PanacheHibernateResourceProcessor {
 
     static final DotName DOTNAME_PANACHE_REPOSITORY_BASE = DotName.createSimple(PanacheRepositoryBase.class.getName());
     private static final DotName DOTNAME_PANACHE_REPOSITORY = DotName.createSimple(PanacheRepository.class.getName());
+
+    static final DotName DOTNAME_PANACHE_ENTITY = DotName.createSimple(PanacheEntity.class.getName());
     static final DotName DOTNAME_PANACHE_ENTITY_BASE = DotName.createSimple(PanacheEntityBase.class.getName());
-    private static final DotName DOTNAME_PANACHE_ENTITY = DotName.createSimple(PanacheEntity.class.getName());
 
     private static final DotName DOTNAME_ENTITY_MANAGER = DotName.createSimple(EntityManager.class.getName());
 
     private static final DotName DOTNAME_ID = DotName.createSimple(Id.class.getName());
-    protected static final String META_INF_PANACHE_ARCHIVE_MARKER = "META-INF/panache-archive.marker";
+
+    private static final DotName DOTNAME_TRANSIENT = DotName.createSimple(Transient.class.getName());
 
     @BuildStep
     FeatureBuildItem featureBuildItem() {
-        return new FeatureBuildItem(FeatureBuildItem.HIBERNATE_ORM_PANACHE);
+        return new FeatureBuildItem(Feature.HIBERNATE_ORM_PANACHE);
     }
 
     @BuildStep
@@ -75,76 +97,147 @@ public final class PanacheHibernateResourceProcessor {
     }
 
     @BuildStep
-    void build(CombinedIndexBuildItem index,
+    void collectEntityClasses(CombinedIndexBuildItem index, BuildProducer<PanacheEntityClassBuildItem> entityClasses) {
+        // NOTE: we don't skip abstract/generic entities because they still need accessors
+        for (ClassInfo panacheEntityBaseSubclass : index.getIndex().getAllKnownSubclasses(DOTNAME_PANACHE_ENTITY_BASE)) {
+            // FIXME: should we really skip PanacheEntity or all MappedSuperClass?
+            if (!panacheEntityBaseSubclass.name().equals(DOTNAME_PANACHE_ENTITY)) {
+                entityClasses.produce(new PanacheEntityClassBuildItem(panacheEntityBaseSubclass));
+            }
+        }
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.STATIC_INIT)
+    @Consume(HibernateEnhancersRegisteredBuildItem.class)
+    void build(
+            PanacheHibernateOrmRecorder recorder,
+            CombinedIndexBuildItem index,
             BuildProducer<BytecodeTransformerBuildItem> transformers,
-            HibernateEnhancersRegisteredBuildItem hibernateMarker,
-            BuildProducer<PanacheEntityClassesBuildItem> entityClasses,
-            List<PanacheMethodCustomizerBuildItem> methodCustomizersBuildItems) throws Exception {
+            List<PanacheEntityClassBuildItem> entityClasses,
+            Optional<JpaModelPersistenceUnitMappingBuildItem> jpaModelPersistenceUnitMapping,
+            List<PanacheMethodCustomizerBuildItem> methodCustomizersBuildItems) {
 
         List<PanacheMethodCustomizer> methodCustomizers = methodCustomizersBuildItems.stream()
-                .map(bi -> bi.getMethodCustomizer()).collect(Collectors.toList());
+                .map(PanacheMethodCustomizerBuildItem::getMethodCustomizer).collect(Collectors.toList());
 
-        PanacheJpaRepositoryEnhancer daoEnhancer = new PanacheJpaRepositoryEnhancer(index.getIndex());
+        PanacheJpaRepositoryEnhancer daoEnhancer = new PanacheJpaRepositoryEnhancer(index.getIndex(),
+                JavaJpaTypeBundle.BUNDLE);
+        Set<String> panacheEntities = new HashSet<>();
         Set<String> daoClasses = new HashSet<>();
         for (ClassInfo classInfo : index.getIndex().getAllKnownImplementors(DOTNAME_PANACHE_REPOSITORY_BASE)) {
             // Skip PanacheRepository
             if (classInfo.name().equals(DOTNAME_PANACHE_REPOSITORY))
                 continue;
-            if (PanacheRepositoryEnhancer.skipRepository(classInfo))
+            if (daoEnhancer.skipRepository(classInfo))
                 continue;
+            List<org.jboss.jandex.Type> typeParameters = JandexUtil
+                    .resolveTypeParameters(classInfo.name(), DOTNAME_PANACHE_REPOSITORY_BASE, index.getIndex());
+            panacheEntities.add(typeParameters.get(0).name().toString());
             daoClasses.add(classInfo.name().toString());
         }
         for (ClassInfo classInfo : index.getIndex().getAllKnownImplementors(DOTNAME_PANACHE_REPOSITORY)) {
-            if (PanacheRepositoryEnhancer.skipRepository(classInfo))
+            if (daoEnhancer.skipRepository(classInfo))
                 continue;
+            List<org.jboss.jandex.Type> typeParameters = JandexUtil
+                    .resolveTypeParameters(classInfo.name(), DOTNAME_PANACHE_REPOSITORY, index.getIndex());
+            panacheEntities.add(typeParameters.get(0).name().toString());
             daoClasses.add(classInfo.name().toString());
         }
         for (String daoClass : daoClasses) {
             transformers.produce(new BytecodeTransformerBuildItem(daoClass, daoEnhancer));
         }
 
-        PanacheJpaEntityEnhancer modelEnhancer = new PanacheJpaEntityEnhancer(index.getIndex(), methodCustomizers);
+        PanacheJpaEntityOperationsEnhancer entityOperationsEnhancer = new PanacheJpaEntityOperationsEnhancer(index.getIndex(),
+                methodCustomizers, JavaJpaTypeBundle.BUNDLE);
         Set<String> modelClasses = new HashSet<>();
-        // Note that we do this in two passes because for some reason Jandex does not give us subtypes
-        // of PanacheEntity if we ask for subtypes of PanacheEntityBase
-        // NOTE: we don't skip abstract/generic entities because they still need accessors
-        for (ClassInfo classInfo : index.getIndex().getAllKnownSubclasses(DOTNAME_PANACHE_ENTITY_BASE)) {
-            // FIXME: should we really skip PanacheEntity or all MappedSuperClass?
-            if (classInfo.name().equals(DOTNAME_PANACHE_ENTITY))
-                continue;
-            if (modelClasses.add(classInfo.name().toString()))
-                modelEnhancer.collectFields(classInfo);
-        }
-        for (ClassInfo classInfo : index.getIndex().getAllKnownSubclasses(DOTNAME_PANACHE_ENTITY)) {
-            if (modelClasses.add(classInfo.name().toString()))
-                modelEnhancer.collectFields(classInfo);
-        }
-        Set<String> modelClassNamesInternal = new HashSet<>();
-        for (String modelClass : modelClasses) {
-            modelClassNamesInternal.add(modelClass.replace(".", "/"));
-            transformers.produce(new BytecodeTransformerBuildItem(true, modelClass, modelEnhancer));
-        }
-        if (!modelClasses.isEmpty()) {
-            entityClasses.produce(new PanacheEntityClassesBuildItem(modelClasses));
+        for (PanacheEntityClassBuildItem entityClass : entityClasses) {
+            String entityClassName = entityClass.get().name().toString();
+            modelClasses.add(entityClassName);
+            transformers.produce(new BytecodeTransformerBuildItem(true, entityClassName, entityOperationsEnhancer));
         }
 
-        MetamodelInfo<EntityModel<EntityField>> modelInfo = modelEnhancer.getModelInfo();
+        panacheEntities.addAll(modelClasses);
+
+        recordPanacheEntityPersistenceUnits(recorder, jpaModelPersistenceUnitMapping, panacheEntities);
+    }
+
+    @BuildStep
+    PanacheEntityClassesBuildItem findEntityClasses() {
+        return null;
+    }
+
+    @BuildStep
+    @Consume(HibernateEnhancersRegisteredBuildItem.class)
+    void replaceFieldAccesses(CombinedIndexBuildItem index,
+            ApplicationArchivesBuildItem applicationArchivesBuildItem,
+            BuildProducer<BytecodeTransformerBuildItem> transformers,
+            BuildProducer<PanacheEntityClassesBuildItem> fieldAccessEnhancedEntityClasses,
+            List<PanacheEntityClassBuildItem> entityClasses) {
+        MetamodelInfo modelInfo = new MetamodelInfo();
+
+        // Generate accessors for public fields in entities
+        PanacheJpaEntityAccessorsEnhancer entityAccessorsEnhancer = new PanacheJpaEntityAccessorsEnhancer(index.getIndex(),
+                modelInfo);
+        for (PanacheEntityClassBuildItem entityClass : entityClasses) {
+            String entityClassName = entityClass.get().name().toString();
+            modelInfo.addEntityModel(createEntityModel(entityClass.get()));
+            transformers.produce(new BytecodeTransformerBuildItem(true, entityClassName, entityAccessorsEnhancer));
+        }
+
         if (modelInfo.hasEntities()) {
+            // Share with other extensions that we generated accessors for some classes
+            fieldAccessEnhancedEntityClasses.produce(new PanacheEntityClassesBuildItem(modelInfo.getEntityClassNames()));
+
+            // Replace field access in application code with calls to accessors
+            Set<String> entityClassNamesInternal = new HashSet<>();
+            for (String entityClassName : modelInfo.getEntityClassNames()) {
+                entityClassNamesInternal.add(entityClassName.replace(".", "/"));
+            }
+
             PanacheFieldAccessEnhancer panacheFieldAccessEnhancer = new PanacheFieldAccessEnhancer(modelInfo);
             QuarkusClassLoader tccl = (QuarkusClassLoader) Thread.currentThread().getContextClassLoader();
             List<ClassPathElement> archives = tccl.getElementsWithResource(META_INF_PANACHE_ARCHIVE_MARKER);
+            Set<String> produced = new HashSet<>();
+            //we always transform the root archive, even though it should be run with the annotation
+            //processor on the CP it might not be if the user is using jpa-modelgen
+            //this won't cover every situation, but we have documented this, and as the fields are now
+            //made private the error should be very obvious
+            //we only do this for hibernate, as it is more common to have an additional annotation processor
+            for (ClassInfo i : applicationArchivesBuildItem.getRootArchive().getIndex().getKnownClasses()) {
+                String cn = i.name().toString();
+                produced.add(cn);
+                transformers.produce(
+                        new BytecodeTransformerBuildItem(cn, panacheFieldAccessEnhancer, entityClassNamesInternal));
+            }
+
             for (ClassPathElement i : archives) {
                 for (String res : i.getProvidedResources()) {
                     if (res.endsWith(".class")) {
                         String cn = res.replace("/", ".").substring(0, res.length() - 6);
-                        if (!modelClasses.contains(cn)) {
-                            transformers.produce(
-                                    new BytecodeTransformerBuildItem(cn, panacheFieldAccessEnhancer, modelClassNamesInternal));
+                        if (produced.contains(cn)) {
+                            continue;
                         }
+                        produced.add(cn);
+                        transformers.produce(
+                                new BytecodeTransformerBuildItem(cn, panacheFieldAccessEnhancer, entityClassNamesInternal));
                     }
                 }
             }
         }
+    }
+
+    private EntityModel createEntityModel(ClassInfo classInfo) {
+        EntityModel entityModel = new EntityModel(classInfo);
+        for (FieldInfo fieldInfo : classInfo.fields()) {
+            String name = fieldInfo.name();
+            if (Modifier.isPublic(fieldInfo.flags())
+                    && !Modifier.isStatic(fieldInfo.flags())
+                    && !fieldInfo.hasAnnotation(DOTNAME_TRANSIENT)) {
+                entityModel.addField(new EntityField(name, DescriptorUtils.typeToString(fieldInfo.type())));
+            }
+        }
+        return entityModel;
     }
 
     @BuildStep
@@ -152,8 +245,8 @@ public final class PanacheHibernateResourceProcessor {
             CombinedIndexBuildItem index) throws BuildException {
         // we verify that no ID fields are defined (via @Id) when extending PanacheEntity
         for (AnnotationInstance annotationInstance : index.getIndex().getAnnotations(DOTNAME_ID)) {
-            ClassInfo info = io.quarkus.panache.common.deployment.JandexUtil.getEnclosingClass(annotationInstance);
-            if (io.quarkus.panache.common.deployment.JandexUtil.isSubclassOf(index.getIndex(), info, DOTNAME_PANACHE_ENTITY)) {
+            ClassInfo info = JandexUtil.getEnclosingClass(annotationInstance);
+            if (JandexUtil.isSubclassOf(index.getIndex(), info, DOTNAME_PANACHE_ENTITY)) {
                 BuildException be = new BuildException("You provide a JPA identifier via @Id inside '" + info.name() +
                         "' but one is already provided by PanacheEntity, " +
                         "your class should extend PanacheEntityBase instead, or use the id provided by PanacheEntity",
@@ -162,5 +255,46 @@ public final class PanacheHibernateResourceProcessor {
             }
         }
         return null;
+    }
+
+    void recordPanacheEntityPersistenceUnits(PanacheHibernateOrmRecorder recorder,
+            Optional<JpaModelPersistenceUnitMappingBuildItem> jpaModelPersistenceUnitMapping,
+            Set<String> panacheEntityClasses) {
+        Map<String, String> panacheEntityToPersistenceUnit = new HashMap<>();
+        if (jpaModelPersistenceUnitMapping.isPresent()) {
+            Map<String, Set<String>> collectedEntityToPersistenceUnits = jpaModelPersistenceUnitMapping.get()
+                    .getEntityToPersistenceUnits();
+
+            Map<String, Set<String>> violatingPanacheEntities = new TreeMap<>();
+
+            for (Map.Entry<String, Set<String>> entry : collectedEntityToPersistenceUnits.entrySet()) {
+                String entityName = entry.getKey();
+                Set<String> selectedPersistenceUnits = entry.getValue();
+                boolean isPanacheEntity = panacheEntityClasses.contains(entityName);
+
+                if (!isPanacheEntity) {
+                    continue;
+                }
+
+                if (selectedPersistenceUnits.size() == 1) {
+                    panacheEntityToPersistenceUnit.put(entityName, selectedPersistenceUnits.iterator().next());
+                } else {
+                    violatingPanacheEntities.put(entityName, selectedPersistenceUnits);
+                }
+            }
+
+            if (violatingPanacheEntities.size() > 0) {
+                StringBuilder message = new StringBuilder(
+                        "Panache entities do not support being attached to several persistence units:\n");
+                for (Entry<String, Set<String>> violatingEntityEntry : violatingPanacheEntities
+                        .entrySet()) {
+                    message.append("\t- ").append(violatingEntityEntry.getKey()).append(" is attached to: ")
+                            .append(String.join(",", violatingEntityEntry.getValue()));
+                    throw new IllegalStateException(message.toString());
+                }
+            }
+        }
+
+        recorder.setEntityToPersistenceUnit(panacheEntityToPersistenceUnit);
     }
 }
