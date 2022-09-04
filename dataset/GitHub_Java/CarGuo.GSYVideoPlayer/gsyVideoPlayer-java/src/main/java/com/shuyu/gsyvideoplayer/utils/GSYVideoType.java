f739e54dd2dbd6313bf029fd87c161264c9b1451
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:35:12 +0200

    ä¿®æ¹æ³¨é

--- dataset/GitHub_Java/CarGuo.GSYVideoPlayer/gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/utils/GSYVideoType.java
+++ dataset/GitHub_Java/CarGuo.GSYVideoPlayer/gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/utils/GSYVideoType.java
@@ -1,6 +1,7 @@
 package com.shuyu.gsyvideoplayer.utils;
 
 /**
+ * video的一些默认配置
  * Created by shuyu on 2016/12/7.
  */
 
@@ -15,25 +16,36 @@ public class GSYVideoType {
     //4:3
     public final static int SCREEN_TYPE_4_3 = 2;
 
-    //全屏裁减显示，为了显示正常 CoverImageView 建议使用FrameLayout作为父布局
+    //18:9
+    public final static int SCREEN_TYPE_18_9 = 6;
+
+    //全屏裁减显示，为了显示正常 surface_container 和 CoverImageView 建议使用 FrameLayout 作为父布局
     public final static int SCREEN_TYPE_FULL = 4;
 
-    //全屏拉伸显示，使用这个属性时，surface_container建议使用FrameLayout
+    //全屏拉伸显示，使用这个属性时，surface_container 建议使用 FrameLayout
     public final static int SCREEN_MATCH_FULL = -4;
 
+    //自定义比例，需要设置 sScreenScaleRatio
+    public final static int SCREEN_TYPE_CUSTOM = -5;
 
-    public final static int IJKPLAYER = 0;
+    /**
+     * 自定义的显示比例
+     */
+    private static float sScreenScaleRatio = 0;
 
-    public final static int IJKEXOPLAYER = 1;
-
-
-    //gl
+    /**
+     * GLSurfaceView 主要用于OpenGL渲染的
+     */
     public final static int GLSURFACE = 2;
 
-    //surface
+    /**
+     * SurfaceView，与动画全屏的效果不是很兼容
+     */
     public final static int SUFRACE = 1;
 
-    //texture
+    /**
+     * TextureView,默认
+     */
     public final static int TEXTURE = 0;
 
 
@@ -117,4 +129,15 @@ public class GSYVideoType {
         sRenderType = renderType;
     }
 
+    public static float getScreenScaleRatio() {
+        return sScreenScaleRatio;
+    }
+
+    /***
+     * SCREEN_TYPE_CUSTOM 下自定义显示比例
+     * @param screenScaleRatio  高宽比，如 16：9
+     */
+    public static void setScreenScaleRatio(float screenScaleRatio) {
+        GSYVideoType.sScreenScaleRatio = screenScaleRatio;
+    }
 }
