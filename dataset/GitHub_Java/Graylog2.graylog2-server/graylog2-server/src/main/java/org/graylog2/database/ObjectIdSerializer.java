ba0542f4108dbbfe83c79f31a8f7c5c227d0b39c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:02:04 +0200

    Moved ObjectIdSerializer to server module

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/database/ObjectIdSerializer.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/database/ObjectIdSerializer.java
@@ -1,19 +1,3 @@
-/**
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.database;
 
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -28,11 +12,6 @@ import java.io.IOException;
  * @author Dennis Oelkers <dennis@torch.sh>
  */
 public class ObjectIdSerializer extends JsonSerializer<ObjectId> {
-    @Override
-    public Class<ObjectId> handledType() {
-        return ObjectId.class;
-    }
-
     @Override
     public void serialize(ObjectId value, JsonGenerator jgen, SerializerProvider provider)
             throws IOException, JsonProcessingException {
