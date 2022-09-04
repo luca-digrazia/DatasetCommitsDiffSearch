e4e630bcf98070c4eb3fadc44dee1af658689228
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:26:24 +0200

    fix broken integration tests for replica set support

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/test/java/org/graylog2/database/MongoBridgeIntegrationTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/test/java/org/graylog2/database/MongoBridgeIntegrationTest.java
@@ -45,7 +45,7 @@ public class MongoBridgeIntegrationTest {
         MongoConnection.getInstance().connect(
             null,
             null,
-            "127.0.0.1",
+            "localhost",
             "graylog2test",
             Integer.valueOf(27017),
             "false",
@@ -112,7 +112,6 @@ public class MongoBridgeIntegrationTest {
         message.setLevel(1);
         message.setHost("junit-test");
         message.setFile("junit-testfile");
-        message.setVersion("1.0");
         message.setLine(9001);
         message.addAdditionalData("something", "yepp");
 
@@ -129,11 +128,38 @@ public class MongoBridgeIntegrationTest {
         assertEquals(res.get("message"), "gelftest");
         assertEquals(res.get("full_message"), "full gelftest\nstuff");
         assertEquals(res.get("level"), 1);
+        assertEquals(res.get("type"), 8);
         assertEquals(res.get("host"), "junit-test");
         assertEquals(res.get("file"), "junit-testfile");
         assertEquals(res.get("line"), 9001);
-        assertEquals(res.get("version"), "1.0");
         assertEquals(res.get("something"), "yepp");
+    }
+
+    /**
+     * Test of distinctHosts method, of class MongoBridge.
+     */
+    @Test
+    public void testDistinctHosts() throws Exception {
+        DB db = MongoConnection.getInstance().getDatabase();
+        MongoBridge m = new MongoBridge();
+
+        // Insert a message.
+        GELFMessage message = new GELFMessage();
+        message.setShortMessage("test");
+        message.setHost("host1");
+        m.insertGelfMessage(message);
+
+        // Second message from another host
+        message.setHost("host2");
+        m.insertGelfMessage(message);
+
+        // Distinct the hosts.
+        m.distinctHosts();
+
+        DBCollection coll = db.getCollection("hosts");
+
+        List<String> hosts = coll.distinct("host");
+        assertTrue(hosts.size() == 2);
     }
 
 }
