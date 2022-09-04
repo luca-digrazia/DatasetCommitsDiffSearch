2a7554d65c6ed801b0f13afaf4967698de1e8fa0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:20:00 +0200

    Allow tests to be run from Eclipse (#2253)
        Adds additional test/application lookup paths to support Eclipse's build
        output locations.

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/PathTestHelper.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/PathTestHelper.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.test.common;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
@@ -33,24 +47,9 @@ public final class PathTestHelper {
     private PathTestHelper() {
     }
 
-    /**
-     * Resolves the directory or the JAR file containing the test class.
-     *
-     * @param testClass the test class
-     * @return directory or JAR containing the test class
-     */
     public static Path getTestClassesLocation(Class<?> testClass) {
         String classFileName = testClass.getName().replace('.', File.separatorChar) + ".class";
         URL resource = testClass.getClassLoader().getResource(classFileName);
-
-        if (resource.getProtocol().equals("jar")) {
-            try {
-                resource = URI.create(resource.getFile().substring(0, resource.getFile().indexOf('!'))).toURL();
-                return toPath(resource);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException("Failed to resolve the location of the JAR containing " + testClass, e);
-            }
-        }
 
         if (!isInTestDir(resource)) {
             throw new RuntimeException(
@@ -62,23 +61,8 @@ public final class PathTestHelper {
         return path.getRoot().resolve(path.subpath(0, path.getNameCount() - Paths.get(classFileName).getNameCount()));
     }
 
-    /**
-     * Resolves the directory or the JAR file containing the application being tested by the test class.
-     *
-     * @param testClass the test class
-     * @return directory or JAR containing the application being tested by the test class
-     */
     public static Path getAppClassLocation(Class<?> testClass) {
-        final String testClassPath = getTestClassesLocation(testClass).toString();
-        if (testClassPath.endsWith(".jar")) {
-            if (testClassPath.endsWith("-tests.jar")) {
-                return Paths.get(new StringBuilder()
-                        .append(testClassPath, 0, testClassPath.length() - "-tests.jar".length())
-                        .append(".jar")
-                        .toString());
-            }
-            return Paths.get(testClassPath);
-        }
+        String testClassPath = getTestClassesLocation(testClass).toString();
         return TEST_TO_MAIN_DIR_FRAGMENTS.entrySet().stream()
                 .filter(e -> testClassPath.contains(e.getKey()))
                 .map(e -> Paths.get(testClassPath.replace(e.getKey(), e.getValue())))
