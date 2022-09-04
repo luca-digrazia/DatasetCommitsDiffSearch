b28532ca7caa17b5a2b5e35e151156bca075e234
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:19:21 +0200

    Add SizeParam as a request parameter

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jersey/src/test/java/io/dropwizard/jersey/params/SizeParamTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jersey/src/test/java/io/dropwizard/jersey/params/SizeParamTest.java
@@ -5,27 +5,27 @@ import io.dropwizard.util.Size;
 import org.junit.Test;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class SizeParamTest {
 
     @Test
-    public void parseSizeKilobytes() {
+    public void parseSizeKilobytes() throws Exception {
         final SizeParam param = new SizeParam("10kb");
         assertThat(param.get())
             .isEqualTo(Size.kilobytes(10));
     }
 
     @Test
-    public void badValueThrowsException() {
-        assertThatThrownBy(() -> new SizeParam("10 kelvins", "degrees"))
-            .isInstanceOfSatisfying(WebApplicationException.class, e -> {
-                assertThat(e.getResponse().getStatus()).isEqualTo(400);
-                assertThat(e.getResponse().getEntity()).isEqualTo(
-                    new ErrorMessage(400, "degrees is not a valid size.")
-                );
-            });
+    public void badValueThrowsException() throws Exception {
+        final Throwable exn = catchThrowable(() -> new SizeParam("10 kelvins", "degrees"));
+        assertThat(exn).isInstanceOf(WebApplicationException.class);
+        final Response response = ((WebApplicationException) exn).getResponse();
+        assertThat(response.getStatus()).isEqualTo(400);
+        assertThat((ErrorMessage) response.getEntity())
+            .isEqualTo(new ErrorMessage(400, "degrees is not a valid size."));
     }
 }
