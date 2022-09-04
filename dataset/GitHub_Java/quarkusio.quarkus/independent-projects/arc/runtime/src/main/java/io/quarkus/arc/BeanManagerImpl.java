130dba50cbd4d3ce078b31f2f19cd87e6b60fcb0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:10:10 +0200

    #2721 - Implement BeanManger#isPassivatingScope

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/runtime/src/main/java/io/quarkus/arc/BeanManagerImpl.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/runtime/src/main/java/io/quarkus/arc/BeanManagerImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.arc;
 
 import java.lang.annotation.Annotation;
@@ -86,7 +102,7 @@ public class BeanManagerImpl implements BeanManager {
 
     @Override
     public <T> CreationalContext<T> createCreationalContext(Contextual<T> contextual) {
-        return new CreationalContextImpl<>(contextual);
+        return new CreationalContextImpl<>();
     }
 
     @Override
@@ -287,7 +303,7 @@ public class BeanManagerImpl implements BeanManager {
 
     @Override
     public Instance<Object> createInstance() {
-        return new InstanceImpl<>(null, INSTANCE_LITERAL.getType(), Collections.emptySet(), new CreationalContextImpl<>(null),
+        return new InstanceImpl<>(null, INSTANCE_LITERAL.getType(), Collections.emptySet(), new CreationalContextImpl<>(),
                 Collections.emptySet(), null, -1);
     }
 
