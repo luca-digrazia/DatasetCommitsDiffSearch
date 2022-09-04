99c2624cc484d53ec51c4baf824f41fe392d58d0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:43:19 +0200

    Disable intermittently failing tests

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/infinispan-client/src/test/java/io/quarkus/example/test/InfinispanClientFunctionalityTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/infinispan-client/src/test/java/io/quarkus/example/test/InfinispanClientFunctionalityTest.java
@@ -3,45 +3,60 @@ package io.quarkus.example.test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 
 /**
  * @author William Burns
  */
 @QuarkusTest
+@QuarkusTestResource(InfinispanServerTestResource.class)
+@Disabled("Intermittently failing")
 public class InfinispanClientFunctionalityTest {
-   @Test
-   public void testClientFunctionalityFromServlet() {
-      RestAssured.when().get("/test").then().body(is("[book1, book2]"));
-   }
 
-   @Test public void testQuery() {
-      RestAssured.when().get("/test/query/So").then().body(is("[Son Martin]"));
-      RestAssured.when().get("/test/query/org").then().body(is("[George Martin]"));
-      RestAssured.when().get("/test/query/o").then().body(is("[George Martin,Son Martin]"));
-   }
+    @Test
+    public void testGetAllKeys() {
+        System.out.println("Running getAllKeys test");
+        RestAssured.when().get("/test").then().body(is("[book1, book2]"));
+    }
 
-   @Test public void testIckleQuery() {
-      RestAssured.when().get("/test/icklequery/So").then().body(is("[Son Martin]"));
-      RestAssured.when().get("/test/icklequery/org").then().body(is("[George Martin]"));
-      RestAssured.when().get("/test/icklequery/o").then().body(is("[George Martin,Son Martin]"));
-   }
+    @Test
+    public void testQuery() {
+        System.out.println("Running query test");
+        RestAssured.when().get("/test/query/So").then().body(is("[Son Martin]"));
+        RestAssured.when().get("/test/query/org").then().body(is("[George Martin]"));
+        RestAssured.when().get("/test/query/o").then().body(is("[George Martin,Son Martin]"));
+    }
 
-   @Test public void increment() {
-      String initialValue = RestAssured.when().get("test/incr/somevalue").body().print();
-      String nextValue = RestAssured.when().get("test/incr/somevalue").body().print();
-      assertEquals(Integer.parseInt(initialValue) + 1, Integer.parseInt(nextValue));
-   }
+    @Test
+    public void testIckleQuery() {
+        System.out.println("Running ickleQuery test");
+        RestAssured.when().get("/test/icklequery/So").then().body(is("[Son Martin]"));
+        RestAssured.when().get("/test/icklequery/org").then().body(is("[George Martin]"));
+        RestAssured.when().get("/test/icklequery/o").then().body(is("[George Martin,Son Martin]"));
+    }
 
-   @Test public void cq() {
-      RestAssured.when().get("/test/cq").then().body(is("2023"));
-   }
+    @Test
+    public void testCounterIncrement() {
+        System.out.println("Running counterIncrement test");
+        String initialValue = RestAssured.when().get("test/incr/somevalue").body().print();
+        String nextValue = RestAssured.when().get("test/incr/somevalue").body().print();
+        assertEquals(Integer.parseInt(initialValue) + 1, Integer.parseInt(nextValue));
+    }
 
-   @Test
-   public void testNearCacheInvalidation() {
-      RestAssured.when().get("/test/nearcache").then().body(is("worked"));
-   }
+    @Test
+    public void testCQ() {
+        System.out.println("Running CQ test");
+        RestAssured.when().get("/test/cq").then().body(is("2023"));
+    }
+
+    @Test
+    public void testNearCacheInvalidation() {
+        System.out.println("Running nearCacheInvalidation test");
+        RestAssured.when().get("/test/nearcache").then().body(is("worked"));
+    }
 }
