78461edfae3989476ae5c6daa14823c01bccf983
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:22:32 +0200

    Move GC registry to deployment, because naming and static init in the JVM.

--- dataset/GitHub_Java/quarkusio.quarkus/metrics/runtime/src/main/java/org/jboss/shamrock/metrics/runtime/MetricsDeploymentTemplate.java
+++ dataset/GitHub_Java/quarkusio.quarkus/metrics/runtime/src/main/java/org/jboss/shamrock/metrics/runtime/MetricsDeploymentTemplate.java
@@ -3,43 +3,45 @@ package org.jboss.shamrock.metrics.runtime;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.ThreadMXBean;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
-
-import org.eclipse.microprofile.metrics.Metadata;
-import org.eclipse.microprofile.metrics.MetricRegistry;
-import org.eclipse.microprofile.metrics.MetricType;
-import org.jboss.logging.Logger;
-import org.jboss.shamrock.runtime.BeanContainer;
-import org.jboss.shamrock.runtime.ContextObject;
 
 import io.smallrye.metrics.MetricRegistries;
 import io.smallrye.metrics.app.CounterImpl;
 import io.smallrye.metrics.interceptors.MetricResolver;
+import org.eclipse.microprofile.metrics.Metadata;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.eclipse.microprofile.metrics.MetricType;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Metric;
 
 /**
  * Created by bob on 7/30/18.
  */
 public class MetricsDeploymentTemplate {
 
-    private static final Logger log = Logger.getLogger("org.jboss.shamrock.metrics");
-
     /*
-            public <E extends Member & AnnotatedElement> void registerCounted(Class<?> topClass, E element) {
-                MetricRegistry registry = MetricRegistries.get(MetricRegistry.Type.APPLICATION);
-                //registry.register(name, new CounterImpl());
-                MetricResolver resolver = new MetricResolver();
-                MetricResolver.Of<Counted> of = resolver.counted(topClass, element);
-                Metadata meta = new Metadata(of.metricName(), MetricType.COUNTER);
-                //registry.register(meta, new CounterImpl());
-            }
-            */
+    public <E extends Member & AnnotatedElement> void registerCounted(Class<?> topClass, E element) {
+        MetricRegistry registry = MetricRegistries.get(MetricRegistry.Type.APPLICATION);
+        //registry.register(name, new CounterImpl());
+        MetricResolver resolver = new MetricResolver();
+        MetricResolver.Of<Counted> of = resolver.counted(topClass, element);
+        Metadata meta = new Metadata(of.metricName(), MetricType.COUNTER);
+        //registry.register(meta, new CounterImpl());
+    }
+    */
     public void registerCounted(String topClassName, String elementName) {
         MetricRegistry registry = MetricRegistries.get(MetricRegistry.Type.APPLICATION);
+        MetricResolver resolver = new MetricResolver();
 
         String name = MetricRegistry.name(topClassName, elementName);
         Metadata meta = new Metadata(name, MetricType.COUNTER);
-        log.debugf("Register: %s", name);
+        System.err.println("register: " + name);
         registry.register(meta, new CounterImpl());
     }
 
@@ -103,15 +105,10 @@ public class MetricsDeploymentTemplate {
 
     }
 
-    public void createRegistries(@ContextObject("bean.container") BeanContainer container) {
-        log.info("Creating registries");
+    public void createRegistries() {
+        System.err.println("creating registries");
         MetricRegistries.get(MetricRegistry.Type.APPLICATION);
         MetricRegistries.get(MetricRegistry.Type.BASE);
         MetricRegistries.get(MetricRegistry.Type.VENDOR);
-
-        //HACK: registration is does via statics, but cleanup is done via pre destroy
-        //however if the bean is not used it will not be created, so no cleanup will be done
-        //we force bean creation here to make sure the container can restart correctly
-        container.instance(MetricRegistries.class).getApplicationRegistry();
     }
 }
