027cbb386566ab6e7e4865708c26ef7c8152580b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:26:34 +0200

    Make sure runfiles fingerprinting accounts for empty files
        Previously, Runfiles#fingerprint (and actions that depend on it for their key) would collide when runfiles differed only by empty files. This is a correctness bug exercised by changes like https://github.com/bazelbuild/bazel/commit/3fc3ddbc0d2a51853c0966bfd41834765ea619a5.
    
        RELNOTES: None
        PiperOrigin-RevId: 214432124

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/Runfiles.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/Runfiles.java
@@ -37,8 +37,8 @@ import com.google.devtools.build.lib.packages.BuildType;
 import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
 import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec.VisibleForSerialization;
 import com.google.devtools.build.lib.skylarkbuildapi.RunfilesApi;
-import com.google.devtools.build.lib.skylarkbuildapi.SymlinkEntryApi;
 import com.google.devtools.build.lib.skylarkinterface.SkylarkPrinter;
+import com.google.devtools.build.lib.skylarkinterface.SkylarkValue;
 import com.google.devtools.build.lib.util.Fingerprint;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import java.io.BufferedReader;
@@ -116,7 +116,7 @@ public final class Runfiles implements RunfilesApi {
   // Goodnight, prince(ss)?, and sweet dreams.
   @AutoCodec
   @VisibleForSerialization
-  static final class SymlinkEntry implements SymlinkEntryApi {
+  static final class SymlinkEntry implements SkylarkValue {
     private final PathFragment path;
     private final Artifact artifact;
 
@@ -126,16 +126,10 @@ public final class Runfiles implements RunfilesApi {
       this.artifact = Preconditions.checkNotNull(artifact);
     }
 
-    @Override
-    public String getPathString() {
-      return getPath().getPathString();
-    }
-
     public PathFragment getPath() {
       return path;
     }
 
-    @Override
     public Artifact getArtifact() {
       return artifact;
     }
@@ -148,8 +142,8 @@ public final class Runfiles implements RunfilesApi {
     @Override
     public void repr(SkylarkPrinter printer) {
       printer.append("SymlinkEntry(path = ");
-      printer.repr(getPathString());
-      printer.append(", target_file = ");
+      printer.repr(getPath().toString());
+      printer.append(", artifact = ");
       getArtifact().repr(printer);
       printer.append(")");
     }
@@ -572,8 +566,9 @@ public final class Runfiles implements RunfilesApi {
     return legacyExternalRunfiles;
   }
 
-  /** Returns the root symlinks. */
-  @Override
+  /**
+   * Returns the root symlinks.
+   */
   public NestedSet<SymlinkEntry> getRootSymlinks() {
     return rootSymlinks;
   }
