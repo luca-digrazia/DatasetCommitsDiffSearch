5db6a28e1ff8b08861467850503ebfaaf5661d6f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:28:45 +0200

    Monitor config additional locations for hot deployment.

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/steps/DevModeBuildStep.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/steps/DevModeBuildStep.java
@@ -1,28 +1,25 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.quarkus.deployment.steps;
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.HotDeploymentConfigFileBuildItem;
+import io.quarkus.deployment.builditem.HotDeploymentWatchedFileBuildItem;
+import io.quarkus.runtime.ConfigConfig;
+import io.quarkus.runtime.LiveReloadConfig;
 
 class DevModeBuildStep {
 
     @BuildStep
-    HotDeploymentConfigFileBuildItem config() {
-        return new HotDeploymentConfigFileBuildItem("META-INF/microprofile-config.properties");
+    List<HotDeploymentWatchedFileBuildItem> watchChanges(ConfigConfig config, LiveReloadConfig liveReloadConfig) {
+        List<String> names = new ArrayList<>();
+        names.add("META-INF/microprofile-config.properties");
+        names.add("application.properties");
+        names.add(Paths.get(".env").toAbsolutePath().toString());
+        config.locations.ifPresent(names::addAll);
+        liveReloadConfig.watchedResources.ifPresent(names::addAll);
+        return names.stream().map(HotDeploymentWatchedFileBuildItem::new).collect(Collectors.toList());
     }
 }
