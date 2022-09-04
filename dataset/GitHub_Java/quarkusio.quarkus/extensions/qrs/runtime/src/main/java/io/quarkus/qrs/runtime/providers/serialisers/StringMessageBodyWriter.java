9b6f87333c6dac91be1f5b1c996cfc18df7fdc4d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:24:50 +0200

    Change response writer to allow for lazy allocation

---
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/qrs/runtime/src/main/java/io/quarkus/qrs/runtime/providers/serialisers/StringMessageBodyWriter.java
@@ -0,0 +1,39 @@
+package io.quarkus.qrs.runtime.providers.serialisers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+import io.quarkus.qrs.runtime.core.RequestContext;
+import io.quarkus.qrs.runtime.spi.QrsMessageBodyWriter;
+import io.vertx.core.http.HttpServerResponse;
+
+@Provider
+public class StringMessageBodyWriter implements QrsMessageBodyWriter<String> {
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return true;
+    }
+
+    @Override
+    public void writeTo(String o, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+            MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        // FIXME: use response encoding
+        entityStream.write(o.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public void writeResponse(String o, RequestContext context) throws WebApplicationException {
+        // FIXME: use response encoding
+        HttpServerResponse vertxResponse = context.getContext().response();
+        vertxResponse.end(o);
+    }
+}
---
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/qrs/runtime/src/main/java/io/quarkus/qrs/runtime/providers/serialisers/VertxBufferMessageBodyWriter.java
@@ -0,0 +1,37 @@
+package io.quarkus.qrs.runtime.providers.serialisers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+import io.quarkus.qrs.runtime.core.RequestContext;
+import io.quarkus.qrs.runtime.spi.QrsMessageBodyWriter;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpServerResponse;
+
+@Provider
+public class VertxBufferMessageBodyWriter implements QrsMessageBodyWriter<Buffer> {
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return true;
+    }
+
+    @Override
+    public void writeTo(Buffer buffer, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+            MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        entityStream.write(buffer.getBytes());
+    }
+
+    @Override
+    public void writeResponse(Buffer buffer, RequestContext context) throws WebApplicationException {
+        HttpServerResponse vertxResponse = context.getContext().response();
+        vertxResponse.end(buffer);
+    }
+}
