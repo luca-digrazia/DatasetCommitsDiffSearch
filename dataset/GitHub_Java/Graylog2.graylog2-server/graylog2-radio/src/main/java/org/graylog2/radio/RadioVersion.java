85cb340860b0de0df284785601abe3620f0b3495
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:18:18 +0200

    set radio version to snapshot

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-radio/src/main/java/org/graylog2/radio/RadioVersion.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-radio/src/main/java/org/graylog2/radio/RadioVersion.java
@@ -1,4 +1,6 @@
 /**
+ * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+ *
  * This file is part of Graylog2.
  *
  * Graylog2 is free software: you can redistribute it and/or modify
@@ -13,18 +15,16 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package org.graylog2.radio;
 
 import org.graylog2.plugin.Version;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Lennart Koopmann <lennart@torch.sh>
  */
 public class RadioVersion {
-    private static final Logger LOG = LoggerFactory.getLogger(RadioVersion.class);
 
     public static final Version vDEV = new Version(0, 20, 0, "dev");
     public static final Version v0_20_0_PREVIEW_7 = new Version(0, 20, 0, "preview.7");
@@ -34,10 +34,7 @@ public class RadioVersion {
     public static final Version v0_20_0 = new Version(0, 20, 0);
 
     public static final Version v0_20_2_SNAPSHOT = new Version(0, 20, 2, "snapshot");
-    public static final Version v0_21_0_SNAPSHOT = new Version(0, 21, 0, "snapshot");
-    public static final Version v0_20_2_RC_1 = new Version(0, 20, 2, "rc.1");
-    public static final Version v0_20_2 = new Version(0, 20, 2);
 
-    public static final Version VERSION = Version.CURRENT_CLASSPATH;
-    public static final String CODENAME = "Moose";
+    public static final Version VERSION = v0_20_2_SNAPSHOT;
+
 }
