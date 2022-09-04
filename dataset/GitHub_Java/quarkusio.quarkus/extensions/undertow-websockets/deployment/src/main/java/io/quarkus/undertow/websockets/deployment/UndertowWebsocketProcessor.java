0bfbf2fb5f6a7f2a2255ae50169f140dab1d5057
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:49:59 +0200

    Register ServerEndpointConfig.Configurator
        Fixes #7509

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/undertow-websockets/deployment/src/main/java/io/quarkus/undertow/websockets/deployment/UndertowWebsocketProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/undertow-websockets/deployment/src/main/java/io/quarkus/undertow/websockets/deployment/UndertowWebsocketProcessor.java
@@ -92,8 +92,7 @@ public class UndertowWebsocketProcessor {
     public ServletContextAttributeBuildItem deploy(final CombinedIndexBuildItem indexBuildItem,
             UndertowWebsocketRecorder recorder,
             BuildProducer<ReflectiveClassBuildItem> reflection,
-            List<AnnotatedWebsocketEndpointBuildItem> annotatedEndpoints,
-            WebsocketConfig websocketConfig) throws Exception {
+            List<AnnotatedWebsocketEndpointBuildItem> annotatedEndpoints) throws Exception {
 
         final Set<String> endpoints = new HashSet<>();
         final Set<String> config = new HashSet<>();
@@ -134,8 +133,7 @@ public class UndertowWebsocketProcessor {
                 new ReflectiveClassBuildItem(true, true, ClientEndpointConfig.Configurator.class.getName()));
 
         return new ServletContextAttributeBuildItem(WebSocketDeploymentInfo.ATTRIBUTE_NAME,
-                recorder.createDeploymentInfo(annotated, endpoints, config, websocketConfig.maxFrameSize,
-                        websocketConfig.dispatchToWorker));
+                recorder.createDeploymentInfo(annotated, endpoints, config));
     }
 
     private void registerCodersForReflection(BuildProducer<ReflectiveClassBuildItem> reflection,
