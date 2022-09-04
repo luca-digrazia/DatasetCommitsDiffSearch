30e0f917120a2c6749f022ee30185e5decb7cf85
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:10:48 +0200

    Set maxDocsPerIndex to previous config file default of 20000000

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/configuration/ElasticsearchConfiguration.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/configuration/ElasticsearchConfiguration.java
@@ -17,11 +17,8 @@
 package org.graylog2.configuration;
 
 import com.github.joschi.jadconfig.Parameter;
-import com.github.joschi.jadconfig.ValidationException;
-import com.github.joschi.jadconfig.ValidatorMethod;
 import com.github.joschi.jadconfig.converters.StringListConverter;
 import com.github.joschi.jadconfig.util.Duration;
-import com.github.joschi.jadconfig.validators.DirectoryWritableValidator;
 import com.github.joschi.jadconfig.validators.FilePathReadableValidator;
 import com.github.joschi.jadconfig.validators.InetPortValidator;
 import com.github.joschi.jadconfig.validators.PositiveDurationValidator;
@@ -29,11 +26,7 @@ import com.github.joschi.jadconfig.validators.PositiveIntegerValidator;
 import com.github.joschi.jadconfig.validators.PositiveLongValidator;
 import org.joda.time.Period;
 
-import javax.validation.constraints.NotNull;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -62,8 +55,11 @@ public class ElasticsearchConfiguration {
     @Parameter(value = "elasticsearch_http_enabled")
     private boolean httpEnabled = false;
 
+    @Parameter(value = "elasticsearch_discovery_zen_ping_multicast_enabled")
+    private boolean multicastDiscovery = true;
+
     @Parameter(value = "elasticsearch_discovery_zen_ping_unicast_hosts", converter = StringListConverter.class)
-    private List<String> unicastHosts = Collections.singletonList("127.0.0.1:9300");
+    private List<String> unicastHosts;
 
     @Parameter(value = "elasticsearch_discovery_initial_state_timeout")
     private String initialStateTimeout = "3s";
@@ -84,7 +80,7 @@ public class ElasticsearchConfiguration {
     private boolean disableVersionCheck = false;
 
     @Parameter(value = "elasticsearch_config_file", validator = FilePathReadableValidator.class)
-    private Path configFile;
+    private Path configFile; // = "/etc/graylog/server/elasticsearch.yml";
 
     @Parameter(value = "elasticsearch_index_prefix", required = true)
     private String indexPrefix = "graylog";
@@ -163,6 +159,10 @@ public class ElasticsearchConfiguration {
 
     public boolean isHttpEnabled() {
         return httpEnabled;
+    }
+
+    public boolean isMulticastDiscovery() {
+        return multicastDiscovery;
     }
 
     public List<String> getUnicastHosts() {
@@ -273,42 +273,5 @@ public class ElasticsearchConfiguration {
 
     public Duration getRequestTimeout() {
         return requestTimeout;
-    }
-
-    @ValidatorMethod
-    @SuppressWarnings("unused")
-    public void validateElasticsearchHomePath() throws ValidationException {
-        if (getPathHome() != null) {
-            final Path homePath = Paths.get(getPathHome());
-            validateElasticsearchPath(homePath);
-        }
-    }
-
-    @ValidatorMethod
-    @SuppressWarnings("unused")
-    public void validateElasticsearchDataPath() throws ValidationException {
-        if (getPathData() != null) {
-            final Path dataPath = Paths.get(getPathData());
-            validateElasticsearchPath(dataPath);
-        }
-    }
-
-    private void validateElasticsearchPath(@NotNull Path path) throws ValidationException {
-        final Path parent = path.getParent();
-        if (Files.exists(parent) && !Files.isDirectory(parent)) {
-            throw new ValidationException("Path " + parent.toAbsolutePath() + " is not a directory.");
-        }
-
-        if (Files.exists(parent) && !Files.isReadable(parent)) {
-            throw new ValidationException("Path " + parent.toAbsolutePath() + " cannot be read.");
-        }
-
-        if (Files.exists(path) && !Files.isDirectory(path)) {
-            throw new ValidationException("Path " + path.toAbsolutePath() + " is not a directory.");
-        }
-
-        if (Files.exists(path) && !Files.isReadable(path)) {
-            throw new ValidationException("Path " + path.toAbsolutePath() + " cannot be read.");
-        }
     }
 }
