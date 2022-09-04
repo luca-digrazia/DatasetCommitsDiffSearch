cac96c586fc9d9534a6733912903374df469cb60
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:36:05 +0200

    Include Starlark options in `blaze config <c1> <c2>` output.
        Note the "config" command is intended as Blaze developer tool.
        It doesn't have the same test coverage as most of Blaze, so
        there are no tests to extend.
    
        PiperOrigin-RevId: 288579798

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/commands/ConfigCommand.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/commands/ConfigCommand.java
@@ -79,6 +79,9 @@ public class ConfigCommand implements BlazeCommand {
   }
 
   @Override
+  public void editOptions(OptionsParser optionsParser) {}
+
+  @Override
   public BlazeCommandResult exec(CommandEnvironment env, OptionsParsingResult options) {
     ImmutableMap<String, BuildConfiguration> configurations = findConfigurations(env);
 
