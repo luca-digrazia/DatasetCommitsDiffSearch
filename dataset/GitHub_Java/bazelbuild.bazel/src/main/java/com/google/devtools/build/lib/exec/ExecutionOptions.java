ad11bf7d345dcc5dd0199ed0349c4929866a72a7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:15:45 +0200

    Flip --incompatible_remove_local_resources to true.
        Fixes #10536.
    
        RELNOTES: --incompatible_remove_local_resources is true by default. Please use --local_ram_resources and --local_cpu_resources instead of --local_resources.
        PiperOrigin-RevId: 298614925

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/ExecutionOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/ExecutionOptions.java
@@ -278,7 +278,7 @@ public class ExecutionOptions extends OptionsBase {
 
   @Option(
       name = "incompatible_remove_ram_utilization_factor",
-      defaultValue = "true",
+      defaultValue = "false",
       documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
       effectTags = {OptionEffectTag.EXECUTION},
       metadataTags = {
