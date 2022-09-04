0ae2d3f450f66cf877664bb99a3eb72283a42470
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:57:01 +0200

    Implement some BeanManager methods
        It's not 100% correct but better than the current
        situation.

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/stereotypes/StereotypeInterceptorTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/stereotypes/StereotypeInterceptorTest.java
@@ -1,44 +1,39 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package io.quarkus.arc.test.stereotypes;
 
-package org.jboss.quarkus.arc.test.stereotypes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.assertEquals;
-
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.test.ArcTestContainer;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 import javax.enterprise.inject.Stereotype;
-
-import org.jboss.quarkus.arc.Arc;
-import org.jboss.quarkus.arc.test.ArcTestContainer;
-import org.junit.Rule;
-import org.junit.Test;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 public class StereotypeInterceptorTest {
 
-    @Rule
-    public ArcTestContainer container = new ArcTestContainer(BeIntercepted.class, IamIntercepted.class, SimpleBinding.class, SimpleInterceptor.class);
+    @RegisterExtension
+    public ArcTestContainer container = new ArcTestContainer(BeIntercepted.class, IamIntercepted.class,
+            SimpleBinding.class,
+            SimpleInterceptor.class);
 
     @Test
     public void testStereotype() {
         assertEquals("interceptedOK", Arc.container().instance(IamIntercepted.class).get().getId());
+    }
+
+    @Test
+    public void testStereotypeBeanManager() {
+        BeanManager beanManager = CDI.current().getBeanManager();
+        Assertions.assertTrue(beanManager.isStereotype(BeIntercepted.class));
+        Assertions.assertFalse(beanManager.isStereotype(SimpleBinding.class));
+
     }
 
     @SimpleBinding
