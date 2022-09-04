540e897ca54198454277da7932f06d7372a7bca3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:01:05 +0200

    ArC - fix InstanceImpl

--- dataset/GitHub_Java/quarkusio.quarkus/ext/arc/runtime/src/main/java/org/jboss/protean/arc/InstanceImpl.java
+++ dataset/GitHub_Java/quarkusio.quarkus/ext/arc/runtime/src/main/java/org/jboss/protean/arc/InstanceImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.protean.arc;
 
 import java.lang.annotation.Annotation;
@@ -28,25 +44,39 @@ class InstanceImpl<T> implements Instance<T> {
 
     private final CreationalContextImpl<?> creationalContext;
 
+    private final Set<InjectableBean<?>> beans;
+
     InstanceImpl(Type type, Set<Annotation> qualifiers, CreationalContextImpl<?> creationalContext) {
         if (type instanceof ParameterizedType) {
-            this.type = ((ParameterizedType) type).getActualTypeArguments()[0];
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            if (parameterizedType.getRawType().equals(Instance.class)) {
+                this.type = parameterizedType.getActualTypeArguments()[0];
+            } else {
+                this.type = type;
+            }
         } else {
-            throw new IllegalArgumentException();
+            this.type = type;
         }
         this.qualifiers = qualifiers != null ? qualifiers : Collections.emptySet();
         this.creationalContext = creationalContext;
+
+        if (this.qualifiers.isEmpty() && Object.class.equals(type)) {
+            // Do not prefetch the beans for Instance<Object> with no qualifiers
+            this.beans = null;
+        } else {
+            this.beans = resolve();
+        }
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new InstanceIterator(getBeans());
+        return new InstanceIterator(beans());
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public T get() {
-        Set<InjectableBean<?>> beans = getBeans();
+        Set<InjectableBean<?>> beans = beans();
         if (beans.isEmpty()) {
             throw new UnsatisfiedResolutionException();
         } else if (beans.size() > 1) {
@@ -66,7 +96,7 @@ class InstanceImpl<T> implements Instance<T> {
     public <U extends T> Instance<U> select(Class<U> subtype, Annotation... qualifiers) {
         Set<Annotation> newQualifiers = new HashSet<>(this.qualifiers);
         Collections.addAll(newQualifiers, qualifiers);
-        return new InstanceImpl<>(type, newQualifiers, creationalContext);
+        return new InstanceImpl<>(subtype, newQualifiers, creationalContext);
     }
 
     @Override
@@ -78,12 +108,12 @@ class InstanceImpl<T> implements Instance<T> {
 
     @Override
     public boolean isUnsatisfied() {
-        return getBeans().isEmpty();
+        return beans().isEmpty();
     }
 
     @Override
     public boolean isAmbiguous() {
-        return getBeans().size() > 1;
+        return beans().size() > 1;
     }
 
     @Override
@@ -94,6 +124,10 @@ class InstanceImpl<T> implements Instance<T> {
         // Try to destroy a dependent instance
         creationalContext.destroyDependentInstance(instance);
     }
+    
+    void destroy() {
+        creationalContext.release();
+    }
 
     private T getBeanInstance(InjectableBean<T> bean) {
         CreationalContextImpl<T> ctx = creationalContext.child();
@@ -102,7 +136,11 @@ class InstanceImpl<T> implements Instance<T> {
         return instance;
     }
 
-    private Set<InjectableBean<?>> getBeans() {
+    private Set<InjectableBean<?>> beans() {
+        return beans != null ? beans : resolve();
+    }
+
+    private Set<InjectableBean<?>> resolve() {
         return ArcContainerImpl.instance().getResolvedBeans(type, qualifiers.toArray(EMPTY_ANNOTATION_ARRAY));
     }
 
