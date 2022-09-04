d105d1c0b2216fe6a5c894a53ee09bd60928a8fd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:34:26 +0200

    VertxHttpRecorder - Router event should be fired only once
        - resolves #4825

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/deployment/src/test/java/io/quarkus/vertx/http/router/RouterEventTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/deployment/src/test/java/io/quarkus/vertx/http/router/RouterEventTest.java
@@ -14,9 +14,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusUnitTest;
 import io.restassured.RestAssured;
-import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
 
 public class RouterEventTest {
 
@@ -33,12 +31,6 @@ public class RouterEventTest {
     public void testRoute() {
         RestAssured.when().get("/boom").then().statusCode(200).body(is("ok"));
         assertEquals(1, RouteProducer.counter);
-
-        RestAssured.given()
-                .body("An example body")
-                .post("/post")
-                .then()
-                .body(is("An example body"));
     }
 
     @Singleton
@@ -49,9 +41,6 @@ public class RouterEventTest {
         void observeRouter(@Observes Router router) {
             counter++;
             router.get("/boom").handler(ctx -> ctx.response().setStatusCode(200).end("ok"));
-            Route post = router.post("/post");
-            post.handler(BodyHandler.create());
-            post.handler(ctx -> ctx.response().end(ctx.getBody()));
         }
 
     }
