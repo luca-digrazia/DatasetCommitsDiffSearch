7f4258cca68d497d900cc110ffd4bd194cc6651f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 23:48:01 +0200

    remote: Have --remote_rest_cache respect --remote_timeout.
        RELNOTES: The --remote_rest_cache flag now respects --remote_timeout.
        PiperOrigin-RevId: 177926523

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/SimpleBlobStoreFactory.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/SimpleBlobStoreFactory.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.remote;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.google.auth.Credentials;
 import com.google.devtools.build.lib.remote.blobstore.OnDiskBlobStore;
 import com.google.devtools.build.lib.remote.blobstore.RestBlobStore;
 import com.google.devtools.build.lib.remote.blobstore.SimpleBlobStore;
@@ -33,10 +32,9 @@ public final class SimpleBlobStoreFactory {
 
   private SimpleBlobStoreFactory() {}
 
-  public static SimpleBlobStore createRest(RemoteOptions options, Credentials creds)
-      throws IOException {
-    return new RestBlobStore(
-        options.remoteRestCache, (int) TimeUnit.SECONDS.toMillis(options.remoteTimeout), creds);
+  public static SimpleBlobStore createRest(RemoteOptions options) throws IOException {
+    return new RestBlobStore(options.remoteRestCache, options.restCachePoolSize,
+        (int) TimeUnit.SECONDS.toMillis(options.remoteTimeout));
   }
 
   public static SimpleBlobStore createLocalDisk(RemoteOptions options, Path workingDirectory)
@@ -45,11 +43,10 @@ public final class SimpleBlobStoreFactory {
         workingDirectory.getRelative(checkNotNull(options.experimentalLocalDiskCachePath)));
   }
 
-  public static SimpleBlobStore create(
-      RemoteOptions options, @Nullable Credentials creds, @Nullable Path workingDirectory)
+  public static SimpleBlobStore create(RemoteOptions options, @Nullable Path workingDirectory)
       throws IOException {
     if (isRestUrlOptions(options)) {
-      return createRest(options, creds);
+      return createRest(options);
     }
     if (workingDirectory != null && isLocalDiskCache(options)) {
       return createLocalDisk(options, workingDirectory);
