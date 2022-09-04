266ec45cb464550d3a1feea3c44b11b32d61cca9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:06:38 +0200

    Test for subscriptions with dynamic GraphQL clients

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/smallrye-graphql-client/src/test/java/io/quarkus/it/smallrye/graphql/client/TypesafeClientTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/smallrye-graphql-client/src/test/java/io/quarkus/it/smallrye/graphql/client/TypesafeClientTest.java
@@ -3,7 +3,6 @@ package io.quarkus.it.smallrye.graphql.client;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URL;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,18 +24,5 @@ public class TypesafeClientTest {
                 .build(LuckyNumbersClientApi.class);
         client.setLuckyNumber(21);
         assertEquals(21, client.luckyNumber());
-    }
-
-    /**
-     * Test a method that contains a `@NonNull` parameter.
-     */
-    @Test
-    public void testNonNull() throws Exception {
-        LuckyNumbersClientApi client = TypesafeGraphQLClientBuilder.newBuilder()
-                .endpoint(url.toString() + "/graphql")
-                .build(LuckyNumbersClientApi.class);
-        List<Integer> someNumbers = List.of(12, 33);
-        List<Integer> returned = client.echoList(someNumbers);
-        assertEquals(someNumbers, returned);
     }
 }
