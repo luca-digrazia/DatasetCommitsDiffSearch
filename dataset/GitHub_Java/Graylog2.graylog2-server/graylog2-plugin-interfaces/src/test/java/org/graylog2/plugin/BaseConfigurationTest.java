8a06e57d01ef4b6c8103efde3ce749b5e48168bb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:26:55 +0200

    Remove deprecated MongoDB configuration settings

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/test/java/org/graylog2/plugin/BaseConfigurationTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/test/java/org/graylog2/plugin/BaseConfigurationTest.java
@@ -1,3 +1,25 @@
+/**
+ * The MIT License
+ * Copyright (c) 2012 Graylog, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.graylog2.plugin;
 
 import com.github.joschi.jadconfig.JadConfig;
@@ -6,22 +28,27 @@ import com.github.joschi.jadconfig.RepositoryException;
 import com.github.joschi.jadconfig.ValidationException;
 import com.github.joschi.jadconfig.repositories.InMemoryRepository;
 import com.google.common.collect.Maps;
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URI;
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 public class BaseConfigurationTest {
     private class Configuration extends BaseConfiguration {
         @Parameter(value = "rest_listen_uri", required = true)
-        private String restListenUri = "http://127.0.0.1:12900/";
+        private URI restListenUri = URI.create("http://127.0.0.1:12900/");
+
+        @Parameter(value = "web_listen_uri", required = true)
+        private URI webListenUri = URI.create("http://127.0.0.1:9000/");
 
         @Parameter(value = "node_id_file", required = false)
-        private String nodeIdFile = "/etc/graylog2-server-node-id";
+        private String nodeIdFile = "/etc/graylog/server/node-id";
 
         @Override
         public String getNodeIdFile() {
@@ -30,35 +57,36 @@ public class BaseConfigurationTest {
 
         @Override
         public URI getRestListenUri() {
-            return Tools.getUriStandard(restListenUri);
+            return Tools.getUriWithPort(restListenUri, BaseConfiguration.GRAYLOG2_DEFAULT_PORT);
+        }
+
+        @Override
+        public URI getWebListenUri() {
+            return Tools.getUriWithPort(webListenUri, BaseConfiguration.GRAYLOG2_DEFAULT_WEB_PORT);
         }
     }
 
     private Map<String, String> validProperties;
     private File tempFile;
 
-    @BeforeMethod
+    @Before
     public void setUp() throws Exception {
         validProperties = Maps.newHashMap();
+        tempFile = File.createTempFile("graylog", null);
 
-        try {
-            tempFile = File.createTempFile("graylog", null);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
         // Required properties
         validProperties.put("password_secret", "ipNUnWxmBLCxTEzXcyamrdy0Q3G7HxdKsAvyg30R9SCof0JydiZFiA3dLSkRsbLF");
         validProperties.put("elasticsearch_config_file", tempFile.getAbsolutePath());
-        validProperties.put("mongodb_useauth", "true");
-        validProperties.put("mongodb_user", "user");
-        validProperties.put("mongodb_password", "pass");
-        validProperties.put("mongodb_database", "test");
-        validProperties.put("mongodb_host", "localhost");
-        validProperties.put("mongodb_port", "27017");
         validProperties.put("use_gelf", "true");
         validProperties.put("gelf_listen_port", "12201");
         validProperties.put("root_password_sha2", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"); // sha2 of admin
+    }
+
+    @After
+    public void tearDown() {
+        if(tempFile != null) {
+            tempFile.delete();
+        }
     }
 
     @Test
@@ -90,5 +118,33 @@ public class BaseConfigurationTest {
         new JadConfig(new InMemoryRepository(validProperties), configuration).process();
 
         Assert.assertEquals("http://10.0.0.1:12900", configuration.getDefaultRestTransportUri().toString());
+    }
+
+    @Test
+    public void testGetRestUriScheme() throws RepositoryException, ValidationException {
+        validProperties.put("rest_enable_tls", "false");
+        final Configuration configWithoutTls = new Configuration();
+        new JadConfig(new InMemoryRepository(validProperties), configWithoutTls).process();
+
+        validProperties.put("rest_enable_tls", "true");
+        final Configuration configWithTls = new Configuration();
+        new JadConfig(new InMemoryRepository(validProperties), configWithTls).process();
+
+        assertEquals("http", configWithoutTls.getRestUriScheme());
+        assertEquals("https", configWithTls.getRestUriScheme());
+    }
+
+    @Test
+    public void testGetWebUriScheme() throws RepositoryException, ValidationException {
+        validProperties.put("web_enable_tls", "false");
+        final Configuration configWithoutTls = new Configuration();
+        new JadConfig(new InMemoryRepository(validProperties), configWithoutTls).process();
+
+        validProperties.put("web_enable_tls", "true");
+        final Configuration configWithTls = new Configuration();
+        new JadConfig(new InMemoryRepository(validProperties), configWithTls).process();
+
+        assertEquals("http", configWithoutTls.getWebUriScheme());
+        assertEquals("https", configWithTls.getWebUriScheme());
     }
 }
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/configuration/MongoDbConfiguration.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/configuration/MongoDbConfiguration.java
@@ -24,6 +24,8 @@ import com.github.joschi.jadconfig.validators.StringNotBlankValidator;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class MongoDbConfiguration {
     @Parameter(value = "mongodb_max_connections", validator = PositiveIntegerValidator.class)
     private int maxConnections = 1000;
