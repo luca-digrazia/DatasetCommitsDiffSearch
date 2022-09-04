35d90339b7b12a786e01a2631ebaef0fc0589e7a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:48:00 +0200

    ä¿®æ¹äºæ­æ¾å¨å¨å±æ¶çéæ©90åº¦é®é¢ (2017-04-11)

--- dataset/GitHub_Java/CarGuo.GSYVideoPlayer/gsyVideoPlayer/src/main/java/com/shuyu/gsyvideoplayer/video/GSYBaseVideoPlayer.java
+++ dataset/GitHub_Java/CarGuo.GSYVideoPlayer/gsyVideoPlayer/src/main/java/com/shuyu/gsyvideoplayer/video/GSYBaseVideoPlayer.java
@@ -273,7 +273,6 @@ public abstract class GSYBaseVideoPlayer extends FrameLayout implements GSYMedia
      * @param actionBar 是否有actionBar，有的话需要隐藏
      * @param statusBar 是否有状态bar，有的话需要隐藏
      */
-    @SuppressWarnings("ResourceType")
     public GSYBaseVideoPlayer startWindowFullscreen(final Context context, final boolean actionBar, final boolean statusBar) {
 
         mSystemUiVisibility = ((Activity) context).getWindow().getDecorView().getSystemUiVisibility();
@@ -419,7 +418,6 @@ public abstract class GSYBaseVideoPlayer extends FrameLayout implements GSYMedia
     /**
      * 回到正常效果
      */
-    @SuppressWarnings("ResourceType")
     private void backToNormal() {
 
         final ViewGroup vp = getViewGroup();
@@ -498,7 +496,6 @@ public abstract class GSYBaseVideoPlayer extends FrameLayout implements GSYMedia
     /**
      * 显示小窗口
      */
-    @SuppressWarnings("ResourceType")
     public GSYBaseVideoPlayer showSmallVideo(Point size, final boolean actionBar, final boolean statusBar) {
 
         final ViewGroup vp = getViewGroup();
@@ -562,7 +559,6 @@ public abstract class GSYBaseVideoPlayer extends FrameLayout implements GSYMedia
     /**
      * 隐藏小窗口
      */
-    @SuppressWarnings("ResourceType")
     public void hideSmallVideo() {
         final ViewGroup vp = getViewGroup();
         GSYVideoPlayer gsyVideoPlayer = (GSYVideoPlayer) vp.findViewById(SMALL_ID);
