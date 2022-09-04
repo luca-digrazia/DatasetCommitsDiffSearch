6788d54f968699b12256ff1bb2e17e1345136d53
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:14:36 +0200

    Refactor to get rid of duplicate code in additional reader / writer handling

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/quarkus-rest/quarkus-rest/deployment/src/main/java/io/quarkus/resteasy/reactive/server/deployment/QuarkusRestProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/quarkus-rest/quarkus-rest/deployment/src/main/java/io/quarkus/resteasy/reactive/server/deployment/QuarkusRestProcessor.java
@@ -63,7 +63,6 @@ import io.quarkus.arc.deployment.BeanArchiveIndexBuildItem;
 import io.quarkus.arc.deployment.BeanContainerBuildItem;
 import io.quarkus.arc.deployment.GeneratedBeanBuildItem;
 import io.quarkus.arc.runtime.BeanContainer;
-import io.quarkus.arc.runtime.ClientProxyUnwrapper;
 import io.quarkus.deployment.Capability;
 import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.GeneratedClassGizmoAdaptor;
@@ -424,7 +423,6 @@ public class QuarkusRestProcessor {
                     .setExceptionMapping(exceptionMapping)
                     .setCtxResolvers(ctxResolvers)
                     .setFeatures(feats)
-                    .setClientProxyUnwrapper(new ClientProxyUnwrapper())
                     .setApplicationSupplier(recorder.handleApplication(applicationClass, singletonClasses.isEmpty()))
                     .setFactoryCreator(recorder.factoryCreator(beanContainerBuildItem.getValue()))
                     .setDynamicFeatures(dynamicFeats)
