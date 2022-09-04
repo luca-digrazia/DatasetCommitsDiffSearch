4e96d1c94472189a14da3f4f40e9bad53b5eca65
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:34:16 +0200

    Include `messages` field if empty in message list search type result. (#6872)
        In 5ee5115642dd0c8d6a1f7ed58f68317a9ccda3f0 an optional name attribute
        defined by the caller for a search type, which is passed through to its
        results, was introduced. To leave it out when it is empty, the
        `@JsonInclude(NON_EMPTY)` annotation was added. Unfortunately, this also
        leaves out the `messages` in the result if it is empty, breaking
        consumer logic which expects it to be present.
    
        This change turns it into `NON_ABSENT` which skips `Optional`s which are
        empty, but includes empty collections.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/plugins/views/search/searchtypes/MessageList.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/plugins/views/search/searchtypes/MessageList.java
@@ -30,26 +30,18 @@ import org.graylog.plugins.views.search.timeranges.DerivedTimeRange;
 import org.graylog.plugins.views.search.Filter;
 import org.graylog.plugins.views.search.SearchType;
 import org.graylog.plugins.views.search.engine.BackendQuery;
-import org.graylog2.contentpacks.EntityDescriptorIds;
-import org.graylog2.contentpacks.model.entities.MessageListEntity;
-import org.graylog2.contentpacks.model.entities.SearchTypeEntity;
-import org.graylog2.decorators.Decorator;
-import org.graylog2.decorators.DecoratorImpl;
 import org.graylog2.plugin.indexer.searches.timeranges.AbsoluteRange;
 import org.graylog2.plugin.indexer.searches.timeranges.KeywordRange;
 import org.graylog.plugins.views.search.timeranges.OffsetRange;
 import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
 import org.graylog2.plugin.indexer.searches.timeranges.TimeRange;
-import org.graylog2.rest.models.messages.responses.DecorationStats;
 import org.graylog2.rest.models.messages.responses.ResultMessageSummary;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @AutoValue
 @JsonTypeName(MessageList.NAME)
@@ -81,17 +73,13 @@ public abstract class MessageList implements SearchType {
     @Nullable
     public abstract List<Sort> sort();
 
-    @JsonProperty
-    public abstract List<Decorator> decorators();
-
     @JsonCreator
     public static Builder builder() {
         return new AutoValue_MessageList.Builder()
                 .type(NAME)
                 .limit(150)
                 .offset(0)
-                .streams(Collections.emptySet())
-                .decorators(Collections.emptyList());
+                .streams(Collections.emptySet());
     }
 
     public abstract Builder toBuilder();
@@ -161,13 +149,6 @@ public abstract class MessageList implements SearchType {
         @JsonProperty
         public abstract Builder sort(@Nullable List<Sort> sort);
 
-        @JsonProperty("decorators")
-        public Builder _decorators(List<DecoratorImpl> decorators) {
-            return decorators(new ArrayList<>(decorators));
-        }
-
-        public abstract Builder decorators(List<Decorator> decorators);
-
         public abstract MessageList build();
     }
 
@@ -187,9 +168,6 @@ public abstract class MessageList implements SearchType {
 
         @JsonProperty
         public abstract List<ResultMessageSummary> messages();
-
-        @JsonProperty
-        public abstract Optional<DecorationStats> decorationStats();
 
         @JsonProperty
         public abstract long totalResults();
@@ -212,26 +190,7 @@ public abstract class MessageList implements SearchType {
 
             public abstract Builder totalResults(long totalResults);
 
-            public abstract Builder decorationStats(DecorationStats decorationStats);
-
             public abstract Result build();
         }
-    }
-
-    @Override
-    public SearchTypeEntity toContentPackEntity(EntityDescriptorIds entityDescriptorIds) {
-        return MessageListEntity.builder()
-                .decorators(decorators())
-                .streams(mappedStreams(entityDescriptorIds))
-                .timerange(timerange().orElse(null))
-                .limit(limit())
-                .offset(offset())
-                .filter(filter())
-                .id(id())
-                .name(name().orElse(null))
-                .query(query().orElse(null))
-                .type(type())
-                .sort(sort())
-                .build();
     }
 }
