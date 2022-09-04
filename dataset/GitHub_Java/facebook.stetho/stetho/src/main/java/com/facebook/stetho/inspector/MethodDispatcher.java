2dcefa0461c26ab1151c7d1067ee9f48d8e8f4a6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:00:14 +0200

    Added ability for domain classes to be inherited: look-up for methods with @ChromeDevtoolsMethod not only in the class itself, but also in the parent classes.

--- dataset/GitHub_Java/facebook.stetho/stetho/src/main/java/com/facebook/stetho/inspector/MethodDispatcher.java
+++ dataset/GitHub_Java/facebook.stetho/stetho/src/main/java/com/facebook/stetho/inspector/MethodDispatcher.java
@@ -1,4 +1,11 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
+/*
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 package com.facebook.stetho.inspector;
 
@@ -105,7 +112,7 @@ public class MethodDispatcher {
       Class<?> handlerClass = domainHandler.getClass();
       String domainName = handlerClass.getSimpleName();
 
-      for (Method method : handlerClass.getDeclaredMethods()) {
+      for (Method method : handlerClass.getMethods()) {
         if (isDevtoolsMethod(method)) {
           MethodDispatchHelper dispatchHelper = new MethodDispatchHelper(
               objectMapper,
