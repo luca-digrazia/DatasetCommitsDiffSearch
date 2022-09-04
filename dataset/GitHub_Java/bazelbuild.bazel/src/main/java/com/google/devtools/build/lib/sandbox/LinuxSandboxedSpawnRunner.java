c83ce0302b0f90ab5e3feb6a0dc5c76239bd98c3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:14:09 +0200

    Put the @Nullable annotation before any modifiers (per the style guide).
        The upcoming unknown commit copy/pasted lines from these files and
        triggered a lint warning about this, so let's just fix all other
        instances of the same issue.
    
        RELNOTES: None.
        PiperOrigin-RevId: 299840548

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxedSpawnRunner.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxedSpawnRunner.java
@@ -149,8 +149,7 @@ final class LinuxSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
     // b/64689608: The execroot of the sandboxed process must end with the workspace name, just like
     // the normal execroot does.
-    String workspaceName = execRoot.getBaseName();
-    Path sandboxExecRoot = sandboxPath.getRelative("execroot").getRelative(workspaceName);
+    Path sandboxExecRoot = sandboxPath.getRelative("execroot").getRelative(execRoot.getBaseName());
     sandboxExecRoot.getParentDirectory().createDirectory();
     sandboxExecRoot.createDirectory();
 
@@ -194,7 +193,6 @@ final class LinuxSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
       return new SandboxfsSandboxedSpawn(
           sandboxfsProcess,
           sandboxPath,
-          workspaceName,
           commandLineBuilder.build(),
           environment,
           SandboxHelpers.processInputFiles(
