cc240b88a31f2f2ea5c83fff462d1d74cb7128b1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:25:26 +0200

    Using execProperties in platform generation.
        Checking to ensure that execProperties are not conflicting with remoteExecutionProperties will be done at PlatformInfo creation time (in a later cl).
    
        RELNOTES: None
        PiperOrigin-RevId: 260930416

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/analysis/platform/PlatformUtilsTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/analysis/platform/PlatformUtilsTest.java
@@ -18,10 +18,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import build.bazel.remote.execution.v2.Platform;
 import com.google.common.collect.ImmutableMap;
-import com.google.devtools.build.lib.actions.Spawn;
-import com.google.devtools.build.lib.exec.util.SpawnBuilder;
 import com.google.devtools.build.lib.remote.options.RemoteOptions;
-import com.google.devtools.common.options.Options;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -29,37 +26,21 @@ import org.junit.runners.JUnit4;
 /** Tests for {@link PlatformUtils } */
 @RunWith(JUnit4.class)
 public final class PlatformUtilsTest {
-  private static String platformOptionsString() {
-    return String.join(
-        "\n",
-        "properties: {",
-        " name: \"b\"",
-        " value: \"2\"",
-        "}",
-        "properties: {",
-        " name: \"a\"",
-        " value: \"1\"",
-        "}");
-  }
-
   private static RemoteOptions remoteOptions() {
-    RemoteOptions remoteOptions = Options.getDefaults(RemoteOptions.class);
-    remoteOptions.remoteDefaultPlatformProperties = platformOptionsString();
+    RemoteOptions remoteOptions = new RemoteOptions();
+    remoteOptions.remoteDefaultPlatformProperties =
+        String.join(
+            "\n",
+            "properties: {",
+            " name: \"b\"",
+            " value: \"2\"",
+            "}",
+            "properties: {",
+            " name: \"a\"",
+            " value: \"1\"",
+            "}");
 
     return remoteOptions;
-  }
-
-  @Test
-  public void testParsePlatformLegacyOptions() throws Exception {
-    Platform expected =
-        Platform.newBuilder()
-            .addProperties(Platform.Property.newBuilder().setName("a").setValue("1"))
-            .addProperties(Platform.Property.newBuilder().setName("b").setValue("2"))
-            .build();
-    PlatformInfo platform =
-        PlatformInfo.builder().setRemoteExecutionProperties(platformOptionsString()).build();
-    Spawn s = new SpawnBuilder("dummy").withPlatform(platform).build();
-    assertThat(PlatformUtils.getPlatformProto(s, null)).isEqualTo(expected);
   }
 
   @Test
@@ -69,21 +50,19 @@ public final class PlatformUtilsTest {
             .addProperties(Platform.Property.newBuilder().setName("a").setValue("1"))
             .addProperties(Platform.Property.newBuilder().setName("b").setValue("2"))
             .build();
-    Spawn s = new SpawnBuilder("dummy").build();
-    assertThat(PlatformUtils.getPlatformProto(s, remoteOptions())).isEqualTo(expected);
+    assertThat(PlatformUtils.getPlatformProto(null, remoteOptions())).isEqualTo(expected);
   }
 
   @Test
   public void testParsePlatformHandlesNull() throws Exception {
-    Spawn s = new SpawnBuilder("dummy").build();
-    assertThat(PlatformUtils.getPlatformProto(s, null)).isEqualTo(null);
+    assertThat(PlatformUtils.getPlatformProto(null, null)).isEqualTo(null);
   }
 
   @Test
-  public void testParsePlatformSortsProperties_execProperties() throws Exception {
+  public void testParsePlatformSortsProperties_ExecProperties() throws Exception {
     // execProperties are chosen even if there are remoteOptions
     ImmutableMap<String, String> map = ImmutableMap.of("aa", "99", "zz", "66", "dd", "11");
-    Spawn s = new SpawnBuilder("dummy").withExecProperties(map).build();
+    PlatformInfo platformInfo = PlatformInfo.builder().setExecProperties(map).build();
 
     Platform expected =
         Platform.newBuilder()
@@ -92,6 +71,6 @@ public final class PlatformUtilsTest {
             .addProperties(Platform.Property.newBuilder().setName("zz").setValue("66"))
             .build();
     // execProperties are sorted by key
-    assertThat(PlatformUtils.getPlatformProto(s, remoteOptions())).isEqualTo(expected);
+    assertThat(PlatformUtils.getPlatformProto(platformInfo, remoteOptions())).isEqualTo(expected);
   }
 }
