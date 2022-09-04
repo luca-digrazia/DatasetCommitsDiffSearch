909faa8063d6b0286dc8b3d9b99d1c261a1ff471
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 00:38:35 +0200

    Use getExecPathString when getting bash_main_file
        Fix https://github.com/bazelbuild/bazel/issues/3491
    
        RELNOTES: None
        PiperOrigin-RevId: 164089270

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/sh/ShBinary.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/sh/ShBinary.java
@@ -13,34 +13,36 @@
 // limitations under the License.
 package com.google.devtools.build.lib.bazel.rules.sh;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.ByteSource;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
 import com.google.devtools.build.lib.analysis.RuleConfiguredTarget.Mode;
 import com.google.devtools.build.lib.analysis.RuleConfiguredTargetBuilder;
-import com.google.devtools.build.lib.analysis.RuleConfiguredTargetFactory;
 import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.analysis.Runfiles;
 import com.google.devtools.build.lib.analysis.RunfilesProvider;
 import com.google.devtools.build.lib.analysis.RunfilesSupport;
+import com.google.devtools.build.lib.analysis.actions.BinaryFileWriteAction;
 import com.google.devtools.build.lib.analysis.actions.ExecutableSymlinkAction;
-import com.google.devtools.build.lib.analysis.actions.TemplateExpansionAction;
-import com.google.devtools.build.lib.analysis.actions.TemplateExpansionAction.Substitution;
-import com.google.devtools.build.lib.analysis.actions.TemplateExpansionAction.Template;
+import com.google.devtools.build.lib.analysis.actions.SpawnAction;
 import com.google.devtools.build.lib.bazel.rules.BazelConfiguration;
-import com.google.devtools.build.lib.bazel.rules.NativeLauncherUtil;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
+import com.google.devtools.build.lib.rules.RuleConfiguredTargetFactory;
 import com.google.devtools.build.lib.util.OS;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 /**
  * Implementation for the sh_binary rule.
  */
 public class ShBinary implements RuleConfiguredTargetFactory {
-  private static final Template STUB_SCRIPT_WINDOWS =
-      Template.forResource(ShBinary.class, "sh_stub_template_windows.txt");
 
   @Override
   public ConfiguredTarget create(RuleContext ruleContext) throws RuleErrorException {
@@ -71,7 +73,7 @@ public class ShBinary implements RuleConfiguredTargetFactory {
 
     Artifact mainExecutable =
         (OS.getCurrent() == OS.WINDOWS) ? launcherForWindows(ruleContext, symlink, src) : symlink;
-    if (!symlink.equals(mainExecutable)) {
+    if (symlink != mainExecutable) {
       filesToBuildBuilder.add(mainExecutable);
       runfilesBuilder.addArtifact(symlink);
     }
@@ -82,16 +84,28 @@ public class ShBinary implements RuleConfiguredTargetFactory {
             .addRunfiles(ruleContext, RunfilesProvider.DEFAULT_RUNFILES)
             .build();
 
-    // Create the RunfilesSupport with the mainExecutable's name. On Windows, this way the runfiles
-    // directory's name is derived from the launcher (yielding "%{name}.cmd.runfiles" or
-    // "%{name}.exe.runfiles").
+    // Create the RunfilesSupport with the symlink's name, even on Windows. This way the runfiles
+    // directory's name is derived from the symlink (yielding "%{name}.runfiles) and not from the
+    // wrapper script (yielding "%{name}.cmd.runfiles").
     RunfilesSupport runfilesSupport =
-        RunfilesSupport.withExecutable(ruleContext, runfiles, mainExecutable);
+        RunfilesSupport.withExecutable(ruleContext, runfiles, symlink);
     return new RuleConfiguredTargetBuilder(ruleContext)
         .setFilesToBuild(filesToBuild)
         .setRunfilesSupport(runfilesSupport, mainExecutable)
         .addProvider(RunfilesProvider.class, RunfilesProvider.simple(runfiles))
         .build();
+  }
+
+  // Write launch info to buffer, return the number of bytes written.
+  private static int writeLaunchInfo(ByteArrayOutputStream buffer, String key, String value)
+      throws IOException {
+    byte[] keyBytes = key.getBytes(UTF_8);
+    byte[] valueBytes = value.getBytes(UTF_8);
+    buffer.write(keyBytes);
+    buffer.write('=');
+    buffer.write(valueBytes);
+    buffer.write('\0');
+    return keyBytes.length + valueBytes.length + 2;
   }
 
   private static boolean isWindowsExecutable(Artifact artifact) {
@@ -100,39 +114,14 @@ public class ShBinary implements RuleConfiguredTargetFactory {
         || artifact.getExtension().equals("bat");
   }
 
-  private static Artifact createWindowsExeLauncher(RuleContext ruleContext)
-      throws RuleErrorException {
-    Artifact bashLauncher =
-        ruleContext.getImplicitOutputArtifact(ruleContext.getTarget().getName() + ".exe");
-
-    ByteArrayOutputStream launchInfo = new ByteArrayOutputStream();
-    try {
-      NativeLauncherUtil.writeLaunchInfo(launchInfo, "binary_type", "Bash");
-      NativeLauncherUtil.writeLaunchInfo(
-          launchInfo, "workspace_name", ruleContext.getWorkspaceName());
-      NativeLauncherUtil.writeLaunchInfo(
-          launchInfo,
-          "bash_bin_path",
-          ruleContext.getFragment(BazelConfiguration.class).getShellExecutable().getPathString());
-      NativeLauncherUtil.writeDataSize(launchInfo);
-    } catch (IOException e) {
-      ruleContext.ruleError(e.getMessage());
-      throw new RuleErrorException();
-    }
-
-    NativeLauncherUtil.createNativeLauncherActions(ruleContext, bashLauncher, launchInfo);
-
-    return bashLauncher;
-  }
-
   private static Artifact launcherForWindows(
       RuleContext ruleContext, Artifact primaryOutput, Artifact mainFile)
       throws RuleErrorException {
     if (isWindowsExecutable(mainFile)) {
+      // If the extensions don't match, we should always respect mainFile's extension.
       if (mainFile.getExtension().equals(primaryOutput.getExtension())) {
         return primaryOutput;
       } else {
-        // If the extensions don't match, we should always respect mainFile's extension.
         ruleContext.ruleError(
             "Source file is a Windows executable file,"
                 + " target name extension should match source file extension");
@@ -140,25 +129,65 @@ public class ShBinary implements RuleConfiguredTargetFactory {
       }
     }
 
-    if (ruleContext.getConfiguration().enableWindowsExeLauncher()) {
-      return createWindowsExeLauncher(ruleContext);
+    // The launcher file consists of a base launcher binary and the launch information appended to
+    // the binary. The length of launch info is a signed 64-bit integer written at the end of
+    // the binary in little endian.
+    Artifact launcher = ruleContext.getPrerequisiteArtifact("$launcher", Mode.HOST);
+    Artifact bashLauncher =
+        ruleContext.getImplicitOutputArtifact(ruleContext.getTarget().getName() + ".exe");
+    Artifact launchInfoFile =
+        ruleContext.getRelatedArtifact(bashLauncher.getRootRelativePath(), ".launch_info");
+
+    ByteArrayOutputStream launchInfo = new ByteArrayOutputStream();
+    Long dataSize = 0L;
+    try {
+      dataSize += writeLaunchInfo(launchInfo, "binary_type", "Bash");
+      dataSize += writeLaunchInfo(launchInfo, "workspace_name", ruleContext.getWorkspaceName());
+      dataSize +=
+          writeLaunchInfo(
+              launchInfo,
+              "bash_bin_path",
+              ruleContext
+                  .getFragment(BazelConfiguration.class)
+                  .getShellExecutable()
+                  .getPathString());
+      dataSize += writeLaunchInfo(launchInfo, "bash_main_file", mainFile.getExecPathString());
+
+      ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+      // All Windows versions are little endian.
+      buffer.order(ByteOrder.LITTLE_ENDIAN);
+      buffer.putLong(dataSize);
+
+      launchInfo.write(buffer.array());
+    } catch (IOException e) {
+      ruleContext.ruleError(e.getMessage());
+      throw new RuleErrorException();
     }
 
-    Artifact wrapper =
-        ruleContext.getImplicitOutputArtifact(ruleContext.getTarget().getName() + ".cmd");
     ruleContext.registerAction(
-        new TemplateExpansionAction(
+        new BinaryFileWriteAction(
             ruleContext.getActionOwner(),
-            wrapper,
-            STUB_SCRIPT_WINDOWS,
-            ImmutableList.of(
-                Substitution.of(
-                    "%bash_exe_path%",
-                    ruleContext
-                        .getFragment(BazelConfiguration.class)
-                        .getShellExecutable()
-                        .getPathString())),
-            true));
-    return wrapper;
+            launchInfoFile,
+            ByteSource.wrap(launchInfo.toByteArray()),
+            /*makeExecutable=*/ false));
+    String path = ruleContext.getConfiguration().getActionEnvironment().getFixedEnv().get("PATH");
+    ruleContext.registerAction(
+        new SpawnAction.Builder()
+            .addInput(launcher)
+            .addInput(launchInfoFile)
+            .addOutput(bashLauncher)
+            .setShellCommand(
+                "cmd.exe /c \"copy /Y /B "
+                    + launcher.getExecPathString().replace('/', '\\')
+                    + "+"
+                    + launchInfoFile.getExecPathString().replace('/', '\\')
+                    + " "
+                    + bashLauncher.getExecPathString().replace('/', '\\')
+                    + " > nul\"")
+            .setEnvironment(ImmutableMap.of("PATH", path))
+            .setMnemonic("BuildBashLauncher")
+            .build(ruleContext));
+
+    return bashLauncher;
   }
 }
