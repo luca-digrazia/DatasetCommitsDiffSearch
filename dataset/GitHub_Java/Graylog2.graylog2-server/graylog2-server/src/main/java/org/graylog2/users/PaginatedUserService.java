ae1044df72c052d644a800ec4afa38a78bd93bb3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:21:22 +0200

    Add missing routes to AuthzRolesResource (#8712)
        * Add missing routes to AuthzRolesResource
    
        Prior this change, there was no way to get all users which had a certain
        role assigned. Also the user wanted to add and remove users from the
        role.
    
        With this change three new routes were introduced:
           - get all users for a role
           - add a role to a user
           - remove a role from a user
    
        * Fix annotations from @bernd
    
        * Add missing delete route

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/users/PaginatedUserService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/users/PaginatedUserService.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.users;
 
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
@@ -9,23 +25,42 @@ import org.mongojack.DBQuery;
 import org.mongojack.DBSort;
 
 import javax.inject.Inject;
+import java.util.Set;
 
-public class PaginatedUserService extends PaginatedDbService<UserDTO> {
+public class PaginatedUserService extends PaginatedDbService<UserOverviewDTO> {
     private static final String COLLECTION_NAME = "users";
 
     @Inject
     public PaginatedUserService(MongoConnection mongoConnection,
                                 MongoJackObjectMapperProvider mapper) {
-        super(mongoConnection, mapper, UserDTO.class, COLLECTION_NAME);
+        super(mongoConnection, mapper, UserOverviewDTO.class, COLLECTION_NAME);
     }
 
     public long count() {
         return db.count();
     }
 
-    public PaginatedList<UserDTO> findPaginated(SearchQuery searchQuery, int page,
-                                                int perPage, String sortField, String order) {
+    public PaginatedList<UserOverviewDTO> findPaginated(SearchQuery searchQuery, int page,
+                                                        int perPage, String sortField, String order) {
         final DBQuery.Query dbQuery = searchQuery.toDBQuery();
+        final DBSort.SortBuilder sortBuilder = getSortBuilder(order, sortField);
+        return findPaginatedWithQueryAndSort(dbQuery, sortBuilder, page, perPage);
+    }
+
+    public PaginatedList<UserOverviewDTO> findPaginatedByUserName(SearchQuery searchQuery, int page,
+                                                                  int perPage, String sortField, String order,
+                                                                  Set<String> usernames) {
+        final DBQuery.Query dbQuery = searchQuery.toDBQuery()
+                .in(UserOverviewDTO.FIELD_USERNAME, usernames);
+        final DBSort.SortBuilder sortBuilder = getSortBuilder(order, sortField);
+        return findPaginatedWithQueryAndSort(dbQuery, sortBuilder, page, perPage);
+    }
+
+    public PaginatedList<UserOverviewDTO> findPaginatedByRole(SearchQuery searchQuery, int page,
+                                                              int perPage, String sortField, String order,
+                                                              String roleId) {
+        final DBQuery.Query dbQuery = searchQuery.toDBQuery()
+                .all(UserImpl.ROLES, roleId);
         final DBSort.SortBuilder sortBuilder = getSortBuilder(order, sortField);
         return findPaginatedWithQueryAndSort(dbQuery, sortBuilder, page, perPage);
     }
