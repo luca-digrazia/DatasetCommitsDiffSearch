79a97241207fb1384101743f1b2b4c6742877c5c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:29:17 +0200

    Add installation_source configuration setting and log it on startup
        Closes #1530
    
        (cherry picked from commit 5d6169c259cf22f2ee815a9338b84f7c0ffcbf2e)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-bootstrap/src/main/java/org/graylog2/bootstrap/ServerBootstrap.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-bootstrap/src/main/java/org/graylog2/bootstrap/ServerBootstrap.java
@@ -1,24 +1,22 @@
 /**
- * This file is part of Graylog2.
+ * This file is part of Graylog.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.bootstrap;
 
-import com.google.common.collect.Iterables;
 import com.google.common.util.concurrent.ServiceManager;
-import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.ProvisionException;
@@ -26,27 +24,27 @@ import io.airlift.airline.Option;
 import org.graylog2.plugin.BaseConfiguration;
 import org.graylog2.plugin.ServerStatus;
 import org.graylog2.plugin.Tools;
-import org.graylog2.plugin.inject.Graylog2Module;
 import org.graylog2.plugin.inputs.MessageInput;
 import org.graylog2.shared.bindings.GenericBindings;
-import org.graylog2.shared.bindings.InstantiationService;
-import org.graylog2.shared.bindings.PluginBindings;
+import org.graylog2.shared.bindings.GenericInitializerBindings;
+import org.graylog2.shared.bindings.MessageInputBindings;
+import org.graylog2.shared.bindings.PluginRestResourceBindings;
+import org.graylog2.shared.bindings.SchedulerBindings;
+import org.graylog2.shared.bindings.ServerStatusBindings;
+import org.graylog2.shared.bindings.SharedPeriodicalBindings;
+import org.graylog2.shared.bindings.ValidatorModule;
 import org.graylog2.shared.initializers.ServiceManagerListener;
 import org.graylog2.shared.system.activities.Activity;
 import org.graylog2.shared.system.activities.ActivityWriter;
 import org.graylog2.shared.system.stats.SystemStatsModule;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -58,8 +56,8 @@ public abstract class ServerBootstrap extends CmdLineTool {
         this.commandName = commandName;
     }
 
-    @Option(name = {"-p", "--pidfile"}, description = "File containing the PID of Graylog2")
-    private String pidFile = TMPDIR + FILE_SEPARATOR + "graylog2.pid";
+    @Option(name = {"-p", "--pidfile"}, description = "File containing the PID of Graylog")
+    private String pidFile = TMPDIR + FILE_SEPARATOR + "graylog.pid";
 
     @Option(name = {"-np", "--no-pid-file"}, description = "Do not write a PID file (overrides -p/--pidfile)")
     private boolean noPidFile = false;
@@ -76,7 +74,8 @@ public abstract class ServerBootstrap extends CmdLineTool {
 
     @Override
     protected void startCommand() {
-        LOG.info("Graylog2 " + commandName + " {} starting up. (JRE: {})", version, Tools.getSystemInformation());
+        LOG.info("Graylog {} {} starting up (JRE: {}, installed from: {})", commandName, version,
+                Tools.getSystemInformation(), configuration.getInstallationSource());
 
         // Do not use a PID file if the user requested not to
         if (!isNoPidFile()) {
@@ -120,13 +119,13 @@ public abstract class ServerBootstrap extends CmdLineTool {
             } catch (TimeoutException timeoutException) {
                 LOG.error("Unable to shutdown properly on time. {}", serviceManager.servicesByState());
             }
-            LOG.error("Graylog2 startup failed. Exiting. Exception was:", e);
+            LOG.error("Graylog startup failed. Exiting. Exception was:", e);
             System.exit(-1);
         }
         LOG.info("Services started, startup times in ms: {}", serviceManager.startupTimes());
 
         activityWriter.write(new Activity("Started up.", Main.class));
-        LOG.info("Graylog2 " + commandName + " up and running.");
+        LOG.info("Graylog " + commandName + " up and running.");
 
         // Block forever.
         try {
@@ -154,28 +153,17 @@ public abstract class ServerBootstrap extends CmdLineTool {
     }
 
     @Override
-    protected List<Module> getSharedBindingsModules(InstantiationService instantiationService) {
-        final List<Module> result = super.getSharedBindingsModules(instantiationService);
-        result.add(new GenericBindings(instantiationService));
-        Reflections reflections = new Reflections("org.graylog2.shared.bindings");
-        final Set<Class<? extends AbstractModule>> generic = reflections.getSubTypesOf(AbstractModule.class);
-        final Set<Class<? extends Graylog2Module>> gl2Modules = reflections.getSubTypesOf(Graylog2Module.class);
-        for (Class<? extends Module> type : Iterables.concat(generic, gl2Modules)) {
-            // skip the some modules, because we have already instantiated it above, avoids a bogus log message
-            if (type.equals(GenericBindings.class) || type.equals(PluginBindings.class)) {
-                continue;
-            }
-            try {
-                Constructor<? extends Module> constructor = type.getConstructor();
-                Module module = constructor.newInstance();
-                result.add(module);
-            } catch (InstantiationException | InvocationTargetException | IllegalAccessException e) {
-                LOG.error("Unable to instantiate Module {}: {}", type, e);
-            } catch (NoSuchMethodException e) {
-                LOG.info("No constructor found for guice module {}", type);
-            }
-        }
+    protected List<Module> getSharedBindingsModules() {
+        final List<Module> result = super.getSharedBindingsModules();
 
+        result.add(new GenericBindings());
+        result.add(new ServerStatusBindings(capabilities()));
+        result.add(new ValidatorModule());
+        result.add(new SharedPeriodicalBindings());
+        result.add(new SchedulerBindings());
+        result.add(new GenericInitializerBindings());
+        result.add(new PluginRestResourceBindings());
+        result.add(new MessageInputBindings());
         result.add(new SystemStatsModule(configuration.isDisableSigar()));
 
         return result;
