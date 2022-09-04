da14a8b2041ebfa9a51ccafecc21f2714f51feb7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:26:13 +0200

    Fixing bugs in Head and Options, the generated code did not compile because of missing parameters in exchange method

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/rest/HeadProcessor.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/rest/HeadProcessor.java
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2010-2011 eBusiness Information, Excilys Group
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed To in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.googlecode.androidannotations.processing.rest;
 
 import java.lang.annotation.Annotation;
@@ -49,13 +34,14 @@ public class HeadProcessor extends MethodProcessor {
 		ExecutableElement executableElement = (ExecutableElement) element;
 
 		TypeMirror returnType = executableElement.getReturnType();
-
+		
 		JClass expectedClass = holder.refClass(returnType.toString());
-
+		
 		Head headAnnotation = element.getAnnotation(Head.class);
 		String urlSuffix = headAnnotation.value();
-
-		generateRestTemplateCallBlock(new MethodProcessorHolder(executableElement, urlSuffix, expectedClass, expectedClass, codeModel));
+		String url = holder.urlPrefix + urlSuffix;
+		
+		generateRestTemplateCallBlock(new MethodProcessorHolder(executableElement, url, expectedClass, expectedClass, codeModel));
 	}
 
 	@Override
--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/rest/OptionsProcessor.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/rest/OptionsProcessor.java
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2010-2011 eBusiness Information, Excilys Group
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed To in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.googlecode.androidannotations.processing.rest;
 
 import java.lang.annotation.Annotation;
@@ -51,19 +36,19 @@ public class OptionsProcessor extends MethodProcessor {
 		ExecutableElement executableElement = (ExecutableElement) element;
 
 		TypeMirror returnType = executableElement.getReturnType();
-
+		
 		DeclaredType declaredReturnType = (DeclaredType) returnType;
 
 		TypeMirror typeParameter = declaredReturnType.getTypeArguments().get(0);
 
 		JClass expectedClass = holder.refClass(typeParameter.toString());
-
+		
 		JClass generatedReturnType = holder.refClass(ProcessorConstants.SET).narrow(expectedClass);
-
+		
 		Options optionsAnnotation = element.getAnnotation(Options.class);
 		String urlSuffix = optionsAnnotation.value();
 		String url = holder.urlPrefix + urlSuffix;
-
+		
 		generateRestTemplateCallBlock(new MethodProcessorHolder(executableElement, url, expectedClass, generatedReturnType, codeModel));
 	}
 
@@ -71,6 +56,7 @@ public class OptionsProcessor extends MethodProcessor {
 	protected JInvocation addResultCallMethod(JInvocation restCall, MethodProcessorHolder methodHolder) {
 		restCall = JExpr.invoke(restCall, "getHeaders");
 		restCall = JExpr.invoke(restCall, "getAllow");
+		
 		return restCall;
 	}
 
