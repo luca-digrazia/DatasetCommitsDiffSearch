9d163d344287b534189a9a03d676fcdb22aaed5b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:55:28 +0200

    fix typo

--- dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/validation/FMeasure.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/validation/FMeasure.java
@@ -1,18 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2010-2019 Haifeng Li
+ * Copyright (c) 2010 Haifeng Li
  *
- * Smile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Smile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
 
 package smile.validation;
@@ -34,15 +33,6 @@ package smile.validation;
  * @author Haifeng Li
  */
 public class FMeasure implements ClassificationMeasure {
-    public final static FMeasure instance = new FMeasure();
-
-    @Override
-    public double measure(int[] truth, int[] prediction) {
-        double p = new Precision().measure(truth, prediction);
-        double r = new Recall().measure(truth, prediction);
-        return (1 + beta2) * (p * r) / (beta2 * p + r);
-    }
-
     /**
      * A positive value such that F-score measures the effectiveness of
      * retrieval with respect to a user who attaches &beta; times
@@ -68,8 +58,10 @@ public class FMeasure implements ClassificationMeasure {
         this.beta2 = beta * beta;
     }
 
-    /** Calculates the F1 score. */
-    public static double apply(int[] truth, int[] prediction) {
-        return instance.measure(truth, prediction);
+    @Override
+    public double measure(int[] truth, int[] prediction) {
+        double p = new Precision().measure(truth, prediction);
+        double r = new Recall().measure(truth, prediction);
+        return (1 + beta2) * (p * r) / (beta2 * p + r);
     }
 }
