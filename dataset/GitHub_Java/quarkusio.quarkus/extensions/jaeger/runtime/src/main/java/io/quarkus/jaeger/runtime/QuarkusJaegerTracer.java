44b44e71aaefcba5b9c1962732ef915d4a47b0d8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:17:52 +0200

    Make sure that tracer is instantiated only once
        Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/jaeger/runtime/src/main/java/io/quarkus/jaeger/runtime/QuarkusJaegerTracer.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/jaeger/runtime/src/main/java/io/quarkus/jaeger/runtime/QuarkusJaegerTracer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.jaeger.runtime;
 
 import io.jaegertracing.Configuration;
@@ -6,11 +22,9 @@ import io.opentracing.Span;
 import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
 import io.opentracing.propagation.Format;
-import io.opentracing.util.ThreadLocalScopeManager;
 
 public class QuarkusJaegerTracer implements Tracer {
 
-    static final String LOG_TRACE_CONTEXT = "JAEGER_LOG_TRACE_CONTEXT";
     private static volatile Tracer tracer;
 
     @Override
@@ -23,23 +37,11 @@ public class QuarkusJaegerTracer implements Tracer {
             synchronized (QuarkusJaegerTracer.class) {
                 if (tracer == null) {
                     tracer = Configuration.fromEnv()
-                            .withMetricsFactory(new QuarkusJaegerMetricsFactory())
-                            .getTracerBuilder()
-                            .withScopeManager(getScopeManager())
-                            .build();
+                            .withMetricsFactory(new QuarkusJaegerMetricsFactory()).getTracer();
                 }
             }
         }
         return tracer;
-    }
-
-    private static ScopeManager getScopeManager() {
-        ScopeManager scopeManager = new ThreadLocalScopeManager();
-        String logTraceContext = System.getProperty(LOG_TRACE_CONTEXT);
-        if ("true".equals(logTraceContext)) {
-            scopeManager = new MDCScopeManager(scopeManager);
-        }
-        return scopeManager;
     }
 
     @Override
