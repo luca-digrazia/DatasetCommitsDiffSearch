c47e6bd6331531d7c730cd92250f9e01b54a905d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:11:00 +0200

    Add explicit @Priority to ContainerRequestFilter
        See https://jersey.java.net/documentation/2.14/filters-and-interceptors.html#d0e8671 for details.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/metrics/jersey2/AbstractMetricsFilter.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/metrics/jersey2/AbstractMetricsFilter.java
@@ -1,5 +1,23 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.shared.metrics.jersey2;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -9,6 +27,7 @@ import java.lang.reflect.Method;
 
 import static com.codahale.metrics.MetricRegistry.name;
 
+@Priority(Priorities.HEADER_DECORATOR)
 public abstract class AbstractMetricsFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     protected String chooseName(String explicitName, boolean absolute, Method method, String... suffixes) {
