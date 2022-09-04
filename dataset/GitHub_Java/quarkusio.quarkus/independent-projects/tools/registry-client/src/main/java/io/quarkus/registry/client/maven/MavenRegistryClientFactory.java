e01cc0aa4c497619d9293a28f24bbf4cbceef748
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:52:16 +0200

    Unless configured, derive the registry URL from the registry ID

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/tools/registry-client/src/main/java/io/quarkus/registry/client/maven/MavenRegistryClientFactory.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/tools/registry-client/src/main/java/io/quarkus/registry/client/maven/MavenRegistryClientFactory.java
@@ -88,17 +88,16 @@ public class MavenRegistryClientFactory implements RegistryClientFactory {
         } catch (BootstrapMavenException e) {
             final StringWriter buf = new StringWriter();
             try (BufferedWriter writer = new BufferedWriter(buf)) {
-                writer.write("Failed to resolve Quarkus extension registry descriptor ");
+                writer.write("Failed to resolve Quarkus extensions registry descriptor ");
                 writer.write(registryDescriptorCoords.toString());
-                writer.write(" from");
+                writer.write(" using the following repositories:");
                 for (RemoteRepository repo : aggregatedRepos) {
-                    if (repo.getPolicy(registryDescriptorCoords.isSnapshot()).isEnabled()) {
-                        writer.append(" ");
-                        writer.write(repo.getId());
-                        writer.write(" (");
-                        writer.write(repo.getUrl());
-                        writer.write(")");
-                    }
+                    writer.newLine();
+                    writer.write("- ");
+                    writer.write(repo.getId());
+                    writer.write(" (");
+                    writer.write(repo.getUrl());
+                    writer.write(")");
                 }
             } catch (IOException e1) {
                 buf.append(e.getLocalizedMessage());
@@ -141,14 +140,15 @@ public class MavenRegistryClientFactory implements RegistryClientFactory {
             config = complete;
         }
 
-        final MavenRegistryArtifactResolver defaultResolver = defaultResolver(resolver, cleanupTimestampedArtifacts);
+        MavenRegistryArtifactResolver defaultResolver = null;
         final RegistryNonPlatformExtensionsResolver nonPlatformExtensionsResolver;
         final RegistryNonPlatformExtensionsConfig nonPlatformExtensions = config.getNonPlatformExtensions();
         if (nonPlatformExtensions == null || nonPlatformExtensions.isDisabled()) {
             log.debug("Non-platform extension catalogs were disabled for registry %s", config.getId());
             nonPlatformExtensionsResolver = null;
         } else {
-            nonPlatformExtensionsResolver = new MavenNonPlatformExtensionsResolver(nonPlatformExtensions, defaultResolver, log);
+            nonPlatformExtensionsResolver = new MavenNonPlatformExtensionsResolver(nonPlatformExtensions,
+                    defaultResolver = defaultResolver(resolver, cleanupTimestampedArtifacts), log);
         }
 
         final RegistryPlatformsResolver platformsResolver;
@@ -157,16 +157,21 @@ public class MavenRegistryClientFactory implements RegistryClientFactory {
             log.debug("Platform catalogs were disabled for registry %s", config.getId());
             platformsResolver = null;
         } else {
-            platformsResolver = new MavenPlatformsResolver(platformsConfig, defaultResolver, log);
+            platformsResolver = new MavenPlatformsResolver(platformsConfig,
+                    defaultResolver == null ? defaultResolver = defaultResolver(resolver, cleanupTimestampedArtifacts)
+                            : defaultResolver,
+                    log);
         }
 
         return new RegistryClientDispatcher(config, platformsResolver,
                 Boolean.TRUE.equals(config.getPlatforms().getExtensionCatalogsIncluded())
-                        ? new MavenPlatformExtensionsResolver(defaultResolver, log)
+                        ? new MavenPlatformExtensionsResolver(
+                                defaultResolver == null ? defaultResolver(resolver, cleanupTimestampedArtifacts)
+                                        : defaultResolver,
+                                log)
                         : new MavenPlatformExtensionsResolver(defaultResolver(originalResolver, cleanupTimestampedArtifacts),
                                 log),
-                nonPlatformExtensionsResolver,
-                new MavenRegistryCache(config, defaultResolver, log));
+                nonPlatformExtensionsResolver);
     }
 
     private static boolean isCleanupTimestampedArtifacts(RegistryConfig config) {
--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/tools/registry-client/src/main/java/io/quarkus/registry/config/RegistriesConfigLocator.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/tools/registry-client/src/main/java/io/quarkus/registry/config/RegistriesConfigLocator.java
@@ -12,6 +12,7 @@ import io.quarkus.registry.config.json.JsonRegistryPlatformsConfig;
 import io.quarkus.registry.config.json.RegistriesConfigMapperHelper;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -19,11 +20,32 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
 
+/**
+ * A helper class with utility methods to locate the registry client configuration file
+ * in the default locations (e.g. user home <code>.quarkus</code> dir, or the project dir) or in
+ * at the location specified by the caller.
+ * Also includes methods to parse the registry client configuration file.
+ */
 public class RegistriesConfigLocator {
 
     public static final String CONFIG_RELATIVE_PATH = ".quarkus/config.yaml";
     public static final String CONFIG_FILE_PATH_PROPERTY = "qer.config";
 
+    /**
+     * Locate the registry client configuration file and deserialize it.
+     * The method will be looking for the file in the following locations in this order:
+     * <ol>
+     * <li>if <code>qer.config</code> system property is set, its value will be used as the location of the configuration
+     * file</li>
+     * <li>current user directory (which usually would be the project dir)</li>
+     * <li><code>.quarkus/config.yaml</code> in the user home directory
+     * </ol>
+     *
+     * Given that the presence of the configuration file is optional, if the configuration file couldn't be located,
+     * an empty configuration would be returned to the caller.
+     *
+     * @return registry client configuration, never null
+     */
     public static RegistriesConfig resolveConfig() {
         final Path configYaml = locateConfigYaml();
         if (configYaml == null) {
@@ -32,6 +54,12 @@ public class RegistriesConfigLocator {
         return load(configYaml);
     }
 
+    /**
+     * Deserializes a given configuration file.
+     *
+     * @param configYaml configuration file
+     * @return deserialized registry client configuration
+     */
     public static RegistriesConfig load(Path configYaml) {
         try {
             return completeRequiredConfig(RegistriesConfigMapperHelper.deserialize(configYaml, JsonRegistriesConfig.class));
@@ -40,6 +68,12 @@ public class RegistriesConfigLocator {
         }
     }
 
+    /**
+     * Deserializes registry client configuration from an input stream.
+     *
+     * @param configYaml input stream
+     * @return deserialized registry client configuration
+     */
     public static RegistriesConfig load(InputStream configYaml) {
         try {
             return completeRequiredConfig(RegistriesConfigMapperHelper.deserializeYaml(configYaml, JsonRegistriesConfig.class));
@@ -48,7 +82,21 @@ public class RegistriesConfigLocator {
         }
     }
 
-    private static Path locateConfigYaml() {
+    /**
+     * Deserializes registry client configuration from a reader.
+     *
+     * @param configYaml reader
+     * @return deserialized registry client configuration
+     */
+    public static RegistriesConfig load(Reader configYaml) {
+        try {
+            return completeRequiredConfig(RegistriesConfigMapperHelper.deserializeYaml(configYaml, JsonRegistriesConfig.class));
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to parse config file " + configYaml, e);
+        }
+    }
+
+    public static Path locateConfigYaml() {
         final String prop = PropertiesUtil.getProperty(CONFIG_FILE_PATH_PROPERTY);
         Path configYaml;
         if (prop != null) {
@@ -93,8 +141,10 @@ public class RegistriesConfigLocator {
         final JsonRegistryConfig config = new JsonRegistryConfig(id);
         config.setUpdatePolicy(original.getUpdatePolicy());
         config.setDescriptor(completeDescriptor(original));
-        config.setMaven(completeRequiedMavenConfig(original));
         if (original != null) {
+            if (original.getMaven() != null) {
+                config.setMaven(original.getMaven());
+            }
             if (original.getNonPlatformExtensions() != null) {
                 config.setNonPlatformExtensions(original.getNonPlatformExtensions());
             }
@@ -104,18 +154,6 @@ public class RegistriesConfigLocator {
             if (!original.getExtra().isEmpty()) {
                 config.setExtra(original.getExtra());
             }
-        }
-        return config;
-    }
-
-    private static RegistryMavenConfig completeRequiedMavenConfig(RegistryConfig original) {
-        RegistryMavenConfig originalMaven = original.getMaven();
-        if (hasRequiredConfig(originalMaven)) {
-            return originalMaven;
-        }
-        final JsonRegistryMavenConfig config = new JsonRegistryMavenConfig();
-        if (originalMaven != null) {
-            config.setRepository(originalMaven.getRepository());
         }
         return config;
     }
@@ -162,16 +200,15 @@ public class RegistriesConfigLocator {
         if (qerConfig.getDescriptor() == null) {
             return false;
         }
-        return hasRequiredConfig(qerConfig.getMaven());
-    }
-
-    private static boolean hasRequiredConfig(RegistryMavenConfig maven) {
-        if (maven == null) {
-            return false;
-        }
         return true;
     }
 
+    /**
+     * Returns the default registry client configuration which should be used in case
+     * no configuration file was found in the user's environment.
+     *
+     * @return default registry client configuration
+     */
     public static RegistryConfig getDefaultRegistry() {
         final JsonRegistryConfig qer = new JsonRegistryConfig();
         qer.setId(Constants.DEFAULT_REGISTRY_ID);
