eab89c1b884a7e148beda65181bd24c7b6f8c2f0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:08:39 +0200

    Arc should only emit once log msg per missing class in index.

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/IndexClassLookupUtils.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/IndexClassLookupUtils.java
@@ -1,12 +1,10 @@
 package io.quarkus.arc.processor;
 
+import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.IndexView;
-import org.jboss.jandex.Type;
-import org.jboss.jandex.Type.Kind;
 import org.jboss.logging.Logger;
 
 final class IndexClassLookupUtils {
@@ -14,29 +12,23 @@ final class IndexClassLookupUtils {
     private static final Logger LOGGER = Logger.getLogger(IndexClassLookupUtils.class);
 
     // set of already encountered and logged DotNames that are missing in the index
-    private static final Set<DotName> alreadyKnown = ConcurrentHashMap.newKeySet();
+    private static Set<DotName> alreadyKnown = new HashSet<>();
 
     private IndexClassLookupUtils() {
     }
 
-    /**
-     * 
-     * @param index
-     * @param type
-     * @return the class for the given type or {@code null} for primitives, arrays and
-     */
-    static ClassInfo getClassByName(IndexView index, Type type) {
-        if (type != null && (type.kind() == Kind.CLASS || type.kind() == Kind.PARAMETERIZED_TYPE)) {
-            return getClassByName(index, type.name());
-        }
-        return null;
-    }
-
     static ClassInfo getClassByName(IndexView index, DotName dotName) {
-        return getClassByName(index, dotName, true);
+        return lookupClassInIndex(index, dotName, true);
     }
 
-    static ClassInfo getClassByName(IndexView index, DotName dotName, boolean withLogging) {
+    /**
+     * Used by {@code BeanArchives.IndexWrapper#getClassByName()} while gathering additional classes for indexing
+     */
+    static ClassInfo getClassByNameNoLogging(IndexView index, DotName dotName) {
+        return lookupClassInIndex(index, dotName, false);
+    }
+
+    private static ClassInfo lookupClassInIndex(IndexView index, DotName dotName, boolean withLogging) {
         if (dotName == null) {
             throw new IllegalArgumentException("Cannot lookup class, provided DotName was null.");
         }
@@ -46,8 +38,8 @@ final class IndexClassLookupUtils {
         ClassInfo info = index.getClassByName(dotName);
         if (info == null && withLogging && !alreadyKnown.contains(dotName)) {
             // class not in index, log info as this may cause the application to blow up or behave weirdly
-            LOGGER.infof("Class for name: %s was not found in Jandex index. Please ensure the class " +
-                    "is part of the index.", dotName);
+            LOGGER.info("Class for name: " + dotName + " was not found in Jandex index. Please ensure the class " +
+                    "is part of the index.");
             alreadyKnown.add(dotName);
         }
         return info;
