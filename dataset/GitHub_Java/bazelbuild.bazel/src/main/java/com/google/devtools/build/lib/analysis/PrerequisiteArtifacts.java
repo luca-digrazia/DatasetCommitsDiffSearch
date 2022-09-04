4606bb22cbb4ae4a238f48b65bf5cfe5ec153fd9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 20:36:16 +0200

    Add fast paths to PrerequisiteArtifacts.get(). Most often,     ruleContext.getPrerequisites() returns 0 or 1 FileProvider instance. In these     cases, there is no need to ever create a HashSet for deduplication.
        RELNOTES: None.
        PiperOrigin-RevId: 231784353

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/PrerequisiteArtifacts.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/PrerequisiteArtifacts.java
@@ -55,7 +55,9 @@ public final class PrerequisiteArtifacts {
     // expanded NestedSet.
     if (prerequisites.size() == 1) {
       return new PrerequisiteArtifacts(
-          ruleContext, attributeName, prerequisites.get(0).getFilesToBuild().toList());
+          ruleContext,
+          attributeName,
+          ImmutableList.copyOf(prerequisites.get(0).getFilesToBuild().toList()));
     }
     Set<Artifact> result = new LinkedHashSet<>();
     for (FileProvider target : prerequisites) {
