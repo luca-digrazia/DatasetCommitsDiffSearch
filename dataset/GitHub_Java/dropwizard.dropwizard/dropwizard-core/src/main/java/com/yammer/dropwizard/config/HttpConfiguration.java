ae384173d7c1c08fe87ecdda2e4a58ba30652ad1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:02:43 +0200

    Fix minThread/maxThread defaults to Jetty's.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/HttpConfiguration.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/HttpConfiguration.java
@@ -64,7 +64,7 @@ public class HttpConfiguration {
 
     @NotNull
     @JsonProperty
-    private Duration maxIdleTime = Duration.seconds(200);
+    private Duration maxIdleTime = Duration.seconds(1);
 
     @Min(1)
     @Max(128)
