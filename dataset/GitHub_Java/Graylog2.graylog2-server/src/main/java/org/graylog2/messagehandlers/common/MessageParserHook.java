c91559122566c66f0e0c32e74c8459747aae67aa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:23:26 +0200

    Changed rule filename

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/messagehandlers/common/MessageParserHook.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/messagehandlers/common/MessageParserHook.java
@@ -20,7 +20,15 @@
 
 package org.graylog2.messagehandlers.common;
 
-import org.graylog2.Main;
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
 import org.graylog2.messagehandlers.gelf.GELFMessage;
 
 /**
@@ -39,6 +47,31 @@ public class MessageParserHook implements MessagePreReceiveHookIF {
 		/**
 		 * Run GELFMessage through the rules engine
 		 */
-    	Main.drools.evaluate(message);
+    	
+		try {
+			// load up the knowledge base
+			KnowledgeBase kbase = readKnowledgeBase();
+			StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+			ksession.insert(message);
+			ksession.fireAllRules();
+			ksession.dispose();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
     }
+    
+	private static KnowledgeBase readKnowledgeBase() throws Exception {
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newFileResource("/etc/graylog2.d/rules/graylog2.drl"), ResourceType.DRL);
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error: errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException("Could not parse knowledge.");
+		}
+		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+		return kbase;
+	}
 }
