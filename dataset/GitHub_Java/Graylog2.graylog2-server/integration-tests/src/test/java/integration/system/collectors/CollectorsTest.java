3444058eee787cedb180579db7363fd8a0313e98
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:49:26 +0200

    Disabling single test case for now.
        The cause for the test failure does not seem to be fixable straight
        away, so the test case is disabled until the corresponding ticket is
        closed.

--- dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/system/collectors/CollectorsTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/system/collectors/CollectorsTest.java
@@ -20,7 +20,6 @@ import integration.BaseRestTest;
 import integration.RequiresAuthentication;
 import integration.RequiresVersion;
 import org.joda.time.DateTime;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -67,8 +66,8 @@ public class CollectorsTest extends BaseRestTest {
                     .assertThat().body("collectors", notNullValue());
     }
 
-    @Ignore("TODO: reenable after #1273 has been fixed.")
-    @Test
+    // TODO: reenable after #1273 has been fixed.
+    //@Test
     @RequiresAuthentication
     public void testGetCollector() throws Exception {
         given().when()
