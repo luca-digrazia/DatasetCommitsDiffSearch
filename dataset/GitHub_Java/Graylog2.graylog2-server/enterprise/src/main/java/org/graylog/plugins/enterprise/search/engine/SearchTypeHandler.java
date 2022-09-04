35138291e0be13b19a8a5f73efa41f5bc35b1477
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:43:22 +0200

    add explanation for type cast

--- dataset/GitHub_Java/Graylog2.graylog2-server/enterprise/src/main/java/org/graylog/plugins/enterprise/search/engine/SearchTypeHandler.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/enterprise/src/main/java/org/graylog/plugins/enterprise/search/engine/SearchTypeHandler.java
@@ -1,7 +1,5 @@
 package org.graylog.plugins.enterprise.search.engine;
 
-import org.graylog.plugins.enterprise.search.Query;
-import org.graylog.plugins.enterprise.search.SearchJob;
 import org.graylog.plugins.enterprise.search.SearchType;
 
 /**
@@ -13,19 +11,19 @@ import org.graylog.plugins.enterprise.search.SearchType;
 @SuppressWarnings("unchecked")
 public interface SearchTypeHandler<S extends SearchType, Q, R> {
 
-    default void generateQueryPart(SearchJob job, Query query, SearchType searchType, Q queryBuilder) {
+    default void generateQueryPart(SearchType searchType, Q queryBuilder) {
         // We need to typecast manually here, because '? extends SearchType' and 'SearchType' are never compatible
         // and thus the compiler won't accept the types at their call sites
         // This allows us to get proper types in the implementing classes instead of having to cast there.
-        doGenerateQueryPart(job, query, (S) searchType, queryBuilder);
+        doGenerateQueryPart((S) searchType, queryBuilder);
     }
 
-    void doGenerateQueryPart(SearchJob job, Query query, S searchType, Q queryBuilder);
+    void doGenerateQueryPart(S searchType, Q queryBuilder);
 
-    default SearchType.Result extractResult(SearchJob job, Query query,SearchType searchType, R queryResult) {
+    default SearchType.Result extractResult(SearchType searchType, R queryResult) {
         // see above for the reason for typecasting
-        return doExtractResult(job, query, (S) searchType, queryResult);
+        return doExtractResult((S) searchType, queryResult);
     }
 
-    SearchType.Result doExtractResult(SearchJob job, Query query, S searchType, R queryResult);
+    SearchType.Result doExtractResult(S searchType, R queryResult);
 }
