2b9759ab5dfa379c6476f33566c4fd2e2f675ffe
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:12:11 +0200

    Hide api-browser and api-docs resources on cloud
        Fixes Graylog2/graylog-plugin-cloud#91

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/shared/rest/resources/documentation/DocumentationResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/shared/rest/resources/documentation/DocumentationResource.java
@@ -1,34 +1,37 @@
-/**
- * This file is part of Graylog.
+/*
+ * Copyright (C) 2020 Graylog, Inc.
  *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
  *
- * Graylog is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package org.graylog2.shared.rest.resources.documentation;
 
 import com.codahale.metrics.annotation.Timed;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableSet;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.graylog2.plugin.BaseConfiguration;
+import org.graylog2.configuration.HttpConfiguration;
+import org.graylog2.plugin.inject.RestControllerPackage;
 import org.graylog2.plugin.rest.PluginRestResource;
 import org.graylog2.rest.RestTools;
 import org.graylog2.shared.plugins.PluginRestResourceClasses;
+import org.graylog2.shared.rest.HideOnCloud;
 import org.graylog2.shared.rest.documentation.generator.Generator;
 import org.graylog2.shared.rest.resources.RestResource;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -37,40 +40,48 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import static java.util.Objects.requireNonNull;
 import static org.graylog2.shared.initializers.JerseyService.PLUGIN_PREFIX;
 
 @Api(value = "Documentation", description = "Documentation of this API in JSON format.")
 @Path("/api-docs")
+@HideOnCloud
 public class DocumentationResource extends RestResource {
 
-    private BaseConfiguration configuration;
-    private final Set<String> restControllerPackages = new HashSet<>();
-    private final Map<Class<?>, String> pluginRestControllerMapping = new HashMap<>();
+    private final Generator generator;
+    private final HttpConfiguration httpConfiguration;
 
     @Inject
-    public DocumentationResource(BaseConfiguration configuration,
-                                 @Named("RestControllerPackages") String[] restControllerPackages,
+    public DocumentationResource(HttpConfiguration httpConfiguration,
+                                 Set<RestControllerPackage> restControllerPackages,
+                                 ObjectMapper objectMapper,
                                  PluginRestResourceClasses pluginRestResourceClasses) {
 
-        this.configuration = configuration;
+        this.httpConfiguration = requireNonNull(httpConfiguration, "httpConfiguration");
 
-        this.restControllerPackages.addAll(Arrays.asList(restControllerPackages));
+        final ImmutableSet.Builder<String> packageNames = ImmutableSet.<String>builder()
+                .addAll(restControllerPackages.stream()
+                        .map(RestControllerPackage::name)
+                        .collect(Collectors.toList()));
 
         // All plugin resources get the plugin prefix + the plugin package.
+        final Map<Class<?>, String> pluginRestControllerMapping = new HashMap<>();
         for (Map.Entry<String, Set<Class<? extends PluginRestResource>>> entry : pluginRestResourceClasses.getMap().entrySet()) {
             final String pluginPackage = entry.getKey();
-            this.restControllerPackages.add(pluginPackage);
+            packageNames.add(pluginPackage);
 
             for (Class<? extends PluginRestResource> pluginRestResource : entry.getValue()) {
-                this.pluginRestControllerMapping.put(pluginRestResource, pluginPackage);
+                pluginRestControllerMapping.put(pluginRestResource, pluginPackage);
             }
         }
+
+        this.generator = new Generator(packageNames.build(), pluginRestControllerMapping, PLUGIN_PREFIX, objectMapper);
     }
 
     @GET
@@ -78,7 +89,16 @@ public class DocumentationResource extends RestResource {
     @ApiOperation(value = "Get API documentation")
     @Produces(MediaType.APPLICATION_JSON)
     public Response overview() {
-        return buildSuccessfulCORSResponse(new Generator(restControllerPackages, pluginRestControllerMapping, PLUGIN_PREFIX, objectMapper).generateOverview());
+        return buildSuccessfulCORSResponse(generator.generateOverview());
+    }
+
+    @GET
+    @Timed
+    @ApiOperation(value = "Get API documentation with cluster global URI path")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/global")
+    public Response globalOverview() {
+        return buildSuccessfulCORSResponse(generator.generateOverview());
     }
 
     @GET
@@ -89,10 +109,16 @@ public class DocumentationResource extends RestResource {
     public Response route(@ApiParam(name = "route", value = "Route to fetch. For example /system", required = true)
                           @PathParam("route") String route,
                           @Context HttpHeaders httpHeaders) {
-        final String basePath = RestTools.buildEndpointUri(httpHeaders, configuration.getWebEndpointUri());
-        return buildSuccessfulCORSResponse(
-                new Generator(restControllerPackages, pluginRestControllerMapping, PLUGIN_PREFIX, objectMapper).generateForRoute(route, basePath)
-        );
+        // If the documentation was requested from "cluster global mode", use the HttpExternalUri for the baseUri.
+        // Otherwise use the per node HttpPublishUri.
+        URI baseUri;
+        if (route.startsWith("global")) {
+            route = route.replace("global", "");
+            baseUri = RestTools.buildExternalUri(httpHeaders.getRequestHeaders(), httpConfiguration.getHttpExternalUri()).resolve(HttpConfiguration.PATH_API);
+        } else {
+            baseUri = httpConfiguration.getHttpPublishUri().resolve(HttpConfiguration.PATH_API);
+        }
+        return buildSuccessfulCORSResponse(generator.generateForRoute(route, baseUri.toString()));
     }
 
     private Response buildSuccessfulCORSResponse(Map<String, Object> result) {
