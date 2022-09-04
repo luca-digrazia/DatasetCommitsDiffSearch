d7a2bce7b3d822dee52d97250c1fac1d7a940a3e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:29:11 +0200

    Implemented Resource injection with CodeModel

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/src/main/java/com/googlecode/androidannotations/processing/ActivityHolder.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/src/main/java/com/googlecode/androidannotations/processing/ActivityHolder.java
@@ -1,5 +1,7 @@
 package com.googlecode.androidannotations.processing;
 
+import com.sun.codemodel.JBlock;
+import com.sun.codemodel.JClass;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JVar;
@@ -9,5 +11,11 @@ public class ActivityHolder {
 	public JDefinedClass activity;
 	public JMethod beforeSetContentView;
 	public JVar beforeSetContentViewSavedInstanceStateParam;
+	public JMethod afterSetContentView;
+	public JBlock extrasNotNullBlock;
+	public JVar extras;
+	public JClass bundleClass;
+    public JVar resources;
+	
 
 }
