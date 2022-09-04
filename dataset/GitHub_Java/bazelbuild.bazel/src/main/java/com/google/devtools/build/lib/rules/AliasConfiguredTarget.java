0e9cdb53c83ebe4bc336d5121de2893fd182b9f2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 00:52:36 +0200

    Remove the functionality from alias() to point nowhere.
        It's never used and was broken anyway (see #getLabel()).
    
        The functionality has been laying there unused since the initial checkin in unknown commit.
    
        RELNOTES: None.
        PiperOrigin-RevId: 161378717

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/AliasConfiguredTarget.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/AliasConfiguredTarget.java
@@ -140,6 +140,11 @@ public final class AliasConfiguredTarget implements ConfiguredTarget, ClassObjec
   }
 
   @Override
+  public boolean isImmutable() {
+    return false;
+  }
+
+  @Override
   public void repr(SkylarkPrinter printer) {
     printer.append("<alias target " + label + " of " + actual.getLabel() + ">");
   }
