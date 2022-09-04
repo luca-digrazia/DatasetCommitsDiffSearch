f8c942ea0d183f7a3ab1184e303fa371df55647d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:59:26 +0200

    Remove unnecessary 'static' qualifiers from enums

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/searches/Searches.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/searches/Searches.java
@@ -67,7 +67,7 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
-import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
+import static org.elasticsearch.index.query.QueryBuilders.queryString;
 
 @Singleton
 public class Searches {
@@ -414,7 +414,7 @@ public class Searches {
                                 .interval(interval.toESInterval()))
                 .filter(standardFilters(range, filter));
 
-        QueryStringQueryBuilder qs = queryStringQuery(query);
+        QueryStringQueryBuilder qs = queryString(query);
         qs.allowLeadingWildcard(configuration.isAllowLeadingWildcardSearches());
 
         SearchRequestBuilder srb = c.prepareSearch();
@@ -445,7 +445,7 @@ public class Searches {
                 )
                 .filter(standardFilters(range, filter));
 
-        QueryStringQueryBuilder qs = queryStringQuery(query);
+        QueryStringQueryBuilder qs = queryString(query);
         qs.allowLeadingWildcard(configuration.isAllowLeadingWildcardSearches());
 
         SearchRequestBuilder srb = c.prepareSearch();
@@ -535,7 +535,7 @@ public class Searches {
         if (query.trim().equals("*")) {
             srb.setQuery(matchAllQuery());
         } else {
-            QueryStringQueryBuilder qs = queryStringQuery(query);
+            QueryStringQueryBuilder qs = queryString(query);
             qs.allowLeadingWildcard(configuration.isAllowLeadingWildcardSearches());
             srb.setQuery(qs);
         }
@@ -606,7 +606,7 @@ public class Searches {
         }
 
         if (filter != null && !filter.isEmpty() && !filter.equals("*")) {
-            bfb.must(FilterBuilders.queryFilter(QueryBuilders.queryStringQuery(filter)));
+            bfb.must(FilterBuilders.queryFilter(QueryBuilders.queryString(filter)));
             set = true;
         }
 
