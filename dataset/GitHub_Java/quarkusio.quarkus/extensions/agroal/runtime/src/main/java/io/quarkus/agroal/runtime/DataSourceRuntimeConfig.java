d374bba1d7f7062b3021ee525e2052cd9470de01
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:41:39 +0200

    Document how to disable background validation of JDBC connections

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/DataSourceRuntimeConfig.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/DataSourceRuntimeConfig.java
@@ -29,26 +29,7 @@ public class DataSourceRuntimeConfig {
     public Optional<String> password;
 
     /**
-     * The credentials provider name
-     */
-    @ConfigItem
-    public Optional<String> credentialsProvider;
-
-    /**
-     * The credentials provider type.
-     * <p>
-     * It is the {@code &#64;Named} value of the credentials provider bean. It is used to discriminate if multiple
-     * CredentialsProvider beans are available.
-     * <p>
-     * For Vault it is: vault-credentials-provider. Not necessary if there is only one credentials provider available.
-     */
-    @ConfigItem
-    public Optional<String> credentialsProviderType;
-
-    /**
-     * The initial size of the pool. Usually you will want to set the initial size to match at least the
-     * minimal size, but this is not enforced so to allow for architectures which prefer a lazy initialization
-     * of the connections on boot, while being able to sustain a minimal pool size after boot.
+     * The initial size of the pool
      */
     @ConfigItem
     public Optional<Integer> initialSize;
@@ -56,7 +37,7 @@ public class DataSourceRuntimeConfig {
     /**
      * The datasource pool minimum size
      */
-    @ConfigItem(defaultValue = "0")
+    @ConfigItem(defaultValue = "5")
     public int minSize;
 
     /**
@@ -108,20 +89,4 @@ public class DataSourceRuntimeConfig {
      */
     @ConfigItem
     public boolean enableMetrics;
-
-    /**
-     * When enabled Agroal will be able to produce a warning when a connection is returned
-     * to the pool without the application having closed all open statements.
-     * This is unrelated with tracking of open connections.
-     * Disable for peak performance, but only when there's high confidence that
-     * no leaks are happening.
-     */
-    @ConfigItem(defaultValue = "true")
-    public boolean detectStatementLeaks;
-
-    /**
-     * Query executed when first using a connection.
-     */
-    @ConfigItem
-    public Optional<String> newConnectionSql;
 }
