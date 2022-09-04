3bd3dda9651d95e578597721f4f1dc3a01006cde
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:32:31 +0200

    fix(quartz): make sure that job identity is unique and deterministic across several builds
        Fix #8661 - by making sure that the sequence is unique for each invoker class i.e a scheduled methods.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/quartz/runtime/src/main/java/io/quarkus/quartz/runtime/QuartzScheduler.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/quartz/runtime/src/main/java/io/quarkus/quartz/runtime/QuartzScheduler.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.PreDestroy;
 import javax.annotation.Priority;
@@ -88,15 +89,12 @@ public class QuartzScheduler implements Scheduler {
                 for (ScheduledMethodMetadata method : context.getScheduledMethods()) {
 
                     invokers.put(method.getInvokerClassName(), context.createInvoker(method.getInvokerClassName()));
-                    int nameSequence = 0;
+                    AtomicInteger triggerNameSequence = new AtomicInteger();
 
                     for (Scheduled scheduled : method.getSchedules()) {
-                        String identity = scheduled.identity().trim();
-                        if (identity.isEmpty()) {
-                            identity = ++nameSequence + "_" + method.getInvokerClassName();
-                        }
+                        String name = triggerNameSequence.getAndIncrement() + "_" + method.getInvokerClassName();
                         JobBuilder jobBuilder = JobBuilder.newJob(InvokerJob.class)
-                                .withIdentity(identity, Scheduler.class.getName())
+                                .withIdentity(name, Scheduler.class.getName())
                                 .usingJobData(INVOKER_KEY, method.getInvokerClassName())
                                 .requestRecovery();
                         ScheduleBuilder<?> scheduleBuilder;
@@ -131,7 +129,7 @@ public class QuartzScheduler implements Scheduler {
                         }
 
                         TriggerBuilder<?> triggerBuilder = TriggerBuilder.newTrigger()
-                                .withIdentity(identity + "_trigger", Scheduler.class.getName())
+                                .withIdentity(name + "_trigger", Scheduler.class.getName())
                                 .withSchedule(scheduleBuilder);
 
                         Long millisToAdd = null;
