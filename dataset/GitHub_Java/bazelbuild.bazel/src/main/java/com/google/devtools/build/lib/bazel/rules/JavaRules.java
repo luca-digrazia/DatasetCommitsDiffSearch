e72ac1113f7594aeacce08ad9ff965bf0c4c2f44
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:11:11 +0200

    Expose information in JavaPluginInfo to Starlark and implement its constructor.
        PiperOrigin-RevId: 372103861

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/JavaRules.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/JavaRules.java
@@ -14,7 +14,7 @@
 package com.google.devtools.build.lib.bazel.rules;
 
 import com.google.common.collect.ImmutableList;
-import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider.Builder;
+import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider.RuleSet;
 import com.google.devtools.build.lib.bazel.rules.java.BazelJavaBinaryRule;
 import com.google.devtools.build.lib.bazel.rules.java.BazelJavaBuildInfoFactory;
@@ -24,23 +24,29 @@ import com.google.devtools.build.lib.bazel.rules.java.BazelJavaPluginRule;
 import com.google.devtools.build.lib.bazel.rules.java.BazelJavaRuleClasses;
 import com.google.devtools.build.lib.bazel.rules.java.BazelJavaSemantics;
 import com.google.devtools.build.lib.bazel.rules.java.BazelJavaTestRule;
+import com.google.devtools.build.lib.bazel.rules.java.BazelJavaToolchain;
 import com.google.devtools.build.lib.rules.core.CoreRules;
 import com.google.devtools.build.lib.rules.extra.ActionListenerRule;
 import com.google.devtools.build.lib.rules.extra.ExtraActionRule;
-import com.google.devtools.build.lib.rules.java.JavaConfigurationLoader;
+import com.google.devtools.build.lib.rules.java.JavaCcLinkParamsProvider;
+import com.google.devtools.build.lib.rules.java.JavaConfiguration;
 import com.google.devtools.build.lib.rules.java.JavaImportBaseRule;
 import com.google.devtools.build.lib.rules.java.JavaInfo;
 import com.google.devtools.build.lib.rules.java.JavaOptions;
 import com.google.devtools.build.lib.rules.java.JavaPackageConfigurationRule;
+import com.google.devtools.build.lib.rules.java.JavaPluginInfo;
 import com.google.devtools.build.lib.rules.java.JavaRuleClasses.IjarBaseRule;
-import com.google.devtools.build.lib.rules.java.JavaRuntimeAlias;
+import com.google.devtools.build.lib.rules.java.JavaRuleClasses.JavaRuntimeBaseRule;
+import com.google.devtools.build.lib.rules.java.JavaRuleClasses.JavaToolchainBaseRule;
+import com.google.devtools.build.lib.rules.java.JavaRuntimeAliasRule;
 import com.google.devtools.build.lib.rules.java.JavaRuntimeRule;
-import com.google.devtools.build.lib.rules.java.JavaRuntimeSuiteRule;
-import com.google.devtools.build.lib.rules.java.JavaSkylarkCommon;
-import com.google.devtools.build.lib.rules.java.JavaToolchainAlias;
+import com.google.devtools.build.lib.rules.java.JavaStarlarkCommon;
+import com.google.devtools.build.lib.rules.java.JavaToolchainAliasRule;
 import com.google.devtools.build.lib.rules.java.JavaToolchainRule;
 import com.google.devtools.build.lib.rules.java.ProguardLibraryRule;
-import com.google.devtools.build.lib.rules.java.proto.JavaProtoSkylarkCommon;
+import com.google.devtools.build.lib.rules.java.ProguardSpecProvider;
+import com.google.devtools.build.lib.rules.java.proto.JavaProtoStarlarkCommon;
+import com.google.devtools.build.lib.starlarkbuildapi.java.JavaBootstrap;
 import com.google.devtools.build.lib.util.ResourceFileLoader;
 import java.io.IOException;
 
@@ -55,14 +61,16 @@ public class JavaRules implements RuleSet {
   }
 
   @Override
-  public void init(Builder builder) {
+  public void init(ConfiguredRuleClassProvider.Builder builder) {
     builder.addConfigurationOptions(JavaOptions.class);
-    builder.addConfigurationFragment(new JavaConfigurationLoader());
+    builder.addConfigurationFragment(JavaConfiguration.class);
 
     builder.addBuildInfoFactory(new BazelJavaBuildInfoFactory());
 
     builder.addRuleDefinition(new BazelJavaRuleClasses.BaseJavaBinaryRule());
     builder.addRuleDefinition(new IjarBaseRule());
+    builder.addRuleDefinition(new JavaToolchainBaseRule());
+    builder.addRuleDefinition(new JavaRuntimeBaseRule());
     builder.addRuleDefinition(new BazelJavaRuleClasses.JavaBaseRule());
     builder.addRuleDefinition(new ProguardLibraryRule());
     builder.addRuleDefinition(new JavaImportBaseRule());
@@ -72,24 +80,29 @@ public class JavaRules implements RuleSet {
     builder.addRuleDefinition(new BazelJavaImportRule());
     builder.addRuleDefinition(new BazelJavaTestRule());
     builder.addRuleDefinition(new BazelJavaPluginRule());
-    builder.addRuleDefinition(new JavaToolchainRule());
+    builder.addRuleDefinition(JavaToolchainRule.create(BazelJavaToolchain.class));
     builder.addRuleDefinition(new JavaPackageConfigurationRule());
     builder.addRuleDefinition(new JavaRuntimeRule());
-    builder.addRuleDefinition(new JavaRuntimeSuiteRule());
-    builder.addRuleDefinition(new JavaRuntimeAlias.JavaRuntimeAliasRule());
-    builder.addRuleDefinition(new JavaToolchainAlias.JavaToolchainAliasRule());
+    builder.addRuleDefinition(new JavaRuntimeAliasRule());
+    builder.addRuleDefinition(new JavaToolchainAliasRule());
 
     builder.addRuleDefinition(new ExtraActionRule());
     builder.addRuleDefinition(new ActionListenerRule());
 
-    builder.addSkylarkAccessibleTopLevels("java_common",
-        new JavaSkylarkCommon(BazelJavaSemantics.INSTANCE));
-    builder.addSkylarkAccessibleTopLevels("JavaInfo", JavaInfo.PROVIDER);
-    builder.addSkylarkAccessibleTopLevels("java_proto_common", JavaProtoSkylarkCommon.class);
+    builder.addStarlarkBootstrap(
+        new JavaBootstrap(
+            new JavaStarlarkCommon(BazelJavaSemantics.INSTANCE),
+            JavaInfo.PROVIDER,
+            JavaPluginInfo.PROVIDER,
+            new JavaProtoStarlarkCommon(),
+            JavaCcLinkParamsProvider.PROVIDER,
+            ProguardSpecProvider.PROVIDER));
 
     try {
-      builder.addWorkspaceFilePrefix(
+      builder.addWorkspaceFileSuffix(
           ResourceFileLoader.loadResource(BazelJavaRuleClasses.class, "jdk.WORKSPACE"));
+      builder.addWorkspaceFileSuffix(
+          ResourceFileLoader.loadResource(JavaRules.class, "coverage.WORKSPACE"));
     } catch (IOException e) {
       throw new IllegalStateException(e);
     }
