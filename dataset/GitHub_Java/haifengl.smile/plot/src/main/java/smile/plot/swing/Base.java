faf0ee2becdc6dd0c801a8dc358b31e23a592f90
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:26:01 +0200

    add Base.setBound

--- dataset/GitHub_Java/haifengl.smile/plot/src/main/java/smile/plot/swing/Base.java
+++ dataset/GitHub_Java/haifengl.smile/plot/src/main/java/smile/plot/swing/Base.java
@@ -34,7 +34,7 @@ public class Base {
     /**
      * The dimensionality of base. Should be 2 or 3.
      */
-    final int dimension;
+    int dimension;
     /**
      * Coordinates of base.
      */
