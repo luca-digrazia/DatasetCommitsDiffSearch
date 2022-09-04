807d75f2edfe864cb828c8b1a9132c4029d6e314
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:53:31 +0200

    Flipping --experimental_skyframe_cpu_heavy_skykeys_thread_pool_size for Bazel.
        This enables 2 separate thread pools for the loading/analysis phase, which resulted in performance gains internally.
    
        RELNOTES: None
        PiperOrigin-RevId: 385962748

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/AnalysisOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/AnalysisOptions.java
@@ -14,10 +14,12 @@
 
 package com.google.devtools.build.lib.analysis;
 
+import com.google.devtools.build.lib.util.CpuResourceConverter;
 import com.google.devtools.build.lib.util.RegexFilter;
 import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionDocumentationCategory;
 import com.google.devtools.common.options.OptionEffectTag;
+import com.google.devtools.common.options.OptionMetadataTag;
 import com.google.devtools.common.options.OptionsBase;
 
 /**
@@ -62,22 +64,33 @@ public class AnalysisOptions extends OptionsBase {
   public int maxConfigChangesToShow;
 
   @Option(
-    name = "experimental_extra_action_filter",
-    defaultValue = "",
-    converter = RegexFilter.RegexFilterConverter.class,
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "Filters set of targets to schedule extra_actions for."
-  )
+      name = "experimental_keep_config_nodes_on_analysis_discard",
+      defaultValue = "true",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.LOSES_INCREMENTAL_STATE},
+      help =
+          "When discarding the analysis cache due to a change in the build options, whether to"
+              + " keep the 'BuildConfigurationValue' nodes in the Skyframe graph. Keeping them"
+              + " uses additional memory but allows the `config` command to operate on configs"
+              + " from previous builds.")
+  public boolean keepConfigNodes;
+
+  @Option(
+      name = "experimental_extra_action_filter",
+      defaultValue = "",
+      converter = RegexFilter.RegexFilterConverter.class,
+      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      help =
+          "Deprecated in favor of aspects. Filters set of targets to schedule extra_actions for.")
   public RegexFilter extraActionFilter;
 
   @Option(
-    name = "experimental_extra_action_top_level_only",
-    defaultValue = "false",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "Only schedules extra_actions for top level targets."
-  )
+      name = "experimental_extra_action_top_level_only",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      help = "Deprecated in favor of aspects. Only schedules extra_actions for top level targets.")
   public boolean extraActionTopLevelOnly;
 
   @Option(
@@ -110,4 +123,47 @@ public class AnalysisOptions extends OptionsBase {
     help = "Switches analysis preparation to a new code path based on Skyframe."
   )
   public boolean skyframePrepareAnalysis;
+
+  @Option(
+      name = "experimental_strict_conflict_checks",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      metadataTags = OptionMetadataTag.INCOMPATIBLE_CHANGE,
+      effectTags = {OptionEffectTag.BAZEL_INTERNAL_CONFIGURATION},
+      help =
+          "Check for action prefix file path conflicts, regardless of action-specific overrides.")
+  public boolean strictConflictChecks;
+
+  @Option(
+      name = "experimental_skyframe_cpu_heavy_skykeys_thread_pool_size",
+      defaultValue = "HOST_CPUS",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      metadataTags = OptionMetadataTag.EXPERIMENTAL,
+      effectTags = {
+        OptionEffectTag.LOADING_AND_ANALYSIS,
+        OptionEffectTag.BAZEL_INTERNAL_CONFIGURATION
+      },
+      help =
+          "If set to a positive value (e.g. \"HOST_CPUS*1.5\"), Skyframe will run the"
+              + " loading/analysis phase with 2 separate thread pools: 1 with <value> threads"
+              + " (ideally close to HOST_CPUS) reserved for CPU-heavy SkyKeys, and 1 \"standard\""
+              + " thread pool (whose size is controlled by --loading_phase_threads) for the rest.",
+      converter = CpuResourceConverter.class)
+  public int cpuHeavySkyKeysThreadPoolSize;
+
+  @Option(
+      name = "experimental_oom_sensitive_skyfunctions_semaphore_size",
+      defaultValue = "HOST_CPUS",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      metadataTags = OptionMetadataTag.EXPERIMENTAL,
+      effectTags = {
+        OptionEffectTag.LOADING_AND_ANALYSIS,
+        OptionEffectTag.BAZEL_INTERNAL_CONFIGURATION
+      },
+      help =
+          "Sets the size of the semaphore used to prevent SkyFunctions with large peak memory"
+              + " requirement from OOM-ing blaze. A value of 0 indicates that no semaphore should"
+              + " be used. Example value: \"HOST_CPUS*0.5\".",
+      converter = CpuResourceConverter.class)
+  public int oomSensitiveSkyFunctionsSemaphoreSize;
 }
