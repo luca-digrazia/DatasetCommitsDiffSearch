d9e51587704e42eec95e734092df0c7eaca85b46
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:10:34 +0200

    Propagate exceptions on @Background/@UIThread annotated methods
        The main goal of this fix is to avoid hiding exception to
        ExceptionHandler.

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/org/androidannotations/test15/ThreadActivity.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/org/androidannotations/test15/ThreadActivity.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2012 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2013 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -18,13 +18,18 @@ package org.androidannotations.test15;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import android.app.Activity;
+import java.util.concurrent.Semaphore;
 
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.UiThread.Propagation;
+import org.androidannotations.test15.ebean.GenericBean;
+import org.androidannotations.test15.ebean.SomeBean;
 import org.androidannotations.test15.instancestate.MySerializableBean;
+
+import android.app.Activity;
+import android.os.Looper;
 
 @EActivity
 public class ThreadActivity extends Activity {
@@ -37,6 +42,46 @@ public class ThreadActivity extends Activity {
 	@Background
 	void emptyBackgroundMethod() {
 
+	}
+
+	@Background(delay = 1000)
+	void emptyDelayedBackgroundMethod() {
+
+	}
+
+	private void add(List<Integer> list, int i, int delay, Semaphore sem) {
+		try {
+			if (delay > 0) {
+				Thread.sleep(delay);
+			}
+			list.add(i);
+			if (sem != null) {
+				sem.release();
+			}
+		} catch (InterruptedException e) {
+		}
+	}
+
+	@Background
+	void addBackground(List<Integer> list, int i, int delay, Semaphore sem) {
+		add(list, i, delay, sem);
+	}
+
+	@Background(serial = "test")
+	void addSerializedBackground(List<Integer> list, int i, int delay,
+			Semaphore sem) {
+		add(list, i, delay, sem);
+	}
+
+	@Background(id = "to_cancel")
+	void addCancellableBackground(List<Integer> list, int i,
+			int interruptibleDelay) {
+		add(list, i, interruptibleDelay, null);
+	}
+
+	@Background(id = "to_cancel_serial", serial = "test")
+	void addCancellableSerializedBackground(List<Integer> list, int i, int delay) {
+		add(list, i, delay, null);
 	}
 
 	@UiThread
@@ -55,7 +100,14 @@ public class ThreadActivity extends Activity {
 	}
 
 	@Background
-	void genericBackgroundMethod(List<Map<String, List<Set<MySerializableBean[]>>>> param) {
+	void genericBackgroundMethod(
+			List<Map<String, List<Set<MySerializableBean[]>>>> param) {
+
+	}
+
+	@Background
+	void genericBackgroundMethod(
+			Set<? extends GenericBean<? extends SomeBean>> param) {
 
 	}
 
@@ -63,16 +115,38 @@ public class ThreadActivity extends Activity {
 	void emptyUiDelayedMethod() {
 
 	}
-	
-	@UiThread
-	void uiThreadedUsingArrayParamtersMethod(MySerializableBean [] array) {}
+
+	@UiThread(propagation = Propagation.ENQUEUE)
+	void emptUiMethodEnqueue() {
+	}
+
+	@UiThread(propagation = Propagation.REUSE)
+	void emptUiMethodReuse() {
+	}
 
 	@UiThread
-	void uiThreadedUsingArrayParamtersMethod(MySerializableBean [][] array) {}
+	void uiThreadedUsingArrayParamtersMethod(MySerializableBean[] array) {
+	}
+
+	@UiThread
+	void uiThreadedUsingArrayParamtersMethod(MySerializableBean[][] array) {
+	}
 
 	@Background
-	void backgrounddUsingArrayParamtersMethod(MySerializableBean [] array) {}
+	void backgrounddUsingArrayParamtersMethod(MySerializableBean[] array) {
+	}
 
 	@Background
-	void backgroundUsingArrayParamtersMethod(MySerializableBean [][] array) {}
+	void backgroundUsingArrayParamtersMethod(MySerializableBean[][] array) {
+	}
+
+	@Background
+	void backgroundThrowException() {
+		throw new RuntimeException();
+	}
+
+	@UiThread
+	void uiThreadThrowException() {
+		throw new RuntimeException();
+	}
 }
