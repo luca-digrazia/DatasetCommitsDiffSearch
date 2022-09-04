99384dfe6c162c690d8f23f58f7760860cd45a5b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:52:06 +0200

    Perform strict deps enforcement on compilations with attribution errors
        Reporting strict deps errors in addition to attribution errors is helpful
        particularly with code-generating annotation processors that may fail due
        to missing transitive deps. Currently those failures prevent add_dep
        suggestions from being emitted, making the problem harder to diagnose.
    
        The presence of other errors may prevent some SJD issues from being detected,
        but in theory should never lead to spurious SJD errors from being reported,
        so the additional errors should generally be helpful.
    
        PiperOrigin-RevId: 206261677

--- dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/plugins/dependency/DependencyModule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/plugins/dependency/DependencyModule.java
@@ -37,7 +37,6 @@ import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -64,11 +63,6 @@ public final class DependencyModule {
     /** Fail the build when transitive dependencies are used directly. */
     ERROR
   }
-
-  private static final ImmutableSet<String> SJD_EXEMPT_PROCESSORS =
-      ImmutableSet.of(
-          // Relax strict deps for dagger-generated code (b/17979436).
-          "dagger.internal.codegen.ComponentProcessor");
 
   private final StrictJavaDeps strictJavaDeps;
   private final FixTool fixDepsTool;
@@ -338,7 +332,7 @@ public final class DependencyModule {
     private Path outputDepsProtoFile;
     private boolean strictClasspathMode = false;
     private FixMessage fixMessage = new DefaultFixMessage();
-    private final Set<String> exemptGenerators = new LinkedHashSet<>(SJD_EXEMPT_PROCESSORS);
+    private final Set<String> exemptGenerators = new HashSet<>();
 
     private static class DefaultFixMessage implements FixMessage {
       @Override
