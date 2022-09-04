a76f7ec338eb5fd3c53218c149c04e6ab0b70674
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:08:38 +0200

    Add ConfigOverride for random application ports during tests (#3561)
        Problem:
    
        We are doing a lot of integration testing where we need to start the application. We primarily use the config overrides instead of a `test-config.yaml` to do the configuration. Our config files of the test module often only contain the configuration for the random ports:
        ```yaml
        server:
          applicationConnectors:
            - type: http
              port: 0
          adminConnectors:
            - type: http
              port: 0
        ```
    
        It would be nice to be able to use random ports with the `DropwizardAppRule`/`DropwizardAppExtension` programmatically.
    
        Solution:
    
        Add a Config Override that configures the default application and admin connectors to use port `0`.
    
        ```java
        private static final DropwizardAppExtension<MyConfiguration> EXTENSION =
            new DropwizardAppExtension<>(MyApplication.class,
                // no config file
                null,
                // adds all necessary config overrides for random ports
                randomPorts());
        ```
    
        Closes #3561

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/io/dropwizard/testing/ConfigOverride.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/io/dropwizard/testing/ConfigOverride.java
@@ -1,24 +1,69 @@
 package io.dropwizard.testing;
 
-public class ConfigOverride {
+import java.util.function.Supplier;
 
-    private final String key;
-    private final String value;
+/**
+ * An override for a field in dropwizard configuration intended for use with
+ * {@link io.dropwizard.testing.junit5.DropwizardAppExtension}.
+ * <p>
+ * Given a configuration file containing
+ * <pre>
+ * ---
+ * server:
+ *   applicationConnectors:
+ *     - type: http
+ *       port: 8000
+ *   adminConnectors:
+ *     - type: http
+ *       port: 8001
+ *
+ * logging:
+ *   loggers:
+ *     com.example.foo: INFO
+ * </pre>
+ * <ul>
+ * <li><code>ConfigOverride.config("debug", "true")</code> will add a top level
+ * field named "debug" mapped to the string "true".</li>
+ * <li><code>ConfigOverride.config("server.applicationConnectors[0].type",
+ * "https")</code> will change the sole application connector to have type
+ * "https" instead of type "http".
+ * <li><code>ConfigOverride.config("logging.loggers.com\\.example\\.bar",
+ * "DEBUG")</code> will add a logger with the name "com.example.bar" configured
+ * for debug logging.</li>
+ * <li><code>ConfigOverride.randomPorts()</code> will change the ports of the
+ * default applicationConnectors and adminConnectors to 0 so the tests start
+ * with random ports.</li>
+ * </ul>
+ */
+public abstract class ConfigOverride {
 
-    private ConfigOverride(String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
+    static final String DEFAULT_PREFIX = "dw.";
 
     public static ConfigOverride config(String key, String value) {
-        return new ConfigOverride(key, value);
+        return new ConfigOverrideValue(DEFAULT_PREFIX, key, () -> value);
     }
 
-    public void addToSystemProperties() {
-        System.setProperty("dw." + key, value);
+    public static ConfigOverride config(String propertyPrefix, String key, String value) {
+        return new ConfigOverrideValue(propertyPrefix, key, () -> value);
     }
 
-    public void removeFromSystemProperties() {
-        System.clearProperty("dw." + key);
+    public static ConfigOverride config(String key, Supplier<String> value) {
+        return new ConfigOverrideValue(DEFAULT_PREFIX, key, value);
     }
+
+    public static ConfigOverride config(String propertyPrefix, String key, Supplier<String> value) {
+        return new ConfigOverrideValue(propertyPrefix, key, value);
+    }
+
+    public static ConfigOverride randomPorts() {
+        return new ConfigOverrideRandomPorts(DEFAULT_PREFIX);
+    }
+
+    public static ConfigOverride randomPorts(String propertyPrefix) {
+        return new ConfigOverrideRandomPorts(propertyPrefix);
+    }
+
+    public abstract void addToSystemProperties();
+
+    public abstract void removeFromSystemProperties();
 }
