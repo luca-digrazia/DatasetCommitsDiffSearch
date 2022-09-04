4fdbc2b0604bbed4b84053109266e0a45ee3ddc1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:02:42 +0200

    Devmode: introduce live-reload.watched-resources

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/steps/DevModeBuildStep.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/steps/DevModeBuildStep.java
@@ -7,19 +7,17 @@ import java.util.stream.Collectors;
 
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.HotDeploymentWatchedFileBuildItem;
-import io.quarkus.runtime.ConfigConfig;
 import io.quarkus.runtime.LiveReloadConfig;
 
 class DevModeBuildStep {
 
     @BuildStep
-    List<HotDeploymentWatchedFileBuildItem> watchChanges(ConfigConfig config, LiveReloadConfig liveReloadConfig) {
+    List<HotDeploymentWatchedFileBuildItem> watchChanges(LiveReloadConfig config) {
         List<String> names = new ArrayList<>();
         names.add("META-INF/microprofile-config.properties");
         names.add("application.properties");
         names.add(Paths.get(".env").toAbsolutePath().toString());
-        config.locations.ifPresent(names::addAll);
-        liveReloadConfig.watchedResources.ifPresent(names::addAll);
+        config.watchedResources.ifPresent(names::addAll);
         return names.stream().map(HotDeploymentWatchedFileBuildItem::new).collect(Collectors.toList());
     }
 }
