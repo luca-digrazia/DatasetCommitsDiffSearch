0250533ac59c8adaaf4ee43c77f09ac8d2a6584a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:27:32 +0200

    Add license headers.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/web/resources/WebInterfaceAssetsResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/web/resources/WebInterfaceAssetsResource.java
@@ -16,68 +16,24 @@
  */
 package org.graylog2.web.resources;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.graylog2.plugin.Plugin;
-import org.graylog2.web.IndexHtmlGenerator;
-import org.graylog2.web.ModuleManifest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 
 @Path("{filename: .*}")
 public class WebInterfaceAssetsResource {
-    private static final Logger LOG = LoggerFactory.getLogger(WebInterfaceAssetsResource.class);
-
     private static String pathPrefix = "web-interface/assets";
-    private static String pluginPathPrefix = "/plugin/";
-    private static String manifestFilename = "module.json";
-    private final IndexHtmlGenerator indexHtmlGenerator;
-    private final ObjectMapper objectMapper;
-
-    @Inject
-    public WebInterfaceAssetsResource(ObjectMapper objectMapper,
-                                      Set<Plugin> plugins) throws IOException {
-        this.objectMapper = objectMapper;
-        final List<String> jsFiles = new ArrayList<>();
-        final List<String> cssFiles = new ArrayList<>();
-
-        plugins.stream().forEach(plugin -> {
-            final ModuleManifest pluginManifest = manifestForPlugin(plugin);
-            final String pathPrefix = pluginPathPrefix + plugin.metadata().getUniqueId() + "/";
-            if (pluginManifest != null) {
-                jsFiles.addAll(pluginManifest.files().jsFiles().stream().map(file -> file.startsWith(pathPrefix) ? file : pathPrefix + file).collect(Collectors.toList()));
-                cssFiles.addAll(pluginManifest.files().cssFiles().stream().map(file -> file.startsWith(pathPrefix) ? file : pathPrefix + file).collect(Collectors.toList()));
-            }
-        });
-        final InputStream packageManifest = ClassLoader.getSystemResourceAsStream(pathPrefix + "/" + manifestFilename);
-        final ModuleManifest manifest = objectMapper.readValue(packageManifest, ModuleManifest.class);
-        jsFiles.addAll(manifest.files().jsFiles());
-        cssFiles.addAll(manifest.files().cssFiles());
-        this.indexHtmlGenerator = new IndexHtmlGenerator("Graylog Web Interface", cssFiles, jsFiles);
-    }
 
     @GET
     public Response get(@PathParam("filename") String filename) {
-        if (filename == null || filename.isEmpty() || filename.equals("/") || filename.equals("index.html")) {
+        if (filename == null || filename.isEmpty() || filename.equals("/")) {
             return getDefaultResponse();
         }
         final InputStream stream = getStreamForFile(filename);
@@ -92,29 +48,13 @@ public class WebInterfaceAssetsResource {
                 .build();
     }
 
-    private ModuleManifest manifestForPlugin(Plugin plugin) {
-        final InputStream manifestStream = plugin.metadata().getClass().getResourceAsStream("/" + manifestFilename);
-        if (manifestStream != null) {
-            try {
-                final ModuleManifest manifest = objectMapper.readValue(manifestStream, ModuleManifest.class);
-                return manifest;
-            } catch (IOException e) {
-                LOG.warn("Unable to read manifest from plugin " + plugin + ": ", e);
-            }
-        }
-
-        LOG.debug("No valid manifest found for plugin " + plugin);
-
-        return null;
-    }
-
     private static InputStream getStreamForFile(String filename) {
         return ClassLoader.getSystemResourceAsStream(pathPrefix + "/" + filename);
     }
 
     private Response getDefaultResponse() {
         return Response
-                .ok(this.indexHtmlGenerator.get())
+                .ok(getStreamForFile("index.html"))
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML)
                 .build();
     }
