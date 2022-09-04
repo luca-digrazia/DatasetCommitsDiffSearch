6f38db8b3bf2327fbdae79a3d9f50676e3ed493e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:51:14 +0200

    add test for multiple actions with @ReceiverAction

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/org/androidannotations/test15/ereceiver/ReceiverWithActions.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/org/androidannotations/test15/ereceiver/ReceiverWithActions.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2014 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,16 +15,25 @@
  */
 package org.androidannotations.test15.ereceiver;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import org.androidannotations.annotations.EReceiver;
 import org.androidannotations.annotations.ReceiverAction;
+import org.androidannotations.api.support.content.AbstractBroadcastReceiver;
 
 @EReceiver
-public class ReceiverWithActions extends BroadcastReceiver {
+public class ReceiverWithActions extends AbstractBroadcastReceiver {
+
+	public static final String ACTION_SIMPLE_TEST = "ACTION_SIMPLE_TEST";
+	public static final String ACTION_SCHEME_TEST = "ACTION_SCHEME_TEST";
+	public static final String ACTION_PARAMETER_TEST = "ACTION_PARAMETER_TEST";
+	public static final String ACTION_MULTIPLE_TEST_1 = "ACTION_MULTIPLE_TEST_1";
+	public static final String ACTION_MULTIPLE_TEST_2 = "ACTION_MULTIPLE_TEST_2";
+	public static final String ACTION_EXTRA_PARAMETER_TEST = "ACTION_EXTRA_PARAMETER_TEST";
+	public static final String EXTRA_ARG_NAME1 = "thisExtraHasAnotherName";
+	public static final String EXTRA_ARG_NAME2 = "thisIsMyParameter";
+	public static final String DATA_SCHEME = "http";
 
 	public boolean simpleActionReceived = false;
+	public boolean actionWithSchemeReceived = false;
 
 	public boolean parameterActionReceived = false;
 	public String parameterActionValue = null;
@@ -32,25 +41,32 @@ public class ReceiverWithActions extends BroadcastReceiver {
 	public boolean extraParameterActionReceived = false;
 	public String extraParameterActionValue = null;
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-	}
+	public int multipleActionCall = 0;
 
-	@ReceiverAction("ACTION_SIMPLE_TEST")
+	@ReceiverAction(actions = ACTION_SIMPLE_TEST)
 	public void onSimpleAction() {
 		simpleActionReceived = true;
 	}
 
-	@ReceiverAction("ACTION_PARAMETER_TEST")
+	@ReceiverAction(actions = ACTION_SCHEME_TEST, dataSchemes = DATA_SCHEME)
+	public void onActionWithReceiver() {
+		actionWithSchemeReceived = true;
+	}
+
+	@ReceiverAction(actions = ACTION_PARAMETER_TEST)
 	public void onParameterAction(@ReceiverAction.Extra String thisIsMyParameter) {
 		parameterActionReceived = true;
 		parameterActionValue = thisIsMyParameter;
 	}
 
-	@ReceiverAction("ACTION_EXTRA_PARAMETER_TEST")
-	public void onExtraParameterAction(
-			@ReceiverAction.Extra("thisExtraHasAnotherName") String thisIsAParameter) {
+	@ReceiverAction(actions = ACTION_EXTRA_PARAMETER_TEST)
+	public void onExtraParameterAction(@ReceiverAction.Extra(EXTRA_ARG_NAME1) String thisIsAParameter) {
 		extraParameterActionReceived = true;
 		extraParameterActionValue = thisIsAParameter;
+	}
+
+	@ReceiverAction(actions = { ACTION_MULTIPLE_TEST_1, ACTION_MULTIPLE_TEST_2 })
+	public void onMultipleActions() {
+		multipleActionCall++;
 	}
 }
