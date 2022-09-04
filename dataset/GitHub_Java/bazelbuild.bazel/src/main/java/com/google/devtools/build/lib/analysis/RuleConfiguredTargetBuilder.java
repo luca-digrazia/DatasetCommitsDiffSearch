8d19468ed33f95376b4e72d258cca55c19b3bbe7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:06:03 +0200

    Automated rollback of commit 22d1ef7f866e74d8d1f1d322ce072150b00ca3f0.
        *** Reason for rollback ***
    
        Breaks nightly TGP.
    
        PiperOrigin-RevId: 270960700

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
@@ -47,6 +47,7 @@ import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.events.Location;
 import com.google.devtools.build.lib.packages.BuildSetting;
 import com.google.devtools.build.lib.packages.InfoInterface;
+import com.google.devtools.build.lib.packages.NativeProvider;
 import com.google.devtools.build.lib.packages.Provider;
 import com.google.devtools.build.lib.packages.Rule;
 import com.google.devtools.build.lib.packages.TargetUtils;
@@ -375,6 +376,14 @@ public final class RuleConfiguredTargetBuilder {
     return this;
   }
 
+  private <T extends TransitiveInfoProvider> void maybeAddSkylarkLegacyProvider(
+      InfoInterface value) {
+    if (value.getProvider() instanceof NativeProvider.WithLegacySkylarkName) {
+      addSkylarkTransitiveInfo(
+          ((NativeProvider.WithLegacySkylarkName) value.getProvider()).getSkylarkName(), value);
+    }
+  }
+
   /**
    * Add a Skylark transitive info. The provider value must be safe (i.e. a String, a Boolean,
    * an Integer, an Artifact, a Label, None, a Java TransitiveInfoProvider or something composed
@@ -437,6 +446,7 @@ public final class RuleConfiguredTargetBuilder {
     Provider constructor = provider.getProvider();
     Preconditions.checkState(constructor.isExported());
     providersBuilder.put(provider);
+    maybeAddSkylarkLegacyProvider(provider);
     return this;
   }
 
