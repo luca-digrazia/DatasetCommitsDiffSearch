a016893905a31fd6c08fd36bcf94ee273dabd185
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:40:13 +0200

    Add fields of all SD-ELEMENTS, not just the first.
        Before this only the fields of the first RFC5424 SD-ELEMENT have been
        added to the fields map. This is fixed now by iterating over all
        existing SD-ELEMENTS returned by the syslog parser.
    
        Known problem:
    
        If two different SD-ELEMENTs share the same key in the SD-PARAMS, they
        overwrite each other. See code comments for details.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-inputs/src/main/java/org/graylog2/inputs/codecs/SyslogCodec.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-inputs/src/main/java/org/graylog2/inputs/codecs/SyslogCodec.java
@@ -37,8 +37,6 @@ import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
-import org.graylog2.plugin.ConfigClass;
-import org.graylog2.plugin.FactoryClass;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.Tools;
 import org.graylog2.plugin.configuration.Configuration;
@@ -61,9 +59,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import static com.google.common.base.Throwables.propagate;
@@ -205,57 +201,51 @@ public class SyslogCodec implements Codec {
         return "syslog";
     }
 
-    @FactoryClass
+    @Nonnull
+    @Override
+    public ConfigurationRequest getRequestedConfiguration() {
+        final ConfigurationRequest r = new ConfigurationRequest();
+
+        r.addField(
+                new BooleanField(
+                        CK_FORCE_RDNS,
+                        "Force rDNS?",
+                        false,
+                        "Force rDNS resolution of hostname? Use if hostname cannot be parsed."
+                )
+        );
+
+        r.addField(
+                new BooleanField(
+                        CK_ALLOW_OVERRIDE_DATE,
+                        "Allow overriding date?",
+                        true,
+                        "Allow to override with current date if date could not be parsed?"
+                )
+        );
+
+        r.addField(
+                new BooleanField(
+                        CK_STORE_FULL_MESSAGE,
+                        "Store full message?",
+                        false,
+                        "Store the full original syslog message as full_message?"
+                )
+        );
+
+        return r;
+    }
+
+    @Override
+    public void overrideDefaultValues(@Nonnull ConfigurationRequest cr) {
+        if (cr.containsField(NettyTransport.CK_PORT)) {
+            cr.getField(NettyTransport.CK_PORT).setDefaultValue(514);
+        }
+    }
+
     public interface Factory extends Codec.Factory<SyslogCodec> {
         @Override
         SyslogCodec create(Configuration configuration);
-
-        @Override
-        Config getConfig();
-    }
-
-    @ConfigClass
-    public static class Config implements Codec.Config {
-        @Override
-        public ConfigurationRequest getRequestedConfiguration() {
-            final ConfigurationRequest r = new ConfigurationRequest();
-
-            r.addField(
-                    new BooleanField(
-                            CK_FORCE_RDNS,
-                            "Force rDNS?",
-                            false,
-                            "Force rDNS resolution of hostname? Use if hostname cannot be parsed."
-                    )
-            );
-
-            r.addField(
-                    new BooleanField(
-                            CK_ALLOW_OVERRIDE_DATE,
-                            "Allow overriding date?",
-                            true,
-                            "Allow to override with current date if date could not be parsed?"
-                    )
-            );
-
-            r.addField(
-                    new BooleanField(
-                            CK_STORE_FULL_MESSAGE,
-                            "Store full message?",
-                            false,
-                            "Store the full original syslog message as full_message?"
-                    )
-            );
-
-            return r;
-        }
-
-        @Override
-        public void overrideDefaultValues(@Nonnull ConfigurationRequest cr) {
-            if (cr.containsField(NettyTransport.CK_PORT)) {
-                cr.getField(NettyTransport.CK_PORT).setDefaultValue(514);
-            }
-        }
     }
 
     /**
@@ -271,14 +261,20 @@ public class SyslogCodec implements Codec {
             Map<String, Object> fields = Maps.newHashMap();
             try {
                 Map raw = msg.getStructuredMessage().getStructuredData();
-                if (raw != null) {
-                    Set ks = raw.keySet();
-                    if (ks.size() > 0) {
-                        Object[] fl = raw.keySet().toArray();
+                if (raw != null && raw.size() > 0) {
+                    // Parsing this structured syslog message results in the following nested Map structure.
+                    // "<165>1 2012-12-25T22:14:15.003Z mymachine evntslog - - [exampleSDID@32473 iut=\"3\" eventID=\"1011\"][meta sequenceId=\"1\"] message"
+                    // {exampleSDID@32473={eventID=1011, iut=3}, meta={sequenceId=1}}
+                    //
+                    // TODO: If two different RFC5424 SD-ELEMENTS share the same SD-PARAM keys, they overwrites each other.
+                    // Example: [test1 test="v1"][test2 test="v2"] might result in "test"="v2" in the fields map.
+                    // Order is not guaranteed in the current syslog4j implementation!
 
-                        if (fl != null && fl.length > 0) {
-                            String sdID = (String) fl[0];
-                            fields = (HashMap) raw.get(sdID);
+                    for (Object o : raw.entrySet()) {
+                        final Map.Entry entry = (Map.Entry) o;
+
+                        if (entry.getValue() instanceof Map) {
+                            fields.putAll((Map) entry.getValue());
                         }
                     }
                 }
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/messagehandlers/syslog/StructuredSyslogTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/messagehandlers/syslog/StructuredSyslogTest.java
@@ -1,6 +1,4 @@
 /**
- * Copyright 2011 Lennart Koopmann <lennart@socketfeed.com>
- *
  * This file is part of Graylog2.
  *
  * Graylog2 is free software: you can redistribute it and/or modify
@@ -15,26 +13,33 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
-
 package org.graylog2.messagehandlers.syslog;
 
+import org.graylog2.inputs.codecs.SyslogCodec;
+import org.graylog2.syslog4j.server.impl.event.structured.StructuredSyslogServerEvent;
 import org.testng.annotations.Test;
-import static org.testng.AssertJUnit.*;
+
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class StructuredSyslogTest {
-
-    @Test
-    public void testTheTruthToWork() {
-        assertTrue(true);
-    }
-
-/*
     // http://tools.ietf.org/rfc/rfc5424.txt
     public static String ValidStructuredMessage = "<165>1 2012-12-25T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"] BOMAn application event log entry";
+    public static String ValidStructuredMultiMessage = "<165>1 2012-12-25T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"][meta sequenceId=\"1\"] BOMAn application event log entry";
+    public static String ValidStructuredMultiMessageSameKey = "<165>1 2012-12-25T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventID=\"1011\"][meta iut=\"10\"] BOMAn application event log entry";
+    public static String ValidStructuredNoStructValues = "<165>1 2012-12-25T22:14:15.003Z mymachine.example.com evntslog - ID47 - BOMAn application event log entry";
     public static String ValidNonStructuredMessage = "<86>Dec 24 17:05:01 nb-lkoopmann CRON[10049]: pam_unix(cron:session): session closed for user root";
     public static String MessageLookingLikeStructured = "<133>NOMA101FW01A: NetScreen device_id=NOMA101FW01A [Root]system-notification-00257(traffic): start_time=\"2011-12-23 17:33:43\" duration=0 reason=Creation";
+
+    private StructuredSyslogServerEvent newEvent(String message) {
+        return new StructuredSyslogServerEvent(message, new InetSocketAddress(514).getAddress());
+    }
 
     @Test
     public void testExtractFields() {
@@ -43,20 +48,46 @@ public class StructuredSyslogTest {
         expected.put("eventID", "1011");
         expected.put("iut", "3");
 
-        Map result = StructuredSyslog.extractFields(ValidStructuredMessage.getBytes());
-        assertEquals(expected, result);
+        Map result = SyslogCodec.StructuredSyslog.extractFields(newEvent(ValidStructuredMessage));
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testExtractMoreFields() {
+        Map expected = new HashMap();
+        expected.put("eventSource", "Application");
+        expected.put("eventID", "1011");
+        expected.put("iut", "3");
+        expected.put("sequenceId", "1");
+
+        Map result = SyslogCodec.StructuredSyslog.extractFields(newEvent(ValidStructuredMultiMessage));
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testExtractFieldsOverwriting() {
+        // TODO: The current implementation does not handle two different SD-ELEMENTS with the same SD-PARAM keys very well.
+        // TODO: Order is not guaranteed in the current syslog4j implementation!
+
+        Map result = SyslogCodec.StructuredSyslog.extractFields(newEvent(ValidStructuredMultiMessageSameKey));
+        assertTrue(Pattern.compile("3|10").matcher((String) result.get("iut")).matches(), "iut value is not 3 or 10!");
+    }
+
+    @Test
+    public void testExtractNoFields() {
+        Map result = SyslogCodec.StructuredSyslog.extractFields(newEvent(ValidStructuredNoStructValues));
+        assertEquals(result, new HashMap());
     }
 
     @Test
     public void testExtractFieldsOfNonStructuredMessage() {
-        Map result = StructuredSyslog.extractFields(ValidNonStructuredMessage.getBytes());
-        assertEquals(0, result.size());
+        Map result = SyslogCodec.StructuredSyslog.extractFields(newEvent(ValidNonStructuredMessage));
+        assertEquals(result.size(), 0);
     }
 
     @Test
     public void testExtractFieldsOfAMessageThatOnlyLooksStructured() {
-        Map result = StructuredSyslog.extractFields(MessageLookingLikeStructured.getBytes());
-        assertEquals(0, result.size());
+        Map result = SyslogCodec.StructuredSyslog.extractFields(newEvent(MessageLookingLikeStructured));
+        assertEquals(result.size(), 0);
     }
-*/
 }
