62322911ea496ca8000d2d17d4ff2ff3bdbe24bf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:40:27 +0200

    Remove a deprecated constructor
        PiperOrigin-RevId: 209666300

--- dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/plugins/errorprone/ErrorPronePlugin.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/plugins/errorprone/ErrorPronePlugin.java
@@ -14,14 +14,9 @@
 
 package com.google.devtools.build.buildjar.javac.plugins.errorprone;
 
-import static java.util.Comparator.comparing;
-
-import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.buildjar.InvalidCommandLineException;
 import com.google.devtools.build.buildjar.javac.plugins.BlazeJavaCompilerPlugin;
-import com.google.devtools.build.buildjar.javac.statistics.BlazeJavacStatistics;
 import com.google.errorprone.BaseErrorProneJavaCompiler;
 import com.google.errorprone.ErrorProneAnalyzer;
 import com.google.errorprone.ErrorProneError;
@@ -36,10 +31,8 @@ import com.sun.tools.javac.comp.Env;
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.Log;
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /**
  * A plugin for BlazeJavaCompiler that performs Error Prone analysis. Error Prone is a static
@@ -67,51 +60,6 @@ public final class ErrorPronePlugin extends BlazeJavaCompilerPlugin {
 
   private ErrorProneAnalyzer errorProneAnalyzer;
   private ErrorProneOptions epOptions;
-  private ErrorProneTimings timings;
-  private final Stopwatch elapsed = Stopwatch.createUnstarted();
-
-  // TODO(cushon): delete this shim after the next Error Prone update
-  static class ErrorProneTimings {
-    static Class<?> clazz;
-
-    static {
-      try {
-        clazz = Class.forName("com.google.errorprone.ErrorProneTimings");
-      } catch (ClassNotFoundException e) {
-        // ignored
-      }
-    }
-
-    private final Object instance;
-
-    public ErrorProneTimings(Object instance) {
-      this.instance = instance;
-    }
-
-    public static ErrorProneTimings instance(Context context) {
-      Object instance = null;
-      if (clazz != null) {
-        try {
-          instance = clazz.getMethod("instance", Context.class).invoke(null, context);
-        } catch (ReflectiveOperationException e) {
-          throw new LinkageError(e.getMessage(), e);
-        }
-      }
-      return new ErrorProneTimings(instance);
-    }
-
-    @SuppressWarnings("unchecked") // reflection
-    public Map<String, Duration> timings() {
-      if (clazz == null) {
-        return ImmutableMap.of();
-      }
-      try {
-        return (Map<String, Duration>) clazz.getMethod("timings").invoke(instance);
-      } catch (ReflectiveOperationException e) {
-        throw new LinkageError(e.getMessage(), e);
-      }
-    }
-  }
 
   /** Registers our message bundle. */
   public static void setupMessageBundle(Context context) {
@@ -144,12 +92,8 @@ public final class ErrorPronePlugin extends BlazeJavaCompilerPlugin {
   }
 
   @Override
-  public void init(
-      Context context,
-      Log log,
-      JavaCompiler compiler,
-      BlazeJavacStatistics.Builder statisticsBuilder) {
-    super.init(context, log, compiler, statisticsBuilder);
+  public void init(Context context, Log log, JavaCompiler compiler) {
+    super.init(context, log, compiler);
 
     setupMessageBundle(context);
 
@@ -158,13 +102,11 @@ public final class ErrorPronePlugin extends BlazeJavaCompilerPlugin {
     }
     errorProneAnalyzer =
         ErrorProneAnalyzer.createByScanningForPlugins(scannerSupplier, epOptions, context);
-    timings = ErrorProneTimings.instance(context);
   }
 
   /** Run Error Prone analysis after performing dataflow checks. */
   @Override
   public void postFlow(Env<AttrContext> env) {
-    elapsed.start();
     try {
       errorProneAnalyzer.finished(new TaskEvent(Kind.ANALYZE, env.toplevel, env.enclClass.sym));
     } catch (ErrorProneError e) {
@@ -172,17 +114,6 @@ public final class ErrorPronePlugin extends BlazeJavaCompilerPlugin {
       // let the exception propagate to javac's main, where it will cause the compilation to
       // terminate with Result.ABNORMAL
       throw e;
-    } finally {
-      elapsed.stop();
     }
-  }
-
-  @Override
-  public void finish() {
-    statisticsBuilder.totalErrorProneTime(elapsed.elapsed());
-    timings.timings().entrySet().stream()
-        .sorted(comparing((Map.Entry<String, Duration> e) -> e.getValue()).reversed())
-        .limit(10) // best-effort to stay under the action metric size limit
-        .forEachOrdered((e) -> statisticsBuilder.addBugpatternTiming(e.getKey(), e.getValue()));
   }
 }
