ffed6807b21a3c595f5c3b2b565209e94a44bee1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:21:37 +0200

    Support access_token query parameter for OAuth (#1593)
        The OAuth Bearer token spec allows to pass the access token not
        only in the Authorization header with the Bearer prefix, but
        also as an access_token query parameter.
    
        See https://tools.ietf.org/html/rfc6750#section-2.3
    
        While it's preferred to use the header for increased security, the
        token can be helpful at times.
    
         - biggest advantage is you can avoid CORS pre-flight requests with the
           query parameter since they are not triggered when no non-simple header
           is present
         - it can be useful when testing/developing where you can simulate requests
           in a browser tab without a REST client

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-auth/src/main/java/io/dropwizard/auth/oauth/OAuthCredentialAuthFilter.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-auth/src/main/java/io/dropwizard/auth/oauth/OAuthCredentialAuthFilter.java
@@ -1,68 +1,82 @@
 package io.dropwizard.auth.oauth;
 
-import com.google.common.base.Optional;
-import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.AuthFilter;
 import io.dropwizard.auth.Authenticator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import javax.annotation.Priority;
-import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.SecurityContext;
 import java.io.IOException;
 import java.security.Principal;
 
 @Priority(Priorities.AUTHENTICATION)
 public class OAuthCredentialAuthFilter<P extends Principal> extends AuthFilter<String, P> {
-    final private static Logger LOGGER = LoggerFactory.getLogger(OAuthCredentialAuthFilter.class);
-    private OAuthCredentialAuthFilter() {}
 
-    @Override
-    public void filter(ContainerRequestContext requestContext) throws IOException {
-        final String header = requestContext.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        if (header != null) {
-            try {
-                final int space = header.indexOf(' ');
-                if (space > 0) {
-                    final String method = header.substring(0, space);
-                    if (prefix.equalsIgnoreCase(method)) {
-                        final String credentials = header.substring(space + 1);
-                        final Optional<P> result = authenticator.authenticate(credentials);
-                        if (result.isPresent()) {
-                            final Principal principal = result.get();
-                            requestContext.setSecurityContext(
-                                    getSecurityContextFunction().apply(new Tuple(requestContext, principal)));
-                            return;
-                        }
-                    }
-                }
-            } catch (AuthenticationException e) {
-                LOGGER.warn("Error authenticating credentials", e);
-                throw new InternalServerErrorException();
-            }
-        }
+    /**
+     * Query parameter used to pass Bearer token
+     *
+     * @see <a href="https://tools.ietf.org/html/rfc6750#section-2.3">The OAuth 2.0 Authorization Framework: Bearer Token Usage</a>
+     */
+    public static final String OAUTH_ACCESS_TOKEN_PARAM = "access_token";
 
-        throw new WebApplicationException(unauthorizedHandler.buildResponse(prefix, realm));
+    private OAuthCredentialAuthFilter() {
     }
 
-    public static class Builder<APrincipal extends Principal, AAuthenticator extends Authenticator<String, APrincipal>>
-            extends AuthFilterBuilder<String, APrincipal, OAuthCredentialAuthFilter<APrincipal>, AAuthenticator> {
-        @Override
-        public OAuthCredentialAuthFilter<APrincipal> buildAuthFilter() {
-            if(realm == null || authenticator == null || prefix == null || securityContextFunction == null) {
-                throw new RuntimeException("Required auth filter parameters not set");
-            }
+    @Override
+    public void filter(final ContainerRequestContext requestContext) throws IOException {
+        String credentials = getCredentials(requestContext.getHeaders().getFirst(HttpHeaders.AUTHORIZATION));
 
-            OAuthCredentialAuthFilter<APrincipal> oauthCredentialAuthFilter = new OAuthCredentialAuthFilter<>();
-            oauthCredentialAuthFilter.setRealm(realm);
-            oauthCredentialAuthFilter.setAuthenticator(authenticator);
-            oauthCredentialAuthFilter.setPrefix(prefix);
-            oauthCredentialAuthFilter.setSecurityContextFunction(securityContextFunction);
-            return oauthCredentialAuthFilter;
+        // If Authorization header is not used, check query parameter where token can be passed as well
+        if (credentials == null) {
+            credentials = requestContext.getUriInfo().getQueryParameters().getFirst(OAUTH_ACCESS_TOKEN_PARAM);
+        }
+
+        if (!authenticate(requestContext, credentials, SecurityContext.BASIC_AUTH)) {
+            throw new WebApplicationException(unauthorizedHandler.buildResponse(prefix, realm));
+        }
+    }
+
+    /**
+     * Parses a value of the `Authorization` header in the form of `Bearer a892bf3e284da9bb40648ab10`.
+     *
+     * @param header the value of the `Authorization` header
+     * @return a token
+     */
+    @Nullable
+    private String getCredentials(String header) {
+        if (header == null) {
+            return null;
+        }
+
+        final int space = header.indexOf(' ');
+        if (space <= 0) {
+            return null;
+        }
+
+        final String method = header.substring(0, space);
+        if (!prefix.equalsIgnoreCase(method)) {
+            return null;
+        }
+
+        return header.substring(space + 1);
+    }
+
+    /**
+     * Builder for {@link OAuthCredentialAuthFilter}.
+     * <p>An {@link Authenticator} must be provided during the building process.</p>
+     *
+     * @param <P> the type of the principal
+     */
+    public static class Builder<P extends Principal>
+            extends AuthFilterBuilder<String, P, OAuthCredentialAuthFilter<P>> {
+
+        @Override
+        protected OAuthCredentialAuthFilter<P> newInstance() {
+            return new OAuthCredentialAuthFilter<>();
         }
     }
 }
--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-auth/src/test/java/io/dropwizard/auth/oauth/OAuthProviderTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-auth/src/test/java/io/dropwizard/auth/oauth/OAuthProviderTest.java
@@ -1,27 +1,27 @@
 package io.dropwizard.auth.oauth;
 
+import com.google.common.collect.ImmutableList;
+
+import org.junit.Test;
+
 import io.dropwizard.auth.AbstractAuthResourceConfig;
 import io.dropwizard.auth.AuthBaseTest;
+import io.dropwizard.auth.AuthFilter;
 import io.dropwizard.auth.AuthResource;
 import io.dropwizard.auth.util.AuthUtil;
 import io.dropwizard.jersey.DropwizardResourceConfig;
-import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-
-import javax.ws.rs.container.ContainerRequestFilter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class OAuthProviderTest extends AuthBaseTest<OAuthProviderTest.OAuthTestResourceConfig> {
+public class OAuthProviderTest extends AuthBaseTest<OAuthProviderTest.OAuthTestResourceConfig>{
     public static class OAuthTestResourceConfig extends AbstractAuthResourceConfig {
         public OAuthTestResourceConfig() {
             register(AuthResource.class);
         }
 
-        @Override protected ContainerRequestFilter getAuthFilter() {
+        @Override protected AuthFilter getAuthFilter() {
             return new OAuthCredentialAuthFilter.Builder<>()
-                .setAuthenticator(AuthUtil.getMultiplyUsersOAuthAuthenticator(Arrays.asList(ADMIN_USER, ORDINARY_USER)))
+                .setAuthenticator(AuthUtil.getMultiplyUsersOAuthAuthenticator(ImmutableList.of(ADMIN_USER, ORDINARY_USER)))
                 .setAuthorizer(AuthUtil.getTestAuthorizer(ADMIN_USER, ADMIN_ROLE))
                 .setPrefix(BEARER_PREFIX)
                 .buildAuthFilter();
@@ -29,7 +29,7 @@ public class OAuthProviderTest extends AuthBaseTest<OAuthProviderTest.OAuthTestR
     }
 
     @Test
-    void checksQueryStringAccessTokenIfAuthorizationHeaderMissing() {
+    public void checksQueryStringAccessTokenIfAuthorizationHeaderMissing() {
         assertThat(target("/test/profile")
             .queryParam(OAuthCredentialAuthFilter.OAUTH_ACCESS_TOKEN_PARAM, getOrdinaryGuyValidToken())
             .request()
