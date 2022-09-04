e9686f7403fdf7b68807db83b9f22ba1dcaf011d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:49:18 +0200

    NettyProcessor marks http2 related classes for runtime initialization even if they are not on the classpath #7576

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/netty/deployment/src/main/java/io/quarkus/netty/deployment/NettyProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/netty/deployment/src/main/java/io/quarkus/netty/deployment/NettyProcessor.java
@@ -169,14 +169,12 @@ class NettyProcessor {
                 .supplier(boss)
                 .scope(ApplicationScoped.class)
                 .addQualifier(BossEventLoopGroup.class)
-                .unremovable()
                 .done());
 
         syntheticBeans.produce(SyntheticBeanBuildItem.configure(EventLoopGroup.class)
                 .supplier(main)
                 .scope(ApplicationScoped.class)
                 .addQualifier(MainEventLoopGroup.class)
-                .unremovable()
                 .done());
     }
 
