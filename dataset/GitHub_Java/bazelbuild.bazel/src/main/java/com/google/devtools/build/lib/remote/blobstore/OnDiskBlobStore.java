6faa6fd09e429c8de16b3b3863c661ba8d00eb1d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 00:55:25 +0200

    Add an on-disk storage option for the remote worker
        I'm planning to switch the remote worker over to the on-disk storage system,
        and delete the in-memory option. Currently, running a Bazel build with the
        in-memory storage system uses up over 50% of the total available memory on my
        machine (I only have 32 GB total), and grinds it to a complete halt (unless I
        close most of my apps) on a simple build of //src:bazel.
    
        PiperOrigin-RevId: 160877546

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/blobstore/OnDiskBlobStore.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/blobstore/OnDiskBlobStore.java
@@ -13,10 +13,9 @@
 // limitations under the License.
 package com.google.devtools.build.lib.remote.blobstore;
 
-import com.google.common.io.ByteStreams;
+import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 
@@ -34,26 +33,17 @@ public final class OnDiskBlobStore implements SimpleBlobStore {
   }
 
   @Override
-  public boolean get(String key, OutputStream out) throws IOException {
+  public byte[] get(String key) throws IOException {
     Path f = toPath(key);
-    if (!f.exists()) {
-      return false;
-    }
-    try (InputStream in = f.getInputStream()) {
-      ByteStreams.copy(in, out);
-    }
-    return true;
+    return f.exists() ? FileSystemUtils.readContent(f) : null;
   }
 
   @Override
-  public void put(String key, InputStream in) throws IOException {
-    // Write a temporary file first, and then rename, to avoid data corruption in case of a crash.
+  public void put(String key, byte[] value) throws IOException {
     Path temp = toPath(UUID.randomUUID().toString());
     try (OutputStream out = temp.getOutputStream()) {
-      ByteStreams.copy(in, out);
+      out.write(value);
     }
-    // TODO(ulfjack): Fsync temp here before we rename it to avoid data loss in the case of machine
-    // crashes (the OS may reorder the writes and the rename).
     Path f = toPath(key);
     temp.renameTo(f);
   }
