fa3accf23063853cd47fe6cd21d8412cec1c4b71
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:55:29 +0200

    Publish peak post GC heap size in BEP whenever we recorded it.
        Previously, it would only be published when `--bep_publish_used_heap_size_post_build` is passed which also triggers an extra GC after the build.
    
        PiperOrigin-RevId: 344250070

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/metrics/MetricsCollector.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/metrics/MetricsCollector.java
@@ -16,191 +16,117 @@ package com.google.devtools.build.lib.metrics;
 import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.Subscribe;
 import com.google.devtools.build.lib.actions.ActionCompletionEvent;
-import com.google.devtools.build.lib.actions.AnalysisGraphStatsEvent;
-import com.google.devtools.build.lib.actions.TotalAndConfiguredTargetOnlyMetric;
 import com.google.devtools.build.lib.analysis.AnalysisPhaseCompleteEvent;
-import com.google.devtools.build.lib.analysis.AnalysisPhaseStartedEvent;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.ActionSummary;
-import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.ArtifactMetrics;
-import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.BuildGraphMetrics;
-import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.CumulativeMetrics;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.MemoryMetrics;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.PackageMetrics;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.TargetMetrics;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.TimingMetrics;
 import com.google.devtools.build.lib.buildtool.BuildPrecompleteEvent;
-import com.google.devtools.build.lib.buildtool.buildevent.ExecutionStartingEvent;
 import com.google.devtools.build.lib.metrics.MetricsModule.Options;
-import com.google.devtools.build.lib.metrics.PostGCMemoryUseRecorder.PeakHeap;
 import com.google.devtools.build.lib.profiler.Profiler;
 import com.google.devtools.build.lib.runtime.CommandEnvironment;
-import com.google.devtools.build.lib.skyframe.ExecutionFinishedEvent;
-import com.google.devtools.build.skyframe.SkyframeGraphStatsEvent;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.time.Duration;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 class MetricsCollector {
+
   private final CommandEnvironment env;
   private final boolean bepPublishUsedHeapSizePostBuild;
-  // For ActionSummary.
   private final AtomicLong executedActionCount = new AtomicLong();
 
-  // For CumulativeMetrics.
-  private final AtomicInteger numAnalyses;
-  private final AtomicInteger numBuilds;
+  private int actionsConstructed;
+  private int targetsLoaded;
+  private int targetsConfigured;
+  private int packagesLoaded;
 
-  private final ActionSummary.Builder actionSummary = ActionSummary.newBuilder();
-  private final TargetMetrics.Builder targetMetrics = TargetMetrics.newBuilder();
-  private final PackageMetrics.Builder packageMetrics = PackageMetrics.newBuilder();
-  private final TimingMetrics.Builder timingMetrics = TimingMetrics.newBuilder();
-  private final ArtifactMetrics.Builder artifactMetrics = ArtifactMetrics.newBuilder();
-  private final BuildGraphMetrics.Builder buildGraphMetrics = BuildGraphMetrics.newBuilder();
-
-  private MetricsCollector(
-      CommandEnvironment env, AtomicInteger numAnalyses, AtomicInteger numBuilds) {
+  MetricsCollector(CommandEnvironment env) {
     this.env = env;
     Options options = env.getOptions().getOptions(Options.class);
     this.bepPublishUsedHeapSizePostBuild =
         options != null && options.bepPublishUsedHeapSizePostBuild;
-    this.numAnalyses = numAnalyses;
-    this.numBuilds = numBuilds;
     env.getEventBus().register(this);
   }
 
-  static void installInEnv(
-      CommandEnvironment env, AtomicInteger numAnalyses, AtomicInteger numBuilds) {
-    new MetricsCollector(env, numAnalyses, numBuilds);
+  static void installInEnv(CommandEnvironment env) {
+    new MetricsCollector(env);
   }
 
-  @SuppressWarnings("unused")
-  @Subscribe
-  public synchronized void logAnalysisStartingEvent(AnalysisPhaseStartedEvent event) {
-    numAnalyses.getAndIncrement();
-  }
-
-  @SuppressWarnings("unused")
   @Subscribe
   public void onAnalysisPhaseComplete(AnalysisPhaseCompleteEvent event) {
-    TotalAndConfiguredTargetOnlyMetric actionsConstructed = event.getActionsConstructed();
-    actionSummary
-        .setActionsCreated(actionsConstructed.total())
-        .setActionsCreatedNotIncludingAspects(actionsConstructed.configuredTargetsOnly());
-    TotalAndConfiguredTargetOnlyMetric targetsConfigured = event.getTargetsConfigured();
-    targetMetrics
-        .setTargetsConfigured(targetsConfigured.total())
-        .setTargetsConfiguredNotIncludingAspects(targetsConfigured.configuredTargetsOnly());
-    packageMetrics.setPackagesLoaded(event.getPkgManagerStats().getPackagesLoaded());
-    timingMetrics.setAnalysisPhaseTimeInMs(event.getTimeInMs());
+    actionsConstructed = event.getActionsConstructed();
+    targetsLoaded = event.getTargetsLoaded();
+    targetsConfigured = event.getTargetsConfigured();
+    packagesLoaded = event.getPkgManagerStats().getPackagesLoaded();
   }
 
-  @SuppressWarnings("unused")
-  @Subscribe
-  public synchronized void logAnalysisGraphStats(AnalysisGraphStatsEvent event) {
-    TotalAndConfiguredTargetOnlyMetric actionLookupValueCount = event.getActionLookupValueCount();
-    TotalAndConfiguredTargetOnlyMetric actionCount = event.getActionCount();
-    buildGraphMetrics
-        .setActionLookupValueCount(actionLookupValueCount.total())
-        .setActionLookupValueCountNotIncludingAspects(
-            actionLookupValueCount.configuredTargetsOnly())
-        .setActionCount(actionCount.total())
-        .setActionCountNotIncludingAspects(actionCount.configuredTargetsOnly())
-        .setOutputArtifactCount(event.getOutputArtifactCount());
-  }
-
-  @SuppressWarnings("unused")
-  @Subscribe
-  public synchronized void logExecutionStartingEvent(ExecutionStartingEvent event) {
-    numBuilds.getAndIncrement();
-  }
-
-  @SuppressWarnings("unused")
   @Subscribe
   @AllowConcurrentEvents
   public void onActionComplete(ActionCompletionEvent event) {
     executedActionCount.incrementAndGet();
   }
 
-  @SuppressWarnings("unused")
-  @Subscribe
-  public void onExecutionComplete(ExecutionFinishedEvent event) {
-    artifactMetrics
-        .setSourceArtifactsRead(event.sourceArtifactsRead())
-        .setOutputArtifactsSeen(event.outputArtifactsSeen())
-        .setOutputArtifactsFromActionCache(event.outputArtifactsFromActionCache())
-        .setTopLevelArtifacts(event.topLevelArtifacts());
-  }
-
-  @SuppressWarnings("unused")
-  @Subscribe
-  public void onSkyframeGraphStats(SkyframeGraphStatsEvent event) {
-    buildGraphMetrics.setPostInvocationSkyframeNodeCount(event.getGraphSize());
-  }
-
-  @SuppressWarnings("unused")
   @Subscribe
   public void onBuildComplete(BuildPrecompleteEvent event) {
     env.getEventBus().post(new BuildMetricsEvent(createBuildMetrics()));
   }
 
   private BuildMetrics createBuildMetrics() {
-    return BuildMetrics.newBuilder()
-        .setActionSummary(finishActionSummary())
-        .setMemoryMetrics(createMemoryMetrics())
-        .setTargetMetrics(targetMetrics.build())
-        .setPackageMetrics(packageMetrics.build())
-        .setTimingMetrics(finishTimingMetrics())
-        .setCumulativeMetrics(createCumulativeMetrics())
-        .setArtifactMetrics(artifactMetrics.build())
-        .setBuildGraphMetrics(buildGraphMetrics.build())
-        .build();
+    BuildMetrics.Builder metrics = BuildMetrics.newBuilder();
+    metrics.setActionSummary(createActionSummary());
+    metrics.setMemoryMetrics(createMemoryMetrics());
+    metrics.setTargetMetrics(createTargetMetrics());
+    metrics.setPackageMetrics(createPackageMetrics());
+    metrics.setTimingMetrics(createTimingMetrics());
+    return metrics.build();
   }
 
-  private ActionSummary finishActionSummary() {
-    return actionSummary.setActionsExecuted(executedActionCount.get()).build();
+  private ActionSummary createActionSummary() {
+    return ActionSummary.newBuilder()
+        .setActionsCreated(actionsConstructed)
+        .setActionsExecuted(executedActionCount.get())
+        .build();
   }
 
   private MemoryMetrics createMemoryMetrics() {
     MemoryMetrics.Builder memoryMetrics = MemoryMetrics.newBuilder();
-    long usedHeapSizePostBuild = 0;
     if (bepPublishUsedHeapSizePostBuild) {
       System.gc();
       MemoryMXBean memBean = ManagementFactory.getMemoryMXBean();
-      usedHeapSizePostBuild = memBean.getHeapMemoryUsage().getUsed();
-      memoryMetrics.setUsedHeapSizePostBuild(usedHeapSizePostBuild);
+      memoryMetrics.setUsedHeapSizePostBuild(memBean.getHeapMemoryUsage().getUsed());
     }
-    PostGCMemoryUseRecorder.get()
-        .getPeakPostGcHeap()
-        .map(PeakHeap::bytes)
-        .ifPresent(memoryMetrics::setPeakPostGcHeapSize);
-
-    if (memoryMetrics.getPeakPostGcHeapSize() < usedHeapSizePostBuild) {
-      // If we just did a GC and computed the heap size, update the one we got from the GC
-      // notification (which may arrive too late for this specific GC).
-      memoryMetrics.setPeakPostGcHeapSize(usedHeapSizePostBuild);
+    Optional<Long> peakPostGcHeapSize = PostGCMemoryUseRecorder.get().getPeakPostGCHeapMemoryUsed();
+    if (peakPostGcHeapSize.isPresent()) {
+      memoryMetrics.setPeakPostGcHeapSize(peakPostGcHeapSize.get());
     }
     return memoryMetrics.build();
   }
 
-  private CumulativeMetrics createCumulativeMetrics() {
-    return CumulativeMetrics.newBuilder()
-        .setNumAnalyses(numAnalyses.get())
-        .setNumBuilds(numBuilds.get())
+  private TargetMetrics createTargetMetrics() {
+    return TargetMetrics.newBuilder()
+        .setTargetsLoaded(targetsLoaded)
+        .setTargetsConfigured(targetsConfigured)
         .build();
   }
 
-  private TimingMetrics finishTimingMetrics() {
+  private PackageMetrics createPackageMetrics() {
+    return PackageMetrics.newBuilder().setPackagesLoaded(packagesLoaded).build();
+  }
+
+  private static TimingMetrics createTimingMetrics() {
+    TimingMetrics.Builder timingMetricsBuilder = TimingMetrics.newBuilder();
     Duration elapsedWallTime = Profiler.elapsedTimeMaybe();
     if (elapsedWallTime != null) {
-      timingMetrics.setWallTimeInMs(elapsedWallTime.toMillis());
+      timingMetricsBuilder.setWallTimeInMs(elapsedWallTime.toMillis());
     }
     Duration cpuTime = Profiler.getProcessCpuTimeMaybe();
     if (cpuTime != null) {
-      timingMetrics.setCpuTimeInMs(cpuTime.toMillis());
+      timingMetricsBuilder.setCpuTimeInMs(cpuTime.toMillis());
     }
-    return timingMetrics.build();
+    return timingMetricsBuilder.build();
   }
 }
