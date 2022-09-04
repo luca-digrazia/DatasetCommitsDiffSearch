f59ce1126709b306cfa5c745a67e7f063028bdb6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:09:25 +0200

    Better swagger specs through extracting inline models. (#10637)
        * Create proper swagger schema.
    
        * Handle generics properly.
    
        * Fixing corner cases and type names.
    
        * Add visitor to replace `type: any` values.
    
        * Extract inline objects.
    
        * Adding json annotation.
    
        * Fixing reference naming, cleaning up unused code.
    
        * Treat parameters identically to response types.
    
        * Fix multiple issues, keep parameter types inline if not objects.
    
        * Fix primitive method response types.
    
        * Fixing enums and longs.
    
        * Inline non-object models.
    
        * Falling back to value when parameter name is not present.
    
        * Use nickname over method name, if present.
    
        * Include missing import.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/models/system/metrics/responses/MetricsSummaryResponse.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/models/system/metrics/responses/MetricsSummaryResponse.java
@@ -1,34 +1,18 @@
-/**
- * This file is part of Graylog.
+/*
+ * Copyright (C) 2020 Graylog, Inc.
  *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
  *
- * Graylog is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package org.graylog2.rest.models.system.metrics.responses;
 
@@ -36,23 +20,26 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
+import org.graylog.autovalue.WithBeanGetter;
 
-import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @AutoValue
+@WithBeanGetter
 @JsonAutoDetect
 public abstract class MetricsSummaryResponse {
     @JsonProperty
     public abstract int total();
     @JsonProperty
-    public abstract Collection metrics();
+    public abstract List<Map<String, Object>> metrics();
 
     @JsonCreator
-    public static MetricsSummaryResponse create(@JsonProperty("total") int total, @JsonProperty("metrics") Collection metrics) {
+    public static MetricsSummaryResponse create(@JsonProperty("total") int total, @JsonProperty("metrics") List<Map<String, Object>> metrics) {
         return new AutoValue_MetricsSummaryResponse(total, metrics);
     }
 
-    public static MetricsSummaryResponse create(Collection metrics) {
+    public static MetricsSummaryResponse create(List<Map<String, Object>> metrics) {
         return new AutoValue_MetricsSummaryResponse(metrics.size(), metrics);
     }
 }
