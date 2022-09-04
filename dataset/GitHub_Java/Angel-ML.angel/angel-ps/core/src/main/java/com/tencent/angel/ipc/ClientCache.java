1ce3610dec5e7fa1b966d1cb6645c105292b3389
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:42:00 +0200

    Update ClientCache.java

--- dataset/GitHub_Java/Angel-ML.angel/angel-ps/core/src/main/java/com/tencent/angel/ipc/ClientCache.java
+++ dataset/GitHub_Java/Angel-ML.angel/angel-ps/core/src/main/java/com/tencent/angel/ipc/ClientCache.java
@@ -1,20 +1,24 @@
-/*
- * Tencent is pleased to support the open source community by making Angel available.
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in 
- * compliance with the License. You may obtain a copy of the License at
- *
- * https://opensource.org/licenses/Apache-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+/**
+ * Add clear method to fix Angel client exit problem.
+ */
 
 package com.tencent.angel.ipc;
 
@@ -22,15 +26,21 @@ import com.tencent.angel.conf.AngelConf;
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.DefaultThreadFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.net.SocketFactory;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.ThreadFactory;
 
 /**
  * Cache a client using its socket factory as the hash key. Enables reuse/sharing of clients on a
@@ -41,80 +51,82 @@ import java.util.Map;
  * drops to zero, the cache mapping is cleared.
  */
 class ClientCache {
-  private static final Logger LOG = LoggerFactory.getLogger(ClientCache.class.getName());
-  private Map<String, NettyTransceiver> clients = new HashMap<String, NettyTransceiver>();
-  private Configuration conf = new Configuration();
+    private static final Logger LOG = LoggerFactory.getLogger(ClientCache.class.getName());
+    private Map<String, NettyTransceiver> clients = new HashMap<String, NettyTransceiver>();
+    private Configuration conf = new Configuration();
 
-  private final Class<? extends Channel> socketChannelClass;
-  private EventLoopGroup workerGroup;
-  private PooledByteBufAllocator pooledAllocator;
+    private final Class<? extends Channel> socketChannelClass;
+    private EventLoopGroup workerGroup;
+    private PooledByteBufAllocator pooledAllocator;
 
-  protected ClientCache() {
-    int nThreads =
-      conf.getInt(AngelConf.CLIENT_IO_THREAD, Math.max(8, (int)(Runtime.getRuntime().availableProcessors() * 0.25)));
-    IOMode ioMode = IOMode.valueOf(conf.get(AngelConf.NETWORK_IO_MODE, "NIO"));
-    workerGroup = NettyUtils.createEventLoop(ioMode, nThreads, "ML-client");
-    pooledAllocator = NettyUtils.createPooledByteBufAllocator(true, true, nThreads);
-    socketChannelClass = NettyUtils.getClientChannelClass(ioMode);
-  }
-
-  /**
-   * Construct & cache an IPC client with the user-provided SocketFactory if no cached client
-   * exists.
-   *
-   * @param conf    Configuration
-   * @param factory socket factory
-   * @return an IPC client
-   */
-  protected synchronized NettyTransceiver getClient(InetSocketAddress addr, SocketFactory factory,
-    Configuration conf) {
-    NettyTransceiver client = clients.get(addr.toString());
-    if (client == null) {
-      try {
-        int connectTimeoutMillis = (int) conf.getLong(AngelConf.ML_CONNECTION_TIMEOUT_MILLIS,
-          AngelConf.DEFAULT_CONNECTION_TIMEOUT_MILLIS);
-        client = new NettyTransceiver(conf, addr, workerGroup, pooledAllocator, socketChannelClass,
-          connectTimeoutMillis);
-      } catch (Exception e) {
-        LOG.debug("create NettyTransceiver client error: " + e);
-        throw new RuntimeException("create NettyTransceiver client error: ", e);
-      }
-
-      clients.put(addr.toString(), client);
-    } else {
-      client.incCount();
+    protected ClientCache() {
+        int nThreads = conf.getInt(AngelConf.CLIENT_IO_THREAD,
+                Runtime.getRuntime().availableProcessors() * 2);
+        IOMode ioMode = IOMode.valueOf(conf.get(AngelConf.NETWORK_IO_MODE, "NIO"));
+        workerGroup = NettyUtils.createEventLoop(ioMode, nThreads, "ML-client");
+        pooledAllocator = NettyUtils.createPooledByteBufAllocator(true, true, nThreads);
+        socketChannelClass = NettyUtils.getClientChannelClass(ioMode);
     }
-    return client;
-  }
 
-  /**
-   * Stop a RPC client connection A RPC client is closed only when its reference count becomes zero.
-   *
-   * @param client client to stop
-   */
-  protected void stopClient(NettyTransceiver client) {
-    synchronized (this) {
-      client.decCount();
-      if (client.isZeroReference()) {
-        clients.remove(client.getRemoteAddr().toString());
-      }
-    }
-    if (client.isZeroReference()) {
-      client.close();
-    }
-  }
+    /**
+     * Construct & cache an IPC client with the user-provided SocketFactory if no cached client
+     * exists.
+     *
+     * @param conf Configuration
+     * @param factory socket factory
+     * @return an IPC client
+     */
+    protected synchronized NettyTransceiver getClient(
+            InetSocketAddress addr,
+            SocketFactory factory,
+            Configuration conf) {
+        NettyTransceiver client = clients.get(addr.toString());
+        if (client == null) {
+            try {
+                int connectTimeoutMillis = (int) conf.getLong(AngelConf.ML_CONNECTION_TIMEOUT_MILLIS,
+                        AngelConf.DEFAULT_CONNECTION_TIMEOUT_MILLIS);
+                client = new NettyTransceiver(conf, addr, workerGroup, pooledAllocator,
+                        socketChannelClass, connectTimeoutMillis);
+            } catch (Exception e) {
+                LOG.debug("create NettyTransceiver client error: " + e);
+                throw new RuntimeException("create NettyTransceiver client error: ", e);
+            }
 
-  /**
-   * Clear all netty client in cache.
-   */
-  public void clear() {
-    synchronized (this) {
-      Iterator<Map.Entry<String, NettyTransceiver>> iter = clients.entrySet().iterator();
-      while (iter.hasNext()) {
-        NettyTransceiver client = iter.next().getValue();
-        client.close();
-        iter.remove();
-      }
+            clients.put(addr.toString(), client);
+        } else {
+            client.incCount();
+        }
+        return client;
     }
-  }
+
+    /**
+     * Stop a RPC client connection A RPC client is closed only when its reference count becomes zero.
+     *
+     * @param client client to stop
+     */
+    protected void stopClient(NettyTransceiver client) {
+        synchronized (this) {
+            client.decCount();
+            if (client.isZeroReference()) {
+                clients.remove(client.getRemoteAddr().toString());
+            }
+        }
+        if (client.isZeroReference()) {
+            client.close();
+        }
+    }
+
+    /**
+     * Clear all netty client in cache.
+     */
+    public void clear() {
+        synchronized (this) {
+            Iterator<Map.Entry<String, NettyTransceiver>> iter = clients.entrySet().iterator();
+            while (iter.hasNext()) {
+                NettyTransceiver client = iter.next().getValue();
+                client.close();
+                iter.remove();
+            }
+        }
+    }
 }
