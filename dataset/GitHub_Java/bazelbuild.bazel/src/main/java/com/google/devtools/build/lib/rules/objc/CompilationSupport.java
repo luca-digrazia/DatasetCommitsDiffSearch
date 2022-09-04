9a605d1c63f3b7cbaf4e0cb3bea0f061eebb9db6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 17:40:28 +0200

    Hook up objc's parse_header feature logic to toolchain's supports_header_parsing attribute
        RELNOTES: None
        PiperOrigin-RevId: 316635766

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
@@ -267,8 +267,7 @@ public class CompilationSupport {
       Artifact pchHdr,
       ObjcCppSemantics semantics,
       String purpose,
-      boolean generateModuleMap,
-      boolean shouldProcessHeaders)
+      boolean generateModuleMap)
       throws RuleErrorException, InterruptedException {
     CcCompilationHelper result =
         new CcCompilationHelper(
@@ -283,8 +282,7 @@ public class CompilationSupport {
                 fdoContext,
                 buildConfiguration,
                 TargetUtils.getExecutionInfo(
-                    ruleContext.getRule(), ruleContext.isAllowTagsPropagation()),
-                shouldProcessHeaders)
+                    ruleContext.getRule(), ruleContext.isAllowTagsPropagation()))
             .addSources(sources)
             .addPublicHeaders(publicHdrs)
             .addPublicTextualHeaders(objcCompilationContext.getPublicTextualHeaders())
@@ -394,8 +392,7 @@ public class CompilationSupport {
             pchHdr,
             semantics,
             purpose,
-            /* generateModuleMap= */ true,
-            /* shouldProcessHeaders= */ true);
+            /* generateModuleMap= */ true);
 
     purpose = String.format("%s_non_objc_arc", semantics.getPurpose());
     extensionBuilder.setArcEnabled(false);
@@ -414,9 +411,7 @@ public class CompilationSupport {
             semantics,
             purpose,
             // Only generate the module map once (see above) and re-use it here.
-            /* generateModuleMap= */ false,
-            // We only need to validate headers once, in arc compilation above.
-            /* shouldProcessHeaders= */ false);
+            /* generateModuleMap= */ false);
 
     FeatureConfiguration featureConfiguration =
         getFeatureConfiguration(ruleContext, ccToolchain, buildConfiguration);
