66f5a9bb0eec1ba39a3f6bac708107013b296ee6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:48:34 +0200

    Qute parser fix - tag end delimiter used in string literal in a tag
        - for example: messages.get("{foo}")

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/Parser.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/Parser.java
@@ -777,9 +777,7 @@ class Parser implements Function<String, Expression>, ParserHelper {
             for (String strParam : strParams) {
                 params.add(parseExpression(idGenerator, strParam.trim(), scope, origin));
             }
-            // Note that an expression may never start with a virtual method
-            String lastPartHint = strPartsIterator.hasNext() ? null : scope.getLastPartHint();
-            return new ExpressionImpl.VirtualMethodPartImpl(name, params, lastPartHint);
+            return new ExpressionImpl.VirtualMethodPartImpl(name, params);
         }
         // Try to parse the literal for bracket notation
         if (Expressions.isBracketNotation(value)) {
@@ -800,19 +798,15 @@ class Parser implements Function<String, Expression>, ParserHelper {
 
         String typeInfo = null;
         if (namespace != null) {
-            // If a namespace is used and it's the first part then prepend the value with the namespace
-            // For example foo -> inject:foo
             typeInfo = first != null ? value : namespace + NAMESPACE_SEPARATOR + value;
         } else if (first == null) {
-            // No namespace used and it's the first part
             // Try to find the binding type for the first part of the expression
             typeInfo = scope.getBinding(value);
         } else if (first.getTypeInfo() != null) {
-            // No namespace and not the first part
             typeInfo = value;
         }
         if (typeInfo != null && !strPartsIterator.hasNext() && scope.getLastPartHint() != null) {
-            // If the type info present then append hint to the last part
+            // If type info present then append hint to the last part
             typeInfo += scope.getLastPartHint();
         }
         return new ExpressionImpl.PartImpl(value, typeInfo);
