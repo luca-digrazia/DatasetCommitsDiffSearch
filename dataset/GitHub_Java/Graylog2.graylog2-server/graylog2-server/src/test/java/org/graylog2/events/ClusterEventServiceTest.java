c33c4ce9e245d878bba8211cf151a6dab71e1ad7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:01:52 +0200

    Try to make ClusterEventServiceTest more robust
        Instead of waiting for a hard-coded time, try to be more robust by using
        Awaitility's await() method in asynchronous tests.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/events/ClusterEventServiceTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/events/ClusterEventServiceTest.java
@@ -24,10 +24,12 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.eventbus.DeadEvent;
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import com.google.common.util.concurrent.Service;
 import com.google.common.util.concurrent.ServiceManager;
-import com.google.common.util.concurrent.Uninterruptibles;
+import com.jayway.awaitility.Duration;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb;
@@ -35,6 +37,7 @@ import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.Bytes;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
+import com.mongodb.WriteConcern;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.MongoConnection;
 import org.graylog2.database.MongoConnectionRule;
@@ -56,6 +59,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import static com.jayway.awaitility.Awaitility.await;
 import static com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb.InMemoryMongoRuleBuilder.newInMemoryMongoDbRule;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
@@ -138,15 +142,23 @@ public class ClusterEventServiceTest {
         assertThat(serviceManager.servicesByState().get(Service.State.RUNNING)).contains(clusterEventService);
         assertThat(clusterEventService.isRunning()).isTrue();
 
-        Uninterruptibles.sleepUninterruptibly(1L, TimeUnit.SECONDS);
+        await().atMost(Duration.FIVE_SECONDS).until(new Runnable() {
+            @Override
+            public void run() {
+                assertThat(collection.count()).isEqualTo(1L);
+            }
+        });
 
-        assertThat(collection.count()).isEqualTo(1L);
+        await().atMost(Duration.FIVE_SECONDS).until(new Runnable() {
+            @Override
+            public void run() {
+                DBObject dbObject = collection.findOne();
 
-        DBObject dbObject = collection.findOne();
-
-        @SuppressWarnings("unchecked")
-        final List<String> consumers = (List<String>) dbObject.get("consumers");
-        assertThat(consumers).containsExactly(nodeId.toString());
+                @SuppressWarnings("unchecked")
+                final List<String> consumers = (List<String>) dbObject.get("consumers");
+                assertThat(consumers).containsExactly(nodeId.toString());
+            }
+        });
 
         serviceManager
                 .stopAsync()
@@ -157,6 +169,61 @@ public class ClusterEventServiceTest {
     }
 
     @Test
+    @UsingDataSet(loadStrategy = LoadStrategyEnum.DELETE_ALL)
+    public void serverEventBusDispatchesTypedEvents() throws Exception {
+        final SimpleEventHandler handler = new SimpleEventHandler();
+        serverEventBus.register(handler);
+
+        DBObject event = new BasicDBObjectBuilder()
+                .add("date", "2015-04-01T00:00:00.000Z")
+                .add("producer", "TEST-PRODUCER")
+                .add("consumers", Collections.emptyList())
+                .add("event_class", SimpleEvent.class.getCanonicalName())
+                .add("payload", ImmutableMap.of("payload", "test"))
+                .get();
+        final DBCollection collection = mongoConnection.getDatabase().getCollection(ClusterEventService.COLLECTION_NAME);
+        assertThat(collection.save(event).getN()).isEqualTo(1);
+        assertThat(collection.count()).isEqualTo(1L);
+        assertThat(handler.invocations).isEqualTo(0);
+
+        ServiceManager serviceManager = new ServiceManager(Collections.singleton(clusterEventService));
+
+        serviceManager
+                .startAsync()
+                .awaitHealthy(1L, TimeUnit.SECONDS);
+
+        assertThat(serviceManager.servicesByState().get(Service.State.RUNNING)).contains(clusterEventService);
+        assertThat(clusterEventService.isRunning()).isTrue();
+
+        await().atMost(Duration.FIVE_SECONDS).until(new Runnable() {
+            @Override
+            public void run() {
+                assertThat(handler.invocations).isEqualTo(1);
+                assertThat(collection.count()).isEqualTo(1L);
+            }
+        });
+
+        await().atMost(Duration.FIVE_SECONDS).until(new Runnable() {
+            @Override
+            public void run() {
+                DBObject dbObject = collection.findOne();
+
+                @SuppressWarnings("unchecked")
+                final List<String> consumers = (List<String>) dbObject.get("consumers");
+                assertThat(consumers).containsExactly(nodeId.toString());
+            }
+        });
+
+        serviceManager
+                .stopAsync()
+                .awaitStopped(5L, TimeUnit.SECONDS);
+
+        verify(serverEventBus, times(1)).post(any(SimpleEvent.class));
+        verify(clusterEventBus, never()).post(any());
+    }
+
+    @Test
+    @UsingDataSet(loadStrategy = LoadStrategyEnum.DELETE_ALL)
     public void testRun() throws Exception {
         DBObject event = new BasicDBObjectBuilder()
                 .add("date", "2015-04-01T00:00:00.000Z")
@@ -179,15 +246,23 @@ public class ClusterEventServiceTest {
         assertThat(serviceManager.servicesByState().get(Service.State.RUNNING)).contains(clusterEventService);
         assertThat(clusterEventService.isRunning()).isTrue();
 
-        Uninterruptibles.sleepUninterruptibly(1L, TimeUnit.SECONDS);
+        await().atMost(Duration.FIVE_SECONDS).until(new Runnable() {
+            @Override
+            public void run() {
+                assertThat(collection.count()).isEqualTo(1L);
+            }
+        });
 
-        assertThat(collection.count()).isEqualTo(1L);
+        await().atMost(Duration.FIVE_SECONDS).until(new Runnable() {
+            @Override
+            public void run() {
+                DBObject dbObject = collection.findOne();
 
-        DBObject dbObject = collection.findOne();
-
-        @SuppressWarnings("unchecked")
-        final List<String> consumers = (List<String>) dbObject.get("consumers");
-        assertThat(consumers).containsExactly(nodeId.toString());
+                @SuppressWarnings("unchecked")
+                final List<String> consumers = (List<String>) dbObject.get("consumers");
+                assertThat(consumers).containsExactly(nodeId.toString());
+            }
+        });
 
         serviceManager
                 .stopAsync()
@@ -221,18 +296,32 @@ public class ClusterEventServiceTest {
     }
 
     @Test
+    @UsingDataSet(loadStrategy = LoadStrategyEnum.DELETE_ALL)
+    public void publishClusterEventSkipsDeadEvent() throws Exception {
+        DBCollection collection = mongoConnection.getDatabase().getCollection(ClusterEventService.COLLECTION_NAME);
+        DeadEvent event = new DeadEvent(clusterEventBus, new SimpleEvent("test"));
+
+        assertThat(collection.count()).isEqualTo(0L);
+
+        clusterEventService.publishClusterEvent(event);
+
+        verify(clusterEventBus, never()).post(any());
+        assertThat(collection.count()).isEqualTo(0L);
+    }
+
+    @Test
     public void prepareCollectionCreatesIndexesOnExistingCollection() throws Exception {
         DBCollection original = mongoConnection.getDatabase().createCollection(ClusterEventService.COLLECTION_NAME, null);
         original.dropIndexes();
         assertThat(original.getName()).isEqualTo(ClusterEventService.COLLECTION_NAME);
         assertThat(original.getIndexInfo()).hasSize(1);
 
-
         DBCollection collection = ClusterEventService.prepareCollection(mongoConnection);
         assertThat(collection.getName()).isEqualTo(ClusterEventService.COLLECTION_NAME);
         assertThat(collection.getIndexInfo()).hasSize(3);
         assertThat(collection.getOptions() & Bytes.QUERYOPTION_AWAITDATA).isEqualTo(Bytes.QUERYOPTION_AWAITDATA);
         assertThat(collection.getOptions() & Bytes.QUERYOPTION_TAILABLE).isEqualTo(Bytes.QUERYOPTION_TAILABLE);
+        assertThat(collection.getWriteConcern()).isEqualTo(WriteConcern.MAJORITY);
     }
 
     @Test
@@ -245,5 +334,15 @@ public class ClusterEventServiceTest {
         assertThat(collection.getIndexInfo()).hasSize(3);
         assertThat(collection.getOptions() & Bytes.QUERYOPTION_AWAITDATA).isEqualTo(Bytes.QUERYOPTION_AWAITDATA);
         assertThat(collection.getOptions() & Bytes.QUERYOPTION_TAILABLE).isEqualTo(Bytes.QUERYOPTION_TAILABLE);
+        assertThat(collection.getWriteConcern()).isEqualTo(WriteConcern.MAJORITY);
+    }
+
+    public static class SimpleEventHandler {
+        public volatile int invocations = 0;
+
+        @Subscribe
+        public void handleSimpleEvent(SimpleEvent event) {
+            invocations++;
+        }
     }
 }
