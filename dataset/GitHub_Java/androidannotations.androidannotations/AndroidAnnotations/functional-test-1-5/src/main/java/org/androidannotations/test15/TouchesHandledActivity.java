51ff3255364f9e868bd315efc3518e039b04c5ea
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:49:21 +0200

    add test for subclasses in @Touch

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/org/androidannotations/test15/TouchesHandledActivity.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/org/androidannotations/test15/TouchesHandledActivity.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2013 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,11 +15,12 @@
  */
 package org.androidannotations.test15;
 
-import android.view.MotionEvent;
-import android.view.View;
-
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Touch;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 @EActivity(R.layout.clickable_widgets)
 public class TouchesHandledActivity extends EventsHandledAbstractActivity {
@@ -28,35 +29,45 @@ public class TouchesHandledActivity extends EventsHandledAbstractActivity {
 	public void conventionButton(MotionEvent evt) {
 		conventionButtonEventHandled = true;
 	}
-	
+
 	@Touch
 	public void snakeCaseButton(MotionEvent evt) {
 		snakeCaseButtonEventHandled = true;
-	}	
-	
+	}
+
 	@Touch
 	public void extendedConventionButtonTouched(MotionEvent evt) {
 		extendedConventionButtonEventHandled = true;
 	}
-	
+
 	@Touch(R.id.configurationOverConventionButton)
 	public void overridenConventionButton(MotionEvent evt) {
 		overridenConventionButtonEventHandled = true;
 	}
-	
+
 	public void unboundButton(MotionEvent evt) {
 		unboundButtonEventHandled = true;
 	}
-	
+
 	@Touch
 	public void buttonWithViewArgument(MotionEvent evt, View viewArgument) {
 		this.viewArgument = viewArgument;
 	}
 
-	@Touch({R.id.button1, R.id.button2})
-	public void multipleButtonWithViewArgument(MotionEvent evt, View viewArgument) {
+	@Touch
+	public void buttonWithButtonArgument(MotionEvent evt, Button button) {
+		viewArgument = button;
+	}
+
+	@Touch
+	public void buttonWithOnlyViewArgument(View viewArgument) {
+	}
+
+	@Touch({ R.id.button1, R.id.button2 })
+	public void multipleButtonWithViewArgument(MotionEvent evt,
+			View viewArgument) {
 		this.viewArgument = viewArgument;
 		multipleButtonsEventHandled = true;
 	}
-	
+
 }
