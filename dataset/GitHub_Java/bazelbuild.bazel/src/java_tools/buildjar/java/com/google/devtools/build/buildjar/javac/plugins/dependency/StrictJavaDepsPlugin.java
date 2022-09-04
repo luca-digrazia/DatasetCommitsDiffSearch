bb9950552903bed42b2650f777d2dc71bfeb6b26
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:15:36 +0200

    Consolidate SJD exemptions
        PiperOrigin-RevId: 217736958

--- dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/plugins/dependency/StrictJavaDepsPlugin.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/plugins/dependency/StrictJavaDepsPlugin.java
@@ -32,8 +32,6 @@ import com.sun.tools.javac.code.Symbol.ClassSymbol;
 import com.sun.tools.javac.comp.AttrContext;
 import com.sun.tools.javac.comp.Env;
 import com.sun.tools.javac.main.JavaCompiler;
-import com.sun.tools.javac.resources.CompilerProperties.Errors;
-import com.sun.tools.javac.resources.CompilerProperties.Warnings;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeInfo;
 import com.sun.tools.javac.tree.TreeScanner;
@@ -176,10 +174,10 @@ public final class StrictJavaDepsPlugin extends BlazeJavaCompilerPlugin {
       try {
         switch (dependencyModule.getStrictJavaDeps()) {
           case ERROR:
-            log.error(diagnostic.pos(), Errors.ProcMessager(diagnostic.message()));
+            log.error(diagnostic.pos(), "proc.messager", diagnostic.message());
             break;
           case WARN:
-            log.warning(diagnostic.pos(), Warnings.ProcMessager(diagnostic.message()));
+            log.warning(diagnostic.pos(), "proc.messager", diagnostic.message());
             break;
           case OFF: // continue below
         }
