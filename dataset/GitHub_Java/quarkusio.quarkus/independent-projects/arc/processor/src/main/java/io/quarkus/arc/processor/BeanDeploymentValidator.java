d7e723206c1aa0c78ae2944444d15985ad1d0b15
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:32:49 +0200

    ArC - take away deprecated stuff marked for removal after v1.4

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanDeploymentValidator.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanDeploymentValidator.java
@@ -1,43 +1,44 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package io.quarkus.arc.processor;
 
-package org.jboss.quarkus.arc.processor;
-
+import java.util.List;
 import javax.enterprise.inject.spi.DeploymentException;
 
 /**
- * Makes it possible to validate the bean deployment.
+ * Makes it possible to validate the bean deployment and also to skip some validation rules for specific components.
  *
  * @author Martin Kouba
  */
 public interface BeanDeploymentValidator extends BuildExtension {
 
     /**
-     * At this point, all beans/observers are registered. This method should call {@link ValidationContext#addDeploymentProblem(Throwable)} if validation fails.
+     * At this point, all beans/observers are registered. This method should call
+     * {@link ValidationContext#addDeploymentProblem(Throwable)} if validation fails.
      *
      * @see Key#INJECTION_POINTS
      * @see Key#BEANS
      * @see Key#OBSERVERS
      * @see DeploymentException
      */
-    void validate(ValidationContext validationContext);
+    default void validate(ValidationContext context) {
+    }
 
     interface ValidationContext extends BuildContext {
 
         void addDeploymentProblem(Throwable t);
+
+        List<Throwable> getDeploymentProblems();
+
+        /**
+         * 
+         * @return a new stream of beans that form the deployment
+         */
+        BeanStream beans();
+
+        /**
+         * 
+         * @return a new stream of beans that are considered {@code unused} and were removed from the deployment
+         */
+        BeanStream removedBeans();
 
     }
 
