27d548c14a35ef9b05d2c71f6011604e41feb57c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:23:25 +0200

    avoid running out of memory by not starting two new threads every second

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/periodical/MasterCacheWorkerThread.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/periodical/MasterCacheWorkerThread.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2012-2014 TORCH GmbH
+/**
+ * Copyright 2013 Lennart Koopmann <lennart@socketfeed.com>
  *
  * This file is part of Graylog2.
  *
@@ -15,12 +15,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package org.graylog2.periodical;
 
 import com.codahale.metrics.Meter;
-import com.codahale.metrics.MetricRegistry;
-import com.google.inject.Inject;
 import org.graylog2.inputs.Cache;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.buffers.Buffer;
@@ -40,17 +39,10 @@ public class MasterCacheWorkerThread extends Periodical {
     private Meter writtenMessages;
     private Meter outOfCapacity;
 
-    private final MetricRegistry metricRegistry;
-
-    @Inject
-    public MasterCacheWorkerThread(MetricRegistry metricRegistry) {
-        this.metricRegistry = metricRegistry;
-    }
-
     @Override
     public void run() {
-        writtenMessages = metricRegistry.meter(name(MasterCacheWorkerThread.class, "writtenMessages"));
-        outOfCapacity =  metricRegistry.meter(name(MasterCacheWorkerThread.class, "FailedWritesOutOfCapacity"));
+        writtenMessages = core.metrics().meter(name(MasterCacheWorkerThread.class, "writtenMessages"));
+        outOfCapacity =  core.metrics().meter(name(MasterCacheWorkerThread.class, "FailedWritesOutOfCapacity"));
 
         new Thread(new Runnable() {
             @Override
