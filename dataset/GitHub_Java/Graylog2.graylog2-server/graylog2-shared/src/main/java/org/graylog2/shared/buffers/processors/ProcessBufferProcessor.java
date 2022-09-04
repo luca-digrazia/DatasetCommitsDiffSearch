bb8f88413b6a7733ad6d061c390e3a56fa6aad7e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:44:40 +0200

    Avoid collection allocation and iteration when possible.
        This adds a few more null checks but avoids collection allocation and
        iteration in a few places.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/buffers/processors/ProcessBufferProcessor.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/buffers/processors/ProcessBufferProcessor.java
@@ -1,105 +1,95 @@
 /**
- * Copyright 2012 Lennart Koopmann <lennart@socketfeed.com>
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.graylog2.shared.buffers.processors;
 
 import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
-import com.lmax.disruptor.EventHandler;
-import org.graylog2.plugin.GraylogServer;
-import org.graylog2.shared.ProcessingHost;
+import com.lmax.disruptor.WorkHandler;
+import org.graylog2.plugin.Message;
+import org.graylog2.plugin.buffers.MessageEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.graylog2.plugin.buffers.MessageEvent;
-import org.graylog2.plugin.filters.MessageFilter;
-import org.graylog2.plugin.Message;
+
+import java.util.Collection;
 
 import static com.codahale.metrics.MetricRegistry.name;
 
 /**
  * @author Lennart Koopmann <lennart@socketfeed.com>
  */
-public class ProcessBufferProcessor implements EventHandler<MessageEvent> {
-
+public abstract class ProcessBufferProcessor implements WorkHandler<MessageEvent> {
     private static final Logger LOG = LoggerFactory.getLogger(ProcessBufferProcessor.class);
 
-    private GraylogServer server;
     private final Meter incomingMessages;
     private final Timer processTime;
-    private final Meter filteredOutMessages;
     private final Meter outgoingMessages;
 
-    private final long ordinal;
-    private final long numberOfConsumers;
-    
-    public ProcessBufferProcessor(GraylogServer server, final long ordinal, final long numberOfConsumers) {
-        this.ordinal = ordinal;
-        this.numberOfConsumers = numberOfConsumers;
-        this.server = server;
+    protected final MetricRegistry metricRegistry;
+    private DecodingProcessor decodingProcessor;
 
-        incomingMessages = server.metrics().meter(name(ProcessBufferProcessor.class, "incomingMessages"));
-        outgoingMessages = server.metrics().meter(name(ProcessBufferProcessor.class, "outgoingMessages"));
-        filteredOutMessages = server.metrics().meter(name(ProcessBufferProcessor.class, "filteredOutMessages"));
-        processTime = server.metrics().timer(name(ProcessBufferProcessor.class, "processTime"));
+    public ProcessBufferProcessor(MetricRegistry metricRegistry) {
+        this.metricRegistry = metricRegistry;
+
+        incomingMessages = metricRegistry.meter(name(ProcessBufferProcessor.class, "incomingMessages"));
+        outgoingMessages = metricRegistry.meter(name(ProcessBufferProcessor.class, "outgoingMessages"));
+        processTime = metricRegistry.timer(name(ProcessBufferProcessor.class, "processTime"));
     }
 
     @Override
-    public void onEvent(MessageEvent event, long sequence, boolean endOfBatch) throws Exception {
-        // Because Trisha said so. (http://code.google.com/p/disruptor/wiki/FrequentlyAskedQuestions)
-        if ((sequence % numberOfConsumers) != ordinal) {
-            return;
+    public void onEvent(MessageEvent event) throws Exception {
+        // Decode the RawMessage to a Message object. The DecodingProcessor used to be a separate handler in the
+        // ProcessBuffer. Due to performance problems discovered during 1.0.0 testing, we decided to move this here.
+        // TODO The DecodingProcessor does not need to be a EventHandler. We decided to do it like this to keep the change as small as possible for 1.0.0.
+        decodingProcessor.onEvent(event, 0L, false);
+
+        if (event.isSingleMessage()) {
+            dispatchMessage(event.getMessage());
+        } else {
+            final Collection<Message> messageList = event.getMessages();
+            if (messageList == null) {
+                // skip message events which could not be decoded properly
+                return;
+            }
+
+            for (final Message message : messageList) {
+                dispatchMessage(message);
+            }
         }
+    }
 
-        server.processBufferWatermark().decrementAndGet();
-        
+    private void dispatchMessage(final Message msg) {
         incomingMessages.mark();
-        final Timer.Context tcx = processTime.time();
-
-        Message msg = event.getMessage();
 
         LOG.debug("Starting to process message <{}>.", msg.getId());
 
-        for (MessageFilter filter : server.getFilters()) {
-            Timer timer = server.metrics().timer(name(filter.getClass(), "executionTime"));
-            final Timer.Context timerContext = timer.time();
-
-            try {
-                LOG.debug("Applying filter [{}] on message <{}>.", filter.getName(), msg.getId());
-
-                if (filter.filter(msg, server)) {
-                    LOG.debug("Filter [{}] marked message <{}> to be discarded. Dropping message.", filter.getName(), msg.getId());
-                    filteredOutMessages.mark();
-                    return;
-                }
-            } catch (Exception e) {
-                LOG.error("Could not apply filter [" + filter.getName() +"] on message <" + msg.getId() +">: ", e);
-            } finally {
-                timerContext.stop();
-            }
+        try (final Timer.Context ignored = processTime.time()) {
+            LOG.debug("Finished processing message <{}>. Writing to output buffer.", msg.getId());
+            handleMessage(msg);
+        } catch (Exception e) {
+            LOG.warn("Unable to process message <{}>: {}", msg.getId(), e);
+        } finally {
+            outgoingMessages.mark();
         }
-
-        LOG.debug("Finished processing message. Writing to output buffer.");
-        server.getOutputBuffer().insertCached(msg, null);
-        
-        outgoingMessages.mark();
-        tcx.stop();
     }
 
+    protected abstract void handleMessage(Message msg);
+
+    public void setDecodingProcessor(DecodingProcessor decodingProcessor) {
+        this.decodingProcessor = decodingProcessor;
+    }
 }
