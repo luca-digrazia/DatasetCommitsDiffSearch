8ed715f00ca7ce66c72f141ac7fc8ae9cf74cc31
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:59:00 +0200

    Use correct Preconditions class in PluginLoader

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/plugins/PluginLoader.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/plugins/PluginLoader.java
@@ -1,43 +1,47 @@
-/*
- * Copyright 2012-2014 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.shared.plugins;
 
-import com.google.common.collect.Sets;
+import com.google.common.base.Function;
+import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Iterables;
 import org.graylog2.plugin.Plugin;
+import org.graylog2.plugin.PluginMetaData;
+import org.graylog2.plugin.PluginModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Properties;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.ServiceLoader;
 import java.util.Set;
 
-public class PluginLoader {
-    private static final Logger log = LoggerFactory.getLogger(PluginLoader.class);
+import static com.google.common.base.Preconditions.checkNotNull;
 
-    public static final String GRAYLOG2_PLUGIN_PROPERTIES = "graylog2-plugin.properties";
+public class PluginLoader {
+    private static final Logger LOG = LoggerFactory.getLogger(PluginLoader.class);
 
     private final File pluginDir;
 
@@ -46,111 +50,121 @@ public class PluginLoader {
     }
 
     public Set<Plugin> loadPlugins() {
-        final HashSet<Plugin> plugins = Sets.newHashSet();
-
-        plugins.addAll(loadClassPathPlugins());
-        plugins.addAll(loadJarPlugins());
-
-        return plugins;
+        return ImmutableSortedSet.orderedBy(new PluginComparator())
+                .addAll(Iterables.transform(loadJarPlugins(), new PluginAdapterFunction()))
+                .addAll(Iterables.transform(loadClassPathPlugins(), new PluginAdapterFunction()))
+                .build();
     }
 
-    private Set<Plugin> loadClassPathPlugins() {
-        final HashSet<Plugin> plugins = Sets.newHashSet();
-        final ClassLoader cl = getClass().getClassLoader();
-        final Set<Class<? extends Plugin>> classes = loadPluginClasses(cl);
-        for (Class<? extends Plugin> pluginClass : classes) {
-            final Plugin plugin = instantiatePlugin(pluginClass);
-            if (plugin != null) {
-                plugins.add(plugin);
-            }
-        }
-
-        return plugins;
+    private Iterable<Plugin> loadClassPathPlugins() {
+        return ServiceLoader.load(Plugin.class);
     }
 
-    public Set<Plugin> loadJarPlugins() {
-        final HashSet<Plugin> plugins = Sets.newHashSet();
-
+    private Iterable<Plugin> loadJarPlugins() {
         if (!pluginDir.exists()) {
-            log.warn("Plugin directory {} does not exist, not loading plugins.", pluginDir.getAbsolutePath());
-            return plugins;
+            LOG.warn("Plugin directory {} does not exist, not loading plugins.", pluginDir.getAbsolutePath());
+            return Collections.emptySet();
         }
+
         if (!pluginDir.isDirectory()) {
-            log.warn("Path {} is not a directory, cannot load plugins.", pluginDir);
-            return plugins;
+            LOG.warn("Path {} is not a directory, cannot load plugins.", pluginDir);
+            return Collections.emptySet();
         }
 
-        final ClassLoader classLoader = getClass().getClassLoader();
-
-        log.debug("Scanning directory <{}> for plugins...", pluginDir.getAbsolutePath());
+        LOG.debug("Scanning directory <{}> for plugins...", pluginDir.getAbsolutePath());
         final File[] files = pluginDir.listFiles();
-        log.debug("Loading [{}] plugins", files.length);
+        if (files == null) {
+            LOG.warn("Could not list files in {}, cannot load plugins.", pluginDir);
+            return Collections.emptySet();
+        }
+
+        LOG.debug("Loading [{}] plugins", files.length);
+        final ClassLoader classLoader = getClass().getClassLoader();
+        final ImmutableSet.Builder<Plugin> plugins = ImmutableSet.builder();
         for (File jar : files) {
-            try {
-                log.debug("Loading <" + jar.getAbsolutePath() + ">");
-                final URLClassLoader pluginClassLoader = new URLClassLoader(new URL[]{ jar.toURI().toURL() }, classLoader);
+            if (!jar.isFile()) {
+                LOG.debug("{} is not a file, skipping.", jar);
+            } else {
+                try {
+                    LOG.debug("Loading <" + jar.getAbsolutePath() + ">");
+                    final ClassLoader pluginClassLoader = new URLClassLoader(new URL[]{jar.toURI().toURL()}, classLoader);
+                    final ServiceLoader<Plugin> pluginServiceLoader = ServiceLoader.load(Plugin.class, pluginClassLoader);
 
-                final Set<Class<? extends Plugin>> pluginClasses = loadPluginClasses(pluginClassLoader);
-                for (Class<? extends Plugin> pluginClass : pluginClasses) {
-                    log.debug("Found plugin " + pluginClass);
-                    final Plugin plugin = instantiatePlugin(pluginClass);
-                    if (plugin != null) {
-                        plugins.add(plugin);
-                    }
+                    plugins.addAll(pluginServiceLoader);
+                } catch (MalformedURLException e) {
+                    LOG.error("Cannot open JAR file for discovering plugins", e);
                 }
-            } catch (MalformedURLException e) {
-                log.error("Cannot open jar for discovering plugins", e);
             }
         }
 
-        return plugins;
+        return plugins.build();
     }
 
-    @SuppressWarnings("unchecked")
-    private Set<Class<? extends Plugin>> loadPluginClasses(ClassLoader cl) {
-        Set<Class<? extends Plugin>> pluginClasses = Sets.newHashSet();
-        final Enumeration<URL> urls;
-        try {
-            urls = cl.getResources(GRAYLOG2_PLUGIN_PROPERTIES);
-        } catch (IOException e) {
-            log.error("Unable to read resources from class loader", e);
-            return pluginClasses;
+    public static class PluginComparator implements Comparator<Plugin> {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int compare(Plugin o1, Plugin o2) {
+            return ComparisonChain.start()
+                    .compare(o1.metadata().getUniqueId(), o2.metadata().getUniqueId())
+                    .compare(o1.metadata().getName(), o2.metadata().getName())
+                    .compare(o1.metadata().getVersion(), o2.metadata().getVersion())
+                    .result();
         }
-
-        while (urls.hasMoreElements()) {
-            final URL url = urls.nextElement();
-            final Properties properties = new Properties();
-            try {
-                final InputStream inputStream = url.openStream();
-                properties.load(inputStream);
-            } catch (IOException e) {
-                log.error("Unable to read plugin properties file", e);
-            }
-            final String pluginClassName = properties.getProperty("plugin");
-            log.debug("Plugin class name is {}", pluginClassName);
-            if (pluginClassName == null) {
-                log.error("Missing plugin property in property descriptor file: {}", url);
-                continue;
-            }
-            Class<? extends Plugin> pluginClass;
-            try {
-                pluginClass = (Class<? extends Plugin>) cl.loadClass(pluginClassName);
-                pluginClasses.add(pluginClass);
-            } catch (ClassNotFoundException e) {
-                log.error("Unable to find plugin class {}, skipping.", pluginClasses);
-            }
-        }
-        return pluginClasses;
     }
 
-    public Plugin instantiatePlugin(Class<? extends Plugin> pluginClass) {
-        final Plugin plugin;
-        try {
-            plugin = pluginClass.getConstructor().newInstance();
-        } catch (Exception e) {
-            log.error("Cannot find constructor for plugin " + pluginClass.getCanonicalName() + ". It must have a public no-args constructor.", e);
-            return null;
+    /**
+     * Adapter for {@link org.graylog2.plugin.Plugin} which implements {@link #equals(Object)} and {@link #hashCode()}
+     * only taking {@link org.graylog2.plugin.PluginMetaData#getUniqueId()} into account.
+     */
+    public static class PluginAdapter implements Plugin {
+        private final Plugin plugin;
+
+        public PluginAdapter(Plugin plugin) {
+            this.plugin = checkNotNull(plugin);
         }
-        return plugin;
+
+        @Override
+        public PluginMetaData metadata() {
+            return plugin.metadata();
+        }
+
+        @Override
+        public Collection<PluginModule> modules() {
+            return plugin.modules();
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(plugin.metadata().getUniqueId());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj instanceof Plugin) {
+                final Plugin that = (Plugin) obj;
+                return Objects.equals(this.metadata().getUniqueId(), that.metadata().getUniqueId());
+            }
+
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            final PluginMetaData metadata = plugin.metadata();
+            return metadata.getName() + " " + metadata.getVersion() + " [" + metadata.getUniqueId() + "]";
+        }
+    }
+
+    private static class PluginAdapterFunction implements Function<Plugin, Plugin> {
+        @Override
+        public Plugin apply(Plugin input) {
+            return new PluginAdapter(input);
+        }
     }
 }
