4a0586c1e67195bebdfe02092ed88685f652410d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:41:44 +0200

    [ISSUE-240] Optimize default partition size for sparse vector/matrix

--- dataset/GitHub_Java/Angel-ML.angel/angel-ps/core/src/main/java/com/tencent/angel/ps/PSPartitioner.java
+++ dataset/GitHub_Java/Angel-ML.angel/angel-ps/core/src/main/java/com/tencent/angel/ps/PSPartitioner.java
@@ -16,9 +16,9 @@
 
 package com.tencent.angel.ps;
 
-import com.tencent.angel.conf.AngelConfiguration;
+import com.tencent.angel.conf.AngelConf;
 import com.tencent.angel.ml.matrix.MatrixContext;
-import com.tencent.angel.protobuf.generated.MLProtos.*;
+import com.tencent.angel.protobuf.generated.MLProtos.Partition;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -33,6 +33,10 @@ import java.util.List;
  */
 public class PSPartitioner implements Partitioner{
   private static final Log LOG = LogFactory.getLog(PSPartitioner.class);
+  private static final long DEFAULT_PARTITION_SIZE_DENSE = 500000;
+  private static final long DEFAULT_PARTITION_SIZE_SPARSE_100000000 = 500000;
+  private static final long DEFAULT_PARTITION_SIZE_SPARSE_1000000000 = 5000000;
+  private static final long DEFAULT_PARTITION_SIZE_SPARSE_10000000000 = 50000000;
   protected MatrixContext mContext;
   protected Configuration conf;
 
@@ -48,30 +52,32 @@ public class PSPartitioner implements Partitioner{
     int id = 0;
     int matrixId = mContext.getId();
     int row = mContext.getRowNum();
-    int col = mContext.getColNum();
+    long col = mContext.getColNum();
+
+    long defaultPartSize = getDefaultPartSize();
 
     int blockRow = mContext.getMaxRowNumInBlock();
-    int blockCol = mContext.getMaxColNumInBlock();
+    long blockCol = mContext.getMaxColNumInBlock();
     if(blockRow == -1 || blockCol == -1) {
-      int serverNum = conf.getInt(AngelConfiguration.ANGEL_PS_NUMBER, AngelConfiguration.DEFAULT_ANGEL_PS_NUMBER);
+      int serverNum = conf.getInt(AngelConf.ANGEL_PS_NUMBER, AngelConf.DEFAULT_ANGEL_PS_NUMBER);
       if(row >= serverNum) {
-        blockRow = Math.min(row / serverNum, Math.max(1, 5000000 / col));
-        blockCol = Math.min(5000000 / blockRow, col);
+        blockRow = (int) Math.min(row / serverNum, Math.max(1, defaultPartSize / col));
+        blockCol = Math.min(defaultPartSize / blockRow, col);
       } else {
         blockRow = row;
-        blockCol = Math.min(5000000 / blockRow, Math.max(100, col / serverNum));
+        blockCol = Math.min(defaultPartSize / blockRow, Math.max(100, col / serverNum));
       }
     }
 
     LOG.info("blockRow = " + blockRow + ", blockCol=" + blockCol);
 
     Partition.Builder partition = Partition.newBuilder();
-    for (int i = 0; i < row; i += blockRow) {
-      for (int j = 0; j < col; j += blockCol) {
+    for (int i = 0; i < row; ) {
+      for (long j = 0; j < col; ) {
         int startRow = i;
-        int startCol = j;
-        int endRow = Math.min(i + blockRow, row);
-        int endCol = Math.min(j + blockCol, col);
+        long startCol = j;
+        int endRow = (i <= (row - blockRow)) ? (i + blockRow) : row;
+        long endCol = (j <= (col - blockCol)) ? (j + blockCol) : col;
         partition.setMatrixId(matrixId);
         partition.setPartitionId(id++);
         partition.setStartRow(startRow);
@@ -79,15 +85,41 @@ public class PSPartitioner implements Partitioner{
         partition.setEndRow(endRow);
         partition.setEndCol(endCol);
         array.add(partition.build());
+
+        j = (j <= (col - blockCol)) ? (j + blockCol) : col;
       }
+      i = (i <= (row - blockRow)) ? (i + blockRow) : row;
     }
     LOG.debug("partition count: " + array.size());
     return array;
   }
 
+  private long getDefaultPartSize() {
+    long maxSize = mContext.getColNum() * mContext.getRowNum();
+    switch (mContext.getRowType()) {
+      case T_DOUBLE_DENSE:
+      case T_FLOAT_DENSE:
+      case T_INT_DENSE:
+        return DEFAULT_PARTITION_SIZE_DENSE;
+
+      default:{
+        if(maxSize < 100000000) {
+          return DEFAULT_PARTITION_SIZE_SPARSE_100000000;
+        } else if(maxSize >= 100000000 && maxSize < 1000000000) {
+          return DEFAULT_PARTITION_SIZE_SPARSE_1000000000;
+        } else if(maxSize >= 1000000000 && maxSize < 10000000000L){
+          return DEFAULT_PARTITION_SIZE_SPARSE_10000000000;
+        } else {
+          int psNum = conf.getInt(AngelConf.ANGEL_PS_NUMBER, AngelConf.DEFAULT_ANGEL_PS_NUMBER);
+          return maxSize / psNum / 10;
+        }
+      }
+    }
+  }
+
   @Override
   public int assignPartToServer(int partId) {
-    int serverNum = conf.getInt(AngelConfiguration.ANGEL_PS_NUMBER, AngelConfiguration.DEFAULT_ANGEL_PS_NUMBER);
+    int serverNum = conf.getInt(AngelConf.ANGEL_PS_NUMBER, AngelConf.DEFAULT_ANGEL_PS_NUMBER);
     return partId % serverNum;
   }
 
