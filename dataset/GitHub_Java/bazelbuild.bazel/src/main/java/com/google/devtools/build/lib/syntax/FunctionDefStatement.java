b46b8b72216be17638736652f375a5aad0cdad43
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:17:22 +0200

    Add the flag --incompatible_keyword_only_syntax to forbid keyword-only syntax.
        Keyword-only syntax is when a parameter happens after the `*` paramter, e.g.
          def foo(a, *, b): pass
    
        or:
          def foo(a, *b, c): pass
    
        The syntax is not compatible with Python 2 (it's supported only in Python 3),
        and breaking tooling based on Python2.
    
        RELNOTES:
          Keyword-only syntax in a function definition is deprecated
          (e.g. `def foo(a, *, b)` or `def foo(a, *b, c)`) and will be removed in the future.
        PiperOrigin-RevId: 155071704

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/FunctionDefStatement.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/FunctionDefStatement.java
@@ -51,12 +51,12 @@ public class FunctionDefStatement extends Statement {
     }
 
     FunctionSignature sig = signature.getSignature();
-    if (env.getSemantics().incompatibleDisallowKeywordOnlyArgs
+    if (env.getSemantics().incompatibleKeywordOnlySyntax
         && sig.getShape().getMandatoryNamedOnly() > 0) {
       throw new EvalException(
           getLocation(),
           "Keyword-only argument is forbidden. You can temporarily disable this "
-              + "error using the flag --incompatible_disallow_keyword_only_args=false");
+              + "error using the flag --incompatible_keyword_only_syntax=false");
     }
 
     env.update(
