cde66a866c08533c41cd77d41cc3fe1c1a239191
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:11:12 +0200

    Add details to NoSuchJobException

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/system/jobs/SystemJobFactory.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/system/jobs/SystemJobFactory.java
@@ -1,20 +1,18 @@
-/*
- * Copyright 2012-2014 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.system.jobs;
 
@@ -22,10 +20,8 @@ import org.graylog2.indexer.healing.FixDeflectorByDeleteJob;
 import org.graylog2.indexer.healing.FixDeflectorByMoveJob;
 
 import javax.inject.Inject;
+import java.util.Locale;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
 public class SystemJobFactory {
     private final FixDeflectorByMoveJob.Factory fixDeflectorByMoveJobFactory;
     private final FixDeflectorByDeleteJob.Factory fixDeflectorByDeleteJobFactory;
@@ -38,14 +34,13 @@ public class SystemJobFactory {
     }
 
     public SystemJob build(String jobName) throws NoSuchJobException {
-        switch(SystemJob.Type.valueOf(jobName.toUpperCase())) {
+        switch (SystemJob.Type.valueOf(jobName.toUpperCase(Locale.ENGLISH))) {
             case FIX_DEFLECTOR_DELETE_INDEX:
                 return fixDeflectorByDeleteJobFactory.create();
             case FIX_DEFLECTOR_MOVE_INDEX:
                 return fixDeflectorByMoveJobFactory.create();
         }
 
-        throw new NoSuchJobException();
+        throw new NoSuchJobException("Unknown system job name \"" + jobName + "\"");
     }
-
 }
