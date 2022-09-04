c495ecd88a5686587bf45707aabd2f94373cd19b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:06:06 +0200

    Internal change
        PiperOrigin-RevId: 157878648

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/common/options/Converters.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/common/options/Converters.java
@@ -16,12 +16,10 @@ package com.google.devtools.common.options;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
-import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -180,47 +178,6 @@ public final class Converters {
   }
 
   /**
-   * Standard converter for the {@link java.time.Duration} type.
-   */
-  public static class DurationConverter implements Converter<Duration> {
-    private final Pattern durationRegex = Pattern.compile("^([0-9]+)(d|h|m|s|ms)$");
-
-    @Override
-    public Duration convert(String input) throws OptionsParsingException {
-      // To be compatible with the previous parser, '0' doesn't need a unit.
-      if ("0".equals(input)) {
-        return Duration.ZERO;
-      }
-      Matcher m = durationRegex.matcher(input);
-      if (!m.matches()) {
-        throw new OptionsParsingException("Illegal duration '" + input + "'.");
-      }
-      long duration = Long.parseLong(m.group(1));
-      String unit = m.group(2);
-      switch(unit) {
-        case "d":
-          return Duration.ofDays(duration);
-        case "h":
-          return Duration.ofHours(duration);
-        case "m":
-          return Duration.ofMinutes(duration);
-        case "s":
-          return Duration.ofSeconds(duration);
-        case "ms":
-          return Duration.ofMillis(duration);
-        default:
-          throw new IllegalStateException("This must not happen. Did you update the regex without "
-              + "the switch case?");
-      }
-    }
-
-    @Override
-    public String getTypeDescription() {
-      return "An immutable length of time.";
-    }
-  }
-
-  /**
    * The converters that are available to the options parser by default. These are used if the
    * {@code @Option} annotation does not specify its own {@code converter}, and its type is one of
    * the following.
@@ -228,14 +185,12 @@ public final class Converters {
   static final Map<Class<?>, Converter<?>> DEFAULT_CONVERTERS = Maps.newHashMap();
 
   static {
-    // 1:1 correspondence with UsesOnlyCoreTypes.CORE_TYPES.
     DEFAULT_CONVERTERS.put(String.class, new Converters.StringConverter());
     DEFAULT_CONVERTERS.put(int.class, new Converters.IntegerConverter());
     DEFAULT_CONVERTERS.put(long.class, new Converters.LongConverter());
     DEFAULT_CONVERTERS.put(double.class, new Converters.DoubleConverter());
     DEFAULT_CONVERTERS.put(boolean.class, new Converters.BooleanConverter());
     DEFAULT_CONVERTERS.put(TriState.class, new Converters.TriStateConverter());
-    DEFAULT_CONVERTERS.put(Duration.class, new Converters.DurationConverter());
     DEFAULT_CONVERTERS.put(Void.class, new Converters.VoidConverter());
   }
 
@@ -275,7 +230,9 @@ public final class Converters {
 
     @Override
     public List<String> convert(String input) {
-      return input.isEmpty() ? ImmutableList.of() : ImmutableList.copyOf(splitter.split(input));
+      return input.equals("")
+          ? ImmutableList.<String>of()
+          : ImmutableList.copyOf(splitter.split(input));
     }
 
     @Override
@@ -309,7 +266,9 @@ public final class Converters {
       try {
         int level = Integer.parseInt(input);
         return LEVELS[level];
-      } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+      } catch (NumberFormatException e) {
+        throw new OptionsParsingException("Not a log level: " + input);
+      } catch (ArrayIndexOutOfBoundsException e) {
         throw new OptionsParsingException("Not a log level: " + input);
       }
     }
