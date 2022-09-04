ce7079195ef1bf90139fbda4555ca12f48b77e2e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:04:21 +0200

    [#10432] Allow injection of a reactive session not wrapped in a Mutiny/CompletionStage

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-reactive/runtime/src/main/java/io/quarkus/hibernate/reactive/runtime/ReactiveSessionProducer.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-reactive/runtime/src/main/java/io/quarkus/hibernate/reactive/runtime/ReactiveSessionProducer.java
@@ -1,5 +1,8 @@
 package io.quarkus.hibernate.reactive.runtime;
 
+import java.util.concurrent.CompletionStage;
+
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
@@ -9,7 +12,9 @@ import org.hibernate.reactive.mutiny.Mutiny;
 import org.hibernate.reactive.stage.Stage;
 
 import io.quarkus.arc.DefaultBean;
+import io.smallrye.mutiny.Uni;
 
+@ApplicationScoped
 public class ReactiveSessionProducer {
 
     @Inject
@@ -21,15 +26,40 @@ public class ReactiveSessionProducer {
     @Produces
     @RequestScoped
     @DefaultBean
-    public Stage.Session createStageSession() {
+    public CompletionStage<Stage.Session> stageSession() {
         return reactiveSessionFactory.openSession();
     }
 
     @Produces
     @RequestScoped
     @DefaultBean
+    public Uni<Mutiny.Session> mutinySession() {
+        return mutinySessionFactory.openSession().cache();
+    }
+
+    public void disposeStageSession(@Disposes CompletionStage<Stage.Session> reactiveSession) {
+        reactiveSession.whenComplete((s, t) -> {
+            if (s != null)
+                s.close();
+        });
+    }
+
+    public void disposeMutinySession(@Disposes Uni<Mutiny.Session> reactiveSession) {
+        reactiveSession.subscribe().with(Mutiny.Session::close);
+    }
+
+    @Produces
+    @RequestScoped
+    @DefaultBean
+    public Stage.Session createStageSession() {
+        return reactiveSessionFactory.createSession();
+    }
+
+    @Produces
+    @RequestScoped
+    @DefaultBean
     public Mutiny.Session createMutinySession() {
-        return mutinySessionFactory.openSession();
+        return mutinySessionFactory.createSession();
     }
 
     public void disposeStageSession(@Disposes Stage.Session reactiveSession) {
