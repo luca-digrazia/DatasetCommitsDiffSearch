9bd51ffaaa646cb8b7361c54218ca38bb7f789ad
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:01:42 +0200

    Added handling for AlreadyColsedException and fixed executor cleanup     (cherry picked from commit 9e3060dd3580d2e027d8c91e263efb29401b72bb)

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/inputs/amqp/AMQPConsumer.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/inputs/amqp/AMQPConsumer.java
@@ -78,9 +78,6 @@ public class AMQPConsumer implements Runnable {
             case SYSLOG:
                 this.syslogProcessor = new SyslogProcessor(server);
                 break;
-            default:
-            	LOG.error("Unknown input type {} for queue {}", queueConfig.getInputType(), queueConfig.getQueueName());
-            	break;
         }
     }
     
@@ -221,8 +218,6 @@ public class AMQPConsumer implements Runnable {
 
                              handledSyslogMessages.mark();
                              break;
-                         default:
-                        	 LOG.error("Unknown input type {} for queue {}", queueConfig.getInputType(), queueConfig.getQueueName());
                      }
                      
                      channel.basicAck(envelope.getDeliveryTag(), false);
