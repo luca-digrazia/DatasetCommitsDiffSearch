932d0c0df90643516080212e91487a847460394d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:04:14 +0200

    Also consider @ValidateOnExecution as a potential trigger for validation
        It's used in CDI to mark beans that should be considered for validation
        even if they don't have constraints proper: constraints may be in the
        hierarchy.

--- dataset/GitHub_Java/quarkusio.quarkus/bean-validation/deployment/src/main/java/org/jboss/shamrock/beanvalidation/BeanValidationProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/bean-validation/deployment/src/main/java/org/jboss/shamrock/beanvalidation/BeanValidationProcessor.java
@@ -25,8 +25,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.validation.Constraint;
-import javax.validation.Valid;
-import javax.validation.executable.ValidateOnExecution;
 
 import org.hibernate.validator.internal.metadata.core.ConstraintHelper;
 import org.hibernate.validator.messageinterpolation.AbstractMessageInterpolator;
@@ -35,7 +33,6 @@ import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.IndexView;
-import org.jboss.jandex.Type;
 import org.jboss.shamrock.annotations.BuildProducer;
 import org.jboss.shamrock.annotations.BuildStep;
 import org.jboss.shamrock.annotations.Record;
@@ -45,7 +42,6 @@ import org.jboss.shamrock.deployment.builditem.AdditionalBeanBuildItem;
 import org.jboss.shamrock.deployment.builditem.CombinedIndexBuildItem;
 import org.jboss.shamrock.deployment.builditem.HotDeploymentConfigFileBuildItem;
 import org.jboss.shamrock.deployment.builditem.InjectionFactoryBuildItem;
-import org.jboss.shamrock.deployment.builditem.SystemPropertyBuildItem;
 import org.jboss.shamrock.deployment.builditem.substrate.ReflectiveClassBuildItem;
 import org.jboss.shamrock.deployment.builditem.substrate.ReflectiveFieldBuildItem;
 import org.jboss.shamrock.deployment.builditem.substrate.ReflectiveMethodBuildItem;
@@ -54,15 +50,7 @@ import org.jboss.shamrock.deployment.recording.RecorderContext;
 
 class BeanValidationProcessor {
 
-    private static final DotName VALIDATE_ON_EXECUTION = DotName.createSimple(ValidateOnExecution.class.getName());
-
-    private static final DotName VALID = DotName.createSimple(Valid.class.getName());
-
-    @BuildStep
-    SystemPropertyBuildItem disableJavaFXIntegrations() {
-        // Bug in GraalVM rc10: see https://github.com/oracle/graal/issues/851
-        return new SystemPropertyBuildItem("org.hibernate.validator.force-disable-javafx-integration", "true");
-    }
+    private static final DotName VALIDATE_ON_EXECUTION = DotName.createSimple("javax.validation.executable.ValidateOnExecution");
 
     @BuildStep
     HotDeploymentConfigFileBuildItem configFile() {
@@ -94,35 +82,27 @@ class BeanValidationProcessor {
             consideredAnnotations.add(constraint.target().asClass().name());
         }
 
-        // Also consider elements that are marked with @Valid
-        consideredAnnotations.add(VALID);
-
         // Also consider elements that are marked with @ValidateOnExecution
         consideredAnnotations.add(VALIDATE_ON_EXECUTION);
 
         Set<Class<?>> classesToBeValidated = new HashSet<>();
 
-        for (DotName consideredAnnotation : consideredAnnotations) {
-            Collection<AnnotationInstance> annotationInstances = indexView.getAnnotations(consideredAnnotation);
+        for (DotName constraint : consideredAnnotations) {
+            Collection<AnnotationInstance> annotationInstances = indexView.getAnnotations(constraint);
 
             for (AnnotationInstance annotation : annotationInstances) {
                 if (annotation.target().kind() == AnnotationTarget.Kind.FIELD) {
-                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asField().declaringClass().name());
+                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asField().declaringClass());
                     reflectiveFields.produce(new ReflectiveFieldBuildItem(annotation.target().asField()));
-                    contributeClassMarkedForCascadingValidation(classesToBeValidated, recorder, indexView, consideredAnnotation, annotation.target().asField().type());
                 } else if (annotation.target().kind() == AnnotationTarget.Kind.METHOD) {
-                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asMethod().declaringClass().name());
+                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asMethod().declaringClass());
                     // we need to register the method for reflection as it could be a getter
                     reflectiveMethods.produce(new ReflectiveMethodBuildItem(annotation.target().asMethod()));
-                    contributeClassMarkedForCascadingValidation(classesToBeValidated, recorder, indexView, consideredAnnotation, annotation.target().asMethod().returnType());
                 } else if (annotation.target().kind() == AnnotationTarget.Kind.METHOD_PARAMETER) {
-                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asMethodParameter().method().declaringClass().name());
+                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asMethodParameter().method().declaringClass());
                     // a getter does not have parameters so it's a pure method: no need for reflection in this case
-                    contributeClassMarkedForCascadingValidation(classesToBeValidated, recorder, indexView, consideredAnnotation,
-                            // FIXME this won't work in the case of synthetic parameters
-                            annotation.target().asMethodParameter().method().parameters().get(annotation.target().asMethodParameter().position()));
                 } else if (annotation.target().kind() == AnnotationTarget.Kind.CLASS) {
-                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asClass().name());
+                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asClass());
                     // no need for reflection in the case of a class level constraint
                 }
             }
@@ -147,9 +127,9 @@ class BeanValidationProcessor {
         }
     }
 
-    private static void contributeClass(Set<Class<?>> classCollector, RecorderContext recorder, IndexView indexView, DotName className) {
-        classCollector.add(recorder.classProxy(className.toString()));
-        for (ClassInfo subclass : indexView.getAllKnownSubclasses(className)) {
+    private static void contributeClass(Set<Class<?>> classCollector, RecorderContext recorder, IndexView indexView, ClassInfo classInfo) {
+        classCollector.add(recorder.classProxy(classInfo.name().toString()));
+        for (ClassInfo subclass : indexView.getAllKnownSubclasses(classInfo.name())) {
             if (Modifier.isAbstract(subclass.flags())) {
                 // we can avoid adding the abstract classes here: either they are parent classes
                 // and they will be dealt with by Hibernate Validator or they are child classes
@@ -157,30 +137,6 @@ class BeanValidationProcessor {
                 continue;
             }
             classCollector.add(recorder.classProxy(subclass.name().toString()));
-        }
-    }
-
-    private static void contributeClassMarkedForCascadingValidation(Set<Class<?>> classCollector, RecorderContext recorder, IndexView indexView,
-            DotName consideredAnnotation, Type type) {
-        if (VALID != consideredAnnotation) {
-            return;
-        }
-
-        DotName className = getClassName(type);
-        if (className != null) {
-            contributeClass(classCollector, recorder, indexView, className);
-        }
-    }
-
-    private static DotName getClassName(Type type) {
-        switch (type.kind()) {
-        case CLASS:
-        case PARAMETERIZED_TYPE:
-            return type.name();
-        case ARRAY:
-            return getClassName( type.asArrayType().component() );
-        default:
-            return null;
         }
     }
 }
