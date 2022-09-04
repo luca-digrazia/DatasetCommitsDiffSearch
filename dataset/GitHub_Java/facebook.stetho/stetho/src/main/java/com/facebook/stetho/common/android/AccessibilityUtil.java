dc90bc900545b43234de83a1ba54438bb8b30141
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:02:06 +0200

    Added ignoredReasons to accessibility inspection

--- dataset/GitHub_Java/facebook.stetho/stetho/src/main/java/com/facebook/stetho/common/android/AccessibilityUtil.java
+++ dataset/GitHub_Java/facebook.stetho/stetho/src/main/java/com/facebook/stetho/common/android/AccessibilityUtil.java
@@ -1,12 +1,17 @@
 /*
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
 
 package com.facebook.stetho.common.android;
 
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +22,6 @@ import android.widget.ScrollView;
 import android.widget.Spinner;
 
 import java.util.List;
-
-import androidx.annotation.Nullable;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
 
 /**
  * This class provides utility methods for determining certain accessibility properties of
