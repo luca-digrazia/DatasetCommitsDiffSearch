7be886ea7046d64f9780acf587c781e79c5e1356
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:23:36 +0200

    dropwizard-testing: Add Jersey Client configurability to ResourceTestRule

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/io/dropwizard/testing/junit/ResourceTestRule.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/io/dropwizard/testing/junit/ResourceTestRule.java
@@ -1,30 +1,28 @@
 package io.dropwizard.testing.junit;
 
-import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
-
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import io.dropwizard.jackson.Jackson;
-import io.dropwizard.jersey.DropwizardResourceConfig;
-import io.dropwizard.jersey.jackson.JacksonMessageBodyProvider;
-import io.dropwizard.logging.LoggingFactory;
-
+import io.dropwizard.jersey.validation.Validators;
+import io.dropwizard.logging.BootstrapLogging;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.servlet.ServletProperties;
+import org.glassfish.jersey.test.DeploymentContext;
 import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.ServletDeploymentContext;
 import org.glassfish.jersey.test.inmemory.InMemoryTestContainerFactory;
-import org.glassfish.jersey.test.spi.TestContainerException;
 import org.glassfish.jersey.test.spi.TestContainerFactory;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.ws.rs.client.Client;
-import javax.ws.rs.core.Application;
-
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 /**
  * A JUnit {@link TestRule} for testing Jersey resources.
@@ -32,17 +30,22 @@ import java.util.Set;
 public class ResourceTestRule implements TestRule {
 
     static {
-        LoggingFactory.bootstrap();
+        BootstrapLogging.bootstrap();
     }
 
+    /**
+     * A {@link ResourceTestRule} builder which enables configuration of a Jersey testing environment.
+     */
     public static class Builder {
 
-        private final Set<Object> singletons = Sets.newHashSet();
-        private final Set<Class<?>> providers = Sets.newHashSet();
-        private final Map<String, Object> properties = Maps.newHashMap();
+        private final Set<Object> singletons = new HashSet<>();
+        private final Set<Class<?>> providers = new HashSet<>();
+        private final Map<String, Object> properties = new HashMap<>();
         private ObjectMapper mapper = Jackson.newObjectMapper();
-        private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        private Validator validator = Validators.newValidator();
+        private Consumer<ClientConfig> clientConfigurator = c -> {};
         private TestContainerFactory testContainerFactory = new InMemoryTestContainerFactory();
+        private boolean registerDefaultExceptionMappers = true;
 
         public Builder setMapper(ObjectMapper mapper) {
             this.mapper = mapper;
@@ -51,6 +54,11 @@ public class ResourceTestRule implements TestRule {
 
         public Builder setValidator(Validator validator) {
             this.validator = validator;
+            return this;
+        }
+
+        public Builder setClientConfigurator(Consumer<ClientConfig> clientConfigurator) {
+            this.clientConfigurator = clientConfigurator;
             return this;
         }
 
@@ -79,44 +87,49 @@ public class ResourceTestRule implements TestRule {
             return this;
         }
 
+        public Builder setRegisterDefaultExceptionMappers(boolean value) {
+            registerDefaultExceptionMappers = value;
+            return this;
+        }
+
+        /**
+         * Builds a {@link ResourceTestRule} with a configured Jersey testing environment.
+         *
+         * @return a new {@link ResourceTestRule}
+         */
         public ResourceTestRule build() {
-            return new ResourceTestRule(singletons, providers, properties, mapper, validator, testContainerFactory);
+            return new ResourceTestRule(new ResourceTestJerseyConfiguration(
+                singletons, providers, properties, mapper, validator,
+                clientConfigurator, testContainerFactory, registerDefaultExceptionMappers));
         }
     }
 
+    /**
+     * Creates a new Jersey testing environment builder for {@link ResourceTestRule}
+     *
+     * @return a new {@link Builder}
+     */
     public static Builder builder() {
         return new Builder();
     }
 
-    private final Set<Object> singletons;
-    private final Set<Class<?>> providers;
-    private final Map<String, Object> properties;
-    private final ObjectMapper mapper;
-    private final Validator validator;
-    private final TestContainerFactory testContainerFactory;
-
+    private ResourceTestJerseyConfiguration configuration;
     private JerseyTest test;
 
-    private ResourceTestRule(Set<Object> singletons,
-                             Set<Class<?>> providers,
-                             Map<String, Object> properties,
-                             ObjectMapper mapper,
-                             Validator validator,
-                             TestContainerFactory testContainerFactory) {
-        this.singletons = singletons;
-        this.providers = providers;
-        this.properties = properties;
-        this.mapper = mapper;
-        this.validator = validator;
-        this.testContainerFactory = testContainerFactory;
+    private ResourceTestRule(ResourceTestJerseyConfiguration configuration) {
+        this.configuration = configuration;
     }
 
     public Validator getValidator() {
-        return validator;
+        return configuration.validator;
     }
 
     public ObjectMapper getObjectMapper() {
-        return mapper;
+        return configuration.mapper;
+    }
+
+    public Consumer<ClientConfig> getClientConfigurator() {
+        return configuration.clientConfigurator;
     }
 
     public Client client() {
@@ -128,38 +141,40 @@ public class ResourceTestRule implements TestRule {
     }
 
     @Override
-    public Statement apply(final Statement base, Description description) {
+    public Statement apply(Statement base, Description description) {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
+                DropwizardTestResourceConfig.CONFIGURATION_REGISTRY.put(configuration.getId(), configuration);
                 try {
                     test = new JerseyTest() {
                         @Override
-                        protected Application configure() {
-                            DropwizardResourceConfig config = DropwizardResourceConfig.forTesting(new MetricRegistry());
-                            for (Class<?> provider : providers) {
-                                config.register(provider);
-                            }
-                            for (Map.Entry<String, Object> property : properties.entrySet()) {
-                                config.property(property.getKey(), property.getValue());
-                            }
-                            config.register(new JacksonMessageBodyProvider(mapper, validator));
-                            for (Object singleton : singletons)
-                                config.register(singleton);
-                            return config;
+                        protected TestContainerFactory getTestContainerFactory() {
+                            return configuration.testContainerFactory;
                         }
 
                         @Override
-                        protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
-                            return testContainerFactory;
+                        protected DeploymentContext configureDeployment() {
+                            return ServletDeploymentContext.builder(new DropwizardTestResourceConfig(configuration))
+                                    .initParam(ServletProperties.JAXRS_APPLICATION_CLASS,
+                                            DropwizardTestResourceConfig.class.getName())
+                                    .initParam(DropwizardTestResourceConfig.CONFIGURATION_ID, configuration.getId())
+                                    .build();
+                        }
+
+                        @Override
+                        protected void configureClient(ClientConfig clientConfig) {
+                            final JacksonJsonProvider jsonProvider = new JacksonJsonProvider();
+                            jsonProvider.setMapper(configuration.mapper);
+                            configuration.clientConfigurator.accept(clientConfig);
+                            clientConfig.register(jsonProvider);
                         }
                     };
                     test.setUp();
                     base.evaluate();
                 } finally {
-                    if (test != null) {
-                        test.tearDown();
-                    }
+                    DropwizardTestResourceConfig.CONFIGURATION_REGISTRY.remove(configuration.getId());
+                    test.tearDown();
                 }
             }
         };
