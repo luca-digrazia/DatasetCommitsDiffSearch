1ed405690e641d1c4a5adca0cf70af916e59970b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:13:50 +0200

    Merges and review comments from Sanne

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/HibernateOrmProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/HibernateOrmProcessor.java
@@ -307,7 +307,6 @@ public final class HibernateOrmProcessor {
 
         final boolean enableORM = hasEntities(domainObjects, nonJpaModelBuildItems);
         final boolean hibernateReactivePresent = capabilities.isPresent(Capability.HIBERNATE_REACTIVE);
-        final boolean jtaPresent = capabilities.isPresent(Capability.TRANSACTIONS);
         //The Hibernate Reactive extension is able to handle registration of PersistenceProviders for both reactive and
         //traditional blocking Hibernate, by depending on this module and delegating to this code.
         //So when the Hibernate Reactive extension is present, trust that it will register its own PersistenceProvider
@@ -372,7 +371,7 @@ public final class HibernateOrmProcessor {
         beanContainerListener
                 .produce(new BeanContainerListenerBuildItem(
                         recorder.initMetadata(allDescriptors, scanner, integratorClasses, serviceContributorClasses,
-                                proxyDefinitions.getProxies(), strategy, jtaPresent)));
+                                proxyDefinitions.getProxies(), strategy)));
     }
 
     private MultiTenancyStrategy getMultiTenancyStrategy() {
--- dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-reactive/deployment/src/main/java/io/quarkus/hibernate/reactive/deployment/HibernateReactiveProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-reactive/deployment/src/main/java/io/quarkus/hibernate/reactive/deployment/HibernateReactiveProcessor.java
@@ -20,6 +20,7 @@ import javax.persistence.spi.PersistenceUnitTransactionType;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.jpa.boot.internal.ParsedPersistenceXmlDescriptor;
 import org.hibernate.loader.BatchFetchStyle;
+import org.hibernate.tool.hbm2ddl.MultipleLinesSqlCommandExtractor;
 
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.datasource.runtime.DataSourcesBuildTimeConfig;
@@ -127,16 +128,12 @@ public final class HibernateReactiveProcessor {
         }
 
         // we only support the default pool for now
-        Optional<String> dbKindOptional = dataSourcesBuildTimeConfig.defaultDataSource.dbKind;
-        if (dbKindOptional.isPresent()) {
-            final String dbKind = dbKindOptional.get();
-            ParsedPersistenceXmlDescriptor reactivePU = generateReactivePersistenceUnit(resourceProducer,
-                    systemPropertyProducer,
-                    dbKind, applicationArchivesBuildItem, launchMode.getLaunchMode());
+        Optional<String> dbKind = dataSourcesBuildTimeConfig.defaultDataSource.dbKind;
 
-            persistenceUnitDescriptorProducer.produce(new PersistenceUnitDescriptorBuildItem(reactivePU, true));
-        }
+        ParsedPersistenceXmlDescriptor reactivePU = generateReactivePersistenceUnit(resourceProducer, systemPropertyProducer,
+                dbKind, applicationArchivesBuildItem, launchMode.getLaunchMode());
 
+        persistenceUnitDescriptorProducer.produce(new PersistenceUnitDescriptorBuildItem(reactivePU));
     }
 
     /**
@@ -150,7 +147,7 @@ public final class HibernateReactiveProcessor {
     private ParsedPersistenceXmlDescriptor generateReactivePersistenceUnit(
             BuildProducer<NativeImageResourceBuildItem> resourceProducer,
             BuildProducer<SystemPropertyBuildItem> systemProperty,
-            String dbKind,
+            Optional<String> dbKind,
             ApplicationArchivesBuildItem applicationArchivesBuildItem,
             LaunchMode launchMode) {
 
@@ -244,6 +241,9 @@ public final class HibernateReactiveProcessor {
                 // enlist resource if present
                 resourceProducer.produce(new NativeImageResourceBuildItem(importFile.get()));
                 desc.getProperties().setProperty(AvailableSettings.HBM2DDL_IMPORT_FILES, importFile.get());
+                desc.getProperties().setProperty(AvailableSettings.HBM2DDL_IMPORT_FILES_SQL_EXTRACTOR,
+                        MultipleLinesSqlCommandExtractor.class.getName());
+
             } else if (hibernateConfig.sqlLoadScript.isPresent()) {
                 //raise exception if explicit file is not present (i.e. not the default)
                 throw new ConfigurationError(
