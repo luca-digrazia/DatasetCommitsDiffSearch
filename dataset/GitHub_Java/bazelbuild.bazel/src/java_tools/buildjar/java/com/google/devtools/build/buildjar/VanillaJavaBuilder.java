ae99b291307551aa6427defdc4f090fff4c90645
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:35:25 +0200

    Add support for native header outputs to VanillaJavaBuilder
        PiperOrigin-RevId: 194503531

--- dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/VanillaJavaBuilder.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/VanillaJavaBuilder.java
@@ -251,13 +251,10 @@ public class VanillaJavaBuilder implements Closeable {
       OptionsParser optionsParser, StandardJavaFileManager fileManager, Path nativeHeaderDir)
       throws IOException {
     fileManager.setLocation(StandardLocation.CLASS_PATH, toFiles(optionsParser.getClassPath()));
-    Iterable<File> bootClassPath =
+    fileManager.setLocation(
+        StandardLocation.PLATFORM_CLASS_PATH,
         Iterables.concat(
-            toFiles(optionsParser.getBootClassPath()), toFiles(optionsParser.getExtClassPath()));
-    // The bootclasspath may legitimately be empty if --release is being used.
-    if (!Iterables.isEmpty(bootClassPath)) {
-      fileManager.setLocation(StandardLocation.PLATFORM_CLASS_PATH, bootClassPath);
-    }
+            toFiles(optionsParser.getBootClassPath()), toFiles(optionsParser.getExtClassPath())));
     fileManager.setLocation(
         StandardLocation.ANNOTATION_PROCESSOR_PATH, toFiles(optionsParser.getProcessorPath()));
     if (optionsParser.getSourceGenDir() != null) {
