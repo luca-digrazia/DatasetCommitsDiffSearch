1d06c0086a91989407d4cee9eba79aefc80b03c9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:16:38 +0200

    Bytecode recorder refactoring to allow bytecode to be sliced into manageable chunks to avoid the size limit

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/steps/ConfigurationSetup.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/steps/ConfigurationSetup.java
@@ -1,8 +1,12 @@
 package io.quarkus.deployment.steps;
 
+import static io.quarkus.deployment.util.ReflectUtil.toError;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -14,11 +18,15 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.Properties;
+import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
+
+import javax.annotation.Priority;
 
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.spi.ConfigBuilder;
@@ -70,7 +78,6 @@ import io.quarkus.runtime.configuration.BuildTimeConfigFactory;
 import io.quarkus.runtime.configuration.ConverterFactory;
 import io.quarkus.runtime.configuration.ConverterSupport;
 import io.quarkus.runtime.configuration.DefaultConfigSource;
-import io.quarkus.runtime.configuration.DeploymentProfileConfigSource;
 import io.quarkus.runtime.configuration.ExpandingConfigSource;
 import io.quarkus.runtime.configuration.NameIterator;
 import io.quarkus.runtime.configuration.SimpleConfigurationProviderResolver;
@@ -143,10 +150,6 @@ public class ConfigurationSetup {
     private static final MethodDescriptor ECS_WRAPPER = MethodDescriptor.ofMethod(ExpandingConfigSource.class, "wrapper",
             UnaryOperator.class, ExpandingConfigSource.Cache.class);
 
-    private static final MethodDescriptor PROFILE_WRAPPER = MethodDescriptor.ofMethod(DeploymentProfileConfigSource.class,
-            "wrapper",
-            UnaryOperator.class);
-
     private static final MethodDescriptor RTD_CTOR = MethodDescriptor.ofConstructor(RUN_TIME_DEFAULTS);
     private static final MethodDescriptor RTD_GET_VALUE = MethodDescriptor.ofMethod(RUN_TIME_DEFAULTS, "getValue", String.class,
             NameIterator.class);
@@ -189,7 +192,7 @@ public class ConfigurationSetup {
         // set up the configuration definitions
         final ConfigDefinition buildTimeConfig = new ConfigDefinition(FieldDescriptor.of("Bogus", "No field", "Nothing"));
         final ConfigDefinition buildTimeRunTimeConfig = new ConfigDefinition(BUILD_TIME_CONFIG_FIELD);
-        final ConfigDefinition runTimeConfig = new ConfigDefinition(RUN_TIME_CONFIG_FIELD, true);
+        final ConfigDefinition runTimeConfig = new ConfigDefinition(RUN_TIME_CONFIG_FIELD);
 
         // populate it with all known types
         for (Class<?> clazz : ServiceUtil.classesNamedIn(extensionClassLoaderBuildItem.getExtensionClassLoader(),
@@ -217,7 +220,6 @@ public class ConfigurationSetup {
         // expand properties
         final ExpandingConfigSource.Cache cache = new ExpandingConfigSource.Cache();
         builder.withWrapper(ExpandingConfigSource.wrapper(cache));
-        builder.withWrapper(DeploymentProfileConfigSource.wrapper());
         builder.addDefaultSources();
         final ApplicationPropertiesConfigSource.InJar inJar = new ApplicationPropertiesConfigSource.InJar();
         final DefaultValuesConfigurationSource defaultSource = new DefaultValuesConfigurationSource(
@@ -501,11 +503,7 @@ public class ConfigurationSetup {
 
                 // property expansion
                 final ResultHandle cache = carc.newInstance(ECS_CACHE_CONSTRUCT);
-                ResultHandle wrapper = carc.invokeStaticMethod(ECS_WRAPPER, cache);
-                carc.invokeVirtualMethod(SRCB_WITH_WRAPPER, builder, wrapper);
-
-                //profiles
-                wrapper = carc.invokeStaticMethod(PROFILE_WRAPPER);
+                final ResultHandle wrapper = carc.invokeStaticMethod(ECS_WRAPPER, cache);
                 carc.invokeVirtualMethod(SRCB_WITH_WRAPPER, builder, wrapper);
 
                 // write out loader for converter types
