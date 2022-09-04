f77b6832de3688bb42fa6d8aa12e14adb8ac17f7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:16:06 +0200

    Make sure we follow the MP-CP spec and disallow our container-managed executor to be shut down
        Since it's the underlying executor service which must be shut down, by Quarkus

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/runtime/src/main/java/io/quarkus/smallrye/context/runtime/SmallRyeContextPropagationProvider.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/runtime/src/main/java/io/quarkus/smallrye/context/runtime/SmallRyeContextPropagationProvider.java
@@ -10,17 +10,16 @@ import javax.inject.Singleton;
 import org.eclipse.microprofile.context.ManagedExecutor;
 import org.eclipse.microprofile.context.ThreadContext;
 
-import io.smallrye.context.SmallRyeManagedExecutor;
-import io.smallrye.context.SmallRyeThreadContext;
+import io.smallrye.context.impl.ManagedExecutorImpl;
+import io.smallrye.context.impl.ThreadContextImpl;
 
 @ApplicationScoped
 public class SmallRyeContextPropagationProvider {
 
-    private volatile SmallRyeManagedExecutor managedExecutor;
+    private volatile ManagedExecutorImpl managedExecutor;
 
     void initialize(ExecutorService executorService) {
-        managedExecutor = new SmallRyeManagedExecutor(-1, -1, (SmallRyeThreadContext) getAllThreadContext(), executorService,
-                "no-ip") {
+        managedExecutor = new ManagedExecutorImpl(-1, -1, (ThreadContextImpl) getAllThreadContext(), executorService, "no-ip") {
             @Override
             public void shutdown() {
                 throw new IllegalStateException("This executor is managed by the container and cannot be shut down.");
