da2819c7f650e5c2708be86b1dc701e7234a7f00
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:12:29 +0200

    Use JavaPluginInfo from plugins, :java_plugins and exported_plugins attributes (instead of JavaInfo).
        PiperOrigin-RevId: 370845638

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaCommon.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaCommon.java
@@ -46,6 +46,7 @@ import com.google.devtools.build.lib.packages.TargetUtils;
 import com.google.devtools.build.lib.packages.Type;
 import com.google.devtools.build.lib.rules.cpp.CcInfo;
 import com.google.devtools.build.lib.rules.cpp.CcNativeLibraryInfo;
+import com.google.devtools.build.lib.rules.cpp.LibraryToLink;
 import com.google.devtools.build.lib.rules.java.JavaCompilationArgsProvider.ClasspathType;
 import com.google.devtools.build.lib.rules.java.JavaPluginInfo.JavaPluginData;
 import com.google.devtools.build.lib.util.FileTypeSet;
@@ -305,6 +306,21 @@ public class JavaCommon {
         }
       }
     }
+  }
+
+  /**
+   * Returns transitive Java native libraries.
+   *
+   * @see JavaNativeLibraryInfo
+   */
+  protected NestedSet<LibraryToLink> collectTransitiveJavaNativeLibraries() {
+    NativeLibraryNestedSetBuilder builder = new NativeLibraryNestedSetBuilder();
+    builder.addJavaTargets(targetsTreatedAsDeps(ClasspathType.BOTH));
+
+    if (ruleContext.getRule().isAttrDefined("data", BuildType.LABEL_LIST)) {
+      builder.addJavaTargets(ruleContext.getPrerequisites("data"));
+    }
+    return builder.build();
   }
 
   /**
@@ -681,16 +697,20 @@ public class JavaCommon {
     javaInfoBuilder.addProvider(JavaExportsProvider.class, exportsProvider);
     javaInfoBuilder.addProvider(JavaCompilationInfoProvider.class, compilationInfoProvider);
 
-    addCcRelatedProviders(javaInfoBuilder);
+    addCcRelatedProviders(builder, javaInfoBuilder);
   }
 
   /** Adds Cc related providers to a Java target. */
-  private void addCcRelatedProviders(JavaInfo.Builder javaInfoBuilder) {
+  private void addCcRelatedProviders(
+      RuleConfiguredTargetBuilder ruleBuilder, JavaInfo.Builder javaInfoBuilder) {
     Iterable<? extends TransitiveInfoCollection> deps = targetsTreatedAsDeps(ClasspathType.BOTH);
+
 
     ImmutableList<CcInfo> ccInfos =
         Streams.concat(
                 AnalysisUtils.getProviders(deps, CcInfo.PROVIDER).stream(),
+                AnalysisUtils.getProviders(deps, JavaCcLinkParamsProvider.PROVIDER).stream()
+                    .map(JavaCcLinkParamsProvider::getCcInfo),
                 JavaInfo.getProvidersFromListOfTargets(JavaCcInfoProvider.class, deps).stream()
                     .map(JavaCcInfoProvider::getCcInfo))
             .collect(toImmutableList());
@@ -699,8 +719,7 @@ public class JavaCommon {
 
     // Collect library paths from all attributes (including data)
     Iterable<? extends TransitiveInfoCollection> data;
-    if (ruleContext.getRule().isAttrDefined("data", BuildType.LABEL_LIST)
-        && !ruleContext.getFragment(JavaConfiguration.class).dontCollectDataLibraries()) {
+    if (ruleContext.getRule().isAttrDefined("data", BuildType.LABEL_LIST)) {
       data = ruleContext.getPrerequisites("data");
     } else {
       data = ImmutableList.of();
@@ -709,6 +728,11 @@ public class JavaCommon {
         CcNativeLibraryInfo.merge(
             Streams.concat(
                     Stream.of(mergedCcInfo.getCcNativeLibraryInfo()),
+                    AnalysisUtils.getProviders(
+                            Iterables.concat(deps, data), JavaNativeLibraryInfo.PROVIDER)
+                        .stream()
+                        .map(JavaNativeLibraryInfo::getTransitiveJavaNativeLibraries)
+                        .map(CcNativeLibraryInfo::new),
                     JavaInfo.getProvidersFromListOfTargets(JavaCcInfoProvider.class, data).stream()
                         .map(JavaCcInfoProvider::getCcInfo)
                         .map(CcInfo::getCcNativeLibraryInfo),
@@ -722,6 +746,11 @@ public class JavaCommon {
             .setCcNativeLibraryInfo(mergedCcNativeLibraryInfo)
             .build();
 
+    if (ruleContext
+        .getFragment(JavaConfiguration.class)
+        .experimentalPublishJavaCcLinkParamsInfo()) {
+      ruleBuilder.addNativeDeclaredProvider(new JavaCcLinkParamsProvider(filteredCcInfo));
+    }
     javaInfoBuilder.addProvider(JavaCcInfoProvider.class, new JavaCcInfoProvider(filteredCcInfo));
   }
 
