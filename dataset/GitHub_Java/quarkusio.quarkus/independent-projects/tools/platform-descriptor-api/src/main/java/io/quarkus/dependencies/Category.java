cefafe1cdbedea8f33cef1211eb291711de6dbb3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:30:36 +0200

    Make const public in descriptor and add new consts

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/tools/platform-descriptor-api/src/main/java/io/quarkus/dependencies/Category.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/tools/platform-descriptor-api/src/main/java/io/quarkus/dependencies/Category.java
@@ -29,11 +29,11 @@ public class Category {
     }
 
     public String getDescription() {
-        return description;
+        return name;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String name) {
+        this.description = name;
     }
 
     public Map<String, Object> getMetadata() {
