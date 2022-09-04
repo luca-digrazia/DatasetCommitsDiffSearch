df9300ffc6d3adafbc208b00aa1f46676192c5f1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:11:42 +0200

    Rename cluster node metrics endpoint and create global metrics endpoint

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/cluster/ClusterMetricsResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/cluster/ClusterMetricsResource.java
@@ -1,108 +1,71 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.rest.resources.cluster;
 
 import com.codahale.metrics.annotation.Timed;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-import com.wordnik.swagger.annotations.ApiResponse;
-import com.wordnik.swagger.annotations.ApiResponses;
-import org.apache.shiro.authz.UnauthorizedException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.graylog2.cluster.Node;
 import org.graylog2.cluster.NodeNotFoundException;
 import org.graylog2.cluster.NodeService;
 import org.graylog2.rest.RemoteInterfaceProvider;
 import org.graylog2.rest.models.system.metrics.requests.MetricsReadRequest;
-import org.graylog2.rest.models.system.metrics.responses.MetricNamesResponse;
 import org.graylog2.rest.models.system.metrics.responses.MetricsSummaryResponse;
+import org.graylog2.shared.rest.resources.ProxiedResource;
 import org.graylog2.shared.rest.resources.system.RemoteMetricsResource;
-import org.graylog2.shared.security.RestPermissions;
-import retrofit.Response;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RequiresAuthentication
-@Api(value = "Cluster/Metrics", description = "Cluster-wide Internal Graylog2 metrics")
-@Path("/cluster/{nodeId}/metrics")
-public class ClusterMetricsResource {
-    private final RemoteMetricsResource remoteMetricsResource;
+@Api(value = "Cluster/Metrics", description = "Cluster-wide Internal Graylog metrics")
+@Path("/cluster/metrics")
+@Produces(MediaType.APPLICATION_JSON)
+public class ClusterMetricsResource extends ProxiedResource {
 
     @Inject
-    public ClusterMetricsResource(@PathParam("nodeId") String nodeId,
-                                  NodeService nodeService,
+    public ClusterMetricsResource(NodeService nodeService,
                                   RemoteInterfaceProvider remoteInterfaceProvider,
-                                  @Context HttpHeaders httpHeaders) throws NodeNotFoundException {
-        final Node targetNode = nodeService.byNodeId(nodeId);
-
-        final List<String> authenticationTokens = httpHeaders.getRequestHeader("Authorization");
-        if (authenticationTokens != null && authenticationTokens.size() >= 1) {
-            this.remoteMetricsResource = remoteInterfaceProvider.get(targetNode, authenticationTokens.get(0), RemoteMetricsResource.class);
-        } else {
-            throw new UnauthorizedException();
-        }
-    }
-
-    @GET
-    @Timed
-    @Path("/names")
-    @ApiOperation(value = "Get all metrics keys/names from node")
-    @RequiresPermissions(RestPermissions.METRICS_ALLKEYS)
-    @Produces(MediaType.APPLICATION_JSON)
-    public MetricNamesResponse metricNames() throws IOException {
-        final Response<MetricNamesResponse> result =  remoteMetricsResource.metricNames().execute();
-        if (result.isSuccess()) {
-            return result.body();
-        } else {
-            throw new WebApplicationException(result.message(), result.code());
-        }
+                                  @Context HttpHeaders httpHeaders) {
+        super(httpHeaders, nodeService, remoteInterfaceProvider);
     }
 
     @POST
     @Timed
     @Path("/multiple")
-    @ApiOperation("Get the values of multiple metrics at once from node")
+    @ApiOperation(value = "Get all metrics of all nodes in the cluster")
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Malformed body")
     })
-    public MetricsSummaryResponse multipleMetrics(@ApiParam(name = "Requested metrics", required = true)
-                                                  @Valid @NotNull MetricsReadRequest request) throws IOException {
-        final Response<MetricsSummaryResponse> result = remoteMetricsResource.multipleMetrics(request).execute();
-        if (result.isSuccess()) {
-            return result.body();
-        } else {
-            throw new WebApplicationException(result.message(), result.code());
-        }
-    }
-
-    @GET
-    @Timed
-    @Path("/namespace/{namespace}")
-    @ApiOperation(value = "Get all metrics of a namespace from node")
-    @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "No such metric namespace")
-    })
-    @Produces(MediaType.APPLICATION_JSON)
-    public MetricsSummaryResponse byNamespace(@ApiParam(name = "namespace", required = true)
-                                              @PathParam("namespace") String namespace) throws IOException {
-        final Response<MetricsSummaryResponse> result = remoteMetricsResource.byNamespace(namespace).execute();
-        if (result.isSuccess()) {
-            return result.body();
-        } else {
-            throw new WebApplicationException(result.message(), result.code());
-        }
+    public Map<String, Optional<MetricsSummaryResponse>> multipleMetricsAllNodes(@ApiParam(name = "Requested metrics", required = true)
+                                                                                 @Valid @NotNull MetricsReadRequest request) throws IOException, NodeNotFoundException {
+        return getForAllNodes(remoteMetricsResource -> remoteMetricsResource.multipleMetrics(request), createRemoteInterfaceProvider(RemoteMetricsResource.class));
     }
 }
