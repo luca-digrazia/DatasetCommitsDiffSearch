162641dc5ae0fab695f92056026bd3c99e945eb9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:37:37 +0200

    Automatic code cleanup.
        PiperOrigin-RevId: 354130318

--- dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/aapt2/CompiledResources.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/aapt2/CompiledResources.java
@@ -13,8 +13,11 @@
 // limitations under the License.
 package com.google.devtools.build.android.aapt2;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.devtools.build.android.ManifestContainer;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
@@ -29,40 +32,86 @@ import javax.annotation.Nullable;
  */
 public class CompiledResources implements ManifestContainer {
 
-  private final Path resources;
-  private final Path manifest;
-  private final List<Path> assetsDirs;
+  /**
+   * File extension for persisting {@code <resources tool:* />} attributes. These files are bundled
+   * with *.flat files in the {@link resources} ZIP file and ignored by aapt2. The format is
+   * internal to ResourceProcessorBusyBox and can be changed at any time.
+   */
+  // TODO(b/143382754): aapt2 should be serializing these directly.
+  public static final String ATTRIBUTES_FILE_EXTENSION = ".attributes";
 
-  private CompiledResources(Path resources, Path manifest, List<Path> assetsDirs) {
+  private final Path resources;
+  @Nullable private final Path manifest;
+  private final List<Path> assetsDirs;
+  @Nullable private final Path stableIds;
+
+  /**
+   * @param resources The path to the zip file containing the compiled resource result of aapt2
+   * @param manifest The path to the manifest file for this set of resources
+   * @param assetsDirs The list of asset directors for the target of this set of resources
+   * @param stableIds The path to the file containing resource ID's to keep stable
+   */
+  private CompiledResources(
+      Path resources, @Nullable Path manifest, List<Path> assetsDirs, @Nullable Path stableIds) {
     this.resources = resources;
     this.manifest = manifest;
     this.assetsDirs = assetsDirs;
+    this.stableIds = stableIds;
+  }
+
+  public static CompiledResources from(Path resources) {
+    return from(resources, null, ImmutableList.of());
   }
 
   public static CompiledResources from(Path resources, Path manifest) {
-    return from(resources, manifest, null);
+    return from(resources, manifest, ImmutableList.of());
   }
 
   public static CompiledResources from(
-      Path resources, Path manifest, @Nullable List<Path> assetDirs) {
+      Path resources, @Nullable Path manifest, @Nullable List<Path> assetDirs) {
     return new CompiledResources(
-        resources, manifest, Optional.ofNullable(assetDirs).orElseGet(ImmutableList::of));
+        resources, manifest, assetDirs != null ? assetDirs : ImmutableList.of(), null);
   }
 
+  /** This zip file contains resource flat files that are the result of aapt2 compile */
   public Path getZip() {
     return resources;
   }
 
+  /** Copies resources archive to a path and returns the new {@link CompiledResources} */
+  public CompiledResources copyResourcesZipTo(Path destination) throws IOException {
+    return new CompiledResources(
+        Files.copy(resources, destination), manifest, assetsDirs, stableIds);
+  }
+
   @Override
+  @Nullable
   public Path getManifest() {
+    if (manifest != null) {
+      Preconditions.checkState(Files.exists(manifest));
+    }
     return manifest;
   }
 
   public List<String> getAssetsStrings() {
-    return assetsDirs.stream().map(Path::toString).collect(Collectors.toList());
+    return assetsDirs.stream()
+        .peek(
+            p -> {
+              Preconditions.checkArgument(Files.exists(p), "does not exist %s", p);
+            })
+        .map(Path::toString)
+        .collect(Collectors.toList());
   }
 
   public CompiledResources processManifest(Function<Path, Path> processManifest) {
-    return new CompiledResources(resources, processManifest.apply(manifest), assetsDirs);
+    return new CompiledResources(resources, processManifest.apply(manifest), assetsDirs, stableIds);
+  }
+
+  public CompiledResources addStableIds(Path stableIds) {
+    return new CompiledResources(resources, manifest, assetsDirs, stableIds);
+  }
+
+  public Optional<Path> getStableIds() {
+    return Optional.ofNullable(stableIds);
   }
 }
