cdb2be4c17c8b017fa91de2f815a793708161cb9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 23:09:12 +0200

    @AutoCodec WriteBuildInfoHeaderAction.
        PiperOrigin-RevId: 187218309

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/WriteBuildInfoHeaderAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/WriteBuildInfoHeaderAction.java
@@ -138,10 +138,12 @@ public final class WriteBuildInfoHeaderAction extends AbstractFileWriteAction {
   }
 
   @Override
-  protected void computeKey(ActionKeyContext actionKeyContext, Fingerprint fp) {
-    fp.addString(GUID);
-    fp.addBoolean(writeStableInfo);
-    fp.addBoolean(writeVolatileInfo);
+  protected String computeKey(ActionKeyContext actionKeyContext) {
+    Fingerprint f = new Fingerprint();
+    f.addString(GUID);
+    f.addBoolean(writeStableInfo);
+    f.addBoolean(writeVolatileInfo);
+    return f.hexDigestAndReset();
   }
 
   @Override
