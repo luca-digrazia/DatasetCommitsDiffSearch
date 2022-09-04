a901b05ffeb65ff56f42f9f801e5610c06690fc4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:29:51 +0200

    Animation types combined

--- dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/sample/src/com/davemorrissey/labs/subscaleview/sample/PageFragment.java
+++ dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/sample/src/com/davemorrissey/labs/subscaleview/sample/PageFragment.java
@@ -1,27 +1,40 @@
+/*
+Copyright 2014 David Morrissey
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.davemorrissey.labs.subscaleview.sample;
 
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.widget.Toast;
+import com.davemorrissey.labs.subscaleview.ImageViewState;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.davemorrissey.labs.subscaleview.sample.R.id;
 
-import java.io.IOException;
+import java.util.Random;
 
 public class PageFragment extends Fragment implements OnClickListener, OnLongClickListener {
 
-    private static final String STATE_SCALE = "state-scale";
-    private static final String STATE_CENTER_X = "state-center-x";
-    private static final String STATE_CENTER_Y = "state-center-y";
-    private static final String STATE_ORIENTATION = "state-orientation";
-    private static final String STATE_ASSET = "state-asset";
+    private static final String BUNDLE_STATE = "state";
+    private static final String BUNDLE_ASSET = "asset";
 
     private int orientation = 0;
 
@@ -38,33 +51,27 @@ public class PageFragment extends Fragment implements OnClickListener, OnLongCli
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.page, container, false);
 
+        ImageViewState imageViewState = null;
         if (savedInstanceState != null) {
-            if (asset == null && savedInstanceState.containsKey(STATE_ASSET)) {
-                asset = savedInstanceState.getString(STATE_ASSET);
+            if (asset == null && savedInstanceState.containsKey(BUNDLE_ASSET)) {
+                asset = savedInstanceState.getString(BUNDLE_ASSET);
             }
-            if (savedInstanceState.containsKey(STATE_ORIENTATION)) {
-                orientation = savedInstanceState.getInt(STATE_ORIENTATION);
+            if (savedInstanceState.containsKey(BUNDLE_STATE)) {
+                imageViewState = (ImageViewState)savedInstanceState.getSerializable(BUNDLE_STATE);
+                orientation = imageViewState.getOrientation();
             }
         }
 
         rootView.findViewById(id.rotate).setOnClickListener(this);
-        try {
-            if (asset != null) {
-                SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)rootView.findViewById(id.imageView);
-                imageView.setOrientation(orientation);
-                imageView.setImageAsset(asset);
-                imageView.setOnClickListener(this);
-                imageView.setOnLongClickListener(this);
-
-                if (savedInstanceState != null &&
-                        savedInstanceState.containsKey(STATE_SCALE) &&
-                        savedInstanceState.containsKey(STATE_CENTER_X) &&
-                        savedInstanceState.containsKey(STATE_CENTER_Y)) {
-                    imageView.setScaleAndCenter(savedInstanceState.getFloat(STATE_SCALE), new PointF(savedInstanceState.getFloat(STATE_CENTER_X), savedInstanceState.getFloat(STATE_CENTER_Y)));
-                }
-            }
-        } catch (IOException e) {
-            Log.e(SampleActivity.class.getSimpleName(), "Could not load asset", e);
+        rootView.findViewById(id.scale).setOnClickListener(this);
+        rootView.findViewById(id.center).setOnClickListener(this);
+        if (asset != null) {
+            SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)rootView.findViewById(id.imageView);
+            imageView.setImageAsset(asset, imageViewState);
+            imageView.setDoubleTapZoomStyle(SubsamplingScaleImageView.ZOOM_FOCUS_CENTER);
+            imageView.setPanLimit(SubsamplingScaleImageView.PAN_LIMIT_INSIDE);
+            imageView.setOnClickListener(this);
+            imageView.setOnLongClickListener(this);
         }
 
         return rootView;
@@ -77,6 +84,22 @@ public class PageFragment extends Fragment implements OnClickListener, OnLongCli
             orientation = (orientation + 90) % 360;
             SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)rootView.findViewById(id.imageView);
             imageView.setOrientation(orientation);
+        } else if (view.getId() == id.scale && rootView != null) {
+            SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)rootView.findViewById(id.imageView);
+            Random random = new Random();
+            if (imageView.isImageReady()) {
+                int sx = random.nextInt(imageView.getSWidth());
+                int sy = random.nextInt(imageView.getSHeight());
+                imageView.animateScaleAndCenter(random.nextFloat() * 2, new PointF(sx, sy)).withDuration(1500).start();
+            }
+        } else if (view.getId() == id.center && rootView != null) {
+            SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)rootView.findViewById(id.imageView);
+            Random random = new Random();
+            if (imageView.isImageReady()) {
+                int sx = random.nextInt(imageView.getSWidth());
+                int sy = random.nextInt(imageView.getSHeight());
+                imageView.animateCenter(new PointF(sx, sy)).withDuration(1500).start();
+            }
         } else if (view.getId() == id.imageView) {
             Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_SHORT).show();
         }
@@ -96,17 +119,13 @@ public class PageFragment extends Fragment implements OnClickListener, OnLongCli
         super.onSaveInstanceState(outState);
         View rootView = getView();
         if (rootView != null) {
+            outState.putString(BUNDLE_ASSET, asset);
             SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)rootView.findViewById(id.imageView);
-            outState.putFloat(STATE_SCALE, imageView.getScale());
-            PointF center = imageView.getCenter();
-            if (center != null) {
-                outState.putFloat(STATE_CENTER_X, center.x);
-                outState.putFloat(STATE_CENTER_Y, center.y);
+            ImageViewState state = imageView.getState();
+            if (state != null) {
+                outState.putSerializable(BUNDLE_STATE, imageView.getState());
             }
-            outState.putString(STATE_ASSET, asset);
-            outState.putInt(STATE_ORIENTATION, orientation);
         }
     }
-
 
 }
