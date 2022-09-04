9762a4491ce6cbd7f781eed2f97e7e83bb97b496
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:00:22 +0200

    cli: create cli codestart

--- dataset/GitHub_Java/quarkusio.quarkus/devtools/cli/src/test/java/io/quarkus/cli/CliProjectJBangTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/devtools/cli/src/test/java/io/quarkus/cli/CliProjectJBangTest.java
@@ -101,10 +101,7 @@ public class CliProjectJBangTest {
 
         result = CliDriver.invokeValidateDryRunBuild(project);
 
-        result = CliDriver.execute(project, "build", "-e", "-B", "--clean", "--verbose",
-                "-Dproperty=value1", "-Dproperty2=value2");
-        Assertions.assertEquals(CommandLine.ExitCode.OK, result.exitCode,
-                "Expected OK return code. Result:\n" + result);
+        CliDriver.invokeValidateBuild(project);
     }
 
     void validateBasicIdentifiers(Path project, String group, String artifact, String version) throws Exception {
