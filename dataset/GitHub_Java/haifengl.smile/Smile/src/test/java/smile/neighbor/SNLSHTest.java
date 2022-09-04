4483506fea9561da902bc4ae6cb493e9ab1c2b7b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:58:59 +0200

    add range and test

--- dataset/GitHub_Java/haifengl.smile/Smile/src/test/java/smile/neighbor/SNLSHTest.java
+++ dataset/GitHub_Java/haifengl.smile/Smile/src/test/java/smile/neighbor/SNLSHTest.java
@@ -1,24 +1,14 @@
 package smile.neighbor;
 
-import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
-import org.apache.commons.io.IOUtils;
-import org.junit.Before;
 import org.junit.Test;
-import smile.hash.SimHash;
-import smile.math.distance.HammingDistance;
-import smile.util.MaxHeap;
 
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Qiyang Zuo
  * @since 15/3/31
- * test data set: http://research.microsoft.com/en-us/downloads/607d14d9-20cd-47e3-85bc-a2f65cd28042/
  */
 public class SNLSHTest {
     private String[] texts = {
@@ -28,64 +18,9 @@ public class SNLSHTest {
             "I want to be far from other cases"
     };
 
-    private List<String> testData;
-    private List<String> trainData;
-    private Map<String, Long> signCache;
-
-    @Before
-    public void before() throws IOException {
-        trainData = loadData("/smile/data/msrp/msr_paraphrase_train.txt");
-        testData = loadData("/smile/data/msrp/msr_paraphrase_test.txt");
-        signCache = new HashMap<String, Long>();
-        for (String q : trainData) {
-            long sign = SimHash.simhash64(q);
-            signCache.put(q, sign);
-        }
-    }
-
-    private List<String> loadData(String path) throws IOException {
-        List<String> data = Lists.newArrayList();
-        List<String> lines = IOUtils.readLines(this.getClass().getResourceAsStream(path));
-        for (String line : lines) {
-            List<String> s = Splitter.on("\t").omitEmptyStrings().splitToList(line);
-            data.add(s.get(s.size() - 1));
-            data.add(s.get(s.size() - 2));
-        }
-        return data.subList(2, data.size());
-    }
-
-    private Neighbor<String, String>[] linearKNN(String q, int k) {
-        @SuppressWarnings("unchecked")
-        Neighbor<String, String>[] neighbors = (Neighbor<String, String>[])Array.newInstance(Neighbor.class, k);
-        MaxHeap<Neighbor<String, String>> heap = new MaxHeap<Neighbor<String, String>>(neighbors);
-        long sign1 = SimHash.simhash64(q);
-        for (String t : trainData) {
-            long sign2 = signCache.get(t);
-            double distance = HammingDistance.d(sign1, sign2);
-            heap.add(new Neighbor<String, String>(t, t, 0, distance));
-        }
-        return heap.toSortedArray();
-    }
-
-    private Neighbor<String, String> linearNearest(String q) {
-        Neighbor<String, String> neighbor = new Neighbor<String, String>(null, null, 0, 0);
-        long sign1 = SimHash.simhash64(q);
-        double minDist = Double.MAX_VALUE;
-        String minKey = null;
-        for (String t : trainData) {
-            long sign2 = signCache.get(t);
-            double distance = HammingDistance.d(sign1, sign2);
-            if (distance < minDist) {
-                minDist = distance;
-                minKey = t;
-            }
-        }
-        return new Neighbor<String, String>(minKey, minKey, 0, minDist);
-    }
-
     @Test
     public void testKNN() {
-        SNLSH<String> lsh = new SNLSH<String>(8);
+        SNLSH<String> lsh = new SNLSH(8, 2);
         for (String t : texts) {
             lsh.put(t, t);
         }
@@ -99,34 +34,8 @@ public class SNLSHTest {
     }
 
     @Test
-    public void testKNNRecall() {
-        SNLSH<String> lsh = new SNLSH<String>(8);
-        for(String t : trainData) {
-            lsh.put(t, t);
-        }
-        double recall = 0.0;
-        for (String q : testData) {
-            int k = 3;
-            Neighbor<String, String>[] n1 = lsh.knn(q, k);
-            Neighbor<String, String>[] n2 = linearKNN(q, k);
-            int hit = 0;
-            for (int m = 0; m < n1.length && n1[m] != null; m++) {
-                for (int n = 0; n < n2.length && n2[n] != null; n++) {
-                    if (n1[m].value.equals(n2[n].value)) {
-                        hit++;
-                        break;
-                    }
-                }
-            }
-            recall += 1.0 * hit / k;
-        }
-        recall /= testData.size();
-        System.out.println("SNLSH KNN recall is " + recall);
-    }
-
-    @Test
     public void testNearest() {
-        SNLSH<String> lsh = new SNLSH<String>(8);
+        SNLSH<String> lsh = new SNLSH(8, 2);
         for (String t : texts) {
             lsh.put(t, t);
         }
@@ -137,26 +46,8 @@ public class SNLSHTest {
     }
 
     @Test
-    public void testNearestRecall() {
-        SNLSH<String> lsh = new SNLSH<String>(8);
-        for(String t : trainData) {
-            lsh.put(t, t);
-        }
-        double recall = 0.0;
-        for (String q : testData) {
-            Neighbor<String, String> n1 = lsh.nearest(q);
-            Neighbor<String, String> n2 = linearNearest(q);
-            if (n1.value.equals(n2.value)) {
-                recall++;
-            }
-        }
-        recall /= testData.size();
-        System.out.println("SNLSH Nearest recall is " + recall);
-    }
-
-    @Test
     public void testRange() {
-        SNLSH<String> lsh = new SNLSH<String>(8);
+        SNLSH<String> lsh = new SNLSH<String>(8, 2);
         for (String t : texts) {
             lsh.put(t, t);
         }
