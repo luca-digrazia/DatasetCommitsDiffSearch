ef15c459bc06f9218bd7e1e7155b35d590b14e6a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:15:34 +0200

    Disable mongodb-client IT in windows as they are unreliable

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/mongodb-client/src/test/java/io/quarkus/it/mongodb/BookResourceTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/mongodb-client/src/test/java/io/quarkus/it/mongodb/BookResourceTest.java
@@ -1,60 +1,32 @@
 package io.quarkus.it.mongodb;
 
-import static io.restassured.RestAssured.get;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
 
 import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
+import io.quarkus.mongodb.health.MongoHealthCheck;
+import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.mongodb.MongoTestResource;
 import io.restassured.RestAssured;
-import io.restassured.mapper.ObjectMapper;
-import io.restassured.mapper.ObjectMapperDeserializationContext;
-import io.restassured.mapper.ObjectMapperSerializationContext;
-import io.restassured.mapper.TypeRef;
-import io.restassured.response.Response;
 
 @QuarkusTest
-class BookResourceTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(BookResourceTest.class);
-    private static MongodExecutable MONGO;
-
+@QuarkusTestResource(MongoTestResource.class)
+@DisabledOnOs(OS.WINDOWS)
+public class BookResourceTest {
     private static Jsonb jsonb;
 
     @BeforeAll
     public static void giveMeAMapper() {
-        jsonb = JsonbBuilder.create();
-        ObjectMapper mapper = new ObjectMapper() {
-            @Override
-            public Object deserialize(ObjectMapperDeserializationContext context) {
-                return jsonb.fromJson(context.getDataToDeserialize().asString(), context.getType());
-            }
-
-            @Override
-            public Object serialize(ObjectMapperSerializationContext context) {
-                return jsonb.toJson(context.getObjectToSerialize());
-            }
-        };
-        RestAssured.objectMapper(mapper);
+        jsonb = Utils.initialiseJsonb();
     }
 
     @AfterAll
@@ -62,86 +34,24 @@ class BookResourceTest {
         jsonb.close();
     }
 
-    @BeforeAll
-    public static void startMongoDatabase() throws IOException {
-        Version.Main version = Version.Main.V4_0;
-        int port = 27018;
-        LOGGER.info("Starting Mongo {} on port {}", version, port);
-        IMongodConfig config = new MongodConfigBuilder()
-                .version(version)
-                .net(new Net(port, Network.localhostIsIPv6()))
-                .build();
-        MONGO = MongodStarter.getDefaultInstance().prepare(config);
-        MONGO.start();
-    }
-
-    @AfterAll
-    public static void stopMongoDatabase() {
-        if (MONGO != null) {
-            MONGO.stop();
-        }
-    }
-
     @Test
     public void testBlockingClient() {
-        callTheEndpoint("/books");
-    }
-
-    private void callTheEndpoint(String endpoint) {
-        List<Book> list = get(endpoint).as(new TypeRef<List<Book>>() {
-        });
-        Assertions.assertEquals(0, list.size());
-
-        Book book1 = new Book().setAuthor("Victor Hugo").setTitle("Les Misérables")
-                .setCategories(Arrays.asList("long", "very long"))
-                .setDetails(new BookDetail().setRating(3).setSummary("A very long book"));
-        Response response = RestAssured
-                .given()
-                .header("Content-Type", "application/json")
-                .body(book1)
-                .post(endpoint)
-                .andReturn();
-        System.out.println("The response is: " + response.statusCode() + " " + response.asString());
-        Assertions.assertEquals(202, response.statusCode());
-
-        Book book2 = new Book().setAuthor("Victor Hugo").setTitle("Notre-Dame de Paris")
-                .setCategories(Arrays.asList("long", "quasimodo"))
-                .setDetails(new BookDetail().setRating(4).setSummary("quasimodo and esmeralda"));
-        response = RestAssured
-                .given()
-                .header("Content-Type", "application/json")
-                .body(book2)
-                .post(endpoint)
-                .andReturn();
-        Assertions.assertEquals(202, response.statusCode());
-
-        list = get(endpoint).as(new TypeRef<List<Book>>() {
-        });
-        Assertions.assertEquals(2, list.size());
-
-        Book book3 = new Book().setAuthor("Charles Baudelaire").setTitle("Les fleurs du mal")
-                .setCategories(Collections.singletonList("poem"))
-                .setDetails(new BookDetail().setRating(2).setSummary("Les Fleurs du mal is a volume of poetry."));
-        response = RestAssured
-                .given()
-                .header("Content-Type", "application/json")
-                .body(book3)
-                .post(endpoint)
-                .andReturn();
-        Assertions.assertEquals(202, response.statusCode());
-
-        list = get(endpoint).as(new TypeRef<List<Book>>() {
-        });
-        Assertions.assertEquals(3, list.size());
-
-        list = get(endpoint + "/Victor Hugo").as(new TypeRef<List<Book>>() {
-        });
-        Assertions.assertEquals(2, list.size());
+        Utils.callTheEndpoint("/books");
     }
 
     @Test
     public void testReactiveClients() {
-        callTheEndpoint("/reactive-books");
+        Utils.callTheEndpoint("/reactive-books");
+    }
+
+    @Test
+    public void health() throws Exception {
+        RestAssured.when().get("/q/health/ready").then()
+                .body("status", is("UP"),
+                        "checks.data", containsInAnyOrder(hasKey(MongoHealthCheck.CLIENT_DEFAULT)),
+                        "checks.data", containsInAnyOrder(hasKey(MongoHealthCheck.CLIENT_DEFAULT_REACTIVE)),
+                        "checks.status", containsInAnyOrder("UP"),
+                        "checks.name", containsInAnyOrder("MongoDB connection health check"));
     }
 
 }
