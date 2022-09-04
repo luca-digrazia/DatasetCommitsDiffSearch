eedea424f5b0219778fb930dcaf8c18bc13c6d28
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:21:11 +0200

    added a new specific message matcher test and simplified specific matching with a map that is iterated now #NO_ISSUE

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/test/java/org/graylog2/streams/matchers/MessageMatcherTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/test/java/org/graylog2/streams/matchers/MessageMatcherTest.java
@@ -24,17 +24,13 @@ import java.util.Map;
 import org.bson.types.ObjectId;
 import com.mongodb.BasicDBObject;
 import java.util.HashMap;
+import org.graylog2.messagehandlers.gelf.GELFMessage;
 import org.graylog2.streams.StreamRule;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MessageMatcherTest {
-    @Test
-    public void testTheTruthToWork() {
-        assertTrue(true);
-    }
 
-/*
     @Test
     public void testSuccessfulMatch() {
         String message = "ohai thar|foo";
@@ -78,7 +74,6 @@ public class MessageMatcherTest {
     /*
      * Testing specific cases reported by users.
      */
-/*
     @Test
     public void testSpecificMatches() {
         Map<String, String> cases = new HashMap<String, String>();
@@ -106,6 +101,6 @@ public class MessageMatcherTest {
             assertTrue(matcher.match(msg, rule));
         }
     }
-*/
+
 
 }
