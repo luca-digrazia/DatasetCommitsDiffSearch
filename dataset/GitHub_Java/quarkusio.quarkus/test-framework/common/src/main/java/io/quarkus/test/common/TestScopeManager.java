01de6a03dd0e32f99eac7f07a970e9a78d5b62f3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:53:10 +0200

    TestScopeManager. tearDown() calls setup() instead of tearDown() #3784

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/TestScopeManager.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/TestScopeManager.java
@@ -8,23 +8,23 @@ import io.quarkus.deployment.test.TestScopeSetup;
 
 public class TestScopeManager {
 
-    private static final List<TestScopeSetup> SCOPE_MANAGERS = new ArrayList<>();
+    private static final List<TestScopeSetup> scopeManagers = new ArrayList<>();
 
     static {
         for (TestScopeSetup i : ServiceLoader.load(TestScopeSetup.class)) {
-            SCOPE_MANAGERS.add(i);
+            scopeManagers.add(i);
         }
     }
 
-    public static void setup(boolean isSubstrateTest) {
-        for (TestScopeSetup i : SCOPE_MANAGERS) {
-            i.setup(isSubstrateTest);
+    public static void setup() {
+        for (TestScopeSetup i : scopeManagers) {
+            i.setup();
         }
     }
 
-    public static void tearDown(boolean isSubstrateTest) {
-        for (TestScopeSetup i : SCOPE_MANAGERS) {
-            i.tearDown(isSubstrateTest);
+    public static void tearDown() {
+        for (TestScopeSetup i : scopeManagers) {
+            i.tearDown();
         }
     }
 }
