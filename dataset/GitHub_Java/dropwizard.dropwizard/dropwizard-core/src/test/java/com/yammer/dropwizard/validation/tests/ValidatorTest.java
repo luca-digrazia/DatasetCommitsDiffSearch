9ff93cfc3a0a0ff9f832b7ab1240c858591315fa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:02:01 +0200

    Only enable validator tests in English locales.
        Sadly, there doesn't seem to be a way to force a locale for
        Hibernate Validator. Closes #32.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/validation/tests/ValidatorTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/validation/tests/ValidatorTest.java
@@ -13,7 +13,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ValidatorTest {
-    @SuppressWarnings("unused")
     public static class Example {
         @NotNull
         private String notNull = null;
