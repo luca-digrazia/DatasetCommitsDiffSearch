116ddd6849b8e30283b5b56d404c0557d771dd61
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:04:21 +0200

    Don't clone the result of `toByteArray`, which already [allocates a new array](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/ByteArrayOutputStream.html#toByteArray()).
        PiperOrigin-RevId: 378214545

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/vfs/inmemoryfs/InMemoryFileInfo.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/vfs/inmemoryfs/InMemoryFileInfo.java
@@ -1,4 +1,4 @@
-// Copyright 2014 The Bazel Authors. All rights reserved.
+// Copyright 2019 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,15 +11,20 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
 package com.google.devtools.build.lib.vfs.inmemoryfs;
 
+import com.google.common.io.ByteStreams;
+import com.google.devtools.build.lib.clock.Clock;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
-import com.google.devtools.build.lib.util.Clock;
-import com.google.devtools.build.lib.util.Preconditions;
-
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.util.function.Consumer;
 
 /**
  * InMemoryFileInfo manages file contents by storing them entirely in memory.
@@ -33,7 +38,7 @@ public class InMemoryFileInfo extends FileInfo {
    */
   protected byte[] content;
 
-  protected InMemoryFileInfo(Clock clock) {
+  InMemoryFileInfo(Clock clock) {
     super(clock);
     content = new byte[0]; // New files start out empty.
   }
@@ -44,8 +49,13 @@ public class InMemoryFileInfo extends FileInfo {
   }
 
   @Override
-  public synchronized byte[] readContent() {
-    return content.clone();
+  public byte[] getxattr(String name) {
+    return null;
+  }
+
+  @Override
+  public byte[] getFastDigest() {
+    return null;
   }
 
   private synchronized void setContent(byte[] newContent) {
@@ -54,44 +64,65 @@ public class InMemoryFileInfo extends FileInfo {
   }
 
   @Override
-  protected synchronized OutputStream getOutputStream(boolean append)
-      throws IOException {
-    OutputStream out = new ByteArrayOutputStream() {
-      private boolean closed = false;
+  public synchronized InputStream getInputStream() {
+    return new ByteArrayInputStream(content);
+  }
+
+  @Override
+  public ReadableByteChannel createReadableByteChannel() {
+    return new ReadableByteChannel() {
+      private int offset = 0;
 
       @Override
-      public void write(byte[] data) throws IOException {
-        Preconditions.checkState(!closed);
-        super.write(data);
+      public int read(ByteBuffer dst) {
+        if (offset >= content.length) {
+          return -1;
+        }
+        int length = Math.min(dst.remaining(), content.length - offset);
+        dst.put(content, offset, length);
+        offset += length;
+        return length;
       }
 
       @Override
-      public void write(int dataByte) {
-        Preconditions.checkState(!closed);
-        super.write(dataByte);
+      public boolean isOpen() {
+        return true;
       }
 
       @Override
-      public void write(byte[] data, int offset, int length) {
-        Preconditions.checkState(!closed);
-        super.write(data, offset, length);
-      }
-
-      @Override
-      public void close() {
-        flush();
-        closed = true;
-      }
-
-      @Override
-      public void flush() {
-        setContent(toByteArray().clone());
-      }
+      public void close() {}
     };
+  }
 
+  @Override
+  public synchronized OutputStream getOutputStream(boolean append) {
+    OutputStream out = new InMemoryOutputStream(this::setContent);
     if (append) {
-      out.write(readContent());
+      try (InputStream in = getInputStream()) {
+        ByteStreams.copy(in, out);
+      } catch (IOException e) {
+        throw new IllegalStateException(e);
+      }
     }
     return out;
+  }
+
+  /** A {@link ByteArrayOutputStream} which notifiers a callback when it has flushed its data. */
+  public static class InMemoryOutputStream extends ByteArrayOutputStream {
+    private final Consumer<byte[]> receiver;
+
+    public InMemoryOutputStream(Consumer<byte[]> receiver) {
+      this.receiver = receiver;
+    }
+
+    @Override
+    public void close() {
+      flush();
+    }
+
+    @Override
+    public synchronized void flush() {
+      receiver.accept(toByteArray());
+    }
   }
 }
