2aaf483f6131051acf322c47b83c1ee3d750d992
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:53:06 +0200

    KubernetesDeploy - close client

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesDeploy.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesDeploy.java
@@ -5,8 +5,6 @@ import static io.quarkus.kubernetes.deployment.Constants.DEPLOY;
 
 import java.util.function.BooleanSupplier;
 
-import javax.net.ssl.SSLHandshakeException;
-
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.logging.Logger;
@@ -19,7 +17,6 @@ public class KubernetesDeploy implements BooleanSupplier {
 
     private final Logger LOGGER = Logger.getLogger(KubernetesDeploy.class);
     private static boolean serverFound = false;
-    private static boolean alreadyWarned = false;
 
     @Override
     public boolean getAsBoolean() {
@@ -39,17 +36,6 @@ public class KubernetesDeploy implements BooleanSupplier {
             serverFound = true;
             return true;
         } catch (Exception e) {
-            if (!alreadyWarned) {
-                if (e.getCause() instanceof SSLHandshakeException) {
-                    String message = "Although a Kubernetes deployment was requested, it will however not take place because the API Server certificates are not trusted. The certificates can be configured using the relevant configuration propertiers under the 'quarkus.kubernetes-client' config root, or \"quarkus.kubernetes-client.trust-certs=true\" can be set to explicitly trust the certificates (not recommended)";
-                    LOGGER.warn(message);
-                } else {
-                    LOGGER.error(
-                            "Although a Kubernetes deployment was requested, it will however not take place because there was an error during communication with the API Server: "
-                                    + e.getMessage());
-                }
-                alreadyWarned = true;
-            }
             return false;
         }
     }
