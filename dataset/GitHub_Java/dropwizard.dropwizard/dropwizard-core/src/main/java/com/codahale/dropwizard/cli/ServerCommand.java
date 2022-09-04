7e821f23aab30b330510bab3d3451c8b17d4e63e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:43:53 +0200

    Make sure ServerCommand doesn't eat errors.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/codahale/dropwizard/cli/ServerCommand.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/codahale/dropwizard/cli/ServerCommand.java
@@ -1,31 +1,26 @@
 package com.codahale.dropwizard.cli;
 
+import com.codahale.dropwizard.Application;
 import com.codahale.dropwizard.Configuration;
-import com.codahale.dropwizard.Service;
-import com.codahale.dropwizard.config.Environment;
-import com.codahale.dropwizard.config.ServerFactory;
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
+import com.codahale.dropwizard.setup.Environment;
 import net.sourceforge.argparse4j.inf.Namespace;
 import org.eclipse.jetty.server.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
-// TODO: 10/12/11 <coda> -- write tests for ServerCommand
-
 /**
- * Runs a service as an HTTP server.
+ * Runs a application as an HTTP server.
  *
  * @param <T> the {@link Configuration} subclass which is loaded from the configuration file
  */
 public class ServerCommand<T extends Configuration> extends EnvironmentCommand<T> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServerCommand.class);
+
     private final Class<T> configurationClass;
 
-    public ServerCommand(Service<T> service) {
-        super(service, "server", "Runs the Dropwizard service as an HTTP server");
-        this.configurationClass = service.getConfigurationClass();
+    public ServerCommand(Application<T> application) {
+        super(application, "server", "Runs the Dropwizard application as an HTTP server");
+        this.configurationClass = application.getConfigurationClass();
     }
 
     /*
@@ -39,26 +34,13 @@ public class ServerCommand<T extends Configuration> extends EnvironmentCommand<T
 
     @Override
     protected void run(Environment environment, Namespace namespace, T configuration) throws Exception {
-        final Server server = new ServerFactory(configuration.getServerConfiguration(),
-                                                environment.getName()).buildServer(environment);
-        final Logger logger = LoggerFactory.getLogger(ServerCommand.class);
-        logBanner(environment.getName(), logger);
+        final Server server = configuration.getServerFactory().build(environment);
         try {
             server.start();
         } catch (Exception e) {
-            logger.error("Unable to start server, shutting down", e);
+            LOGGER.error("Unable to start server, shutting down", e);
             server.stop();
-        }
-    }
-
-    private void logBanner(String name, Logger logger) {
-        try {
-            final String banner = Resources.toString(Resources.getResource("banner.txt"),
-                                                     Charsets.UTF_8);
-            logger.info("Starting {}\n{}", name, banner);
-        } catch (IllegalArgumentException | IOException ignored) {
-            // don't display the banner if there isn't one
-            logger.info("Starting {}", name);
+            throw e;
         }
     }
 }
