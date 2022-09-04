938cd0bc40df1f916c10f163f1e04317686e1317
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:01:43 +0200

    Adding required permission to AgentResource.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/agent/AgentResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/agent/AgentResource.java
@@ -1,11 +1,32 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.rest.resources.system.agent;
 
 import com.codahale.metrics.annotation.Timed;
+import com.google.common.base.Function;
+import com.google.common.primitives.Ints;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.graylog2.Configuration;
 import org.graylog2.agents.Agent;
 import org.graylog2.agents.AgentService;
 import org.graylog2.agents.Agents;
@@ -13,6 +34,8 @@ import org.graylog2.database.NotFoundException;
 import org.graylog2.rest.models.agent.responses.AgentList;
 import org.graylog2.rest.models.agent.responses.AgentSummary;
 import org.graylog2.shared.rest.resources.RestResource;
+import org.graylog2.shared.security.RestPermissions;
+import org.joda.time.DateTime;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -23,24 +46,43 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Api(value = "System/Agents", description = "Management of graylog agents.")
+@Api(value = "System/Agents", description = "Management of Graylog agents.")
 @Path("/system/agents")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@RequiresAuthentication
+@RequiresPermissions(RestPermissions.AGENTS_READ)
 public class AgentResource extends RestResource {
     private final AgentService agentService;
+    private final LostAgentFunction lostAgentFunction;
+
+    public class LostAgentFunction implements Function<Agent, Boolean> {
+        private final long timeOutInSeconds;
+
+        @Inject
+        public LostAgentFunction(long timeOutInSeconds) {
+            this.timeOutInSeconds = timeOutInSeconds;
+        }
+
+        @Override
+        public Boolean apply(Agent agent) {
+            final DateTime threshold = DateTime.now().minusSeconds(Ints.checkedCast(timeOutInSeconds));
+            return agent.getLastSeen().isAfter(threshold);
+        }
+    }
 
     @Inject
-    public AgentResource(AgentService agentService) {
+    public AgentResource(AgentService agentService, Configuration config) {
         this.agentService = agentService;
+        this.lostAgentFunction = new LostAgentFunction(config.getAgentInactiveThreshold().toSeconds());
     }
 
     @GET
     @Timed
-    @ApiOperation(value = "lists all existing agent registrations")
+    @ApiOperation(value = "Lists all existing agent registrations")
     public AgentList list() {
         final List<Agent> agents = agentService.all();
-        final List<AgentSummary> agentSummaries = Agents.toSummaryList(agents);
+        final List<AgentSummary> agentSummaries = Agents.toSummaryList(agents, lostAgentFunction);
         return AgentList.create(agentSummaries);
     }
 
@@ -56,7 +98,7 @@ public class AgentResource extends RestResource {
         final Agent agent = agentService.findById(agentId);
 
         if (agent != null)
-            return Agents.toSummary(agent);
+            return agent.toSummary(lostAgentFunction);
         else
             throw new NotFoundException("Agent <" + agentId + "> not found!");
     }
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/security/RestPermissions.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/security/RestPermissions.java
@@ -18,27 +18,20 @@ package org.graylog2.shared.security;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-
-import static com.google.common.base.Strings.isNullOrEmpty;
+import java.util.*;
 
 public class RestPermissions {
     private static final Logger LOG = LoggerFactory.getLogger(RestPermissions.class);
 
-    /**
-     *These should all be in the form of "group:action", because {@link #allPermissions()} below depends on it.
-     * Should this ever change, you need to adapt the code below, too.
-     */
+    // These should all be in the form of "group:action", because allPermissions() below depends on it.
+    // Should this ever change, you need to adapt the code below, too.
     public static final String USERS_CREATE = "users:create";
     public static final String USERS_EDIT = "users:edit";
     public static final String USERS_LIST = "users:list";
@@ -119,62 +112,39 @@ public class RestPermissions {
     public static final String BUNDLE_EXPORT = "bundle:export";
     public static final String JOURNAL_READ = "journal:read";
     public static final String JOURNAL_EDIT = "journal:edit";
-    public static final String COLLECTORS_READ = "collectors:read";
+    public static final String AGENTS_READ = "agents:read";
+
+    private static Map<String, Collection<String>> allPermissions;
+
+    public static Set<String> adminPermissions = Sets.newHashSet("*");
 
     // Standard set of permissions of readers.
-    private static final Set<String> READER_BASE_PERMISSIONS = ImmutableSet.<String>builder().add(
-                    BUFFERS_READ,
-                    FIELDNAMES_READ,
-                    INDEXERCLUSTER_READ,
-                    INPUTS_READ,
-                    JOURNAL_READ,
-                    JVMSTATS_READ,
-                    MESSAGECOUNT_READ,
-                    MESSAGES_READ,
-                    METRICS_READ,
-                    SYSTEM_READ,
-                    THROUGHPUT_READ,
-                    SAVEDSEARCHES_CREATE,
-                    SAVEDSEARCHES_EDIT,
-                    SAVEDSEARCHES_READ
-    ).build();
-
-    private static final Map<String, Collection<String>> ALL_PERMISSIONS;
-
-    static {
-        final Field[] declaredFields = RestPermissions.class.getDeclaredFields();
-        final ListMultimap<String, String> all = ArrayListMultimap.create();
-        for (Field declaredField : declaredFields) {
-            if (!Modifier.isStatic(declaredField.getModifiers())) {
-                continue;
-            }
-            if (!String.class.isAssignableFrom(declaredField.getType())) {
-                continue;
-            }
-            declaredField.setAccessible(true);
-            try {
-                final String permission = (String) declaredField.get(RestPermissions.class);
-                final Iterator<String> split = Splitter.on(':').limit(2).split(permission).iterator();
-                final String group = split.next();
-                final String action = split.next();
-                all.put(group, action);
-            } catch (IllegalAccessException ignored) {
-            }
-        }
-        ALL_PERMISSIONS = all.asMap();
-    }
+    public static Set<String> readerBasePermissions = Sets.newHashSet(
+            BUFFERS_READ,
+            FIELDNAMES_READ,
+            INDEXERCLUSTER_READ,
+            INPUTS_READ,
+            JOURNAL_READ,
+            JVMSTATS_READ,
+            MESSAGECOUNT_READ,
+            MESSAGES_READ,
+            METRICS_READ,
+            SYSTEM_READ,
+            THROUGHPUT_READ,
+            SAVEDSEARCHES_CREATE,
+            SAVEDSEARCHES_EDIT,
+            SAVEDSEARCHES_READ
+    );
 
     public static Set<String> readerPermissions(String username) {
-        final ImmutableSet.Builder<String> perms = ImmutableSet.<String>builder().addAll(READER_BASE_PERMISSIONS);
-        if (isNullOrEmpty(username)) {
+        final HashSet<String> perms = Sets.newHashSet(readerBasePermissions);
+        if (username == null || username.isEmpty()) {
             LOG.error("Username cannot be empty or null for creating reader permissions");
             throw new IllegalArgumentException("Username was null or empty when getting reader permissions.");
         }
-
         perms.add(perInstance(USERS_EDIT, username));
         perms.add(perInstance(USERS_PASSWORDCHANGE, username));
-
-        return perms.build();
+        return perms;
     }
 
     public static String perInstance(String permission, String instance) {
@@ -182,7 +152,29 @@ public class RestPermissions {
         return permission + ":" + instance;
     }
 
-    public static Map<String, Collection<String>> allPermissions() {
-        return ALL_PERMISSIONS;
+    public static synchronized Map<String, Collection<String>> allPermissions() {
+        if (allPermissions == null) {
+            final Field[] declaredFields = RestPermissions.class.getDeclaredFields();
+            ListMultimap<String, String> all = ArrayListMultimap.create();
+            for (Field declaredField : declaredFields) {
+                if (! Modifier.isStatic(declaredField.getModifiers())) {
+                    continue;
+                }
+                if (! String.class.isAssignableFrom(declaredField.getType())) {
+                    continue;
+                }
+                declaredField.setAccessible(true);
+                try {
+                    final String permission = (String) declaredField.get(RestPermissions.class);
+                    final Iterator<String> split = Splitter.on(':').limit(2).split(permission).iterator();
+                    final String group = split.next();
+                    final String action = split.next();
+                    all.put(group, action);
+                } catch (IllegalAccessException ignored) {
+                }
+            }
+            allPermissions = all.asMap();
+        }
+        return allPermissions;
     }
 }
