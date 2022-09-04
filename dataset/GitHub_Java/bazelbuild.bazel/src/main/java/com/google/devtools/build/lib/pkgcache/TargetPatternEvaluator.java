127151993e99c9cbe667635f993ae8aa11bd6f3d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:27:37 +0200

    Migrate some tests from TPETest to LPRTest
        TargetPatternEvaluatorTest is using the deprecated
        TargetPatternEvaluator interface, which I want to remove. Also update
        some of the comments in the source files.
    
        PiperOrigin-RevId: 213792418

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/pkgcache/TargetPatternEvaluator.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/pkgcache/TargetPatternEvaluator.java
@@ -14,18 +14,12 @@
 
 package com.google.devtools.build.lib.pkgcache;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.devtools.build.lib.cmdline.ResolvedTargets;
 import com.google.devtools.build.lib.cmdline.TargetParsingException;
-import com.google.devtools.build.lib.concurrent.ThreadSafety;
-import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadHostile;
-import com.google.devtools.build.lib.events.EventHandler;
+import com.google.devtools.build.lib.events.ExtendedEventHandler;
 import com.google.devtools.build.lib.packages.Target;
 import com.google.devtools.build.lib.vfs.PathFragment;
-
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 /**
  * A parser for target patterns.  Target patterns are a generalisation of
@@ -41,58 +35,20 @@ import java.util.Map;
  * means "all targets beneath <tt>foo</tt> except for those targets in
  * package <tt>foo/bar</tt>.
  */
-@ThreadSafety.ConditionallyThreadSafe // as long as you don't call updateOffset.
+// TODO(ulfjack): Delete this interface - it's only used in tests.
 public interface TargetPatternEvaluator {
   static FilteringPolicy DEFAULT_FILTERING_POLICY = FilteringPolicies.NO_FILTER;
 
   /**
-   * Attempts to parse an ordered list of target patterns, computing the union
-   * of the set of targets represented by each pattern, unless it is preceded by
-   * "-", in which case the set difference is computed.  Implements the
-   * specification described in the class-level comment.
+   * Attempts to parse an ordered list of target patterns, computing the union of the set of targets
+   * represented by each pattern, unless it is preceded by "-", in which case the set difference is
+   * computed. Implements the specification described in the class-level comment.
    */
-  ResolvedTargets<Target> parseTargetPatternList(EventHandler eventHandler,
-      List<String> targetPatterns, FilteringPolicy policy, boolean keepGoing)
+  ResolvedTargets<Target> parseTargetPatternList(
+      PathFragment relativeWorkingDirectory,
+      ExtendedEventHandler eventHandler,
+      List<String> targetPatterns,
+      FilteringPolicy policy,
+      boolean keepGoing)
       throws TargetParsingException, InterruptedException;
-
-  /**
-  * Attempts to parse a single target pattern while consulting the package
-   * cache to check for the existence of packages and directories and the build
-   * targets in them.  Implements the specification described in the
-   * class-level comment.  Returns a {@link ResolvedTargets} object.
-   *
-   * <p>If an error is encountered, a {@link TargetParsingException} is thrown,
-   * unless {@code keepGoing} is set to true. In that case, the returned object
-   * will have its error bit set.
-   */
-  ResolvedTargets<Target> parseTargetPattern(EventHandler eventHandler, String pattern,
-      boolean keepGoing) throws TargetParsingException, InterruptedException;
-
-  /**
-   * Attempts to parse and load the given collection of patterns; the returned map contains the
-   * results for each pattern successfully parsed.
-   *
-   * <p>If an error is encountered, a {@link TargetParsingException} is thrown, unless {@code
-   * keepGoing} is set to true. In that case, the patterns that failed to load have the error flag
-   * set.
-   */
-  Map<String, ResolvedTargets<Target>> preloadTargetPatterns(EventHandler eventHandler,
-      Collection<String> patterns, boolean keepGoing)
-          throws TargetParsingException, InterruptedException;
-
-  /**
-   * Update the parser's offset, given the workspace and working directory.
-   *
-   * @param relativeWorkingDirectory the working directory relative to the workspace
-   */
-  @ThreadHostile
-  void updateOffset(PathFragment relativeWorkingDirectory);
-
-  /**
-   * @return the offset of this parser from the root of the workspace.
-   *         Non-absolute package-names will be resolved relative
-   *         to this offset.
-   */
-  @VisibleForTesting
-  String getOffset();
 }
