5a57af10c984982607e8db65f779ef8de527cc12
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:04:52 +0200

    Use @NotEmpty over @NotNull for Strings.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -1,13 +1,8 @@
 package com.example.helloworld;
 
-import com.codahale.dropwizard.Configuration;
-import com.codahale.dropwizard.db.DatabaseConfiguration;
 import com.example.helloworld.core.Template;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yammer.dropwizard.config.Configuration;
 import org.hibernate.validator.constraints.NotEmpty;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 public class HelloWorldConfiguration extends Configuration {
     @NotEmpty
@@ -16,41 +11,23 @@ public class HelloWorldConfiguration extends Configuration {
     @NotEmpty
     private String defaultName = "Stranger";
 
-    @Valid
-    @NotNull
-    private DatabaseConfiguration database = new DatabaseConfiguration();
-
-    @JsonProperty
     public String getTemplate() {
         return template;
     }
 
-    @JsonProperty
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @JsonProperty
     public String getDefaultName() {
         return defaultName;
     }
 
-    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
     public void setDefaultName(String defaultName) {
         this.defaultName = defaultName;
     }
 
     public Template buildTemplate() {
         return new Template(template, defaultName);
-    }
-
-    @JsonProperty
-    public DatabaseConfiguration getDatabaseConfiguration() {
-        return database;
-    }
-
-    @JsonProperty
-    public void setDatabaseConfiguration(DatabaseConfiguration databaseConfiguration) {
-        this.database = databaseConfiguration;
     }
 }
