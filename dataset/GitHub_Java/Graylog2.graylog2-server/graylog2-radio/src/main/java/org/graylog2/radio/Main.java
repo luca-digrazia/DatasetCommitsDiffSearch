667354dd734e8ca212a1b7e350333d19fd6e49cf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:54:58 +0200

    Add CLI parameters to dump the configuration of graylog2-radio
        The following CLI parameters have been implemented:
    
        * `--dump-default-config` dumps the default configuration as defined per default values in the `Configuration` class.
        * `--dump-config` dumps the effective configuration after processing the config file.
    
        fixes #616

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-radio/src/main/java/org/graylog2/radio/Main.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-radio/src/main/java/org/graylog2/radio/Main.java
@@ -24,12 +24,9 @@ import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.log4j.InstrumentedAppender;
 import com.github.joschi.jadconfig.JadConfig;
-import com.github.joschi.jadconfig.ParameterException;
 import com.github.joschi.jadconfig.RepositoryException;
 import com.github.joschi.jadconfig.ValidationException;
-import com.github.joschi.jadconfig.repositories.EnvironmentRepository;
 import com.github.joschi.jadconfig.repositories.PropertiesRepository;
-import com.github.joschi.jadconfig.repositories.SystemPropertiesRepository;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ServiceManager;
@@ -55,7 +52,6 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -65,8 +61,6 @@ import java.util.Map;
 public class Main extends NodeRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
-    private static final String ENVIRONMENT_PREFIX = "GRAYLOG2_";
-    private static final String PROPERTIES_PREFIX = "graylog2.";
 
     /**
      * @param args the command line arguments
@@ -175,11 +169,7 @@ public class Main extends NodeRunner {
         final Configuration configuration = new Configuration();
 
         jadConfig.addConfigurationBean(configuration);
-        jadConfig.setRepositories(Arrays.asList(
-                new EnvironmentRepository(ENVIRONMENT_PREFIX),
-                new SystemPropertiesRepository(PROPERTIES_PREFIX),
-                new PropertiesRepository(configFile)
-        ));
+        jadConfig.setRepository(new PropertiesRepository(configFile));
 
         LOG.debug("Loading configuration from config file: {}", configFile);
         try {
@@ -187,7 +177,7 @@ public class Main extends NodeRunner {
         } catch (RepositoryException e) {
             LOG.error("Couldn't load configuration: {}", e.getMessage());
             System.exit(1);
-        } catch (ParameterException | ValidationException e) {
+        } catch (ValidationException e) {
             LOG.error("Invalid configuration", e);
             System.exit(1);
         }
