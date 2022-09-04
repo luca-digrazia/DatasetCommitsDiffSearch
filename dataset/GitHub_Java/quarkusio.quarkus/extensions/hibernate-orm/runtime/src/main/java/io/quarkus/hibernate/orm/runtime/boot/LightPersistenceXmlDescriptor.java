1ae0320002a60fbddde2918dfd140c013f5b24f6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:42:24 +0200

    Hibernate ORM - Throw an exception if datasource provided in persistence units

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/boot/LightPersistenceXmlDescriptor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/boot/LightPersistenceXmlDescriptor.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.hibernate.orm.runtime.boot;
 
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.persistence.SharedCacheMode;
 import javax.persistence.ValidationMode;
@@ -34,7 +52,7 @@ public final class LightPersistenceXmlDescriptor implements PersistenceUnitDescr
         this.validationMode = toClone.getValidationMode();
         this.sharedCachemode = toClone.getSharedCacheMode();
         this.managedClassNames = Collections.unmodifiableList(toClone.getManagedClassNames());
-        this.properties = toClone.getProperties();
+        this.properties = filterNonStrings(toClone.getProperties());
         verifyIgnoredFields(toClone);
     }
 
@@ -59,6 +77,25 @@ public final class LightPersistenceXmlDescriptor implements PersistenceUnitDescr
         if (toClone.getNonJtaDataSource() != null) {
             throw new UnsupportedOperationException("Value found for #getNonJtaDataSource : not supported");
         }
+    }
+
+    private static final Properties filterNonStrings(final Properties properties) {
+        Properties clean = new Properties();
+        final Set<Map.Entry<Object, Object>> entries = properties.entrySet();
+        for (Map.Entry<Object, Object> e : entries) {
+            final Object key = e.getKey();
+            if (!(key instanceof String)) {
+                log.infof("Ignoring persistence unit property key '%s' as it's not a String", key);
+                continue;
+            }
+            final Object value = e.getValue();
+            if (!(value instanceof String)) {
+                log.infof("Ignoring persistence unit property for key '%s' as the value is not a String", key);
+                continue;
+            }
+            clean.setProperty((String) key, (String) value);
+        }
+        return clean;
     }
 
     @Override
