95bdee9d566a02d8b4a50c8e1f0bd3b8243924d2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:32:47 +0200

    Split out the InvocationPolicy parser from the enforcer.
        The encapsulation was off, since the format in which we accept policy has nothing to do with how we then enforce the policy.
    
        PiperOrigin-RevId: 151471747

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/commands/CanonicalizeCommand.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/commands/CanonicalizeCommand.java
@@ -14,7 +14,6 @@
 package com.google.devtools.build.lib.runtime.commands;
 
 import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableList;
 import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.flags.InvocationPolicyEnforcer;
 import com.google.devtools.build.lib.flags.InvocationPolicyParser;
@@ -23,14 +22,13 @@ import com.google.devtools.build.lib.runtime.BlazeCommandUtils;
 import com.google.devtools.build.lib.runtime.BlazeRuntime;
 import com.google.devtools.build.lib.runtime.Command;
 import com.google.devtools.build.lib.runtime.CommandEnvironment;
-import com.google.devtools.build.lib.runtime.proto.InvocationPolicyOuterClass.FlagPolicy;
-import com.google.devtools.build.lib.runtime.proto.InvocationPolicyOuterClass.InvocationPolicy;
 import com.google.devtools.build.lib.util.ExitCode;
 import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionsBase;
 import com.google.devtools.common.options.OptionsParser;
 import com.google.devtools.common.options.OptionsParsingException;
 import com.google.devtools.common.options.OptionsProvider;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -48,63 +46,16 @@ import java.util.List;
 public final class CanonicalizeCommand implements BlazeCommand {
 
   public static class Options extends OptionsBase {
-    @Option(
-      name = "for_command",
-      defaultValue = "build",
-      category = "misc",
-      help = "The command for which the options should be canonicalized."
-    )
+    @Option(name = "for_command",
+            defaultValue = "build",
+            category = "misc",
+            help = "The command for which the options should be canonicalized.")
     public String forCommand;
 
-    @Option(
-      name = "invocation_policy",
-      defaultValue = "",
-      help = "Applies an invocation policy to the options to be canonicalized."
-    )
+    @Option(name = "invocation_policy",
+        defaultValue = "",
+        help = "Applies an invocation policy to the options to be canonicalized.")
     public String invocationPolicy;
-
-    @Option(
-      name = "canonicalize_policy",
-      defaultValue = "false",
-      help =
-          "Output the canonical policy, after expansion and filtering. To keep the output "
-              + "clean, the canonicalized command arguments will NOT be shown when this option is "
-              + "set to true."
-    )
-    public boolean canonicalizePolicy;
-
-    @Option(
-      name = "show_warnings",
-      defaultValue = "false",
-      help = "Output parser warnings to standard error (e.g. for conflicting flag options)."
-    )
-    public boolean showWarnings;
-  }
-
-  /**
-   * These options are used by the incompatible_changes_conflict_test.sh integration test, which
-   * confirms that the warning for conflicting expansion options is working correctly. These flags
-   * are undocumented no-ops, and are not to be used by anything outside of that test.
-   */
-  public static class FlagClashCanaryOptions extends OptionsBase {
-    @Option(name = "flag_clash_canary", defaultValue = "false", category = "undocumented")
-    public boolean flagClashCanary;
-
-    @Option(
-      name = "flag_clash_canary_expander1",
-      defaultValue = "null",
-      category = "undocumented",
-      expansion = {"--flag_clash_canary=1"}
-    )
-    public Void flagClashCanaryExpander1;
-
-    @Option(
-      name = "flag_clash_canary_expander2",
-      defaultValue = "null",
-      category = "undocumented",
-      expansion = {"--flag_clash_canary=0"}
-    )
-    public Void flagClashCanaryExpander2;
   }
 
   @Override
@@ -119,41 +70,23 @@ public final class CanonicalizeCommand implements BlazeCommand {
       return ExitCode.COMMAND_LINE_ERROR;
     }
     Collection<Class<? extends OptionsBase>> optionsClasses =
-        ImmutableList.<Class<? extends OptionsBase>>builder()
-            .addAll(BlazeCommandUtils.getOptions(
-                command.getClass(), runtime.getBlazeModules(), runtime.getRuleClassProvider()))
-            .add(FlagClashCanaryOptions.class)
-            .build();
+        BlazeCommandUtils.getOptions(
+            command.getClass(), runtime.getBlazeModules(), runtime.getRuleClassProvider());
     try {
+      
       OptionsParser parser = OptionsParser.newOptionsParser(optionsClasses);
       parser.setAllowResidue(false);
       parser.parse(options.getResidue());
 
-      InvocationPolicy policy =
-          InvocationPolicyParser.parsePolicy(canonicalizeOptions.invocationPolicy);
-      InvocationPolicyEnforcer invocationPolicyEnforcer = new InvocationPolicyEnforcer(policy);
+      InvocationPolicyEnforcer invocationPolicyEnforcer =
+          new InvocationPolicyEnforcer(
+              InvocationPolicyParser.parsePolicy(canonicalizeOptions.invocationPolicy));
       invocationPolicyEnforcer.enforce(parser, commandName);
 
-      if (canonicalizeOptions.showWarnings) {
-        for (String warning : parser.getWarnings()) {
-          env.getReporter().handle(Event.warn(warning));
-        }
-      }
+      List<String> result = parser.canonicalize();
 
-      // Print out the canonical invocation policy if requested.
-      if (canonicalizeOptions.canonicalizePolicy) {
-        List<FlagPolicy> effectiveFlagPolicies =
-            InvocationPolicyEnforcer.getEffectivePolicy(policy, parser);
-        InvocationPolicy effectivePolicy =
-            InvocationPolicy.newBuilder().addAllFlagPolicies(effectiveFlagPolicies).build();
-        env.getReporter().getOutErr().printOutLn(effectivePolicy.toString());
-
-      } else {
-        // Otherwise, print out the canonical command line
-        List<String> result = parser.canonicalize();
-        for (String piece : result) {
-          env.getReporter().getOutErr().printOutLn(piece);
-        }
+      for (String piece : result) {
+        env.getReporter().getOutErr().printOutLn(piece);
       }
     } catch (OptionsParsingException e) {
       env.getReporter().handle(Event.error(e.getMessage()));
