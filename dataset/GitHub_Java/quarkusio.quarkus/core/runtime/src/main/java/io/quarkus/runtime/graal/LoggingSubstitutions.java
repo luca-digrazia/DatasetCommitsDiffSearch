94f41812a0439fe4e4def2a04d37361cfb116150
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:03:04 +0200

    Do not rely on initialization of log manager via property in native image

--- dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/io/quarkus/runtime/graal/LoggingSubstitutions.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/io/quarkus/runtime/graal/LoggingSubstitutions.java
@@ -3,6 +3,7 @@ package io.quarkus.runtime.graal;
 import java.util.logging.Handler;
 
 import org.jboss.logmanager.LogContext;
+import org.jboss.logmanager.handlers.DelayedHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,8 +12,7 @@ import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
-import io.quarkus.bootstrap.logging.InitialConfigurator;
-import io.quarkus.bootstrap.logging.QuarkusDelayedHandler;
+import io.quarkus.runtime.logging.InitialConfigurator;
 
 /**
  */
@@ -38,7 +38,7 @@ final class Target_org_slf4j_LoggerFactory {
 final class Target_io_quarkus_runtime_logging_InitialConfigurator {
     @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.FromAlias)
     @Alias
-    public static QuarkusDelayedHandler DELAYED_HANDLER = new QuarkusDelayedHandler();
+    public static DelayedHandler DELAYED_HANDLER = new DelayedHandler();
 }
 
 @TargetClass(java.util.logging.Logger.class)
