35aa6b665ea8523abc4bca25be0321306390aa41
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:49:54 +0200

    Tune up Cli a bit.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/cli/Cli.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/cli/Cli.java
@@ -1,10 +1,10 @@
 package com.yammer.dropwizard.cli;
 
 import com.google.common.collect.Maps;
-import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.util.JarLocation;
 import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.action.VersionArgumentAction;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
@@ -12,42 +12,73 @@ import net.sourceforge.argparse4j.inf.Subparser;
 
 import java.util.SortedMap;
 
+/**
+ * The command-line runner for Dropwizard services.
+ */
 public class Cli {
     private static final String COMMAND_NAME_ATTR = "command";
+    private static final String[] HELP = { "-h" };
 
     private final SortedMap<String, Command> commands;
     private final Bootstrap<?> bootstrap;
     private final ArgumentParser parser;
 
-    public Cli(Bootstrap<?> bootstrap, Service<?> service) {
+    /**
+     * Create a new CLI interface for a service and its bootstrapped environment.
+     *
+     * @param serviceClass the service class
+     * @param bootstrap    the bootstrap for the service
+     */
+    public Cli(Class<?> serviceClass, Bootstrap<?> bootstrap) {
         this.commands = Maps.newTreeMap();
-        this.parser = ArgumentParsers.newArgumentParser("java -jar " + new JarLocation(service.getClass()))
-                                     .defaultHelp(true);
-        parser.defaultHelp(true);
+        this.parser = buildParser(serviceClass);
         this.bootstrap = bootstrap;
         for (Command command : bootstrap.getCommands()) {
             addCommand(command);
         }
     }
 
-    private void addCommand(Command command) {
-        commands.put(command.getName(), command);
-        parser.addSubparsers().help("Available commands");
-        final Subparser subparser = parser.addSubparsers().addParser(command.getName());
-        command.configure(subparser);
-        subparser.description(command.getDescription())
-                 .setDefault(COMMAND_NAME_ATTR, command.getName())
-                 .defaultHelp(true);
-    }
-
+    /**
+     * Runs the command line interface given some arguments.
+     *
+     * @param arguments the command line arguments
+     * @throws Exception if something goes wrong
+     */
     public void run(String[] arguments) throws Exception {
         try {
-            final Namespace namespace = parser.parseArgs((arguments.length == 0) ? new String[]{ "-h" } : arguments);
+            // assume -h if no arguments are given
+            final String[] args = (arguments.length == 0) ? HELP : arguments;
+            final Namespace namespace = parser.parseArgs(args);
             final Command command = commands.get(namespace.getString(COMMAND_NAME_ATTR));
             command.run(bootstrap, namespace);
         } catch (ArgumentParserException e) {
             parser.handleError(e);
             System.exit(1);
         }
+    }
+
+    private ArgumentParser buildParser(Class<?> serviceClass) {
+        final String usage = "java -jar " + new JarLocation(serviceClass);
+        final ArgumentParser p = ArgumentParsers.newArgumentParser(usage)
+                                                .defaultHelp(true);
+        final Package pkg = serviceClass.getPackage();
+        final String version = pkg != null ? pkg.getImplementationVersion() :
+                "No service version detected. Add a Implementation-Version " +
+                "entry to your JAR's manifest to enable this.";
+        p.version(version);
+        p.addArgument("-v", "--version")
+         .action(new VersionArgumentAction())
+         .help("show the service version and exit");
+        return p;
+    }
+
+    private void addCommand(Command command) {
+        commands.put(command.getName(), command);
+        parser.addSubparsers().help("available commands");
+        final Subparser subparser = parser.addSubparsers().addParser(command.getName());
+        command.configure(subparser);
+        subparser.description(command.getDescription())
+                 .setDefault(COMMAND_NAME_ATTR, command.getName())
+                 .defaultHelp(true);
     }
 }
