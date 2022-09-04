37810f5c522f5fe9ca6f40e584dbebae729ca36d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 20:58:21 +0200

    Refactor the StandaloneTestStrategyTest
        Use a fake instead of a mock for the ActionExecutionContext, simplify
        test code.
    
        PiperOrigin-RevId: 223325418

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/exec/StandaloneTestStrategyTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/exec/StandaloneTestStrategyTest.java
@@ -44,7 +44,6 @@ import com.google.devtools.build.lib.clock.Clock;
 import com.google.devtools.build.lib.events.ExtendedEventHandler;
 import com.google.devtools.build.lib.events.StoredEventHandler;
 import com.google.devtools.build.lib.exec.TestStrategy.TestOutputFormat;
-import com.google.devtools.build.lib.util.OS;
 import com.google.devtools.build.lib.util.io.FileOutErr;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
@@ -542,11 +541,6 @@ public final class StandaloneTestStrategyTest extends BuildViewTestCase {
                     /*forciblyRunRemotely=*/ false,
                     /*catastrophe=*/ false);
               } else {
-                String testName =
-                    OS.getCurrent() == OS.WINDOWS
-                        ? "standalone/failing_test.exe"
-                        : "standalone/failing_test";
-                assertThat(spawn.getEnvironment()).containsEntry("TEST_BINARY", testName);
                 return ImmutableList.of(xmlGeneratorSpawnResult);
               }
             });
