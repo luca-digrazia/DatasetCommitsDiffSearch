134f28edbda1b31683f0d030571bf75119ec21e7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:29:56 +0200

    ScopeInfo constructors that take DotName

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/ScopeInfo.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/ScopeInfo.java
@@ -1,77 +1,70 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.jboss.quarkus.arc.processor;
+package io.quarkus.arc.processor;
 
 import java.lang.annotation.Annotation;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.Dependent;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Singleton;
-
+import java.lang.annotation.Inherited;
+import java.util.Objects;
 import org.jboss.jandex.DotName;
 
-public enum ScopeInfo {
-
-    DEPENDENT(Dependent.class),
-    SINGLETON(Singleton.class),
-    APPLICATION(ApplicationScoped.class, true),
-    REQUEST(RequestScoped.class, true),
-    ;
+/**
+ * {@link ScopeInfo} is used to create a custom scope for use with {@link BeanConfigurator}.
+ * If using built in scopes, use {@link BuiltinScope} instead to get a {@link ScopeInfo} instance.
+ */
+public class ScopeInfo {
 
     private final DotName dotName;
 
-    private final Class<? extends Annotation> clazz;
-
     private final boolean isNormal;
 
-    private ScopeInfo(Class<? extends Annotation> clazz) {
-        this(clazz, false);
+    private boolean declaresInherited;
+
+    public ScopeInfo(Class<? extends Annotation> clazz, boolean isNormal) {
+        this.dotName = DotName.createSimple(clazz.getName());
+        this.isNormal = isNormal;
+        declaresInherited = clazz.getAnnotation(Inherited.class) != null;
     }
 
-    private ScopeInfo(Class<? extends Annotation> clazz, boolean isNormal) {
-        this.dotName = DotNames.create(clazz);
-        this.clazz = clazz;
+    public ScopeInfo(DotName clazz, boolean isNormal) {
+        this.dotName = clazz;
         this.isNormal = isNormal;
+        declaresInherited = true;
+    }
+
+    public ScopeInfo(DotName clazz, boolean isNormal, boolean declaresInherited) {
+        this.dotName = clazz;
+        this.isNormal = isNormal;
+        this.declaresInherited = declaresInherited;
     }
 
     public DotName getDotName() {
         return dotName;
     }
 
-    public Class<? extends Annotation> getClazz() {
-        return clazz;
-    }
-
     public boolean isNormal() {
         return isNormal;
     }
 
-    public boolean isDefault() {
-        return DEPENDENT == this;
+    public boolean declaresInherited() {
+        return declaresInherited;
     }
 
-    public static ScopeInfo from(DotName name) {
-        for (ScopeInfo scope : ScopeInfo.values()) {
-            if (scope.getDotName().equals(name)) {
-                return scope;
-            }
+    @Override
+    public int hashCode() {
+        return Objects.hash(dotName);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
         }
-        return null;
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ScopeInfo other = (ScopeInfo) obj;
+        return Objects.equals(dotName, other.dotName);
     }
 
 }
