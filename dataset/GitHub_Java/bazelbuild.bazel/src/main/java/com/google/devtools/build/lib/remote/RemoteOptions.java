3ffa62246d9b1a49a190addf89ff4d600344ef97
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:40:24 +0200

    Adding a temporary flag to Bazel to allow Platform override from the command     line. This will be used during development to test new toolchains in docker     containers.
        Example usage: --experimental_remote_platform_override='entry:{ name:"a" value:"b" } entry:{ name:"c" value:"d" }'
    
        TESTED=local server
    
        --
        PiperOrigin-RevId: 149933081
        MOS_MIGRATED_REVID=149933081

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/RemoteOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/RemoteOptions.java
@@ -15,41 +15,25 @@
 package com.google.devtools.build.lib.remote;
 
 import com.google.devtools.common.options.Option;
-import com.google.devtools.common.options.OptionDocumentationCategory;
 import com.google.devtools.common.options.OptionsBase;
-import com.google.devtools.common.options.proto.OptionFilters.OptionEffectTag;
 
 /** Options for remote execution and distributed caching. */
 public final class RemoteOptions extends OptionsBase {
   @Option(
-    name = "remote_rest_cache",
+    name = "rest_cache_url",
     defaultValue = "null",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help =
         "A base URL for a RESTful cache server for storing build artifacts."
             + "It has to support PUT, GET, and HEAD requests."
   )
-  public String remoteRestCache;
-
-  @Option(
-    name = "remote_rest_cache_pool_size",
-    defaultValue = "20",
-    category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "Size of the HTTP pool for making requests to the REST cache."
-  )
-  public int restCachePoolSize;
+  public String restCacheUrl;
 
   @Option(
     name = "hazelcast_node",
     defaultValue = "null",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "A comma separated list of hostnames of hazelcast nodes."
+    help = "A comma separated list of hostnames of hazelcast nodes. For client mode only."
   )
   public String hazelcastNode;
 
@@ -57,9 +41,7 @@ public final class RemoteOptions extends OptionsBase {
     name = "hazelcast_client_config",
     defaultValue = "null",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "A file path to a hazelcast client config XML file."
+    help = "A file path to a hazelcast client config XML file. For client mode only."
   )
   public String hazelcastClientConfig;
 
@@ -67,8 +49,6 @@ public final class RemoteOptions extends OptionsBase {
     name = "hazelcast_standalone_listen_port",
     defaultValue = "0",
     category = "build_worker",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help =
         "Runs an embedded hazelcast server that listens to this port. The server does not join"
             + " any cluster. This is useful for testing."
@@ -76,130 +56,86 @@ public final class RemoteOptions extends OptionsBase {
   public int hazelcastStandaloneListenPort;
 
   @Option(
-    name = "remote_executor",
+    name = "remote_worker",
     defaultValue = "null",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "HOST or HOST:PORT of a remote execution endpoint."
+    help =
+        "Hostname and port number of remote worker in the form of host:port. "
+            + "For client mode only."
   )
-  public String remoteExecutor;
+  public String remoteWorker;
 
   @Option(
     name = "remote_cache",
     defaultValue = "null",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "HOST or HOST:PORT of a remote caching endpoint."
+    help =
+        "Hostname and port number of remote gRPC cache in the form of host:port. "
+            + "For client mode only."
   )
   public String remoteCache;
 
   @Option(
-    name = "remote_timeout",
+    name = "grpc_max_chunk_size_bytes",
+    defaultValue = "400000", // <4MB. Bounded by the gRPC size limit on the overall message.
+    category = "remote",
+    help = "The maximal number of bytes to be sent in a single message. For client mode only."
+  )
+  public int grpcMaxChunkSizeBytes;
+
+  @Option(
+    name = "grpc_max_batch_inputs",
+    defaultValue = "100",
+    category = "remote",
+    help = "The maximal number of input file to be sent in a single batch. For client mode only."
+  )
+  public int grpcMaxBatchInputs;
+
+  @Option(
+    name = "grpc_max_batch_size_bytes",
+    defaultValue = "10485760", // 10MB
+    category = "remote",
+    help = "The maximal number of input bytes to be sent in a single batch. For client mode only."
+  )
+  public int grpcMaxBatchSizeBytes;
+
+  @Option(
+    name = "grpc_timeout_seconds",
     defaultValue = "60",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "The maximum number of seconds to wait for remote execution and cache calls."
+    help = "The maximal number of seconds to wait for remote calls. For client mode only."
   )
-  public int remoteTimeout;
+  public int grpcTimeoutSeconds;
 
   @Option(
     name = "remote_accept_cached",
     defaultValue = "true",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help = "Whether to accept remotely cached action results."
   )
   public boolean remoteAcceptCached;
 
   @Option(
-    name = "remote_local_fallback",
-    defaultValue = "false",
-    category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "Whether to fall back to standalone local execution strategy if remote execution fails."
-  )
-  public boolean remoteLocalFallback;
-
-  @Option(
-    name = "remote_upload_local_results",
+    name = "remote_allow_local_fallback",
     defaultValue = "true",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "Whether to upload locally executed action results to the remote cache."
+    help = "Whether to fall back to standalone strategy if remote fails."
   )
-  public boolean remoteUploadLocalResults;
+  public boolean remoteAllowLocalFallback;
+
+  @Option(
+    name = "remote_local_exec_upload_results",
+    defaultValue = "true",
+    category = "remote",
+    help = "Whether to upload action results to the remote cache after executing locally."
+  )
+  public boolean remoteLocalExecUploadResults;
 
   @Option(
     name = "experimental_remote_platform_override",
     defaultValue = "null",
     category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help = "Temporary, for testing only. Manually set a Platform to pass to remote execution."
   )
   public String experimentalRemotePlatformOverride;
-
-  @Option(
-    name = "remote_instance_name",
-    defaultValue = "",
-    category = "remote",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "Value to pass as instance_name in the remote execution API."
-  )
-  public String remoteInstanceName;
-
-  @Option(
-    name = "experimental_remote_retry",
-    defaultValue = "true",
-    category = "remote",
-    help = "Whether to retry transient remote execution/cache errors."
-  )
-  public boolean experimentalRemoteRetry;
-
-  @Option(
-    name = "experimental_remote_retry_start_delay_millis",
-    defaultValue = "100",
-    category = "remote",
-    help = "The initial delay before retrying a transient error."
-  )
-  public long experimentalRemoteRetryStartDelayMillis;
-
-  @Option(
-    name = "experimental_remote_retry_max_delay_millis",
-    defaultValue = "5000",
-    category = "remote",
-    help = "The maximum delay before retrying a transient error."
-  )
-  public long experimentalRemoteRetryMaxDelayMillis;
-
-  @Option(
-    name = "experimental_remote_retry_max_attempts",
-    defaultValue = "5",
-    category = "remote",
-    help = "The maximum number of attempts to retry a transient error."
-  )
-  public int experimentalRemoteRetryMaxAttempts;
-
-  @Option(
-    name = "experimental_remote_retry_multiplier",
-    defaultValue = "2",
-    category = "remote",
-    help = "The multiplier by which to increase the retry delay on transient errors."
-  )
-  public double experimentalRemoteRetryMultiplier;
-
-  @Option(
-    name = "experimental_remote_retry_jitter",
-    defaultValue = "0.1",
-    category = "remote",
-    help = "The random factor to apply to retry delays on transient errors."
-  )
-  public double experimentalRemoteRetryJitter;
 }
