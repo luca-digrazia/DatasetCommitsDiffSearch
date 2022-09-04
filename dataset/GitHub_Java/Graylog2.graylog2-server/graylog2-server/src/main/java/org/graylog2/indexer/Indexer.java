2fabf0e35e7c8d5563d5f2f67ef2cd21dc78de30
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:28:08 +0200

    add synthetic 0 values for missing field histogram buckets
        fixes Graylog2/graylog2-web-interface#590

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/Indexer.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/Indexer.java
@@ -8,7 +8,6 @@ import org.elasticsearch.action.WriteConsistencyLevel;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequest;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
-import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest.OpType;
@@ -37,8 +36,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
@@ -57,9 +54,7 @@ public class Indexer {
     private Messages messages;
     private Cluster cluster;
     private Indices indices;
-
-    private LinkedBlockingQueue<BulkItemResponse[]> failureQueue;
-
+	
     private Core server;
 
     public static enum DateHistogramInterval {
@@ -84,8 +79,6 @@ public class Indexer {
 
     public Indexer(Core graylogServer) {
         this.server = graylogServer;
-
-        this.failureQueue = new LinkedBlockingQueue<BulkItemResponse[]>(1000);
     }
 
     public void start() {
@@ -221,23 +214,9 @@ public class Indexer {
         LOG.debug("Deflector index: Bulk indexed {} messages, took {} ms, failures: {}",
                 new Object[] { response.getItems().length, response.getTookInMillis(), response.hasFailures() });
 
-        if (response.hasFailures()) {
-            propagateFailure(response.getItems());
-        }
-
         return !response.hasFailures();
     }
-
-    private void propagateFailure(BulkItemResponse[] items) {
-        LOG.error("Failed to index [{}] messages. Please check the index error log in your web interface for the reason.", items.length);
-
-        boolean r = this.failureQueue.offer(items);
-
-        if(!r) {
-            LOG.debug("Could not propagate failure to failure queue. Queue is full.");
-        }
-    }
-
+    
     public Searches searches() {
     	return searches;
     }
@@ -257,14 +236,14 @@ public class Indexer {
     public Indices indices() {
         return indices;
     }
-
-    public LinkedBlockingQueue<BulkItemResponse[]> getFailureQueue() {
-        return failureQueue;
-    }
-
+    
     private IndexRequestBuilder buildIndexRequest(String index, Map<String, Object> source, String id) {
         final IndexRequestBuilder b = new IndexRequestBuilder(client);
-
+        
+        /*
+         * ID is set manually to allow inserting message into recent and total index
+         * with same ID. (Required for linking in frontend)
+         */
         b.setId(id);
         b.setSource(source);
         b.setIndex(index);
