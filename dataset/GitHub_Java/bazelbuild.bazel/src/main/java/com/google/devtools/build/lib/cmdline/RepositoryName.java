447b012603360d86164c7da5f97210c91b5450a8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:59:53 +0200

    Improve creation of `Label` and `RepositoryName` instances.
        In `Label#getRelativeWithRemapping`, when the current label is in the main repository, default the new label to the main repo as well. This saves the intermediate step of creating (and interning) a garbage `Label` with the default repository, only for `resolveRepositoryRelative` to subsequently create another `Label` in the main repo.
    
        In `RepositoryName#create`, optimize for the common repository names (default and main) by skipping the interner cache and other unnecessary checks.
    
        PiperOrigin-RevId: 381857421

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/cmdline/RepositoryName.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/cmdline/RepositoryName.java
@@ -14,27 +14,35 @@
 
 package com.google.devtools.build.lib.cmdline;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.base.Throwables;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
+import com.google.devtools.build.lib.skyframe.serialization.autocodec.SerializationConstant;
 import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.util.StringCanonicalizer;
 import com.google.devtools.build.lib.util.StringUtilities;
+import com.google.devtools.build.lib.vfs.OsPathPolicy;
 import com.google.devtools.build.lib.vfs.PathFragment;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletionException;
 import java.util.regex.Pattern;
 
-/**
- * A human-readable name for the repository.
- */
+/** A human-readable name for the repository. */
+@AutoCodec
 public final class RepositoryName implements Serializable {
+
+  static final String DEFAULT_REPOSITORY = "";
+
+  @SerializationConstant
+  public static final RepositoryName DEFAULT = new RepositoryName(DEFAULT_REPOSITORY);
+
+  @SerializationConstant public static final RepositoryName MAIN = new RepositoryName("@");
+
   private static final Pattern VALID_REPO_NAME = Pattern.compile("@[\\w\\-.]*");
 
   /** Helper for serializing {@link RepositoryName}. */
@@ -76,51 +84,76 @@ public final class RepositoryName implements Serializable {
   }
 
   private static final LoadingCache<String, RepositoryName> repositoryNameCache =
-      CacheBuilder.newBuilder()
-        .weakValues()
-        .build(
-            new CacheLoader<String, RepositoryName>() {
-              @Override
-              public RepositoryName load(String name) throws LabelSyntaxException {
+      Caffeine.newBuilder()
+          .weakValues()
+          .build(
+              name -> {
                 String errorMessage = validate(name);
                 if (errorMessage != null) {
-                  errorMessage = "invalid repository name '"
-                      + StringUtilities.sanitizeControlChars(name) + "': " + errorMessage;
+                  errorMessage =
+                      "invalid repository name '"
+                          + StringUtilities.sanitizeControlChars(name)
+                          + "': "
+                          + errorMessage;
                   throw new LabelSyntaxException(errorMessage);
                 }
                 return new RepositoryName(StringCanonicalizer.intern(name));
-              }
-            });
+              });
 
   /**
    * Makes sure that name is a valid repository name and creates a new RepositoryName using it.
    *
    * @throws LabelSyntaxException if the name is invalid
    */
+  @AutoCodec.Instantiator
   public static RepositoryName create(String name) throws LabelSyntaxException {
+    if (name.isEmpty()) {
+      return DEFAULT;
+    }
+    if (name.equals("@")) {
+      return MAIN;
+    }
     try {
       return repositoryNameCache.get(name);
-    } catch (ExecutionException e) {
-      Throwables.propagateIfInstanceOf(e.getCause(), LabelSyntaxException.class);
-      throw new IllegalStateException("Failed to create RepositoryName from " + name, e);
+    } catch (CompletionException e) {
+      Throwables.propagateIfPossible(e.getCause(), LabelSyntaxException.class);
+      throw e;
     }
   }
 
   /**
-   * Extracts the repository name from a PathFragment that was created with
-   * {@code PackageIdentifier.getPathFragment}.
-   *
-   * @return a {@code Pair} of the extracted repository name and the path fragment with stripped
-   * of "external/"-prefix and repository name, or null if none was found or the repository name
-   * was invalid.
+   * Creates a RepositoryName from a known-valid string (not @-prefixed). Generally this is a
+   * directory that has been created via getSourceRoot() or getPathUnderExecRoot().
    */
-  public static Pair<RepositoryName, PathFragment> fromPathFragment(PathFragment path) {
-    if (path.segmentCount() < 2 || !path.getSegment(0).equals(Label.EXTERNAL_PATH_PREFIX)) {
+  public static RepositoryName createFromValidStrippedName(String name) {
+    return repositoryNameCache.get("@" + name);
+  }
+
+  /**
+   * Extracts the repository name from a PathFragment that was created with {@code
+   * PackageIdentifier.getSourceRoot}.
+   *
+   * @return a {@code Pair} of the extracted repository name and the path fragment with stripped of
+   *     "external/"-prefix and repository name, or null if none was found or the repository name
+   *     was invalid.
+   */
+  public static Pair<RepositoryName, PathFragment> fromPathFragment(
+      PathFragment path, boolean siblingRepositoryLayout) {
+    if (!path.isMultiSegment()) {
       return null;
     }
+
+    PathFragment prefix =
+        siblingRepositoryLayout
+            ? LabelConstants.EXPERIMENTAL_EXTERNAL_PATH_PREFIX
+            : LabelConstants.EXTERNAL_PATH_PREFIX;
+    if (!path.startsWith(prefix)) {
+      return null;
+    }
+
     try {
       RepositoryName repoName = RepositoryName.create("@" + path.getSegment(1));
-      PathFragment subPath = path.subFragment(2, path.segmentCount());
+      PathFragment subPath = path.subFragment(2);
       return Pair.of(repoName, subPath);
     } catch (LabelSyntaxException e) {
       return null;
@@ -133,11 +166,9 @@ public final class RepositoryName implements Serializable {
     this.name = name;
   }
 
-  /**
-   * Performs validity checking.  Returns null on success, an error message otherwise.
-   */
+  /** Performs validity checking. Returns null on success, an error message otherwise. */
   static String validate(String name) {
-    if (name.isEmpty()) {
+    if (name.isEmpty() || name.equals("@")) {
       return null;
     }
 
@@ -171,6 +202,14 @@ public final class RepositoryName implements Serializable {
   }
 
   /**
+   * Returns the repository name without the leading "{@literal @}". For the default repository,
+   * returns "".
+   */
+  public static String stripName(String repoName) {
+    return repoName.startsWith("@") ? repoName.substring(1) : repoName;
+  }
+
+  /**
    * Returns if this is the default repository, that is, {@link #name} is "".
    */
   public boolean isDefault() {
@@ -178,7 +217,7 @@ public final class RepositoryName implements Serializable {
   }
 
   /**
-   * Returns if this is the default repository, that is, {@link #name} is "@".
+   * Returns if this is the main repository, that is, {@link #name} is "@".
    */
   public boolean isMain() {
     return name.equals("@");
@@ -193,22 +232,38 @@ public final class RepositoryName implements Serializable {
   }
 
   /**
-   * Returns the path at which this repository is mapped within the exec root.
+   * Returns the repository name, except that the main repo is conflated with the default repo
+   * ({@code "@"} becomes the empty string).
    */
-  public PathFragment getPathFragment() {
-    return isDefault() || isMain()
-        ? PathFragment.EMPTY_FRAGMENT
-        : new PathFragment(Label.EXTERNAL_PATH_PREFIX).getRelative(strippedName());
+  public String getCanonicalForm() {
+    return isMain() ? "" : name;
   }
 
   /**
-   * Returns the runfiles path for this repository (relative to the x.runfiles/main-repo/
-   * directory). If we don't know the name of this repo (i.e., it is in the main repository),
-   * return an empty path fragment.
+   * Returns the runfiles/execRoot path for this repository. If we don't know the name of this repo
+   * (i.e., it is in the main repository), return an empty path fragment.
+   *
+   * <p>If --experimental_sibling_repository_layout is true, return "$execroot/../repo" (sibling of
+   * __main__), instead of "$execroot/external/repo".
    */
+  public PathFragment getExecPath(boolean siblingRepositoryLayout) {
+    if (isDefault() || isMain()) {
+      return PathFragment.EMPTY_FRAGMENT;
+    }
+    PathFragment prefix =
+        siblingRepositoryLayout
+            ? LabelConstants.EXPERIMENTAL_EXTERNAL_PATH_PREFIX
+            : LabelConstants.EXTERNAL_PATH_PREFIX;
+    return prefix.getRelative(strippedName());
+  }
+
+  /**
+   * Returns the runfiles path relative to the x.runfiles/main-repo directory.
+   */
+  // TODO(kchodorow): remove once execroot is reorg-ed.
   public PathFragment getRunfilesPath() {
     return isDefault() || isMain()
-        ? PathFragment.EMPTY_FRAGMENT : new PathFragment("..").getRelative(strippedName());
+        ? PathFragment.EMPTY_FRAGMENT : PathFragment.create("..").getRelative(strippedName());
   }
 
   /**
@@ -227,11 +282,11 @@ public final class RepositoryName implements Serializable {
     if (!(object instanceof RepositoryName)) {
       return false;
     }
-    return name.equals(((RepositoryName) object).name);
+    return OsPathPolicy.getFilePathOs().equals(name, ((RepositoryName) object).name);
   }
 
   @Override
   public int hashCode() {
-    return name.hashCode();
+    return OsPathPolicy.getFilePathOs().hash(name);
   }
 }
