ccde3f700b24023ff9ee85eb4f13b56cb5c0b437
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:21:54 +0200

    fix stub selenium test
        issue #610

--- dataset/GitHub_Java/Graylog2.graylog2-server/test/selenium/pages/SearchPage.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/test/selenium/pages/SearchPage.java
@@ -33,10 +33,14 @@ public class SearchPage extends FluentPage {
     private String keyword;
     private String interval;
     private Integer pageNumber;
+    private String streamId;
+    private String sortField;
+    private String sortOrder;
+    private String fields;
 
     @Override
     public String getUrl() {
-        return routes.SearchController.index(queryString, rangeType, relativeTimeSpan, fromTime, toTime, keyword, interval, pageNumber).url();
+        return routes.SearchController.index(queryString, rangeType, relativeTimeSpan, fromTime, toTime, keyword, interval, pageNumber, streamId, sortField, sortOrder, fields).url();
     }
 
     @Override
