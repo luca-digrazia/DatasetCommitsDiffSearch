a0991c24bc033af05c70850494f54b8e81f1ba05
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:59:32 +0200

    Fix the request log layout.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/RequestLogHandlerFactory.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/RequestLogHandlerFactory.java
@@ -9,7 +9,6 @@ import ch.qos.logback.core.CoreConstants;
 import ch.qos.logback.core.LayoutBase;
 import ch.qos.logback.core.rolling.RollingFileAppender;
 import ch.qos.logback.core.spi.AppenderAttachableImpl;
-import com.google.common.base.Optional;
 import com.yammer.dropwizard.jetty.AsyncRequestLog;
 import com.yammer.dropwizard.logging.LogbackFactory;
 import org.eclipse.jetty.server.handler.RequestLogHandler;
@@ -56,8 +55,7 @@ public class RequestLogHandlerFactory {
         final ConsoleConfiguration console = config.getConsoleConfiguration();
         if (console.isEnabled()) {
             final ConsoleAppender<ILoggingEvent> appender = LogbackFactory.buildConsoleAppender(console,
-                                                                                                context,
-                                                                                                Optional.<String>absent());
+                                                                                                context);
             appender.stop();
             appender.setLayout(layout);
             appender.start();
@@ -67,8 +65,7 @@ public class RequestLogHandlerFactory {
         final FileConfiguration file = config.getFileConfiguration();
         if (file.isEnabled()) {
             final RollingFileAppender<ILoggingEvent> appender = LogbackFactory.buildFileAppender(file,
-                                                                                                 context,
-                                                                                                 Optional.<String>absent());
+                                                                                                 context);
 
             appender.stop();
             appender.setLayout(layout);
@@ -78,10 +75,7 @@ public class RequestLogHandlerFactory {
 
         final LoggingConfiguration.SyslogConfiguration syslog = config.getSyslogConfiguration();
         if (syslog.isEnabled()) {
-            final SyslogAppender appender = LogbackFactory.buildSyslogAppender(syslog,
-                                                                               context,
-                                                                               name + "-requests",
-                                                                               Optional.<String>absent());
+            final SyslogAppender appender = LogbackFactory.buildSyslogAppender(syslog, context, name + "-requests");
 
             appender.stop();
             appender.setLayout(layout);
