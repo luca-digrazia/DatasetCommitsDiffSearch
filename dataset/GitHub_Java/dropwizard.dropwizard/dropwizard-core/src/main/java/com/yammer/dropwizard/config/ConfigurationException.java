c40a11af01f086946a1a796cb4fb3f0965b86ba0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:07:23 +0200

    Switch to using Validator.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/ConfigurationException.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/ConfigurationException.java
@@ -2,18 +2,7 @@ package com.yammer.dropwizard.config;
 
 import java.io.File;
 
-/**
- * An exception thrown where there is an error parsing a configuration object.
- */
 public class ConfigurationException extends Exception {
-    private static final long serialVersionUID = 5325162099634227047L;
-
-    /**
-     * Creates a new {@link ConfigurationException} for the given file with the given errors.
-     *
-     * @param file      the bad configuration file
-     * @param errors    the errors in the file
-     */
     public ConfigurationException(File file, Iterable<String> errors) {
         super(formatMessage(file, errors));
     }
@@ -22,7 +11,7 @@ public class ConfigurationException extends Exception {
         final StringBuilder msg = new StringBuilder(file.toString())
                 .append(" has the following errors:\n");
         for (String error : errors) {
-            msg.append("  * ").append(error).append('\n');
+            msg.append("  * ").append(error);
         }
         return msg.toString();
     }
--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/ConfigurationFactory.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/ConfigurationFactory.java
@@ -1,9 +1,15 @@
 package com.yammer.dropwizard.config;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
-import com.yammer.dropwizard.json.Yaml;
-import com.yammer.dropwizard.validation.Validator;
+import com.google.common.io.Files;
+import com.yammer.dropwizard.util.Validator;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.error.YAMLException;
+import org.yaml.snakeyaml.introspector.BeanAccess;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 
@@ -11,7 +17,7 @@ public class ConfigurationFactory<T> {
     public static <T> ConfigurationFactory<T> forClass(Class<T> klass, Validator validator) {
         return new ConfigurationFactory<T>(klass, validator);
     }
-
+    
     private final Class<T> klass;
     private final Validator validator;
 
@@ -20,10 +26,24 @@ public class ConfigurationFactory<T> {
         this.validator = validator;
     }
     
-    public T build(File file) throws IOException, ConfigurationException {
-        final T config = new Yaml(file).read(klass);
-        validate(file, config);
-        return config;
+    public T build(File file) throws IOException, ConfigurationException, YAMLException {
+        final BufferedReader reader = Files.newReader(file, Charsets.UTF_8);
+        try {
+            final Yaml yaml = buildYamlParser();
+            final T config = loadYaml(reader, yaml);
+            validate(file, config);
+            return config;
+        } finally {
+            reader.close();
+        }
+    }
+
+    private T defaultInstance() {
+        try {
+            return klass.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void validate(File file, T config) throws ConfigurationException {
@@ -31,5 +51,20 @@ public class ConfigurationFactory<T> {
         if (!errors.isEmpty()) {
             throw new ConfigurationException(file, errors);
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private T loadYaml(BufferedReader reader, Yaml yaml) {
+        final T config = (T) yaml.load(reader);
+        if (config == null) {
+            return defaultInstance();
+        }
+        return config;
+    }
+
+    private Yaml buildYamlParser() {
+        final Yaml yaml = new Yaml(new Constructor(klass));
+        yaml.setBeanAccess(BeanAccess.FIELD);
+        return yaml;
     }
 }
