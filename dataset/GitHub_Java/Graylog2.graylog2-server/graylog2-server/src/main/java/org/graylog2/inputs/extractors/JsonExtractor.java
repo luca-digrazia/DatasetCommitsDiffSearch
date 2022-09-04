4ae196f259e34f9bae6bd995e8b70d24877e7404
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:56:21 +0200

    Support replacing whitespace in nested keys for JSON extractor (#3623)
        Fixes #3622

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/extractors/JsonExtractor.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/extractors/JsonExtractor.java
@@ -21,6 +21,9 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auto.value.AutoValue;
 import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
+import org.graylog.autovalue.WithBeanGetter;
 import org.graylog2.ConfigurationException;
 import org.graylog2.plugin.inputs.Converter;
 import org.graylog2.plugin.inputs.Extractor;
@@ -35,6 +38,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.base.Strings.isNullOrEmpty;
@@ -45,12 +49,20 @@ public class JsonExtractor extends Extractor {
     private static final String CK_LIST_SEPARATOR = "list_separator";
     private static final String CK_KEY_SEPARATOR = "key_separator";
     private static final String CK_KV_SEPARATOR = "kv_separator";
+    private static final String CK_REPLACE_KEY_WHITESPACE = "replace_key_whitespace";
+    private static final String CK_KEY_WHITESPACE_REPLACEMENT = "key_whitespace_replacement";
+    private static final String CK_KEY_PREFIX = "key_prefix";
+    private static final Pattern WHITE_SPACE_PATTERN = Pattern.compile("\\s");
+    private static final RemoveNullPredicate REMOVE_NULL_PREDICATE = new RemoveNullPredicate();
 
     private final ObjectMapper mapper = new ObjectMapper();
     private final boolean flatten;
     private final String listSeparator;
     private final String keySeparator;
     private final String kvSeparator;
+    private final boolean replaceKeyWhitespace;
+    private final String keyWhitespaceReplacement;
+    private final String keyPrefix;
 
     public JsonExtractor(final MetricRegistry metricRegistry,
                          final String id,
@@ -72,8 +84,11 @@ public class JsonExtractor extends Extractor {
 
         this.flatten = firstNonNull((Boolean) extractorConfig.get(CK_FLATTEN), false);
         this.listSeparator = firstNonNull((String) extractorConfig.get(CK_LIST_SEPARATOR), ", ");
-        this.keySeparator = firstNonNull((String) extractorConfig.get(CK_KEY_SEPARATOR), ".");
+        this.keySeparator = firstNonNull((String) extractorConfig.get(CK_KEY_SEPARATOR), "_");
         this.kvSeparator = firstNonNull((String) extractorConfig.get(CK_KV_SEPARATOR), "=");
+        this.replaceKeyWhitespace = firstNonNull((Boolean) extractorConfig.get(CK_REPLACE_KEY_WHITESPACE), false);
+        this.keyWhitespaceReplacement = firstNonNull((String) extractorConfig.get(CK_KEY_WHITESPACE_REPLACEMENT), "_");
+        this.keyPrefix = firstNonNull((String) extractorConfig.get(CK_KEY_PREFIX), "");
     }
 
     @Override
@@ -94,14 +109,15 @@ public class JsonExtractor extends Extractor {
 
         final Map<String, Object> json;
         try {
-            json = mapper.readValue(value, new TypeReference<Map<String, Object>>() {});
+            json = mapper.readValue(value, new TypeReference<Map<String, Object>>() {
+            });
         } catch (IOException e) {
             return Collections.emptyMap();
         }
 
         final Map<String, Object> results = new HashMap<>(json.size());
         for (Map.Entry<String, Object> mapEntry : json.entrySet()) {
-            for(Entry entry : parseValue(mapEntry.getKey(), mapEntry.getValue())) {
+            for (Entry entry : parseValue(keyPrefix + mapEntry.getKey(), mapEntry.getValue())) {
                 results.put(entry.key(), entry.value());
             }
         }
@@ -109,31 +125,48 @@ public class JsonExtractor extends Extractor {
         return results;
     }
 
+    private String parseKey(String key) {
+        if (replaceKeyWhitespace && key.contains(" ")) {
+            return WHITE_SPACE_PATTERN.matcher(key).replaceAll(keyWhitespaceReplacement);
+        } else {
+            if (LOG.isDebugEnabled() && key.contains(" ")) {
+                LOG.debug("Invalid key \"{}\" in JSON object!", key);
+            }
+
+            return key;
+        }
+    }
+
     private Collection<Entry> parseValue(String key, Object value) {
+        final String processedKey = parseKey(key);
         if (value instanceof Boolean) {
-            return Collections.singleton(Entry.create(key, value));
+            return Collections.singleton(Entry.create(processedKey, value));
         } else if (value instanceof Number) {
-            return Collections.singleton(Entry.create(key, value));
+            return Collections.singleton(Entry.create(processedKey, value));
         } else if (value instanceof String) {
-            return Collections.singleton(Entry.create(key, value));
+            return Collections.singleton(Entry.create(processedKey, value));
         } else if (value instanceof Map) {
+            @SuppressWarnings("unchecked")
+            final Map<String, Object> map = (Map<String, Object>) value;
+            final Map<String, Object> withoutNull = Maps.filterEntries(map, REMOVE_NULL_PREDICATE);
             if (flatten) {
-                return Collections.singleton(Entry.create(key, Joiner.on(listSeparator).withKeyValueSeparator(kvSeparator).join((Map) value)));
+                final Joiner.MapJoiner joiner = Joiner.on(listSeparator).withKeyValueSeparator(kvSeparator);
+                return Collections.singleton(Entry.create(processedKey, joiner.join(withoutNull)));
             } else {
-                @SuppressWarnings("unchecked")
-                final Map<String, Object> map = (Map<String, Object>) value;
-                final List<Entry> result = new ArrayList<>();
-                for(Map.Entry<String, Object> entry : map.entrySet()) {
-                    result.addAll(parseValue(key + keySeparator + entry.getKey(), entry.getValue()));
+                final List<Entry> result = new ArrayList<>(map.size());
+                for (Map.Entry<String, Object> entry : map.entrySet()) {
+                    result.addAll(parseValue(processedKey + keySeparator + entry.getKey(), entry.getValue()));
                 }
 
                 return result;
             }
         } else if (value instanceof List) {
             final List values = (List) value;
-            return Collections.singleton(Entry.create(key, Joiner.on(listSeparator).join(values)));
-        } else if(value == null) {
-            return Collections.singleton(Entry.create(key, null));
+            final Joiner joiner = Joiner.on(listSeparator).skipNulls();
+            return Collections.singleton(Entry.create(processedKey, joiner.join(values)));
+        } else if (value == null) {
+            // Ignore null values so we don't try to create fields for that in the message.
+            return Collections.emptySet();
         } else {
             LOG.debug("Unknown type \"{}\" in key \"{}\"", value.getClass(), key);
             return Collections.emptySet();
@@ -141,13 +174,22 @@ public class JsonExtractor extends Extractor {
     }
 
     @AutoValue
+    @WithBeanGetter
     protected abstract static class Entry {
         public abstract String key();
+
         @Nullable
         public abstract Object value();
 
         public static Entry create(String key, @Nullable Object value) {
             return new AutoValue_JsonExtractor_Entry(key, value);
+        }
+    }
+
+    protected final static class RemoveNullPredicate implements Predicate<Map.Entry> {
+        @Override
+        public boolean apply(@Nullable Map.Entry input) {
+            return input != null && input.getKey() != null && input.getValue() != null;
         }
     }
 }
