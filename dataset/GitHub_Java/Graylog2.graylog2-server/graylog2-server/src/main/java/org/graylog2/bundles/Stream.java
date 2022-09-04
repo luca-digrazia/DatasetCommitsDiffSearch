aae711cfcf5d450907cd0eaca131db08d4a8eb37
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:47:56 +0200

    Add ID to Outputs so that Streams can reference them in Config Bundles

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bundles/Stream.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bundles/Stream.java
@@ -17,26 +17,21 @@
 package org.graylog2.bundles;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.NotNull;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 public class Stream {
     @JsonProperty
-    @NotBlank
     private String title;
     @JsonProperty
     private String description;
     @JsonProperty
-    private boolean disabled = false;
+    private boolean disabled;
     @JsonProperty
-    @NotNull
     private List<StreamRule> streamRules = Collections.emptyList();
     @JsonProperty
-    @NotNull
     private Set<String> outputs = Collections.emptySet();
 
     public String getTitle() {
