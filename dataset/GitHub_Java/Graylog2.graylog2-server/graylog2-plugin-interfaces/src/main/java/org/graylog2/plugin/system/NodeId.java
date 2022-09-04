7ea09a7381a1f54dc519b1757424fd3b4d5c3b5a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:46:38 +0200

    Catching the right exception so generating node file does not fail.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/system/NodeId.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/system/NodeId.java
@@ -22,7 +22,6 @@
  */
 package org.graylog2.plugin.system;
 
-import com.google.common.hash.Hashing;
 import org.apache.commons.io.FileUtils;
 import org.graylog2.plugin.Tools;
 import org.slf4j.Logger;
@@ -38,12 +37,13 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class NodeId {
+
     private static final Logger LOG = LoggerFactory.getLogger(NodeId.class);
 
     private final String filename;
     private final String id;
 
-    public NodeId(final String filename) {
+    public NodeId(String filename) {
         this.filename = filename;
         this.id = readOrGenerate();
     }
@@ -58,12 +58,11 @@ public class NodeId {
 
             LOG.info("Node ID: {}", read);
             return read;
-        } catch (FileNotFoundException | NoSuchFileException e) {
+        } catch(FileNotFoundException | NoSuchFileException e) {
             return generate();
         } catch (Exception e2) {
-            final String msg = "Could not read or generate node ID!";
-            LOG.debug(msg, e2);
-            throw new NodeIdPersistenceException(msg, e2);
+            LOG.error("Could not read or generate node ID: ", e2);
+            throw new RuntimeException(e2);
         }
     }
 
@@ -73,15 +72,15 @@ public class NodeId {
         return lines.size() > 0 ? lines.get(0) : "";
     }
 
-    private String generate() throws NodeIdPersistenceException {
+    private String generate() {
         String generated = Tools.generateServerId();
         LOG.info("No node ID file found. Generated: {}", generated);
 
         try {
             persist(generated);
         } catch (IOException e1) {
-            LOG.debug("Could not persist node ID: ", e1);
-            throw new NodeIdPersistenceException("Unable to persist node ID", e1);
+            LOG.error("Could not persist node ID: ", e1);
+            throw new RuntimeException(e1);
         }
 
         return generated;
@@ -91,21 +90,8 @@ public class NodeId {
         FileUtils.writeStringToFile(new File(filename), nodeId);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public String toString() {
         return id;
     }
 
-    /**
-     * Generate an "anonymized" node ID for use with external services. Currently it just hashes the actual node ID
-     * using SHA-256.
-     *
-     * @return The anonymized ID derived from hashing the node ID.
-     */
-    public String anonymize() {
-        return Hashing.sha256().hashString(id, StandardCharsets.UTF_8).toString();
-    }
 }
