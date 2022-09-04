9cd31fc4486545cf1671550749159a890c641a39
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:18:25 +0200

    Removed unused imports

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/streams/matchers/FacilityMatcher.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/streams/matchers/FacilityMatcher.java
@@ -20,16 +20,19 @@
 
 package org.graylog2.streams.matchers;
 
-import org.graylog2.logmessage.LogMessage;
+import org.graylog2.messagehandlers.gelf.GELFMessage;
 import org.graylog2.streams.StreamRule;
 
 /**
+ * FacilityMatcher.java: Mar 27, 2011 5:33:46 PM
+ *
+ * [description]
+ *
  * @author Lennart Koopmann <lennart@socketfeed.com>
  */
-public class FacilityMatcher implements StreamRuleMatcher {
+public class FacilityMatcher implements StreamRuleMatcherIF {
 
-    @Override
-    public boolean match(LogMessage msg, StreamRule rule) {
+    public boolean match(GELFMessage msg, StreamRule rule) {
         return msg.getFacility().equals(rule.getValue());
     }
 
