a5ae0e96d2dd92caca98f2030595d7230ba040c4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:33:52 +0200

    Allow any image to be used in DevServices
        We trust the user that they have supplied an appropriate image.
    
        Fixes #15919

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/devservices/db2/src/main/java/io/quarkus/devservices/db2/deployment/DB2DevServicesProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/devservices/db2/src/main/java/io/quarkus/devservices/db2/deployment/DB2DevServicesProcessor.java
@@ -4,7 +4,6 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
-import java.util.OptionalInt;
 
 import org.testcontainers.containers.Db2Container;
 import org.testcontainers.utility.DockerImageName;
@@ -16,32 +15,18 @@ import io.quarkus.deployment.annotations.BuildStep;
 
 public class DB2DevServicesProcessor {
 
-    /**
-     * If you update this remember to update the container-license-acceptance.txt in the tests
-     */
-    public static final String TAG = "11.5.5.1";
-
     @BuildStep
     DevServicesDatasourceProviderBuildItem setupDB2() {
         return new DevServicesDatasourceProviderBuildItem(DatabaseKind.DB2, new DevServicesDatasourceProvider() {
             @Override
             public RunningDevServicesDatasource startDatabase(Optional<String> username, Optional<String> password,
-                    Optional<String> datasourceName, Optional<String> imageName, Map<String, String> additionalProperties,
-                    OptionalInt fixedExposedPort) {
+                    Optional<String> datasourceName, Optional<String> imageName, Map<String, String> additionalProperties) {
                 Db2Container container = new Db2Container(
-                        DockerImageName.parse(imageName.orElse("ibmcom/db2:" + TAG))
-                                .asCompatibleSubstituteFor(DockerImageName.parse("ibmcom/db2"))) {
-                    @Override
-                    protected void configure() {
-                        super.configure();
-                        if (fixedExposedPort.isPresent()) {
-                            addFixedExposedPort(fixedExposedPort.getAsInt(), DB2_PORT);
-                        }
-                    };
-                }
-                        .withPassword(password.orElse("quarkus"))
-                        .withUsername(username.orElse("quarkus"))
-                        .withDatabaseName(datasourceName.orElse("default"));
+                        DockerImageName.parse(imageName.orElse("ibmcom/db2:" + Db2Container.DEFAULT_TAG))
+                                .asCompatibleSubstituteFor(DockerImageName.parse("ibmcom/db2")))
+                                        .withPassword(password.orElse("quarkus"))
+                                        .withUsername(username.orElse("quarkus"))
+                                        .withDatabaseName(datasourceName.orElse("default"));
                 additionalProperties.forEach(container::withUrlParam);
                 container.start();
                 return new RunningDevServicesDatasource(container.getJdbcUrl(), container.getUsername(),
--- dataset/GitHub_Java/quarkusio.quarkus/extensions/devservices/mysql/src/main/java/io/quarkus/devservices/postgresql/deployment/MySQLDevServicesProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/devservices/mysql/src/main/java/io/quarkus/devservices/postgresql/deployment/MySQLDevServicesProcessor.java
@@ -4,7 +4,6 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
-import java.util.OptionalInt;
 
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -16,29 +15,18 @@ import io.quarkus.deployment.annotations.BuildStep;
 
 public class MySQLDevServicesProcessor {
 
-    public static final String TAG = "8.0.24";
-
     @BuildStep
     DevServicesDatasourceProviderBuildItem setupMysql() {
         return new DevServicesDatasourceProviderBuildItem(DatabaseKind.MYSQL, new DevServicesDatasourceProvider() {
             @Override
             public RunningDevServicesDatasource startDatabase(Optional<String> username, Optional<String> password,
-                    Optional<String> datasourceName, Optional<String> imageName, Map<String, String> additionalProperties,
-                    OptionalInt fixedExposedPort) {
+                    Optional<String> datasourceName, Optional<String> imageName, Map<String, String> additionalProperties) {
                 MySQLContainer container = new MySQLContainer(
-                        DockerImageName.parse(imageName.orElse(MySQLContainer.IMAGE + ":" + TAG))
-                                .asCompatibleSubstituteFor(DockerImageName.parse(MySQLContainer.IMAGE))) {
-                    @Override
-                    protected void configure() {
-                        super.configure();
-                        if (fixedExposedPort.isPresent()) {
-                            addFixedExposedPort(fixedExposedPort.getAsInt(), MySQLContainer.MYSQL_PORT);
-                        }
-                    };
-                }
-                        .withPassword(password.orElse("quarkus"))
-                        .withUsername(username.orElse("quarkus"))
-                        .withDatabaseName(datasourceName.orElse("default"));
+                        DockerImageName.parse(imageName.orElse(MySQLContainer.IMAGE + ":" + MySQLContainer.DEFAULT_TAG))
+                                .asCompatibleSubstituteFor(DockerImageName.parse(MySQLContainer.IMAGE)))
+                                        .withPassword(password.orElse("quarkus"))
+                                        .withUsername(username.orElse("quarkus"))
+                                        .withDatabaseName(datasourceName.orElse("default"));
                 additionalProperties.forEach(container::withUrlParam);
                 container.start();
                 return new RunningDevServicesDatasource(container.getJdbcUrl(), container.getUsername(),
