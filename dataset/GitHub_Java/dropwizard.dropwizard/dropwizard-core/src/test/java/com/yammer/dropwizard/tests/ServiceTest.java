46ac5ce8f936b6a1a09a7fb80f633917b8d89973
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:49:23 +0200

    Fix raw generics.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/tests/ServiceTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/tests/ServiceTest.java
@@ -1,39 +1,60 @@
 package com.yammer.dropwizard.tests;
 
-import com.yammer.dropwizard.Bundle;
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ServiceTest {
-    @SuppressWarnings({"PackageVisibleInnerClass", "EmptyClass"})
-    static class FakeConfiguration extends Configuration {
+    private static class FakeConfiguration extends Configuration {}
 
+    private static class FakeService extends Service<FakeConfiguration> {
+        @Override
+        public void initialize(Bootstrap<FakeConfiguration> bootstrap) {}
+
+        @Override
+        public void run(FakeConfiguration configuration, Environment environment) {}
     }
 
-    private class FakeService extends Service<FakeConfiguration> {
-        FakeService() {
-            super("test");
-            addBundle(bundle);
+    private static class PoserService extends FakeService {}
+
+    private static class WrapperService<C extends FakeConfiguration> extends Service<C> {
+        private final Service<C> service;
+
+        private WrapperService(Service<C> service) {
+            this.service = service;
         }
 
         @Override
-        protected void initialize(FakeConfiguration configuration,
-                                  Environment environment) {
+        public void initialize(Bootstrap<C> bootstrap) {
+            this.service.initialize(bootstrap);
+        }
+
+        @Override
+        public void run(C configuration, Environment environment) throws Exception {
+            this.service.run(configuration, environment);
         }
     }
 
-    private final Bundle bundle = mock(Bundle.class);
-    private final FakeService service = new FakeService();
-
     @Test
     public void hasAReferenceToItsTypeParameter() throws Exception {
-        assertThat(service.getConfigurationClass(),
-                   is(sameInstance(FakeConfiguration.class)));
+        assertThat(new FakeService().getConfigurationClass())
+                .isSameAs(FakeConfiguration.class);
+    }
+
+    @Test
+    public void canDetermineConfiguration() throws Exception {
+        assertThat(new PoserService().getConfigurationClass())
+                .isSameAs(FakeConfiguration.class);
+    }
+
+    @Test
+    public void canDetermineWrappedConfiguration() throws Exception {
+        final PoserService service = new PoserService();
+        assertThat(new WrapperService<FakeConfiguration>(service).getConfigurationClass())
+                .isSameAs(FakeConfiguration.class);
     }
 }
