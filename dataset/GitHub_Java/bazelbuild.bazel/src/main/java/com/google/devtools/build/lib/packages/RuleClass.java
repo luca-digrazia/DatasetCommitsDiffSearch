75de1de4dcd34c51ef3368af962c28d91e8b4e93
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:12:26 +0200

    Enable rules to inherit the ExecutionPlatformConstraintsAllowed value.
        PiperOrigin-RevId: 200247872

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/RuleClass.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/RuleClass.java
@@ -699,7 +699,7 @@ public class RuleClass {
         for (Attribute attribute : parent.getAttributes()) {
           String attrName = attribute.getName();
           Preconditions.checkArgument(
-              !attributes.containsKey(attrName) || attributes.get(attrName).equals(attribute),
+              !attributes.containsKey(attrName) || attributes.get(attrName) == attribute,
               "Attribute %s is inherited multiple times in %s ruleclass",
               attrName,
               name);
