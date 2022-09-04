c0f6b1f45b3cadabb676be4169b062e6039a1212
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:37:10 +0200

    Add a reflection declaration for JsonBindingProvider
        Also move the JsonProviderImpl one to the resteasy-jsonb extension now
        that we have it.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/resteasy-jsonb/deployment/src/main/java/io/quarkus/resteasy/jsonb/deployment/ResteasyJsonbProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/resteasy-jsonb/deployment/src/main/java/io/quarkus/resteasy/jsonb/deployment/ResteasyJsonbProcessor.java
@@ -1,68 +1,37 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.quarkus.resteasy.jsonb.deployment;
 
-import java.util.HashSet;
-import java.util.Set;
+import org.eclipse.yasson.JsonBindingProvider;
+import org.glassfish.json.JsonProviderImpl;
 
-import javax.json.bind.Jsonb;
-
-import org.jboss.jandex.AnnotationInstance;
-import org.jboss.jandex.AnnotationTarget;
-import org.jboss.jandex.DotName;
-import org.jboss.jandex.IndexView;
-
-import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
-import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
-import io.quarkus.arc.processor.DotNames;
-import io.quarkus.deployment.Capabilities;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.resteasy.common.spi.ResteasyJaxrsProviderBuildItem;
-import io.quarkus.resteasy.jsonb.runtime.QuarkusJsonbContextResolver;
+import io.quarkus.deployment.builditem.substrate.ReflectiveClassBuildItem;
 
 public class ResteasyJsonbProcessor {
 
-    private static final DotName JSONB = DotName.createSimple(Jsonb.class.getName());
-
-    @BuildStep(providesCapabilities = { Capabilities.RESTEASY_JSON_EXTENSION, "io.quarkus.resteasy.jsonb" })
-    void build(BuildProducer<FeatureBuildItem> feature) {
-        feature.produce(new FeatureBuildItem(FeatureBuildItem.RESTEASY_JSONB));
-    }
-
     @BuildStep
-    void register(CombinedIndexBuildItem combinedIndexBuildItem,
-            BuildProducer<ResteasyJaxrsProviderBuildItem> jaxrsProvider,
-            BuildProducer<AdditionalBeanBuildItem> additionalBean,
-            BuildProducer<UnremovableBeanBuildItem> unremovable) {
+    void build(BuildProducer<FeatureBuildItem> feature,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
+        feature.produce(new FeatureBuildItem(FeatureBuildItem.RESTEASY_JSONB));
 
-        IndexView index = combinedIndexBuildItem.getIndex();
-
-        jaxrsProvider.produce(new ResteasyJaxrsProviderBuildItem(QuarkusJsonbContextResolver.class.getName()));
-
-        // this needs to be registered manually since the runtime module is not indexed by Jandex
-        additionalBean.produce(AdditionalBeanBuildItem.unremovableOf(QuarkusJsonbContextResolver.class));
-        Set<String> userSuppliedProducers = getUserSuppliedJsonbProducerBeans(index);
-        if (!userSuppliedProducers.isEmpty()) {
-            unremovable.produce(
-                    new UnremovableBeanBuildItem(new UnremovableBeanBuildItem.BeanClassNamesExclusion(userSuppliedProducers)));
-        }
-    }
-
-    /*
-     * We need to find all the user supplied producers and mark them as unremovable since there might be no injection points
-     * for the Jsonb
-     */
-    private Set<String> getUserSuppliedJsonbProducerBeans(IndexView index) {
-        Set<String> result = new HashSet<>();
-        for (AnnotationInstance annotation : index.getAnnotations(DotNames.PRODUCES)) {
-            if (annotation.target().kind() != AnnotationTarget.Kind.METHOD) {
-                continue;
-            }
-            if (JSONB.equals(annotation.target().asMethod().returnType().name())) {
-                result.add(annotation.target().asMethod().declaringClass().name().toString());
-            }
-        }
-        return result;
+        reflectiveClass.produce(new ReflectiveClassBuildItem(false, false,
+                JsonBindingProvider.class.getName(),
+                JsonProviderImpl.class.getName()));
     }
 }
