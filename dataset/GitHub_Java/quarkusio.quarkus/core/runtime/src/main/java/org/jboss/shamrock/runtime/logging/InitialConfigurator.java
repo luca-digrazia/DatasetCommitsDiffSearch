62e5f04c5ace9e4194620903befa3f69f1a398dc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:50:01 +0200

    Fix logging levels of native image, boot, and JVM mode.
        Fixes #812, #799

--- dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/org/jboss/shamrock/runtime/logging/InitialConfigurator.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/org/jboss/shamrock/runtime/logging/InitialConfigurator.java
@@ -31,7 +31,6 @@ public final class InitialConfigurator implements EmbeddedConfigurator {
                     "%d{HH:mm:ss,SSS} %-5p [%c{1.}] %s%e%n"
                 ));
                 handler.setLevel(Level.INFO);
-                handler.setFilter(new LogCleanupFilter());
                 return new Handler[] {
                     handler
                 };
