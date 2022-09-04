d7bcaf1e16d8498381cd8f39e49c81e83f406d23
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:34:20 +0200

    fix getR() of QR for netlib and nd4j

--- dataset/GitHub_Java/haifengl.smile/netlib/src/main/java/smile/netlib/QR.java
+++ dataset/GitHub_Java/haifengl.smile/netlib/src/main/java/smile/netlib/QR.java
@@ -20,6 +20,8 @@ import smile.math.matrix.DenseMatrix;
 import smile.math.matrix.Cholesky;
 import com.github.fommil.netlib.LAPACK;
 import org.netlib.util.intW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * For an m-by-n matrix A with m &ge; n, the QR decomposition is an m-by-n
@@ -37,7 +39,7 @@ import org.netlib.util.intW;
  * @author Haifeng Li
  */
 class QR extends smile.math.matrix.QR {
-    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(QR.class);
+    private static final Logger logger = LoggerFactory.getLogger(QR.class);
 
     /**
      * Constructor.
