63243d54ed11838770a54a1bcf2fb964e2969e6e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:52:35 +0200

    Better document log fie rotating
        Fixes #15432

--- dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/io/quarkus/runtime/logging/FileConfig.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/io/quarkus/runtime/logging/FileConfig.java
@@ -1,36 +1,25 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.quarkus.runtime.logging;
 
 import java.io.File;
 import java.util.Optional;
-import java.util.OptionalLong;
 import java.util.logging.Level;
 
 import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.configuration.MemorySize;
 
 @ConfigGroup
 public class FileConfig {
 
     /**
+     * Default file name where logs should be stored.
+     */
+    public static final String DEFAULT_LOG_FILE_NAME = "quarkus.log";
+
+    /**
      * If file logging should be enabled
      */
-    @ConfigItem(defaultValue = "false")
+    @ConfigItem
     boolean enable;
 
     /**
@@ -40,19 +29,27 @@ public class FileConfig {
     String format;
 
     /**
-     * The file log level
+     * The level of logs to be written into the file.
      */
     @ConfigItem(defaultValue = "ALL")
     Level level;
 
     /**
-     * The file logging log level
+     * The name of the file in which logs will be written.
      */
-    @ConfigItem(defaultValue = "quarkus.log")
+    @ConfigItem(defaultValue = DEFAULT_LOG_FILE_NAME)
     File path;
 
     /**
-     * File rotation config
+     * File async logging config
+     */
+    AsyncConfig async;
+
+    /**
+     * File rotation config.
+     * The time interval is determined by the content of the <code>fileSuffix</code> property.
+     * The size interval is determined by the content of the <code>maxFileSize</code> property.
+     * If both are used, the rotating will be based on time then size.
      */
     RotationConfig rotation;
 
@@ -61,8 +58,8 @@ public class FileConfig {
         /**
          * The maximum file size of the log file after which a rotation is executed.
          */
-        @ConfigItem
-        OptionalLong maxFileSize;
+        @ConfigItem(defaultValueDocumentation = "10")
+        Optional<MemorySize> maxFileSize;
 
         /**
          * The maximum number of backups to keep.
@@ -72,12 +69,17 @@ public class FileConfig {
 
         /**
          * File handler rotation file suffix.
+         * When used, the file will be rotated based on its suffix.
+         *
+         * Example fileSuffix: .yyyy-MM-dd
          */
         @ConfigItem
         Optional<String> fileSuffix;
 
         /**
          * Indicates whether to rotate log files on server initialization.
+         * <p>
+         * You need to either set a {@code max-file-size} or configure a {@code file-suffix} for it to work.
          */
         @ConfigItem(defaultValue = "true")
         boolean rotateOnBoot;
