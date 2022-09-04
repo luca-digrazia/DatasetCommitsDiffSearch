cc148885927e4d7e3e00dfbbe6317d4afede8590
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:03:08 +0200

    Remove ldap settings check from authenticators (#2820)
        we no longer lock external accounts when ldap is disabled because other authenticators can also create external users now
        if people require this we need to track which authenticator created the account in the first place, but that's too large a change for a bug fix release
    
        fixes #2817

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/realm/AccessTokenAuthenticator.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/realm/AccessTokenAuthenticator.java
@@ -1,41 +1,50 @@
-/*
- * Copyright 2013 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.security.realm;
 
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authc.credential.AllowAllCredentialsMatcher;
 import org.apache.shiro.realm.AuthenticatingRealm;
-import org.graylog2.Core;
+import org.graylog2.plugin.database.ValidationException;
 import org.graylog2.security.AccessToken;
-import org.graylog2.security.AccessTokenAuthToken;
-import org.graylog2.users.User;
+import org.graylog2.shared.security.AccessTokenAuthToken;
+import org.graylog2.security.AccessTokenService;
+import org.graylog2.plugin.database.users.User;
+import org.graylog2.shared.security.ShiroSecurityContext;
+import org.graylog2.shared.users.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+
 public class AccessTokenAuthenticator extends AuthenticatingRealm {
-    private static final Logger log = LoggerFactory.getLogger(AccessTokenAuthenticator.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AccessTokenAuthenticator.class);
+    public static final String NAME = "access-token";
 
-    private final Core core;
+    private final AccessTokenService accessTokenService;
+    private final UserService userService;
 
-    public AccessTokenAuthenticator(Core core) {
-        this.core = core;
+    @Inject
+    AccessTokenAuthenticator(AccessTokenService accessTokenService,
+                             UserService userService) {
+        this.accessTokenService = accessTokenService;
+        this.userService = userService;
         setAuthenticationTokenClass(AccessTokenAuthToken.class);
+        setCachingEnabled(false);
         // the presence of a valid access token is enough, we don't have any other credentials
         setCredentialsMatcher(new AllowAllCredentialsMatcher());
     }
@@ -43,21 +52,24 @@ public class AccessTokenAuthenticator extends AuthenticatingRealm {
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         AccessTokenAuthToken authToken = (AccessTokenAuthToken) token;
-        final AccessToken accessToken = AccessToken.load(String.valueOf(authToken.getToken()), core);
+        final AccessToken accessToken = accessTokenService.load(String.valueOf(authToken.getToken()));
 
         if (accessToken == null) {
             return null;
         }
-        final User user = User.load(accessToken.getUserName(), core);
+        final User user = userService.load(accessToken.getUserName());
         if (user == null) {
             return null;
         }
-        if (user.isExternalUser() && !core.getLdapAuthenticator().isEnabled()) {
-            throw new LockedAccountException("LDAP authentication is currently disabled.");
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Found user {} for access token.", user);
         }
-        if (log.isDebugEnabled()) {
-            log.debug("Found user {} for access token.", user);
+        try {
+            accessTokenService.touch(accessToken);
+        } catch (ValidationException e) {
+            LOG.warn("Unable to update access token's last access date.", e);
         }
+        ShiroSecurityContext.requestSessionCreation(false);
         return new SimpleAccount(user.getName(), null, "access token realm");
     }
 }
