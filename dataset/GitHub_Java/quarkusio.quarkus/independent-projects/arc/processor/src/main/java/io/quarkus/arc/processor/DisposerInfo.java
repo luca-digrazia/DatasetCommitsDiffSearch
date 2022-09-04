59e7f5001404b08d5a4b4ac73c415057a581d172
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:19:08 +0200

    Observers - finish EventMetadata support
        - resolves #2380

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/DisposerInfo.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/DisposerInfo.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.arc.processor;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 import javax.enterprise.inject.spi.DefinitionException;
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationTarget.Kind;
@@ -72,11 +87,9 @@ public class DisposerInfo implements InjectionTargetInfo {
     }
 
     Collection<AnnotationInstance> getDisposedParameterQualifiers() {
-        Set<AnnotationInstance> resultingQualifiers = new HashSet<>();
-        Annotations.getParameterAnnotations(declaringBean.getDeployment(), disposerMethod, disposedParameter.position())
-                .stream().forEach(a -> declaringBean.getDeployment().extractQualifiers(a)
-                        .forEach(resultingQualifiers::add));
-        return resultingQualifiers;
+        return Annotations.getParameterAnnotations(declaringBean.getDeployment(), disposerMethod, disposedParameter.position())
+                .stream().filter(a -> declaringBean.getDeployment().getQualifier(a.name()) != null)
+                .collect(Collectors.toList());
     }
 
     Type getDisposedParameterType() {
