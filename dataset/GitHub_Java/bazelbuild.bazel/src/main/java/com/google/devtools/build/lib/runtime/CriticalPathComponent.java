354ae1f55f24d8f41084472326d507cf6f926a22
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 20:13:18 +0200

    Don't crash on missing action complete events
        Note that we will ignore any interrupted actions for the purposes of
        computing the critical path of the build, since there were no completion
        events for those actions.
    
        Progress on #6394.
    
        PiperOrigin-RevId: 240095667

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/CriticalPathComponent.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/CriticalPathComponent.java
@@ -18,7 +18,6 @@ import com.google.devtools.build.lib.actions.Action;
 import com.google.devtools.build.lib.actions.ActionOwner;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.SpawnMetrics;
-import com.google.devtools.build.lib.actions.SpawnResult;
 import com.google.devtools.build.lib.clock.Clock;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadCompatible;
@@ -66,8 +65,6 @@ public class CriticalPathComponent {
 
   /** Spawn metrics for this action. */
   private SpawnMetrics spawnMetrics = SpawnMetrics.EMPTY;
-  /** Name of the runner used for the spawn. */
-  @Nullable private String spawnRunnerName;
   /** An unique identifier of the component for one build execution */
   private final int id;
 
@@ -107,7 +104,6 @@ public class CriticalPathComponent {
     }
   }
 
-  @SuppressWarnings("ReferenceEquality")
   boolean isPrimaryOutput(Artifact possiblePrimaryOutput) {
     // We know that the keys in the CriticalPathComputer are exactly the values returned from
     // action.getPrimaryOutput(), so pointer equality is safe here.
@@ -151,10 +147,9 @@ public class CriticalPathComponent {
    * are run in parallel we should keep the maximum), we keep the maximum. This is better than just
    * keeping the latest one.
    */
-  void addSpawnResult(SpawnResult spawnResult) {
-    if (spawnResult.getMetrics().totalTime().compareTo(spawnMetrics.totalTime()) > 0) {
-      this.spawnMetrics = spawnResult.getMetrics();
-      this.spawnRunnerName = spawnResult.getRunnerName();
+  void addSpawnMetrics(SpawnMetrics spawnMetrics) {
+    if (spawnMetrics.totalTime().compareTo(this.spawnMetrics.totalTime()) > 0) {
+      this.spawnMetrics = spawnMetrics;
     }
   }
 
@@ -164,17 +159,8 @@ public class CriticalPathComponent {
   }
 
   /**
-   * Returns name of the runner used for the finished spawn which took most time (see {@link
-   * #addSpawnResult(SpawnResult)}), null if no spawns have finished for this action (either there
-   * are no spawns or we asked before any have finished).
-   */
-  @Nullable
-  public String getSpawnRunnerName() {
-    return spawnRunnerName;
-  }
-
-  /**
-   * Add statistics for one dependency of this action. Caller should ensure {@code dep} not running.
+   * Add statistics for one dependency of this action. Caller should ensure {@code dep} not
+   * running.
    */
   synchronized void addDepInfo(CriticalPathComponent dep) {
     long childAggregatedWallTime = dep.getAggregatedElapsedTimeNanos();
