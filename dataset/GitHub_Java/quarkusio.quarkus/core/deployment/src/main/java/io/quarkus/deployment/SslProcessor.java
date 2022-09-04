89fcac145eb908411b3d7a22cf5fe46e1f0ba4dd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:22:06 +0200

    Remove conditional code testing JDK 8

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/SslProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/SslProcessor.java
@@ -2,13 +2,17 @@ package io.quarkus.deployment;
 
 import java.util.Optional;
 
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.SslNativeConfigBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeReinitializedClassBuildItem;
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 
 public class SslProcessor {
+
+    private static final String JAVA_11_PLUS_SSL_LOGGER = "sun.security.ssl.SSLLogger";
 
     SslConfig ssl;
 
@@ -24,5 +28,10 @@ public class SslProcessor {
     @BuildStep
     SslNativeConfigBuildItem setupNativeSsl() {
         return new SslNativeConfigBuildItem(ssl.native_);
+    }
+
+    @BuildStep
+    void runtime(BuildProducer<RuntimeReinitializedClassBuildItem> reinitialized) {
+        reinitialized.produce(new RuntimeReinitializedClassBuildItem(JAVA_11_PLUS_SSL_LOGGER));
     }
 }
