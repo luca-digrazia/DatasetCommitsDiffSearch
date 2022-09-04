2768d9f0066923d44dc47aa8337b92a343773b18
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:01:44 +0200

    Use .equals rather than ==, as == is broken for deserialized indexes

--- dataset/GitHub_Java/quarkusio.quarkus/jpa/deployment/src/main/java/org/jboss/shamrock/jpa/JpaJandexScavenger.java
+++ dataset/GitHub_Java/quarkusio.quarkus/jpa/deployment/src/main/java/org/jboss/shamrock/jpa/JpaJandexScavenger.java
@@ -1,27 +1,50 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.shamrock.jpa;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
 
+import org.hibernate.jpa.boot.internal.ParsedPersistenceXmlDescriptor;
+import org.hibernate.jpa.boot.spi.PersistenceUnitDescriptor;
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.ClassType;
 import org.jboss.jandex.DotName;
+import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.IndexView;
-import org.jboss.shamrock.deployment.ArchiveContext;
-import org.jboss.shamrock.deployment.ProcessorContext;
-import org.jboss.shamrock.deployment.codegen.BytecodeRecorder;
+import org.jboss.logging.Logger;
+import org.jboss.shamrock.annotations.BuildProducer;
+import org.jboss.shamrock.deployment.builditem.substrate.ReflectiveClassBuildItem;
+import org.jboss.shamrock.jpa.runtime.JPADeploymentTemplate;
 
 /**
  * Scan the Jandex index to find JPA entities (and embeddables supporting entity models).
- *
+ * <p>
  * The output is then both consumed as plain list to use as a filter for which classes
  * need to be enhanced, collect them for storage in the JPADeploymentTemplate and registered
  * for reflective access.
@@ -35,34 +58,63 @@ final class JpaJandexScavenger {
     private static final DotName JPA_ENTITY = DotName.createSimple(Entity.class.getName());
     private static final DotName EMBEDDABLE = DotName.createSimple(Embeddable.class.getName());
     private static final DotName EMBEDDED = DotName.createSimple(Embedded.class.getName());
+    private static final DotName MAPPED_SUPERCLASS = DotName.createSimple(MappedSuperclass.class.getName());
 
-    private final ArchiveContext archiveContext;
-    private final ProcessorContext processorContext;
+    private static final DotName ENUM = DotName.createSimple(Enum.class.getName());
+    private static final Logger log = Logger.getLogger("org.jboss.shamrock.jpa");
 
-    JpaJandexScavenger(final ArchiveContext archiveContext, final ProcessorContext processorContext) {
-        this.archiveContext = archiveContext;
-        this.processorContext = processorContext;
+    private final List<ParsedPersistenceXmlDescriptor> descriptors;
+    private final BuildProducer<ReflectiveClassBuildItem> reflectiveClass;
+    private final IndexView combinedIndex;
+    private final JPADeploymentTemplate template;
+
+    JpaJandexScavenger(BuildProducer<ReflectiveClassBuildItem> reflectiveClass, List<ParsedPersistenceXmlDescriptor> descriptors, IndexView combinedIndex, JPADeploymentTemplate template) {
+        this.reflectiveClass = reflectiveClass;
+        this.descriptors = descriptors;
+        this.combinedIndex = combinedIndex;
+        this.template = template;
     }
 
     public KnownDomainObjects discoverModelAndRegisterForReflection() throws IOException {
         // list all entities and create a JPADeploymentTemplate out of it
         // Not functional as we will need one deployment template per persistence unit
-        final IndexView index = archiveContext.getCombinedIndex();
-        final DomainObjectSet collector;
-        // TODO what priority to give JPA?
-        try (BytecodeRecorder context = processorContext.addStaticInitTask(100)) {
-            JPADeploymentTemplate template = context.getRecordingProxy(JPADeploymentTemplate.class);
-            collector = new DomainObjectSet(template);
-            enlistJPAModelClasses(JPA_ENTITY, processorContext, collector, index);
-            enlistJPAModelClasses(EMBEDDABLE, processorContext, collector, index);
-            enlistReturnType(processorContext, index);
-            template.enlistPersistenceUnit();
+        final IndexView index = combinedIndex;
+        final DomainObjectSet collector = new DomainObjectSet();
+
+        enlistJPAModelClasses(JPA_ENTITY, collector, index);
+        enlistJPAModelClasses(EMBEDDABLE, collector, index);
+        enlistJPAModelClasses(MAPPED_SUPERCLASS, collector, index);
+        enlistReturnType(collector, index);
+
+        for (PersistenceUnitDescriptor pud : descriptors) {
+            enlistExplicitClasses(pud.getManagedClassNames(), collector, index);
         }
-        collector.clearLinkToTemplate();//no longer need the JPADeploymentTemplate
+
+        collector.registerAllForReflection(reflectiveClass);
+
+        collector.dumpAllToJPATemplate(template);
+        template.enlistPersistenceUnit();
+        template.callHibernateFeatureInit();
+
         return collector;
     }
 
-    private void enlistReturnType(ProcessorContext processorContext, IndexView index) {
+    private static void enlistExplicitClasses(List<String> managedClassNames, DomainObjectSet collector, IndexView index) {
+        for (String className : managedClassNames) {
+            DotName dotName = DotName.createSimple(className);
+            boolean isInIndex = index.getClassByName(dotName) != null;
+            if (isInIndex) {
+                addClassHierarchyToReflectiveList(collector, index, dotName);
+            } else {
+                // We do lipstick service by manually adding explicitly the <class> reference but not navigating the hierarchy
+                // so a class with a complex hierarchy will fail.
+                log.warnf("Did not find `%s` in the indexed jars. You likely forgot to tell Shamrock to index your dependency jar. See https://github.com/protean-project/shamrock/#indexing-and-application-classes for more info.", className);
+                collector.addEntity(className);
+            }
+        }
+    }
+
+    private static void enlistReturnType(DomainObjectSet collector, IndexView index) {
         Collection<AnnotationInstance> annotations = index.getAnnotations(EMBEDDED);
         if (annotations != null && annotations.size() > 0) {
             for (AnnotationInstance annotation : annotations) {
@@ -80,17 +132,17 @@ final class JpaJandexScavenger {
                     default:
                         throw new IllegalStateException("[internal error] @Embedded placed on a unknown element: " + target);
                 }
-                addClassHierarchyToReflectiveList(processorContext, index, jpaClassName);
+                addClassHierarchyToReflectiveList(collector, index, jpaClassName);
             }
         }
     }
 
-    private void enlistJPAModelClasses(DotName dotName, ProcessorContext processorContext, DomainObjectSet collector, IndexView index) {
+    private static void enlistJPAModelClasses(DotName dotName, DomainObjectSet collector, IndexView index) {
         Collection<AnnotationInstance> jpaAnnotations = index.getAnnotations(dotName);
         if (jpaAnnotations != null && jpaAnnotations.size() > 0) {
             for (AnnotationInstance annotation : jpaAnnotations) {
                 DotName targetDotName = annotation.target().asClass().name();
-                addClassHierarchyToReflectiveList(processorContext, index, targetDotName);
+                addClassHierarchyToReflectiveList(collector, index, targetDotName);
                 collector.addEntity(targetDotName.toString());
             }
         }
@@ -99,11 +151,11 @@ final class JpaJandexScavenger {
     /**
      * Add the class to the reflective list with full method and field access.
      * Add the superclasses recursively as well as the interfaces.
-     *
+     * <p>
      * TODO this approach fails if the Jandex index is not complete (e.g. misses somes interface or super types)
      * TODO should we also return the return types of all methods and fields? It could container Enums for example.
      */
-    private void addClassHierarchyToReflectiveList(ProcessorContext processorContext, IndexView index, DotName className) {
+    private static void addClassHierarchyToReflectiveList(DomainObjectSet collector, IndexView index, DotName className) {
         // If type is not Object
         // recursively add superclass and interfaces
         if (className == null) {
@@ -112,44 +164,71 @@ final class JpaJandexScavenger {
         }
         ClassInfo classInfo = index.getClassByName(className);
         if (classInfo == null) {
-            if (className == ClassType.OBJECT_TYPE.name()) {
+            if (className.equals(ClassType.OBJECT_TYPE.name()) || className.toString().equals(Serializable.class.getName())) {
                 return;
-            }
-            else {
+            } else {
                 throw new IllegalStateException("The Jandex index is not complete, missing: " + className.toString());
             }
         }
-        // add class for reflection
-        processorContext.addReflectiveClass(true, true, className.toString());
+        //we need to check for enums
+        for (FieldInfo fieldInfo : classInfo.fields()) {
+            DotName type = fieldInfo.type().name();
+            ClassInfo typeCi = index.getClassByName(type);
+            if (typeCi != null && typeCi.superName().equals(ENUM)) {
+                collector.addEnumType(type.toString());
+            }
+        }
+
+        //Capture this one (for various needs: Reflective access enablement, Hibernate enhancement, JPA Template)
+        collector.addEntity(className.toString());
         // add superclass recursively
-        addClassHierarchyToReflectiveList(processorContext, index, classInfo.superName());
+        addClassHierarchyToReflectiveList(collector, index, classInfo.superName());
         // add interfaces recursively
         for (DotName interfaceDotName : classInfo.interfaceNames()) {
-            addClassHierarchyToReflectiveList(processorContext, index, interfaceDotName);
+            addClassHierarchyToReflectiveList(collector, index, interfaceDotName);
         }
     }
 
     private static class DomainObjectSet implements KnownDomainObjects {
 
-        private JPADeploymentTemplate template;
         private final Set<String> classNames = new HashSet<String>();
+        private final Set<String> enumTypes = new HashSet<String>();
 
-        public DomainObjectSet(final JPADeploymentTemplate template) {
-            this.template = template;
+        public void addEntity(final String className) {
+            classNames.add(className);
         }
 
-        public void addEntity(final String name) {
-            classNames.add(name);
-            template.addEntity(name);
+        void dumpAllToJPATemplate(final JPADeploymentTemplate template) {
+            for (String className : classNames) {
+                template.addEntity(className);
+            }
         }
 
-        void clearLinkToTemplate(){
-            this.template = null;
+
+        void registerAllForReflection(final BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
+            for (String className : classNames) {
+                reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, className));
+            }
+            if (!enumTypes.isEmpty()) {
+                reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, Enum.class.getName()));
+                for (String className : enumTypes) {
+                    reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, className));
+                }
+            }
         }
 
         @Override
         public boolean contains(final String className) {
             return classNames.contains(className);
+        }
+
+        @Override
+        public Set<String> getClassNames() {
+            return classNames;
+        }
+
+        public void addEnumType(String s) {
+            enumTypes.add(s);
         }
     }
 
