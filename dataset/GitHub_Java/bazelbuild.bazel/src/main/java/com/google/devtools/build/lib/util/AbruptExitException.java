184ab9cab0b50e03ae6085869df94dec7e796620
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:10:32 +0200

    When initializing AbruptExitException from DetailedExitCode, use its message too.
        There shouldn't really be a reason to have two different error messages for the exit code and the exception. If there ever is a good reason, we can add these overloads back.
    
        PiperOrigin-RevId: 302453214

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/util/AbruptExitException.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/util/AbruptExitException.java
@@ -27,6 +27,21 @@ public class AbruptExitException extends Exception {
 
   private final DetailedExitCode detailedExitCode;
 
+  public AbruptExitException(String message, ExitCode exitCode) {
+    super(message);
+    this.detailedExitCode = DetailedExitCode.justExitCode(exitCode);
+  }
+
+  public AbruptExitException(String message, ExitCode exitCode, Throwable cause) {
+    super(message, cause);
+    this.detailedExitCode = DetailedExitCode.justExitCode(exitCode);
+  }
+
+  public AbruptExitException(ExitCode exitCode, Throwable cause) {
+    super(cause);
+    this.detailedExitCode = DetailedExitCode.justExitCode(exitCode);
+  }
+
   public AbruptExitException(DetailedExitCode detailedExitCode) {
     super(detailedExitCode.getFailureDetail().getMessage());
     this.detailedExitCode = detailedExitCode;
