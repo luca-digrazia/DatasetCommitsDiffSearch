1aff5cc979f80baf9a92d90ffe1559fd6972cad9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:15:21 +0200

    Refactor into common method (#9189)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/events/notifications/types/HTTPEventNotification.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/events/notifications/types/HTTPEventNotification.java
@@ -29,17 +29,17 @@ import org.graylog.events.notifications.EventNotification;
 import org.graylog.events.notifications.EventNotificationContext;
 import org.graylog.events.notifications.EventNotificationModelData;
 import org.graylog.events.notifications.EventNotificationService;
+import org.graylog.events.notifications.NotificationTestData;
 import org.graylog.events.notifications.PermanentEventNotificationException;
 import org.graylog.events.notifications.TemporaryEventNotificationException;
-import org.graylog.events.processor.EventDefinitionDto;
-import org.graylog.scheduler.JobTriggerDto;
 import org.graylog2.plugin.MessageSummary;
+import org.graylog2.system.urlwhitelist.UrlWhitelistNotificationService;
+import org.graylog2.system.urlwhitelist.UrlWhitelistService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.io.IOException;
-import java.util.Optional;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -52,19 +52,22 @@ public class HTTPEventNotification implements EventNotification {
     private static final Logger LOG = LoggerFactory.getLogger(HTTPEventNotification.class);
 
     private static final MediaType CONTENT_TYPE = MediaType.parse(APPLICATION_JSON);
-    private static final String UNKNOWN = "<unknown>";
 
     private final EventNotificationService notificationCallbackService;
     private final ObjectMapper objectMapper;
     private final OkHttpClient httpClient;
+    private final UrlWhitelistService whitelistService;
+    private final UrlWhitelistNotificationService urlWhitelistNotificationService;
 
     @Inject
-    public HTTPEventNotification(EventNotificationService notificationCallbackService,
-                                 ObjectMapper objectMapper,
-                                 final OkHttpClient httpClient) {
+    public HTTPEventNotification(EventNotificationService notificationCallbackService, ObjectMapper objectMapper,
+            final OkHttpClient httpClient, UrlWhitelistService whitelistService,
+            UrlWhitelistNotificationService urlWhitelistNotificationService) {
         this.notificationCallbackService = notificationCallbackService;
         this.objectMapper = objectMapper;
         this.httpClient = httpClient;
+        this.whitelistService = whitelistService;
+        this.urlWhitelistNotificationService = urlWhitelistNotificationService;
     }
 
     @Override
@@ -78,7 +81,15 @@ public class HTTPEventNotification implements EventNotification {
         }
 
         ImmutableList<MessageSummary> backlog = notificationCallbackService.getBacklogForEvent(ctx);
-        final EventNotificationModelData model = getModel(ctx, backlog);
+        final EventNotificationModelData model = EventNotificationModelData.of(ctx, backlog);
+
+        if (!whitelistService.isWhitelisted(config.url())) {
+            if (!NotificationTestData.TEST_NOTIFICATION_ID.equals(ctx.notificationId())) {
+                publishSystemNotificationForWhitelistFailure(config.url(), model.eventDefinitionTitle());
+            }
+            throw new TemporaryEventNotificationException("URL <" + config.url() + "> is not whitelisted.");
+        }
+
         final byte[] body;
         try {
             body = objectMapper.writeValueAsBytes(model);
@@ -104,19 +115,10 @@ public class HTTPEventNotification implements EventNotification {
         }
     }
 
-    private EventNotificationModelData getModel(EventNotificationContext ctx, ImmutableList<MessageSummary> backlog) {
-        final Optional<EventDefinitionDto> definitionDto = ctx.eventDefinition();
-        final Optional<JobTriggerDto> jobTriggerDto = ctx.jobTrigger();
-
-        return EventNotificationModelData.builder()
-                .eventDefinitionId(definitionDto.map(EventDefinitionDto::id).orElse(UNKNOWN))
-                .eventDefinitionType(definitionDto.map(d -> d.config().type()).orElse(UNKNOWN))
-                .eventDefinitionTitle(definitionDto.map(EventDefinitionDto::title).orElse(UNKNOWN))
-                .eventDefinitionDescription(definitionDto.map(EventDefinitionDto::description).orElse(UNKNOWN))
-                .jobDefinitionId(jobTriggerDto.map(JobTriggerDto::jobDefinitionId).orElse(UNKNOWN))
-                .jobTriggerId(jobTriggerDto.map(JobTriggerDto::id).orElse(UNKNOWN))
-                .event(ctx.event())
-                .backlog(backlog)
-                .build();
+    private void publishSystemNotificationForWhitelistFailure(String url, String eventNotificationTitle) {
+        final String description = "The alert notification \"" + eventNotificationTitle +
+                "\" is trying to access a URL which is not whitelisted. Please check your configuration. [url: \"" +
+                url + "\"]";
+        urlWhitelistNotificationService.publishWhitelistFailure(description);
     }
 }
