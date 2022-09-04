3e19e5fa0cac186dddd2cd041350591d880970a5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:33:13 +0200

    Pass the resource length into the ByteRange parse method to simplify logic

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-servlets/src/test/java/io/dropwizard/servlets/assets/ByteRangeTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-servlets/src/test/java/io/dropwizard/servlets/assets/ByteRangeTest.java
@@ -1,50 +1,49 @@
 package io.dropwizard.servlets.assets;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
 
 public class ByteRangeTest {
 
     private static final int RESOURCE_LENGTH = 10000;
-
+    
     @Test
-    void firstBytes() {
+    public void firstBytes() {
         final ByteRange actual = ByteRange.parse("0-499", RESOURCE_LENGTH);
         assertThat(actual.getStart()).isEqualTo(0);
         assertThat(actual.getEnd()).isEqualTo(499);
     }
 
     @Test
-    void secondBytes() {
+    public void secondBytes() {
         final ByteRange actual = ByteRange.parse("500-999", RESOURCE_LENGTH);
         assertThat(actual.getStart()).isEqualTo(500);
         assertThat(actual.getEnd()).isEqualTo(999);
     }
 
     @Test
-    void finalBytes() {
+    public void finalBytes() {
         final ByteRange actual = ByteRange.parse("-500", RESOURCE_LENGTH);
         assertThat(actual.getStart()).isEqualTo(9500);
         assertThat(actual.getEnd()).isEqualTo(9999);
     }
 
     @Test
-    void noEndBytes() {
+    public void noEndBytes() {
         final ByteRange actual = ByteRange.parse("9500-", RESOURCE_LENGTH);
         assertThat(actual.getStart()).isEqualTo(9500);
         assertThat(actual.getEnd()).isEqualTo(9999);
     }
-
+    
     @Test
-    void startBytes() {
+    public void startBytes() {
         final ByteRange actual = ByteRange.parse("9500", RESOURCE_LENGTH);
         assertThat(actual.getStart()).isEqualTo(9500);
         assertThat(actual.getEnd()).isEqualTo(9999);
     }
 
     @Test
-    void tooManyBytes() {
+    public void tooManyBytes() {
         final ByteRange actual = ByteRange.parse("9000-20000", RESOURCE_LENGTH);
         assertThat(actual.getStart()).isEqualTo(9000);
         assertThat(actual.getEnd()).isEqualTo(9999);
