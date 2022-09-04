39b5f0df323abd855676a3edf1ccbfb1a7801df3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:16:49 +0200

    cosmetic edit

--- dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/regression/GaussianProcessRegression.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/regression/GaussianProcessRegression.java
@@ -112,7 +112,7 @@ public class GaussianProcessRegression<T> implements Regression<T> {
      */
     private Matrix.Cholesky cholesky;
 
-    /** The joint prediction of multiple data points. */
+    /** The prediction results. */
     public class JointPrediction {
         /** The query points where the GP is evaluated. */
         public final T[] x;
