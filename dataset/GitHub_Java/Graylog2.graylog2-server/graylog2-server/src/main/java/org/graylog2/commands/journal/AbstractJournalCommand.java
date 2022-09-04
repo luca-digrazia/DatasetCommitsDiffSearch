21a9c9ea458f4f57c8ce52c2ccb25f9e220ad2ec
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:58:34 +0200

    Add missing AuditBindings to journal commands (#3226)
        Fixes #3126

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/commands/journal/AbstractJournalCommand.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/commands/journal/AbstractJournalCommand.java
@@ -18,16 +18,24 @@ package org.graylog2.commands.journal;
 
 import com.google.inject.Module;
 import org.graylog2.Configuration;
+import org.graylog2.audit.AuditBindings;
+import org.graylog2.bindings.ConfigurationModule;
 import org.graylog2.bootstrap.CmdLineTool;
 import org.graylog2.plugin.KafkaJournalConfiguration;
+import org.graylog2.plugin.Plugin;
+import org.graylog2.plugin.ServerStatus;
 import org.graylog2.shared.bindings.SchedulerBindings;
+import org.graylog2.shared.bindings.ServerStatusBindings;
 import org.graylog2.shared.journal.KafkaJournal;
 import org.graylog2.shared.journal.KafkaJournalModule;
+import org.graylog2.shared.plugins.ChainingClassLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public abstract class AbstractJournalCommand extends CmdLineTool {
     protected static final Logger log = LoggerFactory.getLogger(AbstractJournalCommand.class);
@@ -44,18 +52,17 @@ public abstract class AbstractJournalCommand extends CmdLineTool {
     }
 
     @Override
-    protected boolean validateConfiguration() {
-        if (kafkaJournalConfiguration.getMessageJournalDir() == null) {
-            log.error("No message journal path set. Please define \"message_journal_dir\" in your graylog.conf.");
-            return false;
-        }
-        return true;
+    protected List<Module> getCommandBindings() {
+        return Arrays.asList(new ConfigurationModule(configuration),
+                             new ServerStatusBindings(capabilities()),
+                             new SchedulerBindings(),
+                             new KafkaJournalModule(),
+                             new AuditBindings());
     }
 
     @Override
-    protected List<Module> getCommandBindings() {
-        return Arrays.<Module>asList(new SchedulerBindings(),
-                                     new KafkaJournalModule());
+    protected Set<ServerStatus.Capability> capabilities() {
+        return configuration.isMaster() ? Collections.singleton(ServerStatus.Capability.MASTER) : Collections.emptySet();
     }
 
     @Override
@@ -69,6 +76,11 @@ public abstract class AbstractJournalCommand extends CmdLineTool {
         return true;
     }
 
+    @Override
+    protected Set<Plugin> loadPlugins(String pluginPath, ChainingClassLoader chainingClassLoader) {
+        // these commands do not need plugins, which could cause problems because of not loaded config beans
+        return Collections.emptySet();
+    }
 
     @Override
     protected void startCommand() {
