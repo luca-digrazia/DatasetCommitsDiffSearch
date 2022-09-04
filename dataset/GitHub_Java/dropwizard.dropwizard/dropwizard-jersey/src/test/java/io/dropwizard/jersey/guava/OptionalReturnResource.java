a3b066a956c2a38cfb7d4bea473b8ff874798bad
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:38:26 +0200

    organising import ordering

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalReturnResource.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalReturnResource.java
@@ -2,7 +2,12 @@ package io.dropwizard.jersey.guava;
 
 import com.google.common.base.Optional;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/optional-return/")
@@ -14,8 +19,7 @@ public class OptionalReturnResource {
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Optional<String> showWithFormParam(@QueryParam("id") String id) {
+    public Optional<String> showWithFormParam(@FormParam("id") String id) {
         return Optional.fromNullable(id);
     }
 }
