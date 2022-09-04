55755a6c8ae65be25cdd936268b263c6ef73f621
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:08:21 +0200

    Deprecate shutting a node down via the API (#10986)
        Deprecates the API endpoints used to shut down a node.
        - Adds deprecation annotations to discourage usage in the code
        - Removes Swagger annotations to hide the endpoints in the API browser
        - Logs a warning when the endpoints are called
        - Returns a warning message in the API response where applicable
    
        Fixes #10668

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/cluster/ClusterSystemShutdownResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/cluster/ClusterSystemShutdownResource.java
@@ -1,90 +1,87 @@
-/**
- * This file is part of Graylog.
+/*
+ * Copyright (C) 2020 Graylog, Inc.
  *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
  *
- * Graylog is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-
 package org.graylog2.rest.resources.cluster;
 
 import com.codahale.metrics.annotation.Timed;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.graylog2.audit.AuditEventTypes;
+import org.graylog2.audit.jersey.AuditEvent;
 import org.graylog2.cluster.Node;
 import org.graylog2.cluster.NodeNotFoundException;
 import org.graylog2.cluster.NodeService;
 import org.graylog2.rest.RemoteInterfaceProvider;
 import org.graylog2.rest.resources.system.RemoteSystemShutdownResource;
+import org.graylog2.shared.rest.resources.ProxiedResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import retrofit2.Response;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
 
-import static org.jboss.netty.handler.codec.http.HttpResponseStatus.ACCEPTED;
+import static javax.ws.rs.core.Response.Status.ACCEPTED;
+import static javax.ws.rs.core.Response.Status.BAD_GATEWAY;
 
+/**
+ * @deprecated Shutting down nodes using an API request is discouraged in favor of using a service manager to
+ *         control the server process.
+ */
 @RequiresAuthentication
-@Api(value = "Cluster/Shutdown", description = "Shutdown gracefully nodes in cluster")
 @Path("/cluster/{nodeId}/shutdown")
 @Produces(MediaType.APPLICATION_JSON)
-public class ClusterSystemShutdownResource {
+@Deprecated
+public class ClusterSystemShutdownResource extends ProxiedResource {
     private static final Logger LOG = LoggerFactory.getLogger(ClusterSystemShutdownResource.class);
-
-    private final NodeService nodeService;
-    private final RemoteInterfaceProvider remoteInterfaceProvider;
-    private final String authenticationToken;
 
     @Inject
     public ClusterSystemShutdownResource(NodeService nodeService,
                                          RemoteInterfaceProvider remoteInterfaceProvider,
-                                         @Context HttpHeaders httpHeaders) throws NodeNotFoundException {
-        this.nodeService = nodeService;
-        this.remoteInterfaceProvider = remoteInterfaceProvider;
-
-        final List<String> authenticationTokens = httpHeaders.getRequestHeader("Authorization");
-        if (authenticationTokens != null && authenticationTokens.size() >= 1) {
-            this.authenticationToken = authenticationTokens.get(0);
-        } else {
-            this.authenticationToken = null;
-        }
+                                         @Context HttpHeaders httpHeaders,
+                                         @Named("proxiedRequestsExecutorService") ExecutorService executorService) throws NodeNotFoundException {
+        super(httpHeaders, nodeService, remoteInterfaceProvider, executorService);
     }
 
     @POST
     @Timed
-    @ApiOperation(value = "Shutdown node gracefully.",
-            notes = "Attempts to process all buffered and cached messages before exiting, " +
-                    "shuts down inputs first to make sure that no new messages are accepted.")
-    public void shutdown(@ApiParam(name = "nodeId", value = "The id of the node to shutdown.", required = true)
-                         @PathParam("nodeId") String nodeId) throws IOException, NodeNotFoundException {
+    @AuditEvent(type = AuditEventTypes.NODE_SHUTDOWN_INITIATE)
+    public void shutdown(@PathParam("nodeId") String nodeId) throws IOException, NodeNotFoundException {
+        LOG.warn(
+                "Deprecated API endpoint /cluster/{nodeId}/shutdown was called. Shutting down nodes via the API is " +
+                        "discouraged in favor of using a service manager to control the server process.");
+
         final Node targetNode = nodeService.byNodeId(nodeId);
 
         RemoteSystemShutdownResource remoteSystemShutdownResource = remoteInterfaceProvider.get(targetNode,
                 this.authenticationToken,
                 RemoteSystemShutdownResource.class);
         final Response response = remoteSystemShutdownResource.shutdown().execute();
-        if (response.code() != ACCEPTED.getCode()) {
-            LOG.warn("Unable send shut down signal to node " + nodeId + ": " + response.message());
+        if (response.code() != ACCEPTED.getStatusCode()) {
+            LOG.warn("Unable send shut down signal to node {}: {}", nodeId, response.message());
+            throw new WebApplicationException(response.message(), BAD_GATEWAY);
         }
     }
 }
