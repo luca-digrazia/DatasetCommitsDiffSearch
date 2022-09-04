7d11bf6f57bc6a6ea27932d84614ca39184052f8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:30:08 +0200

    Convert week/month bucket interval to days if value is bigger than 1. (#7140)
        * Convert week/month bucket interval to days if value is bigger than 1.
    
        Due to limitations in Elasticsearch, it does not support time unit
        bucket intervals with values bigger than 1 for units bigger than days.
        Therefore, this PR transparently converts these to days (with multiples
        of 7 resp. 30) if the value is bigger than 1, leaving the original unit
        otherwise.
    
        In contrast to the initial attempt in #7055, this PR is performing the
        change in the backend, transparent to the caller and relieving the
        caller from having to do the conversion before.
    
        Fixes #7022.
    
        * Adding license header.
    
        * Moving private method below its consumer.
    
        * Removing optional radix parameter for clarity.
    
        * Parameterizing tests, extracting message constant.
    
        * Fixing license header.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/plugins/views/search/searchtypes/pivot/buckets/TimeUnitInterval.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/plugins/views/search/searchtypes/pivot/buckets/TimeUnitInterval.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog.plugins.views.search.searchtypes.pivot.buckets;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -8,11 +24,17 @@ import com.google.auto.value.AutoValue;
 import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
 import org.graylog2.plugin.indexer.searches.timeranges.TimeRange;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
 @AutoValue
 @JsonTypeName(TimeUnitInterval.type)
 @JsonDeserialize(builder = TimeUnitInterval.Builder.class)
 public abstract class TimeUnitInterval implements Interval {
     public static final String type = "timeunit";
+    public static final Pattern TIMEUNIT_PATTERN = Pattern.compile("(?<quantity>\\d+)(?<unit>[smhdwM])");
 
     @JsonProperty
     public abstract String type();
@@ -22,7 +44,25 @@ public abstract class TimeUnitInterval implements Interval {
 
     @Override
     public DateHistogramInterval toDateHistogramInterval(TimeRange timerange) {
-        return new DateHistogramInterval(timeunit());
+        return new DateHistogramInterval(adjustUnitsLongerThanDays(timeunit()));
+    }
+
+    private String adjustUnitsLongerThanDays(String timeunit) {
+        final Matcher matcher = TIMEUNIT_PATTERN.matcher(timeunit());
+        checkArgument(matcher.matches(),
+                "Time unit must be {quantity}{unit}, where quantity is a positive number and unit [smhdwM].");
+        final int quantity = Integer.parseInt(matcher.group("quantity"));
+        final String unit = matcher.group("unit");
+
+        switch (unit) {
+            case "s":
+            case "m":
+            case "h":
+            case "d": return timeunit;
+            case "w": return quantity == 1 ? timeunit : (7 * quantity) + "d";
+            case "M": return quantity == 1 ? timeunit : (30 * quantity) + "d";
+            default: throw new RuntimeException("Invalid time unit: " + timeunit);
+        }
     }
 
     @AutoValue.Builder
@@ -33,7 +73,19 @@ public abstract class TimeUnitInterval implements Interval {
         @JsonProperty("timeunit")
         public abstract Builder timeunit(String timeunit);
 
-        public abstract TimeUnitInterval build();
+        abstract TimeUnitInterval autoBuild();
+        public TimeUnitInterval build() {
+            final TimeUnitInterval interval = autoBuild();
+            final Matcher matcher = TIMEUNIT_PATTERN.matcher(interval.timeunit());
+            checkArgument(matcher.matches(),
+                    "Time unit must be {quantity}{unit}, where quantity is a positive number and unit [smhdwM].");
+
+            final int quantity = Integer.parseInt(matcher.group("quantity"), 10);
+            checkArgument(quantity > 0,
+                    "Time unit's value must be a positive number, greater than zero.");
+
+            return interval;
+        }
 
         @JsonCreator
         public static Builder builder() {
