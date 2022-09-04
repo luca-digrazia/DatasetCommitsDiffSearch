1f50e55cbe8e6acb59cf4c8fb0861575ea159a34
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:36:52 +0200

    Update BuildMojo.java

--- dataset/GitHub_Java/quarkusio.quarkus/devtools/maven/src/main/java/io/quarkus/maven/BuildMojo.java
+++ dataset/GitHub_Java/quarkusio.quarkus/devtools/maven/src/main/java/io/quarkus/maven/BuildMojo.java
@@ -32,17 +32,15 @@ import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.repository.RemoteRepository;
 
-import io.quarkus.bootstrap.model.AppArtifact;
-import io.quarkus.bootstrap.model.AppModel;
-import io.quarkus.bootstrap.resolver.AppModelResolverException;
-import io.quarkus.bootstrap.resolver.BootstrapAppModelResolver;
-import io.quarkus.bootstrap.resolver.maven.MavenArtifactResolver;
+import io.quarkus.creator.AppArtifact;
 import io.quarkus.creator.AppCreator;
 import io.quarkus.creator.AppCreatorException;
+import io.quarkus.creator.AppDependency;
 import io.quarkus.creator.phase.augment.AugmentPhase;
 import io.quarkus.creator.phase.curate.CurateOutcome;
 import io.quarkus.creator.phase.runnerjar.RunnerJarOutcome;
 import io.quarkus.creator.phase.runnerjar.RunnerJarPhase;
+import io.quarkus.creator.resolver.maven.ResolvedMavenArtifactDeps;
 
 /**
  *
@@ -109,7 +107,6 @@ public class BuildMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project.build.directory}")
     private File buildDir;
-
     /**
      * The directory for library jars
      */
@@ -134,25 +131,10 @@ public class BuildMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException {
-        final AppArtifact appArtifact = new AppArtifact(project.getGroupId(), project.getArtifactId(), project.getVersion());
-        final AppModel appModel;
-        final BootstrapAppModelResolver modelResolver;
-        try {
-            modelResolver = new BootstrapAppModelResolver(
-                    MavenArtifactResolver.builder()
-                            .setRepositorySystem(repoSystem)
-                            .setRepositorySystemSession(repoSession)
-                            .setRemoteRepositories(repos)
-                            .build());
-            appModel = modelResolver.resolveModel(appArtifact);
-        } catch (AppModelResolverException e) {
-            throw new MojoExecutionException("Failed to resolve application model " + appArtifact + " dependencies", e);
-        }
         try (AppCreator appCreator = AppCreator.builder()
                 // configure the build phases we want the app to go through
                 .addPhase(new AugmentPhase()
                         .setAppClassesDir(outputDirectory.toPath())
-                        .setConfigDir(outputDirectory.toPath())
                         .setTransformedClassesDir(transformedClassesDirectory.toPath())
                         .setWiringClassesDir(wiringClassesDirectory.toPath()))
                 .addPhase(new RunnerJarPhase()
@@ -163,10 +145,15 @@ public class BuildMojo extends AbstractMojo {
                 .setWorkDir(buildDir.toPath())
                 .build()) {
 
+            final AppArtifact appArtifact = new AppArtifact(project.getGroupId(), project.getArtifactId(),
+                    project.getVersion());
+            final List<AppDependency> appDeps = new ResolvedMavenArtifactDeps(project.getGroupId(), project.getArtifactId(),
+                    project.getVersion(), project.getArtifacts()).collectDependencies(appArtifact);
+
             // push resolved application state
             appCreator.pushOutcome(CurateOutcome.builder()
-                    .setAppModelResolver(modelResolver)
-                    .setAppModel(appModel)
+                    .setAppArtifact(appArtifact)
+                    .setInitialDeps(appDeps)
                     .build());
 
             // resolve the outcome we need here
