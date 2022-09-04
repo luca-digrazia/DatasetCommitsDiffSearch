213825227a767d7b06645930722e284b7d8e4ccb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:40:49 +0200

    add validations to JsonCreator methods
        renamed getTotal() -> total(), this is only used in REST json mapping so it's safe

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-models/src/main/java/org/graylog2/rest/models/roles/responses/RolesResponse.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-models/src/main/java/org/graylog2/rest/models/roles/responses/RolesResponse.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.rest.models.roles.responses;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -5,6 +21,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @AutoValue
@@ -12,15 +29,16 @@ import java.util.Set;
 public abstract class RolesResponse {
 
     @JsonProperty
+    @NotNull
     public abstract Set<RoleResponse> roles();
 
     @JsonProperty
-    public int getTotal() {
+    public int total() {
         return roles().size();
     }
 
     @JsonCreator
-    public static RolesResponse create(@JsonProperty("roles") Set<RoleResponse> roles) {
+    public static RolesResponse create(@JsonProperty("roles") @NotNull Set<RoleResponse> roles) {
         return new AutoValue_RolesResponse(roles);
     }
 }
