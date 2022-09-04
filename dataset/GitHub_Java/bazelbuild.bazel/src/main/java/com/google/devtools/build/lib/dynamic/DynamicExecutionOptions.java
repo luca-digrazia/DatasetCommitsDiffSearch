59e2a5c7dd14da4215d7685f9828320cf436c483
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:53:18 +0200

    Automated rollback of commit b41576b2f5bb87f3817dfb1ee09493b8220a9634.
        *** Reason for rollback ***
    
        This setting does help some users, so let's leave it in until we have more concrete advice on what to do instead.
    
        *** Original change description ***
    
        Deprecate `--experimental_spawn_scheduler` flag since its use is usually a poor use of resources.
    
        RELNOTES: None.
        PiperOrigin-RevId: 385990417

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/dynamic/DynamicExecutionOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/dynamic/DynamicExecutionOptions.java
@@ -13,10 +13,14 @@
 // limitations under the License.
 package com.google.devtools.build.lib.dynamic;
 
+import com.google.devtools.common.options.Converters;
 import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionDocumentationCategory;
 import com.google.devtools.common.options.OptionEffectTag;
+import com.google.devtools.common.options.OptionMetadataTag;
 import com.google.devtools.common.options.OptionsBase;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Options related to dynamic spawn execution.
@@ -29,48 +33,61 @@ public class DynamicExecutionOptions extends OptionsBase {
       effectTags = {OptionEffectTag.UNKNOWN},
       defaultValue = "null",
       help =
-          "Run actions locally instead of remotely for incremental builds as long as enough "
-              + "resources are available to execute all runnable actions in parallel.",
-      expansion = {"--internal_spawn_scheduler", "--spawn_strategy=dynamic"})
+          "Enable dynamic execution by running actions locally and remotely in parallel. Bazel "
+              + "spawns each action locally and remotely and picks the one that completes first. "
+              + "If an action supports workers, the local action will be run in the persistent "
+              + "worker mode. To enable dynamic execution for an individual action mnemonic, use "
+              + "the `--internal_spawn_scheduler` and `--strategy=<mnemonic>=dynamic` flags "
+              + "instead.",
+      expansion = {
+        "--internal_spawn_scheduler",
+        "--spawn_strategy=dynamic",
+      })
   public Void experimentalSpawnScheduler;
 
   @Option(
-    name = "internal_spawn_scheduler",
-    documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    defaultValue = "false",
-    help =
-        "Placeholder option so that we can tell in Blaze whether the spawn scheduler was "
-            + "enabled."
-  )
+      name = "internal_spawn_scheduler",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      defaultValue = "false",
+      help =
+          "Placeholder option so that we can tell in Blaze whether the spawn scheduler was "
+              + "enabled.")
   public boolean internalSpawnScheduler;
 
   @Option(
-    name = "dynamic_local_strategy",
-    documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    defaultValue = "sandboxed",
-    help = "Strategy to use when the dynamic spawn scheduler decides to run an action locally."
-  )
-  public String dynamicLocalStrategy;
+      name = "dynamic_local_strategy",
+      converter = Converters.StringToStringListConverter.class,
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      defaultValue = "null",
+      allowMultiple = true,
+      help =
+          "The local strategies, in order, to use for the given mnemonic. Passing"
+              + " 'local' as the mnemonic sets the default for unspecified mnemonics. Takes"
+              + " [mnemonic=]local_strategy[,local_strategy,...]")
+  public List<Map.Entry<String, List<String>>> dynamicLocalStrategy;
 
   @Option(
       name = "dynamic_remote_strategy",
+      converter = Converters.StringToStringListConverter.class,
       documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
       effectTags = {OptionEffectTag.UNKNOWN},
-      defaultValue = "remote",
-      help = "Strategy to use when the dynamic spawn scheduler decides to run an action remotely."
-  )
-  public String dynamicRemoteStrategy;
+      defaultValue = "null",
+      allowMultiple = true,
+      help =
+          "The remote strategies to use for the given mnemonic. Passing 'remote'"
+              + " as the mnemonic sets the default for unspecified mnemonics. Takes"
+              + " [mnemonic=]remote_strategy[,remote_strategy,...]")
+  public List<Map.Entry<String, List<String>>> dynamicRemoteStrategy;
 
   @Option(
       name = "dynamic_worker_strategy",
       documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
       effectTags = {OptionEffectTag.UNKNOWN},
-      defaultValue = "worker",
-      help = "Strategy to use when the dynamic spawn scheduler decides to run an action in a"
-          + " worker."
-  )
+      metadataTags = {OptionMetadataTag.DEPRECATED},
+      defaultValue = "",
+      help = "Deprecated. Please use --dynamic_local_strategy=worker_strategy,local_strategy.")
   public String dynamicWorkerStrategy;
 
   @Option(
@@ -91,4 +108,26 @@ public class DynamicExecutionOptions extends OptionsBase {
     defaultValue = "false"
   )
   public boolean debugSpawnScheduler;
+
+  @Option(
+      name = "experimental_require_availability_info",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      defaultValue = "false",
+      help =
+          "If true, fail the build if there are actions that set requires-darwin but do not have"
+              + "Xcode availability-related execution requirements set.")
+  public boolean requireAvailabilityInfo;
+
+  @Option(
+      name = "experimental_availability_info_exempt",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      defaultValue = "Genrule,TestRunner",
+      converter = Converters.CommaSeparatedOptionListConverter.class,
+      help =
+          "A comma-separated list of mnemonics that are not required to have Xcode-related "
+              + "execution info if --experimental_require_availability_info=true. No-op if "
+              + "--experimental_require_availability_info=false.")
+  public List<String> availabilityInfoExempt;
 }
