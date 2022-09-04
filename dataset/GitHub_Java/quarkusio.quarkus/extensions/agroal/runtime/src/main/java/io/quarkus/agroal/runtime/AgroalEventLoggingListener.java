2c8890287cfbf1ed03959836f5bee1e1e1248ab1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:32:11 +0200

    Implement the Info logging capability

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/AgroalEventLoggingListener.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/AgroalEventLoggingListener.java
@@ -13,7 +13,7 @@ final class AgroalEventLoggingListener implements AgroalDataSourceListener {
     private final String datasourceName;
 
     public AgroalEventLoggingListener(String name) {
-        this.datasourceName = "Datasource '" + name + "'";
+        this.datasourceName = name;
     }
 
     @Override
