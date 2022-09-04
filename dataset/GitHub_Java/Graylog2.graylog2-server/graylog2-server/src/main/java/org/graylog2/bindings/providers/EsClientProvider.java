4b4beffab29d6035c6fbc89311ee61f8c24aa415
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:59:13 +0200

    Remove unneeded MetricRegistry from EsClientProvider

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/providers/EsClientProvider.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/providers/EsClientProvider.java
@@ -16,37 +16,24 @@
  */
 package org.graylog2.bindings.providers;
 
-import com.codahale.metrics.MetricRegistry;
-import com.github.joschi.jadconfig.util.Duration;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.node.Node;
-import org.graylog2.indexer.elasticsearch.GlobalTimeoutClient;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 
 @Singleton
 public class EsClientProvider implements Provider<Client> {
     private final Node node;
-    private final MetricRegistry metricRegistry;
-    private final Duration requestTimeout;
 
     @Inject
-    public EsClientProvider(Node node,
-                            MetricRegistry metricRegistry,
-                            @Named("elasticsearch_request_timeout") Duration requestTimeout) {
+    public EsClientProvider(Node node) {
         this.node = node;
-        this.metricRegistry = metricRegistry;
-        this.requestTimeout = requestTimeout;
     }
 
     @Override
     public Client get() {
-        return new GlobalTimeoutClient(node.client(),
-                                       requestTimeout.getQuantity(),
-                                       requestTimeout.getUnit(),
-                                       metricRegistry);
+        return node.client();
     }
 }
