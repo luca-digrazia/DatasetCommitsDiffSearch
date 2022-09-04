cd77809d80c894f944fee9fa7a9536b876ec4143
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:03:41 +0200

    Refining interfaces, adding implementations, adding tests.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/agents/AgentServiceImpl.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/agents/AgentServiceImpl.java
@@ -1,52 +1,26 @@
-/**
- * This file is part of Graylog.
- *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.agents;
 
 import com.google.common.collect.Lists;
-import com.google.common.primitives.Ints;
-import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.CollectionName;
 import org.graylog2.database.MongoConnection;
-import org.graylog2.rest.models.agent.requests.AgentRegistrationRequest;
-import org.joda.time.DateTime;
 import org.mongojack.DBCursor;
 import org.mongojack.DBQuery;
 import org.mongojack.JacksonDBCollection;
 import org.mongojack.WriteResult;
 
 import javax.inject.Inject;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class AgentServiceImpl implements AgentService {
 
     private final JacksonDBCollection<AgentImpl, String> coll;
-    private final Validator validator;
 
     @Inject
     public AgentServiceImpl(MongoConnection mongoConnection,
-                            MongoJackObjectMapperProvider mapperProvider,
-                            Validator validator) {
-        this.validator = validator;
+                            MongoJackObjectMapperProvider mapperProvider) {
         final String collectionName = AgentImpl.class.getAnnotation(CollectionName.class).value();
         final DBCollection dbCollection = mongoConnection.getDatabase().getCollection(collectionName);
         this.coll = JacksonDBCollection.wrap(dbCollection, AgentImpl.class, String.class, mapperProvider.get());
@@ -61,12 +35,9 @@ public class AgentServiceImpl implements AgentService {
     public Agent save(Agent agent) {
         if (agent instanceof AgentImpl) {
             final AgentImpl agentImpl = (AgentImpl)agent;
-            final Set<ConstraintViolation<AgentImpl>> violations = validator.validate(agentImpl);
-            if (violations.isEmpty()) {
-                return coll.findAndModify(DBQuery.is("id", agent.getId()), new BasicDBObject(), new BasicDBObject(), false, agentImpl, true, true);
-            } else {
-                throw new IllegalArgumentException("Specified object failed validation: " + violations);
-            }
+            final WriteResult<AgentImpl, String> result = coll.save(agentImpl);
+            final DBObject dbObject = result.getDbObject();
+            return result.getSavedObject();
         } else
             throw new IllegalArgumentException("Specified object is not of correct implementation type (" + agent.getClass() + ")!");
     }
@@ -89,22 +60,6 @@ public class AgentServiceImpl implements AgentService {
     @Override
     public int destroy(Agent agent) {
         return coll.remove(DBQuery.is("id", agent.getId())).getN();
-    }
-
-    @Override
-    public int destroyExpired(int time, TimeUnit unit) {
-        int count = 0;
-        final DateTime threshold = DateTime.now().minusSeconds(Ints.checkedCast(unit.toSeconds(time)));
-        for (Agent agent : all())
-            if (agent.getLastSeen().isBefore(threshold))
-                count += destroy(agent);
-
-        return count;
-    }
-
-    @Override
-    public Agent fromRequest(AgentRegistrationRequest request) {
-        return AgentImpl.create(request.id(), request.nodeId(), AgentNodeDetails.create(request.nodeDetails().operatingSystem()), DateTime.now());
     }
 
     private List<Agent> toAbstractListType(DBCursor<AgentImpl> agents) {
