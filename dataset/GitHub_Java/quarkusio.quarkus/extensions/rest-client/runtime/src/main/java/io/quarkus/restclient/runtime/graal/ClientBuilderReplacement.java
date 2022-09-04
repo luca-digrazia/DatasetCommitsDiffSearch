af21d17eca8261410c2ff8ccdfd6e642e47f517c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:54:49 +0200

    Transfer providers discovered at build-time to new ClientBuilder instances

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/rest-client/runtime/src/main/java/io/quarkus/restclient/runtime/graal/ClientBuilderReplacement.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/rest-client/runtime/src/main/java/io/quarkus/restclient/runtime/graal/ClientBuilderReplacement.java
@@ -2,8 +2,6 @@ package io.quarkus.restclient.runtime.graal;
 
 import javax.ws.rs.client.ClientBuilder;
 
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.engines.URLConnectionClientEngineBuilder;
 import org.jboss.resteasy.client.jaxrs.internal.LocalResteasyProviderFactory;
 import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 
@@ -17,14 +15,7 @@ final class ClientBuilderReplacement {
 
     @Substitute
     public static ClientBuilder newBuilder() {
-        ResteasyClientBuilder client = new ResteasyClientBuilderImpl();
-        client.providerFactory(new LocalResteasyProviderFactory(RestClientRecorder.providerFactory));
-        if (!RestClientRecorder.SSL_ENABLED) {
-            client.httpEngine(new URLConnectionClientEngineBuilder().resteasyClientBuilder(client).build());
-            client.sslContext(null);
-            client.trustStore(null);
-            client.keyStore(null, "");
-        }
-        return client;
+        return new ResteasyClientBuilderImpl()
+                .providerFactory(new LocalResteasyProviderFactory(RestClientRecorder.providerFactory));
     }
 }
