c202d6dd19a5700f21a6c7b77cc933277923cbe5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:11:21 +0200

    Make validation errors and exceptions more verbose
        Previously, ValidationExceptions wouldn't print the acutal validation errors which
        makes debugging or simply identifying issues harder than necessary.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/plugin/rest/ApiError.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/plugin/rest/ApiError.java
@@ -17,14 +17,20 @@
 package org.graylog2.plugin.rest;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.google.auto.value.AutoValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonAutoDetect
-@AutoValue
-@JsonTypeName("ApiError") // Explicitly indicates the class type to avoid AutoValue_ at the beginning
-public abstract class ApiError implements GenericError {
-    public static ApiError create(String message) {
-        return new AutoValue_ApiError(message);
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+public class ApiError {
+    private final String message;
+
+    public ApiError(String message) {
+        this.message = message;
+    }
+
+    @JsonProperty
+    public String getMessage() {
+        return message;
     }
 }
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/plugin/rest/ValidationApiError.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/plugin/rest/ValidationApiError.java
@@ -18,8 +18,6 @@ package org.graylog2.plugin.rest;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableMap;
 import org.graylog2.plugin.database.validators.ValidationResult;
 
@@ -27,13 +25,16 @@ import java.util.List;
 import java.util.Map;
 
 @JsonAutoDetect
-@AutoValue
-@JsonTypeName("ValidationApiError") // Explicitly indicates the class type to avoid AutoValue_ at the beginning
-public abstract class ValidationApiError implements GenericError {
-    @JsonProperty
-    public abstract Map<String, List<ValidationResult>> validationErrors();
+public class ValidationApiError extends ApiError {
+    private final Map<String, List<ValidationResult>> validationErrors;
 
-    public static ValidationApiError create(String message, Map<String, List<ValidationResult>> validationErrors) {
-        return new AutoValue_ValidationApiError(message, ImmutableMap.copyOf(validationErrors));
+    public ValidationApiError(String message, Map<String, List<ValidationResult>> validationErrors) {
+        super(message);
+        this.validationErrors = ImmutableMap.copyOf(validationErrors);
+    }
+
+    @JsonProperty
+    public Map<String, List<ValidationResult>> getValidationErrors() {
+        return validationErrors;
     }
 }
