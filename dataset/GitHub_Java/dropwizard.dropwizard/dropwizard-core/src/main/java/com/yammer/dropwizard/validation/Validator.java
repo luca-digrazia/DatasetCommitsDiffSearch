192527e7368f5e968ba3957356dccdcb3e08b58a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:50:18 +0200

    Check for null reference when validating objects
        Null references cannot be validated. Check for null and return
        an error message.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/validation/Validator.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/validation/Validator.java
@@ -9,6 +9,7 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Path;
 import javax.validation.Validation;
 import javax.validation.ValidatorFactory;
+import javax.validation.groups.Default;
 import java.util.Set;
 
 import static java.lang.String.format;
@@ -17,7 +18,15 @@ import static java.lang.String.format;
  * A simple façade for Hibernate Validator.
  */
 public class Validator {
-    private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    private final ValidatorFactory factory;
+
+    public Validator() {
+        this(Validation.buildDefaultValidatorFactory());
+    }
+
+    public Validator(ValidatorFactory factory) {
+        this.factory = factory;
+    }
 
     /**
      * Validates the given object, and returns a list of error messages, if any. If the returned
@@ -28,19 +37,37 @@ public class Validator {
      * @return a list of error messages, if any, regarding {@code o}'s validity
      */
     public <T> ImmutableList<String> validate(T o) {
+        return validate(o, Default.class);
+    }
+
+   /**
+    * Validates the given object, and returns a list of error messages, if any. If the returned
+    * list is empty, the object is valid.
+    * @param o a potentially-valid object
+    * @param groups group or list of groups targeted for validation (default to {@link javax.validation.groups.Default})
+    * @param <T> the type of object to validate
+    * @return a list of error messages, if any, regarding {@code o}'s validity
+    */
+    public <T> ImmutableList<String> validate(T o, Class<?>... groups) {
         final Set<String> errors = Sets.newHashSet();
-        final Set<ConstraintViolation<T>> violations = factory.getValidator().validate(o);
-        for (ConstraintViolation<T> v : violations) {
-            if (v.getConstraintDescriptor().getAnnotation() instanceof ValidationMethod) {
-                final ImmutableList<Path.Node> nodes = ImmutableList.copyOf(v.getPropertyPath());
-                final ImmutableList<Path.Node> usefulNodes = nodes.subList(0, nodes.size() - 1);
-                final String msg = v.getMessage().startsWith(".") ? "%s%s" : "%s %s";
-                errors.add(format(msg, Joiner.on('.').join(usefulNodes), v.getMessage()).trim());
-            } else {
-                errors.add(format("%s %s (was %s)",
-                                  v.getPropertyPath(),
-                                  v.getMessage(),
-                                  v.getInvalidValue()));
+
+        if (o == null) {
+            errors.add("request entity required");
+        }
+        else {
+            final Set<ConstraintViolation<T>> violations = factory.getValidator().validate(o,groups);
+            for (ConstraintViolation<T> v : violations) {
+                if (v.getConstraintDescriptor().getAnnotation() instanceof ValidationMethod) {
+                    final ImmutableList<Path.Node> nodes = ImmutableList.copyOf(v.getPropertyPath());
+                    final ImmutableList<Path.Node> usefulNodes = nodes.subList(0, nodes.size() - 1);
+                    final String msg = v.getMessage().startsWith(".") ? "%s%s" : "%s %s";
+                    errors.add(format(msg, Joiner.on('.').join(usefulNodes), v.getMessage()).trim());
+                } else {
+                    errors.add(format("%s %s (was %s)",
+                                      v.getPropertyPath(),
+                                      v.getMessage(),
+                                      v.getInvalidValue()));
+                }
             }
         }
         return ImmutableList.copyOf(Ordering.natural().sortedCopy(errors));
--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/validation/tests/ValidatorTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/validation/tests/ValidatorTest.java
@@ -1,16 +1,16 @@
 package com.yammer.dropwizard.validation.tests;
 
-import com.google.common.collect.ImmutableList;
 import com.yammer.dropwizard.validation.Validator;
 import org.junit.Test;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
+import java.util.Locale;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ValidatorTest {
+    @SuppressWarnings("unused")
     public static class Example {
         @NotNull
         private String notNull = null;
@@ -31,9 +31,17 @@ public class ValidatorTest {
 
     @Test
     public void returnsASetOfErrorsForAnObject() throws Exception {
-        assertThat(validator.validate(new Example()),
-                   is(ImmutableList.of("notNull may not be null (was null)",
-                                       "tooBig must be less than or equal to 30 (was 50)")));
+        if ("en".equals(Locale.getDefault().getLanguage())) {
+            assertThat(validator.validate(new Example()))
+                    .containsOnly("notNull may not be null (was null)",
+                                  "tooBig must be less than or equal to 30 (was 50)");
+        }
+    }
+
+    @Test
+    public void returnsASetOfErrorsForNull() throws Exception {
+        assertThat(validator.validate(null))
+                .containsOnly("request entity required");
     }
 
     @Test
@@ -42,7 +50,7 @@ public class ValidatorTest {
         example.setNotNull("woo");
         example.setTooBig(20);
 
-        assertThat(validator.validate(example),
-                   is(ImmutableList.<String>of()));
+        assertThat(validator.validate(example))
+                .isEmpty();
     }
 }
