4de71d7c963a82c217718aba356b6d5d617e6ad9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:02:29 +0200

    Allow for non-app routes without a redirect

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/micrometer/deployment/src/main/java/io/quarkus/micrometer/deployment/export/JsonRegistryProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/micrometer/deployment/src/main/java/io/quarkus/micrometer/deployment/export/JsonRegistryProcessor.java
@@ -9,6 +9,7 @@ import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.micrometer.deployment.MicrometerRegistryProviderBuildItem;
 import io.quarkus.micrometer.runtime.config.MicrometerConfig;
 import io.quarkus.micrometer.runtime.export.JsonMeterRegistryProvider;
@@ -34,7 +35,8 @@ public class JsonRegistryProcessor {
             BuildProducer<MicrometerRegistryProviderBuildItem> registryProviders,
             BuildProducer<RouteBuildItem> routes,
             BuildProducer<AdditionalBeanBuildItem> additionalBeans,
-            JsonRecorder recorder) {
+            JsonRecorder recorder,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClasses) {
         additionalBeans.produce(AdditionalBeanBuildItem.builder()
                 .addBeanClass(JsonMeterRegistryProvider.class)
                 .setUnremovable().build());
@@ -44,6 +46,11 @@ public class JsonRegistryProcessor {
                 .handler(recorder.getHandler())
                 .nonApplicationRoute(true)
                 .build());
+        reflectiveClasses.produce(ReflectiveClassBuildItem
+                .builder("org.HdrHistogram.Histogram",
+                        "org.HdrHistogram.DoubleHistogram",
+                        "org.HdrHistogram.ConcurrentHistogram")
+                .constructors(true).build());
         log.debug("Initialized a JSON meter registry on path=" + config.export.json.path);
     }
 
--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/RouteBuildItem.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/RouteBuildItem.java
@@ -1,11 +1,8 @@
 package io.quarkus.vertx.http.deployment;
 
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 import io.quarkus.builder.item.MultiBuildItem;
-import io.quarkus.vertx.http.deployment.devmode.NotFoundPageDisplayableEndpointBuildItem;
-import io.quarkus.vertx.http.deployment.devmode.console.ConfiguredPathInfo;
 import io.quarkus.vertx.http.runtime.BasicRoute;
 import io.quarkus.vertx.http.runtime.HandlerType;
 import io.vertx.core.Handler;
@@ -15,17 +12,11 @@ import io.vertx.ext.web.RoutingContext;
 
 public final class RouteBuildItem extends MultiBuildItem {
 
-    public static Builder builder() {
-        return new Builder();
-    }
-
     private final Function<Router, Route> routeFunction;
     private final Handler<RoutingContext> handler;
     private final HandlerType type;
-    private final RouteType routeType;
+    private final boolean frameworkRoute;
     private final boolean requiresLegacyRedirect;
-    private final NotFoundPageDisplayableEndpointBuildItem notFoundPageDisplayableEndpoint;
-    private final ConfiguredPathInfo devConsoleResolvedPathBuildItem;
 
     /**
      * @deprecated Use the Builder instead.
@@ -35,10 +26,8 @@ public final class RouteBuildItem extends MultiBuildItem {
         this.routeFunction = routeFunction;
         this.handler = handler;
         this.type = type;
-        this.routeType = RouteType.APPLICATION_ROUTE;
+        this.frameworkRoute = false;
         this.requiresLegacyRedirect = false;
-        this.notFoundPageDisplayableEndpoint = null;
-        this.devConsoleResolvedPathBuildItem = null;
     }
 
     /**
@@ -81,14 +70,12 @@ public final class RouteBuildItem extends MultiBuildItem {
         this(new BasicRoute(route), handler);
     }
 
-    RouteBuildItem(Builder builder, RouteType routeType, boolean requiresLegacyRedirect) {
+    private RouteBuildItem(Builder builder) {
         this.routeFunction = builder.routeFunction;
         this.handler = builder.handler;
         this.type = builder.type;
-        this.routeType = routeType;
-        this.requiresLegacyRedirect = requiresLegacyRedirect;
-        this.notFoundPageDisplayableEndpoint = builder.getNotFoundEndpoint();
-        this.devConsoleResolvedPathBuildItem = builder.getRouteConfigInfo();
+        this.frameworkRoute = builder.frameworkRoute;
+        this.requiresLegacyRedirect = builder.requiresLegacyRedirect;
     }
 
     public Handler<RoutingContext> getHandler() {
@@ -104,93 +91,27 @@ public final class RouteBuildItem extends MultiBuildItem {
     }
 
     public boolean isFrameworkRoute() {
-        return routeType.equals(RouteType.FRAMEWORK_ROUTE);
-    }
-
-    public boolean isApplicationRoute() {
-        return routeType.equals(RouteType.APPLICATION_ROUTE);
-    }
-
-    public boolean isAbsoluteRoute() {
-        return routeType.equals(RouteType.ABSOLUTE_ROUTE);
+        return frameworkRoute;
     }
 
     public boolean isRequiresLegacyRedirect() {
         return requiresLegacyRedirect;
     }
 
-    public NotFoundPageDisplayableEndpointBuildItem getNotFoundPageDisplayableEndpoint() {
-        return notFoundPageDisplayableEndpoint;
-    }
-
-    public ConfiguredPathInfo getDevConsoleResolvedPath() {
-        return devConsoleResolvedPathBuildItem;
-    }
-
-    public enum RouteType {
-        FRAMEWORK_ROUTE,
-        APPLICATION_ROUTE,
-        ABSOLUTE_ROUTE
-    }
-
-    /**
-     * HttpRootPathBuildItem.Builder and NonApplicationRootPathBuildItem.Builder extend this.
-     * Please verify the extended builders behavior when changing this one.
-     */
     public static class Builder {
-        protected Function<Router, Route> routeFunction;
-        protected Handler<RoutingContext> handler;
-        protected HandlerType type = HandlerType.NORMAL;
-        protected boolean displayOnNotFoundPage;
-        protected String notFoundPageTitle;
-        protected String notFoundPagePath;
-        protected String routePath;
-        protected String routeConfigKey;
-        protected String absolutePath;
+        private Function<Router, Route> routeFunction;
+        private Handler<RoutingContext> handler;
+        private HandlerType type = HandlerType.NORMAL;
+        private boolean frameworkRoute = false;
+        private boolean requiresLegacyRedirect = false;
 
-        /**
-         * Use HttpRootPathBuildItem and NonApplicationRootPathBuildItem to
-         * ensure paths are constructed/normalized correctly
-         *
-         * @deprecated
-         * @see HttpRootPathBuildItem#routeBuilder()
-         * @see NonApplicationRootPathBuildItem#routeBuilder()
-         */
-        @Deprecated
-        public Builder() {
-        }
-
-        /**
-         * {@link #routeFunction(String, Consumer)} should be used instead
-         *
-         * @param routeFunction
-         * @see #routeFunction(String, Consumer)
-         */
-        @Deprecated
         public Builder routeFunction(Function<Router, Route> routeFunction) {
             this.routeFunction = routeFunction;
             return this;
         }
 
-        /**
-         * @param path A normalized path (e.g. use HttpRootPathBuildItem to construct/resolve the path value) defining
-         *        the route. This path this is also used on the "Not Found" page in dev mode.
-         * @param routeFunction a Consumer of Route
-         */
-        public Builder routeFunction(String path, Consumer<Route> routeFunction) {
-            this.routeFunction = new BasicRoute(path, null, routeFunction);
-            this.notFoundPagePath = this.routePath = path;
-            return this;
-        }
-
-        /**
-         * @param route A normalized path used to define a basic route
-         *        (e.g. use HttpRootPathBuildItem to construct/resolve the path value). This path this is also
-         *        used on the "Not Found" page in dev mode.
-         */
         public Builder route(String route) {
             this.routeFunction = new BasicRoute(route);
-            this.notFoundPagePath = this.routePath = route;
             return this;
         }
 
@@ -214,65 +135,20 @@ public final class RouteBuildItem extends MultiBuildItem {
             return this;
         }
 
-        public Builder displayOnNotFoundPage() {
-            this.displayOnNotFoundPage = true;
+        public Builder nonApplicationRoute() {
+            this.frameworkRoute = true;
+            this.requiresLegacyRedirect = true;
             return this;
         }
 
-        public Builder displayOnNotFoundPage(String notFoundPageTitle) {
-            this.displayOnNotFoundPage = true;
-            this.notFoundPageTitle = notFoundPageTitle;
-            return this;
-        }
-
-        /**
-         * @deprecated Specify the path as part of defining the route
-         * @see #route(String)
-         * @see #routeFunction(String, Consumer)
-         */
-        @Deprecated
-        public Builder displayOnNotFoundPage(String notFoundPageTitle, String notFoundPagePath) {
-            this.displayOnNotFoundPage = true;
-            this.notFoundPageTitle = notFoundPageTitle;
-            this.notFoundPagePath = notFoundPagePath;
-            return this;
-        }
-
-        public Builder routeConfigKey(String attributeName) {
-            this.routeConfigKey = attributeName;
+        public Builder nonApplicationRoute(boolean requiresLegacyRedirect) {
+            this.frameworkRoute = true;
+            this.requiresLegacyRedirect = requiresLegacyRedirect;
             return this;
         }
 
         public RouteBuildItem build() {
-            return new RouteBuildItem(this, RouteType.APPLICATION_ROUTE, false);
-        }
-
-        protected ConfiguredPathInfo getRouteConfigInfo() {
-            if (routeConfigKey == null) {
-                return null;
-            }
-            if (routePath == null) {
-                throw new RuntimeException("Cannot discover value of " + routeConfigKey
-                        + " as no explicit path was specified and a route function is in use");
-            }
-            if (absolutePath != null) {
-                return new ConfiguredPathInfo(routeConfigKey, absolutePath, true);
-            }
-            return new ConfiguredPathInfo(routeConfigKey, routePath, false);
-        }
-
-        protected NotFoundPageDisplayableEndpointBuildItem getNotFoundEndpoint() {
-            if (!displayOnNotFoundPage) {
-                return null;
-            }
-            if (notFoundPagePath == null) {
-                throw new RuntimeException("Cannot display " + routeFunction
-                        + " on not found page as no explicit path was specified and a route function is in use");
-            }
-            if (absolutePath != null) {
-                return new NotFoundPageDisplayableEndpointBuildItem(absolutePath, notFoundPageTitle, true);
-            }
-            return new NotFoundPageDisplayableEndpointBuildItem(notFoundPagePath, notFoundPageTitle, false);
+            return new RouteBuildItem(this);
         }
     }
 }
