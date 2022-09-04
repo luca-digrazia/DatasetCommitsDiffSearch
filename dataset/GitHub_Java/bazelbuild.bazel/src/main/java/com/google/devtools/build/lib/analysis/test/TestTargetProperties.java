0b75facdce3ff580d7d363483afe9e31c3f55f0c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 17:09:02 +0200

    TestTargetProperties: fix typo in cpu reservation
        This is just a comment fix, but as I was looking through the code
        to see how this tag is implemented, the difference between this
        comment and the [documented syntax](https://docs.bazel.build/versions/master/test-encyclopedia.html#other-resources)
        confused me.
    
        Closes #12243.
    
        PiperOrigin-RevId: 337231628

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/test/TestTargetProperties.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/test/TestTargetProperties.java
@@ -20,7 +20,6 @@ import com.google.common.collect.Maps;
 import com.google.devtools.build.lib.actions.ExecutionRequirements;
 import com.google.devtools.build.lib.actions.ExecutionRequirements.ParseableRequirement.ValidationException;
 import com.google.devtools.build.lib.actions.ResourceSet;
-import com.google.devtools.build.lib.actions.Spawn;
 import com.google.devtools.build.lib.actions.UserExecException;
 import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.cmdline.Label;
@@ -28,7 +27,10 @@ import com.google.devtools.build.lib.packages.Rule;
 import com.google.devtools.build.lib.packages.TargetUtils;
 import com.google.devtools.build.lib.packages.TestSize;
 import com.google.devtools.build.lib.packages.TestTimeout;
-import com.google.devtools.build.lib.syntax.Type;
+import com.google.devtools.build.lib.packages.Type;
+import com.google.devtools.build.lib.server.FailureDetails.FailureDetail;
+import com.google.devtools.build.lib.server.FailureDetails.TestAction;
+import com.google.devtools.build.lib.server.FailureDetails.TestAction.Code;
 import java.util.List;
 import java.util.Map;
 
@@ -44,10 +46,10 @@ public class TestTargetProperties {
    * <p>When changing these values, remember to update the documentation at
    * attributes/test/size.html.
    */
-  private static final ResourceSet SMALL_RESOURCES = ResourceSet.create(20, 0.9, 0.00, 1);
-  private static final ResourceSet MEDIUM_RESOURCES = ResourceSet.create(100, 0.9, 0.1, 1);
-  private static final ResourceSet LARGE_RESOURCES = ResourceSet.create(300, 0.8, 0.1, 1);
-  private static final ResourceSet ENORMOUS_RESOURCES = ResourceSet.create(800, 0.7, 0.4, 1);
+  private static final ResourceSet SMALL_RESOURCES = ResourceSet.create(20, 1, 1);
+  private static final ResourceSet MEDIUM_RESOURCES = ResourceSet.create(100, 1, 1);
+  private static final ResourceSet LARGE_RESOURCES = ResourceSet.create(300, 1, 1);
+  private static final ResourceSet ENORMOUS_RESOURCES = ResourceSet.create(800, 1, 1);
   private static final ResourceSet LOCAL_TEST_JOBS_BASED_RESOURCES =
       ResourceSet.createWithLocalTestCount(1);
 
@@ -63,48 +65,66 @@ public class TestTargetProperties {
   private final TestSize size;
   private final TestTimeout timeout;
   private final List<String> tags;
-  private final boolean isLocal;
+  private final boolean isRemotable;
   private final boolean isFlaky;
   private final boolean isExternal;
   private final String language;
   private final ImmutableMap<String, String> executionInfo;
+  private final boolean isPersistentTestRunner;
 
   /**
-   * Creates test target properties instance. Constructor expects that it
-   * will be called only for test configured targets.
+   * Creates test target properties instance. Constructor expects that it will be called only for
+   * test configured targets.
    */
-  TestTargetProperties(RuleContext ruleContext,
-      ExecutionInfo executionRequirements) {
+  TestTargetProperties(
+      RuleContext ruleContext,
+      ExecutionInfo executionRequirements,
+      boolean isPersistentTestRunner) {
     Rule rule = ruleContext.getRule();
 
     Preconditions.checkState(TargetUtils.isTestRule(rule));
     size = TestSize.getTestSize(rule);
     timeout = TestTimeout.getTestTimeout(rule);
     tags = ruleContext.attributes().get("tags", Type.STRING_LIST);
-    boolean isTaggedLocal = TargetUtils.isLocalTestRule(rule)
-        || TargetUtils.isExclusiveTestRule(rule);
 
     // We need to use method on ruleConfiguredTarget to perform validation.
     isFlaky = ruleContext.attributes().get("flaky", Type.BOOLEAN);
     isExternal = TargetUtils.isExternalTestRule(rule);
+    this.isPersistentTestRunner = isPersistentTestRunner;
 
     Map<String, String> executionInfo = Maps.newLinkedHashMap();
     executionInfo.putAll(TargetUtils.getExecutionInfo(rule));
-    if (isTaggedLocal) {
-      executionInfo.put("local", "");
+
+    boolean incompatibleExclusiveTestSandboxed = false;
+
+    TestConfiguration testConfiguration = ruleContext.getFragment(TestConfiguration.class);
+    if (testConfiguration != null) {
+      incompatibleExclusiveTestSandboxed = testConfiguration.incompatibleExclusiveTestSandboxed();
     }
 
-    boolean isRequestedLocalByProvider = false;
+    if (incompatibleExclusiveTestSandboxed) {
+      if (TargetUtils.isLocalTestRule(rule)) {
+        executionInfo.put(ExecutionRequirements.LOCAL, "");
+      } else if (TargetUtils.isExclusiveTestRule(rule)) {
+        executionInfo.put(ExecutionRequirements.NO_REMOTE_EXEC, "");
+      }
+    } else {
+      if (TargetUtils.isLocalTestRule(rule) || TargetUtils.isExclusiveTestRule(rule)) {
+        executionInfo.put(ExecutionRequirements.LOCAL, "");
+      }
+    }
+
     if (executionRequirements != null) {
       // This will overwrite whatever TargetUtils put there, which might be confusing.
       executionInfo.putAll(executionRequirements.getExecutionInfo());
-
-      // We also need to mark it as local if the execution requirements specifies it.
-      isRequestedLocalByProvider = executionRequirements.getExecutionInfo().containsKey("local");
     }
+    ruleContext.getConfiguration().modifyExecutionInfo(executionInfo, TestRunnerAction.MNEMONIC);
     this.executionInfo = ImmutableMap.copyOf(executionInfo);
 
-    isLocal = isTaggedLocal || isRequestedLocalByProvider;
+    isRemotable =
+        !executionInfo.containsKey(ExecutionRequirements.LOCAL)
+            && !executionInfo.containsKey(ExecutionRequirements.NO_REMOTE)
+            && !executionInfo.containsKey(ExecutionRequirements.NO_REMOTE_EXEC);
 
     language = TargetUtils.getRuleLanguage(rule);
   }
@@ -121,8 +141,8 @@ public class TestTargetProperties {
     return tags;
   }
 
-  public boolean isLocal() {
-    return isLocal;
+  public boolean isRemotable() {
+    return isRemotable;
   }
 
   public boolean isFlaky() {
@@ -133,6 +153,10 @@ public class TestTargetProperties {
     return isExternal;
   }
 
+  public boolean isPersistentTestRunner() {
+    return isPersistentTestRunner;
+  }
+
   public ResourceSet getLocalResourceUsage(Label label, boolean usingLocalTestJobs)
       throws UserExecException {
     if (usingLocalTestJobs) {
@@ -141,41 +165,50 @@ public class TestTargetProperties {
 
     ResourceSet testResourcesFromSize = TestTargetProperties.getResourceSetFromSize(size);
 
-    // Tests can override their CPU reservation with a "cpus:<n>" tag.
+    // Tests can override their CPU reservation with a "cpu:<n>" tag.
     ResourceSet testResourcesFromTag = null;
     for (String tag : executionInfo.keySet()) {
       try {
         String cpus = ExecutionRequirements.CPU.parseIfMatches(tag);
         if (cpus != null) {
           if (testResourcesFromTag != null) {
-            throw new UserExecException(
+            String message =
                 String.format(
                     "%s has more than one '%s' tag, but duplicate tags aren't allowed",
-                    label, ExecutionRequirements.CPU.userFriendlyName()));
+                    label, ExecutionRequirements.CPU.userFriendlyName());
+            throw new UserExecException(createFailureDetail(message, Code.DUPLICATE_CPU_TAGS));
           }
           testResourcesFromTag =
               ResourceSet.create(
                   testResourcesFromSize.getMemoryMb(),
                   Float.parseFloat(cpus),
-                  testResourcesFromSize.getIoUsage(),
                   testResourcesFromSize.getLocalTestCount());
         }
       } catch (ValidationException e) {
-        throw new UserExecException(
+        String message =
             String.format(
                 "%s has a '%s' tag, but its value '%s' didn't pass validation: %s",
                 label,
                 ExecutionRequirements.CPU.userFriendlyName(),
                 e.getTagValue(),
-                e.getMessage()));
+                e.getMessage());
+        throw new UserExecException(createFailureDetail(message, Code.INVALID_CPU_TAG));
       }
     }
 
     return testResourcesFromTag != null ? testResourcesFromTag : testResourcesFromSize;
   }
 
+  private static FailureDetail createFailureDetail(String message, Code detailedCode) {
+    return FailureDetail.newBuilder()
+        .setMessage(message)
+        .setTestAction(TestAction.newBuilder().setCode(detailedCode))
+        .build();
+  }
+
   /**
-   * Returns a map of execution info. See {@link Spawn#getExecutionInfo}.
+   * Returns a map of execution info. See {@link
+   * com.google.devtools.build.lib.actions.Spawn#getExecutionInfo}.
    */
   public ImmutableMap<String, String> getExecutionInfo() {
     return executionInfo;
