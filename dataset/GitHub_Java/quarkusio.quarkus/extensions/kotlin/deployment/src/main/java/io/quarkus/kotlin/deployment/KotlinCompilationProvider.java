60cbd84c3bfef7a9953d1b0d737dc42aafb3608b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:31:16 +0200

    Show errors even when Kotlin compiler could not be invoked
        Relates to: #8708

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/kotlin/deployment/src/main/java/io/quarkus/kotlin/deployment/KotlinCompilationProvider.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/kotlin/deployment/src/main/java/io/quarkus/kotlin/deployment/KotlinCompilationProvider.java
@@ -1,11 +1,16 @@
 package io.quarkus.kotlin.deployment;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import org.jboss.logging.Logger;
 import org.jetbrains.kotlin.cli.common.ExitCode;
 import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments;
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation;
@@ -14,18 +19,50 @@ import org.jetbrains.kotlin.cli.common.messages.MessageCollector;
 import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler;
 import org.jetbrains.kotlin.config.Services;
 
-import io.quarkus.dev.CompilationProvider;
+import io.quarkus.deployment.dev.CompilationProvider;
 
 public class KotlinCompilationProvider implements CompilationProvider {
 
+    private static final Logger log = Logger.getLogger(KotlinCompilationProvider.class);
+
+    // see: https://github.com/JetBrains/kotlin/blob/v1.3.71/libraries/tools/kotlin-maven-plugin/src/main/java/org/jetbrains/kotlin/maven/KotlinCompileMojoBase.java#L181
+    private final static Pattern OPTION_PATTERN = Pattern.compile("([^:]+):([^=]+)=(.*)");
+    private static final String KOTLIN_PACKAGE = "org.jetbrains.kotlin";
+
     @Override
-    public String handledExtension() {
-        return ".kt";
+    public Set<String> handledExtensions() {
+        return Collections.singleton(".kt");
     }
 
     @Override
     public void compile(Set<File> filesToCompile, Context context) {
         K2JVMCompilerArguments compilerArguments = new K2JVMCompilerArguments();
+        compilerArguments.setJvmTarget(context.getTargetJvmVersion());
+        compilerArguments.setJavaParameters(true);
+        if (context.getCompilePluginArtifacts() != null && !context.getCompilePluginArtifacts().isEmpty()) {
+            compilerArguments.setPluginClasspaths(context.getCompilePluginArtifacts().toArray(new String[0]));
+        }
+        if (context.getCompilerPluginOptions() != null && !context.getCompilerPluginOptions().isEmpty()) {
+            List<String> sanitizedOptions = new ArrayList<>(context.getCompilerOptions().size());
+            for (String rawOption : context.getCompilerPluginOptions()) {
+                Matcher matcher = OPTION_PATTERN.matcher(rawOption);
+                if (!matcher.matches()) {
+                    log.warn("Kotlin compiler plugin option " + rawOption + " is invalid");
+                }
+
+                String pluginId = matcher.group(1);
+                if (!pluginId.contains(".")) {
+                    // convert the plugin name to the plugin id by simply removing the dash and adding the kotlin package
+                    // this seems to be the appropriate way of doing things for the plugins that were checked
+                    pluginId = KOTLIN_PACKAGE + "." + pluginId.replace("-", "");
+                }
+                String key = matcher.group(2);
+                String value = matcher.group(3);
+                sanitizedOptions.add("plugin:" + pluginId + ":" + key + "=" + value);
+
+                compilerArguments.setPluginOptions(sanitizedOptions.toArray(new String[0]));
+            }
+        }
         compilerArguments.setClasspath(
                 context.getClasspath().stream().map(File::getAbsolutePath).collect(Collectors.joining(File.pathSeparator)));
         compilerArguments.setDestination(context.getOutputDirectory().getAbsolutePath());
@@ -38,12 +75,18 @@ public class KotlinCompilationProvider implements CompilationProvider {
                 compilerArguments);
 
         if (exitCode != ExitCode.OK && exitCode != ExitCode.COMPILATION_ERROR) {
-            throw new RuntimeException("Unable to invoke Kotlin compiler");
+            throw new RuntimeException("Unable to invoke Kotlin compiler. " + String.join("\n", messageCollector.getErrors()));
         }
 
         if (messageCollector.hasErrors()) {
-            throw new RuntimeException("Compilation failed" + String.join("\n", messageCollector.getErrors()));
+            throw new RuntimeException("Compilation failed. " + String.join("\n", messageCollector.getErrors()));
         }
+    }
+
+    @Override
+    public Path getSourcePath(Path classFilePath, Set<String> sourcePaths, String classesPath) {
+        // return same class so it is not removed
+        return classFilePath;
     }
 
     private static class SimpleKotlinCompilerMessageCollector implements MessageCollector {
@@ -63,8 +106,9 @@ public class KotlinCompilationProvider implements CompilationProvider {
         public void report(CompilerMessageSeverity severity, String s, CompilerMessageLocation location) {
             if (severity.isError()) {
                 if ((location != null) && (location.getLineContent() != null)) {
-                    errors.add(String.format("%s\n%s:%d:%d", location.getLineContent(), location.getPath(), location.getLine(),
-                            location.getColumn()));
+                    errors.add(String.format("%s%n%s:%d:%d%nReason: %s", location.getLineContent(), location.getPath(),
+                            location.getLine(),
+                            location.getColumn(), s));
                 } else {
                     errors.add(s);
                 }
