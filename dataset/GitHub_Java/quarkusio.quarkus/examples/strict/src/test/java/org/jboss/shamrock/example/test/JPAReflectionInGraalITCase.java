22585cdea219481c3ffe51b1a838874b73b71016
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:21:45 +0200

    [jpa] Add comprehensive test for JPA Entity reflection
        Make sure the test is run in the native image and called from a servlet
        endpoint

--- dataset/GitHub_Java/quarkusio.quarkus/examples/strict/src/test/java/org/jboss/shamrock/example/test/JPAReflectionInGraalITCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/examples/strict/src/test/java/org/jboss/shamrock/example/test/JPAReflectionInGraalITCase.java
@@ -1,11 +1,19 @@
 package org.jboss.shamrock.example.test;
 
-import static org.junit.Assert.assertEquals;
-
-import org.jboss.shamrock.example.testutils.URLTester;
 import org.jboss.shamrock.junit.GraalTest;
+import org.jboss.shamrock.junit.ShamrockTest;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLConnection;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test reflection around JPA entities
@@ -17,7 +25,15 @@ public class JPAReflectionInGraalITCase {
 
     @Test
     public void testFieldAndGetterReflectionOnEntityFromServlet() throws Exception {
-        assertEquals("OK", URLTester.relative("jpa/test").invokeURL().asString());
+        URL uri = new URL("http://localhost:8080/jpa/test");
+        URLConnection connection = uri.openConnection();
+        InputStream in = connection.getInputStream();
+        byte[] buf = new byte[100];
+        int r;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        while ((r = in.read(buf)) > 0) {
+            out.write(buf, 0, r);
+        }
+        assertEquals("OK", new String(out.toByteArray()));
     }
-
 }
