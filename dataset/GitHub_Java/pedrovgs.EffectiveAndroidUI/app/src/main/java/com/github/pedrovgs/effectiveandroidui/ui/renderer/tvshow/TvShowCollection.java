9a5de63ed31b3d0a4be1a3b816aa1d76a800f348
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:31:25 +0200

    Fix bug related with addAll method in TvShowCollection

--- dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/ui/renderer/tvshow/TvShowCollection.java
+++ dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/ui/renderer/tvshow/TvShowCollection.java
@@ -1,26 +1,47 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.pedrovgs.effectiveandroidui.ui.renderer.tvshow;
 
 import com.github.pedrovgs.effectiveandroidui.domain.tvshow.TvShow;
 import com.pedrogomez.renderers.AdapteeCollection;
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * AdapteeCollection implementation for TvShow.
  *
+ * If you want to lear more about how to use Renderers take a look to this project:
+ * https://github.com/pedrovgs/Renderers.
+ *
  * @author Pedro Vicente Gómez Sánchez
  */
-public class TvShowCollection implements AdapteeCollection<TvShow> {
+public class TvShowCollection implements AdapteeCollection<TvShow>, Serializable {
+  private static final long serialVersionUID = 8799677673292716638L;
 
   private final List<TvShow> tvShows;
 
   public TvShowCollection() {
-    this(new ArrayList<TvShow>());
+    this(new LinkedList<TvShow>());
   }
 
-  public TvShowCollection(List<TvShow> tvShows) {
-    this.tvShows = tvShows;
+  public TvShowCollection(Collection<TvShow> tvShows) {
+    this.tvShows = new LinkedList<TvShow>();
+    this.tvShows.addAll(tvShows);
   }
 
   @Override public int size() {
@@ -44,10 +65,14 @@ public class TvShowCollection implements AdapteeCollection<TvShow> {
   }
 
   @Override public void removeAll(Collection<TvShow> tvShows) {
-    this.tvShows.addAll(tvShows);
+    this.tvShows.removeAll(tvShows);
   }
 
   public void clear() {
     tvShows.clear();
+  }
+
+  public List<TvShow> getAsList() {
+    return (List<TvShow>) ((LinkedList<TvShow>) tvShows).clone();
   }
 }
