5d51c587184131c6a2e1374963e281cfcec75081
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:52:57 +0200

    Provide actionable error message when Jackson can't serialize in native-mode

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/resteasy-reactive/quarkus-resteasy-reactive-jackson/deployment/src/main/java/io/quarkus/resteasy/reactive/jackson/deployment/processor/ResteasyReactiveJacksonProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/resteasy-reactive/quarkus-resteasy-reactive-jackson/deployment/src/main/java/io/quarkus/resteasy/reactive/jackson/deployment/processor/ResteasyReactiveJacksonProcessor.java
@@ -1,41 +1,99 @@
 package io.quarkus.resteasy.reactive.jackson.deployment.processor;
 
 import java.util.Collections;
+import java.util.List;
 
+import javax.ws.rs.Priorities;
 import javax.ws.rs.core.MediaType;
+
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.JacksonMessageBodyReader;
-import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.JacksonMessageBodyWriter;
+import io.quarkus.resteasy.reactive.common.deployment.ServerDefaultProducesHandlerBuildItem;
+import io.quarkus.resteasy.reactive.jackson.runtime.mappers.DefaultMismatchedInputException;
+import io.quarkus.resteasy.reactive.jackson.runtime.mappers.NativeInvalidDefinitionExceptionMapper;
+import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.BasicServerJacksonMessageBodyWriter;
+import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.FullyFeaturedServerJacksonMessageBodyWriter;
+import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.ServerJacksonMessageBodyReader;
+import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.vertx.VertxJsonArrayMessageBodyReader;
+import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.vertx.VertxJsonArrayMessageBodyWriter;
+import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.vertx.VertxJsonObjectMessageBodyReader;
+import io.quarkus.resteasy.reactive.jackson.runtime.serialisers.vertx.VertxJsonObjectMessageBodyWriter;
+import io.quarkus.resteasy.reactive.spi.CustomExceptionMapperBuildItem;
+import io.quarkus.resteasy.reactive.spi.ExceptionMapperBuildItem;
 import io.quarkus.resteasy.reactive.spi.MessageBodyReaderBuildItem;
 import io.quarkus.resteasy.reactive.spi.MessageBodyWriterBuildItem;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
 public class ResteasyReactiveJacksonProcessor {
 
     @BuildStep
     void feature(BuildProducer<FeatureBuildItem> feature) {
-        feature.produce(new FeatureBuildItem(Feature.QUARKUS_REST_JACKSON));
+        feature.produce(new FeatureBuildItem(Feature.RESTEASY_REACTIVE_JACKSON));
     }
 
     @BuildStep
-    void additionalProviders(BuildProducer<AdditionalBeanBuildItem> additionalBean,
+    ServerDefaultProducesHandlerBuildItem jsonDefault() {
+        return ServerDefaultProducesHandlerBuildItem.json();
+    }
+
+    @BuildStep
+    ResteasyReactiveJacksonProviderDefinedBuildItem jacksonRegistered() {
+        return new ResteasyReactiveJacksonProviderDefinedBuildItem();
+    }
+
+    @BuildStep
+    void additionalProviders(List<JacksonFeatureBuildItem> jacksonFeatureBuildItems,
+            BuildProducer<AdditionalBeanBuildItem> additionalBean,
             BuildProducer<MessageBodyReaderBuildItem> additionalReaders,
-            BuildProducer<MessageBodyWriterBuildItem> additionalWriters) {
-        // make these beans to they can get instantiated with the Quarkus CDI configured Jsonb object
+            BuildProducer<MessageBodyWriterBuildItem> additionalWriters,
+            BuildProducer<ExceptionMapperBuildItem> exceptionMappers,
+            BuildProducer<CustomExceptionMapperBuildItem> customExceptionMapper) {
+        boolean applicationNeedsSpecialJacksonFeatures = jacksonFeatureBuildItems.isEmpty();
+        // make these beans to they can get instantiated with the Quarkus CDI configured ObjectMapper object
         additionalBean.produce(AdditionalBeanBuildItem.builder()
-                .addBeanClass(JacksonMessageBodyReader.class.getName())
-                .addBeanClass(JacksonMessageBodyWriter.class.getName())
+                .addBeanClass(ServerJacksonMessageBodyReader.class.getName())
+                .addBeanClass(getJacksonMessageBodyWriter(applicationNeedsSpecialJacksonFeatures))
                 .setUnremovable().build());
 
         additionalReaders
-                .produce(new MessageBodyReaderBuildItem(JacksonMessageBodyReader.class.getName(), Object.class.getName(),
+                .produce(new MessageBodyReaderBuildItem(ServerJacksonMessageBodyReader.class.getName(), Object.class.getName(),
+                        Collections.singletonList(MediaType.APPLICATION_JSON)));
+        additionalReaders
+                .produce(new MessageBodyReaderBuildItem(VertxJsonArrayMessageBodyReader.class.getName(),
+                        JsonArray.class.getName(),
+                        Collections.singletonList(MediaType.APPLICATION_JSON)));
+        additionalReaders
+                .produce(new MessageBodyReaderBuildItem(VertxJsonObjectMessageBodyReader.class.getName(),
+                        JsonObject.class.getName(),
                         Collections.singletonList(MediaType.APPLICATION_JSON)));
         additionalWriters
-                .produce(new MessageBodyWriterBuildItem(JacksonMessageBodyWriter.class.getName(), Object.class.getName(),
+                .produce(new MessageBodyWriterBuildItem(getJacksonMessageBodyWriter(applicationNeedsSpecialJacksonFeatures),
+                        Object.class.getName(),
                         Collections.singletonList(MediaType.APPLICATION_JSON)));
+        additionalWriters
+                .produce(new MessageBodyWriterBuildItem(VertxJsonArrayMessageBodyWriter.class.getName(),
+                        JsonArray.class.getName(),
+                        Collections.singletonList(MediaType.APPLICATION_JSON)));
+        additionalWriters
+                .produce(new MessageBodyWriterBuildItem(VertxJsonObjectMessageBodyWriter.class.getName(),
+                        JsonObject.class.getName(),
+                        Collections.singletonList(MediaType.APPLICATION_JSON)));
+
+        exceptionMappers.produce(new ExceptionMapperBuildItem(DefaultMismatchedInputException.class.getName(),
+                MismatchedInputException.class.getName(), Priorities.USER + 100, false));
+
+        customExceptionMapper
+                .produce(new CustomExceptionMapperBuildItem(NativeInvalidDefinitionExceptionMapper.class.getName()));
+    }
+
+    private String getJacksonMessageBodyWriter(boolean applicationNeedsSpecialJacksonFeatures) {
+        return applicationNeedsSpecialJacksonFeatures ? BasicServerJacksonMessageBodyWriter.class.getName()
+                : FullyFeaturedServerJacksonMessageBodyWriter.class.getName();
     }
 }
