71ce4cec2bee6fb9132258d842af3779815b0a04
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:53:34 +0200

    Adding verbal alert description to alert email templates and subject defaults.
        Fixes #1158

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/alerts/FormattedEmailAlertSender.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/alerts/FormattedEmailAlertSender.java
@@ -33,8 +33,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.google.common.base.MoreObjects.firstNonNull;
-
 public class FormattedEmailAlertSender extends StaticEmailAlertSender implements AlertSender {
     public static final String bodyTemplate = "##########\n" +
             "Alert Description: ${check_result.resultDescription}\n" +
@@ -46,14 +44,12 @@ public class FormattedEmailAlertSender extends StaticEmailAlertSender implements
             "\n" +
             "Triggered condition: ${check_result.triggeredCondition}\n" +
             "##########\n\n" +
-            "${if backlog}" +
+            "${if backlog_size > 0}" +
             "Last messages accounting for this alert:\n" +
-            "${foreach backlog message}" +
+            "${foreach backlog message}\n" +
             "${message}\n" +
-            "${end}" +
-            "${else}" +
-            "<No backlog>\n" +
-            "${end}" +
+            "${end}\n" +
+            "${else}<No backlog.>${end}\n" +
             "\n";
 
     private final Engine engine = new Engine();
@@ -108,10 +104,13 @@ public class FormattedEmailAlertSender extends StaticEmailAlertSender implements
         model.put("check_result", checkResult);
         model.put("stream_url", buildStreamDetailsURL(configuration.getWebInterfaceUri(), checkResult, stream));
 
-        final List<Message> messages = firstNonNull(backlog, Collections.<Message>emptyList());
-        model.put("backlog", messages);
-        model.put("backlog_size", messages.size());
-
+        if (backlog != null) {
+            model.put("backlog", backlog);
+            model.put("backlog_size", backlog.size());
+        } else {
+            model.put("backlog", Collections.<Message>emptyList());
+            model.put("backlog_size", 0);
+        }
         return model;
     }
 }
