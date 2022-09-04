0a25d837c1feff0758363bed16cf8c909852252a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:07:49 +0200

    Improved logging for failed ClusterConfig updates
        Closes #2453

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/ClusterConfigResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/ClusterConfigResource.java
@@ -20,19 +20,26 @@ import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.factories.SchemaFactoryWrapper;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.graylog2.auditlog.jersey.AuditLog;
 import org.graylog2.plugin.cluster.ClusterConfigService;
 import org.graylog2.rest.MoreMediaTypes;
 import org.graylog2.rest.models.system.config.ClusterConfigList;
+import org.graylog2.shared.plugins.ChainingClassLoader;
 import org.graylog2.shared.rest.resources.RestResource;
+import org.graylog2.shared.security.RestPermissions;
 import org.hibernate.validator.constraints.NotBlank;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -55,16 +62,21 @@ import static java.util.Objects.requireNonNull;
 @Path("/system/cluster_config")
 @Produces(MediaType.APPLICATION_JSON)
 public class ClusterConfigResource extends RestResource {
+    private static final Logger LOG = LoggerFactory.getLogger(ClusterConfigResource.class);
+    
     private final ClusterConfigService clusterConfigService;
+    private final ChainingClassLoader chainingClassLoader;
 
     @Inject
-    public ClusterConfigResource(ClusterConfigService clusterConfigService) {
+    public ClusterConfigResource(ClusterConfigService clusterConfigService, ChainingClassLoader chainingClassLoader) {
         this.clusterConfigService = requireNonNull(clusterConfigService);
+        this.chainingClassLoader = chainingClassLoader;
     }
 
     @GET
     @ApiOperation(value = "List all configuration classes")
     @Timed
+    @RequiresPermissions(RestPermissions.CLUSTER_CONFIG_ENTRY_READ)
     public ClusterConfigList list() {
         final Set<Class<?>> classes = clusterConfigService.list();
 
@@ -75,6 +87,7 @@ public class ClusterConfigResource extends RestResource {
     @Path("{configClass}")
     @ApiOperation(value = "Get configuration settings from database")
     @Timed
+    @RequiresPermissions(RestPermissions.CLUSTER_CONFIG_ENTRY_READ)
     public Object read(@ApiParam(name = "configClass", value = "The name of the cluster configuration class", required = true)
                        @PathParam("configClass") @NotBlank String configClass) {
         final Class<?> cls = classFromName(configClass);
@@ -90,6 +103,8 @@ public class ClusterConfigResource extends RestResource {
     @Path("{configClass}")
     @Consumes(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "Update configuration in database")
+    @RequiresPermissions({RestPermissions.CLUSTER_CONFIG_ENTRY_CREATE, RestPermissions.CLUSTER_CONFIG_ENTRY_EDIT})
+    @AuditLog(object = "configuration", captureRequestEntity = true, captureResponseEntity = true)
     public Response update(@ApiParam(name = "configClass", value = "The name of the cluster configuration class", required = true)
                            @PathParam("configClass") @NotBlank String configClass,
                            @ApiParam(name = "body", value = "The payload of the cluster configuration", required = true)
@@ -99,8 +114,22 @@ public class ClusterConfigResource extends RestResource {
             throw new NotFoundException("Couldn't find configuration class \"" + configClass + "\"");
         }
 
-        final Object o = objectMapper.readValue(body, cls);
-        clusterConfigService.write(o);
+        final Object o;
+        try {
+            o = objectMapper.readValue(body, cls);
+        } catch (Exception e) {
+            final String msg = "Couldn't parse cluster configuration \"" + configClass + "\".";
+            LOG.error(msg, e);
+            throw new BadRequestException(msg);
+        }
+
+        try {
+            clusterConfigService.write(o);
+        } catch (Exception e) {
+            final String msg = "Couldn't write cluster config \"" + configClass + "\".";
+            LOG.error(msg, e);
+            throw new InternalServerErrorException(msg, e);
+        }
 
         return Response.accepted(o).build();
     }
@@ -109,6 +138,8 @@ public class ClusterConfigResource extends RestResource {
     @Path("{configClass}")
     @ApiOperation(value = "Delete configuration settings from database")
     @Timed
+    @RequiresPermissions(RestPermissions.CLUSTER_CONFIG_ENTRY_DELETE)
+    @AuditLog(object = "configuration")
     public void delete(@ApiParam(name = "configClass", value = "The name of the cluster configuration class", required = true)
                        @PathParam("configClass") @NotBlank String configClass) {
         final Class<?> cls = classFromName(configClass);
@@ -124,6 +155,7 @@ public class ClusterConfigResource extends RestResource {
     @Produces(MoreMediaTypes.APPLICATION_SCHEMA_JSON)
     @ApiOperation(value = "Get JSON schema of configuration class")
     @Timed
+    @RequiresPermissions(RestPermissions.CLUSTER_CONFIG_ENTRY_READ)
     public JsonSchema schema(@ApiParam(name = "configClass", value = "The name of the cluster configuration class", required = true)
                              @PathParam("configClass") @NotBlank String configClass) {
         final Class<?> cls = classFromName(configClass);
@@ -144,7 +176,7 @@ public class ClusterConfigResource extends RestResource {
     @Nullable
     private Class<?> classFromName(String className) {
         try {
-            return Class.forName(className);
+            return chainingClassLoader.loadClass(className);
         } catch (ClassNotFoundException e) {
             return null;
         }
