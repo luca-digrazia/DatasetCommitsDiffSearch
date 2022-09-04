9569e55f035e0eda675d430427a18ebfe26a530e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:09:00 +0200

    Add the ability to create new instances via default constructor to the bytecode recorder

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/test/java/org/jboss/shamrock/deployment/recording/BytecodeRecorderTestCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/test/java/org/jboss/shamrock/deployment/recording/BytecodeRecorderTestCase.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.jboss.shamrock.deployment.recording;
 
 import java.io.IOException;
@@ -111,9 +95,9 @@ public class BytecodeRecorderTestCase {
     void runTest(Consumer<BytecodeRecorderImpl> generator, Object... expected) throws Exception {
         TestTemplate.RESULT.clear();
         TestClassLoader tcl = new TestClassLoader(getClass().getClassLoader());
-        BytecodeRecorderImpl recorder = new BytecodeRecorderImpl(tcl, false, TEST_CLASS);
+        BytecodeRecorderImpl recorder = new BytecodeRecorderImpl(tcl, false);
         generator.accept(recorder);
-        recorder.writeBytecode(new TestClassOutput(tcl));
+        recorder.writeBytecode(new TestClassOutput(tcl), TEST_CLASS);
 
         StartupTask task = (StartupTask) tcl.loadClass(TEST_CLASS).newInstance();
         task.deploy(new StartupContext());
