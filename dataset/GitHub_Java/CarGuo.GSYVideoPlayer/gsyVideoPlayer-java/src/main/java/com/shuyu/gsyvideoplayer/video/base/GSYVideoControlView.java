7eba0c386ffff6192c09e9db72937f89bbe8cd70
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:45:47 +0200

    ä¼åäºåå»é®é¢  (2017-08-28)

--- dataset/GitHub_Java/CarGuo.GSYVideoPlayer/gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/video/base/GSYVideoControlView.java
+++ dataset/GitHub_Java/CarGuo.GSYVideoPlayer/gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/video/base/GSYVideoControlView.java
@@ -531,6 +531,7 @@ public abstract class GSYVideoControlView extends GSYVideoView implements View.O
             } else {
                 if (mFullscreenButton != null)
                     mFullscreenButton.setImageResource(getEnlargeImageRes());
+                setViewShowState(mBackButton, GONE);
             }
             return true;
         }
