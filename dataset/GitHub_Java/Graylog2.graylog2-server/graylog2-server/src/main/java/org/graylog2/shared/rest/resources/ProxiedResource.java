1468f0932f92aa73e425d70307078f8c24807d79
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:16:05 +0200

    Add missing semicolon. (Never commit that does not even compile!)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/shared/rest/resources/ProxiedResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/shared/rest/resources/ProxiedResource.java
@@ -54,22 +54,22 @@ public abstract class ProxiedResource extends RestResource {
         }
     }
 
-    protected <RemoteInterfaceType, RemoteCallResponseType> Map<String, Optional<RemoteCallResponseType>> getForAllNodes(Function<RemoteInterfaceType,Call<RemoteCallResponseType>> fn, Function<String, Optional<RemoteInterfaceType>> interfaceProvider) {
+    protected <T, K> Map<String, Optional<K>> getForAllNodes(Function<T, Call<K>> fn, Function<String, Optional<T>> interfaceProvider) {
         return getForAllNodes(fn, interfaceProvider, Function.identity());
     }
 
-    protected <RemoteInterfaceType, FinalResponseType, RemoteCallResponseType> Map<String, Optional<FinalResponseType>> getForAllNodes(Function<RemoteInterfaceType, Call<RemoteCallResponseType>> fn, Function<String, Optional<RemoteInterfaceType>> interfaceProvider, Function<RemoteCallResponseType, FinalResponseType> transformer) {
+    protected <T, K, L> Map<String, Optional<K>> getForAllNodes(Function<T, Call<L>> fn, Function<String, Optional<T>> interfaceProvider, Function<L, K> transformer) {
         return this.nodeService.allActive()
             .entrySet()
             .stream()
             .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
-                final Optional<RemoteInterfaceType> remoteInterface = interfaceProvider.apply(entry.getKey());
+                final Optional<T> remoteInterface = interfaceProvider.apply(entry.getKey());
                 if (!remoteInterface.isPresent()) {
                     return Optional.empty();
                 }
-                final Call<RemoteCallResponseType> call = fn.apply(remoteInterface.get());
+                final Call<L> call = fn.apply(remoteInterface.get());
                 try {
-                    final Response<RemoteCallResponseType> response = call.execute();
+                    final Response<L> response = call.execute();
                     if (response.isSuccess()) {
                         return Optional.of(transformer.apply(response.body()));
                     } else {
@@ -83,7 +83,7 @@ public abstract class ProxiedResource extends RestResource {
             }));
     }
 
-    protected <RemoteInterfaceType> Function<String, Optional<RemoteInterfaceType>> createRemoteInterfaceProvider(Class<RemoteInterfaceType> interfaceClass) {
+    protected <T> Function<String, Optional<T>> createRemoteInterfaceProvider(Class<T> interfaceClass) {
         return (nodeId) -> {
             try {
                 final Node targetNode = nodeService.byNodeId(nodeId);
