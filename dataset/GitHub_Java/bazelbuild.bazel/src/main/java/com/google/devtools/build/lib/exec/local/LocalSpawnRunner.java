3a385d3e42a9267d8bcc766c0e7399b193c01b53
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:11:24 +0200

    Stop info logging state transitions
        This can get far too verbose for regular old info logging. There are a lot of
        others that should be revisited too.
    
        PiperOrigin-RevId: 371932207

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/local/LocalSpawnRunner.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/local/LocalSpawnRunner.java
@@ -26,8 +26,8 @@ import com.google.common.flogger.GoogleLogger;
 import com.google.devtools.build.lib.actions.ActionExecutionMetadata;
 import com.google.devtools.build.lib.actions.ActionInput;
 import com.google.devtools.build.lib.actions.Artifact;
+import com.google.devtools.build.lib.actions.CommandLines.ParamFileActionInput;
 import com.google.devtools.build.lib.actions.ExecException;
-import com.google.devtools.build.lib.actions.ForbiddenActionInputException;
 import com.google.devtools.build.lib.actions.ResourceManager;
 import com.google.devtools.build.lib.actions.ResourceManager.ResourceHandle;
 import com.google.devtools.build.lib.actions.Spawn;
@@ -38,9 +38,7 @@ import com.google.devtools.build.lib.actions.cache.VirtualActionInput;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
 import com.google.devtools.build.lib.exec.BinTools;
 import com.google.devtools.build.lib.exec.RunfilesTreeUpdater;
-import com.google.devtools.build.lib.exec.SpawnExecutingEvent;
 import com.google.devtools.build.lib.exec.SpawnRunner;
-import com.google.devtools.build.lib.exec.SpawnSchedulingEvent;
 import com.google.devtools.build.lib.profiler.Profiler;
 import com.google.devtools.build.lib.profiler.ProfilerTask;
 import com.google.devtools.build.lib.profiler.SilentCloseable;
@@ -53,7 +51,6 @@ import com.google.devtools.build.lib.shell.Subprocess;
 import com.google.devtools.build.lib.shell.SubprocessBuilder;
 import com.google.devtools.build.lib.shell.TerminationStatus;
 import com.google.devtools.build.lib.util.NetUtil;
-import com.google.devtools.build.lib.util.OS;
 import com.google.devtools.build.lib.util.io.FileOutErr;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.errorprone.annotations.FormatMethod;
@@ -121,7 +118,7 @@ public class LocalSpawnRunner implements SpawnRunner {
 
   @Override
   public SpawnResult exec(Spawn spawn, SpawnExecutionContext context)
-      throws IOException, InterruptedException, ExecException, ForbiddenActionInputException {
+      throws IOException, InterruptedException, ExecException {
 
     runfilesTreeUpdater.updateRunfilesDirectory(
         execRoot,
@@ -134,10 +131,10 @@ public class LocalSpawnRunner implements SpawnRunner {
         Profiler.instance()
             .profile(ProfilerTask.LOCAL_EXECUTION, spawn.getResourceOwner().getMnemonic())) {
       ActionExecutionMetadata owner = spawn.getResourceOwner();
-      context.report(SpawnSchedulingEvent.create(getName()));
+      context.report(ProgressStatus.SCHEDULING, getName());
       try (ResourceHandle handle =
           resourceManager.acquireResources(owner, spawn.getLocalResources())) {
-        context.report(SpawnExecutingEvent.create(getName()));
+        context.report(ProgressStatus.EXECUTING, getName());
         if (!localExecutionOptions.localLockfreeOutput) {
           context.lockOutputFiles();
         }
@@ -189,8 +186,7 @@ public class LocalSpawnRunner implements SpawnRunner {
       setState(State.PARSING);
     }
 
-    public SpawnResult run()
-        throws InterruptedException, IOException, ForbiddenActionInputException {
+    public SpawnResult run() throws InterruptedException, IOException {
       if (localExecutionOptions.localRetriesOnCrash == 0) {
         return runOnce();
       } else {
@@ -216,8 +212,7 @@ public class LocalSpawnRunner implements SpawnRunner {
       }
     }
 
-    private SpawnResult runOnce()
-        throws InterruptedException, IOException, ForbiddenActionInputException {
+    private SpawnResult runOnce() throws InterruptedException, IOException {
       try {
         return start();
       } catch (InterruptedException | InterruptedIOException e) {
@@ -228,9 +223,6 @@ public class LocalSpawnRunner implements SpawnRunner {
         throw e;
       } catch (Error e) {
         stepLog(SEVERE, e, UNHANDLED_EXCEPTION_MSG);
-        throw e;
-      } catch (ForbiddenActionInputException e) {
-        stepLog(WARNING, e, "Bad input file");
         throw e;
       } catch (IOException e) {
         stepLog(SEVERE, e, "Local I/O error");
@@ -283,8 +275,7 @@ public class LocalSpawnRunner implements SpawnRunner {
     }
 
     /** Parse the request and run it locally. */
-    private SpawnResult start()
-        throws InterruptedException, IOException, ForbiddenActionInputException {
+    private SpawnResult start() throws InterruptedException, IOException {
       logger.atInfo().log("starting local subprocess #%d, argv: %s", id, debugCmdString());
 
       FileOutErr outErr = context.getFileOutErr();
@@ -322,7 +313,7 @@ public class LocalSpawnRunner implements SpawnRunner {
       }
 
       for (ActionInput input : spawn.getInputFiles().toList()) {
-        if (input instanceof VirtualActionInput) {
+        if (input instanceof ParamFileActionInput) {
           VirtualActionInput virtualActionInput = (VirtualActionInput) input;
           Path outputPath = execRoot.getRelative(virtualActionInput.getExecPath());
           if (outputPath.exists()) {
@@ -332,9 +323,6 @@ public class LocalSpawnRunner implements SpawnRunner {
           try (OutputStream outputStream = outputPath.getOutputStream()) {
             virtualActionInput.writeTo(outputStream);
           }
-          // Some of the virtual inputs are tools run as part of the execution, hence we need to set
-          // executable flag.
-          outputPath.setExecutable(true);
         }
       }
 
@@ -455,14 +443,8 @@ public class LocalSpawnRunner implements SpawnRunner {
                         resourceUsage.getBlockInputOperations());
                     spawnResultBuilder.setNumInvoluntaryContextSwitches(
                         resourceUsage.getInvoluntaryContextSwitches());
-                    // The memory usage of the largest child process. For Darwin maxrss returns size
-                    // in bytes.
-                    if (OS.getCurrent() == OS.DARWIN) {
-                      spawnResultBuilder.setMemoryInKb(
-                          resourceUsage.getMaximumResidentSetSize() / 1000);
-                    } else {
-                      spawnResultBuilder.setMemoryInKb(resourceUsage.getMaximumResidentSetSize());
-                    }
+                    // The memory usage of the largest child process
+                    spawnResultBuilder.setMemoryInKb(resourceUsage.getMaximumResidentSetSize());
                   });
         }
         return spawnResultBuilder.build();
