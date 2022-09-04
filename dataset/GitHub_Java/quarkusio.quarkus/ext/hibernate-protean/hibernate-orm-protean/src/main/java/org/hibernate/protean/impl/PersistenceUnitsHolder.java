4ac4382cd4cb839f0b92c3a3f9c56ae048387ee5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:15:35 +0200

    Switch the JPA bootstrap to use the LightPersistenceXmlDescriptor

--- dataset/GitHub_Java/quarkusio.quarkus/ext/hibernate-protean/hibernate-orm-protean/src/main/java/org/hibernate/protean/impl/PersistenceUnitsHolder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/ext/hibernate-protean/hibernate-orm-protean/src/main/java/org/hibernate/protean/impl/PersistenceUnitsHolder.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import javax.persistence.PersistenceException;
 
+import org.hibernate.boot.archive.scan.spi.Scanner;
 import org.hibernate.jpa.boot.internal.ParsedPersistenceXmlDescriptor;
 import org.hibernate.jpa.boot.internal.PersistenceXmlParser;
 import org.hibernate.jpa.boot.spi.PersistenceUnitDescriptor;
@@ -14,13 +15,27 @@ import org.hibernate.protean.recording.RecordedState;
 
 public final class PersistenceUnitsHolder {
 
+	// Populated by Shamrock's runtime phase from Shamrock deployment info
 	private static volatile PUStatus COMPACT_UNITS = null;
 
 	private static final Object NO_NAME_TOKEN = new Object();
 
-	public static void initializeJpa(List<ParsedPersistenceXmlDescriptor> parsedPersistenceXmlDescriptors) {
+	/**
+	 * Initialize JPA for use in Shamrock.
+	 * In a native image. This must be called from within a static init method.
+	 *
+	 * In general the <code>parsedPersistenceXmlDescriptors</code> will be provided by calling {@link #loadOriginalXMLParsedDescriptors()}
+	 * In Shamrock this is done in Shamrock's JPA ResourceProcessor
+	 *
+	 * The scanner may be null to use to default scanner, or a custom scanner can be used to stop hibernate scanning
+	 * It is expected that the scanner will be provided by Shamrock via its hold of Jandex info.
+	 *
+	 * @param parsedPersistenceXmlDescriptors
+	 * @param scanner
+	 */
+	public static void initializeJpa(List<ParsedPersistenceXmlDescriptor> parsedPersistenceXmlDescriptors, Scanner scanner) {
 		final List<PersistenceUnitDescriptor> units = convertPersistenceUnits( parsedPersistenceXmlDescriptors );
-		final Map<String,RecordedState> metadata = constructMetadataAdvance( parsedPersistenceXmlDescriptors );
+		final Map<String,RecordedState> metadata = constructMetadataAdvance( units, scanner );
 		COMPACT_UNITS = new PUStatus( units, metadata );
 	}
 
@@ -36,19 +51,21 @@ public final class PersistenceUnitsHolder {
 		}
 	}
 
+	/**
+	 * Used by Shamrock's JPA ResourceProcessor to load the descriptors.
+	 */
 	public static List<ParsedPersistenceXmlDescriptor> loadOriginalXMLParsedDescriptors() {
 		//Enforce the persistence.xml configuration to be interpreted literally without allowing runtime overrides;
 		//(check for the runtime provided properties to be empty as well)
 		Map<Object, Object> configurationOverrides = Collections.emptyMap();
 		List<ParsedPersistenceXmlDescriptor> ret = PersistenceXmlParser.locatePersistenceUnits(configurationOverrides);
-		initializeJpa(ret);
 		return ret;
 	}
 
-	private static Map<String,RecordedState> constructMetadataAdvance(final List<ParsedPersistenceXmlDescriptor> parsedPersistenceXmlDescriptors) {
+	private static Map<String,RecordedState> constructMetadataAdvance(final List<PersistenceUnitDescriptor> parsedPersistenceXmlDescriptors, Scanner scanner) {
 		Map all = new HashMap(  );
 		for ( PersistenceUnitDescriptor unit : parsedPersistenceXmlDescriptors ) {
-			RecordedState m = createMetadata( unit );
+			RecordedState m = createMetadata( unit , scanner );
 			Object previous = all.put( unitName( unit ), m );
 			if ( previous != null ) {
 				throw new IllegalStateException( "Duplicate persistence unit name: " + unit.getName() );
@@ -58,14 +75,18 @@ public final class PersistenceUnitsHolder {
 	}
 
 	static RecordedState getMetadata(String persistenceUnitName) {
-		if(COMPACT_UNITS == null) {
-			throw new RuntimeException("JPA not initialized yet");
-		}
+		checkJPAInitialization();
 		Object key = persistenceUnitName;
 		if ( persistenceUnitName == null ) {
 			key = NO_NAME_TOKEN;
 		}
 		return COMPACT_UNITS.metadata.get( key );
+	}
+
+	private static void checkJPAInitialization() {
+		if ( COMPACT_UNITS == null ) {
+			throw new RuntimeException("JPA not initialized yet by Shamrock: this is likely a bug.");
+		}
 	}
 
 	private static Object unitName(PersistenceUnitDescriptor unit) {
@@ -76,16 +97,14 @@ public final class PersistenceUnitsHolder {
 		return name;
 	}
 
-	private static RecordedState createMetadata(PersistenceUnitDescriptor unit) {
-		FastBootMetadataBuilder fastBootMetadataBuilder = new FastBootMetadataBuilder( unit );
+	private static RecordedState createMetadata(PersistenceUnitDescriptor unit, Scanner scanner) {
+		FastBootMetadataBuilder fastBootMetadataBuilder = new FastBootMetadataBuilder( unit, scanner );
 		return fastBootMetadataBuilder.build();
 	}
 
 	// Not a public contract but used by Shamrock
 	public static List<PersistenceUnitDescriptor> getPersistenceUnitDescriptors() {
-		if(COMPACT_UNITS == null) {
-			throw new RuntimeException("JPA not initialized yet");
-		}
+		checkJPAInitialization();
 		return COMPACT_UNITS.units;
 	}
 
