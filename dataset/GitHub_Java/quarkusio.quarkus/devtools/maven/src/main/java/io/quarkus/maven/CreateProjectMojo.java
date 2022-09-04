6ab3174d23062ce0d5f5a0723e23cb3191bc34e9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:42:41 +0200

    fix: error handling for gradle wrapper call
        Why:
    
         * if something wrong gradle wrapper would just exit withuot info/error.
    
        This change addreses the need by:
    
         * Use `ProcessBuilder` to easily direct input/output/error to mvn output
         * Check the error code and warn via error
         * Don't pass empty env to the fork as otherwise JAVA_HOME is not picked
           up.
    
        Note - Generated Gradle builds will still fail but that is due to
        https://github.com/quarkusio/quarkus/issues/4365 - a separate concern.
    
        Fixes #4321

--- dataset/GitHub_Java/quarkusio.quarkus/devtools/maven/src/main/java/io/quarkus/maven/CreateProjectMojo.java
+++ dataset/GitHub_Java/quarkusio.quarkus/devtools/maven/src/main/java/io/quarkus/maven/CreateProjectMojo.java
@@ -1,6 +1,5 @@
 package io.quarkus.maven;
 
-import static io.quarkus.generators.ProjectGenerator.BUILD_FILE;
 import static org.fusesource.jansi.Ansi.ansi;
 import static org.twdata.maven.mojoexecutor.MojoExecutor.artifactId;
 import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
@@ -40,7 +39,6 @@ import org.fusesource.jansi.Ansi;
 import io.quarkus.cli.commands.AddExtensionResult;
 import io.quarkus.cli.commands.AddExtensions;
 import io.quarkus.cli.commands.CreateProject;
-import io.quarkus.cli.commands.file.BuildFile;
 import io.quarkus.cli.commands.writer.FileProjectWriter;
 import io.quarkus.generators.BuildTool;
 import io.quarkus.generators.SourceType;
@@ -162,7 +160,7 @@ public class CreateProjectMojo extends AbstractMojo {
             File createdDependenciesBuildFile = new File(projectRoot, buildToolEnum.getDependenciesFile());
             File buildFile = new File(createdDependenciesBuildFile.getAbsolutePath());
             if (success) {
-                AddExtensionResult result = new AddExtensions((BuildFile) context.get(BUILD_FILE))
+                AddExtensionResult result = new AddExtensions(new FileProjectWriter(buildFile.getParentFile()), buildToolEnum)
                         .addExtensions(extensions);
                 if (!result.succeeded()) {
                     success = false;
@@ -185,9 +183,8 @@ public class CreateProjectMojo extends AbstractMojo {
 
     private void createGradleWrapper(File projectDirectory) {
         try {
-            ProcessBuilder pb = new ProcessBuilder("gradle", "wrapper",
-                    "--gradle-version=" + MojoUtils.getGradleWrapperVersion()).directory(projectDirectory)
-                            .inheritIO();
+            ProcessBuilder pb = new ProcessBuilder("gradle", "wrapper").directory(projectDirectory)
+                    .inheritIO();
             Process x = pb.start();
 
             x.waitFor();
