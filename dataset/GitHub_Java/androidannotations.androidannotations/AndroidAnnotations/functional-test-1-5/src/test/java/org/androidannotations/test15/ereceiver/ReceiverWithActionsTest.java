7cce1fdb5392491bf1f1b1cf8c85c99f35a0a890
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:56:58 +0200

    add test for @ReceiverAction.dataSchemes

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/test/java/org/androidannotations/test15/ereceiver/ReceiverWithActionsTest.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/test/java/org/androidannotations/test15/ereceiver/ReceiverWithActionsTest.java
@@ -29,8 +29,6 @@ import android.net.Uri;
 
 @RunWith(RobolectricTestRunner.class)
 public class ReceiverWithActionsTest {
-	private static final String EXTRA_PARAMETER_VALUE = "string value";
-
 	private ReceiverWithActions receiver;
 
 	@Before
@@ -40,14 +38,14 @@ public class ReceiverWithActionsTest {
 
 	@Test
 	public void onSimpleActionTest() {
-		receiver.onReceive(Robolectric.application, new Intent(ReceiverWithActions.ACTION_SIMPLE_TEST));
+		receiver.onReceive(Robolectric.application, new Intent("ACTION_SIMPLE_TEST"));
 
 		assertTrue(receiver.simpleActionReceived);
 	}
 
 	@Test
 	public void onActionWithReceiverTest() {
-		Intent intent = new Intent(ReceiverWithActions.ACTION_SCHEME_TEST, Uri.parse(ReceiverWithActions.DATA_SCHEME + "://androidannotations.org"));
+		Intent intent = new Intent("ACTION_SCHEME_TEST", Uri.parse("http://androidannotations.org"));
 		receiver.onReceive(Robolectric.application, intent);
 
 		assertTrue(receiver.actionWithSchemeReceived);
@@ -55,34 +53,21 @@ public class ReceiverWithActionsTest {
 
 	@Test
 	public void onParameterActionTest() {
-		Intent intent = new Intent(ReceiverWithActions.ACTION_PARAMETER_TEST);
-		intent.putExtra(ReceiverWithActions.EXTRA_ARG_NAME2, EXTRA_PARAMETER_VALUE);
+		Intent intent = new Intent("ACTION_PARAMETER_TEST");
+		intent.putExtra("thisIsMyParameter", "string value");
 		receiver.onReceive(Robolectric.application, intent);
 
 		assertTrue(receiver.parameterActionReceived);
-		assertEquals(EXTRA_PARAMETER_VALUE, receiver.parameterActionValue);
+		assertEquals("string value", receiver.parameterActionValue);
 	}
 
 	@Test
 	public void onExtraParameterActionTest() {
-		Intent intent = new Intent(ReceiverWithActions.ACTION_EXTRA_PARAMETER_TEST);
-		intent.putExtra(ReceiverWithActions.EXTRA_ARG_NAME1, EXTRA_PARAMETER_VALUE);
+		Intent intent = new Intent("ACTION_EXTRA_PARAMETER_TEST");
+		intent.putExtra("thisExtraHasAnotherName", "string value");
 		receiver.onReceive(Robolectric.application, intent);
 
 		assertTrue(receiver.extraParameterActionReceived);
-		assertEquals(EXTRA_PARAMETER_VALUE, receiver.extraParameterActionValue);
-	}
-
-	@Test
-	public void onMultipleActionsTest() {
-		assertEquals(0, receiver.multipleActionCall);
-
-		Intent intent = new Intent(ReceiverWithActions.ACTION_MULTIPLE_TEST_1);
-		receiver.onReceive(Robolectric.application, intent);
-		assertEquals(1, receiver.multipleActionCall);
-
-		intent = new Intent(ReceiverWithActions.ACTION_MULTIPLE_TEST_2);
-		receiver.onReceive(Robolectric.application, intent);
-		assertEquals(2, receiver.multipleActionCall);
+		assertEquals("string value", receiver.extraParameterActionValue);
 	}
 }
