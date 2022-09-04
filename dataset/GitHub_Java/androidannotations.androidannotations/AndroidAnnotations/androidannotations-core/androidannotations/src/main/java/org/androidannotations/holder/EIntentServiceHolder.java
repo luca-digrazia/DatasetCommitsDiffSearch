d7da53e2e89925c7dfd11fd61b004e379bec356e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:34:18 +0200

    Ignore null-Intents in EIntentService classes

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/holder/EIntentServiceHolder.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/holder/EIntentServiceHolder.java
@@ -1,5 +1,6 @@
 /**
- * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2016 eBusiness Information, Excilys Group
+ * Copyright (C) 2016-2017 the AndroidAnnotations project
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,18 +16,19 @@
  */
 package org.androidannotations.holder;
 
-import static com.sun.codemodel.JMod.PUBLIC;
+import static com.helger.jcodemodel.JExpr._null;
+import static com.helger.jcodemodel.JMod.PUBLIC;
 
 import javax.lang.model.element.TypeElement;
 
 import org.androidannotations.AndroidAnnotationsEnvironment;
 import org.androidannotations.helper.AndroidManifest;
 
-import com.sun.codemodel.JBlock;
-import com.sun.codemodel.JExpr;
-import com.sun.codemodel.JInvocation;
-import com.sun.codemodel.JMethod;
-import com.sun.codemodel.JVar;
+import com.helger.jcodemodel.JBlock;
+import com.helger.jcodemodel.JExpr;
+import com.helger.jcodemodel.JInvocation;
+import com.helger.jcodemodel.JMethod;
+import com.helger.jcodemodel.JVar;
 
 public class EIntentServiceHolder extends EServiceHolder {
 
@@ -68,12 +70,15 @@ public class EIntentServiceHolder extends EServiceHolder {
 	}
 
 	private void createOnHandleIntent() {
-		onHandleIntentMethod = generatedClass.method(PUBLIC, codeModel().VOID, "onHandleIntent");
-		onHandleIntentIntent = onHandleIntentMethod.param(classes().INTENT, "intent");
+		onHandleIntentMethod = generatedClass.method(PUBLIC, getCodeModel().VOID, "onHandleIntent");
+		onHandleIntentIntent = onHandleIntentMethod.param(getClasses().INTENT, "intent");
 		onHandleIntentMethod.annotate(Override.class);
 		onHandleIntentBody = onHandleIntentMethod.body();
 		codeModelHelper.callSuperMethod(onHandleIntentMethod, this, onHandleIntentBody);
+
+		onHandleIntentBody._if(onHandleIntentIntent.eq(_null()))._then()._return();
+
 		JInvocation getActionInvocation = JExpr.invoke(onHandleIntentIntent, "getAction");
-		onHandleIntentIntentAction = onHandleIntentBody.decl(classes().STRING, "action", getActionInvocation);
+		onHandleIntentIntentAction = onHandleIntentBody.decl(getClasses().STRING, "action", getActionInvocation);
 	}
 }
