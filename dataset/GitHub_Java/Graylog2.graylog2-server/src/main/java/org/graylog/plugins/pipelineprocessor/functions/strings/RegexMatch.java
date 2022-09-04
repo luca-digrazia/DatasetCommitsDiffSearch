cc5e03274e720fbf18b5640500b46e23236e877f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:19:52 +0200

    remove unused transform

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog/plugins/pipelineprocessor/functions/strings/RegexMatch.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog/plugins/pipelineprocessor/functions/strings/RegexMatch.java
@@ -20,6 +20,7 @@ import com.google.common.collect.ForwardingMap;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import org.graylog.plugins.pipelineprocessor.EvaluationContext;
+import org.graylog.plugins.pipelineprocessor.ast.expressions.Expression;
 import org.graylog.plugins.pipelineprocessor.ast.functions.AbstractFunction;
 import org.graylog.plugins.pipelineprocessor.ast.functions.FunctionArgs;
 import org.graylog.plugins.pipelineprocessor.ast.functions.FunctionDescriptor;
@@ -36,28 +37,32 @@ import static com.google.common.collect.ImmutableList.of;
 
 public class RegexMatch extends AbstractFunction<RegexMatch.RegexMatchResult> {
 
+    public static final String PATTERN_ARG = "pattern";
+    public static final String VALUE_ARG = "value";
+    public static final String GROUP_NAMES_ARG = "group_names";
     public static final String NAME = "regex";
-    private final ParameterDescriptor<String, Pattern> pattern;
-    private final ParameterDescriptor<String, String> value;
-    private final ParameterDescriptor<List, List> optionalGroupNames;
 
-    public RegexMatch() {
-        pattern = ParameterDescriptor.string("pattern", Pattern.class).transform(Pattern::compile).build();
-        value = ParameterDescriptor.string("value").build();
-        optionalGroupNames = ParameterDescriptor.type("group_names", List.class).optional().build();
+    @Override
+    public Object preComputeConstantArgument(FunctionArgs args, String name, Expression arg) {
+        final String stringValue = (String) super.preComputeConstantArgument(args, name, arg);
+        switch (name) {
+            case PATTERN_ARG:
+                return Pattern.compile(stringValue);
+        }
+        return stringValue;
     }
 
     @Override
     public RegexMatchResult evaluate(FunctionArgs args, EvaluationContext context) {
-        final Pattern regex = pattern.required(args, context);
-        final String value = this.value.required(args, context);
+        final Pattern regex = args.param(PATTERN_ARG).evalRequired(args, context, Pattern.class);
+        final String value = args.param(VALUE_ARG).evalRequired(args, context, String.class);
         if (regex == null || value == null) {
-            final String nullArgument = regex == null ? "pattern" : "value";
-            throw new IllegalArgumentException("Argument '" + nullArgument + "' cannot be 'null'");
+            throw new IllegalArgumentException();
         }
         //noinspection unchecked
         final List<String> groupNames =
-                (List<String>) optionalGroupNames.optional(args, context).orElse(Collections.emptyList());
+                (List<String>) args.param(GROUP_NAMES_ARG).eval(args, context, List.class)
+                        .orElse(Collections.emptyList());
 
         final Matcher matcher = regex.matcher(value);
         final boolean matches = matcher.matches();
@@ -73,9 +78,9 @@ public class RegexMatch extends AbstractFunction<RegexMatch.RegexMatchResult> {
                 .pure(true)
                 .returnType(RegexMatchResult.class)
                 .params(of(
-                        pattern,
-                        value,
-                        optionalGroupNames
+                        ParameterDescriptor.string(PATTERN_ARG, Pattern.class).build(),
+                        ParameterDescriptor.string(VALUE_ARG).build(),
+                        ParameterDescriptor.type(GROUP_NAMES_ARG, List.class).optional().build()
                 ))
                 .build();
     }
@@ -98,12 +103,6 @@ public class RegexMatch extends AbstractFunction<RegexMatch.RegexMatchResult> {
                 final int groupCount = matchResult.groupCount();
                 for (int i = 1; i <= groupCount; i++) {
                     final String groupValue = matchResult.group(i);
-
-                    if (groupValue == null) {
-                        // You cannot add null values to an ImmutableMap but optional matcher groups may be null.
-                        continue;
-                    }
-
                     // try to get a group name, if that fails use a 0-based index as the name
                     final String groupName = Iterables.get(groupNames, i - 1, null);
                     builder.put(groupName != null ? groupName : String.valueOf(i - 1), groupValue);
