efdf2c295fec306b34dda6bfe776a6b90486d7cc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:24:05 +0200

    Disable flakey AbsoluteSearchResourceTest

--- dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/search/AbsoluteSearchResourceTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/search/AbsoluteSearchResourceTest.java
@@ -1,17 +1,34 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package integration.search;
 
 import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.ValidatableResponse;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
-import integration.BaseRestTest;
 import integration.RequiresAuthentication;
 import integration.RestTestIncludingElasticsearch;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RequiresAuthentication
+@Ignore("Flakey test")
 public class AbsoluteSearchResourceTest extends RestTestIncludingElasticsearch {
     @Test
     @UsingDataSet(locations = "searchForExistingKeyword.json")
@@ -49,6 +66,32 @@ public class AbsoluteSearchResourceTest extends RestTestIncludingElasticsearch {
 
         assertThat(response.getInt("total_results")).isEqualTo(0);
         assertThat(response.getList("messages")).isEmpty();
+        assertThat(response.getList("used_indices")).hasSize(1);
+    }
+
+    @Test
+    @UsingDataSet(locations = "searchForExistingKeyword.json")
+    public void searchForExistingKeywordOutsideOfTimeRange() {
+        final ValidatableResponse result = doSearchFor("Testmessage", "2015-06-14T11:32:16.827Z", "2015-06-15T11:32:16.827Z");
+        final JsonPath response = result
+                .statusCode(200)
+                .extract().jsonPath();
+
+        assertThat(response.getInt("total_results")).isEqualTo(0);
+        assertThat(response.getList("messages")).isEmpty();
+        assertThat(response.getList("used_indices")).hasSize(1);
+    }
+
+    @Test
+    @UsingDataSet(locations = "searchForExistingKeyword.json")
+    public void searchForExistingKeywordInsideOfVeryNarrowTimeRange() {
+        final ValidatableResponse result = doSearchFor("Testmessage", "2015-06-16T11:32:16.827Z", "2015-06-16T11:32:16.827Z");
+        final JsonPath response = result
+                .statusCode(200)
+                .extract().jsonPath();
+
+        assertThat(response.getInt("total_results")).isEqualTo(1);
+        assertThat(response.getList("messages")).isNotEmpty();
         assertThat(response.getList("used_indices")).hasSize(1);
     }
 
