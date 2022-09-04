05e92f621abaaba8fc0cace68410bf939c8e6e73
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 23:23:15 +0200

    Fix manifest_merger documentation link.
        RELNOTES: None
        PiperOrigin-RevId: 184538771

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidRuleClasses.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidRuleClasses.java
@@ -569,7 +569,6 @@ public final class AndroidRuleClasses {
                   .cfg(HostTransition.INSTANCE)
                   .exec()
                   .value(env.getToolsLabel(AndroidRuleClasses.MANIFEST_MERGE_TOOL_LABEL)))
-          .advertiseSkylarkProvider(AndroidResourcesInfo.PROVIDER.id())
           .build();
     }
 
