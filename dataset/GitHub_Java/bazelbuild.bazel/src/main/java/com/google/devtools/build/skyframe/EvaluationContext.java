bee841c567338d31ab4e212106e0c7ee35a2f976
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:16:13 +0200

    Include isExecutionPhase & useForkJoinPool in EvaluationContext#copyFrom.
        RELNOTES: None
        PiperOrigin-RevId: 368421509

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/skyframe/EvaluationContext.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/skyframe/EvaluationContext.java
@@ -17,6 +17,7 @@ package com.google.devtools.build.skyframe;
 import com.google.common.base.Preconditions;
 import com.google.devtools.build.lib.events.ExtendedEventHandler;
 import com.google.devtools.build.skyframe.WalkableGraph.WalkableGraphFactory;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Supplier;
 import javax.annotation.Nullable;
@@ -26,28 +27,35 @@ import javax.annotation.Nullable;
  * BuildDriver#evaluate} and {@link WalkableGraphFactory#prepareAndGet}
  */
 public class EvaluationContext {
-  @Nullable private final Integer numThreads;
-  @Nullable private final Supplier<ExecutorService> executorService;
+  private final int numThreads;
+  @Nullable private final Supplier<ExecutorService> executorServiceSupplier;
   private final boolean keepGoing;
   private final ExtendedEventHandler eventHandler;
+  private final boolean useForkJoinPool;
+  private final boolean isExecutionPhase;
 
   protected EvaluationContext(
-      @Nullable Integer numThread,
-      @Nullable Supplier<ExecutorService> executorService,
+      int numThreads,
+      @Nullable Supplier<ExecutorService> executorServiceSupplier,
       boolean keepGoing,
-      ExtendedEventHandler eventHandler) {
-    this.numThreads = numThread;
-    this.executorService = executorService;
+      ExtendedEventHandler eventHandler,
+      boolean useForkJoinPool,
+      boolean isExecutionPhase) {
+    Preconditions.checkArgument(0 < numThreads, "numThreads must be positive");
+    this.numThreads = numThreads;
+    this.executorServiceSupplier = executorServiceSupplier;
     this.keepGoing = keepGoing;
-    this.eventHandler = eventHandler;
+    this.eventHandler = Preconditions.checkNotNull(eventHandler);
+    this.useForkJoinPool = useForkJoinPool;
+    this.isExecutionPhase = isExecutionPhase;
   }
 
-  public Integer getNumThreads() {
+  public int getParallelism() {
     return numThreads;
   }
 
-  public Supplier<ExecutorService> getExecutorService() {
-    return executorService;
+  public Optional<Supplier<ExecutorService>> getExecutorServiceSupplier() {
+    return Optional.ofNullable(executorServiceSupplier);
   }
 
   public boolean getKeepGoing() {
@@ -58,36 +66,60 @@ public class EvaluationContext {
     return eventHandler;
   }
 
+  public EvaluationContext getCopyWithKeepGoing(boolean keepGoing) {
+    if (this.keepGoing == keepGoing) {
+      return this;
+    } else {
+      return new EvaluationContext(
+          this.numThreads,
+          this.executorServiceSupplier,
+          keepGoing,
+          this.eventHandler,
+          this.useForkJoinPool,
+          this.isExecutionPhase);
+    }
+  }
+
+  public boolean getUseForkJoinPool() {
+    return useForkJoinPool;
+  }
+
+  public boolean isExecutionPhase() {
+    return isExecutionPhase;
+  }
+
   public static Builder newBuilder() {
     return new Builder();
   }
 
   /** Builder for {@link EvaluationContext}. */
   public static class Builder {
-    private Integer numThread;
-    private Supplier<ExecutorService> executorService;
+    private int numThreads;
+    private Supplier<ExecutorService> executorServiceSupplier;
     private boolean keepGoing;
     private ExtendedEventHandler eventHandler;
+    private boolean useForkJoinPool;
+    private boolean isExecutionPhase = false;
 
     private Builder() {}
 
     public Builder copyFrom(EvaluationContext evaluationContext) {
-      this.numThread = evaluationContext.numThreads;
-      this.executorService = evaluationContext.executorService;
+      this.numThreads = evaluationContext.numThreads;
+      this.executorServiceSupplier = evaluationContext.executorServiceSupplier;
       this.keepGoing = evaluationContext.keepGoing;
       this.eventHandler = evaluationContext.eventHandler;
+      this.isExecutionPhase = evaluationContext.isExecutionPhase;
+      this.useForkJoinPool = evaluationContext.useForkJoinPool;
       return this;
     }
 
-    public Builder setNumThreads(int numThread) {
-      this.numThread = numThread;
-      this.executorService = null;
+    public Builder setNumThreads(int numThreads) {
+      this.numThreads = numThreads;
       return this;
     }
 
-    public Builder setExecutorServiceSupplier(Supplier<ExecutorService> executorService) {
-      this.executorService = executorService;
-      this.numThread = null;
+    public Builder setExecutorServiceSupplier(Supplier<ExecutorService> executorServiceSupplier) {
+      this.executorServiceSupplier = executorServiceSupplier;
       return this;
     }
 
@@ -96,19 +128,29 @@ public class EvaluationContext {
       return this;
     }
 
-    public Builder setEventHander(ExtendedEventHandler eventHandler) {
+    public Builder setEventHandler(ExtendedEventHandler eventHandler) {
       this.eventHandler = eventHandler;
       return this;
     }
 
+    public Builder setUseForkJoinPool(boolean useForkJoinPool) {
+      this.useForkJoinPool = useForkJoinPool;
+      return this;
+    }
+
+    public Builder setExecutionPhase() {
+      isExecutionPhase = true;
+      return this;
+    }
+
     public EvaluationContext build() {
-      Preconditions.checkState(
-          (numThread == null && executorService != null)
-              || (numThread != null && executorService == null),
-          "Exactly one of numThread and executorService must be set. %s %s",
-          numThread,
-          executorService);
-      return new EvaluationContext(numThread, executorService, keepGoing, eventHandler);
+      return new EvaluationContext(
+          numThreads,
+          executorServiceSupplier,
+          keepGoing,
+          eventHandler,
+          useForkJoinPool,
+          isExecutionPhase);
     }
   }
 }
