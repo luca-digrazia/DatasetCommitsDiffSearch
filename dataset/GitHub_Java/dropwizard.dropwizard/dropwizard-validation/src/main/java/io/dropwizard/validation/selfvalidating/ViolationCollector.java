16adeea495f6fd66576560d067a67571b38ebc7f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:09:34 +0200

    Escape EL expressions in ViolationCollector (#3157)
        Fixes #3153

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-validation/src/main/java/io/dropwizard/validation/selfvalidating/ViolationCollector.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-validation/src/main/java/io/dropwizard/validation/selfvalidating/ViolationCollector.java
@@ -1,116 +1,64 @@
 package io.dropwizard.validation.selfvalidating;
 
-import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
-
 import javax.annotation.Nullable;
 import javax.validation.ConstraintValidatorContext;
-import java.util.Collections;
-import java.util.Map;
-
-import static io.dropwizard.validation.InterpolationHelper.escapeMessageParameter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class is a simple wrapper around the ConstraintValidatorContext of hibernate validation.
  * It collects all the violations of the SelfValidation methods of an object.
  */
 public class ViolationCollector {
-    private final ConstraintValidatorContext constraintValidatorContext;
-    private final boolean escapeExpressions;
+    private static final Pattern ESCAPE_PATTERN = Pattern.compile("\\$\\{");
 
     private boolean violationOccurred = false;
+    private ConstraintValidatorContext context;
 
-    public ViolationCollector(ConstraintValidatorContext constraintValidatorContext) {
-        this(constraintValidatorContext, true);
-    }
 
-    public ViolationCollector(ConstraintValidatorContext constraintValidatorContext, boolean escapeExpressions) {
-        this.constraintValidatorContext = constraintValidatorContext;
-        this.escapeExpressions = escapeExpressions;
+    public ViolationCollector(ConstraintValidatorContext context) {
+        this.context = context;
     }
 
     /**
      * Adds a new violation to this collector. This also sets {@code violationOccurred} to {@code true}.
-     * <p>
-     * Prefer the method with explicit message parameters if you want to interpolate the message.
      *
-     * @param message the message of the violation
-     * @see #addViolation(String, Map)
+     * @param message the message of the violation (any EL expression will be escaped and not parsed)
      */
     public void addViolation(String message) {
-        addViolation(message, Collections.emptyMap());
-    }
-
-    /**
-     * Adds a new violation to this collector. This also sets {@code violationOccurred} to {@code true}.
-     *
-     * @param message           the message of the violation
-     * @param messageParameters a map of message parameters which can be interpolated in the violation message
-     * @since 2.0.3
-     */
-    public void addViolation(String message, Map<String, Object> messageParameters) {
         violationOccurred = true;
-        getContextWithMessageParameters(messageParameters)
-                .buildConstraintViolationWithTemplate(sanitizeTemplate(message))
+        String messageTemplate = escapeEl(message);
+        context.buildConstraintViolationWithTemplate(messageTemplate)
                 .addConstraintViolation();
     }
 
     /**
      * Adds a new violation to this collector. This also sets {@code violationOccurred} to {@code true}.
-     * <p>
-     * Prefer the method with explicit message parameters if you want to interpolate the message.
      *
      * @param propertyName the name of the property
-     * @param message      the message of the violation
-     * @see #addViolation(String, String, Map)
+     * @param message      the message of the violation (any EL expression will be escaped and not parsed)
      * @since 2.0.2
      */
     public void addViolation(String propertyName, String message) {
-        addViolation(propertyName, message, Collections.emptyMap());
-    }
-
-    /**
-     * Adds a new violation to this collector. This also sets {@code violationOccurred} to {@code true}.
-     *
-     * @param propertyName      the name of the property
-     * @param message           the message of the violation
-     * @param messageParameters a map of message parameters which can be interpolated in the violation message
-     * @since 2.0.3
-     */
-    public void addViolation(String propertyName, String message, Map<String, Object> messageParameters) {
         violationOccurred = true;
-        getContextWithMessageParameters(messageParameters)
-                .buildConstraintViolationWithTemplate(sanitizeTemplate(message))
+        String messageTemplate = escapeEl(message);
+        context.buildConstraintViolationWithTemplate(messageTemplate)
                 .addPropertyNode(propertyName)
                 .addConstraintViolation();
     }
 
     /**
      * Adds a new violation to this collector. This also sets {@code violationOccurred} to {@code true}.
-     * Prefer the method with explicit message parameters if you want to interpolate the message.
      *
      * @param propertyName the name of the property with the violation
      * @param index        the index of the element with the violation
      * @param message      the message of the violation (any EL expression will be escaped and not parsed)
-     * @see ViolationCollector#addViolation(String, Integer, String, Map)
      * @since 2.0.2
      */
     public void addViolation(String propertyName, Integer index, String message) {
-        addViolation(propertyName, index, message, Collections.emptyMap());
-    }
-
-    /**
-     * Adds a new violation to this collector. This also sets {@code violationOccurred} to {@code true}.
-     *
-     * @param propertyName      the name of the property with the violation
-     * @param index             the index of the element with the violation
-     * @param message           the message of the violation
-     * @param messageParameters a map of message parameters which can be interpolated in the violation message
-     * @since 2.0.3
-     */
-    public void addViolation(String propertyName, Integer index, String message, Map<String, Object> messageParameters) {
         violationOccurred = true;
-        getContextWithMessageParameters(messageParameters)
-                .buildConstraintViolationWithTemplate(sanitizeTemplate(message))
+        String messageTemplate = escapeEl(message);
+        context.buildConstraintViolationWithTemplate(messageTemplate)
                 .addPropertyNode(propertyName)
                 .addBeanNode().inIterable().atIndex(index)
                 .addConstraintViolation();
@@ -121,46 +69,32 @@ public class ViolationCollector {
      *
      * @param propertyName the name of the property with the violation
      * @param key          the key of the element with the violation
-     * @param message      the message of the violation
+     * @param message      the message of the violation (any EL expression will be escaped and not parsed)
      * @since 2.0.2
      */
     public void addViolation(String propertyName, String key, String message) {
-        addViolation(propertyName, key, message, Collections.emptyMap());
-    }
-
-    /**
-     * Adds a new violation to this collector. This also sets {@code violationOccurred} to {@code true}.
-     *
-     * @param propertyName      the name of the property with the violation
-     * @param key               the key of the element with the violation
-     * @param message           the message of the violation
-     * @param messageParameters a map of message parameters which can be interpolated in the violation message
-     * @since 2.0.3
-     */
-    public void addViolation(String propertyName, String key, String message, Map<String, Object> messageParameters) {
         violationOccurred = true;
-        final String messageTemplate = sanitizeTemplate(message);
-        final HibernateConstraintValidatorContext context = getContextWithMessageParameters(messageParameters);
+        String messageTemplate = escapeEl(message);
         context.buildConstraintViolationWithTemplate(messageTemplate)
                 .addPropertyNode(propertyName)
                 .addBeanNode().inIterable().atKey(key)
                 .addConstraintViolation();
     }
 
-    private HibernateConstraintValidatorContext getContextWithMessageParameters(Map<String, Object> messageParameters) {
-        final HibernateConstraintValidatorContext context =
-                constraintValidatorContext.unwrap(HibernateConstraintValidatorContext.class);
-        for (Map.Entry<String, Object> messageParameter : messageParameters.entrySet()) {
-            final Object value = messageParameter.getValue();
-            final String escapedValue = value == null ? null : escapeMessageParameter(value.toString());
-            context.addMessageParameter(messageParameter.getKey(), escapedValue);
-        }
-        return context;
-    }
-
     @Nullable
-    private String sanitizeTemplate(@Nullable String message) {
-        return escapeExpressions ? escapeMessageParameter(message) : message;
+    private String escapeEl(@Nullable String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+
+        final Matcher m = ESCAPE_PATTERN.matcher(s);
+        final StringBuffer sb = new StringBuffer(s.length() + 16);
+        while (m.find()) {
+            m.appendReplacement(sb, "\\\\\\${");
+        }
+        m.appendTail(sb);
+
+        return sb.toString();
     }
 
     /**
@@ -170,7 +104,7 @@ public class ViolationCollector {
      * @return the wrapped Hibernate ConstraintValidatorContext
      */
     public ConstraintValidatorContext getContext() {
-        return constraintValidatorContext;
+        return context;
     }
 
     /**
