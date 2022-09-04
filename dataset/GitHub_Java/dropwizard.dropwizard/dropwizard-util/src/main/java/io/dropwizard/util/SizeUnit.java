e01d93033488b6abc1acbbcc5e33a2f9213f503d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:28:18 +0200

    fix copy/paste error (comment only)

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-util/src/main/java/io/dropwizard/util/SizeUnit.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-util/src/main/java/io/dropwizard/util/SizeUnit.java
@@ -2,10 +2,7 @@ package io.dropwizard.util;
 
 /**
  * A unit of size.
- *
- * @deprecated Use {@link DataSizeUnit} for correct SI and IEC prefixes.
  */
-@Deprecated
 public enum SizeUnit {
     /**
      * Bytes.
@@ -97,29 +94,5 @@ public enum SizeUnit {
      */
     public long toTerabytes(long l) {
         return TERABYTES.convert(l, this);
-    }
-
-    /**
-     * Convert this size unit into a data size unit with correct SI and IEC prefix.
-     *
-     * @return the {@link DataSizeUnit} corresponding to this size unit
-     * @see DataSizeUnit
-     * @since 2.0
-     */
-    public DataSizeUnit toDataSizeUnit() {
-        switch (this) {
-            case BYTES:
-                return DataSizeUnit.BYTES;
-            case KILOBYTES:
-                return DataSizeUnit.KIBIBYTES;
-            case MEGABYTES:
-                return DataSizeUnit.MEBIBYTES;
-            case GIGABYTES:
-                return DataSizeUnit.GIBIBYTES;
-            case TERABYTES:
-                return DataSizeUnit.TEBIBYTES;
-            default:
-                throw new IllegalArgumentException("Unknown unit: " + this);
-        }
     }
 }
