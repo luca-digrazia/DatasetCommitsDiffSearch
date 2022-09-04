fc5d6e93dbe2b053961da9406184250b1ee401d2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 17:27:57 +0200

    Support for custom TestRunnerSpawn on flaky retries.
        PiperOrigin-RevId: 325245985

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/test/TestRunnerAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/test/TestRunnerAction.java
@@ -482,10 +482,9 @@ public class TestRunnerAction extends AbstractAction
    * the test log base name with arbitrary prefix and extension.
    */
   @Override
-  protected void deleteOutputs(
-      Path execRoot, ArtifactPathResolver pathResolver, @Nullable BulkDeleter bulkDeleter)
+  protected void deleteOutputs(Path execRoot, @Nullable BulkDeleter bulkDeleter)
       throws IOException, InterruptedException {
-    super.deleteOutputs(execRoot, pathResolver, bulkDeleter);
+    super.deleteOutputs(execRoot, bulkDeleter);
 
     // We do not rely on globs, as it causes quadratic behavior in --runs_per_test and test
     // shard count.
