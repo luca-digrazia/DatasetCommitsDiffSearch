f59c1cc7402a64e80f395ba2ae59054c6ee398a3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:59:57 +0200

    @AutoCodec TemplateExpansionAction. Have to make some anonymous classes concrete.
        PiperOrigin-RevId: 189467877

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/actions/TemplateExpansionAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/actions/TemplateExpansionAction.java
@@ -47,7 +47,6 @@ import java.util.stream.StreamSupport;
 /**
  * Action to expand a template and write the expanded content to a file.
  */
-@AutoCodec
 @Immutable // if all substitutions are immutable
 public final class TemplateExpansionAction extends AbstractFileWriteAction {
 
