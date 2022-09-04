4f09f0f4899f161bc175d797c8546ada45c62c7c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:48:59 +0200

    Add more @Rest JavaDoc

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/rest/RequiresAuthentication.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/rest/RequiresAuthentication.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2012 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -20,8 +20,66 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <p>
+ * Use on {@link Get}, {@link Post}, … annotated methods to use authentication
+ * on the request.
+ * </p>
+ * <p>
+ * To set the current authentication object to use, you MUST either let your
+ * RestClient interface extends of
+ * {@link org.androidannotations.api.rest.RestClientHeaders RestClientHeaders}
+ * or add the following method to your interface :
+ * <code>void setAuthentication(org.springframework.http.HttpAuthentication auth)</code>
+ * .
+ * </p>
+ * <p>
+ * You can also add a specific method for <a
+ * href="https://en.wikipedia.org/wiki/Basic_access_authentication">Basic
+ * Authentication</a> :
+ * <code>setHttpBasicAuth(String username, String password)</code> or <a
+ * href="https://tools.ietf.org/html/rfc6750" >Bearer (OAuth) authentication</a>
+ * : <code>setBearerAuth(String token)</code>.
+ * </p>
+ * <blockquote>
+ * 
+ * <b>Example :</b>
+ * 
+ * <pre>
+ * &#064;Rest(rootUrl = &quot;http://myserver&quot;, converters = MappingJacksonHttpMessageConverter.class)
+ * public interface MyRestClient {
+ * 
+ * 	&#064;Get("/events/{id}")
+ * 	&#064;RequiresAuthentication
+ * 	Event getEvent(long id);
+ * 
+ * 	setHttpBasicAuth(String username, String password);
+ * }
+ * 
+ * &#064;EBean
+ * public class MyBean {
+ * 
+ * 	&#064;RestService MyRestClient;
+ * 
+ * 	&#064;AfterInject
+ * 	public void init() {
+ * 		myRestClient.setHttpBasicAuth("user", "password");
+ * 	}
+ * }
+ * </pre>
+ * 
+ * </blockquote>
+ * 
+ * @see Rest
+ * @see Get
+ * @see Post
+ * @see Put
+ * @see Delete
+ * @see Head
+ * @see Options
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target({ ElementType.METHOD, ElementType.TYPE })
 public @interface RequiresAuthentication {
-	
+
 }
