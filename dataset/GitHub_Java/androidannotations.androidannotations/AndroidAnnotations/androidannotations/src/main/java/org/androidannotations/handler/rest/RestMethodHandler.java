f415d32548b190d615c0d6dfa38c89ba10a7f57e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:50:23 +0200

    Do not add extra block in @Rest methods

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/handler/rest/RestMethodHandler.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/handler/rest/RestMethodHandler.java
@@ -53,6 +53,7 @@ import com.sun.codemodel.JVar;
 public abstract class RestMethodHandler extends BaseAnnotationHandler<RestHolder> {
 
 	protected final RestAnnotationHelper restAnnotationHelper;
+	protected final APTCodeModelHelper codeModelHelper;
 
 	public RestMethodHandler(Class<?> targetClass, ProcessingEnvironment processingEnvironment) {
 		super(targetClass, processingEnvironment);
