b8e9cd191cc92241566b3d3c94613d6600dae1dc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:02:01 +0200

    Have DefaultDatabaseProvider return filename
        The API for `DatabaseProvider` is to return `List<File>` however the
        current implementation of `DefaultDatabaseProvider` returns a `File`
        that does not exist. We were able to get around this before due to the
        fact that the file was only used to call `File#getName()`.
    
        In #390 this was changed and the actual `File` object was used to open
        the database.
    
        This fixes #431

--- dataset/GitHub_Java/facebook.stetho/stetho/src/main/java/com/facebook/stetho/inspector/database/DefaultDatabaseFilesProvider.java
+++ dataset/GitHub_Java/facebook.stetho/stetho/src/main/java/com/facebook/stetho/inspector/database/DefaultDatabaseFilesProvider.java
@@ -1,15 +1,24 @@
+/*
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 package com.facebook.stetho.inspector.database;
 
 import android.content.Context;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * Provides the results of {@link Context#databaseList()}.
+ * Provides the results of {@link Context#databaseList()} for {@link SqliteDatabaseDriver}.
  */
 public final class DefaultDatabaseFilesProvider implements DatabaseFilesProvider {
-
   private final Context mContext;
 
   public DefaultDatabaseFilesProvider(Context context) {
@@ -19,8 +28,8 @@ public final class DefaultDatabaseFilesProvider implements DatabaseFilesProvider
   @Override
   public List<File> getDatabaseFiles() {
     List<File> databaseFiles = new ArrayList<File>();
-    for (String filename : mContext.databaseList()) {
-      databaseFiles.add(new File(filename));
+    for (String databaseName : mContext.databaseList()) {
+      databaseFiles.add(mContext.getDatabasePath(databaseName));
     }
     return databaseFiles;
   }
