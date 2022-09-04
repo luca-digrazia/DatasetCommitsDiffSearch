771adf0c737cd54b75da93e156c3dba5b2f219f9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:26:01 +0200

    feat: allow generation of configuration docs for external extensions
        Updates following https://github.com/quarkusio/quarkus/issues/7876#issuecomment-624115140.

--- dataset/GitHub_Java/quarkusio.quarkus/core/processor/src/main/java/io/quarkus/annotation/processor/generate_doc/ScannedConfigDocsItemHolder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/processor/src/main/java/io/quarkus/annotation/processor/generate_doc/ScannedConfigDocsItemHolder.java
@@ -4,30 +4,56 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-final public class ScannedConfigDocsItemHolder {
-    private final Map<String, List<ConfigDocItem>> allConfigItemsPerExtension = new HashMap<>();
-    private final Map<String, List<ConfigDocItem>> configGroupConfigItems = new HashMap<>();
+final class ScannedConfigDocsItemHolder {
+    private final Map<String, List<ConfigDocItem>> configGroupConfigItems;
+    private final Map<ConfigRootInfo, List<ConfigDocItem>> configRootConfigItems;
+    private final Map<String, ConfigRootInfo> configRootClassToConfigRootInfo = new HashMap<>();
 
     public ScannedConfigDocsItemHolder() {
+        this(new HashMap<>(), new HashMap<>());
     }
 
-    public Map<String, List<ConfigDocItem>> getAllConfigItemsPerExtension() {
-        return allConfigItemsPerExtension;
+    public ScannedConfigDocsItemHolder(Map<ConfigRootInfo, List<ConfigDocItem>> configRootConfigItems,
+            Map<String, List<ConfigDocItem>> configGroupConfigItems) {
+        this.configRootConfigItems = configRootConfigItems;
+        this.configGroupConfigItems = configGroupConfigItems;
     }
 
     public Map<String, List<ConfigDocItem>> getConfigGroupConfigItems() {
         return configGroupConfigItems;
     }
 
-    public void addToAllConfigItems(String configRootName, List<ConfigDocItem> configDocItems) {
-        allConfigItemsPerExtension.put(configRootName, configDocItems);
+    public Map<ConfigRootInfo, List<ConfigDocItem>> getConfigRootConfigItems() {
+        return configRootConfigItems;
     }
 
     public void addConfigGroupItems(String configGroupName, List<ConfigDocItem> configDocItems) {
         configGroupConfigItems.put(configGroupName, configDocItems);
     }
 
+    public void addConfigRootItems(ConfigRootInfo configRoot, List<ConfigDocItem> configDocItems) {
+        configRootConfigItems.put(configRoot, configDocItems);
+        configRootClassToConfigRootInfo.put(configRoot.getClazz().getQualifiedName().toString(), configRoot);
+    }
+
+    public List<ConfigDocItem> getConfigItemsByRootClassName(String configRootClassName) {
+        ConfigRootInfo configRootInfo = configRootClassToConfigRootInfo.get(configRootClassName);
+        if (configRootInfo == null) {
+            return null;
+        }
+
+        return configRootConfigItems.get(configRootInfo);
+    }
+
     public boolean isEmpty() {
-        return allConfigItemsPerExtension.isEmpty();
+        return configRootConfigItems.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "ScannedConfigDocsItemHolder{" +
+                ", configRootConfigItems=" + configRootConfigItems +
+                ", configGroupConfigItems=" + configGroupConfigItems +
+                '}';
     }
 }
