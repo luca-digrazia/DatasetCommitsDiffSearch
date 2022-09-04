a3836e99c1fc829e5576e025be2275ddfb10e3ac
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:30:56 +0200

    Delaying searches cleanup job by one hour to prevent clashing with migrations.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/plugins/views/search/db/SearchesCleanUpJob.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/plugins/views/search/db/SearchesCleanUpJob.java
@@ -1,23 +1,23 @@
-/*
- * Copyright (C) 2020 Graylog, Inc.
+/**
+ * This file is part of Graylog.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog.plugins.views.search.db;
 
-import org.graylog.plugins.views.search.views.ViewSummaryDTO;
-import org.graylog.plugins.views.search.views.ViewSummaryService;
+import org.graylog.plugins.views.search.views.ViewDTO;
+import org.graylog.plugins.views.search.views.ViewService;
 import org.graylog2.plugin.periodical.Periodical;
 import org.joda.time.Duration;
 import org.joda.time.Instant;
@@ -32,15 +32,15 @@ import java.util.stream.Collectors;
 public class SearchesCleanUpJob extends Periodical {
     private static final Logger LOG = LoggerFactory.getLogger(SearchesCleanUpJob.class);
 
-    private final ViewSummaryService viewSummaryService;
+    private final ViewService viewService;
     private final SearchDbService searchDbService;
     private final Instant mustNotBeOlderThan;
 
     @Inject
-    public SearchesCleanUpJob(ViewSummaryService viewSummaryService,
+    public SearchesCleanUpJob(ViewService viewService,
                               SearchDbService searchDbService,
                               @Named("views_maximum_search_age") Duration maximumSearchAge) {
-        this.viewSummaryService = viewSummaryService;
+        this.viewService = viewService;
         this.searchDbService = searchDbService;
         this.mustNotBeOlderThan = Instant.now().minus(maximumSearchAge);
     }
@@ -87,7 +87,9 @@ public class SearchesCleanUpJob extends Periodical {
 
     @Override
     public void doRun() {
-        final Set<String> requiredIds = viewSummaryService.streamAll().map(ViewSummaryDTO::searchId).collect(Collectors.toSet());
-        searchDbService.getExpiredSearches(requiredIds, mustNotBeOlderThan).forEach(id -> searchDbService.delete(id));
+        final Set<String> requiredIds = viewService.streamAll().map(ViewDTO::searchId).collect(Collectors.toSet());
+        searchDbService.streamAll()
+                .filter(search -> search.createdAt().isBefore(mustNotBeOlderThan) && !requiredIds.contains(search.id()))
+                .forEach(search -> searchDbService.delete(search.id()));
     }
 }
