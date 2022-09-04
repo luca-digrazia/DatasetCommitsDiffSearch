8152adfcae7bfb8450a4b2a64cb8b49bd881ee9e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:32:37 +0200

    Refactor TvShowsApplication to improve plus method

--- dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/TvShowsApplication.java
+++ dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/TvShowsApplication.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.github.pedrovgs.effectiveandroidui;
 
 import android.app.Application;
@@ -39,23 +24,6 @@ public class TvShowsApplication extends Application {
     initializeDependencyInjector();
   }
 
-  /*
-   * We could use this code to enable or disable night mode or eve use a auto night mode.
-   * But to use this feature we have to enable car mode and the UX is not the expected :S
-   * If you enable car mode the application is going to show a persistent notification!
-   *
-   * Use this method inside the onCreate and create a new "values-night" directory with some
-   * color changes to show how it works.
-   *
-   */
-  private void initializeUiManager() {
-    /*
-    UiModeManager uiModeManager = (UiModeManager) getSystemService(UI_MODE_SERVICE);
-    uiModeManager.enableCarMode(0);
-    uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_AUTO);
-    */
-  }
-
   /**
    * Inject every dependency declared in the object with the @Inject annotation if the dependency
    * has been already declared in a module and already initialized by Dagger.
@@ -72,16 +40,18 @@ public class TvShowsApplication extends Application {
    *
    * @param modules used to populate the dependency container.
    */
-  public ObjectGraph plus(List<Object> modules) {
+  public void plus(List<Object> modules) {
     if (modules == null) {
       throw new IllegalArgumentException(
           "You can't plus a null module, review your getModules() implementation");
     }
-    return objectGraph.plus(modules.toArray());
+    for (Object module : modules) {
+      objectGraph.plus(modules);
+    }
   }
 
   private void initializeDependencyInjector() {
-    objectGraph = ObjectGraph.create(new RootModule(this));
+    objectGraph = ObjectGraph.create(new RootModule());
     objectGraph.inject(this);
     objectGraph.injectStatics();
   }
