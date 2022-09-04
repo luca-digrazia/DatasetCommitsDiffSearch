a251fa87d1b1f5f645f008024425c0b946b682b9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:24:09 +0200

    missed a mock
        fixes Graylog2/graylog2-web-interface#642

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/alerts/types/FieldValueAlertConditionTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/alerts/types/FieldValueAlertConditionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 TORCH GmbH
+ * Copyright 2014 TORCH GmbH
  *
  * This file is part of Graylog2.
  *
@@ -19,6 +19,7 @@
 
 package org.graylog2.alerts.types;
 
+import org.graylog2.alerts.Alert;
 import org.graylog2.alerts.AlertCondition;
 import org.graylog2.alerts.AlertConditionTest;
 import org.graylog2.indexer.IndexHelper;
@@ -27,6 +28,7 @@ import org.graylog2.indexer.searches.Searches;
 import org.graylog2.indexer.searches.timeranges.RelativeRange;
 import org.graylog2.plugin.Tools;
 import org.mockito.Matchers;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.testng.annotations.Test;
 
 import java.util.Map;
@@ -35,13 +37,14 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.*;
 
 /**
  * @author Dennis Oelkers <dennis@torch.sh>
  */
-@Test(enabled=false)
+@PrepareForTest(Alert.class)
 public class FieldValueAlertConditionTest extends AlertConditionTest {
+    @Test
     public void testConstructor() throws Exception {
         Map<String, Object> parameters = getParametersMap(0,
                 0,
@@ -56,6 +59,7 @@ public class FieldValueAlertConditionTest extends AlertConditionTest {
         assertNotNull(fieldValueAlertCondition.getDescription());
     }
 
+    @Test
     public void testRunCheckHigherPositive() throws Exception {
         for (FieldValueAlertCondition.CheckType checkType : FieldValueAlertCondition.CheckType.values()) {
             final double threshold = 50.0;
@@ -66,12 +70,13 @@ public class FieldValueAlertConditionTest extends AlertConditionTest {
             fieldStatsShouldReturn(getFieldStatsResult(checkType, higherThanThreshold));
             alertLastTriggered(-1);
 
-            AlertCondition.CheckResult result = alertService.triggered(fieldValueAlertCondition, indexer);
+            AlertCondition.CheckResult result = fieldValueAlertCondition.triggered();
 
             assertTriggered(fieldValueAlertCondition, result);
         }
     }
 
+    @Test
     public void testRunCheckHigherNegative() throws Exception {
         for (FieldValueAlertCondition.CheckType checkType : FieldValueAlertCondition.CheckType.values()) {
             final double threshold = 50.0;
@@ -83,12 +88,13 @@ public class FieldValueAlertConditionTest extends AlertConditionTest {
             fieldStatsShouldReturn(getFieldStatsResult(checkType, lowerThanThreshold));
             alertLastTriggered(-1);
 
-            AlertCondition.CheckResult result = alertService.triggered(fieldValueAlertCondition, indexer);
+            AlertCondition.CheckResult result = fieldValueAlertCondition.triggered();
 
             assertNotTriggered(result);
         }
     }
 
+    @Test
     public void testRunCheckLowerPositive() throws Exception {
         for (FieldValueAlertCondition.CheckType checkType : FieldValueAlertCondition.CheckType.values()) {
             final double threshold = 50.0;
@@ -100,12 +106,13 @@ public class FieldValueAlertConditionTest extends AlertConditionTest {
             fieldStatsShouldReturn(getFieldStatsResult(checkType, lowerThanThreshold));
             alertLastTriggered(-1);
 
-            AlertCondition.CheckResult result = alertService.triggered(fieldValueAlertCondition, indexer);
+            AlertCondition.CheckResult result = fieldValueAlertCondition.triggered();
 
             assertTriggered(fieldValueAlertCondition, result);
         }
     }
 
+    @Test
     public void testRunCheckLowerNegative() throws Exception {
         for (FieldValueAlertCondition.CheckType checkType : FieldValueAlertCondition.CheckType.values()) {
             final double threshold = 50.0;
@@ -117,7 +124,7 @@ public class FieldValueAlertConditionTest extends AlertConditionTest {
             fieldStatsShouldReturn(getFieldStatsResult(checkType, higherThanThreshold));
             alertLastTriggered(-1);
 
-            AlertCondition.CheckResult result = alertService.triggered(fieldValueAlertCondition, indexer);
+            AlertCondition.CheckResult result = fieldValueAlertCondition.triggered();
 
             assertNotTriggered(result);
         }
@@ -138,7 +145,7 @@ public class FieldValueAlertConditionTest extends AlertConditionTest {
     }
 
     protected FieldValueAlertCondition getFieldValueAlertCondition(Map<String,Object> parameters) {
-        return new FieldValueAlertCondition(
+        return new FieldValueAlertCondition(core,
                 stream,
                 CONDITION_ID,
                 Tools.iso8601(),
