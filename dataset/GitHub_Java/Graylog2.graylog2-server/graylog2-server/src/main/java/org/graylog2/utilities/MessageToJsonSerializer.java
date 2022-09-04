e1ac99cadd773f1a19048f131989ba451e8d0ff5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:58:10 +0200

    Add TODO about the massive stream/input instance creation.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/utilities/MessageToJsonSerializer.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/utilities/MessageToJsonSerializer.java
@@ -171,7 +171,7 @@ public class MessageToJsonSerializer {
                                 if (input != null) {
                                     try {
                                         // TODO This might create lots of MessageInput instances. Can we avoid this?
-                                        return inputService.buildMessageInput(input);
+                                        return inputService.getMessageInput(input);
                                     } catch (NoSuchInputTypeException e) {
                                         return null;
                                     }
