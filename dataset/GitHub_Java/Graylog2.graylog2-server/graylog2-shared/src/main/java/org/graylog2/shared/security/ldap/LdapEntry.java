8bbc765136f0fdba4e3b1953535da43155488649
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:40:07 +0200

    add equals/hashcode to LdapEntry to be on the safe side

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/security/ldap/LdapEntry.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/security/ldap/LdapEntry.java
@@ -21,7 +21,6 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
 import java.util.Collection;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -50,11 +49,11 @@ public class LdapEntry {
     }
 
     public String get(String key) {
-        return attributes.get(key.toLowerCase(Locale.ENGLISH));
+        return attributes.get(key.toLowerCase());
     }
 
     public String put(String key, String value) {
-        return attributes.put(key.toLowerCase(Locale.ENGLISH), value);
+        return attributes.put(key.toLowerCase(), value);
     }
 
     public void addGroups(Collection<String> groups) {
