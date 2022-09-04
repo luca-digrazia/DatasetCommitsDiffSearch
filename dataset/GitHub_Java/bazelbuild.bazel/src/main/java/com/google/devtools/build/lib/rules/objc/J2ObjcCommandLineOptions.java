164fc969902f39b916378430c46885551ed0e2b5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:11:11 +0200

    Turn the --explicit_jre_deps flag into a noop.     RELNOTES: Flag --explicit_jre_deps is now a noop.
        PiperOrigin-RevId: 156570342

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/J2ObjcCommandLineOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/J2ObjcCommandLineOptions.java
@@ -14,14 +14,13 @@
 
 package com.google.devtools.build.lib.rules.objc;
 
+import com.google.common.collect.Multimap;
 import com.google.devtools.build.lib.analysis.config.BuildConfiguration.LabelConverter;
 import com.google.devtools.build.lib.analysis.config.FragmentOptions;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.common.options.Converters;
 import com.google.devtools.common.options.Option;
-import com.google.devtools.common.options.OptionDocumentationCategory;
 import com.google.devtools.common.options.OptionsParser.OptionUsageRestrictions;
-import com.google.devtools.common.options.proto.OptionFilters.OptionEffectTag;
 import java.util.List;
 
 /**
@@ -36,8 +35,6 @@ public class J2ObjcCommandLineOptions extends FragmentOptions {
     allowMultiple = true,
     defaultValue = "",
     category = "flags",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help = "Additional options to pass to the J2ObjC tool."
   )
   public List<String> translationFlags;
@@ -46,8 +43,6 @@ public class J2ObjcCommandLineOptions extends FragmentOptions {
     name = "j2objc_dead_code_removal",
     defaultValue = "false",
     optionUsageRestrictions = OptionUsageRestrictions.UNDOCUMENTED,
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help =
         "Whether to perform J2ObjC dead code removal to strip unused code from the final app "
             + "bundle."
@@ -59,8 +54,6 @@ public class J2ObjcCommandLineOptions extends FragmentOptions {
     defaultValue = "null",
     optionUsageRestrictions = OptionUsageRestrictions.UNDOCUMENTED,
     converter = LabelConverter.class,
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help =
         "Allows J2ObjC to strip dead code reported by ProGuard. Takes a label that can "
             + "generate a dead code report as argument."
@@ -71,8 +64,6 @@ public class J2ObjcCommandLineOptions extends FragmentOptions {
     name = "explicit_jre_deps",
     defaultValue = "true",
     category = "flags",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help = "This flag is a noop and will be removed."
   )
   public boolean explicitJreDeps;
@@ -81,9 +72,10 @@ public class J2ObjcCommandLineOptions extends FragmentOptions {
     name = "experimental_j2objc_header_map",
     defaultValue = "true",
     category = "flags",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
     help = "Whether to generate J2ObjC header map in parallel of J2ObjC transpilation."
   )
   public boolean experimentalJ2ObjcHeaderMap;
+
+  @Override
+  public void addAllLabels(Multimap<String, Label> labelMap) {}
 }
