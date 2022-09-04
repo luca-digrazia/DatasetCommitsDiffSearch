4ab550a71a81e5e5c49a40622f674001c65029b4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:23:52 +0200

    Enable AddExtensionIT.testAddExtensionWithASingleExtensionToSubmodule

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/maven/src/test/java/io/quarkus/maven/it/AddExtensionIT.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/maven/src/test/java/io/quarkus/maven/it/AddExtensionIT.java
@@ -10,9 +10,11 @@ import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
 
 import org.apache.maven.model.Dependency;
+import org.apache.maven.model.DependencyManagement;
 import org.apache.maven.model.Model;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.InvocationRequest;
@@ -26,6 +28,7 @@ import org.junit.jupiter.api.Test;
 class AddExtensionIT extends QuarkusPlatformAwareMojoTestBase {
 
     private static final String QUARKUS_GROUPID = "io.quarkus";
+    private static final String BOM_ARTIFACT_ID = "quarkus-bom";
     private static final String VERTX_ARTIFACT_ID = "quarkus-vertx";
     private static final String COMMONS_IO = "commons-io";
     private static final String PROJECT_SOURCE_DIR = "projects/classic";
@@ -43,6 +46,23 @@ class AddExtensionIT extends QuarkusPlatformAwareMojoTestBase {
         expected.setGroupId(QUARKUS_GROUPID);
         expected.setArtifactId(VERTX_ARTIFACT_ID);
         assertThat(contains(model.getDependencies(), expected)).isTrue();
+    }
+
+    @Test
+    void testAddExtensionWithASingleExtensionToSubmodule() throws MavenInvocationException, IOException {
+        testDir = initProject("projects/multimodule", "projects/testAddExtensionWithASingleExtensionToSubmodule");
+        testDir = new File(testDir, "runner");
+        invoker = initInvoker(testDir);
+        addExtension(false, VERTX_ARTIFACT_ID);
+
+        Model model = loadPom(testDir);
+        Dependency expected = new Dependency();
+        expected.setGroupId(QUARKUS_GROUPID);
+        expected.setArtifactId(VERTX_ARTIFACT_ID);
+        assertThat(contains(model.getDependencies(), expected)).isTrue();
+
+        assertThat(Optional.ofNullable(model.getDependencyManagement())
+                .map(DependencyManagement::getDependencies).orElse(Collections.emptyList())).isEmpty();
     }
 
     @Test
@@ -112,8 +132,8 @@ class AddExtensionIT extends QuarkusPlatformAwareMojoTestBase {
         request.setGoals(Collections.singletonList(
                 getMavenPluginGroupId() + ":" + getMavenPluginArtifactId() + ":" + getMavenPluginVersion() + ":add-extension"));
         Properties properties = new Properties();
-        properties.setProperty("platformGroupId", "io.quarkus");
-        properties.setProperty("platformArtifactId", "quarkus-bom");
+        properties.setProperty("platformGroupId", QUARKUS_GROUPID);
+        properties.setProperty("platformArtifactId", BOM_ARTIFACT_ID);
         properties.setProperty("platformVersion", getQuarkusCoreVersion());
         if (plural) {
             properties.setProperty("extensions", ext);
@@ -121,7 +141,7 @@ class AddExtensionIT extends QuarkusPlatformAwareMojoTestBase {
             properties.setProperty("extension", ext);
         }
         request.setProperties(properties);
-        getEnv().forEach(request::addShellEnvironment);
+
         File log = new File(testDir, "build-add-extension-" + testDir.getName() + ".log");
         PrintStreamLogger logger = new PrintStreamLogger(new PrintStream(new FileOutputStream(log), false, "UTF-8"),
                 InvokerLogger.DEBUG);
