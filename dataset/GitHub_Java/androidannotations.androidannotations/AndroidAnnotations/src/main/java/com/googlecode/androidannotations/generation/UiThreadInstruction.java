7df729be6c0d5f08d9ff05b2980a2a26a50826ce
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:30:36 +0200

    adding a message to the exception handling, and fix of a potential bug with public methods on @Background and @UiThread

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/src/main/java/com/googlecode/androidannotations/generation/UiThreadInstruction.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/src/main/java/com/googlecode/androidannotations/generation/UiThreadInstruction.java
@@ -15,35 +15,69 @@
  */
 package com.googlecode.androidannotations.generation;
 
+import java.util.List;
+
 import com.googlecode.androidannotations.model.Instruction;
 
 public class UiThreadInstruction implements Instruction {
 
 	private static final String FORMAT = //
 	"" + //
-			"        @Override\n" + //
-			"        protected void %s() {\n" + //
-			"		 	runOnUiThread(new Runnable() {\n" + //
-			"		      public void run() {\n" + //
-			"               %s.super.%s();\n" + //
-			"		      }\n" + //
-			"           });\n" + //
-			"		 }\n" + //
+			"    @Override\n" + //
+			"    public void %s(%s) {\n" + //
+			"        runOnUiThread(new Runnable() {\n" + //
+			"            public void run() {\n" + //
+			"                try {\n" + //
+			"                    %s.super.%s(%s);\n" + //
+			"                } catch (RuntimeException e) {\n" + //
+			"                    android.util.Log.e(\"%s\", \"A runtime exception was thrown while executing code in the ui thread\", e);\n" + //
+			"                }\n" + //
+			"            }\n" + //
+			"        });\n" + //
+			"    }\n" + //
 			"\n";
 
 	private final String methodName;
 
 	private final String className;
 
+	private final List<String> methodArguments;
 
-	public UiThreadInstruction(String className, String methodName) {
+	private final List<String> methodParameters;
+
+	public UiThreadInstruction(String className, String methodName, List<String> methodArguments, List<String> methodParameters) {
 		this.className = className;
 		this.methodName = methodName;
+		this.methodArguments = methodArguments;
+		this.methodParameters = methodParameters;
 	}
 
 	@Override
 	public String generate() {
-		return String.format(FORMAT, methodName, className, methodName);
+
+		StringBuilder arguments = new StringBuilder();
+		boolean first = true;
+		for(String argument : methodArguments) {
+			if (first) {
+				first = false;
+			} else {
+				arguments.append(", ");
+			}
+			arguments.append("final ").append(argument);
+		}
+		
+		first = true;
+		StringBuilder parameters = new StringBuilder();
+		for(String parameter : methodParameters) {
+			if (first) {
+				first = false;
+			} else {
+				parameters.append(", ");
+			}
+			parameters.append(parameter);
+		}
+
+		return String.format(FORMAT, methodName, arguments.toString(), className, methodName, parameters.toString(), className);
 	}
 
 }
