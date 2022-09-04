fe59f13c71e2ca6cb60563c69ae34408ccf40be7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:08:21 +0200

    Replace Streams usage with String#join (#3646)
        We can use `String.join()` in a couple of places to replace usage of the Streams API.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-configuration/src/main/java/io/dropwizard/configuration/BaseConfigurationFactory.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-configuration/src/main/java/io/dropwizard/configuration/BaseConfigurationFactory.java
@@ -1,18 +1,5 @@
 package io.dropwizard.configuration;
 
-import static java.util.Objects.requireNonNull;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
-
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
@@ -25,8 +12,21 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import com.fasterxml.jackson.databind.node.TreeTraversingParser;
-import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
+
+import javax.annotation.Nullable;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * A generic factory class for loading configuration files, binding them to configuration objects, and
@@ -37,19 +37,24 @@ import com.google.common.base.Splitter;
 public abstract class BaseConfigurationFactory<T> implements ConfigurationFactory<T> {
 
     private static final Pattern ESCAPED_COMMA_PATTERN = Pattern.compile("\\\\,");
-    private static final Splitter ESCAPED_COMMA_SPLITTER = Splitter.on(Pattern.compile("(?<!\\\\),")).trimResults();
+    private static final Pattern ESCAPED_COMMA_SPLIT_PATTERN = Pattern.compile("(?<!\\\\),");
     private static final Pattern ESCAPED_DOT_PATTERN = Pattern.compile("\\\\\\.");
-    private static final Splitter ESCAPED_DOT_SPLITTER = Splitter.on(Pattern.compile("(?<!\\\\)\\.")).trimResults();
+    private static final Pattern ESCAPED_DOT_SPLIT_PATTERN = Pattern.compile("(?<!\\\\)\\.");
 
     private final Class<T> klass;
     private final String propertyPrefix;
     protected final ObjectMapper mapper;
+    private final ConfigurationMetadata configurationMetadata;
+
+    @Nullable
     private final Validator validator;
+
     private final String formatName;
     private final JsonFactory parserFactory;
 
     /**
      * Creates a new configuration factory for the given class.
+     *
      * @param parserFactory  the factory that creates the parser used
      * @param formatName     the name of the format parsed by this factory (used in exceptions)
      * @param klass          the configuration class
@@ -59,22 +64,16 @@ public abstract class BaseConfigurationFactory<T> implements ConfigurationFactor
     public BaseConfigurationFactory(JsonFactory parserFactory,
                                     String formatName,
                                     Class<T> klass,
-                                    Validator validator,
+                                    @Nullable Validator validator,
                                     ObjectMapper objectMapper,
                                     String propertyPrefix) {
         this.klass = klass;
         this.formatName = formatName;
-        this.propertyPrefix = (propertyPrefix == null || propertyPrefix.endsWith("."))
-                ? propertyPrefix : (propertyPrefix + '.');
-        // Sub-classes may choose to omit data-binding; if so, null ObjectMapper passed:
-        if (objectMapper == null) { // sub-class has no need for mapper
-            this.mapper = null;
-            this.parserFactory = null;
-        } else {
-            this.mapper = objectMapper;
-            this.parserFactory = parserFactory;
-        }
+        this.propertyPrefix = propertyPrefix.endsWith(".") ? propertyPrefix : propertyPrefix + '.';
+        this.mapper = objectMapper;
+        this.parserFactory = parserFactory;
         this.validator = validator;
+        this.configurationMetadata = new ConfigurationMetadata(mapper, klass);
     }
 
     @Override
@@ -84,33 +83,35 @@ public abstract class BaseConfigurationFactory<T> implements ConfigurationFactor
 
             if (node == null) {
                 throw ConfigurationParsingException
-                        .builder("Configuration at " + path + " must not be empty")
-                        .build(path);
+                    .builder("Configuration at " + path + " must not be empty")
+                    .build(path);
             }
 
             return build(node, path);
         } catch (JsonParseException e) {
             throw ConfigurationParsingException
-            .builder("Malformed " + formatName)
-            .setCause(e)
-            .setLocation(e.getLocation())
-            .setDetail(e.getMessage())
-            .build(path);
+                .builder("Malformed " + formatName)
+                .setCause(e)
+                .setLocation(e.getLocation())
+                .setDetail(e.getMessage())
+                .build(path);
         }
     }
 
     protected JsonParser createParser(InputStream input) throws IOException {
         return parserFactory.createParser(input);
-    };
+    }
 
     @Override
     public T build() throws IOException, ConfigurationException {
         try {
-            final JsonNode node = mapper.valueToTree(klass.newInstance());
+            final T instance = klass.getDeclaredConstructor().newInstance();
+            final JsonNode node = mapper.valueToTree(instance);
             return build(node, "default configuration");
-        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | SecurityException e) {
-            throw new IllegalArgumentException("Unable create an instance " +
-                    "of the configuration class: '" + klass.getCanonicalName() + "'", e);
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | SecurityException
+                | NoSuchMethodException | InvocationTargetException e) {
+            throw new IllegalArgumentException("Unable to create an instance " +
+                "of the configuration class: '" + klass.getCanonicalName() + "'", e);
         }
     }
 
@@ -129,37 +130,38 @@ public abstract class BaseConfigurationFactory<T> implements ConfigurationFactor
             return config;
         } catch (UnrecognizedPropertyException e) {
             final List<String> properties = e.getKnownPropertyIds().stream()
-                    .map(Object::toString)
-                    .collect(Collectors.toList());
+                .map(Object::toString)
+                .collect(Collectors.toList());
             throw ConfigurationParsingException.builder("Unrecognized field")
-                    .setFieldPath(e.getPath())
-                    .setLocation(e.getLocation())
-                    .addSuggestions(properties)
-                    .setSuggestionBase(e.getPropertyName())
-                    .setCause(e)
-                    .build(path);
+                .setFieldPath(e.getPath())
+                .setLocation(e.getLocation())
+                .addSuggestions(properties)
+                .setSuggestionBase(e.getPropertyName())
+                .setCause(e)
+                .build(path);
         } catch (InvalidFormatException e) {
             final String sourceType = e.getValue().getClass().getSimpleName();
             final String targetType = e.getTargetType().getSimpleName();
             throw ConfigurationParsingException.builder("Incorrect type of value")
-                    .setDetail("is of type: " + sourceType + ", expected: " + targetType)
-                    .setLocation(e.getLocation())
-                    .setFieldPath(e.getPath())
-                    .setCause(e)
-                    .build(path);
+                .setDetail("is of type: " + sourceType + ", expected: " + targetType)
+                .setLocation(e.getLocation())
+                .setFieldPath(e.getPath())
+                .setCause(e)
+                .build(path);
         } catch (JsonMappingException e) {
             throw ConfigurationParsingException.builder("Failed to parse configuration")
-                    .setDetail(e.getMessage())
-                    .setFieldPath(e.getPath())
-                    .setLocation(e.getLocation())
-                    .setCause(e)
-                    .build(path);
+                .setDetail(e.getMessage())
+                .setFieldPath(e.getPath())
+                .setLocation(e.getLocation())
+                .setCause(e)
+                .build(path);
         }
     }
 
     protected void addOverride(JsonNode root, String name, String value) {
         JsonNode node = root;
-        final List<String> parts = ESCAPED_DOT_SPLITTER.splitToList(name).stream()
+        final List<String> parts = Arrays.stream(ESCAPED_DOT_SPLIT_PATTERN.split(name))
+                .map(String::trim)
                 .map(key -> ESCAPED_DOT_PATTERN.matcher(key).replaceAll("."))
                 .collect(Collectors.toList());
         for (int i = 0; i < parts.size(); i++) {
@@ -170,7 +172,7 @@ public abstract class BaseConfigurationFactory<T> implements ConfigurationFactor
             }
             final ObjectNode obj = (ObjectNode) node;
 
-            final String remainingPath = Joiner.on('.').join(parts.subList(i, parts.size()));
+            final String remainingPath = String.join(".", parts.subList(i, parts.size()));
             if (obj.has(remainingPath) && !remainingPath.equals(key)) {
                 if (obj.get(remainingPath).isValueNode()) {
                     obj.put(remainingPath, value);
@@ -187,14 +189,14 @@ public abstract class BaseConfigurationFactory<T> implements ConfigurationFactor
                 child = obj.get(key.substring(0, s));
                 if (child == null) {
                     throw new IllegalArgumentException("Unable to override " + name +
-                            "; node with index not found.");
+                        "; node with index not found.");
                 }
                 if (!child.isArray()) {
                     throw new IllegalArgumentException("Unable to override " + name +
-                            "; node with index is not an array.");
+                        "; node with index is not an array.");
                 } else if (index >= child.size()) {
                     throw new ArrayIndexOutOfBoundsException("Unable to override " + name +
-                            "; index is greater than size of array.");
+                        "; index is greater than size of array.");
                 }
                 if (moreParts) {
                     child = child.get(index);
@@ -212,18 +214,24 @@ public abstract class BaseConfigurationFactory<T> implements ConfigurationFactor
                 }
                 if (child.isArray()) {
                     throw new IllegalArgumentException("Unable to override " + name +
-                            "; target is an array but no index specified");
+                        "; target is an array but no index specified");
                 }
                 node = child;
             }
 
             if (!moreParts) {
-                if (node.get(key) != null && node.get(key).isArray()) {
-                    final ArrayNode arrayNode = (ArrayNode) obj.get(key);
-                    arrayNode.removeAll();
-                    for (String val : ESCAPED_COMMA_SPLITTER.split(value)) {
-                        arrayNode.add(ESCAPED_COMMA_PATTERN.matcher(val).replaceAll(","));
+                if ((node.get(key) != null && node.get(key).isArray())
+                    || (node.get(key) == null && configurationMetadata.isCollectionOfStrings(name))) {
+                    ArrayNode arrayNode = (ArrayNode) obj.get(key);
+                    if (arrayNode == null) {
+                        arrayNode = obj.arrayNode();
+                        obj.set(key, arrayNode);
                     }
+                    arrayNode.removeAll();
+                    Arrays.stream(ESCAPED_COMMA_SPLIT_PATTERN.split(value))
+                            .map(String::trim)
+                            .map(val -> ESCAPED_COMMA_PATTERN.matcher(val).replaceAll(","))
+                            .forEach(arrayNode::add);
                 } else {
                     obj.put(key, value);
                 }
