6b777c36a1ff9500b9ef24865db45ca6491f1307
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:38:10 +0200

    Enhenced Jackson#newObjectMapper to take an custom provided instance of com.fasterxml.jackson.core.JsonFactory. This allows further customizations of the ObjectMapper.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jackson/src/main/java/io/dropwizard/jackson/Jackson.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jackson/src/main/java/io/dropwizard/jackson/Jackson.java
@@ -25,11 +25,12 @@ public class Jackson {
 
     /**
      * Creates a new {@link ObjectMapper} with a custom {@link com.fasterxml.jackson.core.JsonFactory}
-     * with Guava, Logback, and Joda Time support, as well as support for {@link JsonSnakeCase}.
-     * Also includes all {@link Discoverable} interface implementations.
+     * and with support for Guava, Logback, and Joda Time support, as well as
+     * support for {@link JsonSnakeCase}. Also includes all {@link Discoverable} interface implementations.
      *
      * @param jsonFactory instance of {@link com.fasterxml.jackson.core.JsonFactory} to use
      *                    for the created {@link com.fasterxml.jackson.databind.ObjectMapper} instance.
+     *                    Must not be {@code null}
      */
     public static ObjectMapper newObjectMapper(JsonFactory jsonFactory) {
         final ObjectMapper mapper = new ObjectMapper(jsonFactory);
