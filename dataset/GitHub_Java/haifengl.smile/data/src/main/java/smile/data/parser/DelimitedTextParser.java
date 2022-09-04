7e28beae90ff221d79a11a85b745b88517adcba6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:40:33 +0200

    Adds the possibility to ignore columns with DelimitedTextParser by setting a list of column indices to ignore.

--- dataset/GitHub_Java/haifengl.smile/data/src/main/java/smile/data/parser/DelimitedTextParser.java
+++ dataset/GitHub_Java/haifengl.smile/data/src/main/java/smile/data/parser/DelimitedTextParser.java
@@ -360,6 +360,10 @@ public class DelimitedTextParser {
 
         int ncols = attributes.length;
 
+        if (hasRowNames) {
+            ncols++;
+        }
+
         if (responseIndex >= 0) {
             ncols++;
         }
