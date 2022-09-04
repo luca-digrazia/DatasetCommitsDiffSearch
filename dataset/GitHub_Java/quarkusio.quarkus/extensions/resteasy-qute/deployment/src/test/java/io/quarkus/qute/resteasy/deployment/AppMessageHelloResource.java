08fb0e2d2eadc1edda815b3c4b873c81e2b224f2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:55:01 +0200

    Fix CNFE when MessageBundles are used with Dev-mode
        Fixes: #11188

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/resteasy-qute/deployment/src/test/java/io/quarkus/qute/resteasy/deployment/AppMessageHelloResource.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/resteasy-qute/deployment/src/test/java/io/quarkus/qute/resteasy/deployment/AppMessageHelloResource.java
@@ -1,7 +1,5 @@
 package io.quarkus.qute.resteasy.deployment;
 
-import java.util.Locale;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,7 +8,6 @@ import javax.ws.rs.core.MediaType;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import io.quarkus.qute.i18n.MessageBundles;
 
 @Path("hello")
 public class AppMessageHelloResource {
@@ -22,12 +19,5 @@ public class AppMessageHelloResource {
     @Produces(MediaType.TEXT_PLAIN)
     public TemplateInstance get() {
         return hello.instance();
-    }
-
-    @GET
-    @Path("de")
-    @Produces(MediaType.TEXT_PLAIN)
-    public TemplateInstance helloDe() {
-        return hello.instance().setAttribute(MessageBundles.ATTRIBUTE_LOCALE, Locale.GERMAN);
     }
 }
