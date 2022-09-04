b4ed8151b47b669c52f0849e260b73a55189f8c7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:48:46 +0200

    De-register smallrye context manager on shutdown
        Fixes #18864

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/deployment/src/main/java/io/quarkus/smallrye/context/deployment/SmallRyeContextPropagationProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/deployment/src/main/java/io/quarkus/smallrye/context/deployment/SmallRyeContextPropagationProcessor.java
@@ -1,48 +1,88 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.quarkus.smallrye.context.deployment;
 
-import org.jboss.logging.Logger;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.context.spi.ContextManagerExtension;
+import org.eclipse.microprofile.context.spi.ThreadContextProvider;
 
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
-import io.quarkus.arc.deployment.BeanContainerBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.deployment.Feature;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.ExecutorBuildItem;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
+import io.quarkus.deployment.util.ServiceUtil;
 import io.quarkus.smallrye.context.runtime.SmallRyeContextPropagationProvider;
-import io.quarkus.smallrye.context.runtime.SmallRyeContextPropagationTemplate;
+import io.quarkus.smallrye.context.runtime.SmallRyeContextPropagationRecorder;
+import io.smallrye.context.SmallRyeManagedExecutor;
 
 /**
  * The deployment processor for MP-CP applications
  */
 class SmallRyeContextPropagationProcessor {
-    private static final Logger log = Logger.getLogger(SmallRyeContextPropagationProcessor.class.getName());
 
     @BuildStep
-    AdditionalBeanBuildItem registerBean() {
-        return AdditionalBeanBuildItem.builder().addBeanClass(SmallRyeContextPropagationProvider.class).build();
+    void registerBean(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
+        additionalBeans
+                .produce(AdditionalBeanBuildItem.unremovableOf(SmallRyeContextPropagationProvider.class));
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.STATIC_INIT)
+    void buildStatic(SmallRyeContextPropagationRecorder recorder)
+            throws ClassNotFoundException, IOException {
+        List<ThreadContextProvider> discoveredProviders = new ArrayList<>();
+        List<ContextManagerExtension> discoveredExtensions = new ArrayList<>();
+        for (Class<?> provider : ServiceUtil.classesNamedIn(Thread.currentThread().getContextClassLoader(),
+                "META-INF/services/" + ThreadContextProvider.class.getName())) {
+            try {
+                discoveredProviders.add((ThreadContextProvider) provider.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException("Failed to instantiate declared ThreadContextProvider class: " + provider.getName(),
+                        e);
+            }
+        }
+        for (Class<?> extension : ServiceUtil.classesNamedIn(Thread.currentThread().getContextClassLoader(),
+                "META-INF/services/" + ContextManagerExtension.class.getName())) {
+            try {
+                discoveredExtensions.add((ContextManagerExtension) extension.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException("Failed to instantiate declared ThreadContextProvider class: " + extension.getName(),
+                        e);
+            }
+        }
+
+        recorder.configureStaticInit(discoveredProviders, discoveredExtensions);
     }
 
     @BuildStep
     @Record(ExecutionTime.RUNTIME_INIT)
-    void build(SmallRyeContextPropagationTemplate template,
-            BeanContainerBuildItem beanContainer,
-            ExecutorBuildItem executorBuildItem) {
-        template.configure(beanContainer.getValue(), executorBuildItem.getExecutorProxy());
+    void build(SmallRyeContextPropagationRecorder recorder,
+            ExecutorBuildItem executorBuildItem,
+            ShutdownContextBuildItem shutdownContextBuildItem,
+            BuildProducer<FeatureBuildItem> feature,
+            BuildProducer<SyntheticBeanBuildItem> syntheticBeans) {
+        feature.produce(new FeatureBuildItem(Feature.SMALLRYE_CONTEXT_PROPAGATION));
+
+        recorder.configureRuntime(executorBuildItem.getExecutorProxy(), shutdownContextBuildItem);
+
+        // Synthetic bean for ManagedExecutor
+        syntheticBeans.produce(
+                SyntheticBeanBuildItem.configure(SmallRyeManagedExecutor.class)
+                        .scope(ApplicationScoped.class)
+                        .addType(ManagedExecutor.class)
+                        .defaultBean()
+                        .unremovable()
+                        .supplier(recorder.initializeManagedExecutor(executorBuildItem.getExecutorProxy()))
+                        .setRuntimeInit().done());
     }
 }
