0c80315b0c251dbf1fe1313f61bc3e110e456607
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:38:29 +0200

    fix the bug of calculating trainning error

--- dataset/GitHub_Java/haifengl.smile/demo/src/main/java/smile/demo/classification/RBFNetworkDemo.java
+++ dataset/GitHub_Java/haifengl.smile/demo/src/main/java/smile/demo/classification/RBFNetworkDemo.java
@@ -1,18 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2010-2019 Haifeng Li
+ * Copyright (c) 2010 Haifeng Li
+ *   
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Smile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * Smile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
 
 package smile.demo.classification;
@@ -24,7 +23,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-import smile.base.rbf.RBF;
 import smile.classification.RBFNetwork;
 import smile.math.distance.EuclideanDistance;
 import smile.math.rbf.RadialBasisFunction;
@@ -50,8 +48,8 @@ public class RBFNetworkDemo extends ClassificationDemo {
 
     @Override
     public double[][] learn(double[] x, double[] y) {
-        double[][] data = formula.x(dataset[datasetIndex]).toArray();
-        int[] label = formula.y(dataset[datasetIndex]).toIntArray();
+        double[][] data = dataset[datasetIndex].toArray(new double[dataset[datasetIndex].size()][]);
+        int[] label = dataset[datasetIndex].toArray(new int[dataset[datasetIndex].size()]);
         
         try {
             k = Integer.parseInt(kField.getText().trim());
@@ -64,7 +62,9 @@ public class RBFNetworkDemo extends ClassificationDemo {
             return null;
         }
 
-        RBFNetwork<double[]> rbf = RBFNetwork.fit(data, label, RBF.fit(data, k));
+        double[][] centers = new double[k][];
+        RadialBasisFunction basis = SmileUtils.learnGaussianRadialBasis(data, centers);
+        RBFNetwork<double[]> rbf = new RBFNetwork<>(data, label, new EuclideanDistance(), basis, centers);
         int[] pred = new int[label.length];
         for (int i = 0; i < label.length; i++) {
             pred[i] = rbf.predict(data[i]);
