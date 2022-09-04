3ea17d75a728bd2869666d3515c61cde2c3c6588
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:23:35 +0200

    Modified numeric converter to support floating point values.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/inputs/converters/NumericConverterTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/inputs/converters/NumericConverterTest.java
@@ -1,4 +1,6 @@
 /**
+ * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+ *
  * This file is part of Graylog2.
  *
  * Graylog2 is free software: you can redistribute it and/or modify
@@ -13,6 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package org.graylog2.inputs.converters;
 
@@ -21,8 +24,8 @@ import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNull;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
 
 /**
  * @author Lennart Koopmann <lennart@torch.sh>
