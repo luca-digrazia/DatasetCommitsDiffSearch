27792fdfca4f76e2ce556473e949795bf1b62e0c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:48:23 +0200

    Fix ClassLoader getParent() hack
        This replaces the hack where QuarkusClassLoader.getParent() returns
        null, and fixes it with some smaller more limited hacks around the
        problematic API's.
    
        Fixes #14898

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/reactive-streams-operators/smallrye-reactive-streams-operators/deployment/src/main/java/io/quarkus/smallrye/reactivestreamoperators/deployment/SmallRyeReactiveStreamsOperatorsProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/reactive-streams-operators/smallrye-reactive-streams-operators/deployment/src/main/java/io/quarkus/smallrye/reactivestreamoperators/deployment/SmallRyeReactiveStreamsOperatorsProcessor.java
@@ -1,23 +1,26 @@
-package io.quarkus.mutiny.reactive.operators.deployment;
+package io.quarkus.smallrye.reactivestreamoperators.deployment;
 
 import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreamsFactory;
 import org.eclipse.microprofile.reactive.streams.operators.core.ReactiveStreamsFactoryImpl;
 import org.eclipse.microprofile.reactive.streams.operators.spi.ReactiveStreamsEngine;
 
+import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
-import io.quarkus.mutiny.reactive.operators.runtime.ReactiveStreamsOperatorsRecorder;
-import io.smallrye.mutiny.streams.Engine;
+import io.quarkus.smallrye.reactivestreamsoperators.runtime.ReactiveStreamsOperatorsRecorder;
+import io.smallrye.reactive.streams.Engine;
 
-public class MutinyReactiveStreamsOperatorsProcessor {
+public class SmallRyeReactiveStreamsOperatorsProcessor {
 
     @BuildStep
-    public void build(BuildProducer<ServiceProviderBuildItem> serviceProvider) {
-        serviceProvider
-                .produce(new ServiceProviderBuildItem(ReactiveStreamsEngine.class.getName(), Engine.class.getName()));
+    public void build(BuildProducer<ServiceProviderBuildItem> serviceProvider,
+            BuildProducer<FeatureBuildItem> feature) {
+        feature.produce(new FeatureBuildItem(Feature.SMALLRYE_REACTIVE_STREAMS_OPERATORS));
+        serviceProvider.produce(new ServiceProviderBuildItem(ReactiveStreamsEngine.class.getName(), Engine.class.getName()));
         serviceProvider.produce(new ServiceProviderBuildItem(ReactiveStreamsFactory.class.getName(),
                 ReactiveStreamsFactoryImpl.class.getName()));
     }
