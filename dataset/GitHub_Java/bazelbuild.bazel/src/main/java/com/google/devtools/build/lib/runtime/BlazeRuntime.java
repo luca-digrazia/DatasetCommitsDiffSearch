5645728b9c5e82939acf77a659ddc4a0063b5c04
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:37:40 +0200

    Print further details on an AbruptExitException.
        Otherwise we just get a trivial message with the summary of the exception
        that caused the failure... but zero context even if it exists.  This is a
        requirement to get useful information when we fail hard.  Dumping a stack
        trace may not be the nicest for the user, but given that this is improving
        a catch-all location, I'd rather get more information than nothing.  If we
        need to print more succinct messages in other circumstances, we can
        special-case those.
    
        While here, also add missing trailing newlines to a few messages that need
        them.
    
        RELNOTES: None.
        PiperOrigin-RevId: 210409135

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/BlazeRuntime.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/BlazeRuntime.java
@@ -270,8 +270,7 @@ public final class BlazeRuntime {
       BlazeWorkspace workspace,
       CommonCommandOptions options,
       UUID buildID,
-      long execStartTimeNanos,
-      long waitTimeInMs) {
+      long execStartTimeNanos) {
     OutputStream out = null;
     boolean recordFullProfilerData = false;
     ProfiledTaskKinds profiledTasks = ProfiledTaskKinds.NONE;
@@ -325,34 +324,11 @@ public final class BlazeRuntime {
         // the launcher. We're setting the INIT phase marker so that it follows immediately the
         // LAUNCH phase.
         long startupTimeNanos = options.startupTime * 1000000L;
-        long waitTimeNanos = waitTimeInMs * 1000000L;
-        long clientStartTimeNanos = execStartTimeNanos - startupTimeNanos - waitTimeNanos;
         profiler.logSimpleTaskDuration(
-            clientStartTimeNanos,
-            Duration.ofNanos(startupTimeNanos),
+            execStartTimeNanos - startupTimeNanos,
+            Duration.ZERO,
             ProfilerTask.PHASE,
             ProfilePhase.LAUNCH.description);
-        if (options.extractDataTime > 0) {
-          profiler.logSimpleTaskDuration(
-              clientStartTimeNanos,
-              Duration.ofMillis(options.extractDataTime),
-              ProfilerTask.PHASE,
-              "Extracting Bazel binary");
-        }
-        if (options.waitTime > 0) {
-          profiler.logSimpleTaskDuration(
-              clientStartTimeNanos,
-              Duration.ofMillis(options.waitTime),
-              ProfilerTask.PHASE,
-              "Blocking on busy Bazel server (in client)");
-        }
-        if (waitTimeInMs > 0) {
-          profiler.logSimpleTaskDuration(
-              clientStartTimeNanos + startupTimeNanos,
-              Duration.ofMillis(waitTimeInMs),
-              ProfilerTask.PHASE,
-              "Blocking on busy Bazel server (in server)");
-        }
         profiler.logSimpleTaskDuration(
             execStartTimeNanos, Duration.ZERO, ProfilerTask.PHASE, ProfilePhase.INIT.description);
       }
