e9df16dbc22a687cf4da8d5de06f3349bf7c5803
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:29:53 +0200

    Suppress misleading jandex registration warnings for Spring Web

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/spring-web/src/main/java/io/quarkus/it/spring/web/GreetingController.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/spring-web/src/main/java/io/quarkus/it/spring/web/GreetingController.java
@@ -1,8 +1,14 @@
 package io.quarkus.it.spring.web;
 
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +21,18 @@ public class GreetingController {
         this.greetingService = greetingService;
     }
 
-    @GetMapping(produces = "application/json", path = "/json/{message}")
-    public Greeting greet(@PathVariable(name = "message") String message) {
-        return greetingService.greet(message);
+    @GetMapping(path = "/json/{message}")
+    public Greeting greet(@PathVariable String message, @RequestParam String suffix) {
+        return greetingService.greet(message + suffix);
+    }
+
+    @GetMapping(path = "/re/json/{message}")
+    public ResponseEntity<Greeting> responseEntityGreeting(@PathVariable String message, @RequestParam String suffix) {
+        return ResponseEntity.ok(greetingService.greet(message + suffix));
+    }
+
+    @PostMapping(path = "/person")
+    public Greeting newGreeting(@RequestBody @Valid Person person) {
+        return new Greeting("hello " + person.getName());
     }
 }
