0eb6bb786941f2f744b75d0193a929637a606fc3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:24:33 +0200

    Adding MessageFilterHook.java

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/messagehandlers/common/MessageFilterHook.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/messagehandlers/common/MessageFilterHook.java
@@ -20,11 +20,6 @@
 
 package org.graylog2.messagehandlers.common;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.productivity.java.syslog4j.server.SyslogServerEventIF;
-
 /**
  * MessageFilterHook.java: Feb 7, 2011 5:56:21 PM
  *
@@ -39,18 +34,7 @@ public class MessageFilterHook implements MessagePostReceiveHookIF {
      */
     public void process(Object message) {
 	// 1. Get regex filters
-    	String regex = ".*(kernel).*";
-    	Pattern pat = Pattern.compile(regex);
-    	
 	// 2. Match message against regular expression
-    	//
 	// 3. Log filter matched or dropped
-    	String msg = new String(((SyslogServerEventIF) message).getRaw());
-    	Matcher m = pat.matcher(msg);
-    	if(m.matches()){
-    		System.out.println("found a match: " + m.group(1));
-    	} else {
-    		System.out.println("no match: " + msg);
-    	}
     }
 }
