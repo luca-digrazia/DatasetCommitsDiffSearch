bbfeb977ec44efddfb5c7a3e8d3399edff344685
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:38:07 +0200

    Added more tests to ensure that Jackson#newObjectMapper can handle null as parameter.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jackson/src/test/java/io/dropwizard/jackson/JacksonTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jackson/src/test/java/io/dropwizard/jackson/JacksonTest.java
@@ -5,15 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import javax.annotation.Nullable;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
-public class JacksonTest {
+public class JacksonTest
+{
     @Test
     public void objectMapperUsesGivenCustomJsonFactory() {
         JsonFactory factory = Mockito.mock(JsonFactory.class);
@@ -28,37 +23,6 @@ public class JacksonTest {
         ObjectMapper mapper = Jackson.newObjectMapper(null);
 
         assertThat(mapper.getFactory()).isNotNull();
-    }
-
-    @Test
-    public void objectMapperCanDeserializeJdk7Types() throws IOException {
-        final LogMetadata metadata = Jackson.newObjectMapper()
-            .readValue("{\"path\": \"/var/log/app/server.log\"}", LogMetadata.class);
-        assertThat(metadata).isNotNull();
-        assertThat(metadata.path).isEqualTo(Paths.get("/var/log/app/server.log"));
-    }
-
-    @Test
-    public void objectMapperSerializesNullValues() throws IOException {
-        final ObjectMapper mapper = Jackson.newObjectMapper();
-        final Issue1627 pojo = new Issue1627(null, null);
-        final String json = "{\"string\":null,\"uuid\":null}";
-
-        assertThat(mapper.writeValueAsString(pojo)).isEqualTo(json);
-    }
-
-    @Test
-    public void objectMapperIgnoresUnknownProperties() {
-        assertThatCode(() ->
-            Jackson.newObjectMapper()
-                .readValue("{\"unknown\": 4711, \"path\": \"/var/log/app/server.log\"}", LogMetadata.class)
-        ).doesNotThrowAnyException();
-    }
-
-    static class LogMetadata {
-
-        @Nullable
-        public Path path;
     }
 
 }
