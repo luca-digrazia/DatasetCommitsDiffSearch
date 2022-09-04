38f8a8e2a7569bffb934ca5f79efd721968ab1b2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 17:46:48 +0200

    Mark Desugar-Generated Methods from Core Library Desugaring Synthetic
        - Facilitate for common tooling analysis.
    
        PiperOrigin-RevId: 313331093

--- dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/desugar/corelibadapter/ShadowedApiAdaptersGenerator.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/desugar/corelibadapter/ShadowedApiAdaptersGenerator.java
@@ -26,13 +26,14 @@ import static org.objectweb.asm.Opcodes.ACC_SYNTHETIC;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.google.devtools.build.android.desugar.io.FileContentProvider;
+import com.google.devtools.build.android.desugar.io.MapBasedClassFileProvider;
 import com.google.devtools.build.android.desugar.langmodel.ClassName;
-import com.google.devtools.build.android.desugar.langmodel.InvocationSiteTransformationRecord;
 import com.google.devtools.build.android.desugar.langmodel.MethodDeclInfo;
 import com.google.devtools.build.android.desugar.langmodel.MethodInvocationSite;
 import com.google.devtools.build.android.desugar.langmodel.MethodKey;
-import java.io.ByteArrayInputStream;
+import java.util.stream.Stream;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
@@ -41,28 +42,19 @@ import org.objectweb.asm.Type;
 
 /**
  * Generates type adapter classes with methods that bridge the interactions between
- * desugared-mirrored types and their desugar-shadowed built-in types, and delivers the generated
- * classes to runtime library.
+ * desugared-mirrored types and their desugar-shadowed built-in types. Together with the necessary
+ * type converter classes, the class delivers the generated adapter classes to runtime library.
  */
 public final class ShadowedApiAdaptersGenerator {
 
   private static final int TYPE_ADAPTER_CLASS_ACCESS = ACC_PUBLIC | ACC_ABSTRACT | ACC_SYNTHETIC;
-  private static final int TYPE_CONVERSION_METHOD_ACCESS = ACC_PUBLIC | ACC_STATIC;
+  private static final int TYPE_CONVERSION_METHOD_ACCESS = ACC_PUBLIC | ACC_STATIC | ACC_SYNTHETIC;
 
   /** A pre-collected record that tracks the adapter method requests from invocation sites. */
   private final InvocationSiteTransformationRecord invocationAdapterSites;
 
   /** A record with evolving map values that track adapter classes to be generated. */
   private final ImmutableMap<ClassName, ClassWriter> typeAdapters;
-
-  /** The public API that provides the file content of generate adapter classes. */
-  public static ImmutableList<FileContentProvider<ByteArrayInputStream>> generateAdapterClasses(
-      InvocationSiteTransformationRecord callSiteTransformations) {
-    return emitClassWriters(callSiteTransformations)
-        .emitAdapterMethods()
-        .closeClassWriters()
-        .provideFileContents();
-  }
 
   private ShadowedApiAdaptersGenerator(
       InvocationSiteTransformationRecord invocationAdapterSites,
@@ -71,11 +63,17 @@ public final class ShadowedApiAdaptersGenerator {
     this.typeAdapters = typeAdapters;
   }
 
+  /** The public factory method to construct {@link ShadowedApiAdaptersGenerator}. */
+  public static ShadowedApiAdaptersGenerator create(
+      InvocationSiteTransformationRecord callSiteTransformations) {
+    return emitClassWriters(callSiteTransformations).emitAdapterMethods().closeClassWriters();
+  }
+
   private static ShadowedApiAdaptersGenerator emitClassWriters(
       InvocationSiteTransformationRecord callSiteTransformations) {
     return new ShadowedApiAdaptersGenerator(
         callSiteTransformations,
-        callSiteTransformations.record().stream()
+        callSiteTransformations.adapterReplacements().stream()
             .map(ShadowedApiAdapterHelper::getAdapterInvocationSite)
             .map(MethodInvocationSite::owner)
             .distinct()
@@ -97,8 +95,37 @@ public final class ShadowedApiAdaptersGenerator {
     return cw;
   }
 
+  /** Returns desugar-shadowed API adapters with desugar-mirrored types. */
+  public MapBasedClassFileProvider getApiAdapters() {
+    return MapBasedClassFileProvider.builder()
+        .setTag("ShadowedApiAdapters")
+        .setFileContents(
+            Maps.transformEntries(
+                typeAdapters,
+                (className, cw) ->
+                    FileContentProvider.fromBytes(className.classFilePathName(), cw.toByteArray())))
+        .build();
+  }
+
+  /**
+   * Returns type conversion classes that converts between a desugar-shadowed type and its
+   * deusgar-mirrored counterpart.
+   */
+  public ImmutableList<ClassName> getTypeConverters() {
+    return Stream.concat(
+            invocationAdapterSites.inlineConversions().stream(),
+            invocationAdapterSites.adapterReplacements().stream())
+        .flatMap(
+            site ->
+                Stream.concat(Stream.of(site.returnTypeName()), site.argumentTypeNames().stream()))
+        .filter(ClassName::isDesugarShadowedType)
+        .distinct()
+        .map(ClassName::typeConverterOwner)
+        .collect(toImmutableList());
+  }
+
   private ShadowedApiAdaptersGenerator emitAdapterMethods() {
-    for (MethodInvocationSite invocationSite : invocationAdapterSites.record()) {
+    for (MethodInvocationSite invocationSite : invocationAdapterSites.adapterReplacements()) {
       MethodInvocationSite adapterSite =
           ShadowedApiAdapterHelper.getAdapterInvocationSite(invocationSite);
       ClassName adapterOwner = adapterSite.owner();
@@ -153,15 +180,5 @@ public final class ShadowedApiAdaptersGenerator {
   private ShadowedApiAdaptersGenerator closeClassWriters() {
     typeAdapters.values().forEach(ClassVisitor::visitEnd);
     return this;
-  }
-
-  private ImmutableList<FileContentProvider<ByteArrayInputStream>> provideFileContents() {
-    return typeAdapters.entrySet().stream()
-        .map(
-            e ->
-                new FileContentProvider<>(
-                    e.getKey().classFilePathName(),
-                    () -> new ByteArrayInputStream(e.getValue().toByteArray())))
-        .collect(toImmutableList());
   }
 }
