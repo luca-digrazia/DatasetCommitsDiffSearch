e2254514e5cf0f7678f5b11df59ae6564c3a492a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:25:10 +0200

    add has_field function
         - fix type literal for injection

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog/plugins/messageprocessor/ProcessorPluginModule.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog/plugins/messageprocessor/ProcessorPluginModule.java
@@ -4,13 +4,9 @@ import com.google.inject.Binder;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.MapBinder;
 import org.graylog.plugins.messageprocessor.ast.functions.Function;
-import org.graylog.plugins.messageprocessor.functions.BooleanCoercion;
-import org.graylog.plugins.messageprocessor.functions.DoubleCoercion;
-import org.graylog.plugins.messageprocessor.functions.DropMessageFunction;
-import org.graylog.plugins.messageprocessor.functions.HasField;
-import org.graylog.plugins.messageprocessor.functions.InputFunction;
-import org.graylog.plugins.messageprocessor.functions.LongCoercion;
-import org.graylog.plugins.messageprocessor.functions.StringCoercion;
+import org.graylog.plugins.messageprocessor.ast.functions.builtin.DropMessageFunction;
+import org.graylog.plugins.messageprocessor.ast.functions.builtin.HasField;
+import org.graylog.plugins.messageprocessor.ast.functions.builtin.InputFunction;
 import org.graylog.plugins.messageprocessor.processors.NaiveRuleProcessor;
 import org.graylog.plugins.messageprocessor.rest.MessageProcessorRuleResource;
 import org.graylog2.plugin.PluginConfigBean;
@@ -32,11 +28,6 @@ public class ProcessorPluginModule extends PluginModule {
         addRestResource(MessageProcessorRuleResource.class);
 
         // built-in functions
-        addMessageProcessorFunction(BooleanCoercion.NAME, BooleanCoercion.class);
-        addMessageProcessorFunction(DoubleCoercion.NAME, DoubleCoercion.class);
-        addMessageProcessorFunction(LongCoercion.NAME, LongCoercion.class);
-        addMessageProcessorFunction(StringCoercion.NAME, StringCoercion.class);
-
         addMessageProcessorFunction(HasField.NAME, HasField.class);
         addMessageProcessorFunction(InputFunction.NAME, InputFunction.class);
         addMessageProcessorFunction(DropMessageFunction.NAME, DropMessageFunction.class);
