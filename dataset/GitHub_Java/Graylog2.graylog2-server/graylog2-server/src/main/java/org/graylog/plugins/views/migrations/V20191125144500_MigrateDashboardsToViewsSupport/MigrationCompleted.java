78870980ff99460f05844407658d5f1b7262d4c3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:31:27 +0200

    Migrating dashboards to views with identical ids.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20191125144500_MigrateDashboardsToViewsSupport/MigrationCompleted.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/plugins/views/migrations/V20191125144500_MigrateDashboardsToViewsSupport/MigrationCompleted.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog.plugins.views.migrations.V20191125144500_MigrateDashboardsToViewsSupport;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -11,17 +27,20 @@ import java.util.Set;
 @AutoValue
 @JsonAutoDetect
 public abstract class MigrationCompleted {
-    @JsonProperty("dashboard_to_view_migration_ids")
-    public abstract Map<String, String> dashboardToViewMigrationIds();
+    private static final String FIELD_MIGRATED_DASHBOARD_IDS = "migrated_dashboard_ids";
+    private static final String FIELD_WIDGET_MIGRATION_IDS = "widget_migration_ids";
 
-    @JsonProperty("widget_migration_ids")
+    @JsonProperty(FIELD_MIGRATED_DASHBOARD_IDS)
+    public abstract Set<String> migratedDashboardIds();
+
+    @JsonProperty(FIELD_WIDGET_MIGRATION_IDS)
     public abstract Map<String, Set<String>> widgetMigrationIds();
 
     @JsonCreator
     static MigrationCompleted create(
-            @JsonProperty("dashboard_to_view_migration_ids") Map<String, String> dashboardToViewMigrationIds,
-            @JsonProperty("widget_migration_ids") Map<String, Set<String>> widgetMigrationIds
+            @JsonProperty(FIELD_MIGRATED_DASHBOARD_IDS) Set<String> migratedDashboardIds,
+            @JsonProperty(FIELD_WIDGET_MIGRATION_IDS) Map<String, Set<String>> widgetMigrationIds
     ) {
-        return new AutoValue_MigrationCompleted(dashboardToViewMigrationIds, widgetMigrationIds);
+        return new AutoValue_MigrationCompleted(migratedDashboardIds, widgetMigrationIds);
     }
 }
