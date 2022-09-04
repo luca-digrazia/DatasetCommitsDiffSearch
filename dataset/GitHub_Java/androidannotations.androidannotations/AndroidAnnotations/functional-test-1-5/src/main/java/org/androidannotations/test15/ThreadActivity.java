c96918aead3ff99dbdf1cd750de40b3218f30a6f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:10:56 +0200

    Add functional test

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/org/androidannotations/test15/ThreadActivity.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/org/androidannotations/test15/ThreadActivity.java
@@ -139,14 +139,4 @@ public class ThreadActivity extends Activity {
 	@Background
 	void backgroundUsingArrayParamtersMethod(MySerializableBean[][] array) {
 	}
-
-	@Background
-	void backgroundThrowException() {
-		throw new RuntimeException();
-	}
-
-	@UiThread
-	void uiThreadThrowException() {
-		throw new RuntimeException();
-	}
 }
