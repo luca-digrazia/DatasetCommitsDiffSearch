ba4765578a6e6481ece51a9738608810c8af7b6c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:06:47 +0200

    Prevent action-cache duplicate suppression
        Exceptions that are reused between download futures must not be added to
        the suppression of themselves.
    
        Fixes #9362
    
        Closes #9376.
    
        PiperOrigin-RevId: 270658205

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/AbstractRemoteActionCache.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/AbstractRemoteActionCache.java
@@ -92,7 +92,7 @@ public abstract class AbstractRemoteActionCache implements MissingDigestsFinder,
   /** See {@link SpawnExecutionContext#lockOutputFiles()}. */
   @FunctionalInterface
   interface OutputFilesLocker {
-    void lock() throws InterruptedException, IOException;
+    void lock() throws InterruptedException;
   }
 
   private static final ListenableFuture<Void> COMPLETED_SUCCESS = SettableFuture.create();
@@ -382,15 +382,11 @@ public abstract class AbstractRemoteActionCache implements MissingDigestsFinder,
           tmpOutErr.clearErr();
         }
       } catch (IOException e) {
-        if (downloadException != null && e != downloadException) {
-          e.addSuppressed(downloadException);
-        }
-        if (interruptedException != null) {
-          e.addSuppressed(interruptedException);
-        }
-
         // If deleting of output files failed, we abort the build with a decent error message as
         // any subsequent local execution failure would likely be incomprehensible.
+
+        // We don't propagate the downloadException, as this is a recoverable error and the cause
+        // of the build failure is really that we couldn't delete output files.
         throw new EnvironmentalExecException(
             "Failed to delete output files after incomplete download", e);
       }
@@ -509,9 +505,8 @@ public abstract class AbstractRemoteActionCache implements MissingDigestsFinder,
             try {
               out.close();
             } catch (IOException e) {
-              if (t != e) {
-                t.addSuppressed(e);
-              }
+              // Intentionally left empty. The download already failed, so we can ignore
+              // the error on close().
             } finally {
               outerF.setException(t);
             }
