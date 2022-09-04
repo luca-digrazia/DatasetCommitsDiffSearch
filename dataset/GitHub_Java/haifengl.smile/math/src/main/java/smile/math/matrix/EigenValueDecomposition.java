572ab2e6b2fce1c7e998ce822dbe8e16635e32d4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:52:40 +0200

    fix typo

--- dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/math/matrix/EigenValueDecomposition.java
+++ dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/math/matrix/EigenValueDecomposition.java
@@ -92,7 +92,7 @@ public class EigenValueDecomposition {
      */
     private double[] e;
     /**
-     * Array of eigen vectors.
+     * Array of eigenvectors.
      */
     private double[][] V;
 
@@ -170,6 +170,247 @@ public class EigenValueDecomposition {
     }
 
     /**
+     * Returns the largest eigen pair of matrix with the power iteration
+     * under the assumptions A has an eigenvalue that is strictly greater
+     * in magnitude than its other eigenvalues and the starting
+     * vector has a nonzero component in the direction of an eigenvector
+     * associated with the dominant eigenvalue.
+     * @param A the matrix supporting matrix vector multiplication operation.
+     * @param v on input, it is the non-zero initial guess of the eigen vector.
+     * On output, it is the eigen vector corresponding largest eigen value.
+     * @return the largest eigen value.
+     */
+    public static double eigen(IMatrix A, double[] v) {
+        return eigen(A, v, Math.max(1.0E-10, A.nrows() * Math.EPSILON));
+    }
+
+    /**
+     * Returns the largest eigen pair of matrix with the power iteration
+     * under the assumptions A has an eigenvalue that is strictly greater
+     * in magnitude than its other eigenvalues and the starting
+     * vector has a nonzero component in the direction of an eigenvector
+     * associated with the dominant eigenvalue.
+     * @param A the matrix supporting matrix vector multiplication operation.
+     * @param v on input, it is the non-zero initial guess of the eigen vector.
+     * On output, it is the eigen vector corresponding largest eigen value.
+     * @param tol the desired convergence tolerance.
+     * @return the largest eigen value.
+     */
+    public static double eigen(IMatrix A, double[] v, double tol) {
+        return eigen(A, v, 0.0, tol);
+    }
+
+    /**
+     * Returns the largest eigen pair of matrix with the power iteration
+     * under the assumptions A has an eigenvalue that is strictly greater
+     * in magnitude than its other eigenvalues and the starting
+     * vector has a nonzero component in the direction of an eigenvector
+     * associated with the dominant eigenvalue.
+     * @param A the matrix supporting matrix vector multiplication operation.
+     * @param v on input, it is the non-zero initial guess of the eigen vector.
+     * On output, it is the eigen vector corresponding largest eigen value.
+     * @param tol the desired convergence tolerance.
+     * @param maxIter the maximum number of iterations in case that the algorithm
+     * does not converge.
+     * @return the largest eigen value.
+     */
+    public static double eigen(IMatrix A, double[] v, double tol, int maxIter) {
+        return eigen(A, v, 0.0, tol, maxIter);
+    }
+
+    /**
+     * Returns the largest eigen pair of matrix with the power iteration
+     * under the assumptions A has an eigenvalue that is strictly greater
+     * in magnitude than its other eigenvalues and the starting
+     * vector has a nonzero component in the direction of an eigenvector
+     * associated with the dominant eigenvalue.
+     * @param A the matrix supporting matrix vector multiplication operation.
+     * @param v on input, it is the non-zero initial guess of the eigen vector.
+     * On output, it is the eigen vector corresponding largest eigen value.
+     * @param p the origin in the shifting power method. A - pI will be
+     * used in the iteration to accelerate the method. p should be such that
+     * |(&lambda;<sub>2</sub> - p) / (&lambda;<sub>1</sub> - p)| &lt; |&lambda;<sub>2</sub> / &lambda;<sub>1</sub>|,
+     * where &lambda;<sub>2</sub> is the second largest eigenvalue in magnitude.
+     * If we known the eigenvalue spectrum of A, (&lambda;<sub>2</sub> + &lambda;<sub>n</sub>)/2
+     * is the optimal choice of p, where &lambda;<sub>n</sub> is the smallest eigenvalue
+     * in magnitude. Good estimates of &lambda;<sub>2</sub> are more difficult
+     * to compute. However, if &mu; is an approximation to largest eigenvector,
+     * then using any x<sub>0</sub> such that x<sub>0</sub>*&mu; = 0 as the initial
+     * vector for a few iterations may yield a reasonable estimate of &lambda;<sub>2</sub>.
+     * @param tol the desired convergence tolerance.
+     * @return the largest eigen value.
+     */
+    public static double eigen(IMatrix A, double[] v, double p, double tol) {
+        return eigen(A, v, p, tol, Math.max(20, 2 * A.nrows()));
+    }
+
+    /**
+     * Returns the largest eigen pair of matrix with the power iteration
+     * under the assumptions A has an eigenvalue that is strictly greater
+     * in magnitude than its other eigenvalues and the starting
+     * vector has a nonzero component in the direction of an eigenvector
+     * associated with the dominant eigenvalue.
+     * @param A the matrix supporting matrix vector multiplication operation.
+     * @param v on input, it is the non-zero initial guess of the eigen vector.
+     * On output, it is the eigen vector corresponding largest eigen value.
+     * @param p the origin in the shifting power method. A - pI will be
+     * used in the iteration to accelerate the method. p should be such that
+     * |(&lambda;<sub>2</sub> - p) / (&lambda;<sub>1</sub> - p)| &lt; |&lambda;<sub>2</sub> / &lambda;<sub>1</sub>|,
+     * where &lambda;<sub>2</sub> is the second largest eigenvalue in magnitude.
+     * If we known the eigenvalue spectrum of A, (&lambda;<sub>2</sub> + &lambda;<sub>n</sub>)/2
+     * is the optimal choice of p, where &lambda;<sub>n</sub> is the smallest eigenvalue
+     * in magnitude. Good estimates of &lambda;<sub>2</sub> are more difficult
+     * to compute. However, if &mu; is an approximation to largest eigenvector,
+     * then using any x<sub>0</sub> such that x<sub>0</sub>*&mu; = 0 as the initial
+     * vector for a few iterations may yield a reasonable estimate of &lambda;<sub>2</sub>.
+     * @param tol the desired convergence tolerance.
+     * @param maxIter the maximum number of iterations in case that the algorithm
+     * does not converge.
+     * @return the largest eigen value.
+     */
+    public static double eigen(IMatrix A, double[] v, double p, double tol, int maxIter) {
+        if (A.nrows() != A.ncols()) {
+            throw new IllegalArgumentException("Matrix is not square.");
+        }
+
+        if (tol <= 0.0) {
+            throw new IllegalArgumentException("Invalid tolerance: " + tol);            
+        }
+        
+        if (maxIter <= 0) {
+            throw new IllegalArgumentException("Invalid maximum number of iterations: " + maxIter);            
+        }
+        
+        int n = A.nrows();
+        tol = Math.max(tol, Math.EPSILON * n);
+
+        double[] z = new double[n];
+        double lambda = ax(A, v, z, p);
+
+        for (int iter = 1; iter <= maxIter; iter++) {
+            double l = lambda;
+            lambda = ax(A, v, z, p);
+
+            double eps = Math.abs(lambda - l);
+            if (iter % 10 == 0) {
+                logger.trace(String.format("Largest eigenvalue after %3d power iterations: %.5f\n", iter, lambda + p));
+            }
+
+            if (eps < tol) {
+                logger.info(String.format("Largest eigenvalue after %3d power iterations: %.5f\n", iter, lambda + p));
+                return lambda + p;
+            }
+        }
+
+        logger.info(String.format("Largest eigenvalue after %3d power iterations: %.5f\n", maxIter, lambda + p));
+        logger.error("Power iteration exceeded the maximum number of iterations.");
+        return lambda + p;
+    }
+
+    /**
+     * Calculate the page rank vector.
+     * @param A the matrix supporting matrix vector multiplication operation.
+     * @return the page rank vector.
+     */
+    public static double[] pagerank(IMatrix A) {
+    	int n = A.nrows();    	
+    	double[] v = new double[n];
+    	Arrays.fill(v,  1.0 / n);
+    	return pagerank(A, v);
+    }
+    
+    /**
+     * Calculate the page rank vector.
+     * @param A the matrix supporting matrix vector multiplication operation.
+     * @param v the teleportation vector.
+     * @return the page rank vector.
+     */
+    public static double[] pagerank(IMatrix A, double[] v) {
+    	return pagerank(A, v, 0.85, 1E-7, 57);
+    }
+    
+    /**
+     * Calculate the page rank vector.
+     * @param A the matrix supporting matrix vector multiplication operation.
+     * @param v the teleportation vector.
+     * @param damping the damper factor.
+     * @param tol the desired convergence tolerance.
+     * @param maxIter the maximum number of iterations in case that the algorithm
+     * does not converge.
+     * @return the page rank vector.
+     */
+    public static double[] pagerank(IMatrix A, double[] v, double damping, double tol, int maxIter) {
+        if (A.nrows() != A.ncols()) {
+            throw new IllegalArgumentException("Matrix is not square.");
+        }
+
+        if (tol <= 0.0) {
+            throw new IllegalArgumentException("Invalid tolerance: " + tol);            
+        }
+        
+        if (maxIter <= 0) {
+            throw new IllegalArgumentException("Invalid maximum number of iterations: " + maxIter);            
+        }
+        
+        int n = A.nrows();
+        tol = Math.max(tol, Math.EPSILON * n);
+
+        double[] z = new double[n];
+        double[] p = Arrays.copyOf(v, n);
+
+        for (int iter = 1; iter <= maxIter; iter++) {
+            A.ax(p, z);
+            double beta = 1.0 - damping * Math.norm1(z);
+            
+            double delta = 0.0;
+            for (int i = 0; i < n; i++) {
+            	double q = damping * z[i] + beta * v[i];
+            	delta += Math.abs(q - p[i]);
+            	p[i] = q;
+            }
+
+            if (iter % 10 == 0) {
+                logger.info(String.format("PageRank residual after %3d power iterations: %.7f\n", iter, delta));
+            }
+
+            if (delta < tol) {
+                logger.info(String.format("PageRank residual after %3d power iterations: %.7f\n", iter, delta));
+                return p;
+            }
+        }
+
+        logger.error("PageRank iteration exceeded the maximum number of iterations.");
+        return p;
+    }
+
+    /**
+     * Calculate and normalize y = (A - pI) x.
+     * Returns the largest element of y in magnitude.
+     */
+    private static double ax(IMatrix A, double[] x, double[] y, double p) {
+        A.ax(x, y);
+
+        if (p != 0.0) {
+            for (int i = 0; i < y.length; i++) {
+                y[i] -= p * x[i];
+            }
+        }
+
+        double lambda = y[0];
+        for (int i = 1; i < y.length; i++) {
+            if (Math.abs(y[i]) > Math.abs(lambda)) {
+                lambda = y[i];
+            }
+        }
+
+        for (int i = 0; i < y.length; i++) {
+            x[i] = y[i] / lambda;
+        }
+
+        return lambda;
+    }
+
+    /**
      * Find k largest approximate eigen pairs of a symmetric matrix by the
      * Lanczos algorithm.
      *
@@ -177,7 +418,7 @@ public class EigenValueDecomposition {
      * @param k the number of eigenvalues we wish to compute for the input matrix.
      * This number cannot exceed the size of A.
      */
-    public static EigenValueDecomposition decompose(Matrix A, int k) {
+    public static EigenValueDecomposition decompose(IMatrix A, int k) {
         return decompose(A, k, 1.0E-6);
     }
 
@@ -190,7 +431,7 @@ public class EigenValueDecomposition {
      * This number cannot exceed the size of A.
      * @param kappa relative accuracy of ritz values acceptable as eigenvalues.
      */
-    public static EigenValueDecomposition decompose(Matrix A, int k, double kappa) {
+    public static EigenValueDecomposition decompose(IMatrix A, int k, double kappa) {
         if (A.nrows() != A.ncols()) {
             throw new IllegalArgumentException("Matrix is not square.");
         }
@@ -436,7 +677,7 @@ public class EigenValueDecomposition {
      * of operator can be found.
      * @param step   starting index for a Lanczos run
      */
-    private static double startv(Matrix A, double[][] q, double[][] wptr, int step) {
+    private static double startv(IMatrix A, double[][] q, double[][] wptr, int step) {
         // get initial vector; default is random
         double rnm = Math.dot(wptr[0], wptr[0]);
         double[] r = wptr[0];
