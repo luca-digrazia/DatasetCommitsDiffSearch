b17bb0079231300ea96dd8a243b277fc3927517b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:27:40 +0200

    #114 If file does not exist, try URL decoding it to allow for URIs created using Uri.fromFile

--- dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/library/src/com/davemorrissey/labs/subscaleview/ImageSource.java
+++ dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/library/src/com/davemorrissey/labs/subscaleview/ImageSource.java
@@ -1,50 +1,76 @@
 package com.davemorrissey.labs.subscaleview;
 
-import android.content.ContentResolver;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.graphics.Rect;
 import android.net.Uri;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * Helper class used to set the source and additional attributes from a variety of sources. Supports
  * use of a bitmap, asset, resource, external file or any other URI.
  *
  * When you are using a preview image, you must set the dimensions of the full size image on the
- * ImageSource object for the full size image using the {@link #withDimensions(int, int)} method.
+ * ImageSource object for the full size image using the {@link #dimensions(int, int)} method.
  */
 public final class ImageSource {
 
-    private static final String FILE_SCHEME = "file:///";
-    private static final String ASSET_SCHEME = "file:///android_asset/";
+    static final String FILE_SCHEME = "file:///";
+    static final String ASSET_SCHEME = "file:///android_asset/";
 
     private final Uri uri;
     private final Bitmap bitmap;
+    private final Integer resource;
     private boolean tile;
     private int sWidth;
     private int sHeight;
+    private Rect sRegion;
+    private boolean cached;
 
-    private ImageSource(Bitmap bitmap) {
+    private ImageSource(Bitmap bitmap, boolean cached) {
         this.bitmap = bitmap;
         this.uri = null;
+        this.resource = null;
         this.tile = false;
         this.sWidth = bitmap.getWidth();
         this.sHeight = bitmap.getHeight();
+        this.cached = cached;
     }
 
     private ImageSource(Uri uri) {
+        // #114 If file doesn't exist, attempt to url decode the URI and try again
+        String uriString = uri.toString();
+        if (uriString.startsWith(FILE_SCHEME)) {
+            File uriFile = new File(uriString.substring(FILE_SCHEME.length() - 1));
+            if (!uriFile.exists()) {
+                try {
+                    uri = Uri.parse(URLDecoder.decode(uriString, "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    // Fallback to encoded URI. This exception is not expected.
+                }
+            }
+        }
         this.bitmap = null;
         this.uri = uri;
+        this.resource = null;
+        this.tile = true;
+    }
+
+    private ImageSource(int resource) {
+        this.bitmap = null;
+        this.uri = null;
+        this.resource = resource;
         this.tile = true;
     }
 
     /**
      * Create an instance from a resource. The correct resource for the device screen resolution will be used.
      * @param resId resource ID.
-     * @param context application context.
      */
-    public static ImageSource resource(int resId, Context context) {
-        return uri(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.getPackageName() + "/" + resId);
+    public static ImageSource resource(int resId) {
+        return new ImageSource(resId);
     }
 
     /**
@@ -95,31 +121,58 @@ public final class ImageSource {
         if (bitmap == null) {
             throw new NullPointerException("Bitmap must not be null");
         }
-        return new ImageSource(bitmap);
+        return new ImageSource(bitmap, false);
     }
 
     /**
-     * Enable tiling of the image. This does not apply to preview images which are always loaded as a single bitmap.
-     * @return this instance for chaining.
+     * Provide a loaded and cached bitmap for display. This bitmap will not be recycled when it is no
+     * longer needed. Use this method if you loaded the bitmap with an image loader such as Picasso
+     * or Volley.
+     * @param bitmap bitmap to be displayed.
      */
-    public ImageSource withTilingEnabled() {
-        return withTiling(true);
+    public static ImageSource cachedBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            throw new NullPointerException("Bitmap must not be null");
+        }
+        return new ImageSource(bitmap, true);
     }
 
     /**
-     * Disable tiling of the image. This does not apply to preview images which are always loaded as a single bitmap.
+     * Enable tiling of the image. This does not apply to preview images which are always loaded as a single bitmap.,
+     * and tiling cannot be disabled when displaying a region of the source image.
      * @return this instance for chaining.
      */
-    public ImageSource withTilingDisabled() {
-        return withTiling(false);
+    public ImageSource tilingEnabled() {
+        return tiling(true);
     }
 
     /**
-     * Enable or disable tiling of the image. This does not apply to preview images which are always loaded as a single bitmap.
+     * Disable tiling of the image. This does not apply to preview images which are always loaded as a single bitmap,
+     * and tiling cannot be disabled when displaying a region of the source image.
      * @return this instance for chaining.
      */
-    public ImageSource withTiling(boolean tile) {
+    public ImageSource tilingDisabled() {
+        return tiling(false);
+    }
+
+    /**
+     * Enable or disable tiling of the image. This does not apply to preview images which are always loaded as a single bitmap,
+     * and tiling cannot be disabled when displaying a region of the source image.
+     * @return this instance for chaining.
+     */
+    public ImageSource tiling(boolean tile) {
         this.tile = tile;
+        return this;
+    }
+
+    /**
+     * Use a region of the source image. Region must be set independently for the full size image and the preview if
+     * you are using one.
+     * @return this instance for chaining.
+     */
+    public ImageSource region(Rect sRegion) {
+        this.sRegion = sRegion;
+        setInvariants();
         return this;
     }
 
@@ -129,12 +182,21 @@ public final class ImageSource {
      * the image dimensions. Note if the declared dimensions are found to be incorrect, the view will reset.
      * @return this instance for chaining.
      */
-    public ImageSource withDimensions(int sWidth, int sHeight) {
+    public ImageSource dimensions(int sWidth, int sHeight) {
         if (bitmap == null) {
             this.sWidth = sWidth;
             this.sHeight = sHeight;
         }
+        setInvariants();
         return this;
+    }
+
+    private void setInvariants() {
+        if (this.sRegion != null) {
+            this.tile = true;
+            this.sWidth = this.sRegion.width();
+            this.sHeight = this.sRegion.height();
+        }
     }
 
     protected final Uri getUri() {
@@ -143,6 +205,10 @@ public final class ImageSource {
 
     protected final Bitmap getBitmap() {
         return bitmap;
+    }
+
+    protected final Integer getResource() {
+        return resource;
     }
 
     protected final boolean getTile() {
@@ -155,5 +221,13 @@ public final class ImageSource {
 
     protected final int getSHeight() {
         return sHeight;
+    }
+
+    protected final Rect getSRegion() {
+        return sRegion;
+    }
+
+    protected final boolean isCached() {
+        return cached;
     }
 }
