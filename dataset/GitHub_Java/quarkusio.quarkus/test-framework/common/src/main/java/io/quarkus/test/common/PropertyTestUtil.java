ced81810841b60c7b8355aa06b795c01da29e5c6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:34:29 +0200

    when starting native image, use argument for log file location

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/PropertyTestUtil.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/PropertyTestUtil.java
@@ -1,31 +1,34 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.quarkus.test.common;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import io.quarkus.runtime.logging.FileConfig;
-
 public class PropertyTestUtil {
 
-    private static final String LOG_FILE_PATH_PROPERTY = "quarkus.log.file.path";
-
     public static void setLogFileProperty() {
-        System.setProperty(LOG_FILE_PATH_PROPERTY, getLogFileLocation());
-    }
-
-    public static void setLogFileProperty(String logFileName) {
-        System.setProperty(LOG_FILE_PATH_PROPERTY, getLogFileLocation(logFileName));
+        System.setProperty("quarkus.log.file.path", getLogFileLocation());
     }
 
     public static String getLogFileLocation() {
-        return getLogFileLocation(System.getProperty(LOG_FILE_PATH_PROPERTY, FileConfig.DEFAULT_LOG_FILE_NAME));
-    }
-
-    private static String getLogFileLocation(String logFileName) {
         if (Files.isDirectory(Paths.get("build"))) {
-            return "build" + File.separator + logFileName;
+            return "build" + File.separator + "quarkus.log";
         }
-        return "target" + File.separator + logFileName;
+        return "target" + File.separator + "quarkus.log";
     }
 }
