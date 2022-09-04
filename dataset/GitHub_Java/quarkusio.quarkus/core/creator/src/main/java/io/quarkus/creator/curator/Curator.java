7c63204093ccc3cc812aaddc2396313e5ab16119
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:31:31 +0200

    Raise a warning if a banned dependency is used
        For now, I only added the javax/jakarta/jboss ones but we could also add
        the logging ones in the future.

--- dataset/GitHub_Java/quarkusio.quarkus/core/creator/src/main/java/io/quarkus/creator/curator/Curator.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/creator/src/main/java/io/quarkus/creator/curator/Curator.java
@@ -47,7 +47,7 @@ public class Curator {
 
     public static CurateOutcome run(CuratedApplicationCreator ctx) throws AppCreatorException {
 
-        log.debug("provideOutcome depsOrigin=" + ctx.getDepsOrigin() + ", versionUpdate=" + ctx.getUpdate()
+        log.info("provideOutcome depsOrigin=" + ctx.getDepsOrigin() + ", versionUpdate=" + ctx.getUpdate()
                 + ", versionUpdateNumber="
                 + ctx.getUpdateNumber());
 
