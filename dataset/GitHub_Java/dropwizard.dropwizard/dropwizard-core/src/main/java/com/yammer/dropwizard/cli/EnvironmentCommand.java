0da9ae4440135dab490242c5f19680e48b3f66d3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:55:04 +0200

    Moving logging init to EnvironmentCommand.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/cli/EnvironmentCommand.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/cli/EnvironmentCommand.java
@@ -4,46 +4,25 @@ import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
-import net.sourceforge.argparse4j.inf.Namespace;
+import com.yammer.dropwizard.config.LoggingFactory;
 
-/**
- * A command which executes with a configured {@link Environment}.
- *
- * @param <T> the {@link Configuration} subclass which is loaded from the configuration file
- * @see Configuration
- */
 public abstract class EnvironmentCommand<T extends Configuration> extends ConfiguredCommand<T> {
     private final Service<T> service;
 
-    /**
-     * Creates a new environment command.
-     *
-     * @param service     the service providing this command
-     * @param name        the name of the command, used for command line invocation
-     * @param description a description of the command's purpose
-     */
-    protected EnvironmentCommand(Service<T> service, String name, String description) {
-        super(name, description);
+    protected EnvironmentCommand(Service<T> service) {
         this.service = service;
     }
 
     @Override
-    protected final void run(Bootstrap<T> bootstrap, Namespace namespace, T configuration) throws Exception {
+    protected void run(Bootstrap<T> bootstrap, T configuration) throws Exception {
         final Environment environment = new Environment(bootstrap.getName(),
                                                         configuration,
                                                         bootstrap.getObjectMapperFactory().clone());
-        bootstrap.runWithBundles(configuration, environment);
-        service.run(configuration, environment);
-        run(environment, namespace, configuration);
+        bootstrap.runWithBundles(service, configuration, environment);
+        new LoggingFactory(configuration.getLoggingConfiguration(),
+                           bootstrap.getName()).configure();
+        run(environment, configuration);
     }
 
-    /**
-     * Runs the command with the given {@link Environment} and {@link Configuration}.
-     *
-     * @param environment   the configured environment
-     * @param namespace     the parsed command line namespace
-     * @param configuration the configuration object
-     * @throws Exception if something goes wrong
-     */
-    protected abstract void run(Environment environment, Namespace namespace, T configuration) throws Exception;
+    protected abstract void run(Environment environment, T configuration) throws Exception;
 }
