a8bf2d280fa5042e050cc24c4372c0184dcb91f2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:51:50 +0200

    Remove link to nonexistent Kakfa documentation.
        Fixes #983
        Refs Graylog2/documentation#19

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-inputs/src/main/java/org/graylog2/inputs/kafka/KafkaInput.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-inputs/src/main/java/org/graylog2/inputs/kafka/KafkaInput.java
@@ -1,276 +1,89 @@
 /**
- * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.inputs.kafka;
 
-import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
-import kafka.consumer.*;
-import kafka.javaapi.consumer.ConsumerConnector;
-import kafka.message.MessageAndMetadata;
-import org.graylog2.plugin.InputHost;
-import org.graylog2.plugin.Message;
-import org.graylog2.plugin.RadioMessage;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+import org.graylog2.inputs.codecs.RadioMessageCodec;
+import org.graylog2.inputs.transports.KafkaTransport;
+import org.graylog2.plugin.LocalMetricRegistry;
+import org.graylog2.plugin.ServerStatus;
 import org.graylog2.plugin.configuration.Configuration;
-import org.graylog2.plugin.configuration.ConfigurationException;
-import org.graylog2.plugin.configuration.ConfigurationRequest;
-import org.graylog2.plugin.configuration.fields.ConfigurationField;
-import org.graylog2.plugin.configuration.fields.NumberField;
-import org.graylog2.plugin.configuration.fields.TextField;
 import org.graylog2.plugin.inputs.MessageInput;
-import org.graylog2.plugin.inputs.MisfireException;
-import org.joda.time.DateTime;
-import org.msgpack.MessagePack;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.graylog2.plugin.inputs.annotations.ConfigClass;
+import org.graylog2.plugin.inputs.annotations.FactoryClass;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
+import javax.inject.Inject;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
 public class KafkaInput extends MessageInput {
+    private static final String NAME = "Kafka Input";
 
-    // Kaefer.
+    @AssistedInject
+    public KafkaInput(@Assisted Configuration configuration,
+                      MetricRegistry metricRegistry,
+                      KafkaTransport.Factory transport,
+                      RadioMessageCodec.Factory codec,
+                      LocalMetricRegistry localRegistry,
+                      Config config,
+                      Descriptor descriptor, ServerStatus serverStatus) {
+        this(metricRegistry,
+                configuration,
+                transport.create(configuration),
+                codec.create(configuration),
+                localRegistry,
+                config,
+                descriptor, serverStatus);
+    }
 
-    private static final Logger LOG = LoggerFactory.getLogger(KafkaInput.class);
+    protected KafkaInput(MetricRegistry metricRegistry,
+                         Configuration configuration,
+                         KafkaTransport radioKafkaTransport,
+                         RadioMessageCodec radioMessageCodec,
+                         LocalMetricRegistry localRegistry,
+                         MessageInput.Config config,
+                         MessageInput.Descriptor descriptor, ServerStatus serverStatus) {
+        super(metricRegistry, configuration, radioKafkaTransport, localRegistry, radioMessageCodec, config, descriptor, serverStatus);
+    }
 
-    public static final String NAME = "Kafka Input";
+    @FactoryClass
+    public interface Factory extends MessageInput.Factory<KafkaInput> {
+        @Override
+        KafkaInput create(Configuration configuration);
 
-    private InputHost server;
-    private Configuration config;
-    private ConsumerConnector cc;
+        @Override
+        Config getConfig();
 
-    private boolean stopped = false;
+        @Override
+        Descriptor getDescriptor();
+    }
 
-    private AtomicLong totalBytesRead = new AtomicLong(0);
-    private AtomicLong lastSecBytesRead = new AtomicLong(0);
-    private AtomicLong lastSecBytesReadTmp = new AtomicLong(0);
-
-    @Override
-    public void configure(Configuration config, InputHost graylogServer) throws ConfigurationException {
-        this.server = graylogServer;
-        this.config = config;
-
-        if (!checkConfig(config)) {
-            throw new ConfigurationException(config.getSource().toString());
+    public static class Descriptor extends MessageInput.Descriptor {
+        @Inject
+        public Descriptor() {
+            super(NAME, false, "");
         }
     }
 
-    public static final String GROUP_ID = "graylog2";
-
-    public static final String CK_FETCH_MIN_BYTES = "fetch_min_bytes";
-    public static final String CK_FETCH_WAIT_MAX = "fetch_wait_max";
-    public static final String CK_ZOOKEEPER = "zookeeper";
-    public static final String CK_TOPIC_FILTER = "topic_filter";
-
-    @Override
-    public void launch() throws MisfireException {
-        setupMetrics();
-
-        Properties props = new Properties();
-
-        props.put("group.id", GROUP_ID);
-        props.put("client.id", "gl2-" + server.getNodeId() + "-" + getId());
-
-        props.put("fetch.min.bytes", String.valueOf(config.getInt(CK_FETCH_MIN_BYTES)));
-        props.put("fetch.wait.max.ms", String.valueOf(config.getInt(CK_FETCH_WAIT_MAX)));
-        props.put("zookeeper.connect", config.getString(CK_ZOOKEEPER));
-
-        ConsumerConfig consumerConfig = new ConsumerConfig(props);
-        cc = Consumer.createJavaConsumerConnector(consumerConfig);
-
-        TopicFilter filter = new Whitelist(config.getString(CK_TOPIC_FILTER));
-
-        List<KafkaStream<byte[], byte[]>> streams = cc.createMessageStreamsByFilter(filter, 5);
-        ExecutorService executor = Executors.newFixedThreadPool(5);
-
-        final MessageInput thisInput = this;
-
-        for (final KafkaStream<byte[], byte[]> stream : streams) {
-            executor.submit(new Runnable() {
-                public void run() {
-                    MessagePack msgpack = new MessagePack();
-
-                    for(MessageAndMetadata message : stream) {
-                        if (stopped) {
-                            return;
-                        }
-
-                        try {
-                            byte[] bytes = (byte[]) message.message();
-
-                            totalBytesRead.addAndGet(bytes.length);
-                            lastSecBytesReadTmp.addAndGet(bytes.length);
-
-                            RadioMessage msg = msgpack.read(bytes, RadioMessage.class);
-
-                            if (!msg.strings.containsKey("message") || !msg.strings.containsKey("source") || msg.timestamp <= 0) {
-                                LOG.error("Incomplete radio message. Skipping.");
-                                continue;
-                            }
-
-                            Message event = new Message(
-                                    msg.strings.get("message"),
-                                    msg.strings.get("source"),
-                                    new DateTime(msg.timestamp)
-                            );
-
-                            event.addStringFields(msg.strings);
-                            event.addIntegerFields(msg.ints);
-
-                            server.getProcessBuffer().insertCached(event, thisInput);
-                        } catch (Exception e) {
-                            LOG.error("Error while trying to process Kafka message.", e);
-                            continue;
-                        }
-                    }
-                }
-            });
-        }
-
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                lastSecBytesRead.set(lastSecBytesReadTmp.get());
-                lastSecBytesReadTmp.set(0);
-            }
-        }, 1, 1, TimeUnit.SECONDS);
-    }
-
-    @Override
-    public void stop() {
-        stopped = true;
-
-        if (cc != null) {
-            cc.shutdown();
+    @ConfigClass
+    public static class Config extends MessageInput.Config {
+        @Inject
+        public Config(KafkaTransport.Factory transport, RadioMessageCodec.Factory codec) {
+            super(transport.getConfig(), codec.getConfig());
         }
     }
-
-    @Override
-    public ConfigurationRequest getRequestedConfiguration() {
-        ConfigurationRequest cr = new ConfigurationRequest();
-
-        cr.addField(new TextField(
-                CK_ZOOKEEPER,
-                "ZooKeeper address",
-                "192.168.1.1:2181",
-                "Host and port of the ZooKeeper that is managing your Kafka cluster.",
-                ConfigurationField.Optional.NOT_OPTIONAL
-
-        ));
-
-        cr.addField(new TextField(
-                CK_TOPIC_FILTER,
-                "Topic filter regex",
-                "^graylog2$",
-                "Every topic that matches this regular expression will be consumed.",
-                ConfigurationField.Optional.NOT_OPTIONAL
-
-        ));
-
-        cr.addField(new NumberField(
-                CK_FETCH_MIN_BYTES,
-                "Fetch minimum bytes",
-                5,
-                "Wait for a message batch to reach at least this size or the configured maximum wait time before fetching.",
-                ConfigurationField.Optional.NOT_OPTIONAL)
-        );
-
-        cr.addField(new NumberField(
-                CK_FETCH_WAIT_MAX,
-                "Fetch maximum wait time (ms)",
-                100,
-                "Wait for this time or the configured minimum size of a message batch before fetching.",
-                ConfigurationField.Optional.NOT_OPTIONAL)
-        );
-
-        return cr;
-    }
-
-    @Override
-    public boolean isExclusive() {
-        return false;
-    }
-
-    @Override
-    public String getName() {
-        return NAME;
-    }
-
-    @Override
-    public String linkToDocs() {
-        return "";
-    }
-
-    @Override
-    public Map<String, Object> getAttributes() {
-        return config.getSource();
-    }
-
-    protected boolean checkConfig(Configuration config) {
-        return config.intIsSet(CK_FETCH_MIN_BYTES)
-                && config.intIsSet(CK_FETCH_WAIT_MAX)
-                && config.stringIsSet(CK_ZOOKEEPER)
-                && config.stringIsSet(CK_TOPIC_FILTER);
-    }
-
-    private void setupMetrics() {
-        server.metrics().register(MetricRegistry.name(getUniqueReadableId(), "read_bytes_1sec"), new Gauge<Long>() {
-            @Override
-            public Long getValue() {
-                // TODO
-                return lastSecBytesRead.get();
-            }
-        });
-
-        server.metrics().register(MetricRegistry.name(getUniqueReadableId(), "written_bytes_1sec"), new Gauge<Long>() {
-            @Override
-            public Long getValue() {
-                // TODO
-                return 0L;
-            }
-        });
-
-        server.metrics().register(MetricRegistry.name(getUniqueReadableId(), "read_bytes_total"), new Gauge<Long>() {
-            @Override
-            public Long getValue() {
-                return totalBytesRead.get();
-            }
-        });
-
-        server.metrics().register(MetricRegistry.name(getUniqueReadableId(), "written_bytes_total"), new Gauge<Long>() {
-            @Override
-            public Long getValue() {
-                // TODO
-                return 0L;
-            }
-        });
-
-    }
-
 }
