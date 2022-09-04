829a65ff026b2326dafe097d8a1c261fd1626481
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:28:38 +0200

    Fix OpenTracing / RESTEasy native build.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-opentracing/runtime/src/main/java/io/quarkus/smallrye/opentracing/runtime/graal/Target_org_jboss_resteasy_microprofile_config_ServletContextConfigSource.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-opentracing/runtime/src/main/java/io/quarkus/smallrye/opentracing/runtime/graal/Target_org_jboss_resteasy_microprofile_config_ServletContextConfigSource.java
@@ -1,5 +1,7 @@
 package io.quarkus.smallrye.opentracing.runtime.graal;
 
+import java.util.function.BooleanSupplier;
+
 import org.jboss.resteasy.microprofile.config.ServletContextConfigSource;
 import org.jboss.resteasy.microprofile.config.ServletContextConfigSourceImpl;
 
@@ -17,15 +19,30 @@ import com.oracle.svm.core.annotate.TargetClass;
  * GraalVM failing because the class cannot be instantiated.
  *
  * See https://github.com/quarkusio/quarkus/issues/9086
+ *
+ * Furthermore, we only reference the class by its name as not avoid having GraalVM fail if the class is not on classpath
+ * at all.
+ *
+ * See https://github.com/quarkusio/quarkus/issues/14876
  */
-@TargetClass(value = ServletContextConfigSource.class, onlyWith = UndertowMissing.class)
+@TargetClass(className = "org.jboss.resteasy.microprofile.config.ServletContextConfigSource", onlyWith = {
+        UndertowMissing.class,
+        Target_org_jboss_resteasy_microprofile_config_ServletContextConfigSource.ServletContextConfigSourceIsLoaded.class
+})
 final class Target_org_jboss_resteasy_microprofile_config_ServletContextConfigSource {
-
-    @Alias
-    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.FromAlias)
-    private static boolean SERVLET_AVAILABLE = false;
-
     @Alias
     @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)
     private static Class<?> clazz;
+
+    static class ServletContextConfigSourceIsLoaded implements BooleanSupplier {
+        @Override
+        public boolean getAsBoolean() {
+            try {
+                Class.forName("org.jboss.resteasy.microprofile.config.ServletContextConfigSource");
+                return true;
+            } catch (ClassNotFoundException e) {
+                return false;
+            }
+        }
+    }
 }
