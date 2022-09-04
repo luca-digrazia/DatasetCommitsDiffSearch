674fdba4e46b24cbe7b0db12ea73283f11955ef8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:15:56 +0200

    Add license headers.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/logs/RemoteLoggersResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/logs/RemoteLoggersResource.java
@@ -1,18 +1,18 @@
-/*
- * Copyright (C) 2020 Graylog, Inc.
+/**
+ * This file is part of Graylog.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.rest.resources.system.logs;
 
@@ -25,18 +25,18 @@ import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface RemoteLoggersResource {
-    @GET("system/loggers")
+    @GET("/system/loggers")
     Call<LoggersSummary> loggers();
 
-    @GET("system/loggers/subsystems")
+    @GET("/system/loggers/subsystems")
     Call<SubsystemSummary> subsystems();
 
-    @PUT("system/loggers/subsystems/{subsystem}/level/{level}")
+    @PUT("/system/loggers/subsystems/{subsystem}/level/{level}")
     Call<Void> setSubsystemLoggerLevel(@Path("subsystem") String subsystemTitle, @Path("level") String level);
 
-    @PUT("system/loggers/{loggerName}/level/{level}")
-    Call<Void> setSingleLoggerLevel(@Path("loggerName") String loggerName, @Path("level") String level);
+    @PUT("/system/loggers/{loggerName}/level/{level}")
+    Call<Void> setSingleLoggerLevel(String loggerName, String level);
 
-    @GET("system/loggers/messages/recent")
+    @GET("/system/loggers/messages/recent")
     Call<LogMessagesSummary> messages(int limit, String level);
 }
