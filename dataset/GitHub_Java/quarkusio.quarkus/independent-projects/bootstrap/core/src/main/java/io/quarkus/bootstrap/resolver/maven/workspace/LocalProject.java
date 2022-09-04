bec5ae2cb8da44b8808e474e31d86822b85f9b09
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:19:04 +0200

    Properly handle sources configured with ${project.basedir} in dev mode
        Fixes: #2367

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/bootstrap/core/src/main/java/io/quarkus/bootstrap/resolver/maven/workspace/LocalProject.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/bootstrap/core/src/main/java/io/quarkus/bootstrap/resolver/maven/workspace/LocalProject.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.bootstrap.resolver.maven.workspace;
 
 import java.io.IOException;
@@ -32,15 +48,11 @@ public class LocalProject {
     private static final String POM_XML = "pom.xml";
 
     public static LocalProject load(Path path) throws BootstrapException {
-        return new LocalProject(readModel(locateCurrentProjectDir(path, true).resolve(POM_XML)), null);
+        return new LocalProject(readModel(locateCurrentProjectDir(path).resolve(POM_XML)), null);
     }
 
     public static LocalProject loadWorkspace(Path path) throws BootstrapException {
-        return loadWorkspace(path, true);
-    }
-
-    public static LocalProject loadWorkspace(Path path, boolean required) throws BootstrapException {
-        final Path currentProjectDir = locateCurrentProjectDir(path, required);
+        final Path currentProjectDir = locateCurrentProjectDir(path);
         final LocalWorkspace ws = new LocalWorkspace();
         final LocalProject project = load(ws, null, loadRootModel(currentProjectDir), currentProjectDir);
         return project == null ? load(ws, null, readModel(currentProjectDir.resolve(POM_XML)), currentProjectDir) : project;
@@ -101,7 +113,7 @@ public class LocalProject {
         }
     }
 
-    private static Path locateCurrentProjectDir(Path path, boolean required) throws BootstrapException {
+    private static Path locateCurrentProjectDir(Path path) throws BootstrapException {
         Path p = path;
         while(p != null) {
             if(Files.exists(p.resolve(POM_XML))) {
@@ -109,10 +121,7 @@ public class LocalProject {
             }
             p = p.getParent();
         }
-        if(required) {
-            throw new BootstrapException("Failed to locate project pom.xml for " + path);
-        }
-        return null;
+        throw new BootstrapException("Failed to locate project pom.xml for " + path);
     }
 
     private final Model rawModel;
