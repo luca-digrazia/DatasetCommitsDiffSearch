d1bba7f20b31d584957b3a2a5b74af7be9a9f698
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:15:22 +0200

    Making defensive copy of Message before formatting it
        Fixes #559.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/alerts/MessageFormatter.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/alerts/MessageFormatter.java
@@ -1,25 +1,13 @@
-/**
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.alerts;
 
 import com.google.common.collect.Lists;
+import edu.emory.mathcs.backport.java.util.Collections;
 import org.graylog2.plugin.Message;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Dennis Oelkers <dennis@torch.sh>
@@ -40,7 +28,7 @@ public class MessageFormatter {
         fields.remove("message");
 
         List<String> fieldKeys = Lists.newArrayList(fields.keySet());
-        Collections.sort(fieldKeys, new Comparator<String>() {
+        Collections.sort(fieldKeys, new Comparator<String>(){
             @Override
             public int compare(String o1, String o2) {
                 return o1.compareTo(o2);
