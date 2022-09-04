69e3f41aeffb1af54e71fdb0bef90b0776293b52
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 20:00:04 +0200

    Gracefully handle if Windows DEF-file-specific cc_<rule> attributes are missing.
        PiperOrigin-RevId: 244362486

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CcLibrary.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CcLibrary.java
@@ -405,7 +405,7 @@ public abstract class CcLibrary implements RuleConfiguredTargetFactory {
     }
 
     if (!featureConfiguration.isEnabled(CppRuleClasses.HEADER_MODULE_CODEGEN)) {
-      warnAboutEmptyLibraries(ruleContext, ccCompilationOutputs, linkStatic);
+      warnAboutEmptyLibraries(ruleContext, compilationInfo.getCcCompilationOutputs(), linkStatic);
     }
     NestedSet<Artifact> filesToBuild = filesBuilder.build();
 
