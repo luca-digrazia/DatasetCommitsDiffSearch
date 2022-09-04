bf8408507fd37ac4d0b8a984d4e43d3643b4cfb4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:27:35 +0200

    Provide cleaner solution for marking tests as beans

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit4/src/main/java/io/quarkus/test/junit4/QuarkusTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit4/src/main/java/io/quarkus/test/junit4/QuarkusTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.test.junit4;
 
 import static io.quarkus.test.common.PathTestHelper.getAppClassLocation;
@@ -6,20 +22,17 @@ import static io.quarkus.test.common.PathTestHelper.getTestClassesLocation;
 import java.io.IOException;
 import java.util.function.Consumer;
 
+import org.jboss.builder.BuildChainBuilder;
+import org.jboss.builder.BuildContext;
+import org.jboss.builder.BuildStep;
 import org.junit.runner.RunWith;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.model.InitializationError;
 
-import io.quarkus.builder.BuildChainBuilder;
-import io.quarkus.builder.BuildContext;
-import io.quarkus.builder.BuildStep;
 import io.quarkus.deployment.builditem.TestAnnotationBuildItem;
 import io.quarkus.runner.RuntimeRunner;
 import io.quarkus.runtime.LaunchMode;
 import io.quarkus.test.common.PropertyTestUtil;
-import io.quarkus.test.common.TestInjectionManager;
-import io.quarkus.test.common.TestInstantiator;
-import io.quarkus.test.common.http.TestHTTPResourceManager;
 
 public class QuarkusTest extends AbstractQuarkusTestRunner {
 
@@ -63,14 +76,5 @@ public class QuarkusTest extends AbstractQuarkusTestRunner {
         protected void stopQuarkus() throws IOException {
             runtimeRunner.close();
         }
-    }
-
-    @Override
-    protected Object createTest() throws Exception {
-        Object instance = TestInstantiator.instantiateTest(getTestClass().getJavaClass());
-        TestHTTPResourceManager.inject(instance);
-        TestInjectionManager.inject(instance);
-        quarkusRunListener.inject(instance);
-        return instance;
     }
 }
