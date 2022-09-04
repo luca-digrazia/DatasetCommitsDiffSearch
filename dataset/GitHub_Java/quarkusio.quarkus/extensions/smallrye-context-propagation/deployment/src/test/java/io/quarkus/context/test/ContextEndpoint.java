4e2a5fea9df29db40e07666b11650d4e4d9fb70d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:16:35 +0200

    Fixed Arc test for Quarkus and MP-CP

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/deployment/src/test/java/io/quarkus/context/test/ContextEndpoint.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/deployment/src/test/java/io/quarkus/context/test/ContextEndpoint.java
@@ -15,13 +15,12 @@ import javax.ws.rs.core.UriInfo;
 
 import org.eclipse.microprofile.context.ManagedExecutor;
 import org.junit.jupiter.api.Assertions;
-import org.wildfly.common.Assert;
 
 import io.quarkus.arc.Arc;
 
 @Path("/context")
 public class ContextEndpoint {
-
+    
     @Inject
     RequestBean doNotRemoveMe;
 
@@ -43,33 +42,14 @@ public class ContextEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public CompletionStage<String> arcTest() {
         ManagedExecutor me = ManagedExecutor.builder().build();
-        Assert.assertTrue(Arc.container().instance(RequestBean.class).isAvailable());
         RequestBean instance = Arc.container().instance(RequestBean.class).get();
-        String previousValue = instance.callMe();
-        System.err.println("Got bean1: " + previousValue);
+        System.err.println("Got bean1: " + instance.callMe());
+        Assertions.assertNotNull(instance);
         CompletableFuture<String> ret = me.completedFuture("OK");
         return ret.thenApplyAsync(text -> {
             RequestBean instance2 = Arc.container().instance(RequestBean.class).get();
             System.err.println("Got bean2: " + instance2.callMe());
-            Assertions.assertEquals(previousValue, instance2.callMe());
-            return text;
-        });
-    }
-
-    @GET
-    @Path("/noarc")
-    @Produces(MediaType.APPLICATION_JSON)
-    public CompletionStage<String> noarcTest() {
-        ManagedExecutor me = ManagedExecutor.builder().cleared("CDI").build();
-        Assert.assertTrue(Arc.container().instance(RequestBean.class).isAvailable());
-        RequestBean instance = Arc.container().instance(RequestBean.class).get();
-        String previousValue = instance.callMe();
-        System.err.println("Got bean1: " + previousValue);
-        CompletableFuture<String> ret = me.completedFuture("OK");
-        return ret.thenApplyAsync(text -> {
-            RequestBean instance2 = Arc.container().instance(RequestBean.class).get();
-            System.err.println("Got bean2: " + instance2.callMe());
-            Assertions.assertNotEquals(previousValue, instance2.callMe());
+            Assertions.assertEquals(instance, instance2);
             return text;
         });
     }
