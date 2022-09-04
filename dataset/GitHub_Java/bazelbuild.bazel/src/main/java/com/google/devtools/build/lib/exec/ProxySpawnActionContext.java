1e7b833b0da9742bebed6675a7e3b1e31ae6775a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:43:53 +0200

    added a user-friendly message for migration to a list-based strategy
        remote: added a user-friendly message for migration to a list-based strategy
    
        to improve and simplify a migration after a flag flip (#7480) added a link to the issue with migration advises
    
        Closes #8418.
    
        PiperOrigin-RevId: 249805150

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/ProxySpawnActionContext.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/ProxySpawnActionContext.java
@@ -95,10 +95,9 @@ public final class ProxySpawnActionContext implements SpawnActionContext {
       if (listBasedExecutionStrategySelection) {
         throw new UserExecException(
             String.format(
-                "No usable spawn strategy found for spawn with mnemonic %s.  Your"
-                    + " --spawn_strategy, --genrule_strategy or --strategy flags are probably too"
-                    + " strict. Visit https://github.com/bazelbuild/bazel/issues/7480 for"
-                    + " migration advice",
+                "No usable spawn strategy found for spawn with mnemonic %s.  Your --spawn_strategy"
+                    + "or --strategy flags are probably too strict. "
+                    + "Visit https://github.com/bazelbuild/bazel/issues/7480 for migration advises",
                 spawn.getMnemonic()));
       } else {
         throw new UserExecException(
