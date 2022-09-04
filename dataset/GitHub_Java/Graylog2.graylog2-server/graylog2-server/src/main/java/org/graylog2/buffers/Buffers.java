3a96c185f3e94fb8dede1ad1b30533c3d1b267cb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:10:47 +0200

    Drain input buffer when shutting down server (#10247)
        * also drain input buffer in buffer synchronizer
    
        * Use locale in #toLowerCase()
    
        * Fix unused method and start using it

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/buffers/Buffers.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/buffers/Buffers.java
@@ -1,50 +1,145 @@
-/**
- * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+/*
+ * Copyright (C) 2020 Graylog, Inc.
  *
- * This file is part of Graylog2.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package org.graylog2.buffers;
 
-import org.graylog2.Core;
+import com.github.rholder.retry.RetryException;
+import com.github.rholder.retry.Retryer;
+import com.github.rholder.retry.RetryerBuilder;
+import com.github.rholder.retry.StopStrategies;
+import com.github.rholder.retry.WaitStrategies;
+import com.google.common.base.Predicates;
+import org.graylog2.plugin.buffers.EventBuffer;
+import org.graylog2.plugin.buffers.InputBuffer;
+import org.graylog2.shared.buffers.ProcessBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
+import javax.inject.Inject;
+import java.util.EnumSet;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.graylog2.buffers.Buffers.Type.INPUT;
+import static org.graylog2.buffers.Buffers.Type.OUTPUT;
+import static org.graylog2.buffers.Buffers.Type.PROCESS;
+
 public class Buffers {
-
-    private static final Logger LOG = LoggerFactory.getLogger(Buffers.class);
-
-    public static void waitForEmptyBuffers(Core core) {
-        // Wait until the buffers are empty. Messages that were already started to be processed must be fully processed.
-        while(true) {
-            LOG.info("Waiting until all buffers are empty.");
-            if(core.getProcessBuffer().isEmpty() && core.getOutputBuffer().isEmpty()) {
-                break;
-            }
-
-            try {
-                LOG.info("Not all buffers are empty. Waiting another second. ({}p/{}o)", core.getProcessBuffer().getUsage(), core.getOutputBuffer().getUsage());
-                Thread.sleep(1000);
-            } catch (InterruptedException e) { /* */ }
-        }
-
-        LOG.info("All buffers and caches are empty. Continuing.");
+    public enum Type {
+        INPUT,
+        PROCESS,
+        OUTPUT,
     }
 
+    private static final Logger LOG = LoggerFactory.getLogger(Buffers.class);
+    private static final long DEFAULT_MAX_WAIT = 30L;
+
+    private final InputBuffer inputBuffer;
+    private final ProcessBuffer processBuffer;
+    private final OutputBuffer outputBuffer;
+
+    @Inject
+    public Buffers(InputBuffer inputBuffer, final ProcessBuffer processBuffer, final OutputBuffer outputBuffer) {
+        this.inputBuffer = inputBuffer;
+        this.processBuffer = processBuffer;
+        this.outputBuffer = outputBuffer;
+    }
+
+    /**
+     * @deprecated Use {@link #waitForEmptyBuffers(EnumSet)} instead
+     */
+    @Deprecated
+    public void waitForEmptyBuffers() {
+        waitForEmptyBuffers(EnumSet.of(PROCESS, OUTPUT));
+    }
+
+    /**
+     * Wait until the buffers of the given types have been drained or abort after 30 seconds
+     */
+    public void waitForEmptyBuffers(EnumSet<Type> bufferTypes) {
+        waitForEmptyBuffers(bufferTypes, DEFAULT_MAX_WAIT, TimeUnit.SECONDS);
+    }
+
+    /**
+     * @deprecated Use {@link #waitForEmptyBuffers(EnumSet, long, TimeUnit)} instead
+     */
+    @Deprecated
+    public void waitForEmptyBuffers(final long maxWait, final TimeUnit timeUnit) {
+        waitForEmptyBuffers(EnumSet.of(PROCESS, OUTPUT), maxWait, timeUnit);
+    }
+
+    /**
+     * Wait until the buffers of the given types have been drained or abort after a given maximum waiting time
+     */
+    public void waitForEmptyBuffers(EnumSet<Type> bufferTypes, final long maxWait, final TimeUnit timeUnit) {
+        if (bufferTypes.isEmpty()) {
+            return;
+        }
+        LOG.info("Waiting until {} buffers are empty.", bufferTypes);
+
+        final Map<Type, EventBuffer> buffersByTypes = buffersByTypes(bufferTypes);
+
+        final Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+                .retryIfResult(Predicates.not(Predicates.equalTo(Boolean.TRUE)))
+                .withWaitStrategy(WaitStrategies.fixedWait(1, TimeUnit.SECONDS))
+                .withStopStrategy(StopStrategies.stopAfterDelay(maxWait, timeUnit))
+                .build();
+
+        try {
+            retryer.call(() -> {
+                if (buffersByTypes.values().stream().allMatch(EventBuffer::isEmpty)) {
+                    return true;
+                } else {
+                    LOG.info("Waiting for buffers to drain. ({})", getUsageStats(buffersByTypes));
+                }
+                return false;
+            });
+        } catch (RetryException e) {
+            LOG.info("Buffers not empty after {} {}. Giving up.", maxWait, timeUnit.name().toLowerCase(Locale.ENGLISH));
+            return;
+        } catch (ExecutionException e) {
+            LOG.error("Error while waiting for empty buffers.", e);
+            return;
+        }
+
+        LOG.info("All buffers are empty. Continuing.");
+    }
+
+    // e.g. 123i/432p/545o
+    private String getUsageStats(Map<Type, EventBuffer> buffersByTypes) {
+        return buffersByTypes.entrySet().stream()
+                .map(e -> e.getValue().getUsage() + e.getKey().name().substring(0, 1).toLowerCase(Locale.ENGLISH))
+                .collect(Collectors.joining("/"));
+    }
+
+    private Map<Type, EventBuffer> buffersByTypes(EnumSet<Type> bufferTypes) {
+        Map<Type, EventBuffer> bufferMap = new LinkedHashMap<>();
+        if (bufferTypes.contains(INPUT)) {
+            bufferMap.put(INPUT, inputBuffer);
+        }
+        if (bufferTypes.contains(PROCESS)) {
+            bufferMap.put(PROCESS, processBuffer);
+        }
+        if (bufferTypes.contains(OUTPUT)) {
+            bufferMap.put(OUTPUT, outputBuffer);
+        }
+        return bufferMap;
+    }
 }
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/initializers/BufferSynchronizerService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/initializers/BufferSynchronizerService.java
@@ -1,49 +1,62 @@
 /*
- * Copyright 2012-2014 TORCH GmbH
+ * Copyright (C) 2020 Graylog, Inc.
  *
- * This file is part of Graylog2.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
-
 package org.graylog2.initializers;
 
+import com.codahale.metrics.InstrumentedExecutorService;
+import com.codahale.metrics.MetricRegistry;
 import com.google.common.util.concurrent.AbstractIdleService;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.graylog2.Configuration;
 import org.graylog2.buffers.Buffers;
-import org.graylog2.caches.Caches;
+import org.graylog2.indexer.cluster.Cluster;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.EnumSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
-/**
- * @author Dennis Oelkers <dennis@torch.sh>
- */
+import static com.codahale.metrics.MetricRegistry.name;
+import static org.graylog2.buffers.Buffers.Type.INPUT;
+import static org.graylog2.buffers.Buffers.Type.OUTPUT;
+import static org.graylog2.buffers.Buffers.Type.PROCESS;
+
 @Singleton
 public class BufferSynchronizerService extends AbstractIdleService {
-    private static final Logger log = LoggerFactory.getLogger(BufferSynchronizerService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(BufferSynchronizerService.class);
 
     private final Buffers bufferSynchronizer;
-    private final Caches cacheSynchronizer;
-
-    private volatile boolean indexerAvailable = true;
+    private final Cluster cluster;
+    private final Configuration configuration;
+    private final MetricRegistry metricRegistry;
 
     @Inject
-    public BufferSynchronizerService(Buffers bufferSynchronizer, Caches cacheSynchronizer) {
-        this.bufferSynchronizer = bufferSynchronizer;
-        this.cacheSynchronizer = cacheSynchronizer;
+    public BufferSynchronizerService(final Buffers buffers,
+                                     final Cluster cluster,
+                                     final Configuration configuration,
+                                     final MetricRegistry metricRegistry) {
+        this.bufferSynchronizer = buffers;
+        this.cluster = cluster;
+        this.configuration = configuration;
+        this.metricRegistry = metricRegistry;
     }
 
     @Override
@@ -52,15 +65,29 @@ public class BufferSynchronizerService extends AbstractIdleService {
 
     @Override
     protected void shutDown() throws Exception {
-        if (indexerAvailable) {
-            bufferSynchronizer.waitForEmptyBuffers();
-            cacheSynchronizer.waitForEmptyCaches();
+        LOG.debug("Stopping BufferSynchronizerService");
+        final ExecutorService executorService = executorService(metricRegistry);
+
+        EnumSet<Buffers.Type> buffersToDrain = EnumSet.of(INPUT);
+        if (cluster.isConnected() && cluster.isDeflectorHealthy()) {
+            buffersToDrain.add(PROCESS);
+            buffersToDrain.add(OUTPUT);
         } else {
-            log.warn("Indexer is unavailable, not waiting to clear buffers and caches, as we have no connection to Elasticsearch");
+            LOG.warn("Elasticsearch is unavailable. Not waiting to clear process and output buffers and caches, as we" +
+                    " have no healthy cluster.");
         }
+        executorService.submit(() -> bufferSynchronizer.waitForEmptyBuffers(buffersToDrain,
+                configuration.getShutdownTimeout(), TimeUnit.MILLISECONDS));
+        executorService.shutdown();
+        executorService.awaitTermination(configuration.getShutdownTimeout(), TimeUnit.MILLISECONDS);
+        LOG.debug("Stopped BufferSynchronizerService");
     }
 
-    public void setIndexerUnavailable() {
-        indexerAvailable = false;
+    private ExecutorService executorService(MetricRegistry metricRegistry) {
+        final ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("buffer-synchronizer-%d").build();
+        return new InstrumentedExecutorService(
+                Executors.newSingleThreadExecutor(threadFactory),
+                metricRegistry,
+                name(this.getClass(), "executor-service"));
     }
 }
