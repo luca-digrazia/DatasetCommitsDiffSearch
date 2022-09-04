70c9c8b3c0dd8939180bc497194368fa27ba59fe
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:11:51 +0200

    CDI observers - add support for Reception.IF_EXISTS

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/ObserverInfo.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/ObserverInfo.java
@@ -1,26 +1,35 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.arc.processor;
 
-import io.quarkus.arc.processor.BuildExtension.BuildContext;
-import io.quarkus.arc.processor.ObserverTransformer.ObserverTransformation;
-import io.quarkus.arc.processor.ObserverTransformer.TransformationContext;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Set;
 import javax.enterprise.event.Reception;
-import javax.enterprise.event.TransactionPhase;
 import javax.enterprise.inject.spi.DefinitionException;
 import javax.enterprise.inject.spi.ObserverMethod;
 import org.jboss.jandex.AnnotationInstance;
-import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.AnnotationTarget.Kind;
 import org.jboss.jandex.AnnotationValue;
-import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.MethodInfo;
 import org.jboss.jandex.MethodParameterInfo;
 import org.jboss.jandex.Type;
-import org.jboss.logging.Logger;
 
 /**
  * Represents an observer method.
@@ -28,62 +37,6 @@ import org.jboss.logging.Logger;
  * @author Martin Kouba
  */
 public class ObserverInfo implements InjectionTargetInfo {
-
-    private static final Logger LOGGER = Logger.getLogger(ObserverInfo.class.getName());
-
-    static ObserverInfo create(BeanInfo declaringBean, MethodInfo observerMethod, Injection injection, boolean isAsync,
-            List<ObserverTransformer> transformers, BuildContext buildContext) {
-        // Initialize attributes
-        MethodParameterInfo eventParameter = initEventParam(observerMethod, declaringBean.getDeployment());
-        Type observedType = observerMethod.parameters().get(eventParameter.position());
-        Set<AnnotationInstance> qualifiers = initQualifiers(declaringBean.getDeployment(), observerMethod, eventParameter);
-        Reception reception = initReception(isAsync, declaringBean.getDeployment(), observerMethod);
-        TransactionPhase transactionPhase = initTransactionPhase(isAsync, declaringBean.getDeployment(), observerMethod);
-        AnnotationInstance priorityAnnotation = observerMethod.annotation(DotNames.PRIORITY);
-        Integer priority;
-        if (priorityAnnotation != null && priorityAnnotation.target().equals(eventParameter)) {
-            priority = priorityAnnotation.value().asInt();
-        } else {
-            priority = ObserverMethod.DEFAULT_PRIORITY;
-        }
-
-        if (!transformers.isEmpty()) {
-            // Transform attributes if needed
-            ObserverTransformationContext context = new ObserverTransformationContext(buildContext, observerMethod,
-                    observedType, qualifiers, reception, transactionPhase, priority, isAsync);
-
-            for (ObserverTransformer transformer : transformers) {
-                if (transformer.appliesTo(observedType, qualifiers)) {
-                    transformer.transform(context);
-                    if (context.vetoed) {
-                        LOGGER.debugf("Observer method %s.%s() vetoed by %s", observerMethod.declaringClass().name(),
-                                observerMethod.name(), transformer.getClass().getName());
-                        break;
-                    }
-                }
-            }
-            if (context.vetoed) {
-                // Veto the observer method
-                return null;
-            }
-            qualifiers = context.getQualifiers();
-            reception = context.getReception();
-            transactionPhase = context.getTransactionPhase();
-            priority = context.getPriority();
-            isAsync = context.isAsync();
-        }
-
-        if (!TransactionPhase.IN_PROGRESS.equals(transactionPhase)) {
-            final ClassInfo clazz = observerMethod.declaringClass();
-            LOGGER.warnf("The method %s#%s makes use of '%s' transactional observers which are not implemented yet.", clazz,
-                    observerMethod.name(), transactionPhase);
-        }
-
-        // Create an immutable observer metadata
-        return new ObserverInfo(declaringBean, observerMethod, injection, eventParameter, isAsync, priority, reception,
-                transactionPhase, qualifiers);
-
-    }
 
     private final BeanInfo declaringBean;
 
@@ -99,25 +52,19 @@ public class ObserverInfo implements InjectionTargetInfo {
 
     private final boolean isAsync;
 
-    private final Reception reception;
-
-    private final TransactionPhase transactionPhase;
-
-    private final Set<AnnotationInstance> qualifiers;
-
-    ObserverInfo(BeanInfo declaringBean, MethodInfo observerMethod, Injection injection, MethodParameterInfo eventParameter,
-            boolean isAsync, int priority, Reception reception, TransactionPhase transactionPhase,
-            Set<AnnotationInstance> qualifiers) {
+    ObserverInfo(BeanInfo declaringBean, MethodInfo observerMethod, Injection injection, boolean isAsync) {
         this.declaringBean = declaringBean;
         this.observerMethod = observerMethod;
         this.injection = injection;
-        this.eventParameter = eventParameter;
+        this.eventParameter = initEventParam(observerMethod);
         this.eventMetadataParameterPosition = initEventMetadataParam(observerMethod);
+        AnnotationInstance priorityAnnotation = observerMethod.annotation(DotNames.PRIORITY);
+        if (priorityAnnotation != null && priorityAnnotation.target().equals(eventParameter)) {
+            this.priority = priorityAnnotation.value().asInt();
+        } else {
+            this.priority = ObserverMethod.DEFAULT_PRIORITY;
+        }
         this.isAsync = isAsync;
-        this.priority = priority;
-        this.reception = reception;
-        this.transactionPhase = transactionPhase;
-        this.qualifiers = qualifiers;
     }
 
     @Override
@@ -142,14 +89,6 @@ public class ObserverInfo implements InjectionTargetInfo {
         return eventParameter;
     }
 
-    public Reception getReception() {
-        return reception;
-    }
-
-    public TransactionPhase getTransactionPhase() {
-        return transactionPhase;
-    }
-
     int getEventMetadataParameterPosition() {
         return eventMetadataParameterPosition;
     }
@@ -162,12 +101,15 @@ public class ObserverInfo implements InjectionTargetInfo {
         return isAsync;
     }
 
-    public Type getObservedType() {
-        return observerMethod.parameters().get(eventParameter.position());
-    }
-
-    public Set<AnnotationInstance> getQualifiers() {
-        return qualifiers;
+    public Reception getReception() {
+        AnnotationInstance observesAnnotation = isAsync
+                ? declaringBean.getDeployment().getAnnotation(observerMethod, DotNames.OBSERVES_ASYNC)
+                : declaringBean.getDeployment().getAnnotation(observerMethod, DotNames.OBSERVES);
+        AnnotationValue receptionValue = observesAnnotation.value("notifyObserver");
+        if (receptionValue == null) {
+            return Reception.ALWAYS;
+        }
+        return Reception.valueOf(receptionValue.asEnum());
     }
 
     void init(List<Throwable> errors) {
@@ -176,34 +118,15 @@ public class ObserverInfo implements InjectionTargetInfo {
         }
     }
 
-    static Reception initReception(boolean isAsync, BeanDeployment beanDeployment, MethodInfo observerMethod) {
-        AnnotationInstance observesAnnotation = isAsync
-                ? beanDeployment.getAnnotation(observerMethod, DotNames.OBSERVES_ASYNC)
-                : beanDeployment.getAnnotation(observerMethod, DotNames.OBSERVES);
-        AnnotationValue receptionValue = observesAnnotation.value("notifyObserver");
-        if (receptionValue == null) {
-            return Reception.ALWAYS;
-        }
-        return Reception.valueOf(receptionValue.asEnum());
+    public Type getObservedType() {
+        return observerMethod.parameters().get(eventParameter.position());
     }
 
-    static TransactionPhase initTransactionPhase(boolean isAsync, BeanDeployment beanDeployment, MethodInfo observerMethod) {
-        AnnotationInstance observesAnnotation = isAsync
-                ? beanDeployment.getAnnotation(observerMethod, DotNames.OBSERVES_ASYNC)
-                : beanDeployment.getAnnotation(observerMethod, DotNames.OBSERVES);
-        AnnotationValue duringValue = observesAnnotation.value("during");
-        if (duringValue == null) {
-            return TransactionPhase.IN_PROGRESS;
-        }
-        return TransactionPhase.valueOf(duringValue.asEnum());
-    }
-
-    static Set<AnnotationInstance> initQualifiers(BeanDeployment beanDeployment, MethodInfo observerMethod,
-            MethodParameterInfo eventParameter) {
+    public Set<AnnotationInstance> getQualifiers() {
         Set<AnnotationInstance> qualifiers = new HashSet<>();
-        for (AnnotationInstance annotation : beanDeployment.getAnnotations(observerMethod)) {
+        for (AnnotationInstance annotation : declaringBean.getDeployment().getAnnotations(observerMethod)) {
             if (annotation.target().equals(eventParameter)
-                    && beanDeployment.getQualifier(annotation.name()) != null) {
+                    && declaringBean.getDeployment().getQualifier(annotation.name()) != null) {
                 qualifiers.add(annotation);
             }
         }
@@ -214,9 +137,9 @@ public class ObserverInfo implements InjectionTargetInfo {
         return priority;
     }
 
-    static MethodParameterInfo initEventParam(MethodInfo observerMethod, BeanDeployment beanDeployment) {
+    MethodParameterInfo initEventParam(MethodInfo observerMethod) {
         List<MethodParameterInfo> eventParams = new ArrayList<>();
-        for (AnnotationInstance annotation : beanDeployment.getAnnotations(observerMethod)) {
+        for (AnnotationInstance annotation : declaringBean.getDeployment().getAnnotations(observerMethod)) {
             if (Kind.METHOD_PARAMETER == annotation.target().kind()
                     && (annotation.name().equals(DotNames.OBSERVES) || annotation.name().equals(DotNames.OBSERVES_ASYNC))) {
                 eventParams.add(annotation.target().asMethodParameter());
@@ -237,137 +160,6 @@ public class ObserverInfo implements InjectionTargetInfo {
             }
         }
         return -1;
-    }
-
-    private static class ObserverTransformationContext extends AnnotationsTransformationContext<Set<AnnotationInstance>>
-            implements TransformationContext {
-
-        private final Type observedType;
-        private Reception reception;
-        private TransactionPhase transactionPhase;
-        private Integer priority;
-        private boolean vetoed;
-        private boolean async;
-
-        public ObserverTransformationContext(BuildContext buildContext, AnnotationTarget target,
-                Type observedType, Set<AnnotationInstance> qualifiers, Reception reception, TransactionPhase transactionPhase,
-                Integer priority, boolean async) {
-            super(buildContext, target, qualifiers);
-            this.observedType = observedType;
-            this.reception = reception;
-            this.transactionPhase = transactionPhase;
-            this.priority = priority;
-            this.async = async;
-        }
-
-        @Override
-        public MethodInfo getMethod() {
-            return getTarget().asMethod();
-        }
-
-        @Override
-        public Type getObservedType() {
-            return observedType;
-        }
-
-        @Override
-        public Set<AnnotationInstance> getQualifiers() {
-            return getAnnotations();
-        }
-
-        @Override
-        public Reception getReception() {
-            return reception;
-        }
-
-        @Override
-        public TransactionPhase getTransactionPhase() {
-            return transactionPhase;
-        }
-
-        public Integer getPriority() {
-            return priority;
-        }
-
-        @Override
-        public boolean isAsync() {
-            return async;
-        }
-
-        @Override
-        public void veto() {
-            this.vetoed = true;
-        }
-
-        @Override
-        public ObserverTransformation transform() {
-            return new ObserverTransformationImpl(new HashSet<>(getAnnotations()), getMethod(), this);
-        }
-
-    }
-
-    private static class ObserverTransformationImpl
-            extends AbstractAnnotationsTransformation<ObserverTransformation, Set<AnnotationInstance>>
-            implements ObserverTransformation {
-
-        private final ObserverTransformationContext context;
-        private Integer priority;
-        private Reception reception;
-        private TransactionPhase transactionPhase;
-        private Boolean async;
-
-        public ObserverTransformationImpl(Set<AnnotationInstance> qualifiers, MethodInfo observerMethod,
-                ObserverTransformationContext context) {
-            super(qualifiers, observerMethod, null);
-            this.context = context;
-        }
-
-        @Override
-        protected ObserverTransformation self() {
-            return this;
-        }
-
-        @Override
-        public ObserverTransformation priority(int priority) {
-            this.priority = priority;
-            return this;
-        }
-
-        @Override
-        public ObserverTransformation reception(Reception reception) {
-            this.reception = reception;
-            return this;
-        }
-
-        @Override
-        public ObserverTransformation transactionPhase(TransactionPhase transactionPhase) {
-            this.transactionPhase = transactionPhase;
-            return this;
-        }
-        
-        @Override
-        public ObserverTransformation async(boolean value) {
-            this.async = value;
-            return this;
-        }
-
-        @Override
-        public void done() {
-            context.setAnnotations(modifiedAnnotations);
-            if (reception != null) {
-                context.reception = reception;
-            }
-            if (priority != null) {
-                context.priority = priority;
-            }
-            if (transactionPhase != null) {
-                context.transactionPhase = transactionPhase;
-            }
-            if (async != null) {
-                context.async = async;
-            }
-        }
-
     }
 
 }
--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/observers/ifexists/ReceptionIfExistsTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/observers/ifexists/ReceptionIfExistsTest.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2018 Red Hat, Inc.
-
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,11 +19,9 @@ package io.quarkus.arc.test.observers.ifexists;
 import static org.junit.Assert.assertEquals;
 
 import io.quarkus.arc.Arc;
-import io.quarkus.arc.ArcContainer;
 import io.quarkus.arc.test.ArcTestContainer;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Observes;
@@ -41,38 +38,15 @@ public class ReceptionIfExistsTest {
 
     @Test
     public void testObserver() {
-        ArcContainer container = Arc.container();
-        container.beanManager().fireEvent("foo");
+        Arc.container().beanManager().fireEvent("foo");
         assertEquals(1, EVENTS.size());
         assertEquals(DependentObserver.class.getName() + "foo", EVENTS.get(0));
-
-        // Activate the request context but the instance still does not exist
-        EVENTS.clear();
-        container.requestContext().activate();
-        container.beanManager().fireEvent("foo");
-        assertEquals(1, EVENTS.size());
-        assertEquals(DependentObserver.class.getName() + "foo", EVENTS.get(0));
-        container.requestContext().deactivate();
-
-        // Activate the request context and the instance exists
-        EVENTS.clear();
-        container.requestContext().activate();
-        // Force bean instance creation
-        container.instance(RequestScopedObserver.class).get().ping();
-        container.beanManager().fireEvent("foo");
-        assertEquals(2, EVENTS.size());
-        assertEquals(RequestScopedObserver.class.getName() + "foo", EVENTS.get(0));
-        assertEquals(DependentObserver.class.getName() + "foo", EVENTS.get(1));
-        container.requestContext().deactivate();
     }
 
     @RequestScoped
     static class RequestScopedObserver {
 
-        void ping() {
-        }
-
-        void observeString(@Priority(1) @Observes(notifyObserver = Reception.IF_EXISTS) String value) {
+        void observeString(@Observes(notifyObserver = Reception.IF_EXISTS) String value) {
             EVENTS.add(RequestScopedObserver.class.getName() + value);
         }
 
@@ -81,7 +55,7 @@ public class ReceptionIfExistsTest {
     @Dependent
     static class DependentObserver {
 
-        void observeString(@Priority(2) @Observes String value) {
+        void observeString(@Observes String value) {
             EVENTS.add(DependentObserver.class.getName() + value);
         }
 
