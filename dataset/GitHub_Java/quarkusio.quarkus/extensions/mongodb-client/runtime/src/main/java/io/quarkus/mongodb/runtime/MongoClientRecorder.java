e291648806d60af0d93f068fc2d8e8fea0673619
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:00:51 +0200

    fix: wrong log of the error
        The error needs to be pass to the first parameter of the warnf method and not on the varargs parameter.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/mongodb-client/runtime/src/main/java/io/quarkus/mongodb/runtime/MongoClientRecorder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/mongodb-client/runtime/src/main/java/io/quarkus/mongodb/runtime/MongoClientRecorder.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.bson.codecs.configuration.CodecProvider;
@@ -44,7 +43,6 @@ import io.quarkus.runtime.annotations.Recorder;
 public class MongoClientRecorder {
 
     private static final Logger LOGGER = Logger.getLogger(MongoClientRecorder.class);
-    private static final Pattern COLON_PATTERN = Pattern.compile(":");
 
     private static volatile MongoClient client;
     private static volatile ReactiveMongoClient reactiveMongoClient;
@@ -253,7 +251,7 @@ public class MongoClientRecorder {
         return addresses.stream()
                 .map(String::trim)
                 .map(address -> {
-                    String[] segments = COLON_PATTERN.split(address);
+                    String[] segments = address.split(":");
                     if (segments.length == 1) {
                         // Host only, default port
                         return new ServerAddress(address);
