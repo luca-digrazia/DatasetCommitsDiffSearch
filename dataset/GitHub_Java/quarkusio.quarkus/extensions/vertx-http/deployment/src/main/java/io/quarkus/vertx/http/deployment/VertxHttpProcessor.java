3addac83422e5352fe4f77f5847dd93fe5a93bee
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:00:58 +0200

    Fix #6878
        Some classes from Vert.x Web must be initialized at runtime. It is due to a change in the Netty native-image configuration that avoid the usage of buffers
        at build time. This commit allows using SockJS and the event bus SockJS bridge in  native mode.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/VertxHttpProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/VertxHttpProcessor.java
@@ -1,64 +1,47 @@
 package io.quarkus.vertx.http.deployment;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.FileSystem;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.security.CodeSource;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.eclipse.microprofile.config.ConfigProvider;
-import org.jboss.logging.Logger;
 
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.deployment.BeanContainerBuildItem;
-import io.quarkus.bootstrap.util.ZipUtils;
 import io.quarkus.builder.BuildException;
 import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
-import io.quarkus.deployment.builditem.ApplicationStartBuildItem;
-import io.quarkus.deployment.builditem.ExecutorBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.builditem.ServiceStartBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
-import io.quarkus.deployment.builditem.ShutdownListenerBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
 import io.quarkus.deployment.logging.LogCleanupFilterBuildItem;
 import io.quarkus.kubernetes.spi.KubernetesPortBuildItem;
 import io.quarkus.netty.runtime.virtual.VirtualServerChannel;
 import io.quarkus.runtime.LaunchMode;
-import io.quarkus.runtime.LiveReloadConfig;
 import io.quarkus.runtime.RuntimeValue;
-import io.quarkus.runtime.shutdown.ShutdownConfig;
 import io.quarkus.vertx.core.deployment.CoreVertxBuildItem;
 import io.quarkus.vertx.core.deployment.EventLoopCountBuildItem;
-import io.quarkus.vertx.http.deployment.devmode.HttpRemoteDevClientProvider;
+import io.quarkus.vertx.core.deployment.InternalWebVertxBuildItem;
 import io.quarkus.vertx.http.runtime.CurrentVertxRequest;
 import io.quarkus.vertx.http.runtime.HttpBuildTimeConfig;
 import io.quarkus.vertx.http.runtime.HttpConfiguration;
 import io.quarkus.vertx.http.runtime.RouterProducer;
 import io.quarkus.vertx.http.runtime.VertxHttpRecorder;
-import io.quarkus.vertx.http.runtime.attribute.ExchangeAttributeBuilder;
 import io.quarkus.vertx.http.runtime.cors.CORSRecorder;
 import io.quarkus.vertx.http.runtime.filters.Filter;
-import io.quarkus.vertx.http.runtime.filters.GracefulShutdownFilter;
 import io.vertx.core.Handler;
 import io.vertx.core.impl.VertxImpl;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
 class VertxHttpProcessor {
-
-    private static final Logger logger = Logger.getLogger(VertxHttpProcessor.class);
 
     @BuildStep
     HttpRootPathBuildItem httpRoot(HttpBuildTimeConfig httpBuildTimeConfig) {
@@ -102,13 +85,14 @@ class VertxHttpProcessor {
     @BuildStep
     @Record(ExecutionTime.RUNTIME_INIT)
     VertxWebRouterBuildItem initializeRouter(VertxHttpRecorder recorder,
-            CoreVertxBuildItem vertx,
-            List<RouteBuildItem> routes,
+            InternalWebVertxBuildItem vertx,
+            List<RouteBuildItem> routes, LaunchModeBuildItem launchModeBuildItem,
             ShutdownContextBuildItem shutdown) {
 
-        RuntimeValue<Router> router = recorder.initializeRouter(vertx.getVertx());
+        RuntimeValue<Router> router = recorder.initializeRouter(vertx.getVertx(), launchModeBuildItem.getLaunchMode(),
+                shutdown);
         for (RouteBuildItem route : routes) {
-            recorder.addRoute(router, route.getRouteFunction(), route.getHandler(), route.getType());
+            recorder.addRoute(router, route.getRouteFunction(), route.getHandler(), route.getType(), route.isResume());
         }
 
         return new VertxWebRouterBuildItem(router);
@@ -123,20 +107,19 @@ class VertxHttpProcessor {
     @BuildStep
     @Record(ExecutionTime.RUNTIME_INIT)
     ServiceStartBuildItem finalizeRouter(
-            VertxHttpRecorder recorder, BeanContainerBuildItem beanContainer, CoreVertxBuildItem vertx,
-            LaunchModeBuildItem launchMode,
+            VertxHttpRecorder recorder, BeanContainerBuildItem beanContainer,
+            Optional<RequireVirtualHttpBuildItem> requireVirtual, InternalWebVertxBuildItem vertx,
+            LaunchModeBuildItem launchMode, ShutdownContextBuildItem shutdown,
             List<DefaultRouteBuildItem> defaultRoutes, List<FilterBuildItem> filters,
-            VertxWebRouterBuildItem router,
+            VertxWebRouterBuildItem router, EventLoopCountBuildItem eventLoopCount,
             HttpBuildTimeConfig httpBuildTimeConfig, HttpConfiguration httpConfiguration,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
+            List<WebsocketSubProtocolsBuildItem> websocketSubProtocols,
             List<RequireBodyHandlerBuildItem> requireBodyHandlerBuildItems,
             BodyHandlerBuildItem bodyHandlerBuildItem,
-            BuildProducer<ShutdownListenerBuildItem> shutdownListenerBuildItemBuildProducer,
-            ShutdownConfig shutdownConfig,
-            LiveReloadConfig lrc,
-            CoreVertxBuildItem core, // Injected to be sure that Vert.x has been produced before calling this method.
-            ExecutorBuildItem executorBuildItem)
+            CoreVertxBuildItem core // Injected to be sure that Vert.x has been produced before calling this method.
+    )
             throws BuildException, IOException {
-        HttpRemoteDevClientProvider.liveReloadConfig = lrc;
         Optional<DefaultRouteBuildItem> defaultRoute;
         if (defaultRoutes == null || defaultRoutes.isEmpty()) {
             defaultRoute = Optional.empty();
@@ -149,9 +132,6 @@ class VertxHttpProcessor {
             }
         }
 
-        GracefulShutdownFilter gracefulShutdownFilter = recorder.createGracefulShutdownHandler();
-        shutdownListenerBuildItemBuildProducer.produce(new ShutdownListenerBuildItem(gracefulShutdownFilter));
-
         List<Filter> listOfFilters = filters.stream()
                 .filter(f -> f.getHandler() != null)
                 .map(FilterBuildItem::toFilter).collect(Collectors.toList());
@@ -162,26 +142,10 @@ class VertxHttpProcessor {
 
         recorder.finalizeRouter(beanContainer.getValue(),
                 defaultRoute.map(DefaultRouteBuildItem::getRoute).orElse(null),
-                listOfFilters, vertx.getVertx(), lrc, router.getRouter(), httpBuildTimeConfig.rootPath,
+                listOfFilters, vertx.getVertx(), router.getRouter(), httpBuildTimeConfig.rootPath,
                 launchMode.getLaunchMode(),
-                !requireBodyHandlerBuildItems.isEmpty(), bodyHandler, httpConfiguration, gracefulShutdownFilter,
-                shutdownConfig, executorBuildItem.getExecutorProxy());
+                !requireBodyHandlerBuildItems.isEmpty(), bodyHandler, httpConfiguration);
 
-        return new ServiceStartBuildItem("vertx-http");
-    }
-
-    @Record(ExecutionTime.RUNTIME_INIT)
-    @BuildStep
-    void openSocket(ApplicationStartBuildItem start,
-            LaunchModeBuildItem launchMode,
-            CoreVertxBuildItem vertx,
-            ShutdownContextBuildItem shutdown,
-            BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
-            HttpBuildTimeConfig httpBuildTimeConfig, HttpConfiguration httpConfiguration,
-            Optional<RequireVirtualHttpBuildItem> requireVirtual,
-            EventLoopCountBuildItem eventLoopCount,
-            List<WebsocketSubProtocolsBuildItem> websocketSubProtocols,
-            VertxHttpRecorder recorder) throws IOException {
         boolean startVirtual = requireVirtual.isPresent() || httpBuildTimeConfig.virtual;
         if (startVirtual) {
             reflectiveClass
@@ -190,50 +154,16 @@ class VertxHttpProcessor {
         // start http socket in dev/test mode even if virtual http is required
         boolean startSocket = !startVirtual || launchMode.getLaunchMode() != LaunchMode.NORMAL;
         recorder.startServer(vertx.getVertx(), shutdown,
-                httpBuildTimeConfig, httpConfiguration, launchMode.getLaunchMode(), startVirtual, startSocket,
+                httpConfiguration, launchMode.getLaunchMode(), startVirtual, startSocket,
                 eventLoopCount.getEventLoopCount(),
                 websocketSubProtocols.stream().map(bi -> bi.getWebsocketSubProtocols())
                         .collect(Collectors.joining(",")));
+
+        return new ServiceStartBuildItem("vertx-http");
     }
 
     @BuildStep
     RuntimeInitializedClassBuildItem configureNativeCompilation() {
         return new RuntimeInitializedClassBuildItem("io.vertx.ext.web.handler.sockjs.impl.XhrTransport");
-    }
-
-    /**
-     * Register the {@link ExchangeAttributeBuilder} services for native image consumption
-     *
-     * @param exchangeAttributeBuilderService
-     * @throws BuildException
-     */
-    @BuildStep
-    void registerExchangeAttributeBuilders(final BuildProducer<ServiceProviderBuildItem> exchangeAttributeBuilderService)
-            throws BuildException {
-        // get hold of the META-INF/services/io.quarkus.vertx.http.runtime.attribute.ExchangeAttributeBuilder
-        // from within the jar containing the ExchangeAttributeBuilder class
-        final CodeSource codeSource = ExchangeAttributeBuilder.class.getProtectionDomain().getCodeSource();
-        if (codeSource == null) {
-            logger.debug("Skipping registration of service providers for " + ExchangeAttributeBuilder.class);
-            return;
-        }
-        try (final FileSystem jarFileSystem = ZipUtils.newFileSystem(codeSource.getLocation().toURI(),
-                Collections.emptyMap())) {
-            final Path serviceDescriptorFilePath = jarFileSystem.getPath("META-INF", "services",
-                    "io.quarkus.vertx.http.runtime.attribute.ExchangeAttributeBuilder");
-            if (!Files.exists(serviceDescriptorFilePath)) {
-                logger.debug("Skipping registration of service providers for " + ExchangeAttributeBuilder.class
-                        + " since no service descriptor file found");
-                return;
-            }
-            // we register all the listed providers since the access log configuration is a runtime construct
-            // and we won't know at build time which attributes the user application will choose
-            final ServiceProviderBuildItem serviceProviderBuildItem;
-            serviceProviderBuildItem = ServiceProviderBuildItem.allProviders(ExchangeAttributeBuilder.class.getName(),
-                    serviceDescriptorFilePath);
-            exchangeAttributeBuilderService.produce(serviceProviderBuildItem);
-        } catch (IOException | URISyntaxException e) {
-            throw new BuildException(e, Collections.emptyList());
-        }
     }
 }
