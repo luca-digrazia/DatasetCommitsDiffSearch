d78cbfb5f09b18a513e339091bc9e643aa2c4702
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:32:26 +0200

    Remove PlaceholderFragment default implementation from MainActivity

--- dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/di/RootModule.java
+++ dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/di/RootModule.java
@@ -1,13 +1,10 @@
 package com.github.pedrovgs.effectiveandroidui.di;
 
-import android.content.Context;
-import android.view.LayoutInflater;
 import com.github.pedrovgs.effectiveandroidui.TvShowsApplication;
 import com.github.pedrovgs.effectiveandroidui.domain.TvShowsModule;
 import com.github.pedrovgs.effectiveandroidui.executor.ExecutorModule;
 import com.github.pedrovgs.effectiveandroidui.ui.activity.MainActivity;
 import dagger.Module;
-import dagger.Provides;
 
 /**
  * Dagger module created to work as junction of every module with an application scope.
@@ -17,24 +14,11 @@ import dagger.Provides;
 
 @Module(
     includes = {
-        ExecutorModule.class, TvShowsModule.class,
-    },
-    injects = {
-        TvShowsApplication.class, MainActivity.class
-    }, library = true)
-public final class RootModule {
+        FrameworkModule.class, ExecutorModule.class, TvShowsModule.class
+    }, injects = {
+    TvShowsApplication.class, MainActivity.class
+}
+)
+public class RootModule {
 
-  private final Context context;
-
-  public RootModule(Context context) {
-    this.context = context;
-  }
-
-  @Provides Context provideApplicationContext() {
-    return context;
-  }
-
-  @Provides LayoutInflater provideLayoutInflater() {
-    return LayoutInflater.from(context);
-  }
 }
--- dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/ui/activity/MainActivity.java
+++ dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/ui/activity/MainActivity.java
@@ -1,62 +1,55 @@
 package com.github.pedrovgs.effectiveandroidui.ui.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.widget.Toast;
 import com.github.pedrovgs.effectiveandroidui.R;
 import com.github.pedrovgs.effectiveandroidui.domain.GetTvShowById;
 import com.github.pedrovgs.effectiveandroidui.domain.GetTvShows;
 import com.github.pedrovgs.effectiveandroidui.domain.tvshow.TvShow;
-import com.github.pedrovgs.effectiveandroidui.ui.fragment.TvShowCatalogFragment;
-import com.github.pedrovgs.effectiveandroidui.ui.fragment.TvShowDraggableFragment;
-import com.github.pedrovgs.effectiveandroidui.ui.fragment.TvShowFragment;
-import com.github.pedrovgs.effectiveandroidui.ui.presenter.TvShowUIModule;
 import java.util.LinkedList;
 import java.util.List;
 import javax.inject.Inject;
 
-public class MainActivity extends BaseActivity implements TvShowCatalogFragment.Listener {
+public class MainActivity extends BaseActivity {
 
   @Inject GetTvShows getTvShows;
   @Inject GetTvShowById getTvShowsById;
-
-  private TvShowDraggableFragment tvShowDraggableFragment;
-  private TvShowFragment tvShowFragment;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    tvShowDraggableFragment =
-        (TvShowDraggableFragment) getSupportFragmentManager().findFragmentById(
-            R.id.f_tv_show_draggable);
-    tvShowFragment = (TvShowFragment) getSupportFragmentManager().findFragmentById(R.id.f_tv_show);
+
+   /* getTvShows.execute(new GetTvShows.Callback() {
+      @Override public void onTvShowsLoaded(Collection<TvShow> tvShows) {
+        Toast.makeText(getBaseContext(), "Conversations loaded " + tvShows.size(),
+            Toast.LENGTH_LONG).show();
+      }
+
+      @Override public void onConnectionError() {
+        Toast.makeText(getBaseContext(), "Fake error", Toast.LENGTH_LONG).show();
+      }
+    });
+
+*/
+    getTvShowsById.execute("The Mentalist", new GetTvShowById.Callback() {
+      @Override public void onTvShowLoaded(final TvShow tvShow) {
+        Toast.makeText(getBaseContext(), "TvShow loaded " + tvShow.getTitle(), Toast.LENGTH_LONG)
+            .show();
+      }
+
+      @Override public void onTvShowNotFound() {
+        Toast.makeText(getBaseContext(), "TvShow not found", Toast.LENGTH_LONG).show();
+      }
+
+      @Override public void onConnectionError() {
+        Toast.makeText(getBaseContext(), "CONNECTION ERRROR 000000", Toast.LENGTH_LONG).show();
+      }
+    });
   }
 
   @Override
   protected List<Object> getModules() {
-    LinkedList<Object> modules = new LinkedList<Object>();
-    modules.add(new TvShowUIModule());
-    return modules;
-  }
-
-  @Override public void onTvShowClicked(final TvShow tvShow) {
-    showTvShowOnTvShowDraggableFragment(tvShow);
-    showTvShowOnTvShowFragment(tvShow);
-  }
-
-  private void showTvShowOnTvShowDraggableFragment(TvShow tvShow) {
-    if (isFragmentAvailable(tvShowDraggableFragment)) {
-      //tvShowDraggableFragment.showTvShow(tvShow.getTitle());
-    }
-  }
-
-  private void showTvShowOnTvShowFragment(TvShow tvShow) {
-    if (isFragmentAvailable(tvShowFragment)) {
-      tvShowFragment.showTvShow(tvShow.getTitle());
-    }
-  }
-
-  private boolean isFragmentAvailable(Fragment fragment) {
-    return fragment != null;
+    return new LinkedList<Object>();
   }
 }
