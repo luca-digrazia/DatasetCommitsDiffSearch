bd2324c47a17ad5c2777b9ece806ac47f0ff4d93
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:36:33 +0200

    remove sha-256 from change password request
        fixes #432

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/UsersController.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/UsersController.java
@@ -20,51 +20,37 @@ package controllers;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.google.inject.Inject;
-import lib.*;
-import lib.security.RestPermissions;
+import lib.BreadcrumbList;
+import lib.DateTools;
+import lib.Tools;
 import models.PermissionsService;
-import models.StreamService;
 import models.User;
 import models.UserService;
 import models.api.requests.ChangePasswordRequest;
 import models.api.requests.ChangeUserRequest;
-import models.api.requests.ChangeUserRequestForm;
-import models.api.requests.CreateUserRequestForm;
-import org.apache.shiro.subject.Subject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import models.api.requests.CreateUserRequest;
 import play.data.Form;
 import play.mvc.Result;
 import views.html.system.users.edit;
 import views.html.system.users.new_user;
 import views.html.system.users.show;
 
-import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-
-import static lib.security.RestPermissions.USERS_LIST;
-import static views.helpers.Permissions.isPermitted;
 
 public class UsersController extends AuthenticatedController {
-    private static final Logger log = LoggerFactory.getLogger(UsersController.class);
 
-    private static final Form<CreateUserRequestForm> createUserForm = Form.form(CreateUserRequestForm.class);
-    private static final Form<ChangeUserRequestForm> changeUserForm = Form.form(ChangeUserRequestForm.class);
+    private static final Form<CreateUserRequest> createUserForm = Form.form(CreateUserRequest.class);
+    private static final Form<ChangeUserRequest> changeUserForm = Form.form(ChangeUserRequest.class);
     private static final Form<ChangePasswordRequest> changePasswordForm = Form.form(ChangePasswordRequest.class);
 
     @Inject
     private UserService userService;
     @Inject
     private PermissionsService permissionsService;
-    @Inject
-    private StreamService streamService;
 
     public Result index() {
-        final List<User> allUsers = isPermitted(USERS_LIST) ? userService.all() : Collections.<User>emptyList();
+        final List<User> allUsers = userService.all();
         final List<String> permissions = permissionsService.all();
         return ok(views.html.system.users.index.render(currentUser(), breadcrumbs(), allUsers, permissions));
     }
@@ -86,21 +72,13 @@ public class UsersController extends AuthenticatedController {
         bc.addCrumb("New", routes.UsersController.newUserForm());
 
         final List<String> permissions = permissionsService.all();
-        try {
-            return ok(new_user.render(
-                    createUserForm,
-                    currentUser(),
-                    permissions,
-                    ImmutableSet.<String>of(),
-                    DateTools.getGroupedTimezoneIds().asMap(),
-                    streamService.all(),
-                    bc));
-        } catch (IOException e) {
-            return status(504, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
-        } catch (APIException e) {
-            String message = "Could not fetch streams. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
-            return status(504, views.html.errors.error.render(message, e, request()));
-        }
+        return ok(new_user.render(
+                createUserForm,
+                currentUser(),
+                permissions,
+                ImmutableSet.<String>of(),
+                DateTools.getGroupedTimezoneIds().asMap(),
+                bc));
     }
 
     public Result editUserForm(String username) {
@@ -108,62 +86,37 @@ public class UsersController extends AuthenticatedController {
         bc.addCrumb("Edit " + username, routes.UsersController.editUserForm(username));
 
         User user = userService.load(username);
-        final Form<ChangeUserRequestForm> form = changeUserForm.fill(new ChangeUserRequestForm(user));
-        boolean requiresOldPassword = checkRequireOldPassword(username);
-        try {
-            return ok(edit.render(
-                    form,
-                    username,
-                    currentUser(),
-                    user,
-                    requiresOldPassword,
-                    permissionsService.all(),
-                    ImmutableSet.copyOf(user.getPermissions()),
-                    DateTools.getGroupedTimezoneIds().asMap(),
-                    streamService.all(),
-                    bc)
-            );
-        } catch (IOException e) {
-            return status(504, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
-        } catch (APIException e) {
-            String message = "Could not fetch streams. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
-            return status(504, views.html.errors.error.render(message, e, request()));
-        }
+        final Form<ChangeUserRequest> form = changeUserForm.fill(new ChangeUserRequest(user));
+        return ok(edit.render(
+                form,
+                username,
+                currentUser(),
+                permissionsService.all(),
+                ImmutableSet.copyOf(user.getPermissions()),
+                DateTools.getGroupedTimezoneIds().asMap(),
+                bc)
+        );
     }
 
     public Result create() {
-        Form<CreateUserRequestForm> createUserRequestForm = Tools.bindMultiValueFormFromRequest(CreateUserRequestForm.class);
-        final CreateUserRequestForm request = createUserRequestForm.get();
+        Form<CreateUserRequest> createUserRequestForm = Tools.bindMultiValueFormFromRequest(CreateUserRequest.class);
+        final CreateUserRequest request = createUserRequestForm.get();
 
         if (createUserRequestForm.hasErrors()) {
             BreadcrumbList bc = breadcrumbs();
             bc.addCrumb("Create new", routes.UsersController.newUserForm());
             final List<String> permissions = permissionsService.all();
-            try {
-                return badRequest(new_user.render(
-                        createUserRequestForm,
-                        currentUser(),
-                        permissions,
-                        ImmutableSet.copyOf(request.permissions),
-                        DateTools.getGroupedTimezoneIds().asMap(),
-                        streamService.all(),
-                        bc));
-            } catch (IOException e) {
-                return status(504, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
-            } catch (APIException e) {
-                String message = "Could not fetch streams. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
-                return status(504, views.html.errors.error.render(message, e, request()));
-            }
+            return badRequest(new_user.render(
+                    createUserRequestForm,
+                    currentUser(),
+                    permissions,
+                    ImmutableSet.copyOf(request.permissions),
+                    DateTools.getGroupedTimezoneIds().asMap(),
+                    bc));
         }
-        if (request.admin) {
-            request.permissions = Lists.newArrayList("*");
-        } else {
-            request.permissions = permissionsService.readerPermissions(request.username);
-        }
-
-        if (!userService.create(request.toApiRequest())) {
-            flash("error", "Could not create user due to an internal error.");
-        }
+        // TODO PREVIEW: remove hardcoded permissions once the permission editor is ready
+        request.permissions = Lists.newArrayList("*");
+        userService.create(request);
         return redirect(routes.UsersController.index());
     }
 
@@ -184,9 +137,7 @@ public class UsersController extends AuthenticatedController {
     }
 
     public Result saveChanges(String username) {
-        final Form<ChangeUserRequestForm> requestForm = Form.form(ChangeUserRequestForm.class).bindFromRequest();
-        final User user = userService.load(username);
-
+        final Form<ChangeUserRequest> requestForm = Tools.bindMultiValueFormFromRequest(ChangeUserRequest.class);
         if (requestForm.hasErrors()) {
             final BreadcrumbList bc = new BreadcrumbList();
             bc.addCrumb("System", routes.SystemController.index(0));
@@ -194,54 +145,20 @@ public class UsersController extends AuthenticatedController {
             bc.addCrumb("Edit " + username, routes.UsersController.editUserForm(username));
 
             final List<String> all = permissionsService.all();
-            boolean requiresOldPassword = checkRequireOldPassword(username);
 
-            try {
-                return badRequest(edit.render(
-                        requestForm,
-                        username,
-                        currentUser(),
-                        user,
-                        requiresOldPassword,
-                        all,
-                        ImmutableSet.copyOf(requestForm.get().permissions),
-                        DateTools.getGroupedTimezoneIds().asMap(),
-                        streamService.all(),
-                        bc));
-            } catch (IOException e) {
-                return status(504, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
-            } catch (APIException e) {
-                String message = "Could not fetch streams. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
-                return status(504, views.html.errors.error.render(message, e, request()));
-            }
+            return badRequest(edit.render(
+                    requestForm,
+                    username,
+                    currentUser(),
+                    all,
+                    ImmutableSet.copyOf(requestForm.get().permissions),
+                    DateTools.getGroupedTimezoneIds().asMap(),
+                    bc));
         }
-
-        final ChangeUserRequestForm formData = requestForm.get();
-        Set<String> permissions = Sets.newHashSet(user.getPermissions());
-        // TODO this does not handle combined permissions like streams:edit,read:1,2 !
-        for (String streampermission : formData.streampermissions) {
-            permissions.add(RestPermissions.STREAMS_READ + ":" + streampermission);
-        }
-        for (String streameditpermission : formData.streameditpermissions) {
-            permissions.add(RestPermissions.STREAMS_EDIT + ":" + streameditpermission);
-        }
-        final ChangeUserRequest changeRequest = formData.toApiRequest();
-        changeRequest.permissions = Lists.newArrayList(permissions);
-        user.update(changeRequest);
+        final User user = userService.load(username);
+        user.update(requestForm.get());
 
         return redirect(routes.UsersController.index());
-    }
-
-    private boolean checkRequireOldPassword(String username) {
-        boolean requiresOldPassword = true;
-        final User currentUser = currentUser();
-        final Subject subject = currentUser.getSubject();
-        final String currentUserName = currentUser.getName();
-        if (subject.isPermitted("users:passwordchange:*")) {
-            // if own account, require old password, otherwise don't require it
-            requiresOldPassword = currentUserName.equals(username);
-        }
-        return requiresOldPassword;
     }
 
     public Result changePassword(String username) {
@@ -250,9 +167,6 @@ public class UsersController extends AuthenticatedController {
         final ChangePasswordRequest request = requestForm.get();
         final User user = userService.load(username);
 
-        if (checkRequireOldPassword(username) && request.old_password == null) {
-            requestForm.reject("Old password is required.");
-        }
         if (requestForm.hasErrors() || !user.updatePassword(request)) {
             flash("error", "Could not update the password.");
             return redirect(routes.UsersController.editUserForm(username));
