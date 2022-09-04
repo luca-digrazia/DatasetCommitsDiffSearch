8daac42a604fa72d68f0f39da7d41f6d07374b05
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:12:55 +0200

    Demonstrate use of Timed and Metered annotations
        If `@Timed` and `@Metered` are being used on the same resource method,
        at least one of them has to be given a unique name.
    
        Refs #2414

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/resources/HelloWorldResource.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/resources/HelloWorldResource.java
@@ -1,17 +1,22 @@
 package com.example.helloworld.resources;
 
+import com.codahale.metrics.annotation.Metered;
 import com.codahale.metrics.annotation.Timed;
 import com.example.helloworld.api.Saying;
 import com.example.helloworld.core.Template;
-import com.google.common.base.Optional;
 import io.dropwizard.jersey.caching.CacheControl;
 import io.dropwizard.jersey.params.DateTimeParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -29,7 +34,8 @@ public class HelloWorldResource {
     }
 
     @GET
-    @Timed(name = "get-requests")
+    @Timed(name = "get-requests-timed")
+    @Metered(name = "get-requests-metered")
     @CacheControl(maxAge = 1, maxAgeUnit = TimeUnit.DAYS)
     public Saying sayHello(@QueryParam("name") Optional<String> name) {
         return new Saying(counter.incrementAndGet(), template.render(name));
@@ -39,10 +45,18 @@ public class HelloWorldResource {
     public void receiveHello(@Valid Saying saying) {
         LOGGER.info("Received a saying: {}", saying);
     }
-    
+
     @GET
     @Path("/date")
-    public void receiveDate(@QueryParam("date") DateTimeParam dateTimeParam) {
-        LOGGER.info("Received a saying: {}", dateTimeParam.get());
+    @Produces(MediaType.TEXT_PLAIN)
+    public String receiveDate(@QueryParam("date") Optional<DateTimeParam> dateTimeParam) {
+        if (dateTimeParam.isPresent()) {
+            final DateTimeParam actualDateTimeParam = dateTimeParam.get();
+            LOGGER.info("Received a date: {}", actualDateTimeParam);
+            return actualDateTimeParam.get().toString();
+        } else {
+            LOGGER.warn("No received date");
+            return null;
+        }
     }
 }
