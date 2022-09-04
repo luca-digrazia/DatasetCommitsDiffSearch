73b6e7bddb8e8e46d42c9a4d60b83521346fe05e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:00:01 +0200

    Fix reflective access warning in Qute
        - resolves #13716

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/ReflectionValueResolver.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/ReflectionValueResolver.java
@@ -7,24 +7,21 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-/**
- * This value resolver can be used to access public members of classes via reflection.
- */
 public class ReflectionValueResolver implements ValueResolver {
 
     /**
      * Lazy loading cache of lookup attempts (contains both hits and misses)
      */
-    private final ConcurrentMap<MemberKey, Optional<AccessorCandidate>> candidates = new ConcurrentHashMap<>();
+    private final ConcurrentMap<MemberKey, Optional<MemberWrapper>> memberCache = new ConcurrentHashMap<>();
 
-    private static final AccessorCandidate ARRAY_GET_LENGTH = ec -> instance -> CompletableFuture
-            .completedFuture(Array.getLength(instance));
+    private static final MemberWrapper ARRAY_GET_LENGTH = Array::getLength;
 
     public static final String GET_PREFIX = "get";
     public static final String IS_PREFIX = "is";
@@ -41,59 +38,67 @@ public class ReflectionValueResolver implements ValueResolver {
         if (base == null) {
             return false;
         }
-        // Check if there is a member with the given name and number of params
-        return candidates.computeIfAbsent(MemberKey.from(base, context.getName(), context.getParams().size()),
-                this::findCandidate).isPresent();
+        return memberCache.computeIfAbsent(MemberKey.newInstance(base, context.getName()), ReflectionValueResolver::findWrapper)
+                .isPresent();
     }
 
     @Override
     public CompletionStage<Object> resolve(EvalContext context) {
         Object base = context.getBase();
-        MemberKey key = MemberKey.from(base, context.getName(), context.getParams().size());
-        // At this point the candidate for the given key should be already computed
-        AccessorCandidate candidate = candidates.get(key).orElse(null);
-        if (candidate == null) {
-            return Results.notFound(context);
+        MemberKey key = MemberKey.newInstance(base, context.getName());
+        MemberWrapper wrapper = memberCache.computeIfAbsent(key, ReflectionValueResolver::findWrapper).orElse(null);
+
+        if (wrapper == null) {
+            return Results.NOT_FOUND;
         }
-        ValueAccessor accessor = candidate.getAccessor(context);
-        if (accessor == null) {
-            return Results.notFound(context);
+
+        try {
+            return CompletableFuture.completedFuture(wrapper.getValue(base));
+        } catch (Exception e) {
+            throw new IllegalStateException("Reflection invocation error", e);
         }
-        return accessor.getValue(base);
     }
 
-    public void clearCache() {
-        candidates.clear();
+    public void clearMemberCache() {
+        memberCache.clear();
     }
 
-    private Optional<AccessorCandidate> findCandidate(MemberKey key) {
-        if (key.clazz.isArray()) {
-            if (key.name.equals("length") && key.numberOfParams == 0) {
+    private static Optional<MemberWrapper> findWrapper(MemberKey key) {
+
+        if (key.getClazz().isArray()) {
+            if (key.getName().equals("length")) {
                 return Optional.of(ARRAY_GET_LENGTH);
             } else {
                 return Optional.empty();
             }
         }
-        if (key.numberOfParams > 0) {
-            List<Method> methods = findMethods(key.clazz, key.name, key.numberOfParams);
-            return methods.isEmpty() ? Optional.empty() : Optional.of(new MethodsCandidate(methods));
-        } else {
-            Method foundMethod = findMethodNoArgs(key.clazz, key.name);
-            if (foundMethod != null) {
-                foundMethod.trySetAccessible();
-                return Optional.of(new GetterAccessor(foundMethod));
+
+        Method foundMethod = findMethod(key.getClazz(), key.getName());
+
+        if (foundMethod != null) {
+            if (!foundMethod.isAccessible()) {
+                foundMethod.setAccessible(true);
             }
-            Field foundField = findField(key.clazz, key.name);
-            if (foundField != null) {
-                foundField.trySetAccessible();
-                return Optional.of(new FieldAccessor(foundField));
+            return Optional.of(new MethodWrapper(foundMethod));
+        }
+
+        // Find public field
+        Field foundField = findField(key.getClazz(), key.getName());
+
+        if (foundField != null) {
+            if (!foundField.isAccessible()) {
+                foundField.setAccessible(true);
             }
+            return Optional.of(new FieldWrapper(foundField));
         }
         // Member not found
         return Optional.empty();
     }
 
-    private Method findMethodNoArgs(Class<?> clazz, String name) {
+    private static Method findMethod(Class<?> clazz, String name) {
+        Objects.requireNonNull(clazz);
+        Objects.requireNonNull(name);
+
         Method foundMatch = null;
         Method foundGetterMatch = null;
         Method foundBooleanMatch = null;
@@ -136,8 +141,20 @@ public class ReflectionValueResolver implements ValueResolver {
         return foundMatch;
     }
 
-    private Field findField(Class<?> clazz, String name) {
+    /**
+     * Tries to find a public field with the given name on the given class.
+     *
+     * @param clazz
+     * @param name
+     * @return the found field or <code>null</code>
+     */
+    private static Field findField(Class<?> clazz, String name) {
+
+        Objects.requireNonNull(clazz);
+        Objects.requireNonNull(name);
+
         Field found = null;
+
         for (Field field : clazz.getFields()) {
             if (field.getName().equals(name)) {
                 found = field;
@@ -146,38 +163,9 @@ public class ReflectionValueResolver implements ValueResolver {
         return found;
     }
 
-    private static List<Method> findMethods(Class<?> clazz, String name, int numberOfParams) {
-        List<Method> foundMatch = new ArrayList<>();
-
-        List<Class<?>> hierarchy = new ArrayList<>();
-        Collections.addAll(hierarchy, clazz.getInterfaces());
-        Class<?> superClass = clazz.getSuperclass();
-        while (superClass != null) {
-            Collections.addAll(hierarchy, superClass.getInterfaces());
-            superClass = superClass.getSuperclass();
-        }
-        hierarchy.add(clazz);
-
-        for (Class<?> clazzToTest : hierarchy) {
-            for (Method method : clazzToTest.getMethods()) {
-                if (!Modifier.isPublic(method.getModifiers())
-                        || (!method.isVarArgs() && method.getParameterCount() != numberOfParams)
-                        || method.getReturnType().equals(Void.TYPE)
-                        || Object.class.equals(method.getDeclaringClass())
-                        || method.isBridge()
-                        || !name.equals(method.getName())) {
-                    continue;
-                }
-                foundMatch.add(method);
-                method.trySetAccessible();
-            }
-        }
-        return foundMatch.size() == 1 ? Collections.singletonList(foundMatch.get(0)) : foundMatch;
-    }
-
     private static boolean isMethodValid(Method method) {
         return method != null && Modifier.isPublic(method.getModifiers())
-                && method.getParameterCount() == 0
+                && method.getParameterTypes().length == 0
                 && !method.getReturnType().equals(Void.TYPE)
                 && !Object.class.equals(method.getDeclaringClass());
     }
