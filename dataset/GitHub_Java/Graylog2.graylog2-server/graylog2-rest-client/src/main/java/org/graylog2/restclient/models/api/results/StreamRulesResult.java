442f3892db72e3b79df6aa66e1588d99b6db3dd2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:58:42 +0200

    Actually returning stream rules in result.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/api/results/StreamRulesResult.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/api/results/StreamRulesResult.java
@@ -1,8 +1,21 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.restclient.models.api.results;
 
-import com.google.common.collect.Lists;
-import com.google.inject.Inject;
-import org.graylog2.restclient.models.StreamRule;
 import org.graylog2.restclient.models.api.responses.streams.StreamRuleSummaryResponse;
 
 import java.util.List;
@@ -24,13 +37,7 @@ public class StreamRulesResult {
         return total;
     }
 
-    public List<StreamRule> getStreamRules() {
-        List<StreamRule> streamRules = Lists.newArrayList();
-
-        /*for (StreamRuleSummaryResponse srsr : this.streamRules) {
-            streamRules.add(streamRuleFactory.fromSummaryResponse(srsr));
-        }*/
-
+    public List<StreamRuleSummaryResponse> getStreamRules() {
         return streamRules;
     }
 }
