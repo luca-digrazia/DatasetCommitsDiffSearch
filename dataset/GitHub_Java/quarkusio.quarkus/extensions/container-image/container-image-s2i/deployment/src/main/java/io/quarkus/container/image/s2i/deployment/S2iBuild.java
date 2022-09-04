a99dc3960c0fc0a4b46c9e13b756da13ad2a12d5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:35:14 +0200

    remove call to oc version

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/container-image/container-image-s2i/deployment/src/main/java/io/quarkus/container/image/s2i/deployment/S2iBuild.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/container-image/container-image-s2i/deployment/src/main/java/io/quarkus/container/image/s2i/deployment/S2iBuild.java
@@ -14,6 +14,6 @@ public class S2iBuild implements BooleanSupplier {
 
     @Override
     public boolean getAsBoolean() {
-        return containerImageConfig.builder.map(b -> b.equals(S2iProcessor.S2I)).orElse(true);
+        return true;
     }
 }
