675737ab104a26c5742c27c20de426eff2ba97d9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:36:29 +0200

    handle missing class in confusion matrix

--- dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/validation/ConfusionMatrix.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/validation/ConfusionMatrix.java
@@ -1,20 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2010-2019 Haifeng Li
+ * Copyright (c) 2010 Haifeng Li
+ *   
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Smile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * Smile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
-
 package smile.validation;
 
 import java.util.HashSet;
@@ -25,55 +23,52 @@ import java.util.Set;
  * @author owlmsj
  */
 public class ConfusionMatrix {
+	
+	private int[][] matrix;
+	
+	public ConfusionMatrix(int[] truth, int[] prediction) {
+		if (truth.length != prediction.length) {
+			 throw new IllegalArgumentException(String.format("The vector sizes don't match: %d != %d.", truth.length, prediction.length));
+		}
+		
+		Set<Integer> y = new HashSet<>();
 
-    /** Confusion matrix. */
-    private int[][] matrix;
+		// Sometimes, small test data doesn't have all the classes.
+		for (int i = 0; i < truth.length; i++) {
+			y.add(truth[i]);
+			y.add(prediction[i]);
+		}
 
-    /** Constructor. */
-    public ConfusionMatrix(int[] truth, int[] prediction) {
-        if (truth.length != prediction.length) {
-             throw new IllegalArgumentException(String.format("The vector sizes don't match: %d != %d.", truth.length, prediction.length));
-        }
-
-        Set<Integer> y = new HashSet<>();
-
-        // Sometimes, small test data doesn't have all the classes.
-        for (int i = 0; i < truth.length; i++) {
-            y.add(truth[i]);
-            y.add(prediction[i]);
-        }
-
-        int k = 0;
-        for (int c : y) {
-            if (k < c) k = c;
-        }
-        matrix = new int[k+1][k+1];
-
-        for (int i = 0; i < truth.length; i++) {
-            matrix[truth[i]][prediction[i]] += 1;
-        }
-    }
-
-    /** Returns the confusion matrix. */
+		int k = 0;
+		for (int c : y) {
+			if (k < c) k = c;
+		}
+		matrix = new int[k+1][k+1];
+		
+		for (int i = 0; i < truth.length; i++) {
+			matrix[truth[i]][prediction[i]] += 1;
+		}
+	}
+	
     public int[][] getMatrix() {
         return matrix;
     }
     
-    @Override
-    public String toString() {
-
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("ROW=truth and COL=predicted\n");
-
-        for(int i = 0; i < matrix.length; i++){
-            sb.append(String.format("class %2d |", i));
-            for(int j = 0; j < matrix.length; j++){
-                sb.append(String.format("%8d |", matrix[i][j]));
-            }
-            sb.append("\n");
-        }
-
-        return sb.toString().trim();
-    }
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("ROW=truth and COL=predicted\n");
+		
+		for(int i = 0; i < matrix.length; i++){
+			sb.append(String.format("class %2d |", i));
+			for(int j = 0; j < matrix.length; j++){
+				sb.append(String.format("%8d |", matrix[i][j]));
+			}
+			sb.append("\n");
+ 		}
+		
+		return sb.toString().trim();
+	}
 }
