9ff558809a98dd2adc3d186fc6900aa74eae4a32
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 00:16:09 +0200

    Add sanity check that the runfiles middleman root is a middleman root
        This would have prevented a large breakage.
    
        PiperOrigin-RevId: 169364731

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/MiddlemanFactory.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/MiddlemanFactory.java
@@ -13,16 +13,16 @@
 // limitations under the License.
 package com.google.devtools.build.lib.actions;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.actions.ActionAnalysisMetadata.MiddlemanType;
 import com.google.devtools.build.lib.cmdline.Label;
-import com.google.devtools.build.lib.collect.CollectionUtils;
-import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
 import com.google.devtools.build.lib.util.Pair;
+import com.google.devtools.build.lib.util.Preconditions;
 import com.google.devtools.build.lib.vfs.PathFragment;
+
 import java.util.Iterator;
+
 import javax.annotation.Nullable;
 
 /**
@@ -98,9 +98,6 @@ public final class MiddlemanFactory {
   }
 
   private <T> boolean hasExactlyOneInput(Iterable<T> iterable) {
-    if (iterable instanceof NestedSet) {
-      return ((NestedSet) iterable).isSingleton();
-    }
     Iterator<T> it = iterable.iterator();
     if (!it.hasNext()) {
       return false;
@@ -155,7 +152,7 @@ public final class MiddlemanFactory {
   private Pair<Artifact, Action> createMiddleman(
       ActionOwner owner, String middlemanName, String purpose, Iterable<Artifact> inputs,
       Root middlemanDir, MiddlemanType middlemanType) {
-    if (inputs == null || CollectionUtils.isEmpty(inputs)) {
+    if (inputs == null || Iterables.isEmpty(inputs)) {
       return null;
     }
 
