6d816fb26b0e1b1257fe6138a26bcd6567dbcd19
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:53:38 +0200

    Enable CPU usage profiling by default.
        This is implicitly the default for a while now.
        We also plan to deprecate this option.
    
        PiperOrigin-RevId: 345401352

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/CommonCommandOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/CommonCommandOptions.java
@@ -216,16 +216,14 @@ public class CommonCommandOptions extends OptionsBase {
   public String oomMessage;
 
   @Option(
-      name = "generate_json_trace_profile",
-      oldName = "experimental_generate_json_trace_profile",
-      defaultValue = "auto",
+      name = "experimental_generate_json_trace_profile",
+      defaultValue = "false",
       documentationCategory = OptionDocumentationCategory.LOGGING,
       effectTags = {OptionEffectTag.AFFECTS_OUTPUTS, OptionEffectTag.BAZEL_MONITORING},
       help =
           "If enabled, Bazel profiles the build and writes a JSON-format profile into a file in"
-              + " the output base. View profile by loading into chrome://tracing. By default Bazel"
-              + " writes the profile for all build-like commands and query.")
-  public TriState enableTracer;
+              + " the output base. View profile by loading into chrome://tracing.")
+  public boolean enableTracer;
 
   @Option(
       name = "json_trace_compression",
