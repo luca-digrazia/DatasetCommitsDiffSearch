f2f54c1820388dd05241e12e2567be1175bd11b0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:53:42 +0200

    use error percentage in test for random number generator

--- dataset/GitHub_Java/haifengl.smile/math/src/test/java/smile/stat/distribution/LogNormalDistributionTest.java
+++ dataset/GitHub_Java/haifengl.smile/math/src/test/java/smile/stat/distribution/LogNormalDistributionTest.java
@@ -1,18 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2010-2019 Haifeng Li
+ * Copyright (c) 2010 Haifeng Li
+ *   
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Smile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * Smile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
 
 package smile.stat.distribution;
@@ -22,8 +21,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import smile.math.MathEx;
-
 import static org.junit.Assert.*;
 
 /**
@@ -57,23 +54,24 @@ public class LogNormalDistributionTest {
     @Test
     public void testLogNormalDistribution() {
         System.out.println("LogNormalDistribution");
-        MathEx.setSeed(19650218); // to get repeatable results.
         LogNormalDistribution instance = new LogNormalDistribution(3, 2.1);
-        double[] data = instance.rand(1000);
-        LogNormalDistribution est = LogNormalDistribution.fit(data);
-        assertEquals(3.04, est.mu, 1E-2);
-        assertEquals(2.12, est.sigma, 1E-2);
+        double[] data = new double[1000];
+        for (int i = 0; i < data.length; i++)
+            data[i] = instance.rand();
+        LogNormalDistribution est = new LogNormalDistribution(data);
+        assertEquals(0.0, (est.getMu() - 3.0) / 3.0, 0.1);
+        assertEquals(0.0, (est.getSigma() - 2.1) / 2.1, 0.1);
     }
 
     /**
-     * Test of length method, of class LogNormalDistribution.
+     * Test of npara method, of class LogNormalDistribution.
      */
     @Test
-    public void testLength() {
-        System.out.println("length");
+    public void testNpara() {
+        System.out.println("npara");
         LogNormalDistribution instance = new LogNormalDistribution(1.0, 1.0);
         instance.rand();
-        assertEquals(2, instance.length());
+        assertEquals(2, instance.npara());
     }
 
     /**
@@ -88,14 +86,14 @@ public class LogNormalDistributionTest {
     }
 
     /**
-     * Test of variance method, of class LogNormalDistribution.
+     * Test of var method, of class LogNormalDistribution.
      */
     @Test
-    public void testVariance() {
-        System.out.println("variance");
+    public void testVar() {
+        System.out.println("var");
         LogNormalDistribution instance = new LogNormalDistribution(1.0, 1.0);
         instance.rand();
-        assertEquals(34.51261, instance.variance(), 1E-5);
+        assertEquals(34.51261, instance.var(), 1E-5);
     }
 
     /**
