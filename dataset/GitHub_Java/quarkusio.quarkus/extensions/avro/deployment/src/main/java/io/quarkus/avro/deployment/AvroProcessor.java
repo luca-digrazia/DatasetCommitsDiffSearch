737f4e649545893f0df3768ff0ebf8f9db5673f8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:46:06 +0200

    Register for reflection some log4j classes used by Elasticsearch
        Fixes #13575

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/avro/deployment/src/main/java/io/quarkus/avro/deployment/AvroProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/avro/deployment/src/main/java/io/quarkus/avro/deployment/AvroProcessor.java
@@ -2,20 +2,31 @@ package io.quarkus.avro.deployment;
 
 import java.util.Collection;
 
-import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.specific.AvroGenerated;
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.DotName;
 
+import io.quarkus.avro.runtime.AvroRecorder;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageConfigBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageSystemPropertyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 public class AvroProcessor {
+
+    @BuildStep
+    @Record(ExecutionTime.STATIC_INIT)
+    void clearCaches(AvroRecorder recorder, LaunchModeBuildItem launchModeBuildItem) {
+        if (launchModeBuildItem.getLaunchMode().isDevOrTest()) {
+            recorder.clearStaticCaches();
+        }
+    }
 
     @BuildStep
     public void build(CombinedIndexBuildItem indexBuildItem,
@@ -24,14 +35,12 @@ public class AvroProcessor {
             BuildProducer<NativeImageConfigBuildItem> conf) {
 
         NativeImageConfigBuildItem.Builder builder = NativeImageConfigBuildItem.builder();
-        builder.addRuntimeInitializedClass(GenericDatumReader.class.getName());
 
         Collection<AnnotationInstance> annotations = indexBuildItem.getIndex()
                 .getAnnotations(DotName.createSimple(AvroGenerated.class.getName()));
         for (AnnotationInstance annotation : annotations) {
             if (annotation.target().kind() == AnnotationTarget.Kind.CLASS) {
                 String className = annotation.target().asClass().name().toString();
-                builder.addRuntimeInitializedClass(className);
                 reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, true, className));
             }
         }
