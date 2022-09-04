5dd534b4568f4b9a7aeb9668933c44a59243221c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:15:16 +0200

    Make logback work when using shamrock:run
        This should be temporary but it is annoying enough to fix now

--- dataset/GitHub_Java/quarkusio.quarkus/maven/src/main/java/org/jboss/shamrock/maven/runner/RunMojoMain.java
+++ dataset/GitHub_Java/quarkusio.quarkus/maven/src/main/java/org/jboss/shamrock/maven/runner/RunMojoMain.java
@@ -2,98 +2,98 @@ package org.jboss.shamrock.maven.runner;
 
 import java.io.Closeable;
 import java.io.File;
-import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.CountDownLatch;
 
-import org.jboss.logging.Logger;
 import org.jboss.shamrock.deployment.ArchiveContextBuilder;
 import org.jboss.shamrock.runtime.Timing;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
 
 /**
  * The main entry point for the run mojo execution
  */
 public class RunMojoMain {
 
-    private static final Logger log = Logger.getLogger(RunMojoMain.class);
-
+    private static CountDownLatch awaitChangeLatch = null;
+    private static CountDownLatch awaitRestartLatch = null;
     private static volatile boolean keepCl = false;
     private static volatile ClassLoader currentAppClassLoader;
-    private static volatile URLClassLoader runtimeCl;
-    private static File classesRoot;
-    private static File wiringDir;
-
-    private static Closeable closeable;
-    static volatile Throwable deploymentProblem;
 
     public static void main(String... args) throws Exception {
         Timing.staticInitStarted();
         //the path that contains the compiled classes
-        classesRoot = new File(args[0]);
-        wiringDir = new File(args[1]);
-        //TODO: we can't handle an exception on startup with hot replacement, as Undertow might not have started
-
-        doStart();
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (RunMojoMain.class) {
-                    if (closeable != null) {
-                        try {
-                            closeable.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }, "Shamrock Shutdown Thread"));
-    }
-
-    private static synchronized void doStart() {
-        try {
+        File classesRoot = new File(args[0]);
+        File wiringDir = new File(args[1]);
+        URLClassLoader runtimeCl = null;
+        do {
             if (runtimeCl == null || !keepCl) {
                 runtimeCl = new URLClassLoader(new URL[]{classesRoot.toURL()}, ClassLoader.getSystemClassLoader());
             }
+            URL resource = runtimeCl.getResource("logback.xml"); //TODO: this is temporary, until David's logging thing comes it, but not having it is super annoying for demos
+            if(resource != null) {
+                LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+                loggerContext.reset();
+                JoranConfigurator configurator = new JoranConfigurator();
+                try (InputStream configStream = resource.openStream()) {
+                    configurator.setContext(loggerContext);
+                    configurator.doConfigure(configStream); // loads logback file
+                }
+            }
+
             currentAppClassLoader = runtimeCl;
             ClassLoader old = Thread.currentThread().getContextClassLoader();
             //we can potentially throw away this class loader, and reload the app
+            synchronized (RunMojoMain.class) {
+                awaitChangeLatch = new CountDownLatch(1);
+            }
             try {
                 Thread.currentThread().setContextClassLoader(runtimeCl);
                 Class<?> runnerClass = runtimeCl.loadClass("org.jboss.shamrock.runner.RuntimeRunner");
                 ArchiveContextBuilder acb = new ArchiveContextBuilder();
-                Constructor ctor = runnerClass.getDeclaredConstructor(ClassLoader.class, Path.class, Path.class, ArchiveContextBuilder.class);
-                Object runner = ctor.newInstance(runtimeCl, classesRoot.toPath(), wiringDir.toPath(), acb);
+                Constructor ctor = runnerClass.getDeclaredConstructor( ClassLoader.class, Path.class, Path.class, ArchiveContextBuilder.class);
+                Object runner = ctor.newInstance( runtimeCl, classesRoot.toPath(), wiringDir.toPath(), acb);
                 ((Runnable) runner).run();
-                closeable = ((Closeable) runner);
-                deploymentProblem = null;
+                synchronized (RunMojoMain.class) {
+                    if (awaitRestartLatch != null) {
+                        awaitRestartLatch.countDown();
+                        awaitRestartLatch = null;
+                    }
+                }
+                awaitChangeLatch.await();
+                ((Closeable) runner).close();
             } finally {
                 Thread.currentThread().setContextClassLoader(old);
             }
-        } catch (Throwable t) {
-            deploymentProblem = t;
-            log.error("Failed to start shamrock", t);
-        }
+
+        } while (true);
     }
 
-    public static synchronized void restartApp(boolean keepClassloader) {
+    public static void restartApp(boolean keepClassloader) {
         keepCl = keepClassloader;
-        if (closeable != null) {
-
-            ClassLoader old = Thread.currentThread().getContextClassLoader();
-            Thread.currentThread().setContextClassLoader(runtimeCl);
+        Timing.restart();
+        CountDownLatch restart = null;
+        synchronized (RunMojoMain.class) {
+            if (awaitChangeLatch != null) {
+                restart = awaitRestartLatch = new CountDownLatch(1);
+                awaitChangeLatch.countDown();
+            }
+            awaitChangeLatch = null;
+        }
+        if (restart != null) {
             try {
-                closeable.close();
-            } catch (IOException e) {
+                restart.await();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
-            } finally {
-                Thread.currentThread().setContextClassLoader(old);
             }
         }
-        closeable = null;
-        doStart();
     }
 
     public static ClassLoader getCurrentAppClassLoader() {
