47fda2a72fd5af086c8fa9f080b08b0599714f38
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:04:10 +0200

    Refactor CacheResultInterceptor exceptions handling and tests

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/cache/deployment/src/test/java/io/quarkus/cache/test/runtime/ThrowExecutionExceptionCauseTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/cache/deployment/src/test/java/io/quarkus/cache/test/runtime/ThrowExecutionExceptionCauseTest.java
@@ -28,17 +28,29 @@ public class ThrowExecutionExceptionCauseTest {
     CachedService cachedService;
 
     @Test
-    public void testRuntimeExceptionThrow() {
+    public void testRuntimeExceptionThrowDuringCacheComputation() {
         NumberFormatException e = assertThrows(NumberFormatException.class, () -> {
-            cachedService.runtimeExceptionCachedMethod();
+            cachedService.throwRuntimeExceptionDuringCacheComputation();
+        });
+        assertEquals(FORCED_EXCEPTION_MESSAGE, e.getMessage());
+        // Let's check we didn't put an uncompleted future in the cache because of the previous exception.
+        assertThrows(NumberFormatException.class, () -> {
+            cachedService.throwRuntimeExceptionDuringCacheComputation();
+        });
+    }
+
+    @Test
+    public void testCheckedExceptionThrowDuringCacheComputation() {
+        IOException e = assertThrows(IOException.class, () -> {
+            cachedService.throwCheckedExceptionDuringCacheComputation();
         });
         assertEquals(FORCED_EXCEPTION_MESSAGE, e.getMessage());
     }
 
     @Test
-    public void testCheckedExceptionThrow() {
-        IOException e = assertThrows(IOException.class, () -> {
-            cachedService.checkedExceptionCachedMethod();
+    public void testErrorThrowDuringCacheComputation() {
+        OutOfMemoryError e = assertThrows(OutOfMemoryError.class, () -> {
+            cachedService.throwErrorDuringCacheComputation();
         });
         assertEquals(FORCED_EXCEPTION_MESSAGE, e.getMessage());
     }
@@ -46,16 +58,19 @@ public class ThrowExecutionExceptionCauseTest {
     @ApplicationScoped
     static class CachedService {
 
-        private static final String CACHE_NAME = "test-cache";
-
-        @CacheResult(cacheName = CACHE_NAME)
-        public String runtimeExceptionCachedMethod() {
+        @CacheResult(cacheName = "runtime-exception-cache")
+        public String throwRuntimeExceptionDuringCacheComputation() {
             throw new NumberFormatException(FORCED_EXCEPTION_MESSAGE);
         }
 
-        @CacheResult(cacheName = CACHE_NAME)
-        public String checkedExceptionCachedMethod() throws Exception {
+        @CacheResult(cacheName = "checked-exception-cache")
+        public String throwCheckedExceptionDuringCacheComputation() throws IOException {
             throw new IOException(FORCED_EXCEPTION_MESSAGE);
+        }
+
+        @CacheResult(cacheName = "error-cache")
+        public String throwErrorDuringCacheComputation() {
+            throw new OutOfMemoryError(FORCED_EXCEPTION_MESSAGE);
         }
     }
 }
