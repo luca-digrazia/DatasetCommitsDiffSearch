8cb664a117e1a38ef859fdbcbd859656a5512104
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:47:58 +0200

    issue#7632 Apply sun.nio.ch.Iocp substitutions only against Windows (which is where those classes are present)

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-core/runtime/src/main/java/io/quarkus/vertx/core/runtime/graal/JdkSubstitutions.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-core/runtime/src/main/java/io/quarkus/vertx/core/runtime/graal/JdkSubstitutions.java
@@ -1,46 +1,52 @@
 package io.quarkus.vertx.core.runtime.graal;
 
-import java.io.FileDescriptor;
 import java.io.IOException;
 import java.net.URL;
-import java.util.function.BooleanSupplier;
+import java.nio.channels.spi.AsynchronousChannelProvider;
+import java.util.function.Function;
+
+import org.graalvm.nativeimage.Platform;
+import org.graalvm.nativeimage.Platforms;
 
 import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.InjectAccessors;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
+import com.oracle.svm.core.jdk.JDK11OrLater;
+import com.oracle.svm.core.jdk.JDK8OrEarlier;
 
-@TargetClass(className = "sun.misc.URLClassPath$Loader")
-final class Target_sun_misc_URLClassPath$Loader {
+@TargetClass(className = "URLClassPath$Loader", classNameProvider = Package_jdk_internal_loader.class)
+final class Target_URLClassPath$Loader {
 
     @Alias
-    public Target_sun_misc_URLClassPath$Loader(URL url) {
+    public Target_URLClassPath$Loader(URL url) {
     }
 }
 
-@TargetClass(className = "sun.misc.URLClassPath$FileLoader")
-final class Target_sun_misc_URLClassPath$FileLoader {
+@TargetClass(className = "URLClassPath$FileLoader", classNameProvider = Package_jdk_internal_loader.class)
+final class Target_URLClassPath$FileLoader {
 
     @Alias
-    public Target_sun_misc_URLClassPath$FileLoader(URL url) throws IOException {
+    public Target_URLClassPath$FileLoader(URL url) throws IOException {
     }
 }
 
-@TargetClass(className = "sun.misc.URLClassPath")
+@TargetClass(className = "sun.misc.URLClassPath", onlyWith = JDK8OrEarlier.class)
 final class Target_sun_misc_URLClassPath {
 
     @Substitute
-    private Target_sun_misc_URLClassPath$Loader getLoader(final URL url) throws IOException {
+    private Target_URLClassPath$Loader getLoader(final URL url) throws IOException {
         String file = url.getFile();
         if (file != null && file.endsWith("/")) {
             if ("file".equals(url.getProtocol())) {
-                return (Target_sun_misc_URLClassPath$Loader) (Object) new Target_sun_misc_URLClassPath$FileLoader(
+                return (Target_URLClassPath$Loader) (Object) new Target_URLClassPath$FileLoader(
                         url);
             } else {
-                return new Target_sun_misc_URLClassPath$Loader(url);
+                return new Target_URLClassPath$Loader(url);
             }
         } else {
             // that must be wrong, but JarLoader is deleted by SVM
-            return (Target_sun_misc_URLClassPath$Loader) (Object) new Target_sun_misc_URLClassPath$FileLoader(
+            return (Target_URLClassPath$Loader) (Object) new Target_URLClassPath$FileLoader(
                     url);
         }
     }
@@ -51,20 +57,83 @@ final class Target_sun_misc_URLClassPath {
     }
 }
 
-@TargetClass(className = "sun.nio.ch.DatagramChannelImpl", onlyWith = GraalVersion19_0.class)
-final class Target_sun_nio_ch_DatagramChannelImpl {
+@TargetClass(className = "jdk.internal.loader.URLClassPath", onlyWith = JDK11OrLater.class)
+final class Target_jdk_internal_loader_URLClassPath {
 
     @Substitute
-    private static void disconnect0(FileDescriptor fd, boolean isIPv6)
-            throws IOException {
-        throw new RuntimeException("Unimplemented: sun.nio.ch.DatagramChannelImpl.disconnect0(FileDescriptor, boolean)");
+    private Target_URLClassPath$Loader getLoader(final URL url) throws IOException {
+        String file = url.getFile();
+        if (file != null && file.endsWith("/")) {
+            if ("file".equals(url.getProtocol())) {
+                return (Target_URLClassPath$Loader) (Object) new Target_URLClassPath$FileLoader(
+                        url);
+            } else {
+                return new Target_URLClassPath$Loader(url);
+            }
+        } else {
+            // that must be wrong, but JarLoader is deleted by SVM
+            return (Target_URLClassPath$Loader) (Object) new Target_URLClassPath$FileLoader(
+                    url);
+        }
+    }
+
+}
+
+final class Package_jdk_internal_loader implements Function<TargetClass, String> {
+
+    private static final JDK8OrEarlier JDK_8_OR_EARLIER = new JDK8OrEarlier();
+
+    @Override
+    public String apply(TargetClass annotation) {
+        if (JDK_8_OR_EARLIER.getAsBoolean()) {
+            return "sun.misc." + annotation.className();
+        }
+
+        return "jdk.internal.loader." + annotation.className();
     }
 }
 
-final class GraalVersion19_0 implements BooleanSupplier {
-    public boolean getAsBoolean() {
-        final String version = System.getProperty("org.graalvm.version");
-        return version.startsWith("19.0.");
+@Substitute
+@TargetClass(className = "sun.nio.ch.WindowsAsynchronousFileChannelImpl", innerClass = "DefaultIocpHolder", onlyWith = JDK11OrLater.class)
+@Platforms({ Platform.WINDOWS.class })
+final class Target_sun_nio_ch_WindowsAsynchronousFileChannelImpl_DefaultIocpHolder {
+
+    @Alias
+    @InjectAccessors(DefaultIocpAccessor.class)
+    static Target_sun_nio_ch_Iocp defaultIocp;
+}
+
+@TargetClass(className = "sun.nio.ch.Iocp", onlyWith = JDK11OrLater.class)
+@Platforms({ Platform.WINDOWS.class })
+final class Target_sun_nio_ch_Iocp {
+
+    @Alias
+    Target_sun_nio_ch_Iocp(AsynchronousChannelProvider provider, Target_sun_nio_ch_ThreadPool pool) throws IOException {
+    }
+
+    @Alias
+    Target_sun_nio_ch_Iocp start() {
+        return null;
+    }
+}
+
+@TargetClass(className = "sun.nio.ch.ThreadPool", onlyWith = JDK11OrLater.class)
+@Platforms({ Platform.WINDOWS.class })
+final class Target_sun_nio_ch_ThreadPool {
+
+    @Alias
+    static Target_sun_nio_ch_ThreadPool createDefault() {
+        return null;
+    }
+}
+
+final class DefaultIocpAccessor {
+    static Target_sun_nio_ch_Iocp get() {
+        try {
+            return new Target_sun_nio_ch_Iocp(null, Target_sun_nio_ch_ThreadPool.createDefault()).start();
+        } catch (IOException ioe) {
+            throw new InternalError(ioe);
+        }
     }
 }
 
