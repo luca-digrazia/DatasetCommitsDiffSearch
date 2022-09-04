989c434fc9a610758110cf9797250ed21bdc0890
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:11:01 +0200

    Interceptors - always skip bridge methods
        - annotations may be copied to the generated bridge methods (javac does
        copy them since jdk8b94) and so an invocation of a bridge method may
        result in multiple interceptor invocations

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/amazon-lambda/src/test/java/io/quarkus/it/amazon/lambda/AmazonLambdaSimpleTestCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/amazon-lambda/src/test/java/io/quarkus/it/amazon/lambda/AmazonLambdaSimpleTestCase.java
@@ -1,54 +1,59 @@
-/*
- * Copyright 2019 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.quarkus.it.amazon.lambda;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.amazon.lambda.test.LambdaClient;
-import io.quarkus.amazon.lambda.test.LambdaException;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class AmazonLambdaSimpleTestCase {
 
     @Test
-    public void testSimpleLambdaSucess() throws Exception {
+    public void testSimpleLambdaSuccess() throws Exception {
+        resetCounter();
         InputObject in = new InputObject();
         in.setGreeting("Hello");
         in.setName("Stu");
         OutputObject out = LambdaClient.invoke(OutputObject.class, in);
-        Assertions.assertEquals("Hello Stu", out.getResult());
-
+        assertEquals("Hello Stu", out.getResult());
+        assertCounter(1);
+        assertTrue(out.getRequestId().matches("aws-request-\\d"), "Expected requestId as 'aws-request-<number>'");
     }
 
     @Test
     public void testSimpleLambdaFailure() throws Exception {
+        resetCounter();
         InputObject in = new InputObject();
         in.setGreeting("Hello");
         in.setName("Stuart");
         try {
             OutputObject out = LambdaClient.invoke(OutputObject.class, in);
             out.getResult();
-            Assertions.fail();
-        } catch (LambdaException e) {
-            Assertions.assertEquals(ProcessingService.CAN_ONLY_GREET_NICKNAMES, e.getMessage());
-            Assertions.assertEquals(IllegalArgumentException.class.getName(), e.getType());
+            fail();
+        } catch (Exception e) {
+            assertEquals(ProcessingService.CAN_ONLY_GREET_NICKNAMES, e.getMessage());
+            //Assertions.assertEquals(IllegalArgumentException.class.getName(), e.getType());
         }
+        assertCounter(1);
+    }
 
+    private void resetCounter() {
+        if (!isNativeImageTest()) {
+            CounterInterceptor.COUNTER.set(0);
+        }
+    }
+
+    private void assertCounter(int expected) {
+        if (!isNativeImageTest()) {
+            assertEquals(expected, CounterInterceptor.COUNTER.get());
+        }
+    }
+
+    private boolean isNativeImageTest() {
+        return System.getProperty("native.image.path") != null;
     }
 }
