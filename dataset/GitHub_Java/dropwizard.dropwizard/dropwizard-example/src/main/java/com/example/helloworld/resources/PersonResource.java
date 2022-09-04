0c6e68f3ec747e715a4fb1379b6da4ea25c9f05b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:00:58 +0200

    Merged the two Path annotations into one.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/resources/PersonResource.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/resources/PersonResource.java
@@ -1,8 +1,7 @@
 package com.example.helloworld.resources;
 
 import com.example.helloworld.core.Person;
-import com.example.helloworld.db.PersonDAO;
-import com.yammer.dropwizard.hibernate.Transactional;
+import com.example.helloworld.db.PeopleDAO;
 import com.yammer.dropwizard.jersey.params.LongParam;
 
 import javax.ws.rs.GET;
@@ -11,18 +10,17 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/people/{personId}")
+@Path("/person/{personId}")
 @Produces(MediaType.APPLICATION_JSON)
 public class PersonResource {
 
-    private final PersonDAO peopleDAO;
+    private final PeopleDAO peopleDAO;
 
-    public PersonResource(PersonDAO peopleDAO) {
+    public PersonResource(PeopleDAO peopleDAO) {
         this.peopleDAO = peopleDAO;
     }
 
     @GET
-    @Transactional
     public Person getPerson(@PathParam("personId") LongParam personId) {
         return peopleDAO.findById(personId.get());
     }
