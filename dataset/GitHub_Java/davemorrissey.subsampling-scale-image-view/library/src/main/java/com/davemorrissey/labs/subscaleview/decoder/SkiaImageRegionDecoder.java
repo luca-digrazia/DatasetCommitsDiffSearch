a015af439a02cfb1b8d5728b515398d622faa822
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:25:36 +0200

    Restored enforced synchronization on BitmapRegionDecoder for SDK < 21, which did not have internal synchronization

--- dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/library/src/main/java/com/davemorrissey/labs/subscaleview/decoder/SkiaImageRegionDecoder.java
+++ dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/library/src/main/java/com/davemorrissey/labs/subscaleview/decoder/SkiaImageRegionDecoder.java
@@ -10,8 +10,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.text.TextUtils;
 
-import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
-
 import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
@@ -45,13 +43,10 @@ public class SkiaImageRegionDecoder implements ImageRegionDecoder {
     }
 
     public SkiaImageRegionDecoder(Bitmap.Config bitmapConfig) {
-        Bitmap.Config globalBitmapConfig = SubsamplingScaleImageView.getPreferredBitmapConfig();
-        if (bitmapConfig != null) {
-            this.bitmapConfig = bitmapConfig;
-        } else if (globalBitmapConfig != null) {
-            this.bitmapConfig = globalBitmapConfig;
-        } else {
+        if (bitmapConfig == null) {
             this.bitmapConfig = Bitmap.Config.RGB_565;
+        } else {
+            this.bitmapConfig = bitmapConfig;
         }
     }
 
