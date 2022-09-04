37cff31974b9975f7842add16e3541c48f8fb860
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:30:57 +0200

    Demo of restoring scale/center on rotate

--- dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/src/com/davemorrissey/labs/subscaleview/DemoActivity.java
+++ dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/src/com/davemorrissey/labs/subscaleview/DemoActivity.java
@@ -11,8 +11,7 @@ import java.io.IOException;
 public class DemoActivity extends Activity {
 
     private static final String STATE_SCALE = "state-scale";
-    private static final String STATE_CENTER_X = "state-center-x";
-    private static final String STATE_CENTER_Y = "state-center-y";
+    private static final String STATE_CENTER = "state-center";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,11 +21,8 @@ public class DemoActivity extends Activity {
             SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(id.imageView);
             imageView.setImageAsset("DSC00266.JPG");
 
-            if (savedInstanceState != null &&
-                    savedInstanceState.containsKey(STATE_SCALE) &&
-                    savedInstanceState.containsKey(STATE_CENTER_X) &&
-                    savedInstanceState.containsKey(STATE_CENTER_Y)) {
-                imageView.setScaleAndCenter(savedInstanceState.getFloat(STATE_SCALE), new PointF(savedInstanceState.getFloat(STATE_CENTER_X), savedInstanceState.getFloat(STATE_CENTER_Y)));
+            if (savedInstanceState != null && savedInstanceState.containsKey(STATE_SCALE) && savedInstanceState.containsKey(STATE_CENTER)) {
+                imageView.setScaleAndCenter(savedInstanceState.getFloat(STATE_SCALE), (PointF)savedInstanceState.getParcelable(STATE_CENTER));
             }
         } catch (IOException e) {
             Log.e(DemoActivity.class.getSimpleName(), "Could not load asset", e);
@@ -39,10 +35,6 @@ public class DemoActivity extends Activity {
 
         SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(id.imageView);
         outState.putFloat(STATE_SCALE, imageView.getScale());
-        PointF center = imageView.getCenter();
-        if (center != null) {
-            outState.putFloat(STATE_CENTER_X, center.x);
-            outState.putFloat(STATE_CENTER_Y, center.y);
-        }
+        outState.putParcelable(STATE_CENTER, imageView.getCenter());
     }
 }
