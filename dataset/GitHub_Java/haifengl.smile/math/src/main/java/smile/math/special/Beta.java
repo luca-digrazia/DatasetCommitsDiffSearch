dbcb22a6ca7705b3df4d799a49b3ea4acf772b52
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:28:39 +0200

    allow tiny slack in regularized incomplete beta function

--- dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/math/special/Beta.java
+++ dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/math/special/Beta.java
@@ -17,14 +17,6 @@
 
 package smile.math.special;
 
-import static java.lang.Math.abs;
-import static java.lang.Math.exp;
-import static java.lang.Math.log;
-import static java.lang.Math.min;
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
-import static smile.math.special.Gamma.lgamma;
-
 /**
  * The beta function, also called the Euler integral of the first kind.
  * <p>
@@ -52,7 +44,7 @@ public class Beta {
      * The beta function is symmetric, i.e. B(x,y)==B(y,x).
      */
     public static double beta(double x, double y) {
-        return exp(lgamma(x) + lgamma(y) - lgamma(x + y));
+        return Math.exp(Gamma.lgamma(x) + Gamma.lgamma(y) - Gamma.lgamma(x + y));
     }
 
     /**
@@ -65,11 +57,11 @@ public class Beta {
         // greater than 1 or less than 0. We allow tiny slack here to avoid brute exception.
         final double EPS = 1E-8;
 
-        if (abs(x) < EPS) {
+        if (Math.abs(x) < EPS) {
             return 0.0;
         }
 
-        if (abs(x - 1.0) < EPS) {
+        if (Math.abs(x - 1.0) < EPS) {
             return 1.0;
         }
 
@@ -85,7 +77,11 @@ public class Beta {
                 ibeta = 1.0;
             } else {
                 // Term before continued fraction
-                ibeta = exp(lgamma(alpha + beta) - lgamma(alpha) - lgamma(beta) + alpha * log(x) + beta * log(1.0D - x));
+                ibeta = Math.exp(Gamma.lgamma(alpha + beta)
+                        - Gamma.lgamma(alpha)
+                        - Gamma.lgamma(beta)
+                        + alpha * Math.log(x)
+                        + beta * Math.log(1.0D - x));
                 // Continued fraction
                 if (x < (alpha + 1.0) / (alpha + beta + 2.0)) {
                     ibeta = ibeta * incompleteFractionSummation(alpha, beta, x) / alpha;
@@ -111,7 +107,7 @@ public class Beta {
         double aminus1 = alpha - 1.0;
         double c = 1.0;
         double d = 1.0 - aplusb * x / aplus1;
-        if (abs(d) < FPMIN) {
+        if (Math.abs(d) < FPMIN) {
             d = FPMIN;
         }
         d = 1.0 / d;
@@ -124,29 +120,29 @@ public class Beta {
             i2 = 2 * i;
             aa = i * (beta - i) * x / ((aminus1 + i2) * (alpha + i2));
             d = 1.0 + aa * d;
-            if (abs(d) < FPMIN) {
+            if (Math.abs(d) < FPMIN) {
                 d = FPMIN;
             }
             c = 1.0 + aa / c;
-            if (abs(c) < FPMIN) {
+            if (Math.abs(c) < FPMIN) {
                 c = FPMIN;
             }
             d = 1.0 / d;
             h *= d * c;
             aa = -(alpha + i) * (aplusb + i) * x / ((alpha + i2) * (aplus1 + i2));
             d = 1.0 + aa * d;
-            if (abs(d) < FPMIN) {
+            if (Math.abs(d) < FPMIN) {
                 d = FPMIN;
             }
             c = 1.0 + aa / c;
-            if (abs(c) < FPMIN) {
+            if (Math.abs(c) < FPMIN) {
                 c = FPMIN;
             }
             d = 1.0 / d;
             del = d * c;
             h *= del;
             i++;
-            if (abs(del - 1.0) < EPS) {
+            if (Math.abs(del - 1.0) < EPS) {
                 test = false;
             }
             if (i > MAXITER) {
@@ -177,43 +173,43 @@ public class Beta {
 
         if (alpha >= 1. && beta >= 1.) {
             pp = (p < 0.5) ? p : 1. - p;
-            t = sqrt(-2. * log(pp));
+            t = Math.sqrt(-2. * Math.log(pp));
             x = (2.30753 + t * 0.27061) / (1. + t * (0.99229 + t * 0.04481)) - t;
             if (p < 0.5) {
                 x = -x;
             }
             al = (x * x - 3.) / 6.;
             h = 2. / (1. / (2. * alpha - 1.) + 1. / (2. * beta - 1.));
-            w = (x * sqrt(al + h) / h) - (1. / (2. * beta - 1) - 1. / (2. * alpha - 1.)) * (al + 5. / 6. - 2. / (3. * h));
-            x = alpha / (alpha + beta * exp(2. * w));
+            w = (x * Math.sqrt(al + h) / h) - (1. / (2. * beta - 1) - 1. / (2. * alpha - 1.)) * (al + 5. / 6. - 2. / (3. * h));
+            x = alpha / (alpha + beta * Math.exp(2. * w));
         } else {
-            double lna = log(alpha / (alpha + beta));
-            double lnb = log(beta / (alpha + beta));
-            t = exp(alpha * lna) / alpha;
-            u = exp(beta * lnb) / beta;
+            double lna = Math.log(alpha / (alpha + beta));
+            double lnb = Math.log(beta / (alpha + beta));
+            t = Math.exp(alpha * lna) / alpha;
+            u = Math.exp(beta * lnb) / beta;
             w = t + u;
             if (p < t / w) {
-                x = pow(alpha * w * p, 1. / alpha);
+                x = Math.pow(alpha * w * p, 1. / alpha);
             } else {
-                x = 1. - pow(beta * w * (1. - p), 1. / beta);
+                x = 1. - Math.pow(beta * w * (1. - p), 1. / beta);
             }
         }
-        afac = -lgamma(alpha) - lgamma(beta) + lgamma(alpha + beta);
+        afac = -Gamma.lgamma(alpha) - Gamma.lgamma(beta) + Gamma.lgamma(alpha + beta);
         for (int j = 0; j < 10; j++) {
             if (x == 0. || x == 1.) {
                 return x;
             }
             err = regularizedIncompleteBetaFunction(alpha, beta, x) - p;
-            t = exp(a1 * log(x) + b1 * log(1. - x) + afac);
+            t = Math.exp(a1 * Math.log(x) + b1 * Math.log(1. - x) + afac);
             u = err / t;
-            x -= (t = u / (1. - 0.5 * min(1., u * (a1 / x - b1 / (1. - x)))));
+            x -= (t = u / (1. - 0.5 * Math.min(1., u * (a1 / x - b1 / (1. - x)))));
             if (x <= 0.) {
                 x = 0.5 * (x + t);
             }
             if (x >= 1.) {
                 x = 0.5 * (x + t + 1.);
             }
-            if (abs(t) < EPS * x && j > 0) {
+            if (Math.abs(t) < EPS * x && j > 0) {
                 break;
             }
         }
