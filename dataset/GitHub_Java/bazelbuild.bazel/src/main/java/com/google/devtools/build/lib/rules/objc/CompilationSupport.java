3aac0c9405792d94c3bee8c9d1e9b9f94300c07a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:14:39 +0200

    Delete no_enable_modules feature
        This was used to support --experimental_objc_enable_module_maps which
        has been deleted.
    
        PiperOrigin-RevId: 369322002

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
@@ -998,6 +998,7 @@ public class CompilationSupport {
     ObjcVariablesExtension.Builder extension =
         new ObjcVariablesExtension.Builder()
             .setRuleContext(ruleContext)
+            .setCompilationArtifacts(compilationArtifacts)
             .setIntermediateArtifacts(intermediateArtifacts)
             .setConfiguration(buildConfiguration);
 
