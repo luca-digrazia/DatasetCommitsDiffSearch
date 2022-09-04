71f858dca78cc333fab812c1da4479057b8acd79
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:34:24 +0200

    Add missing short-circuit to stream router engine
        Fixes #1396

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/streams/StreamRouterEngine.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/streams/StreamRouterEngine.java
@@ -319,7 +319,7 @@ public class StreamRouterEngine {
         public boolean isMatched() {
             switch (matchingType) {
                 case OR:
-                    return matches.values().contains(true);
+                    return matches.values().contains(false);
                 case AND:
                 default:
                     return matches.size() > 0 && !matches.values().contains(false);
