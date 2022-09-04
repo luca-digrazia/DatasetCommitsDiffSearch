329c2b10a57b9333ec75cd6960be250036aef09f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:11:03 +0200

    Document the difference between Spawn#getExecutionInfo and Spawn#getCombinedExecProperties
        PiperOrigin-RevId: 302082823

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/Spawn.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/Spawn.java
@@ -16,11 +16,10 @@ package com.google.devtools.build.lib.actions;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.devtools.build.lib.actions.extra.SpawnInfo;
-import com.google.devtools.build.lib.vfs.Path;
-import com.google.devtools.build.lib.vfs.PathFragment;
-
+import com.google.devtools.build.lib.analysis.platform.PlatformInfo;
+import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import java.util.Collection;
+import javax.annotation.Nullable;
 
 /**
  * An object representing a subprocess to be invoked, including its command and
@@ -29,48 +28,31 @@ import java.util.Collection;
  * of files it is expected to read and write.
  */
 public interface Spawn {
-
   /**
-   * Returns true iff this command may be executed remotely.
-   */
-  boolean isRemotable();
-
-  /**
-   * Out-of-band data for this spawn. This can be used to signal hints (hardware requirements,
-   * local vs. remote) to the execution subsystem.
+   * Out-of-band data for this spawn. This can be used to signal hints (hardware requirements, local
+   * vs. remote) to the execution subsystem. This data can come from multiple places e.g. tags, hard
+   * coded into rule logic, etc.
    *
-   * <p>String tags from {@link
-   * com.google.devtools.build.lib.rules.test.TestTargetProperties#getExecutionInfo()} can be added
-   * as keys with arbitrary values to this map too.
+   * <p>The data in this field can be in one of two forms and it is up to the caller of this method
+   * to extract the data it cares about. Forms:
+   *
+   * <ul>
+   *   <li>true key-value pairs
+   *   <li>string tags from {@link *
+   *       com.google.devtools.build.lib.analysis.test.TestTargetProperties#getExecutionInfo()}
+   *       which can be added to the map as keys with arbitrary values (canonically the empty
+   *       string)
+   * </ul>
+   *
+   * <p>Callers of this method may also be interested in the {@link #getCombinedExecProperties()}.
+   * See its javadoc for a comparison.
    */
   ImmutableMap<String, String> getExecutionInfo();
-
-  /**
-   * Returns this Spawn as a Bourne shell command.
-   *
-   * @param workingDir the initial working directory of the command
-   */
-  String asShellCommand(Path workingDir);
-
-  /**
-   * Returns the runfiles data for remote execution. Format is (directory, manifest file).
-   */
-  ImmutableMap<PathFragment, Artifact> getRunfilesManifests();
 
   /**
    * Returns the {@link RunfilesSupplier} helper encapsulating the runfiles for this spawn.
    */
   RunfilesSupplier getRunfilesSupplier();
-
-  /**
-   * Returns artifacts for filesets, so they can be scheduled on remote execution.
-   */
-  ImmutableList<Artifact> getFilesetManifests();
-
-  /**
-   * Returns a protocol buffer describing this spawn for use by the extra_action functionality.
-   */
-  SpawnInfo getExtraActionInfo();
 
   /**
    * Returns the command (the first element) and its arguments.
@@ -84,31 +66,37 @@ public interface Spawn {
   ImmutableMap<String, String> getEnvironment();
 
   /**
+   * Map of the execpath at which we expect the Fileset symlink trees, to a list of
+   * FilesetOutputSymlinks which contains the details of the Symlink trees.
+   */
+  ImmutableMap<Artifact, ImmutableList<FilesetOutputSymlink>> getFilesetMappings();
+
+  /**
    * Returns the list of files that are required to execute this spawn (e.g. the compiler binary),
    * in contrast to files necessary for the tool to do its work (e.g. source code to be compiled).
    *
    * <p>The returned set of files is a subset of what getInputFiles() returns.
    *
-   * <p>This method explicitly does not expand middleman artifacts. Pass the result
-   * to an appropriate utility method on {@link com.google.devtools.build.lib.actions.Artifact} to
-   * expand the middlemen.
+   * <p>This method explicitly does not expand middleman artifacts. Pass the result to an
+   * appropriate utility method on {@link com.google.devtools.build.lib.actions.Artifact} to expand
+   * the middlemen.
    *
-   * <p>This is for use with persistent workers, so we can restart workers when their binaries
-   * have changed.
+   * <p>This is for use with persistent workers, so we can restart workers when their binaries have
+   * changed.
    */
-  Iterable<? extends ActionInput> getToolFiles();
+  NestedSet<? extends ActionInput> getToolFiles();
 
   /**
    * Returns the list of files that this command may read.
    *
-   * <p>This method explicitly does not expand middleman artifacts. Pass the result
-   * to an appropriate utility method on {@link com.google.devtools.build.lib.actions.Artifact} to
-   * expand the middlemen.
+   * <p>This method explicitly does not expand middleman artifacts. Pass the result to an
+   * appropriate utility method on {@link com.google.devtools.build.lib.actions.Artifact} to expand
+   * the middlemen.
    *
-   * <p>This is for use with remote execution, so we can ship inputs before starting the
-   * command. Order stability across multiple calls should be upheld for performance reasons.
+   * <p>This is for use with remote execution, so we can ship inputs before starting the command.
+   * Order stability across multiple calls should be upheld for performance reasons.
    */
-  Iterable<? extends ActionInput> getInputFiles();
+  NestedSet<? extends ActionInput> getInputFiles();
 
   /**
    * Returns the collection of files that this command must write.  Callers should not mutate
@@ -121,12 +109,6 @@ public interface Spawn {
   Collection<? extends ActionInput> getOutputFiles();
 
   /**
-   * Instructs the spawn strategy to try to fetch these optional output files in addition to the
-   * usual output artifacts. The PathFragments should be relative to the exec root.
-   */
-  Collection<PathFragment> getOptionalOutputFiles();
-
-  /**
    * Returns the resource owner for local fallback.
    */
   ActionExecutionMetadata getResourceOwner();
@@ -137,12 +119,22 @@ public interface Spawn {
   ResourceSet getLocalResources();
 
   /**
-   * Returns the owner for this action. Production code should supply a non-null owner.
-   */
-  ActionOwner getOwner();
-
-  /**
    * Returns a mnemonic (string constant) for this kind of spawn.
    */
   String getMnemonic();
+
+  /**
+   * Returns execution properties related to this spawn.
+   *
+   * <p>Note that this includes data from the execution platform's exec_properties as well as
+   * target-level exec_properties.
+   *
+   * <p>Callers might also be interested in {@link #getExecutionInfo()} above. {@link
+   * #getExecutionInfo()} can be set by multiple sources while this data is set via the {@code
+   * exec_properties} attribute on targets and platforms.
+   */
+  ImmutableMap<String, String> getCombinedExecProperties();
+
+  @Nullable
+  PlatformInfo getExecutionPlatform();
 }
