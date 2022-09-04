6570a92b8609217e907ba208ad49d1e3d3d5dfc8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:23:06 +0200

    Add missing @Path annotation to jobId argument

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/jobs/RemoteSystemJobResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/jobs/RemoteSystemJobResource.java
@@ -1,11 +1,28 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.rest.resources.system.jobs;
 
 import org.graylog2.rest.models.system.SystemJobSummary;
 import org.graylog2.rest.models.system.jobs.requests.TriggerRequest;
-import retrofit.Call;
-import retrofit.http.Body;
-import retrofit.http.GET;
-import retrofit.http.POST;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 import java.util.List;
 import java.util.Map;
@@ -15,7 +32,7 @@ public interface RemoteSystemJobResource {
     Call<Map<String, List<SystemJobSummary>>> list();
 
     @GET("/system/jobs/{jobId}")
-    Call<SystemJobSummary> get(String jobId);
+    Call<SystemJobSummary> get(@Path("jobId") String jobId);
 
     @POST("/system/jobs")
     Call trigger(@Body TriggerRequest tr);
