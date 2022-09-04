de1ab4a27cd6488ccf120c84c7da1d5bff53e326
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:00:47 +0200

    Cleanup AlertScannerThread

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/periodical/AlertScannerThread.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/periodical/AlertScannerThread.java
@@ -1,88 +1,125 @@
 /**
- * Copyright 2012 Lennart Koopmann <lennart@socketfeed.com>
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.periodical;
 
-import org.graylog2.Core;
-import org.graylog2.alerts.Alert;
-import org.graylog2.alerts.AlertCondition;
+import org.graylog2.Configuration;
+import org.graylog2.alarmcallbacks.AlarmCallbackConfigurationService;
+import org.graylog2.alarmcallbacks.AlarmCallbackFactory;
+import org.graylog2.alarmcallbacks.AlarmCallbackHistoryService;
+import org.graylog2.alarmcallbacks.EmailAlarmCallback;
+import org.graylog2.alerts.AlertScanner;
+import org.graylog2.alerts.AlertService;
+import org.graylog2.initializers.IndexerSetupService;
+import org.graylog2.plugin.periodical.Periodical;
 import org.graylog2.plugin.streams.Stream;
-import org.graylog2.streams.StreamImpl;
+import org.graylog2.streams.StreamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import java.util.List;
 
-/**
- * @author Lennart Koopmann <lennart@socketfeed.com>
- */
-public class AlertScannerThread implements Runnable {
-
+public class AlertScannerThread extends Periodical {
     private static final Logger LOG = LoggerFactory.getLogger(AlertScannerThread.class);
-    
-    public static final int INITIAL_DELAY = 10;
-    public static final int PERIOD = 60;
-    
-    private final Core server;
-    
-    public AlertScannerThread(Core server) {
-        this.server = server;
-    }
-    
-    @Override
-    public void run() {
-        LOG.debug("Running alert checks.");
 
-        List<Stream> alertedStreams = StreamImpl.loadAllWithConfiguredAlertConditions(server);
+    private final StreamService streamService;
+    private final IndexerSetupService indexerSetupService;
+    private final Configuration configuration;
+    private final AlertScanner alertScanner;
+
+    @Inject
+    public AlertScannerThread(final AlertService alertService,
+                              final StreamService streamService,
+                              final AlarmCallbackConfigurationService alarmCallbackConfigurationService,
+                              final AlarmCallbackFactory alarmCallbackFactory,
+                              final EmailAlarmCallback emailAlarmCallback,
+                              final IndexerSetupService indexerSetupService,
+                              final Configuration configuration,
+                              final AlarmCallbackHistoryService alarmCallbackHistoryService,
+                              final AlertScanner alertScanner) {
+        this.streamService = streamService;
+        this.indexerSetupService = indexerSetupService;
+        this.configuration = configuration;
+        this.alertScanner = alertScanner;
+    }
+
+    @Override
+    public void doRun() {
+        if (!indexerSetupService.isRunning()) {
+            LOG.error("Indexer is not running, not checking streams for alerts.");
+            return;
+        }
+
+        LOG.debug("Running alert checks.");
+        final List<Stream> alertedStreams = streamService.loadAllWithConfiguredAlertConditions();
 
         LOG.debug("There are {} streams with configured alert conditions.", alertedStreams.size());
 
         // Load all streams that have configured alert conditions.
-        for (Stream streamIF : alertedStreams) {
-            StreamImpl stream = (StreamImpl) streamIF;
+        for (Stream stream : alertedStreams) {
+            LOG.debug("Stream [{}] has [{}] configured alert conditions.", stream, streamService.getAlertConditions(stream).size());
 
-            LOG.debug("Stream [{}] has [{}] configured alert conditions.", stream, stream.getAlertConditions().size());
-
-            // Check if a threshold is reached.
-            for (AlertCondition alertCondition : stream.getAlertConditions()) {
-                try {
-                    AlertCondition.CheckResult result = alertCondition.triggered();
-                    if (result.isTriggered()) {
-                        // Alert is triggered!
-                        LOG.info("Alert condition [{}] of stream [{}] is triggered. Sending alerts.", alertCondition, stream);
-
-                        // Persist alert.
-                        Alert alert = Alert.factory(result, server);
-                        alert.save();
-
-                        // Send alerts.
-                    } else {
-                        // Alert not triggered.
-                        LOG.debug("Alert condition [{}] of stream [{}] is not triggered.", alertCondition, stream);
-                    }
-                } catch(Exception e) {
-                    LOG.error("Skipping alert check that threw an exception.", e);
-                    continue;
-                }
+            if(stream.isPaused()) {
+                LOG.debug("Stream [{}] has been paused. Skipping alert check.", stream);
+                continue;
             }
 
+            // Check if a threshold is reached.
+            streamService.getAlertConditions(stream).forEach(alertCondition -> alertScanner.checkAlertCondition(stream, alertCondition));
         }
     }
 
+    @Override
+    protected Logger getLogger() {
+        return LOG;
+    }
+
+    @Override
+    public boolean runsForever() {
+        return false;
+    }
+
+    @Override
+    public boolean isDaemon() {
+        return true;
+    }
+
+    @Override
+    public boolean stopOnGracefulShutdown() {
+        return true;
+    }
+
+    @Override
+    public boolean masterOnly() {
+        return true;
+    }
+
+    @Override
+    public boolean startOnThisNode() {
+        return true;
+    }
+
+    @Override
+    public int getInitialDelaySeconds() {
+        return 10;
+    }
+
+    @Override
+    public int getPeriodSeconds() {
+        return configuration.getAlertCheckInterval();
+    }
 }
