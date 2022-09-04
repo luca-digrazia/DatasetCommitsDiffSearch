5d040e3da03317d205e448b384d4e02d5c6e7a30
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:00:53 +0200

    Make sure Jackson ignores the view getters.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-views/src/main/java/com/yammer/dropwizard/views/View.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-views/src/main/java/com/yammer/dropwizard/views/View.java
@@ -1,13 +1,25 @@
 package com.yammer.dropwizard.views;
 
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Timer;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 public abstract class View {
     private final String templateName;
+    private final Timer renderingTimer;
 
     protected View(String templateName) {
         this.templateName = templateName;
+        this.renderingTimer = Metrics.newTimer(getClass(), "rendering");
     }
 
+    @JsonIgnore
     public String getTemplateName() {
         return templateName;
+    }
+
+    @JsonIgnore
+    Timer getRenderingTimer() {
+        return renderingTimer;
     }
 }
