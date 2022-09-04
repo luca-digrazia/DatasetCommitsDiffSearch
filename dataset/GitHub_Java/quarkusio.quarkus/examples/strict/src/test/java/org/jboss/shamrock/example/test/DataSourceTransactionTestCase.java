a30272302b10abc0ca26a06352b9d471339394e2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:08:31 +0200

    Validate the response code and exception

--- dataset/GitHub_Java/quarkusio.quarkus/examples/strict/src/test/java/org/jboss/shamrock/example/test/DataSourceTransactionTestCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/examples/strict/src/test/java/org/jboss/shamrock/example/test/DataSourceTransactionTestCase.java
@@ -12,16 +12,10 @@ public class DataSourceTransactionTestCase {
 
     @Test
     public void testTransactionalAnnotation() {
-        //TODO: this does not really belong here, but it saves having to set all the DB stuff up again
-
         Assert.assertEquals("PASSED", URLTester.relative("rest/datasource/txninterceptor0").invokeURL().asString());
 
-        try {
-            URLResponse resp = URLTester.relative("rest/datasource/txninterceptor1").invokeURL();
-            Assert.fail(resp.asString());
-        } catch (RuntimeException expected) {
-
-        }
+        URLResponse resp = URLTester.relative("rest/datasource/txninterceptor1").invokeURL();
+        Assert.assertTrue(resp.exception() != null);
         Assert.assertEquals("PASSED", URLTester.relative("rest/datasource/txninterceptor2").invokeURL().asString());
 
 
