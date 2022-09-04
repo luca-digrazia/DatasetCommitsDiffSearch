68b0738400324217b4eabc429befed1c45bd2f61
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:32:47 +0200

    work alert checking #356

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/alerts/AlertCondition.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/alerts/AlertCondition.java
@@ -19,16 +19,12 @@
  */
 package org.graylog2.alerts;
 
-import com.google.common.collect.Maps;
 import org.graylog2.Core;
 import org.graylog2.alerts.types.MessageCountAlertCondition;
 import org.graylog2.plugin.Tools;
 import org.graylog2.plugin.database.EmbeddedPersistable;
-import org.graylog2.plugin.streams.Stream;
 import org.graylog2.rest.resources.streams.alerts.requests.CreateConditionRequest;
 import org.joda.time.DateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,14 +34,11 @@ import java.util.Map;
  */
 public abstract class AlertCondition implements EmbeddedPersistable {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AlertCondition.class);
-
     public enum Type {
         MESSAGE_COUNT
     }
 
     protected final String id;
-    protected final Stream stream;
     protected final Type type;
     protected final DateTime createdAt;
     protected final String creatorUserId;
@@ -53,9 +46,8 @@ public abstract class AlertCondition implements EmbeddedPersistable {
 
     private final Map<String, Object> parameters;
 
-    protected AlertCondition(Core core, Stream stream, String id, Type type, DateTime createdAt, String creatorUserId, Map<String, Object> parameters) {
+    protected AlertCondition(Core core, String id, Type type, DateTime createdAt, String creatorUserId, Map<String, Object> parameters) {
         this.id = id;
-        this.stream = stream;
         this.type = type;
         this.createdAt = createdAt;
         this.creatorUserId = creatorUserId;
@@ -65,9 +57,9 @@ public abstract class AlertCondition implements EmbeddedPersistable {
     }
 
     public abstract String getDescription();
-    protected abstract CheckResult runCheck();
+    public abstract boolean triggered();
 
-    public static AlertCondition fromRequest(CreateConditionRequest ccr, DateTime createdAt, Stream stream, Core core) throws NoSuchAlertConditionTypeException {
+    public static AlertCondition fromRequest(CreateConditionRequest ccr, DateTime createdAt, Core core) throws NoSuchAlertConditionTypeException {
         Type type;
         try {
             type = Type.valueOf(ccr.type.toUpperCase());
@@ -81,7 +73,6 @@ public abstract class AlertCondition implements EmbeddedPersistable {
             case MESSAGE_COUNT:
                 return new MessageCountAlertCondition(
                         core,
-                        stream,
                         (String) parameters.get("id"),
                         DateTime.parse((String) parameters.get("created_at")),
                         (String) parameters.get("creator_user_id"),
@@ -92,7 +83,7 @@ public abstract class AlertCondition implements EmbeddedPersistable {
         throw new NoSuchAlertConditionTypeException("Unhandled alert condition type: " + type);
     }
 
-    public static AlertCondition fromPersisted(Map<String, Object> fields, Stream stream, Core core) throws NoSuchAlertConditionTypeException {
+    public static AlertCondition fromPersisted(Map<String, Object> fields, Core core) throws NoSuchAlertConditionTypeException {
         Type type;
         try {
             type = Type.valueOf(((String) fields.get("type")).toUpperCase());
@@ -104,7 +95,6 @@ public abstract class AlertCondition implements EmbeddedPersistable {
             case MESSAGE_COUNT:
                 return new MessageCountAlertCondition(
                         core,
-                        stream,
                         (String) fields.get("id"),
                         DateTime.parse((String) fields.get("created_at")),
                         (String) fields.get("creator_user_id"),
@@ -115,28 +105,14 @@ public abstract class AlertCondition implements EmbeddedPersistable {
         throw new NoSuchAlertConditionTypeException("Unhandled alert condition type: " + type);
     }
 
-    public CheckResult triggered() {
-        LOG.debug("Checking alert condition [" + this + "]");
-        return runCheck();
-    }
-
     public String getId() {
         return id;
-    }
-
-    public Stream getStream() {
-        return stream;
-    }
-
-    public Map<String, Object> getParameters() {
-        return parameters;
     }
 
     @Override
     public String toString() {
         return new StringBuilder().append(id).append(":").append(type)
-                .append("={").append(getDescription()).append("}")
-                .append(", stream:={").append(stream).append("}")
+                .append(" {").append(getDescription()).append("}")
                 .toString();
     }
 
@@ -164,41 +140,6 @@ public abstract class AlertCondition implements EmbeddedPersistable {
     public static class NoSuchAlertConditionTypeException extends Throwable {
         public NoSuchAlertConditionTypeException(String msg) {
             super(msg);
-        }
-    }
-
-    public static class CheckResult {
-
-        private final boolean isTriggered;
-        private final String resultDescription;
-        private final AlertCondition triggeredCondition;
-        private final DateTime triggeredAt;
-
-        public CheckResult(boolean isTriggered, AlertCondition triggeredCondition, String resultDescription, DateTime triggeredAt) {
-            this.isTriggered = isTriggered;
-            this.resultDescription = resultDescription;
-            this.triggeredCondition = triggeredCondition;
-            this.triggeredAt = triggeredAt;
-        }
-
-        public CheckResult(boolean isTriggered) {
-            this(false, null, null, null);
-        }
-
-        public boolean isTriggered() {
-            return isTriggered;
-        }
-
-        public String getResultDescription() {
-            return resultDescription;
-        }
-
-        public AlertCondition getTriggeredCondition() {
-            return triggeredCondition;
-        }
-
-        public DateTime getTriggeredAt() {
-            return triggeredAt;
         }
     }
 
