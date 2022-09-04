0ce4520f189d66d576ca6c0bd62d69818d9a0117
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:49:40 +0200

    Generating getters/setters for now as play is unable to do it.

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/models/LoginRequest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/models/LoginRequest.java
@@ -1,10 +1,61 @@
+/*
+ * Copyright 2013 TORCH UG
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package models;
 
 import play.data.validation.Constraints.Required;
 
 public class LoginRequest {
 
+    public String destination;
 	@Required public String username;
 	@Required public String password;
-	
+	@Required public boolean noStartpage;
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isNoStartpage() {
+        return noStartpage;
+    }
+
+    public void setNoStartpage(boolean noStartpage) {
+        this.noStartpage = noStartpage;
+    }
 }
