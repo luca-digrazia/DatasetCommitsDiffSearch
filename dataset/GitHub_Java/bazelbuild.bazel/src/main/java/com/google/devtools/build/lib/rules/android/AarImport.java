de89156f86d77f59b844e592fc1f18e3b83a3566
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:44:21 +0200

    Remove use of resources ZIP
        This helps unblock removing the legacy "parsed" resource processing:
    
        https://github.com/bazelbuild/bazel/blob/3385fd60072f2a0ea415087596308821d42bd552/src/main/java/com/google/devtools/build/lib/rules/android/AndroidResourceMergingActionBuilder.java#L221
    
        This is being used for its side effects to validate AARs, but since the
        "parsed" resource merging logic is otherwise unused, it would be easy for this
        to diverge from everything.
    
        PiperOrigin-RevId: 283653559

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AarImport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AarImport.java
@@ -31,6 +31,7 @@ import com.google.devtools.build.lib.analysis.actions.SpawnAction;
 import com.google.devtools.build.lib.analysis.configuredtargets.RuleConfiguredTarget.Mode;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
+import com.google.devtools.build.lib.rules.android.AndroidConfiguration.AndroidAaptVersion;
 import com.google.devtools.build.lib.rules.android.databinding.DataBinding;
 import com.google.devtools.build.lib.rules.java.ImportDepsCheckActionBuilder;
 import com.google.devtools.build.lib.rules.java.JavaCommon;
@@ -111,7 +112,10 @@ public class AarImport implements RuleConfiguredTargetFactory {
 
     MergedAndroidAssets mergedAssets =
         AndroidAssets.forAarImport(assets)
-            .process(dataContext, AssetDependencies.fromRuleDeps(ruleContext, neverlink));
+            .process(
+                dataContext,
+                AssetDependencies.fromRuleDeps(ruleContext, neverlink),
+                AndroidAaptVersion.chooseTargetAaptVersion(ruleContext));
 
     ResourceApk resourceApk = ResourceApk.of(validatedResources, mergedAssets, null, null);
 
