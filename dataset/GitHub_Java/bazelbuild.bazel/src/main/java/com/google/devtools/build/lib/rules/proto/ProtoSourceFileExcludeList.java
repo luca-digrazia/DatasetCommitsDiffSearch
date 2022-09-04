f02daa0839f6a42c1ce193bd27b0d461f2324250
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:01:39 +0200

    [inclusive naming fixit] More low hanging fruit.
        These got missed in a previous change.
    
        RELNOTES: None
        PiperOrigin-RevId: 380559980

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/proto/ProtoSourceFileExcludeList.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/proto/ProtoSourceFileExcludeList.java
@@ -20,7 +20,6 @@ import static com.google.devtools.build.lib.packages.BuildType.LABEL_LIST;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Streams;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -81,19 +80,10 @@ public class ProtoSourceFileExcludeList {
    * Checks the proto sources for mixing excluded and non-excluded protos in one single
    * proto_library rule. Registers an attribute error if proto mixing is detected.
    *
-   * @param protoSources the protos to filter.
+   * @param protoFiles the protos to filter.
    * @param topLevelProtoRuleName the name of the top-level rule that generates the protos.
    * @return whether the proto sources are clean without mixing.
    */
-  public boolean checkSrcs(ImmutableList<ProtoSource> protoSources, String topLevelProtoRuleName) {
-    ImmutableList.Builder<Artifact> protoFiles = ImmutableList.builder();
-    for (ProtoSource protoSource : protoSources) {
-      protoFiles.add(protoSource.getOriginalSourceFile());
-    }
-    return checkSrcs(protoFiles.build(), topLevelProtoRuleName);
-  }
-
-  @Deprecated
   public boolean checkSrcs(Iterable<Artifact> protoFiles, String topLevelProtoRuleName) {
     List<Artifact> excluded = new ArrayList<>();
     List<Artifact> nonExcluded = new ArrayList<>();
