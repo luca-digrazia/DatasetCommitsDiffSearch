b2463697496c84286129a3fe6848bb80a18dd7e2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:11:12 +0200

    Handle multiple principals in mongoDB session (#10020) (#10093)
        * Handle multiple principals in mongoDB session
    
        * Make user id extraction from session more resilient
    
        + add test
    
        * add license header
    
        (cherry picked from commit bca595e237ed4b27537cb05dfc3aea81641f538c)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/MongoDbSession.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/MongoDbSession.java
@@ -1,38 +1,46 @@
 /*
- * Copyright 2013-2014 TORCH GmbH
+ * Copyright (C) 2020 Graylog, Inc.
  *
- * This file is part of Graylog2.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package org.graylog2.security;
 
+import com.google.common.collect.Iterables;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
 import org.bson.types.ObjectId;
 import org.graylog2.database.CollectionName;
 import org.graylog2.database.PersistedImpl;
 import org.graylog2.plugin.database.validators.Validator;
+import org.graylog2.shared.SuppressForbidden;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Date;
 import java.util.Map;
+import java.util.Optional;
 
-@CollectionName("sessions")
+@CollectionName(MongoDbSession.COLLECTION_NAME)
 public class MongoDbSession extends PersistedImpl {
-    private static final Logger log = LoggerFactory.getLogger(MongoDbSession.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MongoDbSession.class);
+    static final String COLLECTION_NAME = "sessions";
+    static final String FIELD_SESSION_ID = "session_id";
 
     protected MongoDbSession(Map<String, Object> fields) {
         super(fields);
@@ -52,7 +60,7 @@ public class MongoDbSession extends PersistedImpl {
         return null;
     }
 
-
+    @SuppressForbidden("Deliberate use of ObjectInputStream")
     public Map<Object, Object> getAttributes() {
         final Object attributes = fields.get("attributes");
         if (attributes == null) {
@@ -60,30 +68,50 @@ public class MongoDbSession extends PersistedImpl {
         }
         final ByteArrayInputStream bis = new ByteArrayInputStream((byte[]) attributes);
         try {
+            // FIXME: This could break backward compatibility if different Java versions are being used.
             final ObjectInputStream ois = new ObjectInputStream(bis);
             final Object o = ois.readObject();
             return (Map<Object, Object>) o;
         } catch (IOException e) {
-            log.error("little io. wow.", e);
+            LOG.error("little io. wow.", e);
         } catch (ClassNotFoundException e) {
-            log.error("wrong thingy in db", e);
+            LOG.error("wrong thingy in db", e);
         }
         return null;
     }
 
+    @SuppressForbidden("Deliberate use of ObjectOutputStream")
     public void setAttributes(Map<Object, Object> attributes) {
 
         try {
             final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            // FIXME: This could break backward compatibility if different Java versions are being used.
             final ObjectOutputStream oos = new ObjectOutputStream(bos);
             oos.writeObject(attributes);
             oos.close();
             fields.put("attributes", bos.toByteArray());
         } catch (IOException e) {
-            log.error("too bad :(", e);
+            LOG.error("too bad :(", e);
         }
     }
 
+    public Optional<String> getUserIdAttribute() {
+        final Map<Object, Object> attributes = getAttributes();
+        if (attributes == null) {
+            return Optional.empty();
+        }
+
+        final Object sessionId;
+
+        // A subject can have more than one principal. If that's the case, the user ID is required to be the first one.
+        final Object principals = attributes.get(DefaultSubjectContext.PRINCIPALS_SESSION_KEY);
+        if (principals instanceof Iterable) {
+            sessionId = Iterables.getFirst((Iterable<?>) principals, null);
+        } else {
+            sessionId = principals;
+        }
+        return Optional.ofNullable(sessionId).map(String::valueOf);
+    }
 
     public String getHost() {
         return (String) fields.get("host");
@@ -91,8 +119,10 @@ public class MongoDbSession extends PersistedImpl {
 
     public long getTimeout() {
         final Object timeout = fields.get("timeout");
-        if (timeout == null) return 0;
-        return (Long) timeout;
+        if (timeout == null) {
+            return 0;
+        }
+        return ((Number) timeout).longValue();
     }
 
     public Date getStartTimestamp() {
@@ -129,6 +159,6 @@ public class MongoDbSession extends PersistedImpl {
     }
 
     public String getSessionId() {
-        return String.valueOf(fields.get("session_id"));
+        return String.valueOf(fields.get(FIELD_SESSION_ID));
     }
 }
