d8e2f5ad8fffd6f4e1bc41ff0b62d6d1d901a19c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:43:21 +0200

    Remove some unnecessary imports

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-migrations/src/main/java/com/codahale/dropwizard/migrations/AbstractLiquibaseCommand.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-migrations/src/main/java/com/codahale/dropwizard/migrations/AbstractLiquibaseCommand.java
@@ -2,20 +2,21 @@ package com.codahale.dropwizard.migrations;
 
 import com.codahale.dropwizard.Configuration;
 import com.codahale.dropwizard.cli.ConfiguredCommand;
-import com.codahale.dropwizard.setup.Bootstrap;
-import com.codahale.dropwizard.db.ConfigurationStrategy;
 import com.codahale.dropwizard.db.DatabaseConfiguration;
+import com.codahale.dropwizard.db.DataSourceFactory;
+import com.codahale.dropwizard.setup.Bootstrap;
 import liquibase.Liquibase;
 import liquibase.exception.ValidationFailedException;
 import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
 
 public abstract class AbstractLiquibaseCommand<T extends Configuration> extends ConfiguredCommand<T> {
-    private final ConfigurationStrategy<T> strategy;
+    private final DatabaseConfiguration<T> strategy;
     private final Class<T> configurationClass;
 
     protected AbstractLiquibaseCommand(String name,
                                        String description,
-                                       ConfigurationStrategy<T> strategy,
+                                       DatabaseConfiguration<T> strategy,
                                        Class<T> configurationClass) {
         super(name, description);
         this.strategy = strategy;
@@ -28,15 +29,28 @@ public abstract class AbstractLiquibaseCommand<T extends Configuration> extends 
     }
 
     @Override
+    public void configure(Subparser subparser) {
+        super.configure(subparser);
+
+        subparser.addArgument("--migrations")
+                .dest("migrations-file")
+                .help("the file containing the Liquibase migrations for the application");
+    }
+
+    @Override
     @SuppressWarnings("UseOfSystemOutOrSystemErr")
     protected void run(Bootstrap<T> bootstrap, Namespace namespace, T configuration) throws Exception {
-        final DatabaseConfiguration dbConfig = strategy.getDatabaseConfiguration(configuration);
+        final DataSourceFactory dbConfig = strategy.getDataSourceFactory(configuration);
         dbConfig.setMaxSize(1);
         dbConfig.setMinSize(1);
+        dbConfig.setInitialSize(1);
 
         ManagedLiquibase managedLiquibase = null;
         try {
-            managedLiquibase = new ManagedLiquibase(dbConfig);
+            String migrationsFile = (String) namespace.get("migrations-file");
+            managedLiquibase = migrationsFile == null
+                    ? new ManagedLiquibase(dbConfig)
+                    : new ManagedLiquibase(dbConfig, migrationsFile);
             run(namespace, managedLiquibase);
         } catch (ValidationFailedException e) {
             e.printDescriptiveError(System.err);
