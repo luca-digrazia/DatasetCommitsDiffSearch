19f3cf104334767eaa8647fea3c298a1b9e8b06a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:49:00 +0200

    re-organized search REST resources #245

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/search/RelativeSearchResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/search/RelativeSearchResource.java
@@ -52,13 +52,12 @@ public class RelativeSearchResource extends SearchResource {
     public String searchRelative(
             @ApiParam(title = "query", description = "Query (Lucene syntax)", required = true) @QueryParam("query") String query,
             @ApiParam(title = "range", description = "Relative timeframe to search in. See method description.", required = true) @QueryParam("range") int range,
-            @ApiParam(title = "limit", description = "Maximum number of messages to return.", required = false) @QueryParam("limit") int limit,
-            @ApiParam(title = "offset", description = "Offset", required = false) @QueryParam("offset") int offset) {
+            @ApiParam(title = "limit", description = "Maximum number of messages to return.", required = false) @QueryParam("limit") int limit) {
         checkQuery(query);
 
         try {
             return json(buildSearchResult(
-                    core.getIndexer().searches().search(query, buildRelativeTimeRange(range), limit, offset)
+                    core.getIndexer().searches().search(query, buildRelativeTimeRange(range), limit)
             ));
         } catch (IndexHelper.InvalidRangeFormatException e) {
             LOG.warn("Invalid timerange parameters provided. Returning HTTP 400.", e);
