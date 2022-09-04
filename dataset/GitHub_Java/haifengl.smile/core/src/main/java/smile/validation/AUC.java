d53bc43074b2a7ee835ab2897abef62b23866ded
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:49:53 +0200

    fix AUC overflow issue

--- dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/validation/AUC.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/validation/AUC.java
@@ -1,18 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2010-2019 Haifeng Li
+ * Copyright (c) 2010 Haifeng Li
  *
- * Smile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Smile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
 
 package smile.validation;
@@ -40,13 +39,17 @@ import smile.sort.QuickSort;
  * @author Haifeng Li
  */
 public class AUC {
+
+    public AUC() {
+    }
+
     /**
      * Caulculate AUC for binary classifier.
      * @param truth The sample labels
      * @param probability The posterior probability of positive class.
      * @return AUC
      */
-    public static double apply(int[] truth, double[] probability) {
+    public static double measure(int[] truth, double[] probability) {
         if (truth.length != probability.length) {
             throw new IllegalArgumentException(String.format("The vector sizes don't match: %d != %d.", truth.length, probability.length));
         }
