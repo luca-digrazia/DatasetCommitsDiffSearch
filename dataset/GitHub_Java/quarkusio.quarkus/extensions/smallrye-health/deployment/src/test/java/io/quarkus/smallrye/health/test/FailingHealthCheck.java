feee846e4c7e15087cd8aaf69c4ba90036d49196
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:36:10 +0200

    quarkus-7030 Test that CDI beans in SmallRye Health are present

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-health/deployment/src/test/java/io/quarkus/smallrye/health/test/FailingHealthCheck.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-health/deployment/src/test/java/io/quarkus/smallrye/health/test/FailingHealthCheck.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.quarkus.smallrye.health.test;
 
 import java.util.Collections;
@@ -22,15 +6,21 @@ import java.util.Optional;
 
 import javax.enterprise.context.Dependent;
 
-import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import org.eclipse.microprofile.health.Readiness;
+
+import io.smallrye.health.HealthGroup;
 
 @Dependent
-@Health
+@Liveness
+@Readiness
+@HealthGroup("group1")
+@HealthGroup("group2")
 public class FailingHealthCheck implements HealthCheck {
 
-	@Override
+    @Override
     public HealthCheckResponse call() {
         return new HealthCheckResponse() {
             @Override
