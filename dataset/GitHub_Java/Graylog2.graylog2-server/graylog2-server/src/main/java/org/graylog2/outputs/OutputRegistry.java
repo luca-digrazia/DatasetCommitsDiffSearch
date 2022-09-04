bc6fd67fb509bcd14972c153228c000f42468bae
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:55:48 +0200

    OutputRegistry did not return the default output in the set of available outputs
        this lead to the BatchedESOutputFlusher to not flush any buffers at all
        the buffer was only being flushed whenever it reached the maximum configured size, seemingly losing messages (even though they were still there)
    
        in low traffic environments this could lead to confusion
    
        fixes #635

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/outputs/OutputRegistry.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/outputs/OutputRegistry.java
@@ -76,11 +76,12 @@ public class OutputRegistry {
     }
 
     protected MessageOutput launchOutput(Output output) throws MessageOutputConfigurationException {
-        final MessageOutput messageOutput = messageOutputFactory.fromStreamOutput(output);
-        if (messageOutput == null)
-            throw new IllegalArgumentException("Failed to instantiate MessageOutput from Output: " + output);
-
-        messageOutput.initialize(new Configuration(output.getConfiguration()));
+        MessageOutput messageOutput = messageOutputFactory.fromStreamOutput(output);
+        try {
+            messageOutput.initialize(new Configuration(output.getConfiguration()));
+        } catch (Exception e) {
+            LOG.error("Exception during output initialization: {}", e.toString());
+        }
 
         return messageOutput;
     }
