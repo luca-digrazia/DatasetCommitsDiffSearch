8331d5dfab153a40bd8878fe69f74a6350e17b5d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:03:38 +0200

    Correcting usage of helper method.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/agent/AgentResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/agent/AgentResource.java
@@ -1,30 +1,11 @@
-/**
- * This file is part of Graylog.
- *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.rest.resources.system.agent;
 
 import com.codahale.metrics.annotation.Timed;
-import com.google.common.primitives.Ints;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
-import org.apache.shiro.authz.annotation.RequiresAuthentication;
-import org.graylog2.Configuration;
 import org.graylog2.agents.Agent;
 import org.graylog2.agents.AgentService;
 import org.graylog2.agents.Agents;
@@ -32,7 +13,6 @@ import org.graylog2.database.NotFoundException;
 import org.graylog2.rest.models.agent.responses.AgentList;
 import org.graylog2.rest.models.agent.responses.AgentSummary;
 import org.graylog2.shared.rest.resources.RestResource;
-import org.joda.time.DateTime;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -42,44 +22,25 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
-import java.util.function.Function;
 
-@Api(value = "System/Agents", description = "Management of Graylog agents.")
+@Api(value = "System/Agents", description = "Management of graylog agents.")
 @Path("/system/agents")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@RequiresAuthentication
 public class AgentResource extends RestResource {
     private final AgentService agentService;
-    private final LostAgentFunction lostAgentFunction;
-
-    public class LostAgentFunction implements Function<Agent, Boolean> {
-        private final long timeOutInSeconds;
-
-        @Inject
-        public LostAgentFunction(long timeOutInSeconds) {
-            this.timeOutInSeconds = timeOutInSeconds;
-        }
-
-        @Override
-        public Boolean apply(Agent agent) {
-            final DateTime threshold = DateTime.now().minusSeconds(Ints.checkedCast(timeOutInSeconds));
-            return agent.getLastSeen().isAfter(threshold);
-        }
-    }
 
     @Inject
-    public AgentResource(AgentService agentService, Configuration config) {
+    public AgentResource(AgentService agentService) {
         this.agentService = agentService;
-        this.lostAgentFunction = new LostAgentFunction(config.getAgentInactiveThreshold().toSeconds());
     }
 
     @GET
     @Timed
-    @ApiOperation(value = "Lists all existing agent registrations")
+    @ApiOperation(value = "lists all existing agent registrations")
     public AgentList list() {
         final List<Agent> agents = agentService.all();
-        final List<AgentSummary> agentSummaries = Agents.toSummaryList(agents, lostAgentFunction);
+        final List<AgentSummary> agentSummaries = Agents.toSummaryList(agents);
         return AgentList.create(agentSummaries);
     }
 
@@ -95,7 +56,7 @@ public class AgentResource extends RestResource {
         final Agent agent = agentService.findById(agentId);
 
         if (agent != null)
-            return agent.toSummary(lostAgentFunction);
+            return agent.toSummary();
         else
             throw new NotFoundException("Agent <" + agentId + "> not found!");
     }
