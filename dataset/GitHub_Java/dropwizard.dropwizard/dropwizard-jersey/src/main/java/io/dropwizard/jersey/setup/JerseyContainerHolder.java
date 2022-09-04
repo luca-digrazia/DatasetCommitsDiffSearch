855e2be098ab17b775f01f6b3acfd85b890750c0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:39:43 +0200

    keep returned type ServletContainer in public API

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jersey/src/main/java/io/dropwizard/jersey/setup/JerseyContainerHolder.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jersey/src/main/java/io/dropwizard/jersey/setup/JerseyContainerHolder.java
@@ -6,14 +6,13 @@ import javax.servlet.Servlet;
 public class JerseyContainerHolder {
     private Servlet container;
 
-    public JerseyContainerHolder(Servlet container) {
+    public JerseyContainerHolder(ServletContainer container) {
         this.container = container;
     }
 
     public ServletContainer getContainer() {
-        if (container instanceof ServletContainer) {
+        if (container instanceof ServletContainer)
             return (ServletContainer) container;
-        }
         throw new RuntimeException("Not a ServletContainer. Use getServlet instead.");
     }
 
