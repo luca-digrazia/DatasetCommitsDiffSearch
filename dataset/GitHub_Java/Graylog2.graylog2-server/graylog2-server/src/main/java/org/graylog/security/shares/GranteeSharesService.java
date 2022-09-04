8c6dc3c38629992e5577a82624798bd278d32f4a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:21:13 +0200

    Add "capability" and "entity_type" filter parameters

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/security/shares/GranteeSharesService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/security/shares/GranteeSharesService.java
@@ -18,7 +18,6 @@ package org.graylog.security.shares;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Maps;
 import org.graylog.grn.GRN;
 import org.graylog.grn.GRNDescriptor;
 import org.graylog.grn.GRNDescriptorService;
@@ -39,7 +38,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -60,20 +58,17 @@ public class GranteeSharesService {
     public SharesResponse getPaginatedSharesFor(GRN grantee,
                                                 PaginationParameters paginationParameters,
                                                 String capabilityFilterString,
-                                                String entityTypeFilterString) {
+                                                String entityTypeFilter) {
         final Optional<Capability> capability = parseCapabilityFilter(capabilityFilterString);
         final ImmutableSet<GrantDTO> grants = capability.map(c -> grantService.getForGranteeWithCapability(grantee, c))
                 .orElseGet(() -> grantService.getForGrantee(grantee));
 
-        final Set<GRN> targets = grants.stream().map(GrantDTO::target).collect(Collectors.toSet());
-
-        final Map<GRN, Set<EntityDescriptor.Owner>> targetOwners = getTargetOwners(targets);
-
-        final List<EntityDescriptor> entityDescriptors = targets.stream()
+        final List<EntityDescriptor> entityDescriptors = grants.stream()
+                .map(GrantDTO::target)
                 .map(descriptorService::getDescriptor)
                 .filter(queryPredicate(paginationParameters))
-                .filter(entityTypeFilterPredicate(entityTypeFilterString))
-                .map(toEntityDescriptor(targetOwners))
+                .filter(entityTypeFilterPredicate(entityTypeFilter))
+                .map(grnDescriptor -> EntityDescriptor.create(grnDescriptor.grn(), grnDescriptor.title(), Collections.emptySet()))
                 .sorted(Comparator.comparing(EntityDescriptor::title, (t1, t2) -> {
                     if (paginationParameters.getOrder().toLowerCase(Locale.US).equals("desc")) {
                         return t2.compareTo(t1);
@@ -100,28 +95,6 @@ public class GranteeSharesService {
         );
 
         return SharesResponse.create(paginatedList, granteeCapabilities);
-    }
-
-    private Function<GRNDescriptor, EntityDescriptor> toEntityDescriptor(Map<GRN, Set<EntityDescriptor.Owner>> targetOwners) {
-        return grnDescriptor -> EntityDescriptor.create(
-                grnDescriptor.grn(),
-                grnDescriptor.title(),
-                targetOwners.getOrDefault(grnDescriptor.grn(), Collections.emptySet())
-        );
-    }
-
-    private Map<GRN, Set<EntityDescriptor.Owner>> getTargetOwners(Set<GRN> targets) {
-        return grantService.getOwnersForTargets(targets).entrySet()
-                .stream()
-                .map(entry -> Maps.immutableEntry(entry.getKey(), getOwners(entry)))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-    private Set<EntityDescriptor.Owner> getOwners(Map.Entry<GRN, Set<GRN>> entry) {
-        return descriptorService.getDescriptors(entry.getValue())
-                .stream()
-                .map(descriptor -> EntityDescriptor.Owner.create(descriptor.grn(), descriptor.title()))
-                .collect(Collectors.toSet());
     }
 
     private Optional<Capability> parseCapabilityFilter(String capabilityFilterString) {
