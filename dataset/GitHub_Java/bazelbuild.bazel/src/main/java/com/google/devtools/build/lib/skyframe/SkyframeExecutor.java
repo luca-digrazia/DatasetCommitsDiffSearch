575f0ed6f2c4c642640614bc5f9ec94ba4775d57
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:21:59 +0200

    Removing unused field in ConfiguredTargetFunction.
        RELNOTES: None.
        PiperOrigin-RevId: 262408794

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
@@ -295,6 +295,8 @@ public abstract class SkyframeExecutor implements WalkableGraphFactory {
   protected boolean active = true;
   private final SkyframePackageManager packageManager;
 
+  private final ResourceManager resourceManager;
+
   /** Used to lock evaluator on legacy calls to get existing values. */
   private final Object valueLookupLock = new Object();
 
@@ -424,6 +426,7 @@ public abstract class SkyframeExecutor implements WalkableGraphFactory {
             pkgLocator,
             numPackagesLoaded,
             this);
+    this.resourceManager = ResourceManager.instance();
     this.fileSystem = fileSystem;
     this.directories = Preconditions.checkNotNull(directories);
     this.actionKeyContext = Preconditions.checkNotNull(actionKeyContext);
@@ -1531,7 +1534,6 @@ public abstract class SkyframeExecutor implements WalkableGraphFactory {
    */
   public EvaluationResult<?> buildArtifacts(
       Reporter reporter,
-      ResourceManager resourceManager,
       Executor executor,
       Set<Artifact> artifactsToBuild,
       Collection<ConfiguredTarget> targetsToBuild,
@@ -1584,7 +1586,6 @@ public abstract class SkyframeExecutor implements WalkableGraphFactory {
   /** Asks the Skyframe evaluator to run a single exclusive test. */
   public EvaluationResult<?> runExclusiveTest(
       Reporter reporter,
-      ResourceManager resourceManager,
       Executor executor,
       ConfiguredTarget exclusiveTest,
       OptionsProvider options,
