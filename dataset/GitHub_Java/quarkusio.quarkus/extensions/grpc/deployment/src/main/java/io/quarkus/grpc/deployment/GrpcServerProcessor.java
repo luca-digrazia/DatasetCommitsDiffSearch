cd8b7d58fa0951180c3f0d2a5509a72a55e31296
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:27:53 +0200

    Vert.x 4: Change netty.allocator.maxOrder only if grpc is enabled
        * Increase netty maxOrder to 3 to make grpc runSize fit chunkSize
        * Change netty.allocator.maxOrder only if grpc is enabled

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/grpc/deployment/src/main/java/io/quarkus/grpc/deployment/GrpcServerProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/grpc/deployment/src/main/java/io/quarkus/grpc/deployment/GrpcServerProcessor.java
@@ -1,7 +1,6 @@
 package io.quarkus.grpc.deployment;
 
 import static io.quarkus.deployment.Feature.GRPC_SERVER;
-import static io.quarkus.grpc.deployment.GrpcDotNames.GRPC_SERVICE;
 import static java.util.Arrays.asList;
 
 import java.lang.reflect.Modifier;
@@ -16,12 +15,9 @@ import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.MethodInfo;
 import org.jboss.logging.Logger;
 
-import io.grpc.BindableService;
 import io.grpc.internal.ServerImpl;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
-import io.quarkus.arc.deployment.ValidationPhaseBuildItem;
-import io.quarkus.arc.processor.BeanInfo;
-import io.quarkus.arc.processor.BuiltinScope;
+import io.quarkus.arc.processor.DotNames;
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildProducer;
@@ -36,7 +32,6 @@ import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.builditem.ServiceStartBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
-import io.quarkus.grpc.GrpcService;
 import io.quarkus.grpc.deployment.devmode.FieldDefinalizingVisitor;
 import io.quarkus.grpc.runtime.GrpcContainer;
 import io.quarkus.grpc.runtime.GrpcServerRecorder;
@@ -44,11 +39,8 @@ import io.quarkus.grpc.runtime.config.GrpcConfiguration;
 import io.quarkus.grpc.runtime.config.GrpcServerBuildTimeConfig;
 import io.quarkus.grpc.runtime.health.GrpcHealthEndpoint;
 import io.quarkus.grpc.runtime.health.GrpcHealthStorage;
-import io.quarkus.grpc.runtime.supports.context.GrpcEnableRequestContext;
-import io.quarkus.grpc.runtime.supports.context.GrpcRequestContextCdiInterceptor;
 import io.quarkus.kubernetes.spi.KubernetesPortBuildItem;
 import io.quarkus.netty.deployment.MinNettyAllocatorMaxOrderBuildItem;
-import io.quarkus.runtime.LaunchMode;
 import io.quarkus.smallrye.health.deployment.spi.HealthBuildItem;
 import io.quarkus.vertx.deployment.VertxBuildItem;
 
@@ -72,34 +64,15 @@ public class GrpcServerProcessor {
             CombinedIndexBuildItem combinedIndexBuildItem) {
         Collection<ClassInfo> bindableServices = combinedIndexBuildItem.getIndex()
                 .getAllKnownImplementors(GrpcDotNames.BINDABLE_SERVICE);
-
         for (ClassInfo service : bindableServices) {
-            if (Modifier.isAbstract(service.flags())) {
-                continue;
-            }
-            BindableServiceBuildItem item = new BindableServiceBuildItem(service.name());
-            for (MethodInfo method : service.methods()) {
-                if (method.hasAnnotation(GrpcDotNames.BLOCKING)) {
-                    item.registerBlockingMethod(method.name());
+            if (!Modifier.isAbstract(service.flags()) && service.classAnnotation(DotNames.SINGLETON) != null) {
+                BindableServiceBuildItem item = new BindableServiceBuildItem(service.name());
+                for (MethodInfo method : service.methods()) {
+                    if (method.hasAnnotation(GrpcDotNames.BLOCKING)) {
+                        item.registerBlockingMethod(method.name());
+                    }
                 }
-            }
-            bindables.produce(item);
-        }
-    }
-
-    @BuildStep
-    void validateBindableServices(ValidationPhaseBuildItem validationPhase,
-            BuildProducer<ValidationPhaseBuildItem.ValidationErrorBuildItem> errors) {
-        for (BeanInfo bean : validationPhase.getContext().beans().classBeans().withBeanType(BindableService.class)) {
-            //noinspection OptionalGetWithoutIsPresent
-            if (bean.getTarget().get().asClass().classAnnotation(GRPC_SERVICE) == null) {
-                errors.produce(new ValidationPhaseBuildItem.ValidationErrorBuildItem(
-                        new IllegalStateException(
-                                "A gRPC service bean must be annotated with io.quarkus.GrpcService: " + bean)));
-            }
-            if (!bean.getScope().getDotName().equals(BuiltinScope.SINGLETON.getName())) {
-                errors.produce(new ValidationPhaseBuildItem.ValidationErrorBuildItem(
-                        new IllegalStateException("A gRPC service bean must have the javax.inject.Singleton scope: " + bean)));
+                bindables.produce(item);
             }
         }
     }
@@ -115,14 +88,9 @@ public class GrpcServerProcessor {
     }
 
     @BuildStep
-    void registerBeans(BuildProducer<AdditionalBeanBuildItem> beans,
+    void buildContainerBean(BuildProducer<AdditionalBeanBuildItem> beans,
             List<BindableServiceBuildItem> bindables, BuildProducer<FeatureBuildItem> features) {
-        // @GrpcService is a CDI stereotype
-        beans.produce(new AdditionalBeanBuildItem(GrpcService.class));
-        beans.produce(new AdditionalBeanBuildItem(GrpcRequestContextCdiInterceptor.class));
-        beans.produce(new AdditionalBeanBuildItem(GrpcEnableRequestContext.class));
-
-        if (!bindables.isEmpty() || LaunchMode.current() == LaunchMode.DEVELOPMENT) {
+        if (!bindables.isEmpty()) {
             beans.produce(AdditionalBeanBuildItem.unremovableOf(GrpcContainer.class));
             features.produce(new FeatureBuildItem(GRPC_SERVER));
         } else {
