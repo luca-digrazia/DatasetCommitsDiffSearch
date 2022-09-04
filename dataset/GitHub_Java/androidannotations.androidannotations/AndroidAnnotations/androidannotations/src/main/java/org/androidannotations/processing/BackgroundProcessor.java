493987aa18d5826b33ea7c01567308ba22c8b9c9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:14:09 +0200

    Directly generate Task instantiation
        Instead of create a new Runnable instance which will be wrapped by a
        Task instance, directly create a Task (which is a Runnable).

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/processing/BackgroundProcessor.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/processing/BackgroundProcessor.java
@@ -66,10 +66,9 @@ public class BackgroundProcessor implements DecoratingElementProcessor {
 		Background annotation = element.getAnnotation(Background.class);
 		String id = annotation.id();
 		int delay = annotation.delay();
-		String serial = annotation.serial();
 
 		JClass backgroundExecutorClass = holder.refClass(BackgroundExecutor.class);
-		JInvocation newTask = _new(anonymousTaskClass).arg(lit(id)).arg(lit(delay)).arg(lit(serial));
+		JInvocation newTask = _new(anonymousTaskClass).arg(lit(id)).arg(lit(delay));
 		JInvocation executeCall = backgroundExecutorClass.staticInvoke("execute").arg(newTask);
 
 		delegatingMethod.body().add(executeCall);
