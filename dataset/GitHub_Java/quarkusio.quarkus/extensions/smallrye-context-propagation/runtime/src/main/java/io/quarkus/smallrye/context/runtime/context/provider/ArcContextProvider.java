dd742253b828992e698594f5df945bb85ceb9b8a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:16:24 +0200

    ArcContextProvider: use constant

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/runtime/src/main/java/io/quarkus/smallrye/context/runtime/context/provider/ArcContextProvider.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/runtime/src/main/java/io/quarkus/smallrye/context/runtime/context/provider/ArcContextProvider.java
@@ -3,6 +3,8 @@ package io.quarkus.smallrye.context.runtime.context.provider;
 import java.util.Collection;
 import java.util.Map;
 
+import javax.enterprise.inject.spi.CDI;
+
 import org.eclipse.microprofile.context.ThreadContext;
 import org.eclipse.microprofile.context.spi.ThreadContextController;
 import org.eclipse.microprofile.context.spi.ThreadContextProvider;
@@ -24,12 +26,12 @@ public class ArcContextProvider implements ThreadContextProvider {
 
     @Override
     public ThreadContextSnapshot currentContext(Map<String, String> map) {
-        ArcContainer arc = Arc.container();
-        if (!arc.isRunning()) {
+        if (!isCdiAvailable()) {
             //return null as per docs to state that propagation of this context is not supported
             return null;
         }
 
+        ArcContainer arc = Arc.container();
         if (!isContextActiveOnThisThread(arc)) {
             // request context not active, nothing to propagate, return no-op
             return NOOP_SNAPSHOT;
@@ -65,12 +67,12 @@ public class ArcContextProvider implements ThreadContextProvider {
     @Override
     public ThreadContextSnapshot clearedContext(Map<String, String> map) {
         // note that by cleared we mean that we still activate context if need be, just leave the contents blank
-        ArcContainer arc = Arc.container();
-        if (!arc.isRunning()) {
+        if (!isCdiAvailable()) {
             //return null as per docs to state that propagation of this context is not supported
             return null;
         }
 
+        ArcContainer arc = Arc.container();
         if (!isContextActiveOnThisThread(arc)) {
             // request context not active, nothing to propagate, return no-op
             return NOOP_SNAPSHOT;
@@ -104,6 +106,21 @@ public class ArcContextProvider implements ThreadContextProvider {
     @Override
     public String getThreadContextType() {
         return ThreadContext.CDI;
+    }
+
+    /**
+     * Checks if CDI is available within the application by using {@code CDI.current()}.
+     * If an exception is thrown, it is suppressed and false is returns, otherwise true is returned.
+     *
+     * @return true if CDI can be used, false otherwise
+     */
+    private boolean isCdiAvailable() {
+        try {
+            return CDI.current() != null;
+        } catch (IllegalStateException e) {
+            // no CDI provider found, CDI isn't available
+            return false;
+        }
     }
 
     private boolean isContextActiveOnThisThread(ArcContainer arc) {
