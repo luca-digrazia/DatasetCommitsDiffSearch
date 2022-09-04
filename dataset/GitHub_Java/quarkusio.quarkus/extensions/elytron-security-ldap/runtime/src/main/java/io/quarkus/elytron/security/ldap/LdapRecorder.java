1043ae7c76afc2bb1873574b8baf3a54841cf517
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:06:22 +0200

    Using simplified classes
        Fixes #6606

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/elytron-security-ldap/runtime/src/main/java/io/quarkus/elytron/security/ldap/LdapRecorder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/elytron-security-ldap/runtime/src/main/java/io/quarkus/elytron/security/ldap/LdapRecorder.java
@@ -1,12 +1,15 @@
 package io.quarkus.elytron.security.ldap;
 
+import java.security.Provider;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 import javax.naming.NamingException;
 import javax.naming.directory.DirContext;
 
 import org.wildfly.common.function.ExceptionSupplier;
+import org.wildfly.security.WildFlyElytronProvider;
 import org.wildfly.security.auth.realm.ldap.AttributeMapping;
 import org.wildfly.security.auth.realm.ldap.DirContextFactory;
 import org.wildfly.security.auth.realm.ldap.LdapSecurityRealmBuilder;
@@ -15,29 +18,38 @@ import org.wildfly.security.auth.server.SecurityRealm;
 import io.quarkus.elytron.security.ldap.config.AttributeMappingConfig;
 import io.quarkus.elytron.security.ldap.config.DirContextConfig;
 import io.quarkus.elytron.security.ldap.config.IdentityMappingConfig;
-import io.quarkus.elytron.security.ldap.config.LdapSecurityRealmRuntimeConfig;
+import io.quarkus.elytron.security.ldap.config.LdapSecurityRealmConfig;
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
 
 @Recorder
 public class LdapRecorder {
 
+    private static final Provider[] PROVIDERS = new Provider[] { new WildFlyElytronProvider() };
+
     /**
      * Create a runtime value for a {@linkplain LdapSecurityRealm}
      *
-     * @param runtimeConfig the realm config
-     * @return runtime value wrapper for the SecurityRealm
+     * @param config - the realm config
+     * @return - runtime value wrapper for the SecurityRealm
      */
-    public RuntimeValue<SecurityRealm> createRealm(LdapSecurityRealmRuntimeConfig runtimeConfig) {
+    public RuntimeValue<SecurityRealm> createRealm(LdapSecurityRealmConfig config) {
+        Supplier<Provider[]> providers = new Supplier<Provider[]>() {
+            @Override
+            public Provider[] get() {
+                return PROVIDERS;
+            }
+        };
         LdapSecurityRealmBuilder builder = LdapSecurityRealmBuilder.builder()
-                .setDirContextSupplier(createDirContextSupplier(runtimeConfig.dirContext))
+                .setDirContextSupplier(createDirContextSupplier(config.dirContext))
+                .setProviders(providers)
                 .identityMapping()
-                .map(createAttributeMappings(runtimeConfig.identityMapping))
-                .setRdnIdentifier(runtimeConfig.identityMapping.rdnIdentifier)
-                .setSearchDn(runtimeConfig.identityMapping.searchBaseDn)
+                .map(createAttributeMappings(config.identityMapping))
+                .setRdnIdentifier(config.identityMapping.rdnIdentifier)
+                .setSearchDn(config.identityMapping.searchBaseDn)
                 .build();
 
-        if (runtimeConfig.directVerification) {
+        if (config.directVerification) {
             builder.addDirectEvidenceVerification(false);
         }
 
