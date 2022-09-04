67ff1f0ac5b0b41939b0d91b64472e92a97c6d94
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:24:29 +0200

    Remove comment about legacy package loading.
        RELNOTES: None
        PiperOrigin-RevId: 197504627

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/PackageFunction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/PackageFunction.java
@@ -25,13 +25,10 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.google.devtools.build.lib.actions.FileValue;
-import com.google.devtools.build.lib.actions.InconsistentFilesystemException;
 import com.google.devtools.build.lib.clock.BlazeClock;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.cmdline.LabelSyntaxException;
 import com.google.devtools.build.lib.cmdline.PackageIdentifier;
-import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.events.ExtendedEventHandler.Postable;
@@ -51,7 +48,6 @@ import com.google.devtools.build.lib.packages.RuleVisibility;
 import com.google.devtools.build.lib.packages.Target;
 import com.google.devtools.build.lib.profiler.Profiler;
 import com.google.devtools.build.lib.profiler.ProfilerTask;
-import com.google.devtools.build.lib.profiler.SilentCloseable;
 import com.google.devtools.build.lib.skyframe.GlobValue.InvalidGlobPatternException;
 import com.google.devtools.build.lib.skyframe.SkylarkImportLookupFunction.SkylarkImportFailedException;
 import com.google.devtools.build.lib.syntax.BuildFileAST;
@@ -423,16 +419,6 @@ public class PackageFunction implements SkyFunction {
       return null;
     }
     String workspaceName = workspaceNameValue.getName();
-
-    RepositoryMappingValue repositoryMappingValue =
-        (RepositoryMappingValue)
-            env.getValue(RepositoryMappingValue.key(packageId.getRepository()));
-    if (repositoryMappingValue == null) {
-      return null;
-    }
-    ImmutableMap<RepositoryName, RepositoryName> repositoryMapping =
-        repositoryMappingValue.getRepositoryMapping();
-
     RootedPath buildFileRootedPath = packageLookupValue.getRootedPath(packageId);
     FileValue buildFileValue = null;
     Path buildFilePath = buildFileRootedPath.asPath();
@@ -488,7 +474,6 @@ public class PackageFunction implements SkyFunction {
     LoadedPackageCacheEntry packageCacheEntry =
         loadPackage(
             workspaceName,
-            repositoryMapping,
             replacementContents,
             packageId,
             buildFilePath,
@@ -505,11 +490,7 @@ public class PackageFunction implements SkyFunction {
     try {
       pkgBuilder.buildPartial();
     } catch (NoSuchPackageException e) {
-      throw new PackageFunctionException(
-          e,
-          e.getCause() instanceof SkyframeGlobbingIOException
-              ? Transience.PERSISTENT
-              : Transience.TRANSIENT);
+      throw new PackageFunctionException(e, Transience.TRANSIENT);
     }
     try {
       // Since the Skyframe dependencies we request below in
@@ -1118,7 +1099,7 @@ public class PackageFunction implements SkyFunction {
                   ValueOrException3<
                       IOException, BuildFileNotFoundException, FileSymlinkCycleException>>
               globValueMap)
-          throws SkyframeGlobbingIOException {
+          throws IOException {
         ValueOrException3<IOException, BuildFileNotFoundException, FileSymlinkCycleException>
             valueOrException =
                 Preconditions.checkNotNull(globValueMap.get(globKey), "%s should not be missing",
@@ -1128,18 +1109,11 @@ public class PackageFunction implements SkyFunction {
               "%s should not be missing", globKey).getMatches();
         } catch (BuildFileNotFoundException e) {
           // Legacy package loading is only able to handle an IOException, so a rethrow here is the
-          // best we can do.
-          throw new SkyframeGlobbingIOException(e);
-        } catch (IOException e) {
-          throw new SkyframeGlobbingIOException(e);
+          // best we can do. But after legacy package loading, PackageFunction will go through all
+          // the skyframe deps and properly handle InconsistentFilesystemExceptions.
+          throw new IOException(e.getMessage());
         }
       }
-    }
-  }
-
-  private static class SkyframeGlobbingIOException extends IOException {
-    private SkyframeGlobbingIOException(Exception cause) {
-      super(cause);
     }
   }
 
@@ -1176,7 +1150,6 @@ public class PackageFunction implements SkyFunction {
   @Nullable
   private LoadedPackageCacheEntry loadPackage(
       String workspaceName,
-      ImmutableMap<RepositoryName, RepositoryName> repositoryMapping,
       @Nullable String replacementContents,
       PackageIdentifier packageId,
       Path buildFilePath,
@@ -1189,11 +1162,11 @@ public class PackageFunction implements SkyFunction {
       throws InterruptedException, PackageFunctionException {
     LoadedPackageCacheEntry packageCacheEntry = packageFunctionCache.getIfPresent(packageId);
     if (packageCacheEntry == null) {
+      profiler.startTask(ProfilerTask.CREATE_PACKAGE, packageId.toString());
       if (packageProgress != null) {
         packageProgress.startReadPackage(packageId);
       }
-      try (SilentCloseable c =
-          Profiler.instance().profile(ProfilerTask.CREATE_PACKAGE, packageId.toString())) {
+      try {
         AstParseResult astParseResult = astCache.getIfPresent(packageId);
         if (astParseResult == null) {
           if (showLoadingProgress.get()) {
@@ -1231,7 +1204,7 @@ public class PackageFunction implements SkyFunction {
           StoredEventHandler astParsingEventHandler = new StoredEventHandler();
           BuildFileAST ast =
               PackageFactory.parseBuildFile(
-                  packageId, input, preludeStatements, repositoryMapping, astParsingEventHandler);
+                  packageId, input, preludeStatements, astParsingEventHandler);
           astParseResult = new AstParseResult(ast, astParsingEventHandler);
           astCache.put(packageId, astParseResult);
         }
@@ -1257,18 +1230,16 @@ public class PackageFunction implements SkyFunction {
         GlobberWithSkyframeGlobDeps globberWithSkyframeGlobDeps =
             makeGlobber(buildFilePath, packageId, packageRoot, env);
         long startTimeNanos = BlazeClock.nanoTime();
-        Package.Builder pkgBuilder =
-            packageFactory.createPackageFromAst(
-                workspaceName,
-                repositoryMapping,
-                packageId,
-                buildFilePath,
-                astParseResult,
-                importResult.importMap,
-                importResult.fileDependencies,
-                defaultVisibility,
-                skylarkSemantics,
-                globberWithSkyframeGlobDeps);
+        Package.Builder pkgBuilder = packageFactory.createPackageFromAst(
+            workspaceName,
+            packageId,
+            buildFilePath,
+            astParseResult,
+            importResult.importMap,
+            importResult.fileDependencies,
+            defaultVisibility,
+            skylarkSemantics,
+            globberWithSkyframeGlobDeps);
         long loadTimeNanos = Math.max(BlazeClock.nanoTime() - startTimeNanos, 0L);
         packageCacheEntry = new LoadedPackageCacheEntry(
             pkgBuilder,
@@ -1279,6 +1250,8 @@ public class PackageFunction implements SkyFunction {
           packageProgress.doneReadPackage(packageId);
         }
         packageFunctionCache.put(packageId, packageCacheEntry);
+      } finally {
+        profiler.completeTask(ProfilerTask.CREATE_PACKAGE);
       }
     }
     return packageCacheEntry;
