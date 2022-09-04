a0d527aefb613fcf240f06074d3a62a8c4b44924
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:16:53 +0200

    add Matern Kernel

--- dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/math/kernel/MaternKernel.java
+++ dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/math/kernel/MaternKernel.java
@@ -23,8 +23,8 @@ import smile.math.MathEx;
  * The class of Matérn kernels is a generalization of the RBF.
  * It has an additional parameter nu which controls the smoothness of
  * the kernel function. The smaller nu, the less smooth the approximated
- * function is. As nu -> inf, the kernel becomes equivalent to the
- * Gaussian/RBF kernel. When nu = 1/2, the kernel becomes identical to the
+ * function is. As nu -> inf, the kernel becomes equivalent to the RBF
+ * kernel. When nu = 1/2, the Matérn kernel becomes identical to the
  * absolute exponential kernel. The Matern kernel become especially simple
  * when nu is half-integer. Important intermediate values are 3/2
  * (once differentiable functions) and 5/2 (twice differentiable functions).
