590b0de544890b7e6b05b3a7dfa6175e9fa889c7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:53:30 +0200

    Maintaining spacing

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jdbi/src/main/java/com/yammer/dropwizard/jdbi/DBIFactory.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jdbi/src/main/java/com/yammer/dropwizard/jdbi/DBIFactory.java
@@ -22,7 +22,6 @@ import org.slf4j.LoggerFactory;
 public class DBIFactory {
     private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(DBI.class);
     private static final MetricName RAW_SQL = new MetricName(DBI.class, "raw-sql");
-
     private static class SanerNamingStrategy extends DelegatingStatementNameStrategy {
         private SanerNamingStrategy() {
             super(NameStrategies.CHECK_EMPTY,
@@ -38,19 +37,18 @@ public class DBIFactory {
         }
     }
 
-    private final ManagedDataSourceFactory dataSourceFactory = new ManagedDataSourceFactory();
+    private final Environment environment;
 
-    public DBI build(Environment environment,
-                     DatabaseConfiguration configuration,
-                     String name) throws ClassNotFoundException {
-        final ManagedDataSource dataSource = dataSourceFactory.build(configuration);
-        return build(environment, configuration, dataSource, name);
+    public DBIFactory(Environment environment) {
+        this.environment = environment;
     }
 
-    public DBI build(Environment environment,
-                     DatabaseConfiguration configuration,
-                     ManagedDataSource dataSource,
-                     String name) {
+    public DBI build(DatabaseConfiguration configuration, String name) throws ClassNotFoundException {
+        final ManagedDataSource dataSource = new ManagedDataSourceFactory(configuration).build();
+        return build(configuration, dataSource, name);
+    }
+
+    public DBI build(DatabaseConfiguration configuration, ManagedDataSource dataSource, String name) {
         final String validationQuery = configuration.getValidationQuery();
         final DBI dbi = new DBI(dataSource);
         environment.manage(dataSource);
@@ -58,9 +56,7 @@ public class DBIFactory {
         dbi.setSQLLog(new LogbackLog(LOGGER, Level.TRACE));
         dbi.setTimingCollector(new InstrumentedTimingCollector(Metrics.defaultRegistry(),
                                                                new SanerNamingStrategy()));
-        if (configuration.isAutoCommentsEnabled()) {
-            dbi.setStatementRewriter(new NamePrependingStatementRewriter(new ColonPrefixNamedParamStatementRewriter()));
-        }
+        dbi.setStatementRewriter(new NamePrependingStatementRewriter(new ColonPrefixNamedParamStatementRewriter()));
         dbi.registerArgumentFactory(new OptionalArgumentFactory(configuration.getDriverClass()));
         dbi.registerContainerFactory(new ImmutableListContainerFactory());
         dbi.registerContainerFactory(new ImmutableSetContainerFactory());
