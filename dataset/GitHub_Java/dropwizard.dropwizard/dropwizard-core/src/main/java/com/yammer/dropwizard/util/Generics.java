206e7cb68163ea2f4b47241117023801a7b7bd63
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:49:06 +0200

    Suppress warnings in Generics.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/util/Generics.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/util/Generics.java
@@ -1,16 +1,39 @@
 package com.yammer.dropwizard.util;
 
-import com.yammer.dropwizard.config.Configuration;
-
-import javax.annotation.Nonnull;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Helper methods for class type parameters.
+ * @see <a href="http://gafter.blogspot.com/2006/12/super-type-tokens.html">Super Type Tokens</a>
+ */
 public class Generics {
     private Generics() { /* singleton */ }
 
-    public static Class<?> getTypeParameter(@Nonnull Class<?> klass) {
-        Type t = klass;
+    /**
+     * Finds the type parameter for the given class.
+     *
+     * @param klass    a parameterized class
+     * @return the class's type parameter
+     */
+    public static Class<?> getTypeParameter(Class<?> klass) {
+        return getTypeParameter(klass, Object.class);
+    }
+
+    /**
+     * Finds the type parameter for the given class which is assignable to the bound class.
+     *
+     * @param klass    a parameterized class
+     * @param bound    the type bound
+     * @param <T>      the type bound
+     * @return the class's type parameter
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Class<T> getTypeParameter(Class<?> klass, Class<? super T> bound) {
+        Type t = checkNotNull(klass);
         while (t instanceof Class<?>) {
             t = ((Class<?>) t).getGenericSuperclass();
         }
@@ -23,14 +46,31 @@ public class Generics {
             // should typically have one of type parameters (first one) that matches:
             for (Type param : ((ParameterizedType) t).getActualTypeArguments()) {
                 if (param instanceof Class<?>) {
-                    final Class<?> cls = (Class<?>) param;
-                    if (Configuration.class.isAssignableFrom(cls)) {
-                        return cls;
+                    final Class<T> cls = determineClass(bound, param);
+                    if (cls != null) { return cls; }
+                }
+                else if (param instanceof TypeVariable) {
+                    for (Type paramBound : ((TypeVariable<?>) param).getBounds()) {
+                        if (paramBound instanceof Class<?>) {
+                            final Class<T> cls = determineClass(bound, paramBound);
+                            if (cls != null) { return cls; }
+                        }
                     }
                 }
             }
         }
-        throw new IllegalStateException("Cannot figure out Configuration type parameterization for " +
-                                                klass.getName());
+        throw new IllegalStateException("Cannot figure out type parameterization for " + klass.getName());
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> Class<T> determineClass(Class<? super T> bound, Type candidate) {
+        if (candidate instanceof Class<?>) {
+            final Class<?> cls = (Class<?>) candidate;
+            if (bound.isAssignableFrom(cls)) {
+                return (Class<T>) cls;
+            }
+        }
+
+        return null;
     }
 }
