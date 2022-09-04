d20331ef1df8f09aac1bce06b888a3e73c59710e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:50:07 +0200

    Adapting to new annotation structure.

--- dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/system/inputs/InputsTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/system/inputs/InputsTest.java
@@ -1,42 +1,47 @@
 /**
- * This file is part of Graylog2.
+ * This file is part of Graylog.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package integration.system.inputs;
 
 import integration.BaseRestTest;
+import integration.MongoDbSeed;
+import integration.RequiresAuthentication;
 import integration.RequiresVersion;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-@RequiresVersion(laterThan = "0.90.0")
+@RequiresVersion(">=0.90.0")
+@RequiresAuthentication
 public class InputsTest extends BaseRestTest {
 
     @Test
+    @MongoDbSeed(locations = "graylog")
     public void createInputTest() {
 
         given().when()
                 .body(jsonResourceForMethod()).post("/system/inputs")
                 .then()
-                    .statusCode(404).statusLine(notNullValue());
+                    .statusCode(400).statusLine(notNullValue());
     }
 
-    @Test(dependsOnMethods = "createInputTest")
+    @Test
     public void listInput() {
+        createInputTest();
         given().when().get("/system/inputs").then().statusCode(200);
     }
 }
