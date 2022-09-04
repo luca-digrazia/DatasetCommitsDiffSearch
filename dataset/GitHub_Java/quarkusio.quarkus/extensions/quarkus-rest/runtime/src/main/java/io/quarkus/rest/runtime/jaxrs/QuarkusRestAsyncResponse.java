e6d809ecccc58c16f0e97752d5a8b3e691c45d07
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:21:06 +0200

    AsyncResponse: support timeout

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/quarkus-rest/runtime/src/main/java/io/quarkus/rest/runtime/jaxrs/QuarkusRestAsyncResponse.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/quarkus-rest/runtime/src/main/java/io/quarkus/rest/runtime/jaxrs/QuarkusRestAsyncResponse.java
@@ -24,7 +24,7 @@ public class QuarkusRestAsyncResponse implements AsyncResponse, Handler<Long> {
     private volatile boolean cancelled;
     private volatile TimeoutHandler timeoutHandler;
     // only used with lock, no need for volatile
-    private long timerId = -1;
+    private long timerId;
 
     public QuarkusRestAsyncResponse(QuarkusRestRequestContext context) {
         this.context = context;
