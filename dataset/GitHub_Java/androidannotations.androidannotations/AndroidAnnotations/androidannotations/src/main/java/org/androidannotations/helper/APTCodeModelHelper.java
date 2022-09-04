d7f75edd713d7f51009027a7074b6f6b37d5252a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:02:05 +0200

    add a check in addAnnotation method to prevent adding two annotations on an element

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/helper/APTCodeModelHelper.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/helper/APTCodeModelHelper.java
@@ -19,10 +19,6 @@ import static com.sun.codemodel.JExpr._new;
 import static com.sun.codemodel.JExpr.lit;
 import static org.androidannotations.helper.ModelConstants.GENERATION_SUFFIX;
 
-import org.androidannotations.holder.EComponentHolder;
-import org.androidannotations.holder.GeneratedClassHolder;
-
-import javax.lang.model.util.ElementFilter;
 import java.io.StringWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -45,10 +41,13 @@ import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.type.WildcardType;
+import javax.lang.model.util.ElementFilter;
 import javax.lang.model.util.Types;
 
 import org.androidannotations.annotations.EBean;
 import org.androidannotations.holder.EBeanHolder;
+import org.androidannotations.holder.EComponentHolder;
+import org.androidannotations.holder.GeneratedClassHolder;
 
 import com.sun.codemodel.JAnnotatable;
 import com.sun.codemodel.JAnnotationUse;
@@ -377,14 +376,19 @@ public class APTCodeModelHelper {
 		throw new IllegalStateException("Unable to extract target name from JFieldRef");
 	}
 
-	public JDefinedClass createDelegatingAnonymousRunnableClass(EComponentHolder holder, JBlock previousBody) {
+	public JDefinedClass createDelegatingAnonymousRunnableClass(EComponentHolder holder, JMethod delegatedMethod) {
+
 		JCodeModel codeModel = holder.codeModel();
 
-		JDefinedClass anonymousRunnableClass = codeModel.anonymousClass(Runnable.class);
+		JDefinedClass anonymousRunnableClass;
+		JBlock previousMethodBody = removeBody(delegatedMethod);
+
+		anonymousRunnableClass = codeModel.anonymousClass(Runnable.class);
 
 		JMethod runMethod = anonymousRunnableClass.method(JMod.PUBLIC, codeModel.VOID, "run");
 		runMethod.annotate(Override.class);
-		runMethod.body().add(previousBody);
+
+		runMethod.body().add(previousMethodBody);
 
 		return anonymousRunnableClass;
 	}
