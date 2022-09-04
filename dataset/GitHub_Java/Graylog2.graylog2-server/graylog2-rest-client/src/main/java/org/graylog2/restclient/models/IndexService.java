ace85330e043fd7983a110bdc2e7092ee74ef882
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:14:13 +0200

    add REST endpoint to retrieve the list of reopened indices
        this somewhat duplicates the closed indices REST call, but we can live that right now. going forward this should be changed anyway.
    
        Graylog2/graylog2-web-interface#1072

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/IndexService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/IndexService.java
@@ -33,6 +33,9 @@ import java.util.concurrent.TimeUnit;
 
 import static org.graylog2.restclient.lib.Configuration.apiTimeout;
 
+/**
+ * @author Lennart Koopmann <lennart@torch.sh>
+ */
 public class IndexService {
 
     private final ApiClient api;
@@ -59,20 +62,17 @@ public class IndexService {
 
     public DeflectorInformationResponse getDeflectorInfo() throws APIException, IOException {
         return api.path(routes.DeflectorResource().deflector(), DeflectorInformationResponse.class)
-                .timeout(apiTimeout("deflector_info", 20, TimeUnit.SECONDS))
                 .execute();
     }
 
     public DeflectorConfigResponse getDeflectorConfig() throws APIException, IOException {
         return api.path(routes.DeflectorResource().config(), DeflectorConfigResponse.class)
-                .timeout(apiTimeout("deflector_config", 60, TimeUnit.SECONDS))
                 .onlyMasterNode()
                 .execute();
     }
 
     public ClosedIndicesResponse getClosedIndices() throws APIException, IOException {
         return api.path(routes.IndicesResource().closed(), ClosedIndicesResponse.class)
-                .timeout(apiTimeout("closed_indices", 60, TimeUnit.SECONDS))
                 .execute();
     }
 
