852eed49f3fc55b77275b7d886e0c0b3b11fe724
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:56:52 +0200

    AssetLoader should remove the trailing slash from uriPath.
        - In dropwizard-example:  key.substring(uriPath.length() + 1) == xample.txt but should actually be example.txt

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/servlets/tests/AssetServletTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/servlets/tests/AssetServletTest.java
@@ -14,13 +14,11 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
-/**
- * @author Sam Quigley <quigley@emerose.com>
- */
+@SuppressWarnings({ "serial", "StaticNonFinalField", "StaticVariableMayNotBeInitialized" })
 public class AssetServletTest {
     private static ServletTester servletTester;
-    private static final String DUMMY_SERVLET = "/dummy_servlet";
-    private static final String NOINDEX_SERVLET = "/noindex_servlet";
+    private static final String DUMMY_SERVLET = "/dummy_servlet/";
+    private static final String NOINDEX_SERVLET = "/noindex_servlet/";
     private static final String RESOURCE_PATH = "assets";
     private static final CacheBuilderSpec CACHE_BUILDER_SPEC = CacheBuilderSpec.parse("maximumSize=100");
 
@@ -28,6 +26,7 @@ public class AssetServletTest {
     private HttpTester response;
 
     // ServletTester expects to be able to instantiate the servlet with zero arguments
+
     public static class DummyAssetServlet extends AssetServlet {
         public DummyAssetServlet() {
             super(Resources.getResource(RESOURCE_PATH), CACHE_BUILDER_SPEC, DUMMY_SERVLET, "index.htm");
@@ -43,8 +42,8 @@ public class AssetServletTest {
     @BeforeClass
     public static void setup() throws Exception {
         servletTester = new ServletTester();
-        servletTester.addServlet(DummyAssetServlet.class, DUMMY_SERVLET + "/*");
-        servletTester.addServlet(NoIndexAssetServlet.class, NOINDEX_SERVLET + "/*");
+        servletTester.addServlet(DummyAssetServlet.class, DUMMY_SERVLET + "*");
+        servletTester.addServlet(NoIndexAssetServlet.class, NOINDEX_SERVLET + "*");
         servletTester.start();
     }
 
@@ -52,7 +51,7 @@ public class AssetServletTest {
     public void setupTester() throws Exception {
         request = new HttpTester();
         request.setMethod("GET");
-        request.setURI(DUMMY_SERVLET + "/example.txt");
+        request.setURI(DUMMY_SERVLET + "example.txt");
         request.setVersion("HTTP/1.0");
         response = new HttpTester();
     }
@@ -66,7 +65,7 @@ public class AssetServletTest {
 
     @Test
     public void itThrows404IfTheAssetIsMissing() throws Exception {
-        request.setURI(DUMMY_SERVLET + "/doesnotexist.txt");
+        request.setURI(DUMMY_SERVLET + "doesnotexist.txt");
 
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(404));
@@ -75,10 +74,10 @@ public class AssetServletTest {
     @Test
     public void assetsHaveAConsistentEtag() throws Exception {
         response.parse(servletTester.getResponses(request.generate()));
-        String firstEtag = response.getHeader(HttpHeaders.ETAG);
+        final String firstEtag = response.getHeader(HttpHeaders.ETAG);
 
         response.parse(servletTester.getResponses(request.generate()));
-        String secondEtag = response.getHeader(HttpHeaders.ETAG);
+        final String secondEtag = response.getHeader(HttpHeaders.ETAG);
 
         assertThat(firstEtag, is(not(nullValue())));
         assertThat(firstEtag, is(not("")));
@@ -88,11 +87,11 @@ public class AssetServletTest {
     @Test
     public void etagsDifferForDifferentFiles() throws Exception {
         response.parse(servletTester.getResponses(request.generate()));
-        String firstEtag = response.getHeader(HttpHeaders.ETAG);
+        final String firstEtag = response.getHeader(HttpHeaders.ETAG);
 
-        request.setURI(DUMMY_SERVLET + "/foo.bar");
+        request.setURI(DUMMY_SERVLET + "foo.bar");
         response.parse(servletTester.getResponses(request.generate()));
-        String secondEtag = response.getHeader(HttpHeaders.ETAG);
+        final String secondEtag = response.getHeader(HttpHeaders.ETAG);
 
         assertThat(firstEtag, is(not(equalTo(secondEtag))));
     }
@@ -100,15 +99,15 @@ public class AssetServletTest {
     @Test
     public void itSupportsIfNoneMatchRequests() throws Exception {
         response.parse(servletTester.getResponses(request.generate()));
-        String correctEtag = response.getHeader(HttpHeaders.ETAG);
+        final String correctEtag = response.getHeader(HttpHeaders.ETAG);
 
         request.setHeader(HttpHeaders.IF_NONE_MATCH, correctEtag);
         response.parse(servletTester.getResponses(request.generate()));
-        int statusWithMatchingEtag = response.getStatus();
+        final int statusWithMatchingEtag = response.getStatus();
 
         request.setHeader(HttpHeaders.IF_NONE_MATCH, correctEtag + "FOO");
         response.parse(servletTester.getResponses(request.generate()));
-        int statusWithNonMatchingEtag = response.getStatus();
+        final int statusWithNonMatchingEtag = response.getStatus();
 
         assertThat(statusWithMatchingEtag, is(304));
         assertThat(statusWithNonMatchingEtag, is(200));
@@ -117,10 +116,10 @@ public class AssetServletTest {
     @Test
     public void assetsHaveAConsistentLastModifiedTime() throws Exception {
         response.parse(servletTester.getResponses(request.generate()));
-        long firstLastModifiedTime = response.getDateHeader(HttpHeaders.LAST_MODIFIED);
+        final long firstLastModifiedTime = response.getDateHeader(HttpHeaders.LAST_MODIFIED);
 
         response.parse(servletTester.getResponses(request.generate()));
-        long secondLastModifiedTime = response.getDateHeader(HttpHeaders.LAST_MODIFIED);
+        final long secondLastModifiedTime = response.getDateHeader(HttpHeaders.LAST_MODIFIED);
 
         assertThat(firstLastModifiedTime, is(not(nullValue())));
         assertThat(firstLastModifiedTime, is(equalTo(secondLastModifiedTime)));
@@ -129,19 +128,19 @@ public class AssetServletTest {
     @Test
     public void itSupportsIfModifiedSinceRequests() throws Exception {
         response.parse(servletTester.getResponses(request.generate()));
-        long lastModifiedTime = response.getDateHeader(HttpHeaders.LAST_MODIFIED);
+        final long lastModifiedTime = response.getDateHeader(HttpHeaders.LAST_MODIFIED);
 
         request.setDateHeader(HttpHeaders.IF_MODIFIED_SINCE, lastModifiedTime);
         response.parse(servletTester.getResponses(request.generate()));
-        int statusWithMatchingLastModifiedTime = response.getStatus();
+        final int statusWithMatchingLastModifiedTime = response.getStatus();
 
         request.setDateHeader(HttpHeaders.IF_MODIFIED_SINCE, lastModifiedTime - 100);
         response.parse(servletTester.getResponses(request.generate()));
-        int statusWithStaleLastModifiedTime = response.getStatus();
+        final int statusWithStaleLastModifiedTime = response.getStatus();
 
         request.setDateHeader(HttpHeaders.IF_MODIFIED_SINCE, lastModifiedTime + 100);
         response.parse(servletTester.getResponses(request.generate()));
-        int statusWithRecentLastModifiedTime = response.getStatus();
+        final int statusWithRecentLastModifiedTime = response.getStatus();
 
         assertThat(statusWithMatchingLastModifiedTime, is(304));
         assertThat(statusWithStaleLastModifiedTime, is(200));
@@ -154,7 +153,7 @@ public class AssetServletTest {
         assertThat(response.getStatus(), is(200));
         assertThat(response.getContentType(), is(MimeTypes.TEXT_PLAIN));
 
-        request.setURI(DUMMY_SERVLET + "/foo.bar");
+        request.setURI(DUMMY_SERVLET + "foo.bar");
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(200));
         assertThat(response.getContentType(), is(MimeTypes.TEXT_HTML));
@@ -163,19 +162,19 @@ public class AssetServletTest {
     @Test
     public void itServesAnIndexFileByDefault() throws Exception {
         // Root directory listing:
-        request.setURI(DUMMY_SERVLET + "/");
+        request.setURI(DUMMY_SERVLET);
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(200));
         assertThat(response.getContent(), containsString("/assets Index File"));
 
         // Subdirectory listing:
-        request.setURI(DUMMY_SERVLET + "/some_directory");
+        request.setURI(DUMMY_SERVLET + "some_directory");
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(200));
         assertThat(response.getContent(), containsString("/assets/some_directory Index File"));
 
         // Subdirectory listing with slash:
-        request.setURI(DUMMY_SERVLET + "/some_directory/");
+        request.setURI(DUMMY_SERVLET + "some_directory/");
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(200));
         assertThat(response.getContent(), containsString("/assets/some_directory Index File"));
@@ -184,31 +183,31 @@ public class AssetServletTest {
     @Test
     public void itThrowsA404IfNoIndexFileIsDefined() throws Exception {
         // Root directory listing:
-        request.setURI(NOINDEX_SERVLET + "/");
+        request.setURI(NOINDEX_SERVLET + '/');
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(404));
 
         // Subdirectory listing:
-        request.setURI(NOINDEX_SERVLET + "/some_directory");
+        request.setURI(NOINDEX_SERVLET + "some_directory");
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(404));
 
         // Subdirectory listing with slash:
-        request.setURI(NOINDEX_SERVLET + "/some_directory/");
+        request.setURI(NOINDEX_SERVLET + "some_directory/");
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(404));
     }
 
     @Test
     public void itDoesntAllowOverridingUrls() throws Exception {
-        request.setURI(DUMMY_SERVLET + "/file:/etc/passwd");
+        request.setURI(DUMMY_SERVLET + "file:/etc/passwd");
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(404));
     }
 
     @Test
     public void itDoesntAllowOverridingPaths() throws Exception {
-        request.setURI(DUMMY_SERVLET + "//etc/passwd");
+        request.setURI(DUMMY_SERVLET + "/etc/passwd");
         response.parse(servletTester.getResponses(request.generate()));
         assertThat(response.getStatus(), is(404));
     }
