72950afcb3b54d11dd838e8a22356db9b4fd2602
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:43:36 +0200

    Remove value equality check for proguarding
        ErrorProne has been complaining about this for a while, and unless there's
        something incredibly clever going on that I'm missing (in which case we should
        document it), using equals() should be just as effective.
    
        RELNOTES: none
        PiperOrigin-RevId: 192809127

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidBinary.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidBinary.java
@@ -781,7 +781,7 @@ public abstract class AndroidBinary implements RuleConfiguredTargetFactory {
                   ruleContext.getImplicitOutputArtifact(AndroidRuleClasses.ANDROID_RESOURCES_ZIP))
               .withShrunkJar(proguardOutput.getOutputJar())
               .withProguardMapping(proguardOutput.getMapping())
-              .withPrimary(resourceApk.getPrimaryResources())
+              .withPrimary(resourceApk.getPrimaryResource())
               .withDependencies(resourceApk.getResourceDependencies())
               .setTargetAaptVersion(AndroidAaptVersion.chooseTargetAaptVersion(ruleContext))
               .setResourceFilterFactory(ResourceFilterFactory.fromRuleContext(ruleContext))
@@ -790,7 +790,15 @@ public abstract class AndroidBinary implements RuleConfiguredTargetFactory {
               .build();
       filesBuilder.add(
           ruleContext.getImplicitOutputArtifact(AndroidRuleClasses.ANDROID_RESOURCE_SHRINKER_LOG));
-      return resourceApk.withApk(apk);
+      return new ResourceApk(
+          apk,
+          resourceApk.getResourceJavaSrcJar(),
+          resourceApk.getResourceJavaClassJar(),
+          resourceApk.getResourceDependencies(),
+          resourceApk.getPrimaryResource(),
+          resourceApk.getManifest(),
+          resourceApk.getResourceProguardConfig(),
+          resourceApk.getMainDexProguardConfig());
     }
     return resourceApk;
   }
