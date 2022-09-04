dcc36cd34dfbe4d0e8c85cf3c10ad5d8ac627b2f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:37:00 +0200

    Fix LimitedOptionalStringValidator and add tests

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/database/validators/LimitedOptionalStringValidator.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/database/validators/LimitedOptionalStringValidator.java
@@ -1,43 +1,44 @@
 /**
- * This file is part of Graylog2.
+ * This file is part of Graylog.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.database.validators;
 
 import org.graylog2.plugin.database.validators.ValidationResult;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class LimitedOptionalStringValidator extends OptionalStringValidator {
-    private int maxLength;
+    private final int maxLength;
 
     public LimitedOptionalStringValidator(int maxLength) {
+        checkArgument(maxLength > 0, "maxLength must be greater than 0");
         this.maxLength = maxLength;
     }
 
     @Override
     public ValidationResult validate(Object value) {
-        final ValidationResult superResult = super.validate(value);
+        ValidationResult result = super.validate(value);
 
-        if (superResult instanceof ValidationResult.ValidationPassed) {
+        if (result instanceof ValidationResult.ValidationPassed) {
             final String sValue = (String) value;
-            if (sValue == null || sValue.length() <= maxLength) {
-                new ValidationResult.ValidationPassed();
-            } else {
-                new ValidationResult.ValidationFailed("Value is longer than " + maxLength + " characters!");
+            if (sValue != null && sValue.length() > maxLength) {
+                result = new ValidationResult.ValidationFailed("Value is longer than " + maxLength + " characters!");
             }
         }
 
-        return superResult;
+        return result;
     }
 }
