ff39e82fc0a80388376081bfb1d0dcc76428a088
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:55:05 +0200

    Add .rlib as an allowed extension for static libraries for C++ rules
        With this change we can directly link Rust static libraries into C++ binaries. Without this change we would have to register a symlink action that "renames" the extension from .rlib to .a.
    
        What exactly is rlib?
    
        From https://doc.rust-lang.org/reference/linkage.html:
    
        ```
        This [rlib] is used as an intermediate artifact and can be thought of as a "static Rust library". These rlib files, unlike staticlib files, are interpreted by the compiler in future linkage. This essentially means that rustc will look for metadata in rlib files like it looks for metadata in dynamic libraries. This form of output is used to produce statically linked executables as well as staticlib outputs.
        ```
    
        This file is an ar archive, just like regular .a archives. This file can actually be used by linkers directly. Rlib contains an additional metadata file that is ignored by linkers but used by rustc compilations. From the high level, rlib can be thought of as a Clang module + object files in an ar archive.
    
        RELNOTES: None.
        PiperOrigin-RevId: 385139708

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/rules/cpp/CppFileTypesTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/rules/cpp/CppFileTypesTest.java
@@ -14,8 +14,7 @@
 
 package com.google.devtools.build.lib.rules.cpp;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,22 +28,33 @@ public class CppFileTypesTest {
 
   @Test
   public void testTwoDotExtensions() {
-    assertTrue(CppFileTypes.OBJECT_FILE.matches("test.o"));
-    assertTrue(CppFileTypes.PIC_OBJECT_FILE.matches("test.pic.o"));
-    assertFalse(CppFileTypes.OBJECT_FILE.matches("test.pic.o"));
+    assertThat(CppFileTypes.OBJECT_FILE.matches("test.o")).isTrue();
+    assertThat(CppFileTypes.PIC_OBJECT_FILE.matches("test.pic.o")).isTrue();
+    assertThat(CppFileTypes.OBJECT_FILE.matches("test.pic.o")).isFalse();
+  }
+
+  @Test
+  public void testRlib() {
+    assertThat(CppFileTypes.RUST_RLIB.matches("foo.a")).isFalse();
+    assertThat(CppFileTypes.RUST_RLIB.matches("foo.rlib")).isTrue();
   }
 
   @Test
   public void testVersionedSharedLibraries() {
-    assertTrue(CppFileTypes.SHARED_LIBRARY.matches("somelibrary.so"));
-    assertTrue(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.2"));
-    assertTrue(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.20"));
-    assertTrue(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.20.2"));
-    assertTrue(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("a/somelibrary.so.2"));
-    assertFalse(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.e"));
-    assertFalse(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.2e"));
-    assertFalse(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.e2"));
-    assertFalse(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.20.e2"));
-    assertFalse(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.a.2"));
+    assertThat(CppFileTypes.SHARED_LIBRARY.matches("somelibrary.so")).isTrue();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.2")).isTrue();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.20")).isTrue();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.20.2")).isTrue();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("a/somelibrary.so.2")).isTrue();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.e")).isFalse();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.2e")).isTrue();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.e2")).isFalse();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.20.e2")).isFalse();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.a.2")).isFalse();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.2$")).isFalse();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("somelibrary.so.1a_b2")).isTrue();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("libA.so.gen.empty.def")).isFalse();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("libA.so.if.exp")).isFalse();
+    assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("libA.so.if.lib")).isFalse();
   }
 }
