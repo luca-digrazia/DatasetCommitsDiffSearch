9ec5a0f1e91224ab3db7f545e2d4a3ea4147c74f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:04:36 +0200

    reinitialize vertx pgpool on restart, fixes #3037

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/reactive-pg-client/runtime/src/main/java/io/quarkus/reactive/pg/client/runtime/PgPoolRecorder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/reactive-pg-client/runtime/src/main/java/io/quarkus/reactive/pg/client/runtime/PgPoolRecorder.java
@@ -1,40 +1,21 @@
 package io.quarkus.reactive.pg.client.runtime;
 
 import io.quarkus.arc.runtime.BeanContainer;
-import io.quarkus.datasource.runtime.DataSourceRuntimeConfig;
-import io.quarkus.datasource.runtime.DataSourcesRuntimeConfig;
-import io.quarkus.datasource.runtime.LegacyDataSourceRuntimeConfig;
-import io.quarkus.datasource.runtime.LegacyDataSourcesRuntimeConfig;
-import io.quarkus.reactive.datasource.runtime.DataSourceReactiveRuntimeConfig;
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.ShutdownContext;
 import io.quarkus.runtime.annotations.Recorder;
+import io.reactiverse.pgclient.PgClient;
+import io.reactiverse.pgclient.PgPool;
+import io.reactiverse.pgclient.PgPoolOptions;
 import io.vertx.core.Vertx;
-import io.vertx.pgclient.PgConnectOptions;
-import io.vertx.pgclient.PgPool;
-import io.vertx.sqlclient.PoolOptions;
 
 @Recorder
 public class PgPoolRecorder {
 
     public RuntimeValue<PgPool> configurePgPool(RuntimeValue<Vertx> vertx, BeanContainer container,
-            DataSourcesRuntimeConfig dataSourcesRuntimeConfig,
-            DataSourceReactiveRuntimeConfig dataSourceReactiveRuntimeConfig,
-            DataSourceReactivePostgreSQLConfig dataSourceReactivePostgreSQLConfig,
-            LegacyDataSourcesRuntimeConfig legacyDataSourcesRuntimeConfig,
-            LegacyDataSourceReactivePostgreSQLConfig legacyDataSourceReactivePostgreSQLConfig,
-            boolean isLegacy,
-            ShutdownContext shutdown) {
+            DataSourceConfig dataSourceConfig, PgPoolConfig pgPoolConfig, ShutdownContext shutdown) {
 
-        PgPool pgPool;
-        if (!isLegacy) {
-            pgPool = initialize(vertx.getValue(), dataSourcesRuntimeConfig.defaultDataSource,
-                    dataSourceReactiveRuntimeConfig,
-                    dataSourceReactivePostgreSQLConfig);
-        } else {
-            pgPool = legacyInitialize(vertx.getValue(), dataSourcesRuntimeConfig.defaultDataSource,
-                    legacyDataSourcesRuntimeConfig.defaultDataSource, legacyDataSourceReactivePostgreSQLConfig);
-        }
+        PgPool pgPool = initialize(vertx.getValue(), dataSourceConfig, pgPoolConfig);
 
         PgPoolProducer producer = container.instance(PgPoolProducer.class);
         producer.initialize(pgPool);
@@ -43,114 +24,34 @@ public class PgPoolRecorder {
         return new RuntimeValue<>(pgPool);
     }
 
-    private PgPool initialize(Vertx vertx, DataSourceRuntimeConfig dataSourceRuntimeConfig,
-            DataSourceReactiveRuntimeConfig dataSourceReactiveRuntimeConfig,
-            DataSourceReactivePostgreSQLConfig dataSourceReactivePostgreSQLConfig) {
-        PoolOptions poolOptions = toPoolOptions(dataSourceRuntimeConfig, dataSourceReactiveRuntimeConfig,
-                dataSourceReactivePostgreSQLConfig);
-        PgConnectOptions pgConnectOptions = toPgConnectOptions(dataSourceRuntimeConfig, dataSourceReactiveRuntimeConfig,
-                dataSourceReactivePostgreSQLConfig);
-        return PgPool.pool(vertx, pgConnectOptions, poolOptions);
+    private PgPool initialize(Vertx vertx, DataSourceConfig dataSourceConfig, PgPoolConfig pgPoolConfig) {
+        PgPoolOptions pgPoolOptions = toPgPoolOptions(dataSourceConfig, pgPoolConfig);
+        return PgClient.pool(vertx, pgPoolOptions);
     }
 
-    private PoolOptions toPoolOptions(DataSourceRuntimeConfig dataSourceRuntimeConfig,
-            DataSourceReactiveRuntimeConfig dataSourceReactiveRuntimeConfig,
-            DataSourceReactivePostgreSQLConfig dataSourceReactivePostgreSQLConfig) {
-        PoolOptions poolOptions;
-        poolOptions = new PoolOptions();
+    private PgPoolOptions toPgPoolOptions(DataSourceConfig dataSourceConfig, PgPoolConfig pgPoolConfig) {
+        PgPoolOptions pgPoolOptions;
+        if (dataSourceConfig != null) {
 
-        if (dataSourceReactiveRuntimeConfig.maxSize.isPresent()) {
-            poolOptions.setMaxSize(dataSourceReactiveRuntimeConfig.maxSize.getAsInt());
-        }
+            pgPoolOptions = dataSourceConfig.url
+                    .filter(s -> s.matches("^vertx-reactive:postgre(?:s|sql)://.*$"))
+                    .map(s -> s.substring("vertx-reactive:".length()))
+                    .map(PgPoolOptions::fromUri)
+                    .orElse(new PgPoolOptions());
 
-        return poolOptions;
-    }
+            dataSourceConfig.username.ifPresent(value -> pgPoolOptions.setUser(value));
+            dataSourceConfig.password.ifPresent(value -> pgPoolOptions.setPassword(value));
+            dataSourceConfig.maxSize.ifPresent(value -> pgPoolOptions.setMaxSize(value));
 
-    private PgConnectOptions toPgConnectOptions(DataSourceRuntimeConfig dataSourceRuntimeConfig,
-            DataSourceReactiveRuntimeConfig dataSourceReactiveRuntimeConfig,
-            DataSourceReactivePostgreSQLConfig dataSourceReactivePostgreSQLConfig) {
-        PgConnectOptions pgConnectOptions;
-
-        if (dataSourceReactiveRuntimeConfig.url.isPresent()) {
-            String url = dataSourceReactiveRuntimeConfig.url.get();
-            // clean up the URL to make migrations easier
-            if (url.matches("^vertx-reactive:postgre(?:s|sql)://.*$")) {
-                url = url.substring("vertx-reactive:".length());
-            }
-            pgConnectOptions = PgConnectOptions.fromUri(url);
         } else {
-            pgConnectOptions = new PgConnectOptions();
+            pgPoolOptions = new PgPoolOptions();
         }
 
-        if (dataSourceRuntimeConfig.username.isPresent()) {
-            pgConnectOptions.setUser(dataSourceRuntimeConfig.username.get());
+        if (pgPoolConfig != null) {
+            pgPoolConfig.cachePreparedStatements.ifPresent(value -> pgPoolOptions.setCachePreparedStatements(value));
+            pgPoolConfig.pipeliningLimit.ifPresent(value -> pgPoolOptions.setPipeliningLimit(value));
         }
 
-        if (dataSourceRuntimeConfig.password.isPresent()) {
-            pgConnectOptions.setPassword(dataSourceRuntimeConfig.password.get());
-        }
-
-        if (dataSourceReactivePostgreSQLConfig.cachePreparedStatements.isPresent()) {
-            pgConnectOptions.setCachePreparedStatements(dataSourceReactivePostgreSQLConfig.cachePreparedStatements.get());
-        }
-        if (dataSourceReactivePostgreSQLConfig.pipeliningLimit.isPresent()) {
-            pgConnectOptions.setPipeliningLimit(dataSourceReactivePostgreSQLConfig.pipeliningLimit.getAsInt());
-        }
-
-        return pgConnectOptions;
-    }
-
-    // Legacy configuration
-
-    private PgPool legacyInitialize(Vertx vertx, DataSourceRuntimeConfig dataSourceRuntimeConfig,
-            LegacyDataSourceRuntimeConfig legacyDataSourceRuntimeConfig,
-            LegacyDataSourceReactivePostgreSQLConfig legacyDataSourceReactivePostgreSQLConfig) {
-        PoolOptions poolOptions = legacyToPoolOptionsLegacy(legacyDataSourceRuntimeConfig);
-        PgConnectOptions pgConnectOptions = legacyToPostgreSQLConnectOptions(dataSourceRuntimeConfig,
-                legacyDataSourceRuntimeConfig, legacyDataSourceReactivePostgreSQLConfig);
-        return PgPool.pool(vertx, pgConnectOptions, poolOptions);
-    }
-
-    private PoolOptions legacyToPoolOptionsLegacy(LegacyDataSourceRuntimeConfig legacyDataSourceRuntimeConfig) {
-        PoolOptions poolOptions;
-        poolOptions = new PoolOptions();
-
-        // Slight change of behavior compared to the legacy code: the default max size is set to 20
-        poolOptions.setMaxSize(legacyDataSourceRuntimeConfig.maxSize);
-
-        return poolOptions;
-    }
-
-    private PgConnectOptions legacyToPostgreSQLConnectOptions(DataSourceRuntimeConfig dataSourceRuntimeConfig,
-            LegacyDataSourceRuntimeConfig legacyDataSourceRuntimeConfig,
-            LegacyDataSourceReactivePostgreSQLConfig legacyDataSourceReactivePostgreSQLConfig) {
-        PgConnectOptions pgConnectOptions;
-        if (legacyDataSourceRuntimeConfig.url.isPresent()) {
-            String url = legacyDataSourceRuntimeConfig.url.get();
-            // clean up the URL to make migrations easier
-            if (url.matches("^vertx-reactive:postgre(?:s|sql)://.*$")) {
-                url = url.substring("vertx-reactive:".length());
-            }
-            pgConnectOptions = PgConnectOptions.fromUri(url);
-        } else {
-            pgConnectOptions = new PgConnectOptions();
-        }
-
-        if (dataSourceRuntimeConfig.username.isPresent()) {
-            pgConnectOptions.setUser(dataSourceRuntimeConfig.username.get());
-        }
-
-        if (dataSourceRuntimeConfig.password.isPresent()) {
-            pgConnectOptions.setPassword(dataSourceRuntimeConfig.password.get());
-        }
-
-        if (legacyDataSourceReactivePostgreSQLConfig.cachePreparedStatements.isPresent()) {
-            pgConnectOptions.setCachePreparedStatements(legacyDataSourceReactivePostgreSQLConfig.cachePreparedStatements.get());
-        }
-        if (legacyDataSourceReactivePostgreSQLConfig.pipeliningLimit.isPresent()) {
-            pgConnectOptions.setPipeliningLimit(legacyDataSourceReactivePostgreSQLConfig.pipeliningLimit.getAsInt());
-        }
-
-        return pgConnectOptions;
+        return pgPoolOptions;
     }
 }
