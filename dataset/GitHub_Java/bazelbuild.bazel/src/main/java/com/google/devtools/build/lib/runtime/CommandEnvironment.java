7d789e5667714cff5e69f0ac75ed56b77a1bc86a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 20:10:11 +0200

    Allow setting the OutputService in tests.
        PiperOrigin-RevId: 240609460

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/CommandEnvironment.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/CommandEnvironment.java
@@ -77,7 +77,6 @@ public final class CommandEnvironment {
   private final Set<String> visibleActionEnv = new TreeSet<>();
   private final Set<String> visibleTestEnv = new TreeSet<>();
   private final Map<String, String> actionClientEnv = new TreeMap<>();
-  private final Map<String, String> repoEnv = new TreeMap<>();
   private final TimestampGranularityMonitor timestampGranularityMonitor;
   private final Thread commandThread;
   private final Command command;
@@ -201,14 +200,6 @@ public final class CommandEnvironment {
         if (entry.getValue() == null) {
           visibleTestEnv.add(entry.getKey());
         }
-      }
-    }
-
-    repoEnv.putAll(actionClientEnv);
-    BuildConfiguration.Options configOpts = options.getOptions(BuildConfiguration.Options.class);
-    if (configOpts != null) {
-      for (Map.Entry<String, String> entry : configOpts.repositoryEnvironment) {
-        repoEnv.put(entry.getKey(), entry.getValue());
       }
     }
   }
@@ -676,10 +667,6 @@ public final class CommandEnvironment {
     Path workspace = getWorkspace();
     Path workingDirectory;
     if (inWorkspace()) {
-      if (commonOptions.clientCwd.containsUplevelReferences()) {
-        throw new AbruptExitException(
-            "Client cwd contains uplevel references", ExitCode.COMMAND_LINE_ERROR);
-      }
       workingDirectory = workspace.getRelative(commonOptions.clientCwd);
     } else {
       workspace = FileSystemUtils.getWorkingDirectory(getRuntime().getFileSystem());
@@ -724,10 +711,5 @@ public final class CommandEnvironment {
    */
   public Map<String, String> getActionClientEnv() {
     return Collections.unmodifiableMap(actionClientEnv);
-  }
-
-  /** Returns the client environment with all settings from --action_env and --repo_env. */
-  public Map<String, String> getRepoEnv() {
-    return Collections.unmodifiableMap(repoEnv);
   }
 }
