9de4d32e269a47c181300fd28b4ab5c64c0bb4fb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:44:50 +0200

    use LM instead of LA for ARPACK 'which' parameter

--- dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/math/matrix/Matrix.java
+++ dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/math/matrix/Matrix.java
@@ -308,8 +308,9 @@ public abstract class Matrix implements Serializable {
      */
     public String toString(boolean full) {
         StringBuilder sb = new StringBuilder();
-        int m = full ? nrows() : Math.min(7, nrows());
-        int n = full ? ncols() : Math.min(7, ncols());
+        final int fields = 7;
+        int m = Math.min(fields, nrows());
+        int n = Math.min(fields, ncols());
 
         String newline = n < ncols() ? "...\n" : "\n";
 
