0eeff1f9f82b77d2cb68b56a40b8c165cc959f77
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:46:32 +0200

    Add ID to Streams so that DashboardWidgets can reference them in Config Bundles

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bundles/BundleReceipe.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bundles/BundleReceipe.java
@@ -459,7 +459,7 @@ public class BundleReceipe {
         final Map<String, Object> config = dashboardWidget.getConfiguration();
 
         // Replace "stream_id" in config if it's set
-        final String streamReference = (String) config.get("stream_id");
+        final String streamReference = dashboardWidget.getStream();
         if(!isNullOrEmpty(streamReference)) {
             final org.graylog2.plugin.streams.Stream stream = streamsByReferenceId.get(streamReference);
             if(null != stream) {
