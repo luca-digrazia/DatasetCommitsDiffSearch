4b4f1f663fe5437441a35ce4017f2daa9ff39da0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:46:34 +0200

    Add missing explicit charset in StreamListFingerprint#buildFingerprint()

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/streams/StreamListFingerprint.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/streams/StreamListFingerprint.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of Graylog2.
+ * This file is part of Graylog.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.graylog2.streams;
@@ -21,9 +21,11 @@ import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.Ordering;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.graylog2.plugin.streams.Output;
 import org.graylog2.plugin.streams.Stream;
 import org.graylog2.plugin.streams.StreamRule;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Comparator;
 import java.util.List;
@@ -42,15 +44,27 @@ public class StreamListFingerprint {
     private String buildFingerprint(List<Stream> streams) {
         final MessageDigest sha1Digest = DigestUtils.getSha1Digest();
 
+        final StringBuilder sb = new StringBuilder();
         for (Stream stream : Ordering.from(getStreamComparator()).sortedCopy(streams)) {
-            sha1Digest.update(stream.getId().getBytes());
+            sb.append(stream.hashCode());
 
             for (StreamRule rule : Ordering.from(getStreamRuleComparator()).sortedCopy(stream.getStreamRules())) {
-                sha1Digest.update(rule.getId().getBytes());
+                sb.append(rule.hashCode());
+            }
+            for (Output output : Ordering.from(getOutputComparator()).sortedCopy(stream.getOutputs())) {
+                sb.append(output.hashCode());
             }
         }
+        return String.valueOf(Hex.encodeHex(sha1Digest.digest(sb.toString().getBytes(StandardCharsets.US_ASCII))));
+    }
 
-        return new String(Hex.encodeHex(sha1Digest.digest()));
+    private Comparator<Output> getOutputComparator() {
+        return new Comparator<Output>() {
+            @Override
+            public int compare(Output output1, Output stream2) {
+                return comparisonResult(output1.getId(), stream2.getId());
+            }
+        };
     }
 
     private Comparator<Stream> getStreamComparator() {
