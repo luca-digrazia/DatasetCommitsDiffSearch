fdea91e861872a176dcef4a3cc83f561b3a6073d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:36:56 +0200

    Remove old ProcessingStatus index (#6389)
        This is needed, because mongodb >= 4.2 won't allow
        the creation of identical indixes with a different name.
    
        Fixes #6383

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/system/processing/DBProcessingStatusService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/system/processing/DBProcessingStatusService.java
@@ -20,12 +20,10 @@ import com.github.joschi.jadconfig.util.Duration;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.mongodb.BasicDBObject;
-import com.mongodb.MongoException;
 import org.bson.types.ObjectId;
 import org.graylog.scheduler.clock.JobSchedulerClock;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.MongoConnection;
-import org.graylog2.plugin.BaseConfiguration;
 import org.graylog2.plugin.system.NodeId;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -36,7 +34,6 @@ import org.mongojack.JacksonDBCollection;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -49,14 +46,12 @@ public class DBProcessingStatusService {
     public static final String COLLECTION_NAME = "processing_status";
     private static final String FIELD_WRITTEN_MESSAGES_1M = ProcessingStatusDto.FIELD_INPUT_JOURNAL + "." + ProcessingStatusDto.JournalInfo.FIELD_WRITTEN_MESSAGES_1M_RATE;
     private static final String FIELD_UNCOMMITTED_ENTRIES = ProcessingStatusDto.FIELD_INPUT_JOURNAL + "." + ProcessingStatusDto.JournalInfo.FIELD_UNCOMMITTED_ENTRIES;
-    private static final String FIELD_JOURNAL_ENABLED = ProcessingStatusDto.FIELD_INPUT_JOURNAL + "." + ProcessingStatusDto.JournalInfo.FIELD_JOURNAL_ENABLED;
 
     private final String nodeId;
     private final JobSchedulerClock clock;
     private final Duration updateThreshold;
     private final double journalWriteRateThreshold;
     private final JacksonDBCollection<ProcessingStatusDto, ObjectId> db;
-    private final BaseConfiguration baseConfiguration;
 
     @Inject
     public DBProcessingStatusService(MongoConnection mongoConnection,
@@ -64,33 +59,17 @@ public class DBProcessingStatusService {
                                      JobSchedulerClock clock,
                                      @Named(ProcessingStatusConfig.UPDATE_THRESHOLD) Duration updateThreshold,
                                      @Named(ProcessingStatusConfig.JOURNAL_WRITE_RATE_THRESHOLD) int journalWriteRateThreshold,
-                                     MongoJackObjectMapperProvider mapper,
-                                     BaseConfiguration baseConfiguration) {
+                                     MongoJackObjectMapperProvider mapper) {
         this.nodeId = nodeId.toString();
         this.clock = clock;
         this.updateThreshold = updateThreshold;
         this.journalWriteRateThreshold = ((Number) journalWriteRateThreshold).doubleValue();
-        this.baseConfiguration = baseConfiguration;
         this.db = JacksonDBCollection.wrap(mongoConnection.getDatabase().getCollection(COLLECTION_NAME),
                 ProcessingStatusDto.class,
                 ObjectId.class,
                 mapper.get());
 
         db.createIndex(new BasicDBObject(ProcessingStatusDto.FIELD_NODE_ID, 1), new BasicDBObject("unique", true));
-
-        // Remove the old (3.1.0) index before creating the new one. This is needed, because mongodb >= 4.2 won't allow
-        // the creation of identical indices with a different name. We don't use a migration,
-        // because it can race with the code below that creates the same index with a shorter name.
-        // TODO remove this in a future release (maybe at 3.5)
-        final String OLD_INDEX_NAME = "updated_at_1_input_journal.uncommitted_entries_1_input_journal.written_messages_1m_rate_1";
-        try {
-            if (db.getIndexInfo().stream().anyMatch(dbo -> dbo.get("name").equals(OLD_INDEX_NAME))) {
-                db.dropIndex(OLD_INDEX_NAME);
-            }
-        } catch (MongoException ignored) {
-            // index was either never created or already deleted
-        }
-
         // Use a custom index name to avoid the automatically generated index name which will be pretty long and
         // might cause errors due to the 127 character index name limit. (e.g. when using a long database name)
         // See: https://github.com/Graylog2/graylog2-server/issues/6322
@@ -128,24 +107,17 @@ public class DBProcessingStatusService {
      */
     public Optional<DateTime> earliestPostIndexingTimestamp() {
         final String sortField = ProcessingStatusDto.FIELD_RECEIVE_TIMES + "." + ProcessingStatusDto.ReceiveTimes.FIELD_POST_INDEXING;
-        final DateTime updateThresholdTimestamp = clock.nowUTC().minus(updateThreshold.toMilliseconds());
-        final DBQuery.Query queryWithoutMetrics = DBQuery.greaterThan(FIELD_UPDATED_AT, updateThresholdTimestamp);
-        final DBQuery.Query queryWithMetrics = getDataSelectionQuery(clock, updateThreshold, journalWriteRateThreshold);
+        final DBQuery.Query query = getDataSelectionQuery(clock, updateThreshold, journalWriteRateThreshold);
 
-        // First try to query processing status from nodes that are active (include journal metrics restrictions).
-        // If no result is found, query the processing status again, but without weeding out nodes with a low input volume.
-        // This prevents to completely stall the event processing if the ingestion volume is too low.
-        for (DBQuery.Query query: Arrays.asList(queryWithMetrics, queryWithoutMetrics)) {
-            // Get the earliest timestamp of the post-indexing receive timestamp by sorting and returning the first one.
-            // We use the earliest timestamp because some nodes can be faster than others and we need to make sure
-            // to return the timestamp of the slowest one.
-            try (DBCursor<ProcessingStatusDto> cursor = db.find(query).sort(DBSort.asc(sortField)).limit(1)) {
-                if (cursor.hasNext()) {
-                    return Optional.of(cursor.next().receiveTimes().postIndexing());
-                }
+        // Get the earliest timestamp of the post-indexing receive timestamp by sorting and returning the first one.
+        // We use the earliest timestamp because some nodes can be faster than others and we need to make sure
+        // to return the timestamp of the slowest one.
+        try (DBCursor<ProcessingStatusDto> cursor = db.find(query).sort(DBSort.asc(sortField)).limit(1)) {
+            if (cursor.hasNext()) {
+                return Optional.of(cursor.next().receiveTimes().postIndexing());
             }
+            return Optional.empty();
         }
-        return Optional.empty();
     }
 
     // This has been put into a static method to simplify testing the processing status selection
@@ -162,9 +134,7 @@ public class DBProcessingStatusService {
                         // ... received a certain amount of messages in the last minute
                         DBQuery.greaterThanEquals(FIELD_WRITTEN_MESSAGES_1M, journalWriteRateThreshold),
                         // ... or has messages left in the journal
-                        DBQuery.greaterThanEquals(FIELD_UNCOMMITTED_ENTRIES, 1L),
-                        // ... or has journaling disabled
-                        DBQuery.is(FIELD_JOURNAL_ENABLED, false)
+                        DBQuery.greaterThanEquals(FIELD_UNCOMMITTED_ENTRIES, 1L)
                 )
         );
     }
@@ -190,7 +160,7 @@ public class DBProcessingStatusService {
                 null,
                 null,
                 false,
-                ProcessingStatusDto.of(nodeId, processingStatusRecorder, updatedAt, baseConfiguration.isMessageJournalEnabled()),
+                ProcessingStatusDto.of(nodeId, processingStatusRecorder, updatedAt),
                 true, // We want to return the updated document to the caller
                 true);
     }
