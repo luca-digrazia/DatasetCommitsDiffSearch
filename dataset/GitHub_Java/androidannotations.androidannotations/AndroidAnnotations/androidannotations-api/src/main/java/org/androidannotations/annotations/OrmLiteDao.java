8db8b112fe19dd2e9faa092f537ce671cbd8fad8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:51:17 +0200

    remove model parameter from @OrmLiteDao

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/OrmLiteDao.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/OrmLiteDao.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2013 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -21,14 +21,52 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Injects an OrmLite Dao, configured with the provided mode and helper classes
+ * <p>
+ * Use it on fields of any enhanced class to injects an OrmLite Dao, configured
+ * with the provided mode and helper classes.
+ * </p>
+ * <p>
+ * The helper paramter is mandatory and should hold the class of your database
+ * helper which should extend
+ * com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper
+ * </p>
+ * <p>
+ * <b>Note:</b> The minimum version required of ORMLite is 4.21
+ * </p>
+ * <p>
+ * <b>Note:</b> For getting and releasing the helper, we use the <a href=
+ * "http://ormlite.com/javadoc/ormlite-android/com/j256/ormlite/android/apptools/OpenHelperManager.html"
+ * >OpenHelperManager</a> class, which cannot handle two different helpers at
+ * the same time. So if you are using multiple database helpers, be careful with
+ * {@link OrmLiteDao} annotations.
+ * </p>
+ * <blockquote>
+ * 
+ * Example :
+ * 
+ * <pre>
+ * &#064;EActivity
+ * public class MyActivity extends Activity {
+ * 
+ * 	// UserDao is a Dao&lt;User, Long&gt;
+ * 	&#064;OrmLiteDao(helper = DatabaseHelper.class)
+ * 	UserDao userDao;
+ * 
+ * 	&#064;OrmLiteDao(helper = DatabaseHelper.class)
+ * 	Dao&lt;Car, Long&gt; carDao;
+ * }
+ * </pre>
+ * 
+ * </blockquote>
  */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.FIELD)
 public @interface OrmLiteDao {
 
+	/**
+	 * The class of the used database helper.
+	 * 
+	 * @return the helper class
+	 */
 	Class<?> helper();
-
-	Class<?> model();
-
 }
