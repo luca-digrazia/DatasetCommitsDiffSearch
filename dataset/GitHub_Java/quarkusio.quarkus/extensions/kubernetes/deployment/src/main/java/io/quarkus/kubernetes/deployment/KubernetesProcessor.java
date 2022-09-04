51a2eee9890e89370cb3e011501b04f38cf69502
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:34:45 +0200

    feat: Kubernetes extension now allows configuration via application.properties.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesProcessor.java
@@ -1,9 +1,11 @@
 package io.quarkus.kubernetes.deployment;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -20,22 +22,16 @@ import org.eclipse.microprofile.config.ConfigProvider;
 
 import io.dekorate.Session;
 import io.dekorate.SessionWriter;
-import io.dekorate.kubernetes.config.PortBuilder;
-import io.dekorate.kubernetes.config.ProbeBuilder;
-import io.dekorate.kubernetes.configurator.AddPort;
-import io.dekorate.kubernetes.decorator.AddLivenessProbeDecorator;
-import io.dekorate.kubernetes.decorator.AddReadinessProbeDecorator;
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
+import io.dekorate.kubernetes.generator.DefaultKubernetesApplicationGenerator;
 import io.dekorate.processor.SimpleFileWriter;
-import io.dekorate.project.BuildInfo;
 import io.dekorate.project.FileProjectFactory;
 import io.dekorate.project.Project;
 import io.dekorate.utils.Maps;
-import io.dekorate.utils.Strings;
 import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.ApplicationInfoBuildItem;
-import io.quarkus.deployment.builditem.ArchiveRootBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.GeneratedFileSystemResourceBuildItem;
 import io.quarkus.kubernetes.spi.KubernetesHealthLivenessPathBuildItem;
@@ -44,8 +40,7 @@ import io.quarkus.kubernetes.spi.KubernetesPortBuildItem;
 
 class KubernetesProcessor {
 
-    private static final String PROPERTY_PREFIX = "dekorate.";
-    private static final String ALLOWED_GENERATOR = "kubernetes";
+    private static final String PROPERTY_PREFIX = "dekorate";
 
     @Inject
     BuildProducer<GeneratedFileSystemResourceBuildItem> generatedResourceProducer;
@@ -55,7 +50,7 @@ class KubernetesProcessor {
 
     @BuildStep(onlyIf = IsNormal.class)
     public void build(ApplicationInfoBuildItem applicationInfo,
-            ArchiveRootBuildItem archiveRootBuildItem,
+            KubernetesConfig kubernetesConfig,
             List<KubernetesPortBuildItem> kubernetesPortBuildItems,
             Optional<KubernetesHealthLivenessPathBuildItem> kubernetesHealthLivenessPathBuildItem,
             Optional<KubernetesHealthReadinessPathBuildItem> kubernetesHealthReadinessPathBuildItem)
@@ -79,19 +74,22 @@ class KubernetesProcessor {
 
         Config config = ConfigProvider.getConfig();
         Map<String, Object> configAsMap = StreamSupport.stream(config.getPropertyNames().spliterator(), false)
-                .filter(k -> ALLOWED_GENERATOR.equals(generatorName(k)))
-                .collect(Collectors.toMap(k -> PROPERTY_PREFIX + k, k -> config.getValue(k, String.class)));
+                .filter(k -> k.startsWith(PROPERTY_PREFIX))
+                .collect(Collectors.toMap(k -> k, k -> config.getValue(k, String.class)));
 
         final SessionWriter sessionWriter = new SimpleFileWriter(root, false);
-        Project project = createProject(applicationInfo, archiveRootBuildItem);
+        Project project = FileProjectFactory.create(new File("."));
         sessionWriter.setProject(project);
         final Session session = Session.getSession();
         session.setWriter(sessionWriter);
         session.feed(Maps.fromProperties(configAsMap));
 
-        //Apply build item configurations to the dekorate session.
-        applyBuildItems(session, applicationInfo, kubernetesPortBuildItems, kubernetesHealthLivenessPathBuildItem,
-                kubernetesHealthReadinessPathBuildItem);
+        final Map<String, Integer> ports = verifyPorts(kubernetesPortBuildItems);
+        enableKubernetes(applicationInfo,
+                kubernetesConfig,
+                ports,
+                kubernetesHealthLivenessPathBuildItem.map(i -> i.getPath()),
+                kubernetesHealthReadinessPathBuildItem.map(i -> i.getPath()));
 
         // write the generated resources to the filesystem
         final Map<String, String> generatedResourcesMap = session.close();
@@ -108,30 +106,6 @@ class KubernetesProcessor {
         }
 
         featureProducer.produce(new FeatureBuildItem(FeatureBuildItem.KUBERNETES));
-    }
-
-    private void applyBuildItems(Session session, ApplicationInfoBuildItem applicationInfo,
-            List<KubernetesPortBuildItem> kubernetesPortBuildItems,
-            Optional<KubernetesHealthLivenessPathBuildItem> kubernetesHealthLivenessPathBuildItem,
-            Optional<KubernetesHealthReadinessPathBuildItem> kubernetesHealthReadinessPathBuildItem) {
-
-        //Handle ports
-        final Map<String, Integer> ports = verifyPorts(kubernetesPortBuildItems);
-        ports.entrySet().stream()
-                .map(e -> new PortBuilder().withName(e.getKey()).withContainerPort(e.getValue()).build())
-                .forEach(p -> session.configurators().add(new AddPort(p)));
-
-        //Handle probes
-        kubernetesHealthLivenessPathBuildItem
-                .ifPresent(l -> session.resources()
-                        .decorate(new AddLivenessProbeDecorator(applicationInfo.getName(), new ProbeBuilder()
-                                .withHttpActionPath(l.getPath())
-                                .build())));
-        kubernetesHealthReadinessPathBuildItem
-                .ifPresent(r -> session.resources()
-                        .decorate(new AddReadinessProbeDecorator(applicationInfo.getName(), new ProbeBuilder()
-                                .withHttpActionPath(r.getPath())
-                                .build())));
     }
 
     private Map<String, Integer> verifyPorts(List<KubernetesPortBuildItem> kubernetesPortBuildItems) {
@@ -154,27 +128,38 @@ class KubernetesProcessor {
         return result;
     }
 
-    private Project createProject(ApplicationInfoBuildItem app, ArchiveRootBuildItem archiveRootBuildItem) {
-        //Let dekorate create a Project instance and then override with what is found in ApplicationInfoBuildItem.
-        Project project = FileProjectFactory.create(archiveRootBuildItem.getArchiveLocation().toFile());
-        BuildInfo buildInfo = new BuildInfo(app.getName(), app.getVersion(),
-                "jar", project.getBuildInfo().getOutputFile(),
-                project.getBuildInfo().getClassOutputDir());
+    private void enableKubernetes(ApplicationInfoBuildItem applicationInfo, KubernetesConfig kubernetesConfig,
+            Map<String, Integer> portsMap, Optional<String> httpLivenessProbePath,
+            Optional<String> httpReadinessProbePath) {
+        final Map<String, Object> kubernetesProperties = new HashMap<>();
+        kubernetesProperties.put("group", kubernetesConfig.group);
+        kubernetesProperties.put("name", applicationInfo.getName());
+        kubernetesProperties.put("version", applicationInfo.getVersion());
 
-        return new Project(project.getRoot(), buildInfo, project.getScmInfo());
-    }
-
-    /**
-     * Returns the name of the generators that can handle the specified key.
-     * 
-     * @param key The key.
-     * @return The generator name or null if the key format is unexpected.
-     */
-    private static String generatorName(String key) {
-        if (Strings.isNullOrEmpty(key) || !key.contains(".")) {
-            return null;
+        final List<Map<String, Object>> ports = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : portsMap.entrySet()) {
+            final Map<String, Object> portProperties = new HashMap<>();
+            portProperties.put("name", entry.getKey());
+            portProperties.put("containerPort", entry.getValue());
+            ports.add(portProperties);
         }
-        return key.substring(0, key.indexOf("."));
+
+        kubernetesProperties.put("ports", toArray(ports));
+        if (httpLivenessProbePath.isPresent()) {
+            Map<String, Object> livenessProbeProperties = new HashMap<>();
+            livenessProbeProperties.put("httpActionPath", httpLivenessProbePath.get());
+            kubernetesProperties.put("livenessProbe", livenessProbeProperties);
+        }
+        if (httpReadinessProbePath.isPresent()) {
+            Map<String, Object> readinessProbeProperties = new HashMap<>();
+            readinessProbeProperties.put("httpActionPath", httpReadinessProbePath.get());
+            kubernetesProperties.put("readinessProbe", readinessProbeProperties);
+        }
+
+        final DefaultKubernetesApplicationGenerator generator = new DefaultKubernetesApplicationGenerator();
+        final Map<String, Object> generatorInput = new HashMap<>();
+        generatorInput.put(KubernetesApplication.class.getName(), kubernetesProperties);
+        generator.add(generatorInput);
     }
 
     private <T> T[] toArray(List<T> list) {
