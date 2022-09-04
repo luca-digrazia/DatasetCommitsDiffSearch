7f156f7d4226d75d99cfdd128e572956673c1a56
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:17:42 +0200

    Removing comment, supporting swagger, removing unused import.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-radio/src/main/java/org/graylog2/radio/bindings/RadioBindings.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-radio/src/main/java/org/graylog2/radio/bindings/RadioBindings.java
@@ -30,8 +30,9 @@ import org.graylog2.radio.Configuration;
 import org.graylog2.radio.bindings.providers.AsyncHttpClientProvider;
 import org.graylog2.radio.bindings.providers.RadioTransportProvider;
 import org.graylog2.radio.buffers.processors.RadioProcessBufferProcessor;
-import org.graylog2.radio.inputs.InputStateListener;
+import org.graylog2.shared.inputs.InputStateListener;
 import org.graylog2.radio.inputs.PersistedInputsImpl;
+import org.graylog2.radio.security.RadioSecurityContextFactory;
 import org.graylog2.radio.system.activities.NullActivityWriter;
 import org.graylog2.radio.transports.RadioTransport;
 import org.graylog2.radio.users.NullUserServiceImpl;
@@ -63,24 +64,17 @@ public class RadioBindings extends AbstractModule {
         bindSingletons();
         bindTransport();
         bind(ProcessBufferProcessor.class).to(RadioProcessBufferProcessor.class);
-        SecurityContextFactory instance = null;
-        bind(SecurityContextFactory.class).toProvider(Providers.of(instance));
         bindDynamicFeatures();
         bindContainerResponseFilters();
         bindExceptionMappers();
-        bindAdditionalJerseyComponents();
         bindInterfaces();
-        bindEventBusListeners();
     }
 
     private void bindInterfaces() {
         bind(ActivityWriter.class).to(NullActivityWriter.class);
         bind(PersistedInputs.class).to(PersistedInputsImpl.class);
         bind(UserService.class).to(NullUserServiceImpl.class);
-    }
-
-    private void bindEventBusListeners() {
-        bind(InputStateListener.class).asEagerSingleton();
+        bind(SecurityContextFactory.class).to(RadioSecurityContextFactory.class);
     }
 
     private void bindSingletons() {
@@ -125,9 +119,5 @@ public class RadioBindings extends AbstractModule {
     private void bindExceptionMappers() {
         TypeLiteral<Class<? extends ExceptionMapper>> type = new TypeLiteral<Class<? extends ExceptionMapper>>(){};
         Multibinder<Class<? extends ExceptionMapper>> setBinder = Multibinder.newSetBinder(binder(), type);
-    }
-
-    private void bindAdditionalJerseyComponents() {
-        Multibinder<Class> componentBinder = Multibinder.newSetBinder(binder(), Class.class, Names.named("additionalJerseyComponents"));
     }
 }
