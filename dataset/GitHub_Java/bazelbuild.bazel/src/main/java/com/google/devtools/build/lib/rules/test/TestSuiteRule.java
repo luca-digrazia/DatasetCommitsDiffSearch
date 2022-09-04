62986a80e09f5a2574d81cede63dff374dacf2f1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:03:25 +0200

    Use `/versions/main` rather than `/versions/master` for the link to Bazel documentation.
        See commit `b2dd6fb` for context.
    
        PiperOrigin-RevId: 378784115

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/test/TestSuiteRule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/test/TestSuiteRule.java
@@ -21,6 +21,7 @@ import com.google.devtools.build.lib.analysis.BaseRuleClasses;
 import com.google.devtools.build.lib.analysis.RuleDefinition;
 import com.google.devtools.build.lib.analysis.RuleDefinitionEnvironment;
 import com.google.devtools.build.lib.analysis.test.TestConfiguration;
+import com.google.devtools.build.lib.packages.ConfigurationFragmentPolicy.MissingFragmentPolicy;
 import com.google.devtools.build.lib.packages.RuleClass;
 
 /** Rule object implementing "test_suite". */
@@ -29,8 +30,10 @@ public final class TestSuiteRule implements RuleDefinition {
   public RuleClass build(RuleClass.Builder builder, RuleDefinitionEnvironment env) {
     return builder
         // Technically, test_suite does not use TestConfiguration. But the tests it depends on
-        // will always depend on TestConfiguration, so requiring it here simply acknowledges that.
+        // will always depend on TestConfiguration, so requiring it here simply acknowledges that
+        // and prevents pruning by --trim_test_configuration.
         .requiresConfigurationFragments(TestConfiguration.class)
+        .setMissingFragmentPolicy(TestConfiguration.class, MissingFragmentPolicy.IGNORE)
         .override(
             attr("testonly", BOOLEAN)
                 .value(true)
@@ -55,11 +58,13 @@ public final class TestSuiteRule implements RuleDefinition {
           tests still need to be legal (e.g. not blocked by visibility constraints).
         </p>
         <p>
-          The <code>manual</code> tag keyword is treated specially. It marks the
-          <code>test_suite</code> target as "manual" so that it will be ignored by the wildcard
-          expansion and automated testing facilities. It does not work as a filter on the set
-          of tests in the suite. So when the <code>manual</code> tag is used on a test_suite, test
-          rules do not have to be tagged as <code>manual</code> to be included in the test suite.
+          The <code>manual</code> tag keyword is treated differently than the above by the
+          "test_suite expansion" performed by the <code>blaze test</code> command on invocations
+          involving wildcard
+          <a href="https://docs.bazel.build/versions/main/guide.html#specifying-targets-to-build">target patterns</a>.
+          There, <code>test_suite</code> targets tagged "manual" are filtered out (and thus not
+          expanded). This behavior is consistent with how <code>blaze build</code> and
+          <code>blaze test</code> handle wildcard target patterns in general.
         </p>
         <p>
           Note that a test's <code>size</code> is considered a tag for the purpose of filtering.
@@ -106,18 +111,21 @@ public final class TestSuiteRule implements RuleDefinition {
   public Metadata getMetadata() {
     return RuleDefinition.Metadata.builder()
         .name("test_suite")
-        .ancestors(BaseRuleClasses.BaseRule.class)
+        .ancestors(BaseRuleClasses.NativeBuildRule.class)
         .factoryClass(TestSuite.class)
         .build();
   }
 }
 
-/*<!-- #BLAZE_RULE (NAME = test_suite, TYPE = TEST, FAMILY = General)[GENERIC_RULE] -->
+/*<!-- #BLAZE_RULE (NAME = test_suite, FAMILY = General)[GENERIC_RULE] -->
 
 <p>
 A <code>test_suite</code> defines a set of tests that are considered "useful" to humans. This
 allows projects to define sets of tests, such as "tests you must run before checkin", "our
-project's stress tests" or "all small tests."
+project's stress tests" or "all small tests." The <code>blaze test</code> command respects this sort
+of organization: For an invocation like <code>blaze test //some/test:suite</code>, Blaze first
+enumerates all test targets transitively included by the <code>//some/test:suite</code> target (we
+call this "test_suite expansion"), then Blaze builds and tests those targets.
 </p>
 
 <h4 id="test_suite_examples">Examples</h4>
