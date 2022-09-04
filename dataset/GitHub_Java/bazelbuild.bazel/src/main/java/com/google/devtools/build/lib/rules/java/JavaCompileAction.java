b2c866726d0741389989ef3017e4b67bfd725bea
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:55:26 +0200

    Fix a crash bug in async Java compilation
        Also add an integration test to avoid this happening again.
    
        Progress on #6394.
    
        PiperOrigin-RevId: 245731141

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaCompileAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaCompileAction.java
@@ -309,6 +309,17 @@ public class JavaCompileAction extends AbstractAction
     return continuation.execute();
   }
 
+  // TODO(b/119813262): Move this method to AbstractAction.
+  @Override
+  public ActionResult execute(ActionExecutionContext actionExecutionContext)
+      throws ActionExecutionException, InterruptedException {
+    ActionContinuationOrResult continuation = beginExecution(actionExecutionContext);
+    while (!continuation.isDone()) {
+      continuation = continuation.execute();
+    }
+    return continuation.get();
+  }
+
   @Override
   protected String getRawProgressMessage() {
     StringBuilder sb = new StringBuilder("Building ");
