1ec8e13bb3716f94aff360266d71f871a0ac938b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:14:04 +0200

    Add support for PATCH request to Jersey test client (#2288)
        Closes #2287

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/test/java/io/dropwizard/testing/junit/DropwizardAppRuleTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/test/java/io/dropwizard/testing/junit/DropwizardAppRuleTest.java
@@ -3,6 +3,7 @@ package io.dropwizard.testing.junit;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.testing.app.DropwizardTestApplication;
 import io.dropwizard.testing.app.TestConfiguration;
+import org.assertj.core.api.Assertions;
 import org.junit.ClassRule;
 import org.junit.Test;
 
@@ -11,73 +12,75 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 
 import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 public class DropwizardAppRuleTest {
-    @SuppressWarnings("deprecation")
+
     @ClassRule
     public static final DropwizardAppRule<TestConfiguration> RULE =
         new DropwizardAppRule<>(DropwizardTestApplication.class, resourceFilePath("test-config.yaml"));
 
     @Test
-    void canGetExpectedResourceOverHttp() {
+    public void canGetExpectedResourceOverHttp() {
         final String content = ClientBuilder.newClient().target(
             "http://localhost:" + RULE.getLocalPort() + "/test").request().get(String.class);
 
-        assertThat(content).isEqualTo("Yes, it's here");
+        assertThat(content, is("Yes, it's here"));
     }
 
     @Test
-    void returnsConfiguration() {
+    public void returnsConfiguration() {
         final TestConfiguration config = RULE.getConfiguration();
-        assertThat(config.getMessage()).isEqualTo("Yes, it's here");
+        assertThat(config.getMessage(), is("Yes, it's here"));
     }
 
     @Test
-    void returnsApplication() {
+    public void returnsApplication() {
         final DropwizardTestApplication application = RULE.getApplication();
-        assertThat(application).isNotNull();
+        assertNotNull(application);
     }
 
     @Test
-    void returnsEnvironment() {
+    public void returnsEnvironment() {
         final Environment environment = RULE.getEnvironment();
-        assertThat(environment.getName()).isEqualTo("DropwizardTestApplication");
+        assertThat(environment.getName(), is("DropwizardTestApplication"));
     }
 
     @Test
-    void canPerformAdminTask() {
+    public void canPerformAdminTask() {
         final String response
             = RULE.client().target("http://localhost:"
             + RULE.getAdminPort() + "/tasks/hello?name=test_user")
             .request()
             .post(Entity.entity("", MediaType.TEXT_PLAIN), String.class);
 
-        assertThat(response).isEqualTo("Hello has been said to test_user");
+        assertThat(response, is("Hello has been said to test_user"));
     }
 
     @Test
-    void canPerformAdminTaskWithPostBody() {
+    public void canPerformAdminTaskWithPostBody() {
         final String response
             = RULE.client().target("http://localhost:"
             + RULE.getAdminPort() + "/tasks/echo")
             .request()
             .post(Entity.entity("Custom message", MediaType.TEXT_PLAIN), String.class);
 
-        assertThat(response).isEqualTo("Custom message");
+        assertThat(response, is("Custom message"));
     }
 
     @Test
-    void clientUsesJacksonMapperFromEnvironment() {
-        assertThat(RULE.client().target("http://localhost:" + RULE.getLocalPort() + "/message")
+    public void clientUsesJacksonMapperFromEnvironment() {
+        Assertions.assertThat(RULE.client().target("http://localhost:" + RULE.getLocalPort() + "/message")
             .request()
             .get(DropwizardTestApplication.MessageView.class).getMessage())
             .contains("Yes, it's here");
     }
 
     @Test
-    void clientSupportsPatchMethod() {
-        assertThat(RULE.client().target("http://localhost:" + RULE.getLocalPort() + "/echoPatch")
+    public void clientSupportsPatchMethod() {
+        Assertions.assertThat(RULE.client().target("http://localhost:" + RULE.getLocalPort() + "/echoPatch")
             .request()
             .method("PATCH", Entity.text("Patch is working"), String.class))
             .contains("Patch is working");
