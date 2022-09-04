880cc676106b182ba522f23e8ed8e72a7add90e4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:12:54 +0200

    Add test for RoleImpl#equals()

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/users/RoleImpl.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/users/RoleImpl.java
@@ -1,14 +1,33 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.users;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.graylog2.shared.users.Role;
 
+import javax.annotation.Nullable;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Set;
 
-public class RoleImpl implements Role {
+public final class RoleImpl implements Role {
     @Id
     @org.mongojack.ObjectId
     @JsonProperty("id")
@@ -20,9 +39,15 @@ public class RoleImpl implements Role {
     @NotNull
     public Set<String> permissions;
 
+    @Nullable
+    private String description;
+
+    // readOnly is never set from the outside, except for the two built-in roles "Admin" and "Reader"
+    private boolean readOnly = false;
+
     @JsonProperty
     public String nameLower() {
-        return name.toLowerCase();
+        return name.toLowerCase(Locale.ENGLISH);
     }
 
     @JsonProperty
@@ -47,6 +72,18 @@ public class RoleImpl implements Role {
     }
 
     @Override
+    @JsonProperty
+    @Nullable
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void setDescription(@Nullable String description) {
+        this.description = description;
+    }
+
+    @Override
     public Set<String> getPermissions() {
         return permissions;
     }
@@ -54,5 +91,32 @@ public class RoleImpl implements Role {
     @Override
     public void setPermissions(Set<String> permissions) {
         this.permissions = permissions;
+    }
+
+    @Override
+    @JsonProperty
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    public void setReadOnly(boolean readOnly) {
+        this.readOnly = readOnly;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleImpl role = (RoleImpl) o;
+        return readOnly == role.readOnly &&
+                Objects.equals(_id, role._id) &&
+                Objects.equals(name, role.name) &&
+                Objects.equals(permissions, role.permissions) &&
+                Objects.equals(description, role.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_id, name, permissions, description, readOnly);
     }
 }
