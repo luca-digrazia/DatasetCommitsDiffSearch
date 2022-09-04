7816ff51eb29365955d01e1d1a2511bd1bcdbe51
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:37:13 +0200

    Instead of IOException which has to be handled by the developer, an unchecked exception is sufficient and makes it easier to statically create fixtures used in more than one test case.
        Also, the existing documentation was wrong, when the resource does not exist, fixture() threw an IllegalArgumentException (Preconditions.checkArgument) not an IOException as stated in the javadoc.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/io/dropwizard/testing/FixtureHelpers.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/io/dropwizard/testing/FixtureHelpers.java
@@ -13,31 +13,31 @@ public class FixtureHelpers {
     private FixtureHelpers() { /* singleton */ }
 
     /**
-     * Reads the given fixture file from the classpath (e. g. {@code src/test/resources})
-     * and returns its contents as a UTF-8 string.
+     * Reads the given fixture file from {@code src/test/resources} and returns its contents as a
+     * UTF-8 string.
      *
-     * @param filename the filename of the fixture file
+     * @param filename    the filename of the fixture file
      * @return the contents of {@code src/test/resources/{filename}}
-     * @throws IllegalArgumentException if an I/O error occurs.
+     * @throws IllegalStateException if {@code filename} doesn't exist or can't be opened
      */
     public static String fixture(String filename) {
         return fixture(filename, Charsets.UTF_8);
     }
 
     /**
-     * Reads the given fixture file from the classpath (e. g. {@code src/test/resources})
-     * and returns its contents as a string.
+     * Reads the given fixture file from {@code src/test/resources} and returns its contents as a
+     * string.
      *
-     * @param filename the filename of the fixture file
-     * @param charset  the character set of {@code filename}
+     * @param filename    the filename of the fixture file
+     * @param charset     the character set of {@code filename}
      * @return the contents of {@code src/test/resources/{filename}}
-     * @throws IllegalArgumentException if an I/O error occurs.
+     * @throws IllegalStateException if {@code filename} doesn't exist or can't be opened
      */
     private static String fixture(String filename, Charset charset) {
         try {
             return Resources.toString(Resources.getResource(filename), charset).trim();
-        } catch (IOException e) {
-            throw new IllegalArgumentException(e);
+        } catch (IllegalArgumentException | IOException e) {
+            throw new IllegalStateException(e);
         }
     }
 }
