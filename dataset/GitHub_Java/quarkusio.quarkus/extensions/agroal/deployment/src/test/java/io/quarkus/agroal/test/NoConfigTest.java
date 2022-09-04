7fea18bc0edfd7a52b5e7102ea55ada5bc7015d3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:40:52 +0200

    Agroal - Support having no datasource configured
        Fixes #270.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/agroal/deployment/src/test/java/io/quarkus/agroal/test/NoConfigTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/agroal/deployment/src/test/java/io/quarkus/agroal/test/NoConfigTest.java
@@ -2,6 +2,8 @@ package io.quarkus.agroal.test;
 
 import java.sql.SQLException;
 
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -10,7 +12,8 @@ import io.quarkus.test.QuarkusUnitTest;
 public class NoConfigTest {
 
     @RegisterExtension
-    static final QuarkusUnitTest config = new QuarkusUnitTest();
+    static final QuarkusUnitTest config = new QuarkusUnitTest().setArchiveProducer(
+            () -> ShrinkWrap.create(JavaArchive.class));
 
     @Test
     public void testNoConfig() throws SQLException {
