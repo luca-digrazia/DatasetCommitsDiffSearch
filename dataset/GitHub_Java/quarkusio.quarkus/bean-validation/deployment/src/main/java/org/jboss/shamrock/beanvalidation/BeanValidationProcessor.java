a803567cd538dd90779b5e609674c9fe61a55bef
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:01:12 +0200

    Minor cleanup

--- dataset/GitHub_Java/quarkusio.quarkus/bean-validation/deployment/src/main/java/org/jboss/shamrock/beanvalidation/BeanValidationProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/bean-validation/deployment/src/main/java/org/jboss/shamrock/beanvalidation/BeanValidationProcessor.java
@@ -1,92 +1,183 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.shamrock.beanvalidation;
 
+import static org.jboss.shamrock.annotations.ExecutionTime.STATIC_INIT;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.inject.Inject;
 import javax.validation.Constraint;
-import javax.validation.ConstraintValidator;
+import javax.validation.Valid;
+import javax.validation.executable.ValidateOnExecution;
 
+import org.hibernate.validator.internal.metadata.core.ConstraintHelper;
+import org.hibernate.validator.messageinterpolation.AbstractMessageInterpolator;
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
-import org.jboss.jandex.ParameterizedType;
+import org.jboss.jandex.IndexView;
 import org.jboss.jandex.Type;
+import org.jboss.shamrock.annotations.BuildProducer;
+import org.jboss.shamrock.annotations.BuildStep;
+import org.jboss.shamrock.annotations.Record;
 import org.jboss.shamrock.beanvalidation.runtime.ValidatorProvider;
 import org.jboss.shamrock.beanvalidation.runtime.ValidatorTemplate;
-import org.jboss.shamrock.deployment.ArchiveContext;
-import org.jboss.shamrock.deployment.BeanDeployment;
-import org.jboss.shamrock.deployment.ProcessorContext;
-import org.jboss.shamrock.deployment.ResourceProcessor;
-import org.jboss.shamrock.deployment.RuntimePriority;
-import org.jboss.shamrock.deployment.ShamrockConfig;
-import org.jboss.shamrock.deployment.codegen.BytecodeRecorder;
-import org.jboss.shamrock.runtime.InjectionInstance;
+import org.jboss.shamrock.deployment.builditem.AdditionalBeanBuildItem;
+import org.jboss.shamrock.deployment.builditem.CombinedIndexBuildItem;
+import org.jboss.shamrock.deployment.builditem.HotDeploymentConfigFileBuildItem;
+import org.jboss.shamrock.deployment.builditem.SystemPropertyBuildItem;
+import org.jboss.shamrock.deployment.builditem.substrate.ReflectiveFieldBuildItem;
+import org.jboss.shamrock.deployment.builditem.substrate.ReflectiveMethodBuildItem;
+import org.jboss.shamrock.deployment.builditem.substrate.SubstrateConfigBuildItem;
+import org.jboss.shamrock.deployment.recording.RecorderContext;
 
-class BeanValidationProcessor implements ResourceProcessor {
+class BeanValidationProcessor {
 
-    private static final DotName CONSTRAINT_VALIDATOR = DotName.createSimple(ConstraintValidator.class.getName());
-    @Inject
-    private BeanDeployment beanDeployment;
+    private static final DotName VALIDATE_ON_EXECUTION = DotName.createSimple(ValidateOnExecution.class.getName());
 
-    @Inject
-    private ShamrockConfig config;
+    private static final DotName VALID = DotName.createSimple(Valid.class.getName());
 
-    @Override
-    public void process(ArchiveContext archiveContext, ProcessorContext processorContext) throws Exception {
-        beanDeployment.addAdditionalBean(ValidatorProvider.class);
-        processorContext.addResourceBundle("org.hibernate.validator.ValidationMessages");
-        //TODO: this should not rely on the index and implementation being indexed, this stuff should just be hard coded
-        try (BytecodeRecorder recorder = processorContext.addStaticInitTask(RuntimePriority.BEAN_VALIDATION_DEPLOYMENT)) {
-            ValidatorTemplate template = recorder.getRecordingProxy(ValidatorTemplate.class);
-            template.forceInit((InjectionInstance<ValidatorProvider>) recorder.newInstanceFactory(ValidatorProvider.class.getName()));
+    @BuildStep
+    SystemPropertyBuildItem disableJavaFXIntegrations() {
+        // Bug in GraalVM rc10: see https://github.com/oracle/graal/issues/851
+        return new SystemPropertyBuildItem("org.hibernate.validator.force-disable-javafx-integration", "true");
+    }
+
+    @BuildStep
+    HotDeploymentConfigFileBuildItem configFile() {
+        return new HotDeploymentConfigFileBuildItem("META-INF/validation.xml");
+    }
+
+    @BuildStep
+    AdditionalBeanBuildItem registerBean() {
+        return new AdditionalBeanBuildItem(ValidatorProvider.class);
+    }
+
+    @BuildStep
+    @Record(STATIC_INIT)
+    public void build(ValidatorTemplate template, RecorderContext recorder,
+                      BuildProducer<ReflectiveFieldBuildItem> reflectiveFields,
+                      BuildProducer<ReflectiveMethodBuildItem> reflectiveMethods,
+                      CombinedIndexBuildItem combinedIndexBuildItem) throws Exception {
+
+        IndexView indexView = combinedIndexBuildItem.getIndex();
+
+        Set<DotName> consideredAnnotations = new HashSet<>();
+
+        // Collect the constraint annotations provided by Hibernate Validator and Bean Validation
+        contributeBuiltinConstraints(consideredAnnotations);
+
+        // Add the constraint annotations present in the application itself
+        for (AnnotationInstance constraint : indexView.getAnnotations(DotName.createSimple(Constraint.class.getName()))) {
+            consideredAnnotations.add(constraint.target().asClass().name());
         }
-        processorContext.addReflectiveClass(true, false, Constraint.class.getName());
-        Set<DotName> constraintAnnotations = new HashSet<>();
-        for (AnnotationInstance annotation : archiveContext.getCombinedIndex().getAnnotations(DotName.createSimple(Constraint.class.getName()))) {
-            constraintAnnotations.add(annotation.target().asClass().name());
-            processorContext.addReflectiveClass(true, false, annotation.target().asClass().name().toString());
-        }
-        for (DotName constraint : constraintAnnotations) {
-            for (AnnotationInstance annotation : archiveContext.getCombinedIndex().getAnnotations(constraint)) {
+
+        // Also consider elements that are marked with @Valid
+        consideredAnnotations.add(VALID);
+
+        // Also consider elements that are marked with @ValidateOnExecution
+        consideredAnnotations.add(VALIDATE_ON_EXECUTION);
+
+        Set<Class<?>> classesToBeValidated = new HashSet<>();
+
+        for (DotName consideredAnnotation : consideredAnnotations) {
+            Collection<AnnotationInstance> annotationInstances = indexView.getAnnotations(consideredAnnotation);
+
+            for (AnnotationInstance annotation : annotationInstances) {
                 if (annotation.target().kind() == AnnotationTarget.Kind.FIELD) {
-                    processorContext.addReflectiveField(annotation.target().asField());
+                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asField().declaringClass().name());
+                    reflectiveFields.produce(new ReflectiveFieldBuildItem(annotation.target().asField()));
+                    contributeClassMarkedForCascadingValidation(classesToBeValidated, recorder, indexView, consideredAnnotation, annotation.target().asField().type());
                 } else if (annotation.target().kind() == AnnotationTarget.Kind.METHOD) {
-                    processorContext.addReflectiveMethod(annotation.target().asMethod());
+                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asMethod().declaringClass().name());
+                    // we need to register the method for reflection as it could be a getter
+                    reflectiveMethods.produce(new ReflectiveMethodBuildItem(annotation.target().asMethod()));
+                    contributeClassMarkedForCascadingValidation(classesToBeValidated, recorder, indexView, consideredAnnotation, annotation.target().asMethod().returnType());
                 } else if (annotation.target().kind() == AnnotationTarget.Kind.METHOD_PARAMETER) {
-                    processorContext.addReflectiveMethod(annotation.target().asMethodParameter().method());
+                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asMethodParameter().method().declaringClass().name());
+                    // a getter does not have parameters so it's a pure method: no need for reflection in this case
+                    contributeClassMarkedForCascadingValidation(classesToBeValidated, recorder, indexView, consideredAnnotation,
+                            // FIXME this won't work in the case of synthetic parameters
+                            annotation.target().asMethodParameter().method().parameters().get(annotation.target().asMethodParameter().position()));
                 } else if (annotation.target().kind() == AnnotationTarget.Kind.CLASS) {
-                    processorContext.addReflectiveClass(true, true, annotation.target().asClass().name().toString());
+                    contributeClass(classesToBeValidated, recorder, indexView, annotation.target().asClass().name());
+                    // no need for reflection in the case of a class level constraint
                 }
             }
         }
-        for (ClassInfo classInfo : archiveContext.getCombinedIndex().getAllKnownImplementors(CONSTRAINT_VALIDATOR)) {
-            boolean skip = false;
-            for (Type iface : classInfo.interfaceTypes()) {
-                if (iface.kind() == Type.Kind.PARAMETERIZED_TYPE) {
-                    ParameterizedType pt = iface.asParameterizedType();
-                    if (pt.name().equals(CONSTRAINT_VALIDATOR)) {
-                        if (pt.arguments().size() == 2) {
-                            String type = pt.arguments().get(1).name().toString();
-                            if (type.startsWith("javax.money") ||
-                                    type.startsWith("org.joda")) {
-                                //TODO: what if joda is present?
-                                skip = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            if (!skip) { //such hacks
-                processorContext.addReflectiveClass(false, false, classInfo.name().toString());
-            }
+
+        template.initializeValidatorFactory(classesToBeValidated);
+    }
+
+    @BuildStep
+    SubstrateConfigBuildItem substrateConfig() {
+        return SubstrateConfigBuildItem.builder()
+                .addResourceBundle(AbstractMessageInterpolator.DEFAULT_VALIDATION_MESSAGES)
+                .addResourceBundle(AbstractMessageInterpolator.USER_VALIDATION_MESSAGES)
+                .addResourceBundle(AbstractMessageInterpolator.CONTRIBUTOR_VALIDATION_MESSAGES)
+                .build();
+    }
+
+    private static void contributeBuiltinConstraints(Set<DotName> constraintCollector) {
+        Set<Class<? extends Annotation>> builtinConstraints = new ConstraintHelper().getBuiltinConstraints();
+        for (Class<? extends Annotation> builtinConstraint : builtinConstraints) {
+            constraintCollector.add(DotName.createSimple(builtinConstraint.getName()));
         }
     }
 
-    @Override
-    public int getPriority() {
-        return 1;
+    private static void contributeClass(Set<Class<?>> classCollector, RecorderContext recorder, IndexView indexView, DotName className) {
+        classCollector.add(recorder.classProxy(className.toString()));
+        for (ClassInfo subclass : indexView.getAllKnownSubclasses(className)) {
+            if (Modifier.isAbstract(subclass.flags())) {
+                // we can avoid adding the abstract classes here: either they are parent classes
+                // and they will be dealt with by Hibernate Validator or they are child classes
+                // without any proper implementation and we can ignore them.
+                continue;
+            }
+            classCollector.add(recorder.classProxy(subclass.name().toString()));
+        }
+    }
+
+    private static void contributeClassMarkedForCascadingValidation(Set<Class<?>> classCollector, RecorderContext recorder, IndexView indexView,
+            DotName consideredAnnotation, Type type) {
+        if (VALID != consideredAnnotation) {
+            return;
+        }
+
+        DotName className = getClassName(type);
+        if (className != null) {
+            contributeClass(classCollector, recorder, indexView, className);
+        }
+    }
+
+    private static DotName getClassName(Type type) {
+        switch (type.kind()) {
+        case CLASS:
+        case PARAMETERIZED_TYPE:
+            return type.name();
+        case ARRAY:
+            return getClassName( type.asArrayType().component() );
+        default:
+            return null;
+        }
     }
 }
