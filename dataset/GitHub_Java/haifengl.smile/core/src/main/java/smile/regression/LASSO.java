71c949a97563cc0464bb8ae540e4ca4cf6d4f515
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:36:26 +0200

    init lambda

--- dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/regression/LASSO.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/regression/LASSO.java
@@ -561,7 +561,7 @@ public class LASSO  implements Regression<double[]>, Serializable {
         return sum;
     }
 
-    class PCGMatrix implements Matrix, Preconditioner {
+    class PCGMatrix extends Matrix implements Preconditioner {
 
         Matrix A;
         Matrix AtA;
@@ -578,6 +578,7 @@ public class LASSO  implements Regression<double[]>, Serializable {
             this.d2 = d2;
             this.prb = prb;
             this.prs = prs;
+            setSymmetric(true);
 
             int n = A.nrows();
             ax = new double[n];
@@ -586,11 +587,6 @@ public class LASSO  implements Regression<double[]>, Serializable {
             if ((A.ncols() < 10000) && (A instanceof DenseMatrix)) {
                 AtA = A.ata();
             }
-        }
-
-        @Override
-        public boolean isSymmetric() {
-            return true;
         }
 
         @Override
