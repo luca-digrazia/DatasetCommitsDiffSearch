87dbc596bd55fd003167ea7380814b7a6410d6b6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:30:03 +0200

    Remove stale comments. Earlier in the history of the Bazel codebase, some projects outside Bazel were using package-loading internals directly. Nowadays they use the com.google.devtools.build.lib.skyframe.packages.PackageLoader library.
        RELNOTES: None
        PiperOrigin-RevId: 212884574

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/GlobCache.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/GlobCache.java
@@ -209,7 +209,7 @@ public class GlobCache {
         .setDirectoryFilter(childDirectoryPredicate)
         .setThreadPool(globExecutor)
         .setFilesystemCalls(syscalls)
-        .globAsync();
+        .globAsync(true);
   }
 
   /**
