057e240d43fd4f6d582459c8ff93b80a154a24ac
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:40:19 +0200

    Fixing highlighting for decorated queries. (Graylog2/graylog-plugin-enterprise#1034)
        * Decorating query string for highlighting configuration.
    
        * Adapting tests to constructor parameter.
    
        * Adding test for `ESMessageList`.
    
        * Adding test for `ESMessageList`.

--- dataset/GitHub_Java/Graylog2.graylog2-server/enterprise/src/test/java/org/graylog/plugins/enterprise/search/elasticsearch/ElasticsearchBackendTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/enterprise/src/test/java/org/graylog/plugins/enterprise/search/elasticsearch/ElasticsearchBackendTest.java
@@ -1,27 +1,30 @@
 package org.graylog.plugins.enterprise.search.elasticsearch;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
-import org.graylog.plugins.enterprise.search.Parameter;
 import org.graylog.plugins.enterprise.search.Query;
-import org.graylog.plugins.enterprise.search.QueryInfo;
-import org.graylog.plugins.enterprise.search.QueryParameter;
+import org.graylog.plugins.enterprise.search.QueryMetadata;
 import org.graylog.plugins.enterprise.search.SearchType;
 import org.graylog.plugins.enterprise.search.elasticsearch.searchtypes.ESDateHistogram;
 import org.graylog.plugins.enterprise.search.elasticsearch.searchtypes.ESMessageList;
 import org.graylog.plugins.enterprise.search.elasticsearch.searchtypes.ESSearchTypeHandler;
-import org.graylog.plugins.enterprise.search.params.QueryReferenceBinding;
-import org.graylog.plugins.enterprise.search.params.ValueBinding;
+import org.graylog.plugins.enterprise.search.filter.AndFilter;
+import org.graylog.plugins.enterprise.search.filter.QueryStringFilter;
 import org.graylog.plugins.enterprise.search.searchtypes.DateHistogram;
 import org.graylog.plugins.enterprise.search.searchtypes.MessageList;
+import org.graylog.plugins.enterprise.search.searchtypes.pivot.Pivot;
+import org.graylog2.indexer.ranges.IndexRangeService;
 import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
+import org.graylog2.streams.StreamService;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.inject.Provider;
+import java.util.ArrayList;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class ElasticsearchBackendTest {
 
@@ -30,28 +33,50 @@ public class ElasticsearchBackendTest {
     @BeforeClass
     public static void setup() {
         Map<String, Provider<ESSearchTypeHandler<? extends SearchType>>> handlers = Maps.newHashMap();
-        handlers.put(MessageList.NAME, ESMessageList::new);
+        handlers.put(MessageList.NAME, () -> new ESMessageList(new ESQueryDecorators.Fake()));
         handlers.put(DateHistogram.NAME, ESDateHistogram::new);
-        Map<String, Provider<Parameter.BindingHandler>> bindingHandlers = Maps.newHashMap();
-        bindingHandlers.put(ValueBinding.NAME, ValueBinding.Handler::new);
-        bindingHandlers.put(QueryReferenceBinding.NAME, () -> new QueryReferenceBinding.Handler(new ObjectMapper()));
 
         final QueryStringParser queryStringParser = new QueryStringParser();
-        backend = new ElasticsearchBackend(handlers, bindingHandlers, queryStringParser, null);
+        backend = new ElasticsearchBackend(handlers, queryStringParser, null, mock(IndexRangeService.class), mock(StreamService.class), new ESQueryDecorators.Fake());
     }
 
     @Test
     public void parse() throws Exception {
-        final QueryInfo queryInfo = backend.parse(Query.builder()
+        final QueryMetadata queryMetadata = backend.parse(ImmutableSet.of(), Query.builder()
                 .id("abc123")
                 .query(ElasticsearchQueryString.builder().queryString("user_name:$username$ http_method:$foo$").build())
                 .timerange(RelativeRange.create(600))
                 .build());
 
-        assertThat(queryInfo.parameters())
-                .containsOnly(
-                        Maps.immutableEntry("username", QueryParameter.any("username")),
-                        Maps.immutableEntry("foo", QueryParameter.any("foo")));
+        assertThat(queryMetadata.usedParameterNames())
+                .containsOnly("username", "foo");
     }
 
+    @Test
+    public void parseAlsoConsidersWidgetFilters() throws Exception {
+        final SearchType searchType1 = Pivot.builder()
+                .id("searchType1")
+                .filter(QueryStringFilter.builder().query("source:$bar$").build())
+                .series(new ArrayList<>())
+                .rollup(false)
+                .build();
+        final SearchType searchType2 = Pivot.builder()
+                .id("searchType2")
+                .filter(AndFilter.builder().filters(ImmutableSet.of(
+                        QueryStringFilter.builder().query("http_action:$baz$").build(),
+                        QueryStringFilter.builder().query("source:localhost").build()
+                )).build())
+                .series(new ArrayList<>())
+                .rollup(false)
+                .build();
+        final QueryMetadata queryMetadata = backend.parse(ImmutableSet.of(), Query.builder()
+                .id("abc123")
+                .query(ElasticsearchQueryString.builder().queryString("user_name:$username$ http_method:$foo$").build())
+                .timerange(RelativeRange.create(600))
+                .searchTypes(ImmutableSet.of(searchType1, searchType2))
+                .build());
+
+        assertThat(queryMetadata.usedParameterNames())
+                .containsOnly("username", "foo", "bar", "baz");
+    }
 }
