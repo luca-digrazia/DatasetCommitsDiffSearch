f4388325f18f6118182dada57328ed24a9ffd3cd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:07:51 +0200

    Ensure that Java 12+ interfaces of java.lang.String are as CDI types
        This is needed because when building with Java 12+ but targeting Java 8-11,
        we need to ensure that the extra interfaces that java.lang.String implements
        do not end up in the generated bytecode.
    
        Fixes: #5062

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/Types.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/Types.java
@@ -317,8 +317,7 @@ final class Types {
                     typedClasses.add(type.name());
                 }
                 for (Iterator<Type> iterator = types.iterator(); iterator.hasNext();) {
-                    Type nextType = iterator.next();
-                    if (!typedClasses.contains(nextType.name()) && !DotNames.OBJECT.equals(nextType.name())) {
+                    if (!typedClasses.contains(iterator.next().name())) {
                         iterator.remove();
                     }
                 }
