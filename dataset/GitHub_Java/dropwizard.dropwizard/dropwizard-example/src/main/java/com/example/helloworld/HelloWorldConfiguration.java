4f23ddf2f246d7a5f81454c87cf8692bf4eef5dc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:46:15 +0200

    Fixed UnrecognizedPropertyException.
        Exception in thread "main" com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException: Unrecognized field "database" (class com.example.helloworld.HelloWorldConfiguration), not marked as ignorable (5 known properties: , "server", "template", "databaseConfiguration", "defaultName", "logging"])
         at [Source: N/A; line: -1, column: -1] (through reference chain: com.example.helloworld.HelloWorldConfiguration["database"])

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -1,34 +1,24 @@
 package com.example.helloworld;
 
+import com.codahale.dropwizard.Configuration;
+import com.codahale.dropwizard.db.DatabaseConfiguration;
 import com.example.helloworld.core.Template;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.collect.ImmutableMap;
-import io.dropwizard.Configuration;
-import io.dropwizard.db.DataSourceFactory;
-import io.dropwizard.metrics.graphite.GraphiteReporterFactory;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.Collections;
-import java.util.Map;
 
 public class HelloWorldConfiguration extends Configuration {
     @NotEmpty
     private String template;
-
+    
     @NotEmpty
     private String defaultName = "Stranger";
 
     @Valid
     @NotNull
-    private DataSourceFactory database = new DataSourceFactory();
-
-    @NotNull
-    private Map<String, Map<String, String>> viewRendererConfiguration = Collections.emptyMap();
-
-    @Valid
-    private GraphiteReporterFactory graphiteReporterFactory = new GraphiteReporterFactory();
+    private DatabaseConfiguration database = new DatabaseConfiguration();
 
     @JsonProperty
     public String getTemplate() {
@@ -54,37 +44,13 @@ public class HelloWorldConfiguration extends Configuration {
         return new Template(template, defaultName);
     }
 
-    @JsonProperty("database")
-    public DataSourceFactory getDataSourceFactory() {
+    @JsonProperty
+    public DatabaseConfiguration getDatabaseConfiguration() {
         return database;
     }
 
     @JsonProperty("database")
-    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
-        this.database = dataSourceFactory;
-    }
-
-    @JsonProperty("viewRendererConfiguration")
-    public Map<String, Map<String, String>> getViewRendererConfiguration() {
-        return viewRendererConfiguration;
-    }
-
-    @JsonProperty("viewRendererConfiguration")
-    public void setViewRendererConfiguration(Map<String, Map<String, String>> viewRendererConfiguration) {
-        final ImmutableMap.Builder<String, Map<String, String>> builder = ImmutableMap.builder();
-        for (Map.Entry<String, Map<String, String>> entry : viewRendererConfiguration.entrySet()) {
-            builder.put(entry.getKey(), ImmutableMap.copyOf(entry.getValue()));
-        }
-        this.viewRendererConfiguration = builder.build();
-    }
-
-    @JsonProperty("metrics")
-    public GraphiteReporterFactory getGraphiteReporterFactory() {
-        return graphiteReporterFactory;
-    }
-
-    @JsonProperty("metrics")
-    public void setGraphiteReporterFactory(GraphiteReporterFactory graphiteReporterFactory) {
-        this.graphiteReporterFactory = graphiteReporterFactory;
+    public void setDatabaseConfiguration(DatabaseConfiguration databaseConfiguration) {
+        this.database = databaseConfiguration;
     }
 }
