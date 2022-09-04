07a2f6831461e6b86a5cdf2b134dfe33f117eb1e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:26:47 +0200

    run fieldmappers on non-partial message view, too
        fixes #612

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/MessagesController.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/MessagesController.java
@@ -52,6 +52,7 @@ public class MessagesController extends AuthenticatedController {
     public Result show(String index, String id) {
         try {
             MessageResult message = messagesService.getMessage(index, id);
+            message = FieldMapper.run(message);
             Node sourceNode = getSourceNode(message);
             Radio sourceRadio = getSourceRadio(message);
 
@@ -80,7 +81,7 @@ public class MessagesController extends AuthenticatedController {
 
 	public Result partial(String index, String id) {
 		try {
-            MessageResult message = messagesService.getMessage(index, id);
+            MessageResult message = FieldMapper.run(messagesService.getMessage(index, id));
             Node sourceNode = getSourceNode(message);
             Radio sourceRadio = getSourceRadio(message);
             List<Stream> messageInStreams = Lists.newArrayList();
