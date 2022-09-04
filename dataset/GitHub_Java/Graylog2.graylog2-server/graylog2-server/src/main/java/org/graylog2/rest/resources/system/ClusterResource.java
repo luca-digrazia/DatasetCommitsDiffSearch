1ae30993aaec0c0f91e84d7478d21ef619ab8632
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:24:26 +0200

    Cleanup ClusterResource

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/ClusterResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/ClusterResource.java
@@ -1,73 +1,115 @@
 /**
- * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.rest.resources.system;
 
 import com.codahale.metrics.annotation.Timed;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import com.eaio.uuid.UUID;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 import org.graylog2.cluster.Node;
+import org.graylog2.cluster.NodeNotFoundException;
+import org.graylog2.cluster.NodeService;
 import org.graylog2.plugin.Tools;
-import org.graylog2.rest.resources.RestResource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.graylog2.plugin.cluster.ClusterConfigService;
+import org.graylog2.plugin.cluster.ClusterId;
+import org.graylog2.plugin.system.NodeId;
+import org.graylog2.rest.models.system.cluster.responses.NodeSummary;
+import org.graylog2.rest.models.system.cluster.responses.NodeSummaryList;
+import org.graylog2.shared.rest.resources.RestResource;
+import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
+@Api(value = "System/Cluster", description = "Node discovery")
 @Path("/system/cluster")
+@Produces(MediaType.APPLICATION_JSON)
 public class ClusterResource extends RestResource {
+    private final NodeService nodeService;
+    private final NodeId nodeId;
+    private final ClusterId clusterId;
 
-    private static final Logger LOG = LoggerFactory.getLogger(ClusterResource.class);
-
-    @GET @Timed
-    @Path("/nodes")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String nodes() {
-        List<Map<String, Object>> nodeList = Lists.newArrayList();
-        Map<String, Node> nodes = Node.allActive(core);
-
-        for(Map.Entry<String, Node> e : nodes.entrySet()) {
-            Node node = e.getValue();
-            Map<String, Object> nodeMap = Maps.newHashMap();
-
-            nodeMap.put("id", node.getNodeId());
-            nodeMap.put("is_master", node.isMaster());
-            nodeMap.put("transport_address", node.getTransportAddress());
-            nodeMap.put("last_seen", Tools.getISO8601String(node.getLastSeen()));
-
-            nodeList.add(nodeMap);
-        }
-
-        Map<String, Object> result = Maps.newHashMap();
-        result.put("total", nodes.size());
-        result.put("nodes", nodeList);
-
-        return json(result);
+    @Inject
+    public ClusterResource(NodeService nodeService,
+                           ClusterConfigService clusterConfigService,
+                           NodeId nodeId) {
+        this.nodeService = nodeService;
+        this.nodeId = nodeId;
+        this.clusterId = clusterConfigService.getOrDefault(ClusterId.class, ClusterId.create(UUID.nilUUID().toString()));
     }
 
+    @GET
+    @Timed
+    @Path("/nodes")
+    @ApiOperation(value = "List all active nodes in this cluster.")
+    public NodeSummaryList nodes() {
+        final Map<String, Node> nodes = nodeService.allActive(Node.Type.SERVER);
+        final List<NodeSummary> nodeList = new ArrayList<>(nodes.size());
+        for (Node node : nodes.values()) {
+            nodeList.add(nodeSummary(node));
+        }
 
+        return NodeSummaryList.create(nodeList);
+    }
+
+    @GET
+    @Timed
+    @Path("/node")
+    @ApiOperation(value = "Information about this node.",
+            notes = "This is returning information of this node in context to its state in the cluster. " +
+                    "Use the system API of the node itself to get system information.")
+    public NodeSummary node() throws NodeNotFoundException {
+        return nodeSummary(nodeService.byNodeId(nodeId));
+    }
+
+    @GET
+    @Timed
+    @Path("/nodes/{nodeId}")
+    @ApiOperation(value = "Information about a node.",
+            notes = "This is returning information of a node in context to its state in the cluster. " +
+                    "Use the system API of the node itself to get system information.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Node not found.")
+    })
+    public NodeSummary node(@ApiParam(name = "nodeId", required = true) @PathParam("nodeId") @NotEmpty String nodeId) throws NodeNotFoundException {
+        return nodeSummary(nodeService.byNodeId(nodeId));
+    }
+
+    private NodeSummary nodeSummary(Node node) {
+        return NodeSummary.create(
+                clusterId.clusterId(),
+                node.getNodeId(),
+                node.getType().toString().toLowerCase(Locale.ENGLISH),
+                node.isMaster(),
+                node.getTransportAddress(),
+                Tools.getISO8601String(node.getLastSeen()),
+                node.getShortNodeId(),
+                node.getHostname()
+        );
+    }
 }
