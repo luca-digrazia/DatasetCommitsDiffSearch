901356bb80e9fbde82b9fd5df22ac56b02d4bc0a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:13:52 +0200

    correctly set SO_RCVBUF on UDP inputs

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/inputs/gelf/GELFUDPInput.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/inputs/gelf/GELFUDPInput.java
@@ -60,7 +60,6 @@ public class GELFUDPInput implements MessageInput {
         final ExecutorService workerThreadPool = Executors.newCachedThreadPool();
         final ConnectionlessBootstrap bootstrap = new ConnectionlessBootstrap(new NioDatagramChannelFactory(workerThreadPool));
 
-        bootstrap.setOption("receiveBufferSize", 1048576);
         bootstrap.setOption("receiveBufferSizePredictorFactory", new FixedReceiveBufferSizePredictorFactory(8192));
         bootstrap.setPipelineFactory(new GELFPipelineFactory(graylogServer));
 
