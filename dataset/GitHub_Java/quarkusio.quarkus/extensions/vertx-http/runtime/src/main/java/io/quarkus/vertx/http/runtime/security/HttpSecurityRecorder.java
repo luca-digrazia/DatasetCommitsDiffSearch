1a9a228b54f356817acc37281843cff6ae9bf637
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:24:19 +0200

    Avoid creating Uni objects where possible

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/HttpSecurityRecorder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/HttpSecurityRecorder.java
@@ -95,7 +95,7 @@ public class HttpSecurityRecorder {
                     }
                 });
 
-                Uni<SecurityIdentity> potentialUser = authenticator.attemptAuthentication(event).cache();
+                Uni<SecurityIdentity> potentialUser = authenticator.attemptAuthentication(event);
                 if (proactiveAuthentication) {
                     potentialUser
                             .subscribe().withSubscriber(new UniSubscriber<SecurityIdentity>() {
@@ -183,7 +183,7 @@ public class HttpSecurityRecorder {
                                         }
                                     }
                                 }
-                            }).cache();
+                            });
                     event.put(QuarkusHttpUser.DEFERRED_IDENTITY_KEY, lazyUser);
                     event.next();
                 }
