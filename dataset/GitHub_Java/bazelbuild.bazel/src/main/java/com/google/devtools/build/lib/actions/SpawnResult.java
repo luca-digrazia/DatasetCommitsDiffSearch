a8861ba050eaa3160a372a5e4723782df45c736d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:19:41 +0200

    Clean up SpawnResult documentation and visibility.
        This clarifies and improves the accuracy of its documentation. Also, this
        reduces its unnecessarily broad visibility.
    
        RELNOTES: None.
        PiperOrigin-RevId: 296067625

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/SpawnResult.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/SpawnResult.java
@@ -28,9 +28,6 @@ import javax.annotation.Nullable;
 
 /** The result of a {@link Spawn}'s execution. */
 public interface SpawnResult {
-
-  int POSIX_TIMEOUT_EXIT_CODE = /*SIGNAL_BASE=*/ 128 + /*SIGALRM=*/ 14;
-
   /** The status of the attempted Spawn execution. */
   enum Status {
     /** Subprocess executed successfully, and returned a zero exit code. */
@@ -94,25 +91,17 @@ public interface SpawnResult {
   }
 
   /**
-   * Returns whether the spawn was actually run, regardless of the exit code. I.e., returns {@code
-   * true} if {@link #status} is any of {@link Status#SUCCESS}, {@link Status#NON_ZERO_EXIT}, {@link
-   * Status#TIMEOUT} or {@link Status#OUT_OF_MEMORY}.
+   * Returns whether the spawn was actually run, regardless of the exit code. I.e., returns
+   * {@code true} if {@link #status} is any of {@link Status#SUCCESS}, {@link Status#NON_ZERO_EXIT},
+   * {@link Status#TIMEOUT} or {@link Status#OUT_OF_MEMORY}.
    *
    * <p>Returns false if there were errors that prevented the spawn from being run, such as network
    * errors, missing local files, errors setting up sandboxing, etc.
    */
-  default boolean setupSuccess() {
-    Status status = status();
-    return status == Status.SUCCESS
-        || status == Status.NON_ZERO_EXIT
-        || status == Status.TIMEOUT
-        || status == Status.OUT_OF_MEMORY;
-  }
+  boolean setupSuccess();
 
   /** Returns true if the status was {@link Status#EXECUTION_FAILED_CATASTROPHICALLY}. */
-  default boolean isCatastrophe() {
-    return status() == Status.EXECUTION_FAILED_CATASTROPHICALLY;
-  }
+  boolean isCatastrophe();
 
   /** Returns the status of the attempted Spawn execution. */
   Status status();
@@ -127,10 +116,7 @@ public interface SpawnResult {
    *
    * <p>Returns 128 + 14 (corresponding to the Unix signal SIGALRM) if {@link #status} returns
    * {@link Status#TIMEOUT}.
-   *
-   * <p>Otherwise, the returned value is not meaningful.
    */
-  // TODO(mschaller): clean up all uses of this method when {@code !this.setupSuccess()}
   int exitCode();
 
   /**
@@ -268,6 +254,19 @@ public interface SpawnResult {
       this.inMemoryOutputFile = builder.inMemoryOutputFile;
       this.inMemoryContents = builder.inMemoryContents;
       this.actionMetadataLog = builder.actionMetadataLog;
+    }
+
+    @Override
+    public boolean setupSuccess() {
+      return status == Status.SUCCESS
+          || status == Status.NON_ZERO_EXIT
+          || status == Status.TIMEOUT
+          || status == Status.OUT_OF_MEMORY;
+    }
+
+    @Override
+    public boolean isCatastrophe() {
+      return status == Status.EXECUTION_FAILED_CATASTROPHICALLY;
     }
 
     @Override
@@ -417,9 +416,10 @@ public interface SpawnResult {
       Preconditions.checkArgument(!runnerName.isEmpty());
       if (status == Status.SUCCESS) {
         Preconditions.checkArgument(exitCode == 0);
-      } else if (status == Status.TIMEOUT) {
-        Preconditions.checkArgument(exitCode == POSIX_TIMEOUT_EXIT_CODE);
-      } else if (status == Status.NON_ZERO_EXIT || status == Status.OUT_OF_MEMORY) {
+      }
+      if (status == Status.NON_ZERO_EXIT
+          || status == Status.TIMEOUT
+          || status == Status.OUT_OF_MEMORY) {
         Preconditions.checkArgument(exitCode != 0);
       }
       return new SimpleSpawnResult(this);
