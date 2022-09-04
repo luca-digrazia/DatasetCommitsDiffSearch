b4db386e6ae66ca2985590cddebf22713426bc94
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:54:45 +0200

    Document some more stuff.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/Service.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/Service.java
@@ -53,13 +53,6 @@ public abstract class Service<T extends Configuration> {
     }
 
     /**
-     * Initializes the service bootstrap.
-     *
-     * @param bootstrap the service bootstrap
-     */
-    public abstract void initialize(Bootstrap<T> bootstrap);
-
-    /**
      * When the service runs, this is called after the {@link Bundle}s are run. Override it to add
      * providers, resources, etc. for your service.
      *
@@ -68,6 +61,16 @@ public abstract class Service<T extends Configuration> {
      * @throws Exception if something goes wrong
      */
     public abstract void run(T configuration, Environment environment) throws Exception;
+
+    /**
+     * Initializes the service bootstrap.
+     *
+     * @param bootstrap the service bootstrap
+     */
+    @SuppressWarnings("UnusedParameters")
+    public void initialize(Bootstrap<T> bootstrap) {
+        // no default implementation
+    }
 
     /**
      * Parses command-line arguments and runs the service. Call this method from a {@code public
