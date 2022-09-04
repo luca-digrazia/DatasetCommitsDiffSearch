42cb6e05ce60105cf1ba5f1936e9ae08abc7eba6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:58:45 +0200

    Adding API endpoint for stream rules.

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/api/StreamRulesApiController.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/api/StreamRulesApiController.java
@@ -2,15 +2,13 @@ package controllers.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Lists;
-import com.google.common.net.MediaType;
 import controllers.AuthenticatedController;
-import lib.json.Json;
 import org.graylog2.restclient.lib.APIException;
 import org.graylog2.restclient.models.StreamRule;
 import org.graylog2.restclient.models.StreamRuleService;
 import org.graylog2.restclient.models.api.requests.streams.CreateStreamRuleRequest;
-import org.graylog2.restclient.models.api.responses.streams.CreateStreamRuleResponse;
 import org.graylog2.restclient.models.api.results.StreamRulesResult;
+import play.libs.Json;
 import play.mvc.Result;
 
 import javax.inject.Inject;
@@ -37,17 +35,17 @@ public class StreamRulesApiController extends AuthenticatedController {
         this.streamRuleService = streamRuleService;
     }
 
-    public Result types() {
+    public Result types(String streamId) {
         final List<Type> types = Lists.newArrayList();
         for (StreamRule.Type type : StreamRule.Type.values()) {
             types.add(new Type(type.getId(), type.getShortDesc(), type.getLongDesc()));
         }
-        return ok(Json.toJsonString(types)).as(MediaType.JSON_UTF_8.toString());
+        return ok(Json.toJson(types));
     }
 
     public Result list(String streamId) throws IOException, APIException {
         final StreamRulesResult result = streamRuleService.all(streamId);
-        return ok(Json.toJsonString(result)).as(MediaType.JSON_UTF_8.toString());
+        return ok(Json.toJson(result));
     }
 
     public Result update(String streamId, String streamRuleId) throws APIException, IOException {
@@ -55,17 +53,5 @@ public class StreamRulesApiController extends AuthenticatedController {
         final CreateStreamRuleRequest request = Json.fromJson(json, CreateStreamRuleRequest.class);
         streamRuleService.update(streamId, streamRuleId, request);
         return ok();
-    }
-
-    public Result delete(String streamId, String streamRuleId) throws APIException, IOException {
-        streamRuleService.delete(streamId, streamRuleId);
-        return ok();
-    }
-
-    public Result create(String streamId) throws APIException, IOException {
-        final JsonNode json = request().body().asJson();
-        final CreateStreamRuleRequest request = Json.fromJson(json, CreateStreamRuleRequest.class);
-        final CreateStreamRuleResponse result = streamRuleService.create(streamId, request);
-        return ok(Json.toJsonString(result)).as(MediaType.JSON_UTF_8.toString());
     }
 }
