6f37107a5133e1ad984f305aaeeca48987e837d1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:50:07 +0200

    Fix typo in platform api docs
        This was fun to figure out.
    
        Closes #8256.
    
        PiperOrigin-RevId: 247236312

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skylarkbuildapi/platform/PlatformInfoApi.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skylarkbuildapi/platform/PlatformInfoApi.java
@@ -15,12 +15,11 @@
 package com.google.devtools.build.lib.skylarkbuildapi.platform;
 
 import com.google.devtools.build.lib.cmdline.Label;
-import com.google.devtools.build.lib.skylarkbuildapi.core.StructApi;
+import com.google.devtools.build.lib.skylarkbuildapi.StructApi;
 import com.google.devtools.build.lib.skylarkinterface.SkylarkCallable;
 import com.google.devtools.build.lib.skylarkinterface.SkylarkModule;
 import com.google.devtools.build.lib.skylarkinterface.SkylarkModuleCategory;
 import com.google.devtools.build.lib.syntax.StarlarkSemantics.FlagIdentifier;
-import java.util.Map;
 
 /** Info object representing data about a specific platform. */
 @SkylarkModule(
@@ -34,7 +33,7 @@ public interface PlatformInfoApi<
         ConstraintValueInfoT extends ConstraintValueInfoApi>
     extends StructApi {
 
-  String EXPERIMENTAL_WARNING =
+  static final String EXPERIMENTAL_WARNING =
       "<i>Note: This API is experimental and may change at any time. It is disabled by default, "
           + "but may be enabled with <code>--experimental_platforms_api</code></i>";
 
@@ -60,11 +59,4 @@ public interface PlatformInfoApi<
       structField = true,
       enableOnlyWithFlag = FlagIdentifier.EXPERIMENTAL_PLATFORM_API)
   String remoteExecutionProperties();
-
-  @SkylarkCallable(
-      name = "exec_properties",
-      doc = "Properties to configure a remote execution platform.",
-      structField = true,
-      enableOnlyWithFlag = FlagIdentifier.EXPERIMENTAL_PLATFORM_API)
-  Map<String, String> execProperties();
 }
