ca40040b92affbae89b6b919f5a5306cbfc8412b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:08:16 +0200

    Minor

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit/src/main/java/org/jboss/shamrock/test/ShamrockUnitTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit/src/main/java/org/jboss/shamrock/test/ShamrockUnitTest.java
@@ -1,36 +1,16 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.jboss.shamrock.test;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.net.URL;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 
 import org.jboss.shamrock.runner.RuntimeRunner;
-import org.jboss.shamrock.runtime.InjectionFactoryTemplate;
-import org.jboss.shamrock.runtime.InjectionInstance;
 import org.jboss.shrinkwrap.api.exporter.ExplodedExporter;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.runners.BlockJUnit4ClassRunner;
@@ -39,56 +19,20 @@ import org.junit.runners.model.Statement;
 
 public class ShamrockUnitTest extends BlockJUnit4ClassRunner {
 
-    private static Path deploymentDir;
-    private static RuntimeRunner runtimeRunner;
+    private Path deploymentDir;
+    private RuntimeRunner runtimeRunner;
 
     static boolean started = false;
 
     public ShamrockUnitTest(Class<?> klass) throws InitializationError {
-        super(doSetup(klass));
+        super(klass);
         started = true;
     }
 
     @Override
     protected Object createTest() throws Exception {
-        InjectionInstance<?> factory = InjectionFactoryTemplate.currentFactory().create(getTestClass().getJavaClass());
-        return factory.newInstance();
-    }
-
-    static Class<?> doSetup(Class<?> clazz) {
-        try {
-            Class<?> theClass = clazz;
-            Method deploymentMethod = null;
-            for (Method m : theClass.getMethods()) {
-                if (m.isAnnotationPresent(Deployment.class)) {
-                    deploymentMethod = m;
-                    break;
-                }
-            }
-            if (deploymentMethod == null) {
-                throw new RuntimeException("Could not find @Deployment method on " + theClass);
-            }
-            if (!Modifier.isStatic(deploymentMethod.getModifiers())) {
-                throw new RuntimeException("@Deployment method must be static" + deploymentMethod);
-            }
-
-            JavaArchive archive = (JavaArchive) deploymentMethod.invoke(null);
-            archive.addClass(theClass);
-            deploymentDir = Files.createTempDirectory("shamrock-unit-test");
-
-            archive.as(ExplodedExporter.class).exportExplodedInto(deploymentDir.toFile());
-
-            String classFileName = theClass.getName().replace('.', '/') + ".class";
-            URL resource = theClass.getClassLoader().getResource(classFileName);
-            String testClassLocation = resource.getPath().substring(0, resource.getPath().length() - classFileName.length());
-            runtimeRunner = new RuntimeRunner(clazz.getClassLoader(), deploymentDir, Paths.get(testClassLocation), null, new ArrayList<>());
-            runtimeRunner.run();
-
-            String javaClass = clazz.getName();
-            return Class.forName(javaClass, true, Thread.currentThread().getContextClassLoader());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        Object testInstance = super.createTest();
+        return testInstance;
     }
 
     @Override
@@ -98,6 +42,28 @@ public class ShamrockUnitTest extends BlockJUnit4ClassRunner {
             @Override
             public void evaluate() throws Throwable {
 
+                Class<?> theClass = getTestClass().getJavaClass();
+                Method deploymentMethod = null;
+                for (Method m : theClass.getMethods()) {
+                    if (m.isAnnotationPresent(Deployment.class)) {
+                        deploymentMethod = m;
+                        break;
+                    }
+                }
+                if (deploymentMethod == null) {
+                    throw new RuntimeException("Could not find @Deployment method on " + theClass);
+                }
+                if (!Modifier.isStatic(deploymentMethod.getModifiers())) {
+                    throw new RuntimeException("@Deployment method must be static" + deploymentMethod);
+                }
+
+                JavaArchive archive = (JavaArchive) deploymentMethod.invoke(null);
+                deploymentDir = Files.createTempDirectory("shamrock-unit-test");
+
+                archive.as(ExplodedExporter.class).exportExplodedInto(deploymentDir.toFile());
+
+                runtimeRunner = new RuntimeRunner(getClass().getClassLoader(), deploymentDir, deploymentDir, null, new ArrayList<>());
+                runtimeRunner.run();
                 existing.evaluate();
 
             }
