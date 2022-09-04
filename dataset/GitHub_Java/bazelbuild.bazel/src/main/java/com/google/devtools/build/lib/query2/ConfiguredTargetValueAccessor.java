d4572b09faf5f1a6e6691a33eb60b55a91fa4aaa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:06:57 +0200

    Add ConfiguredTargetValueAccessor.
        This will be necessary for the action graph query which operates on
        ConfiguredTargetValue's.
    
        RELNOTES: None
        PiperOrigin-RevId: 201657526

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/query2/ConfiguredTargetValueAccessor.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/query2/ConfiguredTargetValueAccessor.java
@@ -13,45 +13,28 @@
 // limitations under the License.
 package com.google.devtools.build.lib.query2;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.packages.Rule;
 import com.google.devtools.build.lib.packages.Target;
 import com.google.devtools.build.lib.packages.TargetUtils;
-import com.google.devtools.build.lib.query2.cquery.ConfiguredTargetAccessor;
-import com.google.devtools.build.lib.query2.engine.KeyExtractor;
 import com.google.devtools.build.lib.query2.engine.QueryEnvironment.TargetAccessor;
 import com.google.devtools.build.lib.query2.engine.QueryException;
 import com.google.devtools.build.lib.query2.engine.QueryExpression;
 import com.google.devtools.build.lib.query2.engine.QueryVisibility;
-import com.google.devtools.build.lib.skyframe.AspectValue;
-import com.google.devtools.build.lib.skyframe.ConfiguredTargetKey;
 import com.google.devtools.build.lib.skyframe.ConfiguredTargetValue;
-import com.google.devtools.build.lib.skyframe.SkyFunctions;
-import com.google.devtools.build.skyframe.SkyFunctionName;
-import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.WalkableGraph;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * A {@link TargetAccessor} for {@link ConfiguredTargetValue} objects.
+/** A {@link TargetAccessor} for {@link ConfiguredTargetValue} objects.
  *
- * <p>Incomplete; we'll implement getPrerequisites and getVisibility when needed.
+ * Incomplete; we'll implement getLabelListAttr and getVisibility when needed.
  */
-public class ConfiguredTargetValueAccessor implements TargetAccessor<ConfiguredTargetValue> {
+class ConfiguredTargetValueAccessor implements TargetAccessor<ConfiguredTargetValue> {
 
   private final WalkableGraph walkableGraph;
-  private final KeyExtractor<ConfiguredTargetValue, ConfiguredTargetKey>
-      configuredTargetKeyExtractor;
 
-  public ConfiguredTargetValueAccessor(
-      WalkableGraph walkableGraph,
-      KeyExtractor<ConfiguredTargetValue, ConfiguredTargetKey> configuredTargetKeyExtractor) {
+  ConfiguredTargetValueAccessor(WalkableGraph walkableGraph) {
     this.walkableGraph = walkableGraph;
-    this.configuredTargetKeyExtractor = configuredTargetKeyExtractor;
   }
 
   @Override
@@ -94,7 +77,7 @@ public class ConfiguredTargetValueAccessor implements TargetAccessor<ConfiguredT
   }
 
   @Override
-  public List<ConfiguredTargetValue> getPrerequisites(
+  public List<ConfiguredTargetValue> getLabelListAttr(
       QueryExpression caller,
       ConfiguredTargetValue configuredTargetValue,
       String attrName,
@@ -134,24 +117,5 @@ public class ConfiguredTargetValueAccessor implements TargetAccessor<ConfiguredT
   private Target getTargetFromConfiguredTargetValue(ConfiguredTargetValue configuredTargetValue) {
     return ConfiguredTargetAccessor.getTargetFromConfiguredTarget(
         configuredTargetValue.getConfiguredTarget(), walkableGraph);
-  }
-
-  /** Returns the AspectValues that are attached to the given configuredTarget. */
-  public Collection<AspectValue> getAspectValues(ConfiguredTargetValue configuredTargetValue)
-      throws InterruptedException {
-    Set<AspectValue> result = new HashSet<>();
-    SkyKey skyKey = configuredTargetKeyExtractor.extractKey(configuredTargetValue);
-    Iterable<SkyKey> revDeps =
-        Iterables.concat(walkableGraph.getReverseDeps(ImmutableList.of(skyKey)).values());
-    for (SkyKey revDep : revDeps) {
-      SkyFunctionName skyFunctionName = revDep.functionName();
-      if (SkyFunctions.ASPECT.equals(skyFunctionName)) {
-        AspectValue aspectValue = (AspectValue) walkableGraph.getValue(revDep);
-        if (aspectValue.getLabel().equals(configuredTargetValue.getConfiguredTarget().getLabel())) {
-          result.add(aspectValue);
-        }
-      }
-    }
-    return result;
   }
 }
