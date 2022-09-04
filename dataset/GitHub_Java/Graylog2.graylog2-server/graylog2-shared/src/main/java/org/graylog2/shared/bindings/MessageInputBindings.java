57513c4f594b46e245f5e057e277e734301547a9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:48:15 +0200

    Add Raw and Syslog AMQP inputs

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/bindings/MessageInputBindings.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/bindings/MessageInputBindings.java
@@ -1,58 +1,70 @@
-/*
- * Copyright 2012-2014 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.graylog2.shared.bindings;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.TypeLiteral;
-import com.google.inject.multibindings.Multibinder;
-import org.graylog2.inputs.amqp.AMQPInput;
+import com.google.inject.multibindings.MapBinder;
+import org.graylog2.inputs.codecs.CodecsModule;
+import org.graylog2.inputs.gelf.amqp.GELFAMQPInput;
+import org.graylog2.inputs.gelf.http.GELFHttpInput;
+import org.graylog2.inputs.gelf.kafka.GELFKafkaInput;
 import org.graylog2.inputs.gelf.tcp.GELFTCPInput;
 import org.graylog2.inputs.gelf.udp.GELFUDPInput;
 import org.graylog2.inputs.kafka.KafkaInput;
 import org.graylog2.inputs.misc.jsonpath.JsonPathInput;
 import org.graylog2.inputs.misc.metrics.LocalMetricsInput;
 import org.graylog2.inputs.random.FakeHttpMessageInput;
+import org.graylog2.inputs.raw.amqp.RawAMQPInput;
+import org.graylog2.inputs.raw.kafka.RawKafkaInput;
 import org.graylog2.inputs.raw.tcp.RawTCPInput;
 import org.graylog2.inputs.raw.udp.RawUDPInput;
+import org.graylog2.inputs.syslog.amqp.SyslogAMQPInput;
+import org.graylog2.inputs.syslog.kafka.SyslogKafkaInput;
 import org.graylog2.inputs.syslog.tcp.SyslogTCPInput;
 import org.graylog2.inputs.syslog.udp.SyslogUDPInput;
+import org.graylog2.inputs.transports.TransportsModule;
+import org.graylog2.plugin.inject.Graylog2Module;
 import org.graylog2.plugin.inputs.MessageInput;
 
-/**
- * @author Dennis Oelkers <dennis@torch.sh>
- */
-public class MessageInputBindings extends AbstractModule {
+public class MessageInputBindings extends Graylog2Module {
     @Override
     protected void configure() {
-        TypeLiteral<Class<? extends MessageInput>> typeLiteral = new TypeLiteral<Class<? extends MessageInput>>(){};
-        Multibinder<Class<? extends MessageInput>> messageInputs = Multibinder.newSetBinder(binder(), typeLiteral);
-        messageInputs.addBinding().toInstance(GELFTCPInput.class);
-        messageInputs.addBinding().toInstance(GELFUDPInput.class);
-        messageInputs.addBinding().toInstance(RawTCPInput.class);
-        messageInputs.addBinding().toInstance(RawUDPInput.class);
-        messageInputs.addBinding().toInstance(AMQPInput.class);
-        messageInputs.addBinding().toInstance(KafkaInput.class);
-        messageInputs.addBinding().toInstance(JsonPathInput.class);
-        messageInputs.addBinding().toInstance(LocalMetricsInput.class);
-        messageInputs.addBinding().toInstance(FakeHttpMessageInput.class);
-        messageInputs.addBinding().toInstance(SyslogTCPInput.class);
-        messageInputs.addBinding().toInstance(SyslogUDPInput.class);
+        install(new TransportsModule());
+        install(new CodecsModule());
+
+        final MapBinder<String, MessageInput.Factory<? extends MessageInput>> inputMapBinder = inputsMapBinder();
+        // new style inputs, using transports and codecs
+        installInput(inputMapBinder, RawTCPInput.class, RawTCPInput.Factory.class);
+        installInput(inputMapBinder, RawUDPInput.class, RawUDPInput.Factory.class);
+        installInput(inputMapBinder, RawAMQPInput.class, RawAMQPInput.Factory.class);
+        installInput(inputMapBinder, RawKafkaInput.class, RawKafkaInput.Factory.class);
+        installInput(inputMapBinder, SyslogTCPInput.class, SyslogTCPInput.Factory.class);
+        installInput(inputMapBinder, SyslogUDPInput.class, SyslogUDPInput.Factory.class);
+        installInput(inputMapBinder, SyslogAMQPInput.class, SyslogAMQPInput.Factory.class);
+        installInput(inputMapBinder, SyslogKafkaInput.class, SyslogKafkaInput.Factory.class);
+        installInput(inputMapBinder, FakeHttpMessageInput.class, FakeHttpMessageInput.Factory.class);
+        installInput(inputMapBinder, GELFTCPInput.class, GELFTCPInput.Factory.class);
+        installInput(inputMapBinder, GELFHttpInput.class, GELFHttpInput.Factory.class);
+        installInput(inputMapBinder, GELFUDPInput.class, GELFUDPInput.Factory.class);
+        installInput(inputMapBinder, GELFAMQPInput.class, GELFAMQPInput.Factory.class);
+        installInput(inputMapBinder, GELFKafkaInput.class, GELFKafkaInput.Factory.class);
+        installInput(inputMapBinder, KafkaInput.class, KafkaInput.Factory.class);
+        installInput(inputMapBinder, JsonPathInput.class, JsonPathInput.Factory.class);
+        installInput(inputMapBinder, LocalMetricsInput.class, LocalMetricsInput.Factory.class);
     }
+
+
 }
