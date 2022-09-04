d412220550052f2f434753d631976c8200a88f22
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:45:53 +0200

    Handle more than one level of sub resource params
        Fixes #15114

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/resteasy-reactive/quarkus-resteasy-reactive/deployment/src/test/java/io/quarkus/resteasy/reactive/server/test/resource/basic/SubResourceRequestFilterTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/resteasy-reactive/quarkus-resteasy-reactive/deployment/src/test/java/io/quarkus/resteasy/reactive/server/test/resource/basic/SubResourceRequestFilterTest.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 import org.hamcrest.Matchers;
+import org.jboss.resteasy.reactive.RestPath;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
@@ -30,16 +31,18 @@ public class SubResourceRequestFilterTest {
                 @Override
                 public JavaArchive get() {
                     JavaArchive war = ShrinkWrap.create(JavaArchive.class);
-                    war.addClasses(RestResource.class, RestSubResource.class, SingleExecutionFilter.class);
+                    war.addClasses(RestResource.class, RestSubResource.class, SingleExecutionFilter.class,
+                            MiddleRestResource.class);
                     return war;
                 }
             });
 
     @Test
-    public void testAbortingRequestFilter() {
-        RestAssured.get("/sub-resource/hello")
+    public void testSubResourceFilter() {
+        RestAssured.get("/sub-resource/Bob/Builder")
                 .then()
                 .header("single-filter", Matchers.equalTo("once"))
+                .body(Matchers.equalTo("Bob Builder"))
                 .statusCode(200);
     }
 
@@ -47,9 +50,22 @@ public class SubResourceRequestFilterTest {
     public static class RestResource {
 
         @Inject
+        MiddleRestResource restSubResource;
+
+        @Path("sub-resource/{first}")
+        public MiddleRestResource hello(String first) {
+            return restSubResource;
+        }
+    }
+
+    @ApplicationScoped
+    @Path("/")
+    public static class MiddleRestResource {
+
+        @Inject
         RestSubResource restSubResource;
 
-        @Path("sub-resource/hello")
+        @Path("{last}")
         public RestSubResource hello() {
             return restSubResource;
         }
@@ -59,8 +75,8 @@ public class SubResourceRequestFilterTest {
     public static class RestSubResource {
 
         @GET
-        public Response hello(HttpHeaders headers) {
-            return Response.ok().header("single-filter", headers.getHeaderString("single-filter")).build();
+        public Response hello(HttpHeaders headers, @RestPath String first, @RestPath String last) {
+            return Response.ok(first + " " + last).header("single-filter", headers.getHeaderString("single-filter")).build();
         }
     }
 
