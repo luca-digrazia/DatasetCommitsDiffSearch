d345e1abacdf5fe39f24c599685688e2e15321ec
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:11:44 +0200

    Add --experimental_supports_info_crosstool_configuration to support removal of bazel info crosstool-configuration
        RELNOTES: None.
        PiperOrigin-RevId: 200437121

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/commands/InfoCommand.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/commands/InfoCommand.java
@@ -79,9 +79,9 @@ public class InfoCommand implements BlazeCommand {
         name = "experimental_supports_info_crosstool_configuration",
         defaultValue = "true",
         documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
-        effectTags = {OptionEffectTag.UNKNOWN},
-        metadataTags = {OptionMetadataTag.HIDDEN},
-        help = "Noop.")
+        effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS, OptionEffectTag.TERMINAL_OUTPUT},
+        metadataTags = {OptionMetadataTag.INCOMPATIBLE_CHANGE},
+        help = "Flag to roll out the removal of 'bazel info crosstool-configuration'.")
     public boolean experimentalSupportsInfoCrosstoolConfiguration;
   }
 
