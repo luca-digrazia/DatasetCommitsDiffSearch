61384f6f752e62f16affca49317d420d2f4b5ce4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:14:29 +0200

    Publish events on authentication service activation and deletion (#9346)
        * Publish an event when an auth service gets activated
    
        Refs Graylog2/graylog-plugin-enterprise#1841
    
        * Publish event when deleting authentication service backend
    
        Refs Graylog2/graylog-plugin-enterprise#1794
    
        * Only publish deletion event after a successful delete

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/security/authservice/DBAuthServiceBackendService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog/security/authservice/DBAuthServiceBackendService.java
@@ -16,6 +16,8 @@
  */
 package org.graylog.security.authservice;
 
+import com.google.common.eventbus.EventBus;
+import org.graylog.security.events.AuthServiceBackendDeletedEvent;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.MongoConnection;
 import org.graylog2.database.PaginatedDbService;
@@ -25,14 +27,61 @@ import org.mongojack.DBQuery;
 import org.mongojack.DBSort;
 
 import javax.inject.Inject;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Predicate;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public class DBAuthServiceBackendService extends PaginatedDbService<AuthServiceBackendDTO> {
+    private final Map<String, AuthServiceBackend.Factory<? extends AuthServiceBackend>> backendFactories;
+    private final EventBus eventBus;
+
     @Inject
-    protected DBAuthServiceBackendService(MongoConnection mongoConnection, MongoJackObjectMapperProvider mapper) {
+    protected DBAuthServiceBackendService(MongoConnection mongoConnection,
+                                          MongoJackObjectMapperProvider mapper,
+                                          Map<String, AuthServiceBackend.Factory<? extends AuthServiceBackend>> backendFactories,
+                                          EventBus eventBus) {
         super(mongoConnection, mapper, AuthServiceBackendDTO.class, "auth_service_backends");
+        this.backendFactories = backendFactories;
+        this.eventBus = eventBus;
+    }
+
+    @Override
+    public AuthServiceBackendDTO save(AuthServiceBackendDTO newBackend) {
+        return super.save(prepareUpdate(newBackend));
+    }
+
+    @Override
+    public int delete(String id) {
+        checkArgument(isNotBlank(id), "id cannot be blank");
+        final int delete = super.delete(id);
+        if (delete > 0) {
+            eventBus.post(AuthServiceBackendDeletedEvent.create(id));
+        }
+        return delete;
+    }
+
+    private AuthServiceBackendDTO prepareUpdate(AuthServiceBackendDTO newBackend) {
+        if (newBackend.id() == null) {
+            // It's not an update
+            return newBackend;
+        }
+        final AuthServiceBackendDTO existingBackend = get(newBackend.id())
+                .orElseThrow(() -> new IllegalArgumentException("Couldn't find backend <" + newBackend.id() + ">"));
+
+        // Call AuthServiceBackend#prepareConfigUpdate to give the backend implementation a chance to modify it
+        // (e.g. handling password updates via EncryptedValue)
+        return Optional.ofNullable(backendFactories.get(existingBackend.config().type()))
+                .map(factory -> factory.create(existingBackend))
+                .map(backend -> backend.prepareConfigUpdate(existingBackend, newBackend))
+                .orElseThrow(() -> new IllegalArgumentException("Couldn't find backend implementation for type <" + existingBackend.config().type() + ">"));
+    }
+
+    public long countBackends() {
+        return db.count();
     }
 
     public PaginatedList<AuthServiceBackendDTO> findPaginated(PaginationParameters params,
