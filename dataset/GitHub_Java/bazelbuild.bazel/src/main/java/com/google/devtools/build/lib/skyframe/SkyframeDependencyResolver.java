f1e770624141f7f0d1cd3a819eb9a950bb686272
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:31:12 +0200

    SkyframeDependencyResolver: fix use of String.format
        When using format strings, the format string itself should only
        contain the interpolation directives, and not also interpolated
        strings which might contain character sequences signifying
        interpolation directives themselves. Fix this.
    
        Fixes #8795
    
        Change-Id: Iad69323e580135585e29897876a95e9b04458cdc
        PiperOrigin-RevId: 256553397

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/SkyframeDependencyResolver.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/SkyframeDependencyResolver.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.skyframe;
 import static com.google.devtools.build.lib.cmdline.LabelConstants.EXTERNAL_PACKAGE_IDENTIFIER;
 
 import com.google.common.collect.Maps;
-import com.google.devtools.build.lib.analysis.DependencyKind;
 import com.google.devtools.build.lib.analysis.DependencyResolver;
 import com.google.devtools.build.lib.analysis.TargetAndConfiguration;
 import com.google.devtools.build.lib.causes.Cause;
