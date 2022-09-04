41ac3e201d18a0b81fa4bd5bcca4a673ef816709
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:26:38 +0200

    @see OneVersusOne and OneVersusRest

--- dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/classification/SVM.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/classification/SVM.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2010-2020 Haifeng Li. All rights reserved.
+/*******************************************************************************
+ * Copyright (c) 2010-2019 Haifeng Li
  *
  * Smile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
- */
+ *******************************************************************************/
 
 package smile.classification;
 
@@ -97,7 +97,7 @@ public class SVM<T> extends KernelMachine<T> implements Classifier<T> {
 
     @Override
     public int predict(T x) {
-        return score(x) > 0 ? +1 : -1;
+        return f(x) > 0 ? +1 : -1;
     }
 
     /**
