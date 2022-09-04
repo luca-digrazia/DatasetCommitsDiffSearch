164ba33f33e90efc81b788d9940710e07c2f0630
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:22:31 +0200

    Fixed typo in Sherlock version of the MenuInflater class.

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/OptionsMenuProcessor.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/OptionsMenuProcessor.java
@@ -25,14 +25,11 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 
 import com.googlecode.androidannotations.annotations.OptionsMenu;
-import com.googlecode.androidannotations.helper.AnnotationHelper;
 import com.googlecode.androidannotations.helper.SherlockHelper;
-import com.googlecode.androidannotations.processing.EBeansHolder.Classes;
 import com.googlecode.androidannotations.rclass.IRClass;
 import com.googlecode.androidannotations.rclass.IRClass.Res;
 import com.googlecode.androidannotations.rclass.IRInnerClass;
 import com.sun.codemodel.JBlock;
-import com.sun.codemodel.JClass;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JFieldRef;
 import com.sun.codemodel.JMethod;
@@ -40,12 +37,11 @@ import com.sun.codemodel.JVar;
 
 public class OptionsMenuProcessor implements ElementProcessor {
 
-	private final SherlockHelper sherlockHelper;
-
+	private final ProcessingEnvironment processingEnv;
 	private final IRClass rClass;
 
 	public OptionsMenuProcessor(ProcessingEnvironment processingEnv, IRClass rClass) {
-		sherlockHelper = new SherlockHelper(new AnnotationHelper(processingEnv));
+		this.processingEnv = processingEnv;
 		this.rClass = rClass;
 	}
 
@@ -57,21 +53,9 @@ public class OptionsMenuProcessor implements ElementProcessor {
 	@Override
 	public void process(Element element, JCodeModel codeModel, EBeansHolder activitiesHolder) {
 		EBeanHolder holder = activitiesHolder.getRelativeEBeanHolder(element);
-		Classes classes = holder.classes();
 
-		JClass menuClass;
-		JClass menuInflaterClass;
-		String getMenuInflaterMethodName;
-		if (sherlockHelper.usesSherlock(holder)) {
-			menuClass = classes.SHERLOCK_MENU;
-			menuInflaterClass = classes.SHERLOCK_MENU_INFLATER;
-			getMenuInflaterMethodName = "getSupportMenuInflater";
-		} else {
-			menuClass = classes.MENU;
-			menuInflaterClass = classes.MENU_INFLATER;
-			getMenuInflaterMethodName = "getMenuInflater";
-		}
-
+		boolean usesSherlock = new SherlockHelper(processingEnv).usesSherlock(holder);
+		
 		OptionsMenu layoutAnnotation = element.getAnnotation(OptionsMenu.class);
 		int layoutIdValue = layoutAnnotation.value();
 
@@ -80,14 +64,15 @@ public class OptionsMenuProcessor implements ElementProcessor {
 
 		JMethod method = holder.eBean.method(PUBLIC, codeModel.BOOLEAN, "onCreateOptionsMenu");
 		method.annotate(Override.class);
-		JVar menuParam = method.param(menuClass, "menu");
+		JVar menuParam = method.param(holder.refClass(usesSherlock? "com.actionbarsherlock.view.Menu": "android.view.Menu"), "menu");
 
 		JBlock body = method.body();
 
-		JVar menuInflater = body.decl(menuInflaterClass, "menuInflater", invoke(getMenuInflaterMethodName));
+		JVar menuInflater = body.decl(holder.refClass(usesSherlock? "com.actionbarsherlock.view.MenuInflater": "android.view.MenuInflater"), "menuInflater", invoke(usesSherlock? "getSupportMenuInflater": "getMenuInflater"));
 
 		body.invoke(menuInflater, "inflate").arg(optionsMenuId).arg(menuParam);
 
 		body._return(invoke(_super(), method).arg(menuParam));
 	}
+
 }
