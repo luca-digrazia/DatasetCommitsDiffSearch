fd13d412a271197d6586650c73d82364d41dbad0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:51:41 +0200

    introduce new ApiClient which properly encodes URL parts
        authentication of users and searching now uses new ApiClient

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/models/UniversalSearch.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/models/UniversalSearch.java
@@ -1,28 +1,7 @@
-/*
- * Copyright 2013 TORCH UG
- *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- */
 package models;
 
-import com.google.inject.assistedinject.Assisted;
-import com.google.inject.assistedinject.AssistedInject;
 import lib.APIException;
 import lib.ApiClient;
-import lib.timeranges.TimeRange;
 import models.api.responses.DateHistogramResponse;
 import models.api.responses.FieldStatsResponse;
 import models.api.responses.SearchResultResponse;
@@ -33,27 +12,24 @@ import java.io.IOException;
 
 public class UniversalSearch {
 
-    private final ApiClient api;
     private final String query;
-    private final TimeRange timeRange;
+    private final int timerange;
 
-    @AssistedInject
-    private UniversalSearch(ApiClient api, @Assisted TimeRange timeRange, @Assisted String query) {
-        this.api = api;
+    public UniversalSearch(String query, int timerange) {
         this.query = query;
-        this.timeRange = timeRange;
+        this.timerange = timerange;
     }
 
     public SearchResult search() throws IOException, APIException {
-        SearchResultResponse response = api.get(SearchResultResponse.class)
-                .path("/search/universal/{0}", timeRange.getType().toString().toLowerCase())
-                .queryParams(timeRange.getQueryParams())
+        SearchResultResponse response = ApiClient.get(SearchResultResponse.class)
+                .path("/search/universal")
                 .queryParam("query", query)
+                .queryParam("timerange", timerange)
                 .execute();
 
         SearchResult result = new SearchResult(
                 query,
-                timeRange,
+                timerange,
                 response.total_results,
                 response.time,
                 response.messages,
@@ -64,25 +40,22 @@ public class UniversalSearch {
     }
 
     public DateHistogramResult dateHistogram(String interval) throws IOException, APIException {
-        DateHistogramResponse response = api.get(DateHistogramResponse.class)
-                .path("/search/universal/{0}/histogram", timeRange.getType().toString().toLowerCase())
+        DateHistogramResponse response = ApiClient.get(DateHistogramResponse.class)
+                .path("/search/universal/histogram")
                 .queryParam("interval", interval)
                 .queryParam("query", query)
-                .queryParams(timeRange.getQueryParams())
+                .queryParam("timerange", timerange)
                 .execute();
         return new DateHistogramResult(response.query, response.time, response.interval, response.results);
     }
 
     public FieldStatsResponse fieldStats(String field) throws IOException, APIException {
-        return api.get(FieldStatsResponse.class)
-                .path("/search/universal/{0}/stats", timeRange.getType().toString().toLowerCase())
+        return ApiClient.get(FieldStatsResponse.class)
+                .path("/search/universal/stats")
                 .queryParam("field", field)
                 .queryParam("query", query)
-                .queryParams(timeRange.getQueryParams())
+                .queryParam("timerange", timerange)
                 .execute();
     }
 
-    public interface Factory {
-        UniversalSearch queryWithRange(String query, TimeRange timeRange);
-    }
 }
