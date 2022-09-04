ab64e78d1acda1de60b11113c56b811e492c897d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:07:17 +0200

    #2967 change expected value for the format parameter

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-openapi/runtime/src/main/java/io/quarkus/smallrye/openapi/runtime/OpenApiServlet.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-openapi/runtime/src/main/java/io/quarkus/smallrye/openapi/runtime/OpenApiServlet.java
@@ -1,33 +1,13 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.quarkus.smallrye.openapi.runtime;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import io.smallrye.openapi.api.OpenApiDocument;
 import io.smallrye.openapi.runtime.io.OpenApiSerializer;
 
 /**
@@ -40,10 +20,8 @@ public class OpenApiServlet extends HttpServlet {
 
     private static final String QUERY_PARAM_FORMAT = "format";
 
-    @Inject
-    OpenApiDocument openApiDocument;
-
-    private final Map<OpenApiSerializer.Format, String> cachedModels = new ConcurrentHashMap<>();
+    public static final String GENERATED_DOC_BASE = "quarkus-generated-openapi-doc.";
+    public static final String BASE_NAME = "META-INF/resources/" + GENERATED_DOC_BASE;
 
     @Override
     protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -61,38 +39,21 @@ public class OpenApiServlet extends HttpServlet {
 
         // Check Accept, then query parameter "format" for JSON; else use YAML.
         if ((accept != null && accept.contains(OpenApiSerializer.Format.JSON.getMimeType())) ||
-                (OpenApiSerializer.Format.JSON.getMimeType().equalsIgnoreCase(formatParam))) {
+                ("JSON".equalsIgnoreCase(formatParam))) {
             format = OpenApiSerializer.Format.JSON;
         }
 
-        String oai = getCachedOaiString(format);
-
         addCorsResponseHeaders(resp);
-        resp.addHeader("Content-Type", format.getMimeType());
-        resp.getOutputStream().print(oai);
-    }
-
-    void setOpenApiDocument(OpenApiDocument document) {
-        this.openApiDocument = document;
-    }
-
-    private String getCachedOaiString(OpenApiSerializer.Format format) {
-        return cachedModels.computeIfAbsent(format, this::getModel);
-    }
-
-    private String getModel(OpenApiSerializer.Format format) {
-        try {
-            return OpenApiSerializer.serialize(this.openApiDocument.get(), format);
-        } catch (IOException ioe) {
-            throw new RuntimeException("Unable to serialize OpenAPI in " + format, ioe);
-        }
+        resp.setHeader("Content-Type", format.getMimeType());
+        resp.setCharacterEncoding("UTF-8");
+        req.getRequestDispatcher("/" + GENERATED_DOC_BASE + format).include(req, resp);
     }
 
     private static void addCorsResponseHeaders(HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Credentials", "true");
-        response.addHeader("Access-Control-Allow-Methods", ALLOWED_METHODS);
-        response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        response.addHeader("Access-Control-Max-Age", "86400");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", ALLOWED_METHODS);
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Max-Age", "86400");
     }
 }
