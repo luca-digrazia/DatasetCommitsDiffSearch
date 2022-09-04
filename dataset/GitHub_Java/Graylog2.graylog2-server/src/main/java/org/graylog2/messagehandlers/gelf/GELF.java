d323f2461a381ad4f27440731cffd8b94076a3c6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:27:10 +0200

    3oris

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/messagehandlers/gelf/GELF.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/messagehandlers/gelf/GELF.java
@@ -151,7 +151,7 @@ public final class GELF {
             rawGELFHeader[i] = message.getData()[i];
         }
 
-        // Make sure that GELF header begins with 30,15 (<3 Boris Erdmann)
+        // Make sure that GELF header begins with 30,15 (<3 boris erdmann)
         if (rawGELFHeader[0] != 30 || rawGELFHeader[1] != 15) {
             throw new InvalidGELFHeaderException("Invalid GELF ID.");
         }
