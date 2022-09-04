29b5eb378ad24b04d014b8f29f6a9a2571848e1b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:53:52 +0200

    Exposes generated manifest artifact (this is the resulting artifact after the merge step) to Skylark.
        --
        PiperOrigin-RevId: 147417110
        MOS_MIGRATED_REVID=147417110

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidSkylarkApiProvider.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidSkylarkApiProvider.java
@@ -15,7 +15,6 @@ package com.google.devtools.build.lib.rules.android;
 
 import com.google.common.base.Function;
 import com.google.common.collect.ImmutableCollection;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
@@ -216,11 +215,10 @@ public class AndroidSkylarkApiProvider extends SkylarkApiProvider {
         return null;
       }
 
-      Artifact idlSourceJar = getIdeInfoProvider().getIdlSourceJar();
       return new OutputJar(
           getIdeInfoProvider().getIdlClassJar(),
           null,
-          idlSourceJar == null ? ImmutableList.<Artifact>of() : ImmutableList.of(idlSourceJar)
+          getIdeInfoProvider().getIdlSourceJar()
       );
     }
   }
