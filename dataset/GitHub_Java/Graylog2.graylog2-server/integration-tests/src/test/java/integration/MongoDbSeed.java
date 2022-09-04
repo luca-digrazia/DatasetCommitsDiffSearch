37050af85b5e3342f1968be3f65ffcaa3e35acfa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:49:57 +0200

    Adding default value for annotation parameter.

--- dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/MongoDbSeed.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/MongoDbSeed.java
@@ -25,5 +25,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MongoDbSeed {
     String[] locations() default {};
-    String database() default "";
+    String database() default "graylog2";
 }
