0f5bdfb064a6352c6bb5d5d13e09281520516709
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:31:08 +0200

    Fix Findbugs issue: make inner class static as it doesn't have any reference to container class.

--- dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/ui/viewmodel/ChapterViewModel.java
+++ dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/ui/viewmodel/ChapterViewModel.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.pedrovgs.effectiveandroidui.ui.viewmodel;
 
 import com.github.pedrovgs.effectiveandroidui.domain.tvshow.Chapter;
@@ -68,13 +83,13 @@ public class ChapterViewModel {
 
   public interface Listener {
 
-    public void onRateChanged(final int rate);
+    void onRateChanged(final int rate);
   }
 
   /**
    * NullObject pattern implementation to avoid listener field null checks inside this view model.
    */
-  private class NullListener implements Listener {
+  private static class NullListener implements Listener {
     @Override public void onRateChanged(int rate) {
       //Empty
     }
