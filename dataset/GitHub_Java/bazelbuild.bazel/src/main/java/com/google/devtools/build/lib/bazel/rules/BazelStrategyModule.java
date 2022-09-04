53f5f387eeab0d0993c558a7e3ad78ac5a586f87
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:08:12 +0200

    Update BazelStrategyModule to use registry methods instead of executorInit where possible.
        Part of the roll-forward of https://github.com/bazelbuild/bazel/commit/37aeabcd39fe326d1c4e55693d8d207f9f7ac6c4.
    
        PiperOrigin-RevId: 303939082

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/BazelStrategyModule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/BazelStrategyModule.java
@@ -15,80 +15,80 @@
 package com.google.devtools.build.lib.bazel.rules;
 
 import com.google.common.collect.ImmutableList;
+import com.google.devtools.build.lib.analysis.actions.FileWriteActionContext;
+import com.google.devtools.build.lib.analysis.actions.TemplateExpansionContext;
 import com.google.devtools.build.lib.buildtool.BuildRequest;
-import com.google.devtools.build.lib.exec.ExecutorBuilder;
-import com.google.devtools.build.lib.rules.android.WriteAdbArgsActionContext;
+import com.google.devtools.build.lib.exec.ExecutionOptions;
+import com.google.devtools.build.lib.exec.ModuleActionContextRegistry;
+import com.google.devtools.build.lib.exec.SpawnCache;
+import com.google.devtools.build.lib.exec.SpawnStrategyRegistry;
+import com.google.devtools.build.lib.remote.RemoteModule;
+import com.google.devtools.build.lib.remote.options.RemoteOptions;
+import com.google.devtools.build.lib.rules.cpp.CppIncludeExtractionContext;
+import com.google.devtools.build.lib.rules.cpp.CppIncludeScanningContext;
 import com.google.devtools.build.lib.runtime.BlazeModule;
 import com.google.devtools.build.lib.runtime.Command;
 import com.google.devtools.build.lib.runtime.CommandEnvironment;
-import com.google.devtools.common.options.Converters.AssignmentConverter;
-import com.google.devtools.common.options.Option;
-import com.google.devtools.common.options.OptionDocumentationCategory;
-import com.google.devtools.common.options.OptionEffectTag;
+import com.google.devtools.build.lib.util.OS;
+import com.google.devtools.build.lib.util.RegexFilter;
 import com.google.devtools.common.options.OptionsBase;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /** Module which registers the strategy options for Bazel. */
 public class BazelStrategyModule extends BlazeModule {
-  /**
-   * Execution options affecting how we execute the build actions (but not their semantics).
-   */
-  public static class BazelExecutionOptions extends OptionsBase {
-    @Option(
-      name = "spawn_strategy",
-      defaultValue = "",
-      category = "strategy",
-      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-      effectTags = {OptionEffectTag.UNKNOWN},
-      help =
-          "Specify how spawn actions are executed by default. "
-              + "'standalone' means run all of them locally without any kind of sandboxing. "
-              + "'sandboxed' means to run them in a sandboxed environment with limited privileges "
-              + "(details depend on platform support)."
-    )
-    public String spawnStrategy;
-
-    @Option(
-      name = "genrule_strategy",
-      defaultValue = "",
-      category = "strategy",
-      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-      effectTags = {OptionEffectTag.UNKNOWN},
-      help =
-          "Specify how to execute genrules."
-              + "'standalone' means run all of them locally."
-              + "'sandboxed' means run them in namespaces based sandbox (available only on Linux)"
-    )
-    public String genruleStrategy;
-
-    @Option(
-      name = "strategy",
-      allowMultiple = true,
-      converter = AssignmentConverter.class,
-      defaultValue = "",
-      category = "strategy",
-      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-      effectTags = {OptionEffectTag.UNKNOWN},
-      help =
-          "Specify how to distribute compilation of other spawn actions. "
-              + "Example: 'Javac=local' means to spawn Java compilation locally. "
-              + "'JavaIjar=sandboxed' means to spawn Java Ijar actions in a sandbox. "
-    )
-    public List<Map.Entry<String, String>> strategy;
-  }
-
   @Override
   public Iterable<Class<? extends OptionsBase>> getCommandOptions(Command command) {
     return "build".equals(command.name())
-        ? ImmutableList.<Class<? extends OptionsBase>>of(BazelExecutionOptions.class)
-        : ImmutableList.<Class<? extends OptionsBase>>of();
+        ? ImmutableList.of(ExecutionOptions.class, RemoteOptions.class)
+        : ImmutableList.of();
   }
 
   @Override
-  public void executorInit(CommandEnvironment env, BuildRequest request, ExecutorBuilder builder) {
-    builder.addActionContext(new WriteAdbArgsActionContext(env.getClientEnv().get("HOME")));
-    BazelExecutionOptions options = env.getOptions().getOptions(BazelExecutionOptions.class);
-    builder.addActionContextConsumer(new BazelActionContextConsumer(options));
+  public void registerActionContexts(
+      ModuleActionContextRegistry.Builder registryBuilder,
+      CommandEnvironment env,
+      BuildRequest buildRequest) {
+    registryBuilder
+        .restrictTo(CppIncludeExtractionContext.class, "")
+        .restrictTo(CppIncludeScanningContext.class, "")
+        .restrictTo(FileWriteActionContext.class, "")
+        .restrictTo(TemplateExpansionContext.class, "")
+        .restrictTo(SpawnCache.class, "");
+  }
+
+  @Override
+  public void registerSpawnStrategies(
+      SpawnStrategyRegistry.Builder registryBuilder, CommandEnvironment env) {
+    ExecutionOptions options = env.getOptions().getOptions(ExecutionOptions.class);
+    RemoteOptions remoteOptions = env.getOptions().getOptions(RemoteOptions.class);
+
+    List<String> spawnStrategies = new ArrayList<>(options.spawnStrategy);
+
+    if (spawnStrategies.isEmpty()) {
+      if (RemoteModule.shouldEnableRemoteExecution(remoteOptions)) {
+        spawnStrategies.add("remote");
+      }
+      spawnStrategies.add("worker");
+      // Sandboxing is not yet available on Windows.
+      if (OS.getCurrent() != OS.WINDOWS) {
+        spawnStrategies.add("sandboxed");
+      }
+      spawnStrategies.add("local");
+    }
+    registryBuilder.setDefaultStrategies(spawnStrategies);
+
+    // By adding this filter before the ones derived from --strategy the latter can override the
+    // former.
+    registryBuilder.addMnemonicFilter("Genrule", options.genruleStrategy);
+
+    for (Map.Entry<String, List<String>> strategy : options.strategy) {
+      registryBuilder.addMnemonicFilter(strategy.getKey(), strategy.getValue());
+    }
+
+    for (Map.Entry<RegexFilter, List<String>> entry : options.strategyByRegexp) {
+      registryBuilder.addDescriptionFilter(entry.getKey(), entry.getValue());
+    }
   }
 }
