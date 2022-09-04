8131912e124aa571d1c11c1ca37458f8265a1d95
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:16:33 +0200

    Fix RestPermissions#readerBasePermissions() and add test for it

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/shared/security/RestPermissions.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/shared/security/RestPermissions.java
@@ -117,6 +117,7 @@ public class RestPermissions implements PluginPermissions {
     public static final String BUNDLE_EXPORT = "bundle:export";
     public static final String JOURNAL_READ = "journal:read";
     public static final String JOURNAL_EDIT = "journal:edit";
+    public static final String COLLECTORS_READ = "collectors:read";
     public static final String ROLES_CREATE = "roles:create";
     public static final String ROLES_READ = "roles:read";
     public static final String ROLES_EDIT = "roles:edit";
@@ -210,6 +211,7 @@ public class RestPermissions implements PluginPermissions {
             .add(create(BUNDLE_EXPORT, ""))
             .add(create(JOURNAL_READ, ""))
             .add(create(JOURNAL_EDIT, ""))
+            .add(create(COLLECTORS_READ, ""))
             .add(create(ROLES_CREATE, ""))
             .add(create(ROLES_READ, ""))
             .add(create(ROLES_EDIT, ""))
@@ -235,8 +237,7 @@ public class RestPermissions implements PluginPermissions {
             THROUGHPUT_READ,
             SAVEDSEARCHES_CREATE,
             SAVEDSEARCHES_EDIT,
-            SAVEDSEARCHES_READ,
-            CLUSTER_CONFIG_ENTRY_READ
+            SAVEDSEARCHES_READ
     ).build();
 
     protected static final Set<Permission> READER_BASE_PERMISSIONS = PERMISSIONS.stream()
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/shared/security/RestPermissionsTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/shared/security/RestPermissionsTest.java
@@ -16,81 +16,15 @@
  */
 package org.graylog2.shared.security;
 
-import com.google.common.collect.ImmutableSet;
-import org.graylog2.plugin.security.RestPermission;
-import org.graylog2.plugin.security.RestPermissionsPlugin;
-import org.junit.Before;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class RestPermissionsTest {
-
-    private RestPermissions permissions;
-
-    private static class PermissionsPlugin implements RestPermissionsPlugin {
-        private final Set<RestPermission> permission;
-
-        public PermissionsPlugin(Set<RestPermission> permissions) {
-            this.permission = permissions;
-        }
-
-        @Override
-        public Set<RestPermission> permissions() {
-            return permission;
-        }
-    }
-
-
-    @Before
-    public void setUp() throws Exception {
-        this.permissions = new RestPermissions();
-    }
-
     @Test
-    public void testPluginPermissions() throws Exception {
-        final ImmutableSet<RestPermission> pluginPermissions = ImmutableSet.of(
-                RestPermission.builder().value("foo:bar").description("bar").build(),
-                RestPermission.builder().value("foo:baz").description("baz").build(),
-                RestPermission.builder().value("hello:world").description("hello").build()
-        );
-        final PermissionsPlugin plugin = new PermissionsPlugin(pluginPermissions);
-        final RestPermissions permissions = new RestPermissions(ImmutableSet.of(plugin));
+    public void testReaderBasePermissions() throws Exception {
+        final RestPermissions restPermissions = new RestPermissions();
 
-        assertThat(permissions.allPermissions().get("foo"))
-                .containsExactly("bar", "baz");
-        assertThat(permissions.allPermissions().get("hello"))
-                .containsExactly("world");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testPluginPermissionsWithDuplicatePermission() throws Exception {
-        final ImmutableSet<RestPermission> pluginPermissions = ImmutableSet.of(
-                RestPermission.builder().value("users:edit").description("User edit").build()
-        );
-        final PermissionsPlugin plugin = new PermissionsPlugin(pluginPermissions);
-
-        new RestPermissions(ImmutableSet.of(plugin));
-    }
-
-    @Test
-    public void testUserSelfEditPermissions() throws Exception {
-        assertThat(permissions.userSelfEditPermissions("john"))
-                .containsExactly("users:edit:john", "users:passwordchange:john");
-    }
-
-    @Test
-    public void testReaderBasePermissionsForUser() throws Exception {
-        final HashSet<String> readerPermissions = new HashSet<>();
-
-        readerPermissions.addAll(permissions.readerBasePermissions());
-        readerPermissions.add("users:edit:john");
-        readerPermissions.add("users:passwordchange:john");
-
-        assertThat(permissions.readerPermissions("john"))
-                .containsOnlyElementsOf(readerPermissions);
+        Assertions.assertThat(restPermissions.readerBasePermissions())
+                .hasSize(RestPermissions.READER_BASE_PERMISSION_SELECTION.size());
     }
 }
