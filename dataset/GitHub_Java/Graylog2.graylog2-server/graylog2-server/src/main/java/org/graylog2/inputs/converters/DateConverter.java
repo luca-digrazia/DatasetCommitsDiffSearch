858ca98e1d0e6dc9908881d6913835ed8de10b48
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:22:37 +0200

    improve date converter: fixes timezone, set default year for parsed dates without year

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/converters/DateConverter.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/converters/DateConverter.java
@@ -46,7 +46,7 @@ public class DateConverter extends Converter {
     @Override
     public Object convert(String value) {
         if (value == null || value.isEmpty()) {
-            return null;
+            return value;
         }
 
         DateTime localNow = new DateTime();
