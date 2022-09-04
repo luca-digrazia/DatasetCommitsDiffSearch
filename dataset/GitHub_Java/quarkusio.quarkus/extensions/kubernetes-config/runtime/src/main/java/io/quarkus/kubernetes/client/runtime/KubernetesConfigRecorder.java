d6b2519bb76f663ba41c2e6e81f4a805ec680186
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:55:49 +0200

    Use the new ConfigValue API instead of the equivalent hack

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes-config/runtime/src/main/java/io/quarkus/kubernetes/client/runtime/KubernetesConfigRecorder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes-config/runtime/src/main/java/io/quarkus/kubernetes/client/runtime/KubernetesConfigRecorder.java
@@ -2,29 +2,49 @@ package io.quarkus.kubernetes.client.runtime;
 
 import java.util.Collections;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 import org.eclipse.microprofile.config.spi.ConfigSourceProvider;
 import org.jboss.logging.Logger;
 
 import io.quarkus.runtime.RuntimeValue;
+import io.quarkus.runtime.TlsConfig;
 import io.quarkus.runtime.annotations.Recorder;
+import io.quarkus.runtime.configuration.AbstractRawDefaultConfigSource;
+import io.smallrye.config.ConfigValue;
+import io.smallrye.config.SmallRyeConfig;
 
 @Recorder
 public class KubernetesConfigRecorder {
 
     private static final Logger log = Logger.getLogger(KubernetesConfigRecorder.class);
 
+    private static final String CONFIG_ENABLED_PROPERTY_NAME = "quarkus.kubernetes-config.enabled";
+
     public RuntimeValue<ConfigSourceProvider> configSources(KubernetesConfigSourceConfig kubernetesConfigSourceConfig,
             KubernetesConfigBuildTimeConfig buildTimeConfig,
-            KubernetesClientBuildConfig clientConfig) {
-        if (!kubernetesConfigSourceConfig.enabled && !buildTimeConfig.secretsEnabled) {
+            KubernetesClientBuildConfig clientConfig,
+            TlsConfig tlsConfig) {
+        if ((!kubernetesConfigSourceConfig.enabled && !buildTimeConfig.secretsEnabled) || isExplicitlyDisabled()) {
             log.debug(
                     "No attempt will be made to obtain configuration from the Kubernetes API server because the functionality has been disabled via configuration");
             return emptyRuntimeValue();
         }
 
         return new RuntimeValue<>(new KubernetesConfigSourceProvider(kubernetesConfigSourceConfig, buildTimeConfig,
-                KubernetesClientUtils.createClient(clientConfig)));
+                KubernetesClientUtils.createClient(clientConfig, tlsConfig)));
+    }
+
+    private boolean isExplicitlyDisabled() {
+        SmallRyeConfig smallRyeConfig = (SmallRyeConfig) ConfigProvider.getConfig();
+        ConfigValue configValue = smallRyeConfig.getConfigValue(CONFIG_ENABLED_PROPERTY_NAME);
+        if (AbstractRawDefaultConfigSource.NAME.equals(configValue.getConfigSourceName())) {
+            return false;
+        }
+        if (configValue.getValue() != null) {
+            return !smallRyeConfig.getValue(CONFIG_ENABLED_PROPERTY_NAME, Boolean.class);
+        }
+        return false;
     }
 
     public void warnAboutSecrets(KubernetesConfigSourceConfig config, KubernetesConfigBuildTimeConfig buildTimeConfig) {
