d7c8a8b19e6e9b44f084af61cb1432dd2d7e2e2f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:29:30 +0200

    Check @Id usage inside Hibernate with Panache

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/panache/hibernate-orm-panache/deployment/src/main/java/io/quarkus/hibernate/orm/panache/deployment/PanacheResourceProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/panache/hibernate-orm-panache/deployment/src/main/java/io/quarkus/hibernate/orm/panache/deployment/PanacheResourceProcessor.java
@@ -134,7 +134,7 @@ public final class PanacheResourceProcessor {
                 modelEnhancer.collectFields(classInfo);
         }
         for (String modelClass : modelClasses) {
-            transformers.produce(new BytecodeTransformerBuildItem(true, modelClass, modelEnhancer));
+            transformers.produce(new BytecodeTransformerBuildItem(modelClass, modelEnhancer));
 
             // lookup for `@NamedQuery` on the hierarchy and produce NamedQueryEntityClassBuildStep
             Set<String> typeNamedQueries = new HashSet<>();
