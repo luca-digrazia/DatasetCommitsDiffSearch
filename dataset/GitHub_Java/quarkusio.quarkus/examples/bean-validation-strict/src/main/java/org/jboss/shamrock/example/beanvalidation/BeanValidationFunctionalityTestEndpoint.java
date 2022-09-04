c23421bd06a718f93ab563f649d6ebded165d960
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:04:25 +0200

    Add more tests, most notably to test EL and type argument constraints

--- dataset/GitHub_Java/quarkusio.quarkus/examples/bean-validation-strict/src/main/java/org/jboss/shamrock/example/beanvalidation/BeanValidationFunctionalityTestEndpoint.java
+++ dataset/GitHub_Java/quarkusio.quarkus/examples/bean-validation-strict/src/main/java/org/jboss/shamrock/example/beanvalidation/BeanValidationFunctionalityTestEndpoint.java
@@ -2,6 +2,10 @@ package org.jboss.shamrock.example.beanvalidation;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,15 +16,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Email;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import org.hibernate.validator.constraints.Length;
 
 @WebServlet(name = "BeanValidationFunctionalityTestEndpoint", urlPatterns = "/bean-validation/testfunctionality")
 public class BeanValidationFunctionalityTestEndpoint extends HttpServlet {
 
     @Inject
-    private Validator validator;
+    Validator validator;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -40,8 +47,27 @@ public class BeanValidationFunctionalityTestEndpoint extends HttpServlet {
     public String testFunctionality() {
         ResultBuilder result = new ResultBuilder();
 
-        result.append(validate(new MyBean("Bill Jones", "b")));
-        result.append(validate(new MyBean("Bill Jones", "bill.jones@example.com")));
+        Map<String, List<String>> invalidCategorizedEmails = new HashMap<>();
+        invalidCategorizedEmails.put("a", Collections.singletonList("b"));
+
+        result.append(validate(new MyBean(
+                "Bill Jones",
+                "b",
+                Collections.singletonList("c"),
+                -4d,
+                invalidCategorizedEmails
+        )));
+
+        Map<String, List<String>> validCategorizedEmails = new HashMap<>();
+        validCategorizedEmails.put("Professional", Collections.singletonList("bill.jones@example.com"));
+
+        result.append(validate(new MyBean(
+                "Bill Jones",
+                "bill.jones@example.com",
+                Collections.singletonList("biji@example.com"),
+                5d,
+                validCategorizedEmails
+        )));
 
         return result.build();
     }
@@ -54,7 +80,7 @@ public class BeanValidationFunctionalityTestEndpoint extends HttpServlet {
         }
 
         return "failed: " + violations.stream()
-                .map(v -> v.getPropertyPath().toString())
+                .map(v -> v.getPropertyPath().toString() + " (" + v.getMessage() + ")")
                 .sorted()
                 .collect(Collectors.joining(", "));
     }
@@ -63,12 +89,22 @@ public class BeanValidationFunctionalityTestEndpoint extends HttpServlet {
 
         private String name;
 
-        @Size(min = 3)
+        @Email
         private String email;
 
-        public MyBean(String name, String email) {
+        private List<@Email String> additionalEmails;
+
+        @DecimalMin("0")
+        private Double score;
+
+        private Map<@Length(min = 3) String, List<@Email String>> categorizedEmails;
+
+        public MyBean(String name, String email, List<String> additionalEmails, Double score, Map<String, List<String>> categorizedEmails) {
             this.name = name;
             this.email = email;
+            this.additionalEmails = additionalEmails;
+            this.score = score;
+            this.categorizedEmails = categorizedEmails;
         }
 
         public String getName() {
@@ -85,6 +121,30 @@ public class BeanValidationFunctionalityTestEndpoint extends HttpServlet {
 
         public void setEmail(String email) {
             this.email = email;
+        }
+
+        public List<String> getAdditionalEmails() {
+            return additionalEmails;
+        }
+
+        public void setAdditionalEmails(List<String> additionalEmails) {
+            this.additionalEmails = additionalEmails;
+        }
+
+        public Double getScore() {
+            return score;
+        }
+
+        public void setScore(Double score) {
+            this.score = score;
+        }
+
+        public Map<String, List<String>> getCategorizedEmails() {
+            return categorizedEmails;
+        }
+
+        public void setCategorizedEmails(Map<String, List<String>> categorizedEmails) {
+            this.categorizedEmails = categorizedEmails;
         }
     }
 
