f79ab98f66519c11fb99aabac9ab13cb4afd2c21
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:28:14 +0200

    More unit tests

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/test/java/org/graylog2/messagehandlers/gelf/SimpleGELFClientHandlerTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/test/java/org/graylog2/messagehandlers/gelf/SimpleGELFClientHandlerTest.java
@@ -42,7 +42,7 @@ public class SimpleGELFClientHandlerTest {
         DatagramPacket gelfMessage = GELFTestHelper.buildZLIBCompressedDatagramPacket(this.originalMessage);
 
         // Let the decompression take place.
-        SimpleGELFClientHandler handler = new SimpleGELFClientHandler(gelfMessage);
+        SimpleGELFClientHandler handler = new SimpleGELFClientHandler(gelfMessage, "foo");
 
         assertEquals(handler.getClientMessage(), this.originalMessage);
     }
@@ -56,7 +56,7 @@ public class SimpleGELFClientHandlerTest {
         DatagramPacket gelfMessage = GELFTestHelper.buildGZIPCompressedDatagramPacket(this.originalMessage);
 
         // Let the decompression take place.
-        SimpleGELFClientHandler handler = new SimpleGELFClientHandler(gelfMessage);
+        SimpleGELFClientHandler handler = new SimpleGELFClientHandler(gelfMessage, "foo");
 
         assertEquals(handler.getClientMessage(), this.originalMessage);
     }
