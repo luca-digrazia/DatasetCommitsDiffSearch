c107e048f615bdc48db48e5cd37fc948e1f8db4c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:31:44 +0200

    Fixing license header.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/security/PasswordAlgorithm.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/security/PasswordAlgorithm.java
@@ -24,6 +24,8 @@ package org.graylog2.plugin.security;
 
 public interface PasswordAlgorithm {
     boolean supports(String hashedPassword);
+
     String hash(String password);
+
     boolean matches(String hashedPassword, String otherPassword);
 }
