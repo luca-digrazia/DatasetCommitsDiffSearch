8985915040b285eaf4872f45803b33adf35e8379
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:46:48 +0200

    remote/http: properly complete user promise
        Fixes #4976, #4935
    
        Closes #4991.
    
        PiperOrigin-RevId: 192269206

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/blobstore/http/AbstractHttpHandler.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/blobstore/http/AbstractHttpHandler.java
@@ -90,11 +90,7 @@ abstract class AbstractHttpHandler<T extends HttpObject> extends SimpleChannelIn
   }
 
   protected String constructHost(URI uri) {
-    boolean includePort =
-        (uri.getPort() > 0)
-            && ((uri.getScheme().equals("http") && uri.getPort() != 80)
-                || (uri.getScheme().equals("https") && uri.getPort() != 443));
-    return uri.getHost() + (includePort ? ":" + uri.getPort() : "");
+    return uri.getHost() + ":" + uri.getPort();
   }
 
   @Override
