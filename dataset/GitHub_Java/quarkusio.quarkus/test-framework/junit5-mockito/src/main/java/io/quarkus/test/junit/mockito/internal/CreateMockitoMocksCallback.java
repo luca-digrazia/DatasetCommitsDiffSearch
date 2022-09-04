04637ddfd6faa5b435ef8d82dcdd0b549f9e8e5b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:41:23 +0200

    Use single mock when backing bean is the same instance
        Fixes: #15411

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit5-mockito/src/main/java/io/quarkus/test/junit/mockito/internal/CreateMockitoMocksCallback.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit5-mockito/src/main/java/io/quarkus/test/junit/mockito/internal/CreateMockitoMocksCallback.java
@@ -2,22 +2,17 @@ package io.quarkus.test.junit.mockito.internal;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
-import javax.enterprise.inject.spi.Bean;
-import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Qualifier;
 
 import org.mockito.Mockito;
 
 import io.quarkus.arc.Arc;
-import io.quarkus.arc.ArcContainer;
 import io.quarkus.arc.ClientProxy;
-import io.quarkus.arc.InjectableBean;
+import io.quarkus.arc.InstanceHandle;
 import io.quarkus.test.junit.callback.QuarkusTestAfterConstructCallback;
 import io.quarkus.test.junit.mockito.InjectMock;
 
@@ -73,26 +68,15 @@ public class CreateMockitoMocksCallback implements QuarkusTestAfterConstructCall
     }
 
     static Object getBeanInstance(Object testInstance, Field field, Class<? extends Annotation> annotationType) {
-        Type fieldType = field.getGenericType();
-        Annotation[] qualifiers = getQualifiers(field);
-        ArcContainer container = Arc.container();
-        BeanManager beanManager = container.beanManager();
-        Set<Bean<?>> beans = beanManager.getBeans(fieldType, qualifiers);
-        if (beans.isEmpty()) {
+        Class<?> fieldClass = field.getType();
+        InstanceHandle<?> instance = Arc.container().instance(fieldClass, getQualifiers(field));
+        if (!instance.isAvailable()) {
             throw new IllegalStateException(
-                    "Invalid use of " + annotationType.getTypeName() + " - could not resolve the bean of type: "
-                            + fieldType.getTypeName() + ". Offending field is " + field.getName() + " of test class "
+                    "Invalid use of " + annotationType.getTypeName() + " - could not determine bean of type: "
+                            + fieldClass + ". Offending field is " + field.getName() + " of test class "
                             + testInstance.getClass());
         }
-        Bean<?> bean = beanManager.resolve(beans);
-        if (!beanManager.isNormalScope(bean.getScope())) {
-            throw new IllegalStateException(
-                    "Invalid use of " + annotationType.getTypeName()
-                            + " - the injected bean does not declare a CDI normal scope but: " + bean.getScope().getName()
-                            + ". Offending field is " + field.getName() + " of test class "
-                            + testInstance.getClass());
-        }
-        return container.instance((InjectableBean<?>) bean).get();
+        return instance.get();
     }
 
     static Annotation[] getQualifiers(Field fieldToMock) {
