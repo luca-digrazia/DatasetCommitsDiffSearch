3b5c8a72003cb6b6125c5271d5c2fa5b84e30aae
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:07:41 +0200

    Fix NPE when trying to stop a non-running Groovy Shell Service

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/initializers/GroovyShellSetupService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/initializers/GroovyShellSetupService.java
@@ -1,27 +1,24 @@
-/*
- * Copyright 2012-2014 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.graylog2.shared.initializers;
 
 import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.AbstractIdleService;
-import com.iterative.groovy.service.GroovyShellService;
+import me.bazhenov.groovysh.GroovyShellService;
 import org.graylog2.plugin.BaseConfiguration;
 import org.graylog2.shared.bindings.InstantiationService;
 
@@ -29,9 +26,6 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Map;
 
-/**
- * @author Dennis Oelkers <dennis@torch.sh>
- */
 @Singleton
 public class GroovyShellSetupService extends AbstractIdleService {
     private final GroovyShellService groovyShellService = new GroovyShellService();
@@ -50,14 +44,15 @@ public class GroovyShellSetupService extends AbstractIdleService {
 
     @Override
     protected void startUp() throws Exception {
-        if (!configuration.isGroovyShellEnable())
-            return;
-
-        groovyShellService.start();
+        if (configuration.isGroovyShellEnable()) {
+            groovyShellService.start();
+        }
     }
 
     @Override
     protected void shutDown() throws Exception {
-        groovyShellService.destroy();
+        if(configuration.isGroovyShellEnable()) {
+            groovyShellService.destroy();
+        }
     }
 }
