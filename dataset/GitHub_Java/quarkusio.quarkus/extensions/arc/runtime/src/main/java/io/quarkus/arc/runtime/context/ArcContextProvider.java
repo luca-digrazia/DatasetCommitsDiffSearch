5c78157944ad38733794344375ac8c88e1c20b0a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:09:13 +0200

    Optimize ArcContextProvider
        - reuse snapshot instances if possible

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/arc/runtime/src/main/java/io/quarkus/arc/runtime/context/ArcContextProvider.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/arc/runtime/src/main/java/io/quarkus/arc/runtime/context/ArcContextProvider.java
@@ -66,6 +66,12 @@ public class ArcContextProvider implements ThreadContextProvider {
         return ThreadContext.CDI;
     }
 
+    // see ThreadContextController#endContext()
+    private static void deactivateRequestContext() throws IllegalStateException {
+        // Only deactivate the context
+        Arc.container().requestContext().deactivate();
+    }
+
     private static final class ClearContextSnapshot implements ThreadContextSnapshot {
 
         @Override
@@ -86,7 +92,7 @@ public class ArcContextProvider implements ThreadContextProvider {
             } else {
                 // context not active, activate blank one, deactivate afterwards
                 requestContext.activate();
-                return requestContext::deactivate;
+                return ArcContextProvider::deactivateRequestContext;
             }
         }
     }
@@ -149,7 +155,7 @@ public class ArcContextProvider implements ThreadContextProvider {
             } else {
                 // context not active, activate and pass it new instance, deactivate afterwards
                 requestContext.activate(state);
-                return requestContext::deactivate;
+                return ArcContextProvider::deactivateRequestContext;
             }
         }
 
