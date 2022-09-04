428f9485c1a323a49c09fdeb44f3cce4784961f6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:26:34 +0200

    Changed behaviour of pinch and quickzoom gestures as image reaches edge of screen for smoother zoom. Restructured debug.

--- dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
+++ dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
@@ -2629,14 +2629,6 @@ public class SubsamplingScaleImageView extends View {
     }
 
     /**
-     * Check if an image has been set. The image may not have been loaded and displayed yet.
-     * @return If an image is currently set.
-     */
-    public boolean hasImage() {
-        return uri != null || bitmap != null;
-    }
-
-    /**
      * {@inheritDoc}
      */
     @Override
