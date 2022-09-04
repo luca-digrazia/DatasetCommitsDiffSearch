9364c1e91c4b4ac6e17f8ef7369d79f7f931dd23
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:49:47 +0200

    allow subclasses for @Click

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/Click.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/Click.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2013 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -21,21 +21,64 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Should be used on click listener methods in activity classes
+ * <p>
+ * This annotation is intended to be used on methods to receive events defined
+ * by {@link android.view.View.OnClickListener#onClick(android.view.View)} when
+ * the view has been clicked by the user.
+ * </p>
+ * <p>
+ * The annotation value should be one or several of R.id.* fields. If not set,
+ * the method name will be used as the R.id.* field name.
+ * </p>
+ * <p>
+ * The method MAY have one parameter:
+ * </p>
+ * <ul>
+ * <li>A {@link android.view.View} (or a subclass) parameter to know which view
+ * has been clicked</li>
+ * </ul>
+ * <blockquote>
  * 
- * The method may have zero or one parameter, that MUST be of type
- * android.view.View .
+ * Example :
  * 
- * The annotation value should be one of R.id.* fields. If not set, the method
- * name will be used as the R.id.* field name.
+ * <pre>
+ * &#064;Click(<b>R.id.myButton</b>)
+ * void clickOnMyButton() {
+ * 	// Something Here
+ * }
  * 
+ * &#064;Click
+ * void <b>myButton</b>Clicked(View view) {
+ * 	// Something Here
+ * }
+ * 
+ * &#064;Click
+ * void <b>myButton</b>Clicked(Button view) {
+ * 	// Something Here
+ * }
+ * </pre>
+ * 
+ * </blockquote>
+ * 
+ * @see Touch
+ * @see LongClick
  */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
 public @interface Click {
 
+	/**
+	 * The R.id.* fields which refer to the Views.
+	 * 
+	 * @return the ids of the Views
+	 */
 	int[] value() default ResId.DEFAULT_VALUE;
 
+	/**
+	 * The resource names as strings which refer to the Views.
+	 * 
+	 * @return the resource names of the Views
+	 */
 	String[] resName() default "";
 
 }
--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/handler/ClickHandler.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/handler/ClickHandler.java
@@ -1,38 +1,57 @@
+/**
+ * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.androidannotations.handler;
 
-import com.sun.codemodel.*;
-import org.androidannotations.annotations.Click;
-import org.androidannotations.model.AnnotationElements;
-import org.androidannotations.validation.IsValid;
+import java.util.List;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
-import java.util.List;
 
-public class ClickHandler extends AbstractListenerHandler {
+import org.androidannotations.annotations.Click;
+import org.androidannotations.helper.CanonicalNameConstants;
+import org.androidannotations.holder.EComponentWithViewSupportHolder;
+import org.androidannotations.model.AnnotationElements;
+import org.androidannotations.process.IsValid;
+
+import com.sun.codemodel.JBlock;
+import com.sun.codemodel.JClass;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JInvocation;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JVar;
+
+public class ClickHandler extends AbstractViewListenerHandler {
 
 	public ClickHandler(ProcessingEnvironment processingEnvironment) {
 		super(Click.class, processingEnvironment);
 	}
 
 	@Override
-	public boolean validate(Element element, AnnotationElements validatedElements) {
-		IsValid valid = new IsValid();
-
-		if (!super.validate(element, validatedElements)) {
-			valid.invalidate();
-		}
+	public void validate(Element element, AnnotationElements validatedElements, IsValid valid) {
+		super.validate(element, validatedElements, valid);
 
 		ExecutableElement executableElement = (ExecutableElement) element;
 
 		validatorHelper.returnTypeIsVoid(executableElement, valid);
 
-		validatorHelper.param.zeroOrOneViewParameter(executableElement, valid);
-
-		return valid.isValid();
+		validatorHelper.param.extendsType(CanonicalNameConstants.VIEW).optional().validate(executableElement, valid);
 	}
 
 	@Override
@@ -41,13 +60,13 @@ public class ClickHandler extends AbstractListenerHandler {
 	}
 
 	@Override
-	protected void processParameters(JMethod listenerMethod, JInvocation call, List<? extends VariableElement> parameters) {
+	protected void processParameters(EComponentWithViewSupportHolder holder, JMethod listenerMethod, JInvocation call, List<? extends VariableElement> parameters) {
 		boolean hasItemParameter = parameters.size() == 1;
 
 		JVar viewParam = listenerMethod.param(classes().VIEW, "view");
 
 		if (hasItemParameter) {
-			call.arg(viewParam);
+			call.arg(castArgumentIfNecessary(holder, CanonicalNameConstants.VIEW, viewParam, parameters.get(0)));
 		}
 	}
 
