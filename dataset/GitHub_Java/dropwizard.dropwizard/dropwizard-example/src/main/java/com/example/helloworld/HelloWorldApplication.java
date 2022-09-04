5e1426788427a2406b2987989cbef2a372e7c9d6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:37:40 +0200

    example of filtering

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -4,16 +4,13 @@ import com.example.helloworld.auth.ExampleAuthenticator;
 import com.example.helloworld.cli.RenderCommand;
 import com.example.helloworld.core.Person;
 import com.example.helloworld.core.Template;
-import com.example.helloworld.core.User;
 import com.example.helloworld.db.PersonDAO;
 import com.example.helloworld.filter.DateNotSpecifiedFilterFactory;
 import com.example.helloworld.health.TemplateHealthCheck;
 import com.example.helloworld.resources.*;
-
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
-import io.dropwizard.auth.AuthFactory;
-import io.dropwizard.auth.basic.BasicAuthFactory;
+import io.dropwizard.auth.basic.BasicAuthProvider;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.migrations.MigrationsBundle;
@@ -62,9 +59,8 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
         environment.healthChecks().register("template", new TemplateHealthCheck(template));
         environment.jersey().getResourceConfig().getResourceFilterFactories().add(new DateNotSpecifiedFilterFactory());
 
-        environment.jersey().register(AuthFactory.binder(new BasicAuthFactory<>(new ExampleAuthenticator(),
-                                                                 "SUPER SECRET STUFF",
-                                                                 User.class)));
+        environment.jersey().register(new BasicAuthProvider<>(new ExampleAuthenticator(),
+                                                              "SUPER SECRET STUFF"));
         environment.jersey().register(new HelloWorldResource(template));
         environment.jersey().register(new ViewResource());
         environment.jersey().register(new ProtectedResource());
