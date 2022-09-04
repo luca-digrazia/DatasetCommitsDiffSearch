7c1f7926111547366b820da4db3d7b42b3fb32b3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:03:53 +0200

    Added helper methods for JerseyClient.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-client/src/main/java/com/yammer/dropwizard/client/JerseyClient.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-client/src/main/java/com/yammer/dropwizard/client/JerseyClient.java
@@ -1,6 +1,5 @@
 package com.yammer.dropwizard.client;
 
-import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.client.apache4.ApacheHttpClient4;
 import com.sun.jersey.client.apache4.ApacheHttpClient4Handler;
@@ -17,15 +16,7 @@ public class JerseyClient extends ApacheHttpClient4 {
         return resource(uri).accept(acceptedMediaType).get(klass);
     }
 
-    public <T> T get(URI uri, MediaType acceptedMediaType, GenericType<T> klass) {
-        return resource(uri).accept(acceptedMediaType).get(klass);
-    }
-
     public <T> T put(URI uri, MediaType contentType, Object entity, Class<T> returnType) {
-        return resource(uri).type(contentType).put(returnType, entity);
-    }
-
-    public <T> T put(URI uri, MediaType contentType, Object entity, GenericType<T> returnType) {
         return resource(uri).type(contentType).put(returnType, entity);
     }
 
@@ -33,15 +24,7 @@ public class JerseyClient extends ApacheHttpClient4 {
         return resource(uri).type(contentType).post(returnType, entity);
     }
 
-    public <T> T post(URI uri, MediaType contentType, Object entity, GenericType<T> returnType) {
-        return resource(uri).type(contentType).post(returnType, entity);
-    }
-
     public <T> T delete(URI uri, Class<T> klass) {
-        return resource(uri).delete(klass);
-    }
-
-    public <T> T delete(URI uri, GenericType<T> klass) {
         return resource(uri).delete(klass);
     }
 }
