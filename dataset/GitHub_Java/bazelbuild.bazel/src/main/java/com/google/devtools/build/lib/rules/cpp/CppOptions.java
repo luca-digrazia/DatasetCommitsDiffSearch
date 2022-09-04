7e8182227c6e39ae508dbc0338cc9e132d99669f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:51:56 +0200

    Automated rollback of commit fee9aa08d4214252513d684eb3bca4e3d23a0c3d.
        *** Reason for rollback ***
    
        Removing this because it's less surprising for rule authors if execution configurations start from default flags.
    
        *** Original change description ***
    
        Add getExec method for CppOptions.
    
        Part of work on execution transitions, #7935.
    
        PiperOrigin-RevId: 246562264

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppOptions.java
@@ -918,16 +918,6 @@ public class CppOptions extends FragmentOptions {
     host.dontEnableHostNonhost = dontEnableHostNonhost;
     host.requireCtxInConfigureFeatures = requireCtxInConfigureFeatures;
     host.useStandaloneLtoIndexingCommandLines = useStandaloneLtoIndexingCommandLines;
-
-    // Save host options for further use.
-    host.hostCoptList = hostCoptList;
-    host.hostConlyoptList = hostConlyoptList;
-    host.hostCppCompiler = hostCppCompiler;
-    host.hostCrosstoolTop = hostCrosstoolTop;
-    host.hostCxxoptList = hostCxxoptList;
-    host.hostLibcTopLabel = hostLibcTopLabel;
-    host.hostLinkoptList = hostLinkoptList;
-
     return host;
   }
 
