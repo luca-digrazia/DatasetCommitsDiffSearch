7162e3c792c8d6cea2c9e2a0d5f0a774ac3d1091
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:46:38 +0200

    Adds the new versions of the template substitutions into ios_test.
        --
        PiperOrigin-RevId: 148464084
        MOS_MIGRATED_REVID=148464084

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/TestSupport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/TestSupport.java
@@ -76,10 +76,7 @@ public class TestSupport {
     String runMemleaks =
         ruleContext.getFragment(ObjcConfiguration.class).runMemleaks() ? "true" : "false";
 
-    // TODO(ulfjack): This is missing the action environment, and the inherited parts from both. Is
-    // that intentional? We should either fix it, or clearly document why we're doing that.
-    ImmutableMap<String, String> testEnv =
-        ruleContext.getConfiguration().getTestActionEnvironment().getFixedEnv();
+    ImmutableMap<String, String> testEnv = ruleContext.getConfiguration().getTestEnv();
 
     // The substitutions below are common for simulator and lab device.
     ImmutableList.Builder<Substitution> substitutions =
