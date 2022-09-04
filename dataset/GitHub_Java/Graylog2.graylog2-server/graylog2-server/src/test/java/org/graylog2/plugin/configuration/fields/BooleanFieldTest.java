f03d04cdf1bd6c3622b86b49b1eef1b295e5cebf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:56:30 +0200

    boolean field tests

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/plugin/configuration/fields/BooleanFieldTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/plugin/configuration/fields/BooleanFieldTest.java
@@ -32,44 +32,38 @@ public class BooleanFieldTest {
 
     @Test
     public void testGetFieldType() throws Exception {
-        BooleanField f = new BooleanField("name", "Name", false, "description");
+        BooleanField f = new BooleanField("name", false, "description");
         assertEquals(BooleanField.FIELD_TYPE, f.getFieldType());
     }
 
     @Test
     public void testGetName() throws Exception {
-        BooleanField f = new BooleanField("name", "Name", false, "description");
+        BooleanField f = new BooleanField("name", false, "description");
         assertEquals("name", f.getName());
     }
 
     @Test
-    public void testGetHumanName() throws Exception {
-        BooleanField f = new BooleanField("name", "Name", false, "description");
-        assertEquals("Name", f.getHumanName());
-    }
-
-    @Test
     public void testGetDescription() throws Exception {
-        BooleanField f = new BooleanField("name", "Name", false, "description");
+        BooleanField f = new BooleanField("name", false, "description");
         assertEquals("description", f.getDescription());
     }
 
     @Test
     public void testGetDefaultValue() throws Exception {
-        BooleanField f = new BooleanField("name", "Name", true, "description");
+        BooleanField f = new BooleanField("name", true, "description");
         assertEquals(true, f.getDefaultValue());
     }
 
     @Test
     public void testIsOptional() throws Exception {
-        BooleanField f = new BooleanField("name", "Name", true, "description");
-        assertEquals(ConfigurationField.Optional.OPTIONAL, f.isOptional());
+        BooleanField f = new BooleanField("name", true, "description", ConfigurationField.Optional.NOT_OPTIONAL);
+        assertEquals(ConfigurationField.Optional.NOT_OPTIONAL, f.isOptional());
     }
 
     @Test
     public void testGetAttributes() throws Exception {
         // Boolean field has no attributes.
-        BooleanField f = new BooleanField("name", "Name", true, "description");
+        BooleanField f = new BooleanField("name", true, "description");
         assertNotNull(f.getAttributes());
     }
 
