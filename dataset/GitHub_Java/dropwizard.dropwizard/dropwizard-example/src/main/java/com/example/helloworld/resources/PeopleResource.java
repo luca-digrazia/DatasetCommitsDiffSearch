9edc982e9dc81465bb154908fc3d8d91ec756b3e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:38:01 +0200

    Adding sample tests for the Person and People resources.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/resources/PeopleResource.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-example/src/main/java/com/example/helloworld/resources/PeopleResource.java
@@ -2,11 +2,9 @@ package com.example.helloworld.resources;
 
 import com.example.helloworld.core.Person;
 import com.example.helloworld.db.PersonDAO;
+import io.dropwizard.hibernate.UnitOfWork;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -21,12 +19,13 @@ public class PeopleResource {
     }
 
     @POST
+    @UnitOfWork
     public Person createPerson(Person person) {
-        final long personId = peopleDAO.create(person);
-        return peopleDAO.findById(personId);
+        return peopleDAO.create(person);
     }
 
     @GET
+    @UnitOfWork
     public List<Person> listPeople() {
         return peopleDAO.findAll();
     }
