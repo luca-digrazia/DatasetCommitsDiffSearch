1f597e37a12c46014e0cd8b8f6b80d62eae9d6f6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:41:08 +0200

    Fix default log level
        The default value of the options was still DEBUG.

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/logger/LoggerContext.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/logger/LoggerContext.java
@@ -1,6 +1,5 @@
 /**
- * Copyright (C) 2010-2016 eBusiness Information, Excilys Group
- * Copyright (C) 2016-2017 the AndroidAnnotations project
+ * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -86,9 +85,9 @@ public final class LoggerContext {
 		}
 	}
 
-	public void close(boolean lastRound) {
+	public void close() {
 		for (Appender appender : appenders) {
-			appender.close(lastRound);
+			appender.close();
 		}
 	}
 
