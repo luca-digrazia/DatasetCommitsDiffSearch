1527d6251de906a7d9cbf921cbf9a7b67692961b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:21:44 +0200

    Revert "See #202"
        This reverts commit abad4ef9d4bccc3b0d65f541a009d180f85bba4f.

--- dataset/GitHub_Java/androidannotations.androidannotations/HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyListActivity.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyListActivity.java
@@ -1,37 +1,31 @@
 package com.googlecode.androidannotations.helloworldeclipse;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
+import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.ItemClick;
 import com.googlecode.androidannotations.annotations.ItemLongClick;
 import com.googlecode.androidannotations.annotations.ItemSelect;
-import com.googlecode.androidannotations.annotations.Layout;
-import com.googlecode.androidannotations.annotations.ViewById;
 import com.googlecode.androidannotations.annotations.res.StringArrayRes;
 
-@Layout(R.layout.my_list_activity)
-public class MyListActivity extends Activity {
+// The layout is not set : we use the default layout set in ListActivity
+@EActivity
+public class MyListActivity extends ListActivity {
 
 	@StringArrayRes
 	String[] bestFoods;
-
-	@ViewById
-	ListView list;
 
 	private ListAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, bestFoods);
-
-		list.setAdapter(adapter);
+		setListAdapter(adapter);
 	}
 
 	@ItemClick
