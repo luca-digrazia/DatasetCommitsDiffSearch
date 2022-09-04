03686f7e86b4d98df0e7bce9a9794b11a562a636
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:10:28 +0200

    StringIndexOutOfBoundsException in ApplicationArchiveBuildStep #2747

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/index/ApplicationArchiveBuildStep.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/index/ApplicationArchiveBuildStep.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.deployment.index;
 
 import java.io.FileInputStream;
@@ -83,9 +99,9 @@ public class ApplicationArchiveBuildStep {
         }
 
         List<ApplicationArchive> applicationArchives = scanForOtherIndexes(Thread.currentThread().getContextClassLoader(),
-                markerFiles, root.getArchiveLocation(), additionalApplicationArchiveBuildItem, indexCache);
+                markerFiles, root.getPath(), additionalApplicationArchiveBuildItem, indexCache);
         return new ApplicationArchivesBuildItem(
-                new ApplicationArchiveImpl(appindex.getIndex(), root.getArchiveRoot(), null, false, root.getArchiveLocation()),
+                new ApplicationArchiveImpl(appindex.getIndex(), root.getPath(), null, false, root.getPath()),
                 applicationArchives);
     }
 
--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/test/java/io/quarkus/deployment/index/ApplicationArchiveBuildStepTestCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/test/java/io/quarkus/deployment/index/ApplicationArchiveBuildStepTestCase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.quarkus.deployment.index;
 
 import static io.quarkus.deployment.index.ApplicationArchiveBuildStep.urlToPath;
@@ -13,13 +29,8 @@ public class ApplicationArchiveBuildStepTestCase {
     @Test
     public void testUrlToPath() throws MalformedURLException {
         assertEquals(Paths.get("/a/path"), urlToPath(new URL("jar:file:/a/path!/META-INF/services/my.Service")));
-        assertEquals(Paths.get("/a/path with whitespace"),
-                urlToPath(new URL("jar:file:/a/path%20with%20whitespace!/META-INF/services/my.Service")));
         assertEquals(Paths.get("/a/path"), urlToPath(new URL("file:/a/path/META-INF/services/my.Service")));
-        assertEquals(Paths.get("/a/path with whitespace"),
-                urlToPath(new URL("file:/a/path%20with%20whitespace/META-INF/services/my.Service")));
         assertEquals(Paths.get("/a/path"), urlToPath(new URL("file:/a/path")));
-        assertEquals(Paths.get("/a/path with whitespace"), urlToPath(new URL("file:/a/path%20with%20whitespace")));
     }
 
     @Test(expected = RuntimeException.class)
