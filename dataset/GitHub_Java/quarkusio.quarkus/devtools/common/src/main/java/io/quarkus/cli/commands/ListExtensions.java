170ac1e17f430b762d62623af8956ace0fe2e7c0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:12:09 +0200

    Remove the guide URLs for now

--- dataset/GitHub_Java/quarkusio.quarkus/devtools/common/src/main/java/io/quarkus/cli/commands/ListExtensions.java
+++ dataset/GitHub_Java/quarkusio.quarkus/devtools/common/src/main/java/io/quarkus/cli/commands/ListExtensions.java
@@ -1,12 +1,14 @@
 package io.quarkus.cli.commands;
 
+import static io.quarkus.maven.utilities.MojoUtils.credentials;
 import static io.quarkus.maven.utilities.MojoUtils.getPluginVersion;
 import static io.quarkus.maven.utilities.MojoUtils.loadExtensions;
+import static java.util.stream.Collectors.toList;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -14,22 +16,19 @@ import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.model.Dependency;
+import org.apache.maven.model.DependencyManagement;
+import org.apache.maven.model.Model;
 
-import io.quarkus.cli.commands.file.BuildFile;
-import io.quarkus.cli.commands.writer.ProjectWriter;
 import io.quarkus.dependencies.Extension;
-import io.quarkus.generators.BuildTool;
+import io.quarkus.maven.utilities.QuarkusDependencyPredicate;
 
 public class ListExtensions {
     private static final String FULL_FORMAT = "%-8s %-50s %-50s %-25s%n%s";
-    private static final String CONCISE_FORMAT = "%-50s %-50s";
-    private static final String NAME_FORMAT = "%-50s";
-    private BuildFile buildFile = null;
+    private static final String SIMPLE_FORMAT = "%-50s %-50s";
+    private Model model;
 
-    public ListExtensions(final ProjectWriter writer, final BuildTool buildTool) throws IOException {
-        if (writer != null) {
-            this.buildFile = buildTool.getBuildFile(writer);
-        }
+    public ListExtensions(final Model model) {
+        this.model = model;
     }
 
     public void listExtensions(boolean all, String format, String search) {
@@ -45,26 +44,17 @@ public class ListExtensions {
         if (loadedExtensions.isEmpty()) {
             System.out.println("No extension found with this pattern");
         } else {
+            Consumer<String[]> currentFormatter = "simple".equalsIgnoreCase(format) ? this::simpleFormatter
+                    : this::fullFormatter;
             String extensionStatus = all ? "available" : "installable";
             System.out.println(String.format("%nCurrent Quarkus extensions %s: ", extensionStatus));
-
-            Consumer<String[]> currentFormatter;
-            switch (format.toLowerCase()) {
-                case "name":
-                    currentFormatter = this::nameFormatter;
-                    break;
-                case "full":
-                    currentFormatter = this::fullFormatter;
-                    currentFormatter.accept(new String[] { "Status", "Extension", "ArtifactId", "Updated Version", "Guide" });
-                    break;
-                case "concise":
-                default:
-                    currentFormatter = this::conciseFormatter;
+            if (!"simple".equalsIgnoreCase(format)) {
+                currentFormatter.accept(new String[] { "Status", "Extension", "ArtifactId", "Updated Version", "Guide" });
             }
 
             loadedExtensions.forEach(extension -> display(extension, installed, all, currentFormatter));
 
-            if ("concise".equalsIgnoreCase(format)) {
+            if ("simple".equalsIgnoreCase(format)) {
                 System.out.println("\nTo get more information, append -Dquarkus.extension.format=full to your command line.");
             }
 
@@ -73,28 +63,16 @@ public class ListExtensions {
         }
     }
 
-    public Map<String, Dependency> findInstalled() {
-        if (buildFile != null) {
-            return buildFile.findInstalled();
-        } else {
-            return Collections.emptyMap();
-        }
-    }
-
     private boolean filterBySearch(final Pattern searchPattern, Extension e) {
         return searchPattern.matcher(e.getName()).matches();
     }
 
-    private void conciseFormatter(String[] cols) {
-        System.out.println(String.format(CONCISE_FORMAT, cols[1], cols[2], cols[4]));
+    private void simpleFormatter(String[] cols) {
+        System.out.println(String.format(SIMPLE_FORMAT, cols[1], cols[2], cols[4]));
     }
 
     private void fullFormatter(String[] cols) {
         System.out.println(String.format(FULL_FORMAT, cols[0], cols[1], cols[2], cols[3], cols[4]));
-    }
-
-    private void nameFormatter(String[] cols) {
-        System.out.println(String.format(NAME_FORMAT, cols[2]));
     }
 
     private void display(Extension extension, final Map<String, Dependency> installed, boolean all,
@@ -126,7 +104,7 @@ public class ListExtensions {
     private String extractVersion(final Dependency dependency) {
         String version = dependency != null ? dependency.getVersion() : null;
         if (version != null && version.startsWith("$")) {
-            final String value = (String) buildFile.getProperty(propertyName(version));
+            final String value = (String) model.getProperties().get(propertyName(version));
             if (value != null) {
                 version = value;
             }
@@ -136,6 +114,42 @@ public class ListExtensions {
 
     private String propertyName(final String variable) {
         return variable.substring(2, variable.length() - 1);
+    }
+
+    Map<String, Dependency> findInstalled() {
+        return mapDependencies(model.getDependencies(), loadManaged());
+    }
+
+    private Map<String, Dependency> loadManaged() {
+        final DependencyManagement managed = model.getDependencyManagement();
+        return managed != null ? mapDependencies(managed.getDependencies(), Collections.emptyMap())
+                : Collections.emptyMap();
+    }
+
+    private Map<String, Dependency> mapDependencies(final List<Dependency> dependencies,
+            final Map<String, Dependency> managed) {
+        final Map<String, Dependency> map = new TreeMap<>();
+
+        if (dependencies != null) {
+            final List<Dependency> listed = dependencies.stream()
+                    .filter(new QuarkusDependencyPredicate())
+                    .collect(toList());
+
+            listed.forEach(d -> {
+                if (d.getVersion() == null) {
+                    final Dependency managedDep = managed.get(credentials(d));
+                    if (managedDep != null) {
+                        final String version = managedDep.getVersion();
+                        if (version != null) {
+                            d.setVersion(version);
+                        }
+                    }
+                }
+
+                map.put(credentials(d), d);
+            });
+        }
+        return map;
     }
 
 }
