bd7953b72ac5837add207b602c92f204af1c036c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:46:41 +0200

    Fix broken null check in GelfOutput

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/outputs/GelfOutput.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/outputs/GelfOutput.java
@@ -29,7 +29,6 @@ import org.graylog2.plugin.Message;
 import org.graylog2.plugin.Tools;
 import org.graylog2.plugin.configuration.Configuration;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
-import org.graylog2.plugin.configuration.fields.BooleanField;
 import org.graylog2.plugin.configuration.fields.ConfigurationField;
 import org.graylog2.plugin.configuration.fields.DropdownField;
 import org.graylog2.plugin.configuration.fields.NumberField;
@@ -42,7 +41,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -56,25 +54,20 @@ public class GelfOutput implements MessageOutput {
     private static final String CK_PROTOCOL = "protocol";
     private static final String CK_HOSTNAME = "hostname";
     private static final String CK_PORT = "port";
-    private static final String CK_CONNECT_TIMEOUT = "connect_timeout";
-    private static final String CK_RECONNECT_DELAY = "reconnect_delay";
-    private static final String CK_TCP_NO_DELAY = "tcp_no_delay";
-    private static final String CK_TCP_KEEP_ALIVE = "tcp_keep_alive";
-    private static final String CK_TLS_ENABLED = "tls_enabled";
-    private static final String CK_TLS_VERIFICATION_ENABLED = "tls_verification_enabled";
-    private static final String CK_TLS_TRUST_CERT_CHAIN = "tls_trust_cert_chain";
 
     private final AtomicBoolean isRunning = new AtomicBoolean(false);
 
+    private final Configuration configuration;
     private final GelfTransport transport;
 
     @Inject
     public GelfOutput(@Assisted Configuration configuration) throws MessageOutputConfigurationException {
-        this(buildTransport(configuration));
+        this(configuration, buildTransport(configuration));
     }
 
     @VisibleForTesting
-    GelfOutput(GelfTransport gelfTransport) {
+    GelfOutput(Configuration configuration, GelfTransport gelfTransport) {
+        this.configuration = checkNotNull(configuration);
         this.transport = checkNotNull(gelfTransport);
         isRunning.set(true);
     }
@@ -99,48 +92,13 @@ public class GelfOutput implements MessageOutput {
         final String protocol = configuration.getString(CK_PROTOCOL);
         final String hostname = configuration.getString(CK_HOSTNAME);
         final int port = configuration.getInt(CK_PORT);
-        final int connectTimeout = configuration.getInt(CK_CONNECT_TIMEOUT, 1000);
-        final int reconnectDelay = configuration.getInt(CK_RECONNECT_DELAY, 500);
-        final boolean tcpKeepAlive = configuration.getBoolean(CK_TCP_KEEP_ALIVE, false);
-        final boolean tcpNoDelay = configuration.getBoolean(CK_TCP_NO_DELAY, false);
-        final boolean tlsEnabled = configuration.getBoolean(CK_TLS_ENABLED, false);
-        final boolean tlsVerificationEnabled = configuration.getBoolean(CK_TLS_VERIFICATION_ENABLED, false);
-        final String tlsTrustCertChain = configuration.getString(CK_TLS_TRUST_CERT_CHAIN);
 
         if (isNullOrEmpty(protocol) || isNullOrEmpty(hostname) || !configuration.intIsSet(CK_PORT)) {
             throw new MessageOutputConfigurationException("Protocol and/or hostname missing!");
         }
 
-        if (tlsEnabled && tlsVerificationEnabled && isNullOrEmpty(tlsTrustCertChain)) {
-            throw new MessageOutputConfigurationException("TLS trust certificate chain file missing!");
-        }
-
-        final File tlsTrustCertChainFile;
-        if (tlsEnabled && tlsVerificationEnabled) {
-            tlsTrustCertChainFile = new File(tlsTrustCertChain);
-
-            if (!tlsTrustCertChainFile.isFile() && !tlsTrustCertChainFile.canRead()) {
-                throw new MessageOutputConfigurationException("TLS trust certificate chain file cannot be read!");
-            }
-        } else {
-            tlsTrustCertChainFile = null;
-        }
-
         final GelfConfiguration gelfConfiguration = new GelfConfiguration(hostname, port)
-                .transport(GelfTransports.valueOf(protocol.toUpperCase()))
-                .connectTimeout(connectTimeout)
-                .reconnectDelay(reconnectDelay)
-                .tcpKeepAlive(tcpKeepAlive)
-                .tcpNoDelay(tcpNoDelay);
-
-        if (tlsEnabled) {
-            gelfConfiguration.enableTls();
-
-            if (tlsVerificationEnabled) {
-                gelfConfiguration.enableTlsCertVerification();
-                gelfConfiguration.tlsTrustCertChainFile(tlsTrustCertChainFile);
-            }
-        }
+                .transport(GelfTransports.valueOf(protocol.toUpperCase()));
 
         LOG.debug("Initializing GELF sender and connecting to {}://{}:{}", protocol, hostname, port);
 
@@ -231,21 +189,13 @@ public class GelfOutput implements MessageOutput {
     public static class Config extends MessageOutput.Config {
         @Override
         public ConfigurationRequest getRequestedConfiguration() {
-            final Map<String, String> protocols = ImmutableMap.of(
-                    "TCP", "TCP",
-                    "UDP", "UDP");
             final ConfigurationRequest configurationRequest = new ConfigurationRequest();
             configurationRequest.addField(new TextField(CK_HOSTNAME, "Destination host", "", "This is the hostname of the destination", ConfigurationField.Optional.NOT_OPTIONAL));
             configurationRequest.addField(new NumberField(CK_PORT, "Destination port", 12201, "This is the port of the destination", ConfigurationField.Optional.NOT_OPTIONAL, NumberField.Attribute.IS_PORT_NUMBER));
+            final Map<String, String> protocols = ImmutableMap.of(
+                    "TCP", "TCP",
+                    "UDP", "UDP");
             configurationRequest.addField(new DropdownField(CK_PROTOCOL, "Protocol", "TCP", protocols, "The protocol used to connect", ConfigurationField.Optional.NOT_OPTIONAL));
-            configurationRequest.addField(new NumberField(CK_CONNECT_TIMEOUT, "TCP Connect Timeout", 1000, "Connection timeout for TCP connections in milliseconds", ConfigurationField.Optional.OPTIONAL, NumberField.Attribute.ONLY_POSITIVE));
-            configurationRequest.addField(new NumberField(CK_RECONNECT_DELAY, "TCP Reconnect Delay", 500, "Time to wait between reconnects in milliseconds", ConfigurationField.Optional.OPTIONAL, NumberField.Attribute.ONLY_POSITIVE));
-            configurationRequest.addField(new BooleanField(CK_TCP_NO_DELAY, "TCP No Delay", false, "Whether to use Nagle's algorithm for TCP connections"));
-            configurationRequest.addField(new BooleanField(CK_TCP_KEEP_ALIVE, "TCP Keep Alive", false, "Whether to send TCP keep alive packets"));
-            configurationRequest.addField(new BooleanField(CK_TLS_ENABLED, "Use TLS", false, "Whether to encrypt the connection using TLS"));
-            configurationRequest.addField(new BooleanField(CK_TLS_VERIFICATION_ENABLED, "TLS verification", false, "Whether to verify peers using TLS"));
-            configurationRequest.addField(new TextField(CK_TLS_TRUST_CERT_CHAIN, "TLS Trust Certificate Chain", "", "Local file which contains the trust certificate chain", ConfigurationField.Optional.OPTIONAL));
-
             return configurationRequest;
         }
     }
