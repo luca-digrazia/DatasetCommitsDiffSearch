592bf3ba4f1fdb3082c32d6cb3fe971efb19a871
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:36:05 +0200

    issue #12654 solved add default value for queryPlanCacheMaxSize and defaultNullOrdering

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/HibernateOrmProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/HibernateOrmProcessor.java
@@ -566,7 +566,7 @@ public final class HibernateOrmProcessor {
     @BuildStep
     public void produceLoggingCategories(HibernateOrmConfig hibernateOrmConfig,
             BuildProducer<LogCategoryBuildItem> categories) {
-        if (hibernateOrmConfig.log.bindParam || hibernateOrmConfig.log.bindParameters) {
+        if (hibernateOrmConfig.log.bindParam) {
             categories.produce(new LogCategoryBuildItem("org.hibernate.type.descriptor.sql.BasicBinder", Level.TRACE));
         }
     }
