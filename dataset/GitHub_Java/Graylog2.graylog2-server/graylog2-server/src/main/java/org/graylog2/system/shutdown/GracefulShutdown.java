02c265c9691a0a8e8fdc90ef8efdb9e9339f9918
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:44:23 +0200

    Add GracefulShutdownService (#5738)
        Services can register themselves with the GracefulShutdownService to
        make sure they will be stopped gracefully on server shutdown.
    
        This allows plugins to participate in a graceful server shutdown to
        make sure the server is not terminated before the plugin shutdown
        routine is done.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/system/shutdown/GracefulShutdown.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/system/shutdown/GracefulShutdown.java
@@ -1,91 +1,138 @@
 /**
- * Copyright 2014 Lennart Koopmann <lennart@torch.sh>
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.system.shutdown;
 
-import com.google.common.base.Stopwatch;
-import org.graylog2.caches.Caches;
-import org.graylog2.Core;
-import org.graylog2.buffers.Buffers;
-import org.graylog2.plugin.inputs.InputState;
-import org.graylog2.plugin.inputs.MessageInput;
-import org.graylog2.system.activities.Activity;
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.graylog2.Configuration;
+import org.graylog2.audit.AuditActor;
+import org.graylog2.audit.AuditEventSender;
+import org.graylog2.initializers.BufferSynchronizerService;
+import org.graylog2.plugin.ServerStatus;
+import org.graylog2.shared.initializers.InputSetupService;
+import org.graylog2.shared.initializers.JerseyService;
+import org.graylog2.shared.initializers.PeriodicalsService;
+import org.graylog2.shared.journal.JournalReader;
+import org.graylog2.shared.system.activities.Activity;
+import org.graylog2.shared.system.activities.ActivityWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.concurrent.TimeUnit;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
+import static org.graylog2.audit.AuditEventTypes.NODE_SHUTDOWN_COMPLETE;
+
+@Singleton
 public class GracefulShutdown implements Runnable {
-
     private static final Logger LOG = LoggerFactory.getLogger(GracefulShutdown.class);
+    private static final int SLEEP_SECS = 1;
 
-    public final int SLEEP_SECS = 1;
+    private final Configuration configuration;
+    private final BufferSynchronizerService bufferSynchronizerService;
+    private final PeriodicalsService periodicalsService;
+    private final InputSetupService inputSetupService;
+    private final ServerStatus serverStatus;
+    private final ActivityWriter activityWriter;
+    private final JerseyService jerseyService;
+    private final GracefulShutdownService gracefulShutdownService;
+    private final AuditEventSender auditEventSender;
+    private final JournalReader journalReader;
 
-    private final Core core;
-
-    public GracefulShutdown(Core core) {
-        this.core = core;
+    @Inject
+    public GracefulShutdown(ServerStatus serverStatus,
+                            ActivityWriter activityWriter,
+                            Configuration configuration,
+                            BufferSynchronizerService bufferSynchronizerService,
+                            PeriodicalsService periodicalsService,
+                            InputSetupService inputSetupService,
+                            JerseyService jerseyService,
+                            GracefulShutdownService gracefulShutdownService,
+                            AuditEventSender auditEventSender,
+                            JournalReader journalReader) {
+        this.serverStatus = serverStatus;
+        this.activityWriter = activityWriter;
+        this.configuration = configuration;
+        this.bufferSynchronizerService = bufferSynchronizerService;
+        this.periodicalsService = periodicalsService;
+        this.inputSetupService = inputSetupService;
+        this.jerseyService = jerseyService;
+        this.gracefulShutdownService = gracefulShutdownService;
+        this.auditEventSender = auditEventSender;
+        this.journalReader = journalReader;
     }
 
     @Override
     public void run() {
+        doRun(true);
+    }
+
+    public void runWithoutExit() {
+        doRun(false);
+    }
+
+    private void doRun(boolean exit) {
         LOG.info("Graceful shutdown initiated.");
-        core.getActivityWriter().write(
-                new Activity("Graceful shutdown initiated.", GracefulShutdown.class)
-        );
+        serverStatus.shutdown();
+
+        // Give possible load balancers time to recognize state change. State is DEAD because of HALTING.
+        LOG.info("Node status: [{}]. Waiting <{}sec> for possible load balancers to recognize state change.",
+                serverStatus.getLifecycle(),
+                configuration.getLoadBalancerRecognitionPeriodSeconds());
+        Uninterruptibles.sleepUninterruptibly(configuration.getLoadBalancerRecognitionPeriodSeconds(), TimeUnit.SECONDS);
+
+        activityWriter.write(new Activity("Graceful shutdown initiated.", GracefulShutdown.class));
 
         /*
          * Wait a second to give for example the calling REST call some time to respond
          * to the client. Using a latch or something here might be a bit over-engineered.
          */
-        try {
-            Thread.sleep(SLEEP_SECS*1000);
-        } catch (InterruptedException ignored) { /* nope */ }
+        Uninterruptibles.sleepUninterruptibly(SLEEP_SECS, TimeUnit.SECONDS);
 
-        // Stop all inputs.
-        stopInputs();
+        // Stop REST API service to avoid changes from outside.
+        jerseyService.stopAsync();
 
-        // Wait for empty master caches.
-        Caches.waitForEmptyCaches(core);
+        // stop all inputs so no new messages can come in
+        inputSetupService.stopAsync();
 
-        // Wait for buffers.
-        Buffers.waitForEmptyBuffers(core);
+        jerseyService.awaitTerminated();
+        inputSetupService.awaitTerminated();
 
-        // Shut down. (Note that this will trigger shutdown hooks.)
-        System.exit(0);
-    }
+        journalReader.stopAsync().awaitTerminated();
 
-    private void stopInputs() {
-        for (InputState state : core.inputs().getRunningInputs()) {
-            MessageInput input = state.getMessageInput();
+        // Try to flush all remaining messages from the system
+        bufferSynchronizerService.stopAsync().awaitTerminated();
 
-            LOG.info("Graceful shutdown: Attempting to close input <{}> [{}].", input.getId(), input.getName());
+        // Stop all services that registered with the shutdown service (e.g. plugins)
+        // This must run after the BufferSynchronizerService shutdown to make sure the buffers are empty.
+        gracefulShutdownService.stopAsync();
 
-            Stopwatch s = new Stopwatch().start();
-            input.stop();
-            s.stop();
+        // stop all maintenance tasks
+        periodicalsService.stopAsync().awaitTerminated();
 
-            LOG.info("Input closed. Took [{}ms]", s.elapsed(TimeUnit.MILLISECONDS));
+        // Wait until the shutdown service is done
+        gracefulShutdownService.awaitTerminated();
+
+        auditEventSender.success(AuditActor.system(serverStatus.getNodeId()), NODE_SHUTDOWN_COMPLETE);
+
+        // Shut down hard with no shutdown hooks running.
+        LOG.info("Goodbye.");
+        if (exit) {
+            System.exit(0);
         }
     }
-
 }
