96b7611b1baf9fb34c414f7f9b92bf12cef3e255
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:28:16 +0200

    CC rules that use the :cc_toolchain attribute should also set up toolchain resolution.
        Fixes Bazel's cc_import rule.
    
        PiperOrigin-RevId: 360191200

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCcImportRule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCcImportRule.java
@@ -14,33 +14,21 @@
 
 package com.google.devtools.build.lib.bazel.rules.cpp;
 
-import static com.google.devtools.build.lib.packages.Attribute.attr;
-import static com.google.devtools.build.lib.packages.BuildType.LABEL;
-import static com.google.devtools.build.lib.packages.BuildType.NODEP_LABEL;
 
 import com.google.devtools.build.lib.analysis.BaseRuleClasses;
 import com.google.devtools.build.lib.analysis.RuleDefinition;
 import com.google.devtools.build.lib.analysis.RuleDefinitionEnvironment;
+import com.google.devtools.build.lib.bazel.rules.cpp.BazelCppRuleClasses.CcToolchainRequiringRule;
 import com.google.devtools.build.lib.packages.RuleClass;
-import com.google.devtools.build.lib.packages.RuleClass.Builder;
 import com.google.devtools.build.lib.rules.cpp.CcImportRule;
-import com.google.devtools.build.lib.rules.cpp.CcToolchain;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration;
-import com.google.devtools.build.lib.rules.cpp.CppRuleClasses;
 
 /** Rule definition for the cc_import rule. */
 public final class BazelCcImportRule implements RuleDefinition {
   @Override
-  public RuleClass build(Builder builder, RuleDefinitionEnvironment env) {
+  public RuleClass build(RuleClass.Builder builder, RuleDefinitionEnvironment env) {
     return builder
         .requiresConfigurationFragments(CppConfiguration.class)
-        .add(
-            attr(CcToolchain.CC_TOOLCHAIN_DEFAULT_ATTRIBUTE_NAME, LABEL)
-                .value(CppRuleClasses.ccToolchainAttribute(env)))
-        .add(
-            attr(CcToolchain.CC_TOOLCHAIN_TYPE_ATTRIBUTE_NAME, NODEP_LABEL)
-                .value(CppRuleClasses.ccToolchainTypeAttribute(env)))
-        .add(attr(":stl", LABEL).value(BazelCppRuleClasses.STL))
         .build();
   }
 
@@ -48,7 +36,10 @@ public final class BazelCcImportRule implements RuleDefinition {
   public Metadata getMetadata() {
     return RuleDefinition.Metadata.builder()
         .name("cc_import")
-        .ancestors(BaseRuleClasses.BaseRule.class, CcImportRule.class)
+        .ancestors(
+            BaseRuleClasses.NativeBuildRule.class,
+            CcImportRule.class,
+            CcToolchainRequiringRule.class)
         .factoryClass(BazelCcImport.class)
         .build();
   }
@@ -95,7 +86,7 @@ cc_import(
 )
 </pre>
 
-4. Linking a shared library with <code>system_provided=1</code> (Windows)
+4. Linking a shared library with <code>system_provided=True</code> (Windows)
 <pre class="code">
 cc_import(
   name = "mylib",
