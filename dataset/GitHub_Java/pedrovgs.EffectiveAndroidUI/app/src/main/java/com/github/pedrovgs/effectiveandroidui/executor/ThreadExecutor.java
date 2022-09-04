3671ee4f969c5d013f3debe7f4ea02f381fd15b9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:31:49 +0200

    Add final modifiers to ThreadPoolConstants

--- dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/executor/ThreadExecutor.java
+++ dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/executor/ThreadExecutor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.pedrovgs.effectiveandroidui.executor;
 
 import java.util.concurrent.BlockingQueue;
@@ -19,11 +34,11 @@ import javax.inject.Inject;
  */
 class ThreadExecutor implements Executor {
 
-  private static int CORE_POOL_SIZE = 3;
-  private static int MAX_POOL_SIZE = 5;
-  private static int KEEP_ALIVE_TIME = 120;
-  private static TimeUnit TIME_UNIT = TimeUnit.SECONDS;
-  private static BlockingQueue<Runnable> WORK_QUEUE = new LinkedBlockingQueue<Runnable>();
+  private static final int CORE_POOL_SIZE = 3;
+  private static final int MAX_POOL_SIZE = 5;
+  private static final int KEEP_ALIVE_TIME = 120;
+  private static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
+  private static final BlockingQueue<Runnable> WORK_QUEUE = new LinkedBlockingQueue<Runnable>();
 
   private ThreadPoolExecutor threadPoolExecutor;
 
