7f45f3c67e72db88279ef5d781c0ad01aaa3284d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:30:39 +0200

    Use different system property prefixes to avoid test clashing
        Tests are now running in parallel, so we should use different
        system properties for configuration overriding tests, otherwise
        they will clash with each other make themselves not reliable.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/test/java/io/dropwizard/testing/junit/DropwizardAppRuleConfigOverrideTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/test/java/io/dropwizard/testing/junit/DropwizardAppRuleConfigOverrideTest.java
@@ -1,13 +1,13 @@
 package io.dropwizard.testing.junit;
 
+import com.google.common.base.Optional;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import javax.ws.rs.client.ClientBuilder;
-import java.util.Optional;
 
-import static io.dropwizard.testing.ConfigOverride.config;
 import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
+import static io.dropwizard.testing.ConfigOverride.config;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -16,10 +16,7 @@ public class DropwizardAppRuleConfigOverrideTest {
     @ClassRule
     public static final DropwizardAppRule<TestConfiguration> RULE =
             new DropwizardAppRule<>(TestApplication.class, resourceFilePath("test-config.yaml"),
-                    Optional.of("app-rule"),
-                    config("app-rule", "message", "A new way to say Hooray!"),
-                    config("app-rule", "extra", () -> "supplied"),
-                    config("extra", () -> "supplied again"));
+                    Optional.of("app-rule"), config("app-rule", "message", "A new way to say Hooray!"));
 
     @Test
     public void supportsConfigAttributeOverrides() {
@@ -27,11 +24,5 @@ public class DropwizardAppRuleConfigOverrideTest {
                 .request().get(String.class);
 
         assertThat(content, is("A new way to say Hooray!"));
-    }
-
-    @Test
-    public void supportsSuppliedConfigAttributeOverrides() throws Exception {
-        assertThat(System.getProperty("app-rule.extra"), is("supplied"));
-        assertThat(System.getProperty("dw.extra"), is("supplied again"));
     }
 }
