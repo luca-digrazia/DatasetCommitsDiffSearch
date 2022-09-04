e2bc705dd780434437d27171b6b217653ddf50d4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:16:32 +0200

    Use proper return type for Fragment.onViewCreated(...)

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/EFragmentProcessor.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/EFragmentProcessor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2011 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2012 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -16,28 +16,45 @@
 package com.googlecode.androidannotations.processing;
 
 import static com.googlecode.androidannotations.helper.ModelConstants.GENERATION_SUFFIX;
+import static com.sun.codemodel.JExpr.FALSE;
+import static com.sun.codemodel.JExpr._new;
 import static com.sun.codemodel.JExpr._null;
 import static com.sun.codemodel.JExpr._super;
 import static com.sun.codemodel.JExpr.invoke;
 import static com.sun.codemodel.JMod.FINAL;
 import static com.sun.codemodel.JMod.PRIVATE;
 import static com.sun.codemodel.JMod.PUBLIC;
+import static com.sun.codemodel.JMod.STATIC;
 
 import java.lang.annotation.Annotation;
 
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 
 import com.googlecode.androidannotations.annotations.EFragment;
+import com.googlecode.androidannotations.helper.IdAnnotationHelper;
+import com.googlecode.androidannotations.processing.EBeansHolder.Classes;
+import com.googlecode.androidannotations.rclass.IRClass;
+import com.googlecode.androidannotations.rclass.IRClass.Res;
 import com.sun.codemodel.ClassType;
 import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JClass;
+import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFieldRef;
 import com.sun.codemodel.JFieldVar;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JVar;
 
-public class EFragmentProcessor implements ElementProcessor {
+public class EFragmentProcessor implements GeneratingElementProcessor {
+
+	private final IdAnnotationHelper helper;
+
+	public EFragmentProcessor(ProcessingEnvironment processingEnv, IRClass rClass) {
+		helper = new IdAnnotationHelper(processingEnv, getTarget(), rClass);
+	}
 
 	@Override
 	public Class<? extends Annotation> getTarget() {
@@ -47,39 +64,37 @@ public class EFragmentProcessor implements ElementProcessor {
 	@Override
 	public void process(Element element, JCodeModel codeModel, EBeansHolder eBeansHolder) throws Exception {
 
-		EBeanHolder holder = eBeansHolder.create(element);
-
 		TypeElement typeElement = (TypeElement) element;
 
 		String beanQualifiedName = typeElement.getQualifiedName().toString();
 
 		String generatedBeanQualifiedName = beanQualifiedName + GENERATION_SUFFIX;
 
-		holder.eBean = codeModel._class(PUBLIC | FINAL, generatedBeanQualifiedName, ClassType.CLASS);
+		JDefinedClass generatedClass = codeModel._class(PUBLIC | FINAL, generatedBeanQualifiedName, ClassType.CLASS);
+
+		EBeanHolder holder = eBeansHolder.create(element, getTarget(), generatedClass);
 
 		JClass eBeanClass = codeModel.directClass(beanQualifiedName);
 
-		holder.eBean._extends(eBeanClass);
+		holder.generatedClass._extends(eBeanClass);
 
-		JClass viewClass = holder.refClass("android.view.View");
-		JClass viewGroupClass = holder.refClass("android.view.ViewGroup");
-		JClass layoutInflaterClass = holder.refClass("android.view.LayoutInflater");
-		JClass bundleClass = holder.refClass("android.os.Bundle");
+		Classes classes = holder.classes();
 
 		{
 			// init
-			holder.init = holder.eBean.method(PRIVATE, codeModel.VOID, "init_");
+			holder.init = holder.generatedClass.method(PRIVATE, codeModel.VOID, "init_");
+			holder.init.param(holder.classes().BUNDLE, "savedInstanceState");
 		}
 
 		{
 			// onCreate()
 
-			JMethod onCreate = holder.eBean.method(PUBLIC, codeModel.VOID, "onCreate");
+			JMethod onCreate = holder.generatedClass.method(PUBLIC, codeModel.VOID, "onCreate");
 			onCreate.annotate(Override.class);
-			JVar onCreateSavedInstanceState = onCreate.param(bundleClass, "savedInstanceState");
+			JVar onCreateSavedInstanceState = onCreate.param(classes.BUNDLE, "savedInstanceState");
 			JBlock onCreateBody = onCreate.body();
 
-			onCreateBody.invoke(holder.init);
+			onCreateBody.invoke(holder.init).arg(onCreateSavedInstanceState);
 
 			onCreateBody.invoke(_super(), onCreate).arg(onCreateSavedInstanceState);
 		}
@@ -87,32 +102,55 @@ public class EFragmentProcessor implements ElementProcessor {
 		holder.contextRef = invoke("getActivity");
 
 		// contentView
-		JFieldVar contentView = holder.eBean.field(PRIVATE, viewClass, "contentView_");
+		JFieldVar contentView = holder.generatedClass.field(PRIVATE, classes.VIEW, "contentView_");
 
 		{
 			// afterSetContentView
-			holder.afterSetContentView = holder.eBean.method(PRIVATE, codeModel.VOID, "afterSetContentView_");
+			holder.afterSetContentView = holder.generatedClass.method(PRIVATE, codeModel.VOID, "afterSetContentView_");
 		}
 
 		{
 			// onCreateView()
-			JMethod onCreateView = holder.eBean.method(PUBLIC, viewClass, "onCreateView");
-			JVar inflater = onCreateView.param(layoutInflaterClass, "inflater");
-			JVar container = onCreateView.param(viewGroupClass, "container");
-			JVar savedInstanceState = onCreateView.param(bundleClass, "savedInstanceState");
+			JMethod onCreateView = holder.generatedClass.method(PUBLIC, classes.VIEW, "onCreateView");
+			onCreateView.annotate(Override.class);
+			JVar inflater = onCreateView.param(classes.LAYOUT_INFLATER, "inflater");
+			JVar container = onCreateView.param(classes.VIEW_GROUP, "container");
+			JVar savedInstanceState = onCreateView.param(classes.BUNDLE, "savedInstanceState");
 
 			JBlock body = onCreateView.body();
 			body.assign(contentView, _super().invoke(onCreateView).arg(inflater).arg(container).arg(savedInstanceState));
 
-			body.invoke(holder.afterSetContentView);
+			JFieldRef contentViewId = helper.extractOneAnnotationFieldRef(holder, element, Res.LAYOUT, false);
+
+			if (contentViewId != null) {
+				body._if(contentView.eq(_null())) //
+						._then() //
+						.assign(contentView, inflater.invoke("inflate").arg(contentViewId).arg(container).arg(FALSE));
+			}
 
 			body._return(contentView);
 		}
 
 		{
+			// onViewCreated
+
+			JMethod onViewCreated = holder.generatedClass.method(PUBLIC, codeModel.VOID, "onViewCreated");
+			onViewCreated.annotate(Override.class);
+			JVar view = onViewCreated.param(classes.VIEW, "view");
+			JVar savedInstanceState = onViewCreated.param(classes.BUNDLE, "savedInstanceState");
+
+			JBlock onViewCreatedBody = onViewCreated.body();
+
+			onViewCreatedBody.invoke(_super(), onViewCreated).arg(view).arg(savedInstanceState);
+			
+			onViewCreatedBody.invoke(holder.afterSetContentView);
+		}
+
+
+		{
 			// findViewById
 
-			JMethod findViewById = holder.eBean.method(PUBLIC, viewClass, "findViewById");
+			JMethod findViewById = holder.generatedClass.method(PUBLIC, classes.VIEW, "findViewById");
 			JVar idParam = findViewById.param(codeModel.INT, "id");
 
 			JBlock body = findViewById.body();
@@ -129,5 +167,38 @@ public class EFragmentProcessor implements ElementProcessor {
 			holder.initActivityRef = holder.contextRef;
 		}
 
+		addFragmentBuilder(codeModel, holder, eBeanClass);
+	}
+
+	private void addFragmentBuilder(JCodeModel codeModel, EBeanHolder holder, JClass eBeanClass) throws JClassAlreadyExistsException {
+		JClass bundleClass = holder.classes().BUNDLE;
+
+		{
+			holder.fragmentBuilderClass = holder.generatedClass._class(PUBLIC | STATIC, "FragmentBuilder_");
+			holder.fragmentArgumentsBuilderField = holder.fragmentBuilderClass.field(PRIVATE, bundleClass, "args_");
+
+			{
+				// Constructor
+				JMethod constructor = holder.fragmentBuilderClass.constructor(PRIVATE);
+				JBlock constructorBody = constructor.body();
+				constructorBody.assign(holder.fragmentArgumentsBuilderField, _new(bundleClass));
+			}
+
+			{
+				// build()
+				JMethod method = holder.fragmentBuilderClass.method(PUBLIC, eBeanClass, "build");
+				JBlock body = method.body();
+
+				JVar fragment = body.decl(holder.generatedClass, "fragment_", _new(holder.generatedClass));
+				body.invoke(fragment, "setArguments").arg(holder.fragmentArgumentsBuilderField);
+				body._return(fragment);
+			}
+
+			{
+				// create()
+				JMethod method = holder.generatedClass.method(STATIC | PUBLIC, holder.fragmentBuilderClass, "builder");
+				method.body()._return(_new(holder.fragmentBuilderClass));
+			}
+		}
 	}
 }
