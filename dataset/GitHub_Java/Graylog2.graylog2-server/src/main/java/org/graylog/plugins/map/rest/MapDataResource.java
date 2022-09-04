58c75a74928f87d2feddee28e68b8306e07b2ade
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:17:52 +0200

    Add HTTP API endpoint for map data requests

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog/plugins/map/rest/MapDataResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog/plugins/map/rest/MapDataResource.java
@@ -1,25 +1,9 @@
-/**
- * This file is part of Graylog.
- *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog.plugins.map.rest;
 
 import com.codahale.metrics.annotation.Timed;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.graylog.plugins.map.search.MapDataSearch;
 import org.graylog.plugins.map.search.MapDataSearchRequest;
