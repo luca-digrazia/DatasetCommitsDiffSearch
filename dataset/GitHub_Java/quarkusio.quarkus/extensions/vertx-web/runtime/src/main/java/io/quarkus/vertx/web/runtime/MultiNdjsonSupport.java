5516c3e8bed5a92d2783b0371813fa5b3ca43dc0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:00:33 +0200

    Add test, fix docu, rework header setting

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-web/runtime/src/main/java/io/quarkus/vertx/web/runtime/MultiNdjsonSupport.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-web/runtime/src/main/java/io/quarkus/vertx/web/runtime/MultiNdjsonSupport.java
@@ -34,7 +34,7 @@ public class MultiNdjsonSupport {
     }
 
     public static void subscribeString(Multi<String> multi, RoutingContext rc) {
-        write(multi.map(s -> Buffer.buffer("\"" + s + "\"\n")), rc);
+        write(multi.map(s -> Buffer.buffer("\"" + s + "\"")), rc);
     }
 
     public static void subscribeObject(Multi<Object> multi, RoutingContext rc) {
