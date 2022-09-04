c8455af2ad03cf0a4fd4876aaf35c7b1e018ecc4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:18:46 +0200

    TikaProcessorTest must register a configuration now

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/tika/deployment/src/test/java/io/quarkus/tika/deployment/TikaProcessorTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/tika/deployment/src/test/java/io/quarkus/tika/deployment/TikaProcessorTest.java
@@ -4,101 +4,68 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
-import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.runtime.configuration.QuarkusConfigFactory;
+import io.smallrye.config.SmallRyeConfig;
+import io.smallrye.config.SmallRyeConfigBuilder;
 
 public class TikaProcessorTest {
 
-    @RegisterExtension
-    static final QuarkusUnitTest quarkusUnitTest = new QuarkusUnitTest();
+    // We must register a configuration otherwise we'll get an exception.
 
-    @Test
-    public void testPDFParserName() throws Exception {
-        Set<String> names = getParserNames(null, "pdf");
-        assertEquals(1, names.size());
-        assertTrue(names.contains("org.apache.tika.parser.pdf.PDFParser"));
+    static volatile SmallRyeConfig config;
+
+    @BeforeAll
+    public static void setItUp() {
+        final SmallRyeConfigBuilder builder = new SmallRyeConfigBuilder();
+        builder.addDefaultSources();
+        builder.addDiscoveredConverters();
+        builder.addDiscoveredSources();
+        config = builder.build();
+        QuarkusConfigFactory.setConfig(config);
+        ConfigProviderResolver cpr = ConfigProviderResolver.instance();
+        final Config existingConfig = cpr.getConfig();
+        if (existingConfig != TikaProcessorTest.config) {
+            cpr.releaseConfig(existingConfig);
+        }
     }
 
-    @Test
-    public void testODFParserName() throws Exception {
-        Set<String> names = getParserNames(null, "odf");
-        assertEquals(1, names.size());
-        assertTrue(names.contains("org.apache.tika.parser.odf.OpenDocumentParser"));
+    @AfterAll
+    public static void tearItDown() {
+        ConfigProviderResolver cpr = ConfigProviderResolver.instance();
+        cpr.releaseConfig(config);
     }
 
     @Test
     public void testSupportedParserNames() throws Exception {
-        Set<String> names = getParserNames(null, "pdf,odf");
-        assertEquals(2, names.size());
-        assertTrue(names.contains("org.apache.tika.parser.pdf.PDFParser"));
-        assertTrue(names.contains("org.apache.tika.parser.odf.OpenDocumentParser"));
+        Optional<String> parserNames = Optional.of("pdf");
+        List<String> names = TikaProcessor.getSupportedParserNames(parserNames);
+        assertEquals(1, names.size());
+        assertEquals("org.apache.tika.parser.pdf.PDFParser", names.get(0));
     }
 
     @Test
     public void testResolvableCustomAbbreviation() throws Exception {
-        Set<String> names = getParserConfig(null, "pdf,opendoc", Collections.emptyMap(),
-                Collections.singletonMap("opendoc",
-                        "org.apache.tika.parser.odf.OpenDocumentParser")).keySet();
+        Optional<String> parserNames = Optional.of("pdf,opendoc");
+        List<String> names = TikaProcessor.getSupportedParserNames(parserNames);
         assertEquals(2, names.size());
         assertTrue(names.contains("org.apache.tika.parser.pdf.PDFParser"));
         assertTrue(names.contains("org.apache.tika.parser.odf.OpenDocumentParser"));
     }
 
     @Test
-    public void testPdfParserConfig() throws Exception {
-        Map<String, List<TikaProcessor.TikaParserParameter>> parserConfig = getParserConfig(null, "pdf",
-                Collections.singletonMap("pdf",
-                        Collections.singletonMap("sort-by-position", "true")),
-                Collections.emptyMap());
-        assertEquals(1, parserConfig.size());
-
-        String pdfParserFullName = "org.apache.tika.parser.pdf.PDFParser";
-        assertEquals(1, parserConfig.get(pdfParserFullName).size());
-        assertEquals("sortByPosition", parserConfig.get(pdfParserFullName).get(0).getName());
-        assertEquals("true", parserConfig.get(pdfParserFullName).get(0).getValue());
-    }
-
-    @Test
-    public void testTesseractParserConfig() throws Exception {
-        String ocrParserFullName = "org.apache.tika.parser.ocr.TesseractOCRParser";
-        Map<String, List<TikaProcessor.TikaParserParameter>> parserConfig = getParserConfig(null, "ocr",
-                Collections.singletonMap("ocr",
-                        Collections.singletonMap("tesseract-path", "/opt/tesseract/")),
-                Collections.singletonMap("ocr", ocrParserFullName));
-        assertEquals(1, parserConfig.size());
-
-        assertEquals(1, parserConfig.get(ocrParserFullName).size());
-        assertEquals("tesseractPath", parserConfig.get(ocrParserFullName).get(0).getName());
-        assertEquals("/opt/tesseract/", parserConfig.get(ocrParserFullName).get(0).getValue());
-    }
-
-    @Test
-    public void testUnknownParserConfig() throws Exception {
-        String ocrParserFullName = "org.apache.tika.parser.ocr.TesseractOCRParser";
-        try {
-            Map<String, List<TikaProcessor.TikaParserParameter>> parserConfig = getParserConfig(null, "ocr",
-                    Collections.singletonMap("ocr",
-                            Collections.singletonMap("tesseract-unknown-opt", "/opt/tesseract/")),
-                    Collections.singletonMap("ocr", ocrParserFullName));
-        } catch (Exception e) {
-            // expected
-            assertEquals("Parser org.apache.tika.parser.ocr.TesseractOCRParser has no tesseractUnknownOpt property",
-                    e.getMessage());
-        }
-    }
-
-    @Test
     public void testUnresolvableCustomAbbreviation() throws Exception {
+        Optional<String> parserNames = Optional.of("classparser");
         try {
-            getParserNames(null, "classparser");
+            TikaProcessor.getSupportedParserNames(parserNames);
             fail("'classparser' is not resolvable");
         } catch (IllegalStateException ex) {
             // expected
@@ -107,32 +74,8 @@ public class TikaProcessorTest {
 
     @Test
     public void testAllSupportedParserNames() throws Exception {
-        assertEquals(77, getParserNames(null, null).size());
-    }
-
-    @Test
-    public void testSupportedParserNamesWithTikaConfigPath() throws Exception {
-        Set<String> names = getParserNames("tika-config.xml", "pdf");
-        assertEquals(77, names.size());
-    }
-
-    @Test
-    public void testUnhyphenation() {
-        assertEquals("sortByPosition", TikaProcessor.unhyphenate("sort-by-position"));
-        assertEquals("position", TikaProcessor.unhyphenate("position"));
-    }
-
-    private Set<String> getParserNames(String tikaConfigPath, String parsers) throws Exception {
-        return TikaProcessor.getSupportedParserConfig(
-                Optional.ofNullable(tikaConfigPath), Optional.ofNullable(parsers),
-                Collections.emptyMap(), Collections.emptyMap()).keySet();
-    }
-
-    private Map<String, List<TikaProcessor.TikaParserParameter>> getParserConfig(String tikaConfigPath, String parsers,
-            Map<String, Map<String, String>> parserParamMaps,
-            Map<String, String> parserAbbreviations) throws Exception {
-        return TikaProcessor.getSupportedParserConfig(
-                Optional.ofNullable(tikaConfigPath), Optional.ofNullable(parsers),
-                parserParamMaps, parserAbbreviations);
+        Optional<String> parserNames = Optional.ofNullable(null);
+        List<String> names = TikaProcessor.getSupportedParserNames(parserNames);
+        assertEquals(68, names.size());
     }
 }
