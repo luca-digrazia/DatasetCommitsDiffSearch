935234fb61a164c8c28ee1252c704e22284a2312
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:49:17 +0200

    Add setter for console log format.
        Closes #267.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/LoggingConfiguration.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/LoggingConfiguration.java
@@ -1,89 +1,199 @@
 package com.yammer.dropwizard.config;
 
+import ch.qos.logback.classic.Level;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
-import com.yammer.dropwizard.json.LevelDeserializer;
-import com.yammer.dropwizard.util.Size;
-import org.apache.log4j.Level;
-import org.codehaus.jackson.annotate.JsonProperty;
-import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import com.yammer.dropwizard.validation.ValidationMethod;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
 
-@SuppressWarnings({"FieldCanBeLocal", "FieldMayBeFinal"})
+@SuppressWarnings("UnusedDeclaration")
 public class LoggingConfiguration {
+    static final TimeZone UTC = TimeZone.getTimeZone("UTC");
+
     public static class ConsoleConfiguration {
         @JsonProperty
         private boolean enabled = true;
 
         @NotNull
         @JsonProperty
-        @JsonDeserialize(using = LevelDeserializer.class)
         private Level threshold = Level.ALL;
+
+        @NotNull
+        @JsonProperty
+        private TimeZone timeZone = UTC;
+
+        @JsonProperty
+        private String logFormat;
 
         public boolean isEnabled() {
             return enabled;
         }
 
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
         public Level getThreshold() {
             return threshold;
         }
+
+        public void setThreshold(Level threshold) {
+            this.threshold = threshold;
+        }
+
+        public TimeZone getTimeZone() {
+            return timeZone;
+        }
+
+        public void setTimeZone(TimeZone timeZone) {
+            this.timeZone = timeZone;
+        }
+
+        public Optional<String> getLogFormat() {
+            return Optional.fromNullable(logFormat);
+        }
+
+        public void setLogFormat(String logFormat) {
+            this.logFormat = logFormat;
+        }
     }
 
-    @SuppressWarnings("CanBeFinal")
     public static class FileConfiguration {
         @JsonProperty
         private boolean enabled = false;
 
         @NotNull
         @JsonProperty
-        @JsonDeserialize(using = LevelDeserializer.class)
         private Level threshold = Level.ALL;
 
-        @NotNull
         @JsonProperty
-        private String filenamePattern = "./logs/example.log";
+        private String currentLogFilename;
 
-        @NotNull
         @JsonProperty
-        private Size maxFileSize = Size.megabytes(50);
+        private boolean archive = true;
+
+        @JsonProperty
+        private String archivedLogFilenamePattern;
 
         @Min(1)
         @Max(50)
         @JsonProperty
-        private int retainedFileCount = 5;
+        private int archivedFileCount = 5;
+
+        @NotNull
+        @JsonProperty
+        private TimeZone timeZone = UTC;
+
+        @JsonProperty
+        private String logFormat;
+
+        @ValidationMethod(message = "must have logging.file.archivedLogFilenamePattern if logging.file.archive is true")
+        public boolean isValidArchiveConfiguration() {
+            return !enabled || !archive || (archivedLogFilenamePattern != null);
+        }
+
+        @ValidationMethod(message = "must have logging.file.currentLogFilename if logging.file.enabled is true")
+        public boolean isConfigured() {
+            return !enabled || (currentLogFilename != null);
+        }
 
         public boolean isEnabled() {
             return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
         }
 
         public Level getThreshold() {
             return threshold;
         }
 
-        public String getFilenamePattern() {
-            return filenamePattern;
+        public void setThreshold(Level level) {
+            this.threshold = level;
         }
 
-        public Size getMaxFileSize() {
-            return maxFileSize;
+        public String getCurrentLogFilename() {
+            return currentLogFilename;
         }
 
-        public int getRetainedFileCount() {
-            return retainedFileCount;
+        public void setCurrentLogFilename(String filename) {
+            this.currentLogFilename = filename;
+        }
+
+        public boolean isArchive() {
+            return archive;
+        }
+
+        public void setArchive(boolean archive) {
+            this.archive = archive;
+        }
+
+        public int getArchivedFileCount() {
+            return archivedFileCount;
+        }
+
+        public void setArchivedFileCount(int count) {
+            this.archivedFileCount = count;
+        }
+
+        public String getArchivedLogFilenamePattern() {
+            return archivedLogFilenamePattern;
+        }
+
+        public void setArchivedLogFilenamePattern(String pattern) {
+            this.archivedLogFilenamePattern = pattern;
+        }
+
+        public TimeZone getTimeZone() {
+            return timeZone;
+        }
+
+        public void setTimeZone(TimeZone timeZone) {
+            this.timeZone = timeZone;
+        }
+
+        public Optional<String> getLogFormat() {
+            return Optional.fromNullable(logFormat);
+        }
+
+        public void setLogFormat(String logFormat) {
+            this.logFormat = logFormat;
         }
     }
 
     public static class SyslogConfiguration {
+        public enum Facility {
+            AUTH, AUTHPRIV, DAEMON, CRON, FTP, LPR, KERN, MAIL, NEWS, SYSLOG, USER, UUCP,
+            LOCAL0, LOCAL1, LOCAL2, LOCAL3, LOCAL4, LOCAL5, LOCAL6, LOCAL7;
+
+            @Override
+            @JsonValue
+            public String toString() {
+                return super.toString().replace("_", "+").toLowerCase(Locale.ENGLISH);
+            }
+
+            @JsonCreator
+            public static Facility parse(String facility) {
+                return valueOf(facility.toUpperCase(Locale.ENGLISH).replace('+', '_'));
+            }
+        }
+
         @JsonProperty
-        private boolean enabled = true;
+        private boolean enabled = false;
 
         @NotNull
         @JsonProperty
-        @JsonDeserialize(using = LevelDeserializer.class)
         private Level threshold = Level.ALL;
 
         @NotNull
@@ -92,34 +202,70 @@ public class LoggingConfiguration {
 
         @NotNull
         @JsonProperty
-        @Pattern(regexp = "(auth|authpriv|daemon|cron|ftp|lpr|kern|mail|news|syslog|user|uucp|local[0-7])")
-        private String facility = "local0";
+        private Facility facility = Facility.LOCAL0;
+
+        @NotNull
+        @JsonProperty
+        private TimeZone timeZone = UTC;
+
+        @JsonProperty
+        private String logFormat;
 
         public boolean isEnabled() {
             return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
         }
 
         public Level getThreshold() {
             return threshold;
         }
 
+        public void setThreshold(Level threshold) {
+            this.threshold = threshold;
+        }
+
         public String getHost() {
             return host;
         }
 
-        public String getFacility() {
+        public void setHost(String host) {
+            this.host = host;
+        }
+
+        public Facility getFacility() {
             return facility;
+        }
+
+        public void setFacility(Facility facility) {
+            this.facility = facility;
+        }
+
+        public TimeZone getTimeZone() {
+            return timeZone;
+        }
+
+        public void setTimeZone(TimeZone timeZone) {
+            this.timeZone = timeZone;
+        }
+
+        public Optional<String> getLogFormat() {
+            return Optional.fromNullable(logFormat);
+        }
+
+        public void setLogFormat(String logFormat) {
+            this.logFormat = logFormat;
         }
     }
 
     @NotNull
     @JsonProperty
-    @JsonDeserialize(using = LevelDeserializer.class)
     private Level level = Level.INFO;
 
     @NotNull
     @JsonProperty
-    @JsonDeserialize(contentUsing = LevelDeserializer.class)
     private ImmutableMap<String, Level> loggers = ImmutableMap.of();
 
     @Valid
@@ -141,19 +287,39 @@ public class LoggingConfiguration {
         return level;
     }
 
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
     public ImmutableMap<String, Level> getLoggers() {
         return loggers;
+    }
+
+    public void setLoggers(Map<String, Level> loggers) {
+        this.loggers = ImmutableMap.copyOf(loggers);
     }
 
     public ConsoleConfiguration getConsoleConfiguration() {
         return console;
     }
 
+    public void setConsoleConfiguration(ConsoleConfiguration config) {
+        this.console = config;
+    }
+
     public FileConfiguration getFileConfiguration() {
         return file;
     }
 
+    public void setFileConfiguration(FileConfiguration config) {
+        this.file = config;
+    }
+
     public SyslogConfiguration getSyslogConfiguration() {
         return syslog;
+    }
+
+    public void setSyslogConfiguration(SyslogConfiguration config) {
+        this.syslog = config;
     }
 }
