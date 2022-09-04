c8b58037f953d372cc624f0f68ccca6f30d4144a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:17:50 +0200

    Allow starlark-generated CcCompilationContext to work with include scanning
        We need to add headers to HeaderInfo via addModularHdrs, else include
        scanning won't be able to find any generated headers.
    
        RELNOTES: None
        PiperOrigin-RevId: 297168393

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CcModule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CcModule.java
@@ -281,9 +281,6 @@ public abstract class CcModule
       boolean useTestOnlyFlags,
       boolean isStaticLinkingMode)
       throws EvalException {
-    if (featureConfiguration.getFeatureConfiguration().isEnabled(CppRuleClasses.FDO_INSTRUMENT)) {
-      throw new EvalException("FDO instrumentation not supported");
-    }
     return LinkBuildVariables.setupVariables(
         isUsingLinkerNotArchiver,
         /* binDirectoryPath= */ null,
