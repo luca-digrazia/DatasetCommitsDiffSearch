badd73512bb0cdf4203c0d9e6afb6d8494c86541
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 00:14:16 +0200

    Stop the transitive duplication of FilesetTraversalParams when Filesets are nested.
        RELNOTES: None
        PiperOrigin-RevId: 169723791

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/FilesetEntryFunction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/FilesetEntryFunction.java
@@ -71,14 +71,13 @@ public final class FilesetEntryFunction implements SkyFunction {
       // The absence of "direct" traversal indicates the presence of a "nested" fileset and
       // getNestedTraversal will return the list FilesetTraversalParams corresponding to each
       // FilesetEntry of the nested Fileset.
-      ImmutableList<SkyKey> nestedKeys = FilesetEntryValue.keys(t.getNestedTraversal());
-      Map<SkyKey, SkyValue> results = env.getValues(nestedKeys);
+      Map<SkyKey, SkyValue> results = env.getValues(FilesetEntryValue.keys(t.getNestedTraversal()));
       if (env.valuesMissing()) {
         return null;
       }
 
-      for (SkyKey nestedKey : nestedKeys) {
-        FilesetEntryValue nested = (FilesetEntryValue) results.get(nestedKey);
+      for (SkyValue value : results.values()) {
+        FilesetEntryValue nested = (FilesetEntryValue) value;
         for (FilesetOutputSymlink s : nested.getSymlinks()) {
           if (!exclusions.contains(s.name.getPathString())) {
             maybeStoreSymlink(s, t.getDestPath(), outputSymlinks);
