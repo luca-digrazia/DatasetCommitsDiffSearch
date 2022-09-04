2e367312ac3babd98b0bcdda8df13bdcb49e8dbc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:13:20 +0200

    Precompute the hash code and fingerprint in `FragmentClassSet`.
        Like https://github.com/bazelbuild/bazel/commit/e37a9ab6cf35d80b67f74b25be59f5b322c1cc7d, since it is always interned, there's no reason to lazily compute the hash code - it will be called by the interner.
    
        PiperOrigin-RevId: 370459759

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/config/FragmentClassSet.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/config/FragmentClassSet.java
@@ -14,20 +14,14 @@
 
 package com.google.devtools.build.lib.analysis.config;
 
-import static com.google.common.base.Predicates.not;
 import static java.util.stream.Collectors.joining;
 
 import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Interner;
-import com.google.common.collect.Sets;
 import com.google.devtools.build.lib.concurrent.BlazeInterners;
 import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
-import com.google.devtools.build.lib.skyframe.serialization.autocodec.SerializationConstant;
 import com.google.devtools.build.lib.util.Fingerprint;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Iterator;
 
 /**
  * A wrapper class for an {@code ImmutableSortedSet<Class<? extends BuildConfiguration.Fragment>>}
@@ -35,34 +29,15 @@ import java.util.Iterator;
  * are in frequently used keys. For good measure, we also compute a fingerprint.
  */
 @AutoCodec
-public final class FragmentClassSet implements Iterable<Class<? extends Fragment>> {
-
-  /**
-   * Sorts fragments by class name. This produces a stable order which, e.g., facilitates consistent
-   * output from buildMnemonic.
-   */
-  @SerializationConstant
-  public static final Comparator<Class<? extends Fragment>> LEXICAL_FRAGMENT_SORTER =
-      Comparator.comparing(Class::getName);
+public final class FragmentClassSet {
 
   private static final Interner<FragmentClassSet> interner = BlazeInterners.newWeakInterner();
 
   @AutoCodec.Instantiator
-  public static FragmentClassSet of(Collection<Class<? extends Fragment>> fragments) {
-    ImmutableSortedSet<Class<? extends Fragment>> sortedFragments =
-        ImmutableSortedSet.copyOf(LEXICAL_FRAGMENT_SORTER, fragments);
-    byte[] fingerprint = computeFingerprint(sortedFragments);
+  public static FragmentClassSet of(ImmutableSortedSet<Class<? extends Fragment>> fragments) {
+    byte[] fingerprint = computeFingerprint(fragments);
     return interner.intern(
-        new FragmentClassSet(sortedFragments, fingerprint, Arrays.hashCode(fingerprint)));
-  }
-
-  public static FragmentClassSet union(
-      FragmentClassSet firstFragments, FragmentClassSet secondFragments) {
-    return of(
-        ImmutableSortedSet.orderedBy(LEXICAL_FRAGMENT_SORTER)
-            .addAll(firstFragments)
-            .addAll(secondFragments)
-            .build());
+        new FragmentClassSet(fragments, fingerprint, Arrays.hashCode(fingerprint)));
   }
 
   private static byte[] computeFingerprint(
@@ -85,25 +60,8 @@ public final class FragmentClassSet implements Iterable<Class<? extends Fragment
     this.hashCode = hashCode;
   }
 
-  public int size() {
-    return fragments.size();
-  }
-
-  public boolean contains(Class<? extends Fragment> fragment) {
-    return fragments.contains(fragment);
-  }
-
-  /** Returns a set of fragment classes identical to this one but without the given fragment. */
-  public FragmentClassSet trim(Class<? extends Fragment> fragment) {
-    if (!contains(fragment)) {
-      return this;
-    }
-    return of(Sets.filter(fragments, not(fragment::equals)));
-  }
-
-  @Override
-  public Iterator<Class<? extends Fragment>> iterator() {
-    return fragments.iterator();
+  public ImmutableSortedSet<Class<? extends Fragment>> fragmentClasses() {
+    return fragments;
   }
 
   @Override
