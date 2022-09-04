8edf2bd4a9e16f69ccf5fb8387c2f3d5c75178d5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:39:33 +0200

    Simplify the logic in CppCompileAction#updateActionInputs().
        This way, we'll eventually get closer to the ideal world where inputs are clearly classified as either mandatory, prunable, discovered or discovered and prunable.
    
        --
        PiperOrigin-RevId: 150061912
        MOS_MIGRATED_REVID=150061912

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
@@ -24,8 +24,6 @@ import com.google.common.collect.Sets;
 import com.google.devtools.build.lib.actions.AbstractAction;
 import com.google.devtools.build.lib.actions.ActionExecutionContext;
 import com.google.devtools.build.lib.actions.ActionExecutionException;
-import com.google.devtools.build.lib.actions.ActionLookupValue;
-import com.google.devtools.build.lib.actions.ActionLookupValue.ActionLookupKey;
 import com.google.devtools.build.lib.actions.ActionOwner;
 import com.google.devtools.build.lib.actions.ActionStatusMessage;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -53,6 +51,8 @@ import com.google.devtools.build.lib.profiler.ProfilerTask;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainFeatures.FeatureConfiguration;
 import com.google.devtools.build.lib.rules.cpp.CppCompileActionContext.Reply;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration.Tool;
+import com.google.devtools.build.lib.skyframe.ActionLookupValue;
+import com.google.devtools.build.lib.skyframe.ActionLookupValue.ActionLookupKey;
 import com.google.devtools.build.lib.util.DependencySet;
 import com.google.devtools.build.lib.util.FileType;
 import com.google.devtools.build.lib.util.Fingerprint;
@@ -513,15 +513,7 @@ public class CppCompileAction extends AbstractAction
       ActionLookupValue value = (ActionLookupValue) skyValues.get(skyKey);
       Preconditions.checkNotNull(
           value, "Owner %s of %s not in graph %s", artifact.getArtifactOwner(), artifact, skyKey);
-      // We can get the generating action here because #canRemoveAfterExecution is overridden.
-      Preconditions.checkState(
-          CppFileTypes.CPP_MODULE.matches(artifact.getFilename()),
-          "Non-module? %s (%s %s)",
-          artifact,
-          this,
-          value);
-      CppCompileAction action =
-          (CppCompileAction) value.getGeneratingActionDangerousReadJavadoc(artifact);
+      CppCompileAction action = (CppCompileAction) value.getGeneratingAction(artifact);
       for (Artifact input : action.getInputs()) {
         if (CppFileTypes.CPP_MODULE.matches(input.getFilename())) {
           additionalModules.add(input);
@@ -614,7 +606,7 @@ public class CppCompileAction extends AbstractAction
     for (String opt : cppCompileCommandLine.copts) {
       if (opt.startsWith("-I") && opt.length() > 2) {
         // We insist on the combined form "-Idir".
-        result.add(PathFragment.create(opt.substring(2)));
+        result.add(new PathFragment(opt.substring(2)));
       }
     }
     return result.build();
@@ -634,10 +626,10 @@ public class CppCompileAction extends AbstractAction
       String opt = compilerOptions.get(i);
       if (opt.startsWith("-isystem")) {
         if (opt.length() > 8) {
-          result.add(PathFragment.create(opt.substring(8).trim()));
+          result.add(new PathFragment(opt.substring(8).trim()));
         } else if (i + 1 < compilerOptions.size()) {
           i++;
-          result.add(PathFragment.create(compilerOptions.get(i)));
+          result.add(new PathFragment(compilerOptions.get(i)));
         } else {
           System.err.println("WARNING: dangling -isystem flag in options for " + prettyPrint());
         }
@@ -739,13 +731,6 @@ public class CppCompileAction extends AbstractAction
 
   protected final List<String> getArgv(PathFragment outputFile) {
     return cppCompileCommandLine.getArgv(outputFile, overwrittenVariables);
-  }
-
-  @Override
-  public boolean canRemoveAfterExecution() {
-    // Module-generating actions are needed because the action may be retrieved in
-    // #discoverInputsStage2.
-    return !CppFileTypes.CPP_MODULE.matches(getPrimaryOutput().getFilename());
   }
 
   @Override
