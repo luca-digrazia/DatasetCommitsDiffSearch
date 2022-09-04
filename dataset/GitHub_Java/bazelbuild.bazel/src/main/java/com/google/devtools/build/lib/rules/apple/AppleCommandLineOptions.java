58fcb5d156015b9b23886aff7d32776da95d864e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:51:42 +0200

    Improve documentation on {mac|watch|i|tv}os_minimum_os and {mac|watch|i|tv}os_sdk_version to explain how defaults are determined.
        PiperOrigin-RevId: 279984024

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/apple/AppleCommandLineOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/apple/AppleCommandLineOptions.java
@@ -27,7 +27,7 @@ import com.google.devtools.build.lib.skyframe.serialization.DeserializationConte
 import com.google.devtools.build.lib.skyframe.serialization.SerializationContext;
 import com.google.devtools.build.lib.skyframe.serialization.SerializationException;
 import com.google.devtools.build.lib.skylarkbuildapi.apple.AppleBitcodeModeApi;
-import com.google.devtools.build.lib.syntax.Printer;
+import com.google.devtools.build.lib.skylarkinterface.SkylarkPrinter;
 import com.google.devtools.common.options.Converters.CommaSeparatedOptionListConverter;
 import com.google.devtools.common.options.EnumConverter;
 import com.google.devtools.common.options.Option;
@@ -161,17 +161,6 @@ public class AppleCommandLineOptions extends FragmentOptions {
           "Minimum compatible macOS version for targets. "
               + "If unspecified, uses 'macos_sdk_version'.")
   public DottedVersion.Option macosMinimumOs;
-
-  @Option(
-      name = "experimental_prefer_mutual_xcode",
-      defaultValue = "true",
-      documentationCategory = OptionDocumentationCategory.TOOLCHAIN,
-      effectTags = {OptionEffectTag.LOSES_INCREMENTAL_STATE},
-      help =
-          "If true, use the most recent Xcode that is available both locally and remotely. If"
-              + " false, or if there are no mutual available versions, use the local Xcode version"
-              + " selected via xcode-select.")
-  public boolean preferMutualXcode;
 
   @VisibleForTesting public static final String DEFAULT_IOS_SDK_VERSION = "8.4";
   @VisibleForTesting public static final String DEFAULT_WATCHOS_SDK_VERSION = "2.0";
@@ -426,7 +415,7 @@ public class AppleCommandLineOptions extends FragmentOptions {
     }
 
     @Override
-    public void repr(Printer printer) {
+    public void repr(SkylarkPrinter printer) {
       printer.append(mode);
     }
 
