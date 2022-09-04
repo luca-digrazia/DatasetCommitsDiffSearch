03fe06ee9c8d6b82e326a71f6bb516e48f18f5b1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:45:10 +0200

    Inject a single KubernetesMockServer in the test class hierarchy

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/kubernetes-client/src/main/java/io/quarkus/test/kubernetes/client/KubernetesMockServerTestResource.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/kubernetes-client/src/main/java/io/quarkus/test/kubernetes/client/KubernetesMockServerTestResource.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import io.fabric8.kubernetes.client.Config;
-import io.fabric8.kubernetes.client.NamespacedKubernetesClient;
 import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 
@@ -15,17 +14,16 @@ public class KubernetesMockServerTestResource implements QuarkusTestResourceLife
 
     @Override
     public Map<String, String> start() {
+        mockServer = new KubernetesMockServer(useHttps());
+        mockServer.init();
+
         final Map<String, String> systemProps = new HashMap<>();
+        systemProps.put(Config.KUBERNETES_MASTER_SYSTEM_PROPERTY,
+                mockServer.createClient().getConfiguration().getMasterUrl());
         systemProps.put(Config.KUBERNETES_TRUST_CERT_SYSTEM_PROPERTY, "true");
         systemProps.put(Config.KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY, "false");
         systemProps.put(Config.KUBERNETES_AUTH_TRYSERVICEACCOUNT_SYSTEM_PROPERTY, "false");
         systemProps.put(Config.KUBERNETES_NAMESPACE_SYSTEM_PROPERTY, "test");
-
-        mockServer = new KubernetesMockServer(useHttps());
-        mockServer.init();
-        try (NamespacedKubernetesClient client = mockServer.createClient()) {
-            systemProps.put(Config.KUBERNETES_MASTER_SYSTEM_PROPERTY, client.getConfiguration().getMasterUrl());
-        }
 
         return systemProps;
     }
