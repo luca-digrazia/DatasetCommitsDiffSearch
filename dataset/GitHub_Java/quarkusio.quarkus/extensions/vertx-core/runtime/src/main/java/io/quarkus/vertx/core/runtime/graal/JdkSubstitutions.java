e0efe58aa36c323d7bb3588faa3215993e7f84ba
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:49:07 +0200

    Initialize sun.nio.ch.Iocp at runtime on Windows with JDK 11

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-core/runtime/src/main/java/io/quarkus/vertx/core/runtime/graal/JdkSubstitutions.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-core/runtime/src/main/java/io/quarkus/vertx/core/runtime/graal/JdkSubstitutions.java
@@ -5,9 +5,6 @@ import java.net.URL;
 import java.nio.channels.spi.AsynchronousChannelProvider;
 import java.util.function.Function;
 
-import org.graalvm.nativeimage.Platform;
-import org.graalvm.nativeimage.Platforms;
-
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.InjectAccessors;
 import com.oracle.svm.core.annotate.Substitute;
@@ -95,7 +92,6 @@ final class Package_jdk_internal_loader implements Function<TargetClass, String>
 
 @Substitute
 @TargetClass(className = "sun.nio.ch.WindowsAsynchronousFileChannelImpl", innerClass = "DefaultIocpHolder", onlyWith = JDK11OrLater.class)
-@Platforms({ Platform.WINDOWS.class })
 final class Target_sun_nio_ch_WindowsAsynchronousFileChannelImpl_DefaultIocpHolder {
 
     @Alias
@@ -104,7 +100,6 @@ final class Target_sun_nio_ch_WindowsAsynchronousFileChannelImpl_DefaultIocpHold
 }
 
 @TargetClass(className = "sun.nio.ch.Iocp", onlyWith = JDK11OrLater.class)
-@Platforms({ Platform.WINDOWS.class })
 final class Target_sun_nio_ch_Iocp {
 
     @Alias
@@ -118,7 +113,6 @@ final class Target_sun_nio_ch_Iocp {
 }
 
 @TargetClass(className = "sun.nio.ch.ThreadPool", onlyWith = JDK11OrLater.class)
-@Platforms({ Platform.WINDOWS.class })
 final class Target_sun_nio_ch_ThreadPool {
 
     @Alias
