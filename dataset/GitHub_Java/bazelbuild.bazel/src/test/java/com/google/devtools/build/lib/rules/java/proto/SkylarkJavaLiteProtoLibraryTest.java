ee606325bdb9f7d418d0443339a5d0ee5634e607
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 20:34:32 +0200

    Migrate to find_java_toolchain/find_java_runtime_toolchain for Java toolchain     and runtime resolution
        PiperOrigin-RevId: 232392137

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/rules/java/proto/SkylarkJavaLiteProtoLibraryTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/rules/java/proto/SkylarkJavaLiteProtoLibraryTest.java
@@ -427,7 +427,7 @@ public class SkylarkJavaLiteProtoLibraryTest extends BuildViewTestCase {
           getProvider(JavaSourceJarsProvider.class, getConfiguredTarget("//x:foo_lite_pb2"));
       assertThat(sourceJarsProvider).isNotNull();
       assertThat(prettyArtifactNames(sourceJarsProvider.getSourceJars()))
-          .containsExactly("x/libfoo-lite-src.jar");
+          .containsExactly("x/foo-lite-src.jar");
     }
 
     {
@@ -443,7 +443,7 @@ public class SkylarkJavaLiteProtoLibraryTest extends BuildViewTestCase {
           getProvider(JavaSourceJarsProvider.class, getConfiguredTarget("//x:bar_lite_pb2"));
       assertThat(sourceJarsProvider).isNotNull();
       assertThat(prettyArtifactNames(sourceJarsProvider.getSourceJars()))
-          .containsExactly("x/libbar-lite-src.jar");
+          .containsExactly("x/bar-lite-src.jar");
     }
   }
 
