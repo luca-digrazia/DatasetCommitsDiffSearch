246d165320c228850c6b9c50535da09ac424b9d5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:58:37 +0200

    Prevent misuse of modify* in dev mode tests
        I wasted a few minutes the last time I made
        this mistake until I found out what
        I had done wrong, so best make it clear
        immediately

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit5-internal/src/main/java/io/quarkus/test/QuarkusDevModeTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit5-internal/src/main/java/io/quarkus/test/QuarkusDevModeTest.java
@@ -209,7 +209,6 @@ public class QuarkusDevModeTest
     @Override
     public void afterAll(ExtensionContext context) throws Exception {
         rootLogger.setHandlers(originalRootLoggerHandlers);
-        inMemoryLogHandler.clearRecords();
     }
 
     @Override
