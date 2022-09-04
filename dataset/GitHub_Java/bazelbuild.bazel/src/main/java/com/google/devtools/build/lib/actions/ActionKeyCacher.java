2625c53d5d5a5642d98d9d90d148ffe4c2dbc025
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:44:14 +0200

    Use a double check in getKey instead of synchronizing every call.
        RELNOTES: None.
        PiperOrigin-RevId: 249711891

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/ActionKeyCacher.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/ActionKeyCacher.java
@@ -40,8 +40,6 @@ public abstract class ActionKeyCacher implements ActionAnalysisMetadata {
               getExecutionPlatform().addTo(fp);
             }
 
-            fp.addStringMap(getExecProperties());
-
             // Compute the actual key and store it.
             cachedKey = fp.hexDigestAndReset();
           } catch (CommandLineExpansionException e) {
@@ -56,10 +54,6 @@ public abstract class ActionKeyCacher implements ActionAnalysisMetadata {
   /**
    * See the javadoc for {@link Action} and {@link ActionAnalysisMetadata#getKey} for the contract
    * of this method.
-   *
-   * <p>TODO(b/150305897): subtypes of this are not consistent about adding the UUID as stated in
-   * the ActionAnalysisMetadata. Perhaps ActionKeyCacher should just mandate subclasses provide a
-   * UUID and then add that UUID itself in getKey.
    */
   protected abstract void computeKey(ActionKeyContext actionKeyContext, Fingerprint fp)
       throws CommandLineExpansionException;
