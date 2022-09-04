5199ad53a2d40e35c06c878fdaf8c740f56e83b2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:35:23 +0200

    Allow alternate ConnectorProvider to be used. This makes the client more flexible.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-client/src/main/java/io/dropwizard/client/JerseyClientBuilder.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-client/src/main/java/io/dropwizard/client/JerseyClientBuilder.java
@@ -3,21 +3,20 @@ package io.dropwizard.client;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.httpclient.HttpClientMetricNameStrategy;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import io.dropwizard.jersey.gzip.ConfiguredGZipEncoder;
 import io.dropwizard.jersey.gzip.GZipDecoder;
 import io.dropwizard.jersey.jackson.JacksonMessageBodyProvider;
 import io.dropwizard.setup.Environment;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.config.Registry;
 import org.apache.http.conn.DnsResolver;
-import org.apache.http.conn.routing.HttpRoutePlanner;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.client.spi.Connector;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.RequestEntityProcessing;
 import org.glassfish.jersey.client.spi.ConnectorProvider;
 
 import javax.validation.Validation;
@@ -41,39 +40,31 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * <li>Disables stale connection checks</li>
  * <li>Disables Nagle's algorithm</li>
  * <li>Disables cookie management by default</li>
- * <li>Compress requests and decompress responses using GZIP</li>
- * <li>Supports parsing and generating JSON data using Jackson</li>
  * </ul>
  * </p>
  *
  * @see HttpClientBuilder
  */
 public class JerseyClientBuilder {
-
+    private final HttpClientBuilder builder;
     private final List<Object> singletons = Lists.newArrayList();
     private final List<Class<?>> providers = Lists.newArrayList();
     private final Map<String, Object> properties = Maps.newLinkedHashMap();
-    private JerseyClientConfiguration configuration = new JerseyClientConfiguration();
 
-    private HttpClientBuilder apacheHttpClientBuilder;
+    private JerseyClientConfiguration configuration = new JerseyClientConfiguration();
     private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
     private Environment environment;
     private ObjectMapper objectMapper;
     private ExecutorService executorService;
-    private ConnectorProvider connectorProvider;
+    private ConnectorProvider connectorProvider = new ApacheConnectorProvider();
 
     public JerseyClientBuilder(Environment environment) {
-        this.apacheHttpClientBuilder = new HttpClientBuilder(environment);
+        this.builder = new HttpClientBuilder(environment);
         this.environment = environment;
     }
 
     public JerseyClientBuilder(MetricRegistry metricRegistry) {
-        this.apacheHttpClientBuilder = new HttpClientBuilder(metricRegistry);
-    }
-
-    @VisibleForTesting
-    public void setApacheHttpClientBuilder(HttpClientBuilder apacheHttpClientBuilder) {
-        this.apacheHttpClientBuilder = apacheHttpClientBuilder;
+        this.builder = new HttpClientBuilder(metricRegistry);
     }
 
     /**
@@ -102,15 +93,23 @@ public class JerseyClientBuilder {
     /**
      * Sets the state of the given Jersey property.
      *
-     * <p/><b>WARNING:</b> The default connector ignores Jersey properties.
-     * Use {@link JerseyClientConfiguration} instead.
-     *
      * @param propertyName  the name of the Jersey property
      * @param propertyValue the state of the Jersey property
      * @return {@code this}
      */
     public JerseyClientBuilder withProperty(String propertyName, Object propertyValue) {
         properties.put(propertyName, propertyValue);
+        return this;
+    }
+
+    /**
+     * Uses the {@link org.apache.http.client.HttpRequestRetryHandler} for handling request retries.
+     *
+     * @param httpRequestRetryHandler a HttpRequestRetryHandler
+     * @return {@code this}
+     */
+    public JerseyClientBuilder using(HttpRequestRetryHandler httpRequestRetryHandler) {
+        builder.using(httpRequestRetryHandler);
         return this;
     }
 
@@ -122,7 +121,7 @@ public class JerseyClientBuilder {
      */
     public JerseyClientBuilder using(JerseyClientConfiguration configuration) {
         this.configuration = configuration;
-        apacheHttpClientBuilder.using(configuration);
+        builder.using(configuration);
         return this;
     }
 
@@ -135,6 +134,28 @@ public class JerseyClientBuilder {
      */
     public JerseyClientBuilder using(Environment environment) {
         this.environment = environment;
+        return this;
+    }
+
+    /**
+     * Use the given {@link DnsResolver} instance.
+     *
+     * @param resolver a {@link DnsResolver} instance
+     * @return {@code this}
+     */
+    public JerseyClientBuilder using(DnsResolver resolver) {
+        builder.using(resolver);
+        return this;
+    }
+
+    /**
+     * Use the given {@link Registry} instance.
+     *
+     * @param registry a {@link Registry} instance
+     * @return {@code this}
+     */
+    public JerseyClientBuilder using(Registry<ConnectionSocketFactory> registry) {
+        builder.using(registry);
         return this;
     }
 
@@ -164,92 +185,24 @@ public class JerseyClientBuilder {
     }
 
     /**
-     * Use the given {@link ConnectorProvider} instance.
-     * <p/><b>WARNING:</b> Use it with a caution. Most of features will not
-     * work in a custom connection provider.
+     * Use the given {@link HttpClientMetricNameStrategy} instance.
      *
-     * @param connectorProvider a {@link ConnectorProvider} instance
+     * @param metricNameStrategy    a {@link HttpClientMetricNameStrategy} instance
+     * @return {@code this}
+     */
+    public JerseyClientBuilder using(HttpClientMetricNameStrategy metricNameStrategy) {
+        builder.using(metricNameStrategy);
+        return this;
+    }
+
+    /**
+     * Use the given {@link ConnectorProvider} instance.
+     *
+     * @param connectorProvider    a {@link ConnectorProvider} instance
      * @return {@code this}
      */
     public JerseyClientBuilder using(ConnectorProvider connectorProvider) {
         this.connectorProvider = connectorProvider;
-        return this;
-    }
-
-    /**
-     * Uses the {@link org.apache.http.client.HttpRequestRetryHandler} for handling request retries.
-     *
-     * @param httpRequestRetryHandler a HttpRequestRetryHandler
-     * @return {@code this}
-     */
-    public JerseyClientBuilder using(HttpRequestRetryHandler httpRequestRetryHandler) {
-        apacheHttpClientBuilder.using(httpRequestRetryHandler);
-        return this;
-    }
-
-    /**
-     * Use the given {@link DnsResolver} instance.
-     *
-     * @param resolver a {@link DnsResolver} instance
-     * @return {@code this}
-     */
-    public JerseyClientBuilder using(DnsResolver resolver) {
-        apacheHttpClientBuilder.using(resolver);
-        return this;
-    }
-
-    /**
-     * Use the given {@link Registry} instance of connection socket factories.
-     *
-     * @param registry a {@link Registry} instance of connection socket factories
-     * @return {@code this}
-     */
-    public JerseyClientBuilder using(Registry<ConnectionSocketFactory> registry) {
-        apacheHttpClientBuilder.using(registry);
-        return this;
-    }
-
-    /**
-     * Use the given {@link HttpClientMetricNameStrategy} instance.
-     *
-     * @param metricNameStrategy a {@link HttpClientMetricNameStrategy} instance
-     * @return {@code this}
-     */
-    public JerseyClientBuilder using(HttpClientMetricNameStrategy metricNameStrategy) {
-        apacheHttpClientBuilder.using(metricNameStrategy);
-        return this;
-    }
-
-    /**
-     * Use the given environment name. This is used in the user agent.
-     *
-     * @param environmentName an environment name to use in the user agent.
-     * @return {@code this}
-     */
-    public JerseyClientBuilder name(String environmentName) {
-        apacheHttpClientBuilder.name(environmentName);
-        return this;
-    }
-
-    /**
-     * Use the given {@link HttpRoutePlanner} instance.
-     *
-     * @param routePlanner a {@link HttpRoutePlanner} instance
-     * @return {@code this}
-     */
-    public JerseyClientBuilder using(HttpRoutePlanner routePlanner) {
-        apacheHttpClientBuilder.using(routePlanner);
-        return this;
-    }
-
-    /**
-     * Use the given {@link CredentialsProvider} instance.
-     *
-     * @param credentialsProvider a {@link CredentialsProvider} instance
-     * @return {@code this}
-     */
-    public JerseyClientBuilder using(CredentialsProvider credentialsProvider) {
-        apacheHttpClientBuilder.using(credentialsProvider);
         return this;
     }
 
@@ -265,22 +218,22 @@ public class JerseyClientBuilder {
         }
 
         if (environment == null) {
-            return build(name, executorService, objectMapper, validator);
+            return build(executorService, objectMapper, validator);
         }
 
-        return build(name, environment.lifecycle()
-                .executorService("jersey-client-" + name + "-%d")
-                .minThreads(configuration.getMinThreads())
-                .maxThreads(configuration.getMaxThreads())
-                .build(),
+        return build(environment.lifecycle()
+                        .executorService("jersey-client-" + name + "-%d")
+                        .minThreads(configuration.getMinThreads())
+                        .maxThreads(configuration.getMaxThreads())
+                        .build(),
                 environment.getObjectMapper(),
                 environment.getValidator());
     }
 
-    private Client build(String name, ExecutorService threadPool,
+    private Client build(ExecutorService threadPool,
                          ObjectMapper objectMapper,
                          Validator validator) {
-        final Client client = ClientBuilder.newClient(buildConfig(name, threadPool, objectMapper, validator));
+        final Client client = ClientBuilder.newClient(buildConfig(threadPool, objectMapper, validator));
 
         if (configuration.isGzipEnabled()) {
             client.register(new GZipDecoder());
@@ -290,7 +243,7 @@ public class JerseyClientBuilder {
         return client;
     }
 
-    private Configuration buildConfig(final String name, final ExecutorService threadPool,
+    private Configuration buildConfig(final ExecutorService threadPool,
                                       final ObjectMapper objectMapper,
                                       final Validator validator) {
         final ClientConfig config = new ClientConfig();
@@ -309,16 +262,10 @@ public class JerseyClientBuilder {
             config.property(property.getKey(), property.getValue());
         }
 
+        final RequestEntityProcessing requestEntityProcessing = configuration.isChunkedEncodingEnabled() ? RequestEntityProcessing.CHUNKED : RequestEntityProcessing.BUFFERED;
+        config.property(ClientProperties.REQUEST_ENTITY_PROCESSING, requestEntityProcessing);
+
         config.register(new DropwizardExecutorProvider(threadPool));
-        if (connectorProvider == null) {
-            connectorProvider = new ConnectorProvider() {
-                @Override
-                public Connector getConnector(Client client, Configuration runtimeConfig) {
-                    return new DropwizardApacheConnector(apacheHttpClientBuilder.build(name),
-                            configuration.isChunkedEncodingEnabled());
-                }
-            };
-        }
         config.connectorProvider(connectorProvider);
 
         return config;
