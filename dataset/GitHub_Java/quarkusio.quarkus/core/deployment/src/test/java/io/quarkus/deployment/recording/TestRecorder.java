e0929f6af6322baa459dcdb28b462c23c91cb31c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:33:07 +0200

    Add stricter validation for bytecode recorders
        Fixes #12864

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/test/java/io/quarkus/deployment/recording/TestRecorder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/test/java/io/quarkus/deployment/recording/TestRecorder.java
@@ -95,8 +95,4 @@ public class TestRecorder {
     public void relaxedObject(@RelaxedValidation ValidationFails validationFails) {
         RESULT.add(validationFails);
     }
-
-    public void ignoredProperties(IgnoredProperties ignoredProperties) {
-        RESULT.add(ignoredProperties);
-    }
 }
