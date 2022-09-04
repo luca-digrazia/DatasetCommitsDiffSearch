8f5afdf083b18bcbaeb7549f2bda930cc854d71e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:41:28 +0200

    Make the vert.x failure handler handle non auth related security failures

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/HttpSecurityRecorder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/HttpSecurityRecorder.java
@@ -1,19 +1,13 @@
 package io.quarkus.vertx.http.runtime.security;
 
-import java.util.Map;
 import java.util.concurrent.CompletionException;
 import java.util.function.BiFunction;
-import java.util.function.Supplier;
 
 import javax.enterprise.inject.spi.CDI;
 
-import io.quarkus.arc.runtime.BeanContainer;
-import io.quarkus.arc.runtime.BeanContainerListener;
 import io.quarkus.runtime.annotations.Recorder;
 import io.quarkus.security.AuthenticationFailedException;
 import io.quarkus.security.identity.SecurityIdentity;
-import io.quarkus.vertx.http.runtime.HttpBuildTimeConfig;
-import io.quarkus.vertx.http.runtime.HttpConfiguration;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
 
@@ -22,14 +16,9 @@ public class HttpSecurityRecorder {
 
     public Handler<RoutingContext> authenticationMechanismHandler() {
         return new Handler<RoutingContext>() {
-
-            volatile HttpAuthenticator authenticator;
-
             @Override
             public void handle(RoutingContext event) {
-                if (authenticator == null) {
-                    authenticator = CDI.current().select(HttpAuthenticator.class).get();
-                }
+                HttpAuthenticator authenticator = CDI.current().select(HttpAuthenticator.class).get();
                 //we put the authenticator into the routing context so it can be used by other systems
                 event.put(HttpAuthenticator.class.getName(), authenticator);
                 authenticator.attemptAuthentication(event).handle(new BiFunction<SecurityIdentity, Throwable, Object>() {
@@ -52,9 +41,6 @@ public class HttpSecurityRecorder {
                             }
                             return null;
                         }
-                        if (event.response().ended()) {
-                            return null;
-                        }
                         if (identity != null) {
                             event.setUser(new QuarkusHttpUser(identity));
                         }
@@ -66,32 +52,4 @@ public class HttpSecurityRecorder {
         };
     }
 
-    public Handler<RoutingContext> permissionCheckHandler() {
-        return new Handler<RoutingContext>() {
-            volatile HttpAuthorizer authorizer;
-
-            @Override
-            public void handle(RoutingContext event) {
-                if (authorizer == null) {
-                    authorizer = CDI.current().select(HttpAuthorizer.class).get();
-                }
-                authorizer.checkPermission(event);
-            }
-        };
-    }
-
-    public BeanContainerListener initPermissions(HttpBuildTimeConfig permissions,
-            Map<String, Supplier<HttpSecurityPolicy>> policies) {
-        return new BeanContainerListener() {
-            @Override
-            public void created(BeanContainer container) {
-                container.instance(PathMatchingHttpSecurityPolicy.class).init(permissions, policies);
-            }
-        };
-    }
-
-    public void setupFormAuth(BeanContainer container, HttpConfiguration httpConfiguration,
-            HttpBuildTimeConfig buildTimeConfig) {
-        container.instance(FormAuthenticationMechanism.class).init(httpConfiguration, buildTimeConfig);
-    }
 }
