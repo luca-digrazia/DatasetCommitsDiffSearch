ed1328a838b332a6ba5055fd2e9a914d5d97c9ea
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:00:51 +0200

    When minikube is being used, never push an container-image
        Fixes: #10747

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesDeployer.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesDeployer.java
@@ -2,7 +2,6 @@
 package io.quarkus.kubernetes.deployment;
 
 import static io.quarkus.container.image.deployment.util.ImageUtil.hasRegistry;
-import static io.quarkus.kubernetes.deployment.Constants.KNATIVE;
 import static io.quarkus.kubernetes.deployment.Constants.KUBERNETES;
 import static io.quarkus.kubernetes.deployment.Constants.MINIKUBE;
 import static io.quarkus.kubernetes.deployment.Constants.OPENSHIFT;
@@ -169,11 +168,7 @@ public class KubernetesDeployer {
         try (FileInputStream fis = new FileInputStream(manifest)) {
             KubernetesList list = Serialization.unmarshalAsList(fis);
             distinct(list.getItems()).forEach(i -> {
-                if (KNATIVE.equals(deploymentTarget.getName().toLowerCase())) {
-                    client.resource(i).inNamespace(namespace).deletingExisting().createOrReplace();
-                } else {
-                    client.resource(i).inNamespace(namespace).createOrReplace();
-                }
+                client.resource(i).inNamespace(namespace).createOrReplace();
                 log.info("Applied: " + i.getKind() + " " + i.getMetadata().getName() + ".");
             });
 
