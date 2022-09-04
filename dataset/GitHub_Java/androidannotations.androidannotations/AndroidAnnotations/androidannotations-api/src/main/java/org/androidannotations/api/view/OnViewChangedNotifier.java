fdcbaf67387b81cfc3e165acd16ae17062b3d3fe
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:55:23 +0200

    use Set instead of List in OnViewChangedNotifier
        this avoids @AfterViews annotated method to be called multiple times e.g. when re-adding previouse @EFragment instances

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/api/view/OnViewChangedNotifier.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/api/view/OnViewChangedNotifier.java
@@ -1,7 +1,22 @@
+/**
+ * Copyright (C) 2010-2014 eBusiness Information, Excilys Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.androidannotations.api.view;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class OnViewChangedNotifier {
 
@@ -19,7 +34,7 @@ public class OnViewChangedNotifier {
 		}
 	}
 
-	private final List<OnViewChangedListener> listeners = new LinkedList<OnViewChangedListener>();
+	private final Set<OnViewChangedListener> listeners = new LinkedHashSet<OnViewChangedListener>();
 
 	public void notifyViewChanged(HasViews hasViews) {
 		for (OnViewChangedListener listener : listeners) {
