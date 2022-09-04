4fb4f9401b6fb88857b977ac6d666f3f5749e183
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:03:23 +0200

    Make sure all Arc build items have at least some javadoc.

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/builditem/CombinedIndexBuildItem.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/builditem/CombinedIndexBuildItem.java
@@ -1,6 +1,5 @@
 package io.quarkus.deployment.builditem;
 
-import org.jboss.jandex.DotName;
 import org.jboss.jandex.IndexView;
 
 import io.quarkus.builder.item.SimpleBuildItem;
@@ -14,10 +13,6 @@ import io.quarkus.builder.item.SimpleBuildItem;
  * Compared to {@code BeanArchiveIndexBuildItem}, this index doesn't contain all CDI-related information.
  * On the other hand, it can contain classes from archives/dependencies that had no CDI component declared within them.
  *
- * The computing index can also be used to index classes on demand. This when {@link IndexView#getClassByName(DotName)}
- * is called. Note that this is a mutable index as this will add additional information, so in general this Index
- * should only be used if you actually need it.
- *
  * @see AdditionalApplicationArchiveMarkerBuildItem
  * @see IndexDependencyBuildItem
  */
@@ -25,18 +20,11 @@ public final class CombinedIndexBuildItem extends SimpleBuildItem {
 
     private final IndexView index;
 
-    private final IndexView computingIndex;
-
-    public CombinedIndexBuildItem(IndexView index, IndexView computingIndex) {
+    public CombinedIndexBuildItem(IndexView index) {
         this.index = index;
-        this.computingIndex = computingIndex;
     }
 
     public IndexView getIndex() {
         return index;
-    }
-
-    public IndexView getComputingIndex() {
-        return computingIndex;
     }
 }
