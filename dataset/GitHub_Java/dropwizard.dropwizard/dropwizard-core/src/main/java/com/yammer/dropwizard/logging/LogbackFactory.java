873ad3019ddf63005e8d88f627994da0b0febba6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:49:36 +0200

    The SyslogAppender in Logback does not emit a layout (http://logback.qos.ch/codes.html#syslog_layout), instead should be using the suffixPattern.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/logging/LogbackFactory.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/logging/LogbackFactory.java
@@ -2,72 +2,83 @@ package com.yammer.dropwizard.logging;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.filter.ThresholdFilter;
 import ch.qos.logback.classic.net.SyslogAppender;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
-import ch.qos.logback.core.Context;
+import ch.qos.logback.core.FileAppender;
 import ch.qos.logback.core.rolling.DefaultTimeBasedFileNamingAndTriggeringPolicy;
 import ch.qos.logback.core.rolling.RollingFileAppender;
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import ch.qos.logback.core.spi.FilterAttachable;
+import com.google.common.base.Optional;
 
 import static com.yammer.dropwizard.config.LoggingConfiguration.*;
 
 public class LogbackFactory {
-    private LogbackFactory() {
-        // singleton
-    }
-
+    private LogbackFactory() { /* singleton */ }
 
     public static SyslogAppender buildSyslogAppender(SyslogConfiguration syslog,
-                                                     Context context) {
-        final PatternLayout layout = new PatternLayout();
-        layout.setContext(context);
-        layout.setPattern("%c: %m");
-        layout.start();
-
+                                                     LoggerContext context,
+                                                     String name,
+                                                     Optional<String> logFormat) {
         final SyslogAppender appender = new SyslogAppender();
+        appender.setName(name);
         appender.setContext(context);
-        appender.setLayout(layout);
         appender.setSyslogHost(syslog.getHost());
-        appender.setFacility(syslog.getFacility());
+        appender.setFacility(syslog.getFacility().toString());
         addThresholdFilter(appender, syslog.getThreshold());
+
+        for (String format : logFormat.asSet()) {
+            appender.setSuffixPattern(format);
+        }
+
         appender.start();
 
         return appender;
     }
 
-    public static RollingFileAppender<ILoggingEvent> buildFileAppender(FileConfiguration file,
-                                                                       LoggerContext context) {
-        final DefaultTimeBasedFileNamingAndTriggeringPolicy<ILoggingEvent> triggeringPolicy =
-                new DefaultTimeBasedFileNamingAndTriggeringPolicy<ILoggingEvent>();
-        triggeringPolicy.setContext(context);
-
-        final TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new TimeBasedRollingPolicy<ILoggingEvent>();
-        rollingPolicy.setContext(context);
-        rollingPolicy.setFileNamePattern(file.getArchivedLogFilenamePattern());
-        rollingPolicy.setTimeBasedFileNamingAndTriggeringPolicy(
-                triggeringPolicy);
-        triggeringPolicy.setTimeBasedRollingPolicy(rollingPolicy);
-        rollingPolicy.setMaxHistory(file.getArchivedFileCount());
-
+    public static FileAppender<ILoggingEvent> buildFileAppender(FileConfiguration file,
+                                                                       LoggerContext context,
+                                                                       Optional<String> logFormat) {
         final LogFormatter formatter = new LogFormatter(context, file.getTimeZone());
+        for (String format : logFormat.asSet()) {
+            formatter.setPattern(format);
+        }
         formatter.start();
 
-        final RollingFileAppender<ILoggingEvent> appender = new RollingFileAppender<ILoggingEvent>();
+        final FileAppender<ILoggingEvent> appender = 
+            file.isArchive() ? new RollingFileAppender<ILoggingEvent>() :
+                               new FileAppender<ILoggingEvent>();
+
         appender.setAppend(true);
         appender.setContext(context);
         appender.setLayout(formatter);
         appender.setFile(file.getCurrentLogFilename());
         appender.setPrudent(false);
-        appender.setRollingPolicy(rollingPolicy);
-        appender.setTriggeringPolicy(triggeringPolicy);
+
         addThresholdFilter(appender, file.getThreshold());
 
-        rollingPolicy.setParent(appender);
-        rollingPolicy.start();
+        if (file.isArchive()) {
+
+            final DefaultTimeBasedFileNamingAndTriggeringPolicy<ILoggingEvent> triggeringPolicy =
+                    new DefaultTimeBasedFileNamingAndTriggeringPolicy<ILoggingEvent>();
+            triggeringPolicy.setContext(context);
+
+            final TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new TimeBasedRollingPolicy<ILoggingEvent>();
+            rollingPolicy.setContext(context);
+            rollingPolicy.setFileNamePattern(file.getArchivedLogFilenamePattern());
+            rollingPolicy.setTimeBasedFileNamingAndTriggeringPolicy(
+                    triggeringPolicy);
+            triggeringPolicy.setTimeBasedRollingPolicy(rollingPolicy);
+            rollingPolicy.setMaxHistory(file.getArchivedFileCount());
+
+            ((RollingFileAppender<ILoggingEvent>) appender).setRollingPolicy(rollingPolicy);
+            ((RollingFileAppender<ILoggingEvent>) appender).setTriggeringPolicy(triggeringPolicy);
+
+            rollingPolicy.setParent(appender);
+            rollingPolicy.start();
+        }
 
         appender.stop();
         appender.start();
@@ -76,8 +87,12 @@ public class LogbackFactory {
     }
 
     public static ConsoleAppender<ILoggingEvent> buildConsoleAppender(ConsoleConfiguration console,
-                                                                      LoggerContext context) {
+                                                                      LoggerContext context,
+                                                                      Optional<String> logFormat) {
         final LogFormatter formatter = new LogFormatter(context, console.getTimeZone());
+        for (String format : logFormat.asSet()) {
+            formatter.setPattern(format);
+        }
         formatter.start();
 
         final ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<ILoggingEvent>();
