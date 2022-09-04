664d975194bd28daecc2143d47d4caa13af92ad4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:30:48 +0200

    Modify the headers_list intermediate artifact path to avoid collisions for     headers_list files generated by apple_binary for objc_proto_library in a split     (ios_multi_cpus) transition.
        PiperOrigin-RevId: 257136166

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/IntermediateArtifacts.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/IntermediateArtifacts.java
@@ -21,13 +21,15 @@ import com.google.devtools.build.lib.actions.ArtifactRoot;
 import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.analysis.config.BuildConfiguration;
 import com.google.devtools.build.lib.cmdline.Label;
-import com.google.devtools.build.lib.packages.Type;
 import com.google.devtools.build.lib.rules.cpp.CppModuleMap;
 import com.google.devtools.build.lib.rules.cpp.CppModuleMap.UmbrellaHeaderStrategy;
+import com.google.devtools.build.lib.syntax.Type;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.PathFragment;
 
-/** Factory class for generating artifacts which are used as intermediate output. */
+/**
+ * Factory class for generating artifacts which are used as intermediate output.
+ */
 // TODO(bazel-team): This should really be named DerivedArtifacts as it contains methods for
 // final as well as intermediate artifacts.
 public final class IntermediateArtifacts {
@@ -39,56 +41,31 @@ public final class IntermediateArtifacts {
   private final String outputPrefix;
   private final UmbrellaHeaderStrategy umbrellaHeaderStrategy;
 
-  IntermediateArtifacts(
-      RuleContext ruleContext, String archiveFileNameSuffix, String outputPrefix) {
-    this(
-        ruleContext,
-        archiveFileNameSuffix,
-        outputPrefix,
-        ruleContext.getConfiguration(),
+  IntermediateArtifacts(RuleContext ruleContext, String archiveFileNameSuffix,
+      String outputPrefix) {
+    this(ruleContext, archiveFileNameSuffix, outputPrefix, ruleContext.getConfiguration(),
         UmbrellaHeaderStrategy.DO_NOT_GENERATE);
   }
 
   IntermediateArtifacts(RuleContext ruleContext, String archiveFileNameSuffix) {
-    this(
-        ruleContext,
-        archiveFileNameSuffix,
-        "",
-        ruleContext.getConfiguration(),
+    this(ruleContext, archiveFileNameSuffix, "", ruleContext.getConfiguration(),
         UmbrellaHeaderStrategy.DO_NOT_GENERATE);
   }
 
-  IntermediateArtifacts(
-      RuleContext ruleContext,
-      String archiveFileNameSuffix,
+  IntermediateArtifacts(RuleContext ruleContext, String archiveFileNameSuffix,
       UmbrellaHeaderStrategy umbrellaHeaderStrategy) {
-    this(
-        ruleContext,
-        archiveFileNameSuffix,
-        "",
-        ruleContext.getConfiguration(),
+    this(ruleContext, archiveFileNameSuffix, "", ruleContext.getConfiguration(),
         umbrellaHeaderStrategy);
   }
 
-  IntermediateArtifacts(
-      RuleContext ruleContext,
-      String archiveFileNameSuffix,
-      String outputPrefix,
+  IntermediateArtifacts(RuleContext ruleContext, String archiveFileNameSuffix, String outputPrefix,
       BuildConfiguration buildConfiguration) {
-    this(
-        ruleContext,
-        archiveFileNameSuffix,
-        outputPrefix,
-        buildConfiguration,
+    this(ruleContext, archiveFileNameSuffix, outputPrefix, buildConfiguration,
         UmbrellaHeaderStrategy.DO_NOT_GENERATE);
   }
 
-  IntermediateArtifacts(
-      RuleContext ruleContext,
-      String archiveFileNameSuffix,
-      String outputPrefix,
-      BuildConfiguration buildConfiguration,
-      UmbrellaHeaderStrategy umbrellaHeaderStrategy) {
+  IntermediateArtifacts(RuleContext ruleContext, String archiveFileNameSuffix, String outputPrefix,
+      BuildConfiguration buildConfiguration, UmbrellaHeaderStrategy umbrellaHeaderStrategy) {
     this.ruleContext = ruleContext;
     this.buildConfiguration = buildConfiguration;
     this.archiveFileNameSuffix = Preconditions.checkNotNull(archiveFileNameSuffix);
@@ -96,7 +73,9 @@ public final class IntermediateArtifacts {
     this.umbrellaHeaderStrategy = umbrellaHeaderStrategy;
   }
 
-  /** Returns the archive file name suffix. */
+  /**
+   * Returns the archive file name suffix.
+   */
   public String archiveFileNameSuffix() {
     return archiveFileNameSuffix;
   }
@@ -129,7 +108,7 @@ public final class IntermediateArtifacts {
   }
 
   /**
-   * The .objlist file, which contains a list of paths of object files to archive and is read by
+   * The .objlist file, which contains a list of paths of object files to archive  and is read by
    * libtool (via -filelist flag) in the archive action.
    */
   public Artifact archiveObjList() {
@@ -156,10 +135,10 @@ public final class IntermediateArtifacts {
 
   /**
    * The artifact which is the binary (or library) which is comprised of one or more .a files linked
-   * together. It also contains full debug symbol information, compared to the artifact returned by
-   * {@link #strippedSingleArchitectureBinary}. This artifact will serve as input for the symbol
-   * strip action and is only created when --compilation_mode=opt and --objc_enable_binary_stripping
-   * are specified.
+   * together. It also contains full debug symbol information, compared to the artifact returned
+   * by {@link #strippedSingleArchitectureBinary}. This artifact will serve as input for the symbol
+   * strip action and is only created when --compilation_mode=opt and
+   * --objc_enable_binary_stripping are specified.
    */
   public Artifact unstrippedSingleArchitectureBinary() {
     return appendExtension("_bin_unstripped");
@@ -173,7 +152,9 @@ public final class IntermediateArtifacts {
     return appendExtension("_lipobin");
   }
 
-  /** Lipo archive generated by combining one or more linked archives. */
+  /**
+   * Lipo archive generated by combining one or more linked archives.
+   */
   public Artifact combinedArchitectureArchive() {
     return appendExtension("_lipo.a");
   }
@@ -200,20 +181,32 @@ public final class IntermediateArtifacts {
     return scopedArtifact(scopeRelative, /* inGenfiles = */ false);
   }
 
-  /** The {@code .a} file which contains all the compiled sources for a rule. */
+  /**
+   * The {@code .a} file which contains all the compiled sources for a rule.
+   */
   public Artifact archive() {
     // The path will be {RULE_PACKAGE}/lib{RULEBASENAME}{SUFFIX}.a
     String basename = PathFragment.create(ruleContext.getLabel().getName()).getBaseName();
-    return scopedArtifact(
-        PathFragment.create(String.format("lib%s%s.a", basename, archiveFileNameSuffix)));
+    return scopedArtifact(PathFragment.create(String.format(
+        "lib%s%s.a", basename, archiveFileNameSuffix)));
+  }
+
+  /** The artifact for the .headers file output by the header thinning action for this source. */
+  public Artifact headersListFile(Artifact objectFile) {
+    PathFragment headersListArtifactPath =
+        FileSystemUtils.replaceExtension(objectFile.getRootRelativePath(), ".headers_list");
+    return ruleContext.getUniqueDirectoryArtifact(
+        "_header_lists",
+        headersListArtifactPath,
+        buildConfiguration.getBinDirectory(ruleContext.getRule().getRepository()));
   }
 
   /**
    * Debug symbol file generated for a stripped linked binary.
    *
    * <p>The name of the debug symbol file matches that of stripped binary plus that of the debug
-   * symbol file extension (.dwarf), so we must know if the binary has been stripped or not as that
-   * will modify its name.
+   * symbol file extension (.dwarf), so we must know if the binary has been stripped
+   * or not as that will modify its name.
    */
   public Artifact dsymSymbolForStrippedBinary() {
     return dsymSymbol("bin");
@@ -229,7 +222,9 @@ public final class IntermediateArtifacts {
     return dsymSymbol("bin_unstripped");
   }
 
-  /** Debug symbol file generated for a linked binary, for a specific architecture. */
+  /**
+   * Debug symbol file generated for a linked binary, for a specific architecture.
+   */
   private Artifact dsymSymbol(String suffix) {
     return appendExtension(String.format("_%s.dwarf", suffix));
   }
@@ -244,17 +239,23 @@ public final class IntermediateArtifacts {
     return appendExtension(String.format("_%s%s", arch, suffix));
   }
 
-  /** Linkmap representation */
+  /**
+   * Linkmap representation
+   */
   public Artifact linkmap() {
     return appendExtension(LINKMAP_SUFFIX);
   }
 
-  /** Linkmap representation for a specific architecture. */
+  /**
+   * Linkmap representation for a specific architecture.
+   */
   public Artifact linkmap(String arch) {
     return architectureRepresentation(arch, LINKMAP_SUFFIX);
   }
 
-  /** {@link CppModuleMap} that provides the clang module map for this target. */
+  /**
+   * {@link CppModuleMap} that provides the clang module map for this target.
+   */
   public CppModuleMap moduleMap() {
     String moduleName;
     if (ruleContext.attributes().isAttributeValueExplicitlySpecified("module_name")) {
@@ -291,19 +292,22 @@ public final class IntermediateArtifacts {
    * given the original unpruned static library containing J2ObjC-translated code.
    */
   public Artifact j2objcPrunedArchive(Artifact unprunedArchive) {
-    PathFragment prunedSourceArtifactPath =
-        FileSystemUtils.appendWithoutExtension(unprunedArchive.getRootRelativePath(), "_pruned");
+    PathFragment prunedSourceArtifactPath = FileSystemUtils.appendWithoutExtension(
+        unprunedArchive.getRootRelativePath(), "_pruned");
     return ruleContext.getUniqueDirectoryArtifact(
         "_j2objc_pruned",
         prunedSourceArtifactPath,
         buildConfiguration.getBinDirectory(ruleContext.getRule().getRepository()));
   }
 
-  /** Returns artifact name prefixed with an output prefix if specified. */
+  /**
+   * Returns artifact name prefixed with an output prefix if specified.
+   */
   private String addOutputPrefix(String baseName, String artifactName) {
     if (!outputPrefix.isEmpty()) {
       return String.format("%s-%s%s", baseName, outputPrefix, artifactName);
     }
     return String.format("%s%s", baseName, artifactName);
   }
+
 }
