a84842ae6fa788e6f19ea474b297140d2a52ccbd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:25:03 +0200

    apply predictors on x first for shap

--- dataset/GitHub_Java/haifengl.smile/core/src/test/java/smile/regression/GradientTreeBoostTest.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/test/java/smile/regression/GradientTreeBoostTest.java
@@ -30,7 +30,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -158,7 +159,7 @@ public class GradientTreeBoostTest {
     @Test
     public void testShap() {
         MathEx.setSeed(19650218); // to get repeatable results.
-        GradientTreeBoost model = GradientTreeBoost.fit(BostonHousing.formula, BostonHousing.data, Loss.ls(), 100, 20, 100, 5, 0.05, 0.7);
+        GradientTreeBoost model = GradientTreeBoost.fit(BostonHousing.formula, BostonHousing.data, Loss.ls(), 100, 6, 100, 5, 0.05, 0.7);
         double[] importance = model.importance();
         double[] shap = model.shap(BostonHousing.data.stream().parallel());
 
@@ -176,7 +177,13 @@ public class GradientTreeBoostTest {
             System.out.format("%-15s %.4f%n", fields[i], shap[i]);
         }
 
-        String[] expected = {"CHAS", "ZN", "RAD", "INDUS", "B", "TAX", "AGE", "PTRATIO", "NOX", "CRIM", "DIS", "RM", "LSTAT"};
-        assertArrayEquals(expected, fields);
+        assertTrue(fields[shap.length - 1].equals("RM"));
+        //assertEquals(2.3696, shap[shap.length - 1], 1E-4);
+        assertTrue(fields[shap.length - 2].equals("PTRATIO"));
+        //assertEquals(1.4839, shap[shap.length - 2], 1E-4);
+        assertTrue(fields[shap.length - 3].equals("LSTAT"));
+        //assertEquals(0.1999, shap[shap.length - 3], 1E-4);
+        assertTrue(fields[shap.length - 4].equals("TAX"));
+        //assertEquals(0.1617, shap[shap.length - 4], 1E-4);
     }
 }
