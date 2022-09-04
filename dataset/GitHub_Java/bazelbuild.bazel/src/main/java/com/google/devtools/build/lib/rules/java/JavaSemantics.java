c454aae4f8f3b6cd7da468dd481c22ab7200c7b3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:43:40 +0200

    Refactor java_libary.deps rule kind validation
        PiperOrigin-RevId: 249897560

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaSemantics.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaSemantics.java
@@ -20,7 +20,6 @@ import static com.google.devtools.build.lib.packages.ImplicitOutputsFunction.fro
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Streams;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.OutputGroupInfo;
@@ -32,7 +31,6 @@ import com.google.devtools.build.lib.analysis.Runfiles.Builder;
 import com.google.devtools.build.lib.analysis.TransitiveInfoCollection;
 import com.google.devtools.build.lib.analysis.actions.CustomCommandLine;
 import com.google.devtools.build.lib.analysis.actions.Substitution.ComputedSubstitution;
-import com.google.devtools.build.lib.analysis.configuredtargets.RuleConfiguredTarget.Mode;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
@@ -40,14 +38,15 @@ import com.google.devtools.build.lib.packages.Attribute.LabelLateBoundDefault;
 import com.google.devtools.build.lib.packages.Attribute.LabelListLateBoundDefault;
 import com.google.devtools.build.lib.packages.ImplicitOutputsFunction.SafeImplicitOutputsFunction;
 import com.google.devtools.build.lib.packages.RuleClass.ConfiguredTargetFactory.RuleErrorException;
-import com.google.devtools.build.lib.packages.Type;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainFeatures.FeatureConfiguration;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainProvider;
 import com.google.devtools.build.lib.rules.java.DeployArchiveBuilder.Compression;
 import com.google.devtools.build.lib.rules.java.JavaCompilationArgsProvider.ClasspathType;
+import com.google.devtools.build.lib.rules.java.JavaConfiguration.JavaOptimizationMode;
 import com.google.devtools.build.lib.rules.java.JavaConfiguration.OneVersionEnforcementLevel;
 import com.google.devtools.build.lib.rules.java.proto.GeneratedExtensionRegistryProvider;
 import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
+import com.google.devtools.build.lib.syntax.Type;
 import com.google.devtools.build.lib.util.FileType;
 import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.vfs.PathFragment;
@@ -174,10 +173,12 @@ public interface JavaSemantics {
           JavaConfiguration.class,
           (rule, attributes, javaConfig) -> {
             // Use a modicum of smarts to avoid implicit dependencies where we don't need them.
+            JavaOptimizationMode optMode = javaConfig.getJavaOptimizationMode();
             boolean hasProguardSpecs =
                 attributes.has("proguard_specs")
                     && !attributes.get("proguard_specs", LABEL_LIST).isEmpty();
-            if (!hasProguardSpecs) {
+            if (optMode == JavaOptimizationMode.NOOP
+                || (optMode == JavaOptimizationMode.LEGACY && !hasProguardSpecs)) {
               return ImmutableList.<Label>of();
             }
             return ImmutableList.copyOf(
@@ -319,32 +320,12 @@ public interface JavaSemantics {
    */
   boolean isJavaExecutableSubstitution();
 
-  static TransitiveInfoCollection getTestSupport(RuleContext ruleContext) {
-    if (!isJavaBinaryOrJavaTest(ruleContext)) {
-      return null;
-    }
-
-    if (useLegacyJavaTest(ruleContext)) {
-      return null;
-    }
-
-    boolean createExecutable = ruleContext.attributes().get("create_executable", Type.BOOLEAN);
-    if (createExecutable && ruleContext.attributes().get("use_testrunner", Type.BOOLEAN)) {
-      return Iterables.getOnlyElement(ruleContext.getPrerequisites("$testsupport", Mode.TARGET));
-    } else {
-      return null;
-    }
-  }
-
-  static boolean isJavaBinaryOrJavaTest(RuleContext ruleContext) {
-    return ruleContext.getRule().getRuleClass().equals("java_binary")
-        || ruleContext.getRule().getRuleClass().equals("java_test");
-  }
-
-  static boolean useLegacyJavaTest(RuleContext ruleContext) {
-    return !ruleContext.attributes().isAttributeValueExplicitlySpecified("test_class")
-        && ruleContext.getFragment(JavaConfiguration.class).useLegacyBazelJavaTest();
-  }
+  /**
+   * Optionally creates a file containing the relative classpaths within the runfiles tree. If
+   * {@link Optional#isPresent()}, then the caller should ensure the file appears in the runfiles.
+   */
+  Optional<Artifact> createClasspathsFile(RuleContext ruleContext, JavaCommon javaCommon)
+      throws InterruptedException;
 
   /** Adds extra runfiles for a {@code java_binary} rule. */
   void addRunfilesForBinary(
