3e175899ed4400a6e7cf7b4f8a8d6f38a744adbb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 20:41:35 +0200

    remote: fix unexpected IO error (not a directory)
        Closes #7174.
    
        PiperOrigin-RevId: 229914032

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/SingleBuildFileCache.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/SingleBuildFileCache.java
@@ -60,7 +60,7 @@ public class SingleBuildFileCache implements MetadataProvider {
           .get(
               input.getExecPathString(),
               () -> {
-                Path path = ActionInputHelper.toInputPath(input, execRoot);
+                Path path = ActionInputHelper.toPath(input, execRoot);
                 try {
                   FileArtifactValue metadata = FileArtifactValue.create(path);
                   if (metadata.getType().isDirectory()) {
