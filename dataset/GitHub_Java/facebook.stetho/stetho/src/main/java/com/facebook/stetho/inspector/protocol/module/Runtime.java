3e6d735748707170b7735d2443791d6d43001401
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:00:16 +0200

    Remove private class (CallFunctionOnResponse) from signature of the public method     (otherwise class can't be extended and method overridden (Only @ChromeDevtoolsMethod methods are detected and method needed to be overridden because look-up is done only in declared methods)

--- dataset/GitHub_Java/facebook.stetho/stetho/src/main/java/com/facebook/stetho/inspector/protocol/module/Runtime.java
+++ dataset/GitHub_Java/facebook.stetho/stetho/src/main/java/com/facebook/stetho/inspector/protocol/module/Runtime.java
@@ -105,13 +105,6 @@ public class Runtime implements ChromeDevtoolsDomain {
     return session;
   }
 
-  /**
-   * Removes objects from peer's session previously added by {@link #mapObject}
-   */
-  public static void releaseObject(JsonRpcPeer peer, Integer id) throws JSONException {
-    getSession(peer).getObjects().removeObjectById(id);
-  }
-
   @ChromeDevtoolsMethod
   public void releaseObject(JsonRpcPeer peer, JSONObject params) throws JSONException {
     String objectId = params.getString("objectId");
@@ -124,7 +117,7 @@ public class Runtime implements ChromeDevtoolsDomain {
   }
 
   @ChromeDevtoolsMethod
-  public CallFunctionOnResponse callFunctionOn(JsonRpcPeer peer, JSONObject params)
+  public JsonRpcResult callFunctionOn(JsonRpcPeer peer, JSONObject params)
       throws JsonRpcException {
     CallFunctionOnRequest args = mObjectMapper.convertValue(params, CallFunctionOnRequest.class);
 
