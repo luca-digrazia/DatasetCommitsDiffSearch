844e59f37c2c7102964d988a3209dc8fc6b6fbb0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:24:14 +0200

    Pass env var GRAYLOG_ELASTICSEARCH_VERSION to NodeInstance (#8432)
        The server will need a config setting to decide which Elasticsearch driver to bind.
        This adds a `version` property to `ElasticsearchInstanceFactory`, the value of which is passed to server as an environment variable.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog/testing/completebackend/GraylogBackend.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog/testing/completebackend/GraylogBackend.java
@@ -1,18 +1,18 @@
-/*
- * Copyright (C) 2020 Graylog, Inc.
+/**
+ * This file is part of Graylog.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog.testing.completebackend;
 
@@ -24,9 +24,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.Network;
 
-import java.net.URL;
-import java.nio.file.Path;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -35,21 +32,17 @@ import java.util.concurrent.Future;
 public class GraylogBackend {
 
     private static final Logger LOG = LoggerFactory.getLogger(GraylogBackend.class);
-    private final Network network;
     private final ElasticsearchInstance es;
     private final MongoDBInstance mongodb;
     private final NodeInstance node;
 
     private static GraylogBackend instance;
 
-    public static GraylogBackend createStarted(int[] extraPorts,
-            ElasticsearchInstanceFactory elasticsearchInstanceFactory, List<Path> pluginJars, Path mavenProjectDir,
-            List<URL> mongoDBFixtures) {
+    public static GraylogBackend createStarted(int[] extraPorts, ElasticsearchInstanceFactory elasticsearchInstanceFactory) {
         if (instance == null) {
-            instance = createStartedBackend(extraPorts, elasticsearchInstanceFactory, pluginJars, mavenProjectDir,
-                    mongoDBFixtures);
+            instance = createStartedBackend(extraPorts, elasticsearchInstanceFactory);
         } else {
-            instance.fullReset(mongoDBFixtures);
+            instance.fullReset();
             LOG.info("Reusing running backend");
         }
 
@@ -59,32 +52,24 @@ public class GraylogBackend {
     // Starting ES instance in parallel thread to save time.
     // MongoDB and the node have to be started in sequence however, because the the node might crash,
     // if a MongoDb instance isn't already present while it's starting up.
-    private static GraylogBackend createStartedBackend(int[] extraPorts,
-            ElasticsearchInstanceFactory elasticsearchInstanceFactory, List<Path> pluginJars, Path mavenProjectDir,
-            List<URL> mongoDBFixtures) {
+    private static GraylogBackend createStartedBackend(int[] extraPorts, ElasticsearchInstanceFactory elasticsearchInstanceFactory) {
         Network network = Network.newNetwork();
 
         ExecutorService executor = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setNameFormat("build-es-container-for-api-it").build());
 
         Future<ElasticsearchInstance> esFuture = executor.submit(() -> elasticsearchInstanceFactory.create(network));
 
-        MongoDBInstance mongoDB =
-                MongoDBInstance.createStarted(network, MongoDBInstance.Lifecycle.CLASS);
-        mongoDB.dropDatabase();
-        mongoDB.importFixtures(mongoDBFixtures);
+        MongoDBInstance mongoDB = MongoDBInstance.createStarted(network, MongoDBInstance.Lifecycle.CLASS);
+
+        NodeInstance node = NodeInstance.createStarted(
+                network,
+                MongoDBInstance.internalUri(),
+                ElasticsearchInstance.internalUri(),
+                elasticsearchInstanceFactory.version(),
+                extraPorts);
 
         try {
-            // Wait for ES before starting the Graylog node to avoid any race conditions
-            ElasticsearchInstance esInstance = esFuture.get();
-            NodeInstance node = NodeInstance.createStarted(
-                    network,
-                    MongoDBInstance.internalUri(),
-                    ElasticsearchInstance.internalUri(),
-                    elasticsearchInstanceFactory.version(),
-                    extraPorts,
-                    pluginJars, mavenProjectDir);
-
-            return new GraylogBackend(network, esInstance, mongoDB, node);
+            return new GraylogBackend(esFuture.get(), mongoDB, node);
         } catch (InterruptedException | ExecutionException e) {
             LOG.error("Container creation aborted", e);
             throw new RuntimeException(e);
@@ -93,8 +78,7 @@ public class GraylogBackend {
         }
     }
 
-    private GraylogBackend(Network network, ElasticsearchInstance es, MongoDBInstance mongodb, NodeInstance node) {
-        this.network = network;
+    private GraylogBackend(ElasticsearchInstance es, MongoDBInstance mongodb, NodeInstance node) {
         this.es = es;
         this.mongodb = mongodb;
         this.node = node;
@@ -105,10 +89,8 @@ public class GraylogBackend {
         es.cleanUp();
     }
 
-    public void fullReset(List<URL> mongoDBFixtures) {
-        LOG.debug("Resetting backend.");
+    public void fullReset() {
         purgeData();
-        mongodb.importFixtures(mongoDBFixtures);
         node.restart();
     }
 
@@ -130,9 +112,5 @@ public class GraylogBackend {
 
     public int mappedPortFor(int originalPort) {
         return node.mappedPortFor(originalPort);
-    }
-
-    public Network network() {
-        return this.network;
     }
 }
