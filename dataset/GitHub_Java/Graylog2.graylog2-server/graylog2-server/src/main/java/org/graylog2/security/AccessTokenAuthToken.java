0b4a968daa0c5645303437f800805648310572dc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:35:56 +0200

    Add equals/hashcode/toString to AccessTokenAuthToken

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/AccessTokenAuthToken.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/AccessTokenAuthToken.java
@@ -1,24 +1,25 @@
-/*
- * Copyright 2013 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.security;
 
+import com.google.common.base.MoreObjects;
 import org.apache.shiro.authc.HostAuthenticationToken;
+
+import java.util.Objects;
 
 public class AccessTokenAuthToken implements HostAuthenticationToken {
     private final String token;
@@ -46,5 +47,27 @@ public class AccessTokenAuthToken implements HostAuthenticationToken {
     @Override
     public String getHost() {
         return host;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccessTokenAuthToken that = (AccessTokenAuthToken) o;
+        return Objects.equals(token, that.token) &&
+                Objects.equals(host, that.host);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(token, host);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("token", token)
+                .add("host", host)
+                .toString();
     }
 }
