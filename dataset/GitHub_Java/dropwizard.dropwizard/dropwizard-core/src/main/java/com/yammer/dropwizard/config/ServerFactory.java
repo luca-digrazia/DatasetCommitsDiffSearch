7afce5ef48abfc4542809e2d314e2b9a24127f94
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:01:20 +0200

    Allow admin servlets to run on the same port as the main service (mounted under /admin)

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/ServerFactory.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/config/ServerFactory.java
@@ -176,8 +176,8 @@ public class ServerFactory {
     private Handler createHandler(Environment env) {
         final HandlerCollection collection = new HandlerCollection();
 
-        collection.addHandler(createInternalServlet(env));
         collection.addHandler(createExternalServlet(env.getServlets(), env.getFilters(), env.getServletListeners()));
+        collection.addHandler(createInternalServlet(env));
 
         if (requestLogHandlerFactory.isEnabled()) {
             collection.addHandler(requestLogHandlerFactory.build());
