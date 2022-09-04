21b18a115ce588cf66d0969b2529825e8d2b54ae
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 23:43:29 +0200

    remote: fix out-of-date documentation
        The action cache prefix is "ac", not "actioncache".
    
        Change-Id: I841a026133ab3b4ec5b58a0cf29252dae49434fe
        PiperOrigin-RevId: 179437730

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/blobstore/RestBlobStore.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/remote/blobstore/RestBlobStore.java
@@ -64,16 +64,13 @@ public final class RestBlobStore implements SimpleBlobStore {
    * @param baseUrl base URL for the remote cache
    * @param poolSize maximum number of simultaneous connections
    */
-  public RestBlobStore(String baseUrl, int timeoutMillis, @Nullable Credentials creds)
+  public RestBlobStore(String baseUrl, int poolSize, int timeoutMillis, @Nullable Credentials creds)
       throws IOException {
     validateUrl(baseUrl);
     this.baseUrl = baseUrl;
     PoolingHttpClientConnectionManager connMan = new PoolingHttpClientConnectionManager();
-    // We'll use as many connections as necessary. The connection pool tries to re-use open
-    // connections before creating new ones, so in practice we should have as many connections
-    // as concurrent actions.
-    connMan.setDefaultMaxPerRoute(Integer.MAX_VALUE);
-    connMan.setMaxTotal(Integer.MAX_VALUE);
+    connMan.setDefaultMaxPerRoute(poolSize);
+    connMan.setMaxTotal(poolSize);
     clientFactory = HttpClientBuilder.create();
     clientFactory.setConnectionManager(connMan);
     clientFactory.setConnectionManagerShared(true);
