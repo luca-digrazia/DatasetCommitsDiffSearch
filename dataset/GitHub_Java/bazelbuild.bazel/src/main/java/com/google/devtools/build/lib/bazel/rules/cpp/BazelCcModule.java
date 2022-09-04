188c69c2537458e58340047fc43f0c1af7d3e5e2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:15:13 +0200

    Add support for ObjC in BazelCppSemantics
        PiperOrigin-RevId: 368843101

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCcModule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCcModule.java
@@ -14,94 +14,209 @@
 
 package com.google.devtools.build.lib.bazel.rules.cpp;
 
+import com.google.common.collect.ImmutableList;
 import com.google.devtools.build.lib.actions.Artifact;
-import com.google.devtools.build.lib.analysis.skylark.SkylarkRuleContext;
-import com.google.devtools.build.lib.rules.cpp.CcCompilationHelper.CompilationInfo;
-import com.google.devtools.build.lib.rules.cpp.CcCompilationInfo;
+import com.google.devtools.build.lib.analysis.platform.ConstraintValueInfo;
+import com.google.devtools.build.lib.analysis.starlark.StarlarkActionFactory;
+import com.google.devtools.build.lib.analysis.starlark.StarlarkRuleContext;
+import com.google.devtools.build.lib.rules.cpp.CcCompilationContext;
 import com.google.devtools.build.lib.rules.cpp.CcCompilationOutputs;
-import com.google.devtools.build.lib.rules.cpp.CcLinkParams;
-import com.google.devtools.build.lib.rules.cpp.CcLinkingHelper.LinkingInfo;
-import com.google.devtools.build.lib.rules.cpp.CcLinkingInfo;
+import com.google.devtools.build.lib.rules.cpp.CcDebugInfoContext;
+import com.google.devtools.build.lib.rules.cpp.CcLinkingContext;
+import com.google.devtools.build.lib.rules.cpp.CcLinkingContext.LinkerInput;
+import com.google.devtools.build.lib.rules.cpp.CcLinkingOutputs;
 import com.google.devtools.build.lib.rules.cpp.CcModule;
-import com.google.devtools.build.lib.rules.cpp.CcModule.CcSkylarkInfo;
-import com.google.devtools.build.lib.rules.cpp.CcToolchainFeatures.FeatureConfiguration;
+import com.google.devtools.build.lib.rules.cpp.CcToolchainConfigInfo;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainProvider;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainVariables;
-import com.google.devtools.build.lib.rules.cpp.LinkerInputs.LibraryToLink;
-import com.google.devtools.build.lib.skylarkbuildapi.cpp.BazelCcModuleApi;
-import com.google.devtools.build.lib.syntax.EvalException;
-import com.google.devtools.build.lib.syntax.Runtime;
-import com.google.devtools.build.lib.syntax.SkylarkList;
+import com.google.devtools.build.lib.rules.cpp.CppModuleMap;
+import com.google.devtools.build.lib.rules.cpp.CppSemantics;
+import com.google.devtools.build.lib.rules.cpp.FdoContext;
+import com.google.devtools.build.lib.rules.cpp.FeatureConfigurationForStarlark;
+import com.google.devtools.build.lib.rules.cpp.LibraryToLink;
+import com.google.devtools.build.lib.rules.cpp.LtoBackendArtifacts;
+import com.google.devtools.build.lib.starlarkbuildapi.cpp.BazelCcModuleApi;
+import net.starlark.java.eval.EvalException;
+import net.starlark.java.eval.Sequence;
+import net.starlark.java.eval.StarlarkInt;
+import net.starlark.java.eval.StarlarkList;
+import net.starlark.java.eval.StarlarkThread;
+import net.starlark.java.eval.Tuple;
 
 /**
- * A module that contains Skylark utilities for C++ support.
+ * A module that contains Starlark utilities for C++ support.
  *
- * <p>This is a work in progress. The API is guarded behind
- * --experimental_cc_skylark_api_enabled_packages. The API is under development and unstable.
  */
 public class BazelCcModule extends CcModule
     implements BazelCcModuleApi<
+        StarlarkActionFactory,
+        Artifact,
+        FdoContext,
+        ConstraintValueInfo,
+        StarlarkRuleContext,
         CcToolchainProvider,
-        FeatureConfiguration,
-        CompilationInfo,
-        CcCompilationInfo,
+        FeatureConfigurationForStarlark,
+        CcCompilationContext,
         CcCompilationOutputs,
-        LinkingInfo,
-        CcLinkingInfo,
-        CcToolchainVariables,
+        CcLinkingOutputs,
+        LtoBackendArtifacts,
+        LinkerInput,
         LibraryToLink,
-        CcLinkParams,
-        CcSkylarkInfo> {
+        CcLinkingContext,
+        CcToolchainVariables,
+        CcToolchainConfigInfo,
+        CcDebugInfoContext,
+        CppModuleMap> {
 
   @Override
-  public CompilationInfo compile(
-      SkylarkRuleContext skylarkRuleContext,
-      FeatureConfiguration skylarkFeatureConfiguration,
-      CcToolchainProvider skylarkCcToolchainProvider,
-      SkylarkList<Artifact> sources,
-      SkylarkList<Artifact> headers,
-      Object skylarkIncludes,
-      Object skylarkCopts,
-      SkylarkList<CcCompilationInfo> ccCompilationInfos)
-      throws EvalException {
-    return BazelCcModule.compile(
-        BazelCppSemantics.INSTANCE,
-        skylarkRuleContext,
-        skylarkFeatureConfiguration,
-        skylarkCcToolchainProvider,
-        sources,
-        headers,
-        skylarkIncludes,
-        skylarkCopts,
-        /* generateNoPicOutputs= */ "conditionally",
-        /* generatePicOutputs= */ "conditionally",
-        /* skylarkAdditionalCompilationInputs= */ Runtime.NONE,
-        /* skylarkAdditionalIncludeScanningRoots= */ Runtime.NONE,
-        ccCompilationInfos,
-        /* purpose= */ Runtime.NONE);
+  public CppSemantics getSemantics() {
+    return BazelCppSemantics.CPP;
   }
 
   @Override
-  public LinkingInfo link(
-      SkylarkRuleContext skylarkRuleContext,
-      FeatureConfiguration skylarkFeatureConfiguration,
-      CcToolchainProvider skylarkCcToolchainProvider,
-      CcCompilationOutputs ccCompilationOutputs,
-      Object skylarkLinkopts,
-      Object dynamicLibrary,
-      SkylarkList<CcLinkingInfo> skylarkCcLinkingInfos,
-      boolean neverLink)
+  public CppSemantics getSemantics(Language language) {
+    return (language == Language.CPP) ? BazelCppSemantics.CPP : BazelCppSemantics.OBJC;
+  }
+
+  @Override
+  public Tuple compile(
+      StarlarkActionFactory starlarkActionFactoryApi,
+      FeatureConfigurationForStarlark starlarkFeatureConfiguration,
+      CcToolchainProvider starlarkCcToolchainProvider,
+      Sequence<?> sources, // <Artifact> expected
+      Sequence<?> publicHeaders, // <Artifact> expected
+      Sequence<?> privateHeaders, // <Artifact> expected
+      Object textualHeaders,
+      Object additionalExportedHeaders,
+      Sequence<?> includes, // <String> expected
+      Sequence<?> quoteIncludes, // <String> expected
+      Sequence<?> systemIncludes, // <String> expected
+      Sequence<?> frameworkIncludes, // <String> expected
+      Sequence<?> defines, // <String> expected
+      Sequence<?> localDefines, // <String> expected
+      String includePrefix,
+      String stripIncludePrefix,
+      Sequence<?> userCompileFlags, // <String> expected
+      Sequence<?> ccCompilationContexts, // <CcCompilationContext> expected
+      String name,
+      boolean disallowPicOutputs,
+      boolean disallowNopicOutputs,
+      Sequence<?> additionalInputs, // <Artifact> expected
+      Object moduleMap,
+      Object additionalModuleMaps,
+      Object propagateModuleMapToCompileAction,
+      Object doNotGenerateModuleMap,
+      Object codeCoverageEnabled,
+      Object hdrsCheckingMode,
+      Object variablesExtension,
+      Object language,
+      StarlarkThread thread)
+      throws EvalException, InterruptedException {
+    return compile(
+        starlarkActionFactoryApi,
+        starlarkFeatureConfiguration,
+        starlarkCcToolchainProvider,
+        sources,
+        publicHeaders,
+        privateHeaders,
+        textualHeaders,
+        additionalExportedHeaders,
+        includes,
+        quoteIncludes,
+        systemIncludes,
+        frameworkIncludes,
+        defines,
+        localDefines,
+        includePrefix,
+        stripIncludePrefix,
+        userCompileFlags,
+        ccCompilationContexts,
+        name,
+        disallowPicOutputs,
+        disallowNopicOutputs,
+        /* grepIncludes= */ null,
+        /* headersForClifDoNotUseThisParam= */ ImmutableList.of(),
+        StarlarkList.immutableCopyOf(
+            Sequence.cast(additionalInputs, Artifact.class, "additional_inputs")),
+        moduleMap,
+        additionalModuleMaps,
+        propagateModuleMapToCompileAction,
+        doNotGenerateModuleMap,
+        codeCoverageEnabled,
+        hdrsCheckingMode,
+        variablesExtension,
+        language,
+        thread);
+  }
+
+  @Override
+  public CcLinkingOutputs link(
+      StarlarkActionFactory actions,
+      FeatureConfigurationForStarlark starlarkFeatureConfiguration,
+      CcToolchainProvider starlarkCcToolchainProvider,
+      Object compilationOutputs,
+      Sequence<?> userLinkFlags, // <String> expected
+      Sequence<?> linkingContexts, // <CcLinkingContext> expected
+      String name,
+      String language,
+      String outputType,
+      boolean linkDepsStatically,
+      StarlarkInt stamp,
+      Object additionalInputs, // <Artifact> expected
+      Object grepIncludes,
+      Object linkArtifactNameSuffix,
+      Object neverLink,
+      Object alwaysLink,
+      Object testOnlyTarget,
+      Object variablesExtension,
+      Object nativeDeps,
+      Object wholeArchive,
+      Object additionalLinkstampDefines,
+      Object onlyForDynamicLibs,
+      Object linkerOutputs,
+      StarlarkThread thread)
       throws InterruptedException, EvalException {
-    return BazelCcModule.link(
-        BazelCppSemantics.INSTANCE,
-        skylarkRuleContext,
-        skylarkFeatureConfiguration,
-        skylarkCcToolchainProvider,
-        ccCompilationOutputs,
-        skylarkLinkopts,
-        /* shouldCreateStaticLibraries= */ true,
-        dynamicLibrary,
-        skylarkCcLinkingInfos,
-        neverLink);
+    return super.link(
+        actions,
+        starlarkFeatureConfiguration,
+        starlarkCcToolchainProvider,
+        convertFromNoneable(compilationOutputs, /* defaultValue= */ null),
+        userLinkFlags,
+        linkingContexts,
+        name,
+        language,
+        outputType,
+        linkDepsStatically,
+        stamp,
+        additionalInputs,
+        /* grepIncludes= */ null,
+        linkArtifactNameSuffix,
+        neverLink,
+        alwaysLink,
+        testOnlyTarget,
+        variablesExtension,
+        nativeDeps,
+        wholeArchive,
+        additionalLinkstampDefines,
+        onlyForDynamicLibs,
+        linkerOutputs,
+        thread);
+  }
+
+  @Override
+  public CcCompilationOutputs createCompilationOutputsFromStarlark(
+      Object objectsObject, Object picObjectsObject) throws EvalException {
+    return super.createCompilationOutputsFromStarlark(objectsObject, picObjectsObject);
+  }
+
+  @Override
+  public CcCompilationOutputs mergeCcCompilationOutputsFromStarlark(Sequence<?> compilationOutputs)
+      throws EvalException {
+    CcCompilationOutputs.Builder ccCompilationOutputsBuilder = CcCompilationOutputs.builder();
+    for (CcCompilationOutputs ccCompilationOutputs :
+        Sequence.cast(compilationOutputs, CcCompilationOutputs.class, "compilation_outputs")) {
+      ccCompilationOutputsBuilder.merge(ccCompilationOutputs);
+    }
+    return ccCompilationOutputsBuilder.build();
   }
 }
--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
@@ -14,45 +14,132 @@
 
 package com.google.devtools.build.lib.bazel.rules.cpp;
 
-import com.google.devtools.build.lib.actions.Artifact;
+import com.google.common.collect.ImmutableSet;
 import com.google.devtools.build.lib.analysis.RuleContext;
-import com.google.devtools.build.lib.rules.cpp.CppCompilationContext.Builder;
+import com.google.devtools.build.lib.analysis.RuleErrorConsumer;
+import com.google.devtools.build.lib.analysis.TransitiveInfoCollection;
+import com.google.devtools.build.lib.analysis.config.BuildConfiguration;
+import com.google.devtools.build.lib.cmdline.Label;
+import com.google.devtools.build.lib.packages.AspectDescriptor;
+import com.google.devtools.build.lib.packages.Provider;
+import com.google.devtools.build.lib.packages.StarlarkProvider;
+import com.google.devtools.build.lib.packages.StructImpl;
+import com.google.devtools.build.lib.rules.cpp.AspectLegalCppSemantics;
+import com.google.devtools.build.lib.rules.cpp.CcToolchainFeatures.FeatureConfiguration;
+import com.google.devtools.build.lib.rules.cpp.CcToolchainProvider;
+import com.google.devtools.build.lib.rules.cpp.CppActionNames;
 import com.google.devtools.build.lib.rules.cpp.CppCompileActionBuilder;
-import com.google.devtools.build.lib.rules.cpp.CppCompileActionContext;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration.HeadersCheckingMode;
-import com.google.devtools.build.lib.rules.cpp.CppHelper;
-import com.google.devtools.build.lib.rules.cpp.CppSemantics;
-import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
 
-/**
- * C++ compilation semantics.
- */
-public class BazelCppSemantics implements CppSemantics {
-  public static final CppSemantics INSTANCE = new BazelCppSemantics();
+/** C++ compilation semantics. */
+public class BazelCppSemantics implements AspectLegalCppSemantics {
+  @AutoCodec public static final BazelCppSemantics CPP = new BazelCppSemantics(Language.CPP);
+  @AutoCodec public static final BazelCppSemantics OBJC = new BazelCppSemantics(Language.OBJC);
 
-  private BazelCppSemantics() {
+  // TODO(#10338): We need to check for both providers. With and without the @rules_cc repo name.
+  //  The reason for that is that when we are in a target inside @rules_cc, the provider won't have
+  // the repo name set.
+  public static final Provider.Key CC_SHARED_INFO_PROVIDER_RULES_CC =
+      new StarlarkProvider.Key(
+          Label.parseAbsoluteUnchecked("@rules_cc//examples:experimental_cc_shared_library.bzl"),
+          "CcSharedLibraryInfo");
+
+  public static final Provider.Key CC_SHARED_INFO_PROVIDER =
+      new StarlarkProvider.Key(
+          Label.parseAbsoluteUnchecked("//examples:experimental_cc_shared_library.bzl"),
+          "CcSharedLibraryInfo");
+
+  private enum Language {
+    CPP,
+    OBJC
   }
 
-  @Override
-  public PathFragment getEffectiveSourcePath(Artifact source) {
-    return source.getRootRelativePath();
+  private final Language language;
+
+  private BazelCppSemantics(Language language) {
+    this.language = language;
   }
 
   @Override
   public void finalizeCompileActionBuilder(
-      RuleContext ruleContext, CppCompileActionBuilder actionBuilder) {
-    actionBuilder.setCppConfiguration(ruleContext.getFragment(CppConfiguration.class));
-    actionBuilder.setActionContext(CppCompileActionContext.class);
-    actionBuilder.addTransitiveMandatoryInputs(CppHelper.getToolchain(ruleContext).getCompile());
-  }
-
-  @Override
-  public void setupCompilationContext(RuleContext ruleContext, Builder contextBuilder) {
+      BuildConfiguration configuration,
+      FeatureConfiguration featureConfiguration,
+      CppCompileActionBuilder actionBuilder,
+      RuleErrorConsumer ruleErrorConsumer) {
+    CcToolchainProvider toolchain = actionBuilder.getToolchain();
+    if (language == Language.CPP) {
+      actionBuilder
+          .addTransitiveMandatoryInputs(
+              configuration.getFragment(CppConfiguration.class).useSpecificToolFiles()
+                      && !actionBuilder.getSourceFile().isTreeArtifact()
+                  ? (actionBuilder.getActionName().equals(CppActionNames.ASSEMBLE)
+                      ? toolchain.getAsFiles()
+                      : toolchain.getCompilerFiles())
+                  : toolchain.getAllFiles())
+          .setShouldScanIncludes(false);
+    } else {
+      actionBuilder
+          .addTransitiveMandatoryInputs(toolchain.getAllFilesMiddleman())
+          .setShouldScanIncludes(false);
+    }
   }
 
   @Override
   public HeadersCheckingMode determineHeadersCheckingMode(RuleContext ruleContext) {
     return HeadersCheckingMode.STRICT;
   }
+
+  @Override
+  public HeadersCheckingMode determineStarlarkHeadersCheckingMode(
+      RuleContext ruleContext, CppConfiguration cppConfig, CcToolchainProvider toolchain) {
+    if (cppConfig.strictHeaderCheckingFromStarlark()) {
+      return HeadersCheckingMode.STRICT;
+    }
+    return HeadersCheckingMode.LOOSE;
+  }
+
+  @Override
+  public boolean allowIncludeScanning() {
+    return false;
+  }
+
+  @Override
+  public boolean needsDotdInputPruning(BuildConfiguration configuration) {
+    if (language == Language.CPP) {
+      return true;
+    } else {
+      return configuration.getFragment(CppConfiguration.class).objcShouldGenerateDotdFiles();
+    }
+  }
+
+  @Override
+  public void validateAttributes(RuleContext ruleContext) {
+  }
+
+  @Override
+  public boolean needsIncludeValidation() {
+    return true;
+  }
+
+  @Override
+  public StructImpl getCcSharedLibraryInfo(TransitiveInfoCollection dep) {
+    StructImpl ccSharedLibraryInfo = (StructImpl) dep.get(CC_SHARED_INFO_PROVIDER);
+    if (ccSharedLibraryInfo != null) {
+      return ccSharedLibraryInfo;
+    }
+    ccSharedLibraryInfo = (StructImpl) dep.get(CC_SHARED_INFO_PROVIDER_RULES_CC);
+    if (ccSharedLibraryInfo != null) {
+      return ccSharedLibraryInfo;
+    }
+    return null;
+  }
+
+  @Override
+  public void validateLayeringCheckFeatures(
+      RuleContext ruleContext,
+      AspectDescriptor aspectDescriptor,
+      CcToolchainProvider ccToolchain,
+      ImmutableSet<String> unsupportedFeatures) {}
 }
