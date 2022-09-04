48fea2e238fd0e6e2af4c157013d15c16c8d75c1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:18:32 +0200

    Fix intermittent JaxRSTestCase failure

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/main/src/test/java/io/quarkus/it/main/JaxRSTestCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/main/src/test/java/io/quarkus/it/main/JaxRSTestCase.java
@@ -15,8 +15,6 @@ import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.it.rest.TestResource;
-import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.junit.DisabledOnNativeImage;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
@@ -30,13 +28,11 @@ public class JaxRSTestCase {
     }
 
     @Test
-    @TestHTTPEndpoint(TestResource.class)
     public void testInteger() {
-        RestAssured.when().get("/int/10").then().body(is("11"));
+        RestAssured.when().get("/test/int/10").then().body(is("11"));
     }
 
     @Test
-    @DisabledOnNativeImage //the native image tests will bind to 0.0.0.0 as the image is a production image, but the test datasource in the JVM will want to use localhost
     public void testConfigInjectionOfPort() {
         String host = ConfigProvider.getConfig().getOptionalValue("quarkus.http.host", String.class).orElse("0.0.0.0");
         RestAssured.when().get("/test/config/host").then().body(is(host));
