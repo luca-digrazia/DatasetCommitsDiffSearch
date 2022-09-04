b48bf0b03328277ec2d471724066831f7af41e7a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:51:38 +0200

    set default regularization parameter to 0 for 1-dimensional data

--- dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/stat/distribution/ExponentialFamilyMixture.java
+++ dataset/GitHub_Java/haifengl.smile/math/src/main/java/smile/stat/distribution/ExponentialFamilyMixture.java
@@ -31,7 +31,6 @@ import smile.math.Math;
  * @author Haifeng Li
  */
 public class ExponentialFamilyMixture extends Mixture {
-    private static final long serialVersionUID = 1L;
 
     /**
      * Constructor.
