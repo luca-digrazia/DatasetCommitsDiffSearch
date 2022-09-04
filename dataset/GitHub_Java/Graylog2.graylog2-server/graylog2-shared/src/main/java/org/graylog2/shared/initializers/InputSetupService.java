a54302ebef6eee55d01b8d8ac8b8a0f7eaa4d75f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:53:37 +0200

    Fix visibility race which sometimes prevented inputs to start.
        Convert previousLifecycle to an AtomicReference to ensure visibility
        across threads. Also use Lifecycle.UNINITIALIZED instead of null as
        initial state.
    
        With help from @kroepke.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/initializers/InputSetupService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-shared/src/main/java/org/graylog2/shared/initializers/InputSetupService.java
@@ -69,9 +69,6 @@ public class InputSetupService extends AbstractExecutionThreadService {
         // we can actually process them.
         if ((lifecycle == Lifecycle.RUNNING) && (previousLifecycle.get() == Lifecycle.STARTING || previousLifecycle.get() == Lifecycle.UNINITIALIZED)) {
             LOG.info("Triggering launching persisted inputs, node transitioned from {} to {}", previousLifecycle.get(), lifecycle);
-
-            // Set lifecycle BEFORE counting down the latch to avoid race conditions!
-            previousLifecycle.set(lifecycle);
             startLatch.countDown();
         }
 
@@ -79,6 +76,8 @@ public class InputSetupService extends AbstractExecutionThreadService {
         if (lifecycle == Lifecycle.FAILED) {
             startLatch.countDown();
         }
+
+        previousLifecycle.set(lifecycle);
     }
 
     @Override
@@ -90,8 +89,6 @@ public class InputSetupService extends AbstractExecutionThreadService {
         if (previousLifecycle.get() == Lifecycle.RUNNING) {
             LOG.debug("Launching persisted inputs now.");
             inputRegistry.launchAllPersisted();
-        } else {
-            LOG.error("Not starting any inputs because lifecycle is: {}", previousLifecycle.get());
         }
 
         // next, simply block until we are asked to shutdown, even though we are consuming a thread this way.
