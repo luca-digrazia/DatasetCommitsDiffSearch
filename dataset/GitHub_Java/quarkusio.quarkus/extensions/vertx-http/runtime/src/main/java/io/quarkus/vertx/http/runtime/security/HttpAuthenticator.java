700385d00195dae79ce2b641e9683e906bd7e8bd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:41:14 +0200

    Be defensive with closeTask in HttpAuthenticator#sendChallenge

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/HttpAuthenticator.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/HttpAuthenticator.java
@@ -1,14 +1,8 @@
 package io.quarkus.vertx.http.runtime.security;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
-import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Instance;
@@ -18,8 +12,7 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.quarkus.security.identity.IdentityProvider;
 import io.quarkus.security.identity.IdentityProviderManager;
 import io.quarkus.security.identity.SecurityIdentity;
-import io.quarkus.security.identity.request.AnonymousAuthenticationRequest;
-import io.quarkus.security.identity.request.AuthenticationRequest;
+import io.quarkus.security.identity.request.UsernamePasswordAuthenticationRequest;
 import io.vertx.ext.web.RoutingContext;
 
 /**
@@ -31,55 +24,33 @@ public class HttpAuthenticator {
     @Inject
     IdentityProviderManager identityProviderManager;
 
-    final HttpAuthenticationMechanism[] mechanisms;
+    final HTTPAuthenticationMechanism mechanism;
 
     public HttpAuthenticator() {
-        mechanisms = null;
+        mechanism = null;
     }
 
     @Inject
-    public HttpAuthenticator(Instance<HttpAuthenticationMechanism> instance,
-            Instance<IdentityProvider<?>> providers) {
-        List<HttpAuthenticationMechanism> mechanisms = new ArrayList<>();
-        for (HttpAuthenticationMechanism mechanism : instance) {
-            boolean notFound = false;
-            for (Class<? extends AuthenticationRequest> mechType : mechanism.getCredentialTypes()) {
-                boolean found = false;
-                for (IdentityProvider<?> i : providers) {
-                    if (i.getRequestType().equals(mechType)) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    notFound = true;
-                    break;
-                }
+    public HttpAuthenticator(Instance<HTTPAuthenticationMechanism> instance,
+            Instance<IdentityProvider<UsernamePasswordAuthenticationRequest>> usernamePassword) {
+        if (instance.isResolvable()) {
+            if (instance.isAmbiguous()) {
+                throw new IllegalStateException("Multiple HTTP authentication mechanisms are not implemented yet, discovered "
+                        + instance.stream().collect(Collectors.toList()));
             }
-            if (!notFound) {
-                mechanisms.add(mechanism);
-            }
-        }
-        if (mechanisms.isEmpty()) {
-            this.mechanisms = new HttpAuthenticationMechanism[] { new NoAuthenticationMechanism() };
+            mechanism = instance.get();
         } else {
-            this.mechanisms = mechanisms.toArray(new HttpAuthenticationMechanism[mechanisms.size()]);
-            //validate that we don't have multiple incompatible mechanisms
-            Map<HttpCredentialTransport, HttpAuthenticationMechanism> map = new HashMap<>();
-            for (HttpAuthenticationMechanism i : mechanisms) {
-                HttpCredentialTransport credentialTransport = i.getCredentialTransport();
-                if (credentialTransport == null) {
-                    continue;
-                }
-                HttpAuthenticationMechanism existing = map.get(credentialTransport);
-                if (existing != null) {
-                    throw new RuntimeException("Multiple mechanisms present that use the same credential transport "
-                            + credentialTransport + ". Mechanisms are " + i + " and " + existing);
-                }
-                map.put(credentialTransport, i);
+            if (!usernamePassword.isUnsatisfied()) {
+                //TODO: config
+                mechanism = new BasicAuthenticationMechanism("Quarkus");
+            } else {
+                mechanism = null;
             }
-
         }
+    }
+
+    public HttpAuthenticator(HTTPAuthenticationMechanism mechanism) {
+        this.mechanism = mechanism;
     }
 
     /**
@@ -92,22 +63,10 @@ public class HttpAuthenticator {
      * If no credentials are present it will resolve to null.
      */
     public CompletionStage<SecurityIdentity> attemptAuthentication(RoutingContext routingContext) {
-
-        CompletionStage<SecurityIdentity> result = mechanisms[0].authenticate(routingContext, identityProviderManager);
-        for (int i = 1; i < mechanisms.length; ++i) {
-            HttpAuthenticationMechanism mech = mechanisms[i];
-            result = result.thenCompose(new Function<SecurityIdentity, CompletionStage<SecurityIdentity>>() {
-                @Override
-                public CompletionStage<SecurityIdentity> apply(SecurityIdentity data) {
-                    if (data != null) {
-                        return CompletableFuture.completedFuture(data);
-                    }
-                    return mech.authenticate(routingContext, identityProviderManager);
-                }
-            });
+        if (mechanism == null) {
+            return CompletableFuture.completedFuture(null);
         }
-
-        return result;
+        return mechanism.authenticate(routingContext, identityProviderManager);
     }
 
     /**
@@ -119,63 +78,12 @@ public class HttpAuthenticator {
         if (closeTask == null) {
             closeTask = NoopCloseTask.INSTANCE;
         }
-        CompletionStage<Boolean> result = mechanisms[0].sendChallenge(routingContext);
-        for (int i = 1; i < mechanisms.length; ++i) {
-            HttpAuthenticationMechanism mech = mechanisms[i];
-            result = result.thenCompose(new Function<Boolean, CompletionStage<Boolean>>() {
-                @Override
-                public CompletionStage<Boolean> apply(Boolean aBoolean) {
-                    if (aBoolean) {
-                        return CompletableFuture.completedFuture(true);
-                    }
-                    return mech.sendChallenge(routingContext);
-                }
-            });
-        }
-        return result.thenRun(closeTask);
-    }
-
-    public CompletionStage<ChallengeData> getChallenge(RoutingContext routingContext) {
-        CompletionStage<ChallengeData> result = mechanisms[0].getChallenge(routingContext);
-        for (int i = 1; i < mechanisms.length; ++i) {
-            HttpAuthenticationMechanism mech = mechanisms[i];
-            result = result.thenCompose(new Function<ChallengeData, CompletionStage<ChallengeData>>() {
-                @Override
-                public CompletionStage<ChallengeData> apply(ChallengeData data) {
-                    if (data != null) {
-                        return CompletableFuture.completedFuture(data);
-                    }
-                    return mech.getChallenge(routingContext);
-                }
-            });
-        }
-        return result;
-    }
-
-    static class NoAuthenticationMechanism implements HttpAuthenticationMechanism {
-
-        @Override
-        public CompletionStage<SecurityIdentity> authenticate(RoutingContext context,
-                IdentityProviderManager identityProviderManager) {
+        if (mechanism == null) {
+            routingContext.response().setStatusCode(HttpResponseStatus.FORBIDDEN.code());
+            closeTask.run();
             return CompletableFuture.completedFuture(null);
         }
-
-        @Override
-        public CompletionStage<ChallengeData> getChallenge(RoutingContext context) {
-            ChallengeData challengeData = new ChallengeData(HttpResponseStatus.FORBIDDEN.code(), null, null);
-            return CompletableFuture.completedFuture(challengeData);
-        }
-
-        @Override
-        public Set<Class<? extends AuthenticationRequest>> getCredentialTypes() {
-            return Collections.singleton(AnonymousAuthenticationRequest.class);
-        }
-
-        @Override
-        public HttpCredentialTransport getCredentialTransport() {
-            return null;
-        }
-
+        return mechanism.sendChallenge(routingContext).thenRun(closeTask);
     }
 
     static class NoopCloseTask implements Runnable {
