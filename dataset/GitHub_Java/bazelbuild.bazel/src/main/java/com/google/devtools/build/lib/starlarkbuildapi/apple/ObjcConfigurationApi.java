2c317d01ceec55860fc66c0cba4336a9e0ba86e3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:13:48 +0200

    Expose should_strip_binary to Starlark
        This is needed so we can write some objc feature configuration logic
        in Starlark.  I know we need to clean up the notion of binary
        stripping for Apple, but whatever we have in ObjcConfig should be
        exposed to Starlark.
    
        PiperOrigin-RevId: 370060234

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/starlarkbuildapi/apple/ObjcConfigurationApi.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/starlarkbuildapi/apple/ObjcConfigurationApi.java
@@ -75,6 +75,12 @@ public interface ObjcConfigurationApi<ApplePlatformTypeApiT extends ApplePlatfor
   DottedVersionApi<?> getSimulatorVersionForPlatformType(ApplePlatformTypeApiT platformType);
 
   @StarlarkMethod(
+      name = "generate_dsym",
+      doc = "Whether to generate debug symbol(.dSYM) artifacts.",
+      structField = true)
+  boolean generateDsym();
+
+  @StarlarkMethod(
       name = "generate_linkmap",
       doc = "Whether to generate linkmap artifacts.",
       structField = true)
