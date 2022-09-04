6dc34985e509ff318fb19f7b29542365b3ca6c52
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:08:29 +0200

    Fix metrics name of SyslogTcpTransport

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-inputs/src/main/java/org/graylog2/inputs/transports/SyslogTcpTransport.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-inputs/src/main/java/org/graylog2/inputs/transports/SyslogTcpTransport.java
@@ -1,27 +1,30 @@
 /**
- * This file is part of Graylog2.
+ * This file is part of Graylog.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.inputs.transports;
 
+import com.codahale.metrics.InstrumentedExecutorService;
+import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
 import org.graylog2.inputs.syslog.tcp.SyslogTCPFramingRouterHandler;
-import org.graylog2.plugin.ConfigClass;
-import org.graylog2.plugin.FactoryClass;
+import org.graylog2.plugin.inputs.annotations.ConfigClass;
+import org.graylog2.plugin.inputs.annotations.FactoryClass;
 import org.graylog2.plugin.LocalMetricRegistry;
 import org.graylog2.plugin.configuration.Configuration;
 import org.graylog2.plugin.inputs.MessageInput;
@@ -31,21 +34,37 @@ import org.graylog2.plugin.inputs.util.ThroughputCounter;
 import org.jboss.netty.channel.ChannelHandler;
 
 import javax.inject.Named;
-import javax.inject.Provider;
 import java.util.LinkedHashMap;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+import static com.codahale.metrics.MetricRegistry.name;
 
 public class SyslogTcpTransport extends TcpTransport {
     @AssistedInject
     public SyslogTcpTransport(@Assisted Configuration configuration,
                               @Named("bossPool") Executor bossPool,
-                              @Named("cached") Provider<Executor> workerPoolProvider,
                               ThroughputCounter throughputCounter,
                               ConnectionCounter connectionCounter,
                               LocalMetricRegistry localRegistry) {
-        super(configuration, bossPool, workerPoolProvider, throughputCounter, connectionCounter, localRegistry);
+        super(configuration,
+                bossPool,
+                executorService("worker", "syslog-tcp-transport-worker-%d", localRegistry),
+                throughputCounter,
+                connectionCounter,
+                localRegistry);
     }
+
+    private static Executor executorService(final String executorName, final String threadNameFormat, final MetricRegistry metricRegistry) {
+        final ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat(threadNameFormat).build();
+        return new InstrumentedExecutorService(
+                Executors.newCachedThreadPool(threadFactory),
+                metricRegistry,
+                name(SyslogTcpTransport.class, executorName, "executor-service"));
+    }
+
 
     @Override
     protected LinkedHashMap<String, Callable<? extends ChannelHandler>> getFinalChannelHandlers(MessageInput input) {
