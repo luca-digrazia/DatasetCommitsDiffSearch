f2ee69417e16bd019f654512557eaf46e28e3600
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:38:31 +0200

    Adding Optional @FormParam injectable. Copy of the functionality already provided by for @QueryParams

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalReturnResource.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalReturnResource.java
@@ -1,13 +1,8 @@
 package io.dropwizard.jersey.guava;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.FormParam;
 import com.google.common.base.Optional;
 
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/optional-return/")
@@ -19,7 +14,8 @@ public class OptionalReturnResource {
     }
 
     @POST
-    public Optional<String> showWithFormParam(@FormParam("id") String id) {
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Optional<String> showWithFormParam(@QueryParam("id") String id) {
         return Optional.fromNullable(id);
     }
 }
