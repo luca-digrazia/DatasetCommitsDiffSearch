7fade97d5e1d80443c65ba6011191aa204dc65b3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:19:08 +0200

    Use Optional for empty list in mongodb-client

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/mongodb-client/runtime/src/main/java/io/quarkus/mongodb/runtime/MongoClientConfig.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/mongodb-client/runtime/src/main/java/io/quarkus/mongodb/runtime/MongoClientConfig.java
@@ -63,11 +63,11 @@ public class MongoClientConfig {
     public Optional<String> connectionString;
 
     /**
-     * Configures the MongoDB server addressed (one if single mode).
-     * The addresses are passed as {@code host:port}.
+     * Configures the Mongo server addressed (one if single mode).
+     * The addressed are passed as {@code host:port}.
      */
-    @ConfigItem(defaultValue = "127.0.0.1:27017")
-    public List<String> hosts;
+    @ConfigItem
+    public Optional<List<String>> hosts;
 
     /**
      * Configure the database name.
