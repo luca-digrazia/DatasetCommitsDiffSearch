de5f93ea729a5464698500dd1a89af7a876e573f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:54:12 +0200

    Update comments
        Comments are misleading, as discussed on https://github.com/bazelbuild/bazel/pull/5305#issuecomment-396288826
    
        RELNOTES: None.
        PiperOrigin-RevId: 205841782

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/Identifier.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/Identifier.java
@@ -14,7 +14,6 @@
 
 package com.google.devtools.build.lib.syntax;
 
-import com.google.common.base.Preconditions;
 import com.google.devtools.build.lib.util.SpellChecker;
 import java.io.IOException;
 import java.util.Set;
@@ -33,9 +32,6 @@ import javax.annotation.Nullable;
 public final class Identifier extends Expression {
 
   private final String name;
-  // The scope of the variable. The value is set when the AST has been analysed by
-  // ValidationEnvironment.
-  @Nullable private ValidationEnvironment.Scope scope;
 
   public Identifier(String name) {
     this.name = name;
@@ -59,7 +55,6 @@ public final class Identifier extends Expression {
 
   @Override
   public boolean equals(@Nullable Object object) {
-    // TODO(laurentlb): Remove this. AST nodes should probably not be comparable.
     if (object instanceof Identifier) {
       Identifier that = (Identifier) object;
       return this.name.equals(that.name);
@@ -69,19 +64,12 @@ public final class Identifier extends Expression {
 
   @Override
   public int hashCode() {
-    // TODO(laurentlb): Remove this.
     return name.hashCode();
-  }
-
-  void setScope(ValidationEnvironment.Scope scope) {
-    Preconditions.checkState(this.scope == null);
-    this.scope = scope;
   }
 
   @Override
   Object doEval(Environment env) throws EvalException {
-    Object value =
-        scope == ValidationEnvironment.Scope.Local ? env.localLookup(name) : env.lookup(name);
+    Object value = env.lookup(name);
     if (value == null) {
       throw createInvalidIdentifierException(env.getVariableNames());
     }
