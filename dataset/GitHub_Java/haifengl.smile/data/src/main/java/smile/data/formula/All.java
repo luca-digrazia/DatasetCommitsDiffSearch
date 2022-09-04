6338265d4f92faa41f408c05bb25e4cbe5d31fde
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:33:08 +0200

    add one-hot encoding

--- dataset/GitHub_Java/haifengl.smile/data/src/main/java/smile/data/formula/All.java
+++ dataset/GitHub_Java/haifengl.smile/data/src/main/java/smile/data/formula/All.java
@@ -1,20 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2010-2019 Haifeng Li
+ * Copyright (c) 2010 Haifeng Li
  *
- * Smile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Smile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
-
 package smile.data.formula;
 
 import java.util.Arrays;
@@ -29,6 +27,8 @@ import smile.data.type.StructType;
  * @author Haifeng Li
  */
 class All implements HyperTerm {
+    /** If true, only columns not in the formula. Otherwise, keep all the original columns. */
+    private boolean rest;
     /** All columns in the schema. */
     private List<Variable> columns;
 
@@ -36,7 +36,23 @@ class All implements HyperTerm {
      * Constructor. All columns not otherwise in the formula.
      */
     public All() {
+        this(true);
+    }
 
+    /**
+     * Constructor.
+     * @param rest If true, only columns not in the formula.
+     *             Otherwise, keep all the original columns.
+     */
+    public All(boolean rest) {
+        this.rest = rest;
+    }
+
+    /**
+     * Return true if only columns not in the formula.
+     */
+    public boolean rest() {
+        return rest;
     }
 
     @Override
