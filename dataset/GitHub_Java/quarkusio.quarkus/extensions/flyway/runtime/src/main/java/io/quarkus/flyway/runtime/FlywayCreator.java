2c5ebcbd26441c5042b3f7193a2fe964d622bd40
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:20:52 +0200

    Remove lambdas from Flyway runtime code

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/flyway/runtime/src/main/java/io/quarkus/flyway/runtime/FlywayCreator.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/flyway/runtime/src/main/java/io/quarkus/flyway/runtime/FlywayCreator.java
@@ -1,7 +1,5 @@
 package io.quarkus.flyway.runtime;
 
-import java.util.Arrays;
-
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
@@ -49,19 +47,6 @@ class FlywayCreator {
             configure.baselineDescription(flywayRuntimeConfig.baselineDescription.get());
         }
         configure.placeholders(flywayRuntimeConfig.placeholders);
-        configure.createSchemas(flywayRuntimeConfig.createSchemas);
-
-        /*
-         * Ensure that no classpath scanning takes place by setting the ClassProvider and the ResourceProvider
-         * (see Flyway#createResourceAndClassProviders)
-         */
-
-        // the static fields of this class have already been set at static-init
-        QuarkusPathLocationScanner quarkusPathLocationScanner = new QuarkusPathLocationScanner(
-                Arrays.asList(configure.getLocations()));
-        configure.javaMigrationClassProvider(new QuarkusFlywayClassProvider<>(quarkusPathLocationScanner.scanForClasses()));
-        configure.resourceProvider(new QuarkusFlywayResourceProvider(quarkusPathLocationScanner.scanForResources()));
-
         return configure.load();
     }
 }
