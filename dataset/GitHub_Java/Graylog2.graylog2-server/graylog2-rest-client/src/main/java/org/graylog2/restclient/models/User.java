1ac1b3a8b651e722fb393124b8aa70b604a3dbac
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:40:59 +0200

    replace null checks with MoreObjects.firstNonNull

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/User.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/User.java
@@ -38,7 +38,6 @@ import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -200,7 +199,7 @@ public class User {
             cur.startpage.type = null;
             cur.startpage.id = null;
         } else {
-            cur.startpage.type = startpage.getType().toString().toLowerCase(Locale.ENGLISH);
+            cur.startpage.type = startpage.getType().toString().toLowerCase();
             cur.startpage.id = startpage.getId();
         }
         return update(cur);
