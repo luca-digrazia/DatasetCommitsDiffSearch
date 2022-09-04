89556074af142daad071759f6b5f7499ab6dc0a3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:50:10 +0200

    Make sure ResourceTest doesn't dribble on.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/com/yammer/dropwizard/testing/ResourceTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/com/yammer/dropwizard/testing/ResourceTest.java
@@ -1,55 +1,101 @@
 package com.yammer.dropwizard.testing;
 
-import com.google.common.collect.Lists;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.test.framework.AppDescriptor;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.LowLevelAppDescriptor;
-import com.yammer.dropwizard.bundles.JavaBundle;
-import com.yammer.dropwizard.config.DropwizardResourceConfig;
+import com.yammer.dropwizard.jersey.DropwizardResourceConfig;
 import com.yammer.dropwizard.jersey.JacksonMessageBodyProvider;
-import org.codehaus.jackson.map.Module;
+import com.yammer.dropwizard.json.ObjectMapperFactory;
+import com.yammer.dropwizard.validation.Validator;
 import org.junit.After;
 import org.junit.Before;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * A base test class for testing Dropwizard resources.
  */
 public abstract class ResourceTest {
+    static {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+    }
+
     private final Set<Object> singletons = Sets.newHashSet();
-    private final List<Module> modules = Lists.newArrayList();
+    private final Set<Class<?>> providers = Sets.newHashSet();
+    private final ObjectMapperFactory objectMapperFactory = new ObjectMapperFactory();
+    private final Map<String, Boolean> features = Maps.newHashMap();
+    private final Map<String, Object> properties = Maps.newHashMap();
 
     private JerseyTest test;
+    private Validator validator = new Validator();
 
     protected abstract void setUpResources() throws Exception;
+
+    public Validator getValidator() {
+        return validator;
+    }
+
+    public void setValidator(Validator validator) {
+        this.validator = validator;
+    }
 
     protected void addResource(Object resource) {
         singletons.add(resource);
     }
 
-    protected void addJacksonModule(Module module) {
-        modules.add(module);
+    public void addProvider(Class<?> klass) {
+        providers.add(klass);
     }
 
+    public void addProvider(Object provider) {
+        singletons.add(provider);
+    }
+
+    protected ObjectMapperFactory getObjectMapperFactory() {
+        return objectMapperFactory;
+    }
+
+    protected void addFeature(String feature, Boolean value) {
+        features.put(feature, value);
+    }
+
+    protected void addProperty(String property, Object value) {
+        properties.put(property, value);
+    }
+    
     protected Client client() {
         return test.client();
     }
 
+    protected JerseyTest getJerseyTest() {
+        return test;
+    }
+
     @Before
-    public void setUpJersey() throws Exception {
+    public final void setUpJersey() throws Exception {
         setUpResources();
         this.test = new JerseyTest() {
             @Override
             protected AppDescriptor configure() {
-                final DropwizardResourceConfig config = new DropwizardResourceConfig();
-                for (Object provider : JavaBundle.DEFAULT_PROVIDERS) { // sorry, Scala folks
-                    config.getSingletons().add(provider);
+                final DropwizardResourceConfig config = new DropwizardResourceConfig(true);
+                for (Class<?> provider : providers) {
+                    config.getClasses().add(provider);
                 }
-                config.getSingletons().add(new JacksonMessageBodyProvider(modules));
+                for (Map.Entry<String, Boolean> feature : features.entrySet()) {
+                    config.getFeatures().put(feature.getKey(), feature.getValue());
+                }
+                for (Map.Entry<String, Object> property : properties.entrySet()) {
+                    config.getProperties().put(property.getKey(), property.getValue());
+                }
+                final ObjectMapper mapper = getObjectMapperFactory().build();
+                config.getSingletons().add(new JacksonMessageBodyProvider(mapper, validator));
                 config.getSingletons().addAll(singletons);
                 return new LowLevelAppDescriptor.Builder(config).build();
             }
@@ -58,7 +104,9 @@ public abstract class ResourceTest {
     }
 
     @After
-    public void tearDownJersey() throws Exception {
-        test.tearDown();
+    public final void tearDownJersey() throws Exception {
+        if (test != null) {
+            test.tearDown();
+        }
     }
 }
