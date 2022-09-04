ffae4793ff91ff80543d6219f11aa85c2ba40e4a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:21:56 +0200

    Providing credentials for ES7 connections requiring authentication. (#8608)
        * Providing credentials for ES7 connections requiring authentication.
    
        This change is adding a credentials provider for the rest client for ES7
        connections that contains the authentication details for configured
        connections. This allows the ES7 storage driver module to connect to
        authenticated ES nodes.
    
        * Setting usage of expect continue header to config setting.
    
        * Enable authentication explicitly.
    
        * Adjusting constructor.
    
        * Registering credentials for any scheme instead of protocol scheme.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/RestHighLevelClientProvider.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/RestHighLevelClientProvider.java
@@ -6,9 +6,6 @@ import org.graylog.shaded.elasticsearch7.org.apache.http.client.CredentialsProvi
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.client.RestClient;
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.client.RestClientBuilder;
 import org.graylog.shaded.elasticsearch7.org.elasticsearch.client.RestHighLevelClient;
-import org.graylog.shaded.elasticsearch7.org.elasticsearch.client.sniff.ElasticsearchNodesSniffer;
-import org.graylog.shaded.elasticsearch7.org.elasticsearch.client.sniff.NodesSniffer;
-import org.graylog.shaded.elasticsearch7.org.elasticsearch.client.sniff.Sniffer;
 import org.graylog2.system.shutdown.GracefulShutdownService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,8 +18,6 @@ import javax.inject.Singleton;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
 @Singleton
 public class RestHighLevelClientProvider implements Provider<RestHighLevelClient> {
@@ -30,7 +25,6 @@ public class RestHighLevelClientProvider implements Provider<RestHighLevelClient
     private static final Logger LOG = LoggerFactory.getLogger(RestHighLevelClientProvider.class);
 
     private final RestHighLevelClient client;
-    private final Sniffer sniffer;
 
     @SuppressWarnings("unused")
     @Inject
@@ -47,6 +41,7 @@ public class RestHighLevelClientProvider implements Provider<RestHighLevelClient
             @Named("elasticsearch_discovery_filter") @Nullable String discoveryFilter,
             @Named("elasticsearch_discovery_frequency") Duration discoveryFrequency,
             @Named("elasticsearch_discovery_default_scheme") String defaultSchemeForDiscoveredNodes,
+            @Named("elasticsearch_compression_enabled") boolean compressionEnabled,
             @Named("elasticsearch_use_expect_continue") boolean useExpectContinue,
             CredentialsProvider credentialsProvider) {
         client = buildClient(
@@ -58,36 +53,7 @@ public class RestHighLevelClientProvider implements Provider<RestHighLevelClient
                 useExpectContinue,
                 credentialsProvider);
 
-        sniffer = discoveryEnabled
-                ? createNodeDiscoverySniffer(client.getLowLevelClient(), discoveryFrequency, defaultSchemeForDiscoveredNodes, discoveryFilter)
-                : null;
-
         registerShutdownHook(shutdownService);
-
-        if (discoveryEnabled) {
-            registerSnifferShutdownHook(shutdownService);
-        }
-    }
-
-    private Sniffer createNodeDiscoverySniffer(RestClient restClient, Duration discoveryFrequency, String defaultSchemeForDiscoveredNodes, String discoveryFilter) {
-        final NodesSniffer nodesSniffer = FilteredElasticsearchNodesSniffer.create(
-                restClient,
-                TimeUnit.SECONDS.toMillis(5),
-                mapDefaultScheme(defaultSchemeForDiscoveredNodes),
-                discoveryFilter
-        );
-        return Sniffer.builder(restClient)
-                .setSniffIntervalMillis(Math.toIntExact(discoveryFrequency.toMilliseconds()))
-                .setNodesSniffer(nodesSniffer)
-                .build();
-    }
-
-    private ElasticsearchNodesSniffer.Scheme mapDefaultScheme(String defaultSchemeForDiscoveredNodes) {
-        switch (defaultSchemeForDiscoveredNodes.toUpperCase(Locale.ENGLISH)) {
-            case "HTTP": return ElasticsearchNodesSniffer.Scheme.HTTP;
-            case "HTTPS": return ElasticsearchNodesSniffer.Scheme.HTTPS;
-            default: throw new IllegalArgumentException("Invalid default scheme for discovered ES nodes: " + defaultSchemeForDiscoveredNodes);
-        }
     }
 
     @Override
@@ -129,9 +95,5 @@ public class RestHighLevelClientProvider implements Provider<RestHighLevelClient
                 LOG.warn("Failed to close Elasticsearch RestHighLevelClient", e);
             }
         });
-    }
-
-    private void registerSnifferShutdownHook(GracefulShutdownService shutdownService) {
-        shutdownService.register(sniffer::close);
     }
 }
