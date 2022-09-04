a78d7043bd721e8ccd0e775787495afd6754ae8d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:09:51 +0200

    Avoid doing any xstream related work unless absolutely necessary
        Relates to: https://github.com/quarkusio/quarkus/pull/10081

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit5/src/main/java/io/quarkus/test/junit/internal/XStreamDeepClone.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit5/src/main/java/io/quarkus/test/junit/internal/XStreamDeepClone.java
@@ -7,11 +7,11 @@ import com.thoughtworks.xstream.XStream;
 /**
  * Super simple cloning strategy that just serializes to XML and deserializes it using xstream
  */
-class XStreamDeepClone implements DeepClone {
+public class XStreamDeepClone implements DeepClone {
 
     private final Supplier<XStream> xStreamSupplier;
 
-    XStreamDeepClone(ClassLoader classLoader) {
+    public XStreamDeepClone(ClassLoader classLoader) {
         // avoid doing any work eagerly since the cloner is rarely used
         xStreamSupplier = () -> {
             XStream result = new XStream();
@@ -22,7 +22,6 @@ class XStreamDeepClone implements DeepClone {
         };
     }
 
-    @Override
     public Object clone(Object objectToClone) {
         if (objectToClone == null) {
             return null;
