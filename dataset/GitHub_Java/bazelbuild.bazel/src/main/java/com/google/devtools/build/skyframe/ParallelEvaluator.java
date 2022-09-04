5a42ef5f2edf2b43ac4c6cbf10bda492be664986
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:11:17 +0200

    Avoid waste in registerNewlyDiscovered... when there aren't any
        RELNOTES: None.
        PiperOrigin-RevId: 156553687

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/skyframe/ParallelEvaluator.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/skyframe/ParallelEvaluator.java
@@ -23,7 +23,6 @@ import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadCompatible;
 import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.events.ExtendedEventHandler;
-import com.google.devtools.build.lib.events.ExtendedEventHandler.Postable;
 import com.google.devtools.build.lib.profiler.Profiler;
 import com.google.devtools.build.lib.profiler.ProfilerTask;
 import com.google.devtools.build.lib.util.BlazeClock;
@@ -105,7 +104,6 @@ public final class ParallelEvaluator implements Evaluator {
       final ExtendedEventHandler reporter,
       EmittedEventState emittedEventState,
       EventFilter storedEventFilter,
-      ErrorInfoManager errorInfoManager,
       boolean keepGoing,
       int threadCount,
       DirtyTrackingProgressReceiver progressReceiver) {
@@ -118,9 +116,9 @@ public final class ParallelEvaluator implements Evaluator {
             reporter,
             emittedEventState,
             keepGoing,
+            /*storeErrorsAlongsideValues=*/ true,
             progressReceiver,
             storedEventFilter,
-            errorInfoManager,
             createEvaluateRunnable(),
             threadCount);
     cycleDetector = new SimpleCycleDetector();
@@ -133,13 +131,14 @@ public final class ParallelEvaluator implements Evaluator {
       final ExtendedEventHandler reporter,
       EmittedEventState emittedEventState,
       EventFilter storedEventFilter,
-      ErrorInfoManager errorInfoManager,
       boolean keepGoing,
+      boolean storeErrorsAlongsideValues,
       DirtyTrackingProgressReceiver progressReceiver,
       ForkJoinPool forkJoinPool,
       CycleDetector cycleDetector) {
     this.graph = graph;
     this.cycleDetector = cycleDetector;
+    Preconditions.checkState(storeErrorsAlongsideValues || keepGoing);
     evaluatorContext =
         new ParallelEvaluatorContext(
             graph,
@@ -148,9 +147,9 @@ public final class ParallelEvaluator implements Evaluator {
             reporter,
             emittedEventState,
             keepGoing,
+            storeErrorsAlongsideValues,
             progressReceiver,
             storedEventFilter,
-            errorInfoManager,
             createEvaluateRunnable(),
             Preconditions.checkNotNull(forkJoinPool));
   }
@@ -449,16 +448,15 @@ public final class ParallelEvaluator implements Evaluator {
               }
               ErrorInfo depError = depEntry.getErrorInfo();
               if (depError != null) {
-                isTransitivelyTransient |= depError.isTransitivelyTransient();
+                isTransitivelyTransient |= depError.isTransient();
               }
             }
-            ErrorInfo errorInfo = evaluatorContext.getErrorInfoManager().fromException(
-                skyKey,
-                reifiedBuilderException,
-                isTransitivelyTransient);
+            ErrorInfo errorInfo =
+                ErrorInfo.fromException(reifiedBuilderException, isTransitivelyTransient);
             registerNewlyDiscoveredDepsForDoneEntry(
                 skyKey, state, newlyRequestedDeps, oldDeps, env);
-            env.setError(state, errorInfo);
+            env.setError(
+                state, errorInfo, /*isDirectlyTransient=*/ reifiedBuilderException.isTransient());
             env.commit(
                 state,
                 evaluatorContext.keepGoing()
@@ -1019,12 +1017,9 @@ public final class ParallelEvaluator implements Evaluator {
         if (reifiedBuilderException.getRootCauseSkyKey().equals(parent)) {
           error = ErrorInfo.fromException(reifiedBuilderException,
               /*isTransitivelyTransient=*/ false);
-          bubbleErrorInfo.put(
-              errorKey,
-              ValueWithMetadata.error(
-                  ErrorInfo.fromChildErrors(errorKey, ImmutableSet.of(error)),
-                  env.buildEvents(parentEntry, /*missingChildren=*/ true),
-                  env.buildPosts(parentEntry)));
+          bubbleErrorInfo.put(errorKey,
+              ValueWithMetadata.error(ErrorInfo.fromChildErrors(errorKey, ImmutableSet.of(error)),
+                  env.buildEvents(parentEntry, /*missingChildren=*/true)));
           continue;
         }
       } finally {
@@ -1032,12 +1027,9 @@ public final class ParallelEvaluator implements Evaluator {
         Thread.interrupted();
       }
       // Builder didn't throw an exception, so just propagate this one up.
-      bubbleErrorInfo.put(
-          errorKey,
-          ValueWithMetadata.error(
-              ErrorInfo.fromChildErrors(errorKey, ImmutableSet.of(error)),
-              env.buildEvents(parentEntry, /*missingChildren=*/ true),
-              env.buildPosts(parentEntry)));
+      bubbleErrorInfo.put(errorKey,
+          ValueWithMetadata.error(ErrorInfo.fromChildErrors(errorKey, ImmutableSet.of(error)),
+              env.buildEvents(parentEntry, /*missingChildren=*/true)));
     }
 
     // Reset the interrupt bit if there was an interrupt from outside this evaluator interrupt.
@@ -1088,9 +1080,6 @@ public final class ParallelEvaluator implements Evaluator {
         continue;
       }
       SkyValue value = valueWithMetadata.getValue();
-      for (Postable post : valueWithMetadata.getTransitivePostables()) {
-        evaluatorContext.getReporter().post(post);
-      }
       // TODO(bazel-team): Verify that message replay is fast and works in failure
       // modes [skyframe-core]
       // Note that replaying events here is only necessary on null builds, because otherwise we
