49fbd02839c492963158e16aa6a3b7c3efba89a9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:30:42 +0200

    Support for a custom property prefix
        It's crucial if we don't want situations, when independent
        tests concurrently modify the same system property.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/io/dropwizard/testing/ConfigOverride.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/io/dropwizard/testing/ConfigOverride.java
@@ -1,38 +1,27 @@
 package io.dropwizard.testing;
 
-import java.util.function.Supplier;
-
 public class ConfigOverride {
 
-    public static final String DEFAULT_PREFIX = "dw.";
     private final String key;
-    private final Supplier<String> value;
+    private final String value;
     private final String propertyPrefix;
 
-    private ConfigOverride(String propertyPrefix, String key, Supplier<String> value) {
+    private ConfigOverride(String propertyPrefix, String key, String value) {
         this.key = key;
         this.value = value;
         this.propertyPrefix = propertyPrefix.endsWith(".") ? propertyPrefix : propertyPrefix + ".";
     }
 
     public static ConfigOverride config(String key, String value) {
-        return new ConfigOverride(DEFAULT_PREFIX, key, () -> value);
+        return new ConfigOverride("dw.", key, value);
     }
 
     public static ConfigOverride config(String propertyPrefix, String key, String value) {
-        return new ConfigOverride(propertyPrefix, key, () -> value);
-    }
-
-    public static ConfigOverride config(String key, Supplier<String> value) {
-        return new ConfigOverride(DEFAULT_PREFIX, key, value);
-    }
-
-    public static ConfigOverride config(String propertyPrefix, String key, Supplier<String> value) {
         return new ConfigOverride(propertyPrefix, key, value);
     }
 
     public void addToSystemProperties() {
-        System.setProperty(propertyPrefix + key, value.get());
+        System.setProperty(propertyPrefix + key, value);
     }
 
     public void removeFromSystemProperties() {
