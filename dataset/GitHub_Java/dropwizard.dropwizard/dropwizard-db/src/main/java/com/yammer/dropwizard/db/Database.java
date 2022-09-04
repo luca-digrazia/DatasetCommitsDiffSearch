9fac0ddca0e2f1c9178ac1c435dfea75dff777f5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:54:59 +0200

    Decouple connection pool creation from Database.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-db/src/main/java/com/yammer/dropwizard/db/Database.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-db/src/main/java/com/yammer/dropwizard/db/Database.java
@@ -1,26 +1,55 @@
 package com.yammer.dropwizard.db;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import com.yammer.dropwizard.db.args.OptionalArgumentFactory;
+import com.yammer.dropwizard.db.logging.LogbackLog;
 import com.yammer.dropwizard.lifecycle.Managed;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.tomcat.dbcp.pool.ObjectPool;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.MetricName;
+import com.yammer.metrics.jdbi.InstrumentedTimingCollector;
+import com.yammer.metrics.jdbi.strategies.DelegatingStatementNameStrategy;
+import com.yammer.metrics.jdbi.strategies.NameStrategies;
+import com.yammer.metrics.jdbi.strategies.StatementNameStrategy;
+import org.skife.jdbi.v2.ColonPrefixNamedParamStatementRewriter;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
-import org.skife.jdbi.v2.logging.Log4JLog;
-import org.skife.jdbi.v2.util.IntegerMapper;
+import org.skife.jdbi.v2.StatementContext;
+import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class Database extends DBI implements Managed {
-    private static final Logger LOGGER = Logger.getLogger(Database.class);
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(Database.class);
+    private static final MetricName RAW_SQL = new MetricName(Database.class, "raw-sql");
+    private static class SanerNamingStrategy extends DelegatingStatementNameStrategy {
+        private SanerNamingStrategy() {
+            super(NameStrategies.CHECK_EMPTY,
+                  NameStrategies.CONTEXT_CLASS,
+                  NameStrategies.CONTEXT_NAME,
+                  NameStrategies.SQL_OBJECT,
+                  new StatementNameStrategy() {
+                      @Override
+                      public MetricName getStatementName(StatementContext statementContext) {
+                          return RAW_SQL;
+                      }
+                  });
+        }
+    }
 
-    private final ObjectPool pool;
+    private final ClosableDataSource dataSource;
+    private final String validationQuery;
 
-    public Database(DataSource dataSource, ObjectPool pool) {
+    public Database(ClosableDataSource dataSource, String validationQuery) {
         super(dataSource);
-        this.pool = pool;
-        setSQLLog(new Log4JLog(LOGGER, Level.TRACE));
+        this.dataSource = dataSource;
+        this.validationQuery = validationQuery;
+        setSQLLog(new LogbackLog(LOGGER, Level.TRACE));
+        setTimingCollector(new InstrumentedTimingCollector(Metrics.defaultRegistry(),
+                                                           new SanerNamingStrategy()));
+        setStatementRewriter(new NamePrependingStatementRewriter(new ColonPrefixNamedParamStatementRewriter()));
+        registerArgumentFactory(new OptionalArgumentFactory());
+        registerContainerFactory(new ImmutableListContainerFactory());
     }
 
     @Override
@@ -30,16 +59,13 @@ public class Database extends DBI implements Managed {
 
     @Override
     public void stop() throws Exception {
-        pool.close();
+        dataSource.close();
     }
 
     public void ping() throws SQLException {
         final Handle handle = open();
         try {
-            final Integer first = handle.createQuery("SELECT 1").map(IntegerMapper.FIRST).first();
-            if (!Integer.valueOf(1).equals(first)) {
-                throw new SQLException("Expected 1 from 'SELECT 1', got " + first);
-            }
+            handle.execute(validationQuery);
         } finally {
             handle.close();
         }
