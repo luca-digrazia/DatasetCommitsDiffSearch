4a79f95f53b6011fe0ed710053ee7462bbd65a69
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:51:41 +0200

    Choose quick values widget size dynamically
        Use different sizes for the quick values widget depending on the number
        of data visualisation the user selected.
    
        Fixes Graylog2/graylog2-web-interface#1422

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/dashboards/widgets/QuickvaluesWidget.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/dashboards/widgets/QuickvaluesWidget.java
@@ -1,50 +1,49 @@
 /**
- * Copyright 2014 Lennart Koopmann <lennart@torch.sh>
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.restclient.models.dashboards.widgets;
 
 import com.google.common.collect.Maps;
 import org.graylog2.restclient.lib.timeranges.TimeRange;
 import org.graylog2.restclient.models.dashboards.Dashboard;
-import play.mvc.Call;
 
 import java.util.Map;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
 public class QuickvaluesWidget extends DashboardWidget {
 
-    private static final int WIDTH = 1;
-    private static final int HEIGHT = 2;
+    private static final int DEFAULT_WIDTH = 1;
+    private static final int DEFAULT_HEIGHT_SINGLE_REPRESENTATION = 2;
+    private static final int DEFAULT_HEIGHT_MULTIPLE_REPRESENTATIONS = 3;
 
     private final String field;
     private final String streamId;
 
-    public QuickvaluesWidget(Dashboard dashboard, String query, TimeRange timerange, String field, String description, String streamId) {
-        this(dashboard, null, description, streamId, 0, query, timerange, field, null);
+    private final Boolean showPieChart;
+    private final Boolean showDataTable;
+
+    public QuickvaluesWidget(Dashboard dashboard, String query, TimeRange timerange, String field, String description, boolean showPieChart, boolean showDataTable, String streamId) {
+        this(dashboard, null, description, streamId, 0, query, timerange, field, showPieChart, showDataTable, null);
     }
 
-    public QuickvaluesWidget(Dashboard dashboard, String id, String description, String streamId, int cacheTime, String query, TimeRange timerange, String field, String creatorUserId) {
+    public QuickvaluesWidget(Dashboard dashboard, String id, String description, String streamId, int cacheTime, String query, TimeRange timerange, String field, boolean showPieChart, boolean showDataTable, String creatorUserId) {
         super(Type.QUICKVALUES, id, description, cacheTime, dashboard, creatorUserId, query, timerange);
 
         this.field = field;
+        this.showPieChart = showPieChart;
+        this.showDataTable = showDataTable;
 
         if (streamId != null && !streamId.isEmpty()) {
             this.streamId = streamId;
@@ -60,18 +59,26 @@ public class QuickvaluesWidget extends DashboardWidget {
         config.put("query", getQuery());
         config.put("stream_id", streamId);
         config.put("field", field);
+        config.put("show_pie_chart", showPieChart);
+        config.put("show_data_table", showDataTable);
 
         return config;
     }
 
+    private int getDefaultHeight() {
+        return (showDataTable && showPieChart) ? DEFAULT_HEIGHT_MULTIPLE_REPRESENTATIONS : DEFAULT_HEIGHT_SINGLE_REPRESENTATION;
+    }
+
     @Override
     public int getWidth() {
-        return WIDTH;
+        int storedWidth = super.getWidth();
+        return storedWidth == 0 ? DEFAULT_WIDTH : storedWidth;
     }
 
     @Override
     public int getHeight() {
-        return HEIGHT;
+        int storedHeight = super.getHeight();
+        return storedHeight == 0 ? this.getDefaultHeight() : storedHeight;
     }
 
     @Override
