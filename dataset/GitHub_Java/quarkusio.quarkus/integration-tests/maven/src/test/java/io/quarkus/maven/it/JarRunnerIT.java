5a8f39f77f60d9dc2f325118972cdaa950a1dc42
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:14:27 +0200

    Make the directory containing the output fast jar a constant
        This makes it easy to build a Dockerfile

--- dataset/GitHub_Java/quarkusio.quarkus/integration-tests/maven/src/test/java/io/quarkus/maven/it/JarRunnerIT.java
+++ dataset/GitHub_Java/quarkusio.quarkus/integration-tests/maven/src/test/java/io/quarkus/maven/it/JarRunnerIT.java
@@ -19,13 +19,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.shared.invoker.MavenInvocationException;
-import org.awaitility.core.ConditionTimeoutException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -74,7 +71,7 @@ public class JarRunnerIT extends MojoTestBase {
     }
 
     @Test
-    public void testThatFastJarFormatWorks() throws Exception {
+    public void testThatFastJarFormatWorks() throws MavenInvocationException, IOException {
         File testDir = initProject("projects/classic", "projects/project-classic-console-output-fast-jar");
         RunningInvoker running = new RunningInvoker(testDir, false);
 
@@ -98,12 +95,9 @@ public class JarRunnerIT extends MojoTestBase {
         Process process = doLaunch(jar, output);
         try {
             // Wait until server up
-            dumpFileContentOnFailure(() -> {
-                await()
-                        .pollDelay(1, TimeUnit.SECONDS)
-                        .atMost(1, TimeUnit.MINUTES).until(() -> DevModeTestUtils.getHttpResponse("/app/hello/package", 200));
-                return null;
-            }, output, ConditionTimeoutException.class);
+            await()
+                    .pollDelay(1, TimeUnit.SECONDS)
+                    .atMost(1, TimeUnit.MINUTES).until(() -> DevModeTestUtils.getHttpResponse("/app/hello/package", 200));
 
             String logs = FileUtils.readFileToString(output, "UTF-8");
 
@@ -119,7 +113,7 @@ public class JarRunnerIT extends MojoTestBase {
     }
 
     @Test
-    public void testThatMutableFastJarWorks() throws Exception {
+    public void testThatMutableFastJarWorks() throws MavenInvocationException, IOException, InterruptedException {
         File testDir = initProject("projects/classic", "projects/project-classic-console-output-mutable-fast-jar");
         RunningInvoker running = new RunningInvoker(testDir, false);
 
@@ -144,12 +138,9 @@ public class JarRunnerIT extends MojoTestBase {
         Process process = doLaunch(jar, output);
         try {
             // Wait until server up
-            dumpFileContentOnFailure(() -> {
-                await()
-                        .pollDelay(1, TimeUnit.SECONDS)
-                        .atMost(1, TimeUnit.MINUTES).until(() -> DevModeTestUtils.getHttpResponse("/app/hello/package", 200));
-                return null;
-            }, output, ConditionTimeoutException.class);
+            await()
+                    .pollDelay(1, TimeUnit.SECONDS)
+                    .atMost(1, TimeUnit.MINUTES).until(() -> DevModeTestUtils.getHttpResponse("/app/hello/package", 200));
 
             String logs = FileUtils.readFileToString(output, "UTF-8");
 
@@ -193,54 +184,14 @@ public class JarRunnerIT extends MojoTestBase {
         }
     }
 
-    /**
-     * Tests that quarkus.arc.exclude-dependency.* can be used for modules in a multimodule project
-     */
-    @Test
-    public void testArcExcludeDependencyOnLocalModule() throws Exception {
-        File testDir = initProject("projects/arc-exclude-dependencies");
-        RunningInvoker running = new RunningInvoker(testDir, false);
-
-        MavenProcessInvocationResult result = running.execute(Arrays.asList("package", "-DskipTests"), Collections.emptyMap());
-        await().atMost(1, TimeUnit.MINUTES).until(() -> result.getProcess() != null && !result.getProcess().isAlive());
-        assertThat(running.log()).containsIgnoringCase("BUILD SUCCESS");
-        running.stop();
-
-        File targetDir = new File(testDir.getAbsoluteFile(), "runner" + File.separator + "target");
-        Path jar = targetDir.toPath().toAbsolutePath()
-                .resolve(Paths.get("acme-1.0-SNAPSHOT-runner.jar"));
-        File output = new File(targetDir, "output.log");
-        output.createNewFile();
-
-        Process process = doLaunch(jar, output);
-        try {
-            // Wait until server up
-            AtomicReference<String> response = new AtomicReference<>();
-            await()
-                    .pollDelay(1, TimeUnit.SECONDS)
-                    .atMost(1, TimeUnit.MINUTES).until(() -> {
-                        String ret = DevModeTestUtils.getHttpResponse("/hello", true);
-                        response.set(ret);
-                        return ret.contains("hello:");
-                    });
-
-            // Test that bean is not resolvable
-            assertThat(response.get()).containsIgnoringCase("hello:false");
-        } finally {
-            process.destroy();
-        }
-    }
-
     private Process doLaunch(Path jar, File output) throws IOException {
         List<String> commands = new ArrayList<>();
         commands.add(JavaBinFinder.findBin());
         commands.add("-jar");
         commands.add(jar.toString());
-        // write out the command used to launch the process, into the log file
-        Files.write(output.toPath(), commands);
         ProcessBuilder processBuilder = new ProcessBuilder(commands.toArray(new String[0]));
-        processBuilder.redirectOutput(ProcessBuilder.Redirect.appendTo(output));
-        processBuilder.redirectError(ProcessBuilder.Redirect.appendTo(output));
+        processBuilder.redirectOutput(output);
+        processBuilder.redirectError(output);
         return processBuilder.start();
     }
 
@@ -265,32 +216,5 @@ public class JarRunnerIT extends MojoTestBase {
 
     private static void failResourcesFromTheClasspath() {
         fail("Failed to assert that the application properly reads resources from the classpath");
-    }
-
-    /**
-     * Calls the {@link Callable} and if that call results in an exception of type {@code failureType} then,
-     * prints out the contents of the passed {@code logFile} and throws back the original exception
-     *
-     * @param operation The operation to invoke
-     * @param logFile The file which contains the logs generated when the operation was running
-     * @param failureType The type of failure that should trigger printing out the logs
-     * @throws Exception
-     */
-    private void dumpFileContentOnFailure(final Callable<Void> operation, final File logFile,
-            final Class<? extends Throwable> failureType) throws Exception {
-        try {
-            operation.call();
-        } catch (Throwable t) {
-            if (failureType != null && failureType.isInstance(t)) {
-                final String logs = FileUtils.readFileToString(logFile, "UTF-8");
-                System.out.println("####### LOG DUMP ON FAILURE (start) ######");
-                System.out.println("Dumping logs that were generated in " + logFile + " for an operation that resulted in "
-                        + t.getClass().getName() + ":");
-                System.out.println();
-                System.out.println(logs);
-                System.out.println("####### LOG DUMP ON FAILURE (end) ######");
-            }
-            throw t;
-        }
     }
 }
