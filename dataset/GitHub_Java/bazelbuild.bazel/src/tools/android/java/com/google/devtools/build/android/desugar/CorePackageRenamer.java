3b8cc18ee58a27c138d8caa60c1f40077da860d1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:33:03 +0200

    Whitelist `android/device/collectors/*` from Android Platform Types
        - ` android/device/collectors/*` are from a user library instead pre-installed SDK, and therefore should be subject to normal desugar process instead core library desugaring process.
    
        PiperOrigin-RevId: 356624952

--- dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/desugar/CorePackageRenamer.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/desugar/CorePackageRenamer.java
@@ -13,42 +13,111 @@
 // limitations under the License.
 package com.google.devtools.build.android.desugar;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.Handle;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.commons.ClassRemapper;
+import org.objectweb.asm.commons.MethodRemapper;
 import org.objectweb.asm.commons.Remapper;
 
 /**
- * A visitor that renames some type names and only when the owner is also renamed.
+ * A visitor that renames packages so configured using {@link CoreLibrarySupport}. Additionally
+ * generate bridge-like methods for core library overrides that should be preserved that call the
+ * renamed variants.
  */
 class CorePackageRenamer extends ClassRemapper {
+
+  private String internalName;
 
   public CorePackageRenamer(ClassVisitor cv, CoreLibrarySupport support) {
     super(cv, new CorePackageRemapper(support));
   }
 
-  private static final class CorePackageRemapper extends Remapper {
-    private final CoreLibrarySupport support;
+  @Override
+  public void visit(
+      int version,
+      int access,
+      String name,
+      String signature,
+      String superName,
+      String[] interfaces) {
+    checkState(internalName == null || internalName.equals(name), "Instance already used.");
+    internalName = name;
+    super.visit(version, access, name, signature, superName, interfaces);
+  }
 
-    private CorePackageRemapper(CoreLibrarySupport support) {
-      this.support = support;
+  @Override
+  protected CoreMethodRemapper createMethodRemapper(MethodVisitor methodVisitor) {
+    return new CoreMethodRemapper(methodVisitor, remapper);
+  }
+
+  private class CoreMethodRemapper extends MethodRemapper {
+
+    public CoreMethodRemapper(MethodVisitor methodVisitor, Remapper remapper) {
+      super(methodVisitor, remapper);
     }
 
-    public boolean isRenamed(String owner) {
-      return support.isRenamedCoreLibrary(owner);
+    @Override
+    public void visitMethodInsn(
+        int opcode, String owner, String name, String descriptor, boolean isInterface) {
+      CorePackageRemapper remapper = (CorePackageRemapper) this.remapper;
+      remapper.didSomething = false;
+      super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+      // TODO(b/79121791): Make this more precise: look for all unsupported core library members
+      checkState(
+          !remapper.didSomething
+              || !owner.startsWith("android/")
+              || owner.startsWith("android/arch/")
+              || owner.startsWith("android/device/collectors/") // b/179812549
+              || owner.startsWith("android/support/"),
+          "%s calls %s.%s%s which is not supported with core library desugaring. Please file "
+              + "a feature request to support this method",
+          internalName,
+          owner,
+          name,
+          descriptor);
+    }
+
+    @Override
+    public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
+      CorePackageRemapper remapper = (CorePackageRemapper) this.remapper;
+      remapper.didSomething = false;
+      super.visitFieldInsn(opcode, owner, name, descriptor);
+      // TODO(b/79121791): Make this more precise: look for all unsupported core library members
+      checkState(
+          !remapper.didSomething
+              || !owner.startsWith("android/")
+              || owner.startsWith("android/arch/")
+              || owner.startsWith("android/car/")
+              || owner.startsWith("android/device/collectors/") // b/179812549
+              || owner.startsWith("android/support/"),
+          "%s accesses %s.%s: %s which is not supported with core library desugaring. Please file "
+              + "a feature request to support this field",
+          internalName,
+          owner,
+          name,
+          descriptor);
+    }
+  }
+
+  /** ASM {@link Remapper} based on {@link CoreLibrarySupport}. */
+  private static class CorePackageRemapper extends Remapper {
+
+    private final CoreLibrarySupport support;
+    boolean didSomething = false;
+
+    CorePackageRemapper(CoreLibrarySupport support) {
+      this.support = support;
     }
 
     @Override
     public String map(String typeName) {
-      return isRenamed(typeName) ? support.renameCoreLibrary(typeName) : typeName;
-    }
-
-    @Override
-    public Object mapValue(Object value) {
-      if (value instanceof Handle && !isRenamed(((Handle) value).getOwner())) {
-        return value;
+      if (support.isRenamedCoreLibrary(typeName)) {
+        didSomething = true;
+        return support.renameCoreLibrary(typeName);
       }
-      return super.mapValue(value);
+      return typeName;
     }
   }
 }
