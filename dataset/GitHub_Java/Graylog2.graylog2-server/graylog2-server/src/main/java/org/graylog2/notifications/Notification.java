b5f50e510021355648752a28640ba4d88ad23061
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:38:01 +0200

    Generate system notification if GC takes longer than threshold
        Long garbage collection runs are usually an early signs of an unhealthy system.
        This changeset generates an URGENT system notification of type "gc_too_long" for
        the node which suffered the long GC run.
    
        The user can configure the check interval (`gc_check_interval`, default: 1 minute)
        and the threshold (`gc_warning_threshold`, default: 1 second) in graylog2.conf.
    
        Fixes #760

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/notifications/Notification.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/notifications/Notification.java
@@ -61,9 +61,7 @@ public interface Notification extends Persisted {
         EMAIL_TRANSPORT_CONFIGURATION_INVALID,
         EMAIL_TRANSPORT_FAILED,
         STREAM_PROCESSING_DISABLED,
-        GC_TOO_LONG,
-        JOURNAL_UTILIZATION_TOO_HIGH,
-        JOURNAL_UNCOMMITTED_MESSAGES_DELETED
+        GC_TOO_LONG
     }
 
     public enum Severity {
