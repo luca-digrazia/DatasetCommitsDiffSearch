f0f56dc4de9d92a9ddb1b7fa74dc80b55be53547
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:19:15 +0200

    Create searches cluster config if none exist

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/periodical/ConfigurationManagementPeriodical.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/periodical/ConfigurationManagementPeriodical.java
@@ -19,13 +19,19 @@ package org.graylog2.periodical;
 
 import org.graylog2.configuration.ElasticsearchConfiguration;
 import org.graylog2.indexer.management.IndexManagementConfig;
-import org.graylog2.indexer.rotation.MessageCountRotationStrategy;
-import org.graylog2.indexer.rotation.MessageCountRotationStrategyConfig;
-import org.graylog2.indexer.rotation.SizeBasedRotationStrategy;
-import org.graylog2.indexer.rotation.SizeBasedRotationStrategyConfig;
-import org.graylog2.indexer.rotation.TimeBasedRotationStrategy;
-import org.graylog2.indexer.rotation.TimeBasedRotationStrategyConfig;
+import org.graylog2.indexer.retention.strategies.ClosingRetentionStrategy;
+import org.graylog2.indexer.retention.strategies.ClosingRetentionStrategyConfig;
+import org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy;
+import org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig;
+import org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategy;
+import org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategyConfig;
+import org.graylog2.indexer.rotation.strategies.SizeBasedRotationStrategy;
+import org.graylog2.indexer.rotation.strategies.SizeBasedRotationStrategyConfig;
+import org.graylog2.indexer.rotation.strategies.TimeBasedRotationStrategy;
+import org.graylog2.indexer.rotation.strategies.TimeBasedRotationStrategyConfig;
+import org.graylog2.indexer.searches.SearchesClusterConfig;
 import org.graylog2.plugin.cluster.ClusterConfigService;
+import org.graylog2.plugin.indexer.retention.RetentionStrategy;
 import org.graylog2.plugin.indexer.rotation.RotationStrategy;
 import org.graylog2.plugin.periodical.Periodical;
 import org.slf4j.Logger;
@@ -53,27 +59,41 @@ public class ConfigurationManagementPeriodical extends Periodical {
         final TimeBasedRotationStrategyConfig timeBasedRotationStrategyConfig = clusterConfigService.get(TimeBasedRotationStrategyConfig.class);
 
         if (messageCountRotationStrategyConfig == null) {
-            final MessageCountRotationStrategyConfig config = MessageCountRotationStrategyConfig.create(elasticsearchConfiguration.getMaxDocsPerIndex());
-            clusterConfigService.write(config);
-            LOG.info("Migrated \"{}\" setting: {}", "elasticsearch_max_docs_per_index", config);
+            final MessageCountRotationStrategyConfig countConfig = MessageCountRotationStrategyConfig.create(elasticsearchConfiguration.getMaxDocsPerIndex());
+            clusterConfigService.write(countConfig);
+            LOG.info("Migrated \"{}\" setting: {}", "elasticsearch_max_docs_per_index", countConfig);
         }
         if (sizeBasedRotationStrategyConfig == null) {
-            final SizeBasedRotationStrategyConfig config = SizeBasedRotationStrategyConfig.create(elasticsearchConfiguration.getMaxSizePerIndex());
-            clusterConfigService.write(config);
-            LOG.info("Migrated \"{}\" setting: {}", "elasticsearch_max_size_per_index", config);
+            final SizeBasedRotationStrategyConfig sizeConfig = SizeBasedRotationStrategyConfig.create(elasticsearchConfiguration.getMaxSizePerIndex());
+            clusterConfigService.write(sizeConfig);
+            LOG.info("Migrated \"{}\" setting: {}", "elasticsearch_max_size_per_index", sizeConfig);
         }
         if (timeBasedRotationStrategyConfig == null) {
-            final TimeBasedRotationStrategyConfig config = TimeBasedRotationStrategyConfig.create(elasticsearchConfiguration.getMaxTimePerIndex());
-            clusterConfigService.write(config);
-            LOG.info("Migrated \"{}\" setting: {}", "elasticsearch_max_time_per_index", config);
+            final TimeBasedRotationStrategyConfig timeConfig = TimeBasedRotationStrategyConfig.create(elasticsearchConfiguration.getMaxTimePerIndex());
+            clusterConfigService.write(timeConfig);
+            LOG.info("Migrated \"{}\" setting: {}", "elasticsearch_max_time_per_index", timeConfig);
         }
 
-        // Selected rotation strategy.
-        final IndexManagementConfig indexManagementConfig = clusterConfigService.get(IndexManagementConfig.class);
+        // All default retention strategy settings
+        final ClosingRetentionStrategyConfig closingRetentionStrategyConfig = clusterConfigService.get(ClosingRetentionStrategyConfig.class);
+        final DeletionRetentionStrategyConfig deletionRetentionStrategyConfig = clusterConfigService.get(DeletionRetentionStrategyConfig.class);
 
+        if (closingRetentionStrategyConfig == null) {
+            final ClosingRetentionStrategyConfig closingConfig = ClosingRetentionStrategyConfig.create(elasticsearchConfiguration.getMaxNumberOfIndices());
+            clusterConfigService.write(closingConfig);
+            LOG.info("Migrated \"{}\" setting: {}", "elasticsearch_max_number_of_indices", closingConfig);
+        }
+
+        if (deletionRetentionStrategyConfig == null) {
+            final DeletionRetentionStrategyConfig deletionConfig = DeletionRetentionStrategyConfig.create(elasticsearchConfiguration.getMaxNumberOfIndices());
+            clusterConfigService.write(deletionConfig);
+            LOG.info("Migrated \"{}\" setting: {}", "elasticsearch_max_number_of_indices", deletionConfig);
+        }
+
+        // Selected rotation and retention strategies.
+        final IndexManagementConfig indexManagementConfig = clusterConfigService.get(IndexManagementConfig.class);
         if (indexManagementConfig == null) {
             final Class<? extends RotationStrategy> rotationStrategyClass;
-
             switch (elasticsearchConfiguration.getRotationStrategy()) {
                 case "size":
                     rotationStrategyClass = SizeBasedRotationStrategy.class;
@@ -82,13 +102,38 @@ public class ConfigurationManagementPeriodical extends Periodical {
                     rotationStrategyClass = TimeBasedRotationStrategy.class;
                     break;
                 case "count":
+                    rotationStrategyClass = MessageCountRotationStrategy.class;
+                    break;
                 default:
+                    LOG.warn("Unknown retention strategy \"{}\"", elasticsearchConfiguration.getRotationStrategy());
                     rotationStrategyClass = MessageCountRotationStrategy.class;
             }
 
-            final IndexManagementConfig config = IndexManagementConfig.create(rotationStrategyClass.getCanonicalName());
+            final Class<? extends RetentionStrategy> retentionStrategyClass;
+            switch (elasticsearchConfiguration.getRetentionStrategy()) {
+                case "close":
+                    retentionStrategyClass = ClosingRetentionStrategy.class;
+                    break;
+                case "delete":
+                    retentionStrategyClass = DeletionRetentionStrategy.class;
+                    break;
+                default:
+                    LOG.warn("Unknown retention strategy \"{}\"", elasticsearchConfiguration.getRetentionStrategy());
+                    retentionStrategyClass = DeletionRetentionStrategy.class;
+            }
+
+            final IndexManagementConfig config = IndexManagementConfig.create(
+                    rotationStrategyClass.getCanonicalName(),
+                    retentionStrategyClass.getCanonicalName());
             clusterConfigService.write(config);
-            LOG.info("Migrated \"{}\" setting: {}", "rotation_strategy", config);
+            LOG.info("Migrated \"{}\" and \"{}\" setting: {}", "rotation_strategy", "retention_strategy", config);
+        }
+
+        final SearchesClusterConfig searchesClusterConfig = clusterConfigService.get(SearchesClusterConfig.class);
+        if (searchesClusterConfig == null) {
+            final SearchesClusterConfig config = SearchesClusterConfig.createDefault();
+            LOG.info("Creating searches cluster config: {}", config);
+            clusterConfigService.write(config);
         }
     }
 
