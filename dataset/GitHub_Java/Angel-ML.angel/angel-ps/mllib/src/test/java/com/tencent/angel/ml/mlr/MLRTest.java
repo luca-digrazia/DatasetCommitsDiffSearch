872c46bf3bdda9100e8c17bd653cf3eea9980cb2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:42:57 +0200

    [UT] Fix MLRTest

--- dataset/GitHub_Java/Angel-ML.angel/angel-ps/mllib/src/test/java/com/tencent/angel/ml/mlr/MLRTest.java
+++ dataset/GitHub_Java/Angel-ML.angel/angel-ps/mllib/src/test/java/com/tencent/angel/ml/mlr/MLRTest.java
@@ -1,21 +1,5 @@
-/*
- * Tencent is pleased to support the open source community by making Angel available.
- *
- * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
- *
- * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- *
- * https://opensource.org/licenses/BSD-3-Clause
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- *
- */
-
 package com.tencent.angel.ml.mlr;
+
 
 import com.tencent.angel.conf.AngelConf;
 import com.tencent.angel.ml.classification.mlr.MLRRunner;
@@ -30,6 +14,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 
+/**
+ * Created by hbghh on 2017/8/17.
+ */
 public class MLRTest {
   private Configuration conf = new Configuration();
   private static final Log LOG = LogFactory.getLog(MLRTest.class);
@@ -48,7 +35,7 @@ public class MLRTest {
       // Feature number of train data
       int featureNum = 124;
       // Total iteration number
-      int epochNum = 10;
+      int epochNum = 20;
       // Validation sample Ratio
       double vRatio = 0.1;
       // Data format, libsvm or dummy
@@ -76,7 +63,7 @@ public class MLRTest {
       conf.setBoolean(AngelConf.ANGEL_JOB_OUTPUT_PATH_DELETEONEXIST, true);
 
       // Set data format
-      conf.set(MLConf.ML_DATA_FORMAT(), dataFmt);
+      conf.set(MLConf.ML_DATAFORMAT(), dataFmt);
 
       //set angel resource parameters #worker, #task, #PS
       conf.setInt(AngelConf.ANGEL_WORKERGROUP_NUMBER, 1);
