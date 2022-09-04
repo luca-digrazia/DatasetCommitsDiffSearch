a8eb9224c9cacda8fddedce5cc8c5d03e4cf0a52
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:08:56 +0200

    Use @Identifier instead of @Named in kafka client
        This is needed to remove a warning from SR RM

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/kafka-streams/runtime/src/main/java/io/quarkus/kafka/streams/runtime/KafkaStreamsProducer.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/kafka-streams/runtime/src/main/java/io/quarkus/kafka/streams/runtime/KafkaStreamsProducer.java
@@ -39,7 +39,6 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KafkaStreams.StateListener;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
 import org.apache.kafka.streams.processor.StateRestoreListener;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.logging.Logger;
@@ -74,8 +73,7 @@ public class KafkaStreamsProducer {
     public KafkaStreamsProducer(KafkaStreamsSupport kafkaStreamsSupport, KafkaStreamsRuntimeConfig runtimeConfig,
             Instance<Topology> topology, Instance<KafkaClientSupplier> kafkaClientSupplier,
             @Identifier("default-kafka-broker") Instance<Map<String, Object>> defaultConfiguration,
-            Instance<StateListener> stateListener, Instance<StateRestoreListener> globalStateRestoreListener,
-            Instance<StreamsUncaughtExceptionHandler> uncaughtExceptionHandlerListener) {
+            Instance<StateListener> stateListener, Instance<StateRestoreListener> globalStateRestoreListener) {
         shutdown = false;
         // No producer for Topology -> nothing to do
         if (topology.isUnsatisfied()) {
@@ -106,8 +104,7 @@ public class KafkaStreamsProducer {
         this.executorService = Executors.newSingleThreadExecutor();
 
         this.kafkaStreams = initializeKafkaStreams(kafkaStreamsProperties, runtimeConfig, kafkaAdminClient, topology.get(),
-                kafkaClientSupplier, stateListener, globalStateRestoreListener, uncaughtExceptionHandlerListener,
-                executorService);
+                kafkaClientSupplier, stateListener, globalStateRestoreListener, executorService);
         this.kafkaStreamsTopologyManager = new KafkaStreamsTopologyManager(kafkaAdminClient);
     }
 
@@ -152,7 +149,7 @@ public class KafkaStreamsProducer {
             KafkaStreamsRuntimeConfig runtimeConfig, Admin adminClient, Topology topology,
             Instance<KafkaClientSupplier> kafkaClientSupplier,
             Instance<StateListener> stateListener, Instance<StateRestoreListener> globalStateRestoreListener,
-            Instance<StreamsUncaughtExceptionHandler> uncaughtExceptionHandlerListener, ExecutorService executorService) {
+            ExecutorService executorService) {
         KafkaStreams kafkaStreams;
         if (kafkaClientSupplier.isUnsatisfied()) {
             kafkaStreams = new KafkaStreams(topology, kafkaStreamsProperties);
@@ -165,9 +162,6 @@ public class KafkaStreamsProducer {
         }
         if (!globalStateRestoreListener.isUnsatisfied()) {
             kafkaStreams.setGlobalStateRestoreListener(globalStateRestoreListener.get());
-        }
-        if (!uncaughtExceptionHandlerListener.isUnsatisfied()) {
-            kafkaStreams.setUncaughtExceptionHandler(uncaughtExceptionHandlerListener.get());
         }
 
         executorService.execute(new Runnable() {
