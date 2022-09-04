750bd088ee99b43705c38540308678cbd0e60097
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 17:50:31 +0200

    Add a link to provider guide from provider docs
        Fixes #11310
    
        RELNOTES: None.
        PiperOrigin-RevId: 311535433

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skylarkbuildapi/StarlarkRuleFunctionsApi.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skylarkbuildapi/StarlarkRuleFunctionsApi.java
@@ -17,6 +17,11 @@ package com.google.devtools.build.lib.skylarkbuildapi;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.skylarkbuildapi.StarlarkConfigApi.BuildSettingApi;
 import com.google.devtools.build.lib.skylarkbuildapi.core.ProviderApi;
+import com.google.devtools.build.lib.skylarkinterface.Param;
+import com.google.devtools.build.lib.skylarkinterface.ParamType;
+import com.google.devtools.build.lib.skylarkinterface.SkylarkCallable;
+import com.google.devtools.build.lib.skylarkinterface.SkylarkGlobalLibrary;
+import com.google.devtools.build.lib.skylarkinterface.StarlarkConstructor;
 import com.google.devtools.build.lib.syntax.Dict;
 import com.google.devtools.build.lib.syntax.EvalException;
 import com.google.devtools.build.lib.syntax.NoneType;
@@ -25,17 +30,12 @@ import com.google.devtools.build.lib.syntax.StarlarkCallable;
 import com.google.devtools.build.lib.syntax.StarlarkFunction;
 import com.google.devtools.build.lib.syntax.StarlarkSemantics.FlagIdentifier;
 import com.google.devtools.build.lib.syntax.StarlarkThread;
-import net.starlark.java.annot.Param;
-import net.starlark.java.annot.ParamType;
-import net.starlark.java.annot.StarlarkConstructor;
-import net.starlark.java.annot.StarlarkGlobalLibrary;
-import net.starlark.java.annot.StarlarkMethod;
 
 /**
  * Interface for a global Starlark library containing rule-related helper and registration
  * functions.
  */
-@StarlarkGlobalLibrary
+@SkylarkGlobalLibrary
 public interface StarlarkRuleFunctionsApi<FileApiT extends FileApi> {
 
   String EXEC_COMPATIBLE_WITH_PARAM = "exec_compatible_with";
@@ -52,7 +52,7 @@ public interface StarlarkRuleFunctionsApi<FileApiT extends FileApi> {
           + "<a href='globals.html#provider'><code>provider()</code></a>, except that a legacy "
           + "provider is represented by its string name instead.";
 
-  @StarlarkMethod(
+  @SkylarkCallable(
       name = "provider",
       doc =
           "Creates a declared provider 'constructor'. The return value of this "
@@ -93,7 +93,7 @@ public interface StarlarkRuleFunctionsApi<FileApiT extends FileApi> {
       useStarlarkThread = true)
   ProviderApi provider(String doc, Object fields, StarlarkThread thread) throws EvalException;
 
-  @StarlarkMethod(
+  @SkylarkCallable(
       name = "rule",
       doc =
           "Creates a new rule, which can be called from a BUILD file or a macro to create targets."
@@ -356,7 +356,7 @@ public interface StarlarkRuleFunctionsApi<FileApiT extends FileApi> {
       Boolean outputToGenfiles,
       Sequence<?> fragments,
       Sequence<?> hostFragments,
-      Boolean starlarkTestable,
+      Boolean skylarkTestable,
       Sequence<?> toolchains,
       String doc,
       Sequence<?> providesArg,
@@ -368,7 +368,7 @@ public interface StarlarkRuleFunctionsApi<FileApiT extends FileApi> {
       StarlarkThread thread)
       throws EvalException;
 
-  @StarlarkMethod(
+  @SkylarkCallable(
       name = "aspect",
       doc =
           "Creates a new aspect. The result of this function must be stored in a global value. "
@@ -515,7 +515,7 @@ public interface StarlarkRuleFunctionsApi<FileApiT extends FileApi> {
       StarlarkThread thread)
       throws EvalException;
 
-  @StarlarkMethod(
+  @SkylarkCallable(
       name = "Label",
       doc =
           "Creates a Label referring to a BUILD target. Use "
@@ -544,7 +544,7 @@ public interface StarlarkRuleFunctionsApi<FileApiT extends FileApi> {
   Label label(String labelString, Boolean relativeToCallerRepository, StarlarkThread thread)
       throws EvalException;
 
-  @StarlarkMethod(
+  @SkylarkCallable(
       name = "exec_group",
       // TODO(juliexxia); uncomment or remove based on resolution of b/152637857
       // enableOnlyWithFlag = FlagIdentifier.EXPERIMENTAL_EXEC_GROUPS,
