e32786bfcd5aa7734547d5c5f5a470570996a351
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:57:41 +0200

    Fix typo

--- dataset/GitHub_Java/quarkusio.quarkus/devtools/gradle/src/main/java/io/quarkus/gradle/tasks/QuarkusDev.java
+++ dataset/GitHub_Java/quarkusio.quarkus/devtools/gradle/src/main/java/io/quarkus/gradle/tasks/QuarkusDev.java
@@ -230,8 +230,7 @@ public class QuarkusDev extends QuarkusTask {
                 .buildDir(getBuildDir())
                 .outputDir(getBuildDir())
                 .debug(System.getProperty("debug"))
-                .debugHost(System.getProperty("debugHost"))
-                .debugPort(System.getProperty("debugPort"))
+                .debugHost(System.getProperty("debugHost", "localhost"))
                 .suspend(System.getProperty("suspend"));
         if (System.getProperty(IO_QUARKUS_DEVMODE_ARGS) == null) {
             builder.jvmArgs("-Dquarkus.test.basic-console=true")
