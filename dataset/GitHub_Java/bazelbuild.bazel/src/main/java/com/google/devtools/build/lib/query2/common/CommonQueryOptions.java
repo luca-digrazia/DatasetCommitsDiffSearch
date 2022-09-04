295a3c85c581fe40d36bab21b2915b2d142642e7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:41:01 +0200

    Flag flip for incompatible_display_source_file_location.
        RELNOTES[INC]: Query output=xml/proto/location for source files will now show the location of line 1 of the source file (as the new default) instead of its location in the BUILD file.
    
        PiperOrigin-RevId: 351800418

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/query2/common/CommonQueryOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/query2/common/CommonQueryOptions.java
@@ -23,51 +23,85 @@ import com.google.devtools.common.options.EnumConverter;
 import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionDocumentationCategory;
 import com.google.devtools.common.options.OptionEffectTag;
+import com.google.devtools.common.options.OptionMetadataTag;
 import com.google.devtools.common.options.OptionsBase;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 
-/** Options shared between blaze query and blaze cquery. */
+/** Options shared between blaze query implementations. */
 public class CommonQueryOptions extends OptionsBase {
 
   @Option(
-    name = "universe_scope",
-    defaultValue = "",
-    documentationCategory = OptionDocumentationCategory.QUERY,
-    converter = Converters.CommaSeparatedOptionListConverter.class,
-    effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS},
-    help =
-        "A comma-separated set of target patterns (additive and subtractive). The query may "
-            + "be performed in the universe defined by the transitive closure of the specified "
-            + "targets. This option is used for the query and cquery commands. \n"
-            + "For cquery, the input to this option is the targets all answers are built under and "
-            + "so this option may affect configurations and transitions. If this option is not "
-            + "specified, the top-level targets are assumed to be the targets parsed from the "
-            + "query expression. Note: For cquery, not specifying this option may cause the build "
-            + "to break if targets parsed from the query expression are not buildable with "
-            + "top-level options."
-  )
+      name = "universe_scope",
+      defaultValue = "",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      converter = Converters.CommaSeparatedOptionListConverter.class,
+      effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS},
+      help =
+          "A comma-separated set of target patterns (additive and subtractive). The query may be"
+              + " performed in the universe defined by the transitive closure of the specified"
+              + " targets. This option is used for the query and cquery commands. \n"
+              + "For cquery, the input to this option is the targets all answers are built under"
+              + " and so this option may affect configurations and transitions. If this option is"
+              + " not specified, the top-level targets are assumed to be the targets parsed from"
+              + " the query expression. Note: For cquery, not specifying this option may cause the"
+              + " build to break if targets parsed from the query expression are not buildable"
+              + " with top-level options.")
   public List<String> universeScope;
 
   @Option(
-    name = "host_deps",
-    defaultValue = "true",
-    documentationCategory = OptionDocumentationCategory.QUERY,
-    effectTags = {OptionEffectTag.BUILD_FILE_SEMANTICS},
-    help =
-        "Query: If disabled, dependencies on 'host configuration' targets will not be included in "
-            + "the dependency graph over which the query operates. A 'host configuration' "
-            + "dependency edge, such as the one from any 'proto_library' rule to the Protocol "
-            + "Compiler, usually points to a tool executed during the build (on the host machine) "
-            + "rather than a part of the same 'target' program. \n"
-            + "Cquery: If disabled, filters out all configured targets which cross a host "
-            + "transition from the top-level target that discovered this configured target. That "
-            + "means if the top-level target is in the target configuration, only configured "
-            + "targets also in the target configuration will be returned. If the top-level target "
-            + "is in the host configuration, only host configured targets will be returned."
-  )
-  public boolean includeHostDeps;
+      name = "line_terminator_null",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
+      help = "Whether each format is terminated with \\0 instead of newline.")
+  public boolean lineTerminatorNull;
+
+  /** Ugly workaround since line terminator option default has to be constant expression. */
+  public String getLineTerminator() {
+    if (lineTerminatorNull) {
+      return "\0";
+    }
+
+    return System.lineSeparator();
+  }
+
+  @Option(
+      name = "infer_universe_scope",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS},
+      help =
+          "If set and --universe_scope is unset, then a value of --universe_scope will be inferred"
+              + " as the list of unique target patterns in the query expression. Note that the"
+              + " --universe_scope value inferred for a query expression that uses universe-scoped"
+              + " functions (e.g.`allrdeps`) may not be what you want, so you should use this"
+              + " option only if you know what you are doing. See"
+              + " https://docs.bazel.build/versions/master/query.html#sky-query for details and"
+              + " examples. If --universe_scope is set, then this option's value is ignored. Note:"
+              + " this option applies only to `query` (i.e. not `cquery`).")
+  public boolean inferUniverseScope;
+
+  @Option(
+      name = "tool_deps",
+      oldName = "host_deps",
+      defaultValue = "true",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.BUILD_FILE_SEMANTICS},
+      help =
+          "Query: If disabled, dependencies on 'host configuration' or 'execution' targets will"
+              + " not be included in the dependency graph over which the query operates. A 'host"
+              + " configuration' dependency edge, such as the one from any 'proto_library' rule to"
+              + " the Protocol Compiler, usually points to a tool executed during the build rather"
+              + " than a part of the same 'target' program. \n"
+              + "Cquery: If disabled, filters out all configured targets which cross a host or"
+              + " execution transition from the top-level target that discovered this configured"
+              + " target. That means if the top-level target is in the target configuration, only"
+              + " configured targets also in the target configuration will be returned. If the"
+              + " top-level target is in the host configuration, only host configured targets will"
+              + " be returned.")
+  public boolean includeToolDeps;
 
   @Option(
       name = "implicit_deps",
@@ -92,11 +126,21 @@ public class CommonQueryOptions extends OptionsBase {
               + "all the \"nodep\" attributes in the build language.")
   public boolean includeNoDepDeps;
 
+  @Option(
+      name = "include_aspects",
+      defaultValue = "true",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
+      help =
+          "aquery, cquery: whether to include aspect-generated actions in the output. "
+              + "query: no-op (aspects are always followed).")
+  public boolean useAspects;
+
   /** Return the current options as a set of QueryEnvironment settings. */
   public Set<Setting> toSettings() {
     Set<Setting> settings = EnumSet.noneOf(Setting.class);
-    if (!includeHostDeps) {
-      settings.add(Setting.NO_HOST_DEPS);
+    if (!includeToolDeps) {
+      settings.add(Setting.ONLY_TARGET_DEPS);
     }
     if (!includeImplicitDeps) {
       settings.add(Setting.NO_IMPLICIT_DEPS);
@@ -104,8 +148,31 @@ public class CommonQueryOptions extends OptionsBase {
     if (!includeNoDepDeps) {
       settings.add(Setting.NO_NODEP_DEPS);
     }
+    if (useAspects) {
+      settings.add(Setting.INCLUDE_ASPECTS);
+    }
     return settings;
   }
+
+  ///////////////////////////////////////////////////////////
+  // LOCATION OUTPUT FORMATTER OPTIONS                     //
+  ///////////////////////////////////////////////////////////
+
+  // TODO(tanzhengwei): Clean up in next major release
+  @Option(
+      name = "incompatible_display_source_file_location",
+      defaultValue = "true",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
+      metadataTags = {
+        OptionMetadataTag.INCOMPATIBLE_CHANGE,
+        OptionMetadataTag.TRIGGERED_BY_ALL_INCOMPATIBLE_CHANGES
+      },
+      help =
+          "False by default, displays the target of the source file. "
+              + "If true, displays the location of line 1 of source files in location outputs. "
+              + "This flag only exists for migration purposes.")
+  public boolean displaySourceFileLocation;
 
   ///////////////////////////////////////////////////////////
   // PROTO OUTPUT FORMATTER OPTIONS                        //
@@ -120,8 +187,7 @@ public class CommonQueryOptions extends OptionsBase {
           "If true, the location of BUILD files in xml and proto outputs will be relative. "
               + "By default, the location output is an absolute path and will not be consistent "
               + "across machines. You can set this option to true to have a consistent result "
-              + "across machines."
-  )
+              + "across machines.")
   public boolean relativeLocations;
 
   @Option(
@@ -139,8 +205,7 @@ public class CommonQueryOptions extends OptionsBase {
       effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
       help =
           "If true, attributes whose value is not explicitly specified in the BUILD file are "
-              + "included; otherwise they are omitted. This option is applicable to --output=proto"
-  )
+              + "included; otherwise they are omitted. This option is applicable to --output=proto")
   public boolean protoIncludeDefaultValues;
 
   @Option(
@@ -151,8 +216,7 @@ public class CommonQueryOptions extends OptionsBase {
       help =
           "If enabled, configurable attributes created by select() are flattened. For list types "
               + "the flattened representation is a list containing each value of the select map "
-              + "exactly once. Scalar types are flattened to null."
-  )
+              + "exactly once. Scalar types are flattened to null.")
   public boolean protoFlattenSelects;
 
   @Option(
@@ -164,8 +228,7 @@ public class CommonQueryOptions extends OptionsBase {
       help =
           "Comma separated list of attributes to include in output. Defaults to all attributes. "
               + "Set to empty string to not output any attribute. "
-              + "This option is applicable to --output=proto."
-  )
+              + "This option is applicable to --output=proto.")
   public List<String> protoOutputRuleAttributes = ImmutableList.of("all");
 
   @Option(
@@ -176,7 +239,35 @@ public class CommonQueryOptions extends OptionsBase {
       help = "Whether or not to populate the rule_input and rule_output fields.")
   public boolean protoIncludeRuleInputsAndOutputs;
 
-  /** An enum converter for {@code  AspectResolver.Mode} . Should be used internally only. */
+  @Option(
+      name = "proto:include_synthetic_attribute_hash",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
+      help = "Whether or not to calculate and populate the $internal_attr_hash attribute.")
+  public boolean protoIncludeSyntheticAttributeHash;
+
+  @Option(
+      name = "proto:instantiation_stack",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
+      help =
+          "Populate the instantiation call stack of each rule. "
+              + "Note that this requires the stack to be present")
+  public boolean protoIncludeInstantiationStack;
+
+  @Option(
+      name = "proto:definition_stack",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
+      help =
+          "Populate the definition_stack proto field, which records for each rule instance the "
+              + "Starlark call stack at the moment the rule's class was defined.")
+  public boolean protoIncludeDefinitionStack;
+
+  /** An enum converter for {@code AspectResolver.Mode} . Should be used internally only. */
   public static class AspectResolutionModeConverter extends EnumConverter<Mode> {
     public AspectResolutionModeConverter() {
       super(AspectResolver.Mode.class, "Aspect resolution mode");
@@ -200,4 +291,30 @@ public class CommonQueryOptions extends OptionsBase {
               + "precise mode is not completely precise: the decision whether to compute an aspect "
               + "is decided in the analysis phase, which is not run during 'bazel query'.")
   public AspectResolver.Mode aspectDeps;
+
+  ///////////////////////////////////////////////////////////
+  // GRAPH OUTPUT FORMATTER OPTIONS                        //
+  ///////////////////////////////////////////////////////////
+
+  @Option(
+      name = "graph:node_limit",
+      defaultValue = "512",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
+      help =
+          "The maximum length of the label string for a graph node in the output.  Longer labels"
+              + " will be truncated; -1 means no truncation.  This option is only applicable to"
+              + " --output=graph.")
+  public int graphNodeStringLimit;
+
+  @Option(
+      name = "graph:factored",
+      defaultValue = "true",
+      documentationCategory = OptionDocumentationCategory.QUERY,
+      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
+      help =
+          "If true, then the graph will be emitted 'factored', i.e. topologically-equivalent nodes "
+              + "will be merged together and their labels concatenated. This option is only "
+              + "applicable to --output=graph.")
+  public boolean graphFactored;
 }
