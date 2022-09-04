04886c51f5f2013405b15dcb823844d722049255
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:15:52 +0200

    Fix an early nested set expansion.
        Closes #13309.
    
        PiperOrigin-RevId: 368593772

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/StandaloneTestStrategy.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/exec/StandaloneTestStrategy.java
@@ -472,7 +472,7 @@ public class StandaloneTestStrategy extends TestStrategy {
             .build(),
         /* tools= */ NestedSetBuilder.emptySet(Order.STABLE_ORDER),
         /* outputs= */ ImmutableSet.of(
-            ActionInputHelper.fromPath(action.getCoverageData().getExecPath())),
+            ActionInputHelper.fromPath(action.getCoverageData().getExecPathString())),
         SpawnAction.DEFAULT_RESOURCE_SET);
   }
 
