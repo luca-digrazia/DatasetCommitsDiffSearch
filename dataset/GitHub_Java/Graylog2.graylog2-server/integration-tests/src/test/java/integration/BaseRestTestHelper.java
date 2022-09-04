304b907c9b5e14b98620a47ca8129d14069f5801
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:32:40 +0200

    Add BaseRestTestHelper#containsKeys() method
        The BaseRestTestHelper#containsKeys() method checks for the existence of the given keys
        and ignores any other keys in the JSON (Map) response.

--- dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/BaseRestTestHelper.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/BaseRestTestHelper.java
@@ -27,6 +27,7 @@ import org.hamcrest.Matcher;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
@@ -56,6 +57,32 @@ public class BaseRestTestHelper {
      */
 
     public static ResponseAwareMatcher<Response> containsAllKeys(String... keys) {
+        return new StrictKeysPresentMatcher(keys);
+    }
+
+    /**
+     * Returns a {@link com.jayway.restassured.matcher.ResponseAwareMatcher} which checks that the given keys are present.
+     * Given this JSON
+     * <pre>
+     *     {
+     *      "version": "2.0.0",
+     *      "codename": "foo"
+     *     }
+     * </pre>
+     * to validate that it contains the key <code>version</code>, use
+     * <pre>
+     *     given()
+     *        .when()
+     *          .get("/")
+     *        .then()
+     *          .body(".", containsKeys("version"));
+     * </pre>
+     * If any of the keys are missing from the JSON document, the matcher will fail.
+     * @param keys the keys that need to be present
+     * @return matcher
+     */
+
+    public static ResponseAwareMatcher<Response> containsKeys(String... keys) {
         return new KeysPresentMatcher(keys);
     }
 
@@ -72,13 +99,13 @@ public class BaseRestTestHelper {
      * @param relativeFileName the name of the file relative to the caller's class.
      * @return the bytes in that file.
      */
-    protected byte[] jsonResource(String relativeFileName) {
+    protected String jsonResource(String relativeFileName) {
         final URL resource = Resources.getResource(this.getClass(), relativeFileName);
         if (resource == null) {
             throw new IllegalStateException("Unable to find JSON resource " + relativeFileName + " for test. This is a bug.");
         }
         try {
-            return Resources.toByteArray(resource);
+            return Resources.toString(resource, Charset.defaultCharset());
         } catch (IOException e) {
             throw new IllegalStateException("Unable to read JSON resource " + relativeFileName + " for test. This is a bug.");
         }
@@ -88,7 +115,7 @@ public class BaseRestTestHelper {
      * Same as @{link #jsonResource} but guesses the file name from the caller's method name. Be careful when refactoring.
      * @return the bytes in that file
      */
-    protected byte[] jsonResourceForMethod() {
+    protected String jsonResourceForMethod() {
         final StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
         final String testMethodName = stackTraceElements[2].getMethodName();
 
@@ -100,6 +127,43 @@ public class BaseRestTestHelper {
     private static class KeysPresentMatcher extends ResponseAwareMatcher<Response> {
         private final Set<String> keys = Sets.newHashSet();
         public KeysPresentMatcher(String... keys) {
+            Collections.addAll(this.keys, keys);
+        }
+
+        @Override
+        public Matcher<?> matcher(Response response) throws Exception {
+            return new BaseMatcher<Response>() {
+
+                private Sets.SetView difference;
+
+                @Override
+                public boolean matches(Object item) {
+                    if (item instanceof Map) {
+                        final Set keySet = ((Map) item).keySet();
+                        difference = Sets.difference(keys, keySet);
+                        return difference.isEmpty();
+                    }
+
+                    return false;
+                }
+
+                @Override
+                public void describeTo(Description description) {
+                    description.appendText("JSON contains keys: ").appendValueList("[", ", ", "]", keys);
+                }
+
+                @Override
+                public void describeMismatch(Object item, Description description) {
+                    super.describeMismatch(item, description);
+                    description.appendValueList(" has extra or missing keys: [", ", ", "]", difference);
+                }
+            };
+        }
+    }
+
+    private static class StrictKeysPresentMatcher extends ResponseAwareMatcher<Response> {
+        private final Set<String> keys = Sets.newHashSet();
+        public StrictKeysPresentMatcher(String... keys) {
             Collections.addAll(this.keys, keys);
         }
 
