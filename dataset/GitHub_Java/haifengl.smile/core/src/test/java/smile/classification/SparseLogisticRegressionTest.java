67b295acbef53c6359a18be1420a056428188989
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:20:07 +0200

    add SparseLogisticRegression

--- dataset/GitHub_Java/haifengl.smile/core/src/test/java/smile/classification/SparseLogisticRegressionTest.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/test/java/smile/classification/SparseLogisticRegressionTest.java
@@ -1,4 +1,4 @@
-/*
+/*******************************************************************************
  * Copyright (c) 2010-2020 Haifeng Li. All rights reserved.
  *
  * Smile is free software: you can redistribute it and/or modify
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
- */
+ ******************************************************************************/
 
 package smile.classification;
 
@@ -25,7 +25,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import smile.data.*;
 import smile.util.SparseArray;
-import smile.validation.metric.Error;
+import smile.validation.Error;
 import static org.junit.Assert.*;
 
 /**
