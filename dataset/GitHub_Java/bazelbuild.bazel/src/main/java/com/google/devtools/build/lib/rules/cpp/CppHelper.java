c972cf3387d6b2aa2af18e8d5f46387d421160d5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 23:36:11 +0200

    RELNOTES: Make PIC and non PIC outputs for C++ compilation with Tree Artifacts
        PiperOrigin-RevId: 181330907

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppHelper.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppHelper.java
@@ -616,8 +616,9 @@ public class CppHelper {
 
   private static Artifact getIncludesOutput(RuleContext ruleContext, Artifact src) {
     Preconditions.checkArgument(!src.isSourceArtifact(), src);
-    return ruleContext.getGenfilesArtifact(
-        src.getRootRelativePath().replaceName(src.getFilename() + GREPPED_INCLUDES_SUFFIX));
+    return ruleContext.getShareableArtifact(
+        src.getRootRelativePath().replaceName(src.getFilename() + GREPPED_INCLUDES_SUFFIX),
+        src.getRoot());
   }
 
   /**
