91982def68b110abcfaaeaab759d35228f77a036
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:39:46 +0200

    refactor(scheduler): remove redundant "org.quartz.threadPool.threadPriority" initialization

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/scheduler/runtime/src/main/java/io/quarkus/scheduler/runtime/QuartzScheduler.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/scheduler/runtime/src/main/java/io/quarkus/scheduler/runtime/QuartzScheduler.java
@@ -110,10 +110,6 @@ public class QuartzScheduler implements Scheduler {
     }
 
     void start(@Observes StartupEvent startupEvent) {
-        if (schedulerConfig.getSchedules().isEmpty()) {
-            LOGGER.warn("No @Scheduled methods found, scheduler will not be started.");
-            return;
-        }
         if (running.compareAndSet(false, true)) {
 
             try {
