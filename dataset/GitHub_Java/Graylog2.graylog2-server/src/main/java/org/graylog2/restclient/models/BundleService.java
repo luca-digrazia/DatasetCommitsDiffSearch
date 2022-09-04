a010ecce4c82ffaebd8c2e27baeb3c7a2ab22a5d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:48:13 +0200

    add bundle list methode

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/restclient/models/BundleService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/restclient/models/BundleService.java
@@ -1,11 +1,11 @@
 package org.graylog2.restclient.models;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import org.graylog2.restclient.lib.APIException;
 import org.graylog2.restclient.lib.ApiClient;
 import org.graylog2.restclient.models.api.requests.CreateBundleRequest;
+import org.graylog2.restclient.models.api.responses.bundles.BundleListResponse;
 import org.graylog2.restroutes.generated.BundleResource;
 import org.graylog2.restroutes.generated.routes;
 import org.slf4j.Logger;
@@ -13,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import play.mvc.Http;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class BundleService {
     private static final Logger log = LoggerFactory.getLogger(BundleService.class);
@@ -25,16 +27,16 @@ public class BundleService {
         this.api = api;
     }
 
-    public Multimap<String, ConfigurationBundle> all() {
+    public List<Map<String, String>> all() {
+        List<Map<String, String>> bundles = Lists.newArrayList();
         try {
-            Multimap<String, ConfigurationBundle> response = api.path(routes.BundleResource().listBundles(), Multimap.class).execute();
-            return response;
-        } catch (APIException e) {
-            log.error("Unable to get bundle list from server", e);
+            BundleListResponse response = api.path(routes.BundleResource().listBundles(), BundleListResponse.class).execute();
+        }catch (APIException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
-            log.error("Unable to read from server socket", e);
+            throw new RuntimeException(e);
         }
-        return ArrayListMultimap.create();
+        return bundles;
     }
 
     public boolean create(CreateBundleRequest request) {
