93026fef88e52ab4fc101c59bb69971113c1b998
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:51:36 +0200

    Added support for cache and flush modes.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-hibernate/src/main/java/com/yammer/dropwizard/hibernate/Transactional.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-hibernate/src/main/java/com/yammer/dropwizard/hibernate/Transactional.java
@@ -1,5 +1,8 @@
 package com.yammer.dropwizard.hibernate;
 
+import org.hibernate.CacheMode;
+import org.hibernate.FlushMode;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -7,9 +10,38 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * When annotating a Jersey resource method, wraps the method in a Hibernate session and
+ * transaction. If the resource method returns without throwing an exception, the transaction is
+ * committed and the session is closed. If the resource method throws an exception, the transaction
+ * is rolled back and the session is closed.
+ *
+ * @see TransactionalRequestDispatcher
+ */
 @Target(METHOD)
 @Retention(RUNTIME)
 @Documented
 public @interface Transactional {
+    /**
+     * If {@code true}, the Hibernate session will default to loading read-only entities.
+     *
+     * @see org.hibernate.Session#setDefaultReadOnly(boolean)
+     */
+    boolean readOnly() default false;
 
+    /**
+     * The {@link CacheMode} for the session.
+     *
+     * @see CacheMode
+     * @see org.hibernate.Session#setCacheMode(CacheMode)
+     */
+    CacheMode cacheMode() default CacheMode.NORMAL;
+
+    /**
+     * The {@link FlushMode} for the session.
+     *
+     * @see FlushMode
+     * @see org.hibernate.Session#setFlushMode(org.hibernate.FlushMode)
+     */
+    FlushMode flushMode() default FlushMode.AUTO;
 }
