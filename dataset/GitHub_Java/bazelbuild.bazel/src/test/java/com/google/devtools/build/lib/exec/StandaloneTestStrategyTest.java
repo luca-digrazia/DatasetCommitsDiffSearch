c9120904ecbce335226cd50f78621c3840ae9f79
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:54:12 +0200

    Add test for TestStrategy.getTmpDirName
        To make sure the length of tmp dir name doesn't change unexpected.
    
        RELNOTES: None
        PiperOrigin-RevId: 278599465

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/exec/StandaloneTestStrategyTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/exec/StandaloneTestStrategyTest.java
@@ -95,19 +95,18 @@ public final class StandaloneTestStrategyTest extends BuildViewTestCase {
     public FakeActionExecutionContext(
         FileOutErr fileOutErr, SpawnActionContext spawnActionContext) {
       super(
-          /*executor=*/ null,
-          /*actionInputFileCache=*/ null,
+          /* executor= */ null,
+          /* actionInputFileCache= */ null,
           ActionInputPrefetcher.NONE,
           new ActionKeyContext(),
-          /*metadataHandler=*/ null,
-          LostInputsCheck.NONE,
+          /* metadataHandler= */ null,
           fileOutErr,
           /*eventHandler=*/ null,
-          /*clientEnv=*/ ImmutableMap.of(),
-          /*topLevelFilesets=*/ ImmutableMap.of(),
-          /*artifactExpander=*/ null,
-          /*actionFileSystem=*/ null,
-          /*skyframeDepsResult=*/ null);
+          /* clientEnv= */ ImmutableMap.of(),
+          /* topLevelFilesets= */ ImmutableMap.of(),
+          /* artifactExpander= */ null,
+          /* actionFileSystem= */ null,
+          /* skyframeDepsResult= */ null);
       this.spawnActionContext = spawnActionContext;
     }
 
