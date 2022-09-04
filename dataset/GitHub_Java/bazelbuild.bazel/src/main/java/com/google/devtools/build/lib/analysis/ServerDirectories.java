16902f438c2d0091d78be557a566aa1df5a8f7f1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 23:08:10 +0200

    Remove a misleading comment.     installBase is also used to access the embedded binaries.
        RELNOTES: None
        PiperOrigin-RevId: 187422136

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/ServerDirectories.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/ServerDirectories.java
@@ -14,61 +14,51 @@
 
 package com.google.devtools.build.lib.analysis;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
+import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
 import com.google.devtools.build.lib.vfs.Path;
 import java.util.Objects;
 import javax.annotation.Nullable;
 
 /**
- * Represents the relevant directories for the server: the location of the embedded binaries and the
- * output directories.
+ * Represents the relevant directories for the server: the location of the embedded binaries
+ * and the output directories.
  */
+@AutoCodec
 @Immutable
 public final class ServerDirectories {
-  public static final String EXECROOT = "execroot";
-
-  /** Where Bazel gets unpacked. */
+  /** Top-level user output directory; used, e.g., as default location for caches. */
+  private final Path outputUserRoot;
+  /** Where Blaze gets unpacked. */
   private final Path installBase;
   /** The content hash of everything in installBase. */
   @Nullable private final HashCode installMD5;
-
   /** The root of the temp and output trees. */
   private final Path outputBase;
 
-  /** Top-level user output directory; used, e.g., as default location for caches. */
-  private final Path outputUserRoot;
-
-  private final Path execRootBase;
-
-  // TODO(bazel-team): Use a builder to simplify/unify these constructors. This makes it easier to
-  // have sensible defaults, e.g. execRootBase = outputBase + "/execroot". Then reorder the fields
-  // to be consistent throughout this class.
-
   public ServerDirectories(
-      Path installBase,
-      Path outputBase,
-      Path outputUserRoot,
-      Path execRootBase,
-      @Nullable String installMD5) {
-    this.installBase = installBase;
-    this.installMD5 =
-        Strings.isNullOrEmpty(installMD5) ? null : checkMD5(HashCode.fromString(installMD5));
-    this.outputBase = outputBase;
+      Path installBase, Path outputBase, Path outputUserRoot, @Nullable String installMD5) {
+    this(
+        installBase,
+        outputBase,
+        outputUserRoot,
+        Strings.isNullOrEmpty(installMD5) ? null : checkMD5(HashCode.fromString(installMD5)));
+  }
+
+  @AutoCodec.Instantiator
+  ServerDirectories(Path installBase, Path outputBase, Path outputUserRoot, HashCode installMD5) {
     this.outputUserRoot = outputUserRoot;
-    this.execRootBase = execRootBase;
+    this.installBase = installBase;
+    this.outputBase = outputBase;
+    this.installMD5 = installMD5;
   }
 
   public ServerDirectories(Path installBase, Path outputBase, Path outputUserRoot) {
-    this(
-        // Some tests set installBase to null.
-        // TODO(bazel-team): Be more consistent about whether nulls are permitted. (e.g. equals()
-        // presently doesn't tolerate them for some fields). We should probably just disallow them.
-        installBase, outputBase, outputUserRoot, outputBase.getRelative(EXECROOT), null);
+    this(installBase, outputBase, outputUserRoot, (HashCode) null);
   }
 
   private static HashCode checkMD5(HashCode hash) {
@@ -80,14 +70,6 @@ public final class ServerDirectories {
   /** Returns the installation base directory. */
   public Path getInstallBase() {
     return installBase;
-  }
-
-  /**
-   * Returns the MD5 content hash of the blaze binary (includes deploy JAR, embedded binaries, and
-   * anything else that ends up in the install_base).
-   */
-  public HashCode getInstallMD5() {
-    return installMD5;
   }
 
   /**
@@ -105,28 +87,22 @@ public final class ServerDirectories {
     return outputUserRoot;
   }
 
-  /**
-   * Parent of all execution roots.
-   *
-   * <p>This is physically, always /outputbase/execroot, but might be virtualized.
-   */
-  public Path getExecRootBase() {
-    return execRootBase;
-  }
-
   /** Returns the installed embedded binaries directory, under the shared installBase location. */
   public Path getEmbeddedBinariesRoot() {
-    return getEmbeddedBinariesRoot(installBase);
+    return installBase.getChild("_embedded_binaries");
   }
 
-  @VisibleForTesting
-  public static Path getEmbeddedBinariesRoot(Path installBase) {
-    return installBase;
+  /**
+   * Returns the MD5 content hash of the blaze binary (includes deploy JAR, embedded binaries, and
+   * anything else that ends up in the install_base).
+   */
+  public HashCode getInstallMD5() {
+    return installMD5;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(installBase, installMD5, outputBase, outputUserRoot, execRootBase);
+    return Objects.hash(installBase, installMD5, outputBase, outputUserRoot);
   }
 
   @Override
@@ -141,7 +117,6 @@ public final class ServerDirectories {
     return this.installBase.equals(that.installBase)
         && Objects.equals(this.installMD5, that.installMD5)
         && this.outputBase.equals(that.outputBase)
-        && this.outputUserRoot.equals(that.outputUserRoot)
-        && this.execRootBase.equals(that.execRootBase);
+        && this.outputUserRoot.equals(that.outputUserRoot);
   }
 }
