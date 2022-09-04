7878ad3f56443a855e0f51a96264a6a395e66b68
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 00:52:15 +0200

    Remove extra commented-out line.
        PiperOrigin-RevId: 161414631

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/platform/ToolchainRule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/platform/ToolchainRule.java
@@ -58,7 +58,7 @@ public class ToolchainRule implements RuleDefinition {
         <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
         .add(
             attr(EXEC_COMPATIBLE_WITH_ATTR, BuildType.LABEL_LIST)
-                .mandatoryProviders(ConstraintValueInfo.PROVIDER.id())
+                .mandatoryProviders(ConstraintValueInfo.SKYLARK_CONSTRUCTOR.id())
                 .allowedFileTypes()
                 .nonconfigurable("part of toolchain configuration"))
         /* <!-- #BLAZE_RULE(toolchain).ATTRIBUTE(target_compatible_with) -->
@@ -67,7 +67,7 @@ public class ToolchainRule implements RuleDefinition {
         <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
         .add(
             attr(TARGET_COMPATIBLE_WITH_ATTR, BuildType.LABEL_LIST)
-                .mandatoryProviders(ConstraintValueInfo.PROVIDER.id())
+                .mandatoryProviders(ConstraintValueInfo.SKYLARK_CONSTRUCTOR.id())
                 .allowedFileTypes()
                 .nonconfigurable("part of toolchain configuration"))
         /* <!-- #BLAZE_RULE(toolchain).ATTRIBUTE(toolchain) -->
