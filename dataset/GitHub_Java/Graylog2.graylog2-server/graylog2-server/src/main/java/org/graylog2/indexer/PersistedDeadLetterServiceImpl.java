d3618679e0b8934b959147f92dacf5b86adff2f5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:08:07 +0200

    Moving field names to class constants.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/PersistedDeadLetterServiceImpl.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/PersistedDeadLetterServiceImpl.java
@@ -1,27 +1,31 @@
-/*
- * Copyright 2012-2014 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.graylog2.indexer;
 
-import com.google.inject.Inject;
+import javax.inject.Inject;
+
+import com.google.common.collect.Maps;
+import com.mongodb.BasicDBObject;
+import org.bson.types.ObjectId;
 import org.graylog2.database.MongoConnection;
 import org.graylog2.database.PersistedServiceImpl;
+import org.joda.time.DateTime;
+
+import java.util.Map;
 
 /**
  * @author Dennis Oelkers <dennis@torch.sh>
@@ -30,5 +34,25 @@ public class PersistedDeadLetterServiceImpl extends PersistedServiceImpl impleme
     @Inject
     public PersistedDeadLetterServiceImpl(MongoConnection mongoConnection) {
         super(mongoConnection);
+    }
+
+    @Override
+    public PersistedDeadLetter create(String letterId, DateTime timestamp, Map<String, Object> message) {
+        return create(new ObjectId().toHexString(), letterId, timestamp, message);
+    }
+
+    @Override
+    public PersistedDeadLetter create(String id, String letterId, DateTime timestamp, Map<String, Object> message) {
+        Map<String, Object> doc = Maps.newHashMap();
+        doc.put(PersistedDeadLetterImpl.LETTERID, letterId);
+        doc.put(PersistedDeadLetterImpl.TIMESTAMP, timestamp);
+        doc.put(PersistedDeadLetterImpl.MESSAGE, message);
+
+        return new PersistedDeadLetterImpl(new ObjectId(id), doc);
+    }
+
+    @Override
+    public long count() {
+        return count(PersistedDeadLetterImpl.class, new BasicDBObject());
     }
 }
