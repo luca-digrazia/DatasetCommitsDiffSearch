0e0e3fc2debf9825f19ba45e7ce2fdb4edf46d65
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 23:06:52 +0200

    Fix stray ')' in RecursivePkgKey toString
        RELNOTES: None.
        PiperOrigin-RevId: 187671277

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/RecursivePkgValue.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/RecursivePkgValue.java
@@ -13,20 +13,22 @@
 // limitations under the License.
 package com.google.devtools.build.lib.skyframe;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Interner;
 import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.collect.nestedset.Order;
-import com.google.devtools.build.lib.concurrent.BlazeInterners;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
 import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.build.lib.vfs.RootedPath;
-import com.google.devtools.build.skyframe.SkyFunctionName;
+import com.google.devtools.build.skyframe.LegacySkyKey;
+import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.SkyValue;
+import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * This value represents the result of looking up all the packages under a given package path root,
@@ -37,64 +39,96 @@ import com.google.devtools.build.skyframe.SkyValue;
 public class RecursivePkgValue implements SkyValue {
   @AutoCodec
   static final RecursivePkgValue EMPTY =
-      new RecursivePkgValue(NestedSetBuilder.<String>emptySet(Order.STABLE_ORDER), false);
+      new RecursivePkgValue(NestedSetBuilder.<String>emptySet(Order.STABLE_ORDER));
 
   private final NestedSet<String> packages;
-  private final boolean hasErrors;
 
-  private RecursivePkgValue(NestedSet<String> packages, boolean hasErrors) {
+  private RecursivePkgValue(NestedSet<String> packages) {
     this.packages = packages;
-    this.hasErrors = hasErrors;
   }
 
-  static RecursivePkgValue create(NestedSetBuilder<String> packages, boolean hasErrors) {
-    if (packages.isEmpty() && !hasErrors) {
+  static RecursivePkgValue create(NestedSetBuilder<String> packages) {
+    if (packages.isEmpty()) {
       return EMPTY;
     }
-    return new RecursivePkgValue(packages.build(), hasErrors);
+    return new RecursivePkgValue(packages.build());
   }
 
-  /** Create a transitive package lookup request. */
+  /**
+   * Create a transitive package lookup request.
+   */
   @ThreadSafe
-  public static Key key(
-      RepositoryName repositoryName,
-      RootedPath rootedPath,
+  public static SkyKey key(RepositoryName repositoryName, RootedPath rootedPath,
       ImmutableSet<PathFragment> excludedPaths) {
-    return Key.create(repositoryName, rootedPath, excludedPaths);
+    return LegacySkyKey.create(
+        SkyFunctions.RECURSIVE_PKG, new RecursivePkgKey(repositoryName, rootedPath, excludedPaths));
   }
 
   public NestedSet<String> getPackages() {
     return packages;
   }
 
-  public boolean hasErrors() {
-    return hasErrors;
-  }
-
-  @AutoCodec.VisibleForSerialization
+  /**
+   * A RecursivePkgKey is a tuple of a {@link RootedPath}, {@code rootedPath}, defining the
+   * directory to recurse beneath in search of packages, and an {@link ImmutableSet} of {@link
+   * PathFragment}s, {@code excludedPaths}, relative to {@code rootedPath.getRoot}, defining the set
+   * of subdirectories strictly beneath {@code rootedPath} to skip.
+   *
+   * <p>Throws {@link IllegalArgumentException} if {@code excludedPaths} contains any paths that are
+   * equal to {@code rootedPath} or that are not beneath {@code rootedPath}.
+   */
   @AutoCodec
-  static class Key extends RecursivePkgSkyKey {
-    private static final Interner<Key> interner = BlazeInterners.newWeakInterner();
+  @ThreadSafe
+  public static final class RecursivePkgKey implements Serializable {
+    private final RepositoryName repositoryName;
+    private final RootedPath rootedPath;
+    private final ImmutableSet<PathFragment> excludedPaths;
 
-    private Key(
+    @AutoCodec.Instantiator
+    public RecursivePkgKey(
         RepositoryName repositoryName,
         RootedPath rootedPath,
         ImmutableSet<PathFragment> excludedPaths) {
-      super(repositoryName, rootedPath, excludedPaths);
+      PathFragment.checkAllPathsAreUnder(excludedPaths, rootedPath.getRootRelativePath());
+      Preconditions.checkState(!repositoryName.isDefault());
+      this.repositoryName = repositoryName;
+      this.rootedPath = Preconditions.checkNotNull(rootedPath);
+      this.excludedPaths = Preconditions.checkNotNull(excludedPaths);
     }
 
-    @AutoCodec.VisibleForSerialization
-    @AutoCodec.Instantiator
-    static Key create(
-        RepositoryName repositoryName,
-        RootedPath rootedPath,
-        ImmutableSet<PathFragment> excludedPaths) {
-      return interner.intern(new Key(repositoryName, rootedPath, excludedPaths));
+    public RepositoryName getRepository() {
+      return repositoryName;
+    }
+
+    public RootedPath getRootedPath() {
+      return rootedPath;
+    }
+
+    public ImmutableSet<PathFragment> getExcludedPaths() {
+      return excludedPaths;
     }
 
     @Override
-    public SkyFunctionName functionName() {
-      return SkyFunctions.RECURSIVE_PKG;
+    public String toString() {
+      return "rootedPath=" + rootedPath + ", excludedPaths=<omitted>";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (!(o instanceof RecursivePkgKey)) {
+        return false;
+      }
+
+      RecursivePkgKey that = (RecursivePkgKey) o;
+      return excludedPaths.equals(that.excludedPaths) && rootedPath.equals(that.rootedPath);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(rootedPath, excludedPaths);
     }
   }
 }
