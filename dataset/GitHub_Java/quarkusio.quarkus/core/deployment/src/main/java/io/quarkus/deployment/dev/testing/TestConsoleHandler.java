c608d974682776256a3490389845d095b635c335
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:05:19 +0200

    Print the running test when displaying output

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/dev/testing/TestConsoleHandler.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/dev/testing/TestConsoleHandler.java
@@ -1,9 +1,9 @@
 package io.quarkus.deployment.dev.testing;
 
-import static io.quarkus.deployment.dev.testing.TestConsoleHandler.MessageFormat.BLUE;
 import static io.quarkus.deployment.dev.testing.TestConsoleHandler.MessageFormat.GREEN;
 import static io.quarkus.deployment.dev.testing.TestConsoleHandler.MessageFormat.RED;
 import static io.quarkus.deployment.dev.testing.TestConsoleHandler.MessageFormat.RESET;
+import static io.quarkus.deployment.dev.testing.TestConsoleHandler.MessageFormat.YELLOW;
 import static io.quarkus.deployment.dev.testing.TestConsoleHandler.MessageFormat.helpOption;
 import static io.quarkus.deployment.dev.testing.TestConsoleHandler.MessageFormat.statusFooter;
 import static io.quarkus.deployment.dev.testing.TestConsoleHandler.MessageFormat.statusHeader;
@@ -34,12 +34,10 @@ public class TestConsoleHandler implements TestListener {
 
     private static final Logger log = Logger.getLogger("io.quarkus.test");
 
-    public static final String PAUSED_PROMPT = "Tests paused, press [" + BLUE + "r" + RESET + "] to resume, [" + BLUE + "h"
-            + RESET + "] for more options>" + RESET;
-    public static final String FIRST_RUN_PROMPT = BLUE + "Running Tests for the first time" + RESET;
-    public static final String RUNNING_PROMPT = "Press [" + BLUE + "r" + RESET + "] to re-run, [" + BLUE
-            + "v" + RESET + "] to view full results, [" + BLUE + "p" + RESET + "] to pause, [" + BLUE
-            + "h" + RESET + "] for more options>";
+    public static final String PAUSED_PROMPT = YELLOW + "Tests paused, press [r] to resume, [h] for more options>" + RESET;
+    public static final String FIRST_RUN_PROMPT = YELLOW + "Running Tests for the first time" + RESET;
+    public static final String RUNNING_PROMPT = "Press [r] to re-run, [v] to view full results, [p] to pause, [h] for more options>";
+    public static final String ABORTED_PROMPT = "Test run aborted.";
 
     final DevModeType devModeType;
 
@@ -48,7 +46,7 @@ public class TestConsoleHandler implements TestListener {
     boolean currentlyFailing = false;
     volatile InputHandler.ConsoleStatus promptHandler;
     volatile TestController testController;
-    private String lastResults;
+    private String lastStatus;
 
     public TestConsoleHandler(DevModeType devModeType) {
         this.devModeType = devModeType;
@@ -95,7 +93,7 @@ public class TestConsoleHandler implements TestListener {
                 } else {
                     if (disabled) {
                         if (k == 'r') {
-                            promptHandler.setStatus(BLUE + "Starting tests" + RESET);
+                            promptHandler.setStatus(YELLOW + "Starting tests" + RESET);
                             TestSupport.instance().get().start();
                         }
                     } else if (!firstRun) {
@@ -160,12 +158,10 @@ public class TestConsoleHandler implements TestListener {
         disabled = false;
         if (firstRun) {
             promptHandler.setStatus(null);
-            promptHandler.setResults(null);
             promptHandler.setPrompt(FIRST_RUN_PROMPT);
         } else {
             promptHandler.setPrompt(RUNNING_PROMPT);
-            promptHandler.setResults(lastResults);
-            promptHandler.setStatus(null);
+            promptHandler.setStatus(lastStatus);
         }
     }
 
@@ -174,17 +170,11 @@ public class TestConsoleHandler implements TestListener {
         disabled = true;
         promptHandler.setPrompt(PAUSED_PROMPT);
         promptHandler.setStatus(null);
-        promptHandler.setResults(null);
     }
 
     @Override
     public void testCompileFailed(String message) {
-        promptHandler.setCompileError(message);
-    }
-
-    @Override
-    public void testCompileSucceeded() {
-        promptHandler.setCompileError(null);
+        promptHandler.setStatus(message);
     }
 
     @Override
@@ -236,15 +226,15 @@ public class TestConsoleHandler implements TestListener {
                     end = end + ".";
                 }
                 if (results.getTotalCount() == 0) {
-                    lastResults = BLUE + "No tests found" + RESET;
+                    lastStatus = YELLOW + "No tests found" + RESET;
                 } else if (results.getFailedCount() == 0 && results.getPassedCount() == 0) {
-                    lastResults = String.format(BLUE + "All %d tests were skipped" + RESET, results.getSkippedCount());
+                    lastStatus = String.format(YELLOW + "All %d tests were skipped" + RESET, results.getSkippedCount());
                 } else if (results.getCurrentFailing().isEmpty()) {
                     if (currentlyFailing) {
                         log.info(GREEN + "All tests are now passing" + RESET);
                     }
                     currentlyFailing = false;
-                    lastResults = String.format(
+                    lastStatus = String.format(
                             GREEN + "All %d tests are passing (%d skipped), %d tests were run in %dms." + end + RESET,
                             results.getPassedCount(),
                             results.getSkippedCount(),
@@ -262,18 +252,15 @@ public class TestConsoleHandler implements TestListener {
                     }
                     log.error(
                             statusFooter(RED + results.getCurrentFailedCount() + " TESTS FAILED"));
-                    lastResults = String.format(
-                            RED + "%d tests failed" + RESET + " (" + GREEN + "%d passing" + RESET + ", " + BLUE + "%d skipped"
-                                    + RESET + ")" + RED + ", %d tests were run in %dms." + end + RESET,
+                    lastStatus = String.format(
+                            RED + "%d tests failed" + RESET + " (" + GREEN + "%d passing" + RESET + ", " + YELLOW + "%d skipped"
+                                    + RESET + "), %d tests were run in %dms." + end + RESET,
                             results.getCurrentFailedCount(), results.getPassedCount(), results.getSkippedCount(),
                             results.getCurrentTotalCount(), results.getTotalTime());
                 }
                 //this will re-print when using the basic console
-                if (!disabled) {
-                    promptHandler.setPrompt(RUNNING_PROMPT);
-                    promptHandler.setResults(lastResults);
-                    promptHandler.setStatus(null);
-                }
+                promptHandler.setPrompt(RUNNING_PROMPT);
+                promptHandler.setStatus(lastStatus);
             }
 
             @Override
@@ -283,6 +270,8 @@ public class TestConsoleHandler implements TestListener {
 
             @Override
             public void runAborted() {
+                promptHandler.setStatus(ABORTED_PROMPT);
+                promptHandler.setPrompt(RUNNING_PROMPT);
                 firstRun = false;
             }
 
@@ -307,7 +296,7 @@ public class TestConsoleHandler implements TestListener {
 
         public static final String RED = "\u001B[91m";
         public static final String GREEN = "\u001b[32m";
-        public static final String BLUE = "\u001b[34m";
+        public static final String YELLOW = "\u001b[33m";
         public static final String RESET = "\u001b[0m";
 
         private MessageFormat() {
@@ -326,7 +315,7 @@ public class TestConsoleHandler implements TestListener {
         }
 
         public static String helpOption(String key, String description) {
-            return "[" + BLUE + key + RESET + "] - " + description;
+            return "[" + GREEN + key + RESET + "] - " + description;
         }
 
         public static String helpOption(String key, String description, boolean enabled) {
