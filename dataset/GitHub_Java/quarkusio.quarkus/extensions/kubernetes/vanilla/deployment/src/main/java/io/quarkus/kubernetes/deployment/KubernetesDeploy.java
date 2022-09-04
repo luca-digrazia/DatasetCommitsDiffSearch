c17e92d1e010316d6ae4d67d9ba7cfaa766e82e0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:16:12 +0200

    Provide a proper error message when a k8s token is invalid
        The k8s client returns null when the token is invalid which
        resulted in a NPE being thrown

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesDeploy.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesDeploy.java
@@ -67,6 +67,7 @@ public class KubernetesDeploy {
         KubernetesClient client = KubernetesClientUtils.createClient();
         String masterURL = client.getConfiguration().getMasterUrl();
         try {
+            masterURL = client.getConfiguration().getMasterUrl();
             //Let's check if we can connect.
             VersionInfo version = client.getVersion();
             if (version == null) {
@@ -78,6 +79,7 @@ public class KubernetesDeploy {
             log.info("Kubernetes API Server at '" + masterURL + "' successfully contacted.");
             log.debugf("Kubernetes Version: %s.%s", version.getMajor(), version.getMinor());
             serverFound = true;
+            client.close();
             return Result.enabled();
         } catch (Exception e) {
             if (e.getCause() instanceof SSLHandshakeException) {
@@ -92,8 +94,6 @@ public class KubernetesDeploy {
                                 + masterURL + "'",
                         e));
             }
-        } finally {
-            client.close();
         }
     }
 
