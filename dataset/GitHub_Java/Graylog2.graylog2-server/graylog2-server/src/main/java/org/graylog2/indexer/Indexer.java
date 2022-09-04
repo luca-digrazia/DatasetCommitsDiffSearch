d310aca98e3c3b0b23dc45192c6e9727dc62a9b1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:17:44 +0200

    improve error message for common elasticsearch discovery problems
        guess default HTTP endpoint for elasticsearch and try to read config
    
        * compare cluster.name
        * check compatible version
        * warn if no connection could be made
    
        fixes #506

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/Indexer.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/Indexer.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012-2014 TORCH GmbH
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.graylog2.indexer;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -7,7 +26,6 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.ListenableFuture;
 import com.ning.http.client.Response;
 import org.apache.commons.io.FileUtils;
@@ -29,7 +47,6 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.graylog2.Configuration;
-import org.graylog2.Core;
 import org.graylog2.UI;
 import org.graylog2.indexer.cluster.Cluster;
 import org.graylog2.indexer.counts.Counts;
@@ -37,7 +54,6 @@ import org.graylog2.indexer.indices.Indices;
 import org.graylog2.indexer.messages.Messages;
 import org.graylog2.indexer.searches.Searches;
 import org.graylog2.plugin.Message;
-import org.graylog2.plugin.indexer.MessageGateway;
 import org.joda.time.Period;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,11 +74,15 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 public class Indexer {
     private static final Logger LOG = LoggerFactory.getLogger(Indexer.class);
 
+    private final Configuration configuration;
+    private final Searches.Factory searchesFactory;
+    private final Counts.Factory countsFactory;
+    private final Cluster.Factory clusterFactory;
+    private final Indices.Factory indicesFactory;
     private final AsyncHttpClient httpClient;
 
     private Client client;
     private Node node;
-    private MessageGateway messageGateway;
     public static final String TYPE = "message";
     
     private Searches searches;
@@ -72,8 +92,6 @@ public class Indexer {
     private Indices indices;
 
     private LinkedBlockingQueue<List<DeadLetter>> deadLetterQueue;
-
-    private Core server;
 
     public static enum DateHistogramInterval {
         YEAR(Period.years(1)),
@@ -95,17 +113,24 @@ public class Indexer {
         }
     }
 
-    public Indexer(Core graylogServer) {
-        this.server = graylogServer;
+    public Indexer(Configuration configuration,
+                   Searches.Factory searchesFactory,
+                   Counts.Factory countsFactory,
+                   Cluster.Factory clusterFactory,
+                   Indices.Factory indicesFactory,
+                   AsyncHttpClient httpClient) {
+        this.configuration = configuration;
+        this.searchesFactory = searchesFactory;
+        this.countsFactory = countsFactory;
+        this.clusterFactory = clusterFactory;
+        this.indicesFactory = indicesFactory;
+        this.httpClient = httpClient;
         this.deadLetterQueue = new LinkedBlockingQueue<List<DeadLetter>>(1000);
-        AsyncHttpClientConfig.Builder builder = new AsyncHttpClientConfig.Builder();
-        builder.setAllowPoolingConnection(false);
-        this.httpClient = new AsyncHttpClient(builder.build());
     }
 
     public void start() {
         final NodeBuilder builder = nodeBuilder().client(true);
-        Map<String, String> settings = readNodeSettings(server.getConfiguration());
+        Map<String, String> settings = readNodeSettings(configuration);
 
         builder.settings().put(settings);
         node = builder.node();
@@ -163,12 +188,11 @@ public class Indexer {
 
         }
 
-        messageGateway = new MessageGatewayImpl(server);
-        searches = new Searches(client, server);
-        counts = new Counts(client, server);
-        messages = new Messages(client, server);
-        cluster = new Cluster(client, server);
-        indices = new Indices(client, server);
+        searches = searchesFactory.create(client);
+        counts = countsFactory.create(client);
+        messages = new Messages(client);
+        cluster = clusterFactory.create(client);
+        indices = indicesFactory.create(client);
     }
 
     // default visibility for tests
@@ -218,10 +242,6 @@ public class Indexer {
 
     public Client getClient() {
         return client;
-    }
-
-    public MessageGateway getMessageGateway() {
-        return messageGateway;
     }
 
     public String nodeIdToName(String nodeId) {
