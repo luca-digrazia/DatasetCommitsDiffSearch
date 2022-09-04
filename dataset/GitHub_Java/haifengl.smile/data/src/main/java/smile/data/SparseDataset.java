39221b4f59d00943269ab0914e775f6700145d3c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:33:51 +0200

    revise sparse data reader

--- dataset/GitHub_Java/haifengl.smile/data/src/main/java/smile/data/SparseDataset.java
+++ dataset/GitHub_Java/haifengl.smile/data/src/main/java/smile/data/SparseDataset.java
@@ -1,20 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2010-2019 Haifeng Li
+ * Copyright (c) 2010 Haifeng Li
+ *   
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Smile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * Smile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
-
 package smile.data;
 
 import java.io.IOException;
@@ -26,11 +24,9 @@ import java.util.Collection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-
 import smile.math.MathEx;
+import smile.math.SparseArray;
 import smile.math.matrix.SparseMatrix;
-import smile.util.SparseArray;
 
 /**
  * List of Lists sparse matrix format. LIL stores one list per row,
@@ -168,11 +164,6 @@ public interface SparseDataset extends Dataset<SparseArray> {
      */
     static SparseDataset of(Collection<SparseArray> data, int ncols) {
         return new SparseDatasetImpl(data, ncols);
-    }
-
-    /** Strips the response variable and returns a SparseDataset. */
-    static SparseDataset of(Dataset<Instance<SparseArray>> data) {
-        return of(data.stream().map(i -> i.x()).collect(Collectors.toList()));
     }
 
     /**
