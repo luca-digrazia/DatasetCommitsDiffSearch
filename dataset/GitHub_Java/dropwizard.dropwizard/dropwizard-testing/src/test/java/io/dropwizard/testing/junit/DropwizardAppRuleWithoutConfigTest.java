34d13001e61da2350a34fe145e427a9ccf14c512
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:23:49 +0200

    Reenable test

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/test/java/io/dropwizard/testing/junit/DropwizardAppRuleWithoutConfigTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/test/java/io/dropwizard/testing/junit/DropwizardAppRuleWithoutConfigTest.java
@@ -4,35 +4,31 @@ import com.google.common.collect.ImmutableMap;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
-import io.dropwizard.testing.ConfigOverride;
-import org.junit.ClassRule;
-import org.junit.Test;
-
+import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericType;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
 
 public class DropwizardAppRuleWithoutConfigTest {
 
     @ClassRule
-    public static final DropwizardAppRule<Configuration> RULE = new DropwizardAppRule<>(TestApplication.class, null,
-        ConfigOverride.config("server.applicationConnectors[0].port", "0"),
-        ConfigOverride.config("server.adminConnectors[0].port", "0"));
+    public static final DropwizardAppRule<Configuration> RULE = new DropwizardAppRule<>(TestApplication.class);
+
+    Client client = ClientBuilder.newClient();
 
     @Test
     public void runWithoutConfigFile() {
-        Map<String, String> response = RULE.client().target("http://localhost:" + RULE.getLocalPort() + "/test")
-            .request()
-            .get(new GenericType<Map<String, String>>() {
-            });
-        assertThat(response).containsOnly(entry("color", "orange"));
+        Map<?,?> response = client.target("http://localhost:" + RULE.getLocalPort() + "/test")
+                .request()
+                .get(Map.class);
+        Assert.assertEquals(ImmutableMap.of("color", "orange"), response);
     }
 
     public static class TestApplication extends Application<Configuration> {
