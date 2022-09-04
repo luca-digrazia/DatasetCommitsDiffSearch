92b477dae998fef4c29378289868d97b8c7df61d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:24:47 +0200

    Add test to avoid stackoverflow caused by a bug in android when inflating a layout with a merge tag.

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/EViewGroupProcessor.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/EViewGroupProcessor.java
@@ -17,7 +17,6 @@ package com.googlecode.androidannotations.processing;
 
 import static com.sun.codemodel.JMod.PRIVATE;
 import static com.sun.codemodel.JMod.PUBLIC;
-import static com.sun.codemodel.JMod.STATIC;
 import static javax.lang.model.element.ElementKind.CONSTRUCTOR;
 
 import java.lang.annotation.Annotation;
@@ -44,29 +43,15 @@ import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JExpr;
+import com.sun.codemodel.JExpression;
 import com.sun.codemodel.JFieldRef;
 import com.sun.codemodel.JFieldVar;
 import com.sun.codemodel.JInvocation;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JMod;
 import com.sun.codemodel.JType;
-import com.sun.codemodel.JVar;
 
 public class EViewGroupProcessor extends AnnotationHelper implements ElementProcessor {
-
-	private static final String ALREADY_INFLATED_COMMENT = "" // +
-			+ "The mAlreadyInflated_ hack is needed because of an Android bug\n" // +
-			+ "which leads to infinite calls of onFinishInflate()\n" //
-			+ "when inflating a layout with a parent and using\n" //
-			+ "the <merge /> tag." //
-	;
-
-	private static final String SUPPRESS_WARNING_COMMENT = "" //
-			+ "We use @SuppressWarning here because our java code\n" //
-			+ "generator doesn't know that there is no need\n" //
-			+ "to import OnXXXListeners from View as we already\n" //
-			+ "are in a View." //
-	;
 
 	private final IRClass rClass;
 
@@ -99,39 +84,37 @@ public class EViewGroupProcessor extends AnnotationHelper implements ElementProc
 		}
 
 		holder.eBean = codeModel._class(modifiers, generatedBeanQualifiedName, ClassType.CLASS);
+
 		JClass eBeanClass = codeModel.directClass(eBeanQualifiedName);
 
 		holder.eBean._extends(eBeanClass);
-
-		holder.eBean.annotate(SuppressWarnings.class).param("value", "unused");
-		holder.eBean.javadoc().append(SUPPRESS_WARNING_COMMENT);
 
 		{
 			JClass contextClass = holder.refClass("android.content.Context");
 			holder.contextRef = holder.eBean.field(PRIVATE, contextClass, "context_");
 		}
-
+		
 		{
 			// init
 			holder.init = holder.eBean.method(PRIVATE, codeModel.VOID, "init_");
 			holder.init.body().assign((JFieldVar) holder.contextRef, JExpr.invoke("getContext"));
 		}
-
+		
 		{
 			// afterSetContentView
 			holder.afterSetContentView = holder.eBean.method(PRIVATE, codeModel.VOID, "afterSetContentView_");
 		}
-
+		
 		JFieldVar mAlreadyInflated_ = holder.eBean.field(PRIVATE, JType.parse(codeModel, "boolean"), "mAlreadyInflated_", JExpr.FALSE);
 
 		// onFinishInflate
 		JMethod onFinishInflate = holder.eBean.method(PUBLIC, codeModel.VOID, "onFinishInflate");
 		onFinishInflate.annotate(Override.class);
-		onFinishInflate.javadoc().append(ALREADY_INFLATED_COMMENT);
 
 		JBlock ifNotInflated = onFinishInflate.body()._if(JExpr.ref("mAlreadyInflated_").not())._then();
+		ifNotInflated.directStatement("// See issue #68 for details.");
 		ifNotInflated.assign(mAlreadyInflated_, JExpr.TRUE);
-
+		
 		// inflate layout if ID is given on annotation
 		EViewGroup layoutAnnotation = element.getAnnotation(EViewGroup.class);
 		int layoutIdValue = layoutAnnotation.value();
@@ -142,12 +125,12 @@ public class EViewGroupProcessor extends AnnotationHelper implements ElementProc
 			ifNotInflated.invoke("inflate").arg(JExpr.invoke("getContext")).arg(contentViewId).arg(JExpr._this());
 		}
 		ifNotInflated.invoke(holder.afterSetContentView);
-
+		
 		// finally
 		onFinishInflate.body().invoke(JExpr._super(), "onFinishInflate");
 
-		copyConstructorsAndAddStaticBuilders(element, codeModel, holder, onFinishInflate);
-
+		copyConstructors(element, holder, onFinishInflate);
+		
 		{
 			// init if activity
 			APTCodeModelHelper helper = new APTCodeModelHelper();
@@ -157,7 +140,7 @@ public class EViewGroupProcessor extends AnnotationHelper implements ElementProc
 
 	}
 
-	private void copyConstructorsAndAddStaticBuilders(Element element, JCodeModel codeModel, EBeanHolder holder, JMethod setContentViewMethod) {
+	private void copyConstructors(Element element, EBeanHolder holder, JMethod setContentViewMethod) {
 		List<ExecutableElement> constructors = new ArrayList<ExecutableElement>();
 		for (Element e : element.getEnclosedElements()) {
 			if (e.getKind() == CONSTRUCTOR) {
@@ -167,23 +150,15 @@ public class EViewGroupProcessor extends AnnotationHelper implements ElementProc
 
 		for (ExecutableElement userConstructor : constructors) {
 			JMethod copyConstructor = holder.eBean.constructor(PUBLIC);
-			JClass superType = holder.refClass(element.asType().toString());
-			JMethod staticHelper = holder.eBean.method(PUBLIC | STATIC, superType, "build");
 			JBlock body = copyConstructor.body();
 			JInvocation superCall = body.invoke("super");
-			 JInvocation newInvocation = JExpr._new(holder.eBean);
 			for (VariableElement param : userConstructor.getParameters()) {
 				String paramName = param.getSimpleName().toString();
 				String paramType = param.asType().toString();
 				copyConstructor.param(holder.refClass(paramType), paramName);
-				staticHelper.param(holder.refClass(paramType), paramName);
 				superCall.arg(JExpr.ref(paramName));
-				newInvocation.arg(JExpr.ref(paramName));
 			}
-
-			JVar newCall = staticHelper.body().decl(superType, "instance", newInvocation);
-			staticHelper.body().invoke(newCall, "onFinishInflate");
-			staticHelper.body()._return(newCall);
+			
 			body.invoke(holder.init);
 		}
 	}
