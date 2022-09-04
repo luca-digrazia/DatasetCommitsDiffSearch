17f648fcd41574928e6207569435610492801d45
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:47:14 +0200

    Handle iquote paths in objc more like cpp
        This is to make the migration to CcCompilationContext easier.  Put the
        iquote paths in the provider and use it to propagate them, and reduce
        the amount of custom logic.
    
        "." and "genfiles" are automatically added by
        CcCompilationHelper.initializeCcCompilationContext(), so no need to do
        anything on the objc side.
    
        RELNOTES: None
        PiperOrigin-RevId: 282415307

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
@@ -1051,6 +1051,7 @@ public class CompilationSupport {
     ObjcVariablesExtension.Builder extension =
         new ObjcVariablesExtension.Builder()
             .setRuleContext(ruleContext)
+            .setObjcProvider(objcProvider)
             .setCompilationArtifacts(compilationArtifacts)
             .setIntermediateArtifacts(intermediateArtifacts)
             .setConfiguration(buildConfiguration)
