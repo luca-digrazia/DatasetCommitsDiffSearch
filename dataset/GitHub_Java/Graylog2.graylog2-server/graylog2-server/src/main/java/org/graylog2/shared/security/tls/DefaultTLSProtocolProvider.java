f7f7020013c9d7f7ca6937eea7324e39dd6da555
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:14:26 +0200

    Fix JDK TLS protocol detection (#9356)
        Previously we only checked the protocols that are enabled by default
        instead of checking all available.
    
        This allows the usage of TLSv1.3 in OpenJDK 8u272 and later.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/shared/security/tls/DefaultTLSProtocolProvider.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/shared/security/tls/DefaultTLSProtocolProvider.java
@@ -1,18 +1,18 @@
-/*
- * Copyright (C) 2020 Graylog, Inc.
+/**
+ * This file is part of Graylog.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.shared.security.tls;
 
@@ -23,38 +23,31 @@ import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLContext;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public abstract class DefaultTLSProtocolProvider {
     // Defaults to TLS protocols that are currently considered secure
-    private static final Set<String> SECURE_TLS_PROTOCOLS = ImmutableSet.of("TLSv1.2", "TLSv1.3");
+    public static final Set<String> DEFAULT_TLS_PROTOCOLS = ImmutableSet.of("TLSv1.2", "TLSv1.3");
 
     private static final Logger LOG = LoggerFactory.getLogger(DefaultTLSProtocolProvider.class);
     private static Set<String> defaultSupportedTlsProtocols = null;
 
-    public synchronized static Set<String> getSecureTLSProtocols() {
+    public synchronized static Set<String> getDefaultSupportedTlsProtocols() {
         if (defaultSupportedTlsProtocols != null) {
             return defaultSupportedTlsProtocols;
         }
 
-        final Set<String> tlsProtocols = Sets.newHashSet(SECURE_TLS_PROTOCOLS);
-        final Set<String> supportedProtocols = getAllSupportedTlsProtocols();
-        if (tlsProtocols.retainAll(supportedProtocols)) {
-            LOG.warn("JRE doesn't support all default TLS protocols. Changing <{}> to <{}>", SECURE_TLS_PROTOCOLS, tlsProtocols);
-        }
-        defaultSupportedTlsProtocols = tlsProtocols;
-        return defaultSupportedTlsProtocols;
-    }
-
-    public static Set<String> getAllSupportedTlsProtocols() {
+        final Set<String> tlsProtocols = Sets.newHashSet(DEFAULT_TLS_PROTOCOLS);
         try {
-            // Drop SSLv3, as it's not supported by OpenSSL anymore
-            return Arrays.stream(SSLContext.getDefault().createSSLEngine().getSupportedProtocols()).filter(p -> !p.equals("SSLv3")).collect(Collectors.toSet());
+            final Set<String> supportedProtocols = ImmutableSet.copyOf(SSLContext.getDefault().createSSLEngine().getSupportedProtocols());
+            if (tlsProtocols.retainAll(supportedProtocols)) {
+                LOG.warn("JRE doesn't support all default TLS protocols. Changing <{}> to <{}>", DEFAULT_TLS_PROTOCOLS, tlsProtocols);
+            }
+            defaultSupportedTlsProtocols = tlsProtocols;
         } catch (NoSuchAlgorithmException e) {
-            LOG.error("Failed to detect supported TLS protocols. Keeping default <{}>", SECURE_TLS_PROTOCOLS, e);
-            return SECURE_TLS_PROTOCOLS;
+            LOG.error("Failed to detect supported TLS protocols. Keeping default <{}>", DEFAULT_TLS_PROTOCOLS, e);
+            defaultSupportedTlsProtocols = DEFAULT_TLS_PROTOCOLS;
         }
+        return defaultSupportedTlsProtocols;
     }
 }
