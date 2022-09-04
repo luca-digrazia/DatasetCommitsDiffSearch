6867c201677917ec15c5088909ec9e5dd9a0db2c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:05:36 +0200

    Fix extractor and static fields creation in multi-node setups (#2584)
        The extractor and static fields resource was using the local input
        registry to check if an input exists. This breaks in a multi-node setup
        where the web interface runs on node A and the input on node B.
    
        Fixes #2580

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/inputs/StaticFieldsResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/inputs/StaticFieldsResource.java
@@ -1,49 +1,71 @@
 /**
- * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.rest.resources.system.inputs;
 
 import com.codahale.metrics.annotation.Timed;
-import org.graylog2.database.ValidationException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.graylog2.auditlog.jersey.AuditLog;
+import org.graylog2.database.NotFoundException;
 import org.graylog2.inputs.Input;
+import org.graylog2.inputs.InputService;
 import org.graylog2.plugin.Message;
+import org.graylog2.plugin.database.ValidationException;
 import org.graylog2.plugin.inputs.MessageInput;
-import org.graylog2.rest.documentation.annotations.*;
-import org.graylog2.rest.resources.RestResource;
-import org.graylog2.rest.resources.system.inputs.requests.CreateStaticFieldRequest;
-import org.graylog2.system.activities.Activity;
+import org.graylog2.rest.models.system.inputs.requests.CreateStaticFieldRequest;
+import org.graylog2.shared.inputs.PersistedInputs;
+import org.graylog2.shared.rest.resources.RestResource;
+import org.graylog2.shared.security.RestPermissions;
+import org.graylog2.shared.system.activities.Activity;
+import org.graylog2.shared.system.activities.ActivityWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.*;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
+import java.net.URI;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
+@RequiresAuthentication
 @Api(value = "StaticFields", description = "Static fields of an input")
 @Path("/system/inputs/{inputId}/staticfields")
 public class StaticFieldsResource extends RestResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(StaticFieldsResource.class);
+
+    @Inject
+    private InputService inputService;
+    @Inject
+    private ActivityWriter activityWriter;
+    @Inject
+    private PersistedInputs persistedInputs;
 
     @POST
     @Timed
@@ -55,54 +77,88 @@ public class StaticFieldsResource extends RestResource {
             @ApiResponse(code = 400, message = "Field/Key is reserved."),
             @ApiResponse(code = 400, message = "Missing or invalid configuration.")
     })
-    public Response create(@ApiParam(title = "JSON body", required = true) String body,
-                           @ApiParam(title = "inputId", required = true) @PathParam("inputId") String inputId) {
-        if (inputId == null || inputId.isEmpty()) {
-            LOG.error("Missing inputId. Returning HTTP 400.");
-            throw new WebApplicationException(400);
-        }
+    @AuditLog(object = "static field", captureRequestEntity = true, captureResponseEntity = true)
+    public Response create(@ApiParam(name = "inputId", required = true)
+                           @PathParam("inputId") String inputId,
+                           @ApiParam(name = "JSON body", required = true)
+                           @Valid @NotNull CreateStaticFieldRequest csfr) throws NotFoundException, ValidationException {
+        checkPermission(RestPermissions.INPUTS_EDIT, inputId);
 
-        MessageInput input = core.inputs().getRunningInputs().get(inputId);
+        final MessageInput input = persistedInputs.get(inputId);
 
         if (input == null) {
-            LOG.error("Input <{}> not found.", inputId);
-            throw new WebApplicationException(404);
+            final String msg = "Input <" + inputId + "> not found.";
+            LOG.error(msg);
+            throw new javax.ws.rs.NotFoundException(msg);
         }
 
-        // Build extractor.
-        CreateStaticFieldRequest csfr;
-        try {
-            csfr = objectMapper.readValue(body, CreateStaticFieldRequest.class);
-        } catch(IOException e) {
-            LOG.error("Error while parsing JSON", e);
-            throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
+        // Check if key is a valid message key.
+        if (!Message.validKey(csfr.key())) {
+            final String msg = "Invalid key: [" + csfr.key() + "]";
+            LOG.error(msg);
+            throw new BadRequestException(msg);
         }
 
-        if (csfr.key == null || csfr.value == null || csfr.key.isEmpty() || csfr.value.isEmpty()) {
-            LOG.error("Missing parameters. Returning HTTP 400.");
-            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        if (Message.RESERVED_FIELDS.contains(csfr.key()) && !Message.RESERVED_SETTABLE_FIELDS.contains(csfr.key())) {
+            final String message = "Cannot add static field. Field [" + csfr.key() + "] is reserved.";
+            LOG.error(message);
+            throw new BadRequestException(message);
         }
 
-        if (Message.RESERVED_FIELDS.contains(csfr.key) && !Message.RESERVED_SETTABLE_FIELDS.contains(csfr.key)) {
-            LOG.error("Cannot add static field. Field [{}] is reserved.", csfr.key);
-            throw new WebApplicationException(Response.Status.BAD_REQUEST);
-        }
+        input.addStaticField(csfr.key(), csfr.value());
 
-        input.addStaticField(csfr.key, csfr.value);
+        final Input mongoInput = inputService.find(input.getPersistId());
+        inputService.addStaticField(mongoInput, csfr.key(), csfr.value());
 
-        Input mongoInput = Input.find(core, input.getPersistId());
-        try {
-            mongoInput.addStaticField(csfr.key, csfr.value);
-        } catch (ValidationException e) {
-            LOG.error("Static field persist validation failed.", e);
-            throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
-        }
-
-        String msg = "Added static field [" + csfr.key + "] to input <" + inputId + ">.";
+        final String msg = "Added static field [" + csfr.key() + "] to input <" + inputId + ">.";
         LOG.info(msg);
-        core.getActivityWriter().write(new Activity(msg, StaticFieldsResource.class));
+        activityWriter.write(new Activity(msg, StaticFieldsResource.class));
 
-        return Response.status(Response.Status.CREATED).build();
+        final URI inputUri = getUriBuilderToSelf().path(InputsResource.class)
+                .path("{inputId}")
+                .build(mongoInput.getId());
+
+        return Response.created(inputUri).build();
     }
 
+    @DELETE
+    @Timed
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Remove static field of an input")
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "No such input on this node."),
+            @ApiResponse(code = 404, message = "No such static field.")
+    })
+    @Path("/{key}")
+    @AuditLog(object = "static field")
+    public void delete(@ApiParam(name = "Key", required = true)
+                       @PathParam("key") String key,
+                       @ApiParam(name = "inputId", required = true)
+                       @PathParam("inputId") String inputId) throws NotFoundException {
+        checkPermission(RestPermissions.INPUTS_EDIT, inputId);
+
+        MessageInput input = persistedInputs.get(inputId);
+
+        if (input == null) {
+            final String msg = "Input <" + inputId + "> not found.";
+            LOG.error(msg);
+            throw new javax.ws.rs.NotFoundException(msg);
+        }
+
+        if (!input.getStaticFields().containsKey(key)) {
+            final String msg = "No such static field [" + key + "] on input <" + inputId + ">.";
+            LOG.error(msg);
+            throw new javax.ws.rs.NotFoundException(msg);
+        }
+
+        input.getStaticFields().remove(key);
+
+        Input mongoInput = inputService.find(input.getPersistId());
+        inputService.removeStaticField(mongoInput, key);
+
+        final String msg = "Removed static field [" + key + "] of input <" + inputId + ">.";
+        LOG.info(msg);
+        activityWriter.write(new Activity(msg, StaticFieldsResource.class));
+    }
 }
