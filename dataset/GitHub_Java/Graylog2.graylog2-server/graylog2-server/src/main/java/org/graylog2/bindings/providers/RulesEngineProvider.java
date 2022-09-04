8584a9e3b5c8c9308f3a53077368210ee9855321
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:08:53 +0200

    Clean up ctor of RulesEngineProvider and add test
        (cherry picked from commit 5db3474753f2ada688d60fd281ca68eb44215fbb)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/providers/RulesEngineProvider.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/providers/RulesEngineProvider.java
@@ -1,52 +1,50 @@
-/*
- * Copyright 2012-2014 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.graylog2.bindings.providers;
 
-import org.graylog2.Configuration;
 import org.graylog2.plugin.RulesEngine;
 import org.graylog2.rules.DroolsEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 
-/**
- * @author Dennis Oelkers <dennis@torch.sh>
- */
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 @Singleton
 public class RulesEngineProvider implements Provider<RulesEngine> {
     private static final Logger LOG = LoggerFactory.getLogger(RulesEngineProvider.class);
-    private static RulesEngine rulesEngine = null;
+    private final RulesEngine rulesEngine;
 
     @Inject
-    public RulesEngineProvider(Configuration configuration, DroolsEngine droolsEngine) {
-        rulesEngine = droolsEngine;
-        String rulesFilePath = configuration.getDroolsRulesFile();
+    public RulesEngineProvider(DroolsEngine droolsEngine,
+                               @Named("rules_file") @Nullable String rulesFilePath) {
+        this.rulesEngine = checkNotNull(droolsEngine);
 
-        if (rulesFilePath != null && !rulesFilePath.isEmpty()) {
+        if (!isNullOrEmpty(rulesFilePath)) {
             if (rulesEngine.addRulesFromFile(rulesFilePath)) {
                 LOG.info("Using rules: {}", rulesFilePath);
             } else {
-                LOG.info("Unable to load rules due to load error: {}", rulesFilePath);
+                LOG.warn("Unable to load rules due to load error: {}", rulesFilePath);
             }
         } else {
             LOG.info("No static rules file loaded.");
