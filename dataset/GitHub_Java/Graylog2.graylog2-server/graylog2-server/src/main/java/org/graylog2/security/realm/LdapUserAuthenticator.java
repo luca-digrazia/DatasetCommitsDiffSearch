fd82149cc8f7aa86b290e65c1be41706c0622027
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:10:54 +0200

    Ensure login failure by default (#2072)
        Always return `null` by default in LdapUserAuthenticator#doGetAuthenticationInfo()
        to make it harder to accidentally login the user even if the password is
        wrong or some other error occurs.
    
        Fixes a security issue where all passwords have been accepted when
        using LDAP. This issue has been introduced in commit 9b746fa one day ago
        and has never been released outside of snapshots.
    
        Fixes #2069

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/realm/LdapUserAuthenticator.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/realm/LdapUserAuthenticator.java
@@ -16,7 +16,6 @@
  */
 package org.graylog2.security.realm;
 
-import com.google.common.base.Strings;
 import org.apache.directory.api.ldap.model.cursor.CursorException;
 import org.apache.directory.api.ldap.model.exception.LdapException;
 import org.apache.directory.ldap.client.api.LdapConnectionConfig;
@@ -80,10 +79,6 @@ public class LdapUserAuthenticator extends AuthenticatingRealm {
         config.setCredentials(ldapSettings.getSystemPassword());
 
         final String principal = (String) token.getPrincipal();
-        if (Strings.isNullOrEmpty(principal) || token.getPassword() == null) {
-            // do not try to look a token up in LDAP if there is no principal or password
-            return null;
-        }
         LdapNetworkConnection connection = null;
         try {
             connection = ldapConnector.connect(config);
