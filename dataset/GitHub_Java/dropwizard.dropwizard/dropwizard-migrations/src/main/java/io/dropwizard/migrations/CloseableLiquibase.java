0d4a0e1df5df76dbb3555fc2850a0db2d58a2954
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:33:48 +0200

    Propagating LiquibaseException instead of catching and logging in CloseableLiquibase#close

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-migrations/src/main/java/io/dropwizard/migrations/CloseableLiquibase.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-migrations/src/main/java/io/dropwizard/migrations/CloseableLiquibase.java
@@ -2,23 +2,28 @@ package io.dropwizard.migrations;
 
 import io.dropwizard.db.ManagedDataSource;
 import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseConnection;
+import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
-import liquibase.resource.ResourceAccessor;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.FileSystemResourceAccessor;
 
 import java.sql.SQLException;
 
-public abstract class CloseableLiquibase extends Liquibase implements AutoCloseable {
+public class CloseableLiquibase extends Liquibase implements AutoCloseable {
+    private static final String DEFAULT_MIGRATIONS_FILE = "migrations.xml";
     private final ManagedDataSource dataSource;
 
-    CloseableLiquibase(String changeLogFile, ResourceAccessor resourceAccessor, Database database, ManagedDataSource dataSource) throws LiquibaseException, SQLException {
-        super(changeLogFile, resourceAccessor, database);
+    public CloseableLiquibase(ManagedDataSource dataSource) throws LiquibaseException, SQLException {
+        super(DEFAULT_MIGRATIONS_FILE,
+              new ClassLoaderResourceAccessor(),
+              new JdbcConnection(dataSource.getConnection()));
         this.dataSource = dataSource;
     }
 
-    public CloseableLiquibase(String changeLogFile, ResourceAccessor resourceAccessor, DatabaseConnection conn, ManagedDataSource dataSource) throws LiquibaseException, SQLException {
-        super(changeLogFile, resourceAccessor, conn);
+    public CloseableLiquibase(ManagedDataSource dataSource, String file) throws LiquibaseException, SQLException {
+        super(file,
+              new FileSystemResourceAccessor(),
+              new JdbcConnection(dataSource.getConnection()));
         this.dataSource = dataSource;
     }
 
