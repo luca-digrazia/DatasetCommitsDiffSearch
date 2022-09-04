431bf7288e95a1ea8473614cfaf2b5faf08fe23d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:00:18 +0200

    Silence a few warnings in JAX-RS support

--- dataset/GitHub_Java/quarkusio.quarkus/jaxrs/runtime/src/main/java/org/jboss/shamrock/jaxrs/runtime/graal/ShamrockConstructorInjector.java
+++ dataset/GitHub_Java/quarkusio.quarkus/jaxrs/runtime/src/main/java/org/jboss/shamrock/jaxrs/runtime/graal/ShamrockConstructorInjector.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.shamrock.jaxrs.runtime.graal;
 
 import java.lang.reflect.Constructor;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
-import javax.enterprise.inject.Instance;
-import javax.enterprise.inject.spi.CDI;
 import javax.ws.rs.WebApplicationException;
 
 import org.jboss.resteasy.spi.ApplicationException;
@@ -11,43 +27,49 @@ import org.jboss.resteasy.spi.ConstructorInjector;
 import org.jboss.resteasy.spi.Failure;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.HttpResponse;
-import sun.security.provider.SHA;
+import org.jboss.shamrock.runtime.cdi.BeanContainer;
 
-/**
- * Created by bob on 7/31/18.
- */
 public class ShamrockConstructorInjector implements ConstructorInjector {
-    public ShamrockConstructorInjector(Constructor ctor, ConstructorInjector delegate) {
+
+    private volatile BeanContainer.Factory<?> factory;
+
+    private final ConstructorInjector delegate;
+
+    private final Constructor<?> ctor;
+
+    public ShamrockConstructorInjector(Constructor<?> ctor, ConstructorInjector delegate) {
         this.ctor = ctor;
         this.delegate = delegate;
     }
 
     @Override
-    public Object construct() {
-        System.err.println( "construct() " + this.ctor );
-        return this.delegate.construct();
+    public CompletionStage<Object> construct(boolean unwrapAsync) {
+        return this.delegate.construct(unwrapAsync);
     }
 
     @Override
-    public Object construct(HttpRequest request, HttpResponse response) throws Failure, WebApplicationException, ApplicationException {
-        System.err.println( "construct(req,resp) " + this.ctor );
-        System.err.println( "CAN WE CDI? " + ShamrockInjectorFactory.CONTAINER);
-        Instance object = ShamrockInjectorFactory.CONTAINER.select(this.ctor.getDeclaringClass());
-        //return this.delegate.construct(request, response);
-        return object.get();
+    public CompletionStage<Object> construct(HttpRequest request, HttpResponse response, boolean unwrapAsync)
+            throws Failure, WebApplicationException, ApplicationException {
+        if (ShamrockInjectorFactory.CONTAINER == null) {
+            return delegate.construct(request, response, unwrapAsync);
+        }
+        if(factory == null) {
+            factory = ShamrockInjectorFactory.CONTAINER.instanceFactory(this.ctor.getDeclaringClass());
+        }
+        if(factory == null) {
+            return delegate.construct(request, response, unwrapAsync);
+        }
+        return CompletableFuture.completedFuture(factory.get());
     }
 
     @Override
-    public Object[] injectableArguments() {
-        return this.delegate.injectableArguments();
+    public CompletionStage<Object[]> injectableArguments(boolean unwrapAsync) {
+        return this.delegate.injectableArguments(unwrapAsync);
     }
 
     @Override
-    public Object[] injectableArguments(HttpRequest request, HttpResponse response) throws Failure {
-        return this.delegate.injectableArguments(request, response);
+    public CompletionStage<Object[]> injectableArguments(HttpRequest request, HttpResponse response, boolean unwrapAsync)
+            throws Failure {
+        return this.delegate.injectableArguments(request, response, unwrapAsync);
     }
-
-    private final ConstructorInjector delegate;
-
-    private final Constructor ctor;
 }
