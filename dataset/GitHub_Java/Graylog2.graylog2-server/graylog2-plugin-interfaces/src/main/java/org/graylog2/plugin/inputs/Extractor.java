fc7a01e207ed7f6428a66d796d13300f9d60f341
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:19:37 +0200

    use the correct target field for extractors which can produce multiple result fields, even if they happen to produce just one
        #377

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/inputs/Extractor.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/inputs/Extractor.java
@@ -67,21 +67,7 @@ public abstract class Extractor implements EmbeddedPersistable {
         REGEX,
         SPLIT_AND_INDEX,
         COPY_INPUT,
-        GROK;
-
-        /**
-         * Just like {@link #valueOf(String)} but uses the upper case string and doesn't throw exceptions.
-         *
-         * @param s the string representation of the extractor type.
-         * @return the actual {@link org.graylog2.plugin.inputs.Extractor.Type} or {@code null}.
-         */
-        public static Type fuzzyValueOf(String s) {
-            try {
-                return valueOf(s.toUpperCase());
-            } catch (Exception e) {
-                return null;
-            }
-        }
+        GROK
     }
 
     public enum CursorStrategy {
@@ -391,7 +377,7 @@ public abstract class Extractor implements EmbeddedPersistable {
         public Result(String value, int beginIndex, int endIndex) {
             this(value, null, beginIndex, endIndex);
         }
-
+        
         public Result(String value, String target, int beginIndex, int endIndex) {
             this.value = value;
             this.target = target;
@@ -419,8 +405,6 @@ public abstract class Extractor implements EmbeddedPersistable {
 
     private static class ResultPredicate implements Predicate<Result> {
         @Override
-        public boolean apply(Result input) {
-            return input.getValue() == null;
-        }
+        public boolean apply(Result input) { return input.getValue() == null; }
     }
 }
