a2427bc44ad28585138ebff16040873be0137eea
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:15:55 +0200

    Added support for extensions too

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/deployment/src/main/java/io/quarkus/smallrye/context/deployment/SmallRyeContextPropagationProcessor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/smallrye-context-propagation/deployment/src/main/java/io/quarkus/smallrye/context/deployment/SmallRyeContextPropagationProcessor.java
@@ -47,8 +47,7 @@ class SmallRyeContextPropagationProcessor {
 
     @BuildStep
     @Record(ExecutionTime.STATIC_INIT)
-    void buildStatic(SmallRyeContextPropagationTemplate template)
-            throws ClassNotFoundException, IOException {
+    void buildStatic(SmallRyeContextPropagationTemplate template) throws ClassNotFoundException, IOException {
         List<ThreadContextProvider> discoveredProviders = new ArrayList<>();
         List<ContextManagerExtension> discoveredExtensions = new ArrayList<>();
         for (Class<?> provider : ServiceUtil.classesNamedIn(SmallRyeContextPropagationTemplate.class.getClassLoader(),
