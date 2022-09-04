980e9af37127faffde44e87d960418616cd9f1a5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:03:34 +0200

    Update to AssetBundle to allow user to specify the urlPath where they want the resources available.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/servlets/AssetServlet.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/main/java/com/yammer/dropwizard/servlets/AssetServlet.java
@@ -1,20 +1,18 @@
 package com.yammer.dropwizard.servlets;
 
-import java.io.IOException;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.io.Resources;
+import org.eclipse.jetty.http.MimeTypes;
+import org.eclipse.jetty.util.URIUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.eclipse.jetty.http.MimeTypes;
-import org.eclipse.jetty.io.Buffer;
-
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.io.Resources;
+import java.io.IOException;
 
 public class AssetServlet extends HttpServlet {
     private static final long serialVersionUID = 6393345594784987908L;
@@ -22,8 +20,7 @@ public class AssetServlet extends HttpServlet {
     private static class AssetLoader extends CacheLoader<String, byte[]> {
         private final String resourcePath;
         private final String uriPath;
-        private final static String INDEX_FILENAME = "index.htm"; // TODO: Make this configurable.
-        
+
         private AssetLoader(String resourcePath, String uriPath) {
             this.resourcePath = resourcePath;
             this.uriPath = uriPath;
@@ -33,16 +30,13 @@ public class AssetServlet extends HttpServlet {
         public byte[] load(String key) throws Exception {
             final String resource = key.substring(uriPath.length());
             String fullResourcePath = this.resourcePath + resource;
-            if (key.equals(this.uriPath)) {
-                fullResourcePath = resourcePath + INDEX_FILENAME;
-            }
-            return Resources.toByteArray(Resources.getResource(fullResourcePath.substring(1)));
+
+                return Resources.toByteArray(Resources.getResource(fullResourcePath.substring(1)));
         }
     }
     
     private final transient LoadingCache<String, byte[]> cache;
     private final transient MimeTypes mimeTypes;
-    private final static String DEFAULT_MIME_TYPE = "text/html";
 
     public AssetServlet(String resourcePath, int maxCacheSize, String uriPath) {
         this.cache = buildCache(resourcePath, maxCacheSize, uriPath);
@@ -60,12 +54,7 @@ public class AssetServlet extends HttpServlet {
                          HttpServletResponse resp) throws ServletException, IOException {
         try {
             final byte[] resource = cache.getUnchecked(req.getRequestURI());
-            Buffer mimeType = mimeTypes.getMimeByExtension(req.getRequestURI());
-            if (mimeType == null) {
-                resp.setContentType(DEFAULT_MIME_TYPE);
-            } else {
-                resp.setContentType(mimeType.toString());
-            }
+            resp.setContentType(mimeTypes.getMimeByExtension(req.getRequestURI()).toString());
             final ServletOutputStream output = resp.getOutputStream();
             try {
                 output.write(resource);
