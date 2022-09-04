c2f605489f55fc851087d00bb5ece758dce98b9e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:25:19 +0200

    Ensure NamedSetOfFiles BEP event appears before event referencing the file_set.
        Previously, when a `NestedSet<Artifact>` is referenced by multiple BuildEvents
        (perhaps transitively) there is a race in `BuildEventStreamer`, which receives all
        BuildEvents in an `@AllowConcurrentEvents` EventBus handler. If the BuildEvents are
        posted by different threads, the first will acquire the name for the
        `NamedSetOfFiles` and prepare the protobuf message from the NestedSet. In the
        meantime, the second thread will see the NestedSet's name was already acquired and
        may proceed to write its BuildEvent referencing the `named_set`. This leads to a
        violation of the expected ordering.
    
        Note that the ordering expectation here cannot be guaranteed by parent-child event
        relationships, as a given `NamedSetOfFiles` cannot 'know' all of the events
        that will reference it in advance. This is why `NamedSetOfFiles` events are children
        of `Progress` events.
    
        RELNOTES: Users consuming BEP may assume that a `named_set_of_files` event will
        appear before any event referencing that `named_set` by ID. This allows consumers
        to process the files for such events (eg. `TargetCompleted`) immediately.
        PiperOrigin-RevId: 361822335

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/CountingArtifactGroupNamer.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/CountingArtifactGroupNamer.java
@@ -13,47 +13,76 @@
 // limitations under the License.
 package com.google.devtools.build.lib.runtime;
 
-import com.google.devtools.build.lib.actions.Artifact;
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.google.devtools.build.lib.buildeventstream.ArtifactGroupNamer;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildEventId.NamedSetOfFilesId;
-import com.google.devtools.build.lib.collect.nestedset.NestedSetView;
-import java.util.HashMap;
-import java.util.Map;
-import javax.annotation.concurrent.GuardedBy;
+import com.google.devtools.build.lib.collect.nestedset.NestedSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CountDownLatch;
 import javax.annotation.concurrent.ThreadSafe;
 
 /** Conversion of paths to URIs. */
 @ThreadSafe
 public class CountingArtifactGroupNamer implements ArtifactGroupNamer {
-  @GuardedBy("this")
-  private final Map<Object, Long> reportedArtifactNames = new HashMap<>();
 
-  @GuardedBy("this")
-  private long nextArtifactName;
+  private final ConcurrentMap<NestedSet.Node, LatchedGroupName> nodeNames =
+      new ConcurrentHashMap<>();
 
   @Override
-  public NamedSetOfFilesId apply(Object id) {
-    Long name;
-    synchronized (this) {
-      name = reportedArtifactNames.get(id);
-    }
+  public NamedSetOfFilesId apply(NestedSet.Node id) {
+    LatchedGroupName name = nodeNames.get(id);
     if (name == null) {
       return null;
     }
-    return NamedSetOfFilesId.newBuilder().setId(name.toString()).build();
+    return NamedSetOfFilesId.newBuilder().setId(name.getName()).build();
   }
 
   /**
-   * If the {@link NestedSetView} has no name already, return a new name for it. Return null
-   * otherwise.
+   * If the {@link NestedSet} has no name already, return a new name for it. Return null otherwise.
    */
-  public synchronized String maybeName(NestedSetView<Artifact> view) {
-    if (reportedArtifactNames.containsKey(view.identifier())) {
+  public LatchedGroupName maybeName(NestedSet<?> set) {
+    NestedSet.Node id = set.toNode();
+    LatchedGroupName existingGroupName;
+    LatchedGroupName newGroupName;
+    // synchronized necessary only to ensure node names are chosen uniquely and compactly.
+    // TODO(adgar): consider dropping compactness and unconditionally increment an AtomicLong to
+    // pick unique node names.
+    synchronized (this) {
+      newGroupName = new LatchedGroupName(nodeNames.size());
+      existingGroupName = nodeNames.putIfAbsent(id, newGroupName);
+    }
+    if (existingGroupName != null) {
+      existingGroupName.waitUntilWritten();
       return null;
     }
-    Long name = nextArtifactName;
-    nextArtifactName++;
-    reportedArtifactNames.put(view.identifier(), name);
-    return name.toString();
+    return newGroupName;
+  }
+
+  /**
+   * A name for a {@code NestedSet<?>} that the constructor must {@link #close()} after the set is
+   * written, allowing all other consumers to {@link #waitUntilWritten()}.
+   */
+  public static class LatchedGroupName implements AutoCloseable {
+    private final CountDownLatch latch;
+    private final int name;
+
+    public LatchedGroupName(int name) {
+      this.latch = new CountDownLatch(1);
+      this.name = name;
+    }
+
+    @Override
+    public void close() {
+      latch.countDown();
+    }
+
+    String getName() {
+      return Integer.toString(name);
+    }
+
+    private void waitUntilWritten() {
+      Uninterruptibles.awaitUninterruptibly(latch);
+    }
   }
 }
