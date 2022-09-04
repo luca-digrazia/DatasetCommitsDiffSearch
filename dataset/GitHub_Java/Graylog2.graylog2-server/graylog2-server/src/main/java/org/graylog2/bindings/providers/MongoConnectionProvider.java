3bf09f4d434f3789e3bc234dbcde736b03360836
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:51:13 +0200

    Log error message if there is a MongoDB connection error.
        Fixes #1249

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/providers/MongoConnectionProvider.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/providers/MongoConnectionProvider.java
@@ -1,39 +1,45 @@
-/*
- * Copyright 2014 TORCH GmbH
+/**
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.graylog2.bindings.providers;
 
-import org.graylog2.Configuration;
+import org.graylog2.configuration.MongoDbConfiguration;
 import org.graylog2.database.MongoConnection;
+import org.graylog2.database.MongoConnectionImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 
 public class MongoConnectionProvider implements Provider<MongoConnection> {
+    private static final Logger LOG = LoggerFactory.getLogger(MongoConnectionProvider.class);
     private static MongoConnection mongoConnection = null;
 
     @Inject
-    public MongoConnectionProvider(Configuration configuration) {
+    public MongoConnectionProvider(MongoDbConfiguration configuration) {
         if (mongoConnection == null) {
-            mongoConnection = new MongoConnection(configuration);
+            try {
+                mongoConnection = new MongoConnectionImpl(configuration);
 
-            mongoConnection.connect();
+                mongoConnection.connect();
+            } catch (Exception e) {
+                LOG.error("Error connecting to MongoDB: {}", e.getMessage());
+                throw e;
+            }
         }
     }
 
