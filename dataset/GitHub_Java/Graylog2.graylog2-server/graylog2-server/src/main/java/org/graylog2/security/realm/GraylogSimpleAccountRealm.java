6ee60871258ee7d70acb95681ef231ecd2167786
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:45:01 +0200

    improve logging a bit around authenticating users

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/realm/GraylogSimpleAccountRealm.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/security/realm/GraylogSimpleAccountRealm.java
@@ -1,21 +1,8 @@
-/**
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.security.realm;
 
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAccount;
 import org.apache.shiro.authz.Permission;
 import org.apache.shiro.authz.permission.AllPermission;
@@ -25,14 +12,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GraylogSimpleAccountRealm extends SimpleAccountRealm {
-    private static final Logger LOG = LoggerFactory.getLogger(GraylogSimpleAccountRealm.class);
+    private static final Logger log = LoggerFactory.getLogger(GraylogSimpleAccountRealm.class);
 
     public GraylogSimpleAccountRealm() {
         super("graylog2-in-memory-realm");
     }
 
     public void addRootAccount(String username, String password) {
-        LOG.debug("Adding root account named {}, having all permissions", username);
+        log.debug("Adding root account named {}, having all permissions", username);
         add(new SimpleAccount(
                 username,
                 password,
@@ -42,4 +29,9 @@ public class GraylogSimpleAccountRealm extends SimpleAccountRealm {
         ));
     }
 
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        log.debug("Retrieving authentication info for user {}", token.getPrincipal());
+        return super.doGetAuthenticationInfo(token);
+    }
 }
