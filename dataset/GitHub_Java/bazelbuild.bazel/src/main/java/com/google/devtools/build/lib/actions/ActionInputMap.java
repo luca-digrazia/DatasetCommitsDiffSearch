b06aeaf74d037182127bec7316b212bb594881e7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:08:11 +0200

    Improve efficiency of ActionInputMap.
        This is currently implemented with open addressing (linear scanning). Instead,
        use closed addressing with linked lists in each buckets, but store the pointers
        of the linked list in a flat array.
    
        The reason here is that the assignments into an array of a specific type are
        expensive (probably doing type checks). Using an Object[] internally is much
        more efficient.
    
        That in turn means that we need to store three things (keys, values, and exec
        paths). As soon as we need three things, closed addressing is likely more
        memory efficient.
    
        This should keep the created garbage about the same while substantially
        improving performance.
    
        RELNOTES: None.
        PiperOrigin-RevId: 220289296

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/ActionInputMap.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/ActionInputMap.java
@@ -30,7 +30,7 @@ import javax.annotation.Nullable;
  *
  * <p>This class is thread-compatible.
  */
-public final class ActionInputMap implements MetadataProvider, ActionInputMapSink {
+public final class ActionInputMap implements MetadataProvider {
   /** The number of elements contained in this map. */
   int size;
 
@@ -116,12 +116,8 @@ public final class ActionInputMap implements MetadataProvider, ActionInputMapSin
     return size;
   }
 
-  @Override
-  public boolean put(ActionInput input, FileArtifactValue metadata, @Nullable Artifact depOwner) {
-    return putWithNoDepOwner(input, metadata);
-  }
-
-  public boolean putWithNoDepOwner(ActionInput input, FileArtifactValue metadata) {
+  /** @return true if an entry was added, false if the map already contains {@code input} */
+  public boolean put(ActionInput input, FileArtifactValue metadata) {
     Preconditions.checkNotNull(input);
     if (size >= keys.length) {
       resize();
