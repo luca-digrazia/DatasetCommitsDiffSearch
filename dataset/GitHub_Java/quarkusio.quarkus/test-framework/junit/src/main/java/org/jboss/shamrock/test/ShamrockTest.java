fa5e4665f1150183a88a43d7921786031e4f56f3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:04:58 +0200

    Change the way the native tests work

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit/src/main/java/org/jboss/shamrock/test/ShamrockTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/junit/src/main/java/org/jboss/shamrock/test/ShamrockTest.java
@@ -16,48 +16,83 @@
 
 package org.jboss.shamrock.test;
 
-import static org.jboss.shamrock.test.PathTestHelper.getTestClassesLocation;
-import static org.jboss.shamrock.test.PathTestHelper.getAppClassLocation;
-
-import java.io.IOException;
-import java.util.Collections;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import org.jboss.shamrock.runner.RuntimeRunner;
 import org.junit.runner.Description;
 import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
 import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 
-public class ShamrockTest extends AbstractShamrockTestRunner {
+public class ShamrockTest extends BlockJUnit4ClassRunner {
 
+    static boolean first = true;
+    static boolean started = false;
+    static boolean failed = false;
+
+    /**
+     * Creates a BlockJUnit4ClassRunner to run {@code klass}
+     *
+     * @param klass
+     * @throws InitializationError if the test class is malformed.
+     */
     public ShamrockTest(Class<?> klass) throws InitializationError {
-        super(klass, (c, n) -> new ShamrockRunListener(c, n));
+        super(klass);
     }
 
-    private static class ShamrockRunListener extends AbstractShamrockRunListener {
+    @Override
+    public void run(final RunNotifier notifier) {
+        runInternal(notifier);
+        super.run(notifier);
+    }
 
-        private RuntimeRunner runtimeRunner;
-
-        ShamrockRunListener(Class<?> testClass, RunNotifier runNotifier) {
-            super(testClass, runNotifier);
+    @Override
+    protected void runChild(final FrameworkMethod method, RunNotifier notifier) {
+        if (!failed) {
+            super.runChild(method, notifier);
+        } else {
+            notifier.fireTestIgnored(describeChild(method));
         }
+    }
 
-        @Override
-        protected void startShamrock() {
-            if (ShamrockUnitTest.started) {
-                getRunNotifier().fireTestFailure(new Failure(Description.createSuiteDescription(ShamrockTest.class),
-                        new RuntimeException("Cannot mix ShamrockTest and ShamrockUnitTest in the same test suite")));
-                return;
-            }
+    private void runInternal(RunNotifier notifier) {
+        if (first) {
+            first = false;
+            //now we need to bootstrap shamrock
+            notifier.addListener(new RunListener() {
 
-            runtimeRunner = new RuntimeRunner(getClass().getClassLoader(), getAppClassLocation(getTestClass()),
-                    getTestClassesLocation(getTestClass()), null, Collections.emptyList());
-            runtimeRunner.run();
-        }
 
-        @Override
-        protected void stopShamrock() throws IOException {
-            runtimeRunner.close();
+                @Override
+                public void testStarted(Description description) {
+                    if (ShamrockUnitTest.started) {
+                        notifier.fireTestFailure(new Failure(Description.createSuiteDescription(ShamrockTest.class), new RuntimeException("Cannot mix ShamrockTest and ShamrockUnitTest in the same test suite")));
+                        return;
+                    }
+                    if (!started) {
+                        started = true;
+                        //TODO: so much hacks...
+                        try {
+                            Class<?> theClass = description.getTestClass();
+                            String classFileName = theClass.getName().replace('.', '/') + ".class";
+                            URL resource = theClass.getClassLoader().getResource(classFileName);
+                            String testClassLocation = resource.getPath().substring(0, resource.getPath().length() - classFileName.length());
+                            String appClassLocation = testClassLocation.replace("test-classes", "classes");
+                            Path appRoot = Paths.get(appClassLocation);
+                            RuntimeRunner runtimeRunner = new RuntimeRunner(getClass().getClassLoader(), appRoot, Paths.get(testClassLocation), null, new ArrayList<>());
+                            runtimeRunner.run();
+                        } catch (RuntimeException e) {
+                            failed = true;
+                            throw new RuntimeException("Failed to boot Shamrock during @ShamrockTest runner!", e);
+                        }
+                    }
+                }
+            });
         }
     }
 }
