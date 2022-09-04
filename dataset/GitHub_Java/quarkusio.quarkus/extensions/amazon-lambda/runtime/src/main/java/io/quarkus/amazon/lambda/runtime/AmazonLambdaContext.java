6a158ab7f9fbbaa94b9e6c20b3bb6c75e488d214
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:21:42 +0200

    Improved computation of remainingTimeInMillis

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/amazon-lambda/runtime/src/main/java/io/quarkus/amazon/lambda/runtime/AmazonLambdaContext.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/amazon-lambda/runtime/src/main/java/io/quarkus/amazon/lambda/runtime/AmazonLambdaContext.java
@@ -1,18 +1,9 @@
 package io.quarkus.amazon.lambda.runtime;
 
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.LAMBDA_RUNTIME_AWS_REQUEST_ID;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.LAMBDA_RUNTIME_CLIENT_CONTEXT;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.LAMBDA_RUNTIME_COGNITO_IDENTITY;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.LAMBDA_RUNTIME_DEADLINE_MS;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.LAMBDA_RUNTIME_INVOKED_FUNCTION_ARN;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.functionMemorySize;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.functionName;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.functionVersion;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.logGroupName;
-import static io.quarkus.amazon.lambda.runtime.AmazonLambdaApi.logStreamName;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.Date;
+import java.util.StringJoiner;
 
 import com.amazonaws.services.lambda.runtime.ClientContext;
 import com.amazonaws.services.lambda.runtime.CognitoIdentity;
@@ -37,27 +28,27 @@ public class AmazonLambdaContext implements Context {
 
     public AmazonLambdaContext(HttpURLConnection request, ObjectReader cognitoReader, ObjectReader clientCtxReader)
             throws IOException {
-        awsRequestId = request.getHeaderField(LAMBDA_RUNTIME_AWS_REQUEST_ID);
-        logGroupName = logGroupName();
-        logStreamName = logStreamName();
-        functionName = functionName();
-        functionVersion = functionVersion();
-        invokedFunctionArn = request.getHeaderField(LAMBDA_RUNTIME_INVOKED_FUNCTION_ARN);
+        awsRequestId = request.getHeaderField("Lambda-Runtime-Aws-Request-Id");
+        logGroupName = System.getenv("AWS_LAMBDA_LOG_GROUP_NAME");
+        logStreamName = System.getenv("AWS_LAMBDA_LOG_STREAM_NAME");
+        functionName = System.getenv("AWS_LAMBDA_FUNCTION_NAME");
+        functionVersion = System.getenv("AWS_LAMBDA_FUNCTION_VERSION");
+        invokedFunctionArn = request.getHeaderField("Lambda-Runtime-Invoked-Function-Arn");
 
-        String cognitoIdentityHeader = request.getHeaderField(LAMBDA_RUNTIME_COGNITO_IDENTITY);
+        String cognitoIdentityHeader = request.getHeaderField("Lambda-Runtime-Cognito-Identity");
         if (cognitoIdentityHeader != null) {
             cognitoIdentity = cognitoReader.readValue(cognitoIdentityHeader);
         }
 
-        String clientContextHeader = request.getHeaderField(LAMBDA_RUNTIME_CLIENT_CONTEXT);
+        String clientContextHeader = request.getHeaderField("Lambda-Runtime-Client-Context");
         if (clientContextHeader != null) {
             clientContext = clientCtxReader.readValue(clientContextHeader);
         }
 
-        String functionMemorySize = functionMemorySize();
+        String functionMemorySize = System.getenv("AWS_LAMBDA_FUNCTION_MEMORY_SIZE");
         memoryLimitInMB = functionMemorySize != null ? Integer.valueOf(functionMemorySize) : 0;
 
-        String runtimeDeadline = request.getHeaderField(LAMBDA_RUNTIME_DEADLINE_MS);
+        String runtimeDeadline = request.getHeaderField("Lambda-Runtime-Deadline-Ms");
         if (runtimeDeadline != null) {
             runtimeDeadlineMs = Long.valueOf(runtimeDeadline);
         }
