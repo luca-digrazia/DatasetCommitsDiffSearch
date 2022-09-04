6e1bd2006ed3cf0f8f6a19a9eabeb205a6f91b06
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:05:17 +0200

    Fix java.lang.Math$RandomNumberGeneratorHolder workaround

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildStep.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildStep.java
@@ -507,8 +507,8 @@ public class NativeImageBuildStep {
         final Path javaSourcesPath = outputTargetBuildItem.getOutputDirectory().resolve(
                 Paths.get("..", "src", "main", "java"));
 
-        try (Stream<Path> paths = Files.walk(javaSourcesPath)) {
-            paths.forEach(path -> {
+        try {
+            Files.walk(javaSourcesPath).forEach(path -> {
                 Path targetPath = Paths.get(targetSrc.toString(),
                         path.toString().substring(javaSourcesPath.toString().length()));
                 try {
