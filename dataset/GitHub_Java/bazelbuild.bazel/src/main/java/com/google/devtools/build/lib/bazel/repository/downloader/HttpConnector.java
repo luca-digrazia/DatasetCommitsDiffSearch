fb40840df145d262043e25d682149eb83722fda9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 00:37:40 +0200

    fix for bazel 303 redirect bug: https://github.com/bazelbuild/bazel/issues/3343
        manually turn off HTTP internal reconnection so HTTP can utilize MAX_REDIRECTS, etc.
    
        RELNOTES: None.
        PiperOrigin-RevId: 164294282

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/repository/downloader/HttpConnector.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/repository/downloader/HttpConnector.java
@@ -167,8 +167,6 @@ class HttpConnector {
         }
       } catch (UnrecoverableHttpException e) {
         throw e;
-      } catch (IllegalArgumentException e) {
-        throw new UnrecoverableHttpException(e.getMessage());
       } catch (IOException e) {
         if (connection != null) {
           // If we got here, it means we might not have consumed the entire payload of the
