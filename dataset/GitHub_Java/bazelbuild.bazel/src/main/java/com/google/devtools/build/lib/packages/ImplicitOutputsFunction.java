3b1db2d475ac16ecfa8f642fe9eb1c2e7f898589
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:51:14 +0200

    Add codec for ImmutableMultimap.
        PiperOrigin-RevId: 191323243

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/ImplicitOutputsFunction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/ImplicitOutputsFunction.java
@@ -78,7 +78,6 @@ public abstract class ImplicitOutputsFunction {
   }
 
   /** Implicit output functions executing Skylark code. */
-  @AutoCodec
   public static final class SkylarkImplicitOutputsFunctionWithCallback
       extends SkylarkImplicitOutputsFunction {
 
