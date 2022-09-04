edf6381f4f7744cd9c5b94884cf39074cf1c59ce
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:30:00 +0200

    Rely on the default configuration source for default values
        ...and fall back to zero/null

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/configuration/FloatConfigType.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/configuration/FloatConfigType.java
@@ -5,13 +5,11 @@ import java.lang.reflect.Field;
 import org.wildfly.common.Assert;
 
 import io.quarkus.deployment.AccessorFinder;
-import io.quarkus.deployment.steps.ConfigurationSetup;
 import io.quarkus.gizmo.AssignableResultHandle;
 import io.quarkus.gizmo.BranchResult;
 import io.quarkus.gizmo.BytecodeCreator;
 import io.quarkus.gizmo.MethodDescriptor;
 import io.quarkus.gizmo.ResultHandle;
-import io.quarkus.runtime.configuration.ExpandingConfigSource;
 import io.quarkus.runtime.configuration.NameIterator;
 import io.smallrye.config.SmallRyeConfig;
 
@@ -31,22 +29,21 @@ public class FloatConfigType extends LeafConfigType {
         this.defaultValue = defaultValue;
     }
 
-    public void acceptConfigurationValue(final NameIterator name, final ExpandingConfigSource.Cache cache,
-            final SmallRyeConfig config) {
+    public void acceptConfigurationValue(final NameIterator name, final SmallRyeConfig config) {
         final GroupConfigType container = getContainer(GroupConfigType.class);
         if (isConsumeSegment())
             name.previous();
-        container.acceptConfigurationValueIntoLeaf(this, name, cache, config);
+        container.acceptConfigurationValueIntoLeaf(this, name, config);
         // the iterator is not used after this point
         // if (isConsumeSegment()) name.next();
     }
 
     public void generateAcceptConfigurationValue(final BytecodeCreator body, final ResultHandle name,
-            final ResultHandle cache, final ResultHandle config) {
+            final ResultHandle config) {
         final GroupConfigType container = getContainer(GroupConfigType.class);
         if (isConsumeSegment())
             body.invokeVirtualMethod(NI_PREV_METHOD, name);
-        container.generateAcceptConfigurationValueIntoLeaf(body, this, name, cache, config);
+        container.generateAcceptConfigurationValueIntoLeaf(body, this, name, config);
         // the iterator is not used after this point
         // if (isConsumeSegment()) body.invokeVirtualMethod(NI_NEXT_METHOD, name);
     }
@@ -93,36 +90,30 @@ public class FloatConfigType extends LeafConfigType {
         return float.class;
     }
 
-    void getDefaultValueIntoEnclosingGroup(final Object enclosing, final ExpandingConfigSource.Cache cache,
-            final SmallRyeConfig config, final Field field) {
+    void getDefaultValueIntoEnclosingGroup(final Object enclosing, final SmallRyeConfig config, final Field field) {
         try {
-            field.setFloat(enclosing,
-                    config.convert(ExpandingConfigSource.expandValue(defaultValue, cache), Float.class).floatValue());
+            field.setFloat(enclosing, config.convert(defaultValue, Float.class).floatValue());
         } catch (IllegalAccessException e) {
             throw toError(e);
         }
     }
 
     void generateGetDefaultValueIntoEnclosingGroup(final BytecodeCreator body, final ResultHandle enclosing,
-            final MethodDescriptor setter, final ResultHandle cache, final ResultHandle config) {
+            final MethodDescriptor setter, final ResultHandle config) {
         body.invokeStaticMethod(setter, enclosing,
-                body.invokeVirtualMethod(FLOAT_VALUE_METHOD, getConvertedDefault(body, cache, config)));
+                body.invokeVirtualMethod(FLOAT_VALUE_METHOD, getConvertedDefault(body, config)));
     }
 
     public ResultHandle writeInitialization(final BytecodeCreator body, final AccessorFinder accessorFinder,
-            final ResultHandle cache, final ResultHandle smallRyeConfig) {
-        return body.invokeVirtualMethod(FLOAT_VALUE_METHOD, getConvertedDefault(body, cache, smallRyeConfig));
+            final ResultHandle smallRyeConfig) {
+        return body.invokeVirtualMethod(FLOAT_VALUE_METHOD, getConvertedDefault(body, smallRyeConfig));
     }
 
-    private ResultHandle getConvertedDefault(final BytecodeCreator body, final ResultHandle cache, final ResultHandle config) {
+    private ResultHandle getConvertedDefault(final BytecodeCreator body, final ResultHandle config) {
         return body.checkCast(body.invokeVirtualMethod(
                 SRC_CONVERT_METHOD,
                 config,
-                cache == null ? body.load(defaultValue)
-                        : body.invokeStaticMethod(
-                                ConfigurationSetup.ECS_EXPAND_VALUE,
-                                body.load(defaultValue),
-                                cache),
+                body.load(defaultValue),
                 body.loadClass(Float.class)), Float.class);
     }
 }
--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/configuration/IntConfigType.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/configuration/IntConfigType.java
@@ -6,11 +6,9 @@ import java.util.OptionalInt;
 import org.wildfly.common.Assert;
 
 import io.quarkus.deployment.AccessorFinder;
-import io.quarkus.deployment.steps.ConfigurationSetup;
 import io.quarkus.gizmo.BytecodeCreator;
 import io.quarkus.gizmo.MethodDescriptor;
 import io.quarkus.gizmo.ResultHandle;
-import io.quarkus.runtime.configuration.ExpandingConfigSource;
 import io.quarkus.runtime.configuration.NameIterator;
 import io.smallrye.config.SmallRyeConfig;
 
@@ -31,22 +29,21 @@ public class IntConfigType extends LeafConfigType {
         this.defaultValue = defaultValue;
     }
 
-    public void acceptConfigurationValue(final NameIterator name, final ExpandingConfigSource.Cache cache,
-            final SmallRyeConfig config) {
+    public void acceptConfigurationValue(final NameIterator name, final SmallRyeConfig config) {
         final GroupConfigType container = getContainer(GroupConfigType.class);
         if (isConsumeSegment())
             name.previous();
-        container.acceptConfigurationValueIntoLeaf(this, name, cache, config);
+        container.acceptConfigurationValueIntoLeaf(this, name, config);
         // the iterator is not used after this point
         // if (isConsumeSegment()) name.next();
     }
 
     public void generateAcceptConfigurationValue(final BytecodeCreator body, final ResultHandle name,
-            final ResultHandle cache, final ResultHandle config) {
+            final ResultHandle config) {
         final GroupConfigType container = getContainer(GroupConfigType.class);
         if (isConsumeSegment())
             body.invokeVirtualMethod(NI_PREV_METHOD, name);
-        container.generateAcceptConfigurationValueIntoLeaf(body, this, name, cache, config);
+        container.generateAcceptConfigurationValueIntoLeaf(body, this, name, config);
         // the iterator is not used after this point
         // if (isConsumeSegment()) body.invokeVirtualMethod(NI_NEXT_METHOD, name);
     }
@@ -85,36 +82,30 @@ public class IntConfigType extends LeafConfigType {
         return int.class;
     }
 
-    void getDefaultValueIntoEnclosingGroup(final Object enclosing, final ExpandingConfigSource.Cache cache,
-            final SmallRyeConfig config, final Field field) {
+    void getDefaultValueIntoEnclosingGroup(final Object enclosing, final SmallRyeConfig config, final Field field) {
         try {
-            field.setInt(enclosing,
-                    config.convert(ExpandingConfigSource.expandValue(defaultValue, cache), Integer.class).intValue());
+            field.setInt(enclosing, config.convert(defaultValue, Integer.class).intValue());
         } catch (IllegalAccessException e) {
             throw toError(e);
         }
     }
 
     void generateGetDefaultValueIntoEnclosingGroup(final BytecodeCreator body, final ResultHandle enclosing,
-            final MethodDescriptor setter, final ResultHandle cache, final ResultHandle config) {
+            final MethodDescriptor setter, final ResultHandle config) {
         body.invokeStaticMethod(setter, enclosing,
-                body.invokeVirtualMethod(INT_VALUE_METHOD, getConvertedDefault(body, cache, config)));
+                body.invokeVirtualMethod(INT_VALUE_METHOD, getConvertedDefault(body, config)));
     }
 
     public ResultHandle writeInitialization(final BytecodeCreator body, final AccessorFinder accessorFinder,
-            final ResultHandle cache, final ResultHandle smallRyeConfig) {
-        return body.invokeVirtualMethod(INT_VALUE_METHOD, getConvertedDefault(body, cache, smallRyeConfig));
+            final ResultHandle smallRyeConfig) {
+        return body.invokeVirtualMethod(INT_VALUE_METHOD, getConvertedDefault(body, smallRyeConfig));
     }
 
-    private ResultHandle getConvertedDefault(final BytecodeCreator body, final ResultHandle cache, final ResultHandle config) {
+    private ResultHandle getConvertedDefault(final BytecodeCreator body, final ResultHandle config) {
         return body.checkCast(body.invokeVirtualMethod(
                 SRC_CONVERT_METHOD,
                 config,
-                cache == null ? body.load(defaultValue)
-                        : body.invokeStaticMethod(
-                                ConfigurationSetup.ECS_EXPAND_VALUE,
-                                body.load(defaultValue),
-                                cache),
+                body.load(defaultValue),
                 body.loadClass(Integer.class)), Integer.class);
     }
 }
