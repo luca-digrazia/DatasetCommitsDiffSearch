bec089d6e588a57e82f650061db3376ac231367e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:29:29 +0200

    avoid trying to load already deleted streams in message detail views. fixes #553

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/MessagesController.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/MessagesController.java
@@ -52,7 +52,6 @@ public class MessagesController extends AuthenticatedController {
     public Result show(String index, String id) {
         try {
             MessageResult message = messagesService.getMessage(index, id);
-            message = FieldMapper.run(message);
             Node sourceNode = getSourceNode(message);
             Radio sourceRadio = getSourceRadio(message);
 
