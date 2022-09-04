b6715a62f64aa0af1483204bed430d806191b617
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:45:56 +0200

    Include a list of dependencies in the app
        This will allow vulnerability scanners to easily determine if
        you application has any vulnerabilities.
    
        Note that the 'vulnerability scanner' bit is still very much
        a work in progress, however it makes more sense to include
        this information on our side, then work on the scanner
        once we have something it can work with.

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/pkg/PackageConfig.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/pkg/PackageConfig.java
@@ -3,7 +3,6 @@ package io.quarkus.deployment.pkg;
 import java.util.List;
 import java.util.Optional;
 
-import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigRoot;
 
@@ -21,14 +20,12 @@ public class PackageConfig {
     public static final String LEGACY = "legacy";
     public static final String LEGACY_JAR = "legacy-jar";
     public static final String NATIVE = "native";
-    // does everything 'native' but stops short of actually executing the 'native-image' command
-    public static final String NATIVE_SOURCES = "native-sources";
 
     /**
      * The requested output type.
-     * <p>
+     *
      * The default built in types are 'jar' (which will use 'fast-jar'), 'legacy-jar' for the pre-1.12 default jar
-     * packaging, 'uber-jar', 'native' and 'native-sources'.
+     * packaging, 'uber-jar' and 'native'.
      */
     @ConfigItem(defaultValue = JAR)
     public String type;
@@ -42,7 +39,7 @@ public class PackageConfig {
     /**
      * The entry point of the application. This can either be a a fully qualified name of a standard Java
      * class with a main method, or {@link io.quarkus.runtime.QuarkusApplication}.
-     * <p>
+     *
      * If your application has main classes annotated with {@link io.quarkus.runtime.annotations.QuarkusMain}
      * then this can also reference the name given in the annotation, to avoid the need to specify fully qualified
      * names in the config.
@@ -98,11 +95,11 @@ public class PackageConfig {
 
     /**
      * This is an advanced option that only takes effect for the mutable-jar format.
-     * <p>
+     *
      * If this is specified a directory of this name will be created in the jar distribution. Users can place
      * jar files in this directory, and when re-augmentation is performed these will be processed and added to the
      * class-path.
-     * <p>
+     *
      * Note that before reaugmentation has been performed these jars will be ignored, and if they are updated the app
      * should be reaugmented again.
      */
@@ -117,12 +114,6 @@ public class PackageConfig {
      */
     @ConfigItem(defaultValue = "true")
     public boolean includeDependencyList;
-
-    /**
-     * Fernflower Decompiler configuration
-     */
-    @ConfigItem
-    public FernflowerConfig fernflower;
 
     public boolean isAnyJarType() {
         return (type.equalsIgnoreCase(PackageConfig.JAR) ||
@@ -142,28 +133,5 @@ public class PackageConfig {
     public boolean isLegacyJar() {
         return (type.equalsIgnoreCase(PackageConfig.LEGACY_JAR) ||
                 type.equalsIgnoreCase(PackageConfig.LEGACY));
-    }
-
-    @ConfigGroup
-    public static class FernflowerConfig {
-
-        /**
-         * An advanced option that will decompile generated and transformed bytecode into the 'decompiled' directory.
-         * This is only taken into account when fast-jar is used.
-         */
-        @ConfigItem(defaultValue = "false")
-        public boolean enabled;
-
-        /**
-         * The git hash to use to download the fernflower tool from https://jitpack.io/com/github/fesh0r/fernflower/
-         */
-        @ConfigItem(defaultValue = "dbf407a655")
-        public String hash;
-
-        /**
-         * The directory into which to save the fernflower tool if it doesn't exist
-         */
-        @ConfigItem(defaultValue = "${user.home}/.quarkus")
-        public String jarDirectory;
     }
 }
--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/JarResultBuildStep.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/JarResultBuildStep.java
@@ -5,7 +5,6 @@ import static java.nio.file.StandardOpenOption.CREATE;
 import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 import static java.nio.file.StandardOpenOption.WRITE;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -20,7 +19,6 @@ import java.nio.file.FileVisitOption;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
@@ -82,7 +80,6 @@ import io.quarkus.deployment.pkg.builditem.LegacyJarRequiredBuildItem;
 import io.quarkus.deployment.pkg.builditem.NativeImageSourceJarBuildItem;
 import io.quarkus.deployment.pkg.builditem.OutputTargetBuildItem;
 import io.quarkus.deployment.pkg.builditem.UberJarRequiredBuildItem;
-import io.quarkus.deployment.util.FileUtil;
 
 /**
  * This build step builds both the thin jars and uber jars.
@@ -484,26 +481,6 @@ public class JarResultBuildStep {
         }
         Map<AppArtifactKey, List<Path>> copiedArtifacts = new HashMap<>();
 
-        Path fernflowerJar = null;
-        Path decompiledOutputDir = null;
-        boolean wasDecompiledSuccessfully = true;
-        if (packageConfig.fernflower.enabled) {
-            Path jarDirectory = Paths.get(packageConfig.fernflower.jarDirectory);
-            if (!Files.exists(jarDirectory)) {
-                Files.createDirectory(jarDirectory);
-            }
-            fernflowerJar = jarDirectory.resolve(String.format("fernflower-%s.jar", packageConfig.fernflower.hash));
-            if (!Files.exists(fernflowerJar)) {
-                boolean downloadComplete = downloadFernflowerJar(packageConfig, fernflowerJar);
-                if (!downloadComplete) {
-                    fernflowerJar = null; // will ensure that no decompilation takes place
-                }
-            }
-            decompiledOutputDir = buildDir.getParent().resolve("decompiled");
-            FileUtil.deleteDirectory(decompiledOutputDir);
-            Files.createDirectory(decompiledOutputDir);
-        }
-
         List<Path> jars = new ArrayList<>();
         List<Path> bootJars = new ArrayList<>();
         //we process in order of priority
@@ -522,9 +499,6 @@ public class JarResultBuildStep {
                         Files.write(target, transformed.getData());
                     }
                 }
-            }
-            if (fernflowerJar != null) {
-                wasDecompiledSuccessfully &= decompile(fernflowerJar, decompiledOutputDir, transformedZip);
             }
         }
         //now generated classes and resources
@@ -548,14 +522,6 @@ public class JarResultBuildStep {
                 Files.write(target, i.getClassData());
             }
         }
-        if (fernflowerJar != null) {
-            wasDecompiledSuccessfully &= decompile(fernflowerJar, decompiledOutputDir, generatedZip);
-        }
-
-        if (wasDecompiledSuccessfully && (decompiledOutputDir != null)) {
-            log.info("The decompiled output can be found at: " + decompiledOutputDir.toAbsolutePath().toString());
-        }
-
         //now the application classes
         Path runnerJar = appDir
                 .resolve(outputTargetBuildItem.getBaseName() + ".jar");
@@ -699,58 +665,6 @@ public class JarResultBuildStep {
             });
         }
         return new JarBuildItem(initJar, null, libDir, packageConfig.type, null);
-    }
-
-    private boolean downloadFernflowerJar(PackageConfig packageConfig, Path fernflowerJar) {
-        String downloadURL = String.format("https://jitpack.io/com/github/fesh0r/fernflower/%s/fernflower-%s.jar",
-                packageConfig.fernflower.hash, packageConfig.fernflower.hash);
-        try (BufferedInputStream in = new BufferedInputStream(new URL(downloadURL).openStream());
-                FileOutputStream fileOutputStream = new FileOutputStream(fernflowerJar.toFile())) {
-            byte[] dataBuffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-            }
-            return true;
-        } catch (IOException e) {
-            log.error("Unable to download Fernflower from " + downloadURL, e);
-            return false;
-        }
-    }
-
-    private boolean decompile(Path fernflowerJar, Path decompiledOutputDir, Path jarToDecompile) {
-        int exitCode;
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder(
-                    Arrays.asList("java", "-jar", fernflowerJar.toAbsolutePath().toString(),
-                            jarToDecompile.toAbsolutePath().toString(), decompiledOutputDir.toAbsolutePath().toString()));
-            if (log.isDebugEnabled()) {
-                processBuilder.inheritIO();
-            } else {
-                processBuilder.redirectError(ProcessBuilder.Redirect.DISCARD.file())
-                        .redirectOutput(ProcessBuilder.Redirect.DISCARD.file());
-            }
-            exitCode = processBuilder.start().waitFor();
-        } catch (Exception e) {
-            log.error("Failed to launch Fernflower decompiler.", e);
-            return false;
-        }
-
-        if (exitCode != 0) {
-            log.errorf("Fernflower decompiler exited with error code: %d.", exitCode);
-            return false;
-        }
-
-        String jarFileName = jarToDecompile.getFileName().toString();
-        Path decompiledJar = decompiledOutputDir.resolve(jarFileName);
-        try {
-            ZipUtils.unzip(decompiledJar, decompiledOutputDir.resolve(jarFileName.replace(".jar", "")));
-            Files.deleteIfExists(decompiledJar);
-        } catch (IOException ignored) {
-            // it doesn't really matter if we can't unzip the jar as we do it merely for user convenience
-        }
-
-        return true;
     }
 
     private void copyDependency(CurateOutcomeBuildItem curateOutcomeBuildItem, Map<AppArtifactKey, List<Path>> runtimeArtifacts,
