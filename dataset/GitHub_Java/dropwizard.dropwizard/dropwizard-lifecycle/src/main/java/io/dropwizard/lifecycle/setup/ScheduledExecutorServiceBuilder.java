08aeddd3f163116bd07433c0b10f8c42757934f6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:09:04 +0200

    Reuse DefaultThreadFactory (#3285)

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-lifecycle/src/main/java/io/dropwizard/lifecycle/setup/ScheduledExecutorServiceBuilder.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-lifecycle/src/main/java/io/dropwizard/lifecycle/setup/ScheduledExecutorServiceBuilder.java
@@ -1,26 +1,53 @@
 package io.dropwizard.lifecycle.setup;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.codahale.metrics.InstrumentedThreadFactory;
 import io.dropwizard.lifecycle.ExecutorServiceManager;
 import io.dropwizard.util.Duration;
 
-import java.util.concurrent.*;
+import java.util.Locale;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ScheduledExecutorServiceBuilder {
+
+    private static final AtomicLong COUNT = new AtomicLong(0);
     private final LifecycleEnvironment environment;
     private final String nameFormat;
     private int poolSize;
     private ThreadFactory threadFactory;
     private Duration shutdownTime;
     private RejectedExecutionHandler handler;
+    private boolean removeOnCancel;
 
-    public ScheduledExecutorServiceBuilder(LifecycleEnvironment environment, String nameFormat) {
+    public ScheduledExecutorServiceBuilder(LifecycleEnvironment environment, String nameFormat, ThreadFactory factory) {
         this.environment = environment;
         this.nameFormat = nameFormat;
         this.poolSize = 1;
-        this.threadFactory = new ThreadFactoryBuilder().setNameFormat(nameFormat).build();
+        this.threadFactory = factory;
         this.shutdownTime = Duration.seconds(5);
         this.handler = new ThreadPoolExecutor.AbortPolicy();
+        this.removeOnCancel = false;
+    }
+
+    public ScheduledExecutorServiceBuilder(LifecycleEnvironment environment, String nameFormat, boolean useDaemonThreads) {
+        this(environment, nameFormat, buildThreadFactory(nameFormat, useDaemonThreads));
+    }
+
+    private static ThreadFactory buildThreadFactory(String nameFormat, boolean daemon) {
+        ThreadFactory defaultThreadFactory = Executors.defaultThreadFactory();
+        return r -> {
+            final Thread thread = defaultThreadFactory.newThread(r);
+            if (nameFormat != null) {
+                thread.setName(String.format(Locale.ROOT, nameFormat, COUNT.incrementAndGet()));
+            }
+            thread.setDaemon(daemon);
+            return thread;
+        };
     }
 
     public ScheduledExecutorServiceBuilder threads(int threads) {
@@ -43,8 +70,18 @@ public class ScheduledExecutorServiceBuilder {
         return this;
     }
 
+    public ScheduledExecutorServiceBuilder removeOnCancelPolicy(boolean removeOnCancel) {
+        this.removeOnCancel = removeOnCancel;
+        return this;
+    }
+
     public ScheduledExecutorService build() {
-        final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(poolSize, threadFactory, handler);
+        final InstrumentedThreadFactory instrumentedThreadFactory = new InstrumentedThreadFactory(threadFactory,
+            environment.getMetricRegistry(), nameFormat);
+
+        final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(poolSize, instrumentedThreadFactory, handler);
+        executor.setRemoveOnCancelPolicy(removeOnCancel);
+
         environment.manage(new ExecutorServiceManager(executor, shutdownTime, nameFormat));
         return executor;
     }
