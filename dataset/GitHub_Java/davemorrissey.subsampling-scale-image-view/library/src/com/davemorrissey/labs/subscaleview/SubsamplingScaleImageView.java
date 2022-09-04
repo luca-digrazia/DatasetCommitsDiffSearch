da8e136c3dc5aeb1bc644106fb955a50983c17ec
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:26:48 +0200

    Debug logging

--- dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
+++ dataset/GitHub_Java/davemorrissey.subsampling-scale-image-view/library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
@@ -1528,9 +1528,7 @@ public class SubsamplingScaleImageView extends View {
         this.sHeight = sHeight;
         this.sOrientation = sOrientation;
         checkReady();
-        if (!checkImageLoaded() && maxTileWidth > 0 && maxTileHeight > 0 && getWidth() > 0 && getHeight() > 0) {
-            initialiseBaseLayer(new Point(maxTileWidth, maxTileHeight));
-        }
+        checkImageLoaded();
         invalidate();
         requestLayout();
     }
