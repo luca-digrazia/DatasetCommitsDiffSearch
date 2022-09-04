c881982989e8190ff8d95fd5f1fea0028b8d4446
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:00:07 +0200

    Update test with Stage for Hibrnate Reactive
        The test with the injected session is not working
        but I've added another one that uses the SessionFactory
        instead.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-reactive/deployment/src/test/java/io/quarkus/hibernate/reactive/singlepersistenceunit/SinglePersistenceUnitCdiStageSessionTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-reactive/deployment/src/test/java/io/quarkus/hibernate/reactive/singlepersistenceunit/SinglePersistenceUnitCdiStageSessionTest.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import org.hibernate.reactive.stage.Stage;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -24,15 +25,31 @@ public class SinglePersistenceUnitCdiStageSessionTest {
     @Inject
     Stage.Session session;
 
+    @Inject
+    Stage.SessionFactory sessionFactory;
+
     @Test
+    @ActivateRequestContext
+    public void testWithFactory() {
+        DefaultEntity entity = new DefaultEntity("default");
+
+        DefaultEntity retrievedEntity = sessionFactory.withTransaction((session, tx) -> session.persist(entity))
+                .thenCompose(v -> sessionFactory.withSession(session -> session.find(DefaultEntity.class, entity.getId())))
+                .toCompletableFuture().join();
+
+        assertThat(retrievedEntity)
+                .isNotSameAs(entity)
+                .returns(entity.getName(), DefaultEntity::getName);
+    }
+
+    @Test
+    @Disabled("#14812: We're getting a ContextNotActiveException for some (unknown) reason")
     @ActivateRequestContext
     public void test() {
         DefaultEntity entity = new DefaultEntity("default");
 
-        DefaultEntity retrievedEntity = session.persist(entity)
-                .thenCompose($ -> session.flush())
-                .thenApply($ -> session.clear())
-                .thenCompose($ -> session.find(DefaultEntity.class, entity.getId()))
+        DefaultEntity retrievedEntity = session.withTransaction(tx -> session.persist(entity))
+                .thenCompose($ -> session.withTransaction(tx -> session.clear().find(DefaultEntity.class, entity.getId())))
                 .toCompletableFuture().join();
 
         assertThat(retrievedEntity)
