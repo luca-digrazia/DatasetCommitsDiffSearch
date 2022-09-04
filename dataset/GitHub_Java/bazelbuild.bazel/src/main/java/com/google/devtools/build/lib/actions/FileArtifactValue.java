d1554c81e21a2dbec9a5becb5064c0c464b2b80b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:06:54 +0200

    Add a method in FileArtifactValue to create a new instance given an existing FileStatus.
        PiperOrigin-RevId: 201683773

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/FileArtifactValue.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/FileArtifactValue.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.actions;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
-import com.google.common.hash.Hashing;
 import com.google.common.io.BaseEncoding;
 import com.google.devtools.build.lib.actions.cache.DigestUtils;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
@@ -398,10 +397,6 @@ public abstract class FileArtifactValue implements SkyValue {
     public InlineFileArtifactValue(byte[] data, byte[] digest) {
       this.data = Preconditions.checkNotNull(data);
       this.digest = Preconditions.checkNotNull(digest);
-    }
-
-    public InlineFileArtifactValue(byte[] bytes) {
-      this(bytes, Hashing.md5().hashBytes(bytes).asBytes());
     }
 
     public ByteArrayInputStream getInputStream() {
