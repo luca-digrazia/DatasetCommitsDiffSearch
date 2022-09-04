4d44a5dc67c535a612c5dbbff5e17564df9b5400
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:20:52 +0200

    don't log a line every time the checks detects the condition.
        only log when the notification wasn't there before so we don't flood the log unnecessarily
        also mention the threshold value in the log line
        increase the level from info to warn because it really is a warning

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/periodical/IndexerClusterCheckerThread.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/periodical/IndexerClusterCheckerThread.java
@@ -1,28 +1,7 @@
-/*
- * Copyright 2012-2014 TORCH GmbH
- *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package org.graylog2.periodical;
 
-import com.google.inject.Inject;
 import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
 import org.graylog2.notifications.Notification;
-import org.graylog2.notifications.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,16 +14,9 @@ public class IndexerClusterCheckerThread extends Periodical {
 
     public static final int MINIMUM_OPEN_FILES_LIMIT = 64000;
 
-    private NotificationService notificationService;
-
-    @Inject
-    public IndexerClusterCheckerThread(NotificationService notificationService) {
-        this.notificationService = notificationService;
-    }
-
     @Override
     public void run() {
-        if (!notificationService.isFirst(Notification.Type.ES_OPEN_FILES))
+        if (!Notification.isFirst(core, Notification.Type.ES_OPEN_FILES))
             return;
         boolean allHigher = true;
         for (NodeInfo node : core.getIndexer().cluster().getDataNodes()) {
@@ -52,10 +24,10 @@ public class IndexerClusterCheckerThread extends Periodical {
             if (node.getProcess().getMaxFileDescriptors() < MINIMUM_OPEN_FILES_LIMIT) {
 
                 // Write notification.
-                 Notification notification = notificationService.buildNow()
+                final boolean published = Notification.buildNow(core)
                         .addType(Notification.Type.ES_OPEN_FILES)
-                        .addSeverity(Notification.Severity.URGENT);
-                final boolean published = notificationService.publishIfFirst(notification);
+                        .addSeverity(Notification.Severity.URGENT)
+                        .publishIfFirst();
                 if (published) {
                     LOG.warn("Indexer node <{}> open file limit is too low: [{}]. Set it to at least {}.",
                              new Object[] {
@@ -68,8 +40,7 @@ public class IndexerClusterCheckerThread extends Periodical {
             }
         }
         if (allHigher) {
-            Notification notification = notificationService.build().addType(Notification.Type.ES_OPEN_FILES);
-            notificationService.fixed(notification);
+            Notification.build(core).addType(Notification.Type.ES_OPEN_FILES).fixed();
         }
     }
 
