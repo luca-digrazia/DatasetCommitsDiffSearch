fd849d6655f4f3ac84680d8fbebce2211c05b24c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:45:48 +0200

    Removing Nullable as property is set in creator to non null value.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/streams/requests/CreateStreamRequest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/streams/requests/CreateStreamRequest.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of Graylog2.
+ * This file is part of Graylog.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.rest.resources.streams.requests;
 
@@ -20,7 +20,9 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
+import org.graylog2.plugin.streams.Stream;
 import org.graylog2.rest.resources.streams.rules.requests.CreateStreamRuleRequest;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -32,20 +34,26 @@ public abstract class CreateStreamRequest {
     public abstract String title();
 
     @JsonProperty
+    @Nullable
     public abstract String description();
 
     @JsonProperty
+    @Nullable
     public abstract List<CreateStreamRuleRequest> rules();
 
     @JsonProperty
     @Nullable
     public abstract String contentPack();
 
+    @JsonProperty
+    public abstract Stream.MatchingType matchingType();
+
     @JsonCreator
-    public static CreateStreamRequest create(@JsonProperty("title") String title,
-                                             @JsonProperty("description") String description,
-                                             @JsonProperty("rules") List<CreateStreamRuleRequest> rules,
-                                             @JsonProperty("content_pack") @Nullable String contentPack) {
-        return new AutoValue_CreateStreamRequest(title, description, rules, contentPack);
+    public static CreateStreamRequest create(@JsonProperty("title") @NotEmpty String title,
+                                             @JsonProperty("description") @Nullable String description,
+                                             @JsonProperty("rules") @Nullable List<CreateStreamRuleRequest> rules,
+                                             @JsonProperty("content_pack") @Nullable String contentPack,
+                                             @JsonProperty("matching_type") @Nullable String matchingType) {
+        return new AutoValue_CreateStreamRequest(title, description, rules, contentPack, Stream.MatchingType.valueOfOrDefault(matchingType));
     }
 }
