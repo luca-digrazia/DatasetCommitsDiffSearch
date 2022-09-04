f4a1a0f48723283b4b37c6e232d6d85c6ed900a5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:06:58 +0200

    Update javadoc for resource injection and prefs annotations

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/res/IntArrayRes.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/res/IntArrayRes.java
@@ -24,8 +24,7 @@ import org.androidannotations.annotations.ResId;
 
 /**
  * Use on {@link java.lang.Integer} or <code>int</code> array fields in any
- * enhanced classes that should be injected with this specific int array
- * resource.
+ * enhanced classes that should be injected with this specific color resource.
  * <p/>
  * The annotation value must be one of R.array.* fields. If the value is not
  * set, the field name will be used as the R.array.* field name.
