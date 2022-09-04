0888539c0b1e7658e7d6abc2746868f74e7aad31
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:25:43 +0200

    Support repeatable QuarkusTestResource annotation
        - resolves #5256

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/TestResourceManager.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/TestResourceManager.java
@@ -1,7 +1,18 @@
 package io.quarkus.test.common;
 
-import java.io.Closeable;
+import static io.quarkus.test.common.PathTestHelper.getTestClassesLocation;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -12,12 +23,12 @@ import java.util.Map;
 import java.util.ServiceLoader;
 import java.util.Set;
 
-import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.IndexView;
+import org.jboss.jandex.Indexer;
 
-public class TestResourceManager implements Closeable {
+public class TestResourceManager {
 
     private final List<QuarkusTestResourceLifecycleManager> testResources;
     private Map<String, String> oldSystemProps;
@@ -30,10 +41,7 @@ public class TestResourceManager implements Closeable {
         Map<String, String> ret = new HashMap<>();
         for (QuarkusTestResourceLifecycleManager testResource : testResources) {
             try {
-                Map<String, String> start = testResource.start();
-                if (start != null) {
-                    ret.putAll(start);
-                }
+                ret.putAll(testResource.start());
             } catch (Exception e) {
                 throw new RuntimeException("Unable to start Quarkus test resource " + testResource, e);
             }
@@ -56,7 +64,7 @@ public class TestResourceManager implements Closeable {
         }
     }
 
-    public void close() {
+    public void stop() {
         if (oldSystemProps != null) {
             for (Map.Entry<String, String> e : oldSystemProps.entrySet()) {
                 if (e.getValue() == null) {
@@ -75,16 +83,11 @@ public class TestResourceManager implements Closeable {
                 throw new RuntimeException("Unable to stop Quarkus test resource " + testResource, e);
             }
         }
-        try {
-            ConfigProviderResolver cpr = ConfigProviderResolver.instance();
-            cpr.releaseConfig(cpr.getConfig());
-        } catch (Throwable ignored) {
-        }
     }
 
     @SuppressWarnings("unchecked")
     private List<QuarkusTestResourceLifecycleManager> getTestResources(Class<?> testClass) {
-        IndexView index = TestClassIndexer.readIndex(testClass);
+        IndexView index = indexTestClasses(testClass);
 
         Set<Class<? extends QuarkusTestResourceLifecycleManager>> testResourceRunnerClasses = new LinkedHashSet<>();
 
@@ -97,7 +100,7 @@ public class TestResourceManager implements Closeable {
         for (AnnotationInstance annotation : testResourceAnnotations) {
             try {
                 testResourceRunnerClasses.add((Class<? extends QuarkusTestResourceLifecycleManager>) Class
-                        .forName(annotation.value().asString(), true, Thread.currentThread().getContextClassLoader()));
+                        .forName(annotation.value().asString()));
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException("Unable to find the class for the test resource " + annotation.value().asString());
             }
@@ -115,7 +118,7 @@ public class TestResourceManager implements Closeable {
         }
 
         for (QuarkusTestResourceLifecycleManager quarkusTestResourceLifecycleManager : ServiceLoader
-                .load(QuarkusTestResourceLifecycleManager.class, Thread.currentThread().getContextClassLoader())) {
+                .load(QuarkusTestResourceLifecycleManager.class)) {
             testResourceRunners.add(quarkusTestResourceLifecycleManager);
         }
 
@@ -124,4 +127,55 @@ public class TestResourceManager implements Closeable {
         return testResourceRunners;
     }
 
+    private IndexView indexTestClasses(Class<?> testClass) {
+        final Indexer indexer = new Indexer();
+        final Path testClassesLocation = getTestClassesLocation(testClass);
+        try {
+            if (Files.isDirectory(testClassesLocation)) {
+                indexTestClassesDir(indexer, testClassesLocation);
+            } else {
+                try (FileSystem jarFs = FileSystems.newFileSystem(testClassesLocation, null)) {
+                    for (Path p : jarFs.getRootDirectories()) {
+                        indexTestClassesDir(indexer, p);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to index the test-classes/ directory.", e);
+        }
+        return indexer.complete();
+    }
+
+    private void indexTestClassesDir(Indexer indexer, final Path testClassesLocation) throws IOException {
+        Files.walkFileTree(testClassesLocation, new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                    throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (!file.toString().endsWith(".class")) {
+                    return FileVisitResult.CONTINUE;
+                }
+                try (InputStream inputStream = Files.newInputStream(file, StandardOpenOption.READ)) {
+                    indexer.index(inputStream);
+                } catch (Exception e) {
+                    // ignore
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
 }
