ed10befc0052fe05d477cf46ec5d73c4e281f505
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:56:29 +0200

    When disposing of a Mutiny.Session on a vert.x thread don't block it

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-reactive/runtime/src/main/java/io/quarkus/hibernate/reactive/runtime/ReactiveSessionProducer.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/hibernate-reactive/runtime/src/main/java/io/quarkus/hibernate/reactive/runtime/ReactiveSessionProducer.java
@@ -1,55 +1,47 @@
 package io.quarkus.hibernate.reactive.runtime;
 
-import java.util.concurrent.CompletionStage;
+import java.util.concurrent.CompletableFuture;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
 import org.hibernate.reactive.mutiny.Mutiny;
-import org.hibernate.reactive.stage.Stage;
 
 import io.quarkus.arc.DefaultBean;
-import io.smallrye.mutiny.Uni;
 
-@ApplicationScoped
 public class ReactiveSessionProducer {
 
     @Inject
-    private Stage.SessionFactory reactiveSessionFactory;
-
-    @Inject
-    private Mutiny.SessionFactory mutinySessionFactory;
+    Mutiny.SessionFactory mutinySessionFactory;
 
     @Produces
     @RequestScoped
     @DefaultBean
-    public CompletionStage<Stage.Session> stageSession() {
-        return reactiveSessionFactory.openSession();
-    }
-
-    @Produces
-    @RequestScoped
-    @DefaultBean
-    public Uni<Mutiny.Session> mutinySession() {
+    public Mutiny.Session createMutinySession() {
         return mutinySessionFactory.openSession();
     }
 
-    public void disposeStageSession(@Disposes CompletionStage<Stage.Session> reactiveSession) {
-        reactiveSession.whenComplete((s, t) -> {
-            if (s != null)
-                s.close();
-        });
-    }
-
-    public void disposeMutinySession(@Disposes Uni<Mutiny.Session> reactiveSession) {
-    	// TODO: @AGG this is not working, need to check w/ Clement to figure out the proper way
-        reactiveSession.on().termination((session, ex, cancelled) -> {
-            if (session != null)
-                session.close();
-        });
+    public void disposeMutinySession(@Disposes Mutiny.Session reactiveSession) {
+        if (reactiveSession != null) {
+            //N.B. make sure to subscribe as this is a Mutiny based Session:
+            // operations don't happen at all if there is no subscription.
+            final CompletableFuture<Void> closeOperation = reactiveSession.close()
+                    .subscribe()
+                    .asCompletionStage();
+            if (!io.vertx.core.Context.isOnVertxThread()) {
+                //When invoked from blocking code, behave as expected and block on the operation
+                //so to not starve resources with a deferred close.
+                closeOperation.join();
+            }
+            // [else] no need to block. Worst that can happen is that the opening
+            // of a new Mutiny.Session needs to wait for an available connection,
+            // which implicitly orders it as "downstream" from the previous close
+            // to have actually happened as the connection pool is reactive.
+            // Also, if connections are available there is no real need to wait for
+            // it, so this should be good.
+        }
     }
 
 }
