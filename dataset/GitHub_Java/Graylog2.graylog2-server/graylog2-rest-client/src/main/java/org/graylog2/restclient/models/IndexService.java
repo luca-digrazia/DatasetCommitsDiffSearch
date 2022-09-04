14bc6c20d9133be7e000555be817314a506ce850
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:29:05 +0200

    Merge pull request #1532 from Graylog2/recalculate-single-index-range
        Recalculate single index range
        (cherry picked from commit fc53ef329e1229585c189ca77af7d41f0c9954c7)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/IndexService.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/models/IndexService.java
@@ -1,40 +1,42 @@
 /**
- * Copyright 2013 Lennart Koopmann <lennart@torch.sh>
+ * This file is part of Graylog.
  *
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.restclient.models;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
-import com.google.inject.Inject;
+import org.graylog2.rest.models.system.indexer.responses.AllIndicesInfo;
+import org.graylog2.rest.models.system.indexer.responses.IndexInfo;
+import org.graylog2.rest.models.system.indexer.responses.IndexRangeSummary;
+import org.graylog2.rest.models.system.indexer.responses.IndexRangesResponse;
 import org.graylog2.restclient.lib.APIException;
 import org.graylog2.restclient.lib.ApiClient;
-import org.graylog2.restclient.models.api.responses.system.indices.*;
+import org.graylog2.restclient.models.api.responses.system.indices.ClosedIndicesResponse;
+import org.graylog2.restclient.models.api.responses.system.indices.DeflectorConfigResponse;
+import org.graylog2.restclient.models.api.responses.system.indices.DeflectorInformationResponse;
 import org.graylog2.restroutes.generated.routes;
 
+import javax.inject.Inject;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.graylog2.restclient.lib.Configuration.apiTimeout;
 
-/**
- * @author Lennart Koopmann <lennart@torch.sh>
- */
 public class IndexService {
 
     private final ApiClient api;
@@ -52,31 +54,55 @@ public class IndexService {
         IndexRangesResponse rr = api.path(routes.IndexRangesResource().list(), IndexRangesResponse.class)
                 .execute();
 
-        for (IndexRangeSummary range : rr.ranges) {
+        for (IndexRangeSummary range : rr.ranges()) {
             indices.add(indexFactory.fromRangeResponse(range));
         }
 
         return indices;
     }
 
+    public IndexInfo indexInfo(String index) throws APIException, IOException {
+        return api.path(routes.IndicesResource().single(index), IndexInfo.class).execute();
+    }
+
+    public Map<String, IndexInfo> allIndicesInfo() throws APIException, IOException {
+        final AllIndicesInfo allIndicesInfo = api.path(routes.IndicesResource().all(), AllIndicesInfo.class).execute();
+
+        return ImmutableMap.copyOf(allIndicesInfo.indices());
+    }
+
     public DeflectorInformationResponse getDeflectorInfo() throws APIException, IOException {
         return api.path(routes.DeflectorResource().deflector(), DeflectorInformationResponse.class)
+                .timeout(apiTimeout("deflector_info", 20, TimeUnit.SECONDS))
                 .execute();
     }
 
     public DeflectorConfigResponse getDeflectorConfig() throws APIException, IOException {
         return api.path(routes.DeflectorResource().config(), DeflectorConfigResponse.class)
+                .timeout(apiTimeout("deflector_config", 60, TimeUnit.SECONDS))
                 .onlyMasterNode()
                 .execute();
     }
 
     public ClosedIndicesResponse getClosedIndices() throws APIException, IOException {
         return api.path(routes.IndicesResource().closed(), ClosedIndicesResponse.class)
+                .timeout(apiTimeout("closed_indices", 60, TimeUnit.SECONDS))
+                .execute();
+    }
+
+    public ClosedIndicesResponse getReopenedIndices() throws APIException, IOException {
+        return api.path(routes.IndicesResource().reopened(), ClosedIndicesResponse.class)
                 .execute();
     }
 
     public void recalculateRanges() throws APIException, IOException {
         api.path(routes.IndexRangesResource().rebuild())
+                .expect(202)
+                .execute();
+    }
+
+    public void recalculateIndexRange(String index) throws APIException, IOException {
+        api.path(routes.IndexRangesResource().rebuildIndex(index))
                 .expect(202)
                 .execute();
     }
@@ -106,10 +132,9 @@ public class IndexService {
 
     // Not part an Index model instance method because opening/closing can be applied to indices without calculated ranges.
     public void delete(String index) throws APIException, IOException {
-        api.path(routes.IndicesResource().single(index))
+        api.path(routes.IndicesResource().delete(index))
                 .timeout(apiTimeout("index_delete", 60, TimeUnit.SECONDS))
                 .expect(204)
                 .execute();
     }
-
 }
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-routes/src/main/java/org/graylog2/restroutes/internal/RouteClassGenerator.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-routes/src/main/java/org/graylog2/restroutes/internal/RouteClassGenerator.java
@@ -1,6 +1,43 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.restroutes.internal;
 
-import com.sun.codemodel.*;
+import com.sun.codemodel.JBlock;
+import com.sun.codemodel.JClassAlreadyExistsException;
+import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
 import org.graylog2.restroutes.PathMethod;
 
 import javax.ws.rs.PathParam;
@@ -34,8 +71,7 @@ public class RouteClassGenerator {
             for (Map.Entry<PathParam, Class<?>> entry : route.getPathParams().entrySet()) {
                 String fieldName = entry.getKey().value();
                 method.param(entry.getValue(), fieldName);
-                path = path.replace("{" + fieldName + "}", "\"+"+fieldName+"+\"");
-
+                path = path.replaceAll("\\{" + fieldName + "(:?[^}]*)\\}", "\"+com.google.common.net.UrlEscapers.urlPathSegmentEscaper().escape(" + fieldName + ")+\"");
             }
             JBlock block = method.body();
             block.directStatement("return new PathMethod(\"" + route.getHttpMethod() + "\", \"" + path + "\");");
