8aa80deb930d5e55ee8fd13a10f766bf94656a27
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:39:58 +0200

    Adding Factory to Stream class utilizing Assisted Injection
        Using this, we are able to inject UserService class which allows us to
        return a User object in getCreatorUser.

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/models/ModelFactoryModule.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/models/ModelFactoryModule.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2013 Kay Roepke <kay@torch.sh>
+/*
+ * Copyright 2013 TORCH UG
  *
  * This file is part of Graylog2.
  *
@@ -15,12 +15,13 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 package models;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import models.accounts.LdapSettings;
+import models.dashboards.Dashboard;
 
 /**
  * Provides the bindings for the factories of our models to avoid having lots of static methods everywhere.
@@ -29,7 +30,14 @@ public class ModelFactoryModule extends AbstractModule {
     @Override
     protected void configure() {
         install(new FactoryModuleBuilder().build(Node.Factory.class));
+        install(new FactoryModuleBuilder().build(Input.Factory.class));
         install(new FactoryModuleBuilder().build(SystemJob.Factory.class));
+        install(new FactoryModuleBuilder().build(UniversalSearch.Factory.class));
+        install(new FactoryModuleBuilder().build(User.Factory.class));
+        install(new FactoryModuleBuilder().build(Extractor.Factory.class));
+        install(new FactoryModuleBuilder().build(Dashboard.Factory.class));
+        install(new FactoryModuleBuilder().build(LdapSettings.Factory.class));
+        install(new FactoryModuleBuilder().build(Stream.Factory.class));
         // TODO crutch, because we need the factory for systemjobs in all().
         // can this be done with a second factory for the list?
         // or possibly with a factory method returning List<SystemJob> ?
--- dataset/GitHub_Java/Graylog2.graylog2-server/app/models/Stream.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/models/Stream.java
@@ -1,26 +1,91 @@
+/*
+ * Copyright 2013 TORCH UG
+ *
+ * This file is part of Graylog2.
+ *
+ * Graylog2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package models;
 
-import java.io.IOException;
-import java.net.URL;
+import com.google.common.collect.Lists;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+import models.api.responses.streams.StreamRuleSummaryResponse;
+import models.api.responses.streams.StreamSummaryResponse;
+import models.api.responses.TimestampResponse;
+import org.joda.time.DateTime;
 
-import lib.APIException;
-import lib.Api;
-import models.api.responses.GetStreamsResponse;
-import models.api.responses.StreamSummaryResponse;
-import models.api.results.StreamsResult;
+import java.util.List;
 
 public class Stream {
+
+    public interface Factory {
+        public Stream fromSummaryResponse(StreamSummaryResponse ssr);
+    }
 	
 	private final String id;
+    private final String title;
+    private final String creatorUserId;
+    private final String createdAt;
+    private final List<StreamRule> streamRules;
+    private final Boolean disabled;
 
-	public Stream(StreamSummaryResponse ssr) {
+    private UserService userService;
+
+	@AssistedInject
+    private Stream(UserService userService, @Assisted StreamSummaryResponse ssr) {
 		this.id = ssr.id;
+        this.title = ssr.title;
+        this.creatorUserId = ssr.creatorUserId;
+        this.createdAt = ssr.createdAt;
+
+        this.streamRules = Lists.newArrayList();
+
+        this.disabled = ssr.disabled;
+
+        this.userService = userService;
+
+        for (StreamRuleSummaryResponse streamRuleSummaryResponse : ssr.streamRules) {
+            streamRules.add(new StreamRule(streamRuleSummaryResponse));
+        }
 	}
 
-	public static StreamsResult allEnabled() throws IOException, APIException {
-		GetStreamsResponse r = Api.get("streams", GetStreamsResponse.class);
-		
-		return new StreamsResult(r.total, r.streams);
-	}
-	
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCreatorUserId() {
+        return creatorUserId;
+    }
+
+    public User getCreatorUser() {
+        return userService.load(this.creatorUserId);
+    }
+
+    public DateTime getCreatedAt() {
+        return DateTime.parse(createdAt);
+    }
+
+    public List<StreamRule> getStreamRules() {
+        return streamRules;
+    }
+
+    public Boolean getDisabled() {
+        return (disabled != null && disabled);
+    }
 }
