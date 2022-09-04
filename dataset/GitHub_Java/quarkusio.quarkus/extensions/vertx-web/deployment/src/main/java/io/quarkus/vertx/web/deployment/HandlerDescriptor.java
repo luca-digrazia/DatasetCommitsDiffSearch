3bede6fb9d7d3035e70d807d1e4500fc05547663
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:42:13 +0200

    Use DotNames for BeanValidationAnnotationsBuildItem
        Per discussion with Martin and ClÃ©ment.

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-web/deployment/src/main/java/io/quarkus/vertx/web/deployment/HandlerDescriptor.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/vertx-web/deployment/src/main/java/io/quarkus/vertx/web/deployment/HandlerDescriptor.java
@@ -1,6 +1,7 @@
 package io.quarkus.vertx.web.deployment;
 
 import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.DotName;
 import org.jboss.jandex.MethodInfo;
 import org.jboss.jandex.Type;
 
@@ -100,7 +101,8 @@ class HandlerDescriptor {
         if (type == null) {
             return false;
         }
-        return type.name().equals(DotNames.RX_BUFFER);
+        return type.name()
+                .equals(DotName.createSimple(io.vertx.reactivex.core.buffer.Buffer.class.getName()));
     }
 
     boolean isContentTypeMutinyBuffer() {
@@ -108,7 +110,7 @@ class HandlerDescriptor {
         if (type == null) {
             return false;
         }
-        return type.name().equals(DotNames.MUTINY_BUFFER);
+        return type.name().equals(DotName.createSimple(io.vertx.mutiny.core.buffer.Buffer.class.getName()));
     }
 
 }
