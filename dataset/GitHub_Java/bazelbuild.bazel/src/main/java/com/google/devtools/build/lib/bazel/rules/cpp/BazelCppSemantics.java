360cd02db1bea6c0755f27c2b663d04a52a9a8d5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 01:51:55 +0200

    Rollback of commit 686fde1b1aeb4e5d03dca7446cf39cd9f1114d79.
        *** Reason for rollback ***
    
        Breaks  //src/test/shell/bazel:bazel_sandboxing_cpp_test on all platforms: http://ci.bazel.io/job/bazel-tests/567/BAZEL_VERSION=HEAD,PLATFORM_NAME=linux-x86_64/
    
        *** Original change description ***
    
        Disable .d input pruning
    
        My fix for input discovery is pending some changes by kchodorow, and this is
        blocking the next release.
    
        Temporary workaround for #2490.
    
        --
        PiperOrigin-RevId: 147725685
        MOS_MIGRATED_REVID=147725685

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
@@ -25,7 +25,6 @@ import com.google.devtools.build.lib.rules.cpp.CppCompileActionContext;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration.HeadersCheckingMode;
 import com.google.devtools.build.lib.rules.cpp.CppSemantics;
-import com.google.devtools.build.lib.rules.cpp.FeatureSpecification;
 import com.google.devtools.build.lib.rules.cpp.IncludeProcessing;
 import com.google.devtools.build.lib.rules.cpp.NoProcessing;
 import com.google.devtools.build.lib.vfs.PathFragment;
@@ -49,9 +48,7 @@ public class BazelCppSemantics implements CppSemantics {
 
   @Override
   public void finalizeCompileActionBuilder(
-      RuleContext ruleContext,
-      CppCompileActionBuilder actionBuilder,
-      FeatureSpecification featureSpecification) {
+      RuleContext ruleContext, CppCompileActionBuilder actionBuilder) {
     actionBuilder.setCppConfiguration(ruleContext.getFragment(CppConfiguration.class));
     actionBuilder.setActionContext(CppCompileActionContext.class);
     // Because Bazel does not support include scanning, we need the entire crosstool filegroup,
