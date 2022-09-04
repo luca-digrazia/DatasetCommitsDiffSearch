4cae696e208dfbbc61c5c0fb0878fc3cf0f56b85
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:01:56 +0200

    Enlist handlers for methods that have an HTTP method annotation

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/startup/RuntimeResourceDeployment.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/startup/RuntimeResourceDeployment.java
@@ -27,7 +27,6 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.MessageBodyWriter;
 import org.jboss.logging.Logger;
-import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.common.ResteasyReactiveConfig;
 import org.jboss.resteasy.reactive.common.model.MethodParameter;
 import org.jboss.resteasy.reactive.common.model.ParameterType;
@@ -128,7 +127,7 @@ public class RuntimeResourceDeployment {
 
         Map<String, Integer> pathParameterIndexes = buildParamIndexMap(classPathTemplate, methodPathTemplate);
         List<ServerRestHandler> handlers = new ArrayList<>();
-        addHandlers(handlers, clazz, method, info, HandlerChainCustomizer.Phase.AFTER_MATCH);
+        addHandlers(handlers, method, info, HandlerChainCustomizer.Phase.AFTER_MATCH);
         MediaType sseElementType = null;
         if (method.getSseElementType() != null) {
             sseElementType = MediaType.valueOf(method.getSseElementType());
@@ -268,7 +267,7 @@ public class RuntimeResourceDeployment {
             handlers.add(instanceHandler);
         }
 
-        addHandlers(handlers, clazz, method, info, HandlerChainCustomizer.Phase.RESOLVE_METHOD_PARAMETERS);
+        addHandlers(handlers, method, info, HandlerChainCustomizer.Phase.RESOLVE_METHOD_PARAMETERS);
         for (int i = 0; i < parameters.length; i++) {
             ServerMethodParameter param = (ServerMethodParameter) parameters[i];
             boolean single = param.isSingle();
@@ -299,7 +298,7 @@ public class RuntimeResourceDeployment {
                     converter, param.parameterType,
                     param.isObtainedAsCollection(), param.isOptional()));
         }
-        addHandlers(handlers, clazz, method, info, HandlerChainCustomizer.Phase.BEFORE_METHOD_INVOKE);
+        addHandlers(handlers, method, info, HandlerChainCustomizer.Phase.BEFORE_METHOD_INVOKE);
         EndpointInvoker invoker = method.getInvoker().get();
         ServerRestHandler alternate = alternateInvoker(method, invoker);
         if (alternate != null) {
@@ -307,7 +306,7 @@ public class RuntimeResourceDeployment {
         } else {
             handlers.add(new InvocationHandler(invoker));
         }
-        addHandlers(handlers, clazz, method, info, HandlerChainCustomizer.Phase.AFTER_METHOD_INVOKE);
+        addHandlers(handlers, method, info, HandlerChainCustomizer.Phase.AFTER_METHOD_INVOKE);
 
         Type returnType = TypeSignatureParser.parse(method.getReturnType());
         Type nonAsyncReturnType = getNonAsyncReturnType(returnType);
@@ -395,7 +394,7 @@ public class RuntimeResourceDeployment {
             responseFilterHandlers = Collections.emptyList();
         } else {
             handlers.add(new ResponseHandler());
-            addHandlers(handlers, clazz, method, info, HandlerChainCustomizer.Phase.AFTER_RESPONSE_CREATED);
+            addHandlers(handlers, method, info, HandlerChainCustomizer.Phase.AFTER_RESPONSE_CREATED);
             responseFilterHandlers = new ArrayList<>(interceptorDeployment.setupResponseFilterHandler());
             handlers.addAll(responseFilterHandlers);
             handlers.add(new ResponseWriterHandler(dynamicEntityWriter));
@@ -432,14 +431,13 @@ public class RuntimeResourceDeployment {
         return buildTimeWriters.get(0) instanceof ServerMessageBodyWriter.AllWriteableMessageBodyWriter;
     }
 
-    private void addHandlers(List<ServerRestHandler> handlers, ResourceClass clazz, ServerResourceMethod method,
-            DeploymentInfo info,
+    private void addHandlers(List<ServerRestHandler> handlers, ServerResourceMethod method, DeploymentInfo info,
             HandlerChainCustomizer.Phase phase) {
         for (int i = 0; i < info.getGlobalHandlerCustomizers().size(); i++) {
-            handlers.addAll(info.getGlobalHandlerCustomizers().get(i).handlers(phase, clazz, method));
+            handlers.addAll(info.getGlobalHandlerCustomizers().get(i).handlers(phase));
         }
         for (int i = 0; i < method.getHandlerChainCustomizers().size(); i++) {
-            handlers.addAll(method.getHandlerChainCustomizers().get(i).handlers(phase, clazz, method));
+            handlers.addAll(method.getHandlerChainCustomizers().get(i).handlers(phase));
         }
     }
 
@@ -549,9 +547,6 @@ public class RuntimeResourceDeployment {
                 return type.getActualTypeArguments()[0];
             }
             if (type.getRawType() == Multi.class) {
-                return type.getActualTypeArguments()[0];
-            }
-            if (type.getRawType() == RestResponse.class) {
                 return type.getActualTypeArguments()[0];
             }
             return returnType;
