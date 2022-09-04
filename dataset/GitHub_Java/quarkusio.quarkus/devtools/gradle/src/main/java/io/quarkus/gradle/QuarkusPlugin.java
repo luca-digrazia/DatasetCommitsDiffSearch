9d0eb8245ca51dad83117e66457f520ae0ad6575
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:35:00 +0200

    Fix broken gradle buildNative
        The task needs to be executed after assemble

--- dataset/GitHub_Java/quarkusio.quarkus/devtools/gradle/src/main/java/io/quarkus/gradle/QuarkusPlugin.java
+++ dataset/GitHub_Java/quarkusio.quarkus/devtools/gradle/src/main/java/io/quarkus/gradle/QuarkusPlugin.java
@@ -58,9 +58,6 @@ public class QuarkusPlugin implements Plugin<Project> {
                     Task classesTask = tasks.getByName(JavaPlugin.CLASSES_TASK_NAME);
                     quarkusDev.dependsOn(classesTask);
                     quarkusBuild.dependsOn(classesTask);
-
-                    Task jarTask = tasks.getByName(JavaPlugin.JAR_TASK_NAME);
-                    quarkusBuild.dependsOn(jarTask);
                 });
 
         Task buildNative = tasks.create("buildNative", QuarkusNative.class);
