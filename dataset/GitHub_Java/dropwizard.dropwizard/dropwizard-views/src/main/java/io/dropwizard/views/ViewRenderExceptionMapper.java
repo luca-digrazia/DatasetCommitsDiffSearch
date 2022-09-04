1d28d6f827ff81017652278deb827f4dc4815493
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:18:16 +0200

    Move ViewExceptionMapper to an ExtendedExceptionMapper

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-views/src/main/java/io/dropwizard/views/ViewRenderExceptionMapper.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-views/src/main/java/io/dropwizard/views/ViewRenderExceptionMapper.java
@@ -3,17 +3,23 @@ package io.dropwizard.views;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.glassfish.jersey.spi.ExtendedExceptionMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * An {@link ExceptionMapper} that returns a 500 error response with a generic
- * HTML error page when a {@link ViewRenderException} is thrown.
- * 
+ * An {@link ExtendedExceptionMapper} that returns a 500 error response with a generic
+ * HTML error page when a {@link ViewRenderException} is the cause.
+ *
  * @since 1.1.0
  */
 @Provider
-public class ViewRenderExceptionMapper implements ExceptionMapper<WebApplicationException> {
+public class ViewRenderExceptionMapper implements ExtendedExceptionMapper<WebApplicationException> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ViewRenderExceptionMapper.class);
 
     /**
      * The generic HTML error page template.
@@ -26,13 +32,15 @@ public class ViewRenderExceptionMapper implements ExceptionMapper<WebApplication
 
     @Override
     public Response toResponse(WebApplicationException exception) {
-        if (exception.getCause() instanceof ViewRenderException) {
-            return Response.serverError()
-                    .type(MediaType.TEXT_HTML_TYPE)
-                    .entity(TEMPLATE_ERROR_MSG)
-                    .build();
-        }
-        return exception.getResponse();
+        LOGGER.error("Template Error", exception);
+        return Response.serverError()
+                .type(MediaType.TEXT_HTML_TYPE)
+                .entity(TEMPLATE_ERROR_MSG)
+                .build();
     }
 
+    @Override
+    public boolean isMappable(WebApplicationException e) {
+        return ExceptionUtils.indexOfThrowable(e, ViewRenderException.class) != -1;
+    }
 }
