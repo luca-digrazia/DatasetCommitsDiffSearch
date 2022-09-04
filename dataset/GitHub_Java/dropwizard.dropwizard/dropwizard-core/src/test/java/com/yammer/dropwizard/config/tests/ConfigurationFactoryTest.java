32e23c3a9e0b4ede758a709a321e0ab0df55b633
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:01:33 +0200

    Fix tests.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/config/tests/ConfigurationFactoryTest.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-core/src/test/java/com/yammer/dropwizard/config/tests/ConfigurationFactoryTest.java
@@ -1,11 +1,9 @@
 package com.yammer.dropwizard.config.tests;
 
-import com.fasterxml.jackson.dataformat.yaml.snakeyaml.error.YAMLException;
 import com.google.common.io.Resources;
 import com.yammer.dropwizard.config.ConfigurationException;
 import com.yammer.dropwizard.config.ConfigurationFactory;
 import com.yammer.dropwizard.validation.Validator;
-import org.junit.Before;
 import org.junit.Test;
 
 import javax.validation.constraints.NotNull;
@@ -14,8 +12,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-import static org.fest.assertions.api.Assertions.failBecauseExceptionWasNotThrown;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class ConfigurationFactoryTest {
     @SuppressWarnings("UnusedDeclaration")
@@ -30,34 +29,25 @@ public class ConfigurationFactoryTest {
     }
 
     private final Validator validator = new Validator();
-    private final ConfigurationFactory<Example> factory =
-            ConfigurationFactory.forClass(Example.class, validator);
-    private File malformedFile;
-    private File invalidFile;
-    private File validFile;
-
-    @Before
-    public void setUp() throws Exception {
-        this.malformedFile = new File(Resources.getResource("factory-test-malformed.yml").toURI());
-        this.invalidFile = new File(Resources.getResource("factory-test-invalid.yml").toURI());
-        this.validFile = new File(Resources.getResource("factory-test-valid.yml").toURI());
-    }
+    private final ConfigurationFactory<Example> factory = ConfigurationFactory.forClass(Example.class, validator);
+    private final File malformedFile = new File(Resources.getResource("factory-test-malformed.yml").getFile());
+    private final File invalidFile = new File(Resources.getResource("factory-test-invalid.yml").getFile());
+    private final File validFile = new File(Resources.getResource("factory-test-valid.yml").getFile());
 
     @Test
     public void loadsValidConfigFiles() throws Exception {
         final Example example = factory.build(validFile);
-        assertThat(example.getName())
-                .isEqualTo("Coda Hale");
+        assertThat(example.getName(),
+                   is("Coda Hale"));
     }
 
     @Test
     public void throwsAnExceptionOnMalformedFiles() throws Exception {
         try {
             factory.build(malformedFile);
-            failBecauseExceptionWasNotThrown(YAMLException.class);
+            fail("expected a YAMLException to be thrown, but none was");
         } catch (IOException e) {
-            assertThat(e.getMessage())
-                    .startsWith("Can not instantiate");
+            assertThat(e.getMessage(), startsWith("Can not instantiate"));
         }
     }
 
@@ -67,9 +57,9 @@ public class ConfigurationFactoryTest {
             factory.build(invalidFile);
         } catch (ConfigurationException e) {
             if ("en".equals(Locale.getDefault().getLanguage())) {
-                assertThat(e.getMessage())
-                        .endsWith("factory-test-invalid.yml has the following errors:\n" +
-                                          "  * name must match \"[\\w]+[\\s]+[\\w]+\" (was Boop)\n");
+                assertThat(e.getMessage(),
+                           endsWith("factory-test-invalid.yml has the following errors:\n" +
+                                    "  * name must match \"[\\w]+[\\s]+[\\w]+\" (was Boop)\n"));
             }
         }
     }
