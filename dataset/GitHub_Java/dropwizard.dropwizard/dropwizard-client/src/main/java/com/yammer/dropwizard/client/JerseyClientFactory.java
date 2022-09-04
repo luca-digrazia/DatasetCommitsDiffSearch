694eb1fdadac9dcc268ccf401d72dfa71d81d825
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:54:48 +0200

    Allow building JerseyClients outside of an Environment.
        Closes #185.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-client/src/main/java/com/yammer/dropwizard/client/JerseyClientFactory.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-client/src/main/java/com/yammer/dropwizard/client/JerseyClientFactory.java
@@ -1,9 +1,7 @@
 package com.yammer.dropwizard.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.filter.GZIPContentEncodingFilter;
-import com.sun.jersey.client.apache4.ApacheHttpClient4;
 import com.sun.jersey.client.apache4.ApacheHttpClient4Handler;
 import com.sun.jersey.client.apache4.config.ApacheHttpClient4Config;
 import com.sun.jersey.client.apache4.config.DefaultApacheHttpClient4Config;
@@ -22,13 +20,8 @@ public class JerseyClientFactory {
     private final ApacheHttpClient4Config config;
 
     public JerseyClientFactory(JerseyClientConfiguration configuration) {
-        this(configuration, new HttpClientFactory(configuration));
-    }
-
-    public JerseyClientFactory(JerseyClientConfiguration configuration,
-                               HttpClientFactory httpClientFactory) {
         this.configuration = configuration;
-        this.factory = httpClientFactory;
+        this.factory = new HttpClientFactory(configuration);
         this.config = new DefaultApacheHttpClient4Config();
     }
 
@@ -54,7 +47,7 @@ public class JerseyClientFactory {
         return this;
     }
 
-    public Client build(Environment environment) {
+    public JerseyClient build(Environment environment) {
         final ExecutorService threadPool = environment.managedExecutorService("jersey-client-%d",
                                                                               configuration.getMinThreads(),
                                                                               configuration.getMaxThreads(),
@@ -65,12 +58,11 @@ public class JerseyClientFactory {
         return build(threadPool, objectMapper);
     }
 
-    public Client build(ExecutorService threadPool, ObjectMapper objectMapper) {
+    public JerseyClient build(ExecutorService threadPool, ObjectMapper objectMapper) {
         final HttpClient client = factory.build();
         final ApacheHttpClient4Handler handler = new ApacheHttpClient4Handler(client, null, true);
         config.getSingletons().add(new JacksonMessageBodyProvider(objectMapper));
-
-        final Client jerseyClient = new ApacheHttpClient4(handler, config);
+        final JerseyClient jerseyClient = new JerseyClient(handler, config);
         jerseyClient.setExecutorService(threadPool);
 
         if (configuration.isGzipEnabled()) {
