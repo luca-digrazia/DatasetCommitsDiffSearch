a21db7cd86b2b53750c33f16ec026d9d03ce2671
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:42:56 +0200

    Remove the addRJarToJavaInfo method.
        Now that the Starlark migration is complete, remove the addRJarToJavaInfo method which provided a way to make the classpaths in the JavaInfo exactly the same as ones created by the native rules.
    
        RELNOTES: None.
        PiperOrigin-RevId: 284128420

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidSkylarkCommon.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidSkylarkCommon.java
@@ -14,28 +14,43 @@
 package com.google.devtools.build.lib.rules.android;
 
 import com.google.devtools.build.lib.actions.Artifact;
-import com.google.devtools.build.lib.skylarkinterface.SkylarkCallable;
-import com.google.devtools.build.lib.skylarkinterface.SkylarkModule;
-import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.rules.java.JavaCompilationArgsProvider;
+import com.google.devtools.build.lib.rules.java.JavaInfo;
+import com.google.devtools.build.lib.skylarkbuildapi.android.AndroidSkylarkCommonApi;
+import com.google.devtools.build.lib.skylarkbuildapi.android.AndroidSplitTransititionApi;
 
-/**
- * Common utilities for Skylark rules related to Android.
- */
-@SkylarkModule(
-  name = "android_common",
-  doc = "Common utilities and fucntionality related to Android rules."
-)
-public class AndroidSkylarkCommon {
-  @SkylarkCallable(
-    name = "resource_source_directory",
-    allowReturnNones = true,
-    doc =
-        "Returns a source directory for Android resource file. "
-            + "The source directory is a prefix of resource's relative path up to "
-            + "a directory that designates resource kind (cf. "
-            + "http://developer.android.com/guide/topics/resources/providing-resources.html)."
-  )
-  public static PathFragment getSourceDirectoryRelativePathFromResource(Artifact resource) {
-    return AndroidCommon.getSourceDirectoryRelativePathFromResource(resource);
+/** Common utilities for Skylark rules related to Android. */
+public class AndroidSkylarkCommon implements AndroidSkylarkCommonApi<Artifact, JavaInfo> {
+
+  @Override
+  public AndroidDeviceBrokerInfo createDeviceBrokerInfo(String deviceBrokerType) {
+    return new AndroidDeviceBrokerInfo(deviceBrokerType);
+  }
+
+  @Override
+  public String getSourceDirectoryRelativePathFromResource(Artifact resource) {
+    return AndroidCommon.getSourceDirectoryRelativePathFromResource(resource).toString();
+  }
+
+  @Override
+  public AndroidSplitTransititionApi getAndroidSplitTransition() {
+    return AndroidRuleClasses.ANDROID_SPLIT_TRANSITION;
+  }
+
+  /**
+   * TODO(b/14473160): Provides a Starlark compatibility layer for the sourceless deps bug. When a
+   * sourceless target is defined, the deps of the target are implicitly exported. Specifically only
+   * the {@link JavaCompilationArgsProvider} is propagated. This method takes the existing JavaInfo
+   * and produces a new one, only containing the {@link JavaCompilationArgsProvider} to be added to
+   * the exports field of the java_common.compile method. Remove this method once the bug has been
+   * fixed.
+   */
+  @Override
+  public JavaInfo enableImplicitSourcelessDepsExportsCompatibility(JavaInfo javaInfo) {
+    return JavaInfo.Builder.create()
+        .addProvider(
+            JavaCompilationArgsProvider.class,
+            javaInfo.getProvider(JavaCompilationArgsProvider.class))
+        .build();
   }
 }
--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skylarkbuildapi/android/AndroidSkylarkCommonApi.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skylarkbuildapi/android/AndroidSkylarkCommonApi.java
@@ -14,16 +14,23 @@
 package com.google.devtools.build.lib.skylarkbuildapi.android;
 
 import com.google.devtools.build.lib.skylarkbuildapi.FileApi;
+import com.google.devtools.build.lib.skylarkbuildapi.java.JavaInfoApi;
 import com.google.devtools.build.lib.skylarkinterface.Param;
 import com.google.devtools.build.lib.skylarkinterface.SkylarkCallable;
 import com.google.devtools.build.lib.skylarkinterface.SkylarkModule;
-import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.syntax.StarlarkSemantics.FlagIdentifier;
+import com.google.devtools.build.lib.syntax.StarlarkValue;
 
 /** Common utilities for Skylark rules related to Android. */
 @SkylarkModule(
     name = "android_common",
-    doc = "Common utilities and functionality related to Android rules.")
-public interface AndroidSkylarkCommonApi<FileT extends FileApi> {
+    doc =
+        "Do not use this module. It is intended for migration purposes only. If you depend on it, "
+            + "you will be broken when it is removed."
+            + "Common utilities and functionality related to Android rules.",
+    documented = false)
+public interface AndroidSkylarkCommonApi<FileT extends FileApi, JavaInfoT extends JavaInfoApi<?>>
+    extends StarlarkValue {
 
   @SkylarkCallable(
       name = "create_device_broker_info",
@@ -38,14 +45,41 @@ public interface AndroidSkylarkCommonApi<FileT extends FileApi> {
           "Returns a source directory for Android resource file. "
               + "The source directory is a prefix of resource's relative path up to "
               + "a directory that designates resource kind (cf. "
-              + "http://developer.android.com/guide/topics/resources/providing-resources.html).")
-  PathFragment getSourceDirectoryRelativePathFromResource(FileT resource);
+              + "http://developer.android.com/guide/topics/resources/providing-resources.html).",
+      documented = false,
+      parameters = {
+        @Param(
+            name = "resource",
+            doc = "The android resource file.",
+            positional = true,
+            named = false,
+            type = FileApi.class)
+      })
+  String getSourceDirectoryRelativePathFromResource(FileT resource);
 
   @SkylarkCallable(
       name = "multi_cpu_configuration",
       doc =
           "A configuration for rule attributes that compiles native code according to "
               + "the --fat_apk_cpu and --android_crosstool_top flags.",
+      documented = false,
       structField = true)
   AndroidSplitTransititionApi getAndroidSplitTransition();
+
+  @SkylarkCallable(
+      name = "enable_implicit_sourceless_deps_exports_compatibility",
+      doc = "Takes a JavaInfo and converts it to an implicit exportable JavaInfo.",
+      documented = false,
+      enableOnlyWithFlag = FlagIdentifier.EXPERIMENTAL_ENABLE_ANDROID_MIGRATION_APIS,
+      parameters = {
+        @Param(
+            name = "dep",
+            doc =
+                "A JavaInfo that will be used as an implicit export for sourceless deps exports"
+                    + " compatibility.",
+            positional = true,
+            named = false,
+            type = JavaInfoApi.class)
+      })
+  JavaInfoT enableImplicitSourcelessDepsExportsCompatibility(JavaInfoT javaInfo);
 }
