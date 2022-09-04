f6436f02b6bf81b27243171645d0e54632eb4af2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:32:22 +0200

    Improved handling UnitOfWork with interfaces (differentiate between handling- and definitionMethod)

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-hibernate/src/main/java/io/dropwizard/hibernate/UnitOfWorkApplicationListener.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-hibernate/src/main/java/io/dropwizard/hibernate/UnitOfWorkApplicationListener.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import javax.ws.rs.ext.Provider;
 
-import org.glassfish.jersey.server.internal.process.MappableException;
 import org.glassfish.jersey.server.model.Resource;
 import org.glassfish.jersey.server.model.ResourceMethod;
 import org.glassfish.jersey.server.monitoring.ApplicationEvent;
@@ -85,7 +84,7 @@ public class UnitOfWorkApplicationListener implements ApplicationEventListener {
                         commitTransaction();
                     } catch (Exception e) {
                         rollbackTransaction();
-                        throw new MappableException(e);
+                        this.<RuntimeException>rethrow(e);
                     }
                     finally {
                         this.session.close();
@@ -136,6 +135,11 @@ public class UnitOfWorkApplicationListener implements ApplicationEventListener {
                     txn.commit();
                 }
             }
+        }
+
+        @SuppressWarnings("unchecked")
+        private <E extends Exception> void rethrow(Exception e) throws E {
+            throw (E) e;
         }
     }
 
