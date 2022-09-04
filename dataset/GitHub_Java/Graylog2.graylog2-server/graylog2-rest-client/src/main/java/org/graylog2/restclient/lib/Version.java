6a8a329ec4ef6f0546b1f743ea084656705fe002
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:10:44 +0200

    bump rest client version to 1.0.0-rc.4

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/lib/Version.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-rest-client/src/main/java/org/graylog2/restclient/lib/Version.java
@@ -75,9 +75,8 @@ public class Version {
     public static final Version v1_0_0_RC2 = new Version(1, 0, 0, "rc.2");
     public static final Version v1_0_0_RC3 = new Version(1, 0, 0, "rc.3");
     public static final Version v1_0_0_RC4 = new Version(1, 0, 0, "rc.4");
-    public static final Version v1_0_0 = new Version(1, 0, 0);
 
-    public static final Version VERSION = v1_0_0;
+    public static final Version VERSION = v1_0_0_RC4;
 
     @JsonProperty
     public int major;
