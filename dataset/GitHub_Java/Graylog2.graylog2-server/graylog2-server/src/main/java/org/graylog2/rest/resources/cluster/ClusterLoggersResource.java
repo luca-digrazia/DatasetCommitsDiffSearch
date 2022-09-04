297b3a61da9202fac58486a69517364b9af7b015
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:08:00 +0200

    Add cluster wide Loggers endpoint (#10882)
        We currently only support changing entire subsystems on the entire cluster.
    
        For debugging specific problems it might help to change the loglevel
        of classes under a certain package name.
        E.g.:
    
            http --auth admin:admin put :9000/api/cluster/system/loggers/org.graylog.plugins.events/level/DEBUG  X-Requested-By:httpie

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/cluster/ClusterLoggersResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/cluster/ClusterLoggersResource.java
@@ -1,12 +1,29 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 package org.graylog2.rest.resources.cluster;
 
 import com.codahale.metrics.annotation.Timed;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-import com.wordnik.swagger.annotations.ApiResponse;
-import com.wordnik.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.graylog2.audit.jersey.NoAuditEvent;
 import org.graylog2.cluster.Node;
 import org.graylog2.cluster.NodeNotFoundException;
 import org.graylog2.cluster.NodeService;
@@ -15,9 +32,10 @@ import org.graylog2.rest.models.system.loggers.responses.LoggersSummary;
 import org.graylog2.rest.models.system.loggers.responses.SubsystemSummary;
 import org.graylog2.rest.resources.system.logs.RemoteLoggersResource;
 import org.graylog2.shared.rest.resources.ProxiedResource;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.constraints.NotEmpty;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -29,6 +47,7 @@ import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
 
 @RequiresAuthentication
 @Api(value = "Cluster/System/Loggers", description = "Cluster-wide access to internal Graylog loggers")
@@ -37,8 +56,9 @@ public class ClusterLoggersResource extends ProxiedResource {
     @Inject
     public ClusterLoggersResource(NodeService nodeService,
                                     RemoteInterfaceProvider remoteInterfaceProvider,
-                                    @Context HttpHeaders httpHeaders) throws NodeNotFoundException {
-        super(httpHeaders, nodeService, remoteInterfaceProvider);
+                                    @Context HttpHeaders httpHeaders,
+                                    @Named("proxiedRequestsExecutorService") ExecutorService executorService) throws NodeNotFoundException {
+        super(httpHeaders, nodeService, remoteInterfaceProvider, executorService);
     }
 
     @GET
@@ -66,6 +86,7 @@ public class ClusterLoggersResource extends ProxiedResource {
     @ApiResponses(value = {
         @ApiResponse(code = 404, message = "No such subsystem.")
     })
+    @NoAuditEvent("proxy resource, audit event will be emitted on target nodes")
     public void setSubsystemLoggerLevel(
         @ApiParam(name = "nodeId", required = true) @PathParam("nodeId") @NotEmpty String nodeId,
         @ApiParam(name = "subsystem", required = true) @PathParam("subsystem") @NotEmpty String subsystemTitle,
@@ -74,5 +95,17 @@ public class ClusterLoggersResource extends ProxiedResource {
         final RemoteLoggersResource remoteLoggersResource = this.remoteInterfaceProvider.get(node, this.authenticationToken, RemoteLoggersResource.class);
 
         remoteLoggersResource.setSubsystemLoggerLevel(subsystemTitle, level).execute();
+    }
+
+    @PUT
+    @Timed
+    @Path("/{loggerName}/level/{level}")
+    @ApiOperation(value = "Set the loglevel of a single logger",
+            notes = "Provided level is falling back to DEBUG if it does not exist")
+    @NoAuditEvent("proxy resource, audit event will be emitted on target nodes")
+    public Map<String, CallResult<Void>> setClusterSingleLoggerLevel(
+            @ApiParam(name = "loggerName", required = true) @PathParam("loggerName") @NotEmpty String loggerName,
+            @ApiParam(name = "level", required = true) @PathParam("level") @NotEmpty String level) throws NodeNotFoundException, IOException {
+        return requestOnAllNodes(createRemoteInterfaceProvider(RemoteLoggersResource.class), client -> client.setSingleLoggerLevel(loggerName, level));
     }
 }
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/logs/RemoteLoggersResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/system/logs/RemoteLoggersResource.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
 package org.graylog2.rest.resources.system.logs;
 
 import org.graylog2.rest.models.system.loggers.responses.LogMessagesSummary;
@@ -9,18 +25,18 @@ import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface RemoteLoggersResource {
-    @GET("/system/loggers")
+    @GET("system/loggers")
     Call<LoggersSummary> loggers();
 
-    @GET("/system/loggers/subsystems")
+    @GET("system/loggers/subsystems")
     Call<SubsystemSummary> subsystems();
 
-    @PUT("/system/loggers/subsystems/{subsystem}/level/{level}")
+    @PUT("system/loggers/subsystems/{subsystem}/level/{level}")
     Call<Void> setSubsystemLoggerLevel(@Path("subsystem") String subsystemTitle, @Path("level") String level);
 
-    @PUT("/system/loggers/{loggerName}/level/{level}")
-    Call<Void> setSingleLoggerLevel(String loggerName, String level);
+    @PUT("system/loggers/{loggerName}/level/{level}")
+    Call<Void> setSingleLoggerLevel(@Path("loggerName") String loggerName, @Path("level") String level);
 
-    @GET("/system/loggers/messages/recent")
+    @GET("system/loggers/messages/recent")
     Call<LogMessagesSummary> messages(int limit, String level);
 }
