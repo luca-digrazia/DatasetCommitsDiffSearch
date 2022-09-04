4381d8407eded52a854aee5e4d31015c172ff13c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:23:20 +0200

    Added a new test that show errors on generated code when @Transactional is used with parameterized parameters or array parameters

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/com/googlecode/androidannotations/test15/TransactionalActivity.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/functional-test-1-5/src/main/java/com/googlecode/androidannotations/test15/TransactionalActivity.java
@@ -21,7 +21,6 @@ import android.app.Activity;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.Trace;
 import com.googlecode.androidannotations.annotations.Transactional;
 import com.googlecode.androidannotations.test15.instancestate.MySerializableBean;
 
@@ -36,11 +35,6 @@ public class TransactionalActivity extends Activity {
 	@Transactional
 	void rollbackedTransaction(SQLiteDatabase db) {
 		throw new IllegalArgumentException();
-	}
-
-	@Transactional
-	MySerializableBean transactionnalWithReturnStatement(SQLiteDatabase db) {
-		return null;
 	}
 
 	@Transactional
