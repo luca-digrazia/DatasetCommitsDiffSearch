914002ed05bc51c586a7e305f07019ada6e7fe78
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:41:53 +0200

    GET single dashboard REST resource. Graylog2/graylog2-web-interface#360

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/dashboards/DashboardsResource.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/resources/dashboards/DashboardsResource.java
@@ -24,15 +24,10 @@ import com.codahale.metrics.annotation.Timed;
 import com.google.common.collect.Maps;
 import org.bson.types.ObjectId;
 import org.graylog2.dashboards.Dashboard;
-import org.graylog2.dashboards.widgets.DashboardWidget;
 import org.graylog2.database.*;
-import org.graylog2.database.NotFoundException;
-import org.graylog2.indexer.searches.timeranges.InvalidRangeParametersException;
 import org.graylog2.rest.documentation.annotations.*;
 import org.graylog2.rest.resources.RestResource;
-import org.graylog2.rest.resources.dashboards.requests.AddWidgetRequest;
 import org.graylog2.rest.resources.dashboards.requests.CreateRequest;
-import org.graylog2.rest.resources.dashboards.requests.UpdateWidgetRequest;
 import org.graylog2.system.activities.Activity;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -45,7 +40,6 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 /**
  * @author Lennart Koopmann <lennart@torch.sh>
@@ -60,12 +54,7 @@ public class DashboardsResource extends RestResource {
     @ApiOperation(value = "Create a dashboard")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiResponses(value = {
-            @ApiResponse(code = 403, message = "Request must be performed against master node.")
-    })
     public Response create(@ApiParam(title = "JSON body", required = true) String body) {
-        restrictToMaster();
-
         CreateRequest cr;
         try {
             cr = objectMapper.readValue(body, CreateRequest.class);
@@ -90,8 +79,6 @@ public class DashboardsResource extends RestResource {
             throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
         }
 
-        core.dashboards().add(dashboard);
-
         Map<String, Object> result = Maps.newHashMap();
         result.put("dashboard_id", id.toStringMongod());
 
@@ -99,13 +86,9 @@ public class DashboardsResource extends RestResource {
     }
 
     @GET @Timed
-    @ApiOperation(value = "Get a list of all dashboards and all configurations of their widgets.")
+    @ApiOperation(value = "Get a list of all dashboards")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiResponses(value = {
-            @ApiResponse(code = 403, message = "Request must be performed against master node.")
-    })
     public String list() {
-        restrictToMaster();
         List<Map<String, Object>> dashboards = Lists.newArrayList();
 
         for (Dashboard dashboard: Dashboard.all(core)) {
@@ -120,16 +103,14 @@ public class DashboardsResource extends RestResource {
     }
 
     @GET @Timed
-    @ApiOperation(value = "Get a single dashboards and all configurations of its widgets.")
+    @ApiOperation(value = "Get a single dashboards")
     @Path("/{dashboardId}")
     @ApiResponses(value = {
             @ApiResponse(code = 404, message = "Dashboard not found."),
-            @ApiResponse(code = 403, message = "Request must be performed against master node.")
+            @ApiResponse(code = 400, message = "Invalid ObjectId.")
     })
     @Produces(MediaType.APPLICATION_JSON)
     public String get(@ApiParam(title = "dashboardId", required = true) @PathParam("dashboardId") String dashboardId) {
-        restrictToMaster();
-
         try {
             Dashboard dashboard = Dashboard.load(loadObjectId(dashboardId), core);
             return json(dashboard.asMap());
@@ -144,11 +125,9 @@ public class DashboardsResource extends RestResource {
     @Path("/{dashboardId}")
     @ApiResponses(value = {
             @ApiResponse(code = 404, message = "Dashboard not found."),
-            @ApiResponse(code = 403, message = "Request must be performed against master node.")
+            @ApiResponse(code = 400, message = "Invalid ObjectId.")
     })
     public Response delete(@ApiParam(title = "dashboardId", required = true) @PathParam("dashboardId") String dashboardId) {
-        restrictToMaster();
-
         try {
             Dashboard dashboard = Dashboard.load(loadObjectId(dashboardId), core);
             dashboard.destroy();
@@ -163,187 +142,5 @@ public class DashboardsResource extends RestResource {
         return Response.status(Response.Status.fromStatusCode(204)).build();
     }
 
-    @POST
-    @Timed
-    @ApiOperation(value = "Add a widget to a dashboard")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Dashboard not found."),
-            @ApiResponse(code = 400, message = "Validation error."),
-            @ApiResponse(code = 400, message = "No such widget type."),
-            @ApiResponse(code = 403, message = "Request must be performed against master node.")
-    })
-    @Path("/{dashboardId}/widgets")
-    public Response addWidget(@ApiParam(title = "JSON body", required = true) String body,
-                              @ApiParam(title = "dashboardId", required = true) @PathParam("dashboardId") String dashboardId) {
-        restrictToMaster();
-
-        AddWidgetRequest awr;
-        try {
-            awr = objectMapper.readValue(body, AddWidgetRequest.class);
-        } catch(IOException e) {
-            LOG.error("Error while parsing JSON", e);
-            throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
-        }
-
-        DashboardWidget widget;
-        try {
-            widget = DashboardWidget.fromRequest(core, awr);
-            Dashboard dashboard = Dashboard.load(new ObjectId(dashboardId), core);
-
-            dashboard.addWidget(widget);
-        } catch(NotFoundException e) {
-            LOG.error("Dashboard not found.", e);
-            throw new WebApplicationException(e, Response.Status.NOT_FOUND);
-        } catch (ValidationException e1) {
-            LOG.error("Validation error.", e1);
-            throw new WebApplicationException(e1, Response.Status.BAD_REQUEST);
-        } catch (DashboardWidget.NoSuchWidgetTypeException e2) {
-            LOG.error("No such widget type.", e2);
-            throw new WebApplicationException(e2, Response.Status.BAD_REQUEST);
-        } catch (InvalidRangeParametersException e3) {
-            LOG.error("Invalid timerange parameters provided.", e3);
-            throw new WebApplicationException(e3, Response.Status.BAD_REQUEST);
-        }
-
-        Map<String, Object> result = Maps.newHashMap();
-        result.put("widget_id", widget.getId());
-
-        return Response.status(Response.Status.CREATED).entity(json(result)).build();
-    }
-
-    @DELETE @Timed
-    @ApiOperation(value = "Delete a widget")
-    @Path("/{dashboardId}/widgets/{widgetId}")
-    @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Dashboard not found."),
-            @ApiResponse(code = 404, message = "Widget not found."),
-            @ApiResponse(code = 403, message = "Request must be performed against master node.")
-    })
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response remove(
-            @ApiParam(title = "dashboardId", required = true) @PathParam("dashboardId") String dashboardId,
-            @ApiParam(title = "widgetId", required = true) @PathParam("widgetId") String widgetId) {
-        restrictToMaster();
-
-        if (dashboardId == null || dashboardId.isEmpty()) {
-            LOG.error("Missing dashboard ID. Returning HTTP 400.");
-            throw new WebApplicationException(400);
-        }
-
-        if (widgetId == null || widgetId.isEmpty()) {
-            LOG.error("Missing widget ID. Returning HTTP 400.");
-            throw new WebApplicationException(400);
-        }
-
-        try {
-            Dashboard dashboard = Dashboard.load(new ObjectId(dashboardId), core);
-            dashboard.removeWidget(widgetId);
-        } catch(NotFoundException e) {
-            LOG.error("Dashboard not found.", e);
-            throw new WebApplicationException(e, Response.Status.NOT_FOUND);
-        }
-
-        String msg = "Deleted widget <" + widgetId + "> from dashboard <" + dashboardId + ">. Reason: REST request.";
-        LOG.info(msg);
-        core.getActivityWriter().write(new Activity(msg, DashboardsResource.class));
-
-        return Response.status(Response.Status.NO_CONTENT).build();
-    }
-
-    @GET @Timed
-    @ApiOperation(value = "Get a single widget value.")
-    @Path("/{dashboardId}/widgets/{widgetId}/value")
-    @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Dashboard not found."),
-            @ApiResponse(code = 404, message = "Widget not found."),
-            @ApiResponse(code = 403, message = "Request must be performed against master node."),
-            @ApiResponse(code = 504, message = "Computation failed on indexer side.")
-    })
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response widgetValue(@ApiParam(title = "dashboardId", required = true) @PathParam("dashboardId") String dashboardId,
-                              @ApiParam(title = "widgetId", required = true) @PathParam("widgetId") String widgetId) {
-        restrictToMaster();
-
-        Dashboard dashboard = core.dashboards().get(dashboardId);
-
-        if (dashboard == null) {
-            LOG.error("Dashboard not found.");
-            throw new WebApplicationException(404);
-        }
-
-        DashboardWidget widget = dashboard.getWidget(widgetId);
-
-        if (widget == null) {
-            LOG.error("Widget not found.");
-            throw new WebApplicationException(404);
-        }
-
-        try {
-            return Response.status(Response.Status.OK).entity(json(widget.getComputationResult().asMap())).build();
-        } catch (ExecutionException e) {
-            LOG.error("Error while computing dashboard.", e);
-            return Response.status(Response.Status.GATEWAY_TIMEOUT).build();
-        }
-    }
-
-    @PUT @Timed
-    @ApiOperation(value = "Update description of a widget")
-    @Path("/{dashboardId}/widgets/{widgetId}/description")
-    @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Dashboard not found."),
-            @ApiResponse(code = 404, message = "Widget not found."),
-            @ApiResponse(code = 403, message = "Request must be performed against master node.")
-    })
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response updateDescription(
-            @ApiParam(title = "JSON body", required = true) String body,
-            @ApiParam(title = "dashboardId", required = true) @PathParam("dashboardId") String dashboardId,
-            @ApiParam(title = "widgetId", required = true) @PathParam("widgetId") String widgetId) {
-        restrictToMaster();
-
-        UpdateWidgetRequest uwr;
-        try {
-            uwr = objectMapper.readValue(body, UpdateWidgetRequest.class);
-        } catch(IOException e) {
-            LOG.error("Error while parsing JSON", e);
-            throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
-        }
-
-        if (dashboardId == null || dashboardId.isEmpty()) {
-            LOG.error("Missing dashboard ID. Returning HTTP 400.");
-            throw new WebApplicationException(400);
-        }
-
-        if (widgetId == null || widgetId.isEmpty()) {
-            LOG.error("Missing widget ID. Returning HTTP 400.");
-            throw new WebApplicationException(400);
-        }
-
-        try {
-            Dashboard dashboard = core.dashboards().get(dashboardId);
-
-            if (dashboard == null) {
-                LOG.error("Dashboard not found.");
-                throw new WebApplicationException(404);
-            }
-
-            DashboardWidget widget = dashboard.getWidget(widgetId);
-
-            if (widget == null) {
-                LOG.error("Widget not found.");
-                throw new WebApplicationException(404);
-            }
-
-            dashboard.updateWidgetDescription(widget, uwr.description);
-        } catch (ValidationException e) {
-            LOG.error("Validation error.", e);
-            throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
-        }
-
-        LOG.info("Updated description of widget <" + widgetId + "> on dashboard <" + dashboardId + ">. Reason: REST request.");
-        return Response.status(Response.Status.OK).build();
-    }
 
 }
