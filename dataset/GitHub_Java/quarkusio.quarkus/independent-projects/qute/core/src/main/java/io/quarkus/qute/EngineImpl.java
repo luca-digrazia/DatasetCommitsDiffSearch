b3fd79bea3a51cbde37c20dbe8ebf53063603347
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:38:51 +0200

    Qute - fix parser and computed section helpers used during validation
        - resolves #8217

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/EngineImpl.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/EngineImpl.java
@@ -10,13 +10,17 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.ServiceLoader;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.jboss.logging.Logger;
 
 /**
@@ -34,37 +38,38 @@ class EngineImpl implements Engine {
     private final Map<String, Template> templates;
     private final List<TemplateLocator> locators;
     private final List<ResultMapper> resultMappers;
+    private final PublisherFactory publisherFactory;
     private final AtomicLong idGenerator = new AtomicLong(0);
-    private final List<ParserHook> parserHooks;
-    final boolean removeStandaloneLines;
 
-    EngineImpl(EngineBuilder builder) {
-        this.sectionHelperFactories = Collections.unmodifiableMap(new HashMap<>(builder.sectionHelperFactories));
-        this.valueResolvers = sort(builder.valueResolvers);
-        this.namespaceResolvers = ImmutableList.<NamespaceResolver> builder()
-                .addAll(builder.namespaceResolvers).add(new TemplateImpl.DataNamespaceResolver()).build();
-        this.evaluator = new EvaluatorImpl(this.valueResolvers, this.namespaceResolvers);
+    EngineImpl(Map<String, SectionHelperFactory<?>> sectionHelperFactories, List<ValueResolver> valueResolvers,
+            List<NamespaceResolver> namespaceResolvers, List<TemplateLocator> locators,
+            List<ResultMapper> resultMappers, Function<String, SectionHelperFactory<?>> sectionHelperFunc) {
+        this.sectionHelperFactories = Collections.unmodifiableMap(new HashMap<>(sectionHelperFactories));
+        this.valueResolvers = sort(valueResolvers);
+        this.namespaceResolvers = ImmutableList.copyOf(namespaceResolvers);
+        this.evaluator = new EvaluatorImpl(this.valueResolvers);
         this.templates = new ConcurrentHashMap<>();
-        this.locators = sort(builder.locators);
-        this.resultMappers = sort(builder.resultMappers);
-        this.sectionHelperFunc = builder.sectionHelperFunc;
-        this.parserHooks = ImmutableList.copyOf(builder.parserHooks);
-        this.removeStandaloneLines = builder.removeStandaloneLines;
+        this.locators = sort(locators);
+        ServiceLoader<PublisherFactory> loader = ServiceLoader.load(PublisherFactory.class);
+        Iterator<PublisherFactory> iterator = loader.iterator();
+        if (iterator.hasNext()) {
+            this.publisherFactory = iterator.next();
+        } else {
+            this.publisherFactory = null;
+        }
+        if (iterator.hasNext()) {
+            throw new IllegalStateException(
+                    "Multiple reactive factories found: " + StreamSupport.stream(loader.spliterator(), false)
+                            .map(Object::getClass).map(Class::getName).collect(Collectors.joining(",")));
+        }
+        this.resultMappers = sort(resultMappers);
+        this.sectionHelperFunc = sectionHelperFunc;
     }
 
     @Override
-    public Template parse(String content, Variant variant, String id) {
+    public Template parse(String content, Variant variant) {
         String generatedId = generateId();
-        return newParser(id != null ? id : generatedId, new StringReader(content), Optional.ofNullable(variant), generatedId)
-                .parse();
-    }
-
-    private Parser newParser(String id, Reader reader, Optional<Variant> variant, String generatedId) {
-        Parser parser = new Parser(this, reader, id, generatedId, variant);
-        for (ParserHook parserHook : parserHooks) {
-            parserHook.beforeParsing(parser);
-        }
-        return parser;
+        return new Parser(this).parse(new StringReader(content), Optional.ofNullable(variant), generatedId, generatedId);
     }
 
     @Override
@@ -114,6 +119,10 @@ class EngineImpl implements Engine {
         templates.keySet().removeIf(test);
     }
 
+    PublisherFactory getPublisherFactory() {
+        return publisherFactory;
+    }
+
     String generateId() {
         return "" + idGenerator.incrementAndGet();
     }
@@ -123,7 +132,7 @@ class EngineImpl implements Engine {
             Optional<TemplateLocation> location = locator.locate(id);
             if (location.isPresent()) {
                 try (Reader r = location.get().read()) {
-                    return newParser(id, ensureBufferedReader(r), location.get().getVariant(), generateId()).parse();
+                    return new Parser(this).parse(ensureBufferedReader(r), location.get().getVariant(), id, generateId());
                 } catch (IOException e) {
                     LOGGER.warn("Unable to close the reader for " + id, e);
                 }
--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/Parser.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/Parser.java
@@ -289,7 +289,6 @@ class Parser implements Function<String, Expression> {
                     typeInfoStack.addFirst(typeInfos);
                 }
 
-                // A new block - stop ignoring the block content
                 ignoreContent = false;
 
             } else {
@@ -346,7 +345,6 @@ class Parser implements Function<String, Expression> {
                             "section block end tag [" + name + "] does not match the start tag [" + block.getLabel() + "]");
                 }
                 section.addBlock(sectionBlockStack.pop().build());
-                // Ignore the block content until a next block starts or the current section ends
                 ignoreContent = true;
             } else {
                 // Section end
@@ -356,11 +354,7 @@ class Parser implements Function<String, Expression> {
                 }
                 section = sectionStack.pop();
                 if (!ignoreContent) {
-                    // Add the current block to the current section
                     section.addBlock(sectionBlockStack.pop().build());
-                } else {
-                    // The current section ends - stop ignoring the block content
-                    ignoreContent = false;
                 }
                 sectionBlockStack.peek().addNode(section.build());
             }
