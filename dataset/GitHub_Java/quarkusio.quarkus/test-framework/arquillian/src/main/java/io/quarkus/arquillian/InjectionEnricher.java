84ee291ce6ec80889af2310058e4f6515d911d7c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:29:02 +0200

    Fix of hasNonArquillianDataProvider which resolves always to true

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/arquillian/src/main/java/io/quarkus/arquillian/InjectionEnricher.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/arquillian/src/main/java/io/quarkus/arquillian/InjectionEnricher.java
@@ -1,20 +1,14 @@
 package io.quarkus.arquillian;
 
-import static io.quarkus.arquillian.QuarkusProtocol.convertToCL;
+import static io.quarkus.arquillian.QuarkusProtocol.convertToTCCL;
 
-import java.io.Closeable;
-import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
 
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.BeanManager;
 
-import org.jboss.arquillian.container.spi.context.annotation.DeploymentScoped;
 import org.jboss.arquillian.core.api.InstanceProducer;
 import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.test.spi.TestEnricher;
@@ -22,7 +16,6 @@ import org.jboss.arquillian.test.spi.annotation.TestScoped;
 import org.jboss.logging.Logger;
 
 import io.quarkus.arc.Arc;
-import io.quarkus.arc.ArcContainer;
 
 /**
  * Enricher that provides method argument injection.
@@ -33,11 +26,20 @@ public class InjectionEnricher implements TestEnricher {
 
     @Inject
     @TestScoped
-    private InstanceProducer<CreationContextHolder> creationalContextProducer;
+    private InstanceProducer<CreationalContext<?>> creationalContextProducer;
 
-    @Inject
-    @DeploymentScoped
-    private InstanceProducer<ClassLoader> appClassloader;
+    public BeanManager getBeanManager() {
+        return Arc.container().beanManager();
+    }
+
+    public CreationalContext<?> getCreationalContext() {
+        CreationalContext<?> cc = creationalContextProducer.get();
+        if (cc == null) {
+            cc = getBeanManager().createCreationalContext(null);
+            creationalContextProducer.set(cc);
+        }
+        return cc;
+    }
 
     @Override
     public void enrich(Object testCase) {
@@ -45,92 +47,8 @@ public class InjectionEnricher implements TestEnricher {
 
     @Override
     public Object[] resolve(Method method) {
-        //we need to resolve from inside the
-        if (method.getParameterTypes().length > 0) {
-            ClassLoader old = Thread.currentThread().getContextClassLoader();
-            try {
-                CreationContextHolder holder = getCreationalContext();
-                ClassLoader cl = appClassloader.get() != null ? appClassloader.get() : getClass().getClassLoader();
-                Thread.currentThread().setContextClassLoader(cl);
-                Class<?> c = cl.loadClass(IsolatedEnricher.class.getName());
-                BiFunction<Method, Object, Object[]> function = (BiFunction<Method, Object, Object[]>) c.newInstance();
-                return function.apply(method, holder.creationalContext);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            } finally {
-                Thread.currentThread().setContextClassLoader(old);
-            }
-        }
-        return new Object[0];
-    }
-
-    private CreationContextHolder getCreationalContext() {
-        try {
-            ClassLoader cl = appClassloader.get() != null ? appClassloader.get() : getClass().getClassLoader();
-            Class<?> c = cl.loadClass(IsolatedCreationContextCreator.class.getName());
-            Supplier<Map.Entry<Closeable, Object>> supplier = (Supplier<Map.Entry<Closeable, Object>>) c.newInstance();
-            Map.Entry<Closeable, Object> val = supplier.get();
-            return new CreationContextHolder(val.getKey(), val.getValue());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static class IsolatedCreationContextCreator implements Supplier<Map.Entry<Closeable, Object>> {
-
-        private BeanManager getBeanManager() {
-            ArcContainer container = Arc.container();
-            if (container == null) {
-                return null;
-            }
-            return container.beanManager();
-        }
-
-        @Override
-        public Map.Entry<Closeable, Object> get() {
-            CreationalContext<?> cc = getBeanManager().createCreationalContext(null);
-            return new Map.Entry<Closeable, Object>() {
-                @Override
-                public Closeable getKey() {
-                    return new Closeable() {
-                        @Override
-                        public void close() throws IOException {
-                            cc.release();
-                        }
-                    };
-                }
-
-                @Override
-                public Object getValue() {
-                    return cc;
-                }
-
-                @Override
-                public Object setValue(Object value) {
-                    return null;
-                }
-            };
-        }
-    }
-
-    public static class IsolatedEnricher implements BiFunction<Method, Object, Object[]> {
-
-        @SuppressWarnings("unchecked")
-        private <T> T getInstanceByType(BeanManager manager, final int position, final Method method, CreationalContext<?> cc) {
-            return (T) manager.getInjectableReference(new MethodParameterInjectionPoint<T>(method, position), cc);
-        }
-
-        private BeanManager getBeanManager() {
-            ArcContainer container = Arc.container();
-            if (container == null) {
-                return null;
-            }
-            return container.beanManager();
-        }
-
-        @Override
-        public Object[] apply(Method method, Object creationalContext) {
-            Object[] values = new Object[method.getParameterTypes().length];
+        Object[] values = new Object[method.getParameterTypes().length];
+        if (values.length > 0) {
 
             // TestNG - we want to skip resolution if a non-arquillian dataProvider is used
             boolean hasNonArquillianDataProvider = false;
@@ -156,44 +74,29 @@ public class InjectionEnricher implements TestEnricher {
             }
             try {
                 // obtain the same method definition but from the TCCL
-                method = getClass().getClassLoader()
+                method = Thread.currentThread().getContextClassLoader()
                         .loadClass(method.getDeclaringClass().getName())
-                        .getMethod(method.getName(), convertToCL(method.getParameterTypes(), getClass().getClassLoader()));
+                        .getMethod(method.getName(), convertToTCCL(method.getParameterTypes()));
             } catch (Throwable t) {
                 throw new RuntimeException(t);
             }
             Class<?>[] parameterTypes = method.getParameterTypes();
             for (int i = 0; i < parameterTypes.length; i++) {
                 try {
-                    values[i] = getInstanceByType(beanManager, i, method, (CreationalContext<?>) creationalContext);
+                    values[i] = getInstanceByType(beanManager, i, method);
                 } catch (Exception e) {
                     log.warn("InjectionEnricher tried to lookup method parameter of type "
                             + parameterTypes[i] + " but caught exception", e);
                 }
             }
-            return values;
         }
+        return values;
     }
 
-    public static class CreationContextHolder implements Closeable {
-
-        final Closeable closeable;
-        final Object creationalContext;
-
-        public CreationContextHolder(Closeable closeable, Object creationalContext) {
-            this.closeable = closeable;
-            this.creationalContext = creationalContext;
-        }
-
-        @Override
-        public void close() throws IOException {
-            //don't think about this too much
-            if (closeable != null) {
-                closeable.close();
-            } else {
-                ((CreationalContext) creationalContext).release();
-            }
-        }
+    @SuppressWarnings("unchecked")
+    private <T> T getInstanceByType(BeanManager manager, final int position, final Method method) {
+        CreationalContext<?> cc = getCreationalContext();
+        return (T) manager.getInjectableReference(new MethodParameterInjectionPoint<T>(method, position), cc);
     }
 
 }
