43c2ba008c03ecb28478c6a7347eaaf1c7bc38fc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:27:57 +0200

    added some javadoc on @Extra

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/annotations/Extra.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/annotations/Extra.java
@@ -1,13 +1,13 @@
-/**
- * Copyright (C) 2010-2011 eBusiness Information, Excilys Group
- *
+/*
+ * Copyright 2010-2011 Pierre-Yves Ricau (py.ricau at gmail.com)
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed To in writing, software
+ * 
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
@@ -24,11 +24,9 @@ import java.lang.annotation.Target;
  * Use on Extra fields in activity classes. This String value field correspond
  * to the Android extra name.
  * 
- * If {@link Extra} is used on fields in an Activity, the intent builder will hold dedicated methods for these extras.
- * 
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.FIELD)
 public @interface Extra {
-	String value();
+    String value();
 }
