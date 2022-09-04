573920ff359f6acb970068e294be8cc649e010d6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:52:15 +0200

    fix incorrect index range calculation
        the check for the deflector index target was inverted
    
        fixes #1217, fixes Graylog2/graylog2-web-interface#1266

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/ranges/CreateNewSingleIndexRangeJob.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/ranges/CreateNewSingleIndexRangeJob.java
@@ -19,27 +19,31 @@ package org.graylog2.indexer.ranges;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
 import org.graylog2.indexer.Deflector;
+import org.graylog2.indexer.EmptyIndexException;
+import org.graylog2.indexer.searches.Searches;
+import org.graylog2.plugin.database.ValidationException;
 import org.graylog2.shared.system.activities.ActivityWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import java.util.Map;
 
 public class CreateNewSingleIndexRangeJob extends RebuildIndexRangesJob {
     private static final Logger LOG = LoggerFactory.getLogger(CreateNewSingleIndexRangeJob.class);
     private final String indexName;
 
     public interface Factory {
-        CreateNewSingleIndexRangeJob create(Deflector deflector, String indexName);
+        public CreateNewSingleIndexRangeJob create(Deflector deflector, String indexName);
     }
 
     @AssistedInject
     public CreateNewSingleIndexRangeJob(@Assisted Deflector deflector,
                                         @Assisted String indexName,
+                                        Searches searches,
                                         ActivityWriter activityWriter,
                                         IndexRangeService indexRangeService) {
-        super(deflector, activityWriter, indexRangeService);
-        this.indexName = checkNotNull(indexName);
+        super(deflector, searches, activityWriter, indexRangeService);
+        this.indexName = indexName;
     }
 
     @Override
@@ -56,11 +60,23 @@ public class CreateNewSingleIndexRangeJob extends RebuildIndexRangesJob {
     public void execute() {
         LOG.info("Calculating ranges for index {}.", indexName);
         try {
-            final IndexRange indexRange = indexRangeService.calculateRange(indexName);
+            final Map<String, Object> range;
+            if (deflector.getCurrentActualTargetIndex().equals(indexName)) {
+                range = getDeflectorIndexRange(indexName);
+            } else {
+                range = calculateRange(indexName);
+            }
+
+            final IndexRange indexRange = indexRangeService.create(range);
+            indexRangeService.destroy(indexName);
             indexRangeService.save(indexRange);
             LOG.info("Created ranges for index {}.", indexName);
+        } catch (EmptyIndexException e) {
+            LOG.error("Unable to calculate ranges for index {}: {}", indexName, e);
+        } catch (ValidationException e) {
+            LOG.error("Unable to save index range for index {}: {}", indexName, e);
         } catch (Exception e) {
-            LOG.error("Exception during index range calculation for index " + indexName, e);
+            LOG.error("Exception during index range calculation for index {}: ", indexName, e);
         }
     }
 
