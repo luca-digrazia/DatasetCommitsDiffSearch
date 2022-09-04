8b057fff2ca1564878fa269f6f679b57e4820f41
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:12:20 +0200

    add Dataset.toList()

--- dataset/GitHub_Java/haifengl.smile/data/src/main/java/smile/data/Dataset.java
+++ dataset/GitHub_Java/haifengl.smile/data/src/main/java/smile/data/Dataset.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import smile.math.matrix.Matrix;
 
@@ -34,7 +35,6 @@ import smile.math.matrix.Matrix;
 public interface Dataset<T> {
     /**
      * Returns true if the dataset is distributed over multiple machines.
-     * @return true if the dataset is distributed over multiple machines.
      */
     default boolean distributed() {
         return false;
@@ -42,14 +42,10 @@ public interface Dataset<T> {
 
     /**
      * Returns the number of elements in this collection.
-     * @return the number of elements in this collection.
      */
     int size();
 
-    /**
-     * Returns true if the dataset is empty.
-     * @return true if the dataset is empty.
-     */
+    /** Returns true if the dataset is empty. */
     default boolean isEmpty() {
         return size() == 0;
     }
@@ -57,14 +53,12 @@ public interface Dataset<T> {
     /**
      * Returns the element at the specified position in this dataset.
      * @param i the index of the element to be returned.
-     * @return the i-th element.
      */
     T get(int i);
 
     /**
      * Returns the element at the specified position in this dataset.
      * @param i the index of the element to be returned.
-     * @return the i-th element.
      */
     default T apply(int i) {
         return get(i);
@@ -82,13 +76,12 @@ public interface Dataset<T> {
      * @return the <code>List</code> of data items.
      */
     default List<T> toList() {
-        return stream().collect(java.util.stream.Collectors.toList());
+        return stream().collect(Collectors.toList());
     }
 
     /**
-     * Returns the string representation of the dataset.
-     * @param numRows the number of rows to show.
-     * @return the string representation of the dataset.
+     * Returns the string representation of the row.
+     * @param numRows Number of rows to show
      */
     default String toString(int numRows) {
         StringBuilder sb = new StringBuilder();
@@ -104,59 +97,42 @@ public interface Dataset<T> {
         return sb.toString();
     }
 
-    /**
-     * Returns a default implementation of Dataset from a collection.
-     * @param data the data collection.
-     * @param <T> the type of input elements.
-     * @return the dataset.
-     */
+    /** Returns a default implementation of Dataset from a collection. */
     static <T> Dataset<T> of(Collection<T> data) {
         return new DatasetImpl<>(data);
     }
 
-    /** Stream collectors. */
-    interface Collectors {
-        /**
-         * Returns a stream collector that accumulates elements into a Dataset.
-         *
-         * @param <T> the type of input elements.
-         * @return the stream collector.
-         */
-        static <T> Collector<T, List<T>, Dataset<T>> toDataset() {
-            return Collector.of(
-                    // supplier
-                    ArrayList::new,
-                    // accumulator
-                    List::add,
-                    // combiner
-                    (c1, c2) -> {
-                        c1.addAll(c2);
-                        return c1;
-                    },
-                    // finisher
-                    Dataset::of
-            );
-        }
+    /**
+     * Returns a stream collector that accumulates elements into a Dataset.
+     *
+     * @param <T> the type of input elements to the reduction operation
+     */
+    static <T> Collector<T, List<T>, Dataset<T>> toDataset() {
+        return Collector.of(
+                // supplier
+                ArrayList::new,
+                // accumulator
+                List::add,
+                // combiner
+                (c1, c2) -> { c1.addAll(c2); return c1; },
+                // finisher
+                Dataset::of
+        );
+    }
 
-        /**
-         * Returns a stream collector that accumulates elements into a Matrix.
-         *
-         * @return the stream collector.
-         */
-        static Collector<double[], List<double[]>, Matrix> toMatrix() {
-            return Collector.of(
-                    // supplier
-                    ArrayList::new,
-                    // accumulator
-                    List::add,
-                    // combiner
-                    (c1, c2) -> {
-                        c1.addAll(c2);
-                        return c1;
-                    },
-                    // finisher
-                    (container) -> new Matrix(container.toArray(new double[container.size()][]))
-            );
-        }
+    /**
+     * Returns a stream collector that accumulates elements into a Matrix.
+     */
+    static Collector<double[], List<double[]>, Matrix> toMatrix() {
+        return Collector.of(
+                // supplier
+                ArrayList::new,
+                // accumulator
+                List::add,
+                // combiner
+                (c1, c2) -> { c1.addAll(c2); return c1; },
+                // finisher
+                (container) -> new Matrix(container.toArray(new double[container.size()][]))
+        );
     }
 }
