f8587d5a7fd59205d1fa914b02855439d9b0f326
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:14:36 +0200

    Add e2e test for eof exception mapper (#2235)

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-e2e/src/test/java/com/example/app1/App1Test.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-e2e/src/test/java/com/example/app1/App1Test.java
@@ -1,16 +1,14 @@
 package com.example.app1;
 
+import com.google.common.collect.ImmutableMap;
 import io.dropwizard.Configuration;
 import io.dropwizard.client.JerseyClientBuilder;
-import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.jackson.Jackson;
 import io.dropwizard.testing.ResourceHelpers;
-import io.dropwizard.testing.junit5.DropwizardAppExtension;
-import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
-import io.dropwizard.util.Duration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
@@ -20,34 +18,27 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
-import static org.awaitility.Awaitility.await;
 
-@ExtendWith(DropwizardExtensionsSupport.class)
 public class App1Test {
-    public static final DropwizardAppExtension<Configuration> RULE =
-        new DropwizardAppExtension<>(App1.class, ResourceHelpers.resourceFilePath("app1/config.yml"));
+    @ClassRule
+    public static final DropwizardAppRule<Configuration> RULE =
+        new DropwizardAppRule<>(App1.class, ResourceHelpers.resourceFilePath("app1/config.yml"));
 
     private static Client client;
 
-    @BeforeAll
+    @BeforeClass
     public static void setup() {
-        final JerseyClientConfiguration config = new JerseyClientConfiguration();
-        // Avoid flakiness with default timeouts in CI builds
-        config.setTimeout(Duration.seconds(5));
         client = new JerseyClientBuilder(RULE.getEnvironment())
             .withProvider(new CustomJsonProvider(Jackson.newObjectMapper()))
-            .using(config)
             .build("test client");
     }
 
     @Test
-    void custom204OnEmptyOptional() {
+    public void custom204OnEmptyOptional() {
         final Client client = new JerseyClientBuilder(RULE.getEnvironment()).build("test client 1");
         final String url = String.format("http://localhost:%d/empty-optional", RULE.getLocalPort());
         final Response response = client.target(url).request().get();
@@ -55,7 +46,7 @@ public class App1Test {
     }
 
     @Test
-    void custom404OnViewRenderMissingMustacheTemplate() {
+    public void custom404OnViewRenderMissingMustacheTemplate() {
         final Client client = new JerseyClientBuilder(RULE.getEnvironment()).build("test client 2");
         final String url = String.format("http://localhost:%d/view-with-missing-tpl", RULE.getLocalPort());
 
@@ -64,7 +55,7 @@ public class App1Test {
     }
 
     @Test
-    void earlyEofTest() throws IOException, InterruptedException {
+    public void earlyEofTest() throws IOException, InterruptedException {
         // Only eof test so we ensure it's false before test
         ((App1)RULE.getApplication()).wasEofExceptionHit = false;
 
@@ -78,13 +69,13 @@ public class App1Test {
         conn.getOutputStream().write("{".getBytes(StandardCharsets.UTF_8));
         conn.disconnect();
 
-        await().atMost(5, TimeUnit.SECONDS)
-                .until(() -> ((App1) RULE.getApplication()).wasEofExceptionHit);
-        assertThat(((App1) RULE.getApplication()).wasEofExceptionHit).isTrue();
+        // Wait a bit for the app to process the request.
+        Thread.sleep(500);
+        assertThat(((App1)RULE.getApplication()).wasEofExceptionHit).isTrue();
     }
 
     @Test
-    void customJsonProvider() {
+    public void customJsonProvider() {
         final String url = String.format("http://localhost:%d/mapper", RULE.getLocalPort());
         final String response = client.target(url)
             .request()
@@ -94,7 +85,7 @@ public class App1Test {
     }
 
     @Test
-    void customJsonProviderMissingHeader() {
+    public void customJsonProviderMissingHeader() {
         final String url = String.format("http://localhost:%d/mapper", RULE.getLocalPort());
         final Response response = client.target(url)
             .request()
@@ -103,29 +94,29 @@ public class App1Test {
     }
 
     @Test
-    void customJsonProviderClient() {
+    public void customJsonProviderClient() {
         final String url = String.format("http://localhost:%d/mapper", RULE.getLocalPort());
         final String response = client.target(url)
             .request()
-            .post(Entity.json(Collections.singletonMap("check", "mate")), String.class);
+            .post(Entity.json(ImmutableMap.of("check", "mate")), String.class);
         assertThat(response).isEqualTo("/** A Dropwizard specialty */\n" +
             "{\"check\":\"mate\",\"hello\":\"world\"}");
     }
 
     @Test
-    void customJsonProviderRoundtrip() {
+    public void customJsonProviderRoundtrip() {
         final String url = String.format("http://localhost:%d/mapper", RULE.getLocalPort());
         final GenericType<Map<String, String>> typ = new GenericType<Map<String, String>>() {
         };
 
         final Map<String, String> response = client.target(url)
             .request()
-            .post(Entity.json(Collections.singletonMap("check", "mate")), typ);
+            .post(Entity.json(ImmutableMap.of("check", "mate")), typ);
         assertThat(response).containsExactly(entry("check", "mate"), entry("hello", "world"));
     }
 
     @Test
-    void customBodyWriterTest() {
+    public void customBodyWriterTest() {
         final String url = String.format("http://localhost:%d/custom-class", RULE.getLocalPort());
         final String response = client.target(url)
             .request()
