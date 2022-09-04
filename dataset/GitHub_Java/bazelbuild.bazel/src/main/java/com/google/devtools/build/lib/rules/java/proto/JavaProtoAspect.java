7847293a88c154afa6341c3ba5001a858980337f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 17:05:30 +0200

    Include source jar in transitive jars in Java proto aspects
        The source jar is expected to be in the set of transitive jars in a
        JavaInfo provider.
    
        Fixes https://github.com/bazelbuild/bazel/issues/11830
    
        PiperOrigin-RevId: 338998311

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/proto/JavaProtoAspect.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/proto/JavaProtoAspect.java
@@ -42,7 +42,6 @@ import com.google.devtools.build.lib.rules.java.JavaConfiguration;
 import com.google.devtools.build.lib.rules.java.JavaInfo;
 import com.google.devtools.build.lib.rules.java.JavaRuleClasses;
 import com.google.devtools.build.lib.rules.java.JavaRuleOutputJarsProvider;
-import com.google.devtools.build.lib.rules.java.JavaRuleOutputJarsProvider.OutputJar;
 import com.google.devtools.build.lib.rules.java.JavaSemantics;
 import com.google.devtools.build.lib.rules.java.JavaSourceJarsProvider;
 import com.google.devtools.build.lib.rules.proto.ProtoCompileActionBuilder;
@@ -130,7 +129,7 @@ public class JavaProtoAspect extends NativeAspectClass implements ConfiguredAspe
                 ImmutableList.of(StarlarkProviderIdentifier.forKey(JavaInfo.PROVIDER.getKey())))
             .add(
                 attr(JavaProtoAspectCommon.SPEED_PROTO_TOOLCHAIN_ATTR, LABEL)
-                    // TODO(carmi): reinstate mandatoryBuiltinProviders(ProtoLangToolchainProvider)
+                    // TODO(carmi): reinstate mandatoryNativeProviders(ProtoLangToolchainProvider)
                     // once it's in a Bazel release.
                     .legacyAllowAnyFileType()
                     .value(getSpeedProtoToolchainLabel(defaultSpeedProtoToolchainLabel)))
@@ -219,11 +218,10 @@ public class JavaProtoAspect extends NativeAspectClass implements ConfiguredAspe
         JavaRuleOutputJarsProvider ruleOutputJarsProvider =
             JavaRuleOutputJarsProvider.builder()
                 .addOutputJar(
-                    OutputJar.builder()
-                        .setClassJar(outputJar)
-                        .setCompileJar(compileTimeJar)
-                        .addSourceJar(sourceJar)
-                        .build())
+                    outputJar,
+                    compileTimeJar,
+                    null /* manifestProto */,
+                    ImmutableList.of(sourceJar))
                 .build();
         JavaSourceJarsProvider sourceJarsProvider =
             JavaSourceJarsProvider.create(
