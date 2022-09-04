5241d94582c9fb01f37e64d184f036d80eb34d7a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:17:25 +0200

    More JPA fixes

--- dataset/GitHub_Java/quarkusio.quarkus/jpa/deployment/src/test/java/org/jboss/shamrock/jpa/HqlNodeScannerTestCase.java
+++ dataset/GitHub_Java/quarkusio.quarkus/jpa/deployment/src/test/java/org/jboss/shamrock/jpa/HqlNodeScannerTestCase.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.jboss.shamrock.jpa;
 
 import java.io.FileInputStream;
@@ -29,7 +13,7 @@ import org.jboss.jandex.Indexer;
 import org.junit.Ignore;
 import org.junit.Test;
 
-//not a real test, but you can use it to
+//not a real test, but you can use to to
 //get all not implementation that need to be added for reflection
 @Ignore
 public class HqlNodeScannerTestCase {
