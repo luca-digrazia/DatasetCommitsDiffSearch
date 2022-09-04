0e491780f0b97623a62a82c3efdde25cefacb077
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:44:07 +0200

    some input types were not fixed in #273

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/gelf/udp/GELFUDPInput.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/gelf/udp/GELFUDPInput.java
@@ -21,6 +21,8 @@
 package org.graylog2.inputs.gelf.udp;
 
 
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.graylog2.inputs.gelf.GELFInputBase;
 import org.graylog2.plugin.inputs.*;
@@ -30,6 +32,7 @@ import org.jboss.netty.channel.socket.nio.NioDatagramChannelFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -44,6 +47,11 @@ public class GELFUDPInput extends GELFInputBase {
 
     @Override
     public void launch() throws MisfireException {
+        // Register throughput counter gauges.
+        for(Map.Entry<String,Gauge<Long>> gauge : throughputCounter.gauges().entrySet()) {
+            core.metrics().register(MetricRegistry.name(getUniqueReadableId(), gauge.getKey()), gauge.getValue());
+        }
+
         final ExecutorService workerThreadPool = Executors.newCachedThreadPool(
                 new ThreadFactoryBuilder()
                         .setNameFormat("input-" + inputId + "-gelfudp-worker-%d")
@@ -54,10 +62,10 @@ public class GELFUDPInput extends GELFInputBase {
         bootstrap.setOption("receiveBufferSizePredictorFactory", new FixedReceiveBufferSizePredictorFactory(
                 core.getConfiguration().getUdpRecvBufferSizes())
         );
-        bootstrap.setPipelineFactory(new GELFUDPPipelineFactory(core, this));
+        bootstrap.setPipelineFactory(new GELFUDPPipelineFactory(core, this, throughputCounter));
 
         try {
-            ((ConnectionlessBootstrap) bootstrap).bind(socketAddress);
+            channel = ((ConnectionlessBootstrap) bootstrap).bind(socketAddress);
             LOG.info("Started GELF UDP input on {}", socketAddress);
         } catch (Exception e) {
             String msg = "Could not bind UDP GELF input to address " + socketAddress;
