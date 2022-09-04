249db984eeb81f47e51015a6a0363697950aa77a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:47:49 +0200

    Qute - register param expressions for includes and user-defined tags
        - only registered expression can be validated at build time

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/IncludeSectionHelper.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/main/java/io/quarkus/qute/IncludeSectionHelper.java
@@ -13,6 +13,7 @@ import java.util.function.Supplier;
 
 public class IncludeSectionHelper implements SectionHelper {
 
+    static final String DEFAULT_NAME = "$default$";
     private static final String TEMPLATE = "template";
 
     private final Supplier<Template> templateSupplier;
@@ -30,7 +31,7 @@ public class IncludeSectionHelper implements SectionHelper {
     public CompletionStage<ResultNode> resolve(SectionResolutionContext context) {
         if (parameters.isEmpty()) {
             return ((TemplateImpl) templateSupplier.get()).root
-                    .resolve(context.resolutionContext().createChild(null, null, extendingBlocks));
+                    .resolve(context.resolutionContext().createChild(null, extendingBlocks));
         } else {
             CompletableFuture<ResultNode> result = new CompletableFuture<>();
             evaluateParams(parameters, context.resolutionContext()).whenComplete((evaluatedParams, t1) -> {
@@ -41,7 +42,7 @@ public class IncludeSectionHelper implements SectionHelper {
                         // Execute the template with the params as the root context object
                         TemplateImpl tagTemplate = (TemplateImpl) templateSupplier.get();
                         tagTemplate.root
-                                .resolve(context.resolutionContext().createChild(evaluatedParams, null, extendingBlocks))
+                                .resolve(context.resolutionContext().createChild(Mapper.wrap(evaluatedParams), extendingBlocks))
                                 .whenComplete((resultNode, t2) -> {
                                     if (t2 != null) {
                                         result.completeExceptionally(t2);
@@ -77,12 +78,29 @@ public class IncludeSectionHelper implements SectionHelper {
         }
 
         @Override
+        public Scope initializeBlock(Scope outerScope, BlockInfo block) {
+            if (block.getLabel().equals(MAIN_BLOCK_NAME)) {
+                for (Entry<String, String> entry : block.getParameters().entrySet()) {
+                    if (!entry.getKey().equals(TEMPLATE)) {
+                        block.addExpression(entry.getKey(), entry.getValue());
+                    }
+                }
+                return outerScope;
+            } else {
+                return outerScope;
+            }
+        }
+
+        @Override
         public IncludeSectionHelper initialize(SectionInitContext context) {
 
             Map<String, SectionBlock> extendingBlocks = new HashMap<>();
-            if (context.getBlocks().size() > 1) {
-                for (SectionBlock block : context.getBlocks().subList(1, context.getBlocks().size())) {
-                    extendingBlocks.put(block.label, block);
+            for (SectionBlock block : context.getBlocks()) {
+                String name = block.id.equals(MAIN_BLOCK_NAME) ? DEFAULT_NAME : block.label;
+                if (extendingBlocks.put(name, block) != null) {
+                    throw new TemplateException(context.getBlocks().get(0).origin, String.format(
+                            "Multiple blocks define the content for the insert section '%s' in template %s",
+                            name, block.origin.getTemplateId()));
                 }
             }
 
@@ -93,7 +111,7 @@ public class IncludeSectionHelper implements SectionHelper {
                 params = new HashMap<>();
                 for (Entry<String, String> entry : context.getParameters().entrySet()) {
                     if (!entry.getKey().equals(TEMPLATE)) {
-                        params.put(entry.getKey(), context.parseValue(entry.getValue()));
+                        params.put(entry.getKey(), context.getExpression(entry.getKey()));
                     }
                 }
             }
--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/test/java/io/quarkus/qute/IncludeTest.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/qute/core/src/test/java/io/quarkus/qute/IncludeTest.java
@@ -1,6 +1,8 @@
 package io.quarkus.qute;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,6 +50,109 @@ public class IncludeTest {
         engine.putTemplate("detail", engine.parse("<strong>{name}</strong>:{price}"));
         assertEquals("<strong>Al</strong>:100",
                 engine.parse("{#include detail/}").render(data));
+    }
+
+    @Test
+    public void testOptionalBlockEndTags() {
+        Engine engine = Engine.builder().addDefaults().build();
+        engine.putTemplate("super", engine.parse("{#insert header}header{/}:{#insert footer /}"));
+        assertEquals("super header:super footer",
+                engine.parse("{#include super}{#header}super header{#footer}super footer{/include}").render());
+    }
+
+    @Test
+    public void testIncludeInLoop() {
+        Engine engine = Engine.builder().addDefaults().build();
+        engine.putTemplate("foo", engine.parse("{#insert snippet}empty{/insert}"));
+        assertEquals("1.2.3.4.5.",
+                engine.parse("{#for i in 5}{#include foo}{#snippet}{count}.{/snippet} this should be ingored {/include}{/for}")
+                        .render());
+    }
+
+    @Test
+    public void testIncludeInIf() {
+        Engine engine = Engine.builder().addDefaults().build();
+        engine.putTemplate("foo", engine.parse("{#insert snippet}empty{/insert}"));
+        assertEquals("1",
+                engine.parse("{#if true}{#include foo} {#snippet}1{/snippet} {/include}{/if}")
+                        .render());
+    }
+
+    @Test
+    public void testUserTagInsideInsert() {
+        Engine engine = Engine.builder().addDefaults().addSectionHelper(new UserTagSectionHelper.Factory("hello", "hello"))
+                .build();
+        engine.putTemplate("hello", engine.parse("{name}"));
+        engine.putTemplate("base", engine.parse("{#insert snippet}{/insert}"));
+        assertEquals("foo",
+                engine.parse("{#include base} {#snippet}{#hello name='foo'/}{/snippet} {/include}")
+                        .render());
+    }
+
+    @Test
+    public void testIncludeStandaloneLines() {
+        Engine engine = Engine.builder().addDefaults().removeStandaloneLines(true).build();
+        engine.putTemplate("super", engine.parse("{#insert header}\n"
+                + "default header\n"
+                + "{/insert}"));
+        assertEquals("super header\n",
+                engine.parse("{#include super}\n"
+                        + "{#header}\n"
+                        + "super header\n"
+                        + "{/header}\n"
+                        + "{/include}").render());
+    }
+
+    @Test
+    public void testEmptyInclude() {
+        Engine engine = Engine.builder().addDefaults().build();
+        engine.putTemplate("bar/fool.html", engine.parse("{foo} and {that}"));
+        assertEquals("1 and true", engine.parse("{#include bar/fool.html that=true /}").data("foo", 1).render());
+    }
+
+    @Test
+    public void testInsertParam() {
+        Engine engine = Engine.builder().addDefaults().build();
+        engine.putTemplate("super", engine.parse("{#insert header}default header{/insert} and {#insert footer}{that}{/}"));
+        Template foo = engine.parse("{#include 'super' that=foo}{#header}{that}{/}{/}");
+        // foo, that
+        assertEquals(2, foo.getExpressions().size());
+        assertEquals("1 and 1", foo.data("foo", 1).render());
+    }
+
+    @Test
+    public void testDefaultInsert() {
+        Engine engine = Engine.builder().addDefaults().build();
+        engine.putTemplate("super", engine.parse("<html>"
+                + "<head>"
+                + "<meta charset=\"UTF-8\">"
+                + "<title>{#insert title}Default Title{/}</title>"
+                + "</head>"
+                + "<body>"
+                + "  {#insert}No body!{/}"
+                + "</body>"
+                + "</html>"));
+        assertEquals("<html>"
+                + "<head>"
+                + "<meta charset=\"UTF-8\">"
+                + "<title>My Title</title>"
+                + "</head>"
+                + "<body>"
+                + "  Body 1!"
+                + "</body>"
+                + "</html>", engine.parse("{#include super}{#title}My Title{/title}Body {foo}!{/}").data("foo", 1).render());
+    }
+
+    @Test
+    public void testAmbiguousInserts() {
+        Engine engine = Engine.builder().addDefaults().build();
+        engine.putTemplate("super", engine.parse("{#insert header}default header{/insert}"));
+        try {
+            engine.parse("{#include super}{#header}1{/}{#header}2{/}{/}");
+            fail();
+        } catch (TemplateException expected) {
+            assertTrue(expected.getMessage().contains("Multiple blocks"));
+        }
     }
 
 }
