64e49038504416e62c799c6946f2e03654be05a7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:58:31 +0200

    Make sure the @DefaultMessageOutput is always the same instance.
        Unbreaks at least the ElasticSearch cache flush thread.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/MessageOutputBindings.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/MessageOutputBindings.java
@@ -1,19 +1,3 @@
-/**
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.bindings;
 
 import com.google.inject.AbstractModule;
@@ -22,7 +6,6 @@ import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.Multibinder;
 import org.graylog2.outputs.BatchedElasticSearchOutput;
 import org.graylog2.outputs.DefaultMessageOutput;
-import org.graylog2.outputs.GelfOutput;
 import org.graylog2.outputs.LoggingOutput;
 import org.graylog2.plugin.outputs.MessageOutput;
 
@@ -37,7 +20,7 @@ public class MessageOutputBindings extends AbstractModule {
         TypeLiteral<Class<? extends MessageOutput>> typeLiteral = new TypeLiteral<Class<? extends MessageOutput>>(){};
         Multibinder<Class<? extends MessageOutput>> messageOutputs = Multibinder.newSetBinder(binder(), typeLiteral);
 
+        messageOutputs.addBinding().toInstance(BatchedElasticSearchOutput.class);
         messageOutputs.addBinding().toInstance(LoggingOutput.class);
-        messageOutputs.addBinding().toInstance(GelfOutput.class);
     }
 }
