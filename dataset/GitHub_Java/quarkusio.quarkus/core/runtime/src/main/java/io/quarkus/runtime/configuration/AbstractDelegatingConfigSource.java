b0d8a5bd2420002be19443525a943a72dbbaaab4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:17:37 +0200

    Delegate ordinal across delegating configuration source
        Fixes #2471

--- dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/io/quarkus/runtime/configuration/AbstractDelegatingConfigSource.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/runtime/src/main/java/io/quarkus/runtime/configuration/AbstractDelegatingConfigSource.java
@@ -1,6 +1,5 @@
 package io.quarkus.runtime.configuration;
 
-import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,8 +12,7 @@ import io.smallrye.config.ConfigSourceMap;
 /**
  * A base class for configuration sources which delegate to other configuration sources.
  */
-public abstract class AbstractDelegatingConfigSource implements ConfigSource, Serializable {
-    private static final long serialVersionUID = -6636734120743034580L;
+public abstract class AbstractDelegatingConfigSource implements ConfigSource {
     protected final ConfigSource delegate;
     private Map<String, String> propertiesMap;
 
