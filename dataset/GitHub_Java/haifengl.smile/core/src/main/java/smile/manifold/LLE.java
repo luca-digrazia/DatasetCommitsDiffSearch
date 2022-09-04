b8a628ff6a26db0c269d6bd349aea856f9735be5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 02:45:36 +0200

    disable ARPACK eigen decomposition in LLE as it is not stable there

--- dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/manifold/LLE.java
+++ dataset/GitHub_Java/haifengl.smile/core/src/main/java/smile/manifold/LLE.java
@@ -15,6 +15,7 @@
  *******************************************************************************/
 package smile.manifold;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Comparator;
 import org.slf4j.Logger;
@@ -212,7 +213,7 @@ public class LLE {
         /*
         try {
             Class<?> clazz = Class.forName("smile.netlib.ARPACK");
-            java.lang.reflect.Method method = clazz.getMethod("eigen", Matrix.class, Integer.TYPE, String.class);
+            Method method = clazz.getMethod("eigen", Matrix.class, Integer.TYPE, String.class);
             EVD eigen = (EVD) method.invoke(null, M, d+1, "SA");
             DenseMatrix V = eigen.getEigenVectors();
             coordinates = new double[n][d];
