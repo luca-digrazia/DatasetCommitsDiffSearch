7953f7841f1dd14e1d907c6f3362081a2eb81bd2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:40:29 +0200

    [Release-1.5.0] optimize some parameter default values

--- dataset/GitHub_Java/Angel-ML.angel/angel-ps/core/src/main/java/com/tencent/angel/conf/AngelConf.java
+++ dataset/GitHub_Java/Angel-ML.angel/angel-ps/core/src/main/java/com/tencent/angel/conf/AngelConf.java
@@ -36,6 +36,7 @@ import java.util.Properties;
  * Angel system parameters.
  */
 public class AngelConf extends Configuration {
+
   public AngelConf(Configuration conf) {
     super(conf);
   }
@@ -240,7 +241,7 @@ public class AngelConf extends Configuration {
   @Deprecated
   public static final String ANGEL_AM_MEMORY_MB = ANGEL_AM_PREFIX + "memory.mb";
   @Deprecated
-  public static final int DEFAULT_ANGEL_AM_MEMORY_MB = 1280;
+  public static final int DEFAULT_ANGEL_AM_MEMORY_MB = 2048;
 
   /** Memory quota for AppMaster in GB. */
   public static final String ANGEL_AM_MEMORY_GB = ANGEL_AM_PREFIX + "memory.gb";
@@ -327,15 +328,6 @@ public class AngelConf extends Configuration {
    */
   public static final String ANGEL_AM_MATRIX_DISKIO_WORKER_POOL_SIZE = ANGEL_AM_PREFIX + "matrix.diskio.worker.pool.size";
   public static final int DEFAULT_ANGEL_AM_MATRIX_DISKIO_WORKER_POOL_SIZE = Math.max(8, (int)(Runtime.getRuntime().availableProcessors() * 0.25));
-
-  public static final String ANGEL_MODEL_PARTITIONER_PARTITION_SIZE = "angel.model.partitioner.partition.size";
-  public static final long DEFAULT_ANGEL_MODEL_PARTITIONER_PARTITION_SIZE = 500000;
-
-  public static final String ANGEL_MODEL_PARTITIONER_MAX_PARTITION_NUM = "angel.model.partitioner.max.partition.number";
-  public static final int DEFAULT_ANGEL_MODEL_PARTITIONER_MAX_PARTITION_NUM = 10000;
-
-  public static final String ANGEL_MODEL_PARTITIONER_PARTITION_NUM_PERSERVER = "angel.model.partitioner.partition.number.perserver";
-  public static final int DEFAULT_ANGEL_MODEL_PARTITIONER_PARTITION_NUM_PERSERVER = 1;
 
   // //////////////////////////////
   // Worker Configs
@@ -670,7 +662,7 @@ public class AngelConf extends Configuration {
    */
   public static final String ANGEL_MATRIXTRANSFER_MAX_REQUESTNUM_PERSERVER = ANGEL_PREFIX
       + "matrixtransfer.max.requestnum.perserver";
-  public static final int DEFAULT_ANGEL_MATRIXTRANSFER_MAX_REQUESTNUM_PERSERVER = 16;
+  public static final int DEFAULT_ANGEL_MATRIXTRANSFER_MAX_REQUESTNUM_PERSERVER = 8;
 
   public static final String ANGEL_MATRIXTRANSFER_CLIENT_REQUESTER_POOL_SIZE = ANGEL_PREFIX
     + "matrixtransfer.client.requester.pool.size";
@@ -928,8 +920,5 @@ public class AngelConf extends Configuration {
     for (Map.Entry<String, String> e : srcConf) {
       destConf.set(e.getKey(), e.getValue());
     }
-  }
-
-  public class ANGEL_MODEL_PARTITIONER_DEFAULT_PARTITION_SIZE {
   }
 }
