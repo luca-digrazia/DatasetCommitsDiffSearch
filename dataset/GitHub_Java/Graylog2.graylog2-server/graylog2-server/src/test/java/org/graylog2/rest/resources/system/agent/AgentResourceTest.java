9adf1a402f01a48286fb697e261ab15b94d226fd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:57:36 +0200

    Remove superfluous Agent#getOperatingSystem() method

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/rest/resources/system/agent/AgentResourceTest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/test/java/org/graylog2/rest/resources/system/agent/AgentResourceTest.java
@@ -1,28 +1,61 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog2.rest.resources.system.agent;
 
+import com.github.joschi.jadconfig.util.Duration;
+import com.google.common.base.Function;
 import com.google.common.collect.Lists;
+import org.graylog2.Configuration;
 import org.graylog2.agents.Agent;
+import org.graylog2.agents.AgentNodeDetails;
+import org.graylog2.agents.AgentService;
+import org.graylog2.database.NotFoundException;
 import org.graylog2.rest.models.agent.responses.AgentList;
 import org.graylog2.rest.models.agent.responses.AgentSummary;
 import org.graylog2.rest.resources.RestResourceBaseTest;
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(value = MockitoJUnitRunner.class)
 public class AgentResourceTest extends RestResourceBaseTest {
     private AgentResource resource;
     private List<Agent> agents;
 
+    @Mock
+    private AgentService agentService;
+
     @Before
     public void setUp() throws Exception {
         this.agents = getDummyAgentList();
-        this.resource = new AgentResource();
+        final Configuration config = mock(Configuration.class);
+        when(config.getAgentInactiveThreshold()).thenReturn(Duration.minutes(1));
+        this.resource = new AgentResource(agentService, config);
+        when(agentService.all()).thenReturn(agents);
     }
 
     @Test
@@ -34,7 +67,7 @@ public class AgentResourceTest extends RestResourceBaseTest {
         assertEquals("Agent list should be of same size as dummy list", agents.size(), response.agents().size());
     }
 
-    @Test
+    @Test(expected = NotFoundException.class)
     public void testGetNotExisting() throws Exception {
         final AgentSummary response = this.resource.get("Nonexisting");
 
@@ -44,22 +77,26 @@ public class AgentResourceTest extends RestResourceBaseTest {
     @Test
     public void testGet() throws Exception {
         final Agent agent = agents.get(agents.size() - 1);
+        when(agentService.findById(agent.getId())).thenReturn(agent);
+        final AgentSummary agentSummary = mock(AgentSummary.class);
+        when(agent.toSummary(any(AgentResource.LostAgentFunction.class))).thenReturn(agentSummary);
+
         final AgentSummary response = this.resource.get(agent.getId());
 
         assertNotNull(response);
-        assertEquals(agent.getId(), response.id());
-        assertEquals(agent.getNodeId(), response.nodeId());
-        assertNotNull(response.nodeDetails());
-        assertEquals(agent.getOperatingSystem(), response.nodeDetails().operatingSystem());
-        assertEquals(agent.getLastSeen(), response.lastSeen());
+        assertEquals(agentSummary, response);
     }
 
     private Agent getDummyAgent(String id, String nodeId, DateTime lastSeen, String operatingSystem) {
+        final AgentNodeDetails agentNodeDetails = mock(AgentNodeDetails.class);
+        when(agentNodeDetails.operatingSystem()).thenReturn(operatingSystem);
+
         final Agent agent = mock(Agent.class);
         when(agent.getId()).thenReturn(id);
         when(agent.getNodeId()).thenReturn(nodeId);
         when(agent.getLastSeen()).thenReturn(lastSeen);
-        when(agent.getOperatingSystem()).thenReturn(operatingSystem);
+        when(agent.getNodeDetails()).thenReturn(agentNodeDetails);
+        when(agent.getNodeDetails().operatingSystem()).thenReturn(operatingSystem);
 
         return agent;
     }
