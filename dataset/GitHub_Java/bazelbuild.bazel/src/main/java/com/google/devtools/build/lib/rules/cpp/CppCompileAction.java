b5f2a0f719e473c9d92e37d0d412664dc6a3b71b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:29:29 +0200

    Properly return separate module as an allowed derived input. Otherwise, the     action cache does not properly understand these inputs.
        RELNOTES: None.
        PiperOrigin-RevId: 359729150

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
@@ -25,7 +25,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.common.io.ByteStreams;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -102,6 +101,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -585,13 +585,6 @@ public class CppCompileAction extends AbstractAction implements IncludeScannable
     return additionalInputs;
   }
 
-  @Override
-  protected final NestedSet<Artifact> getOriginalInputs() {
-    return NestedSetBuilder.fromNestedSet(mandatoryInputs)
-        .addTransitive(inputsForInvalidation)
-        .build();
-  }
-
   private Predicate<Artifact> getValidUndeclaredHeaderPredicate(
       ActionExecutionContext actionExecutionContext) {
     if (getDotdFile() != null) {
@@ -843,7 +836,7 @@ public class CppCompileAction extends AbstractAction implements IncludeScannable
   public ImmutableMap<String, String> getIncompleteEnvironmentForTesting()
       throws ActionExecutionException {
     try {
-      return getEffectiveEnvironment(ImmutableMap.of());
+      return getEnvironment(ImmutableMap.of());
     } catch (CommandLineExpansionException e) {
       String message =
           String.format(
@@ -854,10 +847,9 @@ public class CppCompileAction extends AbstractAction implements IncludeScannable
     }
   }
 
-  @Override()
-  public ImmutableMap<String, String> getEffectiveEnvironment(Map<String, String> clientEnv)
+  public ImmutableMap<String, String> getEnvironment(Map<String, String> clientEnv)
       throws CommandLineExpansionException {
-    Map<String, String> environment = Maps.newLinkedHashMapWithExpectedSize(env.size());
+    Map<String, String> environment = new LinkedHashMap<>(env.size());
     env.resolve(environment, clientEnv);
 
     if (!getExecutionInfo().containsKey(ExecutionRequirements.REQUIRES_DARWIN)) {
@@ -925,7 +917,7 @@ public class CppCompileAction extends AbstractAction implements IncludeScannable
     }
     // TODO(ulfjack): Extra actions currently ignore the client environment.
     for (Map.Entry<String, String> envVariable :
-        getEffectiveEnvironment(/*clientEnv=*/ ImmutableMap.of()).entrySet()) {
+        getEnvironment(/* clientEnv= */ ImmutableMap.of()).entrySet()) {
       info.addVariable(
           EnvironmentVariable.newBuilder()
               .setName(envVariable.getKey())
@@ -944,7 +936,7 @@ public class CppCompileAction extends AbstractAction implements IncludeScannable
   /** Returns the compiler options. */
   @VisibleForTesting
   public List<String> getCompilerOptions() throws CommandLineExpansionException {
-    return compileCommandLine.getCompilerOptions(/*overwrittenVariables=*/ null);
+    return compileCommandLine.getCompilerOptions(/* overwrittenVariables= */ null);
   }
 
   @Override
@@ -1489,7 +1481,7 @@ public class CppCompileAction extends AbstractAction implements IncludeScannable
       return new SimpleSpawn(
           this,
           ImmutableList.copyOf(getArguments()),
-          getEffectiveEnvironment(clientEnv),
+          getEnvironment(clientEnv),
           executionInfo,
           inputs,
           getOutputs(),
