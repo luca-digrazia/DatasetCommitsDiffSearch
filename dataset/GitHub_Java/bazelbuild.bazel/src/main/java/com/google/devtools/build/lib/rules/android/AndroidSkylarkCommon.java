b735b0725c0eaf463c8bc8d600ad1d5e88590529
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:40:33 +0200

    Remove the R.jar from the JDeps provider.
        PiperOrigin-RevId: 251504176

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidSkylarkCommon.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidSkylarkCommon.java
@@ -20,6 +20,7 @@ import com.google.devtools.build.lib.rules.java.JavaInfo;
 import com.google.devtools.build.lib.rules.java.JavaStrictCompilationArgsProvider;
 import com.google.devtools.build.lib.skylarkbuildapi.android.AndroidSkylarkCommonApi;
 import com.google.devtools.build.lib.skylarkbuildapi.android.AndroidSplitTransititionApi;
+import com.google.devtools.build.lib.vfs.PathFragment;
 
 /** Common utilities for Skylark rules related to Android. */
 public class AndroidSkylarkCommon implements AndroidSkylarkCommonApi<Artifact, JavaInfo> {
@@ -30,8 +31,8 @@ public class AndroidSkylarkCommon implements AndroidSkylarkCommonApi<Artifact, J
   }
 
   @Override
-  public String getSourceDirectoryRelativePathFromResource(Artifact resource) {
-    return AndroidCommon.getSourceDirectoryRelativePathFromResource(resource).toString();
+  public PathFragment getSourceDirectoryRelativePathFromResource(Artifact resource) {
+    return AndroidCommon.getSourceDirectoryRelativePathFromResource(resource);
   }
 
   @Override
