070bf4f7645e85f9c10718e5ee31e7f29184de76
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 03:47:48 +0200

    JUnit rule now resets property overrides after test class

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/com/yammer/dropwizard/testing/junit/DropwizardServiceRule.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard-testing/src/main/java/com/yammer/dropwizard/testing/junit/DropwizardServiceRule.java
@@ -1,8 +1,6 @@
 package com.yammer.dropwizard.testing.junit;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import com.yammer.dropwizard.Bundle;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.cli.ServerCommand;
 import com.yammer.dropwizard.config.Bootstrap;
@@ -15,33 +13,62 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-public class DropwizardServiceRule<S extends Service, C extends Configuration> implements TestRule {
+import java.util.Enumeration;
+import java.util.Set;
 
-    private final Class<S> serviceClass;
-    private final Class<C> configClass;
+/**
+ * A JUnit rule for starting and stopping your service at the start and end of a test class.
+ * @param <C> the configuration type
+ */
+public class DropwizardServiceRule<C extends Configuration> implements TestRule {
+
+    private final Class<? extends Service<C>> serviceClass;
     private final String configPath;
 
+    private C configuration;
+    private Service<C> service;
+    private Environment environment;
     private Server jettyServer;
 
-    public DropwizardServiceRule(Class<S> serviceClass, Class<C> configClass, String configPath) {
+    /**
+     *
+     * @param serviceClass the class for your service
+     * @param configPath the file path to your YAML config
+     * @param configOverrides configuration value overrides (no need to prefix with dw.)
+     */
+    public DropwizardServiceRule(Class<? extends Service<C>> serviceClass,
+                                 String configPath,
+                                 ConfigOverride... configOverrides) {
         this.serviceClass = serviceClass;
-        this.configClass = configClass;
         this.configPath = configPath;
+        for (ConfigOverride configOverride: configOverrides) {
+            configOverride.addToSystemProperties();
+        }
     }
 
     @Override
-    public Statement apply(final Statement baseStatement, Description description) {
+    public Statement apply(final Statement base, Description description) {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
                 startIfRequired();
                 try {
-                    baseStatement.evaluate();
+                    base.evaluate();
                 } finally {
+                    resetConfigOverrides();
                     jettyServer.stop();
                 }
             }
         };
+    }
+
+    private void resetConfigOverrides() {
+        for (Enumeration<?> props = System.getProperties().propertyNames(); props.hasMoreElements();) {
+            String keyString = (String) props.nextElement();
+            if (keyString.startsWith("dw.")) {
+                System.clearProperty(keyString);
+            }
+        }
     }
 
     private void startIfRequired() {
@@ -50,29 +77,46 @@ public class DropwizardServiceRule<S extends Service, C extends Configuration> i
         }
 
         try {
-            Service<C> service = serviceClass.newInstance();
-            final Bootstrap<C> bootstrap = new Bootstrap<C>(service);
-            bootstrap.addBundle(new Bundle() {
-                @Override
-                public void initialize(Bootstrap<?> bootstrap) {
-                }
+            service = serviceClass.newInstance();
 
+            final Bootstrap<C> bootstrap = new Bootstrap<C>(service) {
                 @Override
-                public void run(Environment environment) {
-                    environment.addServerLifecycleListener(new ServerLifecycleListener() {
-                        @Override
-                        public void serverStarted(Server server) {
-                            jettyServer = server;
-                        }
-                    });
+                public void runWithBundles(C configuration, Environment environment) throws Exception {
+                    environment.getLifecycleEnvironment().addServerLifecycleListener(new ServerLifecycleListener() {
+                                    @Override
+                                    public void serverStarted(Server server) {
+                                        jettyServer = server;
+                                    }
+                                });
+                    DropwizardServiceRule.this.configuration = configuration;
+                    DropwizardServiceRule.this.environment = environment;
+                    super.runWithBundles(configuration, environment);
                 }
-            });
+            };
+
             service.initialize(bootstrap);
-            ServerCommand<C> command = new ServerCommand<C>(service, false);
-            Namespace namespace = new Namespace(ImmutableMap.<String, Object>of("file", configPath));
+            final ServerCommand<C> command = new ServerCommand<C>(service);
+            final Namespace namespace = new Namespace(ImmutableMap.<String, Object>of("file", configPath));
             command.run(bootstrap, namespace);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public C getConfiguration() {
+        return configuration;
+    }
+
+    public int getLocalPort() {
+        return jettyServer.getConnectors()[0].getLocalPort();
+    }
+
+    @SuppressWarnings("unchecked")
+    public <S extends Service<C>> S getService() {
+        return (S) service;
+    }
+
+    public Environment getEnvironment() {
+        return environment;
     }
 }
