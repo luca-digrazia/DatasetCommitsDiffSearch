83c1c4d5f418e5b7d41e5d73f9a93064814bcda0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:32:28 +0200

    Add missing placeholder for second argument

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/internal/generation/SourceCodeWriter.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/internal/generation/SourceCodeWriter.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2010-2016 eBusiness Information, Excilys Group
+ * Copyright (C) 2016-2018 the AndroidAnnotations project
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -17,6 +18,7 @@ package org.androidannotations.internal.generation;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.FilerException;
@@ -44,8 +46,8 @@ public class SourceCodeWriter extends AbstractCodeWriter {
 		}
 	}
 
-	public SourceCodeWriter(Filer filer, OriginatingElements originatingElements) {
-		super(null);
+	public SourceCodeWriter(Filer filer, OriginatingElements originatingElements, Charset charset) {
+		super(charset, getDefaultNewLine());
 		this.filer = filer;
 		this.originatingElements = originatingElements;
 	}
@@ -68,7 +70,7 @@ public class SourceCodeWriter extends AbstractCodeWriter {
 
 			return sourceFile.openOutputStream();
 		} catch (FilerException e) {
-			LOGGER.error("Could not generate source file for {}", qualifiedClassName, e.getMessage());
+			LOGGER.error("Could not generate source file for {} due to error: {}", qualifiedClassName, e.getMessage());
 			/*
 			 * This exception is expected, when some files are created twice. We
 			 * cannot delete existing files, unless using a dirty hack. Files a
