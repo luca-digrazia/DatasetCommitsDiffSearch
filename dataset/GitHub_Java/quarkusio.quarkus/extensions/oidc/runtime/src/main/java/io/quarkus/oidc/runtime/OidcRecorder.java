2fa347bc40ac603f56ddd08c59b02eca1ea8fd6d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 12:50:10 +0200

    Fix a connection-delay processing

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/oidc/runtime/src/main/java/io/quarkus/oidc/runtime/OidcRecorder.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/oidc/runtime/src/main/java/io/quarkus/oidc/runtime/OidcRecorder.java
@@ -2,30 +2,22 @@ package io.quarkus.oidc.runtime;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import org.jboss.logging.Logger;
 
 import io.quarkus.arc.runtime.BeanContainer;
 import io.quarkus.oidc.OIDCException;
-import io.quarkus.oidc.runtime.OidcTenantConfig.ApplicationType;
-import io.quarkus.oidc.runtime.OidcTenantConfig.Credentials;
-import io.quarkus.oidc.runtime.OidcTenantConfig.Credentials.Secret;
+import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
-import io.quarkus.runtime.configuration.ConfigurationException;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
-import io.vertx.core.net.ProxyOptions;
 import io.vertx.ext.auth.PubSecKeyOptions;
 import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2ClientOptions;
-import io.vertx.ext.auth.oauth2.impl.OAuth2AuthProviderImpl;
 import io.vertx.ext.auth.oauth2.providers.KeycloakAuth;
 import io.vertx.ext.jwt.JWTOptions;
 
@@ -34,8 +26,8 @@ public class OidcRecorder {
 
     private static final Logger LOG = Logger.getLogger(OidcRecorder.class);
 
-    public void setup(OidcConfig config, Supplier<Vertx> vertx, BeanContainer beanContainer) {
-        final Vertx vertxValue = vertx.get();
+    public void setup(OidcConfig config, RuntimeValue<Vertx> vertx, BeanContainer beanContainer) {
+        final Vertx vertxValue = vertx.getValue();
         Map<String, TenantConfigContext> tenantsConfig = new HashMap<>();
 
         for (Map.Entry<String, OidcTenantConfig> tenant : config.namedTenants.entrySet()) {
@@ -47,62 +39,26 @@ public class OidcRecorder {
                 throw new OIDCException("Configuration has 2 different tenant-id values: '"
                         + tenant.getKey() + "' and '" + tenant.getValue().getTenantId().get() + "'");
             }
-            tenantsConfig.put(tenant.getKey(), createTenantContext(vertxValue, tenant.getValue(), tenant.getKey()));
+            tenantsConfig.put(tenant.getKey(), createTenantContext(vertxValue, tenant.getValue()));
         }
 
         DefaultTenantConfigResolver resolver = beanContainer.instance(DefaultTenantConfigResolver.class);
 
-        resolver.setDefaultTenant(createTenantContext(vertxValue, config.defaultTenant, "Default"));
+        resolver.setDefaultTenant(createTenantContext(vertxValue, config.defaultTenant));
         resolver.setTenantsConfig(tenantsConfig);
         resolver.setTenantConfigContextFactory(new Function<OidcTenantConfig, TenantConfigContext>() {
             @Override
             public TenantConfigContext apply(OidcTenantConfig config) {
-                // OidcTenantConfig resolved by TenantConfigResolver must have its optional tenantId
-                // initialized which is also enforced by DefaultTenantConfigResolver
-                return createTenantContext(vertxValue, config, config.getTenantId().get());
+                return createTenantContext(vertxValue, config);
             }
         });
     }
 
-    private TenantConfigContext createTenantContext(Vertx vertx, OidcTenantConfig oidcConfig, String tenantId) {
-        if (!oidcConfig.tenantEnabled) {
-            LOG.debugf("%s tenant configuration is disabled", tenantId);
-            return null;
-        }
-
+    private TenantConfigContext createTenantContext(Vertx vertx, OidcTenantConfig oidcConfig) {
         OAuth2ClientOptions options = new OAuth2ClientOptions();
 
-        if (oidcConfig.getClientId().isPresent()) {
-            options.setClientID(oidcConfig.getClientId().get());
-        }
-
-        if (oidcConfig.getToken().issuer.isPresent()) {
-            options.setValidateIssuer(false);
-        }
-
-        if (oidcConfig.getToken().getExpirationGrace().isPresent()) {
-            JWTOptions jwtOptions = new JWTOptions();
-            jwtOptions.setLeeway(oidcConfig.getToken().getExpirationGrace().get());
-            options.setJWTOptions(jwtOptions);
-        }
-
-        if (oidcConfig.getPublicKey().isPresent()) {
-            if (oidcConfig.applicationType == ApplicationType.WEB_APP) {
-                throw new ConfigurationException("'public-key' property can only be used with the 'service' applications");
-            }
-            LOG.info("'public-key' property for the local token verification is set,"
-                    + " no connection to the OIDC server will be created");
-            options.addPubSecKey(new PubSecKeyOptions()
-                    .setAlgorithm("RS256")
-                    .setPublicKey(oidcConfig.getPublicKey().get()));
-
-            return new TenantConfigContext(new OAuth2AuthProviderImpl(vertx, options), oidcConfig);
-        }
-
-        if (!oidcConfig.getAuthServerUrl().isPresent() || !oidcConfig.getClientId().isPresent()) {
-            throw new ConfigurationException(
-                    "Both 'auth-server-url' and 'client-id' or alterntively 'public-key' must be configured"
-                            + " when the quarkus-oidc extension is enabled");
+        if (!oidcConfig.getAuthServerUrl().isPresent()) {
+            return null;
         }
 
         // Base IDP server URL
@@ -117,27 +73,26 @@ public class OidcRecorder {
             options.setJwkPath(oidcConfig.getJwksPath().get());
         }
 
-        Credentials creds = oidcConfig.getCredentials();
-        if (creds.secret.isPresent() && (creds.clientSecret.value.isPresent() || creds.clientSecret.method.isPresent())) {
-            throw new ConfigurationException(
-                    "'credentials.secret' and 'credentials.client-secret' properties are mutually exclusive");
-        }
-        // TODO: The workaround to support client_secret_post is added below and have to be removed once
-        // it is supported again in VertX OAuth2.
-        if (creds.secret.isPresent()
-                || creds.clientSecret.value.isPresent()
-                        && creds.clientSecret.method.orElseGet(() -> Secret.Method.BASIC) == Secret.Method.BASIC) {
-            // If it is set for client_secret_post as well then VertX OAuth2 will only use client_secret_basic
-            options.setClientSecret(creds.secret.orElseGet(() -> creds.clientSecret.value.get()));
-        } else {
-            // Avoid the client_secret set in CodeAuthenticationMechanism when client_secret_post is enabled
-            // from being reset to null in VertX OAuth2
-            options.setClientSecretParameterName(null);
+        if (oidcConfig.getClientId().isPresent()) {
+            options.setClientID(oidcConfig.getClientId().get());
         }
 
-        Optional<ProxyOptions> proxyOpt = toProxyOptions(oidcConfig.getProxy());
-        if (proxyOpt.isPresent()) {
-            options.setProxyOptions(proxyOpt.get());
+        if (oidcConfig.getCredentials().secret.isPresent()) {
+            options.setClientSecret(oidcConfig.getCredentials().secret.get());
+        }
+        if (oidcConfig.getPublicKey().isPresent()) {
+            options.addPubSecKey(new PubSecKeyOptions()
+                    .setAlgorithm("RS256")
+                    .setPublicKey(oidcConfig.getPublicKey().get()));
+        }
+        if (oidcConfig.getToken().issuer.isPresent()) {
+            options.setValidateIssuer(false);
+        }
+
+        if (oidcConfig.getToken().getExpirationGrace().isPresent()) {
+            JWTOptions jwtOptions = new JWTOptions();
+            jwtOptions.setLeeway(oidcConfig.getToken().getExpirationGrace().get());
+            options.setJWTOptions(jwtOptions);
         }
 
         final long connectionDelayInSecs = oidcConfig.getConnectionDelay().isPresent()
@@ -193,23 +148,6 @@ public class OidcRecorder {
                 + "Please make sure it is correct. Note it has to end with a realm value if you work with Keycloak, for example:"
                 + " 'https://localhost:8180/auth/realms/quarkus'";
         return new OIDCException(message, cause);
-    }
-
-    protected static Optional<ProxyOptions> toProxyOptions(OidcTenantConfig.Proxy proxyConfig) {
-        // Proxy is enabled if (at least) "host" is configured.
-        if (!proxyConfig.host.isPresent()) {
-            return Optional.empty();
-        }
-        JsonObject jsonOptions = new JsonObject();
-        jsonOptions.put("host", proxyConfig.host.get());
-        jsonOptions.put("port", proxyConfig.port);
-        if (proxyConfig.username.isPresent()) {
-            jsonOptions.put("username", proxyConfig.username.get());
-        }
-        if (proxyConfig.password.isPresent()) {
-            jsonOptions.put("password", proxyConfig.password.get());
-        }
-        return Optional.of(new ProxyOptions(jsonOptions));
     }
 
 }
