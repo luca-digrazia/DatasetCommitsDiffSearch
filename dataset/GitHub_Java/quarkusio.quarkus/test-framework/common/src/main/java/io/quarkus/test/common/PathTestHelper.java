a856c36e7a7ac92dbd430df9859e789c26969d30
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 13:40:48 +0200

    Support running tests from third party test-jars transformed using     rpkgtests-maven-plugin

--- dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/PathTestHelper.java
+++ dataset/GitHub_Java/quarkusio.quarkus/test-framework/common/src/main/java/io/quarkus/test/common/PathTestHelper.java
@@ -3,14 +3,12 @@ package io.quarkus.test.common;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-
-import io.quarkus.runtime.util.ClassPathUtils;
 
 /**
  * Maps between builder test and application class directories.
@@ -24,20 +22,11 @@ public final class PathTestHelper {
                 "bin" + File.separator + "main");
         // gradle
         TEST_TO_MAIN_DIR_FRAGMENTS.put(
-                "classes" + File.separator + "java" + File.separator + "native-test",
-                "classes" + File.separator + "java" + File.separator + "main");
-        TEST_TO_MAIN_DIR_FRAGMENTS.put(
                 "classes" + File.separator + "java" + File.separator + "test",
                 "classes" + File.separator + "java" + File.separator + "main");
         TEST_TO_MAIN_DIR_FRAGMENTS.put(
-                "classes" + File.separator + "kotlin" + File.separator + "native-test",
-                "classes" + File.separator + "kotlin" + File.separator + "main");
-        TEST_TO_MAIN_DIR_FRAGMENTS.put(
                 "classes" + File.separator + "kotlin" + File.separator + "test",
                 "classes" + File.separator + "kotlin" + File.separator + "main");
-        TEST_TO_MAIN_DIR_FRAGMENTS.put(
-                "classes" + File.separator + "scala" + File.separator + "native-test",
-                "classes" + File.separator + "scala" + File.separator + "main");
         TEST_TO_MAIN_DIR_FRAGMENTS.put(
                 "classes" + File.separator + "scala" + File.separator + "test",
                 "classes" + File.separator + "scala" + File.separator + "main");
@@ -87,54 +76,32 @@ public final class PathTestHelper {
      * @return directory or JAR containing the application being tested by the test class
      */
     public static Path getAppClassLocation(Class<?> testClass) {
-        return getAppClassLocationForTestLocation(getTestClassesLocation(testClass).toString());
-    }
-
-    /**
-     * Resolves the directory or the JAR file containing the application being tested by a test from the given location.
-     *
-     * @param testClassLocation the test class location
-     * @return directory or JAR containing the application being tested by a test from the given location
-     */
-    public static Path getAppClassLocationForTestLocation(String testClassLocation) {
-        if (testClassLocation.endsWith(".jar")) {
-            if (testClassLocation.endsWith("-tests.jar")) {
+        final String testClassPath = getTestClassesLocation(testClass).toString();
+        if (testClassPath.endsWith(".jar")) {
+            if (testClassPath.endsWith("-tests.jar")) {
                 return Paths.get(new StringBuilder()
-                        .append(testClassLocation, 0, testClassLocation.length() - "-tests.jar".length())
+                        .append(testClassPath, 0, testClassPath.length() - "-tests.jar".length())
                         .append(".jar")
                         .toString());
-            } else if (testClassLocation.contains("-rpkgtests")) {
+            } else if (testClassPath.contains("-rpkgtests")) {
                 // This is a third party test-jar transformed using rpkgtests-maven-plugin
-                return Paths.get(testClassLocation.replace("-rpkgtests", ""));
+                return Paths.get(testClassPath.replace("-rpkgtests", ""));
             }
-            return Paths.get(testClassLocation);
+            return Paths.get(testClassPath);
         }
         return TEST_TO_MAIN_DIR_FRAGMENTS.entrySet().stream()
-                .filter(e -> testClassLocation.contains(e.getKey()))
-                .map(e -> Paths.get(testClassLocation.replace(e.getKey(), e.getValue())))
+                .filter(e -> testClassPath.contains(e.getKey()))
+                .map(e -> Paths.get(testClassPath.replace(e.getKey(), e.getValue())))
                 .findFirst()
-                .orElseThrow(() -> new IllegalStateException("Unable to translate path for " + testClassLocation));
+                .orElseThrow(() -> new IllegalStateException("Unable to translate path for " + testClass.getName()));
     }
 
-    public static boolean isTestClass(String className, ClassLoader classLoader, Path testLocation) {
+    public static boolean isTestClass(String className, ClassLoader classLoader) {
         String classFileName = className.replace('.', File.separatorChar) + ".class";
         URL resource = classLoader.getResource(classFileName);
-        if (resource == null) {
-            return false;
-        }
-        if (Files.isDirectory(testLocation)) {
-            return resource.getProtocol().startsWith("file") && isInTestDir(resource);
-        }
-        if (!resource.getProtocol().equals("jar")) {
-            return false;
-        }
-        String path = resource.getPath();
-        if (!path.startsWith("file:")) {
-            return false;
-        }
-        path = path.substring(5, path.lastIndexOf('!'));
-
-        return testLocation.equals(Paths.get(path));
+        return resource != null
+                && resource.getProtocol().startsWith("file")
+                && isInTestDir(resource);
     }
 
     private static boolean isInTestDir(URL resource) {
@@ -144,6 +111,10 @@ public final class PathTestHelper {
     }
 
     private static Path toPath(URL resource) {
-        return ClassPathUtils.toLocalPath(resource);
+        try {
+            return Paths.get(resource.toURI());
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Failed to convert URL " + resource, e);
+        }
     }
 }
