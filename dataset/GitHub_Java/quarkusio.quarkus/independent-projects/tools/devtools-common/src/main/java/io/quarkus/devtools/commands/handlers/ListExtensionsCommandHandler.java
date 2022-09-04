b43a6a05769f5f50250fb5dc053190f1430db0d8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 11:46:47 +0200

    Removed extra arguments to String.format

--- dataset/GitHub_Java/quarkusio.quarkus/independent-projects/tools/devtools-common/src/main/java/io/quarkus/devtools/commands/handlers/ListExtensionsCommandHandler.java
+++ dataset/GitHub_Java/quarkusio.quarkus/independent-projects/tools/devtools-common/src/main/java/io/quarkus/devtools/commands/handlers/ListExtensionsCommandHandler.java
@@ -1,25 +1,24 @@
 package io.quarkus.devtools.commands.handlers;
 
 import static io.quarkus.devtools.project.extensions.Extensions.toKey;
-import static io.quarkus.platform.catalog.processor.ExtensionProcessor.getGuide;
 import static java.util.stream.Collectors.toMap;
 
+import io.quarkus.bootstrap.model.AppArtifactCoords;
+import io.quarkus.bootstrap.model.AppArtifactKey;
+import io.quarkus.dependencies.Extension;
 import io.quarkus.devtools.commands.ListExtensions;
 import io.quarkus.devtools.commands.data.QuarkusCommandException;
 import io.quarkus.devtools.commands.data.QuarkusCommandInvocation;
 import io.quarkus.devtools.commands.data.QuarkusCommandOutcome;
-import io.quarkus.devtools.messagewriter.MessageWriter;
 import io.quarkus.devtools.project.BuildTool;
 import io.quarkus.devtools.project.extensions.ExtensionManager;
-import io.quarkus.maven.ArtifactCoords;
-import io.quarkus.maven.ArtifactKey;
-import io.quarkus.platform.catalog.processor.ExtensionProcessor;
-import io.quarkus.registry.catalog.Extension;
-import io.quarkus.registry.catalog.ExtensionOrigin;
+import io.quarkus.platform.tools.MessageWriter;
+import io.quarkus.registry.DefaultExtensionRegistry;
+import io.quarkus.registry.ExtensionRegistry;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
@@ -29,70 +28,58 @@ import java.util.function.Function;
  */
 public class ListExtensionsCommandHandler implements QuarkusCommandHandler {
 
-    private static final String FULL_FORMAT = "%-8s %-50s %-50s %-25s%s";
+    private static final String FULL_FORMAT = "%-8s %-50s %-50s %-25s%n%s";
     private static final String CONCISE_FORMAT = "%-50s %-50s";
     private static final String NAME_FORMAT = "%-50s";
-    private static final String ORIGINS_FORMAT = "%-50s %-60s %s";
 
     @Override
     public QuarkusCommandOutcome execute(QuarkusCommandInvocation invocation) throws QuarkusCommandException {
 
         final MessageWriter log = invocation.log();
         final boolean all = invocation.getValue(ListExtensions.ALL, true);
-        final boolean installedOnly = invocation.getValue(ListExtensions.INSTALLED, false);
-        final boolean cli = invocation.getValue(ListExtensions.FROM_CLI, false);
         final String format = invocation.getValue(ListExtensions.FORMAT, "concise");
         final String search = invocation.getValue(ListExtensions.SEARCH, "*");
         final ExtensionManager extensionManager = invocation.getValue(ListExtensions.EXTENSION_MANAGER,
                 invocation.getQuarkusProject().getExtensionManager());
-
-        final Collection<Extension> extensions = search == null ? invocation.getExtensionsCatalog().getExtensions()
-                : QuarkusCommandHandlers.select(search, invocation.getExtensionsCatalog().getExtensions(), true)
-                        .getExtensions();
-
-        if (extensions.isEmpty()) {
-            log.info("No extension found with pattern '%s'", search);
-            return QuarkusCommandOutcome.success();
+        ExtensionRegistry extensionRegistry = invocation.getValue(ListExtensions.EXTENSION_REGISTRY);
+        if (extensionRegistry == null) {
+            extensionRegistry = DefaultExtensionRegistry.fromPlatform(invocation.getPlatformDescriptor());
         }
-
-        if (!cli) {
-            String extensionStatus = all ? "available" : "installable";
-            if (installedOnly)
-                extensionStatus = "installed";
-            log.info("%nCurrent Quarkus extensions %s: ", extensionStatus);
-        }
-
-        BiConsumer<MessageWriter, Object[]> currentFormatter;
-        switch (format.toLowerCase()) {
-            case "name":
-                currentFormatter = this::nameFormatter;
-                break;
-            case "full":
-                currentFormatter = this::fullFormatter;
-                log.info(String.format(FULL_FORMAT, "Status", "Extension", "ArtifactId", "Updated Version", "Guide"));
-                break;
-            case "origins":
-                currentFormatter = this::originsFormatter;
-                break;
-            case "concise":
-            default:
-                currentFormatter = this::conciseFormatter;
-        }
-
-        Map<ArtifactKey, ArtifactCoords> installedByKey;
+        Map<AppArtifactKey, AppArtifactCoords> installedByKey;
         try {
             installedByKey = extensionManager.getInstalled().stream()
-                    .collect(toMap(ArtifactCoords::getKey, Function.identity()));
+                    .collect(toMap(AppArtifactCoords::getKey, Function.identity()));
         } catch (IOException e) {
             throw new QuarkusCommandException("Failed to determine the list of installed extensions", e);
         }
-        extensions.stream()
-                .filter(e -> !ExtensionProcessor.of(e).isUnlisted())
-                .forEach(e -> display(log, e, installedByKey.get(toKey(e)), all, installedOnly, currentFormatter));
-        final BuildTool buildTool = invocation.getQuarkusProject().getBuildTool();
-        boolean isGradle = BuildTool.GRADLE.equals(buildTool) || BuildTool.GRADLE_KOTLIN_DSL.equals(buildTool);
+        String quarkusVersion = invocation.getPlatformDescriptor().getQuarkusVersion();
+        Collection<Extension> platformExtensions = extensionRegistry.list(quarkusVersion, search);
+        if (platformExtensions.isEmpty()) {
+            log.info("No extension found with pattern '%s'", search);
+        } else {
+            String extensionStatus = all ? "available" : "installable";
+            log.info("%nCurrent Quarkus extensions %s: ", extensionStatus);
 
-        if (!cli) {
+            BiConsumer<MessageWriter, String[]> currentFormatter;
+            switch (format.toLowerCase()) {
+                case "name":
+                    currentFormatter = this::nameFormatter;
+                    break;
+                case "full":
+                    currentFormatter = this::fullFormatter;
+                    currentFormatter.accept(log,
+                            new String[] { "Status", "Extension", "ArtifactId", "Updated Version", "Guide" });
+                    break;
+                case "concise":
+                default:
+                    currentFormatter = this::conciseFormatter;
+            }
+
+            platformExtensions.forEach(platformExtension -> display(log, platformExtension,
+                    installedByKey.get(toKey(platformExtension)), all, currentFormatter));
+            final BuildTool buildTool = invocation.getQuarkusProject().getBuildTool();
+            boolean isGradle = BuildTool.GRADLE.equals(buildTool) || BuildTool.GRADLE_KOTLIN_DSL.equals(buildTool);
+
             if ("concise".equalsIgnoreCase(format)) {
                 if (isGradle) {
                     log.info("\nTo get more information, append --format=full to your command line.");
@@ -109,56 +96,28 @@ public class ListExtensionsCommandHandler implements QuarkusCommandHandler {
                 log.info("\nAdd an extension to your project by adding the dependency to your " +
                         "pom.xml or use `./mvnw quarkus:add-extension -Dextensions=\"artifactId\"`");
             }
+
         }
 
         return QuarkusCommandOutcome.success();
     }
 
-    private void conciseFormatter(MessageWriter writer, Object[] cols) {
-        Extension e = (Extension) cols[1];
-        writer.info(String.format(CONCISE_FORMAT, e.getName(), e.getArtifact().getArtifactId()));
+    private void conciseFormatter(MessageWriter writer, String[] cols) {
+        writer.info(String.format(CONCISE_FORMAT, cols[1], cols[2]));
     }
 
-    private void fullFormatter(MessageWriter writer, Object[] cols) {
-        Extension e = (Extension) cols[1];
-        final String guide = getGuide(e);
-        writer.info(String.format(FULL_FORMAT, cols[0], e.getName(), e.getArtifact().getArtifactId(), cols[2],
-                guide == null ? "" : guide));
+    private void fullFormatter(MessageWriter writer, String[] cols) {
+        writer.info(String.format(FULL_FORMAT, cols[0], cols[1], cols[2], cols[3], cols[4]));
     }
 
-    private void nameFormatter(MessageWriter writer, Object[] cols) {
-        Extension e = (Extension) cols[1];
-        writer.info(String.format(NAME_FORMAT, e.getArtifact().getArtifactId()));
+    private void nameFormatter(MessageWriter writer, String[] cols) {
+        writer.info(String.format(NAME_FORMAT, cols[2]));
     }
 
-    private void originsFormatter(MessageWriter writer, Object[] cols) {
-        Extension e = (Extension) cols[1];
-        String origin = null;
-        int i = 0;
-        final List<ExtensionOrigin> origins = e.getOrigins();
-        while (i < origins.size() && origin == null) {
-            final ExtensionOrigin o = origins.get(i++);
-            if (o.isPlatform()) {
-                origin = o.getBom().toString();
-            }
-        }
-        writer.info(String.format(ORIGINS_FORMAT, e.getName(), e.getArtifact().getVersion(), origin == null ? "" : origin));
-        while (i < origins.size()) {
-            final ExtensionOrigin o = origins.get(i++);
-            if (o.isPlatform()) {
-                writer.info(String.format(ORIGINS_FORMAT, "", "", o.getBom().toString()));
-            }
-        }
-    }
-
-    private void display(MessageWriter messageWriter, final Extension e, final ArtifactCoords installed,
+    private void display(MessageWriter messageWriter, final Extension platformExtension, final AppArtifactCoords installed,
             boolean all,
-            boolean installedOnly,
-            BiConsumer<MessageWriter, Object[]> formatter) {
-        if (installedOnly && installed == null) {
-            return;
-        }
-        if (!installedOnly && !all && installed != null) {
+            BiConsumer<MessageWriter, String[]> formatter) {
+        if (!all && installed != null) {
             return;
         }
 
@@ -169,17 +128,24 @@ public class ListExtensionsCommandHandler implements QuarkusCommandHandler {
             final String installedVersion = installed.getVersion();
             if (installedVersion == null) {
                 label = "default";
-                version = e.getArtifact().getVersion();
-            } else if (installedVersion.equalsIgnoreCase(e.getArtifact().getVersion())) {
+                version = platformExtension.getVersion();
+            } else if (installedVersion.equalsIgnoreCase(platformExtension.getVersion())) {
                 label = "custom";
                 version = installedVersion;
             } else {
                 label = "custom*";
-                version = String.format("%s* <> %s", installedVersion, e.getArtifact().getVersion());
+                version = String.format("%s* <> %s", installedVersion, platformExtension.getVersion());
             }
         }
 
-        formatter.accept(messageWriter, new Object[] { label, e, version });
+        String[] result = new String[] { label, platformExtension.getName(), platformExtension.getArtifactId(), version,
+                platformExtension.getGuide() };
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Objects.toString(result[i], "");
+        }
+
+        formatter.accept(messageWriter, result);
     }
 
 }
