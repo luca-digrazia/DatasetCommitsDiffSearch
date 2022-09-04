3dfa5b93f2c50bd9c67613aaa9021ed3a5888912
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:54:58 +0200

    Add private constructor to LoggerContext
        The class should be only instantiated with the getInstance() method.

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/logger/LoggerContext.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/logger/LoggerContext.java
@@ -1,35 +1,62 @@
+/**
+ * Copyright (C) 2010-2014 eBusiness Information, Excilys Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.androidannotations.logger;
 
+import org.androidannotations.helper.OptionsHelper;
+import org.androidannotations.logger.appender.Appender;
+import org.androidannotations.logger.appender.ConsoleAppender;
+import org.androidannotations.logger.appender.FileAppender;
+import org.androidannotations.logger.appender.MessagerAppender;
+import org.androidannotations.logger.formatter.Formatter;
+
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoggerContext {
 
-	public static final String LOG_FILE_OPTION = "logFile";
-	public static final String LOG_LEVEL_OPTION = "logLevel";
-	private static LoggerContext INSTANCE = null;
+	private static LoggerContext instance = null;
 	private static final Level DEFAULT_LEVEL = Level.DEBUG;
 
 	private Level currentLevel = DEFAULT_LEVEL;
-	private Appender appender = new Appender();
-	private Formatter formatter = new Formatter();
+	private List<Appender> appenders = new ArrayList<Appender>();
 
+	private LoggerContext() {
+		
+	}
+	
 	public static LoggerContext getInstance() {
-		if (INSTANCE == null) {
+		if (instance == null) {
 			synchronized (LoggerContext.class) {
-				if (INSTANCE == null) {
-					INSTANCE = new LoggerContext();
+				if (instance == null) {
+					instance = new LoggerContext();
 				}
 			}
 		}
-		return INSTANCE;
+		return instance;
 	}
 
-	LoggerContext() {
-	}
-
-	public void writeLog(Level level, String loggerName, String message, Throwable thr, Object... args) {
-		String log = formatter.buildLog(level, loggerName, message, thr, args);
-		appender.append(log);
+	public void writeLog(Level level, String loggerName, String message, Element element, AnnotationMirror annotationMirror, Throwable thr, Object... args) {
+		for (Appender appender : appenders) {
+			Formatter formatter = appender.getFormatter();
+			String log = formatter.buildLog(level, loggerName, message, thr, args);
+			appender.append(level, element, annotationMirror, log);
+		}
 	}
 
 	public Level getCurrentLevel() {
@@ -41,20 +68,38 @@ public class LoggerContext {
 	}
 
 	public void setProcessingEnv(ProcessingEnvironment processingEnv) {
-		appender.setProcessingEnv(processingEnv);
-		appender.resolveLogFile();
+		appenders.clear();
+		OptionsHelper optionsHelper = new OptionsHelper(processingEnv);
+		resolveLogLevel(optionsHelper);
+		addConsoleAppender(optionsHelper);
+		addFileAppender(optionsHelper);
+		appenders.add(new MessagerAppender());
 
-		resolveLogLevel(processingEnv);
+		for (Appender appender : appenders) {
+			appender.setProcessingEnv(processingEnv);
+			appender.open();
+		}
 	}
 
 	public void close() {
-		appender.closeFile();
+		for (Appender appender : appenders) {
+			appender.close();
+		}
 	}
 
-	private void resolveLogLevel(ProcessingEnvironment processingEnv) {
-		if (processingEnv.getOptions().containsKey(LoggerContext.LOG_LEVEL_OPTION)) {
-			Level logLevel = Level.parse(processingEnv.getOptions().get(LoggerContext.LOG_LEVEL_OPTION));
-			setCurrentLevel(logLevel);
+	private void resolveLogLevel(OptionsHelper optionsHelper) {
+		setCurrentLevel(optionsHelper.getLogLevel());
+	}
+
+	private void addConsoleAppender(OptionsHelper optionsHelper) {
+		if (optionsHelper.shouldUseConsoleAppender()) {
+			appenders.add(new ConsoleAppender());
+		}
+	}
+
+	private void addFileAppender(OptionsHelper optionsHelper) {
+		if (optionsHelper.shouldUseFileAppender()) {
+			appenders.add(new FileAppender());
 		}
 	}
 
