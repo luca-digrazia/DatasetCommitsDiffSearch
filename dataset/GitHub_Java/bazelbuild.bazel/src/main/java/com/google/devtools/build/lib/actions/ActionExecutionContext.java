c3685e803d26f82e353611eabdaa9d0840ea5bd8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 22:14:37 +0200

    Automated rollback of commit 4c72a82ada742bd369185cd07c57f96c497ce440.
        *** Reason for rollback ***
    
        Breaks, at least, //ads/aswan/tools:format_mr_results.
    
        *** Original change description ***
    
        Remove python3/ prefix to generated .pyc files.
    
        That makes:
        (a) merging PY2 and PY3 .runfiles impossible (which is incorrect anyway) and
        (b) generated .py source files incompatible with 2to3 (src_version=PY2) - that's OK as we deprecate 2to3.
    
        RELNOTES: n/a
        PiperOrigin-RevId: 199661399

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/ActionExecutionContext.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/ActionExecutionContext.java
@@ -59,8 +59,6 @@ public class ActionExecutionContext implements Closeable {
 
   @Nullable private ImmutableList<FilesetOutputSymlink> outputSymlinks;
 
-  private final ArtifactPathResolver pathResolver;
-
   private ActionExecutionContext(
       Executor executor,
       MetadataProvider actionInputFileCache,
@@ -84,9 +82,6 @@ public class ActionExecutionContext implements Closeable {
     this.artifactExpander = artifactExpander;
     this.env = env;
     this.actionFileSystem = actionFileSystem;
-    this.pathResolver = createPathResolver(actionFileSystem,
-        // executor is only ever null in testing.
-        executor == null ? null : executor.getExecRoot());
   }
 
   public ActionExecutionContext(
@@ -173,25 +168,22 @@ public class ActionExecutionContext implements Closeable {
    * {@link Artifact.getRoot}.
    */
   public Path getInputPath(ActionInput input) {
-    return pathResolver.toPath(input);
+    if (input instanceof Artifact) {
+      Artifact artifact = (Artifact) input;
+      if (actionFileSystem != null) {
+        return actionFileSystem.getPath(artifact.getPath().getPathString());
+      }
+      return artifact.getPath();
+    }
+    return getExecRoot().getRelative(input.getExecPath());
   }
 
   public Root getRoot(Artifact artifact) {
-    return pathResolver.transformRoot(artifact.getRoot().getRoot());
-  }
-
-  private static ArtifactPathResolver createPathResolver(FileSystem actionFileSystem,
-      Path execRoot) {
-    if (actionFileSystem == null) {
-      return ArtifactPathResolver.forExecRoot(execRoot);
-    } else {
-      return ArtifactPathResolver.withTransformedFileSystem(
-          actionFileSystem.getPath(execRoot.asFragment()));
+    if (actionFileSystem != null) {
+      return Root.fromPath(
+          actionFileSystem.getPath(artifact.getRoot().getRoot().asPath().getPathString()));
     }
-  }
-
-  public ArtifactPathResolver getPathResolver() {
-    return pathResolver;
+    return artifact.getRoot().getRoot();
   }
 
   /**
