f05f091fbf32bab996708425204e9c68f7b7b747
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 23:12:45 +0200

    Remove top level resources and assets from android_local_test.
        android_local_test should not allow specifying resources and assets on the rule itself. If a user wants to add test specific resources/assets then they should wrap them in an android_library and add it to the deps of the android_local_test.
    
        RELNOTES: None
        PiperOrigin-RevId: 186724709

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidCommon.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidCommon.java
@@ -45,6 +45,7 @@ import com.google.devtools.build.lib.packages.Rule;
 import com.google.devtools.build.lib.packages.RuleClass.ConfiguredTargetFactory.RuleErrorException;
 import com.google.devtools.build.lib.packages.TriState;
 import com.google.devtools.build.lib.rules.android.AndroidConfiguration.AndroidAaptVersion;
+import com.google.devtools.build.lib.rules.android.ResourceContainer.ResourceType;
 import com.google.devtools.build.lib.rules.cpp.CcLinkParams;
 import com.google.devtools.build.lib.rules.cpp.CcLinkParamsInfo;
 import com.google.devtools.build.lib.rules.cpp.CcLinkParamsStore;
@@ -788,6 +789,14 @@ public class AndroidCommon {
         javaCommon.getJavaSemantics(),
         javaCommon.getJavaCompilationArtifacts(),
         asNeverLink);
+  }
+
+  public static PathFragment getAssetDir(RuleContext ruleContext) {
+    if (ruleContext.attributes().has(ResourceType.ASSETS.getAttribute() + "_dir")) {
+      return PathFragment.create(
+          ruleContext.attributes().get(ResourceType.ASSETS.getAttribute() + "_dir", Type.STRING));
+    }
+    return PathFragment.EMPTY_FRAGMENT;
   }
 
   /**
