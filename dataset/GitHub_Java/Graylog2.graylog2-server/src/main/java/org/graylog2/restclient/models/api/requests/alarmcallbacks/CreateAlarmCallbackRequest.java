a8ed14985ff3b59091cfe7f6a68269c12709c04a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:01:43 +0200

    Correcting type information for create alarm callback requests.

--- dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/restclient/models/api/requests/alarmcallbacks/CreateAlarmCallbackRequest.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/src/main/java/org/graylog2/restclient/models/api/requests/alarmcallbacks/CreateAlarmCallbackRequest.java
@@ -13,7 +13,31 @@ public class CreateAlarmCallbackRequest extends ApiRequest {
     @Constraints.Required
     public String type;
     @Constraints.Required
-    public Map<String, String> configuration;
+    public Map<String, Object> configuration;
     @SerializedName("creator_user_id")
     public String creatorUserId;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Map<String, Object> getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(Map<String, Object> configuration) {
+        this.configuration = configuration;
+    }
+
+    public String getCreatorUserId() {
+        return creatorUserId;
+    }
+
+    public void setCreatorUserId(String creatorUserId) {
+        this.creatorUserId = creatorUserId;
+    }
 }
