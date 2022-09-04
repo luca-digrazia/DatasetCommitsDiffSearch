8a36172011f721ccddd2c918cb522c03a45335d7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 08:57:56 +0200

    Fix javadoc compilation errors on jdk8

--- dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/IgnoredWhenDetached.java
+++ dataset/GitHub_Java/androidannotations.androidannotations/AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/IgnoredWhenDetached.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-2013 eBusiness Information, Excilys Group
+ * Copyright (C) 2010-2014 eBusiness Information, Excilys Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -21,16 +21,46 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * When used standalone in an {@link EFragment} or in conjunction with the {@link UiThread}
- * or {@link Background} annotations, the annotated method will be wrapped in an 'if attached'
- * block such that no code will be executed if the {@link EFragment} is no longer bound to
- * its parent activity.
- * <p/>
+ * <p>
+ * When used standalone in an {@link EFragment} or in conjunction with the
+ * {@link UiThread} or {@link Background} annotations, the annotated method will
+ * be wrapped in an 'if attached' block such that no code will be executed if
+ * the {@link EFragment} is no longer bound to its parent activity.
+ * </p>
+ * <p>
  * Should be used on method that must meet the following criteria
- * <p/>
- * 1) Can only be used in conjunction with classes annotated with {@link EFragment}
- * <p/>
+ * </p>
+ * <p>
+ * 1) Can only be used in conjunction with classes annotated with
+ * {@link EFragment}
+ * </p>
+ * <p>
  * 2) The annotated method MUST return void and MAY contain parameters.
+ * </p>
+ *
+ * <blockquote> <b>Example</b> :
+ *
+ * <pre>
+ * &#064;EFragment
+ * public class LoaderFragment extends Fragment {
+ * 
+ * ...
+ * 
+ * 	&#064;UiThread
+ * 	&#064;IgnoredWhenDetached
+ * 	void killActivity() {
+ * 		getActivity().finish();
+ * 	}
+ * 
+ * 
+ * 	&#064;IgnoredWhenDetached
+ * 	void updateTitle(String title) {
+ * 		getActivity().setTitle(title);
+ * 	}
+ * }
+ * </pre>
+ *
+ * </blockquote>
  *
  * @see org.androidannotations.annotations.EFragment
  * @see org.androidannotations.annotations.UiThread
