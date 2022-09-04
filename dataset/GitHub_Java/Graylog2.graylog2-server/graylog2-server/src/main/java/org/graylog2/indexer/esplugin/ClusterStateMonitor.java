74c9aa63eae0782b2e7a188c1bf9f7935bff704c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:08:23 +0200

    Fix check for applied ES cluster states in ClusterStateMonitor
        Elasticsearch 1.3.7 (which was previously used) seems to behave differently from
        Elasticsearch 1.4.4 (which is being used now) in that regard. Checking the current
        cluster state status being APPLIED or BEING_APPLIED seems to do the trick.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/esplugin/ClusterStateMonitor.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/indexer/esplugin/ClusterStateMonitor.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of Graylog2.
+ * This file is part of Graylog.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
+ * Graylog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog2 is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.indexer.esplugin;
 
@@ -20,6 +20,7 @@ import com.google.common.collect.Maps;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.cluster.ClusterChangedEvent;
 import org.elasticsearch.cluster.ClusterService;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.ClusterStateListener;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.common.hppc.cursors.ObjectObjectCursor;
@@ -28,13 +29,17 @@ import org.graylog2.indexer.cluster.Cluster;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.EnumSet;
 import java.util.HashMap;
 
 public class ClusterStateMonitor extends org.elasticsearch.common.component.AbstractLifecycleComponent<ClusterStateMonitor> implements ClusterStateListener {
     private static final Logger log = LoggerFactory.getLogger(ClusterStateMonitor.class);
+    private static final EnumSet<ClusterState.ClusterStateStatus> VALID_CLUSTER_STATES =
+            EnumSet.of(ClusterState.ClusterStateStatus.BEING_APPLIED, ClusterState.ClusterStateStatus.APPLIED);
+
     private final ClusterService clusterService;
 
-    // Yes, this sucks, but ES and Graylog2 use different injectors and it's not obvious how to bridge them, so I'm using a static. Shoot me.
+    // Yes, this sucks, but ES and Graylog use different injectors and it's not obvious how to bridge them, so I'm using a static. Shoot me.
     private static Cluster cluster;
 
     @org.elasticsearch.common.inject.Inject
@@ -50,6 +55,11 @@ public class ClusterStateMonitor extends org.elasticsearch.common.component.Abst
 
     @Override
     public void clusterChanged(ClusterChangedEvent event) {
+        if (!VALID_CLUSTER_STATES.contains(event.state().status())) {
+            // Only process fully applied cluster states
+            return;
+        }
+
         if (event.state().getNodes().masterAndDataNodes().isEmpty()) {
             log.warn("No Elasticsearch data nodes in cluster, cluster is completely offline.");
         }
@@ -57,7 +67,7 @@ public class ClusterStateMonitor extends org.elasticsearch.common.component.Abst
             // ignore events that don't contain node changes, we don't need to track this now
             return;
         }
-        if (cluster != null){
+        if (cluster != null) {
             final HashMap<String, DiscoveryNode> nodes = Maps.newHashMap();
             for (ObjectObjectCursor<String, DiscoveryNode> cursor : event.state().getNodes().dataNodes()) {
                 nodes.put(cursor.key, cursor.value);
