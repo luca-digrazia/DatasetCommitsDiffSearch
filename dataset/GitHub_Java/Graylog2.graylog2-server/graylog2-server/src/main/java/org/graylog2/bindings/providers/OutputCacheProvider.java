04c08929b65523e57c369565c447e4d70e1b1702
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:51:30 +0200

    Showing helpful error message and exiting when disk journal spool is corrupt.
        To do this, we are wrapping manual instance creation in a provider,
        catching relevant exceptions. Exiting from a provider is dirty, we
        might want to change it to a to-be-implemented "fast shutdown" route.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/providers/OutputCacheProvider.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/bindings/providers/OutputCacheProvider.java
@@ -1,19 +1,3 @@
-/**
- * This file is part of Graylog2.
- *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.graylog2.bindings.providers;
 
 import com.codahale.metrics.MetricRegistry;
@@ -54,7 +38,7 @@ public class OutputCacheProvider implements Provider<OutputCache> {
             System.exit(-1);
         } catch (DiskJournalCacheCorruptSpoolException e) {
             LOG.error("Unable to initialize output journal spool.");
-            LOG.error("This means that your spool files (in directory \"{}\") got corrupted. Please repair or remove them.", configuration.getMessageCacheSpoolDir());
+            LOG.error("This means that your spool files (in directory {}) got corrupted. Please repair or remove them.", configuration.getMessageCacheSpoolDir());
             System.exit(-1);
         }
         return null;
