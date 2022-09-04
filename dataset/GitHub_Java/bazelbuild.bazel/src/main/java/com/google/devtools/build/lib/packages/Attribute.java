e1d80b3114a9ed76dba7b1faea6a8a80e2835ab9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 19:39:37 +0200

    Make most of Attribute DynamicCodec'ed, rather than use AutoCodec. This is a general clean-up, but also I'm getting a bizarre test failure from the @AutoCodec annotation and it goes away when I remove it.
        PiperOrigin-RevId: 251715454

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/Attribute.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/Attribute.java
@@ -43,6 +43,7 @@ import com.google.devtools.build.lib.syntax.ClassObject;
 import com.google.devtools.build.lib.syntax.EvalException;
 import com.google.devtools.build.lib.syntax.EvalUtils;
 import com.google.devtools.build.lib.syntax.Runtime;
+import com.google.devtools.build.lib.syntax.SkylarkCallbackFunction;
 import com.google.devtools.build.lib.syntax.Type;
 import com.google.devtools.build.lib.syntax.Type.ConversionException;
 import com.google.devtools.build.lib.syntax.Type.LabelClass;
@@ -1383,7 +1384,7 @@ public final class Attribute implements Comparable<Attribute> {
    */
   public static final class SkylarkComputedDefaultTemplate {
     private final Type<?> type;
-    private final StarlarkCallbackHelper callback;
+    private final SkylarkCallbackFunction callback;
     private final Location location;
     private final ImmutableList<String> dependencies;
 
@@ -1400,7 +1401,7 @@ public final class Attribute implements Comparable<Attribute> {
     public SkylarkComputedDefaultTemplate(
         Type<?> type,
         ImmutableList<String> dependencies,
-        StarlarkCallbackHelper callback,
+        SkylarkCallbackFunction callback,
         Location location) {
       this.type = Preconditions.checkNotNull(type);
       // Order is important for #createDependencyAssignmentTuple.
