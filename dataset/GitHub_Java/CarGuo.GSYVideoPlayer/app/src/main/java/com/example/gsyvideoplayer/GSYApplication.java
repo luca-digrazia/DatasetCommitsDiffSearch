b3439f16a4016dc8d6dc2d9dc33b853b8bccee95
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:49:40 +0200

    1.4.5 å¢å äºEXOPlayerçæ­æ¾åæ¢æ¯æ

--- dataset/GitHub_Java/CarGuo.GSYVideoPlayer/app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
+++ dataset/GitHub_Java/CarGuo.GSYVideoPlayer/app/src/main/java/com/example/gsyvideoplayer/GSYApplication.java
@@ -24,8 +24,5 @@ public class GSYApplication extends Application {
         //LeakCanary.install(this);
         //GSYVideoType.enableMediaCodec();
         //GSYVideoManager.instance().setVideoType(this, GSYVideoType.IJKEXOPLAYER);
-        //GSYVideoType.setShowType(GSYVideoType.SCREEN_MATCH_FULL);
-        //GSYVideoType.setShowType(GSYVideoType.SCREEN_TYPE_FULL);
-        //GSYVideoType.setRenderType(GSYVideoType.SUFRACE);
     }
 }
