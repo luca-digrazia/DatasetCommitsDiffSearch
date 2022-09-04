618da52fc3d1f1358bbe82dfeef2a759ed15e94d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:50:19 +0200

    Cleaning db before seeding, adding JSON support, adding search paths.
        The logic for finding seeds for a test method is this:
        1. Check for resource of test class which is a directory containing bson
        dumps
        2. Check for resource of test class ending with .json
        3. Check for resource in global seed path ("resources/integration/seeds/mongodb")
        with the correct name which is a directory containing bson dumps
        4. Check for resource in global seed path which ends with .json
    
        If the annotation is present but the locations array is empty, this
        means that the database is cleared but no seeding is done.

--- dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/util/graylog/GraylogControl.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/integration-tests/src/test/java/integration/util/graylog/GraylogControl.java
@@ -1,22 +1,42 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package integration.util.graylog;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.client.utils.URIBuilder;
-import org.testng.SkipException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class GraylogControl {
+    private static final Logger log = LoggerFactory.getLogger(GraylogControl.class);
     private URL url;
     private static final int HTTP_TIMEOUT = 1000;
 
     public GraylogControl() {
-        this.url = getServerUrl();
+        this.url = getDefaultServerUrl();
     }
 
     public GraylogControl(URL url) {
@@ -24,7 +44,7 @@ public class GraylogControl {
     }
 
     public void startServer() {
-        System.out.println("Starting Graylog server...");
+        log.debug("Starting Graylog server...");
         try {
             URL startupUrl = new URL("http", url.getHost(), 5000, "/sv/up/graylog-server");
 
@@ -36,7 +56,7 @@ public class GraylogControl {
     }
 
     public void stopServer() {
-        System.out.println("Stoping Graylog server...");
+        log.debug("Stopping Graylog server...");
         try {
             URL shutdownUrl = new URL("http", url.getHost(), 5000, "/sv/down/graylog-server");
 
@@ -48,7 +68,7 @@ public class GraylogControl {
 
     }
     public void restartServer() {
-        System.out.println("Reestarting Graylog server...");
+        log.debug("Reestarting Graylog server...");
         stopServer();
         startServer();
     }
@@ -125,31 +145,25 @@ public class GraylogControl {
             InputStream inputStream = connection.getInputStream();
             JsonNode json = mapper.readTree(inputStream);
             return json.get("server_id").toString();
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return "00000000-0000-0000-0000-000000000000";
     }
 
-    public URL getServerUrl() {
-        URL url;
+    public URL getUrl() {
+        return url;
+    }
+
+    private URL getDefaultServerUrl() {
         try {
-            URIBuilder uriBuilder = new URIBuilder(System.getProperty("gl2.baseuri", "http://localhost"));
+            final URIBuilder uriBuilder = new URIBuilder(System.getProperty("gl2.baseuri", "http://localhost"));
             uriBuilder.setPort(Integer.parseInt(System.getProperty("gl2.port", "12900")));
             uriBuilder.setUserInfo(System.getProperty("gl2.admin_user", "admin"), System.getProperty("gl2.admin_password", "admin"));
-            url = uriBuilder.build().toURL();
-        } catch (URISyntaxException e) {
-            throw new SkipException("Invalid URI given. Skipping integration tests.");
-        } catch (MalformedURLException e) {
-            throw new SkipException("Invalid URI given. Skipping integration tests.");
+            return uriBuilder.build().toURL();
+        } catch (URISyntaxException | MalformedURLException e) {
+            throw new RuntimeException("Invalid URI given. Skipping integration tests.");
         }
-        return url;
     }
 
     private static void sleep(long millis) {
