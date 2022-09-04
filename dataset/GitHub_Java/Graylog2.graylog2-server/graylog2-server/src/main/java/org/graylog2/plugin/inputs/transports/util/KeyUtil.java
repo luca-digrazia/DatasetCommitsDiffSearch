3042dcd842c05a9bc9914f0dca4c6fd0cbf90200
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:03:17 +0200

    Fix file handle leak in KeyUtil (#2808)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/plugin/inputs/transports/util/KeyUtil.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/plugin/inputs/transports/util/KeyUtil.java
@@ -157,7 +157,6 @@ public class KeyUtil {
             final String[] keyAlgorithms = {"RSA", "DSA", "EC"};
             for(String keyAlgorithm : keyAlgorithms) {
                 try {
-                    @SuppressWarnings("InsecureCryptoUsage")
                     final KeyFactory keyFactory = KeyFactory.getInstance(keyAlgorithm);
                     return keyFactory.generatePrivate(keySpec);
                 } catch (InvalidKeySpecException e) {
@@ -181,7 +180,6 @@ public class KeyUtil {
         final PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray());
         final SecretKey secretKey = kf.generateSecret(keySpec);
 
-        @SuppressWarnings("InsecureCryptoUsage")
         final Cipher cipher = Cipher.getInstance(pkInfo.getAlgName());
         cipher.init(Cipher.DECRYPT_MODE, secretKey, pkInfo.getAlgParameters());
 
