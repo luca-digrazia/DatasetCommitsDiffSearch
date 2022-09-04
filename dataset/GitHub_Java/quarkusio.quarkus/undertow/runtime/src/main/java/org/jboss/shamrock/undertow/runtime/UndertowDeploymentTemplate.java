2e88de28748d3dd3288fb379fe0cd0d4d590bd54
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:21:34 +0200

    Re-enable static file support

--- dataset/GitHub_Java/quarkusio.quarkus/undertow/runtime/src/main/java/org/jboss/shamrock/undertow/runtime/UndertowDeploymentTemplate.java
+++ dataset/GitHub_Java/quarkusio.quarkus/undertow/runtime/src/main/java/org/jboss/shamrock/undertow/runtime/UndertowDeploymentTemplate.java
@@ -1,11 +1,5 @@
 package org.jboss.shamrock.undertow.runtime;
 
-import java.util.EventListener;
-import java.util.concurrent.atomic.AtomicReference;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.Filter;
-import javax.servlet.MultipartConfigElement;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 
@@ -20,9 +14,7 @@ import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
-import io.undertow.servlet.api.FilterInfo;
 import io.undertow.servlet.api.InstanceFactory;
-import io.undertow.servlet.api.ListenerInfo;
 import io.undertow.servlet.api.ServletContainer;
 import io.undertow.servlet.api.ServletInfo;
 
@@ -62,55 +54,16 @@ public class UndertowDeploymentTemplate {
         return new ShamrockInstanceFactory<T>(injectionInstance);
     }
 
-    public AtomicReference<ServletInfo> registerServlet(@ContextObject("deploymentInfo") DeploymentInfo info,
-                                                        String name, Class<?> servletClass,
-                                                        boolean asyncSupported,
-                                                        int loadOnStartup,
-                                                        InstanceFactory<? extends Servlet> instanceFactory) throws Exception {
+    public void registerServlet(@ContextObject("deploymentInfo") DeploymentInfo info, String name, Class<?> servletClass, boolean asyncSupported, InstanceFactory<? extends Servlet> instanceFactory) throws Exception {
         ServletInfo servletInfo = new ServletInfo(name, (Class<? extends Servlet>) servletClass, instanceFactory);
+        servletInfo.setLoadOnStartup(1);
         info.addServlet(servletInfo);
         servletInfo.setAsyncSupported(asyncSupported);
-        if (loadOnStartup > 0) {
-            servletInfo.setLoadOnStartup(loadOnStartup);
-        }
-        return new AtomicReference<>(servletInfo);
-    }
-
-    public void addServletInitParam(AtomicReference<ServletInfo> info, String name, String value) {
-        info.get().addInitParam(name, value);
     }
 
     public void addServletMapping(@ContextObject("deploymentInfo") DeploymentInfo info, String name, String mapping) throws Exception {
         ServletInfo sv = info.getServlets().get(name);
         sv.addMapping(mapping);
-    }
-
-    public void setMultipartConfig(AtomicReference<ServletInfo> sref, String location, long fileSize, long maxRequestSize, int fileSizeThreshold) {
-        MultipartConfigElement mp = new MultipartConfigElement(location, fileSize, maxRequestSize, fileSizeThreshold);
-        sref.get().setMultipartConfig(mp);
-    }
-
-    public AtomicReference<FilterInfo> registerFilter(@ContextObject("deploymentInfo") DeploymentInfo info,
-                                                      String name, Class<?> filterClass,
-                                                      boolean asyncSupported,
-                                                      InstanceFactory<? extends Filter> instanceFactory) throws Exception {
-        FilterInfo filterInfo = new FilterInfo(name, (Class<? extends Filter>) filterClass, instanceFactory);
-        info.addFilter(filterInfo);
-        filterInfo.setAsyncSupported(asyncSupported);
-        return new AtomicReference<>(filterInfo);
-    }
-
-    public void addFilterInitParam(AtomicReference<FilterInfo> info, String name, String value) {
-        info.get().addInitParam(name, value);
-    }
-
-    public void addFilterMapping(@ContextObject("deploymentInfo") DeploymentInfo info, String name, String mapping, DispatcherType dispatcherType) throws Exception {
-        info.addFilterUrlMapping(name, mapping, dispatcherType);
-    }
-
-
-    public void registerListener(@ContextObject("deploymentInfo") DeploymentInfo info, Class<?> listenerClass, InstanceFactory<? extends EventListener> factory) {
-        info.addListener(new ListenerInfo((Class<? extends EventListener>)listenerClass, factory));
     }
 
     public void addServletContextParameter(@ContextObject("deploymentInfo") DeploymentInfo info, String name, String value) {
