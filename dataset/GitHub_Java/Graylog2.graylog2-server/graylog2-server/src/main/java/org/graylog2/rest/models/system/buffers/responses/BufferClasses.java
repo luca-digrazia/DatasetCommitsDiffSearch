17e48bd0db3f8663a5004a63db4423300289ab2d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:24:08 +0200

    Remove license duplicates (the ones which say Graylog2)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/models/system/buffers/responses/BufferClasses.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/models/system/buffers/responses/BufferClasses.java
@@ -16,13 +16,11 @@
  */
 package org.graylog2.rest.models.system.buffers.responses;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 
 @AutoValue
-@JsonAutoDetect
 public abstract class BufferClasses {
     @JsonProperty
     public abstract String inputBuffer();
--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/models/system/metrics/responses/MetricsSummaryResponse.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/rest/models/system/metrics/responses/MetricsSummaryResponse.java
@@ -1,18 +1,18 @@
-/*
- * Copyright (C) 2020 Graylog, Inc.
+/**
+ * This file is part of Graylog.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Graylog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Server Side Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog2.rest.models.system.metrics.responses;
 
@@ -20,26 +20,23 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
-import org.graylog.autovalue.WithBeanGetter;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Collection;
 
 @AutoValue
-@WithBeanGetter
 @JsonAutoDetect
 public abstract class MetricsSummaryResponse {
     @JsonProperty
     public abstract int total();
     @JsonProperty
-    public abstract List<Map<String, Object>> metrics();
+    public abstract Collection metrics();
 
     @JsonCreator
-    public static MetricsSummaryResponse create(@JsonProperty("total") int total, @JsonProperty("metrics") List<Map<String, Object>> metrics) {
+    public static MetricsSummaryResponse create(@JsonProperty("total") int total, @JsonProperty("metrics") Collection metrics) {
         return new AutoValue_MetricsSummaryResponse(total, metrics);
     }
 
-    public static MetricsSummaryResponse create(List<Map<String, Object>> metrics) {
+    public static MetricsSummaryResponse create(Collection metrics) {
         return new AutoValue_MetricsSummaryResponse(metrics.size(), metrics);
     }
 }
