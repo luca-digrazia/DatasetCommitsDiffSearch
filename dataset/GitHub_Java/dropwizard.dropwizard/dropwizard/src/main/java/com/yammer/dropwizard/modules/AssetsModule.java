f8088327272ac66509d7dc48a59455247d39b382
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:05:00 +0200

    Expose the max cache size.

--- dataset/GitHub_Java/dropwizard.dropwizard/dropwizard/src/main/java/com/yammer/dropwizard/modules/AssetsModule.java
+++ dataset/GitHub_Java/dropwizard.dropwizard/dropwizard/src/main/java/com/yammer/dropwizard/modules/AssetsModule.java
@@ -2,7 +2,7 @@ package com.yammer.dropwizard.modules;
 
 import com.yammer.dropwizard.Module;
 import com.yammer.dropwizard.config.Environment;
-import org.eclipse.jetty.servlet.DefaultServlet;
+import com.yammer.dropwizard.servlets.AssetServlet;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -10,16 +10,20 @@ import static com.google.common.base.Preconditions.checkArgument;
  * A module for serving static asset files from the classpath.
  */
 public class AssetsModule implements Module {
+    public static final String DEFAULT_PATH = "/assets";
+    public static final int DEFAULT_MAX_CACHE_SIZE = 100;
+    
     private final String path;
+    private final int maxCacheSize;
 
     /**
      * Creates a new {@link AssetsModule} which serves up static assets from
      * {@code src/main/resources/assets/*} as {@code /assets/*}.
      *
-     * @see AssetsModule#AssetsModule(String)
+     * @see AssetsModule#AssetsModule(String, int)
      */
     public AssetsModule() {
-        this("/assets");
+        this(DEFAULT_PATH, DEFAULT_MAX_CACHE_SIZE);
     }
 
     /**
@@ -28,19 +32,33 @@ public class AssetsModule implements Module {
      * {@code path} of {@code "/assets"}, {@code src/main/resources/assets/example.js} would be
      * served up from {@code /assets/example.js}.
      *
-     * @param path    the classpath and URI root of the static asset files
+     * @param path the classpath and URI root of the static asset files
+     * @see AssetsModule#AssetsModule(String, int)
      */
     public AssetsModule(String path) {
+        this(path, DEFAULT_MAX_CACHE_SIZE);
+    }
+
+    /**
+     * Creates a new {@link AssetsModule} which will configure the service to serve the static files
+     * located in {@code src/main/resources/${path}} as {@code /${path}}. For example, given a
+     * {@code path} of {@code "/assets"}, {@code src/main/resources/assets/example.js} would be
+     * served up from {@code /assets/example.js}.
+     *
+     * @param path            the classpath and URI root of the static asset files
+     * @param maxCacheSize    the maximum number of resources to cache
+     */
+    public AssetsModule(String path, int maxCacheSize) {
         checkArgument(path.startsWith("/"), "%s is not an absolute path", path);
         checkArgument(!"/".equals(path), "%s is the classpath root");
         this.path = path.endsWith("/") ? path : (path + '/');
+        this.maxCacheSize = maxCacheSize;
     }
+
+
 
     @Override
     public void initialize(Environment environment) {
-        environment.addServlet(DefaultServlet.class, path + '*')
-                   .setInitParam("dirAllowed", "false")
-                   .setInitParam("pathInfoOnly", "true")
-                   .setInitParam("relativeResourceBase", path);
+        environment.addServlet(new AssetServlet(path, maxCacheSize), path + '*');
     }
 }
