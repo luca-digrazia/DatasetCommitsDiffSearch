cb253b4c1a5395b6f5b400a10cfb46a145abb3bc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 07:53:41 +0200

    fix split&count boundaries

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/extractors/SplitAndIndexExtractor.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/extractors/SplitAndIndexExtractor.java
@@ -36,17 +36,8 @@ public class SplitAndIndexExtractor extends Extractor {
     public final String splitChar;
     public final int index;
 
-    public SplitAndIndexExtractor(String id,
-                                  String title,
-                                  CursorStrategy cursorStrategy,
-                                  String sourceField,
-                                  String targetField,
-                                  Map<String, Object> extractorConfig,
-                                  String creatorUserId,
-                                  List<Converter> converters,
-                                  ConditionType conditionType,
-                                  String conditionValue) throws ReservedFieldException, ConfigurationException {
-        super(id, title, Type.SPLIT_AND_INDEX, cursorStrategy, sourceField, targetField, extractorConfig, creatorUserId, converters, conditionType, conditionValue);
+    public SplitAndIndexExtractor(String id, String title, CursorStrategy cursorStrategy, String sourceField, String targetField, Map<String, Object> extractorConfig, String creatorUserId, List<Converter> converters) throws ReservedFieldException, ConfigurationException {
+        super(id, title, Type.SPLIT_AND_INDEX, cursorStrategy, sourceField, targetField, extractorConfig, creatorUserId, converters);
 
         if (extractorConfig == null || extractorConfig.get("index") == null || extractorConfig.get("split_by") == null) {
             throw new ConfigurationException("Missing configuration fields. Required: index, split_by");
@@ -61,16 +52,37 @@ public class SplitAndIndexExtractor extends Extractor {
     }
 
     @Override
-    protected Result run(String value) {
-        String result = cut(value, splitChar, index);
-
-        if (result == null) {
-            return null;
+    public void run(Message msg) {
+        // We can only work on Strings.
+        if (!(msg.getField(sourceField) instanceof String)) {
+            return;
         }
 
-        int[] range = getCutIndices(value, splitChar, index);
+        String original = (String) msg.getField(sourceField);
 
-        return new Result(result, range[0], range[1]);
+        String result = cut(original, splitChar, index);
+
+        if (result == null) {
+            return;
+        }
+
+        msg.addField(targetField, result);
+
+        // Remove original from message?
+        if (cursorStrategy.equals(CursorStrategy.CUT)) {
+            int[] range = getCutIndices(original, splitChar, index);
+            StringBuilder sb = new StringBuilder(original);
+            sb.delete(range[0], range[1]);
+
+            String finalResult = sb.toString();
+
+            if(finalResult.isEmpty()) {
+                finalResult = "fullyCutByExtractor";
+            }
+
+            msg.removeField(sourceField);
+            msg.addField(sourceField, finalResult);
+        }
     }
 
     public static String cut(String s, String splitChar, int index) {
