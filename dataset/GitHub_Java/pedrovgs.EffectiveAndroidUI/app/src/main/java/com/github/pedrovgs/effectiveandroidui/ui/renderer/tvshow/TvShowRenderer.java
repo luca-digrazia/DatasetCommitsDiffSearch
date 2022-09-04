032777d8750a49851d1a5df5ed531ca52f44b780
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 15:31:14 +0200

    Fix typo
        Replace seassons with seasons

--- dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/ui/renderer/tvshow/TvShowRenderer.java
+++ dataset/GitHub_Java/pedrovgs.EffectiveAndroidUI/app/src/main/java/com/github/pedrovgs/effectiveandroidui/ui/renderer/tvshow/TvShowRenderer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.pedrovgs.effectiveandroidui.ui.renderer.tvshow;
 
 import android.content.Context;
@@ -76,7 +91,7 @@ public class TvShowRenderer extends Renderer<TvShow> {
   }
 
   private void renderSeasonCounter(TvShow tvShow) {
-    String seassons = context.getString(R.string.seasons_counter, tvShow.getNumberOfSeasons());
-    seasonsCounterTextView.setText(seassons);
+    String seasons = context.getString(R.string.seasons_counter, tvShow.getNumberOfSeasons());
+    seasonsCounterTextView.setText(seasons);
   }
 }
