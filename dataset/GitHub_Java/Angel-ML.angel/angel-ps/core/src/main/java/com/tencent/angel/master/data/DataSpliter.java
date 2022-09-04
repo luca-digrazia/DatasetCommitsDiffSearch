8abdbda3dc464e68db8fab13bb2dbfdb9809db50
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 09:44:23 +0200

    Fix NullPointerException when using old mapper api

--- dataset/GitHub_Java/Angel-ML.angel/angel-ps/core/src/main/java/com/tencent/angel/master/data/DataSpliter.java
+++ dataset/GitHub_Java/Angel-ML.angel/angel-ps/core/src/main/java/com/tencent/angel/master/data/DataSpliter.java
@@ -27,7 +27,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.util.ReflectionUtils;
 
 import java.io.IOException;
@@ -130,7 +129,6 @@ public class DataSpliter {
     JobID jobID = JobID.forName(jobIDStr);
     JobContext jobConf = new JobContextImpl(new JobConf(conf), jobID);
 
-    jobConf.getCredentials().addAll(UserGroupInformation.getCurrentUser().getCredentials());
     // Set split minsize and maxsize to expected split size. We need to get the total size of data
     // first, then divided by expected split number
     long totalInputFileSize = HdfsUtil.getInputFileTotalSize(jobConf);
