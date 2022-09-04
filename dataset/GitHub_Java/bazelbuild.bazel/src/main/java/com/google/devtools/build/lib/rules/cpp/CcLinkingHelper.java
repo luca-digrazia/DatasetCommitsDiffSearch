1dfab0869c487e2a214f0f4776a39419df94eecc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 21:10:00 +0200

    Automated rollback of commit 54983e28ad8b46adf90142928e1df82d6dcd2dc4.
        *** Reason for rollback ***
    
        Breaks the nightly.
    
        PiperOrigin-RevId: 219682986

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CcLinkingHelper.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CcLinkingHelper.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.rules.cpp;
 
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Streams;
@@ -368,23 +367,6 @@ public final class CcLinkingHelper {
     return ccLinkingOutputs;
   }
 
-  public CcLinkingInfo buildCcLinkingInfoFromLibraryToLinkWrappers(
-      ImmutableCollection<LibraryToLinkWrapper> libraryToLinkWrappers,
-      CcCompilationContext ccCompilationContext) {
-    CcLinkingInfo ccLinkingInfo =
-        LibraryToLinkWrapper.toCcLinkingInfo(
-            cppConfiguration.forcePic(),
-            libraryToLinkWrappers,
-            ImmutableList.copyOf(linkopts),
-            linkstamps.build(),
-            ccCompilationContext,
-            nonCodeLinkerInputs);
-    ImmutableList.Builder<CcLinkingInfo> mergedCcLinkingInfos = ImmutableList.builder();
-    mergedCcLinkingInfos.add(ccLinkingInfo);
-    mergedCcLinkingInfos.addAll(ccLinkingInfos);
-    return CcLinkingInfo.merge(mergedCcLinkingInfos.build());
-  }
-
   public CcLinkingInfo buildCcLinkingInfo(
       CcLinkingOutputs ccLinkingOutputs, CcCompilationContext ccCompilationContext) {
     Preconditions.checkNotNull(ccCompilationContext);
@@ -484,12 +466,7 @@ public final class CcLinkingHelper {
       createDynamicLibrary(result, env, usePic, libraryIdentifier, ccOutputs);
     }
 
-    CcLinkingOutputs ccLinkingOutputs = result.build();
-    Preconditions.checkState(ccLinkingOutputs.getStaticLibraries().size() <= 1);
-    Preconditions.checkState(ccLinkingOutputs.getPicStaticLibraries().size() <= 1);
-    Preconditions.checkState(ccLinkingOutputs.getDynamicLibrariesForLinking().size() <= 1);
-    Preconditions.checkState(ccLinkingOutputs.getDynamicLibrariesForRuntime().size() <= 1);
-    return ccLinkingOutputs;
+    return result.build();
   }
 
   public CcLinkingHelper setWillOnlyBeLinkedIntoDynamicLibraries(
