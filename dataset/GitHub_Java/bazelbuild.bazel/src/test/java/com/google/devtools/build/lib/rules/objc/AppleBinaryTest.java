1019af17b684d40cba4f51ccc9f73d3fd7aee77f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 18:37:20 +0200

    Set execution requirements for lipo and j2objc strip actions based on the availability of the selected Xcode.
        This is part of a series of changes that set the execution requirements for actions generated by rules that require Xcodes. The dynamic scheduler will later be modified to run those actions locally/remotely accordingly, but this does not currently change behavior.
    
        RELNOTES: None.
        PiperOrigin-RevId: 288336582

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/rules/objc/AppleBinaryTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/rules/objc/AppleBinaryTest.java
@@ -1716,7 +1716,7 @@ public class AppleBinaryTest extends ObjcRuleTestCase {
 
   @Test
   public void testCustomModuleMap() throws Exception {
-    checkCustomModuleMap(getRuleType());
+    checkCustomModuleMapNotPropagatedByTargetUnderTest(getRuleType());
   }
 
   @Test
