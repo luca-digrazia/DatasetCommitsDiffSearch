fcc28702811edc3d9ae86f2621a24dcbcd34fe22
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:30:22 +0200

    Remove unnecessary imports/annotation.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/streams/StreamRuleType.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/streams/StreamRuleType.java
@@ -1,48 +1,81 @@
+/**
+ * The MIT License
+ * Copyright (c) 2012 Graylog, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.graylog2.plugin.streams;
 
-/**
- * @author Dennis Oelkers <dennis@torch.sh>
- */
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum StreamRuleType {
-    EXACT {
-        @Override
-        public Integer toInteger() {
-            return 1;
-        }
-    },
-    GREATER {
-        @Override
-        public Integer toInteger() {
-            return 3;
-        }
-    },
-    SMALLER {
-        @Override
-        public Integer toInteger() {
-            return 4;
-        }
-    },
-    REGEX {
-        @Override
-        public Integer toInteger() {
-            return 2;
-        }
-    };
+    EXACT(1, "match exactly", "match exactly"),
+    REGEX(2, "match regular expression", "match regular expression"),
+    GREATER(3, "greater than", "be greater than"),
+    SMALLER(4, "smaller than", "be smaller than"),
+    PRESENCE(5, "field presence", "be present");
 
-    public abstract Integer toInteger();
+    private final int value;
+    private final String shortDesc;
+    private final String longDesc;
 
-    public static StreamRuleType fromInteger(int numberic) {
-        switch (numberic) {
-            case 1:
-                return EXACT;
-            case 2:
-                return REGEX;
-            case 3:
-                return GREATER;
-            case 4:
-                return SMALLER;
-            default:
-                return null;
+    StreamRuleType(final int value, final String shortDesc, final String longDesc) {
+        this.value = value;
+        this.shortDesc = shortDesc;
+        this.longDesc = longDesc;
+    }
+
+    public int toInteger() {
+        return value;
+    }
+
+    public static StreamRuleType fromInteger(final int numeric) {
+        for (final StreamRuleType streamRuleType : StreamRuleType.values()) {
+            if (streamRuleType.value == numeric) {
+                return streamRuleType;
+            }
         }
+
+        return null;
+    }
+
+    @JsonCreator
+    public static StreamRuleType fromName(final String name) {
+        for (final StreamRuleType streamRuleType : StreamRuleType.values()) {
+            if (streamRuleType.name().equals(name)) {
+                return streamRuleType;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid Stream Rule Type specified: " + name);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public String getLongDesc() {
+        return longDesc;
     }
 }
