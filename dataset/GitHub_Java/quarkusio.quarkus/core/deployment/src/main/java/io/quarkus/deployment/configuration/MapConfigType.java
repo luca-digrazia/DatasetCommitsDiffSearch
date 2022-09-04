112236ecab9f39ffab39ddf4d4f979a6d55708ce
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 14:44:28 +0200

    MapConfigType: Map methods are interface methods

--- dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/configuration/MapConfigType.java
+++ dataset/GitHub_Java/quarkusio.quarkus/core/deployment/src/main/java/io/quarkus/deployment/configuration/MapConfigType.java
@@ -4,13 +4,13 @@ import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.jboss.protean.gizmo.AssignableResultHandle;
+import org.jboss.protean.gizmo.BytecodeCreator;
+import org.jboss.protean.gizmo.MethodDescriptor;
+import org.jboss.protean.gizmo.ResultHandle;
 import org.wildfly.common.Assert;
 
 import io.quarkus.deployment.AccessorFinder;
-import io.quarkus.gizmo.AssignableResultHandle;
-import io.quarkus.gizmo.BytecodeCreator;
-import io.quarkus.gizmo.MethodDescriptor;
-import io.quarkus.gizmo.ResultHandle;
 import io.quarkus.runtime.configuration.NameIterator;
 import io.smallrye.config.SmallRyeConfig;
 
@@ -19,27 +19,23 @@ import io.smallrye.config.SmallRyeConfig;
 public class MapConfigType extends CompoundConfigType {
 
     private static final MethodDescriptor TREE_MAP_CTOR = MethodDescriptor.ofConstructor(TreeMap.class);
-    private static final MethodDescriptor MAP_GET_METHOD = MethodDescriptor.ofMethod(Map.class, "get", Object.class,
-            Object.class);
-    private static final MethodDescriptor MAP_PUT_METHOD = MethodDescriptor.ofMethod(Map.class, "put", Object.class,
-            Object.class, Object.class);
 
     public MapConfigType(final String containingName, final CompoundConfigType container, final boolean consumeSegment) {
         super(containingName, container, consumeSegment);
     }
 
-    public void load() {
+    public void load() throws ClassNotFoundException {
     }
 
     @SuppressWarnings("unchecked")
     Object getChildObject(final NameIterator name, final SmallRyeConfig config, final Object self, final String childName) {
-        return ((TreeMap<String, Object>) self).get(name.getNextSegment());
+        return ((TreeMap<String, Object>) self).get(childName);
     }
 
     ResultHandle generateGetChildObject(final BytecodeCreator body, final ResultHandle name, final ResultHandle config,
             final ResultHandle self, final String childName) {
-        return body.invokeInterfaceMethod(MAP_GET_METHOD, body.checkCast(self, Map.class),
-                body.invokeVirtualMethod(NI_GET_NEXT_SEGMENT, name));
+        return body.invokeInterfaceMethod(MethodDescriptor.ofMethod(Map.class, "get", Object.class, Object.class),
+                body.checkCast(self, Map.class), body.load(childName));
     }
 
     @SuppressWarnings("unchecked")
@@ -49,8 +45,8 @@ public class MapConfigType extends CompoundConfigType {
 
     void generateSetChildObject(final BytecodeCreator body, final ResultHandle name, final ResultHandle self,
             final String containingName, final ResultHandle value) {
-        body.invokeInterfaceMethod(MAP_PUT_METHOD, body.checkCast(self, Map.class),
-                body.invokeVirtualMethod(NI_GET_NEXT_SEGMENT, name), value);
+        body.invokeInterfaceMethod(MethodDescriptor.ofMethod(Map.class, "put", Object.class, Object.class, Object.class),
+                body.checkCast(self, Map.class), body.invokeVirtualMethod(NI_GET_NEXT_SEGMENT, name), value);
     }
 
     TreeMap<String, Object> getOrCreate(final NameIterator name, final SmallRyeConfig config) {
