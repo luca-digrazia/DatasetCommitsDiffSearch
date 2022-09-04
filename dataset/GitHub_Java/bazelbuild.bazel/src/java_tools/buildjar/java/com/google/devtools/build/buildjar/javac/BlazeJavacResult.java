a95928b39a49a4c9a321c37bb62388ade663396a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 16:06:23 +0200

    Restore another heuristic from https://github.com/bazelbuild/bazel/commit/428aedd82fe50e185d9cd84c7049406c9b4d8bed
        PiperOrigin-RevId: 376287478

--- dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/BlazeJavacResult.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/BlazeJavacResult.java
@@ -16,37 +16,71 @@ package com.google.devtools.build.buildjar.javac;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
-import com.sun.tools.javac.main.Main.Result;
+import com.google.devtools.build.buildjar.javac.statistics.BlazeJavacStatistics;
+import javax.annotation.Nullable;
 
 /** The result of a single compilation performed by {@link BlazeJavacMain}. */
 public class BlazeJavacResult {
+  /** The compilation result. */
+  public enum Status {
+    OK,
+    ERROR,
+    CRASH,
+    REQUIRES_FALLBACK,
+  }
 
-  private final Result javacResult;
+  private final Status status;
   private final ImmutableList<FormattedDiagnostic> diagnostics;
   private final String output;
   private final BlazeJavaCompiler compiler;
+  private final BlazeJavacStatistics statistics;
 
   public static BlazeJavacResult ok() {
-    return new BlazeJavacResult(Result.OK, ImmutableList.of(), "", null);
+    return createFullResult(Status.OK, ImmutableList.of(), "", null, BlazeJavacStatistics.empty());
   }
 
   public static BlazeJavacResult error(String message) {
-    return new BlazeJavacResult(Result.ERROR, ImmutableList.of(), message, null);
+    return createFullResult(
+        Status.ERROR, ImmutableList.of(), message, null, BlazeJavacStatistics.empty());
   }
 
-  public BlazeJavacResult(
-      Result javacResult,
+  public static BlazeJavacResult fallback() {
+    return createFullResult(
+        Status.REQUIRES_FALLBACK, ImmutableList.of(), "", null, BlazeJavacStatistics.empty());
+  }
+
+  public BlazeJavacResult withStatistics(BlazeJavacStatistics statistics) {
+    return new BlazeJavacResult(status, diagnostics, output, compiler, statistics);
+  }
+
+  private BlazeJavacResult(
+      Status status,
       ImmutableList<FormattedDiagnostic> diagnostics,
       String output,
-      BlazeJavaCompiler compiler) {
-    this.javacResult = javacResult;
+      @Nullable BlazeJavaCompiler compiler,
+      BlazeJavacStatistics statistics) {
+    this.status = status;
     this.diagnostics = diagnostics;
     this.output = output;
     this.compiler = compiler;
+    this.statistics = statistics;
   }
 
-  public Result javacResult() {
-    return javacResult;
+  public static BlazeJavacResult createFullResult(
+      Status status,
+      ImmutableList<FormattedDiagnostic> diagnostics,
+      String output,
+      BlazeJavaCompiler compiler,
+      BlazeJavacStatistics statistics) {
+    return new BlazeJavacResult(status, diagnostics, output, compiler, statistics);
+  }
+
+  public boolean isOk() {
+    return status == Status.OK;
+  }
+
+  public Status status() {
+    return status;
   }
 
   public ImmutableList<FormattedDiagnostic> diagnostics() {
@@ -55,6 +89,10 @@ public class BlazeJavacResult {
 
   public String output() {
     return output;
+  }
+
+  public BlazeJavacStatistics statistics() {
+    return statistics;
   }
 
   @VisibleForTesting
