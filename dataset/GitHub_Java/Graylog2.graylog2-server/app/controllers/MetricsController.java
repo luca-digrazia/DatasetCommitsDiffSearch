80a85e9a8847293b861b429756d92066f6d99fa9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 06:57:27 +0200

    Removing obsolete statement.

--- dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/MetricsController.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/app/controllers/MetricsController.java
@@ -20,14 +20,18 @@
 package controllers;
 
 import com.google.inject.Inject;
-import lib.APIException;
-import lib.ApiClient;
+import org.graylog2.restclient.lib.APIException;
+import org.graylog2.restclient.lib.ApiClient;
 import lib.BreadcrumbList;
-import models.Node;
-import models.NodeService;
+import org.graylog2.restclient.lib.metrics.Metric;
+import org.graylog2.restclient.models.ClusterEntity;
+import org.graylog2.restclient.models.Node;
+import org.graylog2.restclient.models.NodeService;
+import org.graylog2.restclient.models.Radio;
 import play.mvc.Result;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author Lennart Koopmann <lennart@torch.sh>
@@ -38,22 +42,60 @@ public class MetricsController extends AuthenticatedController {
     private NodeService nodeService;
 
     public Result ofNode(String nodeId, String preFilter) {
-        Node node = nodeService.loadNode(nodeId);
-
-        BreadcrumbList bc = new BreadcrumbList();
-        bc.addCrumb("System", routes.SystemController.index(0));
-        bc.addCrumb("Nodes", routes.SystemController.nodes());
-        bc.addCrumb(node.getShortNodeId(), routes.SystemController.node(node.getNodeId()));
-        bc.addCrumb("Metrics", routes.MetricsController.ofNode(node.getNodeId(), ""));
-
         try {
-            return ok(views.html.system.metrics.of_node.render(currentUser(), bc, node, node.getMetrics("org.graylog2"), preFilter));
+            Node node = nodeService.loadNode(nodeId);
+
+            BreadcrumbList bc = new BreadcrumbList();
+            bc.addCrumb("System", routes.SystemController.index(0));
+            bc.addCrumb("Nodes", routes.NodesController.nodes());
+            bc.addCrumb(node.getShortNodeId(), routes.NodesController.node(node.getNodeId()));
+            bc.addCrumb("Metrics", routes.MetricsController.ofNode(node.getNodeId(), ""));
+
+            Map<String, Metric> metrics = node.getMetrics("org.graylog2");
+
+            return ok(views.html.system.metrics.of_node.render(currentUser(), bc, node, metrics, preFilter));
         } catch (IOException e) {
             return status(500, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
         } catch (APIException e) {
             String message = "Could not fetch system information. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
             return status(500, views.html.errors.error.render(message, e, request()));
+        } catch (NodeService.NodeNotFoundException e) {
+            return status(404, views.html.errors.error.render(ApiClient.ERROR_MSG_NODE_NOT_FOUND, e, request()));
         }
     }
+
+    public Result ofMasterNode(String preFilter) {
+        try {
+            String masterId = nodeService.loadMasterNode().getNodeId();
+            return redirect(routes.MetricsController.ofNode(masterId, preFilter));
+        } catch (IOException e) {
+            return status(500, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
+        } catch (APIException e) {
+            String message = "Could not fetch master node information. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
+            return status(500, views.html.errors.error.render(message, e, request()));
+        }
+    }
+
+    public Result ofRadio(String radioId, String preFilter) {
+        try {
+            Radio radio = nodeService.loadRadio(radioId);
+
+            BreadcrumbList bc = new BreadcrumbList();
+            bc.addCrumb("System", routes.SystemController.index(0));
+            bc.addCrumb("Nodes", routes.NodesController.nodes());
+            bc.addCrumb(radio.getShortNodeId(), routes.RadiosController.show(radio.getId()));
+            bc.addCrumb("Metrics", routes.MetricsController.ofRadio(radio.getId(), ""));
+
+            return ok(views.html.system.metrics.of_node.render(currentUser(), bc, radio, radio.getMetrics("org.graylog2"), preFilter));
+        } catch (IOException e) {
+            return status(500, views.html.errors.error.render(ApiClient.ERROR_MSG_IO, e, request()));
+        } catch (APIException e) {
+            String message = "Could not fetch system information. We expected HTTP 200, but got a HTTP " + e.getHttpCode() + ".";
+            return status(500, views.html.errors.error.render(message, e, request()));
+        } catch (NodeService.NodeNotFoundException e) {
+            return status(404, views.html.errors.error.render(ApiClient.ERROR_MSG_NODE_NOT_FOUND, e, request()));
+        }
+    }
+
 
 }
