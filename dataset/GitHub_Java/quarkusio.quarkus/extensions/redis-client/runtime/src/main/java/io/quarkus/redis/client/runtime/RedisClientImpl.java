0d3ae4a254a487a63e3926e8776993d86b108e7f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 10:44:37 +0200

    Use `Duration` instead of `long` (holding seconds) for Redis synchronous client timeout

--- dataset/GitHub_Java/quarkusio.quarkus/extensions/redis-client/runtime/src/main/java/io/quarkus/redis/client/runtime/RedisClientImpl.java
+++ dataset/GitHub_Java/quarkusio.quarkus/extensions/redis-client/runtime/src/main/java/io/quarkus/redis/client/runtime/RedisClientImpl.java
@@ -33,8 +33,8 @@ class RedisClientImpl implements RedisClient {
     }
 
     @Override
-    public Response auth(List<String> args) {
-        return await(redisAPI.auth(args));
+    public Response auth(String arg0) {
+        return await(redisAPI.auth(arg0));
     }
 
     @Override
@@ -103,8 +103,8 @@ class RedisClientImpl implements RedisClient {
     }
 
     @Override
-    public Response command(List<String> args) {
-        return await(redisAPI.command(args));
+    public Response command() {
+        return await(redisAPI.command());
     }
 
     @Override
