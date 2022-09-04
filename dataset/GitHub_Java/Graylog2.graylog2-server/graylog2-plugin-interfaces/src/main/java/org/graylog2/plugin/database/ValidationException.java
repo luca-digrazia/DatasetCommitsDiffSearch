3aefc2ec2e0f85cc5c122b5d51ab9dc46885298c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 05:29:33 +0200

    Set Throwable#detailMessage in ValidationException
        Refs #1543
    
        (cherry picked from commit 85f2d17f6bf4efcb3f079f6d6a9b1b196e1caaa3)

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/database/ValidationException.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-plugin-interfaces/src/main/java/org/graylog2/plugin/database/ValidationException.java
@@ -1,25 +1,31 @@
 /**
- * This file is part of Graylog2.
+ * The MIT License
+ * Copyright (c) 2012 Graylog, Inc.
  *
- * Graylog2 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Graylog2 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License
- * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package org.graylog2.plugin.database;
 
+import com.google.common.collect.ImmutableMap;
 import org.graylog2.plugin.database.validators.ValidationResult;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +33,7 @@ public class ValidationException extends Exception {
     private final Map<String, List<ValidationResult>> errors;
 
     public ValidationException(Map<String, List<ValidationResult>> errors) {
-        this.errors = errors;
+        this.errors = ImmutableMap.copyOf(errors);
     }
 
     public ValidationException(final String message) {
@@ -35,8 +41,8 @@ public class ValidationException extends Exception {
     }
 
     public ValidationException(final String field, final String message) {
-        this.errors = new HashMap<>();
-        this.errors.put(field, Collections.<ValidationResult>singletonList(new ValidationResult.ValidationFailed(message)));
+        super(message);
+        this.errors = ImmutableMap.of(field, Collections.<ValidationResult>singletonList(new ValidationResult.ValidationFailed(message)));
     }
 
     public Map<String, List<ValidationResult>> getErrors() {
