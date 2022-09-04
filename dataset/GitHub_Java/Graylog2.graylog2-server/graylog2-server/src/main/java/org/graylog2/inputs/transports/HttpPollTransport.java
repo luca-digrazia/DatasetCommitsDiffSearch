f4801717fe382ab202c5f298124078641ba800ba
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-04 04:08:58 +0200

    Prevent HTTPPollTransport from stopping on non 2XX (#10717)
        Fix for issue #9941
        As the ScheduledExecutorService stops any further execution when it received an Exception once, a HTTP response other than 2XX should be logged, but no exception should be thrown.

--- dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/transports/HttpPollTransport.java
+++ dataset/GitHub_Java/Graylog2.graylog2-server/graylog2-server/src/main/java/org/graylog2/inputs/transports/HttpPollTransport.java
@@ -1,18 +1,18 @@
-/**
- * This file is part of Graylog.
+/*
+ * Copyright (C) 2020 Graylog, Inc.
  *
- * Graylog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
  *
- * Graylog is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package org.graylog2.inputs.transports;
 
@@ -154,17 +154,17 @@ public class HttpPollTransport extends ThrottleableTransport {
             if (isThrottled()) {
                 // this transport won't block, but we can simply skip this iteration
                 LOG.debug("Not polling HTTP resource {} because we are throttled.", url);
+                return;
             }
 
             final Request.Builder requestBuilder = new Request.Builder().get()
                     .url(url)
                     .headers(Headers.of(headers));
 
-            try {
-                final Response r = httpClient.newCall(requestBuilder.build()).execute();
-
+            try (final Response r = httpClient.newCall(requestBuilder.build()).execute()) {
                 if (!r.isSuccessful()) {
-                    throw new RuntimeException("Expected successful HTTP status code [2xx], got " + r.code());
+                    LOG.error("Expected successful HTTP status code [2xx], got " + r.code());
+                    return;
                 }
 
                 input.processRawMessage(new RawMessage(r.body().bytes(), remoteAddress));
