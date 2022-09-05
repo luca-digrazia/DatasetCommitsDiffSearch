9f1f6816a78f9d1461f50428cd7bb11815148768
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:15:08 +0200

    Add a function to JavaCommon to create instrumented jars.
        --
        MOS_MIGRATED_REVID=91277053

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaCommon.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaCommon.java
@@ -159,12 +159,6 @@ public class JavaCommon {
         .build();
   }
 
-  public static ImmutableList<String> getConstraints(RuleContext ruleContext) {
-    return ruleContext.getRule().isAttrDefined("constraints", Type.STRING_LIST)
-        ? ImmutableList.copyOf(ruleContext.attributes().get("constraints", Type.STRING_LIST))
-        : ImmutableList.<String>of();
-  }
-
   public void setClassPathFragment(ClasspathConfiguredFragment classpathFragment) {
     this.classpathFragment = classpathFragment;
   }
@@ -640,7 +634,7 @@ public class JavaCommon {
    *
    * @return the value of the neverlink attribute.
    */
-  public static final boolean isNeverLink(RuleContext ruleContext) {
+  public final boolean isNeverLink() {
     return ruleContext.getRule().isAttrDefined("neverlink", Type.BOOLEAN) &&
         ruleContext.attributes().get("neverlink", Type.BOOLEAN);
   }
