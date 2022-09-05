076d0c19a78d332226accbed6e2fe175c943c86d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:15:19 +0200

    ä¼åæ ç»æ

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-core/src/main/java/org/hswebframework/web/context/ContextUtils.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-core/src/main/java/org/hswebframework/web/context/ContextUtils.java
@@ -18,12 +18,11 @@ public class ContextUtils {
     }
 
     public static Mono<Context> reactiveContext() {
-        return Mono
-                .subscriberContext()
+        return Mono.subscriberContext()
                 .<Context>handle((context, sink) -> {
                     if (context.hasKey(Context.class)) {
                         sink.next(context.get(Context.class));
-                    } else {
+                    }else {
                         sink.complete();
                     }
                 })
