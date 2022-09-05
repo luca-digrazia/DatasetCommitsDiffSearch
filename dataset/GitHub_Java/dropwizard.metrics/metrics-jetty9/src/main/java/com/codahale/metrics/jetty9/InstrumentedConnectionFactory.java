aab44ee08189f19c684dc098580e2ae86d6bb584
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:19:45 +0200

    Upgrade Jetty integration to Jetty 9.4
        This update removes reflection hacks for supporting Jetty 9.3 and higher and
        allows to test the Jetty and Servlets modules against the latest Jetty version
        9.4.0.v20161208
    
        (cherry picked from commit 2a1054da2499bc8b529298c2dca1eb2038c96dc3)
    
        # Conflicts:
        #       metrics-jetty9/src/main/java/com/codahale/metrics/jetty9/InstrumentedConnectionFactory.java
        #       metrics-servlets/pom.xml
        #       pom.xml

--- dataset/GitHub_Java/dropwizard.metrics/metrics-jetty9/src/main/java/com/codahale/metrics/jetty9/InstrumentedConnectionFactory.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-jetty9/src/main/java/com/codahale/metrics/jetty9/InstrumentedConnectionFactory.java
@@ -1,6 +1,5 @@
 package com.codahale.metrics.jetty9;
 
-import com.codahale.metrics.Counter;
 import com.codahale.metrics.Timer;
 import org.eclipse.jetty.io.Connection;
 import org.eclipse.jetty.io.EndPoint;
@@ -13,16 +12,10 @@ import java.util.List;
 public class InstrumentedConnectionFactory extends ContainerLifeCycle implements ConnectionFactory {
     private final ConnectionFactory connectionFactory;
     private final Timer timer;
-    private final Counter counter;
 
     public InstrumentedConnectionFactory(ConnectionFactory connectionFactory, Timer timer) {
-        this(connectionFactory, timer, null);
-    }
-
-    public InstrumentedConnectionFactory(ConnectionFactory connectionFactory, Timer timer, Counter counter) {
         this.connectionFactory = connectionFactory;
         this.timer = timer;
-        this.counter = counter;
         addBean(connectionFactory);
     }
 
@@ -45,17 +38,11 @@ public class InstrumentedConnectionFactory extends ContainerLifeCycle implements
             @Override
             public void onOpened(Connection connection) {
                 this.context = timer.time();
-                if (counter != null) {
-                    counter.inc();
-                }
             }
 
             @Override
             public void onClosed(Connection connection) {
                 context.stop();
-                if (counter != null) {
-                    counter.dec();
-                }
             }
         });
         return connection;
