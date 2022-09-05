6d5179716d0fcd8c2936a43b1d3317a12c7e2c90
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:27:02 +0200

    Improvements to InstrumentedExecutorServiceTest

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/test/java/com/codahale/metrics/InstrumentedExecutorServiceTest.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/test/java/com/codahale/metrics/InstrumentedExecutorServiceTest.java
@@ -2,20 +2,15 @@ package com.codahale.metrics;
 
 import org.junit.After;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class InstrumentedExecutorServiceTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(InstrumentedExecutorServiceTest.class);
-
     private final ExecutorService executor = Executors.newCachedThreadPool();
     private final MetricRegistry registry = new MetricRegistry();
     private final InstrumentedExecutorService instrumentedExecutorService = new InstrumentedExecutorService(executor, registry, "xs");
@@ -54,7 +49,7 @@ public class InstrumentedExecutorServiceTest {
     public void tearDown() throws Exception {
         instrumentedExecutorService.shutdown();
         if (!instrumentedExecutorService.awaitTermination(2, TimeUnit.SECONDS)) {
-            LOGGER.error("InstrumentedExecutorService did not terminate.");
+            System.err.println("InstrumentedExecutorService did not terminate.");
         }
     }
 
