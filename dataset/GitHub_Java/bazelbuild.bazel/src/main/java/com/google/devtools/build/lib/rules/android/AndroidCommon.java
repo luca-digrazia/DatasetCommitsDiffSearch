eaad8559b1cd6e682784142cebacc62a1644eb24
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:20:09 +0200

    Rollback of commit 67b4d5250edcefa7220e928e529b1f385e2dc464.
        *** Reason for rollback ***
    
        Memory regression: []
    
        *** Original change description ***
    
        Pass direct jars to Jack.
    
        The R class generator produces a compiled jar, not a java source file.
        This means all android libraries/binaries with resources will need to
        convert this jar to jack to ensure that they have access to jars.
    
        This change will not fix Jack builds, but it is a necessary step toward
        fixing them.
    
        --
        PiperOrigin-RevId: 141091215
        MOS_MIGRATED_REVID=141091215

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidCommon.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidCommon.java
@@ -97,7 +97,7 @@ public class AndroidCommon {
     IterablesChain.Builder<T> builder = IterablesChain.builder();
     AttributeMap attributes = ruleContext.attributes();
     for (String attr : TRANSITIVE_ATTRIBUTES) {
-      if (attributes.has(attr, BuildType.LABEL_LIST)) {
+      if (ruleContext.attributes().has(attr, BuildType.LABEL_LIST)) {
         builder.add(ruleContext.getPrerequisites(attr, mode, classType));
       }
     }
@@ -993,9 +993,6 @@ public class AndroidCommon {
       compileDeps = JavaCommon.defaultDeps(ruleContext, semantics, ClasspathType.COMPILE_ONLY);
       if (useDataBinding) {
         compileDeps = DataBinding.addSupportLibs(ruleContext, compileDeps);
-      }
-      if (AndroidIdlHelper.hasIdlSrcs(ruleContext)) {
-        compileDeps = AndroidIdlHelper.addSupportLibs(ruleContext, compileDeps);
       }
       runtimeDeps = JavaCommon.defaultDeps(ruleContext, semantics, ClasspathType.RUNTIME_ONLY);
       bothDeps = JavaCommon.defaultDeps(ruleContext, semantics, ClasspathType.BOTH);
