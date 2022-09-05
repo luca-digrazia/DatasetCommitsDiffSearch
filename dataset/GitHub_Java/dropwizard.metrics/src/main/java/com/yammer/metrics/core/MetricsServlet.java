fb6702c9f49ed6db07faa1306fca1ee9e58d9b05
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:50:36 +0200

    Added default landing page.

--- dataset/GitHub_Java/dropwizard.metrics/src/main/java/com/yammer/metrics/core/MetricsServlet.java
+++ dataset/GitHub_Java/dropwizard.metrics/src/main/java/com/yammer/metrics/core/MetricsServlet.java
@@ -190,7 +190,7 @@ public class MetricsServlet extends HttpServlet {
 
 	private void handleMetrics(String classPrefix, boolean showFullSamples, HttpServletResponse resp) throws IOException {
 		resp.setStatus(HttpServletResponse.SC_OK);
-		resp.setContentType("application/json");
+		resp.setContentType("text/plain");
 		final OutputStream output = resp.getOutputStream();
 		final JsonGenerator json = factory.createJsonGenerator(output, JsonEncoding.UTF8);
 		json.writeStartObject();
@@ -346,8 +346,8 @@ public class MetricsServlet extends HttpServlet {
 		json.writeStartObject();
 		{
 			json.writeStringField("type", "meter");
-			json.writeStringField("event_type", meter.eventType());
-			json.writeStringField("unit", meter.scaleUnit().toString().toLowerCase());
+			json.writeStringField("event_type", meter.getEventType());
+			json.writeStringField("unit", meter.getScaleUnit().toString().toLowerCase());
 			json.writeNumberField("count", meter.count());
 			json.writeNumberField("mean", meter.meanRate());
 			json.writeNumberField("m1", meter.oneMinuteRate());
@@ -364,7 +364,7 @@ public class MetricsServlet extends HttpServlet {
 			json.writeFieldName("duration");
 			json.writeStartObject();
 			{
-				json.writeStringField("unit", timer.durationUnit().toString().toLowerCase());
+				json.writeStringField("unit", timer.getDurationUnit().toString().toLowerCase());
 				json.writeNumberField("min", timer.min());
 				json.writeNumberField("max", timer.max());
 				json.writeNumberField("mean", timer.mean());
@@ -387,7 +387,7 @@ public class MetricsServlet extends HttpServlet {
 			json.writeFieldName("rate");
 			json.writeStartObject();
 			{
-				json.writeStringField("unit", timer.rateUnit().toString().toLowerCase());
+				json.writeStringField("unit", timer.getRateUnit().toString().toLowerCase());
 				json.writeNumberField("count", timer.count());
 				json.writeNumberField("mean", timer.meanRate());
 				json.writeNumberField("m1", timer.oneMinuteRate());
