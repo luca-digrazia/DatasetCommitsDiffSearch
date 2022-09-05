cf5f4c39feb78ce5797ae1822551a7e7761552ee
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:18:09 +0200

    Fix NPE in TestsInSuiteFunction.
        Note that this only affects the new Skyframe-based implementation of the
        LoadingPhaseRunner, which isn't live yet.
    
        --
        MOS_MIGRATED_REVID=114230017

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/TestsInSuiteFunction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/TestsInSuiteFunction.java
@@ -35,6 +35,7 @@ import com.google.devtools.build.skyframe.SkyFunction;
 import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.SkyValue;
 import com.google.devtools.build.skyframe.ValueOrException;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -43,6 +44,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+
 import javax.annotation.Nullable;
 
 /**
@@ -52,7 +54,7 @@ import javax.annotation.Nullable;
 // TODO(ulfjack): What about test_suite rules that include each other.
 final class TestsInSuiteFunction implements SkyFunction {
   @Override
-  public SkyValue compute(SkyKey key, Environment env) throws InterruptedException {
+  public SkyValue compute(SkyKey key, Environment env) {
     TestsInSuite expansion = (TestsInSuite) key.argument();
     ResolvedTargets<Target> result =
         computeTestsInSuite(env, expansion.getTestSuite(), expansion.isStrict());
@@ -63,18 +65,21 @@ final class TestsInSuiteFunction implements SkyFunction {
   }
 
   /**
-   * Populates 'result' with all the tests associated with the specified 'testSuite'. Throws an
-   * exception if any target is missing.
+   * Populates 'result' with all the tests associated with the specified
+   * 'testSuite'.  Throws an exception if any target is missing.
    *
-   * <p>CAUTION! Keep this logic consistent with {@code TestSuite}!
+   * <p>CAUTION!  Keep this logic consistent with {@code TestSuite}!
    */
-  private static ResolvedTargets<Target> computeTestsInSuite(
-      Environment env, Rule testSuite, boolean strict) throws InterruptedException {
+  private ResolvedTargets<Target> computeTestsInSuite(
+      Environment env, Rule testSuite, boolean strict) {
     ResolvedTargets.Builder<Target> builder = ResolvedTargets.builder();
     List<Target> testsAndSuites = new ArrayList<>();
     // Note that testsAndSuites can contain input file targets; the test_suite rule does not
     // restrict the set of targets that can appear in tests or suites.
     builder.mergeError(getPrerequisites(env, testSuite, "tests", testsAndSuites));
+    if (testSuite.getRuleClassObject().hasAttr("suites", BuildType.LABEL_LIST)) {
+      builder.mergeError(getPrerequisites(env, testSuite, "suites", testsAndSuites));
+    }
 
     // 1. Add all tests
     for (Target test : testsAndSuites) {
@@ -127,9 +132,8 @@ final class TestsInSuiteFunction implements SkyFunction {
    * found a problem during the lookup process; the actual error message is reported to the
    * environment.
    */
-  private static boolean getPrerequisites(
-      Environment env, Rule testSuite, String attrName, List<Target> targets)
-      throws InterruptedException {
+  private boolean getPrerequisites(Environment env, Rule testSuite, String attrName,
+      List<Target> targets) {
     List<Label> labels =
         NonconfigurableAttributeMapper.of(testSuite).get(attrName, BuildType.LABEL_LIST);
     Set<PackageIdentifier> pkgIdentifiers = new LinkedHashSet<>();
