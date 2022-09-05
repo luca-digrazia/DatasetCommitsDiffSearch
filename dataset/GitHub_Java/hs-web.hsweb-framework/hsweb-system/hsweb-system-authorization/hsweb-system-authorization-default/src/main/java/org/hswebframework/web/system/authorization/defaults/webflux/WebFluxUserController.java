b3153e3ca086fd142ff714d81b5d99cc07e0e1be
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:14:20 +0200

    ä¼åè¯´æ

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-default/src/main/java/org/hswebframework/web/system/authorization/defaults/webflux/WebFluxUserController.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-default/src/main/java/org/hswebframework/web/system/authorization/defaults/webflux/WebFluxUserController.java
@@ -1,12 +1,13 @@
 package org.hswebframework.web.system.authorization.defaults.webflux;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import lombok.Setter;
 import org.hswebframework.web.authorization.Authentication;
 import org.hswebframework.web.authorization.User;
-import org.hswebframework.web.authorization.annotation.Authorize;
-import org.hswebframework.web.authorization.annotation.Resource;
-import org.hswebframework.web.authorization.annotation.SaveAction;
+import org.hswebframework.web.authorization.annotation.*;
 import org.hswebframework.web.authorization.exception.UnAuthorizedException;
 import org.hswebframework.web.crud.web.reactive.ReactiveServiceQueryController;
 import org.hswebframework.web.system.authorization.api.entity.UserEntity;
@@ -19,6 +20,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/user")
 @Authorize
 @Resource(id = "user", name = "系统用户", group = "system")
+@Tag(name = "用户管理")
 public class WebFluxUserController implements ReactiveServiceQueryController<UserEntity, String> {
 
     @Autowired
@@ -26,6 +28,7 @@ public class WebFluxUserController implements ReactiveServiceQueryController<Use
 
     @PatchMapping
     @SaveAction
+    @Operation(summary = "保存用户信息")
     public Mono<Boolean> saveUser(@RequestBody Mono<UserEntity> user) {
         return Authentication
                 .currentReactive()
@@ -38,14 +41,37 @@ public class WebFluxUserController implements ReactiveServiceQueryController<Use
                 .as(reactiveUserService::saveUser);
     }
 
+
+    @PutMapping("/me")
+    @Operation(summary = "修改当前用户信息")
+    @ResourceAction(id = "update-self-info",name = "修改当前用户信息")
+    public Mono<Boolean> updateLoginUserInfo(@RequestBody UserEntity request) {
+        return Authentication
+                .currentReactive()
+                .switchIfEmpty(Mono.error(UnAuthorizedException::new))
+                .map(Authentication::getUser)
+                .map(User::getId)
+                .flatMap(userId -> reactiveUserService.updateById(userId, Mono.just(request)).map(integer -> integer > 0));
+    }
+
     @PutMapping("/{id:.+}/{state}")
     @SaveAction
-    public Mono<Integer> changeState(@PathVariable String id, @PathVariable Byte state) {
+    @Operation(summary = "修改用户状态")
+    public Mono<Integer> changeState(@PathVariable @Parameter(description = "用户ID") String id,
+                                     @PathVariable @Parameter(description = "状态,0禁用,1启用") Byte state) {
         return reactiveUserService.changeState(Mono.just(id), state);
     }
 
+    @DeleteMapping("/{id:.+}")
+    @DeleteAction
+    @Operation(summary = "删除用户")
+    public Mono<Boolean> deleteUser(@PathVariable String id) {
+        return reactiveUserService.deleteUser(id);
+    }
+
     @PutMapping("/passwd")
-    @Authorize(merge = false)
+    @ResourceAction(id = "update-self-pwd",name = "修改当前用户密码")
+    @Operation(summary = "修改当前用户密码")
     public Mono<Boolean> changePassword(@RequestBody ChangePasswordRequest request) {
         return Authentication
                 .currentReactive()
@@ -54,6 +80,7 @@ public class WebFluxUserController implements ReactiveServiceQueryController<Use
                 .map(User::getId)
                 .flatMap(userId -> reactiveUserService.changePassword(userId, request.getOldPassword(), request.getNewPassword()));
     }
+
 
     @Override
     public DefaultReactiveUserService getService() {
