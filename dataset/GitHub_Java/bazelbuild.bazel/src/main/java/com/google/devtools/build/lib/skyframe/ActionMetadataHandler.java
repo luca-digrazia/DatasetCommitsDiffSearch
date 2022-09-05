b7679e43b07fec86264de504a5e4242f5dff7f01
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:16:22 +0200

    Delete debugging code used to diagnose bug when shared actions raced to execute.
        --
        MOS_MIGRATED_REVID=91007686

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/ActionMetadataHandler.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/ActionMetadataHandler.java
@@ -35,6 +35,7 @@ import com.google.devtools.build.lib.vfs.Symlinks;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -281,13 +282,11 @@ public class ActionMetadataHandler implements MetadataHandler {
   }
 
   @Override
-  public void discardOutputMetadata() {
+  public void discardMetadata(Collection<Artifact> artifactList) {
     Preconditions.checkState(injectedArtifacts.isEmpty(),
         "Artifacts cannot be injected before action execution: %s", injectedArtifacts);
-    Preconditions.checkState(omittedOutputs.isEmpty(),
-        "Artifacts cannot be marked omitted before action execution: %s", omittedOutputs);
-    outputArtifactData.clear();
-    additionalOutputData.clear();
+    outputArtifactData.keySet().removeAll(artifactList);
+    additionalOutputData.keySet().removeAll(artifactList);
   }
 
   @Override
