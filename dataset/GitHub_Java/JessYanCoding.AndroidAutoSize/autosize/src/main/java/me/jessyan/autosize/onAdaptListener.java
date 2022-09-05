232930be8723867ce7036c665e2def78e3e7c6f5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:55:18 +0200

    Create onAdaptListener

--- dataset/GitHub_Java/JessYanCoding.AndroidAutoSize/autosize/src/main/java/me/jessyan/autosize/onAdaptListener.java
+++ dataset/GitHub_Java/JessYanCoding.AndroidAutoSize/autosize/src/main/java/me/jessyan/autosize/onAdaptListener.java
@@ -16,6 +16,7 @@
 package me.jessyan.autosize;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
 
 /**
  * ================================================
@@ -30,7 +31,7 @@ public interface onAdaptListener {
     /**
      * 在屏幕适配前调用
      *
-     * @param target   需要屏幕适配的对象 (可能是 {@link Activity} 或者 Fragment)
+     * @param target   需要屏幕适配的对象 (可能是 {@link Activity} 或者 {@link Fragment})
      * @param activity 当前 {@link Activity}
      */
     void onAdaptBefore(Object target, Activity activity);
@@ -38,7 +39,7 @@ public interface onAdaptListener {
     /**
      * 在屏幕适配后调用
      *
-     * @param target   需要屏幕适配的对象 (可能是 {@link Activity} 或者 Fragment)
+     * @param target   需要屏幕适配的对象 (可能是 {@link Activity} 或者 {@link Fragment})
      * @param activity 当前 {@link Activity}
      */
     void onAdaptAfter(Object target, Activity activity);
