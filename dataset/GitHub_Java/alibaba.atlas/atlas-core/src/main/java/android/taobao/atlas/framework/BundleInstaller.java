6ae5ca530ea0ffdfc3805cb62bfd7711efbbf5c4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 09:14:21 +0200

    [atlas-core]crashmanager reflect fix

--- dataset/GitHub_Java/alibaba.atlas/atlas-core/src/main/java/android/taobao/atlas/framework/BundleInstaller.java
+++ dataset/GitHub_Java/alibaba.atlas/atlas-core/src/main/java/android/taobao/atlas/framework/BundleInstaller.java
@@ -543,7 +543,7 @@ public class BundleInstaller implements Callable{
         if(bundle==null){
             bundle = Framework.restoreFromExistedBundle(bundleName);
         }
-        if(bundle==null && (BaselineInfoManager.instance().isDexPatched(bundleName) || BaselineInfoManager.instance().isUpdated(bundleName))){
+        if(bundle==null && BaselineInfoManager.instance().isChanged(bundleName)){
             throw new RuntimeException("restore existed bundle failed");
         }
         return bundle;
@@ -616,11 +616,6 @@ public class BundleInstaller implements Callable{
 
     /**
      * 获取bundle源文件地址
-     *
-     * bundle finding order by follwing paths:
-     * 1) ${getApplicationInfo().dataDir}/lib/
-     * 2) ${getApplicationInfo().nativeLibraryDir}
-     * 3) apk's "lib/armeabi" path.
      * @param location
      */
     private void findBundleSource(String location) throws IOException{
@@ -630,10 +625,8 @@ public class BundleInstaller implements Callable{
         String bundleFileName = String.format("lib%s.so",location.replace(".","_"));
         String bundlePath = String.format("%s/lib/%s", dataDir,bundleFileName);
         File bundleFile = new File(bundlePath);
-        if(!bundleFile.exists()){
-            bundleFile = new File(RuntimeVariables.androidApplication.getApplicationInfo().nativeLibraryDir,bundleFileName);
-        }
-        if(bundleFile.exists() && AtlasBundleInfoManager.instance().isInternalBundle(location)){
+        BundleListing.BundleInfo info = AtlasBundleInfoManager.instance().getBundleInfo(location);
+        if(bundleFile.exists() && info.isInternal()){
             mTmpBundleSourceFile = bundleFile;
             Log.e("BundleInstaller","find valid bundle : "+bundleFile.getAbsolutePath());
         }else{
