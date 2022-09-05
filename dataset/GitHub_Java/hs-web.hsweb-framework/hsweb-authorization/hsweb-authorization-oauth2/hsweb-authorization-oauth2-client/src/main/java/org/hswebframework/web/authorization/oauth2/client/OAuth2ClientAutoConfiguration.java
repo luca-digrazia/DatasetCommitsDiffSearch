8368fb4503474fa9ff1fd0be1d62ae45b6ed9df0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:44:01 +0200

    å¢å HswebResponseJudgeSupport

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-oauth2/hsweb-authorization-oauth2-client/src/main/java/org/hswebframework/web/authorization/oauth2/client/OAuth2ClientAutoConfiguration.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-oauth2/hsweb-authorization-oauth2-client/src/main/java/org/hswebframework/web/authorization/oauth2/client/OAuth2ClientAutoConfiguration.java
@@ -7,9 +7,7 @@ import org.hswebframework.web.authorization.oauth2.client.request.DefaultRespons
 import org.hswebframework.web.authorization.oauth2.client.simple.*;
 import org.hswebframework.web.authorization.oauth2.client.simple.provider.HswebResponseConvertSupport;
 import org.hswebframework.web.authorization.oauth2.client.simple.provider.HswebResponseJudgeSupport;
-import org.hswebframework.web.authorization.oauth2.client.simple.provider.github.GithubResponseConvert;
 import org.hswebframework.web.authorization.oauth2.client.simple.request.builder.SimpleOAuth2RequestBuilderFactory;
-import org.hswebframework.web.concurrent.lock.LockManager;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -44,12 +42,8 @@ public class OAuth2ClientAutoConfiguration {
 
     @ConditionalOnMissingBean(OAuth2RequestService.class)
     @Bean
-    public SimpleOAuth2RequestService simpleOAuth2RequestService(OAuth2ServerConfigRepository configRepository
-            , OAuth2UserTokenRepository userTokenRepository
-            , OAuth2RequestBuilderFactory builderFactory
-            , LockManager lockManager) {
-
-        return new SimpleOAuth2RequestService(configRepository, userTokenRepository, builderFactory,lockManager);
+    public SimpleOAuth2RequestService simpleOAuth2RequestService(OAuth2ServerConfigRepository configRepository, OAuth2UserTokenRepository userTokenRepository, OAuth2RequestBuilderFactory builderFactory) {
+        return new SimpleOAuth2RequestService(configRepository, userTokenRepository, builderFactory);
     }
 
     @ConditionalOnMissingBean(OAuth2ServerConfigRepository.class)
