8d80c79199432ed534aed45cda6972cb821fdd0c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:56:22 +0200

    å é¤æ ç¨ä¾èµ

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service/hsweb-web-service-simple/src/main/java/org/hsweb/web/service/impl/AbstractServiceImpl.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service/hsweb-web-service-simple/src/main/java/org/hsweb/web/service/impl/AbstractServiceImpl.java
@@ -1,12 +1,16 @@
 package org.hsweb.web.service.impl;
 
-import org.hsweb.web.bean.common.*;
+import org.hsweb.commons.ClassUtils;
 import org.hsweb.web.bean.po.GenericPo;
 import org.hsweb.web.bean.validator.ValidateResults;
 import org.hsweb.web.core.exception.NotFoundException;
 import org.hsweb.web.core.exception.ValidationException;
-import org.hsweb.web.dao.GenericMapper;
+import org.hsweb.web.dao.*;
 import org.hsweb.web.service.GenericService;
+import org.hsweb.web.service.commons.SimpleDeleteService;
+import org.hsweb.web.service.commons.SimpleInsertService;
+import org.hsweb.web.service.commons.SimpleQueryService;
+import org.hsweb.web.service.commons.SimpleUpdateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +18,23 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 /**
- * Created by 浩 on 2016-01-22 0022.
+ * 抽象通用服务实现类，通过指定{@link GenericMapper} 实现通用的增删改查方法
+ *
+ * @param <Po> PO类型
+ * @param <PK> 主键类型
+ * @author zhouhao
+ * @see GenericService
+ * @since 1.0
  */
 @Transactional(rollbackFor = Throwable.class)
-public abstract class AbstractServiceImpl<Po, PK> implements GenericService<Po, PK> {
+public abstract class AbstractServiceImpl<Po extends GenericPo<PK>, PK> implements GenericService<Po, PK>
+        , SimpleQueryService<Po, PK>
+        , SimpleUpdateService<Po, PK>
+        , SimpleDeleteService<PK>
+        , SimpleInsertService<Po, PK> {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
@@ -31,112 +43,38 @@ public abstract class AbstractServiceImpl<Po, PK> implements GenericService<Po, 
     protected abstract GenericMapper<Po, PK> getMapper();
 
     @Override
-    @Transactional(readOnly = true)
-    public PagerResult<Po> selectPager(QueryParam param) {
-        PagerResult<Po> pagerResult = new PagerResult<>();
-        param.setPaging(false);
-        int total = getMapper().total(param);
-        pagerResult.setTotal(total);
-        //根据实际记录数量重新指定分页参数
-        param.rePaging(total);
-        pagerResult.setData(getMapper().select(param));
-        return pagerResult;
+    public Class<PK> getPKType() {
+        return (Class<PK>) ClassUtils.getGenericType(org.springframework.util.ClassUtils.getUserClass(this.getClass()), 1);
     }
 
     @Override
-    public PK insert(Po data) {
-        PK primaryKey = null;
-        if (data instanceof GenericPo) {
-            if (((GenericPo) data).getId() == null)
-                ((GenericPo) data).setId(GenericPo.createUID());
-            primaryKey = (PK) ((GenericPo) data).getId();
+    public QueryMapper<Po, PK> getQueryMapper() {
+        return getMapper();
+    }
+
+    @Override
+    public UpdateMapper<Po> getUpdateMapper() {
+        return getMapper();
+    }
+
+    @Override
+    public DeleteMapper getDeleteMapper() {
+        return getMapper();
+    }
+
+    @Override
+    public InsertMapper<Po> getInsertMapper() {
+        return getMapper();
+    }
+
+    public void tryValidPo(Po data) {
+        Set<ConstraintViolation<Object>> set = validator.validate(data);
+        ValidateResults results = new ValidateResults();
+        for (ConstraintViolation<Object> violation : set) {
+            results.addResult(violation.getPropertyPath().toString(), violation.getMessage());
         }
-        tryValidPo(data);
-        getMapper().insert(InsertParam.build(data));
-        return primaryKey;
-    }
-
-    public List<PK> batchInsert(List<Po> data, boolean skipFail) {
-        List<PK> pkList = new ArrayList<>();
-        List<Po> insertData = new ArrayList<>();
-        //build
-        for (Po po : data) {
-            if (data instanceof GenericPo) {
-                if (((GenericPo) data).getId() == null)
-                    ((GenericPo) data).setId(GenericPo.createUID());
-                PK primaryKey = (PK) ((GenericPo) data).getId();
-                try {
-                    tryValidPo(po);
-                    insertData.add(po);
-                    pkList.add(primaryKey);
-                } catch (ValidationException e) {
-                    if (!skipFail) {
-                        throw e;
-                    } else if (logger.isWarnEnabled()) {
-                        logger.warn("data validate fail:{}", e);
-                    }
-                }
-
-            }
-        }
-        getMapper().insert((InsertParam) InsertParam.build(insertData));
-        return pkList;
-    }
-
-    @Override
-    public int delete(PK pk) {
-        return getMapper().delete(DeleteParam.build().where("id", pk));
-    }
-
-    @Override
-    public int update(Po data) {
-        return getMapper().update(UpdateParam.build(data));
-    }
-
-    @Override
-    public int update(List<Po> data) {
-        int i = 0;
-        for (Po po : data) {
-            i += getMapper().update(UpdateParam.build(po));
-        }
-        return i;
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<Po> select(QueryParam param) {
-        return this.getMapper().select(param);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Po> select() {
-        return this.getMapper().select(QueryParam.build().noPaging());
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public int total(QueryParam param) {
-        return this.getMapper().total(param);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Po selectByPk(PK pk) {
-        return this.getMapper().selectByPk(pk);
-    }
-
-    @Override
-    public int saveOrUpdate(Po po) {
-        if (po instanceof GenericPo) {
-            Po old = selectByPk((PK) ((GenericPo) po).getId());
-            if (old != null)
-                return update(po);
-            else
-                insert(po);
-        } else {
-            throw new UnsupportedOperationException("不支持此操作");
-        }
-        return 1;
+        if (!results.isSuccess())
+            throw new ValidationException(results);
     }
 
     protected void assertNotNull(Object po, String message) {
@@ -145,15 +83,8 @@ public abstract class AbstractServiceImpl<Po, PK> implements GenericService<Po, 
         }
     }
 
-    protected void tryValidPo(Po data) {
-        Set<ConstraintViolation<Object>> set = validator.validate(data);
-        ValidateResults results = new ValidateResults();
-        if (set.isEmpty()) {
-            for (ConstraintViolation<Object> violation : set) {
-                results.addResult(violation.getPropertyPath().toString(), violation.getMessage());
-            }
-        }
-        if (!results.isSuccess())
-            throw new ValidationException(results);
+    protected void assertNotNull(Object po) {
+        assertNotNull(po, "数据不存在");
     }
+
 }
