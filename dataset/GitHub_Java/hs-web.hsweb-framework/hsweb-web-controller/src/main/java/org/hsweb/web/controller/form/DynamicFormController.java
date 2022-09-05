0a37ebeeba90a9b9005b7eb9fd8d4a017f573d03
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:07:39 +0200

    æ°å¢å¨æè¡¨åæ§å¶å¨

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-controller/src/main/java/org/hsweb/web/controller/form/DynamicFormController.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-controller/src/main/java/org/hsweb/web/controller/form/DynamicFormController.java
@@ -1,6 +1,5 @@
 package org.hsweb.web.controller.form;
 
-import org.hsweb.web.authorize.annotation.Authorize;
 import org.hsweb.web.bean.common.InsertMapParam;
 import org.hsweb.web.bean.common.QueryParam;
 import org.hsweb.web.bean.common.UpdateMapParam;
@@ -25,52 +24,71 @@ public class DynamicFormController {
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     @AccessLogger("查看列表")
-    @Authorize(expression = "#user.hasAccessModuleAction(#name,'R')")
     public ResponseMessage list(@PathVariable("name") String name,
-                                @RequestParam(required = false) QueryParam param) throws Exception {
-        // 获取条件查询
-        Object data;
-        if (!param.isPaging())//不分页
-            data = dynamicFormService.select(name, param);
-        else
-            data = dynamicFormService.selectPager(name, param);
-        return ResponseMessage.ok(data)
-                .include(Map.class, param.getIncludes())
-                .exclude(Map.class, param.getExcludes())
-                .onlyData();
+                                @RequestParam(required = false) QueryParam param) {
+        try {
+            // 获取条件查询
+            Object data;
+            if (!param.isPaging())//不分页
+                data = dynamicFormService.select(name, param);
+            else
+                data = dynamicFormService.selectPager(name, param);
+            return new ResponseMessage(true, data)
+                    .include(Map.class, param.getIncludes())
+                    .exclude(Map.class, param.getExcludes())
+                    .onlyData();
+        } catch (Exception e) {
+            return new ResponseMessage(false, e);
+        }
     }
 
     @RequestMapping(value = "/{name}/{primaryKey}", method = RequestMethod.GET)
     @AccessLogger("按主键查询")
     public ResponseMessage info(@PathVariable("name") String name,
-                                @PathVariable("primaryKey") String primaryKey) throws Exception {
-        Map<String, Object> data = dynamicFormService.selectByPk(name, primaryKey);
-        return ResponseMessage.ok(data);
+                                @PathVariable("primaryKey") String primaryKey) {
+        try {
+            Map<String, Object> data = dynamicFormService.selectByPk(name, primaryKey);
+            return new ResponseMessage(true, data);
+        } catch (Exception e) {
+            return new ResponseMessage(false, e);
+        }
     }
 
     @RequestMapping(value = "/{name}", method = RequestMethod.POST)
     @AccessLogger("新增数据")
     public ResponseMessage insert(@PathVariable("name") String name,
-                                  @RequestBody(required = true) Map<String, Object> data) throws Exception {
-        String pk = dynamicFormService.insert(name, new InsertMapParam(data));
-        return ResponseMessage.ok(pk);
+                                  @RequestBody(required = true) Map<String, Object> data) {
+        try {
+            String pk = dynamicFormService.insert(name, new InsertMapParam(data));
+            return new ResponseMessage(true, pk);
+        } catch (Exception e) {
+            return new ResponseMessage(false, e);
+        }
     }
 
     @RequestMapping(value = "/{name}/{primaryKey}", method = RequestMethod.PUT)
     @AccessLogger("更新数据")
     public ResponseMessage update(@PathVariable("name") String name,
                                   @PathVariable("primaryKey") String primaryKey,
-                                  @RequestBody(required = true) Map<String, Object> data) throws Exception {
-        int i = dynamicFormService.updateByPk(name, primaryKey, new UpdateMapParam(data));
-        return ResponseMessage.ok(i);
+                                  @RequestBody(required = true) Map<String, Object> data) {
+        try {
+            int i = dynamicFormService.updateByPk(name, primaryKey, new UpdateMapParam(data));
+            return new ResponseMessage(true, i);
+        } catch (Exception e) {
+            return new ResponseMessage(false, e);
+        }
     }
 
     @RequestMapping(value = "/{name}/{primaryKey}", method = RequestMethod.DELETE)
     @AccessLogger("删除数据")
     public ResponseMessage delete(@PathVariable("name") String name,
-                                  @PathVariable("primaryKey") String primaryKey) throws Exception {
-        dynamicFormService.deleteByPk(name, primaryKey);
-        return ResponseMessage.ok();
+                                  @PathVariable("primaryKey") String primaryKey) {
+        try {
+            boolean success = dynamicFormService.deleteByPk(name, primaryKey);
+            return new ResponseMessage(true, success);
+        } catch (Exception e) {
+            return new ResponseMessage(false, e);
+        }
     }
 
 }
