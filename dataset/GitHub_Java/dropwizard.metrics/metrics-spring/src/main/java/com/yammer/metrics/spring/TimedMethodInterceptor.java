226dd2c8cf40704e63da8360e0d6a95aa6a39dee
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:39:53 +0200

    One mustn't use PriorityOrdered lightly.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-spring/src/main/java/com/yammer/metrics/spring/TimedMethodInterceptor.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-spring/src/main/java/com/yammer/metrics/spring/TimedMethodInterceptor.java
@@ -1,7 +1,9 @@
 package com.yammer.metrics.spring;
 
-import com.yammer.metrics.annotation.Timed;
-import com.yammer.metrics.core.*;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.core.Ordered;
@@ -9,55 +11,49 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.util.ReflectionUtils.MethodCallback;
 import org.springframework.util.ReflectionUtils.MethodFilter;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+import com.yammer.metrics.annotation.Timed;
+import com.yammer.metrics.core.MetricsRegistry;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.core.TimerContext;
 
 public class TimedMethodInterceptor implements MethodInterceptor, MethodCallback, Ordered {
 
-    private static final MethodFilter filter = new AnnotationFilter(Timed.class);
+	private static final MethodFilter filter = new AnnotationMethodFilter(Timed.class);
 
-    private final MetricsRegistry metrics;
-    private final Class<?> targetClass;
-    private final Map<String, Timer> timers;
+	private final MetricsRegistry metrics;
+	private final Class<?> targetClass;
+	private final Map<String, Timer> timers;
 
-    public TimedMethodInterceptor(final MetricsRegistry metrics, final Class<?> targetClass) {
-        this.metrics = metrics;
-        this.targetClass = targetClass;
-        this.timers = new HashMap<String, Timer>();
+	public TimedMethodInterceptor(final MetricsRegistry metrics, final Class<?> targetClass) {
+		this.metrics = metrics;
+		this.targetClass = targetClass;
+		this.timers = new HashMap<String, Timer>();
 
-        ReflectionUtils.doWithMethods(targetClass, this, filter);
-    }
+		ReflectionUtils.doWithMethods(targetClass, this, filter);
+	}
 
-    @Override
-    public Object invoke(MethodInvocation invocation) throws Throwable {
-        final TimerContext tc = timers.get(invocation.getMethod().getName()).time();
-        try {
-            return invocation.proceed();
-        } finally {
-            tc.stop();
-        }
-    }
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		TimerContext tc = timers.get(invocation.getMethod().getName()).time();
+		try {
+			return invocation.proceed();
+		} finally {
+			tc.stop();
+		}
+	}
 
-    @Override
-    public void doWith(Method method) throws IllegalArgumentException, IllegalAccessException {
-        final Timed timed = method.getAnnotation(Timed.class);
+	@Override
+	public void doWith(Method method) throws IllegalArgumentException, IllegalAccessException {
+		Timed timed = method.getAnnotation(Timed.class);
+		String methodName = method.getName();
+		String timerName = timed.name().isEmpty() ? methodName: timed.name();
+		Timer timer = metrics.newTimer(targetClass, timerName, timed.durationUnit(), timed.rateUnit());
+		timers.put(methodName, timer);
+	}
 
-        final String methodName = method.getName();
-        final String group = MetricName.chooseGroup(timed.group(), targetClass);
-        final String type = MetricName.chooseType(timed.type(), targetClass);
-        final String name = timed.name() == null || timed.name().equals("") ? methodName : timed.name();
-        final MetricName metricName = new MetricName(group, type, name);
-
-        final Timer timer = metrics.newTimer(metricName,
-                                             timed.durationUnit(),
-                                             timed.rateUnit());
-        timers.put(methodName, timer);
-    }
-
-    @Override
-    public int getOrder() {
-        return HIGHEST_PRECEDENCE;
-    }
+	@Override
+	public int getOrder() {
+		return HIGHEST_PRECEDENCE;
+	}
 
 }
