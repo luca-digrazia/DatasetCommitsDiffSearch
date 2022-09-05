f8d57d39c2c30db2016d69067e5d6677447a5182
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:35:02 +0200

    ä¼åæé

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-entity/src/main/java/org/hswebframework/web/entity/authorization/bind/SimpleBindRoleUserEntity.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-entity/src/main/java/org/hswebframework/web/entity/authorization/bind/SimpleBindRoleUserEntity.java
@@ -1,5 +1,8 @@
 package org.hswebframework.web.entity.authorization.bind;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hswebframework.web.entity.authorization.SimpleUserEntity;
 
 import java.util.ArrayList;
@@ -7,38 +10,21 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * TODO 完成注释
- *
  * @author zhouhao
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class SimpleBindRoleUserEntity extends SimpleUserEntity implements BindRoleUserEntity {
 
     private List<String> roles;
 
     @Override
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
-
-    @Override
     public SimpleBindRoleUserEntity clone() {
-        SimpleBindRoleUserEntity target = new SimpleBindRoleUserEntity();
-        target.setId(getId());
-        target.setName(getName());
-        target.setCreateTime(getCreateTime());
-        target.setCreatorId(getCreatorId());
-        target.setEnabled(isEnabled());
-        target.setLastLoginIp(getLastLoginIp());
-        target.setLastLoginTime(getLastLoginTime());
-        target.setSalt(getSalt());
-        if (roles != null)
+        SimpleBindRoleUserEntity target = ((SimpleBindRoleUserEntity) super.clone());
+        if (roles != null) {
             target.setRoles(new ArrayList<>(getRoles()));
-        if (getProperties() != null)
-            target.setProperties(new HashMap<>(getProperties()));
+        }
         return target;
     }
 }
