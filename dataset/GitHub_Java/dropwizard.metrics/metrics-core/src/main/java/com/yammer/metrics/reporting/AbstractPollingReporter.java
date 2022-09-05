1b3ea93bfbe39dc34522e9ab274a3a342a8aa79c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:45:50 +0200

    Make sure AbstractPollingReporter gets an Executor from the appropriate place.
        Fixes #81.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/reporting/AbstractPollingReporter.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/reporting/AbstractPollingReporter.java
@@ -1,16 +1,16 @@
 package com.yammer.metrics.reporting;
 
+import com.yammer.metrics.core.MetricsRegistry;
+
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
-import com.yammer.metrics.core.MetricsRegistry;
 
 public abstract class AbstractPollingReporter extends AbstractReporter implements Runnable {
     private final ScheduledExecutorService executor;
 
     protected AbstractPollingReporter(MetricsRegistry registry, String name) {
         super(registry);
-        this.executor = registry.newScheduledThreadPool(1, name);
+        this.executor = registry.threadPools().newScheduledThreadPool(1, name);
     }
 
     public void start(long pollingTime, TimeUnit pollingTimeUnit) {
