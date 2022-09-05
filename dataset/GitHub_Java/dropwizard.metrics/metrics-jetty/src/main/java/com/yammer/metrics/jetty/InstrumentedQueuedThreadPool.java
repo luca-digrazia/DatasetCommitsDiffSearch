c2c99d76531d926da2aeb0f87fef209b7b1e8b3f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:46:47 +0200

    Don't require a maxThreads for InstrumentedQueuedThreadPool.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-jetty/src/main/java/com/yammer/metrics/jetty/InstrumentedQueuedThreadPool.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-jetty/src/main/java/com/yammer/metrics/jetty/InstrumentedQueuedThreadPool.java
@@ -1,11 +1,9 @@
 package com.yammer.metrics.jetty;
 
-import com.yammer.metrics.util.RatioGauge;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
-
 import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.Gauge;
+import com.yammer.metrics.core.GaugeMetric;
 import com.yammer.metrics.core.MetricsRegistry;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 public class InstrumentedQueuedThreadPool extends QueuedThreadPool {
     public InstrumentedQueuedThreadPool() {
@@ -14,24 +12,22 @@ public class InstrumentedQueuedThreadPool extends QueuedThreadPool {
 
     public InstrumentedQueuedThreadPool(MetricsRegistry registry) {
         super();
-        registry.newGauge(QueuedThreadPool.class, "percent-idle", new RatioGauge() {
+        registry.newGauge(QueuedThreadPool.class, "percent-idle", new GaugeMetric<Integer>() {
             @Override
-            protected double getNumerator() {
-                return getIdleThreads();
-            }
-
-            @Override
-            protected double getDenominator() {
-                return getThreads();
+            public Integer value() {
+                final double percent = getThreads() > 0 ?
+                        getIdleThreads() / ((double) getThreads()) :
+                        0.0;
+                return (int) (percent * 100);
             }
         });
-        registry.newGauge(QueuedThreadPool.class, "active-threads", new Gauge<Integer>() {
+        registry.newGauge(QueuedThreadPool.class, "active-threads", new GaugeMetric<Integer>() {
             @Override
             public Integer value() {
                 return getThreads();
             }
         });
-        registry.newGauge(QueuedThreadPool.class, "idle-threads", new Gauge<Integer>() {
+        registry.newGauge(QueuedThreadPool.class, "idle-threads", new GaugeMetric<Integer>() {
             @Override
             public Integer value() {
                 return getIdleThreads();
