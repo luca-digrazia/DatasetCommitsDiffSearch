6cdf25d465cc958f87c0a8506cd4b63fd42d98da
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:39:47 +0200

    "MINUTES_PER_SECOND = 60.0" => "SECONDS_PER_MINUTE = 60.0"

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/stats/EWMA.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/stats/EWMA.java
@@ -8,13 +8,20 @@ import static java.lang.Math.exp;
 /**
  * An exponentially-weighted moving average.
  *
- * @see <a href="http://www.teamquest.com/pdfs/whitepaper/ldavg1.pdf">UNIX Load Average Part 1: How It Works</a>
- * @see <a href="http://www.teamquest.com/pdfs/whitepaper/ldavg2.pdf">UNIX Load Average Part 2: Not Your Average Average</a>
+ * @see <a href="http://www.teamquest.com/pdfs/whitepaper/ldavg1.pdf">UNIX Load Average Part 1: How
+ *      It Works</a>
+ * @see <a href="http://www.teamquest.com/pdfs/whitepaper/ldavg2.pdf">UNIX Load Average Part 2: Not
+ *      Your Average Average</a>
  */
 public class EWMA {
-    private static final double M1_ALPHA  = 1 - exp(-5 / 60.0);
-    private static final double M5_ALPHA  = 1 - exp(-5 / 60.0 / 5);
-    private static final double M15_ALPHA = 1 - exp(-5 / 60.0 / 15);
+    private static final int INTERVAL = 5;
+    private static final double SECONDS_PER_MINUTE = 60.0;
+    private static final int ONE_MINUTE = 1;
+    private static final int FIVE_MINUTES = 5;
+    private static final int FIFTEEN_MINUTES = 15;
+    private static final double M1_ALPHA = 1 - exp(-INTERVAL / SECONDS_PER_MINUTE / ONE_MINUTE);
+    private static final double M5_ALPHA = 1 - exp(-INTERVAL / SECONDS_PER_MINUTE / FIVE_MINUTES);
+    private static final double M15_ALPHA = 1 - exp(-INTERVAL / SECONDS_PER_MINUTE / FIFTEEN_MINUTES);
 
     private volatile boolean initialized = false;
     private volatile double rate = 0.0;
@@ -23,40 +30,40 @@ public class EWMA {
     private final double alpha, interval;
 
     /**
-     * Creates a new EWMA which is equivalent to the UNIX one minute load average and which expects to be ticked every
-     * 5 seconds.
+     * Creates a new EWMA which is equivalent to the UNIX one minute load average and which expects
+     * to be ticked every 5 seconds.
      *
      * @return a one-minute EWMA
      */
     public static EWMA oneMinuteEWMA() {
-        return new EWMA(M1_ALPHA, 5, TimeUnit.SECONDS);
+        return new EWMA(M1_ALPHA, INTERVAL, TimeUnit.SECONDS);
     }
 
     /**
-     * Creates a new EWMA which is equivalent to the UNIX five minute load average and which expects to be ticked every
-     * 5 seconds.
+     * Creates a new EWMA which is equivalent to the UNIX five minute load average and which expects
+     * to be ticked every 5 seconds.
      *
      * @return a five-minute EWMA
      */
     public static EWMA fiveMinuteEWMA() {
-        return new EWMA(M5_ALPHA, 5, TimeUnit.SECONDS);
+        return new EWMA(M5_ALPHA, INTERVAL, TimeUnit.SECONDS);
     }
 
     /**
-     * Creates a new EWMA which is equivalent to the UNIX fifteen minute load average and which expects to be ticked
-     * every 5 seconds.
+     * Creates a new EWMA which is equivalent to the UNIX fifteen minute load average and which
+     * expects to be ticked every 5 seconds.
      *
      * @return a fifteen-minute EWMA
      */
     public static EWMA fifteenMinuteEWMA() {
-        return new EWMA(M15_ALPHA, 5, TimeUnit.SECONDS);
+        return new EWMA(M15_ALPHA, INTERVAL, TimeUnit.SECONDS);
     }
 
     /**
      * Create a new EWMA with a specific smoothing constant.
      *
-     * @param alpha the smoothing constant
-     * @param interval the expected tick interval
+     * @param alpha        the smoothing constant
+     * @param interval     the expected tick interval
      * @param intervalUnit the time unit of the tick interval
      */
     public EWMA(double alpha, long interval, TimeUnit intervalUnit) {
@@ -78,7 +85,7 @@ public class EWMA {
      */
     public void tick() {
         final long count = uncounted.getAndSet(0);
-        double instantRate = count / interval;
+        final double instantRate = count / interval;
         if (initialized) {
             rate += (alpha * (instantRate - rate));
         } else {
