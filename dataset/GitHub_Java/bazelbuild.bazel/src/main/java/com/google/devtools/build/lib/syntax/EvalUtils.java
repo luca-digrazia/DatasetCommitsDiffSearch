464183ebc3ed7342b558bb16bc80b3797bfb5f0a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:59:23 +0200

    Simplification in EvalUtils.optionMap
        --
        MOS_MIGRATED_REVID=95106558

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/EvalUtils.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/EvalUtils.java
@@ -460,7 +460,32 @@ public abstract class EvalUtils {
       o = o.toString();  // Pretty-print a label like a string
     }
     if (o instanceof String) {
-      Printer.writeString(buffer, (String) o);
+      String s = (String) o;
+      buffer.append('"');
+      for (int ii = 0, len = s.length(); ii < len; ++ii) {
+        char c = s.charAt(ii);
+        switch (c) {
+        case '\r':
+          buffer.append('\\').append('r');
+          break;
+        case '\n':
+          buffer.append('\\').append('n');
+          break;
+        case '\t':
+          buffer.append('\\').append('t');
+          break;
+        case '\"':
+          buffer.append('\\').append('"');
+          break;
+        default:
+          if (c < 32) {
+            buffer.append(String.format("\\x%02x", (int) c));
+          } else {
+            buffer.append(c); // no need to support UTF-8
+          }
+        } // endswitch
+      }
+      buffer.append('\"');
     } else {
       printValueX(o, buffer);
     }
