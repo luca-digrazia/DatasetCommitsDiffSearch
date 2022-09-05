e834aeb5608d240ce389527f2a65e13cccd56afc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:30:00 +0200

    Implement action cache resolution for ObjcCompileAction.  This allows for     correct behavior in dotd pruning after a bazel shutdown - the action will     realize that its inputs are not known and will consult the on-disk action     cache.
        --
        MOS_MIGRATED_REVID=138868221

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/ObjcCompileAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/ObjcCompileAction.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.devtools.build.lib.actions.ActionExecutionContext;
 import com.google.devtools.build.lib.actions.ActionExecutionException;
+import com.google.devtools.build.lib.actions.ActionInput;
 import com.google.devtools.build.lib.actions.ActionOwner;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.ArtifactResolver;
@@ -30,6 +31,7 @@ import com.google.devtools.build.lib.actions.Executor;
 import com.google.devtools.build.lib.actions.PackageRootResolutionException;
 import com.google.devtools.build.lib.actions.PackageRootResolver;
 import com.google.devtools.build.lib.actions.ResourceSet;
+import com.google.devtools.build.lib.actions.Spawn;
 import com.google.devtools.build.lib.analysis.actions.CommandLine;
 import com.google.devtools.build.lib.analysis.actions.SpawnAction;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
@@ -40,6 +42,7 @@ import com.google.devtools.build.lib.profiler.ProfilerTask;
 import com.google.devtools.build.lib.rules.apple.AppleConfiguration;
 import com.google.devtools.build.lib.rules.apple.Platform;
 import com.google.devtools.build.lib.rules.cpp.CppCompileAction.DotdFile;
+import com.google.devtools.build.lib.rules.cpp.CppFileTypes;
 import com.google.devtools.build.lib.rules.cpp.HeaderDiscovery;
 import com.google.devtools.build.lib.rules.cpp.HeaderDiscovery.DotdPruningMode;
 import com.google.devtools.build.lib.rules.cpp.IncludeScanningContext;
@@ -66,10 +69,35 @@ import javax.annotation.concurrent.GuardedBy;
  */
 public class ObjcCompileAction extends SpawnAction {
 
+  /**
+   * A spawn that provides all headers to sandboxed execution to allow pruned headers to be
+   * re-introduced into action inputs.
+   */
+  public class ObjcCompileActionSpawn extends ActionSpawn {
+    
+    public ObjcCompileActionSpawn(Map<String, String> clientEnv) {
+      super(clientEnv);
+    }
+
+    @Override
+    public Iterable<? extends ActionInput> getInputFiles() {
+      ImmutableList.Builder<ActionInput> inputs = ImmutableList.<ActionInput>builder()
+          .addAll(super.getInputFiles());
+      
+      for (Artifact headerArtifact : headers) {
+        if (CppFileTypes.OBJC_HEADER.matches(headerArtifact.getFilename())) {
+            inputs.add(headerArtifact);
+          }
+      }
+      return inputs.build();
+    }
+  }
+  
   private final DotdFile dotdFile;
   private final Artifact sourceFile;
   private final NestedSet<Artifact> mandatoryInputs;
   private final HeaderDiscovery.DotdPruningMode dotdPruningPlan;
+  private final NestedSet<Artifact> headers;
 
   // This can be read/written from multiple threads, so accesses must be synchronized.
   @GuardedBy("this")
@@ -94,7 +122,8 @@ public class ObjcCompileAction extends SpawnAction {
       DotdFile dotdFile,
       Artifact sourceFile,
       NestedSet<Artifact> mandatoryInputs,
-      HeaderDiscovery.DotdPruningMode dotdPruningPlan) {
+      HeaderDiscovery.DotdPruningMode dotdPruningPlan,
+      NestedSet<Artifact> headers) {
     super(
         owner,
         tools,
@@ -115,6 +144,7 @@ public class ObjcCompileAction extends SpawnAction {
     this.sourceFile = sourceFile;
     this.mandatoryInputs = mandatoryInputs;
     this.dotdPruningPlan = dotdPruningPlan;
+    this.headers = headers;
     this.inputsKnown = (dotdPruningPlan == DotdPruningMode.DO_NOT_USE);
   }
 
@@ -124,6 +154,11 @@ public class ObjcCompileAction extends SpawnAction {
     return dotdPruningPlan;
   }
 
+  @Override
+  public final Spawn getSpawn(Map<String, String> clientEnv) {
+    return new ObjcCompileActionSpawn(clientEnv);
+  }
+  
   @Override
   public synchronized boolean inputsKnown() {
     return inputsKnown;
@@ -136,8 +171,7 @@ public class ObjcCompileAction extends SpawnAction {
 
   @Override
   public Iterable<Artifact> discoverInputs(ActionExecutionContext actionExecutionContext) {
-    // We do not use include scanning for objc
-    return null;
+    return headers;
   }
  
   // Keep in sync with {@link CppCompileAction#resolveInputsFromCache}
@@ -296,6 +330,7 @@ public class ObjcCompileAction extends SpawnAction {
     private Artifact sourceFile;
     private final NestedSetBuilder<Artifact> mandatoryInputs = new NestedSetBuilder<>(STABLE_ORDER);
     private HeaderDiscovery.DotdPruningMode dotdPruningPlan;
+    private NestedSetBuilder<Artifact> headers = NestedSetBuilder.stableOrder();
 
     /**
      * Creates a new compile action builder with apple environment variables set that are typically
@@ -363,6 +398,20 @@ public class ObjcCompileAction extends SpawnAction {
       this.dotdPruningPlan = dotdPruningPlan;
       return this;
     }
+    
+    /** Adds to the set of all possible headers that could be required by this compile action. */
+    public Builder addTransitiveHeaders(NestedSet<Artifact> headers) {
+      this.headers.addTransitive(Preconditions.checkNotNull(headers));
+      this.addTransitiveInputs(headers);
+      return this;
+    }
+
+    /** Adds to the set of all possible headers that could be required by this compile action. */
+    public Builder addHeaders(Iterable<Artifact> headers) {
+      this.headers.addAll(Preconditions.checkNotNull(headers));
+      this.addInputs(headers);
+      return this;
+    }
 
     @Override
     protected SpawnAction createSpawnAction(
@@ -395,7 +444,8 @@ public class ObjcCompileAction extends SpawnAction {
           dotdFile,
           sourceFile,
           mandatoryInputs.build(),
-          dotdPruningPlan);
+          dotdPruningPlan,
+          headers.build());
     }
   }
 }
