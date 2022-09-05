5889e7dcd2c355fdb3a18b15c7c87ab56afc25e5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:55:44 +0200

    Add tests documenting the way rc-options are combined
        Note that this patch does not change any semantics, it just adds tests.
        In this way, we have a machine-checkable (hence up to date) documentation
        of the semantics of how rc options are handled.
    
        In particular, our semantics is that
        - options get collected from all rc files,
        - more specific options take precedence over less specific ones, and
        - for equally specific options, the most specific rc file takes precedence.
    
        --
        Change-Id: I6abadd15eb02a2f952debc3b005668e04dccd62a
        Reviewed-on: https://bazel-review.googlesource.com/#/c/3241
        MOS_MIGRATED_REVID=119083139

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/runtime/BlazeCommandDispatcherRcoptionsTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/runtime/BlazeCommandDispatcherRcoptionsTest.java
@@ -15,22 +15,12 @@ package com.google.devtools.build.lib.runtime;
 
 import static org.junit.Assert.assertEquals;
 
-import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.devtools.build.lib.analysis.BlazeDirectories;
 import com.google.devtools.build.lib.analysis.ConfigurationCollectionFactory;
-import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
-import com.google.devtools.build.lib.analysis.config.BuildConfiguration;
 import com.google.devtools.build.lib.analysis.config.ConfigurationFactory;
-import com.google.devtools.build.lib.analysis.config.FragmentOptions;
-import com.google.devtools.build.lib.cmdline.Label;
-import com.google.devtools.build.lib.runtime.BlazeCommandDispatcher.LockingMode;
 import com.google.devtools.build.lib.runtime.BlazeCommandDispatcher.ShutdownBlazeServerException;
-import com.google.devtools.build.lib.runtime.proto.InvocationPolicyOuterClass;
 import com.google.devtools.build.lib.testutil.Scratch;
 import com.google.devtools.build.lib.util.ExitCode;
 import com.google.devtools.build.lib.util.io.RecordingOutErr;
@@ -46,8 +36,6 @@ import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Tests the handling of rc-options in {@link BlazeCommandDispatcher}.
@@ -108,22 +96,11 @@ public class BlazeCommandDispatcherRcoptionsTest {
     public void editOptions(CommandEnvironment env, OptionsParser optionsParser) {}
   }
 
-  @Command(
-    name = "reportallinherited",
-    options = {FooOptions.class},
-    shortDescription = "",
-    help = "",
-    inherits = ReportAllCommand.class
-  )
-  private static class ReportAllInheritedCommand extends ReportAllCommand {
-  }
-
 
   private final Scratch scratch = new Scratch();
   private final RecordingOutErr outErr = new RecordingOutErr();
   private final ReportNumCommand reportNum = new ReportNumCommand();
   private final ReportAllCommand reportAll = new ReportAllCommand();
-  private final ReportAllCommand reportAllInherited = new ReportAllInheritedCommand();
   private BlazeRuntime runtime;
 
   @Before
@@ -138,17 +115,6 @@ public class BlazeCommandDispatcherRcoptionsTest {
                 OptionsParser.newOptionsParser(BlazeServerStartupOptions.class))
             .setConfigurationFactory(
                 new ConfigurationFactory(Mockito.mock(ConfigurationCollectionFactory.class)))
-            .addBlazeModule(
-                new BlazeModule() {
-                  @Override
-                  public void initializeRuleClasses(ConfiguredRuleClassProvider.Builder builder) {
-                    // We must add these options so that the defaults package can be created.
-                    builder.addConfigurationOptions(BuildConfiguration.Options.class);
-                    // The defaults package asserts that it is not empty, so we provide options.
-                    builder.addConfigurationOptions(MockFragmentOptions.class);
-                  }
-                })
-            .setInvocationPolicy(InvocationPolicyOuterClass.InvocationPolicy.getDefaultInstance())
             .build();
   }
 
@@ -162,7 +128,7 @@ public class BlazeCommandDispatcherRcoptionsTest {
     List<String> cmdLine = Lists.newArrayList("reportnum");
     cmdLine.addAll(blazercOpts);
 
-    dispatch.exec(cmdLine, LockingMode.ERROR_OUT, "test", outErr);
+    dispatch.exec(cmdLine, outErr);
     String out = outErr.outAsLatin1();
     assertEquals("Common options should be used", "99", out);
   }
@@ -179,7 +145,7 @@ public class BlazeCommandDispatcherRcoptionsTest {
     List<String> cmdLine = Lists.newArrayList("reportnum");
     cmdLine.addAll(blazercOpts);
 
-    dispatch.exec(cmdLine, LockingMode.ERROR_OUT, "test", outErr);
+    dispatch.exec(cmdLine, outErr);
     String out = outErr.outAsLatin1();
     assertEquals("Specific options should dominate common options", "42", out);
   }
@@ -196,13 +162,13 @@ public class BlazeCommandDispatcherRcoptionsTest {
     List<String> cmdLine = Lists.newArrayList("reportnum");
     cmdLine.addAll(blazercOpts);
 
-    dispatch.exec(cmdLine, LockingMode.ERROR_OUT, "test", outErr);
+    dispatch.exec(cmdLine, outErr);
     String out = outErr.outAsLatin1();
     assertEquals("Specific options should dominate common options", "42", out);
   }
 
   @Test
-  public void testOptionsCombined() throws Exception {
+  public void testOptionsComined() throws Exception {
     List<String> blazercOpts =
         ImmutableList.of(
             "--rc_source=/etc/bazelrc",
@@ -214,13 +180,13 @@ public class BlazeCommandDispatcherRcoptionsTest {
     List<String> cmdLine = Lists.newArrayList("reportall");
     cmdLine.addAll(blazercOpts);
 
-    dispatch.exec(cmdLine, LockingMode.ERROR_OUT, "test", outErr);
+    dispatch.exec(cmdLine, outErr);
     String out = outErr.outAsLatin1();
     assertEquals("Options should get accumulated over different rc files", "99 foo", out);
   }
 
   @Test
-  public void testOptionsCombinedWithOverride() throws Exception {
+  public void testOptionsCominedWithOverride() throws Exception {
     List<String> blazercOpts =
         ImmutableList.of(
             "--rc_source=/etc/bazelrc",
@@ -233,13 +199,13 @@ public class BlazeCommandDispatcherRcoptionsTest {
     List<String> cmdLine = Lists.newArrayList("reportall");
     cmdLine.addAll(blazercOpts);
 
-    dispatch.exec(cmdLine, LockingMode.ERROR_OUT, "test", outErr);
+    dispatch.exec(cmdLine, outErr);
     String out = outErr.outAsLatin1();
     assertEquals("The more specific rc-file should override", "99 foo", out);
   }
 
   @Test
-  public void testOptionsCombinedWithOverrideOtherName() throws Exception {
+  public void testOptionsCominedWithOverrideOtherName() throws Exception {
     List<String> blazercOpts =
         ImmutableList.of(
             "--rc_source=/home/jrluser/.blazerc",
@@ -252,56 +218,29 @@ public class BlazeCommandDispatcherRcoptionsTest {
     List<String> cmdLine = Lists.newArrayList("reportall");
     cmdLine.addAll(blazercOpts);
 
-    dispatch.exec(cmdLine, LockingMode.ERROR_OUT, "test", outErr);
+    dispatch.exec(cmdLine, outErr);
     String out = outErr.outAsLatin1();
     assertEquals("The more specific rc-file should override irrespective of name", "99 foo", out);
   }
 
   @Test
-  public void testInheritedOptionsWithSpecificOverride() throws Exception {
-    ImmutableList<ImmutableList<String>> blazercOpts =
+  public void testOptionsCominedWithSpecificOverride() throws Exception {
+    List<String> blazercOpts =
         ImmutableList.of(
-            ImmutableList.of(
-                "--rc_source=/doesnt/matter/0/bazelrc",
-                "--default_override=0:common=--stringoption=common",
-                "--default_override=0:common=--numoption=42"),
-            ImmutableList.of(
-                "--rc_source=/doesnt/matter/1/bazelrc",
-                "--default_override=0:reportall=--stringoption=reportall"),
-            ImmutableList.of(
-                "--rc_source=/doesnt/matter/2/bazelrc",
-                "--default_override=0:reportallinherited=--stringoption=reportallinherited"));
+            "--rc_source=/home/jrluser/.blazerc",
+            "--default_override=0:common=--stringoption=foo",
+            "--default_override=0:reportall=--numoption=42",
+            "--rc_source=/etc/bazelrc",
+            "--default_override=1:common=--stringoption=bar",
+            "--default_override=1:common=--numoption=99");
 
-    for (List<ImmutableList<String>> e : Collections2.permutations(blazercOpts)) {
-      outErr.reset();
-      BlazeCommandDispatcher dispatch =
-          new BlazeCommandDispatcher(runtime, reportNum, reportAll, reportAllInherited);
-      List<String> cmdLine = Lists.newArrayList("reportallinherited");
-      List<String> orderedOpts = ImmutableList.copyOf(Iterables.concat(e));
-      cmdLine.addAll(orderedOpts);
+    BlazeCommandDispatcher dispatch = new BlazeCommandDispatcher(runtime, reportNum, reportAll);
+    List<String> cmdLine = Lists.newArrayList("reportall");
+    cmdLine.addAll(blazercOpts);
 
-      dispatch.exec(cmdLine, LockingMode.ERROR_OUT, "test", outErr);
-      String out = outErr.outAsLatin1();
-      assertEquals(
-          String.format(
-              "The more specific option should override, irrespective of source file or order. %s",
-              orderedOpts),
-          "42 reportallinherited",
-          out);
-    }
-  }
-
-  /** Options class for testing, so that defaults package has some content. */
-  public static class MockFragmentOptions extends FragmentOptions {
-    public MockFragmentOptions() {}
-
-    @Option(name = "fake_opt", defaultValue = "false")
-    public boolean fakeOpt;
-
-    @Override
-    public Map<String, Set<Label>> getDefaultsLabels(BuildConfiguration.Options commonOptions) {
-      return ImmutableMap.<String, Set<Label>>of(
-          "mock_target", ImmutableSet.of(Label.parseAbsoluteUnchecked("//mock:target")));
-    }
+    dispatch.exec(cmdLine, outErr);
+    String out = outErr.outAsLatin1();
+    assertEquals(
+        "The more specific option should override, irrespecitve of source file", "42 bar", out);
   }
 }
