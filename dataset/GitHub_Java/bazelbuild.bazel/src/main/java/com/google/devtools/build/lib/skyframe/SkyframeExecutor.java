7320eaff14ffcb9719f1690ddfa1fb59149c0112
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:22:02 +0200

    Move tests from SkyframeLoadingAndAnalysisTest to LoadingPhaseRunnerTest.
        This exposes an issue with the new Skyframe-based implementation, which is
        fixed here as well.
    
        --
        MOS_MIGRATED_REVID=113556169

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
@@ -376,9 +376,9 @@ public abstract class SkyframeExecutor implements WalkableGraphFactory {
     map.put(SkyFunctions.BUILD_CONFIGURATION,
         new BuildConfigurationFunction(directories, ruleClassProvider));
     map.put(SkyFunctions.CONFIGURATION_COLLECTION, new ConfigurationCollectionFunction(
-        configurationFactory, ruleClassProvider));
+        configurationFactory));
     map.put(SkyFunctions.CONFIGURATION_FRAGMENT, new ConfigurationFragmentFunction(
-        configurationFragments, ruleClassProvider));
+        configurationFragments));
     map.put(SkyFunctions.WORKSPACE_AST, new WorkspaceASTFunction(ruleClassProvider));
     map.put(
         SkyFunctions.WORKSPACE_FILE,
