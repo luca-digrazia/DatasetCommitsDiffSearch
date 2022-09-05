5457c3e343ec5a5071f909198145d049014e6abd
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:06:24 +0200

    Fixed a problem with wrong test statuses when using notest_keep_going (especially with sharding).
        --
        MOS_MIGRATED_REVID=116975152

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/AggregatingTestListener.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/AggregatingTestListener.java
@@ -214,7 +214,7 @@ public class AggregatingTestListener {
   @AllowConcurrentEvents
   public void targetComplete(TargetCompleteEvent event) {
     if (event.failed()) {
-      targetFailure(LabelAndConfiguration.of(event.getTarget()));
+      targetFailure(new LabelAndConfiguration(event.getTarget()));
     }
   }
 
@@ -254,6 +254,6 @@ public class AggregatingTestListener {
   }
 
   private LabelAndConfiguration asKey(ConfiguredTarget target) {
-    return LabelAndConfiguration.of(target);
+    return new LabelAndConfiguration(target);
   }
 }
