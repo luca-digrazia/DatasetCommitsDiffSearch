60414ec85862a4cd417cb4190b618aeed1dd5d8c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:44:34 +0200

    Skip weekday views on Talkback navigation (#780)

--- dataset/GitHub_Java/prolificinteractive.material-calendarview/library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPagerView.java
+++ dataset/GitHub_Java/prolificinteractive.material-calendarview/library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPagerView.java
@@ -154,12 +154,6 @@ abstract class CalendarPagerView extends ViewGroup implements View.OnClickListen
         }
     }
 
-    public void setDayFormatterContentDescription(DayFormatter formatter) {
-        for (DayView dayView : dayViews) {
-            dayView.setDayFormatterContentDescription(formatter);
-        }
-    }
-
     public void setMinimumDate(CalendarDay minDate) {
         this.minDate = minDate;
         updateUi();
