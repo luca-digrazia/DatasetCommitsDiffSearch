fd2044f99c7dd653edbde0f9222550df42e61d28
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:20:03 +0200

    Make rule.getLabels deterministic.
        HashMultimap provides no guarantee on the iteration order.
    
        --
        MOS_MIGRATED_REVID=90153110

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/Rule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/Rule.java
@@ -484,7 +484,7 @@ public final class Rule implements Target {
    * will retain the relative order in which they were declared.
    */
   void populateOutputFiles(EventHandler eventHandler,
-      Package.Builder pkgBuilder) throws SyntaxException {
+      Package.AbstractBuilder<?, ?> pkgBuilder) throws SyntaxException {
     Preconditions.checkState(outputFiles == null);
     // Order is important here: implicit before explicit
     outputFiles = Lists.newArrayList();
@@ -520,7 +520,7 @@ public final class Rule implements Target {
    * of the rule's "name", "srcs", and other attributes.
    */
   private void populateImplicitOutputFiles(EventHandler eventHandler,
-      Package.Builder pkgBuilder) {
+      Package.AbstractBuilder<?, ?> pkgBuilder) {
     try {
       for (String out : ruleClass.getImplicitOutputsFunction().getImplicitOutputs(attributeMap)) {
         try {
