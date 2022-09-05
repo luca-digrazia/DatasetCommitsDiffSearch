952f278bb94abd270c9d203165d66a3f277d464b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:38:28 +0200

    å®åæµè¯

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-oauth2/src/main/java/org/hswebframework/web/service/oauth2/AbstractOAuth2CrudService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-oauth2/src/main/java/org/hswebframework/web/service/oauth2/AbstractOAuth2CrudService.java
@@ -2,10 +2,12 @@ package org.hswebframework.web.service.oauth2;
 
 import org.hswebframework.utils.ClassUtils;
 import org.hswebframework.web.authorization.oauth2.client.OAuth2RequestService;
+import org.hswebframework.web.commons.entity.factory.EntityFactory;
+import org.hswebframework.web.service.CreateEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SuppressWarnings("unchecked")
-public abstract class AbstractOAuth2CrudService<E, PK> implements OAuth2CrudService<E, PK> {
+public abstract class AbstractOAuth2CrudService<E, PK> implements CreateEntityService<E>, OAuth2CrudService<E, PK> {
 
     private Class<E> entityType;
 
@@ -13,21 +15,27 @@ public abstract class AbstractOAuth2CrudService<E, PK> implements OAuth2CrudServ
 
     private OAuth2RequestService oAuth2RequestService;
 
+    private EntityFactory entityFactory;
+
     public AbstractOAuth2CrudService() {
         entityType = (Class) ClassUtils.getGenericType(this.getClass(), 0);
         primaryKeyType = (Class) ClassUtils.getGenericType(this.getClass(), 1);
     }
 
     @Override
-    public OAuth2RequestService getRequestService() {
-        return oAuth2RequestService;
+    public E createEntity() {
+        return entityFactory.newInstance(entityType);
     }
 
     @Override
-    public abstract String getServiceId();
+    public Class<E> getEntityInstanceType() {
+        return entityType;
+    }
 
     @Override
-    public abstract String getUriPrefix();
+    public OAuth2RequestService getRequestService() {
+        return oAuth2RequestService;
+    }
 
     @Override
     public Class<E> getEntityType() {
@@ -37,6 +45,11 @@ public abstract class AbstractOAuth2CrudService<E, PK> implements OAuth2CrudServ
     @Override
     public Class<PK> getPrimaryKeyType() {
         return primaryKeyType;
+    }
+
+    @Autowired
+    public void setEntityFactory(EntityFactory entityFactory) {
+        this.entityFactory = entityFactory;
     }
 
     @Autowired
