aaf3a28411671a01b86887807f056b83ed936df0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:40:25 +0200

    username is login

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-examples/hsweb-examples-oauth2/hsweb-examples-oauth2-client/src/main/java/org/hswebframework/web/example/oauth2/github/GithubSSOAuthorizingListener.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-examples/hsweb-examples-oauth2/hsweb-examples-oauth2-client/src/main/java/org/hswebframework/web/example/oauth2/github/GithubSSOAuthorizingListener.java
@@ -52,11 +52,12 @@ public class GithubSSOAuthorizingListener
 
         String name = String.valueOf(userInfo.get("name"));
         String id = String.valueOf(userInfo.get("id"));
-        String bio = String.valueOf(userInfo.get("bio"));
+        String login = String.valueOf(userInfo.get("login"));
 
         Authentication authentication = new SimpleAuthenticationBuilder(new SimpleDataAccessConfigBuilderFactory())
-                .user(SimpleUser.builder().username(bio).name(name).id("github-user:" + id).build())
-                .role(Arrays.asList(SimpleRole.builder().id("github-user").name("github用户").build()))
+                .user(SimpleUser.builder().username(login).name(name)
+                        .type("github").id("github-user:" + id).build())
+                .role(Arrays.asList(SimpleRole.builder().id("admin").name("github用户").build()))
                 .permission(Arrays.asList(SimplePermission.builder().id("user-info").actions(new HashSet<>(Arrays.asList("get"))).build()))
                 .attributes((Map) userInfo)
                 .build();
