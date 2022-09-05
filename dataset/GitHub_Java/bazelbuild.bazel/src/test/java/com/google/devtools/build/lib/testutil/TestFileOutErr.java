0e03fc3d7d12d0dcb19ad59bf64e9ec4eebe9083
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:56:05 +0200

    Minor refactoring of TestFileOutErr.
        --
        MOS_MIGRATED_REVID=95769513

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/testutil/TestFileOutErr.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/testutil/TestFileOutErr.java
@@ -1,4 +1,4 @@
-// Copyright 2014 The Bazel Authors. All rights reserved.
+// Copyright 2014 Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ import com.google.devtools.build.lib.util.io.FileOutErr;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.inmemoryfs.InMemoryFileSystem;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -31,10 +32,9 @@ public class TestFileOutErr extends FileOutErr {
 
   }
 
-  public TestFileOutErr(Path root) {
-    super(
-        new FlushingFileRecordingOutputStream(root.getChild("out.log")),
-        new FlushingFileRecordingOutputStream(root.getChild("err.log")));
+  public TestFileOutErr(File root) {
+    super(new FlushingFileRecordingOutputStream(newInMemoryFile(root, "out.log")),
+        new FlushingFileRecordingOutputStream(newInMemoryFile(root, "err.log")));
   }
 
   private static Path newInMemoryFile(File root, String name) {
