1d0aa68d3963fd70136bd0f43236be392bd79caf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:44:46 +0200

    Prevent interleaved slow interrupt stack traces
        It's possible that there are multiple outstanding interrupts. When this
        happens avoid interleaving their stack traces.
    
        --
        MOS_MIGRATED_REVID=134477540

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/util/ThreadUtils.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/util/ThreadUtils.java
@@ -13,6 +13,7 @@
 // limitations under the License.
 package com.google.devtools.build.lib.util;
 
+
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,22 +22,22 @@ import java.util.logging.Logger;
  * Utility methods relating to threads and stack traces.
  */
 public class ThreadUtils {
-  private static final Logger logger = Logger.getLogger(ThreadUtils.class.getName());
+  private static final Logger LOG = Logger.getLogger(ThreadUtils.class.getName());
 
   private ThreadUtils() {
   }
 
   /** Write a thread dump to the blaze.INFO log if interrupt took too long. */
   public static synchronized void warnAboutSlowInterrupt() {
-    logger.warning("Interrupt took too long. Dumping thread state.");
+    LOG.warning("Interrupt took too long. Dumping thread state.");
     for (Map.Entry <Thread, StackTraceElement[]> e : Thread.getAllStackTraces().entrySet()) {
       Thread t = e.getKey();
-      logger.warning(
-          "\"" + t.getName() + "\"" + " " + " Thread id=" + t.getId() + " " + t.getState());
+      LOG.warning("\"" + t.getName() + "\"" + " "
+          + " Thread id=" + t.getId() + " " + t.getState());
       for (StackTraceElement line : e.getValue()) {
-        logger.warning("\t" + line);
+        LOG.warning("\t" + line);
       }
-      logger.warning("");
+      LOG.warning("");
     }
     LoggingUtil.logToRemote(Level.WARNING, "Slow interrupt", new SlowInterruptException());
   }
