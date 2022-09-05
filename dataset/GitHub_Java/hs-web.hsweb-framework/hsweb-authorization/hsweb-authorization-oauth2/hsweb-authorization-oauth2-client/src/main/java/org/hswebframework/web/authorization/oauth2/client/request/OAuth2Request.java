19a5a54eda7643fa06a2d7bb75a9e3ac891723c5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:42:15 +0200

    add params

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-oauth2/hsweb-authorization-oauth2-client/src/main/java/org/hswebframework/web/authorization/oauth2/client/request/OAuth2Request.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-oauth2/hsweb-authorization-oauth2-client/src/main/java/org/hswebframework/web/authorization/oauth2/client/request/OAuth2Request.java
@@ -20,8 +20,6 @@ package org.hswebframework.web.authorization.oauth2.client.request;
 
 import org.hswebframework.web.authorization.oauth2.client.response.OAuth2Response;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.Map;
 import java.util.function.Consumer;
 
@@ -45,11 +43,7 @@ public interface OAuth2Request {
      */
     OAuth2Request param(String name, Object value);
 
-    OAuth2Request params(Map<String, String> params);
-
-    OAuth2Response upload(String name, InputStream inputStream);
-
-    OAuth2Response upload(String name, InputStream inputStream,String fileName);
+    OAuth2Request params(Map<String,String> params);
 
     /**
      * 设置请求体,将内容根据contentType(默认application/json)序列化为对应的请求数据
