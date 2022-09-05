64098e1275ede3182d6cad14583e2aeaca91aa38
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:07:56 +0200

    æ°å¢æ ¹æ®ä¸»é®å é¤åä¿®æ¹

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-interface/src/main/java/org/hsweb/web/service/form/DynamicFormService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-interface/src/main/java/org/hsweb/web/service/form/DynamicFormService.java
@@ -3,8 +3,6 @@ package org.hsweb.web.service.form;
 import org.hsweb.web.bean.common.*;
 import org.hsweb.web.bean.po.form.Form;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +10,6 @@ import java.util.Map;
  * Created by zhouhao on 16-4-14.
  */
 public interface DynamicFormService {
-
-    Object parseMeta(Form form) throws Exception;
 
     void deploy(Form form) throws Exception;
 
@@ -27,8 +23,6 @@ public interface DynamicFormService {
 
     String insert(String name, InsertParam<Map<String, Object>> data) throws Exception;
 
-    String saveOrUpdate(String name, Map<String, Object> map) throws Exception;
-
     int delete(String name, DeleteParam param) throws Exception;
 
     boolean deleteByPk(String name, String pk) throws Exception;
@@ -38,8 +32,4 @@ public interface DynamicFormService {
     int updateByPk(String name, String pk, UpdateParam<Map<String, Object>> param) throws Exception;
 
     <T> T selectByPk(String name, Object pk) throws Exception;
-
-    void exportExcel(String name, QueryParam param, OutputStream outputStream) throws Exception;
-
-    Map<String, Object> importExcel(String name, InputStream inputStream) throws Exception;
 }
