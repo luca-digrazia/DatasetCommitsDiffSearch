619be08de16e1f837e026949c840d8e6a61722ff
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:15:36 +0200

    ä¼åè¯´æ

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/web/ResponseMessage.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/web/ResponseMessage.java
@@ -2,36 +2,34 @@ package org.hswebframework.web.crud.web;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
-import lombok.Setter;
-import org.hswebframework.web.api.crud.entity.EntityFactoryHolder;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseMessage<T> implements Serializable {
 
     private static final long serialVersionUID = 8992436576262574064L;
 
     @Schema(description = "消息提示")
-    private String message;
+    protected String message;
 
     @Schema(description = "数据内容")
-    private T result;
+    protected T result;
 
     @Schema(description = "状态码")
     private int status;
 
     @Schema(description = "业务码")
-    private String code;
+    protected String code;
 
     @Schema(description = "时间戳(毫秒)")
-    private Long timestamp = System.currentTimeMillis();
-
-    public ResponseMessage() {
-    }
+    protected Long timestamp = System.currentTimeMillis();
 
     public static <T> ResponseMessage<T> ok() {
         return ok(null);
@@ -39,7 +37,11 @@ public class ResponseMessage<T> implements Serializable {
 
     @SuppressWarnings("all")
     public static <T> ResponseMessage<T> ok(T result) {
-        return of("success", result, 200, null, System.currentTimeMillis());
+        return (ResponseMessage) ResponseMessage.builder()
+                .result(result)
+                .status(200)
+                .code("success")
+                .build();
     }
 
     public static <T> ResponseMessage<T> error(String message) {
@@ -51,21 +53,10 @@ public class ResponseMessage<T> implements Serializable {
     }
 
     public static <T> ResponseMessage<T> error(int status, String code, String message) {
-        return of(message, null, status, code, System.currentTimeMillis());
-    }
-
-    public static <T> ResponseMessage<T> of(String message,
-                                            T result,
-                                            int status,
-                                            String code,
-                                            Long timestamp) {
-        @SuppressWarnings("all")
-        ResponseMessage<T> msg = EntityFactoryHolder.newInstance(ResponseMessage.class, ResponseMessage::new);
-        msg.setMessage(message);
-        msg.setResult(result);
-        msg.setStatus(status);
-        msg.setCode(code);
-        msg.setTimestamp(timestamp);
+        ResponseMessage<T> msg = new ResponseMessage<>();
+        msg.message = message;
+        msg.code = code;
+        msg.status = status;
         return msg;
     }
 
