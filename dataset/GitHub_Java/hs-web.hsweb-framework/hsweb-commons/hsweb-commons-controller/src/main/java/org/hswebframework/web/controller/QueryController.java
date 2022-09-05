0ffb7af554359c983832ad06cb2cb8eabcc06b24
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:56:47 +0200

    ä¿®å¤æªæ³¨è§£PathVariableå¯¼è´åæ°æªæ³¨å¥

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-controller/src/main/java/org/hswebframework/web/controller/QueryController.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-controller/src/main/java/org/hswebframework/web/controller/QueryController.java
@@ -17,15 +17,8 @@
 
 package org.hswebframework.web.controller;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.hswebframework.web.NotFoundException;
-import org.hswebframework.web.authorization.Permission;
 import org.hswebframework.web.authorization.annotation.Authorize;
 import org.hswebframework.web.commons.entity.Entity;
-import org.hswebframework.web.commons.entity.PagerResult;
-import org.hswebframework.web.commons.entity.param.QueryParamEntity;
 import org.hswebframework.web.controller.message.ResponseMessage;
 import org.hswebframework.web.logging.AccessLogger;
 import org.hswebframework.web.service.QueryByEntityService;
@@ -36,63 +29,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import static org.hswebframework.web.controller.message.ResponseMessage.ok;
 
 /**
- * 通用查询控制器。
+ * TODO 完成注释
  *
- * @param <E>  实体类型
- * @param <PK> 主键类型
- * @param <Q>  查询条件实体类型,默认提供{@link QueryParamEntity}实现
  * @author zhouhao
- * @see QueryParamEntity
- * @see 3.0
  */
 public interface QueryController<E, PK, Q extends Entity> {
 
-    /**
-     * 获取实现了{@link QueryByEntityService}和{@link QueryService}的服务类
-     *
-     * @param <T> 服务类泛型
-     * @return 服务类实例
-     */
     <T extends QueryByEntityService<E> & QueryService<E, PK>> T getService();
 
-    /**
-     * 根据参数动态查询。<br>
-     * 参数泛型如果为QueryParamEntity,
-     * 客户的参数 ?terms[0].column=name&terms[0].value=小明
-     * 则执行查询条件 where name = '小明'
-     * 具体使用方法参照 {@link QueryParamEntity}
-     *
-     * @param param 参数
-     * @return 查询结果
-     */
-    @Authorize(action = Permission.ACTION_QUERY)
+    @Authorize(action = "read")
     @GetMapping
-    @AccessLogger("{dynamic_query}")
-    @ApiOperation(value = "根据动态条件查询数据", responseReference = "get")
-    default ResponseMessage<PagerResult<E>> list(Q param) {
+    @AccessLogger("查询")
+    default ResponseMessage list(Q param) {
         return ok(getService().selectPager(param));
     }
 
-    @Authorize(action = Permission.ACTION_QUERY)
-    @GetMapping("/count")
-    @AccessLogger("{dynamic_query}")
-    @ApiOperation(value = "根据动态条件统计数据", responseReference = "get")
-    default ResponseMessage<Integer> count(Q param) {
-        return ok(getService().count(param));
-    }
-
-
-    @Authorize(action = Permission.ACTION_GET)
-    @GetMapping(path = "/{id:.+}")
-    @AccessLogger("{get_by_id}")
-    @ApiOperation("根据主键查询数据")
-    default ResponseMessage<E> getByPrimaryKey(@PathVariable PK id) {
-        return ok(assertNotNull(getService().selectByPk(id)));
-    }
-
-    static <T> T assertNotNull(T obj) {
-        if (null == obj) throw new NotFoundException("{data_not_exist}");
-        return obj;
+    @Authorize(action = "read")
+    @GetMapping(path = "/{id}")
+    @AccessLogger("根据主键查询")
+    default ResponseMessage getByPrimaryKey(@PathVariable PK id) {
+        return ok(getService().selectByPk(id));
     }
 
 }
