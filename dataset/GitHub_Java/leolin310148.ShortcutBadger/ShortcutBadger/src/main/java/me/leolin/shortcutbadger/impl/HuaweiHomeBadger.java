db26f628d15237e5c7ea593e1e35a51e02e45ad2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:42:36 +0200

    Disable logs by default

--- dataset/GitHub_Java/leolin310148.ShortcutBadger/ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/HuaweiHomeBadger.java
+++ dataset/GitHub_Java/leolin310148.ShortcutBadger/ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/HuaweiHomeBadger.java
@@ -4,6 +4,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,11 +17,20 @@ import me.leolin.shortcutbadger.ShortcutBadgeException;
  */
 public class HuaweiHomeBadger implements Badger {
 
+    private static final String LOG_TAG = HuaweiHomeBadger.class.getSimpleName();
+
     @Override
     public void executeBadge(Context context, ComponentName componentName, int badgeCount) throws ShortcutBadgeException {
+        String launcherClassName = componentName.getClassName();
+        if (launcherClassName == null) {
+            if (Log.isLoggable(LOG_TAG, Log.DEBUG)) {
+                Log.d(LOG_TAG, "Main activity is null");
+            }
+            return;
+        }
         Bundle localBundle = new Bundle();
         localBundle.putString("package", context.getPackageName());
-        localBundle.putString("class", componentName.getClassName());
+        localBundle.putString("class", launcherClassName);
         localBundle.putInt("badgenumber", badgeCount);
         context.getContentResolver().call(Uri.parse("content://com.huawei.android.launcher.settings/badge/"), "change_badge", null, localBundle);
     }
