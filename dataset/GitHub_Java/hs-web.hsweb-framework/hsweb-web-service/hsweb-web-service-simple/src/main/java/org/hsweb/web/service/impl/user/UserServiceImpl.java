ace98a7ef7a86dc85571010ac7590f42e9c5d6e9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:57:42 +0200

    ä¿®å¤ä¸è½ä¿®æ¹æ°æ®é®é¢

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service/hsweb-web-service-simple/src/main/java/org/hsweb/web/service/impl/user/UserServiceImpl.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service/hsweb-web-service-simple/src/main/java/org/hsweb/web/service/impl/user/UserServiceImpl.java
@@ -1,22 +1,23 @@
 package org.hsweb.web.service.impl.user;
 
+import org.hsweb.commons.MD5;
 import org.hsweb.web.bean.common.InsertParam;
-import org.hsweb.web.bean.common.QueryParam;
-import org.hsweb.web.bean.common.UpdateParam;
 import org.hsweb.web.bean.po.module.Module;
 import org.hsweb.web.bean.po.role.UserRole;
 import org.hsweb.web.bean.po.user.User;
+import org.hsweb.web.bean.po.user.User.Property;
+import org.hsweb.web.core.exception.BusinessException;
 import org.hsweb.web.core.exception.NotFoundException;
+import org.hsweb.web.core.utils.RandomUtil;
 import org.hsweb.web.dao.role.UserRoleMapper;
 import org.hsweb.web.dao.user.UserMapper;
-import org.hsweb.web.core.exception.BusinessException;
+import org.hsweb.web.service.GenericService;
+import org.hsweb.web.service.QueryService;
 import org.hsweb.web.service.impl.AbstractServiceImpl;
 import org.hsweb.web.service.module.ModuleService;
 import org.hsweb.web.service.user.UserService;
-import org.hsweb.web.core.utils.RandomUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
-import org.hsweb.commons.MD5;
 
 import javax.annotation.Resource;
 import java.util.*;
@@ -43,12 +44,12 @@ public class UserServiceImpl extends AbstractServiceImpl<User, String> implement
         return this.userMapper;
     }
 
-    public User selectByUserName(String username)  {
+    public User selectByUserName(String username) {
         return this.getMapper().selectByUserName(username);
     }
 
     @Override
-    public String insert(User data)  {
+    public String insert(User data) {
         tryValidPo(data);
         Assert.isNull(selectByUserName(data.getUsername()), "用户已存在!");
         data.setId(RandomUtil.randomChar(6));
@@ -59,7 +60,7 @@ public class UserServiceImpl extends AbstractServiceImpl<User, String> implement
         userMapper.insert(new InsertParam<>(data));
         String id = data.getId();
         //添加角色关联
-        if (data.getUserRoles().size() != 0) {
+        if (data.getUserRoles() != null) {
             for (UserRole userRole : data.getUserRoles()) {
                 userRole.setId(RandomUtil.randomChar());
                 userRole.setUserId(data.getId());
@@ -70,12 +71,7 @@ public class UserServiceImpl extends AbstractServiceImpl<User, String> implement
     }
 
     @Override
-    public List<String> batchInsert(List<User> data, boolean skipFail)  {
-        throw new UnsupportedOperationException("不支持此操作");
-    }
-
-    @Override
-    public int update(User data)  {
+    public int update(User data) {
         tryValidPo(data);
         User old = this.selectByUserName(data.getUsername());
         if (old != null && !old.getId().equals(data.getId())) {
@@ -86,8 +82,8 @@ public class UserServiceImpl extends AbstractServiceImpl<User, String> implement
             data.setPassword(MD5.encode(data.getPassword()));
             userMapper.updatePassword(data);
         }
-        int i = userMapper.update(new UpdateParam<>(data).excludes("status", "password", "createDate"));
-        if (data.getUserRoles().size() != 0) {
+        int i = createUpdate(data).excludes(Property.status, Property.password, Property.createDate).fromBean().where(Property.id).exec();
+        if (data.getUserRoles() != null) {
             //删除所有
             userRoleMapper.deleteByUserId(data.getId());
             for (UserRole userRole : data.getUserRoles()) {
@@ -100,10 +96,8 @@ public class UserServiceImpl extends AbstractServiceImpl<User, String> implement
     }
 
     @Override
-    public void initAdminUser(User user)  {
-        QueryParam queryParam = new QueryParam().noPaging();
-        queryParam.orderBy("sortIndex");
-        List<Module> modules = moduleService.select(queryParam);
+    public void initAdminUser(User user) {
+        List<Module> modules = moduleService.createQuery().orderByAsc(Module.Property.sortIndex).listNoPaging();
         Map<Module, Set<String>> roleInfo = new LinkedHashMap<>();
         for (Module module : modules) {
             roleInfo.put(module, new LinkedHashSet<>(module.getOptionalMap().keySet()));
@@ -112,30 +106,32 @@ public class UserServiceImpl extends AbstractServiceImpl<User, String> implement
     }
 
     @Override
-    public void initGuestUser(User user)  {
-        List<UserRole> userRoles = userRoleMapper.select(new QueryParam().where("roleId", "guest").noPaging());
+    public void initGuestUser(User user) {
+        List<UserRole> userRoles = QueryService.createQuery(userRoleMapper).where(UserRole.Property.roleId, "guest").list();
         user.setUserRoles(userRoles);
         user.initRoleInfo();
     }
 
     @Override
-    public void enableUser(String id)  {
+    public void enableUser(String id) {
         User user = selectByPk(id);
         if (user == null) throw new NotFoundException("用户不存在!");
         user.setStatus(1);
-        getMapper().update(new UpdateParam<>(user).includes("status").where("id", id));
+        createUpdate(user).includes(Property.status).where(Property.id, id).exec();
     }
 
     @Override
-    public void disableUser(String id)  {
+    public void disableUser(String id) {
         User user = selectByPk(id);
         if (user == null) throw new NotFoundException("用户不存在!");
         user.setStatus(-1);
-        getMapper().update(new UpdateParam<>(user).includes("status").where("id", id));
+        createUpdate(user).includes(Property.status).where(Property.id, id).exec();
     }
 
     @Override
-    public int delete(String s)  {
+    public int delete(String s) {
         throw new BusinessException("服务不支持", 500);
     }
+
+
 }
