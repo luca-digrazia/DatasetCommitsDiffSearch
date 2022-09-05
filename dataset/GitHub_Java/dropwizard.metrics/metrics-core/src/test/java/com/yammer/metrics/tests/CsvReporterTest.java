646d7dc5ddc63255fb29002bb269cb1f8c2f9ddf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:32:53 +0200

    Added a builder for CsvReporter.
        Closes #362.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/test/java/com/yammer/metrics/tests/CsvReporterTest.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/test/java/com/yammer/metrics/tests/CsvReporterTest.java
@@ -33,12 +33,13 @@ public class CsvReporterTest {
         this.dataDirectory = folder.newFolder();
 
         this.reporter = CsvReporter.forRegistry(registry)
+                                   .outputTo(dataDirectory)
                                    .formatFor(Locale.US)
                                    .convertRatesTo(TimeUnit.SECONDS)
                                    .convertDurationsTo(TimeUnit.MILLISECONDS)
                                    .withClock(clock)
                                    .filter(MetricFilter.ALL)
-                                   .build(dataDirectory);
+                                   .build();
     }
 
     @Test
