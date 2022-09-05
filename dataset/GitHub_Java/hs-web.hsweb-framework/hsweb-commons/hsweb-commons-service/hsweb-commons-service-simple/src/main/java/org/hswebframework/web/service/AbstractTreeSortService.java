cc6200929d0fa0af6a9d4945f8789204c977f4d1
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:25:14 +0200

    ä¼åidå¤æ­

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-simple/src/main/java/org/hswebframework/web/service/AbstractTreeSortService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-simple/src/main/java/org/hswebframework/web/service/AbstractTreeSortService.java
@@ -17,88 +17,168 @@
 
 package org.hswebframework.web.service;
 
+import org.hswebframework.utils.RandomUtil;
 import org.hswebframework.web.commons.entity.TreeSortSupportEntity;
 import org.hswebframework.web.commons.entity.TreeSupportEntity;
-import org.hswebframework.web.id.IDGenerator;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * TODO 完成注释
+ * 抽象树形结构服务类
  *
  * @author zhouhao
+ * @see TreeSortSupportEntity
+ * @since 3.0
  */
 public abstract class AbstractTreeSortService<E extends TreeSortSupportEntity<PK>, PK>
         extends GenericEntityService<E, PK> implements TreeService<E, PK> {
 
-    protected abstract IDGenerator<PK> getIDGenerator();
+    @Override
+    @Transactional(readOnly = true)
+    public List<E> selectParentNode(PK childId) {
+        assertNotNull(childId);
+        E old = selectByPk(childId);
+        if (null == old) {
+            return new ArrayList<>();
+        }
+        return createQuery()
+                .where()
+                // where ? like concat(path,'%')
+                .and("path$like$reverse$startWith", old.getPath())
+                .listNoPaging();
+    }
 
     @Override
     @Transactional(readOnly = true)
     public List<E> selectAllChildNode(PK parentId) {
         assertNotNull(parentId);
         E old = selectByPk(parentId);
-        assertNotNull(old);
-        return createQuery().where().like$(TreeSupportEntity.treeCode, old.getTreeCode()).noPaging().list();
+        if (null == old) {
+            return new ArrayList<>();
+        }
+        return createQuery()
+                .where()
+                .like$(TreeSupportEntity.path, old.getPath())
+                .listNoPaging();
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<E> selectChildNode(PK parentId) {
         assertNotNull(parentId);
-        return createQuery().where(TreeSupportEntity.parentId, parentId).noPaging().list();
+        return createQuery()
+                .where(TreeSupportEntity.parentId, parentId)
+                .listNoPaging();
+    }
+
+    //当父节点不存在时,创建parentId
+    @SuppressWarnings("unchecked")
+    protected PK createParentIdOnExists() {
+        if (getPrimaryKeyType() == String.class) {
+            return (PK) "-1";
+        }
+        return null;
+    }
+
+    protected void applyPath(E entity) {
+        if (StringUtils.isEmpty(entity.getParentId())) {
+            if (entity.getSortIndex() == null) {
+                entity.setSortIndex(0L);
+            }
+            entity.setParentId(createParentIdOnExists());
+            entity.setLevel(0);
+            entity.setPath(RandomUtil.randomChar(4));
+            return;
+        }
+        if (!StringUtils.isEmpty(entity.getPath())) {
+            return;
+        }
+
+        TreeSortSupportEntity<PK> parent = selectByPk(entity.getParentId());
+        if (null == parent) {
+            if (entity.getSortIndex() == null) {
+                entity.setSortIndex(0L);
+            }
+            entity.setParentId(createParentIdOnExists());
+            entity.setPath(RandomUtil.randomChar(4));
+            entity.setLevel(0);
+        } else {
+            if (entity.getSortIndex() == null && parent.getSortIndex() != null) {
+                entity.setSortIndex(parent.getSortIndex() * 10);
+            }
+            entity.setPath(parent.getPath() + "-" + RandomUtil.randomChar(4));
+            entity.setLevel(entity.getPath().split("[-]").length);
+        }
     }
 
     @Override
     public PK insert(E entity) {
-        entity.setId(getIDGenerator().generate());
+        if (StringUtils.isEmpty(entity.getId())) {
+            entity.setId(getIDGenerator().generate());
+        }
+        applyPath(entity);
         List<E> childrenList = new ArrayList<>();
         TreeSupportEntity.expandTree2List(entity, childrenList, getIDGenerator());
-        super.insert(entity);
         childrenList.forEach(this::saveOrUpdateForSingle);
         return entity.getId();
     }
 
+    @Override
+    public List<PK> insertBatch(Collection<E> data) {
+        return data.stream()
+                .map(this::insert)
+                .collect(Collectors.toList());
+    }
+
+    @Override
     public int updateBatch(Collection<E> data) {
-        Assert.notNull(data);
-        return data.stream().map(this::updateByPk).reduce(Math::addExact).orElse(0);
+        assertNotNull(data);
+        return data.stream()
+                .mapToInt(this::updateByPk)
+                .sum();
     }
 
     @Override
     public int updateByPk(E entity) {
-        Assert.notNull(entity);
+        assertNotNull(entity);
         List<E> childrenList = new ArrayList<>();
         TreeSupportEntity.expandTree2List(entity, childrenList, getIDGenerator());
-        return this.saveOrUpdateForSingle(entity) +
-                childrenList.stream()
-                        .map(this::saveOrUpdateForSingle)
-                        .reduce(Math::addExact)
-                        .orElse(0);
+        childrenList.forEach(this::saveOrUpdateForSingle);
+        return childrenList.size() + 1;
     }
 
-    public int saveOrUpdateForSingle(E entity) {
-        Assert.notNull(entity);
+    protected PK saveOrUpdateForSingle(E entity) {
+        assertNotNull(entity);
         PK id = entity.getId();
-        if (null == id || this.selectByPk(id) == null) {
-            if (null == id)
+        if (StringUtils.isEmpty(id) || this.selectByPk(id) == null) {
+            if (StringUtils.isEmpty(id)) {
                 entity.setId(getIDGenerator().generate());
-            super.insert(entity);
-            return 1;
+            }
+            applyPath(entity);
+            return super.insert(entity);
         }
-        return super.updateByPk(entity);
+        super.updateByPk(entity);
+        return id;
     }
 
     @Override
-    public int deleteByPk(PK id) {
+    public E deleteByPk(PK id) {
         E old = selectByPk(id);
         assertNotNull(old);
-        return DefaultDSLDeleteService.createDelete(getDao())
-                // where tree_code like 'treeCode%'
-                .where().like$(TreeSupportEntity.treeCode, old.getTreeCode())
-                .exec();
+        if (StringUtils.isEmpty(old.getPath())) {
+            getDao().deleteByPk(id);
+        } else {
+            DefaultDSLDeleteService.createDelete(getDao())
+                    // where path like 'path%'
+                    .where().like$(TreeSupportEntity.path, old.getPath())
+                    .exec();
+        }
+        return old;
     }
 }
--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-simple/src/main/java/org/hswebframework/web/service/GenericEntityService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-simple/src/main/java/org/hswebframework/web/service/GenericEntityService.java
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright 2019 http://www.hswebframework.org
+ *  * Copyright 2016 http://www.hswebframework.org
  *  *
  *  * Licensed under the Apache License, Version 2.0 (the "License");
  *  * you may not use this file except in compliance with the License.
@@ -19,9 +19,7 @@
 package org.hswebframework.web.service;
 
 import org.hswebframework.web.commons.entity.GenericEntity;
-import org.hswebframework.web.commons.entity.LogicalDeleteEntity;
 import org.hswebframework.web.commons.entity.RecordCreationEntity;
-import org.hswebframework.web.commons.entity.RecordModifierEntity;
 import org.hswebframework.web.dao.CrudDao;
 import org.hswebframework.web.id.IDGenerator;
 import org.hswebframework.web.validator.DuplicateKeyException;
@@ -70,36 +68,23 @@ public abstract class GenericEntityService<E extends GenericEntity<PK>, PK>
 
     @Override
     public E deleteByPk(PK pk) {
+        Assert.notNull(pk, "parameter can not be null");
         E old = selectByPk(pk);
-        if (old == null) {
-            return null;
-        }
-        if (old instanceof LogicalDeleteEntity) {
-            LogicalDeleteEntity deleteEntity = (LogicalDeleteEntity) old;
-            deleteEntity.setDeleted(true);
-            deleteEntity.setDeleteTime(System.currentTimeMillis());
-            createUpdate()
-                    .set(deleteEntity::getDeleted)
-                    .set(deleteEntity::getDeleteTime)
-                    .where(GenericEntity.id, pk)
-                    .exec();
-        } else {
-            getDao().deleteByPk(pk);
-        }
+        getDao().deleteByPk(pk);
         return old;
     }
 
     @Override
     public int updateByPk(PK pk, E entity) {
         Assert.notNull(pk, "primary key can not be null");
-        Assert.hasText(String.valueOf(pk), "primary key can not be null");
         Assert.notNull(entity, "entity can not be null");
         entity.setId(pk);
         tryValidate(entity, UpdateGroup.class);
+
         return createUpdate(entity)
                 //如果是RecordCreationEntity则不修改creator_id和creator_time
                 .when(entity instanceof RecordCreationEntity,
-                        update -> update.and().excludes(((RecordCreationEntity) entity).getCreatorIdProperty(), RecordCreationEntity.createTime))
+                        update -> update.and().excludes(RecordCreationEntity.creatorId, RecordCreationEntity.createTime))
                 .where(GenericEntity.id, pk)
                 .exec();
     }
