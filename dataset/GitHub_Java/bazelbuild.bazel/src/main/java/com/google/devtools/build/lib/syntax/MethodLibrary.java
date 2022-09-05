908de0a43f3bea12619eea8dd2e9612270db28de
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:14:10 +0200

    Following commit 9c4629dafc3aafc10f952824efd448700ad3363e, specify that hashing is deterministic and uses a specific algorithm.
        --
        MOS_MIGRATED_REVID=128070666

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/MethodLibrary.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/MethodLibrary.java
@@ -2120,7 +2120,7 @@ public class MethodLibrary {
       parameters = {
         @Param(name = "x", doc = "The struct whose attribute is accessed."),
         @Param(name = "name", doc = "The name of the struct attribute."),
-        @Param(name = "default", defaultValue = "unbound",
+        @Param(name = "default", defaultValue = "None",
             doc = "The default value to return in case the struct "
             + "doesn't have an attribute of the given name.")},
       useLocation = true, useEnvironment = true)
@@ -2133,7 +2133,7 @@ public class MethodLibrary {
         // 'Real' describes methods with structField() == false. Because DotExpression.eval returned
         // null in this case, we know that structField() cannot return true.
         boolean isRealMethod = hasMethod(obj, name, loc);
-        if (defaultValue != Runtime.UNBOUND && !isRealMethod) {
+        if (defaultValue != Runtime.NONE && !isRealMethod) {
           return defaultValue;
         }
         throw new EvalException(loc, Printer.format("Object of type '%s' has no attribute %r%s",
