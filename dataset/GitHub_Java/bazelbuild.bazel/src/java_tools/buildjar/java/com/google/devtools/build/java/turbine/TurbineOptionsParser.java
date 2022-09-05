f123a0f8e28bce6892bc85983e4446263fad421d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:07:51 +0200

    Global cleanup change.
        --
        MOS_MIGRATED_REVID=116498132

--- dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/java/turbine/TurbineOptionsParser.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/java/turbine/TurbineOptionsParser.java
@@ -82,7 +82,7 @@ public class TurbineOptionsParser {
           builder.setTempDir(readOne(argumentDeque));
           break;
         case "--processors":
-          builder.addProcessors(readList(argumentDeque));
+          builder.setProcessors(readList(argumentDeque));
           break;
         case "--processorpath":
           builder.addProcessorPathEntries(splitClasspath(readOne(argumentDeque)));
