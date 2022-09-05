a9536d4529f7d4d2898027a650e7e0be4870769f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:00:56 +0200

    Global cleanup change.
        --
        PiperOrigin-RevId: 146005461
        MOS_MIGRATED_REVID=146005461

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/concurrent/MoreFutures.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/concurrent/MoreFutures.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.concurrent;
 import static com.google.common.util.concurrent.Futures.addCallback;
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 
-import com.google.common.base.Throwables;
 import com.google.common.collect.Iterables;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
@@ -28,20 +27,21 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import javax.annotation.Nullable;
 
-/** Utility class for working with futures. */
+/**
+ * Utility class for working with futures.
+ */
 public class MoreFutures {
 
   private MoreFutures() {}
 
   /**
    * Waits for the first one of the following to occur:
-   *
    * <ul>
-   *   <li>All of the given futures complete successfully.
-   *   <li>One of the given futures has an {@link ExecutionException}. This {@link
-   *       ExecutionException} is propagated. (N.B. If multiple futures have {@link
-   *       ExecutionExceptions}s, one will be selected non-deterministically.)
-   *   <li>The calling thread is interrupted. The {@link InterruptedException} is propagated.
+   * <li>All of the given futures complete successfully.
+   * <li>One of the given futures has an {@link ExecutionException}. This {@link ExecutionException}
+   *   is propagated. (N.B. If multiple futures have {@link ExecutionExceptions}s, one will be
+   *   selected non-deterministically.)
+   * <li>The calling thread is interrupted. The {@link InterruptedException} is propagated.
    * </ul>
    */
   public static <V> void waitForAllInterruptiblyFailFast(
@@ -55,6 +55,8 @@ public class MoreFutures {
           future.get(1, TimeUnit.MILLISECONDS);
         } catch (TimeoutException te) {
           continue;
+        } catch (ExecutionException ee) {
+          throw ee;
         }
         numCompletedFutures++;
       }
@@ -65,11 +67,11 @@ public class MoreFutures {
   }
 
   /**
-   * Creates a new {@code ListenableFuture} whose value is a list containing the values of all its
-   * input futures, if all succeed. If any input fails, the returned future fails. If any of the
-   * futures fails, it cancels all the other futures.
+   * Creates a new {@code ListenableFuture} whose value is a list containing the
+   * values of all its input futures, if all succeed. If any input fails, the
+   * returned future fails. If any of the futures fails, it cancels all the other futures.
    *
-   * <p>This method is similar to {@code Futures.allAsList} but additionally it cancels all the
+   * <p> This method is similar to {@code Futures.allAsList} but additionally it cancels all the
    * futures in case any of them fails.
    */
   public static <V> ListenableFuture<List<V>> allAsListOrCancelAll(
@@ -94,41 +96,5 @@ public class MoreFutures {
         },
         directExecutor());
     return combinedFuture;
-  }
-
-  /**
-   * Returns the result of {@code future}. If it threw an {@link InterruptedException} (wrapped in
-   * an {@link ExecutionException}), throws that underlying {@link InterruptedException}. Crashes on
-   * all other exceptions.
-   */
-  public static <R> R waitForFutureAndGet(Future<R> future) throws InterruptedException {
-    try {
-      return future.get();
-    } catch (ExecutionException e) {
-      Throwables.propagateIfPossible(e.getCause(), InterruptedException.class);
-      throw new IllegalStateException(e);
-    }
-  }
-
-  public static <R, E extends Exception> R waitForFutureAndGetWithCheckedException(
-      Future<R> future, Class<E> exceptionClass) throws E, InterruptedException {
-    return waitForFutureAndGetWithCheckedException(future, exceptionClass, null);
-  }
-
-  public static <R, E1 extends Exception, E2 extends Exception>
-      R waitForFutureAndGetWithCheckedException(
-          Future<R> future, Class<E1> exceptionClass1, @Nullable Class<E2> exceptionClass2)
-          throws E1, E2, InterruptedException {
-    try {
-      return future.get();
-    } catch (ExecutionException e) {
-      if (exceptionClass2 == null) {
-        Throwables.propagateIfPossible(e.getCause(), exceptionClass1);
-      } else {
-        Throwables.propagateIfPossible(e.getCause(), exceptionClass1, exceptionClass2);
-      }
-      Throwables.throwIfInstanceOf(e.getCause(), InterruptedException.class);
-      throw new IllegalStateException(e);
-    }
   }
 }
