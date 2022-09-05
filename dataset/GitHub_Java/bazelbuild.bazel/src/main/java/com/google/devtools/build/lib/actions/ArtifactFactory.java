780606d81155215c6509e7084865d8028f2bc703
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:33:52 +0200

    Remove incorrect preconditions checks when resolving artifacts.
        --
        MOS_MIGRATED_REVID=100501490

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/ArtifactFactory.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/actions/ArtifactFactory.java
@@ -21,8 +21,8 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Lists;
 import com.google.devtools.build.lib.actions.Artifact.SpecialArtifactType;
-import com.google.devtools.build.lib.cmdline.PackageIdentifier;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
+import com.google.devtools.build.lib.packages.PackageIdentifier;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 
@@ -284,10 +284,10 @@ public class ArtifactFactory implements ArtifactResolver, ArtifactSerializer, Ar
   }
 
   /**
-   * Returns an {@link Artifact} with exec path formed by composing {@code baseExecPath} and
-   * {@code relativePath} (via {@code baseExecPath.getRelative(relativePath)} if baseExecPath is
+   * Returns an {@link Artifact} with exec path formed by composing {@param baseExecPath} and
+   * {@param relativePath} (via {@code baseExecPath.getRelative(relativePath)} if baseExecPath is
    * not null). That Artifact will have root determined by the package roots of this factory if it
-   * lives in a subpackage distinct from that of baseExecPath, and {@code baseRoot} otherwise.
+   * lives in a subpackage distinct from that of baseExecPath, and {@param baseRoot} otherwise.
    */
   public synchronized Artifact resolveSourceArtifactWithAncestor(
       PathFragment relativePath, PathFragment baseExecPath, Root baseRoot) {
