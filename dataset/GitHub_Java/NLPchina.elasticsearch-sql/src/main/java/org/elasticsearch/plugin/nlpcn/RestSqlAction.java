a7b14fbe7e25075e8bcbf2ffc1ba2aaedd16de6d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:00:59 +0200

    fix explain

--- dataset/GitHub_Java/NLPchina.elasticsearch-sql/src/main/java/org/elasticsearch/plugin/nlpcn/RestSqlAction.java
+++ dataset/GitHub_Java/NLPchina.elasticsearch-sql/src/main/java/org/elasticsearch/plugin/nlpcn/RestSqlAction.java
@@ -1,69 +1,98 @@
 package org.elasticsearch.plugin.nlpcn;
 
-import org.elasticsearch.action.count.CountResponse;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchRequestBuilder;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.plugin.nlpcn.executors.ActionRequestRestExecuterFactory;
+import org.elasticsearch.plugin.nlpcn.executors.RestExecutor;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.BytesRestResponse;
-import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestRequest;
-import org.elasticsearch.rest.RestResponse;
 import org.elasticsearch.rest.RestStatus;
-import org.elasticsearch.rest.action.support.RestBuilderListener;
-import org.elasticsearch.rest.action.support.RestStatusToXContentListener;
-import org.nlpcn.es4sql.domain.Select;
+import org.nlpcn.es4sql.SearchDao;
 import org.nlpcn.es4sql.exception.SqlParseException;
-import org.nlpcn.es4sql.parse.SqlParser;
-import org.nlpcn.es4sql.query.AggregationQuery;
-import org.nlpcn.es4sql.query.DefaultQuery;
-import org.nlpcn.es4sql.query.Query;
+import org.nlpcn.es4sql.query.QueryAction;
+
+import java.io.IOException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 
 public class RestSqlAction extends BaseRestHandler {
 
-	@Inject
-	public RestSqlAction(Settings settings, Client client, RestController restController) {
-		super(settings, client);
-		restController.registerHandler(RestRequest.Method.POST, "/_sql/_explain", this);
-		restController.registerHandler(RestRequest.Method.GET, "/_sql/_explain", this);
-		restController.registerHandler(RestRequest.Method.POST, "/_sql", this);
-		restController.registerHandler(RestRequest.Method.GET, "/_sql", this);
-	}
+    private static final Logger LOGGER = LogManager.getLogger();
 
-	@Override
-	protected void handleRequest(RestRequest request, RestChannel channel, final Client client) throws Exception {
+    public RestSqlAction(RestController restController) {
+        restController.registerHandler(RestRequest.Method.POST, "/_nlpcn/sql/explain", this);
+        restController.registerHandler(RestRequest.Method.GET, "/_nlpcn/sql/explain", this);
+        restController.registerHandler(RestRequest.Method.POST, "/_nlpcn/sql", this);
+        restController.registerHandler(RestRequest.Method.GET, "/_nlpcn/sql", this);
+    }
 
-		final String sql = request.param("sql");
+    @Override
+    public String getName() {
+        return "sql_action";
+    }
 
-		if (request.path().endsWith("/_explain")) {
+    @Override
+    protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) {
+        try (XContentParser parser = request.contentOrSourceParamParser()) {
+            parser.mapStrings().forEach((k, v) -> request.params().putIfAbsent(k, v));
+        } catch (IOException e) {
+            LOGGER.warn("Please use json format params, like: {\"sql\":\"SELECT * FROM test\"}");
+        }
 
-			Select select = new SqlParser().parseSelect(sql);
-			SearchRequestBuilder explan = select2Query(select, client).explan();
-			BytesRestResponse bytesRestResponse = new BytesRestResponse(RestStatus.OK, explan.toString());
-			channel.sendResponse(bytesRestResponse);
-		} else {
-			Select select = new SqlParser().parseSelect(sql);
-			SearchRequest searchRequest = select2Query(select, client).explan().request();
-			searchRequest.listenerThreaded(false);
-			client.search(searchRequest, new RestStatusToXContentListener<SearchResponse>(channel));
-		}
-	}
+        String sql = request.param("sql");
 
-	private Query select2Query(Select select, Client client) throws SqlParseException {
+        if (sql == null) {
+            sql = request.content().utf8ToString();
+        }
+        try {
+            SearchDao searchDao = new SearchDao(client);
+            QueryAction queryAction = null;
 
-		Query query = null;
+            queryAction = searchDao.explain(sql);//zhongshu-comment 语法解析，将sql字符串解析为一个Java查询对象
 
-		if (select.isAgg) {
-			query = new AggregationQuery(client, select);
-		} else {
-			query = new DefaultQuery(client, select);
-		}
-		return query;
-	}
+            // TODO add unit tests to explain. (rest level?)
+            if (request.path().endsWith("/explain")) {
+                final String jsonExplanation = queryAction.explain().explain();
+                return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, XContentType.JSON.mediaType(), jsonExplanation));
+            } else {
+                Map<String, String> params = request.params();
+
+                //zhongshu-comment 生成一个负责用rest方式查询es的对象RestExecutor，返回的实现类是：ElasticDefaultRestExecutor
+                RestExecutor restExecutor = ActionRequestRestExecuterFactory.createExecutor(params.get("format"));
+                final QueryAction finalQueryAction = queryAction;
+                //doing this hack because elasticsearch throws exception for un-consumed props
+                Map<String, String> additionalParams = new HashMap<>();
+                for (String paramName : responseParams()) {
+                    if (request.hasParam(paramName)) {
+                        additionalParams.put(paramName, request.param(paramName));
+                    }
+                }
+                //zhongshu-comment restExecutor.execute()方法里会调用es查询的相关rest api
+                //zhongshu-comment restExecutor.execute()方法的第1、4个参数是框架传进来的参数，第2、3个参数是可以自己生成的参数，所以要多注重一点
+                //zhongshu-comment 默认调用的是ElasticDefaultRestExecutor这个子类
+                //todo 这是什么语法：搜索java8 -> lambda表达式：https://blog.csdn.net/ioriogami/article/details/12782141
+                return channel -> restExecutor.execute(client, additionalParams, finalQueryAction, channel);
+            }
+        } catch (SqlParseException | SQLFeatureNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected Set<String> responseParams() {
+        Set<String> responseParams = new HashSet<>(super.responseParams());
+        responseParams.addAll(Arrays.asList("sql", "flat", "separator", "_score", "_type", "_id", "_scroll_id", "newLine", "format", "showHeader"));
+        return responseParams;
+    }
 }
