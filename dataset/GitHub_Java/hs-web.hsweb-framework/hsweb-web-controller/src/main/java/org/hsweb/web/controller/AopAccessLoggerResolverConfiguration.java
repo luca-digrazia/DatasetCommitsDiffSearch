a2133d4c30b30bc8f28461ff6e78e88fd0fcbdf9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:02:36 +0200

    ä¼åå¨æèæ¬

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-controller/src/main/java/org/hsweb/web/controller/AopAccessLoggerResolverConfiguration.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-controller/src/main/java/org/hsweb/web/controller/AopAccessLoggerResolverConfiguration.java
@@ -5,17 +5,20 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.hsweb.web.bean.po.logger.LoggerInfo;
 import org.hsweb.web.bean.po.user.User;
-import org.hsweb.web.exception.BusinessException;
-import org.hsweb.web.logger.AccessLoggerPersisting;
-import org.hsweb.web.logger.AopAccessLoggerResolver;
-import org.hsweb.web.message.ResponseMessage;
-import org.hsweb.web.utils.WebUtil;
+import org.hsweb.web.core.exception.BusinessException;
+import org.hsweb.web.core.logger.AccessLoggerPersisting;
+import org.hsweb.web.core.logger.AopAccessLoggerResolver;
+import org.hsweb.web.core.message.FastJsonHttpMessageConverter;
+import org.hsweb.web.core.message.ResponseMessage;
+import org.hsweb.web.core.utils.WebUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.webbuilder.utils.common.StringUtils;
+import org.hsweb.commons.StringUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 /**
@@ -24,15 +27,22 @@ import java.util.List;
 @Aspect
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@ConditionalOnProperty(prefix = "logger.access", value = "on")
 public class AopAccessLoggerResolverConfiguration extends AopAccessLoggerResolver {
 
-    @Autowired
+    @Autowired(required = false)
     private FastJsonHttpMessageConverter fastJsonHttpMessageConverter;
 
     @Autowired(required = false)
     private List<AccessLoggerPersisting> accessLoggerPersisting;
 
-    @Around(value = "execution(* org.hsweb.web.controller..*Controller..*(..))")
+    @PostConstruct
+    private void init() {
+        if (fastJsonHttpMessageConverter == null)
+            fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+    }
+
+    @Around(value = "execution(* org.hsweb.web..controller..*Controller..*(..))||@annotation(org.hsweb.web.core.logger.annotation.AccessLogger)")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
         LoggerInfo loggerInfo = resolver(pjp);
         long requestTime = System.currentTimeMillis();
@@ -40,20 +50,23 @@ public class AopAccessLoggerResolverConfiguration extends AopAccessLoggerResolve
         try {
             result = pjp.proceed();
         } catch (Throwable e) {
-            result = new ResponseMessage(false, e);
-            if (!(e instanceof BusinessException))
-                loggerInfo.setException_info(StringUtils.throwable2String(e));
+            if (!(e instanceof BusinessException)) {
+                result = ResponseMessage.error(e.getMessage());
+                loggerInfo.setExceptionInfo(StringUtils.throwable2String(e));
+            } else {
+                result = ResponseMessage.error(e.getMessage(), ((BusinessException) e).getStatus());
+            }
             throw e;
         } finally {
             long responseTime = System.currentTimeMillis();
             User user = WebUtil.getLoginUser();
-            loggerInfo.setRequest_time(requestTime);
-            loggerInfo.setResponse_time(responseTime);
-            loggerInfo.setResponse_content(fastJsonHttpMessageConverter.converter(result));
+            loggerInfo.setRequestTime(requestTime);
+            loggerInfo.setResponseTime(responseTime);
+            loggerInfo.setResponseContent(fastJsonHttpMessageConverter.converter(result));
             if (user != null)
-                loggerInfo.setUser_id(user.getU_id());
+                loggerInfo.setUserId(user.getId());
             if (result instanceof ResponseMessage)
-                loggerInfo.setResponse_code(((ResponseMessage) result).getCode());
+                loggerInfo.setResponseCode(String.valueOf(((ResponseMessage) result).getCode()));
             if (accessLoggerPersisting != null) {
                 accessLoggerPersisting.forEach(loggerPersisting -> loggerPersisting.save(loggerInfo));
             }
