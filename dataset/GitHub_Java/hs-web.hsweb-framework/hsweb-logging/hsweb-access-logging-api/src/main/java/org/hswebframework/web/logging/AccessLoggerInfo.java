61f7314cbf96d7e256aef43eb173b37a66b2a432
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:48:58 +0200

    ä¼åè®¿é®æ¥å¿åæ°åºååé»è¾

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-logging/hsweb-access-logging-api/src/main/java/org/hswebframework/web/logging/AccessLoggerInfo.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-logging/hsweb-access-logging-api/src/main/java/org/hswebframework/web/logging/AccessLoggerInfo.java
@@ -4,6 +4,7 @@ import java.io.PrintWriter;
 import java.io.Serializable;
 import java.io.StringWriter;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.StringJoiner;
@@ -197,10 +198,7 @@ public class AccessLoggerInfo {
     }
 
     public Map<String, Object> toSimpleMap(Function<Object, Serializable> noSerialExchange) {
-        return toSimpleMap(noSerialExchange, new LinkedHashMap<>());
-    }
-
-    public Map<String, Object> toSimpleMap(Function<Object, Serializable> objectFilter, Map<String, Object> map) {
+        Map<String, Object> map = new HashMap<>(16);
         map.put("action", action);
         map.put("describe", describe);
         if (method != null) {
@@ -217,7 +215,7 @@ public class AccessLoggerInfo {
         Map<String, Object> newParameter = new LinkedHashMap<>(parameters);
         newParameter.entrySet().forEach(entry -> {
             if (entry.getValue() != null) {
-                entry.setValue(objectFilter.apply(entry.getValue()));
+                entry.setValue(noSerialExchange.apply(entry.getValue()));
             }
         });
 
@@ -226,7 +224,11 @@ public class AccessLoggerInfo {
         map.put("httpMethod", httpMethod);
         map.put("ip", ip);
         map.put("url", url);
-        map.put("response", objectFilter.apply(response));
+        if (response instanceof Serializable) {
+            map.put("response", response);
+        } else {
+            map.put("response", noSerialExchange.apply(response));
+        }
         map.put("requestTime", requestTime);
         map.put("responseTime", responseTime);
         map.put("useTime", responseTime - requestTime);
@@ -235,6 +237,8 @@ public class AccessLoggerInfo {
             exception.printStackTrace(new PrintWriter(writer));
             map.put("exception", writer.toString());
         }
+
+
         return map;
     }
 }
