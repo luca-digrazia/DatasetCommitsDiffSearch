7b06db68b02db6a787b09d3f3b1bd277955ecd12
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:02:01 +0200

    ä¼åBusinessException,å¢å ååºç 

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-controller/src/main/java/org/hsweb/web/controller/RestControllerExceptionTranslator.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-controller/src/main/java/org/hsweb/web/controller/RestControllerExceptionTranslator.java
@@ -3,8 +3,6 @@ package org.hsweb.web.controller;
 import com.alibaba.fastjson.JSON;
 import org.hsweb.web.core.exception.*;
 import org.hsweb.web.core.message.ResponseMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 @Order(1)
 public class RestControllerExceptionTranslator {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
@@ -25,10 +21,10 @@ public class RestControllerExceptionTranslator {
         return ResponseMessage.error(exception.getMessage(), 400);
     }
 
-    @ExceptionHandler(org.hsweb.ezorm.rdb.exception.ValidationException.class)
+    @ExceptionHandler(org.hsweb.ezorm.exception.ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    ResponseMessage handleException(org.hsweb.ezorm.rdb.exception.ValidationException exception) {
+    ResponseMessage handleException(org.hsweb.ezorm.exception.ValidationException exception) {
         return ResponseMessage.error(JSON.toJSONString(exception.getValidateResult()), 400);
     }
 
@@ -37,11 +33,9 @@ public class RestControllerExceptionTranslator {
     @ResponseBody
     ResponseMessage handleException(BusinessException exception, HttpServletResponse response) {
         response.setStatus(exception.getStatus());
-        if (exception.getCause() != null) {
-            logger.error("{}:{}", exception.getMessage(), exception.getStatus(), exception.getCause());
-        }
         return ResponseMessage.error(exception.getMessage(), exception.getStatus());
     }
+
 
     @ExceptionHandler(AuthorizeException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
@@ -69,7 +63,6 @@ public class RestControllerExceptionTranslator {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     ResponseMessage handleException(Throwable exception) {
-        logger.error("未知错误", exception);
         return ResponseMessage.error(exception.getMessage(), 500);
     }
 
