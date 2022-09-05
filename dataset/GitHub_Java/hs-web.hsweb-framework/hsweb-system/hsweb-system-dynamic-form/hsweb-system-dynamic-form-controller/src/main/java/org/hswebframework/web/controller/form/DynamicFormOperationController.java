aa33efe28acacbea339dbf6ad90eb0a99007ad4f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:47:31 +0200

    å¢å å¨æè¡¨åæä½æ¹æ³

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-dynamic-form/hsweb-system-dynamic-form-controller/src/main/java/org/hswebframework/web/controller/form/DynamicFormOperationController.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-dynamic-form/hsweb-system-dynamic-form-controller/src/main/java/org/hswebframework/web/controller/form/DynamicFormOperationController.java
@@ -1,14 +1,11 @@
 package org.hswebframework.web.controller.form;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.hswebframework.web.authorization.Permission;
-import org.hswebframework.web.authorization.annotation.Authorize;
 import org.hswebframework.web.commons.entity.PagerResult;
 import org.hswebframework.web.commons.entity.param.DeleteParamEntity;
 import org.hswebframework.web.commons.entity.param.QueryParamEntity;
 import org.hswebframework.web.commons.entity.param.UpdateParamEntity;
 import org.hswebframework.web.controller.message.ResponseMessage;
+import org.hswebframework.web.logging.AccessLogger;
 import org.hswebframework.web.service.form.DynamicFormOperationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +19,8 @@ import java.util.Map;
  * @since 3.0
  */
 @RestController
-@Api(value = "动态表单操作", tags = "动态表单-数据操作")
+@AccessLogger("动态表单操作")
 @RequestMapping("/dynamic/form/operation")
-@Authorize(permission = "dynamic-form-operation", description = "动态表单数据操作")
 public class DynamicFormOperationController {
 
     private DynamicFormOperationService dynamicFormOperationService;
@@ -35,57 +31,28 @@ public class DynamicFormOperationController {
     }
 
     @GetMapping("/{formId}")
-    @ApiOperation("动态查询")
-    @Authorize(action = Permission.ACTION_QUERY)
+    @AccessLogger("查询")
+//    @Authorize(action = Permission.ACTION_QUERY)
     public ResponseMessage<PagerResult<Object>> selectPager(@PathVariable String formId, QueryParamEntity paramEntity) {
         return ResponseMessage.ok(dynamicFormOperationService.selectPager(formId, paramEntity));
     }
 
-    @GetMapping("/{formId}/single")
-    @ApiOperation("动态查询返回单条数据")
-    @Authorize(action = Permission.ACTION_QUERY)
-    public ResponseMessage<Object> selectSingle(@PathVariable String formId, QueryParamEntity paramEntity) {
-        return ResponseMessage.ok(dynamicFormOperationService.selectSingle(formId, paramEntity));
-    }
-
-    @GetMapping("/{formId}/count")
-    @ApiOperation("动态查询返回数据条数")
-    @Authorize(action = Permission.ACTION_QUERY)
-    public ResponseMessage<Object> selectCount(@PathVariable String formId, QueryParamEntity paramEntity) {
-        return ResponseMessage.ok(dynamicFormOperationService.count(formId, paramEntity));
-    }
-
     @PostMapping("/{formId}")
-    @ApiOperation("新增")
-    @Authorize(action = Permission.ACTION_ADD)
-    public ResponseMessage<Map<String, Object>> add(@PathVariable String formId,
-                                                    @RequestBody Map<String, Object> data) {
+    @AccessLogger("新增")
+    public ResponseMessage<Map<String, Object>> add(@PathVariable String formId, @RequestBody Map<String, Object> data) {
         dynamicFormOperationService.insert(formId, data);
         return ResponseMessage.ok(data);
     }
 
     @PutMapping("/{formId}")
-    @ApiOperation("动态修改")
-    @Authorize(action = Permission.ACTION_UPDATE)
-    public ResponseMessage<Integer> update(@PathVariable String formId,
-                                           @RequestBody UpdateParamEntity<Map<String, Object>> paramEntity) {
+    @AccessLogger("修改")
+    public ResponseMessage<Integer> update(@PathVariable String formId, @RequestBody UpdateParamEntity<Map<String, Object>> paramEntity) {
         return ResponseMessage.ok(dynamicFormOperationService.update(formId, paramEntity));
     }
 
-    @PutMapping("/{formId}/{id}")
-    @ApiOperation("根据主键修改")
-    @Authorize(action = Permission.ACTION_UPDATE)
-    public ResponseMessage<Map<String, Object>> update(@PathVariable String formId,
-                                                       @PathVariable String id,
-                                                       @RequestBody Map<String, Object> param) {
-        return ResponseMessage.ok(dynamicFormOperationService.updateById(formId, id, param));
-    }
-
-    @DeleteMapping("/{formId}/{id}")
-    @ApiOperation("根据主键删除")
-    @Authorize(action = Permission.ACTION_DELETE)
-    public ResponseMessage<Integer> delete(@PathVariable String formId,
-                                           @PathVariable String id) {
-        return ResponseMessage.ok(dynamicFormOperationService.deleteById(formId, id));
+    @DeleteMapping("/{formId}")
+    @AccessLogger("删除")
+    public ResponseMessage<Integer> delete(@PathVariable String formId, DeleteParamEntity paramEntity) {
+        return ResponseMessage.ok(dynamicFormOperationService.delete(formId, paramEntity));
     }
 }
