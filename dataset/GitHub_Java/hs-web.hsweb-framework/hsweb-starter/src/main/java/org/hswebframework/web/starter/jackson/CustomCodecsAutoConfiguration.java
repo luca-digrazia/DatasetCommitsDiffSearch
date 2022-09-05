f965cb7fa6204966a005f4906a58dbcef8221788
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:12:35 +0200

    ä¼åæä¸¾å­å¸ååºåå

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-starter/src/main/java/org/hswebframework/web/starter/jackson/CustomCodecsAutoConfiguration.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-starter/src/main/java/org/hswebframework/web/starter/jackson/CustomCodecsAutoConfiguration.java
@@ -1,7 +1,12 @@
 package org.hswebframework.web.starter.jackson;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.std.EnumDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleDeserializers;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.type.ClassKey;
 import org.hswebframework.web.api.crud.entity.EntityFactory;
+import org.hswebframework.web.dict.EnumDict;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -16,22 +21,40 @@ import org.springframework.http.codec.CodecConfigurer;
 @AutoConfigureAfter(JacksonAutoConfiguration.class)
 public class CustomCodecsAutoConfiguration {
 
-	@Configuration(proxyBeanMethods = false)
-	@ConditionalOnClass(ObjectMapper.class)
-	static class JacksonDecoderConfiguration {
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass(ObjectMapper.class)
+    static class JacksonDecoderConfiguration {
 
-		@Bean
-		@Order(1)
-		@ConditionalOnBean(ObjectMapper.class)
-		CodecCustomizer jacksonDecoderCustomizer(EntityFactory entityFactory, ObjectMapper objectMapper) {
-			objectMapper.setTypeFactory(new CustomTypeFactory(entityFactory));
-			return (configurer) -> {
-				CodecConfigurer.DefaultCodecs defaults = configurer.defaultCodecs();
-				defaults.jackson2JsonDecoder(new CustomJackson2JsonDecoder(objectMapper));
-			};
-		}
+        @Bean
+        @Order(1)
+        @ConditionalOnBean(ObjectMapper.class)
+        @SuppressWarnings("all")
+        CodecCustomizer jacksonDecoderCustomizer(EntityFactory entityFactory, ObjectMapper objectMapper) {
+            //	objectMapper.setTypeFactory(new CustomTypeFactory(entityFactory));
+            SimpleModule module = new SimpleModule();
+            module.setDeserializers(new SimpleDeserializers() {
+                @Override
+                public JsonDeserializer<?> findEnumDeserializer(Class<?> type,
+                                                                DeserializationConfig config,
+                                                                BeanDescription beanDesc) {
+                    JsonDeserializer<?> deser = null;
+                    if (type.isEnum() && EnumDict.class.isAssignableFrom(type)) {
+                        deser = new EnumDict.EnumDictJSONDeserializer(val -> EnumDict
+                                .find((Class) type, val)
+                                .orElse(null));
+                    }
+                    return deser;
+                }
+            });
+            objectMapper.registerModule(module);
 
-	}
+            return (configurer) -> {
+                CodecConfigurer.DefaultCodecs defaults = configurer.defaultCodecs();
+                defaults.jackson2JsonDecoder(new CustomJackson2JsonDecoder(entityFactory, objectMapper));
+            };
+        }
+
+    }
 
 
 }
