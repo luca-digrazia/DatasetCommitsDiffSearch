4c350137ba48c045400dd70e44cdb131f96ad905
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:49:29 +0200

    Adds manifest_values attribute to android_binary as a preliminary for removing application_id, version_code and version_name attributes and switching to the Gradle manifest merger.
        --
        MOS_MIGRATED_REVID=120335565

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidBinaryOnlyRule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidBinaryOnlyRule.java
@@ -35,6 +35,40 @@ public final class AndroidBinaryOnlyRule implements RuleDefinition {
   @Override
   public RuleClass build(RuleClass.Builder builder, final RuleDefinitionEnvironment env) {
     return builder
+//       /* <!-- #BLAZE_RULE(android_binary).ATTRIBUTE(application_id) -->
+//       A full Java-language-style package name for the application. The name should be unique.
+//       The name may contain uppercase or lowercase letters ('A' through 'Z'), numbers, and
+//       underscores ('_'). However, individual package name parts may only start with letters.
+//       The package name serves as a unique identifier for the application. It's also the default
+//       name for the application process (see the &lt;application&gt; element's process attribute)
+//       and the default task affinity of an activity.
+//
+//       This overrides the value declared in the manifest.
+//       <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
+        .add(attr("application_id", STRING).undocumented("not ready for production use"))
+//       /* <!-- #BLAZE_RULE(android_binary).ATTRIBUTE(version_code) -->
+//       An internal version number. This number is used only to determine whether one version is
+//       more recent than another, with higher numbers indicating more recent versions. This is not
+//       the version number shown to users; that number is set by the version_name attribute.
+//       The value must be set as an integer, such as "100". Each successive version must have a
+//       higher number.
+//       This overrides the value declared in the manifest.
+//
+//       Subject to <a href="make-variables.html">"Make" variable</a> substitution.
+//       Suggested practice is to declare a varrdef and reference it here so that a particular build
+//       invocation will be used to generate apks for release.
+//       <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
+        .add(attr("version_code", STRING).undocumented("not ready for production use"))
+//       /* <!-- #BLAZE_RULE(android_binary).ATTRIBUTE(version_name) -->
+//       The version number shown to users. The string has no other purpose than to be displayed to
+//       users. The version_code attribute holds the significant version number used internally.
+//       This overrides the value declared in the manifest.
+//
+//       Subject to <a href="make-variables.html">"Make" variable</a> substitution.
+//       Suggested practice is to declare a varrdef and reference it here so that a particular build
+//       invocation will be used to generate apks for release.
+//       <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
+        .add(attr("version_name", STRING).undocumented("not ready for production use"))
 //        /* <!-- #BLAZE_RULE(android_binary).ATTRIBUTE(manifest_values) -->
 //        A dictionary of values to be overridden in the manifest. Any instance of ${name} in the
 //        manifest will be replaced with the value corresponding to name in this dictionary.
@@ -72,21 +106,23 @@ public final class AndroidBinaryOnlyRule implements RuleDefinition {
             .value(env.getToolsLabel(AndroidRuleClasses.MANIFEST_MERGE_TOOL_LABEL)))
 
         /* <!-- #BLAZE_RULE(android_binary).ATTRIBUTE(multidex) -->
-        Whether to split code into multiple dex files.<br/>
+        Whether to split code into multiple dex files.
         Possible values:
         <ul>
-          <li><code>native</code>: Split code into multiple dex files when the dex 64K index limit
-            is exceeded. Assumes native platform support for loading multidex classes at runtime.
-            <em class="harmful">This works with only Android L and newer</em>.</li>
-          <li><code>legacy</code>: Split code into multiple dex files when the dex 64K index limit
-            is exceeded. Assumes multidex classes are loaded through application code (i.e. no
-            native platform support).</li>
-          <li><code>manual_main_dex</code>: Split code into multiple dex files when the dex 64K
-            index limit is exceeded. The content of the main dex file needs to be specified by
-            providing a list of classes in a text file using the
-            <a href="${link android_binary.main_dex_list}">main_dex_list</a> attribute.</li>
-          <li><code>off</code>: Compile all code to a single dex file, even if it exceeds the index
-            limit.</li>
+          <li><code>native</code>: Split code into multiple dex files when the
+            dex 64K index limit is exceeded.
+            Assumes native platform support for loading multidex classes at
+            runtime. <em class="harmful">This only works with Android L and
+            newer</em>.</li>
+          <li><code>legacy</code>: Split code into multiple dex files when the
+            dex 64K index limit is exceeded. Assumes multidex classes are
+            loaded through application code (i.e. no platform support).</li>
+          <li><code>manual_main_dex</code>: Split code into multiple dex files when the
+            dex 64K index limit is exceeded. The content of the main dex file
+            needs to be specified by providing a list of classes in a text file
+            using the <a href="#android_binary.main_dex_list">main_dex_list</a>.</li>
+          <li><code>off</code>: Compile all code to a single dex file, even if
+            if exceeds the index limit.</li>
         </ul>
         <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
         .add(attr("multidex", STRING)
