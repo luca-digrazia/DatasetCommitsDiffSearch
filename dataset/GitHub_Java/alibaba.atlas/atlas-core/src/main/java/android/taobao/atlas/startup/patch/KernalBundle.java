d275cf1d2366a35a9fdec7064a0b238f831b24da
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 09:07:23 +0200

    [atlas-core]dp3çelementå¼å®¹ è¡¥æäº¤

--- dataset/GitHub_Java/alibaba.atlas/atlas-core/src/main/java/android/taobao/atlas/startup/patch/KernalBundle.java
+++ dataset/GitHub_Java/alibaba.atlas/atlas-core/src/main/java/android/taobao/atlas/startup/patch/KernalBundle.java
@@ -276,7 +276,7 @@ public class KernalBundle{
                 kernalBundle.patchKernalDex(application);
                 kernalBundle.patchKernalResource(application);
                 return true;
-            } catch (Exception e) {
+            } catch (Throwable e) {
                 e.printStackTrace();
                 kernalBundle = null;
                 deleteDirectory(kernalUpdateDir);
@@ -308,10 +308,8 @@ public class KernalBundle{
                     DexFile dexFile = (DexFile)KernalConstants.dexBooster.loadDex(KernalConstants.baseContext,KernalConstants.baseContext.getApplicationInfo().sourceDir,
                                                                                   new File(patchFile.getParent(),"base.dex").getAbsolutePath(),0,true) ;
 
-                    File internalDebugBundleDir = new File(new File(application.getFilesDir(), "debug_storage"), KERNAL_BUNDLE_NAME);
-                    internalDebugBundleDir.mkdirs();
                     DexFile patchDexFile = (DexFile)KernalConstants.dexBooster.loadDex(KernalConstants.baseContext,patchFile.getAbsolutePath(),
-                            new File(internalDebugBundleDir,"patch.dex").getAbsolutePath(),0,true) ;
+                            new File(patchFile.getParent(),"patch.dex").getAbsolutePath(),0,true) ;
                     bundle.installKernalBundle(KernalConstants.baseContext.getClassLoader(), patchFile, new DexFile[]{patchDexFile,dexFile}, null,
                                                true /*(app_info.flags & ApplicationInfo.FLAG_VM_SAFE_MODE) != 0*/);
                     if(bundle.needReplaceClassLoader(application)){
@@ -555,7 +553,7 @@ public class KernalBundle{
     }
 
     private static boolean replaceElement(Object instance,String fieldName,Object replaceElement) throws NoSuchFieldException,IllegalArgumentException,
-             IllegalAccessException{
+            IllegalAccessException{
         boolean replaceSuccess = false;
         Field jlrField = findField(instance, fieldName);
         Object[] original = (Object[]) jlrField.get(instance);
@@ -664,23 +662,31 @@ public class KernalBundle{
     static Class[] constructorArgs2 = {File.class, File.class, DexFile.class};
     static Class[] constructorArgs1 = {File.class, boolean.class, File.class, DexFile.class};
 
+    static Class[] constructorArgsO = {DexFile.class,File.class};
+
+
     public static Object[] makeDexElement(File file,DexFile[] dex) throws Exception{
         Object []objects = new Object[dex.length];
         for (int i = 0; i < dex.length; i ++) {
             try {
                 Class Element = Class.forName("dalvik.system.DexPathList$Element");
-                Constructor cons = getElementConstructor(Element, constructorArgs1);
-                File apkFile = new File(KernalConstants.baseContext.getApplicationInfo().sourceDir);
-                if (cons != null) {
-                    objects[i] = cons.newInstance(apkFile, false, apkFile, dex[i]);
-                } else {
-                    cons = getElementConstructor(Element, constructorArgs2);
+                if(Build.VERSION.SDK_INT>25 || Build.VERSION.SDK_INT==25&&Build.VERSION.PREVIEW_SDK_INT>0){
+                    Constructor cons = getElementConstructor(Element, constructorArgsO);
+                    objects[i] = cons.newInstance(dex[i],file);
+                }else {
+                    File apkFile = new File(KernalConstants.baseContext.getApplicationInfo().sourceDir);
+                    Constructor cons = getElementConstructor(Element, constructorArgs1);
                     if (cons != null) {
-                        objects[i] =  cons.newInstance(apkFile, apkFile, dex[i]);
+                        objects[i] = cons.newInstance(apkFile, false, apkFile, dex[i]);
                     } else {
-                        cons = getElementConstructor(Element, constructorArgs3);
+                        cons = getElementConstructor(Element, constructorArgs2);
                         if (cons != null) {
-                            objects[i] = cons.newInstance(apkFile, null, dex[i]);
+                            objects[i] = cons.newInstance(apkFile, apkFile, dex[i]);
+                        } else {
+                            cons = getElementConstructor(Element, constructorArgs3);
+                            if (cons != null) {
+                                objects[i] = cons.newInstance(apkFile, null, dex[i]);
+                            }
                         }
                     }
                 }
