37214b9158ab9ba03c269fe71ea2ae742824fa83
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:23:45 +0200

    ä¼åå­å¸

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-dictionary/hsweb-system-dictionary-local/src/main/java/org/hswebframework/web/dictionary/simple/BoostDictDefineRepository.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-dictionary/hsweb-system-dictionary-local/src/main/java/org/hswebframework/web/dictionary/simple/BoostDictDefineRepository.java
@@ -1,19 +1,28 @@
 package org.hswebframework.web.dictionary.simple;
 
-import org.hswebframework.web.dict.ClassDictDefine;
+import lombok.extern.slf4j.Slf4j;
+import org.hswebframework.web.commons.entity.DataStatus;
+import org.hswebframework.web.commons.entity.param.QueryParamEntity;
 import org.hswebframework.web.dict.DictDefine;
-import org.hswebframework.web.dict.ItemDefine;
+import org.hswebframework.web.dict.EnumDict;
 import org.hswebframework.web.dict.defaults.DefaultDictDefine;
 import org.hswebframework.web.dict.defaults.DefaultDictDefineRepository;
-import org.hswebframework.web.dict.defaults.DefaultItemDefine;
+import org.hswebframework.web.dictionary.api.DictionaryItemService;
 import org.hswebframework.web.dictionary.api.DictionaryService;
 import org.hswebframework.web.dictionary.api.entity.DictionaryEntity;
+import org.hswebframework.web.dictionary.api.entity.DictionaryItemEntity;
+import org.hswebframework.web.dictionary.api.events.ClearDictionaryCacheEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionalEventListener;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -22,26 +31,32 @@ import java.util.stream.Collectors;
  */
 @Service
 @CacheConfig(cacheNames = "dictionary")
+@Slf4j
+@SuppressWarnings("all")
 public class BoostDictDefineRepository extends DefaultDictDefineRepository {
 
     @Autowired
     private DictionaryService dictionaryService;
 
+    @Autowired
+    private DictionaryItemService itemService;
+
+    @TransactionalEventListener
+    @CacheEvict(key = "'dictDefineById:'+#event.dictionaryId", condition = "#event!=null")
+    public void clearCache(ClearDictionaryCacheEvent event) {
+        log.info("clear dictionary define cache:{}", event.getDictionaryId());
+    }
+
     @Override
-    @Cacheable(key = "'DictDefineById:'+#id")
+    @Cacheable(key = "'dictDefineById:'+#id")
     public DictDefine getDefine(String id) {
         DictionaryEntity entity = dictionaryService.selectByPk(id);
         if (entity == null) {
             return super.getDefine(id);
         }
-        List<ItemDefine> items = entity.getItems()
+        List<EnumDict<Object>> items = (List) itemService.selectByDictId(id)
                 .stream()
-                .map(item -> DefaultItemDefine
-                        .builder()
-                        .value(item.getValue())
-                        .text(item.getText())
-                        .comments(item.getDescribe())
-                        .build())
+                .filter(e -> DataStatus.STATUS_ENABLED.equals(e.getStatus()))
                 .collect(Collectors.toList());
 
         return DefaultDictDefine.builder()
@@ -52,8 +67,28 @@ public class BoostDictDefineRepository extends DefaultDictDefineRepository {
     }
 
     @Override
-    @Cacheable(key = "'DictDefineByClass:'+#type.name")
-    public List<ClassDictDefine> getDefine(Class type) {
-        return super.getDefine(type);
+    public List<DictDefine> getAllDefine() {
+        //查询所有的字典项并按字典ID分组
+        Map<String, List<DictionaryItemEntity>> items = QueryParamEntity.newQuery()
+                .where(DictionaryItemEntity::getStatus, DataStatus.STATUS_ENABLED)
+                .noPaging()
+                .execute(itemService::select)
+                .stream().collect(Collectors.groupingBy(DictionaryItemEntity::getDictId));
+
+        //转换为字段
+        List<DictDefine> all =QueryParamEntity.newQuery()
+                .where(DictionaryEntity::getStatus,DataStatus.STATUS_ENABLED)
+                .noPaging()
+                .execute(dictionaryService::select)
+                .stream()
+                .map(dict -> DefaultDictDefine.builder()
+                        .id(dict.getId())
+                        .comments(dict.getDescribe())
+                        .items((List) items.getOrDefault(dict.getId(),Collections.emptyList()))
+                        .build())
+                .collect(Collectors.toList());
+        //添加默认的字典
+        all.addAll(super.getAllDefine());
+        return all;
     }
 }
