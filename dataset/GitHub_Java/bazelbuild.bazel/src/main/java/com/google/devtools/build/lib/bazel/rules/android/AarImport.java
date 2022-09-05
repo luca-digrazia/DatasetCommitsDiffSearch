ae6caf9a69b34fe322fff2421a8b9eaf97ad2da7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:38:42 +0200

    Change aar_import rule to directly unzip the resource files in a spawn action.     PopulateTreeArtifactAction is going away.
        --
        MOS_MIGRATED_REVID=136093999

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/android/AarImport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/android/AarImport.java
@@ -22,19 +22,20 @@ import com.google.devtools.build.lib.analysis.RuleConfiguredTarget.Mode;
 import com.google.devtools.build.lib.analysis.RuleConfiguredTargetBuilder;
 import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.analysis.RunfilesProvider;
-import com.google.devtools.build.lib.analysis.actions.PopulateTreeArtifactAction;
+import com.google.devtools.build.lib.analysis.TransitiveInfoCollection;
 import com.google.devtools.build.lib.analysis.actions.SpawnAction;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.rules.RuleConfiguredTargetFactory;
-import com.google.devtools.build.lib.rules.android.AndroidCommon;
 import com.google.devtools.build.lib.rules.android.AndroidResourcesProvider;
+import com.google.devtools.build.lib.rules.android.AndroidRuleClasses;
 import com.google.devtools.build.lib.rules.android.ApplicationManifest;
 import com.google.devtools.build.lib.rules.android.LocalResourceContainer;
 import com.google.devtools.build.lib.rules.android.ResourceApk;
 import com.google.devtools.build.lib.rules.android.ResourceDependencies;
 import com.google.devtools.build.lib.rules.java.JavaCommon;
 import com.google.devtools.build.lib.rules.java.JavaRuleOutputJarsProvider;
+import com.google.devtools.build.lib.rules.java.JavaRuleOutputJarsProvider.OutputJar;
 import com.google.devtools.build.lib.vfs.PathFragment;
 
 /**
@@ -60,23 +61,23 @@ public class AarImport implements RuleConfiguredTargetFactory {
     // classes.jar is required in every AAR.
     Artifact classesJar = createAarArtifact(ruleContext, CLASSES_JAR);
     ruleContext.registerAction(
-        createSingleFileExtractor(ruleContext, aar, CLASSES_JAR, classesJar));
+        createEmbeddedJarExtractorActions(ruleContext, aar, CLASSES_JAR, classesJar));
 
     // AndroidManifest.xml is required in every AAR.
     Artifact androidManifestArtifact = createAarArtifact(ruleContext, ANDROID_MANIFEST);
-    ruleContext.registerAction(
-        createSingleFileExtractor(ruleContext, aar, ANDROID_MANIFEST, androidManifestArtifact));
+    ruleContext.registerAction(createSingleFileExtractorActions(
+        ruleContext, aar, ANDROID_MANIFEST, androidManifestArtifact));
 
     Artifact resourcesManifest = createAarArtifact(ruleContext, "resource_manifest");
     ruleContext.registerAction(
-        createManifestExtractor(ruleContext, aar, "res/.*", resourcesManifest));
+        createManifestExtractorActions(ruleContext, aar, "res/.*", resourcesManifest));
 
     Artifact resources = createResourcesTreeArtifact(ruleContext);
-    ruleContext.registerAction(createTreePopulater(ruleContext, aar, resourcesManifest, resources));
+    ruleContext.registerAction(
+        createManifestFileEntriesExtractorActions(ruleContext, aar, resourcesManifest, resources));
 
     ApplicationManifest androidManifest =
-        ApplicationManifest.fromExplicitManifest(ruleContext, androidManifestArtifact)
-            .renamePackage(ruleContext, AndroidCommon.getJavaPackage(ruleContext));
+        ApplicationManifest.fromExplicitManifest(ruleContext, androidManifestArtifact);
 
     FileProvider resourcesProvider = new FileProvider(
         new NestedSetBuilder<Artifact>(Order.NAIVE_LINK_ORDER).add(resources).build());
@@ -86,52 +87,86 @@ public class AarImport implements RuleConfiguredTargetFactory {
         new LocalResourceContainer.Builder(ruleContext)
             .withResources(ImmutableList.of(resourcesProvider))
             .build(),
-        ResourceDependencies.fromRuleDeps(ruleContext, JavaCommon.isNeverLink(ruleContext)));
+        ResourceDependencies.fromRuleDeps(ruleContext, JavaCommon.isNeverLink(ruleContext)),
+        ruleContext.getImplicitOutputArtifact(AndroidRuleClasses.ANDROID_R_TXT),
+        ruleContext.getImplicitOutputArtifact(AndroidRuleClasses.ANDROID_SYMBOLS_TXT),
+        ruleContext.getImplicitOutputArtifact(AndroidRuleClasses.ANDROID_PROCESSED_MANIFEST),
+        ruleContext.getImplicitOutputArtifact(AndroidRuleClasses.ANDROID_RESOURCES_ZIP));
+
+    NestedSetBuilder<Artifact> filesToBuildBuilder =
+        NestedSetBuilder.<Artifact>stableOrder().add(resources).add(classesJar);
+
+    JavaRuleOutputJarsProvider.Builder jarProviderBuilder = new JavaRuleOutputJarsProvider.Builder()
+        .addOutputJar(classesJar, null, null);
+    for (TransitiveInfoCollection export : ruleContext.getPrerequisites("exports", Mode.TARGET)) {
+      for (OutputJar jar : export.getProvider(JavaRuleOutputJarsProvider.class).getOutputJars()) {
+        jarProviderBuilder.addOutputJar(jar);
+        filesToBuildBuilder.add(jar.getClassJar());
+      }
+    }
 
     return ruleBuilder
-        .setFilesToBuild(
-            NestedSetBuilder
-                .<Artifact>stableOrder()
-                .add(resources)
-                .add(classesJar)
-                .build())
-        .add(RunfilesProvider.class, RunfilesProvider.EMPTY)
-        .add(AndroidResourcesProvider.class, resourceApk.toResourceProvider(ruleContext.getLabel()))
-        .add(
-            JavaRuleOutputJarsProvider.class,
-            new JavaRuleOutputJarsProvider.Builder()
-                .addOutputJar(classesJar, null, null)
-                .build())
+        .setFilesToBuild(filesToBuildBuilder.build())
+        .addProvider(RunfilesProvider.class, RunfilesProvider.EMPTY)
+        .addProvider(
+            AndroidResourcesProvider.class, resourceApk.toResourceProvider(ruleContext.getLabel()))
+        .addProvider(JavaRuleOutputJarsProvider.class, jarProviderBuilder.build())
         .build();
   }
 
-  private static Action[] createSingleFileExtractor(RuleContext ruleContext, Artifact aar,
+  private static Action[] createSingleFileExtractorActions(RuleContext ruleContext, Artifact aar,
       String filename, Artifact outputArtifact) {
     return new SpawnAction.Builder()
-        .setExecutable(ruleContext.getExecutablePrerequisite("$unzip", Mode.HOST))
-        .addArgument("-j")
+        .setExecutable(ruleContext.getExecutablePrerequisite("$zipper", Mode.HOST))
+        .setMnemonic("AarFileExtractor")
+        .setProgressMessage("Extracting " + filename + " from " + aar.getFilename())
+        .addArgument("x")
         .addInputArgument(aar)
-        .addArgument(filename)
         .addArgument("-d")
+        .addOutput(outputArtifact)
+        .addArgument(outputArtifact.getExecPath().getParentDirectory().getPathString())
+        .addArgument(filename)
+        .build(ruleContext);
+  }
+
+  private static Action[] createManifestFileEntriesExtractorActions(RuleContext ruleContext,
+      Artifact aar, Artifact manifest, Artifact outputTree) {
+    return new SpawnAction.Builder()
+        .setExecutable(ruleContext.getExecutablePrerequisite("$zipper", Mode.HOST))
+        .setMnemonic("AarManifestFileEntriesExtractor")
+        .addArgument("x")
+        .addInputArgument(aar)
+        .addArgument("-d")
+        .addOutputArgument(outputTree)
+        .addArgument("@" + manifest.getExecPathString())
+        .addInput(manifest)
+        .build(ruleContext);
+  }
+
+  // Extracts a jar file from the aar if it exists, otherwise outputs an empty jar file.
+  private static Action[] createEmbeddedJarExtractorActions(RuleContext ruleContext, Artifact aar,
+      String filename, Artifact outputArtifact) {
+    return new SpawnAction.Builder()
+        .setExecutable(ruleContext.getExecutablePrerequisite("$embedded_jar_extractor", Mode.HOST))
+        .setMnemonic("AarJarExtractor")
+        .setProgressMessage("Extracting " + filename + " from " + aar.getFilename())
+        .addArgument("--input_archive")
+        .addInputArgument(aar)
+        .addArgument("--filename")
+        .addArgument(filename)
+        .addArgument("--output_dir")
         .addOutput(outputArtifact)
         .addArgument(outputArtifact.getExecPath().getParentDirectory().getPathString())
         .build(ruleContext);
   }
 
-  private static Action createTreePopulater(RuleContext ruleContext, Artifact aar,
-      Artifact manifest, Artifact outputTree) {
-    return new PopulateTreeArtifactAction(
-        ruleContext.getActionOwner(),
-        aar,
-        manifest,
-        outputTree,
-        ruleContext.getExecutablePrerequisite("$zipper", Mode.HOST));
-  }
-
-  private static Action[] createManifestExtractor(RuleContext ruleContext, Artifact aar,
+  private static Action[] createManifestExtractorActions(RuleContext ruleContext, Artifact aar,
       String filenameRegexp, Artifact manifest) {
     return new SpawnAction.Builder()
         .setExecutable(ruleContext.getExecutablePrerequisite("$zip_manifest_creator", Mode.HOST))
+        .setMnemonic("ZipManifestCreator")
+        .setProgressMessage(
+            "Creating manifest for " + aar.getFilename() + " matching " + filenameRegexp)
         .addArgument(filenameRegexp)
         .addInputArgument(aar)
         .addOutputArgument(manifest)
