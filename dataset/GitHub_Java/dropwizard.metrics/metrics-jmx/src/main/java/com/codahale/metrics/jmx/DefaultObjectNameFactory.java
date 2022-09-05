eccc04874dac818f81c44e9a38cbac8df61c7573
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:11:07 +0200

    `JMXReporter` may create invalid ObjectNames (#2031)
        JMX managed ObjectNames cannot contain properties with values that are unquoted if the aforementioned values contain any of comma, equals, colon or double-quote characters. This fixes the default object naming strategy used by JMX reporter.
    
        Note that construction of the `ObjectName` does not fail, but JMX clients break on deserialization.
    
        Fixes #2030
    
        (cherry picked from commit 3ac20d3c1abe23a92abcaf27b37d2f8c826e40d0)

--- dataset/GitHub_Java/dropwizard.metrics/metrics-jmx/src/main/java/com/codahale/metrics/jmx/DefaultObjectNameFactory.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-jmx/src/main/java/com/codahale/metrics/jmx/DefaultObjectNameFactory.java
@@ -1,5 +1,7 @@
 package com.codahale.metrics.jmx;
 
+import java.util.Hashtable;
+
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 
@@ -8,15 +10,34 @@ import org.slf4j.LoggerFactory;
 
 public class DefaultObjectNameFactory implements ObjectNameFactory {
 
+    private static final char[] QUOTABLE_CHARS = new char[] {',', '=', ':', '"'};
     private static final Logger LOGGER = LoggerFactory.getLogger(JmxReporter.class);
 
     @Override
     public ObjectName createName(String type, String domain, String name) {
         try {
-            ObjectName objectName = new ObjectName(domain, "name", name);
-            if (objectName.isPattern()) {
-                objectName = new ObjectName(domain, "name", ObjectName.quote(name));
+            ObjectName objectName;
+            Hashtable<String, String> properties = new Hashtable<>();
+
+            properties.put("name", name);
+            properties.put("type", type);
+            objectName = new ObjectName(domain, properties);
+
+            /*
+             * The only way we can find out if we need to quote the properties is by
+             * checking an ObjectName that we've constructed.
+             */
+            if (objectName.isDomainPattern()) {
+                domain = ObjectName.quote(domain);
             }
+            if (objectName.isPropertyValuePattern("name") || shouldQuote(objectName.getKeyProperty("name"))) {
+                properties.put("name", ObjectName.quote(name));
+            }
+            if (objectName.isPropertyValuePattern("type") || shouldQuote(objectName.getKeyProperty("type"))) {
+                properties.put("type", ObjectName.quote(type));
+            }
+            objectName = new ObjectName(domain, properties);
+
             return objectName;
         } catch (MalformedObjectNameException e) {
             try {
@@ -26,6 +47,23 @@ public class DefaultObjectNameFactory implements ObjectNameFactory {
                 throw new RuntimeException(e1);
             }
         }
+    }
+
+    /**
+     * Determines whether the value requires quoting.
+     * According to the {@link ObjectName} documentation, values can be quoted or unquoted. Unquoted
+     * values may not contain any of the characters comma, equals, colon, or quote.
+     *
+     * @param value a value to test
+     * @return true when it requires quoting, false otherwise
+     */
+    private boolean shouldQuote(final String value) {
+        for (char quotableChar : QUOTABLE_CHARS) {
+            if (value.indexOf(quotableChar) != -1) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
