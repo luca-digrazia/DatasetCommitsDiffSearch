7abf733fe104cc344e97f2e36adb10f857a56e4b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:50:07 +0200

    Only run the worker if the build is incremental.
        This feature is tunable through --worker_max_changed_files flag. If unspecified, all builds are considered incremental.
    
        --
        MOS_MIGRATED_REVID=96968367

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/worker/WorkerModule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/worker/WorkerModule.java
@@ -13,35 +13,38 @@
 // limitations under the License.
 package com.google.devtools.build.lib.worker;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.eventbus.Subscribe;
-import com.google.devtools.build.lib.actions.ActionContextConsumer;
 import com.google.devtools.build.lib.actions.ActionContextProvider;
 import com.google.devtools.build.lib.buildtool.BuildRequest;
-import com.google.devtools.build.lib.buildtool.buildevent.BuildCompleteEvent;
-import com.google.devtools.build.lib.buildtool.buildevent.BuildInterruptedEvent;
 import com.google.devtools.build.lib.buildtool.buildevent.BuildStartingEvent;
-import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.runtime.BlazeModule;
+import com.google.devtools.build.lib.runtime.BlazeRuntime;
 import com.google.devtools.build.lib.runtime.Command;
-import com.google.devtools.build.lib.runtime.CommandEnvironment;
-import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.common.options.OptionsBase;
 
 import org.apache.commons.pool2.impl.GenericKeyedObjectPoolConfig;
-
-import java.io.IOException;
 
 /**
  * A module that adds the WorkerActionContextProvider to the available action context providers.
  */
 public class WorkerModule extends BlazeModule {
-  private WorkerPool workers;
+  private final WorkerPool workers;
 
-  private CommandEnvironment env;
   private BuildRequest buildRequest;
-  private boolean verbose;
+  private BlazeRuntime blazeRuntime;
+
+  public WorkerModule() {
+    GenericKeyedObjectPoolConfig config = new GenericKeyedObjectPoolConfig();
+    config.setTimeBetweenEvictionRunsMillis(10 * 1000);
+    workers = new WorkerPool(new WorkerFactory(), config);
+  }
+
+  @Override
+  public Iterable<ActionContextProvider> getActionContextProviders() {
+    return ImmutableList.<ActionContextProvider>of(
+        new WorkerActionContextProvider(buildRequest, workers, blazeRuntime.getEventBus()));
+  }
 
   @Override
   public Iterable<Class<? extends OptionsBase>> getCommandOptions(Command command) {
@@ -51,92 +54,18 @@ public class WorkerModule extends BlazeModule {
   }
 
   @Override
-  public void beforeCommand(Command command, CommandEnvironment env) {
-    this.env = env;
-    env.getEventBus().register(this);
-
-    if (workers == null) {
-      Path logDir = env.getRuntime().getOutputBase().getRelative("worker-logs");
-      try {
-        logDir.createDirectory();
-      } catch (IOException e) {
-        env
-            .getReporter()
-            .handle(Event.error("Could not create directory for worker logs: " + logDir));
-      }
-
-      GenericKeyedObjectPoolConfig config = new GenericKeyedObjectPoolConfig();
-      config.setTimeBetweenEvictionRunsMillis(10 * 1000);
-
-      workers = new WorkerPool(new WorkerFactory(), config);
-      workers.setReporter(env.getReporter());
-      workers.setLogDirectory(logDir);
-    }
+  public void beforeCommand(BlazeRuntime blazeRuntime, Command command) {
+    this.blazeRuntime = blazeRuntime;
+    blazeRuntime.getEventBus().register(this);
   }
 
   @Subscribe
   public void buildStarting(BuildStartingEvent event) {
-    Preconditions.checkNotNull(workers);
-
-    this.buildRequest = event.getRequest();
-
-    WorkerOptions options = buildRequest.getOptions(WorkerOptions.class);
-    workers.setMaxTotalPerKey(options.workerMaxInstances);
-    workers.setMaxIdlePerKey(options.workerMaxInstances);
-    workers.setMinIdlePerKey(options.workerMaxInstances);
-    workers.setVerbose(options.workerVerbose);
-    this.verbose = options.workerVerbose;
-  }
-
-  @Override
-  public Iterable<ActionContextProvider> getActionContextProviders() {
-    Preconditions.checkNotNull(env);
-    Preconditions.checkNotNull(buildRequest);
-    Preconditions.checkNotNull(workers);
-
-    return ImmutableList.<ActionContextProvider>of(
-        new WorkerActionContextProvider(
-            env.getRuntime(), buildRequest, workers, env.getEventBus()));
-  }
-
-  @Override
-  public Iterable<ActionContextConsumer> getActionContextConsumers() {
-    return ImmutableList.<ActionContextConsumer>of(new WorkerActionContextConsumer());
-  }
-
-  @Subscribe
-  public void buildComplete(BuildCompleteEvent event) {
-    if (workers != null && buildRequest.getOptions(WorkerOptions.class).workerQuitAfterBuild) {
-      if (verbose) {
-        env
-            .getReporter()
-            .handle(Event.info("Build completed, shutting down worker pool..."));
-      }
-      workers.close();
-      workers = null;
-    }
-  }
-
-  // Kill workers on Ctrl-C to quickly end the interrupted build.
-  // TODO(philwo) - make sure that this actually *kills* the workers and not just politely waits
-  // for them to finish.
-  @Subscribe
-  public void buildInterrupted(BuildInterruptedEvent event) {
-    if (workers != null) {
-      if (verbose) {
-        env
-            .getReporter()
-            .handle(Event.info("Build interrupted, shutting down worker pool..."));
-      }
-      workers.close();
-      workers = null;
-    }
+    buildRequest = event.getRequest();
   }
 
   @Override
   public void afterCommand() {
-    this.env = null;
-    this.buildRequest = null;
-    this.verbose = false;
+    buildRequest = null;
   }
 }
