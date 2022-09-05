f5d6d5271dda7f64a567590a91dcc73ebc31deb0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:21:13 +0200

    Allow set custom prefix Jetty QueuedThreadPool's metrics (#908)
        Prior to this fix all Jetty QueuedThreadPool's metrics are prefixed
        by the class. The fix allows set the metric's name prefix. The origin value is
        used as the default value because of backward compatibility. We don't want to
        arbitrary change the name of metrics once existing users switch to new version of
        the library.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-jetty9/src/test/java/com/codahale/metrics/jetty9/InstrumentedQueuedThreadPoolTest.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-jetty9/src/test/java/com/codahale/metrics/jetty9/InstrumentedQueuedThreadPoolTest.java
@@ -1,17 +1,18 @@
 package com.codahale.metrics.jetty9;
 
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.junit.After;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 public class InstrumentedQueuedThreadPoolTest {
     private static final String PREFIX = "prefix";
@@ -26,27 +27,23 @@ public class InstrumentedQueuedThreadPoolTest {
     }
 
     @Test
-    public void customMetricsPrefix() throws Exception {
+    public void customMetricsPrefix() throws Exception{
         iqtp.setPrefix(PREFIX);
         iqtp.doStart();
 
         verify(metricRegistry, atLeastOnce()).register(metricNameCaptor.capture(), any(Metric.class));
         String metricName = metricNameCaptor.getValue();
-        assertThat(metricName)
-                .overridingErrorMessage("Custom metric's prefix doesn't match")
-                .startsWith(PREFIX);
+        assertThat("Custom metric's prefix doesn't match", metricName, startsWith(PREFIX));
 
     }
 
     @Test
-    public void metricsPrefixBackwardCompatible() throws Exception {
+    public void metricsPrefixBackwardCompatible() throws Exception{
         iqtp.doStart();
 
         verify(metricRegistry, atLeastOnce()).register(metricNameCaptor.capture(), any(Metric.class));
         String metricName = metricNameCaptor.getValue();
-        assertThat(metricName)
-                .overridingErrorMessage("The default metrics prefix was changed")
-                .startsWith(QueuedThreadPool.class.getName());
+        assertThat("The default metrics prefix was changed", metricName, startsWith(QueuedThreadPool.class.getName()));
     }
 
 }
