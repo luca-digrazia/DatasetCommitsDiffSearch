3722e3e560578316b0dc3f115d7a730b0aed7ec4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:25:34 +0200

    ä¼åparser

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-entity/src/main/java/org/hswebframework/web/commons/entity/param/TermExpressionParser.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-entity/src/main/java/org/hswebframework/web/commons/entity/param/TermExpressionParser.java
@@ -19,37 +19,19 @@ public class TermExpressionParser {
 
         NestConditional<?> nest = null;
 
-        // 字符容器
         char[] buf = new char[128];
-        // 记录词项的长度, Arrays.copyOf使用
         byte len = 0;
-        // 空格数量?
         byte spaceLen = 0;
-        // 当前列
-        char[] currentColumn = null;
-        // 当前列对应的值
-        char[] currentValue = null;
-        // 当前条件类型 eq btw in ...
-        String currentTermType = null;
-        // 当前链接类型 and / or
-        String currentType = "and";
-        // 是否是引号, 单引号 / 双引号
-        byte quotationMarks = 0;
-        // 表达式字符数组
-        char[] all = expression.toCharArray();
 
+        char[] currentColumn = null;
+
+        String currentTermType = null;
+        char[] currentValue = null;
+        char[] all = expression.toCharArray();
+        String currentType = "and";
         for (char c : all) {
 
-            if (c == '\'' || c == '"') {
-                if (quotationMarks != 0) {
-                    // 碰到(结束的)单/双引号, 标志归零, 跳过
-                    quotationMarks = 0;
-                    continue;
-                }
-                // 碰到(开始的)单/双引号, 做记录, 跳过
-                quotationMarks++;
-                continue;
-            } else if (c == '(') {
+            if (c == '(') {
                 nest = (nest == null ?
                         (currentType.equals("or") ? conditional.orNest() : conditional.nest()) :
                         (currentType.equals("or") ? nest.orNest() : nest.nest()));
@@ -86,11 +68,6 @@ public class TermExpressionParser {
                 continue;
             } else if (c == ' ') {
                 if (len == 0) {
-                    continue;
-                }
-                if (quotationMarks != 0) {
-                    // 如果当前字符是空格，并且前面迭代时碰到过单/双引号, 不处理并且添加到buf中
-                    buf[len++] = c;
                     continue;
                 }
                 spaceLen++;
@@ -140,10 +117,6 @@ public class TermExpressionParser {
                         len = 0;
                         spaceLen++;
                     }
-                } else {
-                    currentColumn = Arrays.copyOf(buf, len);
-                    len = 0;
-                    spaceLen++;
                 }
                 continue;
             }
