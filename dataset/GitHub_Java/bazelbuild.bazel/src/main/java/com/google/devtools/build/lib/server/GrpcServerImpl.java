1ef6de74581500ef61d44fc11abfb7cf64160db7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:09:02 +0200

    Prefer IPv6 when binding the gRPC server to localhost, because the OS X sandbox will not allow us to bind to the IPv4 one. Automatically falls back to IPv4 if binding to [::1] fails.
        --
        MOS_MIGRATED_REVID=129206917

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
@@ -40,6 +40,7 @@ import io.grpc.netty.NettyServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.BindException;
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
 import java.security.SecureRandom;
@@ -275,7 +276,7 @@ public class GrpcServerImpl extends RPCServer {
     InetSocketAddress address = new InetSocketAddress("[::1]", port);
     try {
       server = NettyServerBuilder.forAddress(address).addService(commandServer).build().start();
-    } catch (IOException e) {
+    } catch (BindException e) {
       address = new InetSocketAddress("127.0.0.1", port);
       server = NettyServerBuilder.forAddress(address).addService(commandServer).build().start();
     }
