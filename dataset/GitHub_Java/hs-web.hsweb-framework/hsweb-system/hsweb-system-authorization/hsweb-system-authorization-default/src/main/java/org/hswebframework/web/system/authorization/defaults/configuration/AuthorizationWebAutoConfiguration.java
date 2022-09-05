a8aa105dd743cefa56fdebd4246e7a349639373f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:13:19 +0200

    ä¼åæééç½®

---
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-default/src/main/java/org/hswebframework/web/system/authorization/defaults/configuration/AuthorizationWebAutoConfiguration.java
@@ -0,0 +1,50 @@
+package org.hswebframework.web.system.authorization.defaults.configuration;
+
+import org.hswebframework.web.system.authorization.defaults.webflux.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthorizationWebAutoConfiguration {
+
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+    public static class WebFluxAuthorizationConfiguration {
+
+        @Bean
+        public WebFluxPermissionController webFluxPermissionController() {
+            return new WebFluxPermissionController();
+        }
+
+        @Bean
+        public WebFluxAuthorizationSettingController webFluxAuthorizationSettingController() {
+            return new WebFluxAuthorizationSettingController();
+        }
+
+        @Bean
+        public WebFluxDimensionController webFluxDimensionController() {
+            return new WebFluxDimensionController();
+        }
+
+
+        @Bean
+        @ConditionalOnMissingBean
+        public WebFluxUserController webFluxUserController() {
+            return new WebFluxUserController();
+        }
+
+        @Bean
+        public WebFluxDimensionUserController webFluxDimensionUserController() {
+            return new WebFluxDimensionUserController();
+        }
+
+        @Bean
+        public WebFluxDimensionTypeController webFluxDimensionTypeController() {
+            return new WebFluxDimensionTypeController();
+        }
+    }
+
+}
