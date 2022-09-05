d1b0432a7b3a27a60c53d6f9cbdd30356d1c1fc0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:58:34 +0200

    More runfiles consolidation
        --
        MOS_MIGRATED_REVID=95261769

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/RunfilesSupplierImpl.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/RunfilesSupplierImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2015 The Bazel Authors. All rights reserved.
+// Copyright 2015 Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
 
 package com.google.devtools.build.lib.analysis;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
@@ -23,17 +22,17 @@ import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.BaseSpawn;
 import com.google.devtools.build.lib.actions.RunfilesSupplier;
 import com.google.devtools.build.lib.vfs.PathFragment;
+
 import java.io.IOException;
 import java.util.Map;
-import javax.annotation.Nullable;
+import java.util.Map.Entry;
 
-/** {@link RunfilesSupplier} implementation wrapping a single {@link Runfiles} directory mapping. */
-// TODO(bazel-team): Consider renaming to SingleRunfilesSupplierImpl.
+/**
+ * {@link RunfilesSupplier} implementation wrapping directory to {@link Runfiles} objects mappings.
+ */
 public class RunfilesSupplierImpl implements RunfilesSupplier {
-  private final PathFragment runfilesDir;
-  private final Runfiles runfiles;
-  @Nullable
-  private final Artifact manifest;
+
+  private final ImmutableMap<PathFragment, Runfiles> inputRunfiles;
 
   /**
    * Create an instance for an executable.
@@ -47,49 +46,43 @@ public class RunfilesSupplierImpl implements RunfilesSupplier {
   }
 
   /**
-   * Create an instance. When a manifest is available consider using
-   * {@link #RunfilesSupplierImpl(PathFragment, Runfiles, Artifact)} instead.
-   */
-  public RunfilesSupplierImpl(PathFragment runfilesDir, Runfiles runfiles) {
-    this(runfilesDir, runfiles, /*manifest=*/ null);
-  }
-
-  /**
-   * Create an instance mapping {@code runfiles} to {@code runfilesDir}.
+   * Create an instance when you have a a single mapping.
    *
    * @param runfilesDir the desired runfiles directory. Should be relative.
-   * @param runfiles the runfiles for runilesDir.
-   * @param manifest runfiles' associated runfiles manifest artifact, if present.
+   * @param runfiles the runfiles for runfilesDir
    */
-  public RunfilesSupplierImpl(
-      PathFragment runfilesDir,
-      Runfiles runfiles,
-      @Nullable Artifact manifest) {
-    Preconditions.checkArgument(!runfilesDir.isAbsolute());
-    this.runfilesDir = Preconditions.checkNotNull(runfilesDir);
-    this.runfiles = Preconditions.checkNotNull(runfiles);
-    this.manifest = manifest;
+  public RunfilesSupplierImpl(PathFragment runfilesDir, Runfiles runfiles) {
+    this.inputRunfiles = ImmutableMap.of(runfilesDir, runfiles);
+  }
+
+  @VisibleForTesting
+  public RunfilesSupplierImpl(Map<PathFragment, Runfiles> inputRunfiles) {
+    this.inputRunfiles = ImmutableMap.copyOf(inputRunfiles);
   }
 
   @Override
   public Iterable<Artifact> getArtifacts() {
-    return Iterables.filter(runfiles.getAllArtifacts(), Artifact.MIDDLEMAN_FILTER);
+    ImmutableSet.Builder<Artifact> builder = ImmutableSet.builder();
+    for (Entry<PathFragment, Runfiles> entry : inputRunfiles.entrySet()) {
+      builder.addAll(
+          Iterables.filter(entry.getValue().getAllArtifacts(), Artifact.MIDDLEMAN_FILTER));
+    }
+    return builder.build();
   }
 
   @Override
   public ImmutableSet<PathFragment> getRunfilesDirs() {
-    return ImmutableSet.of(runfilesDir);
+    return inputRunfiles.keySet();
   }
 
   @Override
   public ImmutableMap<PathFragment, Map<PathFragment, Artifact>> getMappings() throws IOException {
-    return ImmutableMap.of(
-        runfilesDir,
-        runfiles.getRunfilesInputs(/*eventHandler=*/ null, /*location=*/ null));
+    ImmutableMap.Builder<PathFragment, Map<PathFragment, Artifact>> result =
+        ImmutableMap.builder();
+    for (Entry<PathFragment, Runfiles> entry : inputRunfiles.entrySet()) {
+      result.put(entry.getKey(), entry.getValue().getRunfilesInputs(null, null));
+    }
+    return result.build();
   }
 
-  @Override
-  public ImmutableList<Artifact> getManifests() {
-    return manifest != null ? ImmutableList.of(manifest) : ImmutableList.<Artifact>of();
-  }
 }
