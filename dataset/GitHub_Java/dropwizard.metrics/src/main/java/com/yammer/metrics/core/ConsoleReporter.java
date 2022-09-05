da70dfae0a856d82a99ef5bcf51e2f7bf8988479
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:50:07 +0200

    Simplify the sharing of registered metrics between classes.

--- dataset/GitHub_Java/dropwizard.metrics/src/main/java/com/yammer/metrics/core/ConsoleReporter.java
+++ dataset/GitHub_Java/dropwizard.metrics/src/main/java/com/yammer/metrics/core/ConsoleReporter.java
@@ -6,11 +6,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.SortedMap;
-import java.util.TreeMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,17 +20,13 @@ public class ConsoleReporter implements Runnable {
 	private static final ScheduledExecutorService TICK_THREAD =
 			Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory("metrics-console-reporter"));
 	private final PrintStream out;
-	private final Map<MetricName,Metric> metrics;
-	private ScheduledFuture<?> future;
 
 	/**
 	 * Creates a new {@link ConsoleReporter}.
 	 *
-	 * @param metrics a map of {@link MetricName}s to {@link Metric}s
-	 * @param out the {@link PrintStream} to which output will be written
+	 * @param out the {@link java.io.PrintStream} to which output will be written
 	 */
-	/*package*/ ConsoleReporter(Map<MetricName, Metric> metrics, PrintStream out) {
-		this.metrics = metrics;
+	public ConsoleReporter(PrintStream out) {
 		this.out = out;
 	}
 
@@ -44,32 +37,7 @@ public class ConsoleReporter implements Runnable {
 	 * @param unit the time unit of {@code period}
 	 */
 	public void start(long period, TimeUnit unit) {
-		this.future = TICK_THREAD.scheduleAtFixedRate(this, period, period, unit);
-	}
-
-	/**
-	 * Stops printing to the console.
-	 */
-	public void stop() {
-		if (future != null) {
-			future.cancel(true);
-			future = null;
-		}
-	}
-
-	private SortedMap<String, SortedMap<String, Metric>> sortedMetrics() {
-		final SortedMap<String, SortedMap<String, Metric>> sortedMetrics =
-				new TreeMap<String, SortedMap<String, Metric>>();
-		for (Entry<MetricName, Metric> entry : metrics.entrySet()) {
-			final String packageName = entry.getKey().getKlass().getCanonicalName();
-			SortedMap<String, Metric> submetrics = sortedMetrics.get(packageName);
-			if (submetrics == null) {
-				submetrics = new TreeMap<String, Metric>();
-				sortedMetrics.put(packageName, submetrics);
-			}
-			submetrics.put(entry.getKey().getName(), entry.getValue());
-		}
-		return sortedMetrics;
+		TICK_THREAD.scheduleAtFixedRate(this, period, period, unit);
 	}
 
 	@Override
@@ -84,7 +52,7 @@ public class ConsoleReporter implements Runnable {
 			}
 			out.println();
 
-			for (Entry<String, SortedMap<String, Metric>> entry : sortedMetrics().entrySet()) {
+			for (Entry<String, Map<String, Metric>> entry : Utils.sortMetrics(Metrics.allMetrics()).entrySet()) {
 				out.print(entry.getKey());
 				out.println(':');
 
@@ -98,6 +66,8 @@ public class ConsoleReporter implements Runnable {
 						printGauge((GaugeMetric) metric);
 					} else if (metric instanceof CounterMetric) {
 						printCounter((CounterMetric) metric);
+					} else if (metric instanceof HistogramMetric) {
+						printHistogram((HistogramMetric) metric);
 					} else if (metric instanceof MeterMetric) {
 						printMeter((MeterMetric) metric);
 					} else if (metric instanceof TimerMetric) {
@@ -114,50 +84,60 @@ public class ConsoleReporter implements Runnable {
 		}
 	}
 
-	private void printGauge(GaugeMetric metric) {
-		final GaugeMetric valueMetric = metric;
+	private void printGauge(GaugeMetric gauge) {
 		out.print("    value = ");
-		out.println(valueMetric.value());
+		out.println(gauge.value());
 	}
 
-	private void printCounter(CounterMetric metric) {
-		final CounterMetric counter = metric;
+	private void printCounter(CounterMetric counter) {
 		out.print("    count = ");
 		out.println(counter.count());
 	}
 
-	private void printMeter(MeterMetric metric) {
-		final MeterMetric meter = metric;
-		final String unit = abbrev(meter.getScaleUnit());
+	private void printMeter(MeterMetric meter) {
+		final String unit = abbrev(meter.scaleUnit());
 		out.printf("             count = %d\n", meter.count());
-		out.printf("         mean rate = %2.2f %s/%s\n", meter.meanRate(), meter.getEventType(), unit);
-		out.printf("     1-minute rate = %2.2f %s/%s\n", meter.oneMinuteRate(), meter.getEventType(), unit);
-		out.printf("     5-minute rate = %2.2f %s/%s\n", meter.fiveMinuteRate(), meter.getEventType(), unit);
-		out.printf("    15-minute rate = %2.2f %s/%s\n", meter.fifteenMinuteRate(), meter.getEventType(), unit);
+		out.printf("         mean rate = %2.2f %s/%s\n", meter.meanRate(), meter.eventType(), unit);
+		out.printf("     1-minute rate = %2.2f %s/%s\n", meter.oneMinuteRate(), meter.eventType(), unit);
+		out.printf("     5-minute rate = %2.2f %s/%s\n", meter.fiveMinuteRate(), meter.eventType(), unit);
+		out.printf("    15-minute rate = %2.2f %s/%s\n", meter.fifteenMinuteRate(), meter.eventType(), unit);
 	}
 
-	private void printTimer(TimerMetric metric) {
-		final TimerMetric timer = metric;
-		final String rateUnit = abbrev(timer.getRateUnit());
-		final String latencyUnit = abbrev(timer.getLatencyUnit());
+	private void printHistogram(HistogramMetric histogram) {
+		final double[] percentiles = histogram.percentiles(0.5, 0.75, 0.95, 0.98, 0.99, 0.999);
+		out.printf("               min = %2.2f\n", histogram.min());
+		out.printf("               max = %2.2f\n", histogram.max());
+		out.printf("              mean = %2.2f\n", histogram.mean());
+		out.printf("            stddev = %2.2f\n", histogram.stdDev());
+		out.printf("            median = %2.2f\n", percentiles[0]);
+		out.printf("              75%% <= %2.2f\n", percentiles[1]);
+		out.printf("              95%% <= %2.2f\n", percentiles[2]);
+		out.printf("              98%% <= %2.2f\n", percentiles[3]);
+		out.printf("              99%% <= %2.2f\n", percentiles[4]);
+		out.printf("            99.9%% <= %2.2f\n", percentiles[5]);
+	}
+
+	private void printTimer(TimerMetric timer) {
+		final String rateUnit = abbrev(timer.rateUnit());
+		final String durationUnit = abbrev(timer.durationUnit());
 
 		out.printf("             count = %d\n", timer.count());
-		out.printf("         mean rate = %2.2f %s/%s\n", timer.meanRate(), timer.getEventType(), rateUnit);
-		out.printf("     1-minute rate = %2.2f %s/%s\n", timer.oneMinuteRate(), timer.getEventType(), rateUnit);
-		out.printf("     5-minute rate = %2.2f %s/%s\n", timer.fiveMinuteRate(), timer.getEventType(), rateUnit);
-		out.printf("    15-minute rate = %2.2f %s/%s\n", timer.fifteenMinuteRate(), timer.getEventType(), rateUnit);
+		out.printf("         mean rate = %2.2f %s/%s\n", timer.meanRate(), timer.eventType(), rateUnit);
+		out.printf("     1-minute rate = %2.2f %s/%s\n", timer.oneMinuteRate(), timer.eventType(), rateUnit);
+		out.printf("     5-minute rate = %2.2f %s/%s\n", timer.fiveMinuteRate(), timer.eventType(), rateUnit);
+		out.printf("    15-minute rate = %2.2f %s/%s\n", timer.fifteenMinuteRate(), timer.eventType(), rateUnit);
 
 		final double[] percentiles = timer.percentiles(0.5, 0.75, 0.95, 0.98, 0.99, 0.999);
-		out.printf("               min = %2.2f%s\n", timer.min(), latencyUnit);
-		out.printf("               max = %2.2f%s\n", timer.max(), latencyUnit);
-		out.printf("              mean = %2.2f%s\n", timer.mean(), latencyUnit);
-		out.printf("            stddev = %2.2f%s\n", timer.stdDev(), latencyUnit);
-		out.printf("            median = %2.2f%s\n", percentiles[0], latencyUnit);
-		out.printf("              75%% <= %2.2f%s\n", percentiles[1], latencyUnit);
-		out.printf("              95%% <= %2.2f%s\n", percentiles[2], latencyUnit);
-		out.printf("              98%% <= %2.2f%s\n", percentiles[3], latencyUnit);
-		out.printf("              99%% <= %2.2f%s\n", percentiles[4], latencyUnit);
-		out.printf("            99.9%% <= %2.2f%s\n", percentiles[5], latencyUnit);
+		out.printf("               min = %2.2f%s\n", timer.min(), durationUnit);
+		out.printf("               max = %2.2f%s\n", timer.max(), durationUnit);
+		out.printf("              mean = %2.2f%s\n", timer.mean(), durationUnit);
+		out.printf("            stddev = %2.2f%s\n", timer.stdDev(), durationUnit);
+		out.printf("            median = %2.2f%s\n", percentiles[0], durationUnit);
+		out.printf("              75%% <= %2.2f%s\n", percentiles[1], durationUnit);
+		out.printf("              95%% <= %2.2f%s\n", percentiles[2], durationUnit);
+		out.printf("              98%% <= %2.2f%s\n", percentiles[3], durationUnit);
+		out.printf("              99%% <= %2.2f%s\n", percentiles[4], durationUnit);
+		out.printf("            99.9%% <= %2.2f%s\n", percentiles[5], durationUnit);
 	}
 
 	private String abbrev(TimeUnit unit) {
--- dataset/GitHub_Java/dropwizard.metrics/src/main/java/com/yammer/metrics/core/JmxReporter.java
+++ dataset/GitHub_Java/dropwizard.metrics/src/main/java/com/yammer/metrics/core/JmxReporter.java
@@ -1,10 +1,7 @@
 package com.yammer.metrics.core;
 
 import java.lang.management.ManagementFactory;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -19,9 +16,7 @@ import javax.management.*;
 public class JmxReporter implements Runnable {
 	private static final ScheduledExecutorService TICK_THREAD =
 			Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory("metrics-jmx-reporter"));
-	private final Map<MetricName, Metric> metrics;
 	private final Map<MetricName, MetricMBean> beans;
-	private ScheduledFuture<?> future;
 	private final MBeanServer server;
 
 	public static interface MetricMBean {
@@ -107,12 +102,12 @@ public class JmxReporter implements Runnable {
 
 		@Override
 		public String getEventType() {
-			return metric.getEventType();
+			return metric.eventType();
 		}
 
 		@Override
 		public TimeUnit getUnit() {
-			return metric.getScaleUnit();
+			return metric.scaleUnit();
 		}
 
 		@Override
@@ -136,20 +131,8 @@ public class JmxReporter implements Runnable {
 		}
 	}
 
-	public static interface TimerMBean extends MetricMBean {
+	public static interface HistogramMBean extends MetricMBean {
 		public long getCount();
-
-		public TimeUnit getRateUnit();
-
-		public double getMeanRate();
-
-		public double getOneMinuteRate();
-
-		public double getFiveMinuteRate();
-
-		public double getFifteenMinuteRate();
-
-		public TimeUnit getLatencyUnit();
 
 		public double getMin();
 
@@ -170,13 +153,15 @@ public class JmxReporter implements Runnable {
 		public double get99thPercentile();
 
 		public double get999thPercentile();
+
+		public List<Long> values();
 	}
 
-	public class Timer implements TimerMBean {
+	public class Histogram implements HistogramMBean {
 		private final ObjectName objectName;
-		private final TimerMetric metric;
+		private final HistogramMetric metric;
 
-		public Timer(TimerMetric metric, ObjectName objectName) {
+		public Histogram(HistogramMetric metric, ObjectName objectName) {
 			this.metric = metric;
 			this.objectName = objectName;
 		}
@@ -194,36 +179,6 @@ public class JmxReporter implements Runnable {
 		@Override
 		public long getCount() {
 			return metric.count();
-		}
-
-		@Override
-		public TimeUnit getRateUnit() {
-			return metric.getRateUnit();
-		}
-
-		@Override
-		public double getMeanRate() {
-			return metric.meanRate();
-		}
-
-		@Override
-		public double getOneMinuteRate() {
-			return metric.oneMinuteRate();
-		}
-
-		@Override
-		public double getFiveMinuteRate() {
-			return metric.fiveMinuteRate();
-		}
-
-		@Override
-		public double getFifteenMinuteRate() {
-			return metric.fifteenMinuteRate();
-		}
-
-		@Override
-		public TimeUnit getLatencyUnit() {
-			return metric.getLatencyUnit();
 		}
 
 		@Override
@@ -270,49 +225,191 @@ public class JmxReporter implements Runnable {
 		public double get999thPercentile() {
 			return metric.percentiles(0.999)[0];
 		}
+
+		@Override
+		public List<Long> values() {
+			return metric.values();
+		}
 	}
 
-	public JmxReporter(Map<MetricName, Metric> metrics) {
-		this.metrics = metrics;
-		this.beans = new HashMap<MetricName, MetricMBean>(metrics.size());
+	public static interface TimerMBean extends MetricMBean {
+		public long getCount();
+
+		public TimeUnit getRateUnit();
+
+		public double getMeanRate();
+
+		public double getOneMinuteRate();
+
+		public double getFiveMinuteRate();
+
+		public double getFifteenMinuteRate();
+
+		public TimeUnit getLatencyUnit();
+
+		public double getMin();
+
+		public double getMax();
+
+		public double getMean();
+
+		public double getStdDev();
+
+		public double get50thPercentile();
+
+		public double get75thPercentile();
+
+		public double get95thPercentile();
+
+		public double get98thPercentile();
+
+		public double get99thPercentile();
+
+		public double get999thPercentile();
+
+		public List<Double> values();
+	}
+
+	public class Timer implements TimerMBean {
+		private final ObjectName objectName;
+		private final TimerMetric metric;
+
+		public Timer(TimerMetric metric, ObjectName objectName) {
+			this.metric = metric;
+			this.objectName = objectName;
+		}
+
+		@Override
+		public ObjectName objectName() {
+			return objectName;
+		}
+
+		@Override
+		public double get50thPercentile() {
+			return metric.percentiles(0.5)[0];
+		}
+
+		@Override
+		public long getCount() {
+			return metric.count();
+		}
+
+		@Override
+		public TimeUnit getRateUnit() {
+			return metric.rateUnit();
+		}
+
+		@Override
+		public double getMeanRate() {
+			return metric.meanRate();
+		}
+
+		@Override
+		public double getOneMinuteRate() {
+			return metric.oneMinuteRate();
+		}
+
+		@Override
+		public double getFiveMinuteRate() {
+			return metric.fiveMinuteRate();
+		}
+
+		@Override
+		public double getFifteenMinuteRate() {
+			return metric.fifteenMinuteRate();
+		}
+
+		@Override
+		public TimeUnit getLatencyUnit() {
+			return metric.durationUnit();
+		}
+
+		@Override
+		public double getMin() {
+			return metric.min();
+		}
+
+		@Override
+		public double getMax() {
+			return metric.max();
+		}
+
+		@Override
+		public double getMean() {
+			return metric.mean();
+		}
+
+		@Override
+		public double getStdDev() {
+			return metric.stdDev();
+		}
+
+		@Override
+		public double get75thPercentile() {
+			return metric.percentiles(0.75)[0];
+		}
+
+		@Override
+		public double get95thPercentile() {
+			return metric.percentiles(0.95)[0];
+		}
+
+		@Override
+		public double get98thPercentile() {
+			return metric.percentiles(0.98)[0];
+		}
+
+		@Override
+		public double get99thPercentile() {
+			return metric.percentiles(0.99)[0];
+		}
+
+		@Override
+		public double get999thPercentile() {
+			return metric.percentiles(0.999)[0];
+		}
+
+		@Override
+		public List<Double> values() {
+			return metric.values();
+		}
+	}
+
+	/*package*/ JmxReporter() {
+		this.beans = new HashMap<MetricName, MetricMBean>(Metrics.allMetrics().size());
 		this.server = ManagementFactory.getPlatformMBeanServer();
 	}
 
 	public void start() {
-		this.future = TICK_THREAD.scheduleAtFixedRate(this, 0, 1, TimeUnit.SECONDS);
-	}
-
-	public void stop() {
-		if (future != null) {
-			future.cancel(true);
-			future = null;
-			for (MetricMBean bean : beans.values()) {
-				try {
-					server.unregisterMBean(bean.objectName());
-				} catch (Exception ignored) {
-				}
-			}
+		TICK_THREAD.scheduleAtFixedRate(this, 0, 1, TimeUnit.MINUTES);
+		// then schedule the tick thread every 100ms for the next second so
+		// as to pick up the initialization of most metrics (in the first 1s of
+		// the application lifecycle) w/o incurring a high penalty later on
+		for (int i = 1; i <= 900; i++) {
+			TICK_THREAD.schedule(this, i * 100, TimeUnit.MILLISECONDS);
 		}
 	}
 
 	@Override
 	public void run() {
-		final Set<MetricName> newMetrics = new HashSet<MetricName>(metrics.keySet());
+		final Set<MetricName> newMetrics = new HashSet<MetricName>(Metrics.allMetrics().keySet());
 		newMetrics.removeAll(beans.keySet());
 
 		for (MetricName name : newMetrics) {
-			final Metric metric = metrics.get(name);
+			final Metric metric = Metrics.allMetrics().get(name);
 			if (metric != null) {
 				try {
 					final ObjectName objectName = new ObjectName(
 							String.format("%s:type=%s,name=%s",
 										  name.getKlass().getPackage().getName(),
-										  name.getKlass().getSimpleName(),
+										  name.getKlass().getSimpleName().replaceAll("\\$$", ""),
 										  name.getName()));
 					if (metric instanceof GaugeMetric) {
 						registerBean(name, new Gauge((GaugeMetric<?>) metric, objectName), objectName);
 					} else if (metric instanceof CounterMetric) {
 						registerBean(name, new Counter((CounterMetric) metric, objectName), objectName);
+					} else if (metric instanceof HistogramMetric) {
+						registerBean(name, new Histogram((HistogramMetric) metric, objectName), objectName);
 					} else if (metric instanceof MeterMetric) {
 						registerBean(name, new Meter((MeterMetric) metric, objectName), objectName);
 					} else if (metric instanceof TimerMetric) {
