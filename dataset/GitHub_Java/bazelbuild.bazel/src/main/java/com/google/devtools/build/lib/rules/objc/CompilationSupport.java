cf056e01cb16eba126025244585ee4fc9851420a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:28:15 +0200

    Remove one instance of -F interspersing with framework linking.     Previously we were interspersing these doubly, resulting in constructs like "-F -F -F FrameworkPath"
        --
        MOS_MIGRATED_REVID=125071017

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
@@ -498,6 +498,7 @@ public final class CompilationSupport {
         .add(CLANG);
     if (ObjcRuleClasses.CPP_SOURCES.matches(sourceFile.getExecPath())) {
       commandLine.add("-stdlib=libc++");
+      commandLine.add("-std=c++11");
     }
 
     if (compilationArtifacts.hasSwiftSources()) {
@@ -1146,7 +1147,8 @@ public final class CompilationSupport {
     if (objcProvider.is(USES_CPP)) {
       commandLine
           .add(CLANG_PLUSPLUS)
-          .add("-stdlib=libc++");
+          .add("-stdlib=libc++")
+          .add("-std=c++11");
     } else {
       commandLine.add(CLANG);
     }
