b1df54327a10f544bfac353f6d17510092f71038
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:56:24 +0200

    å é¤æ ç¨ä¾èµ

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-controller/src/main/java/org/hsweb/web/controller/ControllerExceptionTranslator.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-controller/src/main/java/org/hsweb/web/controller/ControllerExceptionTranslator.java
@@ -2,7 +2,7 @@ package org.hsweb.web.controller;
 
 import org.hsweb.web.core.exception.BusinessException;
 import org.hsweb.web.core.message.ResponseMessage;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.hsweb.web.core.utils.WebUtil;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,6 +20,7 @@ public class ControllerExceptionTranslator {
         ModelAndView modelAndView = new ModelAndView("error/" + exception.getStatus());
         modelAndView.addAllObjects(ResponseMessage.error(exception.getMessage(), exception.getStatus()).toMap());
         modelAndView.addObject("exception", exception);
+        modelAndView.addObject("absPath", WebUtil.getBasePath(WebUtil.getHttpServletRequest()));
         return modelAndView;
     }
 
@@ -29,6 +30,7 @@ public class ControllerExceptionTranslator {
         ModelAndView modelAndView = new ModelAndView("error/" + 500);
         modelAndView.addAllObjects(ResponseMessage.error(exception.getMessage(), 500).toMap());
         modelAndView.addObject("exception", exception);
+        modelAndView.addObject("absPath", WebUtil.getBasePath(WebUtil.getHttpServletRequest()));
         return modelAndView;
     }
 }
