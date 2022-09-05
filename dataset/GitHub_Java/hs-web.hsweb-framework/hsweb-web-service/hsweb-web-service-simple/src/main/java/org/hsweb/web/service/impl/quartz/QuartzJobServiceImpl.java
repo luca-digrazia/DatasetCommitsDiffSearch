5d6b8caf273feeb2c72f842a7b51657e3ca15e85
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:58:19 +0200

    ä¿®å¤ insertæ¸ç©ºç¼å­

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service/hsweb-web-service-simple/src/main/java/org/hsweb/web/service/impl/quartz/QuartzJobServiceImpl.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service/hsweb-web-service-simple/src/main/java/org/hsweb/web/service/impl/quartz/QuartzJobServiceImpl.java
@@ -169,7 +169,6 @@ public class QuartzJobServiceImpl extends AbstractServiceImpl<QuartzJob, String>
         Assert.notNull(job, "任务不存在");
         String hisId = quartzJobHistoryService.createAndInsertHistory(id);
         String strRes = null;
-        QuartzJobHistory.Status status = FAIL;
         try {
             if (logger.isDebugEnabled())
                 logger.debug("start job [{}]", job.getName());
@@ -200,9 +199,8 @@ public class QuartzJobServiceImpl extends AbstractServiceImpl<QuartzJob, String>
                 if (res instanceof String)
                     strRes = ((String) res);
                 else strRes = JSON.toJSONString(res);
-                status = SUCCESS;
+                quartzJobHistoryService.endHistory(hisId, strRes, SUCCESS);
             } else {
-                status = FAIL;
                 if (result.getException() != null) {
                     strRes = StringUtils.throwable2String(result.getException());
                     logger.error("job failed", result.getException());
@@ -217,7 +215,7 @@ public class QuartzJobServiceImpl extends AbstractServiceImpl<QuartzJob, String>
                 }
             }
         } finally {
-            quartzJobHistoryService.endHistory(hisId, strRes, status);
+            quartzJobHistoryService.endHistory(hisId, strRes, FAIL);
         }
         return strRes;
     }
