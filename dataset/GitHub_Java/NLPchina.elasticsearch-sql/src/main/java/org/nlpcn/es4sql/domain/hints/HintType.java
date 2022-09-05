ffcbecb3df84eb935b72070033792ba8454b5cec
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:01:02 +0200

    fix #997 support track_total_hits

--- dataset/GitHub_Java/NLPchina.elasticsearch-sql/src/main/java/org/nlpcn/es4sql/domain/hints/HintType.java
+++ dataset/GitHub_Java/NLPchina.elasticsearch-sql/src/main/java/org/nlpcn/es4sql/domain/hints/HintType.java
@@ -1,8 +1,5 @@
 package org.nlpcn.es4sql.domain.hints;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by Eliran on 29/8/2015.
  */
@@ -10,6 +7,20 @@ public enum HintType
 {
     HASH_WITH_TERMS_FILTER,
     JOIN_LIMIT,
-    USE_NESTED_LOOPS;
-
+    USE_NESTED_LOOPS,
+    NL_MULTISEARCH_SIZE,
+    USE_SCROLL,
+    IGNORE_UNAVAILABLE,
+    DOCS_WITH_AGGREGATION,
+    ROUTINGS,
+    SHARD_SIZE,
+    HIGHLIGHT,
+    MINUS_FETCH_AND_RESULT_LIMITS,
+    MINUS_USE_TERMS_OPTIMIZATION,
+    COLLAPSE,
+    POST_FILTER,
+    STATS,
+    CONFLICTS,
+    PREFERENCE,
+    TRACK_TOTAL_HITS
 }
