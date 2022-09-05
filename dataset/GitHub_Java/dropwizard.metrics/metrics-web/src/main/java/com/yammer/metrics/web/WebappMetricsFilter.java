96a640d39cac16f5b1b43a7bf4bd7b4dbd967d4e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:36:04 +0200

    fixed indention

--- dataset/GitHub_Java/dropwizard.metrics/metrics-web/src/main/java/com/yammer/metrics/web/WebappMetricsFilter.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-web/src/main/java/com/yammer/metrics/web/WebappMetricsFilter.java
@@ -1,89 +1,120 @@
 package com.yammer.metrics.web;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.TimeUnit;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.*;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
-
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.CounterMetric;
-import com.yammer.metrics.core.MeterMetric;
-import com.yammer.metrics.core.TimerMetric;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
 /**
- * {@link Filter} implementation which captures request information and a breakdown of the response codes being returned.
+ * {@link Filter} implementation which captures request information and a breakdown of the response
+ * codes being returned.
  */
 public abstract class WebappMetricsFilter implements Filter {
-	
-    private final Map<Integer, MeterMetric> metersByStatusCode = new HashMap<Integer, MeterMetric>();
-    private final MeterMetric otherMeterMetric;
-    private final CounterMetric activeRequests;
-    private final TimerMetric requestTimer;
+    private final String otherMetricName;
+    private final Map<Integer, String> meterNamesByStatusCode;
+    private final String registryAttribute;
 
-    public WebappMetricsFilter(Map<Integer, String> meterNamesByStatusCode, String otherMetricName) {
-        
-        for (Entry<Integer, String> entry : meterNamesByStatusCode.entrySet()) {
-            metersByStatusCode.put(entry.getKey(), Metrics.newMeter(WebappMetricsFilter.class, entry.getValue(), "responses", TimeUnit.SECONDS));
-        }
-        
-        otherMeterMetric = Metrics.newMeter(WebappMetricsFilter.class, otherMetricName, "responses", TimeUnit.SECONDS);
-        activeRequests = Metrics.newCounter(WebappMetricsFilter.class, "activeRequests");
-        requestTimer = Metrics.newTimer(WebappMetricsFilter.class, "requests", TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
-        
+    // initialized after call of init method
+    private ConcurrentMap<Integer, Meter> metersByStatusCode;
+    private Meter otherMeter;
+    private Counter activeRequests;
+    private Timer requestTimer;
+
+
+    /**
+     * Creates a new instance of the filter.
+     *
+     * @param registryAttribute the attribute used to look up the metrics registry in the servlet context
+     * @param meterNamesByStatusCode A map, keyed by status code, of meter names that we are
+     *                               interested in.
+     * @param otherMetricName        The name used for the catch-all meter.
+     */
+    public WebappMetricsFilter(String registryAttribute, Map<Integer, String> meterNamesByStatusCode,
+                               String otherMetricName) {
+        this.registryAttribute = registryAttribute;
+        this.otherMetricName = otherMetricName;
+        this.meterNamesByStatusCode = meterNamesByStatusCode;
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        final MetricsRegistry metricsRegistry = getMetricsFactory(filterConfig);
+
+        this.metersByStatusCode = new ConcurrentHashMap<Integer, Meter>(meterNamesByStatusCode
+                .size());
+        for (Entry<Integer, String> entry : meterNamesByStatusCode.entrySet()) {
+            metersByStatusCode.put(entry.getKey(),
+                    metricsRegistry.newMeter(WebappMetricsFilter.class,
+                            entry.getValue(),
+                            "responses",
+                            TimeUnit.SECONDS));
+        }
+        this.otherMeter = metricsRegistry.newMeter(WebappMetricsFilter.class,
+                otherMetricName,
+                "responses",
+                TimeUnit.SECONDS);
+        this.activeRequests = metricsRegistry.newCounter(WebappMetricsFilter.class, "activeRequests");
+        this.requestTimer = metricsRegistry.newTimer(WebappMetricsFilter.class,
+                "requests",
+                TimeUnit.MILLISECONDS,
+                TimeUnit.SECONDS);
+
+    }
+
+    private MetricsRegistry getMetricsFactory(FilterConfig filterConfig) {
+        final MetricsRegistry metricsRegistry;
+
+        final Object o = filterConfig.getServletContext().getAttribute(this.registryAttribute);
+        if (o instanceof MetricsRegistry) {
+            metricsRegistry = (MetricsRegistry) o;
+        } else {
+            metricsRegistry = Metrics.defaultRegistry();
+        }
+        return metricsRegistry;
     }
 
     @Override
     public void destroy() {
+        
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        
-        long start = System.currentTimeMillis();
-
-        StatusExposingServletResponse wrappedResponse = new StatusExposingServletResponse((HttpServletResponse) response);
-        
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        final StatusExposingServletResponse wrappedResponse =
+                new StatusExposingServletResponse((HttpServletResponse) response);
         activeRequests.inc();
-
+        final TimerContext context = requestTimer.time();
         try {
             chain.doFilter(request, wrappedResponse);
         } finally {
+            context.stop();
             activeRequests.dec();
             markMeterForStatusCode(wrappedResponse.getStatus());
-            requestTimer.update(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
         }
-
     }
 
     private void markMeterForStatusCode(int status) {
-
-        MeterMetric metric = metersByStatusCode.get(status);
-
+        final Meter metric = metersByStatusCode.get(status);
         if (metric != null) {
             metric.mark();
         } else {
-            otherMeterMetric.mark();
+            otherMeter.mark();
         }
-
     }
 
     private static class StatusExposingServletResponse extends HttpServletResponseWrapper {
-
-        private int httpStatus;
+        // The Servlet spec says: calling setStatus is optional, if no status is set, the default is 200.
+        private int httpStatus = 200;
 
         public StatusExposingServletResponse(HttpServletResponse response) {
             super(response);
@@ -110,7 +141,5 @@ public abstract class WebappMetricsFilter implements Filter {
         public int getStatus() {
             return httpStatus;
         }
-
     }
-
 }
