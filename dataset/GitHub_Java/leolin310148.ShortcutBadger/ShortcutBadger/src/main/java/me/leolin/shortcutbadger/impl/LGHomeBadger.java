5f80bea0ae3c4013a1f6a4ed10c2d6ac8002cb88
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:41:31 +0200

    Oreo support: fallback to default intent if oreo one failed

--- dataset/GitHub_Java/leolin310148.ShortcutBadger/ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/LGHomeBadger.java
+++ dataset/GitHub_Java/leolin310148.ShortcutBadger/ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/LGHomeBadger.java
@@ -4,12 +4,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
-import me.leolin.shortcutbadger.Badger;
-import me.leolin.shortcutbadger.ShortcutBadgeException;
-import me.leolin.shortcutbadger.ShortcutBadger;
-
 import java.util.Arrays;
 import java.util.List;
+
+import me.leolin.shortcutbadger.Badger;
+import me.leolin.shortcutbadger.ShortcutBadgeException;
+import me.leolin.shortcutbadger.util.BroadcastHelper;
 
 /**
  * @author Leo Lin
@@ -18,7 +18,7 @@ import java.util.List;
 @Deprecated
 public class LGHomeBadger implements Badger {
 
-    private static final String INTENT_ACTION = "android.intent.action.BADGE_COUNT_UPDATE";
+    private static final String INTENT_ACTION = IntentConstants.DEFAULT_INTENT_ACTION;
     private static final String INTENT_EXTRA_BADGE_COUNT = "badge_count";
     private static final String INTENT_EXTRA_PACKAGENAME = "badge_count_package_name";
     private static final String INTENT_EXTRA_ACTIVITY_NAME = "badge_count_class_name";
@@ -29,7 +29,8 @@ public class LGHomeBadger implements Badger {
         intent.putExtra(INTENT_EXTRA_BADGE_COUNT, badgeCount);
         intent.putExtra(INTENT_EXTRA_PACKAGENAME, componentName.getPackageName());
         intent.putExtra(INTENT_EXTRA_ACTIVITY_NAME, componentName.getClassName());
-        context.sendBroadcast(intent);
+
+        BroadcastHelper.sendDefaultIntentExplicitly(context, intent);
     }
 
     @Override
