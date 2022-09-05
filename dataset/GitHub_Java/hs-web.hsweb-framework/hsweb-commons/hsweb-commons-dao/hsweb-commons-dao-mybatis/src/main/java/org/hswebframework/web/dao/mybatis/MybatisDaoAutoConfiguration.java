5c5c7f74ea3af3331b210030cf031f62bdeff9c8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:29:03 +0200

    ä¼åMapperScan

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-dao/hsweb-commons-dao-mybatis/src/main/java/org/hswebframework/web/dao/mybatis/MybatisDaoAutoConfiguration.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-dao/hsweb-commons-dao-mybatis/src/main/java/org/hswebframework/web/dao/mybatis/MybatisDaoAutoConfiguration.java
@@ -40,7 +40,7 @@ import java.util.Set;
 @ComponentScan("org.hswebframework.web.dao.mybatis")
 @MapperScan(value = "org.hswebframework.web.dao"
         , markerInterface = Dao.class
-        , sqlSessionFactoryRef = "sqlSessionFactory")
+        ,sqlSessionFactoryRef = "sqlSessionFactory")
 @AutoConfigureAfter(MyBatisAutoConfiguration.class)
 @EnableConfigurationProperties(MybatisProperties.class)
 public class MybatisDaoAutoConfiguration {
@@ -71,7 +71,6 @@ public class MybatisDaoAutoConfiguration {
                 Dialect.H2
                 , Dialect.MYSQL
                 , Dialect.ORACLE
-                , Dialect.POSTGRES
                 , Dialect.MSSQL);
 
         return new BeanPostProcessor() {
