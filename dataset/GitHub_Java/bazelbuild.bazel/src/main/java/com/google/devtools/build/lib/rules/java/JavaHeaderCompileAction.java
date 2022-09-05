d8db6009b00779b8e0c68a7f2a6cd46e0b73e2a4
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:19:09 +0200

    Refactor JavaHeaderCompileAction command line and input collection
        Make the distinction between transitive and direct inputs clearer,
        and decouple command line construction from the use of params files.
    
        --
        PiperOrigin-RevId: 141341256
        MOS_MIGRATED_REVID=141341256

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaHeaderCompileAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaHeaderCompileAction.java
@@ -298,6 +298,7 @@ public class JavaHeaderCompileAction extends SpawnAction {
           NestedSetBuilder.<Artifact>stableOrder()
               .addTransitive(directInputs)
               .addTransitive(classpathEntries)
+              .addAll(bootclasspathEntries)
               .addAll(processorPath)
               .addAll(compileTimeDependencyArtifacts)
               .add(paramsFile)
