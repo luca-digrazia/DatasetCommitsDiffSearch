7baffc85efcb3e61aceafe5d42b513f34ec6d903
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:30:25 +0200

    Clean up JmxReporter a little.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/codahale/metrics/JmxReporter.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/codahale/metrics/JmxReporter.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.management.*;
-import java.io.Closeable;
 import java.lang.management.ManagementFactory;
 import java.util.Collections;
 import java.util.Locale;
@@ -16,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * A reporter which listens for new metrics and exposes them as namespaced MBeans.
  */
-public class JmxReporter implements Closeable {
+public class JmxReporter {
     /**
      * Returns a new {@link Builder} for {@link JmxReporter}.
      *
@@ -703,13 +702,5 @@ public class JmxReporter implements Closeable {
     public void stop() {
         registry.removeListener(listener);
         listener.unregisterAll();
-    }
-
-    /**
-     * Stops the reporter.
-     */
-    @Override
-    public void close() {
-        stop();
     }
 }
