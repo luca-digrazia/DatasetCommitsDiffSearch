f17fb09c497404f12408990c349a1b729e33f649
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:35:59 +0200

    ä¿®å¤staticå­æ®µä¹è¢«å å¥å°æ°æ®è§å¾ä¸­äº

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-starter/src/main/java/org/hswebframework/web/authorization/starter/AutoSyncPermission.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-starter/src/main/java/org/hswebframework/web/authorization/starter/AutoSyncPermission.java
@@ -1,17 +1,25 @@
 package org.hswebframework.web.authorization.starter;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.extern.slf4j.Slf4j;
+import org.hswebframework.utils.ClassUtils;
 import org.hswebframework.web.authorization.Permission;
+import org.hswebframework.web.authorization.define.AopAuthorizeDefinition;
 import org.hswebframework.web.authorization.define.AuthorizeDefinition;
 import org.hswebframework.web.authorization.define.AuthorizeDefinitionInitializedEvent;
 import org.hswebframework.web.commons.entity.DataStatus;
 import org.hswebframework.web.commons.entity.factory.EntityFactory;
 import org.hswebframework.web.entity.authorization.ActionEntity;
+import org.hswebframework.web.entity.authorization.OptionalField;
 import org.hswebframework.web.entity.authorization.PermissionEntity;
 import org.hswebframework.web.service.authorization.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -20,10 +28,10 @@ import java.util.stream.Stream;
 @Slf4j
 public class AutoSyncPermission implements ApplicationListener<AuthorizeDefinitionInitializedEvent> {
 
-    @Autowired
+
     private PermissionService permissionService;
 
-    @Autowired
+
     private EntityFactory entityFactory;
 
     private static Map<String, String> actionDescMapping = new HashMap<>();
@@ -41,93 +49,142 @@ public class AutoSyncPermission implements ApplicationListener<AuthorizeDefiniti
 
     }
 
+    @Autowired
+    public void setPermissionService(PermissionService permissionService) {
+        this.permissionService = permissionService;
+    }
+
+    @Autowired
+    public void setEntityFactory(EntityFactory entityFactory) {
+        this.entityFactory = entityFactory;
+    }
+
     @Override
     public void onApplicationEvent(AuthorizeDefinitionInitializedEvent event) {
         List<AuthorizeDefinition> definitions = event.getAllDefinition();
 
         Map<String, List<AuthorizeDefinition>> grouping = new HashMap<>();
 
-
+        //以permissionId分组
         for (AuthorizeDefinition definition : definitions) {
             for (String permissionId : definition.getPermissions()) {
                 grouping.computeIfAbsent(permissionId, id -> new ArrayList<>())
                         .add(definition);
             }
         }
-        Map<String, PermissionEntity> permissionEntityMap = new HashMap<>();
 
-        for (Map.Entry<String, List<AuthorizeDefinition>> permissionEntiry : grouping.entrySet()) {
-            String permissionId = permissionEntiry.getKey();
-            List<AuthorizeDefinition> allPermission = permissionEntiry.getValue();
-            if (allPermission.isEmpty()) {
+        //创建权限实体
+        Map<String, PermissionEntity> waitToSyncPermissions = new HashMap<>();
+        for (Map.Entry<String, List<AuthorizeDefinition>> permissionDefinition : grouping.entrySet()) {
+            String permissionId = permissionDefinition.getKey();
+            //一个权限的全部定义(一个permission多个action)
+            List<AuthorizeDefinition> allDefinition = permissionDefinition.getValue();
+            if (allDefinition.isEmpty()) {
                 return;
             }
-            AuthorizeDefinition tmp = allPermission.get(0);
-
-            List<String> descs = allPermission.stream()
+            AuthorizeDefinition tmp = allDefinition.get(0);
+            //action描述
+            List<String> actionDescription = allDefinition.stream()
                     .map(AuthorizeDefinition::getActionDescription)
                     .flatMap(Stream::of)
                     .collect(Collectors.toList());
-
-            List<String> actions = allPermission
+            //action
+            List<String> actions = allDefinition
                     .stream()
                     .map(AuthorizeDefinition::getActions)
                     .flatMap(Collection::stream)
                     .collect(Collectors.toList());
 
+            //创建action实体
             Set<ActionEntity> actionEntities = new HashSet<>(actions.size());
             if (!actions.isEmpty()) {
                 for (int i = 0; i < actions.size(); i++) {
                     String action = actions.get(i);
-                    String desc = descs.size() > i ? descs.get(i) : actionDescMapping.getOrDefault(actions.get(i), action);
+                    String desc = actionDescription.size() > i ? actionDescription.get(i) : actionDescMapping.getOrDefault(actions.get(i), action);
                     ActionEntity actionEntity = new ActionEntity();
                     actionEntity.setAction(action);
                     actionEntity.setDescribe(desc);
                     actionEntities.add(actionEntity);
                 }
             }
+            //创建permission
             PermissionEntity entity = entityFactory.newInstance(PermissionEntity.class);
+            if (tmp instanceof AopAuthorizeDefinition) {
+                AopAuthorizeDefinition aopAuthorizeDefinition = ((AopAuthorizeDefinition) tmp);
+                Class type = aopAuthorizeDefinition.getTargetClass();
+                Class genType = entityFactory.getInstanceType(ClassUtils.getGenericType(type));
+                List<OptionalField> optionalFields = new ArrayList<>();
+                entity.setOptionalFields(optionalFields);
+                if (genType != Object.class) {
+                    List<Field> fields = new ArrayList<>();
+
+                    ReflectionUtils.doWithFields(genType, fields::add, field -> field.getModifiers() != Modifier.STATIC);
+
+                    for (Field field : fields) {
+                        if ("id".equals(field.getName())) {
+                            continue;
+                        }
+                        ApiModelProperty property = field.getAnnotation(ApiModelProperty.class);
+                        OptionalField optionalField = new OptionalField();
+                        optionalField.setName(field.getName());
+                        if (null != property) {
+                            if (property.hidden()) {
+                                continue;
+                            }
+                            optionalField.setDescribe(property.value());
+                        }
+                        optionalFields.add(optionalField);
+                    }
+                }
+            }
             entity.setId(permissionId);
             entity.setName(tmp.getPermissionDescription().length > 0 ? tmp.getPermissionDescription()[0] : permissionId);
             entity.setActions(new ArrayList<>(actionEntities));
             entity.setType("default");
             entity.setStatus(DataStatus.STATUS_ENABLED);
-
-            permissionEntityMap.putIfAbsent(entity.getId(), entity);
+            waitToSyncPermissions.putIfAbsent(entity.getId(), entity);
         }
 
-        Map<String, PermissionEntity> old = permissionService
-                .selectByPk(new ArrayList<>(permissionEntityMap.keySet()))
+        //查询出全部旧的权限数据并载入缓存
+        Map<String, PermissionEntity> oldCache = permissionService
+                .select()
                 .stream()
                 .collect(Collectors.toMap(PermissionEntity::getId, Function.identity()));
 
-        permissionEntityMap.forEach((permissionId, permission) -> {
+        waitToSyncPermissions.forEach((permissionId, permission) -> {
             log.info("try sync permission[{}].{}", permissionId, permission.getActions());
-            PermissionEntity oldPermission = old.get(permissionId);
+            PermissionEntity oldPermission = oldCache.get(permissionId);
             if (oldPermission == null) {
                 permissionService.insert(permission);
             } else {
-                List<ActionEntity> oldAction = oldPermission.getActions();
-                if (oldAction == null) {
-                    oldAction = new ArrayList<>();
+                Set<ActionEntity> oldAction = new HashSet<>();
+                if (oldPermission.getActions() != null) {
+                    oldAction.addAll(oldPermission.getActions());
                 }
                 Map<String, ActionEntity> actionCache = oldAction
-                        .stream().collect(Collectors.toMap(ActionEntity::getAction, Function.identity()));
+                        .stream()
+                        .collect(Collectors.toMap(ActionEntity::getAction, Function.identity()));
                 boolean permissionChanged = false;
                 for (ActionEntity actionEntity : permission.getActions()) {
+                    //添加新的action到旧的action
                     if (actionCache.get(actionEntity.getAction()) == null) {
                         oldAction.add(actionEntity);
                         permissionChanged = true;
                     }
                 }
                 if (permissionChanged) {
-                    oldPermission.setActions(oldAction);
-
+                    oldPermission.setActions(new ArrayList<>(oldAction));
                     permissionService.updateByPk(oldPermission.getId(), oldPermission);
                 }
+                actionCache.clear();
             }
 
         });
 
+        oldCache.clear();
+        waitToSyncPermissions.clear();
+        definitions.clear();
+        grouping.clear();
     }
+
 }
