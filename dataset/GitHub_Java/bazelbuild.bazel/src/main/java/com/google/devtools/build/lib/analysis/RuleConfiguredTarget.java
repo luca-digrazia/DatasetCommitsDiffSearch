780e06647658b63e52600f6e7ed1ba6432f1e21b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:22:16 +0200

    Add a test that asserts aspects propagating along implicit deps.
        Fix a minor bug in the process.
    
        --
        MOS_MIGRATED_REVID=113519574

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTarget.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTarget.java
@@ -18,6 +18,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.UnmodifiableIterator;
+import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.config.ConfigMatchingProvider;
 import com.google.devtools.build.lib.analysis.config.RunUnder;
 import com.google.devtools.build.lib.packages.OutputFile;
@@ -50,10 +51,12 @@ public final class RuleConfiguredTarget extends AbstractConfiguredTarget {
   }
 
   private final ImmutableMap<Class<? extends TransitiveInfoProvider>, Object> providers;
+  private final ImmutableList<Artifact> mandatoryStampFiles;
   private final Set<ConfigMatchingProvider> configConditions;
   private final ImmutableList<ConfiguredAspect> configuredAspects;
 
   RuleConfiguredTarget(RuleContext ruleContext,
+      ImmutableList<Artifact> mandatoryStampFiles,
       ImmutableMap<String, Object> skylarkProviders,
       Map<Class<? extends TransitiveInfoProvider>, TransitiveInfoProvider> providers) {
     super(ruleContext);
@@ -75,6 +78,7 @@ public final class RuleConfiguredTarget extends AbstractConfiguredTarget {
     providerBuilder.put(SkylarkProviders.class, new SkylarkProviders(skylarkProviders));
 
     this.providers = ImmutableMap.copyOf(providerBuilder);
+    this.mandatoryStampFiles = mandatoryStampFiles;
     this.configConditions = ruleContext.getConfigConditions();
     this.configuredAspects = ImmutableList.of();
 
@@ -186,6 +190,7 @@ public final class RuleConfiguredTarget extends AbstractConfiguredTarget {
       }
       this.providers = builder.build();
     }
+    this.mandatoryStampFiles = base.mandatoryStampFiles;
     this.configConditions = base.configConditions;
     this.configuredAspects = ImmutableList.copyOf(aspects);
   }
@@ -239,6 +244,10 @@ public final class RuleConfiguredTarget extends AbstractConfiguredTarget {
   @Override
   public Object get(String providerKey) {
     return getProvider(SkylarkProviders.class).getValue(providerKey);
+  }
+
+  public ImmutableList<Artifact> getMandatoryStampFiles() {
+    return mandatoryStampFiles;
   }
 
   @Override
