c4e7918cac29c300aff9bddf229d6bfde64a5462
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:14:44 +0200

    Enable easier customizing of MetricsServlet. (#1285)
        - MetricsModule should now be easier to customize with other serializers.
        - setupMetricsModule can be overridden to facilitate introduction of new
          MetricsModule like objects.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-json/src/main/java/com/codahale/metrics/json/MetricsModule.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-json/src/main/java/com/codahale/metrics/json/MetricsModule.java
@@ -1,13 +1,19 @@
 package com.codahale.metrics.json;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Snapshot;
+import com.codahale.metrics.Timer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleSerializers;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.codahale.metrics.*;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -15,9 +21,13 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class MetricsModule extends Module {
-    static final Version VERSION = new Version(3, 0, 0, "", "com.codahale.metrics", "metrics-json");
+    static final Version VERSION = new Version(4, 0, 0, "", "io.dropwizard.metrics", "metrics-json");
 
+    @SuppressWarnings("rawtypes")
     private static class GaugeSerializer extends StdSerializer<Gauge> {
+
+        private static final long serialVersionUID = 1L;
+
         private GaugeSerializer() {
             super(Gauge.class);
         }
@@ -39,6 +49,9 @@ public class MetricsModule extends Module {
     }
 
     private static class CounterSerializer extends StdSerializer<Counter> {
+
+        private static final long serialVersionUID = 1L;
+
         private CounterSerializer() {
             super(Counter.class);
         }
@@ -54,6 +67,9 @@ public class MetricsModule extends Module {
     }
 
     private static class HistogramSerializer extends StdSerializer<Histogram> {
+
+        private static final long serialVersionUID = 1L;
+
         private final boolean showSamples;
 
         private HistogramSerializer(boolean showSamples) {
@@ -88,6 +104,9 @@ public class MetricsModule extends Module {
     }
 
     private static class MeterSerializer extends StdSerializer<Meter> {
+
+        private static final long serialVersionUID = 1L;
+
         private final String rateUnit;
         private final double rateFactor;
 
@@ -103,8 +122,8 @@ public class MetricsModule extends Module {
                               SerializerProvider provider) throws IOException {
             json.writeStartObject();
             json.writeNumberField("count", meter.getCount());
-            json.writeNumberField("m15_rate", meter.getOneMinuteRate() * rateFactor);
-            json.writeNumberField("m1_rate", meter.getFifteenMinuteRate() * rateFactor);
+            json.writeNumberField("m15_rate", meter.getFifteenMinuteRate() * rateFactor);
+            json.writeNumberField("m1_rate", meter.getOneMinuteRate() * rateFactor);
             json.writeNumberField("m5_rate", meter.getFiveMinuteRate() * rateFactor);
             json.writeNumberField("mean_rate", meter.getMeanRate() * rateFactor);
             json.writeStringField("units", rateUnit);
@@ -113,6 +132,9 @@ public class MetricsModule extends Module {
     }
 
     private static class TimerSerializer extends StdSerializer<Timer> {
+
+        private static final long serialVersionUID = 1L;
+
         private final String rateUnit;
         private final double rateFactor;
         private final String durationUnit;
@@ -158,8 +180,8 @@ public class MetricsModule extends Module {
             }
 
             json.writeNumberField("stddev", snapshot.getStdDev() * durationFactor);
-            json.writeNumberField("m15_rate", timer.getOneMinuteRate() * rateFactor);
-            json.writeNumberField("m1_rate", timer.getFifteenMinuteRate() * rateFactor);
+            json.writeNumberField("m15_rate", timer.getFifteenMinuteRate() * rateFactor);
+            json.writeNumberField("m1_rate", timer.getOneMinuteRate() * rateFactor);
             json.writeNumberField("m5_rate", timer.getFiveMinuteRate() * rateFactor);
             json.writeNumberField("mean_rate", timer.getMeanRate() * rateFactor);
             json.writeStringField("duration_units", durationUnit);
@@ -169,8 +191,14 @@ public class MetricsModule extends Module {
     }
 
     private static class MetricRegistrySerializer extends StdSerializer<MetricRegistry> {
-        private MetricRegistrySerializer() {
+
+        private static final long serialVersionUID = 1L;
+
+        private final MetricFilter filter;
+
+        private MetricRegistrySerializer(MetricFilter filter) {
             super(MetricRegistry.class);
+            this.filter = filter;
         }
 
         @Override
@@ -179,23 +207,29 @@ public class MetricsModule extends Module {
                               SerializerProvider provider) throws IOException {
             json.writeStartObject();
             json.writeStringField("version", VERSION.toString());
-            json.writeObjectField("gauges", registry.getGauges());
-            json.writeObjectField("counters", registry.getCounters());
-            json.writeObjectField("histograms", registry.getHistograms());
-            json.writeObjectField("meters", registry.getMeters());
-            json.writeObjectField("timers", registry.getTimers());
+            json.writeObjectField("gauges", registry.getGauges(filter));
+            json.writeObjectField("counters", registry.getCounters(filter));
+            json.writeObjectField("histograms", registry.getHistograms(filter));
+            json.writeObjectField("meters", registry.getMeters(filter));
+            json.writeObjectField("timers", registry.getTimers(filter));
             json.writeEndObject();
         }
     }
 
-    private final TimeUnit rateUnit;
-    private final TimeUnit durationUnit;
-    private final boolean showSamples;
+    protected final TimeUnit rateUnit;
+    protected final TimeUnit durationUnit;
+    protected final boolean showSamples;
+    protected final MetricFilter filter;
 
     public MetricsModule(TimeUnit rateUnit, TimeUnit durationUnit, boolean showSamples) {
+        this(rateUnit, durationUnit, showSamples, MetricFilter.ALL);
+    }
+
+    public MetricsModule(TimeUnit rateUnit, TimeUnit durationUnit, boolean showSamples, MetricFilter filter) {
         this.rateUnit = rateUnit;
         this.durationUnit = durationUnit;
         this.showSamples = showSamples;
+        this.filter = filter;
     }
 
     @Override
@@ -210,13 +244,13 @@ public class MetricsModule extends Module {
 
     @Override
     public void setupModule(SetupContext context) {
-        context.addSerializers(new SimpleSerializers(Arrays.<JsonSerializer<?>>asList(
+        context.addSerializers(new SimpleSerializers(Arrays.asList(
                 new GaugeSerializer(),
                 new CounterSerializer(),
                 new HistogramSerializer(showSamples),
                 new MeterSerializer(rateUnit),
                 new TimerSerializer(rateUnit, durationUnit, showSamples),
-                new MetricRegistrySerializer()
+                new MetricRegistrySerializer(filter)
         )));
     }
 
