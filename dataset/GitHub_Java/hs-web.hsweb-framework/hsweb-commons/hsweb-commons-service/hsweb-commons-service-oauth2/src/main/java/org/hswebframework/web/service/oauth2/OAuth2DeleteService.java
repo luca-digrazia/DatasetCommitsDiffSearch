2402149584ffcf1201a98348dc4100594894f870
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:42:04 +0200

    ä¼åoauth2 serviceå®ç°

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-oauth2/src/main/java/org/hswebframework/web/service/oauth2/OAuth2DeleteService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-oauth2/src/main/java/org/hswebframework/web/service/oauth2/OAuth2DeleteService.java
@@ -2,9 +2,9 @@ package org.hswebframework.web.service.oauth2;
 
 import org.hswebframework.web.service.DeleteService;
 
-public interface OAuth2DeleteService<E, PK> extends DeleteService<E, PK>, OAuth2ServiceSupport {
+public interface OAuth2DeleteService<PK> extends DeleteService<PK>, OAuth2ServiceSupport {
     @Override
-    default E deleteByPk(PK pk) {
-        return createRequest("/" + pk).delete().as(getEntityType());
+    default int deleteByPk(PK pk) {
+        return createRequest("/" + pk).delete().as(Integer.class);
     }
 }
