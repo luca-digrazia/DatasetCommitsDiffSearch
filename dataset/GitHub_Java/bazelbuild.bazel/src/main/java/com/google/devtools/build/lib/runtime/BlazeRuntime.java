606d6dadba02c3cd0edaa632eda06d2a792d47f8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:06:24 +0200

    Dedupe code: parse startup options in newRuntime, instead of calling it twice.
        --
        MOS_MIGRATED_REVID=129856323

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/BlazeRuntime.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/BlazeRuntime.java
@@ -882,9 +882,11 @@ public final class BlazeRuntime {
           "Bad --output_base option specified: '" + outputBase + "'");
     }
 
+    PathFragment outputPathFragment = BlazeDirectories.outputPathFromOutputBase(
+        outputBase, workspaceDirectory, startupOptions.deepExecRoot, productName);
     FileSystem fs = null;
     for (BlazeModule module : blazeModules) {
-      FileSystem moduleFs = module.getFileSystem(options);
+      FileSystem moduleFs = module.getFileSystem(options, outputPathFragment);
       if (moduleFs != null) {
         Preconditions.checkState(fs == null, "more than one module returns a file system");
         fs = moduleFs;
