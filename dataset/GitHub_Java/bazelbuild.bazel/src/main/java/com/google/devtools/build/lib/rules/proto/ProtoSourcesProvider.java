88d34b117bd4a3c034650957e1a5a4473f6d25aa
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:08:15 +0200

    Expose proto sources in the Skylark provider
        --
        MOS_MIGRATED_REVID=103947930

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/proto/ProtoSourcesProvider.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/proto/ProtoSourcesProvider.java
@@ -14,107 +14,72 @@
 
 package com.google.devtools.build.lib.rules.proto;
 
-import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.TransitiveInfoProvider;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
-import com.google.devtools.build.lib.skylarkinterface.SkylarkCallable;
-import com.google.devtools.build.lib.skylarkinterface.SkylarkModule;
-import javax.annotation.Nullable;
+import com.google.devtools.build.lib.syntax.SkylarkCallable;
+import com.google.devtools.build.lib.syntax.SkylarkModule;
 
-// TODO(carmi): Rename the class to ProtoInfoProvider.
 /**
  * Configured target classes that implement this class can contribute .proto files to the
  * compilation of proto_library rules.
  */
-@AutoValue
 @Immutable
 @SkylarkModule(name = "ProtoSourcesProvider", doc = "")
-public abstract class ProtoSourcesProvider implements TransitiveInfoProvider {
+public final class ProtoSourcesProvider implements TransitiveInfoProvider {
   /** The name of the field in Skylark used to access this class. */
   public static final String SKYLARK_NAME = "proto";
 
-  public static ProtoSourcesProvider create(
+  private final NestedSet<Artifact> transitiveImports;
+  private final NestedSet<Artifact> transitiveProtoSources;
+  private final ImmutableList<Artifact> protoSources;
+
+  public ProtoSourcesProvider(
       NestedSet<Artifact> transitiveImports,
       NestedSet<Artifact> transitiveProtoSources,
-      ImmutableList<Artifact> protoSources,
-      ImmutableList<Artifact> checkDepsProtoSources,
-      @Nullable Artifact descriptorSet) {
-    return new AutoValue_ProtoSourcesProvider(
-        transitiveImports,
-        transitiveProtoSources,
-        protoSources,
-        checkDepsProtoSources,
-        descriptorSet);
+      ImmutableList<Artifact> protoSources) {
+    this.transitiveImports = transitiveImports;
+    this.transitiveProtoSources = transitiveProtoSources;
+    this.protoSources = protoSources;
   }
 
   /**
-   * Transitive imports including weak dependencies This determines the order of "-I" arguments to
-   * the protocol compiler, and that is probably important
+   * Transitive imports including weak dependencies
+   * This determines the order of "-I" arguments to the protocol compiler, and
+   * that is probably important
    */
   @SkylarkCallable(
-    name = "transitive_imports",
-    doc = "Transitive imports including weak dependencies.",
-    structField = true
-  )
-  public abstract NestedSet<Artifact> getTransitiveImports();
+      name = "transitive_imports",
+      doc = "Transitive imports including weak dependencies",
+      structField = true)
+  public NestedSet<Artifact> getTransitiveImports() {
+    return transitiveImports;
+  }
 
   /**
    * Returns the proto sources for this rule and all its dependent protocol
    * buffer rules.
    */
   @SkylarkCallable(
-    name = "transitive_sources",
-    doc = "Proto sources for this rule and all its dependent protocol buffer rules.",
-    structField = true
-  )
-  // TODO(bazel-team): The difference between transitive imports and transitive proto sources
-  // should never be used by Skylark or by an Aspect. One of these two should be removed,
-  // preferably soon, before Skylark users start depending on them.
-  public abstract NestedSet<Artifact> getTransitiveProtoSources();
-
-  /**
-   * Returns the proto sources from the 'srcs' attribute.
-   */
-  // TODO(bazel-team): This should be NestedSets.
-  @SkylarkCallable(
-    name = "direct_sources",
-    doc = "Proto sources from the 'srcs' attribute.",
-    structField = true
-  )
-  public abstract ImmutableList<Artifact> getDirectProtoSources();
+      name = "transitive_sources",
+      doc = "Proto sources for this rule and all its dependent protocol buffer rules.",
+      structField = true)
+  public NestedSet<Artifact> getTransitiveProtoSources() {
+    return transitiveProtoSources;
+  }
 
   /**
    * Returns the proto sources from the 'srcs' attribute. If the library is a proxy library
    * that has no sources, return the sources from the direct deps.
    */
-  // TODO(bazel-team): This should be NestedSets.
   @SkylarkCallable(
-    name = "check_deps_sources",
-    doc =
-        "Proto sources from the 'srcs' attribute. If the library is a proxy library "
-            + "that has no sources, it contains the check_deps_sources"
-            + "from this library's direct deps.",
-    structField = true
-  )
-  public abstract ImmutableList<Artifact> getCheckDepsProtoSources();
-
-  /**
-   * Be careful while using this artifact - it is the parsing of the transitive set of .proto files.
-   * It's possible to cause a O(n^2) behavior, where n is the length of a proto chain-graph.
-   */
-  @SkylarkCallable(
-    name = "descriptor_set",
-    doc =
-        "The FileDescriptorSet of all transitive sources. Returns None if "
-            + "--output_descriptor_set isn't enabled or if there are no sources",
-    structField = true,
-    allowReturnNones = true
-  )
-  @Nullable
-  public abstract Artifact descriptorSet();
-
-  ProtoSourcesProvider() {}
+      name = "sources",
+      doc = "Proto sources from the 'srcs' attribute. If the library is a proxy library "
+          + "that has no sources, it contains the sources from the direct deps.",
+      structField = true)
+  public ImmutableList<Artifact> getProtoSources() {
+    return protoSources;
+  }
 }
