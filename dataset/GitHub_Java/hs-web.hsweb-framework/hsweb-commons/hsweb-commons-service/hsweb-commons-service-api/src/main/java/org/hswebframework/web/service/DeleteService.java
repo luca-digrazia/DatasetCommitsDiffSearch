6f842d418d1b5f5d45b059d465b749e35302e881
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:36:23 +0200

    ä¼åè¯´æ

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-api/src/main/java/org/hswebframework/web/service/DeleteService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-service/hsweb-commons-service-api/src/main/java/org/hswebframework/web/service/DeleteService.java
@@ -21,13 +21,13 @@ package org.hswebframework.web.service;
 /**
  * @author zhouhao
  */
-public interface DeleteService<E,PK> extends Service {
+public interface DeleteService<PK> extends Service {
     /**
      * 根据主键删除记录
      *
      * @param pk 主键
      * @return 影响记录数
      */
-    E deleteByPk(PK pk);
+    int deleteByPk(PK pk);
 
 }
