959f3886f2ef1ae7fbd5b4f8fc38791f46295b13
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:54:21 +0200

    Create a value class PatternWithEquality to wrap Pattern so that equal strings can compare equal as Patterns.
        --
        MOS_MIGRATED_REVID=96041927

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppOptions.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppOptions.java
@@ -24,11 +24,11 @@ import com.google.devtools.build.lib.analysis.config.BuildConfiguration.LabelCon
 import com.google.devtools.build.lib.analysis.config.CompilationMode;
 import com.google.devtools.build.lib.analysis.config.FragmentOptions;
 import com.google.devtools.build.lib.analysis.config.PerLabelOptions;
-import com.google.devtools.build.lib.cmdline.LabelSyntaxException;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration.HeadersCheckingMode;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration.LibcTop;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration.StripMode;
 import com.google.devtools.build.lib.syntax.Label;
+import com.google.devtools.build.lib.syntax.Label.SyntaxException;
 import com.google.devtools.build.lib.util.OptionsUtils;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.build.lib.view.config.crosstool.CrosstoolConfig.LipoMode;
@@ -134,7 +134,7 @@ public class CppOptions extends FragmentOptions {
       try {
         Label label = Label.parseAbsolute(input).getRelative(LIBC_RELATIVE_LABEL);
         return new LibcTop(label);
-      } catch (LabelSyntaxException e) {
+      } catch (SyntaxException e) {
         throw new OptionsParsingException(e.getMessage());
       }
     }
@@ -192,8 +192,8 @@ public class CppOptions extends FragmentOptions {
   @Option(name = "thin_archives",
           defaultValue = "false",
           category = "strategy",  // but also adds edges to the action graph
-          help = "Pass the 'T' flag to ar if supported by the toolchain. "
-                 + "All supported toolchains support this setting.")
+          help = "Pass the 'T' flag to ar if supported by the toolchain. " +
+                 "All supported toolchains support this setting.")
   public boolean useThinArchives;
 
   // O intrepid reaper of unused options: Be warned that the [no]start_end_lib
@@ -353,8 +353,7 @@ public class CppOptions extends FragmentOptions {
           category = "flags",
           implicitRequirements = {"--copt=-Wno-error"},
           help = "Generate binaries with FDO instrumentation. Specify the relative " +
-                 "directory name for the .gcda files at runtime. It also accepts " +
-                 "an LLVM profile output file path.")
+                 "directory name for the .gcda files at runtime.")
   public PathFragment fdoInstrument;
 
   @Option(name = "fdo_optimize",
@@ -365,7 +364,7 @@ public class CppOptions extends FragmentOptions {
                  "an auto profile. This flag also accepts files specified as labels, for " +
                  "example //foo/bar:file.afdo. Such labels must refer to input files; you may " +
                  "need to add an exports_files directive to the corresponding package to make " +
-                 "the file visible to Blaze. It also accepts an indexed LLVM profile file.")
+                 "the file visible to Blaze.")
   public String fdoOptimize;
 
   @Option(name = "autofdo_lipo_data",
