10b32cceebf0106a9112baed5d161980afb20d3a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:09:21 +0200

    æ°å¢èªå¨å°INæ¡ä»¶å¼è½¬ä¸ºListå¯¹è±¡

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-bean/src/main/java/org/hsweb/web/bean/common/SqlParam.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-bean/src/main/java/org/hsweb/web/bean/common/SqlParam.java
@@ -1,7 +1,5 @@
 package org.hsweb.web.bean.common;
 
-import com.alibaba.fastjson.JSON;
-import org.webbuilder.utils.common.MapUtils;
 import org.webbuilder.utils.common.StringUtils;
 
 import java.util.*;
@@ -10,14 +8,10 @@ import java.util.*;
  * Created by zhouhao on 16-4-19.
  */
 public class SqlParam<R extends SqlParam> {
-
-    @Deprecated
-    protected Map<String, Object> term = new HashMap<>();
-
     /**
-     * 条件
+     * 执行条件
      */
-    protected List<Term> terms = new LinkedList<>();
+    protected Map<String, Object> term = new HashMap<>();
 
     /**
      * 指定要处理的字段
@@ -28,53 +22,6 @@ public class SqlParam<R extends SqlParam> {
      * 指定不处理的字段
      */
     protected Set<String> excludes = new LinkedHashSet<>();
-
-    public R or(String termString, Object value) {
-        Term term = new Term();
-        term.setField(termString);
-        term.setValue(value);
-        term.setType(Term.Type.or);
-        terms.add(term);
-        return (R) this;
-    }
-
-    public R and(String termString, Object value) {
-        Term term = new Term();
-        term.setField(termString);
-        term.setValue(value);
-        term.setType(Term.Type.and);
-        terms.add(term);
-        return (R) this;
-    }
-
-    public Term nest() {
-        return nest(null, null);
-    }
-
-    public Term orNest() {
-        return orNest(null, null);
-    }
-
-    public Term nest(String termString, Object value) {
-        Term term = new Term();
-        term.setField(termString);
-        term.setValue(value);
-        term.setType(Term.Type.and);
-        term.setNest(true);
-        terms.add(term);
-        return term;
-    }
-
-    public Term orNest(String termString, Object value) {
-        Term term = new Term();
-        term.setField(termString);
-        term.setValue(value);
-        term.setType(Term.Type.or);
-        term.setNest(true);
-        terms.add(term);
-        return term;
-    }
-
 
     public R includes(String... fields) {
         includes.addAll(Arrays.asList(fields));
@@ -88,22 +35,26 @@ public class SqlParam<R extends SqlParam> {
     }
 
     public R where(String key, Object value) {
-        and(key, value);
+        this.term.put(key, changeTermValue(key, value));
         return (R) this;
     }
 
     public R where(Map<String, Object> conditions) {
-        initTermByMap(conditions);
+        changeTerm(conditions);
+        this.term.putAll(conditions);
         return (R) this;
     }
 
+    public Map<String, Object> getTerm() {
+        changeTerm(this.term);
+        return term;
+    }
+
     public Set<String> getIncludes() {
-        if (includes == null) includes = new LinkedHashSet<>();
         return includes;
     }
 
     public Set<String> getExcludes() {
-        if (excludes == null) excludes = new LinkedHashSet<>();
         return excludes;
     }
 
@@ -115,52 +66,43 @@ public class SqlParam<R extends SqlParam> {
         this.excludes = excludes;
     }
 
-    public List<Term> getTerms() {
-        return terms;
-    }
-
-    public void setTerms(List<Term> terms) {
-        this.terms = terms;
-    }
-
-    @Deprecated
-    public Map<String, Object> getTerm() {
-        if (term.isEmpty() && !terms.isEmpty()) {
-            terms.forEach(term1 -> {
-                if (!StringUtils.isNullOrEmpty(term1.getField()))
-                    term.put(term1.getField(), term1.getValue());
-            });
-        }
-        return term;
-    }
-
-    @Deprecated
-    public void setTerm(Map<String, Object> term) {
+    public R setTerm(Map<String, Object> term) {
         this.term = term;
+        return (R) this;
     }
 
-    @Override
-    public String toString() {
-        return JSON.toJSONString(this);
+    protected void changeTerm(Map<String, Object> term) {
+        if (term != null) {
+            term.entrySet().forEach((e) -> e.setValue(changeTermValue(e.getKey(), e.getValue())));
+        }
     }
 
-    protected void initTermByMap(Map<String, Object> param) {
-        param.forEach((k, v) -> {
-            String field = String.valueOf(param.get("field"));
-            Object value = param.get("value");
-            if (StringUtils.isNullOrEmpty(field) || StringUtils.isNullOrEmpty(value)) return;
-            String type = String.valueOf(param.get("type"));
-            String queryType = String.valueOf(param.get("queryType"));
-            Term nest = new Term();
-            nest.setField(field);
-            nest.setValue(value);
-            nest.setType(Term.Type.valueOf(type));
-            nest.setTermType(TermType.valueOf(queryType));
-            terms.add(nest);
-        });
+    public Object changeTermValue(String key, Object value) {
+        //将IN条件的值转换为Iterable
+        if (key.endsWith("$IN")) {
+            if (value == null) return new ArrayList<>();
+            if (!(value instanceof Iterable)) {
+                if (value instanceof String) {
+                    String[] arr = ((String) value).split("[, ;]");
+                    Object[] objArr = new Object[arr.length];
+                    for (int i = 0; i < arr.length; i++) {
+                        String str = arr[i];
+                        Object val = str;
+                        if (StringUtils.isInt(str))
+                            val = StringUtils.toInt(str);
+                        else if (StringUtils.isDouble(str))
+                            val = StringUtils.toDouble(str);
+                        objArr[i] = val;
+                    }
+                    return Arrays.asList(objArr);
+                } else if (value.getClass().isArray()) {
+                    return Arrays.asList(((Object[]) value));
+                } else {
+                    return Arrays.asList(value);
+                }
+            }
+        }
+        return value;
     }
 
-    public static  SqlParam build() {
-        return new SqlParam<>();
-    }
 }
