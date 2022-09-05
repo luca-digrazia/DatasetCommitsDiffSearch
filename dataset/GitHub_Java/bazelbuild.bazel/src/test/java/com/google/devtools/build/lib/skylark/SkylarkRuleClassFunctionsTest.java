f0e7ca39408cae26559137f2e560c6cfaaaabda8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:10:12 +0200

    Add JSON conversion for Skylark structs.
        This change adds a to_json method on Skylark structs, paralleling the
        existing to_proto built-in.
    
        RELNOTES[NEW]: Adds a to_json method to Skylark structs, providing conversion to JSON format.
    
        --
        MOS_MIGRATED_REVID=115873229

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/skylark/SkylarkRuleClassFunctionsTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/skylark/SkylarkRuleClassFunctionsTest.java
@@ -599,10 +599,7 @@ public class SkylarkRuleClassFunctionsTest extends SkylarkTestCase {
   public void testJsonEscapes() throws Exception {
     checkJson("struct(name='a\"b').to_json()", "{\"name\":\"a\\\"b\"}");
     checkJson("struct(name='a\\'b').to_json()", "{\"name\":\"a'b\"}");
-    checkJson("struct(name='a\\\\b').to_json()", "{\"name\":\"a\\\\b\"}");
     checkJson("struct(name='a\\nb').to_json()", "{\"name\":\"a\\nb\"}");
-    checkJson("struct(name='a\\rb').to_json()", "{\"name\":\"a\\rb\"}");
-    checkJson("struct(name='a\\tb').to_json()", "{\"name\":\"a\\tb\"}");
   }
 
   @Test
