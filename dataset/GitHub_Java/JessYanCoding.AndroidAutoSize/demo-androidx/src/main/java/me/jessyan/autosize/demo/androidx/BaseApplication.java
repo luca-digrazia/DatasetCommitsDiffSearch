7bd8584936deee3ae792f2074b9f794197bfb38e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:53:37 +0200

    ä¼å: å¯¹å¨æäºè®¾å¤ä¸ AndroidAutoSize åæ¶èªå¨å¯å¨çè§£å³æ¹æ¡

--- dataset/GitHub_Java/JessYanCoding.AndroidAutoSize/demo-androidx/src/main/java/me/jessyan/autosize/demo/androidx/BaseApplication.java
+++ dataset/GitHub_Java/JessYanCoding.AndroidAutoSize/demo-androidx/src/main/java/me/jessyan/autosize/demo/androidx/BaseApplication.java
@@ -49,8 +49,14 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         //当 App 中出现多进程, 并且您需要适配所有的进程, 就需要在 App 初始化时调用 initCompatMultiProcess()
-        //在 Demo 中跳转的三方库中的 DefaultErrorActivity 就是在另外一个进程中, 所以要想适配这个 Activity 就需要调用 initCompatMultiProcess()
         AutoSize.initCompatMultiProcess(this);
+
+        //如果在某些特殊情况下出现 InitProvider 未能正常实例化, 导致 AndroidAutoSize 未能完成初始化
+        //可以主动调用 AutoSize.checkAndInit(this) 方法, 完成 AndroidAutoSize 的初始化后即可正常使用
+//        AutoSize.checkAndInit(this);
+
+//        如何控制 AndroidAutoSize 的初始化，让 AndroidAutoSize 在某些设备上不自动启动？https://github.com/JessYanCoding/AndroidAutoSize/issues/249
+
         /**
          * 以下是 AndroidAutoSize 可以自定义的参数, {@link AutoSizeConfig} 的每个方法的注释都写的很详细
          * 使用前请一定记得跳进源码，查看方法的注释, 下面的注释只是简单描述!!!
@@ -93,6 +99,7 @@ public class BaseApplication extends Application {
                 //是否使用设备的实际尺寸做适配, 默认为 false, 如果设置为 false, 在以屏幕高度为基准进行适配时
                 //AutoSize 会将屏幕总高度减去状态栏高度来做适配
                 //设置为 true 则使用设备的实际屏幕高度, 不会减去状态栏高度
+                //在全面屏或刘海屏幕设备中, 获取到的屏幕高度可能不包含状态栏高度, 所以在全面屏设备中不需要减去状态栏高度，所以可以 setUseDeviceSize(true)
 //                .setUseDeviceSize(true)
 
                 //是否全局按照宽度进行等比例适配, 默认为 true, 如果设置为 false, AutoSize 会全局按照高度进行适配
