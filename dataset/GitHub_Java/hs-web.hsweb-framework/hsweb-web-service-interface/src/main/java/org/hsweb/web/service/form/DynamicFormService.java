2a9f321896f863aa3dcaac3568d118297fcf4078
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:09:03 +0200

    æ°å¢å¨æè¡¨åæä½æå¡ç±»

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-interface/src/main/java/org/hsweb/web/service/form/DynamicFormService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-interface/src/main/java/org/hsweb/web/service/form/DynamicFormService.java
@@ -1,6 +1,7 @@
 package org.hsweb.web.service.form;
 
-import org.hsweb.web.bean.common.*;
+import org.hsweb.web.bean.common.PagerResult;
+import org.hsweb.web.bean.common.QueryParam;
 import org.hsweb.web.bean.po.form.Form;
 
 import java.util.List;
@@ -21,11 +22,11 @@ public interface DynamicFormService {
 
     int total(String name, QueryParam param) throws Exception;
 
-    int insert(String name, InsertParam<Map<String, Object>> data) throws Exception;
+    int insert(String name, Map<String, Object> data) throws Exception;
 
-    int delete(String name,DeleteParam param) throws Exception;
+    int delete(String name, Map<String, Object> data) throws Exception;
 
-    int update(String name, UpdateParam<Map<String, Object>> param) throws Exception;
+    int update(String name, Map<String, Object> data) throws Exception;
 
     <T> T selectByPk(String name, Object pk) throws Exception;
 }
