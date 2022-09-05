c9bcfccf465d0e0eb47bd576dcdd8188445f6c8d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:41:22 +0200

    Stop catching RuntimeException. This made the errors in issue #1234 more confusing. And we should never catch RuntimeException unless we're about to crash.
        --
        MOS_MIGRATED_REVID=122031190

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/Statement.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/syntax/Statement.java
@@ -13,6 +13,12 @@
 // limitations under the License.
 package com.google.devtools.build.lib.syntax;
 
+import com.google.common.base.Optional;
+import com.google.devtools.build.lib.syntax.compiler.DebugInfo;
+import com.google.devtools.build.lib.syntax.compiler.LoopLabels;
+import com.google.devtools.build.lib.syntax.compiler.VariableScope;
+
+import net.bytebuddy.implementation.bytecode.ByteCodeAppender;
 
 /**
  * Base class for all statements nodes in the AST.
@@ -44,4 +50,35 @@ public abstract class Statement extends ASTNode {
    * @throws InterruptedException may be thrown in a sub class.
    */
   abstract void doExec(Environment env) throws EvalException, InterruptedException;
+
+  /**
+   * Checks the semantics of the Statement using the Environment according to
+   * the rules of the Skylark language. The Environment can be used e.g. to check
+   * variable type collision, read only variables, detecting recursion, existence of
+   * built-in variables, functions, etc.
+   *
+   * <p>The semantical check should be performed after the Skylark extension is loaded
+   * (i.e. is syntactically correct) and before is executed. The point of the semantical check
+   * is to make sure (as much as possible) that no error can occur during execution (Skylark
+   * programmers get a "compile time" error). It should also check execution branches (e.g. in
+   * if statements) that otherwise might never get executed.
+   *
+   * @throws EvalException if the Statement has a semantical error.
+   */
+  abstract void validate(ValidationEnvironment env) throws EvalException;
+
+  /**
+   * Builds a {@link ByteCodeAppender} that implements this statement.
+   *
+   * <p>A statement implementation should never require any particular state of the byte code
+   * stack and should leave it in the state it was before.
+   *
+   * @throws EvalException for any error that would have occurred during evaluation of the
+   *    function definition that contains this statement, e.g. type errors.
+   */
+  ByteCodeAppender compile(
+      VariableScope scope, Optional<LoopLabels> loopLabels, DebugInfo debugInfo)
+      throws EvalException {
+    throw new UnsupportedOperationException(this.getClass().getSimpleName() + " unsupported.");
+  }
 }
