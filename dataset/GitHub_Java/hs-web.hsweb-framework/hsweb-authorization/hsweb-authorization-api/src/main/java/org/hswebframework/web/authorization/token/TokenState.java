298db722ddcb343a0e63a13baea285bc68a48483
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:34:18 +0200

    å¢å lockç¶æ

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-api/src/main/java/org/hswebframework/web/authorization/token/TokenState.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-api/src/main/java/org/hswebframework/web/authorization/token/TokenState.java
@@ -1,48 +1,31 @@
 package org.hswebframework.web.authorization.token;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.hswebframework.web.dict.EnumDict;
-
 /**
  * 令牌状态
  */
-@Getter
-@AllArgsConstructor
-public enum TokenState implements EnumDict<String> {
+public enum TokenState {
     /**
      * 正常，有效
      */
-    @Deprecated
-    effective("effective", "正常"),
-
-    /**
-     * 正常，有效
-     */
-    normal("normal","正常"),
+    effective,
 
     /**
      * 已被禁止访问
      */
-    deny("deny", "已被禁止访问"),
+    deny,
 
     /**
      * 已过期
      */
-    expired("expired", "用户未登录"),
+    expired,
 
     /**
      * 已被踢下线
-     * @see AllopatricLoginMode#offlineOther
      */
-    offline("offline", "用户已在其他地方登录"),
+    offline,
 
     /**
      * 锁定
      */
-    lock("lock", "登录状态已被锁定");
-
-    private String value;
-
-    private String text;
+    lock
 }
