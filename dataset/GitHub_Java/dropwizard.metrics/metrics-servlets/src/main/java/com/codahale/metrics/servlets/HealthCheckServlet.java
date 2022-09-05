c970578b8f71f9aa87e9758c4cf8106815c9a5a0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:11:25 +0200

    HealthCheckServlet object mapper and status indicator (#1871)
        - Allow overriding the `ObjectMapper` instance used in `HealthCheckServlet` with the servlet attribute `com.codahale.metrics.servlets.HealthCheckServlet.mapper`
        - Allow setting the HTTP status code used to indicate health to 200 (OK) with the servlet attribute `com.codahale.metrics.servlets.HealthCheckServlet.httpStatusIndicator`, or with the HTTP query parameter `httpStatusIndicator` per request
    
        Refs #1821

--- dataset/GitHub_Java/dropwizard.metrics/metrics-servlets/src/main/java/com/codahale/metrics/servlets/HealthCheckServlet.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-servlets/src/main/java/com/codahale/metrics/servlets/HealthCheckServlet.java
@@ -1,12 +1,16 @@
 package com.codahale.metrics.servlets;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.codahale.metrics.health.HealthCheck;
+import com.codahale.metrics.health.HealthCheckFilter;
 import com.codahale.metrics.health.HealthCheckRegistry;
 import com.codahale.metrics.json.HealthCheckModule;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,40 +22,122 @@ import java.util.SortedMap;
 import java.util.concurrent.ExecutorService;
 
 public class HealthCheckServlet extends HttpServlet {
+    public static abstract class ContextListener implements ServletContextListener {
+        /**
+         * @return the {@link HealthCheckRegistry} to inject into the servlet context.
+         */
+        protected abstract HealthCheckRegistry getHealthCheckRegistry();
+
+        /**
+         * @return the {@link ExecutorService} to inject into the servlet context, or {@code null}
+         * if the health checks should be run in the servlet worker thread.
+         */
+        protected ExecutorService getExecutorService() {
+            // don't use a thread pool by default
+            return null;
+        }
+
+        /**
+         * @return the {@link HealthCheckFilter} that shall be used to filter health checks,
+         * or {@link HealthCheckFilter#ALL} if the default should be used.
+         */
+        protected HealthCheckFilter getHealthCheckFilter() {
+            return HealthCheckFilter.ALL;
+        }
+
+        /**
+         * @return the {@link ObjectMapper} that shall be used to render health checks,
+         * or {@code null} if the default object mapper should be used.
+         */
+        protected ObjectMapper getObjectMapper() {
+            // don't use an object mapper by default
+            return null;
+        }
+
+        @Override
+        public void contextInitialized(ServletContextEvent event) {
+            final ServletContext context = event.getServletContext();
+            context.setAttribute(HEALTH_CHECK_REGISTRY, getHealthCheckRegistry());
+            context.setAttribute(HEALTH_CHECK_EXECUTOR, getExecutorService());
+            context.setAttribute(HEALTH_CHECK_MAPPER, getObjectMapper());
+        }
+
+        @Override
+        public void contextDestroyed(ServletContextEvent event) {
+            // no-op
+        }
+    }
+
     public static final String HEALTH_CHECK_REGISTRY = HealthCheckServlet.class.getCanonicalName() + ".registry";
     public static final String HEALTH_CHECK_EXECUTOR = HealthCheckServlet.class.getCanonicalName() + ".executor";
+    public static final String HEALTH_CHECK_FILTER = HealthCheckServlet.class.getCanonicalName() + ".healthCheckFilter";
+    public static final String HEALTH_CHECK_MAPPER = HealthCheckServlet.class.getCanonicalName() + ".mapper";
+    public static final String HEALTH_CHECK_HTTP_STATUS_INDICATOR = HealthCheckServlet.class.getCanonicalName() + ".httpStatusIndicator";
 
     private static final long serialVersionUID = -8432996484889177321L;
     private static final String CONTENT_TYPE = "application/json";
+    private static final String HTTP_STATUS_INDICATOR_PARAM = "httpStatusIndicator";
 
     private transient HealthCheckRegistry registry;
     private transient ExecutorService executorService;
+    private transient HealthCheckFilter filter;
     private transient ObjectMapper mapper;
+    private transient boolean httpStatusIndicator;
+
     public HealthCheckServlet() {
-    	super();
     }
+
     public HealthCheckServlet(HealthCheckRegistry registry) {
-    	this();
-    	this.registry = registry;
+        this.registry = registry;
     }
-    
+
     @Override
     public void init(ServletConfig config) throws ServletException {
-    	if (null == registry) {
-	        final Object registryAttr = config.getServletContext().getAttribute(HEALTH_CHECK_REGISTRY);
-	        if (registryAttr instanceof HealthCheckRegistry) {
-	            this.registry = (HealthCheckRegistry) registryAttr;
-	        } else {
-	            throw new ServletException("Couldn't find a HealthCheckRegistry instance.");
-	        }
-    	}
+        super.init(config);
 
-        final Object executorAttr = config.getServletContext().getAttribute(HEALTH_CHECK_EXECUTOR);
+        final ServletContext context = config.getServletContext();
+        if (null == registry) {
+            final Object registryAttr = context.getAttribute(HEALTH_CHECK_REGISTRY);
+            if (registryAttr instanceof HealthCheckRegistry) {
+                this.registry = (HealthCheckRegistry) registryAttr;
+            } else {
+                throw new ServletException("Couldn't find a HealthCheckRegistry instance.");
+            }
+        }
+
+        final Object executorAttr = context.getAttribute(HEALTH_CHECK_EXECUTOR);
         if (executorAttr instanceof ExecutorService) {
             this.executorService = (ExecutorService) executorAttr;
         }
 
-        this.mapper = new ObjectMapper().registerModule(new HealthCheckModule());
+        final Object filterAttr = context.getAttribute(HEALTH_CHECK_FILTER);
+        if (filterAttr instanceof HealthCheckFilter) {
+            filter = (HealthCheckFilter) filterAttr;
+        }
+        if (filter == null) {
+            filter = HealthCheckFilter.ALL;
+        }
+
+        final Object mapperAttr = context.getAttribute(HEALTH_CHECK_MAPPER);
+        if (mapperAttr instanceof ObjectMapper) {
+            this.mapper = (ObjectMapper) mapperAttr;
+        } else {
+            this.mapper = new ObjectMapper();
+        }
+        this.mapper.registerModule(new HealthCheckModule());
+
+        final Object httpStatusIndicatorAttr = context.getAttribute(HEALTH_CHECK_HTTP_STATUS_INDICATOR);
+        if (httpStatusIndicatorAttr instanceof Boolean) {
+            this.httpStatusIndicator = (Boolean) httpStatusIndicatorAttr;
+        } else {
+            this.httpStatusIndicator = true;
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        registry.shutdown();
     }
 
     @Override
@@ -63,18 +149,18 @@ public class HealthCheckServlet extends HttpServlet {
         if (results.isEmpty()) {
             resp.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
         } else {
-            if (isAllHealthy(results)) {
+            final String reqParameter = req.getParameter(HTTP_STATUS_INDICATOR_PARAM);
+            final boolean httpStatusIndicatorParam = Boolean.parseBoolean(reqParameter);
+            final boolean useHttpStatusForHealthCheck = reqParameter == null ? httpStatusIndicator : httpStatusIndicatorParam;
+            if (!useHttpStatusForHealthCheck || isAllHealthy(results)) {
                 resp.setStatus(HttpServletResponse.SC_OK);
             } else {
                 resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             }
         }
 
-        final OutputStream output = resp.getOutputStream();
-        try {
+        try (OutputStream output = resp.getOutputStream()) {
             getWriter(req).writeValue(output, results);
-        } finally {
-            output.close();
         }
     }
 
@@ -88,9 +174,9 @@ public class HealthCheckServlet extends HttpServlet {
 
     private SortedMap<String, HealthCheck.Result> runHealthChecks() {
         if (executorService == null) {
-            return registry.runHealthChecks();
+            return registry.runHealthChecks(filter);
         }
-        return registry.runHealthChecks(executorService);
+        return registry.runHealthChecks(executorService, filter);
     }
 
     private static boolean isAllHealthy(Map<String, HealthCheck.Result> results) {
@@ -100,5 +186,10 @@ public class HealthCheckServlet extends HttpServlet {
             }
         }
         return true;
+    }
+
+    // visible for testing
+    ObjectMapper getMapper() {
+        return mapper;
     }
 }
