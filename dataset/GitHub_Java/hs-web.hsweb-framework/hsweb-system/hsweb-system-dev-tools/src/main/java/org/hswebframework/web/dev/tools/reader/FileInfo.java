45df4b375f19c9fac855e3a3d9edee92dccf12e0
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:34:59 +0200

    å¢å æä»¶ç±»å

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-dev-tools/src/main/java/org/hswebframework/web/dev/tools/reader/FileInfo.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-dev-tools/src/main/java/org/hswebframework/web/dev/tools/reader/FileInfo.java
@@ -1,7 +1,5 @@
 package org.hswebframework.web.dev.tools.reader;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.File;
@@ -11,35 +9,29 @@ import java.io.File;
  * @since 3.0
  */
 @Data
-@ApiModel("文件信息")
 public class FileInfo {
-
-    @ApiModelProperty("文件名")
     private String name;
 
-    @ApiModelProperty("文件长度")
     private Long length;
 
-    @ApiModelProperty("父目录")
     private String parent;
 
-    @ApiModelProperty("文件长度")
     private String absolutePath;
 
-    @ApiModelProperty("是否为文件")
+    private String type;
+
     private boolean file;
 
-    @ApiModelProperty("是否为目录")
     private boolean dir;
 
-    public static FileInfo from(File file) {
-        FileInfo info = new FileInfo();
+    public static FileInfo from(File file){
+        FileInfo info=new FileInfo();
         info.setName(file.getName());
         info.setLength(file.length());
         info.setParent(file.getParent());
         info.setAbsolutePath(file.getAbsolutePath());
         info.setFile(file.isFile());
         info.setDir(file.isDirectory());
-        return info;
+        return  info;
     }
 }
