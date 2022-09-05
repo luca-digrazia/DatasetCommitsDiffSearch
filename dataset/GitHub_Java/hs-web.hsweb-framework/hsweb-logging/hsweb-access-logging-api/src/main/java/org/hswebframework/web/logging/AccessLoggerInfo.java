b1ccebf6bdb1a7ec26af3cf2e581dbcfe4575d5e
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:52:46 +0200

    ä¼åè¯·æ±æ¥å¿

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-logging/hsweb-access-logging-api/src/main/java/org/hswebframework/web/logging/AccessLoggerInfo.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-logging/hsweb-access-logging-api/src/main/java/org/hswebframework/web/logging/AccessLoggerInfo.java
@@ -214,7 +214,7 @@ public class AccessLoggerInfo {
         map.put("target", target != null ? target.getName() : "");
         Map<String, Object> newParameter = new LinkedHashMap<>(parameters);
         newParameter.entrySet().forEach(entry -> {
-            if (entry.getValue() != null) {
+            if (entry.getValue() != null && !(entry.getValue() instanceof Serializable)) {
                 entry.setValue(noSerialExchange.apply(entry.getValue()));
             }
         });
