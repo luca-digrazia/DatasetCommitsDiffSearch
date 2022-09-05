a0d41711dc889dd120bfd1fa333e0a0c8c492e14
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:23:49 +0200

    Update coverage documentation. Prepare for open sourcing CoverageCommand.
        Progress on #1118.
    
        --
        MOS_MIGRATED_REVID=140121703

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/test/CoverageReportActionFactory.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/test/CoverageReportActionFactory.java
@@ -1,4 +1,4 @@
-// Copyright 2014 Google Inc. All rights reserved.
+// Copyright 2014 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,28 +14,57 @@
 
 package com.google.devtools.build.lib.rules.test;
 
-import com.google.devtools.build.lib.actions.Action;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.devtools.build.lib.actions.ActionAnalysisMetadata;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.ArtifactFactory;
 import com.google.devtools.build.lib.actions.ArtifactOwner;
+import com.google.devtools.build.lib.analysis.BlazeDirectories;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
-
-import java.util.Set;
-
+import com.google.devtools.build.lib.events.EventHandler;
+import java.util.Collection;
 import javax.annotation.Nullable;
 
 /**
  * A factory class to create coverage report actions.
  */
 public interface CoverageReportActionFactory {
+  /**
+   * Wraps the necessary actions to get a coverage report as well as the final output artifacts.
+   * The lcovWriteAction creates a file containing a set of lcov files. This file is used as an
+   * input artifact for coverageReportAction. We are only interested about the output artifacts from
+   * coverageReportAction.
+   */
+  public static final class CoverageReportActionsWrapper {
+    private final ActionAnalysisMetadata lcovWriteAction;
+    private final ActionAnalysisMetadata coverageReportAction;
+
+    public CoverageReportActionsWrapper (
+        ActionAnalysisMetadata lcovWriteAction, ActionAnalysisMetadata coverageReportAction) {
+      this.lcovWriteAction = lcovWriteAction;
+      this.coverageReportAction = coverageReportAction;
+    }
+
+    public ImmutableList<ActionAnalysisMetadata> getActions() {
+      return ImmutableList.of(lcovWriteAction, coverageReportAction);
+    }
+
+    public ImmutableSet<Artifact> getCoverageOutputs() {
+      return coverageReportAction.getOutputs();
+    }
+  }
 
   /**
-   * Returns a coverage report Action. May return null if it's not necessary to create
-   * such an Action based on the input parameters and some other data available to
-   * the factory implementation, such as command line arguments.
+   * Returns a CoverageReportActionsWrapper. May return null if it's not necessary to create
+   * such Actions based on the input parameters and some other data available to the factory
+   * implementation, such as command line options.
    */
   @Nullable
-  public Action createCoverageReportAction(Iterable<ConfiguredTarget> targetsToTest,
-      Set<Artifact> baselineCoverageArtifacts,
+  CoverageReportActionsWrapper createCoverageReportActionsWrapper(
+      EventHandler eventHandler,
+      BlazeDirectories directories,
+      Collection<ConfiguredTarget> targetsToTest,
+      Iterable<Artifact> baselineCoverageArtifacts,
       ArtifactFactory artifactFactory, ArtifactOwner artifactOwner);
 }
