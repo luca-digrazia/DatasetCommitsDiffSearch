a69c45ccd2accac07ab4175b89e8a83021529bb3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:38:52 +0200

    Thread through a map of the attributes which we have serialized into postProcess().
        --
        MOS_MIGRATED_REVID=122542545

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/query2/output/ProtoOutputFormatter.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/query2/output/ProtoOutputFormatter.java
@@ -27,8 +27,9 @@ import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.graph.Digraph;
 import com.google.devtools.build.lib.packages.AggregatingAttributeMapper;
 import com.google.devtools.build.lib.packages.Attribute;
-import com.google.devtools.build.lib.packages.AttributeFormatter;
+import com.google.devtools.build.lib.packages.AttributeSerializer;
 import com.google.devtools.build.lib.packages.BuildType;
+import com.google.devtools.build.lib.packages.DependencyFilter;
 import com.google.devtools.build.lib.packages.EnvironmentGroup;
 import com.google.devtools.build.lib.packages.InputFile;
 import com.google.devtools.build.lib.packages.OutputFile;
@@ -46,6 +47,7 @@ import com.google.devtools.build.lib.query2.proto.proto2api.Build.GeneratedFile;
 import com.google.devtools.build.lib.query2.proto.proto2api.Build.QueryResult.Builder;
 import com.google.devtools.build.lib.query2.proto.proto2api.Build.SourceFile;
 import com.google.devtools.build.lib.syntax.Environment;
+
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Collection;
@@ -67,6 +69,9 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
    */
   public static final String RULE_IMPLEMENTATION_HASH_ATTR_NAME = "$rule_implementation_hash";
 
+  private transient DependencyFilter dependencyFilter;
+  protected transient AspectResolver aspectResolver;
+
   private boolean relativeLocations = false;
   protected boolean includeDefaultValues = true;
 
@@ -80,14 +85,13 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
   }
 
   @Override
-  public void setOptions(QueryOptions options, AspectResolver aspectResolver) {
-    super.setOptions(options, aspectResolver);
-    this.relativeLocations = options.relativeLocations;
+  public OutputFormatterCallback<Target> createStreamCallback(QueryOptions options,
+      final PrintStream out, AspectResolver aspectResolver) {
+    relativeLocations = options.relativeLocations;
+    this.aspectResolver = aspectResolver;
     this.includeDefaultValues = options.protoIncludeDefaultValues;
-  }
+    setDependencyFilter(options);
 
-  @Override
-  public OutputFormatterCallback<Target> createStreamCallback(final PrintStream out) {
     return new OutputFormatterCallback<Target>() {
 
       private Builder queryResult;
@@ -126,7 +130,8 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
   /**
    * Converts a logical {@link Target} object into a {@link Build.Target} protobuffer.
    */
-  protected Build.Target toTargetProtoBuffer(Target target) throws InterruptedException {
+  protected Build.Target toTargetProtoBuffer(Target target)
+      throws InterruptedException {
     Build.Target.Builder targetPb = Build.Target.newBuilder();
 
     String location = getLocation(target, relativeLocations);
@@ -150,10 +155,11 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
             AggregatingAttributeMapper.flattenAttributeValues(
                 attr.getType(), possibleAttributeValues);
         Build.Attribute serializedAttribute =
-            AttributeFormatter.getAttributeProto(
+            AttributeSerializer.getAttributeProto(
                 attr,
                 flattenedAttributeValue,
                 rule.isAttributeValueExplicitlySpecified(attr),
+                /*includeGlobs=*/ false,
                 /*encodeBooleanAndTriStateAsIntegerAndString=*/ true);
         rulePb.addAttribute(serializedAttribute);
         serializedAttributes.put(attr, serializedAttribute);
@@ -184,10 +190,11 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
         }
         Object attributeValue = getAspectAttributeValue(attribute, labels);
         Build.Attribute serializedAttribute =
-            AttributeFormatter.getAttributeProto(
+            AttributeSerializer.getAttributeProto(
                 attribute,
                 attributeValue,
                 /*explicitlySpecified=*/ false,
+                /*includeGlobs=*/ false,
                 /*encodeBooleanAndTriStateAsIntegerAndString=*/ true);
         rulePb.addAttribute(serializedAttribute);
       }
