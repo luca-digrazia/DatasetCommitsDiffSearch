731065161f73ee5d700c9c82198dd6f94416ada9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:39:10 +0200

    Add the coverage report generator to TestParams when the configuration says we should collect coverage, not only when the rule says so.
        --
        MOS_MIGRATED_REVID=135896208

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/test/TestActionBuilder.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/test/TestActionBuilder.java
@@ -14,8 +14,6 @@
 
 package com.google.devtools.build.lib.rules.test;
 
-import static com.google.devtools.build.lib.packages.BuildType.LABEL;
-
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -34,14 +32,15 @@ import com.google.devtools.build.lib.packages.TargetUtils;
 import com.google.devtools.build.lib.packages.TestSize;
 import com.google.devtools.build.lib.packages.TestTimeout;
 import com.google.devtools.build.lib.rules.test.TestProvider.TestParams;
-import com.google.devtools.build.lib.syntax.Type;
 import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.util.Preconditions;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.common.options.EnumConverter;
+
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
 import javax.annotation.Nullable;
 
 /**
@@ -88,7 +87,7 @@ public final class TestActionBuilder {
     // heuristically sharding is currently experimental. Also, we do detect
     // false-positive cases and return an error.
     return runfilesSupport.getRunfilesSymlinkNames().contains(
-        PathFragment.create("tools/test_sharding_compliant"));
+        new PathFragment("tools/test_sharding_compliant"));
   }
 
   /**
@@ -180,7 +179,7 @@ public final class TestActionBuilder {
    *    TestResultAnalyzer to keep track of completed and pending test runs.
    */
   private TestParams createTestAction(int shards) {
-    PathFragment targetName = PathFragment.create(ruleContext.getLabel().getName());
+    PathFragment targetName = new PathFragment(ruleContext.getLabel().getName());
     BuildConfiguration config = ruleContext.getConfiguration();
     AnalysisEnvironment env = ruleContext.getAnalysisEnvironment();
     Root root = config.getTestLogsDirectory(ruleContext.getRule().getRepository());
@@ -208,17 +207,7 @@ public final class TestActionBuilder {
       NestedSet<Artifact> metadataFiles = instrumentedFiles.getInstrumentationMetadataFiles();
       inputsBuilder.addTransitive(metadataFiles);
       inputsBuilder.addTransitive(PrerequisiteArtifacts.nestedSet(
-          ruleContext, "$coverage_support", Mode.DONT_CHECK));
-      // We don't add this attribute to non-supported test target
-      if (ruleContext.isAttrDefined("$lcov_merger", LABEL)) {
-        Artifact lcovMerger = ruleContext.getPrerequisiteArtifact("$lcov_merger", Mode.TARGET);
-        if (lcovMerger != null) {
-          inputsBuilder.addTransitive(
-              PrerequisiteArtifacts.nestedSet(ruleContext, "$lcov_merger", Mode.TARGET));
-          // Pass this LcovMerger_deploy.jar path to collect_coverage.sh
-          testEnv.put("LCOV_MERGER", lcovMerger.getExecPathString());
-        }
-      }
+          ruleContext, "$coverage_support", Mode.HOST));
 
       Artifact instrumentedFileManifest =
           InstrumentedFileManifestAction.getInstrumentedFileManifest(ruleContext,
@@ -249,11 +238,6 @@ public final class TestActionBuilder {
     int shardRuns = (shards > 0 ? shards : 1);
     List<Artifact> results = Lists.newArrayListWithCapacity(runsPerTest * shardRuns);
     ImmutableList.Builder<Artifact> coverageArtifacts = ImmutableList.builder();
-
-    boolean useTestRunner = false;
-    if (ruleContext.attributes().has("use_testrunner", Type.BOOLEAN)) {
-      useTestRunner = ruleContext.attributes().get("use_testrunner", Type.BOOLEAN);
-    }
 
     for (int run = 0; run < runsPerTest; run++) {
       // Use a 1-based index for user friendliness.
@@ -293,8 +277,7 @@ public final class TestActionBuilder {
             testLog, cacheStatus,
             coverageArtifact, microCoverageArtifact,
             testProperties, testEnv, executionSettings,
-            shard, run, config, ruleContext.getWorkspaceName(),
-            useTestRunner));
+            shard, run, config, ruleContext.getWorkspaceName()));
         results.add(cacheStatus);
       }
     }
