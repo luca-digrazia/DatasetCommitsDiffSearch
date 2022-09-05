7f175ace669d34b024114af18ad4949014696e90
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:28:00 +0200

    Include source jar count in progress message for java compile actions
        The avoids misleading "Building libfoo.jar (0 files)" progress messages for
        sourcejar-only compilations.
    
        --
        MOS_MIGRATED_REVID=112512879

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaCompileAction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/java/JavaCompileAction.java
@@ -412,6 +412,11 @@ public class JavaCompileAction extends AbstractAction {
   }
 
   @Override
+  public String describeStrategy(Executor executor) {
+    return getContext(executor).strategyLocality(getMnemonic(), true);
+  }
+
+  @Override
   public ResourceSet estimateResourceConsumption(Executor executor) {
     if (getContext(executor).isRemotable(getMnemonic(), true)) {
       return ResourceSet.ZERO;
