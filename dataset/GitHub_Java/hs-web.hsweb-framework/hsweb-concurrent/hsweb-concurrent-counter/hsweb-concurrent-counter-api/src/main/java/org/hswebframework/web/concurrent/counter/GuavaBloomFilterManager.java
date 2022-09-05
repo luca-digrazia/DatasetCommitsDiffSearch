beb83d9ee73baefa07ccc52f3a79a247d7508a9d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:26:08 +0200

    ä¼åbloom filter åå§å

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-concurrent/hsweb-concurrent-counter/hsweb-concurrent-counter-api/src/main/java/org/hswebframework/web/concurrent/counter/GuavaBloomFilterManager.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-concurrent/hsweb-concurrent-counter/hsweb-concurrent-counter-api/src/main/java/org/hswebframework/web/concurrent/counter/GuavaBloomFilterManager.java
@@ -17,16 +17,6 @@ public class GuavaBloomFilterManager extends AbstractBoomFilterManager {
     protected BloomFilter createBloomFilter(String name) {
         com.google.common.hash.BloomFilter<String>
                 filter = com.google.common.hash.BloomFilter.create((str, sink) -> sink.putString(str, StandardCharsets.UTF_8), expectedInsertions, fpp);
-        return new BloomFilter() {
-            @Override
-            public boolean put(String unique) {
-                return filter.put(unique);
-            }
-
-            @Override
-            public boolean contains(String unique) {
-                return filter.mightContain(unique);
-            }
-        };
+        return filter::put;
     }
 }
--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-concurrent/hsweb-concurrent-counter/hsweb-concurrent-counter-redis/src/main/java/org/hswebframework/web/counter/redis/RedisBloomFilterManager.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-concurrent/hsweb-concurrent-counter/hsweb-concurrent-counter-redis/src/main/java/org/hswebframework/web/counter/redis/RedisBloomFilterManager.java
@@ -16,16 +16,6 @@ public class RedisBloomFilterManager extends AbstractBoomFilterManager {
     protected BloomFilter createBloomFilter(String name) {
         RBloomFilter<String> filter = redissonClient.getBloomFilter("hsweb:bloom-filter:" + name, StringCodec.INSTANCE);
         filter.tryInit(55000000L, 0.01);
-        return new BloomFilter() {
-            @Override
-            public boolean put(String unique) {
-                return filter.add(unique);
-            }
-
-            @Override
-            public boolean contains(String unique) {
-                return filter.contains(unique);
-            }
-        };
+        return filter::add;
     }
 }
