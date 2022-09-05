554e5ac76c8884162352ce4b05e82353af9e7404
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:15:48 +0200

    å¢å æä»¶ä¸ä¼ éå¶

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-file/src/main/java/org/hswebframework/web/file/web/ReactiveFileController.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-file/src/main/java/org/hswebframework/web/file/web/ReactiveFileController.java
@@ -1,16 +1,11 @@
 package org.hswebframework.web.file.web;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterStyle;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.hswebframework.web.authorization.annotation.Resource;
 import org.hswebframework.web.authorization.annotation.ResourceAction;
 import org.hswebframework.web.authorization.exception.AccessDenyException;
 import org.hswebframework.web.file.FileUploadProperties;
-import org.hswebframework.web.file.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.http.codec.multipart.Part;
@@ -26,30 +21,25 @@ import java.io.File;
 @Resource(id = "file", name = "文件上传")
 @Slf4j
 @RequestMapping("/file")
-@Tag(name = "文件上传")
 public class ReactiveFileController {
 
-    private final FileUploadProperties properties;
+    @Autowired
+    private FileUploadProperties properties;
 
-    private final FileStorageService fileStorageService;
-
-    public ReactiveFileController(FileUploadProperties properties, FileStorageService fileStorageService) {
-        this.properties = properties;
-        this.fileStorageService = fileStorageService;
-    }
-
+    @ResourceAction(id = "upload-static", name = "静态文件")
     @PostMapping("/static")
     @SneakyThrows
-    @ResourceAction(id = "upload-static", name = "静态文件")
-    @Operation(summary = "上传静态文件")
-    public Mono<String> uploadStatic(@RequestPart("file")
-                                     @Parameter(name = "file", description = "文件", style = ParameterStyle.FORM) Part part) {
+    public Mono<String> uploadStatic(@RequestPart("file") Part part) {
+        FileUploadProperties.StaticFileInfo name;
         if (part instanceof FilePart) {
             FilePart filePart = ((FilePart) part);
             if (properties.denied(filePart.filename(), filePart.headers().getContentType())) {
                 throw new AccessDenyException();
             }
-            return fileStorageService.saveFile(filePart);
+            name = properties.createStaticSavePath(filePart.filename());
+            return ((FilePart) part)
+                    .transferTo(new File(name.getSavePath()))
+                    .thenReturn(name.getLocation());
         } else {
             return Mono.error(() -> new IllegalArgumentException("[file] part is not a file"));
         }
