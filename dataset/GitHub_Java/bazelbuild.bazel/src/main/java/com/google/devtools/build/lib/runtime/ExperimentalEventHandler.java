1fce9005b75671fe8f4b161d5ad0749553824f94
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:08:05 +0200

    experimental UI: merge simultaneous progress bar updates
        Events may reach the UI over the event bus simultaneously or
        with very small time difference. Updating the UI on the terminal,
        however is an expensive operation. So update the UI only once
        for simultaneous events.
    
        --
        Change-Id: I3e6881f18b37626f6be3ad7b97043b42773812f3
        Reviewed-on: https://bazel-review.googlesource.com/#/c/3047
        MOS_MIGRATED_REVID=116352143

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/ExperimentalEventHandler.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/ExperimentalEventHandler.java
@@ -17,7 +17,6 @@ import com.google.common.eventbus.Subscribe;
 import com.google.devtools.build.lib.actions.ActionCompletionEvent;
 import com.google.devtools.build.lib.actions.ActionStartedEvent;
 import com.google.devtools.build.lib.analysis.AnalysisPhaseCompleteEvent;
-import com.google.devtools.build.lib.analysis.NoBuildEvent;
 import com.google.devtools.build.lib.buildtool.buildevent.BuildCompleteEvent;
 import com.google.devtools.build.lib.buildtool.buildevent.BuildStartingEvent;
 import com.google.devtools.build.lib.buildtool.buildevent.ExecutionProgressReceiverAvailableEvent;
@@ -94,9 +93,7 @@ public class ExperimentalEventHandler extends BlazeCommandEventHandler {
           case ERROR:
           case WARNING:
           case INFO:
-            if (!buildComplete) {
-              clearProgressBar();
-            }
+            clearProgressBar();
             setEventKindColor(event.getKind());
             terminal.writeString(event.getKind() + ": ");
             terminal.resetTerminal();
@@ -107,9 +104,7 @@ public class ExperimentalEventHandler extends BlazeCommandEventHandler {
               terminal.writeString(event.getMessage());
             }
             crlf();
-            if (!buildComplete) {
-              addProgressBar();
-            }
+            addProgressBar();
             terminal.flush();
             break;
         }
@@ -161,11 +156,6 @@ public class ExperimentalEventHandler extends BlazeCommandEventHandler {
   public void buildComplete(BuildCompleteEvent event) {
     stateTracker.buildComplete(event);
     refresh();
-    buildComplete = true;
-  }
-
-  @Subscribe
-  public void noBuild(NoBuildEvent event) {
     buildComplete = true;
   }
 
