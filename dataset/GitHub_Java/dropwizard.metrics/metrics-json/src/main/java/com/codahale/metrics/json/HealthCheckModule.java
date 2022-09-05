50283b7c08de2adb420aa7e0e76dbe947e1ac792
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:19:00 +0200

    Replace an iterator to a for loop

--- dataset/GitHub_Java/dropwizard.metrics/metrics-json/src/main/java/com/codahale/metrics/json/HealthCheckModule.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-json/src/main/java/com/codahale/metrics/json/HealthCheckModule.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Map;
 
 public class HealthCheckModule extends Module {
