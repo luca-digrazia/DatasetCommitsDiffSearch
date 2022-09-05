314825af17566f30c2455e1d265833572dd24c84
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:05:25 +0200

    å é¤åå§åæ¹æ³

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-impl-common/src/main/java/org/hsweb/web/service/impl/form/DynamicFormServiceImpl.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-impl-common/src/main/java/org/hsweb/web/service/impl/form/DynamicFormServiceImpl.java
@@ -323,7 +323,7 @@ public class DynamicFormServiceImpl implements DynamicFormService {
             var.put("headerMapper", headerMapper);
             var.put("excelData", excelData);
             var.put("dataList", dataList);
-            metaData.on("export.import.before", var);
+            metaData.on("export.excel", var);
         } else
             excelData.forEach(data -> {
                 Map<String, Object> newData = new HashMap<>();
@@ -347,7 +347,7 @@ public class DynamicFormServiceImpl implements DynamicFormService {
                     var.put("headerMapper", headerMapper);
                     var.put("excelData", excelData);
                     var.put("dataList", dataList);
-                    TriggerResult triggerResult = metaData.on("export.import.each", var);
+                    TriggerResult triggerResult = metaData.on("export.excel", var);
                     if (!triggerResult.isSuccess()) {
                         throw new TriggerException(triggerResult.getMessage());
                     }
