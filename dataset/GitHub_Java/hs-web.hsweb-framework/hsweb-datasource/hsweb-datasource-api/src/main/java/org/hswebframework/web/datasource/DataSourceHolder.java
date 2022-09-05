2bc6384d53529837b41c241d09efdbf45de6dc80
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:53:39 +0200

    ä¼åå¨ææ°æ®æºæ¯å¦çæå¤æ­

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-datasource/hsweb-datasource-api/src/main/java/org/hswebframework/web/datasource/DataSourceHolder.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-datasource/hsweb-datasource-api/src/main/java/org/hswebframework/web/datasource/DataSourceHolder.java
@@ -2,34 +2,27 @@ package org.hswebframework.web.datasource;
 
 import org.hswebframework.web.datasource.exception.DataSourceNotFoundException;
 import org.hswebframework.web.datasource.switcher.DataSourceSwitcher;
-import org.hswebframework.web.datasource.switcher.DefaultDataSourceSwitcher;
-import org.hswebframework.web.datasource.switcher.TableSwitcher;
 
 /**
  * 用于操作动态数据源,如获取当前使用的数据源,使用switcher切换数据源等
  *
  * @author zhouhao
- * @since 3.0
+ * @see 3.0
  */
 public final class DataSourceHolder {
-
-    private static final DataSourceSwitcher defaultSwitcher = new DefaultDataSourceSwitcher();
 
     /**
      * 动态数据源切换器
      */
-    static volatile DataSourceSwitcher dataSourceSwitcher = defaultSwitcher;
+    static DataSourceSwitcher dataSourceSwitcher;
+
     /**
      * 动态数据源服务
      */
-    static volatile DynamicDataSourceService dynamicDataSourceService;
-
-    static volatile TableSwitcher tableSwitcher;
+    static DynamicDataSourceService dynamicDataSourceService;
 
     public static void checkDynamicDataSourceReady() {
-        if (dynamicDataSourceService == null) {
-            throw new UnsupportedOperationException("dataSourceService not ready");
-        }
+        if (dynamicDataSourceService == null) throw new UnsupportedOperationException("dynamicDataSourceService not ready");
     }
 
     /**
@@ -40,30 +33,10 @@ public final class DataSourceHolder {
     }
 
     /**
-     * @return 表切换器, 用于动态切换系统功能表
-     */
-    public static TableSwitcher tableSwitcher() {
-        return tableSwitcher;
-    }
-
-    /**
      * @return 默认数据源
      */
     public static DynamicDataSource defaultDataSource() {
-        checkDynamicDataSourceReady();
         return dynamicDataSourceService.getDefaultDataSource();
-    }
-
-    /**
-     * 根据指定的数据源id获取动态数据源
-     *
-     * @param dataSourceId 数据源id
-     * @return 动态数据源
-     * @throws DataSourceNotFoundException 如果数据源不存在将抛出此异常
-     */
-    public static DynamicDataSource dataSource(String dataSourceId) {
-        checkDynamicDataSourceReady();
-        return dynamicDataSourceService.getDataSource(dataSourceId);
     }
 
     /**
@@ -71,9 +44,7 @@ public final class DataSourceHolder {
      */
     public static DynamicDataSource currentDataSource() {
         String id = dataSourceSwitcher.currentDataSourceId();
-        if (id == null) {
-            return defaultDataSource();
-        }
+        if (id == null) return defaultDataSource();
         checkDynamicDataSourceReady();
         return dynamicDataSourceService.getDataSource(id);
     }
@@ -104,9 +75,7 @@ public final class DataSourceHolder {
      * @return 当前使用的数据源是否存在
      */
     public static boolean currentExisting() {
-        if (currentIsDefault()) {
-            return true;
-        }
+        if (currentIsDefault()) return true;
         try {
             return currentDataSource() != null;
         } catch (DataSourceNotFoundException e) {
