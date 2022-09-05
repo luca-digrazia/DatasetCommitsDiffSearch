8cf7f5136126dbb3dce6081853b970fc85ef19a2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:06:46 +0200

    If bundlemerge was called without a bundleInfoPlistFile it would generate an empty plist file. Added test to verify this doesn't get broken in the future.
        --
        MOS_MIGRATED_REVID=116670228

--- dataset/GitHub_Java/bazelbuild.bazel/src/objc_tools/bundlemerge/java/com/google/devtools/build/xcode/bundlemerge/BundleMerging.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/objc_tools/bundlemerge/java/com/google/devtools/build/xcode/bundlemerge/BundleMerging.java
@@ -1,4 +1,4 @@
-// Copyright 2014 Google Inc. All rights reserved.
+// Copyright 2014 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,23 +20,21 @@ import static com.google.devtools.build.singlejar.ZipCombiner.OutputMode.FORCE_D
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.singlejar.ZipCombiner;
 import com.google.devtools.build.xcode.bundlemerge.proto.BundleMergeProtos.BundleFile;
 import com.google.devtools.build.xcode.bundlemerge.proto.BundleMergeProtos.Control;
 import com.google.devtools.build.xcode.bundlemerge.proto.BundleMergeProtos.MergeZip;
-import com.google.devtools.build.xcode.bundlemerge.proto.BundleMergeProtos.VariableSubstitution;
-import com.google.devtools.build.xcode.common.Platform;
-import com.google.devtools.build.xcode.common.TargetDeviceFamily;
-import com.google.devtools.build.xcode.plmerge.KeysToRemoveIfEmptyString;
 import com.google.devtools.build.xcode.plmerge.PlistMerging;
+import com.google.devtools.build.xcode.zip.ZipFiles;
 import com.google.devtools.build.xcode.zip.ZipInputEntry;
+import com.google.devtools.build.zip.ZipFileEntry;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -45,7 +43,7 @@ import javax.annotation.CheckReturnValue;
 /**
  * Implementation of the final steps to create an iOS application bundle.
  *
- * TODO(bazel-team): Add asset catalog compilation and bundling to this logic.
+ * <p>TODO(bazel-team): Add asset catalog compilation and bundling to this logic.
  */
 public final class BundleMerging {
   @VisibleForTesting final FileSystem fileSystem;
@@ -94,48 +92,26 @@ public final class BundleMerging {
       Path tempDir, FileSystem fileSystem, Control control, String bundleRoot,
       ImmutableList.Builder<ZipInputEntry> packagedFilesBuilder,
       ImmutableList.Builder<MergeZip> mergeZipsBuilder, boolean includePkgInfo) throws IOException {
-    Path tempMergedPlist = Files.createTempFile(tempDir, null, INFOPLIST_FILENAME);
-    Path tempPkgInfo = Files.createTempFile(tempDir, null, PKGINFO_FILENAME);
-
-    // Generate the Info.plist and PkgInfo files to include in the app bundle.
-    ImmutableList.Builder<Path> sourcePlistFilesBuilder = new ImmutableList.Builder<>();
-    for (String sourcePlist : control.getSourcePlistFileList()) {
-      sourcePlistFilesBuilder.add(fileSystem.getPath(sourcePlist));
-    }
-    ImmutableList<Path> sourcePlistFiles = sourcePlistFilesBuilder.build();
-    ImmutableMap.Builder<String, String> substitutionMap = ImmutableMap.builder();
-    for (VariableSubstitution substitution : control.getVariableSubstitutionList()) {
-      substitutionMap.put(substitution.getName(), substitution.getValue());
-    }
-    PlistMerging plistMerging = PlistMerging.from(
-        sourcePlistFiles,
-        PlistMerging.automaticEntries(
-            TargetDeviceFamily.fromBundleMergeNames(control.getTargetDeviceFamilyList()),
-            Platform.valueOf(control.getPlatform()),
-            control.getSdkVersion(),
-            control.getMinimumOsVersion()),
-        substitutionMap.build(),
-        new KeysToRemoveIfEmptyString("CFBundleIconFile", "NSPrincipalClass"));
-    if (control.hasExecutableName()) {
-      plistMerging.setExecutableName(control.getExecutableName());
-    }
-    
-    plistMerging.setBundleIdentifier(
-        control.hasPrimaryBundleIdentifier() ? control.getPrimaryBundleIdentifier() : null,
-        control.hasFallbackBundleIdentifier() ? control.getFallbackBundleIdentifier() : null);
-   
-    plistMerging.write(tempMergedPlist, tempPkgInfo);
-
-
     bundleRoot = joinPath(bundleRoot, control.getBundleRoot());
 
-    // Add files to zip configuration which creates the final application bundle.
-    packagedFilesBuilder
-        .add(new ZipInputEntry(tempMergedPlist, joinPath(bundleRoot, INFOPLIST_FILENAME)));
-    if (includePkgInfo) {
+    if (control.hasBundleInfoPlistFile()) {
+      Path tempMergedPlist = Files.createTempFile(tempDir, null, INFOPLIST_FILENAME);
+      Path tempPkgInfo = Files.createTempFile(tempDir, null, PKGINFO_FILENAME);
+      Path bundleInfoPlist = fileSystem.getPath(control.getBundleInfoPlistFile());
+      new PlistMerging(PlistMerging.readPlistFile(bundleInfoPlist))
+          .setBundleIdentifier(
+              control.hasPrimaryBundleIdentifier() ? control.getPrimaryBundleIdentifier() : null,
+              control.hasFallbackBundleIdentifier() ? control.getFallbackBundleIdentifier() : null)
+          .writePlist(tempMergedPlist)
+          .writePkgInfo(tempPkgInfo);
       packagedFilesBuilder
-          .add(new ZipInputEntry(tempPkgInfo, joinPath(bundleRoot, PKGINFO_FILENAME)));
+          .add(new ZipInputEntry(tempMergedPlist, joinPath(bundleRoot, INFOPLIST_FILENAME)));
+      if (includePkgInfo) {
+        packagedFilesBuilder
+            .add(new ZipInputEntry(tempPkgInfo, joinPath(bundleRoot, PKGINFO_FILENAME)));
+      }
     }
+
     for (BundleFile bundleFile : control.getBundleFileList()) {
       int externalFileAttribute = bundleFile.hasExternalFileAttribute()
           ? bundleFile.getExternalFileAttribute() : ZipInputEntry.DEFAULT_EXTERNAL_FILE_ATTRIBUTE;
@@ -146,11 +122,6 @@ public final class BundleMerging {
               externalFileAttribute));
     }
 
-    for (String mergeZip : control.getMergeWithoutNamePrefixZipList()) {
-      mergeZipsBuilder.add(MergeZip.newBuilder()
-          .setSourcePath(mergeZip)
-          .build());
-    }
     mergeZipsBuilder.addAll(control.getMergeZipList());
 
     for (Control nestedControl : control.getNestedBundleList()) {
@@ -167,8 +138,7 @@ public final class BundleMerging {
   static BundleMerging merging(Path tempDir, FileSystem fileSystem, Control control)
       throws IOException {
     ImmutableList.Builder<MergeZip> mergeZipsBuilder = new ImmutableList.Builder<>();
-    ImmutableList.Builder<ZipInputEntry> packagedFilesBuilder =
-        new ImmutableList.Builder<ZipInputEntry>();
+    ImmutableList.Builder<ZipInputEntry> packagedFilesBuilder = new ImmutableList.Builder<>();
 
     mergeInto(tempDir, fileSystem, control, /*bundleRoot=*/"", packagedFilesBuilder,
         mergeZipsBuilder, /*includePkgInfo=*/true);
@@ -184,19 +154,39 @@ public final class BundleMerging {
    */
   private void addEntriesFromOtherZip(ZipCombiner combiner, Path sourceZip, String entryNamesPrefix)
       throws IOException {
+    Map<String, Integer> externalFileAttributes = ZipFiles.unixExternalFileAttributes(sourceZip);
     try (ZipInputStream zipIn = new ZipInputStream(Files.newInputStream(sourceZip))) {
       while (true) {
         ZipEntry zipInEntry = zipIn.getNextEntry();
         if (zipInEntry == null) {
           break;
         }
-        // TODO(bazel-team): preserve the external file attribute field in the source zip entry.
-        combiner.addFile(entryNamesPrefix + zipInEntry.getName(), DOS_EPOCH, zipIn,
-            ZipInputEntry.DEFAULT_DIRECTORY_ENTRY_INFO);
+        // TODO(bazel-dev): Add support for soft links because we will need them for MacOS support
+        // in frameworks at the very least. https://github.com/bazelbuild/bazel/issues/289
+        String name = entryNamesPrefix + zipInEntry.getName();
+        if (zipInEntry.isDirectory()) {
+          // If we already have a directory entry with this name then don't attempt to
+          // add it again. It's not an error to attempt to merge in two zip files that contain
+          // the same directories. It's only an error to attempt to merge in two zip files with the
+          // same leaf files.
+          if (!combiner.containsFile(name)) {
+            combiner.addDirectory(name, DOS_EPOCH);
+          }
+          continue;
+        }
+        Integer externalFileAttr = externalFileAttributes.get(zipInEntry.getName());
+        if (externalFileAttr == null) {
+          externalFileAttr = ZipInputEntry.DEFAULT_EXTERNAL_FILE_ATTRIBUTE;
+        }
+        ZipFileEntry zipOutEntry = new ZipFileEntry(name);
+        zipOutEntry.setTime(DOS_EPOCH.getTime());
+        zipOutEntry.setVersion(ZipInputEntry.MADE_BY_VERSION);
+        zipOutEntry.setExternalAttributes(externalFileAttr);
+        combiner.addFile(zipOutEntry, zipIn);
       }
     }
-  }
-
+  }  
+  
   @VisibleForTesting
   void execute() throws IOException {
     try (OutputStream out = Files.newOutputStream(outputZip);
