ca4e994180eb910b238ed8fcd48f85a3da48d053
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:23:01 +0200

    Use correct file path separator.
        --
        MOS_MIGRATED_REVID=113349046

--- dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/plugins/dependency/DependencyModule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/plugins/dependency/DependencyModule.java
@@ -57,7 +57,7 @@ public final class DependencyModule {
     /** Fail the build when transitive dependencies are used directly. */
     ERROR(true);
 
-    private final boolean enabled;
+    private boolean enabled;
 
     StrictJavaDeps(boolean enabled) {
       this.enabled = enabled;
@@ -69,10 +69,10 @@ public final class DependencyModule {
     }
   }
 
-  private final StrictJavaDeps strictJavaDeps;
+  private StrictJavaDeps strictJavaDeps = StrictJavaDeps.OFF;
   private final Map<String, String> directJarsToTargets;
   private final Map<String, String> indirectJarsToTargets;
-  private final boolean strictClasspathMode;
+  private boolean strictClasspathMode;
   private final Set<String> depsArtifacts;
   private final String ruleKind;
   private final String targetLabel;
