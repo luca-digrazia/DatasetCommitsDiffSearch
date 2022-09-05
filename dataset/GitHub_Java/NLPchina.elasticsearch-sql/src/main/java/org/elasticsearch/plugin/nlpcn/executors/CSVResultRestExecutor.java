a28210c215644154cc176e204c0292d562fbdba3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:03:15 +0200

    csv add includeId parameter

--- dataset/GitHub_Java/NLPchina.elasticsearch-sql/src/main/java/org/elasticsearch/plugin/nlpcn/executors/CSVResultRestExecutor.java
+++ dataset/GitHub_Java/NLPchina.elasticsearch-sql/src/main/java/org/elasticsearch/plugin/nlpcn/executors/CSVResultRestExecutor.java
@@ -1,7 +1,7 @@
 package org.elasticsearch.plugin.nlpcn.executors;
 
+import com.google.common.base.Joiner;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.common.base.Joiner;
 import org.elasticsearch.plugin.nlpcn.QueryActionElasticExecutor;
 import org.elasticsearch.rest.BytesRestResponse;
 import org.elasticsearch.rest.RestChannel;
@@ -18,15 +18,16 @@ public class CSVResultRestExecutor implements RestExecutor {
     @Override
     public void execute(Client client, Map<String, String> params, QueryAction queryAction, RestChannel channel) throws Exception {
         Object queryResult = QueryActionElasticExecutor.executeAnyAction(client, queryAction);
-        boolean flat = false;
-        if(params.containsKey("flat")){
-            flat = Boolean.parseBoolean(params.get("flat"));
-        }
+
+        boolean flat = getBooleanOrDefault(params,"flat",false);
         String separator = ",";
         if(params.containsKey("separator")){
          separator = params.get("separator");
         }
-        CSVResult result  = new CSVResultsExtractor().extractResults(queryResult,flat,separator);
+        boolean includeScore = getBooleanOrDefault(params,"_score",false);
+        boolean includeType = getBooleanOrDefault(params,"_type",false);
+        boolean includeId = getBooleanOrDefault(params,"_id",false);
+        CSVResult result  = new CSVResultsExtractor(includeScore,includeType,includeId).extractResults(queryResult,flat,separator);
         String newLine = "\n";
         if(params.containsKey("newLine")){
          newLine = params.get("newLine");
@@ -34,6 +35,35 @@ public class CSVResultRestExecutor implements RestExecutor {
         String csvString = buildString(separator, result, newLine);
         BytesRestResponse bytesRestResponse = new BytesRestResponse(RestStatus.OK, csvString);
         channel.sendResponse(bytesRestResponse);
+    }
+
+    @Override
+    public String execute(Client client, Map<String, String> params, QueryAction queryAction) throws Exception {
+        Object queryResult = QueryActionElasticExecutor.executeAnyAction(client, queryAction);
+
+        boolean flat = getBooleanOrDefault(params,"flat",false);
+        String separator = ",";
+        if(params.containsKey("separator")){
+            separator = params.get("separator");
+        }
+        boolean includeScore = getBooleanOrDefault(params,"_score",false);
+        boolean includeType = getBooleanOrDefault(params,"_type",false);
+        boolean includeId = getBooleanOrDefault(params,"_id",false);
+        CSVResult result  = new CSVResultsExtractor(includeScore,includeType,includeId).extractResults(queryResult,flat,separator);
+        String newLine = "\n";
+        if(params.containsKey("newLine")){
+            newLine = params.get("newLine");
+        }
+        String csvString = buildString(separator, result, newLine);
+        return csvString;
+    }
+
+    private boolean getBooleanOrDefault(Map<String, String> params, String param, boolean defaultValue) {
+        boolean flat = defaultValue;
+        if(params.containsKey(param)){
+            flat = Boolean.parseBoolean(params.get(param));
+        }
+        return flat;
     }
 
     private String buildString(String separator, CSVResult result, String newLine) {
