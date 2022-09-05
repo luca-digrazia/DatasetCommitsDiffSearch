cd381dbc6d74999fc02ac7cfa65f5b87d9b47449
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:48:50 +0200

    Make GaugeMetric an interface.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/core/GaugeMetric.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/core/GaugeMetric.java
@@ -1,9 +1,8 @@
 package com.yammer.metrics.core;
 
-
 /**
- * A gauge metric is an instantaneous reading of a particular value. To instrument a queue's depth,
- * for example:<br>
+ * A gauge metric is an instantaneous reading of a particular value. To
+ * instrument a queue's depth, for example:<br>
  * <pre><code>
  * Queue<String> queue = new ConcurrentLinkedQueue<String>();
  * GaugeMetric<Integer> queueDepth = new GaugeMetric<Integer>() {
@@ -11,21 +10,16 @@ package com.yammer.metrics.core;
  *         return queue.size();
  *     }
  * };
- * <p/>
- * </code></pre>
  *
+ * </code></pre>
+ * @author coda
  * @param <T> the type of the metric's value
  */
-public abstract class GaugeMetric<T> implements Metric {
+public interface GaugeMetric<T> extends Metric {
     /**
      * Returns the metric's current value.
      *
      * @return the metric's current value
      */
     public abstract T value();
-
-    @Override
-    public <U> void processWith(MetricsProcessor<U> processor, MetricName name, U context) throws Exception {
-        processor.processGauge(name, this, context);
-    }
 }
--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/core/JmxGauge.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/core/JmxGauge.java
@@ -1,45 +1,25 @@
 package com.yammer.metrics.core;
 
-import javax.management.MBeanServer;
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
 import java.lang.management.ManagementFactory;
+import javax.management.*;
 
 /**
  * A gauge which exposes an attribute of a JMX MBean.
  */
-public class JmxGauge extends Gauge<Object> {
+public class JmxGauge implements GaugeMetric<Object> {
     private static final MBeanServer SERVER = ManagementFactory.getPlatformMBeanServer();
-    private final ObjectName objectName;
-    private final String attribute;
+    private ObjectName name;
+    private String attribute;
 
-    /**
-     * Creates a new {@link JmxGauge} for the given attribute of the given MBean.
-     *
-     * @param objectName    the string value of the MBean's {@link ObjectName}
-     * @param attribute     the MBean attribute's name
-     *
-     * @throws MalformedObjectNameException if {@code objectName} is malformed
-     */
-    public JmxGauge(String objectName, String attribute) throws MalformedObjectNameException {
-        this(new ObjectName(objectName), attribute);
-    }
-
-    /**
-     * Creates a new {@link JmxGauge} for the given attribute of the given MBean.
-     *
-     * @param objectName    the MBean's {@link ObjectName}
-     * @param attribute     the MBean attribute's name
-     */
-    public JmxGauge(ObjectName objectName, String attribute) {
-        this.objectName = objectName;
+    public JmxGauge(String name, String attribute) throws MalformedObjectNameException {
+        this.name = new ObjectName(name);
         this.attribute = attribute;
     }
 
     @Override
     public Object value() {
         try {
-            return SERVER.getAttribute(objectName, attribute);
+            return SERVER.getAttribute(name, attribute);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
