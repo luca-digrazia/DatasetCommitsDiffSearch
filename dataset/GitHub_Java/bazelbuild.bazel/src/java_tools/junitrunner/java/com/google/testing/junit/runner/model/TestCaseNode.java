55646b77472c35035c37a8b47c3f348639026d2f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:57:08 +0200

    Declare field as ConcurrentMap instead of ConcurrentHashMap.
        --
        MOS_MIGRATED_REVID=131734056

--- dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/junitrunner/java/com/google/testing/junit/runner/model/TestCaseNode.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/java_tools/junitrunner/java/com/google/testing/junit/runner/model/TestCaseNode.java
@@ -17,36 +17,29 @@ package com.google.testing.junit.runner.model;
 import static com.google.testing.junit.runner.util.TestPropertyExporter.INITIAL_INDEX_FOR_REPEATED_PROPERTY;
 
 import com.google.testing.junit.runner.model.TestResult.Status;
-import com.google.testing.junit.runner.util.TestIntegration;
-import com.google.testing.junit.runner.util.TestIntegrationsExporter;
 import com.google.testing.junit.runner.util.TestPropertyExporter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
 import javax.annotation.Nullable;
 import org.junit.runner.Description;
 
-/** A leaf in the test suite model. */
-class TestCaseNode extends TestNode
-    implements TestPropertyExporter.Callback, TestIntegrationsExporter.Callback {
-  private static final Set<State> INITIAL_STATES = Collections.unmodifiableSet(
-      EnumSet.of(State.INITIAL, State.PENDING));
+/**
+ * A leaf in the test suite model.
+ */
+class TestCaseNode extends TestNode implements TestPropertyExporter.Callback {
   private final TestSuiteNode parent;
   private final Map<String, String> properties = new ConcurrentHashMap<>();
   private final Map<String, Integer> repeatedPropertyNamesToRepetitions = new HashMap<>();
   private final Queue<Throwable> globalFailures = new ConcurrentLinkedQueue<>();
   private final ConcurrentMap<Description, List<Throwable>> dynamicTestToFailures =
       new ConcurrentHashMap<>();
-  private final Set<TestIntegration> integrations =
-      Collections.newSetFromMap(new ConcurrentHashMap<TestIntegration, Boolean>());
 
   @Nullable private volatile TestInterval runTimeInterval = null;
   private volatile State state = State.INITIAL;
@@ -62,12 +55,6 @@ class TestCaseNode extends TestNode
     return Collections.emptyList();
   }
 
-  /**
-   * Indicates that the test represented by this node is scheduled to start.
-   */
-  void pending() {
-    compareAndSetState(State.INITIAL, State.PENDING, -1);
-  }
 
   /**
    * Indicates that the test represented by this node has started.
@@ -75,7 +62,7 @@ class TestCaseNode extends TestNode
    * @param now Time that the test started
    */
   public void started(long now) {
-    compareAndSetState(INITIAL_STATES, State.STARTED, now);
+    compareAndSetState(State.INITIAL, State.STARTED, now);
   }
 
   @Override
@@ -83,7 +70,7 @@ class TestCaseNode extends TestNode
     if (compareAndSetState(State.STARTED, State.INTERRUPTED, now)) {
       return;
     }
-    compareAndSetState(INITIAL_STATES, State.CANCELLED, now);
+    compareAndSetState(State.INITIAL, State.CANCELLED, now);
   }
 
   @Override
@@ -99,11 +86,6 @@ class TestCaseNode extends TestNode
   }
 
   @Override
-  public void exportTestIntegration(TestIntegration testIntegration) {
-    integrations.add(testIntegration);
-  }
-
-  @Override
   public void testSkipped(long now) {
     compareAndSetState(State.STARTED, State.SKIPPED, now);
   }
@@ -111,7 +93,7 @@ class TestCaseNode extends TestNode
 
   @Override
   public void testSuppressed(long now) {
-    compareAndSetState(INITIAL_STATES, State.SUPPRESSED, now);
+    compareAndSetState(State.INITIAL, State.SUPPRESSED, now);
   }
 
   /**
@@ -125,13 +107,13 @@ class TestCaseNode extends TestNode
 
   @Override
   public void testFailure(Throwable throwable, long now) {
-    compareAndSetState(INITIAL_STATES, State.FINISHED, now);
+    compareAndSetState(State.INITIAL, State.FINISHED, now);
     globalFailures.add(throwable);
   }
 
   @Override
   public void dynamicTestFailure(Description test, Throwable throwable, long now) {
-    compareAndSetState(INITIAL_STATES, State.FINISHED, now);
+    compareAndSetState(State.INITIAL, State.FINISHED, now);
     addThrowableToDynamicTestToFailures(test, throwable);
   }
 
@@ -165,30 +147,15 @@ class TestCaseNode extends TestNode
     return previousRepetitionsNr;
   }
 
-  private boolean compareAndSetState(State fromState, State toState, long now) {
-    if (fromState == null) {
+  private synchronized boolean compareAndSetState(State fromState, State toState, long now) {
+    if (fromState == null || toState == null || state == null) {
       throw new NullPointerException();
     }
-    return compareAndSetState(Collections.singleton(fromState), toState, now);
-  }
 
-  // TODO(bazel-team): Use AtomicReference instead of a synchronized method.
-  private synchronized boolean compareAndSetState(Set<State> fromStates, State toState, long now) {
-    if (fromStates == null || toState == null || state == null) {
-      throw new NullPointerException();
-    }
-    if (fromStates.isEmpty()) {
-      throw new IllegalArgumentException();
-    }
-
-    if (fromStates.contains(state) && toState != state) {
+    if (fromState == state && toState != state) {
       state = toState;
-      if (toState != State.PENDING) {
-        runTimeInterval =
-            runTimeInterval == null
-            ? new TestInterval(now, now)
-            : runTimeInterval.withEndMillis(now);
-      }
+      runTimeInterval =
+          runTimeInterval == null ? new TestInterval(now, now) : runTimeInterval.withEndMillis(now);
       return true;
     }
     return false;
@@ -240,7 +207,6 @@ class TestCaseNode extends TestNode
         .numTests(numTests)
         .numFailures(numFailures)
         .childResults(childResults)
-        .integrations(integrations)
         .build();
   }
 
@@ -263,7 +229,6 @@ class TestCaseNode extends TestNode
         .numTests(1)
         .numFailures(failed ? 1 : 0)
         .childResults(Collections.<TestResult>emptyList())
-        .integrations(Collections.<TestIntegration>emptySet())
         .build();
   }
 
@@ -271,8 +236,7 @@ class TestCaseNode extends TestNode
    * States of a TestCaseNode (see (link) for all the transitions and states descriptions).
    */
   private static enum State {
-    INITIAL(TestResult.Status.FILTERED),
-    PENDING(TestResult.Status.CANCELLED),
+    INITIAL(TestResult.Status.SKIPPED),
     STARTED(TestResult.Status.INTERRUPTED),
     SKIPPED(TestResult.Status.SKIPPED),
     SUPPRESSED(TestResult.Status.SUPPRESSED),
