c245bb18d1988078372a5048abb103fa3d575857
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:55:08 +0200

    Refactor apple multi-arch platform-specific split transition and rule classes to be reusable for apple_static_library and apple_dynamic_library
        --
        MOS_MIGRATED_REVID=132066681

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/AppleBinary.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/AppleBinary.java
@@ -15,8 +15,10 @@
 package com.google.devtools.build.lib.rules.objc;
 
 import static com.google.devtools.build.lib.rules.objc.ObjcProvider.MULTI_ARCH_LINKED_BINARIES;
-import static com.google.devtools.build.lib.rules.objc.ObjcRuleClasses.DylibDependingRule.DYLIBS_ATTR_NAME;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
@@ -28,11 +30,11 @@ import com.google.devtools.build.lib.analysis.config.BuildConfiguration;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.rules.RuleConfiguredTargetFactory;
 import com.google.devtools.build.lib.rules.apple.AppleConfiguration;
-import com.google.devtools.build.lib.rules.apple.Platform;
 import com.google.devtools.build.lib.rules.apple.Platform.PlatformType;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainProvider;
 import com.google.devtools.build.lib.rules.objc.CompilationSupport.ExtraLinkArgs;
-import java.util.Map;
+import com.google.devtools.build.lib.rules.objc.ObjcCommon.ResourceAttributes;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -40,44 +42,139 @@ import java.util.Set;
  */
 public class AppleBinary implements RuleConfiguredTargetFactory {
 
+  @VisibleForTesting
+  static final String REQUIRES_AT_LEAST_ONE_SOURCE_FILE =
+      "At least one source file is required (srcs, non_arc_srcs, or precompiled_srcs).";
+
   @Override
   public final ConfiguredTarget create(RuleContext ruleContext)
       throws InterruptedException, RuleErrorException {
     PlatformType platformType = MultiArchSplitTransitionProvider.getPlatformType(ruleContext);
-    AppleConfiguration appleConfiguration = ruleContext.getFragment(AppleConfiguration.class);
-
-    Platform platform = appleConfiguration.getMultiArchPlatform(platformType);
     ImmutableListMultimap<BuildConfiguration, ObjcProvider> configurationToNonPropagatedObjcMap =
         ruleContext.getPrerequisitesByConfiguration("non_propagated_deps", Mode.SPLIT,
             ObjcProvider.class);
     ImmutableListMultimap<BuildConfiguration, TransitiveInfoCollection> configToDepsCollectionMap =
         ruleContext.getPrerequisitesByConfiguration("deps", Mode.SPLIT);
-    Iterable<ObjcProvider> dylibProviders =
-        ruleContext.getPrerequisites(DYLIBS_ATTR_NAME, Mode.TARGET, ObjcProvider.class);
+
     Set<BuildConfiguration> childConfigurations = getChildConfigurations(ruleContext);
-    Artifact outputArtifact =
-        ObjcRuleClasses.intermediateArtifacts(ruleContext).combinedArchitectureBinary();
 
-    MultiArchBinarySupport multiArchBinarySupport = new MultiArchBinarySupport(ruleContext);
-    Map<BuildConfiguration, ObjcCommon> objcCommonByDepConfiguration =
-        multiArchBinarySupport.objcCommonByDepConfiguration(childConfigurations,
-            configToDepsCollectionMap, configurationToNonPropagatedObjcMap, dylibProviders);
-    multiArchBinarySupport.registerActions(platform, new ExtraLinkArgs(),
-        objcCommonByDepConfiguration, configToDepsCollectionMap, outputArtifact);
+    IntermediateArtifacts ruleIntermediateArtifacts =
+        ObjcRuleClasses.intermediateArtifacts(ruleContext);
 
+    NestedSetBuilder<Artifact> binariesToLipo =
+        NestedSetBuilder.<Artifact>stableOrder();
     NestedSetBuilder<Artifact> filesToBuild =
-        NestedSetBuilder.<Artifact>stableOrder().add(outputArtifact);
+        NestedSetBuilder.<Artifact>stableOrder()
+            .add(ruleIntermediateArtifacts.combinedArchitectureBinary());
+
+    ObjcProvider.Builder objcProviderBuilder = new ObjcProvider.Builder();
+
+    for (BuildConfiguration childConfig : childConfigurations) {
+      ProtobufSupport protoSupport =
+          new ProtobufSupport(ruleContext, childConfig)
+              .registerGenerationActions()
+              .registerCompilationActions();
+
+      Optional<ObjcProvider> protosObjcProvider = protoSupport.getObjcProvider();
+
+      IntermediateArtifacts intermediateArtifacts =
+          ObjcRuleClasses.intermediateArtifacts(ruleContext, childConfig);
+
+      ObjcCommon common =
+          common(
+              ruleContext,
+              childConfig,
+              intermediateArtifacts,
+              nullToEmptyList(configToDepsCollectionMap.get(childConfig)),
+              nullToEmptyList(configurationToNonPropagatedObjcMap.get(childConfig)),
+              protosObjcProvider);
+      ImmutableList.Builder<J2ObjcMappingFileProvider> j2ObjcMappingFileProviders =
+          ImmutableList.builder();
+      J2ObjcEntryClassProvider.Builder j2ObjcEntryClassProviderBuilder =
+          new J2ObjcEntryClassProvider.Builder();
+      for (TransitiveInfoCollection dep : configToDepsCollectionMap.get(childConfig)) {
+        if (dep.getProvider(J2ObjcMappingFileProvider.class) != null) {
+          j2ObjcMappingFileProviders.add(dep.getProvider(J2ObjcMappingFileProvider.class));
+        }
+        if (dep.getProvider(J2ObjcEntryClassProvider.class) != null) {
+          j2ObjcEntryClassProviderBuilder.addTransitive(
+              dep.getProvider(J2ObjcEntryClassProvider.class));
+        }
+      }
+      J2ObjcMappingFileProvider j2ObjcMappingFileProvider =
+          J2ObjcMappingFileProvider.union(j2ObjcMappingFileProviders.build());
+      J2ObjcEntryClassProvider j2ObjcEntryClassProvider = j2ObjcEntryClassProviderBuilder.build();
+
+      binariesToLipo.add(intermediateArtifacts.strippedSingleArchitectureBinary());
+
+      new CompilationSupport(ruleContext, childConfig)
+          .registerCompileAndArchiveActions(common)
+          .registerLinkActions(
+              common.getObjcProvider(),
+              j2ObjcMappingFileProvider,
+              j2ObjcEntryClassProvider,
+              new ExtraLinkArgs(),
+              ImmutableList.<Artifact>of(),
+              DsymOutputType.APP)
+          .validateAttributes();
+      ruleContext.assertNoErrors();
+
+      objcProviderBuilder.addTransitiveAndPropagate(common.getObjcProvider());
+    }
+
+    AppleConfiguration appleConfiguration = ruleContext.getFragment(AppleConfiguration.class);
+
+    new LipoSupport(ruleContext)
+        .registerCombineArchitecturesAction(
+            binariesToLipo.build(),
+            ruleIntermediateArtifacts.combinedArchitectureBinary(),
+            appleConfiguration.getMultiArchPlatform(platformType));
+
     RuleConfiguredTargetBuilder targetBuilder =
         ObjcRuleClasses.ruleConfiguredTarget(ruleContext, filesToBuild.build());
 
-    ObjcProvider.Builder objcProviderBuilder = new ObjcProvider.Builder();
-    for (ObjcCommon objcCommon : objcCommonByDepConfiguration.values()) {
-      objcProviderBuilder.addTransitiveAndPropagate(objcCommon.getObjcProvider());
-    }
-    objcProviderBuilder.add(MULTI_ARCH_LINKED_BINARIES, outputArtifact);
+    objcProviderBuilder.add(
+        MULTI_ARCH_LINKED_BINARIES, ruleIntermediateArtifacts.combinedArchitectureBinary());
 
     targetBuilder.addProvider(ObjcProvider.class, objcProviderBuilder.build());
     return targetBuilder.build();
+  }
+
+  private ObjcCommon common(
+      RuleContext ruleContext,
+      BuildConfiguration buildConfiguration,
+      IntermediateArtifacts intermediateArtifacts,
+      List<TransitiveInfoCollection> propagatedDeps,
+      List<ObjcProvider> nonPropagatedObjcDeps,
+      Optional<ObjcProvider> protosObjcProvider) {
+
+    CompilationArtifacts compilationArtifacts =
+        CompilationSupport.compilationArtifacts(ruleContext, intermediateArtifacts);
+
+    ObjcCommon.Builder commonBuilder = new ObjcCommon.Builder(ruleContext, buildConfiguration)
+        .setCompilationAttributes(
+            CompilationAttributes.Builder.fromRuleContext(ruleContext).build())
+        .setCompilationArtifacts(compilationArtifacts)
+        .setResourceAttributes(new ResourceAttributes(ruleContext))
+        .addDefines(ruleContext.getTokenizedStringListAttr("defines"))
+        .addDeps(propagatedDeps)
+        .addDepObjcProviders(
+            ruleContext.getPrerequisites("bundles", Mode.TARGET, ObjcProvider.class))
+        .addDepObjcProviders(protosObjcProvider.asSet())
+        .addNonPropagatedDepObjcProviders(nonPropagatedObjcDeps)
+        .setIntermediateArtifacts(intermediateArtifacts)
+        .setAlwayslink(false)
+        // TODO(b/29152500): Enable module map generation.
+        .setLinkedBinary(intermediateArtifacts.strippedSingleArchitectureBinary());
+        
+    if (ObjcRuleClasses.objcConfiguration(ruleContext).generateDsym()) {
+      commonBuilder.addDebugArtifacts(DsymOutputType.APP);
+    }
+    return commonBuilder.build();
+  }
+
+  private <T> List<T> nullToEmptyList(List<T> inputList) {
+    return inputList != null ? inputList : ImmutableList.<T>of();
   }
 
   private Set<BuildConfiguration> getChildConfigurations(RuleContext ruleContext) {
