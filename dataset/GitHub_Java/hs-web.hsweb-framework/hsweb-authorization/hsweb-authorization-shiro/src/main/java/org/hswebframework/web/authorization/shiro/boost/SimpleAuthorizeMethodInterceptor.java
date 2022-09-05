dd26df3d98e9a17f7302d70763059475ed501775
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:49:39 +0200

    ä¿®å¤æéå¤æ­æ æ

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-shiro/src/main/java/org/hswebframework/web/authorization/shiro/boost/SimpleAuthorizeMethodInterceptor.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-shiro/src/main/java/org/hswebframework/web/authorization/shiro/boost/SimpleAuthorizeMethodInterceptor.java
@@ -18,21 +18,20 @@
 
 package org.hswebframework.web.authorization.shiro.boost;
 
-import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.shiro.aop.AnnotationResolver;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.UnauthenticatedException;
 import org.apache.shiro.authz.aop.AuthorizingAnnotationHandler;
 import org.apache.shiro.authz.aop.AuthorizingAnnotationMethodInterceptor;
-import org.hswebframework.expands.script.engine.DynamicScriptEngine;
-import org.hswebframework.expands.script.engine.DynamicScriptEngineFactory;
-import org.hswebframework.web.authorization.Authorization;
-import org.hswebframework.web.authorization.AuthorizationHolder;
+import org.hswebframework.utils.ClassUtils;
+import org.hswebframework.utils.StringUtils;
+import org.hswebframework.web.ExpressionUtils;
+import org.hswebframework.web.authorization.Authentication;
 import org.hswebframework.web.authorization.Permission;
 import org.hswebframework.web.authorization.Role;
 import org.hswebframework.web.authorization.annotation.Authorize;
 import org.hswebframework.web.authorization.annotation.Logical;
-import org.hswebframwork.utils.ClassUtils;
-import org.hswebframwork.utils.StringUtils;
+import org.hswebframework.web.boost.aop.context.MethodInterceptorHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,11 +47,12 @@ import java.util.stream.Collectors;
  * @author zhouhao
  */
 public class SimpleAuthorizeMethodInterceptor extends AuthorizingAnnotationMethodInterceptor {
-    public SimpleAuthorizeMethodInterceptor() {
-        super(new AuthorizeAnnotationHandler());
+    public SimpleAuthorizeMethodInterceptor(AnnotationResolver resolver) {
+        super(new AuthorizeAnnotationHandler(), resolver);
     }
 
     private static final Logger logger = LoggerFactory.getLogger(SimpleAuthorizeMethodInterceptor.class);
+
 
     static class AuthorizeAnnotationHandler extends AuthorizingAnnotationHandler {
 
@@ -65,26 +65,30 @@ public class SimpleAuthorizeMethodInterceptor extends AuthorizingAnnotationMetho
             if (!(a instanceof Authorize)) return;
             MethodInterceptorHolder holder = MethodInterceptorHolder.current();
             if (null == holder) {
+                logger.warn("MethodInterceptorHolder is null,maybe config is error!");
                 return;
             }
             AuthorizeConfig authorizeConfig = new AuthorizeConfig(holder.getArgs());
             Authorize authorize = ((Authorize) a);
+            if (authorize.ignore()) return;
+
             if (authorize.merge()) {
                 Authorize classAnn = ClassUtils.getAnnotation(holder.getTarget().getClass(), Authorize.class);
                 if (null != classAnn) {
+                    if (classAnn.ignore()) return;
                     authorizeConfig.put(classAnn);
                 }
             }
             authorizeConfig.put(authorize);
 
-            Authorization authorization = AuthorizationHolder.get();
-            if (null == authorization) throw new UnauthenticatedException(authorizeConfig.message);
+            Authentication authentication = Authentication.current()
+                    .orElseThrow(() -> new UnauthenticatedException(authorizeConfig.message));
             boolean access = true;
             Logical logical = authorizeConfig.logical == Logical.DEFAULT ? Logical.OR : authorizeConfig.logical;
             boolean logicalIsOr = logical == Logical.OR;
             // 控制权限
             if (!authorizeConfig.permission.isEmpty()) {
-                List<Permission> permissions = authorization.getPermissions().stream()
+                List<Permission> permissions = authentication.getPermissions().stream()
                         .filter(permission -> {
                             // 未持有任何一个权限
                             if (!authorizeConfig.permission.contains(permission.getId())) return false;
@@ -96,16 +100,22 @@ public class SimpleAuthorizeMethodInterceptor extends AuthorizingAnnotationMetho
                                     .stream()
                                     .filter(authorizeConfig.action::contains)
                                     .collect(Collectors.toList());
+                            
+                            if (actions.isEmpty()) return false;
+
                             //如果 控制逻辑是or,则只要过滤结果数量不为0.否则过滤结果数量必须和配置的数量相同
-                            return logicalIsOr ? actions.size() > 0 : actions.size() == permission.getActions().size();
+                            return logicalIsOr ? actions.size() > 0 : permission.getActions().containsAll(actions);
                         }).collect(Collectors.toList());
-                access = logicalIsOr ? permissions.size() > 0 : permissions.size() == authorizeConfig.permission.size();
+                access = logicalIsOr ?
+                        permissions.size() > 0 :
+                        //权限数量和配置的数量相同
+                        permissions.size() == authorizeConfig.permission.size();
             }
             //控制角色
             if (!authorizeConfig.role.isEmpty()) {
                 Function<Predicate<Role>, Boolean> func = logicalIsOr
-                        ? authorization.getRoles().stream()::anyMatch
-                        : authorization.getRoles().stream()::allMatch;
+                        ? authentication.getRoles().stream()::anyMatch
+                        : authentication.getRoles().stream()::allMatch;
                 access = func.apply(role -> authorizeConfig.role.contains(role.getId()));
             }
             //控制用户
@@ -113,7 +123,7 @@ public class SimpleAuthorizeMethodInterceptor extends AuthorizingAnnotationMetho
                 Function<Predicate<String>, Boolean> func = logicalIsOr
                         ? authorizeConfig.user.stream()::anyMatch
                         : authorizeConfig.user.stream()::allMatch;
-                access = func.apply(authorization.getUser().getId()::equals);
+                access = func.apply(authentication.getUser().getUsername()::equals);
             }
             if (!access) {
                 throw new AuthorizationException(authorizeConfig.message);
@@ -147,22 +157,27 @@ public class SimpleAuthorizeMethodInterceptor extends AuthorizingAnnotationMetho
         }
 
         public String tryCompileExpression(String express) {
-            if (express.startsWith("${") && express.endsWith("}")) {
-                express = express.substring(2, express.length() - 1);
-                DynamicScriptEngine spelEngine = DynamicScriptEngineFactory.getEngine("spel");
-                String id = DigestUtils.md5Hex(express);
-                try {
-                    if (!spelEngine.compiled(id))
-                        spelEngine.compile(id, express);
-                    return String.valueOf(spelEngine.execute(id, var).getIfSuccess());
-                } catch (Exception e) {
-                    throw new AuthorizationException("系统错误", e);
-                } finally {
-                    //     spelEngine.remove(id);
-                }
-            } else {
-                return express;
+            try {
+                return ExpressionUtils.analytical(express, var, "spel");
+            } catch (Exception e) {
+                throw new AuthorizationException("系统错误", e);
             }
+//            if (express.startsWith("${") && express.endsWith("}")) {
+//                express = express.substring(2, express.length() - 1);
+//                DynamicScriptEngine spelEngine = DynamicScriptEngineFactory.getEngine("spel");
+//                String id = DigestUtils.md5Hex(express);
+//                try {
+//                    if (!spelEngine.compiled(id))
+//                        spelEngine.compile(id, express);
+//                    return String.valueOf(spelEngine.execute(id, var).getIfSuccess());
+//                } catch (Exception e) {
+//                    throw new AuthorizationException("系统错误", e);
+//                } finally {
+//                    //     spelEngine.remove(id);
+//                }
+//            } else {
+//                return express;
+//            }
         }
 
         public Collection<String> tryCompileExpression(String... expresses) {
