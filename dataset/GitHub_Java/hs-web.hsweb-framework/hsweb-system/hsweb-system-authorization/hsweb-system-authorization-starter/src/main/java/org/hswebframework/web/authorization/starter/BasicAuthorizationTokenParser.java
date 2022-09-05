5e36d159440a570920bece056d63cfa8ac97309c
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:34:22 +0200

    ä¿®å¤æææéè¯¯

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-starter/src/main/java/org/hswebframework/web/authorization/starter/BasicAuthorizationTokenParser.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-authorization/hsweb-system-authorization-starter/src/main/java/org/hswebframework/web/authorization/starter/BasicAuthorizationTokenParser.java
@@ -41,7 +41,7 @@ public class BasicAuthorizationTokenParser implements UserTokenForTypeParser {
         try {
             String usernameAndPassword = new String(Base64.decodeBase64(authorization));
             UserToken token = userTokenManager.getByToken(usernameAndPassword);
-            if (token != null && token.isNormal()) {
+            if (token != null && token.isEffective()) {
                 return new ParsedToken() {
                     @Override
                     public String getToken() {
