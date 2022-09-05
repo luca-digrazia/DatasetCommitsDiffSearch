76f4ba790b84b7344b0a09b0b04b684a8680d06a
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:13:01 +0200

    Change graphite connect logic to lookup dns names on reconnect (#1493)

--- dataset/GitHub_Java/dropwizard.metrics/metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteUDP.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteUDP.java
@@ -2,17 +2,20 @@ package com.codahale.metrics.graphite;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
-import java.util.regex.Pattern;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * A client to a Carbon server using unconnected UDP
  */
 public class GraphiteUDP implements GraphiteSender {
-    private static final Pattern WHITESPACE = Pattern.compile("[\\s]+");
 
-    private final InetSocketAddress address;
+    private final String hostname;
+    private final int port;
+    private InetSocketAddress address;
 
     private DatagramChannel datagramChannel = null;
     private int failures;
@@ -20,38 +23,50 @@ public class GraphiteUDP implements GraphiteSender {
     /**
      * Creates a new client which sends data to given address using UDP
      *
+     * @param hostname The hostname of the Carbon server
+     * @param port     The port of the Carbon server
+     */
+    public GraphiteUDP(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+        this.address = null;
+    }
+
+    /**
+     * Creates a new client which sends data to given address using UDP
+     *
      * @param address the address of the Carbon server
      */
     public GraphiteUDP(InetSocketAddress address) {
-    	this.address = address;
+        this.hostname = null;
+        this.port = -1;
+        this.address = address;
     }
 
     @Override
     public void connect() throws IllegalStateException, IOException {
-    	// Only open the channel the first time...
-    	if (datagramChannel == null){
-        	datagramChannel = DatagramChannel.open();
-    	}
+        if (isConnected()) {
+            throw new IllegalStateException("Already connected");
+        }
+
+        // Resolve hostname
+        if (hostname != null) {
+            address = new InetSocketAddress(InetAddress.getByName(hostname), port);
+        }
+
+        datagramChannel = DatagramChannel.open();
+    }
+
+    @Override
+    public boolean isConnected() {
+        return datagramChannel != null && !datagramChannel.socket().isClosed();
     }
 
     @Override
     public void send(String name, String value, long timestamp) throws IOException {
-    	// Underlying socket can be closed by ICMP
-    	if (datagramChannel.socket().isClosed()){
-    		datagramChannel.close();
-        	datagramChannel = DatagramChannel.open();
-    	}
-
         try {
-        	StringBuilder buf = new StringBuilder();
-            buf.append(sanitize(name));
-            buf.append(' ');
-            buf.append(sanitize(value));
-            buf.append(' ');
-            buf.append(Long.toString(timestamp));
-            buf.append('\n');
-            String str = buf.toString();
-            ByteBuffer byteBuffer = ByteBuffer.wrap(str.getBytes());
+            String str = sanitize(name) + ' ' + sanitize(value) + ' ' + Long.toString(timestamp) + '\n';
+            ByteBuffer byteBuffer = ByteBuffer.wrap(str.getBytes(UTF_8));
             datagramChannel.send(byteBuffer, address);
             this.failures = 0;
         } catch (IOException e) {
@@ -66,11 +81,38 @@ public class GraphiteUDP implements GraphiteSender {
     }
 
     @Override
+    public void flush() throws IOException {
+        // Nothing to do
+    }
+
+    @Override
     public void close() throws IOException {
-    	// Leave channel & socket open for next metrics
+        if (datagramChannel != null) {
+            try {
+                datagramChannel.close();
+            } finally {
+                datagramChannel = null;
+            }
+        }
     }
 
     protected String sanitize(String s) {
-        return WHITESPACE.matcher(s).replaceAll("-");
+        return GraphiteSanitize.sanitize(s);
+    }
+
+    DatagramChannel getDatagramChannel() {
+        return datagramChannel;
+    }
+
+    void setDatagramChannel(DatagramChannel datagramChannel) {
+        this.datagramChannel = datagramChannel;
+    }
+
+    InetSocketAddress getAddress() {
+        return address;
+    }
+
+    void setAddress(InetSocketAddress address) {
+        this.address = address;
     }
 }
