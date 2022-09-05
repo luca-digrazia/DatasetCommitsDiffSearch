69e8b95e9d5c5761886ad14694a99faf1718f934
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:10:57 +0200

    Expose DEVELOPER_DIR to Skylark
        * Also make constants private since they both have getters now.
    
        --
        PiperOrigin-RevId: 143973536
        MOS_MIGRATED_REVID=143973536

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/apple/AppleToolchain.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/apple/AppleToolchain.java
@@ -135,7 +135,7 @@ public class AppleToolchain {
           relativePath = DEVELOPER_FRAMEWORK_PATH;
         }
         break;
-      case MACOS:
+      case MACOS_X:
         relativePath = DEVELOPER_FRAMEWORK_PATH;
         break;
       case WATCHOS_DEVICE:
