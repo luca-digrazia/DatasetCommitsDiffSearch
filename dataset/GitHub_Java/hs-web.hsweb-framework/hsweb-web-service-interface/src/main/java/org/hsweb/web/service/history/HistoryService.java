8a7eea0e9e68cada11fda2fe85bc8430de136def
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:08:08 +0200

    æ°å¢æä½è®°å½åè½

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-interface/src/main/java/org/hsweb/web/service/history/HistoryService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-interface/src/main/java/org/hsweb/web/service/history/HistoryService.java
@@ -13,5 +13,5 @@ public interface HistoryService extends GenericService<History, String> {
      * @param type 类型
      * @return 查询结果
      */
-    History selectLastHistoryByType(String type);
+    History selectLastHistoryByType(String type) throws Exception;
 }
