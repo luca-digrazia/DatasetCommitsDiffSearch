58bf05b82d1540ebf97b9e2db5f7655e1ea45a90
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:23:38 +0200

    Reformatted

--- dataset/GitHub_Java/dropwizard.metrics/metrics-graphite/src/main/java/com/codahale/metrics/graphite/PickledGraphite.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-graphite/src/main/java/com/codahale/metrics/graphite/PickledGraphite.java
@@ -226,19 +226,12 @@ public class PickledGraphite implements GraphiteSender {
 
     @Override
     public void close() throws IOException {
-        try {
-            flush();
-            if (writer != null) {
-                writer.close();
-            }
-        } catch (IOException ex) {
-            if (socket != null) {
-                socket.close();
-            }
-        } finally {
-            this.socket = null;
-            this.writer = null;
+        flush();
+        if (socket != null) {
+            socket.close();
         }
+        this.socket = null;
+        this.writer = null;
     }
 
     @Override
