c764c5785fad98867e958c666f7c1d12b8ae68b6
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:10:42 +0200

    Update doc for [new_]http_archive to mention tarballs.
        --
        Change-Id: Ib1270d1bbd549de96e7b82f8e674f6d67df8f450
        Reviewed-on: https://bazel-review.googlesource.com/#/c/2020
        MOS_MIGRATED_REVID=103740143

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/workspace/NewHttpArchiveRule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/workspace/NewHttpArchiveRule.java
@@ -1,4 +1,4 @@
-// Copyright 2014 The Bazel Authors. All rights reserved.
+// Copyright 2014 Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -63,7 +63,7 @@ public class NewHttpArchiveRule implements RuleDefinition {
          "tgz" here.</p>
          <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
         .add(attr("type", STRING))
-        /* <!-- #BLAZE_RULE(new_http_archive).ATTRIBUTE(strip_prefix) -->
+        /* <!-- #BLAZE_RULE(http_archive).ATTRIBUTE(strip_prefix) -->
          A directory prefix to strip from the extracted files.
          ${SYNOPSIS}
 
