e66308603b524c31ba461bc07f5d7933a824229f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:50:19 +0200

    Bind the gRPC command server port only to localhost.
        Work towards #930.
    
        --
        MOS_MIGRATED_REVID=120205147

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
@@ -14,10 +14,6 @@
 
 package com.google.devtools.build.lib.server;
 
-import com.google.common.base.Verify;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import com.google.devtools.build.lib.runtime.BlazeCommandDispatcher.LockingMode;
 import com.google.devtools.build.lib.runtime.CommandExecutor;
 import com.google.devtools.build.lib.server.CommandProtos.CancelRequest;
 import com.google.devtools.build.lib.server.CommandProtos.CancelResponse;
@@ -28,7 +24,6 @@ import com.google.devtools.build.lib.server.CommandProtos.RunResponse;
 import com.google.devtools.build.lib.util.Clock;
 import com.google.devtools.build.lib.util.ExitCode;
 import com.google.devtools.build.lib.util.Preconditions;
-import com.google.devtools.build.lib.util.ThreadUtils;
 import com.google.devtools.build.lib.util.io.OutErr;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
@@ -41,18 +36,12 @@ import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
-import java.nio.charset.Charset;
 import java.security.SecureRandom;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicLong;
-
-import javax.annotation.concurrent.GuardedBy;
 
 /**
  * gRPC server class.
@@ -61,11 +50,6 @@ import javax.annotation.concurrent.GuardedBy;
  * bootstrapping.
  */
 public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.CommandServer {
-  // UTF-8 won't do because we want to be able to pass arbitrary binary strings.
-  // Not that the internals of Bazel handle that correctly, but why not make at least this little
-  // part correct?
-  private static final Charset CHARSET = Charset.forName("ISO-8859-1");
-
   private class RunningCommand implements AutoCloseable {
     private final Thread thread;
     private final String id;
@@ -75,7 +59,6 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
       id = UUID.randomUUID().toString();
       synchronized (runningCommands) {
         runningCommands.put(id, this);
-        runningCommands.notify();
       }
     }
 
@@ -83,7 +66,6 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
     public void close() {
       synchronized (runningCommands) {
         runningCommands.remove(id);
-        runningCommands.notify();
       }
     }
   }
@@ -94,8 +76,8 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
   public static class Factory implements RPCServer.Factory {
     @Override
     public RPCServer create(CommandExecutor commandExecutor, Clock clock, int port,
-      Path serverDirectory, int maxIdleSeconds) throws IOException {
-      return new GrpcServerImpl(commandExecutor, clock, port, serverDirectory, maxIdleSeconds);
+      Path serverDirectory) throws IOException {
+      return new GrpcServerImpl(commandExecutor, clock, port, serverDirectory);
     }
   }
 
@@ -142,32 +124,28 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
   }
 
   // These paths are all relative to the server directory
-  private static final String PORT_FILE = "command_port";
+  private static final String PORT_FILE = "grpc_port";
   private static final String REQUEST_COOKIE_FILE = "request_cookie";
   private static final String RESPONSE_COOKIE_FILE = "response_cookie";
 
-  @GuardedBy("runningCommands")
   private final Map<String, RunningCommand> runningCommands = new HashMap<>();
   private final CommandExecutor commandExecutor;
   private final Clock clock;
   private final Path serverDirectory;
   private final String requestCookie;
   private final String responseCookie;
-  private final AtomicLong interruptCounter = new AtomicLong(0);
-  private final int maxIdleSeconds;
 
   private Server server;
   private int port;  // mutable so that we can overwrite it if port 0 is passed in
   boolean serving;
 
   public GrpcServerImpl(CommandExecutor commandExecutor, Clock clock, int port,
-      Path serverDirectory, int maxIdleSeconds) throws IOException {
+      Path serverDirectory) throws IOException {
     super(serverDirectory);
     this.commandExecutor = commandExecutor;
     this.clock = clock;
     this.serverDirectory = serverDirectory;
     this.port = port;
-    this.maxIdleSeconds = maxIdleSeconds;
     this.serving = false;
 
     SecureRandom random = new SecureRandom();
@@ -186,110 +164,21 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
     return result.toString();
   }
 
-  private void startSlowInterruptWatcher(final ImmutableSet<String> commandIds) {
-    if (commandIds.isEmpty()) {
-      return;
-    }
-
-    Runnable interruptWatcher = new Runnable() {
-      @Override
-      public void run() {
-        try {
-          boolean ok;
-          Thread.sleep(10 * 1000);
-          synchronized (runningCommands) {
-            ok = Collections.disjoint(commandIds, runningCommands.keySet());
-          }
-          if (!ok) {
-            // At least one command was not interrupted. Interrupt took too long.
-            ThreadUtils.warnAboutSlowInterrupt();
-          }
-        } catch (InterruptedException e) {
-          // Ignore.
-        }
-      }
-    };
-
-    Thread interruptWatcherThread =
-        new Thread(interruptWatcher, "interrupt-watcher-" + interruptCounter.incrementAndGet());
-    interruptWatcherThread.setDaemon(true);
-    interruptWatcherThread.start();
-  }
-
-  private void timeoutThread() {
-    synchronized (runningCommands) {
-      boolean idle = runningCommands.isEmpty();
-      boolean wasIdle = false;
-      long shutdownTime = -1;
-
-      while (true) {
-        if (!wasIdle && idle) {
-          shutdownTime = System.currentTimeMillis() + ((long) maxIdleSeconds) * 1000;
-        }
-
-        try {
-          if (idle) {
-            Verify.verify(shutdownTime > 0);
-            long waitTime = shutdownTime - System.currentTimeMillis();
-            if (waitTime > 0) {
-              runningCommands.wait(waitTime);
-            }
-          } else {
-            runningCommands.wait();
-          }
-        } catch (InterruptedException e) {
-          // Dealt with by checking the current time below.
-        }
-
-        wasIdle = idle;
-        idle = runningCommands.isEmpty();
-        if (wasIdle && idle && System.currentTimeMillis() >= shutdownTime) {
-          break;
-        }
-      }
-    }
-
-    server.shutdownNow();
-  }
-
-  @Override
-  public void interrupt() {
-    synchronized (runningCommands) {
-      for (RunningCommand command : runningCommands.values()) {
-        command.thread.interrupt();
-      }
-
-      startSlowInterruptWatcher(ImmutableSet.copyOf(runningCommands.keySet()));
-    }
-  }
-
   @Override
   public void serve() throws IOException {
     Preconditions.checkState(!serving);
-    InetAddress loopbackAddress = InetAddress.getLoopbackAddress();
-    server = NettyServerBuilder.forAddress(new InetSocketAddress(loopbackAddress, port))
+    server = NettyServerBuilder.forAddress(new InetSocketAddress("localhost", port))
         .addService(CommandServerGrpc.bindService(this))
         .build();
 
     server.start();
-    if (maxIdleSeconds > 0) {
-      Thread timeoutThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-          timeoutThread();
-        }
-      });
-      
-      timeoutThread.setDaemon(true);
-      timeoutThread.start();
-    }
     serving = true;
 
     if (port == 0) {
       port = getActualServerPort();
     }
 
-    writeServerFile(PORT_FILE, loopbackAddress.getHostAddress() + ":" + Integer.toString(port));
+    writeServerFile(PORT_FILE, Integer.toString(port));
     writeServerFile(REQUEST_COOKIE_FILE, requestCookie);
     writeServerFile(RESPONSE_COOKIE_FILE, responseCookie);
 
@@ -346,18 +235,12 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
   @Override
   public void run(
       RunRequest request, StreamObserver<RunResponse> observer) {
-    if (!request.getCookie().equals(requestCookie)
-        || request.getClientDescription().isEmpty()) {
+    if (!request.getCookie().equals(requestCookie)) {
       observer.onNext(RunResponse.newBuilder()
           .setExitCode(ExitCode.LOCAL_ENVIRONMENTAL_ERROR.getNumericExitCode())
           .build());
       observer.onCompleted();
       return;
-    }
-
-    ImmutableList.Builder<String> args = ImmutableList.builder();
-    for (ByteString requestArg : request.getArgList()) {
-      args.add(requestArg.toString(CHARSET));
     }
 
     String commandId;
@@ -368,19 +251,13 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
           new RpcOutputStream(observer, command.id, StreamType.STDOUT),
           new RpcOutputStream(observer, command.id, StreamType.STDERR));
 
-      exitCode = commandExecutor.exec(
-          args.build(), rpcOutErr,
-          request.getBlockForLock() ? LockingMode.WAIT : LockingMode.ERROR_OUT,
-          request.getClientDescription(), clock.currentTimeMillis());
-    } catch (InterruptedException e) {
-      exitCode = ExitCode.INTERRUPTED.getNumericExitCode();
-      commandId = "";  // The default value, the client will ignore it
+      exitCode = commandExecutor.exec(request.getArgList(), rpcOutErr, clock.currentTimeMillis());
     }
 
     // There is a chance that a cancel request comes in after commandExecutor#exec() has finished
     // and no one calls Thread.interrupted() to receive the interrupt. So we just reset the
     // interruption state here to make these cancel requests not have any effect outside of command
-    // execution (after the try block above, the cancel request won't find the thread to interrupt)
+    // execution.
     Thread.interrupted();
 
     RunResponse response = RunResponse.newBuilder()
@@ -423,8 +300,6 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
       if (command != null) {
         command.thread.interrupt();
       }
-
-      startSlowInterruptWatcher(ImmutableSet.of(request.getCommandId()));
     }
 
     streamObserver.onNext(CancelResponse.newBuilder().setCookie(responseCookie).build());
