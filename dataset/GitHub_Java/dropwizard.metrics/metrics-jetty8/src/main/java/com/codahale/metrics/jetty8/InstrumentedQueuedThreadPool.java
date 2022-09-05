d100f93d93b8823b7f946aa4f97d638e07cc1da9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:28:29 +0200

    Add utilization-max to jetty8

--- dataset/GitHub_Java/dropwizard.metrics/metrics-jetty8/src/main/java/com/codahale/metrics/jetty8/InstrumentedQueuedThreadPool.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-jetty8/src/main/java/com/codahale/metrics/jetty8/InstrumentedQueuedThreadPool.java
@@ -34,7 +34,7 @@ public class InstrumentedQueuedThreadPool extends QueuedThreadPool {
             public Integer getValue() {
                 // This assumes the QueuedThreadPool is using a BlockingArrayQueue or
                 // ArrayBlockingQueue for its queue, and is therefore a constant-time operation.
-                return getQueue() != null ? getQueue().size() : 0;
+                return getQueue().size();
             }
         });
         registry.register(name(QueuedThreadPool.class, "utilization-max"), new RatioGauge() {
