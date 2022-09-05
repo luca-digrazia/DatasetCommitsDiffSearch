817c39465846142d6f9566435e66b58d5de7cc47
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:03:00 +0200

    Testers using --experimental_objc_library will use apple crosstool     transition at the top level for the expanded set of apple rule classes
        --
        PiperOrigin-RevId: 145421255
        MOS_MIGRATED_REVID=145421255

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfigurationCollection.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfigurationCollection.java
@@ -17,8 +17,10 @@ package com.google.devtools.build.lib.bazel.rules;
 import com.google.common.cache.Cache;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Table;
 import com.google.devtools.build.lib.analysis.ConfigurationCollectionFactory;
@@ -34,7 +36,11 @@ import com.google.devtools.build.lib.events.EventHandler;
 import com.google.devtools.build.lib.packages.Attribute.ConfigurationTransition;
 import com.google.devtools.build.lib.packages.Attribute.SplitTransition;
 import com.google.devtools.build.lib.packages.Attribute.Transition;
+import com.google.devtools.build.lib.packages.Target;
+import com.google.devtools.build.lib.rules.apple.AppleConfiguration;
 import com.google.devtools.build.lib.rules.cpp.CppRuleClasses.LipoTransition;
+import com.google.devtools.build.lib.rules.objc.AppleCrosstoolSplitTransition;
+import com.google.devtools.build.lib.rules.objc.AppleCrosstoolTransition;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
@@ -97,13 +103,29 @@ public class BazelConfigurationCollection implements ConfigurationCollectionFact
     }
 
     @Override
-    public Transition getDynamicTransition(Transition configurationTransition) {
+    protected Transition getDynamicTransition(Transition configurationTransition) {
       if (configurationTransition == ConfigurationTransition.DATA) {
         return ConfigurationTransition.NONE;
       } else {
         return super.getDynamicTransition(configurationTransition);
       }
     }
+
+    @Override
+    public BuildConfiguration toplevelConfigurationHook(Target toTarget) {
+      ImmutableList<String> appleCrosstoolRuleClasses =
+          AppleCrosstoolTransition.shouldUseAppleCrosstoolTransition(configuration.getOptions())
+              ? AppleConfiguration.APPLE_CROSSTOOL_RULE_CLASSES
+              : AppleConfiguration.APPLE_CROSSTOOL_RULE_CLASSES_FOR_STATIC_CONFIGS;
+
+      return (appleCrosstoolRuleClasses.contains(toTarget.getAssociatedRule().getRuleClass()))
+          ? Iterables.getOnlyElement(
+              configuration
+                  .getTransitions()
+                  .getSplitConfigurations(
+                      AppleCrosstoolSplitTransition.APPLE_CROSSTOOL_SPLIT_TRANSITION))
+          : configuration;
+    } 
   }
 
   @Override
