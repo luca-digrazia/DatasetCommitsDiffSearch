65655d698f0fe85368e7f217ace9df87f828adb5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:49:05 +0200

    Deflake GlobTest dealing with glob interruption. Do not assume that an interrupted result implies that the thread pool is idle.
        --
        MOS_MIGRATED_REVID=97125559

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/vfs/GlobTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/vfs/GlobTest.java
@@ -1,4 +1,4 @@
-// Copyright 2014 The Bazel Authors. All rights reserved.
+// Copyright 2014 Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -364,7 +364,7 @@ public class GlobTest {
           .globInterruptible();
       fail();
     } catch (IllegalArgumentException e) {
-      assertThat(e.getMessage()).containsMatch("in glob pattern");
+      MoreAsserts.assertContainsRegex("in glob pattern", e.getMessage());
     }
   }
 
