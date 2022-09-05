c59bdaf316d96cd21634a5c8af908f60e28f8006
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:45:18 +0200

    Ensure that the first call of CalendarWeekDayFormatter.format() returns a correct day-of-week text.

--- dataset/GitHub_Java/prolificinteractive.material-calendarview/library/src/main/java/com/prolificinteractive/materialcalendarview/format/CalendarWeekDayFormatter.java
+++ dataset/GitHub_Java/prolificinteractive.material-calendarview/library/src/main/java/com/prolificinteractive/materialcalendarview/format/CalendarWeekDayFormatter.java
@@ -15,16 +15,29 @@ public class CalendarWeekDayFormatter implements WeekDayFormatter {
     private final Calendar calendar;
 
     /**
+     * Format with a specific calendar
+     *
      * @param calendar Calendar to retrieve formatting information from
      */
     public CalendarWeekDayFormatter(Calendar calendar) {
+        // recompute all fields of the calendar based on current date
+        // See "Getting and Setting Calendar Field Values"
+        // in https://developer.android.com/reference/java/util/Calendar.html
+        calendar.get(Calendar.DAY_OF_WEEK);  // Any fields to get is OK to recompute all fields in the calendar.
         this.calendar = calendar;
+
     }
 
+    /**
+     * Format with a default calendar
+     */
     public CalendarWeekDayFormatter() {
-        calendar = CalendarUtils.getInstance();
+        this(CalendarUtils.getInstance());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public CharSequence format(int dayOfWeek) {
         calendar.set(Calendar.DAY_OF_WEEK, dayOfWeek);
