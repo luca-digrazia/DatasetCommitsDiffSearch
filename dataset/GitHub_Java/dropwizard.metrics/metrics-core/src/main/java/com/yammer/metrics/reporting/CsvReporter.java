5388e673272612509ef7a94712e73950a02792d2
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:44:38 +0200

    Use Clock.time() instead of bogus attempt of converting ticks to milliseconds

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/reporting/CsvReporter.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/reporting/CsvReporter.java
@@ -1,8 +1,5 @@
 package com.yammer.metrics.reporting;
 
-import com.yammer.metrics.core.*;
-import com.yammer.metrics.util.MetricPredicate;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,8 +10,18 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-public class CsvReporter extends AbstractPollingReporter implements
-                                                         MetricsProcessor<CsvReporter.Context> {
+import com.yammer.metrics.core.Clock;
+import com.yammer.metrics.core.CounterMetric;
+import com.yammer.metrics.core.GaugeMetric;
+import com.yammer.metrics.core.HistogramMetric;
+import com.yammer.metrics.core.MeterMetric;
+import com.yammer.metrics.core.Metric;
+import com.yammer.metrics.core.MetricName;
+import com.yammer.metrics.core.MetricsRegistry;
+import com.yammer.metrics.core.TimerMetric;
+import com.yammer.metrics.util.MetricPredicate;
+
+public class CsvReporter extends AbstractPollingReporter {
     private final MetricPredicate predicate;
     private final File outputDir;
     private final Map<MetricName, PrintStream> streamMap;
@@ -22,11 +29,11 @@ public class CsvReporter extends AbstractPollingReporter implements
     private long startTime;
 
     public CsvReporter(File outputDir,
-                       MetricsRegistry metricsRegistry,
-                       MetricPredicate predicate) throws Exception {
+            MetricsRegistry metricsRegistry,
+            MetricPredicate predicate) throws Exception {
         this(outputDir, metricsRegistry, predicate, Clock.DEFAULT);
     }
-
+    
     public CsvReporter(File outputDir,
                        MetricsRegistry metricsRegistry,
                        MetricPredicate predicate,
@@ -44,7 +51,7 @@ public class CsvReporter extends AbstractPollingReporter implements
         this(outputDir, metricsRegistry, MetricPredicate.ALL);
     }
 
-    private PrintStream getPrintStream(MetricName metricName, String header)
+    private PrintStream getPrintStream(MetricName metricName, Metric metric)
             throws IOException {
         PrintStream stream;
         synchronized (streamMap) {
@@ -52,12 +59,22 @@ public class CsvReporter extends AbstractPollingReporter implements
             if (stream == null) {
                 stream = createStreamForMetric(metricName);
                 streamMap.put(metricName, stream);
-                stream.println(header);
+                if (metric instanceof GaugeMetric<?>) {
+                    stream.println("# time,value");
+                } else if (metric instanceof CounterMetric) {
+                    stream.println("# time,count");
+                } else if (metric instanceof HistogramMetric) {
+                    stream.println("# time,min,max,mean,median,stddev,90%,95%,99%");
+                } else if (metric instanceof MeterMetric) {
+                    stream.println("# time,count,1 min rate,mean rate,5 min rate,15 min rate");
+                } else if (metric instanceof TimerMetric) {
+                    stream.println("# time,min,max,mean,median,stddev,90%,95%,99%");
+                }
             }
         }
         return stream;
     }
-
+    
     /**
      * Override to do tricks (such as testing).
      */
@@ -78,89 +95,57 @@ public class CsvReporter extends AbstractPollingReporter implements
                 final MetricName metricName = entry.getKey();
                 final Metric metric = entry.getValue();
                 if (predicate.matches(metricName, metric)) {
-                    final Context context = new Context() {
-                        @Override
-                        public PrintStream getStream(String header) throws IOException {
-                            final PrintStream stream = getPrintStream(metricName, header);
-                            stream.print(time);
-                            stream.print(',');
-                            return stream;
-                        }
+                    final StringBuilder buf = new StringBuilder();
+                    buf.append(time).append(",");
+                    if (metric instanceof GaugeMetric<?>) {
+                        final Object objVal = ((GaugeMetric<?>) metric).value();
+                        buf.append(objVal);
+                    } else if (metric instanceof CounterMetric) {
+                        buf.append(((CounterMetric) metric).count());
+                    } else if (metric instanceof HistogramMetric) {
+                        final HistogramMetric timer = (HistogramMetric) metric;
 
-                    };
-                    metric.processWith(this, entry.getKey(), context);
+                        final double[] percentiles = timer.percentiles(0.5, 0.90, 0.95, 0.99);
+                        buf.append(timer.min()).append(",");
+                        buf.append(timer.max()).append(",");
+                        buf.append(timer.mean()).append(",");
+                        buf.append(percentiles[0]).append(","); // median
+                        buf.append(timer.stdDev()).append(",");
+                        buf.append(percentiles[1]).append(","); // 90%
+                        buf.append(percentiles[2]).append(","); // 95%
+                        buf.append(percentiles[3]); // 99 %
+                    } else if (metric instanceof MeterMetric) {
+                        buf.append(((MeterMetric) metric).count()).append(",");
+                        buf.append(((MeterMetric) metric).oneMinuteRate())
+                           .append(",");
+                        buf.append(((MeterMetric) metric).meanRate()).append(
+                                ",");
+                        buf.append(((MeterMetric) metric).fiveMinuteRate())
+                           .append(",");
+                        buf.append(((MeterMetric) metric).fifteenMinuteRate());
+                    } else if (metric instanceof TimerMetric) {
+                        final TimerMetric timer = (TimerMetric) metric;
+
+                        final double[] percentiles = timer.percentiles(0.5, 0.90, 0.95, 0.99);
+                        buf.append(timer.min()).append(",");
+                        buf.append(timer.max()).append(",");
+                        buf.append(timer.mean()).append(",");
+                        buf.append(percentiles[0]).append(","); // median
+                        buf.append(timer.stdDev()).append(",");
+                        buf.append(percentiles[1]).append(","); // 90%
+                        buf.append(percentiles[2]).append(","); // 95%
+                        buf.append(percentiles[3]); // 99 %
+                    }
+
+                    final PrintStream out = getPrintStream(metricName, metric);
+                    out.println(buf.toString());
+                    out.flush();
                 }
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static interface Context {
-        public PrintStream getStream(String header) throws IOException;
-    }
-
-    @Override
-    public void processMeter(MetricName name, Metered meter, Context context) throws IOException {
-        final PrintStream stream = context.getStream(
-                "# time,count,1 min rate,mean rate,5 min rate,15 min rate");
-        stream.append(new StringBuilder()
-                              .append(meter.count()).append(',')
-                              .append(meter.oneMinuteRate()).append(',')
-                              .append(meter.meanRate()).append(',')
-                              .append(meter.fiveMinuteRate()).append(',')
-                              .append(meter.fifteenMinuteRate()).toString())
-              .println();
-        stream.flush();
-    }
-
-    @Override
-    public void processCounter(MetricName name, CounterMetric counter, Context context) throws IOException {
-        final PrintStream stream = context.getStream("# time,count");
-        stream.println(counter.count());
-        stream.flush();
-    }
-
-    @Override
-    public void processHistogram(MetricName name, HistogramMetric histogram, Context context) throws IOException {
-        final PrintStream stream = context.getStream("# time,min,max,mean,median,stddev,90%,95%,99%");
-        final Double[] percentiles = histogram.percentiles(0.5, 0.90, 0.95, 0.99);
-        stream.append(new StringBuilder()
-                              .append(histogram.min()).append(',')
-                              .append(histogram.max()).append(',')
-                              .append(histogram.mean()).append(',')
-                              .append(percentiles[0]).append(',')     // median
-                              .append(histogram.stdDev()).append(',')
-                              .append(percentiles[1]).append(',')     // 90%
-                              .append(percentiles[2]).append(',')     // 95%
-                              .append(percentiles[3]).toString())     // 99 %
-                .println();
-        stream.println();
-        stream.flush();
-    }
-
-    @Override
-    public void processTimer(MetricName name, TimerMetric timer, Context context) throws IOException {
-        final PrintStream stream = context.getStream("# time,min,max,mean,median,stddev,90%,95%,99%");
-        final Double[] percentiles = timer.percentiles(0.5, 0.90, 0.95, 0.99);
-        stream.append(new StringBuilder()
-                              .append(timer.min()).append(',')
-                              .append(timer.max()).append(',')
-                              .append(timer.mean()).append(',')
-                              .append(percentiles[0]).append(',')     // median
-                              .append(timer.stdDev()).append(',')
-                              .append(percentiles[1]).append(',')     // 90%
-                              .append(percentiles[2]).append(',')     // 95%
-                              .append(percentiles[3]).toString())     // 99 %
-                .println();
-        stream.flush();
-    }
-
-    @Override
-    public void processGauge(MetricName name, GaugeMetric<?> gauge, Context context) throws IOException {
-        final PrintStream stream = context.getStream("# time,value");
-        stream.println(gauge.value());
-        stream.flush();
     }
 
     @Override
