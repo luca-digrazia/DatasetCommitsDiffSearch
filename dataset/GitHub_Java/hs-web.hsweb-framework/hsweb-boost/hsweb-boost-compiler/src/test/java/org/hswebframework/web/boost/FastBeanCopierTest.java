ca68a7ddc39997092132b14a87b4c073409af267
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:37:04 +0200

    ä¼åtest

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-boost/hsweb-boost-compiler/src/test/java/org/hswebframework/web/boost/FastBeanCopierTest.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-boost/hsweb-boost-compiler/src/test/java/org/hswebframework/web/boost/FastBeanCopierTest.java
@@ -9,9 +9,7 @@ import org.junit.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 
 /**
  * TODO 完成注释
@@ -31,24 +29,17 @@ public class FastBeanCopierTest {
         nestObject.setAge(10);
         nestObject.setName("测试2");
         source.setNestObject(nestObject);
-
-
         Target target = new Target();
+        Copier copier = FastBeanCopier.createCopier(Source.class, Target.class);
         FastBeanCopier.copy(source, target);
-
         long t = System.currentTimeMillis();
         for (int i = 100_0000; i > 0; i--) {
-            FastBeanCopier.copy(source, target);
+            copier.copy(source, target, new HashSet<>(), FastBeanCopier.DEFAULT_CONVERT);
         }
         System.out.println(System.currentTimeMillis() - t);
 
         System.out.println(target);
         System.out.println(target.getNestObject() == source.getNestObject());
-//        Source source1=new Source();
-
-//        FastBeanCopier.copy(source,source1);
-
-//        System.out.println(source1);
 //
 //        t = System.currentTimeMillis();
 //
@@ -62,23 +53,6 @@ public class FastBeanCopierTest {
 //        System.out.println(System.currentTimeMillis() - t);
 //        System.out.println(target);
 //        System.out.println(target.getNestObject() == source.getNestObject());
-    }
-
-
-    @Test
-    public void testCopyToMap() {
-        Source source = new Source();
-        source.setAge(100);
-        source.setName("测试");
-        source.setIds(new String[]{"1", "2", "3"});
-        NestObject nestObject = new NestObject();
-        nestObject.setAge(10);
-        nestObject.setName("测试2");
-        source.setNestObject(nestObject);
-        Map<String,Object> target = new HashMap<>();
-        FastBeanCopier.copy(source, target);
-
-        System.out.println(target);
     }
 
 
