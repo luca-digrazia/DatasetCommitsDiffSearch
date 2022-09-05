516c43a0762dc2d5aa4aa50ddd1ed31d68943245
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:50:54 +0200

    Add NonNull annotations

--- dataset/GitHub_Java/prolificinteractive.material-calendarview/library/src/main/java/com/prolificinteractive/materialcalendarview/format/DateFormatDayFormatter.java
+++ dataset/GitHub_Java/prolificinteractive.material-calendarview/library/src/main/java/com/prolificinteractive/materialcalendarview/format/DateFormatDayFormatter.java
@@ -16,24 +16,19 @@ public class DateFormatDayFormatter implements DayFormatter {
     private final DateFormat dateFormat;
 
     /**
-     * Format using a default format
+     * Uses "MMMM yyyy" for formatting
      */
     public DateFormatDayFormatter() {
         this.dateFormat = new SimpleDateFormat("d", Locale.getDefault());
     }
 
     /**
-     * Format using a specific {@linkplain DateFormat}
-     *
      * @param format the format to use
      */
     public DateFormatDayFormatter(@NonNull DateFormat format) {
         this.dateFormat = format;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public @NonNull String format(@NonNull CalendarDay day) {
         return dateFormat.format(day.getDate());
