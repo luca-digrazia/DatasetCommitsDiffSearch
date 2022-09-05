15056b9d4696bbd22b1502bb1a7f5e946bc43874
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:15:11 +0200

    å¢å creatorNameåmodifierName

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/web/reactive/ReactiveServiceSaveController.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/web/reactive/ReactiveServiceSaveController.java
@@ -1,5 +1,7 @@
 package org.hswebframework.web.crud.web.reactive;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.hswebframework.ezorm.rdb.mapping.defaults.SaveResult;
 import org.hswebframework.web.api.crud.entity.RecordCreationEntity;
 import org.hswebframework.web.api.crud.entity.RecordModifierEntity;
 import org.hswebframework.web.authorization.Authentication;
@@ -7,6 +9,7 @@ import org.hswebframework.web.authorization.annotation.Authorize;
 import org.hswebframework.web.authorization.annotation.SaveAction;
 import org.hswebframework.web.crud.service.ReactiveCrudService;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ReactiveServiceSaveController<E,K>  {
@@ -19,21 +22,21 @@ public interface ReactiveServiceSaveController<E,K>  {
         RecordCreationEntity creationEntity = ((RecordCreationEntity) entity);
         creationEntity.setCreateTimeNow();
         creationEntity.setCreatorId(authentication.getUser().getId());
-
+        creationEntity.setCreatorName(authentication.getUser().getName());
         return entity;
     }
 
     @Authorize(ignore = true)
     default E applyModifierEntity(Authentication authentication, E entity) {
-        RecordModifierEntity creationEntity = ((RecordModifierEntity) entity);
-        creationEntity.setModifyTimeNow();
-        creationEntity.setModifierId(authentication.getUser().getId());
-
+        RecordModifierEntity modifierEntity = ((RecordModifierEntity) entity);
+        modifierEntity.setModifyTimeNow();
+        modifierEntity.setModifierId(authentication.getUser().getId());
+        modifierEntity.setModifierName(authentication.getUser().getName());
         return entity;
     }
 
     @Authorize(ignore = true)
-    default E applyAuthentication(Authentication authentication, E entity) {
+    default E applyAuthentication(E entity, Authentication authentication) {
         if (entity instanceof RecordCreationEntity) {
             entity = applyCreationEntity(authentication, entity);
         }
@@ -45,28 +48,47 @@ public interface ReactiveServiceSaveController<E,K>  {
 
     @PatchMapping
     @SaveAction
-    default Mono<E> save(@RequestBody Mono<E> payload) {
+    @Operation(summary = "保存数据", description = "如果传入了id,并且对应数据存在,则尝试覆盖,不存在则新增.")
+    default Mono<SaveResult> save(@RequestBody Flux<E> payload) {
         return Authentication.currentReactive()
-                .zipWith(payload, this::applyAuthentication)
+                .flatMapMany(auth -> payload.map(entity -> applyAuthentication(entity, auth)))
                 .switchIfEmpty(payload)
-                .flatMap(entity -> getService().save(Mono.just(entity)).thenReturn(entity));
+                .as(getService()::save);
+    }
+
+    @PostMapping("/_batch")
+    @SaveAction
+    @Operation(summary = "批量新增数据")
+    default Mono<Integer> add(@RequestBody Flux<E> payload) {
+
+        return Authentication.currentReactive()
+                .flatMapMany(auth -> payload.map(entity -> applyAuthentication(entity, auth)))
+                .switchIfEmpty(payload)
+                .collectList()
+                .as(getService()::insertBatch);
     }
 
     @PostMapping
     @SaveAction
+    @Operation(summary = "新增单个数据,并返回新增后的数据.")
     default Mono<E> add(@RequestBody Mono<E> payload) {
-        return  Authentication.currentReactive()
-                .zipWith(payload, this::applyAuthentication)
+        return Authentication.currentReactive()
+                .flatMap(auth -> payload.map(entity -> applyAuthentication(entity, auth)))
                 .switchIfEmpty(payload)
                 .flatMap(entity -> getService().insert(Mono.just(entity)).thenReturn(entity));
     }
 
+
     @PutMapping("/{id}")
     @SaveAction
-    default Mono<E> update(@PathVariable K id, @RequestBody Mono<E> payload) {
-        return  Authentication.currentReactive()
-                .zipWith(payload, this::applyAuthentication)
+    @Operation(summary = "根据ID修改数据")
+    default Mono<Boolean> update(@PathVariable K id, @RequestBody Mono<E> payload) {
+
+        return Authentication.currentReactive()
+                .flatMap(auth -> payload.map(entity -> applyAuthentication(entity, auth)))
                 .switchIfEmpty(payload)
-                .flatMap(entity -> getService().updateById(id,Mono.just(entity)).thenReturn(entity));
+                .flatMap(entity -> getService().updateById(id, Mono.just(entity)))
+                .thenReturn(true);
+
     }
 }
