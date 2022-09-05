1d34328805ed0a15185c150869fe2db8a61fe639
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:37:22 +0200

    Allow registration of gauges to non-default registry

---
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-sigar/src/main/java/com/yammer/metrics/sigar/SigarMetrics.java
@@ -0,0 +1,42 @@
+package com.yammer.metrics.sigar;
+
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.MetricsRegistry;
+
+import org.hyperic.sigar.Sigar;
+
+public class SigarMetrics implements CanRegisterGauges {
+    private static final SigarMetrics instance = new SigarMetrics();
+
+    public static SigarMetrics getInstance() {
+        return instance;
+    }
+
+    private final Sigar sigar = new Sigar();
+    private final CpuMetrics cpu = new CpuMetrics(sigar); 
+    private final MemoryMetrics memory = new MemoryMetrics(sigar); 
+
+    private SigarMetrics() {
+        // singleton
+    }
+
+    /**
+     * Register all gauges in the default registry.
+     */
+    public void registerGauges() {
+        registerGauges(Metrics.defaultRegistry());
+    }
+
+    public void registerGauges(MetricsRegistry registry) {
+        cpu.registerGauges(registry);
+        memory.registerGauges(registry);
+    }
+
+    public CpuMetrics cpu() {
+        return cpu;
+    }
+
+    public MemoryMetrics memory() {
+        return memory;
+    }
+}
