55203e8fa02e9a6f6c1b4fe1ab5b2bc9364c20c5
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:23:05 +0200

    Make FileSystemUtils#asByteSource() private again
        --
        MOS_MIGRATED_REVID=101976891

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/vfs/FileSystemUtils.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/vfs/FileSystemUtils.java
@@ -608,7 +608,7 @@ public class FileSystemUtils {
         copyTreesBelow(entry, subDir);
       } else if (entry.isSymbolicLink()) {
         Path newLink = to.getChild(entry.getBaseName());
-        newLink.createSymbolicLink(entry.readSymbolicLinkUnchecked());
+        newLink.createSymbolicLink(entry.readSymbolicLink());
       } else {
         Path newEntry = to.getChild(entry.getBaseName());
         copyFile(entry, newEntry);
