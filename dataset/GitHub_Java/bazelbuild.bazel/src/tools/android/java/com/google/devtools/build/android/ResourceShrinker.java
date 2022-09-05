15fa35b0ef45cdd61af8b443173974aaf04f4bbf
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:19:45 +0200

    Add output log for android resource shrinking, following the format produced by the Gradle resource shrinker in resources.txt.
        --
        MOS_MIGRATED_REVID=126420534

--- dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/ResourceShrinker.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/ResourceShrinker.java
@@ -24,7 +24,6 @@ import static com.android.SdkConstants.DOT_GIF;
 import static com.android.SdkConstants.DOT_JPEG;
 import static com.android.SdkConstants.DOT_JPG;
 import static com.android.SdkConstants.DOT_PNG;
-import static com.android.SdkConstants.DOT_SVG;
 import static com.android.SdkConstants.DOT_XML;
 import static com.android.SdkConstants.FD_RES_VALUES;
 import static com.android.SdkConstants.PREFIX_ANDROID;
@@ -639,8 +638,7 @@ public class ResourceShrinker {
             || endsWith(path, DOT_PNG) //also true for endsWith(name, DOT_9PNG)
             || endsWith(path, DOT_JPG)
             || endsWith(path, DOT_GIF)
-            || endsWith(path, DOT_JPEG)
-            || endsWith(path, DOT_SVG))) {
+            || endsWith(path, DOT_JPEG))) {
           List<ResourceType> types = FolderTypeRelationship.getRelatedResourceTypes(
               folderType);
           ResourceType type = types.get(0);
