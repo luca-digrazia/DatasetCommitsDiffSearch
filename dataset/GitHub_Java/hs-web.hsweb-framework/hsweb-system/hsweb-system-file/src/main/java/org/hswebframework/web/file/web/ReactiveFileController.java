36842db35cdd95c18ab84ddee9034469d16cb392
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:15:08 +0200

    ä¼åæä»¶å­å¨

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-file/src/main/java/org/hswebframework/web/file/web/ReactiveFileController.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-system/hsweb-system-file/src/main/java/org/hswebframework/web/file/web/ReactiveFileController.java
@@ -1,52 +1,57 @@
 package org.hswebframework.web.file.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.hswebframework.web.authorization.annotation.Resource;
 import org.hswebframework.web.authorization.annotation.ResourceAction;
+import org.hswebframework.web.authorization.exception.AccessDenyException;
 import org.hswebframework.web.file.FileUploadProperties;
+import org.hswebframework.web.file.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.http.codec.multipart.Part;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.channels.AsynchronousFileChannel;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
 @RestController
 @Resource(id = "file", name = "文件上传")
 @Slf4j
 @RequestMapping("/file")
+@Tag(name = "文件上传")
 public class ReactiveFileController {
 
-    @Autowired
-    private FileUploadProperties properties;
+    private final FileUploadProperties properties;
 
-    @ResourceAction(id = "upload-static", name = "静态文件")
+    private final FileStorageService fileStorageService;
+
+    public ReactiveFileController(FileUploadProperties properties, FileStorageService fileStorageService) {
+        this.properties = properties;
+        this.fileStorageService = fileStorageService;
+    }
+
     @PostMapping("/static")
     @SneakyThrows
-    public Mono<String> uploadStatic(@RequestPart("file") Part part) {
-        FileUploadProperties.StaticFileInfo name ;
-        if(part instanceof FilePart){
-            name = properties.createStaticSavePath(((FilePart)part).filename());
-            return ((FilePart)part)
-                    .transferTo(new File(name.getSavePath()))
-                    .thenReturn(name.getLocation());
-        }else{
-           return Mono.error(()->new IllegalArgumentException("[file] part is not a file"));
+    @ResourceAction(id = "upload-static", name = "静态文件")
+    @Operation(summary = "上传静态文件")
+    public Mono<String> uploadStatic(@RequestPart("file")
+                                     @Parameter(name = "file", description = "文件", style = ParameterStyle.FORM) Part part) {
+        if (part instanceof FilePart) {
+            FilePart filePart = ((FilePart) part);
+            if (properties.denied(filePart.filename(), filePart.headers().getContentType())) {
+                throw new AccessDenyException();
+            }
+            return fileStorageService.saveFile(filePart);
+        } else {
+            return Mono.error(() -> new IllegalArgumentException("[file] part is not a file"));
         }
 
     }
