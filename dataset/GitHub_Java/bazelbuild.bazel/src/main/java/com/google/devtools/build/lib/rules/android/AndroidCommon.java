8cbbafb7aa681b2fbb340d3ff93869957779a890
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:35:09 +0200

    Dedupe Jar files produced for Android binary targets for desugaring purposes.
        --
        MOS_MIGRATED_REVID=137305868

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidCommon.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/android/AndroidCommon.java
@@ -636,7 +636,6 @@ public class AndroidCommon {
         // sources
         .addJavaSources(attributes.getSourceFiles())
         .addSourceJars(attributes.getSourceJars())
-        .addCompiledJars(attributes.getDirectJars().toCollection())
         .addResources(attributes.getResources())
         .addProcessorNames(attributes.getProcessorNames())
         .addProcessorClasspathJars(attributes.getProcessorPath())
