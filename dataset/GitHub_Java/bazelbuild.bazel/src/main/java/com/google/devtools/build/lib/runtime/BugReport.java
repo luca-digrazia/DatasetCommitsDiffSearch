cd1c9522d1a8a998f2e7175c2b0aa9f024bba0fc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:37:50 +0200

    Fix shutdown deadlock when a shutdown hook crashes - halt() instead of exit().     See associated bug for more details.
        --
        MOS_MIGRATED_REVID=86708715

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/BugReport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/runtime/BugReport.java
@@ -73,7 +73,9 @@ public abstract class BugReport {
     BugReport.printBug(OutErr.SYSTEM_OUT_ERR, throwable);
     System.err.println("Blaze crash in async thread:");
     throwable.printStackTrace();
-    int exitCode = getExitCodeForThrowable(throwable);
+    int exitCode =
+        (throwable instanceof OutOfMemoryError) ? ExitCode.OOM_ERROR.getNumericExitCode()
+            : ExitCode.BLAZE_INTERNAL_ERROR.getNumericExitCode();
     if (runtime != null) {
       runtime.notifyCommandComplete(exitCode);
       // We don't call runtime#shutDown() here because all it does is shut down the modules, and who
@@ -84,13 +86,6 @@ public abstract class BugReport {
     // deadlock. This call would block on the shutdown sequence completing, but the shutdown
     // sequence would in turn be blocked on this thread finishing. Instead, exit fast via halt().
     Runtime.getRuntime().halt(exitCode);
-  }
-
-  /** Get exit code corresponding to throwable. */
-  public static int getExitCodeForThrowable(Throwable throwable) {
-    return (throwable instanceof OutOfMemoryError)
-        ? ExitCode.OOM_ERROR.getNumericExitCode()
-        : ExitCode.BLAZE_INTERNAL_ERROR.getNumericExitCode();
   }
 
   private static void printThrowableTo(OutErr outErr, Throwable e) {
