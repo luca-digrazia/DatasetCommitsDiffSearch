567c965f3d2805ce4a261d6ab109c4ce99397801
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:08:02 +0200

    å¢å  å¤æ¬¡é¨ç½²æµè¯

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-impl-common/src/test/java/org/hsweb/web/service/impl/form/FormServiceImplTest.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-impl-common/src/test/java/org/hsweb/web/service/impl/form/FormServiceImplTest.java
@@ -1,25 +1,22 @@
 package org.hsweb.web.service.impl.form;
 
-import org.hsweb.commons.file.FileUtils;
-import org.hsweb.ezorm.executor.SqlExecutor;
-import org.hsweb.ezorm.run.Database;
-import org.hsweb.ezorm.run.Table;
-import org.hsweb.web.core.authorize.ExpressionScopeBean;
 import org.hsweb.web.bean.po.form.Form;
-import org.hsweb.web.service.form.DynamicFormService;
 import org.hsweb.web.service.form.FormService;
 import org.hsweb.web.service.impl.AbstractTestCase;
-import org.hsweb.web.core.utils.RandomUtil;
+import org.hsweb.web.utils.RandomUtil;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import org.webbuilder.sql.DataBase;
+import org.webbuilder.sql.param.insert.InsertParam;
+import org.webbuilder.sql.param.query.QueryParam;
+import org.webbuilder.sql.support.common.CommonSql;
+import org.webbuilder.sql.support.executor.SqlExecutor;
 
 import javax.annotation.Resource;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -31,16 +28,12 @@ public class FormServiceImplTest extends AbstractTestCase {
 
     @Resource
     protected FormService formService;
+
     @Resource
-    private DynamicFormService dynamicFormService;
-    @Resource
-    protected Database dataBase;
+    protected DataBase dataBase;
 
     @Resource
     protected SqlExecutor sqlExecutor;
-
-    @Autowired(required = false)
-    private Map<String, ExpressionScopeBean> expressionScopeBeanMap;
 
     protected Form form;
 
@@ -48,34 +41,33 @@ public class FormServiceImplTest extends AbstractTestCase {
             "{" +
                     "\"id1\":[" +
                     "{\"key\":\"name\",\"value\":\"u_id\",\"describe\":\"名称\"}," +
-                    "{\"key\":\"comment\",\"value\":\"ID\",\"describe\":\"ID\"}," +
+                    "{\"key\":\"comment\",\"value\":\"ID\",\"describe\":\"字段描述\"}," +
                     "{\"key\":\"javaType\",\"value\":\"string\",\"describe\":\"java类型\"}," +
                     "{\"key\":\"dataType\",\"value\":\"varchar2(32)\",\"describe\":\"数据库类型\"}" +
                     "]" +
                     ",\"id2\":[" +
                     "{\"key\":\"name\",\"value\":\"name\",\"describe\":\"名称\"}," +
-                    "{\"key\":\"comment\",\"value\":\"姓名\",\"describe\":\"姓名\"}," +
+                    "{\"key\":\"comment\",\"value\":\"test\",\"describe\":\"字段描述\"}," +
                     "{\"key\":\"javaType\",\"value\":\"string\",\"describe\":\"java类型\"}," +
-                    "{\"key\":\"validator-list\",\"value\":\"[{\\\"validator\\\":\\\"NotNull\\\"}]\",,\"describe\":\"java类型\"}," +
                     "{\"key\":\"dataType\",\"value\":\"varchar2(32)\",\"describe\":\"数据库类型\"}" +
                     "]" +
                     "}",
             "{" +
                     "\"id1\":[" +
                     "{\"key\":\"name\",\"value\":\"u_id\",\"describe\":\"名称\"}," +
-                    "{\"key\":\"comment\",\"value\":\"ID\",\"describe\":\"ID\"}," +
+                    "{\"key\":\"comment\",\"value\":\"ID\",\"describe\":\"字段描述\"}," +
                     "{\"key\":\"javaType\",\"value\":\"string\",\"describe\":\"java类型\"}," +
                     "{\"key\":\"dataType\",\"value\":\"varchar2(32)\",\"describe\":\"数据库类型\"}" +
                     "]" +
                     ",\"id2\":[" +
                     "{\"key\":\"name\",\"value\":\"name\",\"describe\":\"名称\"}," +
-                    "{\"key\":\"comment\",\"value\":\"姓名\",\"describe\":\"字段描述\"}," +
+                    "{\"key\":\"comment\",\"value\":\"test\",\"describe\":\"字段描述\"}," +
                     "{\"key\":\"javaType\",\"value\":\"string\",\"describe\":\"java类型\"}," +
                     "{\"key\":\"dataType\",\"value\":\"varchar2(32)\",\"describe\":\"数据库类型\"}" +
                     "]" +
                     ",\"id3\":[" +
                     "{\"key\":\"name\",\"value\":\"sex\",\"describe\":\"名称\"}," +
-                    "{\"key\":\"comment\",\"value\":\"性别\",\"describe\":\"性别\"}," +
+                    "{\"key\":\"comment\",\"value\":\"test\",\"describe\":\"性别\"}," +
                     "{\"key\":\"javaType\",\"value\":\"string\",\"describe\":\"java类型\"}," +
                     "{\"key\":\"dataType\",\"value\":\"varchar2(32)\",\"describe\":\"数据库类型\"}" +
                     "]" +
@@ -84,28 +76,54 @@ public class FormServiceImplTest extends AbstractTestCase {
 
     @Before
     public void setup() throws Exception {
+        sqlExecutor.exec(new CommonSql("drop table if exists s_form"));
+        sqlExecutor.exec(new CommonSql("create table s_form\n" +
+                "(\n" +
+                "  u_id        varchar(256) not null,\n" +
+                "  name        varchar(256) not null,\n" +
+                "  html        clob,\n" +
+                "  meta        clob,\n" +
+                "  config      clob,\n" +
+                "  version     number(32),\n" +
+                "  using       int,\n" +
+                "  create_date date not null,\n" +
+                "  update_date date,\n" +
+                "  remark      varchar2(200)\n" +
+                ")"));
+        sqlExecutor.exec(new CommonSql("drop table if exists s_history"));
+        sqlExecutor.exec(new CommonSql("create table s_history\n" +
+                "(\n" +
+                "  u_id              varchar2(32) not null,\n" +
+                "  type              varchar2(64) not null,\n" +
+                "  describe          varchar2(512),\n" +
+                "  primary_key_name  varchar2(32),\n" +
+                "  primary_key_value varchar2(64),\n" +
+                "  change_before     clob,\n" +
+                "  change_after      clob,\n" +
+                "  create_date       date not null,\n" +
+                "  creator_id        varchar2(32)\n" +
+                ")"));
         form = new Form();
         form.setName("test_form");
-        form.setCreateDate(new Date());
-        form.setHtml("<input fieldId='id1'/><input fieldId='id2'/>");
+        form.setCreate_date(new Date());
+        form.setHtml("<input field-id='id1'/><input field-id='id2'/>");
         form.setMeta(meta[0]);
-        form.setId(RandomUtil.randomChar());
+        form.setU_id(RandomUtil.randomChar());
         formService.insert(form);
     }
 
     @Test
     public void testDeploy() throws Exception {
-        formService.deploy(form.getId());
-        dataBase.getTable("test_form").createInsert().value(new HashMap<String, Object>() {{
-            put("u_id", "test");
-            put("name", "张三2");
-        }}).exec();
-
-        Table<Map<String, Object>> table = dataBase.getTable("test_form");
-        table.createUpdate().set("name", "张三").where("u_id", "test").exec();
-        Map<String, Object> data = table.createQuery().where("name$LIKE", "张三").single();
+        //部署
+        formService.deploy(form.getU_id());
+        dataBase.getTable("test_form").createInsert()
+                .insert(new InsertParam().value("name", "张三").value("u_id", "test"));
+        Map<String, Object> data = dataBase.getTable("test_form")
+                .createQuery().single(new QueryParam().where("name$LIKE", "张三"));
         Assert.assertEquals("张三", data.get("name"));
+//        form.setMeta(meta[1]);
+//        formService.update(form);
+//        formService.deploy(form.getU_id());
     }
-
 
 }
