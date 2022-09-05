74b6240c1b4838c3b98c6532e5a04da4b0f8ba0b
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:04:02 +0200

    Rename "bundle" apple_binary to "loadable_bundle".
        Also some apple binary test cleanup along the way.
    
        --
        PiperOrigin-RevId: 145123804
        MOS_MIGRATED_REVID=145123804

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/AppleBinaryRule.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/AppleBinaryRule.java
@@ -38,16 +38,7 @@ import com.google.devtools.build.lib.rules.apple.AppleConfiguration;
 public class AppleBinaryRule implements RuleDefinition {
 
   public static final String BINARY_TYPE_ATTR = "binary_type";
-  public static final String BUNDLE_LOADER_ATTR_NAME = "bundle_loader";
-
-  private final ObjcProtoAspect objcProtoAspect;
-
-  /**
-   * Constructor that returns a newly configured AppleBinaryRule object.
-   */
-  public AppleBinaryRule(ObjcProtoAspect objcProtoAspect) {
-    this.objcProtoAspect = objcProtoAspect;
-  }
+  public static final String BUNDLE_LOADER_ATTR = "bundle_loader";
 
   /**
    * Template for the fat binary output (using Apple's "lipo" tool to combine binaries of
@@ -116,15 +107,18 @@ public class AppleBinaryRule implements RuleDefinition {
             attr(BINARY_TYPE_ATTR, STRING)
                 .value(AppleBinary.BinaryType.EXECUTABLE.toString())
                 .allowedValues(new AllowedValueSet(AppleBinary.BinaryType.getValues())))
+        /* <!-- #BLAZE_RULE(apple_binary).ATTRIBUTE(bundle_loader) -->
+        The bundle loader to be used when linking this bundle. Can only be set when binary_type is
+        "bundle". This bundle loader may contain symbols that were not linked into the bundle to
+        reduce binary size.
+        <!-- #END_BLAZE_RULE.ATTRIBUTE -->*/
         .add(
-            attr(BUNDLE_LOADER_ATTR_NAME, LABEL)
-                .direct_compile_time_input()
+            attr(BUNDLE_LOADER_ATTR, LABEL)
                 .mandatoryNativeProviders(
                     ImmutableList.<Class<? extends TransitiveInfoProvider>>of(
-                        AppleExecutableBinaryProvider.class))
-                .allowedFileTypes()
-                .singleArtifact()
-                .aspect(objcProtoAspect))
+                        BundleLoaderProvider.class))
+                .legacyAllowAnyFileType()
+                .singleArtifact())
         .override(builder.copy("deps").cfg(splitTransitionProvider))
         .override(builder.copy("non_propagated_deps").cfg(splitTransitionProvider))
         /*<!-- #BLAZE_RULE(apple_binary).IMPLICIT_OUTPUTS -->
