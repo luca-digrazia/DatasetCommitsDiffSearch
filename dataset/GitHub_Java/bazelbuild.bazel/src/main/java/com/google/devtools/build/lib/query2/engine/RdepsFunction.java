71d122fd83c49782df0d2f7013e4101ff207c736
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:50:19 +0200

    Add "allrdeps" function that computes rdeps in whatever universe happens to be loaded.
        This avoids having to materialize the full universe scope in memory when doing an rdeps query over "//...".
    
        Another option considered was special-casing the string "//..." if it was the first argument to rdeps, but this seemed cleaner.
    
        --
        MOS_MIGRATED_REVID=96878999

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/query2/engine/RdepsFunction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/query2/engine/RdepsFunction.java
@@ -54,7 +54,7 @@ final class RdepsFunction extends AllRdepsFunction {
    */
   @Override
   public <T> Set<T> eval(QueryEnvironment<T> env, QueryExpression expression, List<Argument> args)
-      throws QueryException, InterruptedException {
+      throws QueryException {
     Set<T> universeValue = args.get(0).getExpression().eval(env);
     env.buildTransitiveClosure(expression, universeValue, Integer.MAX_VALUE);
 
