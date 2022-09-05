c2ee2c5aecf21d9ee55e26ddadaba66a95357c18
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 02:33:28 +0200

    Android native libraries compile with -mstackrealign for 32bit x86 clang.
        This is a workaround for a clang bug. See
        https://code.google.com/p/android/issues/detail?id=220159.
    
        RELNOTES: Fix for Android clang++ std::stack segfault on 32bit x86. See https://code.google.com/p/android/issues/detail?id=220159
    
        --
        MOS_MIGRATED_REVID=137871199

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/android/ndkcrosstools/r12/X86Crosstools.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/android/ndkcrosstools/r12/X86Crosstools.java
@@ -53,6 +53,7 @@ class X86Crosstools {
             .addAllToolPath(ndkPaths.createClangToolpaths("x86-4.9", "i686-linux-android", null))
             .setBuiltinSysroot(ndkPaths.createBuiltinSysroot("x86"));
 
+    ndkPaths.addToolchainIncludePaths(x86Clang, "x86-4.9", "i686-linux-android", "4.9.x");
     stlImpl.addStlImpl(x86Clang, "4.9");
     toolchains.add(x86Clang);
 
@@ -63,12 +64,10 @@ class X86Crosstools {
             .setTargetCpu("x86_64")
             .setCompiler("gcc-4.9")
             .addAllToolPath(ndkPaths.createToolpaths("x86_64-4.9", "x86_64-linux-android"))
-            .addAllCxxBuiltinIncludeDirectory(
-                ndkPaths.createGccToolchainBuiltinIncludeDirectories(
-                    "x86_64-4.9", "x86_64-linux-android", "4.9.x"))
             .setBuiltinSysroot(ndkPaths.createBuiltinSysroot("x86_64"))
             .addCompilerFlag("-fstack-protector-strong");
 
+    ndkPaths.addToolchainIncludePaths(x8664, "x86_64-4.9", "x86_64-linux-android", "4.9.x");
     stlImpl.addStlImpl(x8664, "4.9");
     toolchains.add(x8664);
 
@@ -80,6 +79,7 @@ class X86Crosstools {
                 ndkPaths.createClangToolpaths("x86_64-4.9", "x86_64-linux-android", null))
             .setBuiltinSysroot(ndkPaths.createBuiltinSysroot("x86_64"));
 
+    ndkPaths.addToolchainIncludePaths(x8664Clang, "x86_64-4.9", "x86_64-linux-android", "4.9.x");
     stlImpl.addStlImpl(x8664Clang, "4.9");
     toolchains.add(x8664Clang);
 
@@ -101,12 +101,10 @@ class X86Crosstools {
             .setTargetCpu("x86")
             .setCompiler("gcc-4.9")
             .addAllToolPath(ndkPaths.createToolpaths("x86-4.9", "i686-linux-android"))
-            .addAllCxxBuiltinIncludeDirectory(
-                ndkPaths.createGccToolchainBuiltinIncludeDirectories(
-                    "x86-4.9", "i686-linux-android", "4.9.x"))
             .setBuiltinSysroot(ndkPaths.createBuiltinSysroot("x86"))
             .addCompilerFlag("-fstack-protector-strong");
 
+    ndkPaths.addToolchainIncludePaths(toolchain, "x86-4.9", "i686-linux-android", "4.9.x");
     stlImpl.addStlImpl(toolchain, "4.9");
     return toolchain;
   }
@@ -146,10 +144,6 @@ class X86Crosstools {
 
     return CToolchain.newBuilder()
         .setCompiler("clang3.8")
-
-        .addCxxBuiltinIncludeDirectory(
-            ndkPaths.createClangToolchainBuiltinIncludeDirectory(
-                AndroidNdkCrosstoolsR12.CLANG_VERSION))
 
         // Compiler flags
         .addCompilerFlag("-gcc-toolchain")
