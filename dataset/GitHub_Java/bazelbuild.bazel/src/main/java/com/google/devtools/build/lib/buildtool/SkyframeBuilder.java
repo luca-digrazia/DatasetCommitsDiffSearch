b92e909eb223923c28edc6d4146240862b3d1853
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:08:45 +0200

    Extract ExecutionProgressReceiver into a top-level class.
        --
        Change-Id: I7f9edbef4dd94b5d37aabb1a17333224af3615c6
        Reviewed-on: https://bazel-review.googlesource.com/#/c/3042
        MOS_MIGRATED_REVID=116163936

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/buildtool/SkyframeBuilder.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/buildtool/SkyframeBuilder.java
@@ -28,7 +28,6 @@ import com.google.devtools.build.lib.actions.MissingInputFileException;
 import com.google.devtools.build.lib.actions.ResourceManager;
 import com.google.devtools.build.lib.actions.TestExecException;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
-import com.google.devtools.build.lib.buildtool.buildevent.ExecutionProgressReceiverAvailableEvent;
 import com.google.devtools.build.lib.events.EventHandler;
 import com.google.devtools.build.lib.events.Reporter;
 import com.google.devtools.build.lib.packages.BuildFileNotFoundException;
@@ -104,9 +103,6 @@ public class SkyframeBuilder implements Builder {
     ExecutionProgressReceiver executionProgressReceiver =
         new ExecutionProgressReceiver(Preconditions.checkNotNull(builtTargets),
             countTestActions(exclusiveTests), skyframeExecutor.getEventBus());
-    skyframeExecutor
-        .getEventBus()
-        .post(new ExecutionProgressReceiverAvailableEvent(executionProgressReceiver));
     ResourceManager.instance().setEventBus(skyframeExecutor.getEventBus());
 
     boolean success = false;
