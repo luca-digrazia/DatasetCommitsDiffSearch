a4faeb7ab7d79898f028c316bc447dcce2c6f489
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 05:32:57 +0200

    Improve thread pools where we allow idle threads to be reclaimed, by just increasing the thread worker timeout.
        --
        MOS_MIGRATED_REVID=100820878

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
@@ -939,7 +939,6 @@ public final class PackageFactory {
       public Environment.NoneType invoke(Map<String, Object> kwargs,
           FuncallExpression ast, Environment env)
           throws EvalException {
-        env.checkLoadingPhase(ruleClass, ast.getLocation());
         try {
           addRule(ruleFactory, ruleClass, getContext(env, ast), kwargs, ast);
         } catch (RuleFactory.InvalidRuleException | Package.NameConflictException e) {
@@ -1234,7 +1233,6 @@ public final class PackageFactory {
     // Important: Environment should be unreachable by the end of this method!
     StoredEventHandler eventHandler = new StoredEventHandler();
     Environment pkgEnv = new Environment(globalEnv, eventHandler);
-    pkgEnv.setLoadingPhase();
 
     Package.LegacyBuilder pkgBuilder = new Package.LegacyBuilder(packageId);
 
@@ -1304,7 +1302,6 @@ public final class PackageFactory {
     }
     // Important: Environment should be unreachable by the end of this method!
     Environment pkgEnv = new Environment();
-    pkgEnv.setLoadingPhase();
 
     Package.LegacyBuilder pkgBuilder = new Package.LegacyBuilder(packageId);
 
