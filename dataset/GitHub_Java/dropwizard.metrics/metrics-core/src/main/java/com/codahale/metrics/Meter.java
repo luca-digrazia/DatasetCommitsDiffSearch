f44b89596677c5bd881cd11b83fe12e1f9b2f79d
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:29:06 +0200

    make sure that EWMA intervals are adjacent

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/codahale/metrics/Meter.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/codahale/metrics/Meter.java
@@ -1,29 +1,25 @@
 package com.codahale.metrics;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * A meter metric which measures mean throughput and one-, five-, and fifteen-minute
- * moving average throughputs.
+ * exponentially-weighted moving average throughputs.
  *
- * @see MovingAverages
+ * @see EWMA
  */
 public class Meter implements Metered {
+    private static final long TICK_INTERVAL = TimeUnit.SECONDS.toNanos(5);
 
-    private final MovingAverages movingAverages;
+    private final EWMA m1Rate = EWMA.oneMinuteEWMA();
+    private final EWMA m5Rate = EWMA.fiveMinuteEWMA();
+    private final EWMA m15Rate = EWMA.fifteenMinuteEWMA();
+
     private final LongAdder count = new LongAdder();
     private final long startTime;
+    private final AtomicLong lastTick;
     private final Clock clock;
-
-    /**
-     * Creates a new {@link Meter}.
-     *
-     * @param movingAverages the {@link MovingAverages} implementation to use
-     */
-    public Meter(MovingAverages movingAverages) {
-        this(movingAverages, Clock.defaultClock());
-    }
 
     /**
      * Creates a new {@link Meter}.
@@ -35,22 +31,12 @@ public class Meter implements Metered {
     /**
      * Creates a new {@link Meter}.
      *
-     * @param clock the clock to use for the meter ticks
+     * @param clock      the clock to use for the meter ticks
      */
     public Meter(Clock clock) {
-        this(new ExponentialMovingAverages(clock), clock);
-    }
-
-    /**
-     * Creates a new {@link Meter}.
-     *
-     * @param movingAverages the {@link MovingAverages} implementation to use
-     * @param clock          the clock to use for the meter ticks
-     */
-    public Meter(MovingAverages movingAverages, Clock clock) {
-        this.movingAverages = movingAverages;
         this.clock = clock;
         this.startTime = this.clock.getTick();
+        this.lastTick = new AtomicLong(startTime);
     }
 
     /**
@@ -66,9 +52,28 @@ public class Meter implements Metered {
      * @param n the number of events
      */
     public void mark(long n) {
-        movingAverages.tickIfNecessary();
+        tickIfNecessary();
         count.add(n);
-        movingAverages.update(n);
+        m1Rate.update(n);
+        m5Rate.update(n);
+        m15Rate.update(n);
+    }
+
+    private void tickIfNecessary() {
+        final long oldTick = lastTick.get();
+        final long newTick = clock.getTick();
+        final long age = newTick - oldTick;
+        if (age > TICK_INTERVAL) {
+            final long newIntervalStartTick = newTick - age % TICK_INTERVAL;
+            if (lastTick.compareAndSet(oldTick, newIntervalStartTick)) {
+                final long requiredTicks = age / TICK_INTERVAL;
+                for (long i = 0; i < requiredTicks; i++) {
+                    m1Rate.tick();
+                    m5Rate.tick();
+                    m15Rate.tick();
+                }
+            }
+        }
     }
 
     @Override
@@ -78,14 +83,14 @@ public class Meter implements Metered {
 
     @Override
     public double getFifteenMinuteRate() {
-        movingAverages.tickIfNecessary();
-        return movingAverages.getM15Rate();
+        tickIfNecessary();
+        return m15Rate.getRate(TimeUnit.SECONDS);
     }
 
     @Override
     public double getFiveMinuteRate() {
-        movingAverages.tickIfNecessary();
-        return movingAverages.getM5Rate();
+        tickIfNecessary();
+        return m5Rate.getRate(TimeUnit.SECONDS);
     }
 
     @Override
@@ -93,14 +98,14 @@ public class Meter implements Metered {
         if (getCount() == 0) {
             return 0.0;
         } else {
-            final double elapsed = clock.getTick() - startTime;
+            final double elapsed = (clock.getTick() - startTime);
             return getCount() / elapsed * TimeUnit.SECONDS.toNanos(1);
         }
     }
 
     @Override
     public double getOneMinuteRate() {
-        movingAverages.tickIfNecessary();
-        return movingAverages.getM1Rate();
+        tickIfNecessary();
+        return m1Rate.getRate(TimeUnit.SECONDS);
     }
 }
