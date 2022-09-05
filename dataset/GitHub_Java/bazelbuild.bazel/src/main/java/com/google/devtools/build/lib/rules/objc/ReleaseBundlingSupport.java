edc1895fc39645fb18413167d1e79dead183e7b7
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:03:13 +0200

    Propagate DEVELOPER_DIR environment variable to the apple release bundling codesign action.
        --
        MOS_MIGRATED_REVID=130530871

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/ReleaseBundlingSupport.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/objc/ReleaseBundlingSupport.java
@@ -768,7 +768,6 @@ public final class ReleaseBundlingSupport {
     Bundling.Builder bundling =
         new Builder()
             .setName(bundleName)
-            .setExecutableName(bundleName)
             // Architecture that determines which nested bundles are kept.
             .setArchitecture(appleConfiguration.getDependencySingleArchitecture())
             .setBundleDirFormat(bundleDirFormat)
