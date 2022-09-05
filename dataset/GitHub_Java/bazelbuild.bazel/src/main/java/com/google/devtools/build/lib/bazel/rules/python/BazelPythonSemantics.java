7a600584d09180ad5f86cba60db6cb38c1abb994
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:07:29 +0200

    Make python exetutable zip a real self-extracting binary
        Only by doing this, we can load shared library from packaged python
        binary.
        Also fixed some bugs in building python executable zip.
    
        --
        Change-Id: I80fb5f99cf06f78b86c3e5de410578a350d31846
        Reviewed-on: https://bazel-review.googlesource.com/#/c/4263
        MOS_MIGRATED_REVID=129726147

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/python/BazelPythonSemantics.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/bazel/rules/python/BazelPythonSemantics.java
@@ -41,6 +41,7 @@ import com.google.devtools.build.lib.rules.test.InstrumentedFilesCollector.Instr
 import com.google.devtools.build.lib.syntax.Type;
 import com.google.devtools.build.lib.util.FileTypeSet;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -104,16 +105,6 @@ public class BazelPythonSemantics implements PythonSemantics {
     return result;
   }
 
-  /** @return An artifact next to the executable file with ".zip" suffix */
-  public Artifact getPythonZipArtifact(RuleContext ruleContext, Artifact executable) {
-    return ruleContext.getRelatedArtifact(executable.getRootRelativePath(), ".zip");
-  }
-
-  /** @return An artifact next to the executable file with ".temp" suffix */
-  public Artifact getPythonTemplateMainArtifact(RuleContext ruleContext, Artifact executable) {
-    return ruleContext.getRelatedArtifact(executable.getRootRelativePath(), ".temp");
-  }
-
   @Override
   public void createExecutable(
       RuleContext ruleContext,
@@ -142,42 +133,46 @@ public class BazelPythonSemantics implements PythonSemantics {
                   Substitution.of("%main%", main),
                   Substitution.of("%python_binary%", pythonBinary),
                   Substitution.of("%imports%", Joiner.on(":").join(imports)),
-                  Substitution.of("%workspace_name%", ruleContext.getWorkspaceName()),
-                  Substitution.of("%is_zipfile%", "False")),
+                  Substitution.of("%workspace_name%", ruleContext.getWorkspaceName())),
               true));
     } else {
-      Artifact zipFile = getPythonZipArtifact(ruleContext, executable);
-      Artifact templateMain = getPythonTemplateMainArtifact(ruleContext, executable);
+      Artifact zipFile = common.getPythonZipArtifact();
       PathFragment workspaceName = getWorkspaceNameForPythonZip(ruleContext.getWorkspaceName());
       main = workspaceName.getRelative(common.determineMainExecutableSource(false)).toString();
       PathFragment defaultWorkspacename = new PathFragment(Label.DEFAULT_REPOSITORY_DIRECTORY);
-      List<PathFragment> importPaths = new ArrayList<>();
+      StringBuilder importPaths = new StringBuilder();
+      importPaths.append(File.pathSeparator).append("$PYTHON_RUNFILES/").append(workspaceName);
       for (PathFragment path : imports) {
         if (path.startsWith(defaultWorkspacename)) {
           path = new PathFragment(workspaceName, path.subFragment(1, path.segmentCount()));
         }
-        importPaths.add(path);
+        importPaths.append(File.pathSeparator).append("$PYTHON_RUNFILES/").append(path);
       }
-      // The executable zip file will unzip itself into a tmp directory and then run from there
-      ruleContext.registerAction(
-          new TemplateExpansionAction(
-              ruleContext.getActionOwner(),
-              templateMain,
-              STUB_TEMPLATE,
-              ImmutableList.of(
-                  Substitution.of("%main%", main),
-                  Substitution.of("%python_binary%", pythonBinary),
-                  Substitution.of("%imports%", Joiner.on(":").join(importPaths)),
-                  Substitution.of("%workspace_name%", ruleContext.getWorkspaceName()),
-                  Substitution.of("%is_zipfile%", "True")),
-              true));
-
+      // The executable zip file wil unzip itself into a tmp directory and then run from there
+      String zipHeader =
+          "#!/bin/sh\n"
+              + "export TMPDIR=${TMPDIR:-/tmp/Bazel}\n"
+              + "mkdir -p \"${TMPDIR}\"\n"
+              + "export PYTHON_RUNFILES=$(mktemp -d \"${TMPDIR%%/}/runfiles.XXXXXXXX\")\n"
+              + "export PYTHONPATH=\"$PYTHONPATH"
+              + importPaths
+              + "\"\n"
+              + "unzip -q -o $0 -d \"$PYTHON_RUNFILES\" 2> /dev/null\n"
+              + "retCode=0\n"
+              + pythonBinary
+              + " \"$PYTHON_RUNFILES/"
+              + main
+              + "\" $@ || retCode=$?\n"
+              + "rm -rf \"$PYTHON_RUNFILES\"\n"
+              + "exit $retCode\n";
       ruleContext.registerAction(
           new SpawnAction.Builder()
               .addInput(zipFile)
               .addOutput(executable)
               .setShellCommand(
-                  "echo '#!/usr/bin/env python' | cat - "
+                  "echo '"
+                      + zipHeader
+                      + "' | cat - "
                       + zipFile.getExecPathString()
                       + " > "
                       + executable.getExecPathString())
@@ -192,13 +187,12 @@ public class BazelPythonSemantics implements PythonSemantics {
       PyCommon common) throws InterruptedException {
     if (ruleContext.getConfiguration().buildPythonZip()) {
       FilesToRunProvider zipper = ruleContext.getExecutablePrerequisite("$zipper", Mode.HOST);
-      Artifact executable = common.getExecutable();
       if (!ruleContext.hasErrors()) {
         createPythonZipAction(
             ruleContext,
-            executable,
-            getPythonZipArtifact(ruleContext, executable),
-            getPythonTemplateMainArtifact(ruleContext, executable),
+            common.getExecutable(),
+            common.getPythonZipArtifact(),
+            common.determineMainExecutableSource(false),
             zipper,
             runfilesSupport);
       }
@@ -236,15 +230,15 @@ public class BazelPythonSemantics implements PythonSemantics {
       RuleContext ruleContext,
       Artifact executable,
       Artifact zipFile,
-      Artifact templateMain,
+      String main,
       FilesToRunProvider zipper,
       RunfilesSupport runfilesSupport) {
 
     NestedSetBuilder<Artifact> inputsBuilder = NestedSetBuilder.stableOrder();
     PathFragment workspaceName = getWorkspaceNameForPythonZip(ruleContext.getWorkspaceName());
     CustomCommandLine.Builder argv = new CustomCommandLine.Builder();
-    inputsBuilder.add(templateMain);
-    argv.add("__main__.py=" + templateMain.getExecPathString());
+
+    argv.add("__main__.py=" + main);
 
     // Creating __init__.py files under each directory
     argv.add("__init__.py=");
