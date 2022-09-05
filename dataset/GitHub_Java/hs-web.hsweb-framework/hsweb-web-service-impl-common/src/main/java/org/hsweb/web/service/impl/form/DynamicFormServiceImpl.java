46d1ef43ba4566a9dc79c1ff91d6c620ed390643
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:08:51 +0200

    å®åé¨ç½²åè½

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-impl-common/src/main/java/org/hsweb/web/service/impl/form/DynamicFormServiceImpl.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-impl-common/src/main/java/org/hsweb/web/service/impl/form/DynamicFormServiceImpl.java
@@ -1,18 +1,14 @@
 package org.hsweb.web.service.impl.form;
 
-import com.alibaba.fastjson.JSON;
-import org.hsweb.web.Install;
-import org.hsweb.web.bean.common.*;
-import org.hsweb.web.bean.po.GenericPo;
+import org.hsweb.web.bean.common.PagerResult;
+import org.hsweb.web.bean.common.QueryParam;
 import org.hsweb.web.bean.po.form.Form;
-import org.hsweb.web.bean.po.history.History;
 import org.hsweb.web.service.form.DynamicFormService;
 import org.hsweb.web.service.form.FormService;
-import org.hsweb.web.service.history.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
-import org.webbuilder.sql.*;
+import org.webbuilder.sql.DataBase;
+import org.webbuilder.sql.TableMetaData;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -37,50 +33,19 @@ public class DynamicFormServiceImpl implements DynamicFormService {
     @Resource
     protected FormService formService;
 
-    @Resource
-    protected HistoryService historyService;
-
-    protected void initDefaultField(TableMetaData metaData) {
-        String dataType;
-        switch (Install.getDatabaseType()) {
-            case "oracle":
-                dataType = "varchar2(32)";
-                break;
-            case "h2":
-                dataType = "varchar2(32)";
-                break;
-            default:
-                dataType = "varchar(32)";
-        }
-        FieldMetaData UID = new FieldMetaData("u_id", String.class, dataType);
-        UID.setPrimaryKey(true);
-        UID.setNotNull(true);
-        UID.setComment("主键");
-        metaData.attr("primaryKey", "u_id");
-        metaData.addField(UID);
-
-    }
-
     @Override
     public void deploy(Form form) throws Exception {
         Lock writeLock = lock.writeLock();
         try {
             writeLock.lock();
             TableMetaData metaData = formParser.parse(form);
-            initDefaultField(metaData);
-            History history = historyService.selectLastHistoryByType("form.deploy." + form.getName());
-            //首次部署
-            if (history == null) {
+            List<Form> oldList = formService.select(new QueryParam().where("name", form.getName()).where("version", form.getVersion() - 1));
+            Form old = null;
+            if (oldList.size() > 0) old = oldList.get(0);
+            if (old != null)
+                dataBase.updateTable(metaData);
+            else
                 dataBase.createTable(metaData);
-            } else {
-                Form lastDeploy = JSON.parseObject(history.getChange_after(), Form.class);
-                TableMetaData lastDeployMetaData = formParser.parse(lastDeploy);
-                initDefaultField(lastDeployMetaData);
-                //向上发布
-                dataBase.updateTable(lastDeployMetaData);//先放入旧的结构
-                //更新结构
-                dataBase.alterTable(metaData);
-            }
         } finally {
             writeLock.unlock();
         }
@@ -91,201 +56,53 @@ public class DynamicFormServiceImpl implements DynamicFormService {
         Lock writeLock = lock.writeLock();
         try {
             writeLock.lock();
-            dataBase.removeTable(form.getName());
         } finally {
             writeLock.unlock();
         }
     }
 
-    public Table getTableByName(String name) throws Exception {
-        Table table = dataBase.getTable(name.toUpperCase());
-        if (table == null)
-            table = dataBase.getTable(name.toLowerCase());
-        Assert.notNull(table, "表单[" + name + "]不存在");
-        return table;
-    }
-
     @Override
     public <T> PagerResult<T> selectPager(String name, QueryParam param) throws Exception {
         Lock readLock = lock.readLock();
-        PagerResult<T> result = new PagerResult<>();
         try {
             readLock.lock();
-            Table table = getTableByName(name);
-            Query query = table.createQuery();
-            QueryParamProxy proxy = QueryParamProxy.build(param);
-            int total = query.total(proxy);
-            result.setTotal(total);
-            param.rePaging(total);
-            proxy = QueryParamProxy.build(param);
-            result.setData(query.list(proxy));
+            System.out.println("执行");
+            Thread.sleep(1000);
         } finally {
             readLock.unlock();
         }
-        return result;
+        return null;
     }
 
     @Override
     public <T> List<T> select(String name, QueryParam param) throws Exception {
-        Lock readLock = lock.readLock();
-        try {
-            readLock.lock();
-            Table table = getTableByName(name);
-            Query query = table.createQuery();
-            param.setPaging(false);
-            QueryParamProxy proxy = QueryParamProxy.build(param);
-            return query.list(proxy);
-        } finally {
-            readLock.unlock();
-        }
+        return null;
     }
 
     @Override
     public int total(String name, QueryParam param) throws Exception {
-        Lock readLock = lock.readLock();
-        try {
-            readLock.lock();
-            Table table = getTableByName(name);
-            Query query = table.createQuery();
-            param.setPaging(false);
-            QueryParamProxy proxy = QueryParamProxy.build(param);
-            return query.total(proxy);
-        } finally {
-            readLock.unlock();
-        }
+        return 0;
     }
 
     @Override
-    public String insert(String name, InsertParam<Map<String, Object>> param) throws Exception {
-        Lock readLock = lock.readLock();
-        try {
-            readLock.lock();
-            Table table = getTableByName(name);
-            Insert insert = table.createInsert();
-            InsertParamProxy paramProxy = InsertParamProxy.build(param);
-            String primaryKeyName = getPrimaryKeyName(name);
-            String pk = GenericPo.createUID();
-            paramProxy.value(primaryKeyName, pk);
-            insert.insert(paramProxy);
-            return pk;
-        } finally {
-            readLock.unlock();
-        }
+    public int insert(String name, Map<String, Object> data) throws Exception {
+        return 0;
     }
 
     @Override
-    public boolean deleteByPk(String name, String pk) throws Exception {
-        Lock readLock = lock.readLock();
-        try {
-            readLock.lock();
-            String primaryKeyName = getPrimaryKeyName(name);
-            Table table = getTableByName(name);
-            Delete delete = table.createDelete();
-            return delete.delete(DeleteParamProxy.build(new DeleteParam()).where(primaryKeyName, pk)) == 1;
-        } finally {
-            readLock.unlock();
-        }
+    public int delete(String name, Map<String, Object> data) throws Exception {
+        return 0;
     }
 
     @Override
-    public int delete(String name, DeleteParam where) throws Exception {
-        Lock readLock = lock.readLock();
-        try {
-            readLock.lock();
-            Table table = getTableByName(name);
-            Delete delete = table.createDelete();
-            return delete.delete(DeleteParamProxy.build(where));
-        } finally {
-            readLock.unlock();
-        }
-    }
-
-    @Override
-    public int updateByPk(String name, String pk, UpdateParam<Map<String, Object>> param) throws Exception {
-        Lock readLock = lock.readLock();
-        try {
-            readLock.lock();
-            Table table = getTableByName(name);
-            Update update = table.createUpdate();
-            UpdateParamProxy paramProxy = UpdateParamProxy.build(param);
-            paramProxy.where(getPrimaryKeyName(name), pk);
-            return update.update(paramProxy);
-        } finally {
-            readLock.unlock();
-        }
-    }
-
-    @Override
-    public int update(String name, UpdateParam<Map<String, Object>> param) throws Exception {
-        Lock readLock = lock.readLock();
-        try {
-            readLock.lock();
-            Table table = getTableByName(name);
-            Update update = table.createUpdate();
-            UpdateParamProxy paramProxy = UpdateParamProxy.build(param);
-            return update.update(paramProxy);
-        } finally {
-            readLock.unlock();
-        }
-    }
-
-    public String getPrimaryKeyName(String tableName) throws Exception {
-        Table table = getTableByName(tableName);
-        return table.getMetaData().attrWrapper("primaryKey", "u_id").toString();
+    public int update(String name, Map<String, Object> data) throws Exception {
+        return 0;
     }
 
     @Override
     public <T> T selectByPk(String name, Object pk) throws Exception {
-        Lock readLock = lock.readLock();
-        try {
-            readLock.lock();
-            Table table = getTableByName(name);
-            Query query = table.createQuery();
-            QueryParamProxy proxy = new QueryParamProxy();
-            proxy.where(getPrimaryKeyName(name), pk);
-            return query.single(proxy);
-        } finally {
-            readLock.unlock();
-        }
+        return null;
     }
 
-    public static class QueryParamProxy extends org.webbuilder.sql.param.query.QueryParam {
-        public static QueryParamProxy build(QueryParam param) {
-            QueryParamProxy proxy = new QueryParamProxy();
-            proxy.where(param.getTerm());
-            proxy.exclude(param.getExcludes());
-            proxy.include(param.getIncludes());
-            proxy.orderBy("desc".equals(param.getSortOrder()), param.getSortField());
-            proxy.doPaging(param.getPageIndex(), param.getPageSize());
-            proxy.setPaging(param.isPaging());
-            return proxy;
-        }
-    }
 
-    public static class UpdateParamProxy extends org.webbuilder.sql.param.update.UpdateParam {
-        public static UpdateParamProxy build(UpdateParam<Map<String, Object>> param) {
-            UpdateParamProxy proxy = new UpdateParamProxy();
-            proxy.where(param.getTerm());
-            proxy.exclude(param.getExcludes());
-            proxy.include(param.getIncludes());
-            proxy.set(param.getData());
-            return proxy;
-        }
-    }
-
-    public static class InsertParamProxy extends org.webbuilder.sql.param.insert.InsertParam {
-        public static InsertParamProxy build(InsertParam<Map<String, Object>> param) {
-            InsertParamProxy proxy = new InsertParamProxy();
-            proxy.values(param.getData());
-            return proxy;
-        }
-    }
-
-    public static class DeleteParamProxy extends org.webbuilder.sql.param.delete.DeleteParam {
-        public static DeleteParamProxy build(DeleteParam param) {
-            DeleteParamProxy proxy = new DeleteParamProxy();
-            proxy.where(param.getTerm());
-            return proxy;
-        }
-    }
 }
--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-impl-common/src/main/java/org/hsweb/web/service/impl/form/FormServiceImpl.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-impl-common/src/main/java/org/hsweb/web/service/impl/form/FormServiceImpl.java
@@ -1,26 +1,20 @@
 package org.hsweb.web.service.impl.form;
 
-import com.alibaba.fastjson.JSON;
-import org.hsweb.web.bean.common.InsertParam;
-import org.hsweb.web.core.authorize.annotation.Authorize;
 import org.hsweb.web.bean.common.QueryParam;
 import org.hsweb.web.bean.common.UpdateParam;
 import org.hsweb.web.bean.po.form.Form;
-import org.hsweb.web.bean.po.history.History;
+import org.hsweb.web.dao.GenericMapper;
 import org.hsweb.web.dao.form.FormMapper;
+import org.hsweb.web.exception.BusinessException;
 import org.hsweb.web.service.form.DynamicFormService;
 import org.hsweb.web.service.form.FormService;
-import org.hsweb.web.service.history.HistoryService;
 import org.hsweb.web.service.impl.AbstractServiceImpl;
-import org.hsweb.web.core.utils.RandomUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hsweb.web.utils.RandomUtil;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
-import org.webbuilder.utils.common.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.Date;
@@ -35,14 +29,8 @@ public class FormServiceImpl extends AbstractServiceImpl<Form, String> implement
 
     private static final String CACHE_KEY = "form";
 
-    @Autowired(required = false)
-    protected FormParser formParser = new DefaultFormParser();
-
     @Resource
     private FormMapper formMapper;
-
-    @Resource
-    private HistoryService historyService;
 
     @Override
     protected FormMapper getMapper() {
@@ -53,7 +41,7 @@ public class FormServiceImpl extends AbstractServiceImpl<Form, String> implement
     protected DynamicFormService dynamicFormService;
 
     @Override
-    @Cacheable(value = CACHE_KEY, key = "'form.'+#id")
+    @Cacheable(value = CACHE_KEY, key = "#id")
     public Form selectByPk(String id) throws Exception {
         return super.selectByPk(id);
     }
@@ -61,46 +49,39 @@ public class FormServiceImpl extends AbstractServiceImpl<Form, String> implement
     @Override
     public String createNewVersion(String oldVersionId) throws Exception {
         Form old = this.selectByPk(oldVersionId);
-        Assert.notNull(old, "表单不存在!");
+        Assert.isNull(old, "表单不存在!");
         old.setU_id(RandomUtil.randomChar());
         old.setVersion(old.getVersion() + 1);
         old.setCreate_date(new Date());
         old.setUpdate_date(null);
-        old.setRevision(1);
-        old.setUsing(false);
-        getMapper().insert(new InsertParam<>(old));
         return old.getU_id();
     }
 
     @Override
     public String insert(Form data) throws Exception {
         List<Form> old = this.select(new QueryParam().where("name", data.getName()));
-        Assert.isTrue(old.isEmpty(), "表单 [" + data.getName() + "] 已存在!");
+        Assert.notNull(old, "表单 [" + data.getName() + "] 已存在!");
         data.setCreate_date(new Date());
         data.setVersion(1);
-        if (StringUtils.isNullOrEmpty(data.getU_id()))
-            data.setU_id(RandomUtil.randomChar());
         super.insert(data);
         return data.getU_id();
     }
 
     @Override
-    @CacheEvict(value = {CACHE_KEY}, key = "'form.'+#data.u_id")
+    @CacheEvict(value = CACHE_KEY, key = "#data.u_id")
     public int update(Form data) throws Exception {
         Form old = this.selectByPk(data.getU_id());
-        Assert.notNull(old, "表单不存在!");
+        Assert.isNull(old, "表单不存在!");
         data.setUpdate_date(new Date());
-        data.setRevision(old.getRevision() + 1);
-        UpdateParam<Form> param = new UpdateParam<>(data).excludes("create_date", "release", "version", "using");
-        return getMapper().update(param);
+        return super.update(data);
     }
 
     @Override
-    @CacheEvict(value = CACHE_KEY, key = "'form.'+#id")
+    @CacheEvict(value = CACHE_KEY, key = "#id")
     public int delete(String id) throws Exception {
         Form old = this.selectByPk(id);
-        Assert.notNull(old, "表单不存在!");
-        Assert.isTrue(!old.isUsing(), "表单正在使用，无法删除!");
+        Assert.isNull(old, "表单不存在!");
+        Assert.isTrue(old.isUsing(), "表单正在使用，无法删除!");
         return super.delete(id);
     }
 
@@ -117,29 +98,7 @@ public class FormServiceImpl extends AbstractServiceImpl<Form, String> implement
     }
 
     @Override
-    public Form selectByVersion(String name, int version) throws Exception {
-        QueryParam param = QueryParam.newInstance()
-                .where("name", name).where("version", version);
-        List<Form> formList = formMapper.selectLatestList(param);
-        return formList.size() > 0 ? formList.get(0) : null;
-    }
-
-    @Override
-    public Form selectLatest(String name) throws Exception {
-        QueryParam param = QueryParam.newInstance()
-                .where("name", name).orderBy("version").desc().doPaging(0, 1);
-        List<Form> formList = formMapper.selectLatestList(param);
-        return formList.size() > 0 ? formList.get(0) : null;
-    }
-
-    @Override
     @Transactional(rollbackFor = Throwable.class)
-    @Caching(evict = {
-            @CacheEvict(value = {CACHE_KEY + ".deploy"},
-                    key = "'form.deploy.'+target.selectByPk(#formId).getName()+'.html'"),
-            @CacheEvict(value = {CACHE_KEY},
-                    key = "'form.using.'+target.selectByPk(#formId).getName()")
-    })
     public void deploy(String formId) throws Exception {
         Form old = this.selectByPk(formId);
         Assert.notNull(old, "表单不存在");
@@ -151,15 +110,7 @@ public class FormServiceImpl extends AbstractServiceImpl<Form, String> implement
         //开始发布
         old.setUsing(true);
         dynamicFormService.deploy(old);
-        old.setRelease(old.getRevision());//发布修订版本
-        getMapper().update(new UpdateParam<>(old).includes("using", "release").where("u_id", old.getU_id()));
-        //加入发布历史记录
-        History history = History.newInstace("form.deploy." + old.getName());
-        history.setPrimary_key_name("u_id");
-        history.setPrimary_key_value(old.getU_id());
-        history.setChange_before("{}");
-        history.setChange_after(JSON.toJSONString(old));
-        historyService.insert(history);
+        getMapper().update(new UpdateParam<>(old).includes("using").where("u_id", old.getU_id()));
     }
 
     @Override
@@ -175,25 +126,7 @@ public class FormServiceImpl extends AbstractServiceImpl<Form, String> implement
     }
 
     @Override
-    @Cacheable(value = CACHE_KEY + ".deploy", key = "'form.deploy.'+#name+'.html'")
-    public String createDeployHtml(String name) throws Exception {
-        History history = historyService.selectLastHistoryByType("form.deploy." + name);
-        Assert.notNull(history, "表单不存在");
-        return formParser.parseHtml(JSON.parseObject(history.getChange_after(), Form.class));
+    public String createHtml(String formId) throws Exception {
+        return null;
     }
-
-    @Override
-    public String createViewHtml(String formId) throws Exception {
-        Form form = this.selectByPk(formId);
-        Assert.notNull(form, "表单不存在");
-        return formParser.parseHtml(form);
-    }
-
-    @Override
-    @Cacheable(value = CACHE_KEY, key = "'form.using.'+#name")
-    public Form selectUsing(String name) throws Exception {
-        return formMapper.selectUsing(name);
-    }
-
-
 }
