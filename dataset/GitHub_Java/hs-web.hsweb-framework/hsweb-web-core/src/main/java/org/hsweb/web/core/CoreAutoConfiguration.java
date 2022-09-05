01aac86fbab9829d40ad499012b77b99abf910f8
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:04:41 +0200

    ä¼åéç½®

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-core/src/main/java/org/hsweb/web/core/CoreAutoConfiguration.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-core/src/main/java/org/hsweb/web/core/CoreAutoConfiguration.java
@@ -1,8 +1,13 @@
 package org.hsweb.web.core;
 
+import org.hsweb.web.core.session.HttpSessionManager;
+import org.hsweb.web.core.session.siample.SimpleHttpSessionManager;
+import org.hsweb.web.core.session.siample.UserLoginOutListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
 /**
  * Created by zhouhao on 16-5-6.
  */
@@ -10,5 +15,21 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("org.hsweb.web.core")
 public class CoreAutoConfiguration {
 
+    @Bean
+    @ConditionalOnMissingBean(HttpSessionManager.class)
+    @ConditionalOnWebApplication
+    public HttpSessionManager simpleHttpSessionManager() {
+        SimpleHttpSessionManager httpSessionManager = new SimpleHttpSessionManager();
+        return httpSessionManager;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(HttpSessionManager.class)
+    @ConditionalOnWebApplication
+    public UserLoginOutListener sessionListener() {
+        UserLoginOutListener loginOutListener = new UserLoginOutListener();
+        loginOutListener.setHttpSessionManager(simpleHttpSessionManager());
+        return loginOutListener;
+    }
 
 }
