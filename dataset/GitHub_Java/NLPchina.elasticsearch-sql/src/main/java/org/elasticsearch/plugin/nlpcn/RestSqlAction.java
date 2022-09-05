70d77a22680bf21053e546eb173d4966d32b9575
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:09:57 +0200

    fix explain feature, fix bug explained in question #30

--- dataset/GitHub_Java/NLPchina.elasticsearch-sql/src/main/java/org/elasticsearch/plugin/nlpcn/RestSqlAction.java
+++ dataset/GitHub_Java/NLPchina.elasticsearch-sql/src/main/java/org/elasticsearch/plugin/nlpcn/RestSqlAction.java
@@ -10,15 +10,15 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.io.stream.BytesStreamOutput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.rest.*;
+import org.elasticsearch.rest.BaseRestHandler;
+import org.elasticsearch.rest.BytesRestResponse;
+import org.elasticsearch.rest.RestChannel;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.rest.action.support.RestBuilderListener;
 import org.elasticsearch.rest.action.support.RestStatusToXContentListener;
-import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
-import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.bucket.terms.Terms;
-import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
 import org.nlpcn.es4sql.SearchDao;
-import org.nlpcn.es4sql.query.explain.ExplainManager;
 
 import java.io.FileOutputStream;
 
@@ -35,74 +35,23 @@ public class RestSqlAction extends BaseRestHandler {
 
 	@Override
 	protected void handleRequest(RestRequest request, RestChannel channel, final Client client) throws Exception {
-		final String aggsStart = "<aggs>";
-		final String aggsEnd = "</aggs>";
-
 
 		String sql = request.param("sql");
-		String aggs = request.param("aggs");
 
 		if (sql == null) {
 			sql = request.content().toUtf8();
-			int startIndex = sql.indexOf(aggsStart);
-			int endIndex = sql.indexOf(aggsEnd);
-
-			// If <aggs></aggs> is properly included in the Post Body
-			if (startIndex >= 0 && endIndex >= 0) {
-				// Grab the the aggs element
-				String aggElement = sql.substring(startIndex, endIndex + aggsEnd.length());
-				// Remove it from the sql param (body)
-				sql = sql.replaceAll(aggElement, "");
-				// Assign aggs to the value of the element with the the tags
-				aggs = aggElement.substring(aggsStart.length(), aggElement.length() - aggsEnd.length());
-			}
 		}
 
 		SearchDao searchDao = new SearchDao(client);
 		ActionRequestBuilder actionRequestBuilder = searchDao.explain(sql);
-
 		ActionRequest actionRequest = actionRequestBuilder.request();
-
-		// Add our aggregations to the action request builder if necessary
-		handleAggs(aggs, actionRequestBuilder);
 
 		// TODO add unittests to explain. (rest level?)
 		if (request.path().endsWith("/_explain")) {
-			String jsonExplanation = ExplainManager.explain(actionRequestBuilder);
-			BytesRestResponse bytesRestResponse = new BytesRestResponse(RestStatus.OK, jsonExplanation);
+			BytesRestResponse bytesRestResponse = new BytesRestResponse(RestStatus.OK, actionRequestBuilder.toString());
 			channel.sendResponse(bytesRestResponse);
 		} else {
 			new ActionRequestExecuter(actionRequest, channel, client).execute();
-		}
-	}
-
-	private void handleAggs(String aggs, ActionRequestBuilder actionRequestBuilder) {
-		// If the aggs parameter isn't null or empty
-		if (aggs != null && aggs.trim().length() > 0) {
-			// Get a reference to Action Request Builder
-			SearchRequestBuilder b = (SearchRequestBuilder) actionRequestBuilder;
-
-			// Split the comma delimited field names
-			String[] arAggs = aggs.split(",");
-			for (String strAgg : arAggs) {
-				strAgg = strAgg.trim();
-
-				// Split again on colons for sub aggregates
-				String[] arSubAggs = strAgg.split(":");
-				TermsBuilder objOriginalTerms = null;
-				for (String strSubAgg: arSubAggs) {
-					if (objOriginalTerms == null) {
-						objOriginalTerms = AggregationBuilders.terms(strSubAgg).field(strSubAgg).size(0).order(Terms.Order.term(true));
-						// Add our aggregation to the actionRequestBuilder
-						b.addAggregation(objOriginalTerms);
-					}
-					else {
-						TermsBuilder objSubTerms = AggregationBuilders.terms(strSubAgg).field(strSubAgg).size(0).order(Terms.Order.term(true));
-						objOriginalTerms.subAggregation(objSubTerms);
-						objOriginalTerms = objSubTerms;
-					}
-				}
-			}
 		}
 	}
 }
