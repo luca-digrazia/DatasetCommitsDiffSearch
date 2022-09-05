7d75e290220482f1591546924cf8aed4631920bc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:45:47 +0200

    Formatted and styled WebappMetricsFilter.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-web/src/main/java/com/yammer/metrics/web/WebappMetricsFilter.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-web/src/main/java/com/yammer/metrics/web/WebappMetricsFilter.java
@@ -19,6 +19,7 @@ import java.util.concurrent.TimeUnit;
  * codes being returned.
  */
 public abstract class WebappMetricsFilter implements Filter {
+
     private final Map<Integer, MeterMetric> metersByStatusCode;
     private final MeterMetric otherMeterMetric;
     private final CounterMetric activeRequests;
