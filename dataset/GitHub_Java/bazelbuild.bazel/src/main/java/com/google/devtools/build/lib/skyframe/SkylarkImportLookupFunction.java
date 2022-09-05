8e142d87a9d8f958dee8a0f7bf1a1a1255dd0473
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:45:41 +0200

    Exit with an error message, rather than crashing, when an attempt is made to load a Skylark file not contained in a package.
        --
        MOS_MIGRATED_REVID=107811126

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/SkylarkImportLookupFunction.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/skyframe/SkylarkImportLookupFunction.java
@@ -210,13 +210,10 @@ public class SkylarkImportLookupFunction implements SkyFunction {
   /** Computes the Label corresponding to a relative import path. */
   private static Label labelForRelativeImport(PathFragment importPath, Label containingFileLabel)
       throws SkylarkImportFailedException {
-    // The twistiness of the code below is due to the fact that the containing file may be in
-    // a subdirectory of the package that contains it. We need to construct a Label with
-    // the import file in the same subdirectory.
     PackageIdentifier pkgIdForImport = containingFileLabel.getPackageIdentifier();
-    PathFragment containingDirInPkg =
+    PathFragment containingDir =
         (new PathFragment(containingFileLabel.getName())).getParentDirectory();
-    String targetNameForImport = containingDirInPkg.getRelative(importPath).toString();
+    String targetNameForImport = importPath.getRelative(containingDir).toString();
     try {
       return Label.create(pkgIdForImport, targetNameForImport);
     } catch (LabelSyntaxException e) {
