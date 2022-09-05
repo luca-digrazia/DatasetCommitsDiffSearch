760aa7ed77e4715898b8034ccb6d3d5001de2ce3
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:57:18 +0200

    Improve AutoAdaptStrategy

--- dataset/GitHub_Java/JessYanCoding.AndroidAutoSize/autosize/src/main/java/me/jessyan/autosize/AutoAdaptStrategy.java
+++ dataset/GitHub_Java/JessYanCoding.AndroidAutoSize/autosize/src/main/java/me/jessyan/autosize/AutoAdaptStrategy.java
@@ -17,6 +17,7 @@ package me.jessyan.autosize;
 
 import android.app.Activity;
 import android.app.Application;
+import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
 
 /**
@@ -35,7 +36,7 @@ public interface AutoAdaptStrategy {
     /**
      * 开始执行屏幕适配逻辑
      *
-     * @param target   需要屏幕适配的对象 (可能是 {@link Activity} 或者 Fragment)
+     * @param target   需要屏幕适配的对象 (可能是 {@link Activity} 或者 {@link Fragment})
      * @param activity 需要拿到当前的 {@link Activity} 才能修改 {@link DisplayMetrics#density}
      */
     void applyAdapt(Object target, Activity activity);
