b59bb5bf4524fbe454af564ab4f99cd61fe2a769
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:43:46 +0200

    ä¼åclientéè¯¯å¤æ­

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-oauth2/hsweb-authorization-oauth2-client/src/main/java/org/hswebframework/web/authorization/oauth2/client/simple/provider/HswebResponseJudgeSupport.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-authorization/hsweb-authorization-oauth2/hsweb-authorization-oauth2-client/src/main/java/org/hswebframework/web/authorization/oauth2/client/simple/provider/HswebResponseJudgeSupport.java
@@ -28,7 +28,6 @@ import org.springframework.stereotype.Component;
 /**
  * @author zhouhao
  */
-@Component
 public class HswebResponseJudgeSupport implements ResponseJudgeForProviderDefinition {
 
     @Override
@@ -42,19 +41,27 @@ public class HswebResponseJudgeSupport implements ResponseJudgeForProviderDefini
         if (result == null) {
             return ErrorType.OTHER;
         }
-        JSONObject jsonRes = JSON.parseObject(result);
-        Integer status = jsonRes.getInteger("status");
-        if (status == null && response.status() == 200) {
+        if (!result.trim().startsWith("{")) {
             return null;
         }
-        if (status != null) {
-            if (status == 200) {
+        try {
+            JSONObject jsonRes = JSON.parseObject(result);
+            if (jsonRes.size() > 5) return null;
+            Integer status = jsonRes.getInteger("status");
+            if (status == null && response.status() == 200) {
                 return null;
             }
-            return ErrorType.fromCode(status).orElse(ErrorType.OTHER);
-        }
-        if (jsonRes.get("message") != null) {
-            return ErrorType.valueOf(jsonRes.getString("message"));
+            if (status != null) {
+                if (status == 200) {
+                    return null;
+                }
+                return ErrorType.fromCode(status).orElse(ErrorType.OTHER);
+            }
+            if (jsonRes.get("message") != null) {
+                return ErrorType.valueOf(jsonRes.getString("message"));
+            }
+        } catch (Exception ignore) {
+
         }
         return null;
     }
