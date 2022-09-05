a8c1b8e474b249a346f2e868d22f9bdd546373ca
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:12:06 +0200

    Fix ExponentiallyDecayingReservoir constant rescaling (#1654)
        This fixes a bug in `ExponentiallyDecayingReservoir` in which the hour prior to the system precise clock (`System.nanotime()`) rolling from `Long.MAX_VALUE` to `Long.MIN_VALUE` caused every interaction with the reservoir to rescale.
    
        It's easy to forget that the tick value is allowed to overflow.
    
        In heavily loaded systems, this results in substantial performance degradation due to write lock acquisition for each update.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/codahale/metrics/ExponentiallyDecayingReservoir.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/codahale/metrics/ExponentiallyDecayingReservoir.java
@@ -2,6 +2,7 @@ package com.codahale.metrics;
 
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -9,27 +10,29 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 import static java.lang.Math.exp;
 import static java.lang.Math.min;
 
+import com.codahale.metrics.WeightedSnapshot.WeightedSample;
+
 /**
  * An exponentially-decaying random reservoir of {@code long}s. Uses Cormode et al's
  * forward-decaying priority reservoir sampling method to produce a statistically representative
  * sampling reservoir, exponentially biased towards newer entries.
  *
- * @see <a href="http://www.research.att.com/people/Cormode_Graham/library/publications/CormodeShkapenyukSrivastavaXu09.pdf">
- *      Cormode et al. Forward Decay: A Practical Time Decay Model for Streaming Systems. ICDE '09:
- *      Proceedings of the 2009 IEEE International Conference on Data Engineering (2009)</a>
+ * @see <a href="http://dimacs.rutgers.edu/~graham/pubs/papers/fwddecay.pdf">
+ * Cormode et al. Forward Decay: A Practical Time Decay Model for Streaming Systems. ICDE '09:
+ * Proceedings of the 2009 IEEE International Conference on Data Engineering (2009)</a>
  */
 public class ExponentiallyDecayingReservoir implements Reservoir {
     private static final int DEFAULT_SIZE = 1028;
     private static final double DEFAULT_ALPHA = 0.015;
     private static final long RESCALE_THRESHOLD = TimeUnit.HOURS.toNanos(1);
 
-    private final ConcurrentSkipListMap<Double, Long> values;
+    private final ConcurrentSkipListMap<Double, WeightedSample> values;
     private final ReentrantReadWriteLock lock;
     private final double alpha;
     private final int size;
     private final AtomicLong count;
     private volatile long startTime;
-    private final AtomicLong nextScaleTime;
+    private final AtomicLong lastScaleTick;
     private final Clock clock;
 
     /**
@@ -58,16 +61,17 @@ public class ExponentiallyDecayingReservoir implements Reservoir {
      * @param size  the number of samples to keep in the sampling reservoir
      * @param alpha the exponential decay factor; the higher this is, the more biased the reservoir
      *              will be towards newer values
+     * @param clock the clock used to timestamp samples and track rescaling
      */
     public ExponentiallyDecayingReservoir(int size, double alpha, Clock clock) {
-        this.values = new ConcurrentSkipListMap<Double, Long>();
+        this.values = new ConcurrentSkipListMap<>();
         this.lock = new ReentrantReadWriteLock();
         this.alpha = alpha;
         this.size = size;
         this.clock = clock;
         this.count = new AtomicLong(0);
         this.startTime = currentTimeInSeconds();
-        this.nextScaleTime = new AtomicLong(clock.getTick() + RESCALE_THRESHOLD);
+        this.lastScaleTick = new AtomicLong(clock.getTick());
     }
 
     @Override
@@ -90,14 +94,16 @@ public class ExponentiallyDecayingReservoir implements Reservoir {
         rescaleIfNeeded();
         lockForRegularUsage();
         try {
-            final double priority = weight(timestamp - startTime) / ThreadLocalRandom.current()
-                                                                                     .nextDouble();
+            final double itemWeight = weight(timestamp - startTime);
+            final WeightedSample sample = new WeightedSample(value, itemWeight);
+            final double priority = itemWeight / ThreadLocalRandom.current().nextDouble();
+
             final long newCount = count.incrementAndGet();
-            if (newCount <= size) {
-                values.put(priority, value);
+            if (newCount <= size || values.isEmpty()) {
+                values.put(priority, sample);
             } else {
                 Double first = values.firstKey();
-                if (first < priority && values.putIfAbsent(priority, value) == null) {
+                if (first < priority && values.putIfAbsent(priority, sample) == null) {
                     // ensure we always remove an item
                     while (values.remove(first) == null) {
                         first = values.firstKey();
@@ -111,17 +117,18 @@ public class ExponentiallyDecayingReservoir implements Reservoir {
 
     private void rescaleIfNeeded() {
         final long now = clock.getTick();
-        final long next = nextScaleTime.get();
-        if (now >= next) {
-            rescale(now, next);
+        final long lastScaleTickSnapshot = lastScaleTick.get();
+        if (now - lastScaleTickSnapshot >= RESCALE_THRESHOLD) {
+            rescale(now, lastScaleTickSnapshot);
         }
     }
 
     @Override
     public Snapshot getSnapshot() {
+        rescaleIfNeeded();
         lockForRegularUsage();
         try {
-            return new Snapshot(values.values());
+            return new WeightedSnapshot(values.values());
         } finally {
             unlockForRegularUsage();
         }
@@ -153,23 +160,32 @@ public class ExponentiallyDecayingReservoir implements Reservoir {
      * landmark L′ (and then use this new L′ at query time). This can be done with
      * a linear pass over whatever data structure is being used."
      */
-    private void rescale(long now, long next) {
-        if (nextScaleTime.compareAndSet(next, now + RESCALE_THRESHOLD)) {
-            lockForRescale();
-            try {
+    private void rescale(long now, long lastTick) {
+        lockForRescale();
+        try {
+            if (lastScaleTick.compareAndSet(lastTick, now)) {
                 final long oldStartTime = startTime;
                 this.startTime = currentTimeInSeconds();
-                final ArrayList<Double> keys = new ArrayList<Double>(values.keySet());
-                for (Double key : keys) {
-                    final Long value = values.remove(key);
-                    values.put(key * exp(-alpha * (startTime - oldStartTime)), value);
+                final double scalingFactor = exp(-alpha * (startTime - oldStartTime));
+                if (Double.compare(scalingFactor, 0) == 0) {
+                    values.clear();
+                } else {
+                    final ArrayList<Double> keys = new ArrayList<>(values.keySet());
+                    for (Double key : keys) {
+                        final WeightedSample sample = values.remove(key);
+                        final WeightedSample newSample = new WeightedSample(sample.value, sample.weight * scalingFactor);
+                        if (Double.compare(newSample.weight, 0) == 0) {
+                            continue;
+                        }
+                        values.put(key * scalingFactor, newSample);
+                    }
                 }
 
                 // make sure the counter is in sync with the number of stored samples.
                 count.set(values.size());
-            } finally {
-                unlockForRescale();
             }
+        } finally {
+            unlockForRescale();
         }
     }
 
