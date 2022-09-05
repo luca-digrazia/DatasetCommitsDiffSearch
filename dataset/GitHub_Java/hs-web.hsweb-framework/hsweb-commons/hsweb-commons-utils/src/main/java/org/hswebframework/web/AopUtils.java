1be1c06ffe775af3c39cd24e654b623e6b2d3beb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 07:36:02 +0200

    ä¿®å¤æ æ³è·åéåäºæ¥å£defaultæ¹æ³ä¸æ³¨è§£çé®é¢

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-utils/src/main/java/org/hswebframework/web/AopUtils.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-commons/hsweb-commons-utils/src/main/java/org/hswebframework/web/AopUtils.java
@@ -18,27 +18,93 @@
 package org.hswebframework.web;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Stream;
 
-public class AopUtils {
+public final class AopUtils {
 
-    public static final String getMethodBody(JoinPoint pjp) {
+    private AopUtils() {
+    }
+
+    public static <T extends Annotation> T findMethodAnnotation(Class targetClass, Method method, Class<T> annClass) {
+        Method m = method;
+        T a = AnnotationUtils.findAnnotation(m, annClass);
+        if (a != null) {
+            return a;
+        }
+        m = ClassUtils.getMostSpecificMethod(m, targetClass);
+        a = AnnotationUtils.findAnnotation(m, annClass);
+        if (a == null) {
+            List<Class> supers = new ArrayList<>();
+            supers.addAll(Arrays.asList(targetClass.getInterfaces()));
+            if (targetClass.getSuperclass() != Object.class) {
+                supers.add(targetClass.getSuperclass());
+            }
+
+            for (Class aClass : supers) {
+                if(aClass==null){
+                    continue;
+                }
+                Method ims[] = new Method[1];
+
+                ReflectionUtils.doWithMethods(aClass, im -> {
+                    if (im.getName().equals(method.getName()) && im.getParameterCount() == method.getParameterCount()) {
+                        ims[0] = im;
+                    }
+                });
+
+                if (ims[0] != null) {
+                    a = findMethodAnnotation(aClass, ims[0], annClass);
+                    if (a != null) {
+                        return a;
+                    }
+                }
+            }
+        }
+        return a;
+    }
+
+    public static <T extends Annotation> T findAnnotation(Class targetClass, Class<T> annClass) {
+        return AnnotationUtils.findAnnotation(targetClass, annClass);
+    }
+
+    public static <T extends Annotation> T findAnnotation(Class targetClass, Method method, Class<T> annClass) {
+        T a = findMethodAnnotation(targetClass, method, annClass);
+        if (a != null) {
+            return a;
+        }
+        return findAnnotation(targetClass, annClass);
+    }
+
+    public static <T extends Annotation> T findAnnotation(JoinPoint pjp, Class<T> annClass) {
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Method m = signature.getMethod();
+        Class<?> targetClass = pjp.getTarget().getClass();
+        return findAnnotation(targetClass, m, annClass);
+    }
+
+    public static String getMethodBody(JoinPoint pjp) {
         StringBuilder methodName = new StringBuilder(pjp.getSignature().getName()).append("(");
         MethodSignature signature = (MethodSignature) pjp.getSignature();
         String[] names = signature.getParameterNames();
         Class[] args = signature.getParameterTypes();
         for (int i = 0, len = args.length; i < len; i++) {
-            if (i != 0) methodName.append(",");
+            if (i != 0) {
+                methodName.append(",");
+            }
             methodName.append(args[i].getSimpleName()).append(" ").append(names[i]);
         }
         return methodName.append(")").toString();
     }
 
-    public static final Map<String, Object> getArgsMap(JoinPoint pjp) {
+    public static Map<String, Object> getArgsMap(JoinPoint pjp) {
         MethodSignature signature = (MethodSignature) pjp.getSignature();
         Map<String, Object> args = new LinkedHashMap<>();
         String names[] = signature.getParameterNames();
