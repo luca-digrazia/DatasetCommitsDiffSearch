7432839779878740bc34d3a3dfcb7a2d8f1da624
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:10:01 +0200

    ä¼åæ¥å£æ¹æ³

--- dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-interface/src/main/java/org/hsweb/web/service/config/ConfigService.java
+++ dataset/GitHub_Java/hs-web.hsweb-framework/hsweb-web-service-interface/src/main/java/org/hsweb/web/service/config/ConfigService.java
@@ -3,7 +3,6 @@ package org.hsweb.web.service.config;
 import org.hsweb.web.bean.po.config.Config;
 import org.hsweb.web.service.GenericService;
 
-import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -16,16 +15,18 @@ public interface ConfigService extends GenericService<Config, String> {
      *
      * @param name 配置名称
      * @return 配置内容
+     * @throws Exception 异常信息
      */
-    String getContent(String name);
+    String getContent(String name) throws Exception;
 
     /**
-     * 根据配置名称，获取配置内容，并解析为map格式
+     * 根据配置名称，获取配置内容，并解析为Properties格式
      *
      * @param name 配置名称
      * @return 配置内容
+     * @throws Exception 异常信息
      */
-    Map<Object, Object> get(String name);
+    Properties get(String name) throws Exception;
 
     /**
      * 获取配置中指定key的值
@@ -33,9 +34,9 @@ public interface ConfigService extends GenericService<Config, String> {
      * @param name 配置名称
      * @param key  key 异常信息
      * @return 指定的key对应的value
-     * @
+     * @throws Exception
      */
-    String get(String name, String key);
+    String get(String name, String key) throws Exception;
 
     /**
      * 获取配置中指定key的值，并指定一个默认值，如果对应的key未获取到，则返回默认值
@@ -51,17 +52,17 @@ public interface ConfigService extends GenericService<Config, String> {
     /**
      * 参照 {@link ConfigService#get(String, String)}，将值转为int类型
      */
-    int getInt(String name, String key);
+    int getInt(String name, String key) throws Exception;
 
     /**
      * 参照 {@link ConfigService#get(String, String)}，将值转为double类型
      */
-    double getDouble(String name, String key);
+    double getDouble(String name, String key) throws Exception;
 
     /**
      * 参照 {@link ConfigService#get(String, String)}，将值转为long类型
      */
-    long getLong(String name, String key);
+    long getLong(String name, String key) throws Exception;
 
     /**
      * 参照 {@link ConfigService#get(String, String, String)}，将值转为int类型
