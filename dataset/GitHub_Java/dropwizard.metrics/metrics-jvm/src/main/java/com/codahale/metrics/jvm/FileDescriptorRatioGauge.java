52555f3577d8b5325ac11f185eafc12a4890dbc9
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:12:41 +0200

    Fix NoClassDefFoundError with missing UnixOperatingSystemMXBean (#1562)
        Fixes #1519

--- dataset/GitHub_Java/dropwizard.metrics/metrics-jvm/src/main/java/com/codahale/metrics/jvm/FileDescriptorRatioGauge.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-jvm/src/main/java/com/codahale/metrics/jvm/FileDescriptorRatioGauge.java
@@ -4,14 +4,23 @@ import com.codahale.metrics.RatioGauge;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 /**
  * A gauge for the ratio of used to total file descriptors.
  */
 public class FileDescriptorRatioGauge extends RatioGauge {
+    private static boolean unixOperatingSystemMXBeanExists = false;
+
     private final OperatingSystemMXBean os;
+
+    static {
+        try {
+            Class.forName("com.sun.management.UnixOperatingSystemMXBean");
+            unixOperatingSystemMXBeanExists = true;
+        } catch (ClassNotFoundException e) {
+            // do nothing
+        }
+    }
 
     /**
      * Creates a new gauge using the platform OS bean.
@@ -23,7 +32,7 @@ public class FileDescriptorRatioGauge extends RatioGauge {
     /**
      * Creates a new gauge using the given OS bean.
      *
-     * @param os    an {@link OperatingSystemMXBean}
+     * @param os an {@link OperatingSystemMXBean}
      */
     public FileDescriptorRatioGauge(OperatingSystemMXBean os) {
         this.os = os;
@@ -31,21 +40,11 @@ public class FileDescriptorRatioGauge extends RatioGauge {
 
     @Override
     protected Ratio getRatio() {
-        try {
-            return Ratio.of(invoke("getOpenFileDescriptorCount"),
-                            invoke("getMaxFileDescriptorCount"));
-        } catch (NoSuchMethodException e) {
-            return Ratio.of(Double.NaN, Double.NaN);
-        } catch (IllegalAccessException e) {
-            return Ratio.of(Double.NaN, Double.NaN);
-        } catch (InvocationTargetException e) {
+        if (unixOperatingSystemMXBeanExists && os instanceof com.sun.management.UnixOperatingSystemMXBean) {
+            final com.sun.management.UnixOperatingSystemMXBean unixOs = (com.sun.management.UnixOperatingSystemMXBean) os;
+            return Ratio.of(unixOs.getOpenFileDescriptorCount(), unixOs.getMaxFileDescriptorCount());
+        } else {
             return Ratio.of(Double.NaN, Double.NaN);
         }
-    }
-
-    private long invoke(String name) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        final Method method = os.getClass().getDeclaredMethod(name);
-        method.setAccessible(true);
-        return (Long) method.invoke(os);
     }
 }
