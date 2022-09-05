4f46ede41bc0eb5dcbc5f7a027e11af3049ab5fb
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 03:10:48 +0200

    If the toolchain defines an action_config for each type of CppLinkAction,     hardcoded action_configs are not used.
        --
        MOS_MIGRATED_REVID=128695960

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppConfiguration.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/rules/cpp/CppConfiguration.java
@@ -38,6 +38,7 @@ import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
 import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.events.EventHandler;
 import com.google.devtools.build.lib.rules.cpp.CppConfigurationLoader.CppConfigurationParameters;
+import com.google.devtools.build.lib.rules.cpp.CppLinkActionConfigs.CppLinkPlatform;
 import com.google.devtools.build.lib.rules.cpp.Link.LinkTargetType;
 import com.google.devtools.build.lib.skylarkinterface.SkylarkCallable;
 import com.google.devtools.build.lib.skylarkinterface.SkylarkModule;
@@ -53,7 +54,6 @@ import com.google.devtools.build.lib.view.config.crosstool.CrosstoolConfig.LipoM
 import com.google.devtools.common.options.OptionsParsingException;
 import com.google.protobuf.TextFormat;
 import com.google.protobuf.TextFormat.ParseException;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.EnumSet;
@@ -683,6 +683,7 @@ public class CppConfiguration extends BuildConfiguration.Fragment {
 
     return result.build();
   }
+  
   private boolean linkActionsAreConfigured(CToolchain toolchain) {
     for (LinkTargetType type : LinkTargetType.values()) {
       boolean typeIsConfigured = false;
@@ -713,6 +714,17 @@ public class CppConfiguration extends BuildConfiguration.Fragment {
       return toolchain;
     }
     try {
+      if (!linkActionsAreConfigured(toolchain)) {
+        if (getTargetLibc().equals("macosx")) {
+          TextFormat.merge(
+              CppLinkActionConfigs.getCppLinkActionConfigs(CppLinkPlatform.MAC), toolchainBuilder);
+        } else {
+          TextFormat.merge(
+              CppLinkActionConfigs.getCppLinkActionConfigs(CppLinkPlatform.LINUX),
+              toolchainBuilder);
+        }
+      }
+
       if (!features.contains("dependency_file")) {
         // Gcc options:
         //  -MD turns on .d file output as a side-effect (doesn't imply -E)
@@ -857,15 +869,14 @@ public class CppConfiguration extends BuildConfiguration.Fragment {
                 + "  flag_set {"
                 + "    action: 'c-compile'"
                 + "    action: 'c++-compile'"
-                + "    action: 'c++-link-static-library'"
-                + "    action: 'c++-link-pic-static-library'"
                 + "    action: 'c++-link-interface-dynamic-library'"
                 + "    action: 'c++-link-dynamic-library'"
-                + "    action: 'c++-link-alwayslink-static-library'"
-                + "    action: 'c++-link-alwayslink-pic-static-library'"
                 + "    action: 'c++-link-executable'"
                 + "    flag_group {"
-                + "      flag: '-fprofile-generate=%{fdo_instrument_path}'"
+                + "      flag: '-Xgcc-only=-fprofile-generate=%{fdo_instrument_path}'"
+                + "      flag: '-Xclang-only=-fprofile-instr-generate=%{fdo_instrument_path}'"
+                + "    }"
+                + "    flag_group {"
                 + "      flag: '-fno-data-sections'"
                 + "    }"
                 + "  }"
@@ -883,7 +894,8 @@ public class CppConfiguration extends BuildConfiguration.Fragment {
                 + "    action: 'c++-compile'"
                 + "    expand_if_all_available: 'fdo_profile_path'"
                 + "    flag_group {"
-                + "      flag: '-fprofile-use=%{fdo_profile_path}'"
+                + "      flag: '-Xgcc-only=-fprofile-use=%{fdo_profile_path}'"
+                + "      flag: '-Xclang-only=-fprofile-instr-use=%{fdo_profile_path}'"
                 + "      flag: '-Xclang-only=-Wno-profile-instr-unprofiled'"
                 + "      flag: '-Xclang-only=-Wno-profile-instr-out-of-date'"
                 + "      flag: '-fprofile-correction'"
@@ -948,12 +960,8 @@ public class CppConfiguration extends BuildConfiguration.Fragment {
                 + "    }"
                 + "  }"
                 + "  flag_set {"
-                + "    action: 'c++-link-static-library'"
-                + "    action: 'c++-link-pic-static-library'"
                 + "    action: 'c++-link-interface-dynamic-library'"
                 + "    action: 'c++-link-dynamic-library'"
-                + "    action: 'c++-link-always-link-static-library'"
-                + "    action: 'c++-link-always-link-pic-static-library'"
                 + "    action: 'c++-link-executable'"
                 + "    flag_group {"
                 + "      flag: '-lgcov'"
