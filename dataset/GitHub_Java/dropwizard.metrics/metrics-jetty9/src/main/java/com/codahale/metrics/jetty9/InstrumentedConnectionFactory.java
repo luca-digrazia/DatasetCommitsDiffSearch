978bc86ca18fb0f4cc04a5e201ec3eafe8b0a58f
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:14:30 +0200

    Add counter for active connections (#1291)

--- dataset/GitHub_Java/dropwizard.metrics/metrics-jetty9/src/main/java/com/codahale/metrics/jetty9/InstrumentedConnectionFactory.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-jetty9/src/main/java/com/codahale/metrics/jetty9/InstrumentedConnectionFactory.java
@@ -1,23 +1,39 @@
 package com.codahale.metrics.jetty9;
 
+import com.codahale.metrics.Counter;
 import com.codahale.metrics.Timer;
 import org.eclipse.jetty.io.Connection;
 import org.eclipse.jetty.io.EndPoint;
 import org.eclipse.jetty.server.ConnectionFactory;
 import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.util.component.ContainerLifeCycle;
 
-public class InstrumentedConnectionFactory implements ConnectionFactory {
+import java.util.List;
+
+public class InstrumentedConnectionFactory extends ContainerLifeCycle implements ConnectionFactory {
     private final ConnectionFactory connectionFactory;
     private final Timer timer;
+    private final Counter counter;
 
     public InstrumentedConnectionFactory(ConnectionFactory connectionFactory, Timer timer) {
+        this(connectionFactory, timer, null);
+    }
+
+    public InstrumentedConnectionFactory(ConnectionFactory connectionFactory, Timer timer, Counter counter) {
         this.connectionFactory = connectionFactory;
         this.timer = timer;
+        this.counter = counter;
+        addBean(connectionFactory);
     }
 
     @Override
     public String getProtocol() {
         return connectionFactory.getProtocol();
+    }
+
+    @Override
+    public List<String> getProtocols() {
+        return connectionFactory.getProtocols();
     }
 
     @Override
@@ -29,11 +45,17 @@ public class InstrumentedConnectionFactory implements ConnectionFactory {
             @Override
             public void onOpened(Connection connection) {
                 this.context = timer.time();
+                if (counter != null) {
+                    counter.inc();
+                }
             }
 
             @Override
             public void onClosed(Connection connection) {
                 context.stop();
+                if (counter != null) {
+                    counter.dec();
+                }
             }
         });
         return connection;
