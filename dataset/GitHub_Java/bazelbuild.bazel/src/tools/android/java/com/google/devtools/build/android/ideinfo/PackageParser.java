fbd4a2730487063211cb731bedae7eaf8874d622
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:34:15 +0200

    Write package proto even if it's empty.
        Rules that parse to nothing won't produce an output, which blaze isn't
        happy about.
    
        --
        MOS_MIGRATED_REVID=110264213

--- dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/ideinfo/PackageParser.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/tools/android/java/com/google/devtools/build/android/ideinfo/PackageParser.java
@@ -23,18 +23,17 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.devtools.build.android.Converters.PathConverter;
 import com.google.devtools.build.android.Converters.PathListConverter;
-import com.google.devtools.build.lib.ideinfo.androidstudio.PackageManifestOuterClass.ArtifactLocation;
 import com.google.devtools.build.lib.ideinfo.androidstudio.PackageManifestOuterClass.JavaSourcePackage;
 import com.google.devtools.build.lib.ideinfo.androidstudio.PackageManifestOuterClass.PackageManifest;
 import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionsBase;
 import com.google.devtools.common.options.OptionsParser;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -44,6 +43,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -54,20 +54,13 @@ public class PackageParser {
 
   /** The options for a {@PackageParser} action. */
   public static final class PackageParserOptions extends OptionsBase {
-    @Option(name = "sources",
+    @Option(name = "sources_absolute_paths",
         defaultValue = "null",
-        converter = ArtifactLocationListConverter.class,
+        converter = PathListConverter.class,
         category = "input",
-        help = "The locations of the java source files. The expected format is a "
-            + "colon-separated list.")
-    public List<ArtifactLocation> sources;
-
-    @Option(name = "output_manifest",
-        defaultValue = "null",
-        converter = PathConverter.class,
-        category = "output",
-        help = "The path to the manifest file this parser writes to.")
-    public Path outputManifest;
+        help = "The absolute paths of the java source files. The expected format is a "
+               + "colon-separated list.")
+    public List<Path> sourcesAbsolutePaths;
 
     @Option(name = "sources_execution_paths",
         defaultValue = "null",
@@ -76,31 +69,38 @@ public class PackageParser {
         help = "The execution paths of the java source files. The expected format is a "
             + "colon-separated list.")
     public List<Path> sourcesExecutionPaths;
+
+    @Option(name = "output_manifest",
+        defaultValue = "null",
+        converter = PathConverter.class,
+        category = "output",
+        help = "The path to the manifest file this parser writes to.")
+    public Path outputManifest;
   }
 
   private static final Logger logger = Logger.getLogger(PackageParser.class.getName());
 
-  private static final Pattern PACKAGE_PATTERN =
-      Pattern.compile("^\\s*package\\s+([\\w\\.]+)");
+  private static final Pattern JAVA_PACKAGE_PATTERN =
+      Pattern.compile("^\\s*package\\s+([\\w\\.]+);");
 
   public static void main(String[] args) throws Exception {
     PackageParserOptions options = parseArgs(args);
+    Preconditions.checkNotNull(options.sourcesAbsolutePaths);
+    Preconditions.checkNotNull(options.sourcesExecutionPaths);
+    Preconditions.checkState(
+        options.sourcesAbsolutePaths.size() == options.sourcesExecutionPaths.size());
     Preconditions.checkNotNull(options.outputManifest);
 
     try {
       PackageParser parser = new PackageParser(PackageParserIoProvider.INSTANCE);
-      Map<ArtifactLocation, String> outputMap = parser.parsePackageStrings(options.sources);
+      Map<Path, String> outputMap = parser.parsePackageStrings(options.sourcesAbsolutePaths,
+          options.sourcesExecutionPaths);
       parser.writeManifest(outputMap, options.outputManifest);
     } catch (Throwable e) {
       logger.log(Level.SEVERE, "Error parsing package strings", e);
       System.exit(1);
     }
     System.exit(0);
-  }
-
-  @Nonnull
-  private static Path getExecutionPath(@Nonnull ArtifactLocation location) {
-    return Paths.get(location.getRootExecutionPathFragment(), location.getRelativePath());
   }
 
   @VisibleForTesting
@@ -131,16 +131,13 @@ public class PackageParser {
   }
 
   @VisibleForTesting
-  public void writeManifest(
-      @Nonnull Map<ArtifactLocation, String> sourceToPackageMap,
-      Path outputFile)
+  public void writeManifest(@Nonnull Map<Path, String> sourceToPackageMap, Path outputFile)
       throws IOException {
     PackageManifest.Builder builder = PackageManifest.newBuilder();
-    for (Entry<ArtifactLocation, String> entry : sourceToPackageMap.entrySet()) {
-      JavaSourcePackage.Builder srcBuilder = JavaSourcePackage.newBuilder()
-          .setPackageString(entry.getValue())
-          .setArtifactLocation(entry.getKey());
-      builder.addSources(srcBuilder.build());
+    for (Entry<Path, String> entry : sourceToPackageMap.entrySet()) {
+      builder.addSources(JavaSourcePackage.newBuilder()
+          .setAbsolutePath(entry.getKey().toAbsolutePath().toString())
+          .setPackageString(entry.getValue()));
     }
 
     try {
@@ -153,23 +150,24 @@ public class PackageParser {
 
   @Nonnull
   @VisibleForTesting
-  public Map<ArtifactLocation, String> parsePackageStrings(@Nonnull List<ArtifactLocation> sources)
-      throws Exception {
+  public Map<Path, String> parsePackageStrings(@Nonnull List<Path> absolutePaths,
+      @Nonnull List<Path> executionPaths) throws Exception {
 
     ListeningExecutorService executorService = MoreExecutors.listeningDecorator(
         Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
 
-    Map<ArtifactLocation, ListenableFuture<String>> futures = Maps.newHashMap();
-    for (final ArtifactLocation source : sources) {
-      futures.put(source, executorService.submit(new Callable<String>() {
+    Map<Path, ListenableFuture<String>> futures = Maps.newHashMap();
+    for (int i = 0; i < absolutePaths.size(); i++) {
+      final Path source = executionPaths.get(i);
+      futures.put(absolutePaths.get(i), executorService.submit(new Callable<String>() {
         @Override
         public String call() throws Exception {
           return getDeclaredPackageOfJavaFile(source);
         }
       }));
     }
-    Map<ArtifactLocation, String> map = Maps.newHashMap();
-    for (Entry<ArtifactLocation, ListenableFuture<String>> entry : futures.entrySet()) {
+    Map<Path, String> map = Maps.newHashMap();
+    for (Entry<Path, ListenableFuture<String>> entry : futures.entrySet()) {
       String value = entry.getValue().get();
       if (value != null) {
         map.put(entry.getKey(), value);
@@ -179,8 +177,8 @@ public class PackageParser {
   }
 
   @Nullable
-  private String getDeclaredPackageOfJavaFile(@Nonnull ArtifactLocation source) {
-    try (BufferedReader reader = ioProvider.getReader(getExecutionPath(source))) {
+  private String getDeclaredPackageOfJavaFile(@Nonnull Path source) {
+    try (BufferedReader reader = ioProvider.getReader(source)) {
       return parseDeclaredPackage(reader);
 
     } catch (IOException e) {
@@ -194,7 +192,7 @@ public class PackageParser {
   public static String parseDeclaredPackage(@Nonnull BufferedReader reader) throws IOException {
     String line;
     while ((line = reader.readLine()) != null) {
-      Matcher packageMatch = PACKAGE_PATTERN.matcher(line);
+      Matcher packageMatch = JAVA_PACKAGE_PATTERN.matcher(line);
       if (packageMatch.find()) {
         return packageMatch.group(1);
       }
