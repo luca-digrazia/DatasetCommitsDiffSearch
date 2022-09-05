cd82b42f3963fe492bf6ec7f4352bb0fbbc75bbc
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:14:24 +0200

    [jvm] Params to include synch info in thread dump.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-jvm/src/main/java/com/codahale/metrics/jvm/ThreadDump.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-jvm/src/main/java/com/codahale/metrics/jvm/ThreadDump.java
@@ -7,13 +7,14 @@ import java.lang.management.LockInfo;
 import java.lang.management.MonitorInfo;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
-import java.nio.charset.Charset;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 
 /**
  * A convenience class for getting a thread dump.
  */
 public class ThreadDump {
-    private static final Charset UTF_8 = Charset.forName("UTF-8");
 
     private final ThreadMXBean threadMXBean;
 
@@ -22,32 +23,49 @@ public class ThreadDump {
     }
 
     /**
-     * Dumps all of the threads' current information to an output stream.
+     * Dumps all of the threads' current information, including synchronization, to an output stream.
      *
      * @param out an output stream
      */
     public void dump(OutputStream out) {
-        final ThreadInfo[] threads = this.threadMXBean.dumpAllThreads(true, true);
+        dump(true, true, out);
+    }
+
+    /**
+     * Dumps all of the threads' current information, optionally including synchronization, to an output stream.
+     *
+     * Having control over including synchronization info allows using this method (and its wrappers, i.e.
+     * ThreadDumpServlet) in environments where getting object monitor and/or ownable synchronizer usage is not
+     * supported. It can also speed things up.
+     *
+     * See {@link ThreadMXBean#dumpAllThreads(boolean, boolean)}
+     *
+     * @param lockedMonitors dump all locked monitors if true
+     * @param lockedSynchronizers dump all locked ownable synchronizers if true
+     * @param out an output stream
+     */
+    public void dump(boolean lockedMonitors, boolean lockedSynchronizers, OutputStream out) {
+        final ThreadInfo[] threads = this.threadMXBean.dumpAllThreads(lockedMonitors, lockedSynchronizers);
         final PrintWriter writer = new PrintWriter(new OutputStreamWriter(out, UTF_8));
 
         for (int ti = threads.length - 1; ti >= 0; ti--) {
             final ThreadInfo t = threads[ti];
             writer.printf("\"%s\" id=%d state=%s",
-                          t.getThreadName(),
-                          t.getThreadId(),
-                          t.getThreadState());
+                    t.getThreadName(),
+                    t.getThreadId(),
+                    t.getThreadState());
             final LockInfo lock = t.getLockInfo();
             if (lock != null && t.getThreadState() != Thread.State.BLOCKED) {
                 writer.printf("%n    - waiting on <0x%08x> (a %s)",
-                              lock.getIdentityHashCode(),
-                              lock.getClassName());
+                        lock.getIdentityHashCode(),
+                        lock.getClassName());
                 writer.printf("%n    - locked <0x%08x> (a %s)",
-                              lock.getIdentityHashCode(),
-                              lock.getClassName());
+                        lock.getIdentityHashCode(),
+                        lock.getClassName());
             } else if (lock != null && t.getThreadState() == Thread.State.BLOCKED) {
                 writer.printf("%n    - waiting to lock <0x%08x> (a %s)",
-                              lock.getIdentityHashCode(),
-                              lock.getClassName());
+                        lock.getIdentityHashCode(),
+                        lock.getClassName());
             }
 
             if (t.isSuspended()) {
