140442c37d3712efc17bdba77cca0e5d2a94faae
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:11:49 +0200

    Description redacted.     --     MOS_MIGRATED_REVID=92016074

--- dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/analysis/constraints/AbstractConstraintsTest.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/test/java/com/google/devtools/build/lib/analysis/constraints/AbstractConstraintsTest.java
@@ -73,7 +73,7 @@ public abstract class AbstractConstraintsTest extends BuildViewTestCase {
           .append(getAttrDef("environments", environments.toArray(new String[0])) + ",\n")
           .append(getAttrDef("defaults", defaults.toArray(new String[0])) + ",\n")
           .append(")");
-      scratch.file("" + name + "/BUILD", builder.toString());
+      scratchFile("" + name + "/BUILD", builder.toString());
     }
   }
 
