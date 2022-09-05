5bda190e5d4e2277618bee1922a0bbb8d625c878
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 06:43:49 +0200

    added support for adw launcher

--- dataset/GitHub_Java/leolin310148.ShortcutBadger/ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/AdwHomeBadger.java
+++ dataset/GitHub_Java/leolin310148.ShortcutBadger/ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/AdwHomeBadger.java
@@ -1,44 +1,30 @@
 package me.leolin.shortcutbadger.impl;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
-import me.leolin.shortcutbadger.Badger;
 import me.leolin.shortcutbadger.ShortcutBadgeException;
 import me.leolin.shortcutbadger.ShortcutBadger;
-import me.leolin.shortcutbadger.util.BroadcastHelper;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author Gernot Pansy
  */
-public class AdwHomeBadger implements Badger {
+public class AdwHomeBadger extends ShortcutBadger {
 
     public static final String INTENT_UPDATE_COUNTER = "org.adw.launcher.counter.SEND";
     public static final String PACKAGENAME = "PNAME";
     public static final String COUNT = "COUNT";
 
-    @Override
-    public void executeBadge(Context context, ComponentName componentName, int badgeCount) throws ShortcutBadgeException {
-
-        Intent intent = new Intent(INTENT_UPDATE_COUNTER);
-        intent.putExtra(PACKAGENAME, componentName.getPackageName());
-        intent.putExtra(COUNT, badgeCount);
-        if(BroadcastHelper.canResolveBroadcast(context, intent)) {
-            context.sendBroadcast(intent);
-        } else {
-            throw new ShortcutBadgeException("unable to resolve intent: " + intent.toString());
-        }
+    public AdwHomeBadger(Context context) {
+        super(context);
     }
 
     @Override
-    public List<String> getSupportLaunchers() {
-        return Arrays.asList(
-                "org.adw.launcher",
-                "org.adwfreak.launcher"
-        );
+    protected void executeBadge(int badgeCount) throws ShortcutBadgeException {
+
+        Intent intent = new Intent(INTENT_UPDATE_COUNTER);
+        intent.putExtra(PACKAGENAME, getContextPackageName());
+        intent.putExtra(COUNT, badgeCount);
+        mContext.sendBroadcast(intent);
     }
 }
