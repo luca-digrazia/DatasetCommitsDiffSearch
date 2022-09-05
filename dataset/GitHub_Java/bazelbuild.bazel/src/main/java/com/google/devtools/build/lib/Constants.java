e165a051d001146c1f51a500c254d84654216aba
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 04:22:24 +0200

    Remove the ALLOW_CC_INCLUDE_SCANNING constant since it is not being used anymore.
        --
        MOS_MIGRATED_REVID=113512761

--- dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/Constants.java
+++ dataset/GitHub_Java/bazelbuild.bazel/src/main/java/com/google/devtools/build/lib/Constants.java
@@ -21,6 +21,9 @@ import com.google.common.collect.ImmutableSet;
  * A temporary class of constants; these encode differences between Google's internal setup and
  * Bazel. We're working to remove this class, which requires cleaning up our internal code base.
  * Please don't add anything here unless you know what you're doing.
+ *
+ * <p>The extra {@code .toString()} calls are there so that javac doesn't inline these constants
+ * so that we can replace this class file within a binary.
  */
 public final class Constants {
   private Constants() {}
@@ -31,16 +34,20 @@ public final class Constants {
   // Default value for the --package_path flag if not otherwise set.
   public static final ImmutableList<String> DEFAULT_PACKAGE_PATH = ImmutableList.of("%workspace%");
 
+  // The rule documentation generator iterates over all rules in the class by this name.
+  public static final String MAIN_RULE_CLASS_PROVIDER =
+      "com.google.devtools.build.lib.bazel.rules.BazelRuleClassProvider";
+
   // Native Java deps are all linked into a single file, which is named with this value + ".so".
   public static final String NATIVE_DEPS_LIB_SUFFIX = "_nativedeps";
 
-  // Locations of implicit Android SDK dependencies.
-  public static final String ANDROID_DEFAULT_SDK = "//external:android/sdk";
-
+  // Locations of implicit Android SDK and NDK dependencies.
+  public static final String ANDROID_DEFAULT_SDK = "//external:android/sdk".toString();
+  public static final String ANDROID_DEFAULT_CROSSTOOL = "//external:android/crosstool".toString();
   // If the --fat_apk_cpu flag is not set, we use this as the default value.
   public static final ImmutableList<String> ANDROID_DEFAULT_FAT_APK_CPUS =
       ImmutableList.<String>of("armeabi-v7a");
 
   // Most other tools dependencies use this; we plan to split it into per-language repositories.
-  public static final String TOOLS_REPOSITORY = "@bazel_tools";
+  public static final String TOOLS_REPOSITORY = "@bazel_tools".toString();
 }
