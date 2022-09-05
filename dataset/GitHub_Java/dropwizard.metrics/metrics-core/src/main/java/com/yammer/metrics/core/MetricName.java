78d382770476a9ee8d1bd0d08ee064ff51a99416
Luca Di Grazia <luca.digrazia94@gmail.com>
2022-09-05 08:41:05 +0200

    Another small whitespace issue.

--- dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/core/MetricName.java
+++ dataset/GitHub_Java/dropwizard.metrics/metrics-core/src/main/java/com/yammer/metrics/core/MetricName.java
@@ -1,20 +1,35 @@
 package com.yammer.metrics.core;
 
+
 /**
  * A value class encapsulating a metric's owning class and name.
  */
-public class MetricName {
-    private final Class<?> klass;
-    private final String name, scope;
+public class MetricName implements Comparable<MetricName> {
+    private final String group;
+    private final String type;
+    private final String name;
+    private final String scope;
+    private final String mBeanName;
 
     /**
      * Creates a new {@link MetricName} without a scope.
      *
      * @param klass the {@link Class} to which the {@link Metric} belongs
-     * @param name the name of the {@link Metric}
+     * @param name  the name of the {@link Metric}
      */
     public MetricName(Class<?> klass, String name) {
         this(klass, name, null);
+    }
+
+    /**
+     * Creates a new {@link MetricName} without a scope.
+     *
+     * @param group the group to which the {@link Metric} belongs
+     * @param type  the type to which the {@link Metric} belongs
+     * @param name  the name of the {@link Metric}
+     */
+    public MetricName(String group, String type, String name) {
+        this(group, type, name, null);
     }
 
     /**
@@ -25,18 +40,66 @@ public class MetricName {
      * @param scope the scope of the {@link Metric}
      */
     public MetricName(Class<?> klass, String name, String scope) {
-        this.klass = klass;
-        this.name = name;
-        this.scope = scope;
+        this(klass.getPackage() == null ? "" : klass.getPackage().getName(),
+             klass.getSimpleName().replaceAll("\\$$", ""),
+             name,
+             scope);
     }
 
     /**
-     * Returns the {@link Class} to which the {@link Metric} belongs.
+     * Creates a new {@link MetricName} without a scope.
      *
-     * @return the {@link Class} to which the {@link Metric} belongs
+     * @param group the group to which the {@link Metric} belongs
+     * @param type  the type to which the {@link Metric} belongs
+     * @param name  the name of the {@link Metric}
+     * @param scope the scope of the {@link Metric}
      */
-    public Class<?> getKlass() {
-        return klass;
+    public MetricName(String group, String type, String name, String scope) {
+        this(group, type, name, scope, createMBeanName(group, type, name, scope));
+    }
+
+    /**
+     * Creates a new {@link MetricName} without a scope.
+     *
+     * @param group     the group to which the {@link Metric} belongs
+     * @param type      the type to which the {@link Metric} belongs
+     * @param name      the name of the {@link Metric}
+     * @param scope     the scope of the {@link Metric}
+     * @param mBeanName the 'ObjectName', represented as a string, to use when registering the
+     *                  MBean.
+     */
+    public MetricName(String group, String type, String name, String scope, String mBeanName) {
+        if (group == null || type == null) {
+            throw new IllegalArgumentException("Both group and type need to be specified");
+        }
+        if (name == null) {
+            throw new IllegalArgumentException("Name needs to be specified");
+        }
+        this.group = group;
+        this.type = type;
+        this.name = name;
+        this.scope = scope;
+        this.mBeanName = mBeanName;
+    }
+
+    /**
+     * Returns the group to which the {@link Metric} belongs. For class-based metrics, this will be
+     * the package name of the {@link Class} to which the {@link Metric} belongs.
+     *
+     * @return the group to which the {@link Metric} belongs
+     */
+    public String getGroup() {
+        return group;
+    }
+
+    /**
+     * Returns the type to which the {@link Metric} belongs. For class-based metrics, this will be
+     * the simple class name of the {@link Class} to which the {@link Metric} belongs.
+     *
+     * @return the type to which the {@link Metric} belongs
+     */
+    public String getType() {
+        return type;
     }
 
     /**
@@ -58,8 +121,7 @@ public class MetricName {
     }
 
     /**
-     * Returns {@code true} if the {@link Metric} has a scope, {@code false}
-     * otherwise.
+     * Returns {@code true} if the {@link Metric} has a scope, {@code false} otherwise.
      *
      * @return {@code true} if the {@link Metric} has a scope
      */
@@ -67,24 +129,51 @@ public class MetricName {
         return scope != null;
     }
 
+    /**
+     * Returns the MBean name for the {@link Metric} identified by this metric name.
+     *
+     * @return the MBean name
+     */
+    public String getMBeanName() {
+        return mBeanName;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        MetricName that = (MetricName) o;
-
-        return !(klass != null ? !klass.equals(that.klass) : that.klass != null)
-                && !(name != null ? !name.equals(that.name) : that.name != null)
-                && !(scope != null ? !scope.equals(that.scope) : that.scope != null);
-
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
+        final MetricName that = (MetricName) o;
+        return mBeanName.equals(that.mBeanName);
     }
 
     @Override
     public int hashCode() {
-        int result = klass != null ? klass.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (scope != null ? scope.hashCode() : 0);
-        return result;
+        return mBeanName.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return mBeanName;
+    }
+
+    @Override
+    public int compareTo(MetricName o) {
+        return mBeanName.compareTo(o.mBeanName);
+    }
+
+    private static String createMBeanName(String group, String type, String name, String scope) {
+        final StringBuilder nameBuilder = new StringBuilder();
+        nameBuilder.append(group);
+        nameBuilder.append(":type=");
+        nameBuilder.append(type);
+        if (scope != null) {
+            nameBuilder.append(",scope=");
+            nameBuilder.append(scope);
+        }
+        if (name.length() > 0) {
+            nameBuilder.append(",name=");
+            nameBuilder.append(name);
+        }
+        return nameBuilder.toString();
     }
 }
